f6a2b87a6a61e9f0bb11e5cbaaaa2216b0965759
zengzm <zengzm@lenovo.com>
2016-05-11 16:32:53 +0800

    [k52m][teei]teei采用软链接(CR)
    Change-Id: I83c7355a54da2b07dee5b3c5599fd64c9f4cb4f2

--- drivers/misc/mediatek/teei/V1.0/ut_keymaster/mt_keymaster_service.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MT_KEYMASTER_SERVICE_H__
-#define __MT_KEYMASTER_SERVICE_H__
-
-extern unsigned long mt_keymaster_message_buf;
-extern unsigned long mt_keymaster_fmessage_buf;
-extern unsigned long mt_keymaster_bmessage_buf;
-
-long create_keymaster_cmd_buf(void);
-#endif
