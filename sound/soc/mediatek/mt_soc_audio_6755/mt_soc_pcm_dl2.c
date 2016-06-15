97250c46f089da6d8e0be91e1f9b27ce4d603e5a
chengx2 <chengx2@lenovo.com>
2016-06-15 11:52:14 +0800

    Add connection to smartpa for DL2 DAI link
    Change-Id: Ie41ce228c16061067e0251d6480067d084da5d18
    Reviewed-on: http://sc.mt.com:8080/404085
    Reviewed-by: 廖沪俊 <liaohj@mt.com>
    Tested-by: 廖沪俊 <liaohj@mt.com>

--- sound/soc/mediatek/mt_soc_audio_6755/mt_soc_pcm_dl2.c
+++ sound/soc/mediatek/mt_soc_audio_6755/mt_soc_pcm_dl2.c
@@ -364,7 +364,10 @@ static int mtk_pcm_dl2_start(struct snd_pcm_substream *substream)
 		      Soc_Aud_InterConnectionOutput_O03);
 	SetConnection(Soc_Aud_InterCon_Connection, Soc_Aud_InterConnectionInput_I08,
 		      Soc_Aud_InterConnectionOutput_O04);
-
+	SetConnection(Soc_Aud_InterCon_Connection, Soc_Aud_InterConnectionInput_I07,
+		      Soc_Aud_InterConnectionOutput_O00); 
+	SetConnection(Soc_Aud_InterCon_Connection, Soc_Aud_InterConnectionInput_I08,
+		      Soc_Aud_InterConnectionOutput_O01);
 #ifdef CONFIG_MTK_FPGA
 	/* set loopback test interconnection */
 	SetConnection(Soc_Aud_InterCon_Connection, Soc_Aud_InterConnectionInput_I07,
