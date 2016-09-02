7f856d2bbcb3bfded81a874c36ae2bba1d0d8153
caoyi1 <caoyi1@lenovo.com>
2016-09-02 11:12:13 +0800

    【Kungfu】 mmc36xx Msensor bring up (CR)
    Change-Id: I0a7ec664f702812af4cb4370b059140bf93fd906
    Reviewed-on: http://sc.mt.com:8080/416568
    Reviewed-by: 廖沪俊 <liaohj@mt.com>
    Tested-by: 廖沪俊 <liaohj@mt.com>
    Reviewed-on: http://sc.mt.com:8080/416691
    Reviewed-by: 马建民 <majm1@mt.com>
    Tested-by: 马建民 <majm1@mt.com>

--- drivers/misc/mediatek/magnetometer/mmc3530/mmc3524x.h
+++ drivers/misc/mediatek/magnetometer/mmc3530/mmc3524x.h
@@ -1,43 +1,72 @@
 /*
- * Definitions for mmc3524x magnetic sensor chip.
+ * Definitions for MMC3524X magnetic sensor chip.
  */
-#ifndef __MMC3524x_H__
-#define __MMC3524x_H__
+#ifndef __MMC3524X_H__
+#define __MMC3524X_H__
 
 #include <linux/ioctl.h>
 
-#define MMC3524x_I2C_NAME		"mmc3524x"
+#define MMC3524x_I2C_NAME		"memsic_mag"
 #define CALIBRATION_DATA_SIZE	12
 #define SENSOR_DATA_SIZE 9
 #ifndef TRUE
 	#define TRUE 1
 #endif
-#define MMC3524x_I2C_ADDR		0x60	// 8-bit    7-bit  0x30
+#define MMC3524X_I2C_ADDR		0x30	// 8-bit    7-bit  0x30
 
-/* MMC3524x register address */
-#define MMC3524X_REG_CTRL		0x07
-#define MMC3524X_REG_BITS		0x08
-#define MMC3524X_REG_DATA		0x00
-#define MMC3524X_REG_DS			0x06
-#define MMC3524X_REG_PRODUCTID_0		0x10
-#define MMC3524X_REG_PRODUCTID_1		0x20
+/* MMC3524X register address */
+#define MMC3524X_REG_CTRL		         0x07
+#define MMC3524X_REG_BITS		         0x08
+#define MMC3524X_REG_DATA		         0x00
+#define MMC3524X_REG_DS			         0x06
+#define MMC3524X_REG_PRODUCTID_0		 0x10
+#define MMC3524X_CHIPID_ADDR             0x20
+#define MMC3524X_MAG_REG_ADDR_OTP        0x1b
 
-/* MMC3524x control bit */
+/*Mmc36xx register address*/
+#define MMC36XX_I2C_ADDR                 0x30
+#define MEMSIC_REG_DATA                  0x00
+#define MMC36XX_REG_CTRL0                0x08
+#define MMC36XX_REG_CTRL1                0x09
+#define MMC36XX_REG_CTRL2                0x0A
+#define MMC36XX_CHIPID_ADDR              0x2f
+#define MMC36XX_MAG_REG_ADDR_OTP_GATE0   0x0F
+#define MMC36XX_MAG_REG_ADDR_OTP_GATE1   0x12
+#define MMC36XX_MAG_REG_ADDR_OTP_GATE2   0x13
+#define MMC36XX_MAG_REG_ADDR_OTP         0x2A
+
+/*mmc3630x control bit value*/
+#define MMC3630X_CTRL_TM                 0x01
+#define MMC3630X_CTRL_NOBOOST            0x10
+#define MMC3630X_CTRL_SET                0x08
+#define MMC3630X_CTRL_RESET              0x10
+#define MMC3630X_CTRL_REFILL             0x20
+
+/*MEMSIC MMC36XX  Command to make OTP ready*/
+#define MMC36XX_MAG_OTP_OPEN0                   0xE1
+#define MMC36XX_MAG_OTP_OPEN1                   0x11
+#define MMC36XX_MAG_OTP_OPEN2                   0x80
+
+#define MEMSIC_SENSOR_MMC3630X           0xA
+#define MEMSIC_SENSOR_MMC3530X           0x9
+#define MEMSIC_SENSOR_MMC3524X           0x8
+
+/* MMC3524X control bit */
 #define MMC3524X_CTRL_TM			0x01
 #define MMC3524X_CTRL_CM			0x02
-#define MMC3524X_CTRL_50HZ		0x00
-#define MMC3524X_CTRL_25HZ		0x04
-#define MMC3524X_CTRL_12HZ		0x08
-#define MMC3524X_CTRL_NOBOOST            0x10
+#define MMC3524X_CTRL_50HZ		    0x00
+#define MMC3524X_CTRL_25HZ		    0x04
+#define MMC3524X_CTRL_12HZ		    0x08
+#define MMC3524X_CTRL_NOBOOST       0x10
 #define MMC3524X_CTRL_SET  	        0x20
-#define MMC3524X_CTRL_RESET              0x40
-#define MMC3524X_CTRL_REFILL             0x80
+#define MMC3524X_CTRL_RESET         0x40
+#define MMC3524X_CTRL_REFILL        0x80
 
 #define MMC3524X_BITS_SLOW_16            0x00
 #define MMC3524X_BITS_FAST_16            0x01
 #define MMC3524X_BITS_14                 0x02
 
-// conversion of magnetic data (for mmc3524x) to uT units
+// conversion of magnetic data (for MMC3524X) to uT units
 // conversion of magnetic data to uT units
 // 32768 = 1Guass = 100 uT
 // 100 / 32768 = 25 / 8192
@@ -59,18 +88,5 @@
 #define MMC3524X_SENSITIVITY_Y		1024
 #define MMC3524X_SENSITIVITY_Z		1024
 
-
-
-#define MMC3524X_MODE_SNG_MEASURE	0x01
-#define MMC3524X_MODE_SELF_TEST		0x10
-#define MMC3524X_MODE_FUSE_ACCESS	0x1F
-#define MMC3524X_MODE_POWERDOWN		0x00
-#define MMC3524X_RESET_DATA			0x01
-#define COMPAT_MMC3524X_IOC_READ_REG           _IOWR(MSENSOR, 0x32, unsigned char)
-#define COMPAT_MMC3524X_IOC_WRITE_REG          _IOW(MSENSOR, 0x33, unsigned char[2])
-#define COMPAT_MMC3524X_IOC_READ_REGS          _IOWR(MSENSOR, 0x34, unsigned char[10])
-#define MMC3524X_IOC_READ_REG		    _IOWR(MSENSOR, 0x23, unsigned char)
-#define MMC3524X_IOC_WRITE_REG		    _IOW(MSENSOR,  0x24, unsigned char[2])
-#define MMC3524X_IOC_READ_REGS		    _IOWR(MSENSOR, 0x25, unsigned char[10])
-#endif /* __MMC3524x_H__ */
+#endif /* __MMC3524X_H__ */
 
