7a4771a12a6ed232f31633bd8d3678d75043db7e
yangqh5 <yangqh5@lenovo.com>
2016-11-18 17:03:53 +0800

    [MTK Patch][(CR)]For_lenovo6755_66_n_alps-mp-n0.mp7-V1_P9
    Patch Type:
      Customer Request
    CR ID:
      ALPS03023414
    Severity:
    
    Description:
      [Patch Request] [PMS] LENOVO, Project: LENOVO6755_66_N, SW Version: alps-mp-n0.mp7-V1N/A
    
    Associated Files:
      device/lenovo/lenovo6755_66_n/ProjectConfig.mk
    
    Patch Type:
      Customer Request
    CR ID:
      ALPS02923245
    Severity:
      Critical
    Description:
      [Google Security P***h][(CR)-6674]Elevation of privilege vulnerability in sys***_server (device specific)
      [[問題標題]]
      [Google Security P***h][(CR)-6674]Elevation of privilege vulnerability in sys***_server (device specific)
      [[問題現象]]
      [Google Security P***h][(CR)-6674]Elevation of privilege vulnerability in sys***_server (device specific)
      [[問題影響]]
      N/A
      [[問題復現步驟]]
      N/A
      [[建議驗證模塊]]
      N/A
    
      Notice: Following i***o***ation added by you will be provided to customer dire***ly. Please input in English.
      [[Title for Customer]]
      [Google Security P***h][(CR)-6674]Elevation of privilege vulnerability in sys***_server (device specific)
      [[Problem Description]]
      [Google Security P***h][(CR)-6674]Elevation of privilege vulnerability in sys***_server (device specific)
      [[Impa***]]
      N/A
      [[Reproduced Steps]]
      N/A
      [[Modules to be verified after taking p***h]]
      N/AN/A
    
    Associated Files:
      (CR).18/drivers/staging/android/binder.c
    
    Patch Type:
      Customer Request
    CR ID:
      ALPS02923253
    Severity:
      Critical
    Description:
      [Google Security P***h][(CR)-8955]Elevation of privilege vulnerability in kernel perfo******e subsys*** (device specific)
      [[問題標題]]
      [Google Security P***h][(CR)-8955]Elevation of privilege vulnerability in kernel perfo******e subsys*** (device specific)
      [[問題現象]]
      perf core在處理多個HW PMU ***t的時候可能出錯
      需要增加一些判斷來避免問題
      [[問題影響]]
      N/A
      [[問題復現步驟]]
      N/A
      [[建議驗證模塊]]
      N/A
    
      Notice: Following i***o***ation added by you will be provided to customer dire***ly. Please input in English.
      [[Title for Customer]]
      [Google Security P***h][(CR)-8955]Elevation of privilege vulnerability in kernel perfo******e subsys*** (device specific)
      [[Problem Description]]
      The perf core implicitly reje***s ***ts spanning multiple HW PMUs, as ***hese cases the ***t->***x will differ. However this validation is perfo***ed after pmu::***t_init() is called in perf_init_***t(), and
      thus pmu::***t_init() may be called with a group leader from a different HW PMU.
    
      The A***64 PMU driver does not take this fa*** ***o account, and when validating groups assumes that it c***all to_a***_pmu(***t->pmu) f***ny HW ***t. When the ***t in question is from another HW PMU this is wrong, and results in dereferencing garbage.
      [[Impa***]]
      N/A
      [[Reproduced Steps]]
      N/A
      [[Modules to be verified after taking p***h]]
      N/AN/A
    
    Associated Files:
      (CR).18/arch/arm64/kernel/perf_event.c
    
    Change-Id: I339bda75685d10d2b90c2451f4c1a2adeb53e8ca
    Reviewed-on: http://sc.lenovo.com:8080/423071

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -1918,7 +1918,8 @@ static int binder_dec_node(struct binder_node *node, int strong, int internal)
 	return 0;
 }
 
-static struct binder_ref *binder_get_ref(struct binder_proc *proc, uint32_t desc)
+static struct binder_ref *binder_get_ref(struct binder_proc *proc,
+					uint32_t desc, bool need_strong_ref)
 {
 	struct rb_node *n = proc->refs_by_desc.rb_node;
 	struct binder_ref *ref;
@@ -1926,12 +1927,16 @@ static struct binder_ref *binder_get_ref(struct binder_proc *proc, uint32_t desc
 	while (n) {
 		ref = rb_entry(n, struct binder_ref, rb_node_desc);
 
-		if (desc < ref->desc)
+		if (desc < ref->desc) {
 			n = n->rb_left;
-		else if (desc > ref->desc)
+		} else if (desc > ref->desc) {
 			n = n->rb_right;
-		else
+		} else if (need_strong_ref && !ref->strong) {
+			binder_user_error("tried to use weak ref as strong ref\n");
+			return NULL;
+		} else {
 			return ref;
+		}
 	}
 	return NULL;
 }
@@ -2195,7 +2200,8 @@ static void binder_transaction_buffer_release(struct binder_proc *proc,
 			break;
 		case BINDER_TYPE_HANDLE:
 		case BINDER_TYPE_WEAK_HANDLE:{
-				struct binder_ref *ref = binder_get_ref(proc, fp->handle);
+				struct binder_ref *ref = binder_get_ref(proc, fp->handle,
+							fp->type == BINDER_TYPE_HANDLE);
 
 				if (ref == NULL) {
 					pr_err
@@ -2445,7 +2451,7 @@ static void binder_transaction(struct binder_proc *proc,
 		if (tr->target.handle) {
 			struct binder_ref *ref;
 
-			ref = binder_get_ref(proc, tr->target.handle);
+			ref = binder_get_ref(proc, tr->target.handle, true);
 			if (ref == NULL) {
 				binder_user_error
 				    ("%d:%d got transaction to invalid handle\n",
@@ -2703,7 +2709,9 @@ static void binder_transaction(struct binder_proc *proc,
 					fp->type = BINDER_TYPE_HANDLE;
 				else
 					fp->type = BINDER_TYPE_WEAK_HANDLE;
+				fp->binder = 0;
 				fp->handle = ref->desc;
+				fp->cookie = 0;
 				binder_inc_ref(ref, fp->type == BINDER_TYPE_HANDLE, &thread->todo);
 
 				trace_binder_transaction_node_to_ref(t, node, ref);
@@ -2715,7 +2723,8 @@ static void binder_transaction(struct binder_proc *proc,
 			break;
 		case BINDER_TYPE_HANDLE:
 		case BINDER_TYPE_WEAK_HANDLE:{
-				struct binder_ref *ref = binder_get_ref(proc, fp->handle);
+				struct binder_ref *ref = binder_get_ref(proc, fp->handle,
+							fp->type == BINDER_TYPE_HANDLE);
 
 				if (ref == NULL) {
 					binder_user_error
@@ -2755,7 +2764,9 @@ static void binder_transaction(struct binder_proc *proc,
 						return_error = BR_FAILED_REPLY;
 						goto err_binder_get_ref_for_node_failed;
 					}
+					fp->binder = 0;
 					fp->handle = new_ref->desc;
+					fp->cookie = 0;
 					binder_inc_ref(new_ref,
 						       fp->type == BINDER_TYPE_HANDLE, NULL);
 					trace_binder_transaction_ref_to_ref(t, ref, new_ref);
@@ -2822,6 +2833,7 @@ static void binder_transaction(struct binder_proc *proc,
 				binder_debug(BINDER_DEBUG_TRANSACTION,
 					     "        fd %d -> %d\n", fp->handle, target_fd);
 				/* TODO: fput? */
+				fp->binder = 0;
 				fp->handle = target_fd;
 #ifdef BINDER_MONITOR
 				e->fd = target_fd;
@@ -3012,7 +3024,9 @@ static int binder_thread_write(struct binder_proc *proc,
 					ref->desc);
 				}
 			} else
-				ref = binder_get_ref(proc, target);
+				ref = binder_get_ref(proc, target,
+						cmd == BC_ACQUIRE ||
+						cmd == BC_RELEASE);
 			if (ref == NULL) {
 				binder_user_error("%d:%d refcount change on invalid ref %d\n",
 					proc->pid, thread->pid, target);
@@ -3217,7 +3231,7 @@ static int binder_thread_write(struct binder_proc *proc,
 			if (get_user(cookie, (binder_uintptr_t __user *) ptr))
 				return -EFAULT;
 			ptr += sizeof(binder_uintptr_t);
-			ref = binder_get_ref(proc, target);
+			ref = binder_get_ref(proc, target, false);
 			if (ref == NULL) {
 				binder_user_error("%d:%d %s invalid ref %d\n",
 					proc->pid, thread->pid,
