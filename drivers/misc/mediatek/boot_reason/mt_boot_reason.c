8590e7e054e76075fd7202621ef4f47e29e05e87
shaohui <shaohui@longcheer.net>
2016-12-09 16:37:08 +0800

    [Purp][nicklaus][bootinfo] Add bootinfo for lenovo
    [Modify]
    1. Add CONFIG_LCT_BOOTINFO_SUPPORT,and set yes
    
    [mark]
    Maybe some boot reasons should be adjusted again.
    
    Change-Id: I312b9cb346447ccb79cd1398e914e4ead59ead2c

--- drivers/misc/mediatek/boot_reason/mt_boot_reason.c
+++ drivers/misc/mediatek/boot_reason/mt_boot_reason.c
@@ -49,6 +49,84 @@ static atomic_t g_br_state = ATOMIC_INIT(BOOT_REASON_UNINIT);
 static atomic_t g_br_errcnt = ATOMIC_INIT(0);
 static atomic_t g_br_status = ATOMIC_INIT(0);
 
+
+#ifdef CONFIG_LCT_BOOTINFO_SUPPORT
+/*	Lenovo(MOTO) boot reason list	*/
+#define TIME_OF_DAY_ALARM	0x00000008
+#define USB_CABLE			0x00000010
+#define FACTORY_CABLE		0x00000020
+#define PWR_KEY_PRESS		0x00000080
+#define CHARGE				0x00000100
+#define POWER_CUT			0x00000200
+#define SYSTEM_RESTART		0x00000800
+#define SW_AP_RESET			0x00004000
+#define WDOG_AP_RESET		0x00008000
+#define AP_KERNEL_PANIC		0x00020000
+#define HW_RESET			0x00100000
+#define PMIC_WD				0x00400000
+#define INVALID_TURN_ON		0x00000000
+/*
+typedef enum {
+    BR_POWER_KEY = 0,
+    BR_USB,
+    BR_RTC,
+    BR_WDT,
+    BR_WDT_BY_PASS_PWK,
+    BR_TOOL_BY_PASS_PWK,
+    BR_2SEC_REBOOT,
+    BR_UNKNOWN,
+    BR_KERNEL_PANIC,
+    BR_WDT_SW,
+    BR_WDT_HW
+	//shaohui add ,feature to be added
+	,BR_CHR,
+	BR_PWR_RST
+} boot_reason_t;
+*/
+static int bootinfo_map[]={
+	/* Lenovo(MOTO) */		/*   Mediatek	*/
+	PWR_KEY_PRESS,			/*BR_POWER_KEY*/
+	USB_CABLE,				/*BR_USB*/
+	TIME_OF_DAY_ALARM,		/*BR_RTC*/
+	WDOG_AP_RESET,			/*BR_WDT*/
+	SW_AP_RESET,			/*BR_WDT_BY_PASS_PWK*/
+	SW_AP_RESET,			/*BR_TOOL_BY_PASS_PWK*/
+	POWER_CUT,				/*BR_2SEC_REBOOT*/
+	INVALID_TURN_ON,		/*BR_UNKNOWN*/
+	AP_KERNEL_PANIC,		/*BR_KERNEL_PANIC*/
+	WDOG_AP_RESET,			/*BR_WDT_SW*/
+	HW_RESET,				/*BR_WDT_HW*/
+	CHARGE,					/*BR_CHR*/
+	HW_RESET				/*BR_PWR_RST*/
+};
+
+static int proc_bootinfo_show(struct seq_file *m, void *v)
+{
+	char ret[16];
+	int bootnum;
+	if (BOOT_REASON_INITIALIZED != atomic_read(&g_br_state))
+		return 0;
+	bootnum=(int)get_boot_reason();
+	printk("boot reason num:%d\n",bootnum);
+	snprintf(ret,16,"0x%08x\n",bootinfo_map[bootnum]);
+	seq_puts(m, ret);
+
+	return 0;
+}
+
+static int proc_bootinfo_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, proc_bootinfo_show, NULL);
+}
+
+static const struct file_operations bootinfo_proc_fops = {
+	.open = proc_bootinfo_open,
+	.read = seq_read,
+};
+
+#endif
+
+
 #ifdef CONFIG_OF
 static int __init dt_get_boot_reason(unsigned long node, const char *uname, int depth, void *data)
 {
@@ -62,6 +140,10 @@ static int __init dt_get_boot_reason(unsigned long node, const char *uname, int 
 		br_ptr = strstr(ptr, "boot_reason=");
 		if (br_ptr != 0) {
 			g_boot_reason = br_ptr[12] - '0';	/* get boot reason */
+#ifdef CONFIG_LCT_BOOTINFO_SUPPORT
+			if(br_ptr[13]>='0' && br_ptr[13]<='9')
+				g_boot_reason = g_boot_reason*10 + (br_ptr[13] - '0');
+#endif
 			atomic_set(&g_br_status, 1);
 		} else {
 			pr_warn("'boot_reason=' is not found\n");
@@ -125,6 +207,9 @@ static int __init boot_reason_init(void)
 {
 	pr_debug("boot_reason = %d, state(%d,%d,%d)", g_boot_reason,
 		 atomic_read(&g_br_state), atomic_read(&g_br_errcnt), atomic_read(&g_br_status));
+#ifdef CONFIG_LCT_BOOTINFO_SUPPORT
+	proc_create("bootinfo", S_IRUGO | S_IWUSR, NULL, &bootinfo_proc_fops);
+#endif
 	return 0;
 }
 
