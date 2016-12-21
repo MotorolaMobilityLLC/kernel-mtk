2433281330122208f28c29f2dccabf5c58074d8c
yangqh5 <yangqh5@lenovo.com>
2016-12-21 16:10:30 +0800

    [MTK Patch][(CR)]For_lenovo6755_66_n_alps-mp-n0.mp7-V1_P34
    Patch Type:
      Customer Request
    CR ID:
      ALPS03084186
    Severity:
    
    Description:
      [Patch Request] [PMS] LENOVO, Project: LENOVO6755_66_N, SW Version: alps-mp-n0.mp7-V1N/A
    
    Associated Files:
      device/lenovo/lenovo6755_66_n/ProjectConfig.mk
    
    Patch Type:
      Customer Request
    CR ID:
      ALPS02973195
    Severity:
      Critical
    Description:
      [External Feedback][KE]Security Vulnerability - Kernel Arbitrary Code Executio***ro***r space in Mediatek /dev/hwmsensor driver
      [[問題標題]]
      hwmsensor设备的未进行参数检查，可导致数组溢出(可导致KE)
    
      [[問題現象]]
      hwmsensor设备可能数组溢出
    
      [[問題影響]]
      可导致数组溢出(可能会导致KE)
    
      [[問題復現步驟]]
      使用超出数组下标的参数，对hwmsensor设备恶意攻击。
    
      [[建議驗證模塊]]
      hwmsensor
    
      [[注意: 請勿刪除此行]]
    
      Notice: Following i***o***ation added by you will be provided to customer dire***ly. Please input in English.
      [[Title for Customer]]
      hwmsensor driver didn't do parameters calibration, so the array index is overflow(cause the KE).
    
      [[Problem Description]]
      hwmsensor driver array index is overflow.
    
      [[Impa***]]
      The array index is overflow(May cause the KE).
    
      [[Reproduced Steps]]
      Use the index which is out of range to atta***he hwmsensor.
    
      [[Modules to be verified after taking p***h]]
      hwmsensor
    
      [[Notice: Please don't dele***his line]]N/A
    
    Associated Files:
      (CR).18/drivers/misc/mediatek/hwmon/hwmsen/hwmsen_dev.c
    
    Change-Id: I0655d4ed8fe63c8da5ae3f27bd818241ea423378
    Reviewed-on: http://sc.lenovo.com:8080/425777

--- drivers/misc/mediatek/hwmon/hwmsen/hwmsen_dev.c
+++ drivers/misc/mediatek/hwmon/hwmsen/hwmsen_dev.c
@@ -593,6 +593,10 @@ static int hwmsen_enable(struct hwmdev_object *obj, int sensor, int enable)
 	uint64_t sensor_type;
 
 	sensor_type = 1LL << sensor;
+	if (sensor > MAX_ANDROID_SENSOR_NUM || sensor < 0) {
+		HWM_ERR("handle %d!\n", sensor);
+		return -EINVAL;
+	}
 
 	if (!obj) {
 		HWM_ERR("hwmdev obj pointer is NULL!\n");
@@ -711,6 +715,10 @@ static int hwmsen_enable_nodata(struct hwmdev_object *obj, int sensor, int enabl
 
 	HWM_FUN(f);
 	sensor_type = 1LL << sensor;
+	if (sensor > MAX_ANDROID_SENSOR_NUM || sensor < 0) {
+		HWM_ERR("handle %d!\n", sensor);
+		return -EINVAL;
+	}
 
 	if (NULL == obj) {
 		HWM_ERR("hwmdev obj pointer is NULL!\n");
@@ -770,7 +778,7 @@ static int hwmsen_set_delay(int delay, int handle)
 	int err = 0;
 	struct hwmsen_context *cxt = NULL;
 
-	if (handle > MAX_ANDROID_SENSOR_NUM) {
+	if (handle > MAX_ANDROID_SENSOR_NUM || handle < 0) {
 		HWM_ERR("handle %d!\n", handle);
 		return -EINVAL;
 	}
