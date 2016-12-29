b5a28daf917c2e38668b5cacc85e52188a6c39df
huyunge <huyunge@wind-mobi.com>
2016-12-29 20:22:41 +0800

    [Purp][woods][modem] One image add new modem [Modi] 	Add RoAp Band28 and LATAM_SKY77638 [Mark]none 	modified:   drivers/misc/mediatek/ccci_util/ccci_util_lib_load_img.c
    Change-Id: Iab09bd6d4de1b0c33e8d76945e32138c12e20056

--- drivers/misc/mediatek/ccci_util/ccci_util_lib_load_img.c
+++ drivers/misc/mediatek/ccci_util/ccci_util_lib_load_img.c
@@ -679,17 +679,29 @@ void get_md_postfix(int md_id, char k[], char buf[], char buf_ex[])
 					case EMEA_DS_NA_DVT:
 					case EMEA_SS_NA_DVT:
 					case EMEA_SS_NFC_DVT:
+					case EMEA_DS_NA_DVT2:
+					case EMEA_SS_NFC_DVT2:
 						snprintf(buf, IMG_POSTFIX_LEN, "%d_%s_1", X, type_str[curr_ubin_id]);
 						break;
 					case LATAM_DS_NA_EVT:
 					case LATAM_DS_NA_DVT:
 					case ROLA_SS_NA_DVT:
 					case ROLA_SS_NA_EVT:
+					case LATAM_DS_NA_SKY77643_DVT2:
+					case ROLA_SS_NA_SKY77643_DVT2:
 						snprintf(buf, IMG_POSTFIX_LEN, "%d_%s_2", X, type_str[curr_ubin_id]);
 						break;
 					case AP_DS_NA_EVT:
 					case AP_DS_NA_DVT:
+					case AP_DS_NA_DVT2:
 						snprintf(buf, IMG_POSTFIX_LEN, "%d_%s_3", X, type_str[curr_ubin_id]);
+						break;
+					case LATAM_DS_NA_SKY77638_DVT2:
+					case ROLA_SS_NA_SKY77638_DVT2:
+						snprintf(buf, IMG_POSTFIX_LEN, "%d_%s_4", X, type_str[curr_ubin_id]);
+						break;
+					case AP_B28_DS_NA_DVT2:
+						snprintf(buf, IMG_POSTFIX_LEN, "%d_%s_5", X, type_str[curr_ubin_id]);
 						break;
 					default:
 						snprintf(buf, IMG_POSTFIX_LEN, "%d_%s_1", X, type_str[curr_ubin_id]);
@@ -744,18 +756,29 @@ void get_md_postfix(int md_id, char k[], char buf[], char buf_ex[])
 			case EMEA_DS_NA_DVT:
 			case EMEA_SS_NA_DVT:
 			case EMEA_SS_NFC_DVT:
+			case EMEA_DS_NA_DVT2:
+			case EMEA_SS_NFC_DVT2:
 				snprintf(YY_K, IMG_POSTFIX_LEN, "_%s_1", type_str[feature_val]);
 				break;
 			case LATAM_DS_NA_EVT:
 			case LATAM_DS_NA_DVT:
 			case ROLA_SS_NA_DVT:
 			case ROLA_SS_NA_EVT:
+			case LATAM_DS_NA_SKY77643_DVT2:
+			case ROLA_SS_NA_SKY77643_DVT2:
 				snprintf(YY_K, IMG_POSTFIX_LEN, "_%s_2", type_str[feature_val]);
 				break;
 			case AP_DS_NA_EVT:
 			case AP_DS_NA_DVT:
+			case AP_DS_NA_DVT2:
 				snprintf(YY_K, IMG_POSTFIX_LEN, "_%s_3", type_str[feature_val]);
 				break;
+			case LATAM_DS_NA_SKY77638_DVT2:
+			case ROLA_SS_NA_SKY77638_DVT2:
+				snprintf(YY_K, IMG_POSTFIX_LEN, "_%s_4", type_str[feature_val]);
+			case AP_B28_DS_NA_DVT2:
+				snprintf(YY_K, IMG_POSTFIX_LEN, "_%s_5", type_str[feature_val]);				
+				break;	
 			default:
 				snprintf(YY_K, IMG_POSTFIX_LEN, "_%s_1", type_str[feature_val]);
 				break;
