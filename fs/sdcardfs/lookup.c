c0b2c08de0d5c74f857a1aac4f08cdd44cdd2288
jixj <jixj@lenovo.com>
2017-01-17 10:47:45 +0800

    (CR) sdcardfs: fix "camera record vedio on extern sdcard and cause anr"
    filp.f_pos maybe always change
    
    Change-Id: Ibf3fef1a2da56d9e4171c03dafa0f1b6dad7df45
    Signed-off-by: jixj <jixj@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/941328
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Signed-off-by: guoyc1 <guoyc1@lenovo.com>
    Signed-off-by: guoyc1 <guoyc1@motorola.com>

--- fs/sdcardfs/lookup.c
+++ fs/sdcardfs/lookup.c
@@ -219,7 +219,7 @@ out:
 /* The dir context used by  sdcardfs_lower_filldir() */
 struct sdcardfs_lower_getent_cb {
 	struct dir_context ctx;
-	loff_t pos;
+	int count;
 	const char *target; /* search target */
 	int target_len;
 	char alias[NAME_MAX+1]; /* alias name found in lower dir */
@@ -239,7 +239,7 @@ sdcardfs_lower_filldir(struct dir_context *ctx, const char *name, int namelen,
 	if (!buf->result)  /* entry already found, skip search */
 		return 0;
 
-	buf->pos = buf->ctx.pos;
+	buf->count++;
 	if (!strncasecmp(name, buf->target, namelen) && namelen == buf->target_len) {
 		strncpy(buf->alias, name, namelen);
 		buf->alias_len = namelen;
@@ -261,11 +261,9 @@ static int sdcardfs_ci_path_lookup(struct path *folder, const char *name, struct
 {
 	int ret = 0;
 	struct file *filp;
-	loff_t last_pos;
 	struct sdcardfs_lower_getent_cb buf = {
 			.ctx.actor = sdcardfs_lower_filldir,
 			.ctx.pos = 0,
-			.pos = 0,
 			.target = name,
 			.alias_len = 0,
 			.result = -ENOENT
@@ -280,9 +278,9 @@ static int sdcardfs_ci_path_lookup(struct path *folder, const char *name, struct
 		return -ENOENT;
 
 	while (ret >= 0) {
-		last_pos = filp->f_pos;
+		buf.count = 0;
 		ret = iterate_dir(filp, &buf.ctx);
-		if (last_pos == filp->f_pos || !buf.result)  /* reaches end or found matching entry */
+		if (!buf.count || !buf.result)  /* reaches end or found matching entry */
 			break;
 	}
 
