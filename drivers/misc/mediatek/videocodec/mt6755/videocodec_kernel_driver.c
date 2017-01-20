e51eb3c824b7b91ab3d55ea7d4956c0264eea5c6
yangqh5 <yangqh5@lenovo.com>
2017-01-20 15:52:12 +0800

    [MTK Patch][(CR)]For_lenovo6755_66_n_alps-mp-n0.mp7-V1_P50
    Patch Type:
      Customer Request
    CR ID:
      ALPS03129123
    Severity:
    
    Description:
      [Patch Request] [PMS] LENOVO, Project: LENOVO6755_66_N, SW Version: alps-mp-n0.mp7-V1N/A
    
    Associated Files:
      device/lenovo/lenovo6755_66_n/ProjectConfig.mk
    
    Patch Type:
      Customer Request
    CR ID:
      ALPS03069985
    Severity:
      Critical
    Description:
      [Buganizer]Security Vulnerability - Security Vulnerability - *** driver bug(arbitrary to read)
      [[問題標題]]
      [Buganizer]Security Vulnerability - Security Vulnerability - *** driver bug(arbitrary to read)
      Io*** VCODEC_GET_CORE_LOADING
      The ***eger variables are not considered whether is negative, Thus resulting in an arbitrary read problem
    
      [[問題現象]]
      [Buganizer]Security Vulnerability - Security Vulnerability - *** driver bug(arbitrary to read)
      [[問題影響]]
      N/A
      [[問題復現步驟]]
      N/A
      [[建議驗證模塊]]
      N/A
      [[Title for Customer]]
      [Buganizer]Security Vulnerability - Security Vulnerability - *** driver bug(arbitrary to read)
      [[Problem Description]]
      [Buganizer]Security Vulnerability - Security Vulnerability - *** driver bug(arbitrary to read)
      [[Impa***]]
      N/A
      [[Reproduced Steps]]
      N/A
      [[Modules to be verified after taking p***h]]
      N/AN/A
    
    Associated Files:
      (CR).18/drivers/misc/mediatek/videocodec/mt6755/videocodec_kernel_driver.c
    
    Change-Id: Iacbcb12735d38d747fdb68344e5f7cb40b3e6184
    Reviewed-on: http://sc.lenovo.com:8080/428205

--- drivers/misc/mediatek/videocodec/mt6755/videocodec_kernel_driver.c
+++ drivers/misc/mediatek/videocodec/mt6755/videocodec_kernel_driver.c
@@ -1427,6 +1427,15 @@ static long vcodec_unlocked_ioctl(struct file *file, unsigned int cmd, unsigned 
 			MODULE_MFV_LOGE("[ERROR] VCODEC_GET_CORE_LOADING, copy_from_user failed: %lu\n", ret);
 			return -EFAULT;
 		}
+		if (rTempCoreLoading.CPUid < 0) {
+			MODULE_MFV_LOGE("[ERROR] rTempCoreLoading.CPUid < 0\n");
+			return -EFAULT;
+		}
+		if (rTempCoreLoading.CPUid > num_possible_cpus()) {
+			MODULE_MFV_LOGE("[ERROR] rTempCoreLoading.CPUid(%d) > num_possible_cpus(%d)\n",
+			rTempCoreLoading.CPUid, num_possible_cpus());
+			return -EFAULT;
+		}
 		rTempCoreLoading.Loading = get_cpu_load(rTempCoreLoading.CPUid);
 		ret = copy_to_user(user_data_addr, &rTempCoreLoading, sizeof(VAL_VCODEC_CORE_LOADING_T));
 		if (ret) {
