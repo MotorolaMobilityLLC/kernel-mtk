b2fc997209b884d048bb49a93c09f77b9034c1c1
jiatianbao <jiatianbao@longcheer.com>
2017-02-24 15:09:00 +0800

    [Purp][Marino][TP]Mod tp_info vendor
    Change-Id: I574d5f79c0192f80ffaec5f764bbf334de70b150

--- drivers/input/touchscreen/mediatek/lct_gt917d_l3510/gt9xx_driver.c
+++ drivers/input/touchscreen/mediatek/lct_gt917d_l3510/gt9xx_driver.c
@@ -49,7 +49,7 @@ static int ctp_proc_read_show (struct seq_file* m, void* data)
 	if(temp_pid == 0x00){	//add by liuzhen
 		sprintf(vendor_name,"%s","Djn");
 	}else if(temp_pid == 0x01){
-		sprintf(vendor_name,"%s","HELITECH");
+		sprintf(vendor_name,"%s","TopTouch");
 	}else if(temp_pid == 0x02){
 		sprintf(vendor_name,"%s","Mudong");
 	}else if(temp_pid == 0x05){
@@ -1782,7 +1782,7 @@ static s32 tpd_i2c_probe(struct i2c_client *client, const struct i2c_device_id *
 		devinfo_ctp_regchar("unknown","O-Film",temp_ver,DEVINFO_USED);
 			break;
 		case 0x01:
-		devinfo_ctp_regchar("unknown","HELITECH",temp_ver,DEVINFO_USED);
+		devinfo_ctp_regchar("unknown","TopTouch",temp_ver,DEVINFO_USED);
 			break;
 		case 0x02:
 		devinfo_ctp_regchar("unknown","Mudong",temp_ver,DEVINFO_USED);
