c6f2e52c9e366b42516ca37d2053481daee46fb8
yiyinglin <yiyinglin@huaqin.com>
2017-02-28 18:18:32 +0800

    MANNING-306 driver:lcm:modify backlight setting
    modify backlight setting of ili7807d IC
    
    Change-Id: I94f71e6df84f4a704998ca5245e83c1d0c042838
    Signed-off-by: yiyinglin <yiyinglin@huaqin.com>

--- drivers/misc/mediatek/lcm/ili7807d_fhd_dsi_vdo_djn/ili7807d_fhd_dsi_vdo_djn.c
+++ drivers/misc/mediatek/lcm/ili7807d_fhd_dsi_vdo_djn/ili7807d_fhd_dsi_vdo_djn.c
@@ -126,7 +126,7 @@ static void init_lcm_registers(void)
 	dsi_set_cmdq(data_array, 2, 1);
 
 	data_array[0] = 0x00023902;
-	data_array[1] = 0x0000FF51;
+	data_array[1] = 0x00000051;
 	dsi_set_cmdq(data_array, 2, 1);
 
 	data_array[0] = 0x00023902;
