7fd57a2258e0f2c272987ea1083a15c06a0e4a8b
huangsh4 <huangsh4@motorola.com>
2017-03-04 23:42:12 +0800

    (CR): camera: enable s5k4h8 and s5k3l8 otp for brady project.
    -enable s5k4h8 and s5k3l8 otp for brady project.
    
    Change-Id: Ia4f92b1312f4cbcd5870db82bf99ee14acbbb8fa

--- drivers/misc/mediatek/imgsensor/src/mt6757/s5k3l8_mipi_raw/s5k3l8mipiraw_pdaf.c
+++ drivers/misc/mediatek/imgsensor/src/mt6757/s5k3l8_mipi_raw/s5k3l8mipiraw_pdaf.c
@@ -7,28 +7,15 @@
 #include <linux/fs.h>
 #include <asm/atomic.h>
 #include <linux/slab.h>
-//#include <linux/xlog.h>
-#include"kd_camera_typedef.h"
 
-/*===FEATURE SWITH===*/
- // #define FPTPDAFSUPPORT   //for pdaf switch
- // #define FANPENGTAO   //for debug log
- #define LOG_INF LOG_INF_LOD
-/*===FEATURE SWITH===*/
 
-/****************************Modify Following Strings for Debug****************************/
-#define PFX "S5K3L8PDAF"
-#define LOG_INF_NEW(format, args...)    pr_debug(PFX "[%s] " format, __FUNCTION__, ##args)
-//#define LOG_INF_LOD(format, args...)    xlog_printk(ANDROID_LOG_INFO   , PFX, "[%s] " format, __FUNCTION__, ##args)
-#define LOG_1 LOG_INF("S5K3L8,MIPI 4LANE\n")
-#define SENSORDB LOG_INF
-/****************************   Modify end    *******************************************/
+#define PFX "S5K3L8_pdafotp"
+#define LOG_INF(format, args...)	pr_debug(PFX "[%s] " format, __FUNCTION__, ##args)
 
 #include "kd_camera_hw.h"
 #include "kd_imgsensor.h"
 #include "kd_imgsensor_define.h"
 #include "kd_imgsensor_errcode.h"
-
 extern int iReadRegI2C(u8 *a_pSendData , u16 a_sizeSendData, u8 * a_pRecvData, u16 a_sizeRecvData, u16 i2cId);
 extern int iWriteRegI2C(u8 *a_pSendData , u16 a_sizeSendData, u16 i2cId);
 //extern void kdSetI2CSpeed(u16 i2cSpeed);
@@ -38,77 +25,88 @@ extern int iMultiReadReg(u16 a_u2Addr , u8 * a_puBuff , u16 i2cId, u8 number);
 
 #define USHORT             unsigned short
 #define BYTE               unsigned char
-#define Sleep(ms)          mdelay(ms)
+#define Sleep(ms) mdelay(ms)
 
-/**************  CONFIG BY SENSOR >>> ************/
-#define EEPROM_WRITE_ID   0xa0
-#define I2C_SPEED        100  
-#define MAX_OFFSET		    0xFFFF
-#define DATA_SIZE         1404
-#define START_ADDR        0X0763
-BYTE S5K3L8_eeprom_data[DATA_SIZE]= {0};
+#define S5K3L8_EEPROM_READ_ID  0xA0
+#define S5K3L8_EEPROM_WRITE_ID   0xA0
+#define S5K3L8_I2C_SPEED        100
+#define S5K3L8_MAX_OFFSET		0xFFFF
 
-/**************  CONFIG BY SENSOR <<< ************/
+#define DATA_SIZE 2048
+BYTE s5k3L8_eeprom_data[DATA_SIZE]= {0};
+static bool get_done = false;
+static int last_size = 0;
+static int last_offset = 0;
 
-static kal_uint16 read_cmos_sensor_byte(kal_uint16 addr)
+
+static bool selective_read_eeprom(kal_uint16 addr, BYTE* data)
 {
-    kal_uint16 get_byte=0;
-    char pu_send_cmd[2] = {(char)(addr >> 8) , (char)(addr & 0xFF) };
-
-   // kdSetI2CSpeed(I2C_SPEED); // Add this func to set i2c speed by each sensor
-    iReadRegI2C(pu_send_cmd , 2, (u8*)&get_byte,1,EEPROM_WRITE_ID);
-    return get_byte;
-}
-#if 0
-static kal_uint16 read_cmos_sensor(kal_uint32 addr)
-{
-    kal_uint16 get_byte=0;
-    char pu_send_cmd[2] = {(char)(addr >> 8), (char)(addr & 0xFF) };
-
-    kdSetI2CSpeed(I2C_SPEED); // Add this func to set i2c speed by each sensor
-    iReadRegI2C(pu_send_cmd, 2, (u8*)&get_byte, 1, EEPROM_WRITE_ID);
-    return get_byte;
+	char pu_send_cmd[2] = {(char)(addr >> 8) , (char)(addr & 0xFF) };
+    if(addr > S5K3L8_MAX_OFFSET)
+        return false;
+	//kdSetI2CSpeed(S5K3L8_I2C_SPEED);
+	if(iReadRegI2C(pu_send_cmd, 2, (u8*)data, 1, S5K3L8_EEPROM_WRITE_ID)<0)
+		return false;
+    return true;
 }
 
-static void write_cmos_sensor_byte(kal_uint32 addr, kal_uint32 para)
-{
-    char pu_send_cmd[3] = {(char)(addr >> 8), (char)(addr & 0xFF), (char)(para & 0xFF)};
-
-    kdSetI2CSpeed(I2C_SPEED); // Add this func to set i2c speed by each sensor
-    iWriteRegI2C(pu_send_cmd, 3, EEPROM_WRITE_ID);
-}
-
-static void write_cmos_sensor(kal_uint16 addr, kal_uint16 para)
-{
-    char pusendcmd[4] = {(char)(addr >> 8) , (char)(addr & 0xFF) ,(char)(para >> 8),(char)(para & 0xFF)};
-
-    kdSetI2CSpeed(I2C_SPEED); // Add this func to set i2c speed by each sensor
-    iWriteRegI2C(pusendcmd , 4, EEPROM_WRITE_ID);
-}
-#endif
-static bool _read_eeprom(kal_uint16 addr, kal_uint32 size ){
-  //continue read reg by byte:
-  int i=0;
-  for(; i<size; i++){
-    S5K3L8_eeprom_data[i] = read_cmos_sensor_byte(addr+i);
-    printk("add = 0x%x,\tvalue = 0x%x",i, S5K3L8_eeprom_data[i]);
-  }
-  return true;
+static bool _read_3L8_eeprom(kal_uint16 addr, BYTE* data, kal_uint32 size ){
+	int i = 0;
+	int offset = addr;
+	for(i = 0; i < size; i++) {
+		if(!selective_read_eeprom(offset, &data[i])){
+			return false;
+		}
+		LOG_INF("read_eeprom 0x%0x %d=== %0x\n",offset, data[i],data[i]);
+		offset++;
+	}
+	get_done = true;
+	last_size = size;
+	last_offset = addr;
+    return true;
 }
 
 bool S5K3L8_read_eeprom( kal_uint16 addr, BYTE* data, kal_uint32 size){
-	addr = START_ADDR;
-	size = DATA_SIZE;
-	
-	printk("Read EEPROM, addr = 0x%x, size = 0d%d\n", addr, size);
+	int i;
+	addr = 0x0779;
+//	size = 1404;
+	//BYTE header[9]= {0};
+	//_read_3L8_eeprom(0x0000, header, 9);
 
-	if(!_read_eeprom(addr, size)){
-	printk("error:read_eeprom fail!\n");
-	return false;
+	LOG_INF("read 3L8 eeprom, size = %d\n", size);
+
+	if(!get_done || last_size != size || last_offset != addr) {
+		LOG_INF("read 3L8 eeprom, size = %d\n", size);
+		if(!_read_3L8_eeprom(addr, s5k3L8_eeprom_data, size)){
+			get_done = 0;
+            last_size = 0;
+            last_offset = 0;
+			return false;
+		}
+	}
+//if((s5k3L8_eeprom_data[0] != 1) || (s5k3L8_eeprom_data[498] != 1) || (s5k3L8_eeprom_data[1306] != 1))
+	if((s5k3L8_eeprom_data[0] != 1) || (s5k3L8_eeprom_data[498] != 1))
+	{
+		LOG_INF("3M2 PDAF eeprom data invalid\n");
+		return false;
+	}
+	
+	for(i = 0; i < 496; i++)   // proc1 data
+	{
+		data[i] = s5k3L8_eeprom_data[i+1];
 	}
 
-	memcpy(data, S5K3L8_eeprom_data, size);
-	return true;
+	for(i = 496; i < 1404; i++)  // proc2 data
+	{
+		data[i] = s5k3L8_eeprom_data[i+3];
+	}	
+
+	for(i = 0; i < 1404; i++)
+	{
+		LOG_INF("data[%d] = %d\n", i, data[i]);
+	}
+	//memcpy(data, s5k3L8_eeprom_data, size); //huangsh4 make pdaf buffer correct
+    return true;
 }
 
 
