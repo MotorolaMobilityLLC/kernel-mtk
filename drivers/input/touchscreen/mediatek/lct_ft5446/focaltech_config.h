b4e4ae7de412778b6355eec0de636a0a30403535
zhudaolong <zhudaolong@longcheer.com>
2017-03-09 20:37:02 +0800

    [Purp][Nicklaus][TP] Modify for 3rd tp each point
       [Modify]
    
    	1.Fixed 3rd tp each point failure
    
       [Mark]
           none
    
    Change-Id: I24566bd405d08222c16385847f3a68f859a2e4ce

--- drivers/input/touchscreen/mediatek/lct_ft5446/focaltech_config.h
+++ drivers/input/touchscreen/mediatek/lct_ft5446/focaltech_config.h
@@ -169,7 +169,7 @@
  * FW_APP.i file for upgrade
  * define your own fw_app, the sample one is invalid
  */
-#define FTS_UPGRADE_FW_APP                      "include/firmware/FT5446_LQ_L3600_V10_D01_20170112_app.i"
+#define FTS_UPGRADE_FW_APP                      "include/firmware/FT5446_LQ_L3600_V11_D01_20170309_app.i"
 
 /*
  * auto upgrade with app.bin in sdcard
