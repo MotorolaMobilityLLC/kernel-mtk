d7554929ce2cd9a04bd0425b216d2f41efb94477
jiatianbao <jiatianbao@longcheer.com>
2017-03-17 15:37:36 +0800

    [Purp][Marino][LCM]Mod max backlight to 350 nits for Marino
    Change-Id: Id9ea43b35d372f08e10bcbcd6714b99e3c07e1b8

--- drivers/misc/mediatek/pmic/rt5081/rt5081_pmu_bled.c
+++ drivers/misc/mediatek/pmic/rt5081/rt5081_pmu_bled.c
@@ -442,7 +442,7 @@ static inline int rt_parse_dt(struct device *dev)
 	else
 	{
 #ifdef CONFIG_L3510_MAINBOARD
-		pdata->max_bled_brightness = 1024;//Mod max brightness 350 nits for Marino
+		pdata->max_bled_brightness = 950;//Mod max brightness 350 nits for Marino
 #else
 		pdata->max_bled_brightness = tmp;
 #endif
