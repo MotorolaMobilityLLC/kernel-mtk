a674c2684805d02d9706a4bb39d52f2150ddc7a7
tuwenzan <tuwenzan@wind-mobi.com>
2017-03-23 14:06:23 +0800

    [Purp][woods][psensor]modify psensor oil parameter
    [Modi]
    modify psensor oil parameter
    [Mark]none
    	modified:   drivers/misc/mediatek/alsps/ltr778/ltr778.c
    
    Change-Id: I860e091da2ea04e153b81814b869076911dd6ba6

--- drivers/misc/mediatek/alsps/ltr778/ltr778.c
+++ drivers/misc/mediatek/alsps/ltr778/ltr778.c
@@ -795,7 +795,7 @@ out:
 	return psdata;
 }
 
-//tuwenzan@wind-mobi.com modify at 20170307 begin
+//tuwenzan@wind-mobi.com modify at 20170323 begin
 #ifdef GN_MTK_BSP_PS_DYNAMIC_CALI
 static int ltr778_dynamic_calibrate(void)
 {
@@ -849,38 +849,38 @@ static int ltr778_dynamic_calibrate(void)
 			ps_thd_val_high = noise + 30;
 			ps_thd_val_low  = noise + 15;
 			ps_persist_val_high = 2046;// modified by steven
-			ps_persist_val_low  = noise + 60;
+			ps_persist_val_low  = noise + 100;
 		}
 		else if (noise < 200) {
 			ps_thd_val_high = noise + 30;
 			ps_thd_val_low  = noise + 15;
 			ps_persist_val_high = noise + 1700;
-			ps_persist_val_low  = noise + 65;
+			ps_persist_val_low  = noise + 120;
 		//liujinzhou@wind-mobi.com modify at 20161215 end
 		}
 		else if (noise < 300) {
 			ps_thd_val_high = noise + 30;
 			ps_thd_val_low  = noise + 15;
 			ps_persist_val_high = noise + 1600;
-			ps_persist_val_low  = noise + 70;
+			ps_persist_val_low  = noise + 160;
 		}
 		else if (noise < 400) {
 			ps_thd_val_high = noise + 30;
 			ps_thd_val_low  = noise + 15;
 			ps_persist_val_high = noise + 1600;
-			ps_persist_val_low  = noise + 100;
+			ps_persist_val_low  = noise + 200;
 		}
 		else if (noise < 600) {
 			ps_thd_val_high = noise + 47;
 			ps_thd_val_low  = noise + 30;
 			ps_persist_val_high = noise + 1400;
-			ps_persist_val_low  = noise + 120;
+			ps_persist_val_low  = noise + 220;
 		}
 		else if (noise < 1500) {
 			ps_thd_val_high = noise + 47;
 			ps_thd_val_low  = noise + 30;
 			ps_persist_val_high = 2047;
-			ps_persist_val_low  = noise + 150;
+			ps_persist_val_low  = noise + 250;
 		}
 		else {
 			ps_thd_val_high = 1900;
@@ -911,7 +911,7 @@ static int ltr778_dynamic_calibrate(void)
 	return 0;
 }
 #endif
-//tuwenzan@wind-mobi.com modify at 20170307 end
+//tuwenzan@wind-mobi.com modify at 20170323 end
 
 /********************************************************************/
 /* 
