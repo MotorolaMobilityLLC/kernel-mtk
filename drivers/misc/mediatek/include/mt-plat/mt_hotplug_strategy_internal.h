b7937f92be81fef1f50815ead13a227785b3f0f5
wangwang1 <wangwang@motorola.com>
2017-03-28 10:51:03 +0800

    don't make cpu hotplug runs too frequently
    Change-Id: Idf8b4760e07472cf3a86e5bd0c62a7888c82e046
    Reviewed-on: https://gerrit.mot.com/969335
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: xiangpo zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/misc/mediatek/include/mt-plat/mt_hotplug_strategy_internal.h
+++ drivers/misc/mediatek/include/mt-plat/mt_hotplug_strategy_internal.h
@@ -23,7 +23,7 @@
 
 /* CONFIG - compile time */
 #define HPS_TASK_PRIORITY		(MAX_RT_PRIO - 3)
-#define HPS_TIMER_INTERVAL_MS		40
+#define HPS_TIMER_INTERVAL_MS		100
 
 #define HPS_PERIODICAL_BY_WAIT_QUEUE        (1)
 #define HPS_PERIODICAL_BY_TIMER             (2)
