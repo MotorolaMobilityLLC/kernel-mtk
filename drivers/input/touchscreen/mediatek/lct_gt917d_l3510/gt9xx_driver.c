1caec094aa9eb5ec402506ad802f26eacac2ed50
yangjiangzhu <yangjiangzhu@longcheer.com>
2017-04-06 01:53:13 +0800

    [L3510]kernel:modif oufeiTPconfig and firmware
    Change-Id: Iceac43a4721de13422a3f4c0145a8b3e34bad720

--- drivers/input/touchscreen/mediatek/lct_gt917d_l3510/gt9xx_driver.c
+++ drivers/input/touchscreen/mediatek/lct_gt917d_l3510/gt9xx_driver.c
@@ -1762,14 +1762,14 @@ static s32 tpd_i2c_probe(struct i2c_client *client, const struct i2c_device_id *
 
 	if (ret < 0) {
 		GTP_ERROR("Read version failed.");
-		return -1;
+		//return -1;
 	}
 
 	ret = gtp_init_panel(client);
 
 	if (ret < 0) {
 		GTP_ERROR("GTP init panel failed.");
-		return -1;
+		//return -1;
 	}
 	//add devinfo start
    #ifdef CONFIG_LCT_DEVINFO_SUPPORT 
