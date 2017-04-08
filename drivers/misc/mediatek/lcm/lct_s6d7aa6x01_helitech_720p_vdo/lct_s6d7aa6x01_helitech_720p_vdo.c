80a18033bb300c39a0553d75607c1fbf737af091
zhouhua <zhouhua@longcheer.net>
2017-04-08 12:11:27 +0800

    [Purp][Brady][LCM]modify the helitech lcm mipi clock
    Change-Id: Ib4de38607a1c8a3ce5f0b0fef3d46d28b168b665

--- drivers/misc/mediatek/lcm/lct_s6d7aa6x01_helitech_720p_vdo/lct_s6d7aa6x01_helitech_720p_vdo.c
+++ drivers/misc/mediatek/lcm/lct_s6d7aa6x01_helitech_720p_vdo/lct_s6d7aa6x01_helitech_720p_vdo.c
@@ -246,7 +246,7 @@ static void lcm_get_params(LCM_PARAMS *params)
 #if (LCM_DSI_CMD_MODE)
 	params->dsi.PLL_CLOCK = 150;	/* this value must be in MTK suggested table */
 #else
-	params->dsi.PLL_CLOCK = 210;	/* this value must be in MTK suggested table */
+	params->dsi.PLL_CLOCK = 229;	/* this value must be in MTK suggested table */
 #endif
 
 	params->dsi.cont_clock=0;
