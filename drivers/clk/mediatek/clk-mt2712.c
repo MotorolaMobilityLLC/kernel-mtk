3e41e594d92e798c24c6042e7139071739aad1ce
Yong Liang <yong.liang@mediatek.com>
2017-05-03 16:32:55 +0800

    <reset controller>: register reset controller
    [Detail] register reset controller and
    open CONFIG_RESET_CONTROLLER config
    
    [Solution] register reset controller and
    open CONFIG_RESET_CONTROLLER config
    
    Test: Pass
    
    Change-Id: If9b845f55383f0049c003eb47fa29d4cca9abe6d
    Signed-off-by: Yong Liang <yong.liang@mediatek.com>
    CR-Id: AUTO00003648

--- drivers/clk/mediatek/clk-mt2712.c
+++ drivers/clk/mediatek/clk-mt2712.c
@@ -1310,6 +1310,7 @@ static int clk_mt2712_infra_probe(struct platform_device *pdev)
 		pr_err("%s(): could not register clock provider: %d\n",
 			__func__, r);
 
+	mtk_register_reset_controller(node, 2, 0x30);
 	return r;
 }
 
@@ -1329,6 +1330,7 @@ static int clk_mt2712_peri_probe(struct platform_device *pdev)
 		pr_err("%s(): could not register clock provider: %d\n",
 			__func__, r);
 
+	mtk_register_reset_controller(node, 2, 0);
 	return r;
 }
 
