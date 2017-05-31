1634748ff0015622cb79973ac273dd62f655b33c
wangwang <wangwang1@lenovo.com>
2017-05-31 17:57:54 +0800

    (CR) fix sdcardfs dead lock on locking parent 3/3
    Change-Id: Ie0b006e187fe6076e9a6ae5d8837bc7fad7af7e4
    Reviewed-on: https://gerrit.mot.com/1004521
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- fs/sdcardfs/inode.c
+++ fs/sdcardfs/inode.c
@@ -415,9 +415,9 @@ static int sdcardfs_rmdir(struct inode *dir, struct dentry *dentry)
 	 */
 	sdcardfs_get_real_lower(dentry, &lower_path);
 
-	sdcardfs_drop_shared_icache(dir->i_sb, lower_dentry->d_inode);
 	lower_dentry = lower_path.dentry;
 	lower_mnt = lower_path.mnt;
+	sdcardfs_drop_shared_icache(dir->i_sb, lower_dentry->d_inode);
 	lower_dir_dentry = lock_parent(lower_dentry);
 
 	err = vfs_rmdir2(lower_mnt, d_inode(lower_dir_dentry), lower_dentry);
