8df863d2c1dad15a16d3e817d9a0ee8049c351ab
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 14:21:26 +0800

    [ALPS02086458] arm: use arm_lowmem_limit to setup max_low
    [Detail]
    In find_limits(), max_low is set by memblock_get_current_limit().
    However, memblock_get_current_limit() can be changed by
    memblock_set_current_limit() at any point, make max_low a unstable
    value.
    
    [Solution]
    use arm_lowmem_limit to setup max_low
    
    [Feature] arm
    
    MTK-Commit-Id: 95dcec8469b95a3d5803c31a463af733d7e3c19f
    
    Change-Id: Iddfc78b6f8fc377c2ce60c9685c93404ebb9ccd2
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02086458

--- arch/arm/mm/init.c
+++ arch/arm/mm/init.c
@@ -137,7 +137,7 @@ void show_mem(unsigned int filter)
 static void __init find_limits(unsigned long *min, unsigned long *max_low,
 			       unsigned long *max_high)
 {
-	*max_low = PFN_DOWN(memblock_get_current_limit());
+	*max_low = PFN_DOWN(arm_lowmem_limit);
 	*min = PFN_UP(memblock_start_of_DRAM());
 	*max_high = PFN_DOWN(memblock_end_of_DRAM());
 }
