802d6d7c7256113e31d8bf9105c5968cecb1e967
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 01:16:24 +0800

    [ALPS02386912] suspend: mask SCP source request.
    [Detail] SCP use other request pin to request resource
    
    [Solution] 1. mask md32_srcclkena_infra_mask_b
               2. mask md32_apsrcreq_infra_mask_b
    
    [Feature] Power Management
    
    MTK-Commit-Id: fb38c035a49de5b6b121bc53536dee63929f0377
    
    Change-Id: I296c62f6846e3b9c5526b0c2b4b0cc6b07ea1183
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02386912

--- drivers/misc/mediatek/base/power/spm_v2/mt_spm_sleep.c
+++ drivers/misc/mediatek/base/power/spm_v2/mt_spm_sleep.c
@@ -229,21 +229,17 @@ static struct pwr_ctrl suspend_ctrl = {
 	.md_srcclkena_0_infra_mask_b = 0,
 	.md_srcclkena_1_infra_mask_b = 0,
 	.conn_srcclkena_infra_mask_b = 0,
-#if defined(CONFIG_ARCH_MT6755)
 	.md32_srcclkena_infra_mask_b = 0,
+#if defined(CONFIG_ARCH_MT6755)
 	.srcclkeni_infra_mask_b = 1,
 #elif defined(CONFIG_ARCH_MT6797)
-	.md32_srcclkena_infra_mask_b = 1,
 	.srcclkeni_infra_mask_b = 0,
 #endif
+
 	.md_apsrcreq_0_infra_mask_b = 1,
 	.md_apsrcreq_1_infra_mask_b = 0,
 	.conn_apsrcreq_infra_mask_b = 1,
-#if defined(CONFIG_ARCH_MT6755)
 	.md32_apsrcreq_infra_mask_b = 0,
-#elif defined(CONFIG_ARCH_MT6797)
-	.md32_apsrcreq_infra_mask_b = 1,
-#endif
 	.md_ddr_en_0_mask_b = 1,
 	.md_ddr_en_1_mask_b = 0,
 	.md_vrf18_req_0_mask_b = 1,
