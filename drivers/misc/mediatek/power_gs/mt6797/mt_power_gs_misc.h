84ea1402dcb1520ef5fcd0a5b282b3fb078ca222
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 03:42:53 +0800

    [ALPS02439414] power_gs: Golden setting modifcation
    [Detail] Some golden settings need to added or modified
    
    [Solution] Refine golden setting
    
    MTK-Commit-Id: 5514fa4a1b04413bbabcca9da82c89ac674272f7
    
    Change-Id: Ic46f3eea599b0c37441296f3c15b46c3b8486ad4
    CR-Id: ALPS02439414
    Feature: Thermal Management
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>

--- drivers/misc/mediatek/power_gs/mt6797/mt_power_gs_misc.h
+++ drivers/misc/mediatek/power_gs/mt6797/mt_power_gs_misc.h
@@ -11,10 +11,16 @@ const unsigned int gs_mdp_array[] = {
 	0x2002000, 0x00000010, 0x00000010,	/* EN */
 	0x2006008, 0x80000000, 0x80000000,	/* WDMA_EN */
 	0x200707C, 0x00010000, 0x00010000,	/* VIDO_ROT_EN */
-	0x200807C, 0x00010000, 0x00010000	/* VIDO_ROT_EN */
+	0x200807C, 0x00010000, 0x00010000,	/* VIDO_ROT_EN */
+	0x2001028, 0xFFFFFFFF, 0x00010371,	/* GMCIF_CON */
+	0x2001240, 0xFFFFFFFF, 0x07000024,	/* DMABUF_CON_0 */
+	0x2001250, 0xFFFFFFFF, 0x03000013,	/* DMABUF_CON_1 */
+	0x2001260, 0xFFFFFFFF, 0x03000013,	/* DMABUF_CON_2 */
+	0x2007008, 0xFFFFFFFF, 0x01802000,	/* VIDO_MAIN_BUF_SIZE */
+	0x2007004, 0xFFFFFFFF, 0x3F000000	/* VIDO_DMA_PERF */
 };
 const unsigned int *gs_mdp_array_ptr = gs_mdp_array;
-unsigned int gs_mdp_array_len = 21;
+unsigned int gs_mdp_array_len = 39;
 
 /*Base address = 0x12000000 */
 const unsigned int gs_disp_array[] = {
@@ -43,17 +49,31 @@ unsigned int gs_disp_array_len = 51;
 /*Base address = 0x12000000*/
 const unsigned int gs_smi_array[] = {
 	/* Offset     Mask        Golden Setting Value */
-	0x2020010, 0x0000FFF0, 0x000000F0,	/* SMI_LARB0 */
-	0x4010010, 0x0000FFF0, 0x000000F0,	/* SMI_LARB1 */
-	0x8001010, 0x0000FFF0, 0x000000F0,	/* SMI_LARB2 */
-	0x5001010, 0x0000FFF0, 0x000000F0,	/* SMI_LARB3 */
-	0x0002010, 0x0000FFF0, 0x000000F0,	/* SMI_LARB4 */
-	0x2021010, 0x0000FFF0, 0x000000F0,	/* SMI_LARB5 */
-	0x3001010, 0x0000FFF0, 0x000000F0,	/* SMI_LARB6 */
-	0x2022300, 0x00003FFF, 0x00000401	/* SMI_DCM */
+	0x2020010, 0x0000FFF0, 0x000007F0,	/* SMI_LARB0 */
+	0x4010010, 0x0000FFF0, 0x000007F0,	/* SMI_LARB1 */
+	0x8001010, 0x0000FFF0, 0x000007F0,	/* SMI_LARB2 */
+	0x5001010, 0x0000FFF0, 0x000007F0,	/* SMI_LARB3 */
+	0x0002010, 0x0000FFF0, 0x000007F0,	/* SMI_LARB4 */
+	0x2021010, 0x0000FFF0, 0x000007F0,	/* SMI_LARB5 */
+	0x3001010, 0x0000FFF0, 0x000007F0,	/* SMI_LARB6 */
+	0x2022300, 0x00003FFF, 0x00000403,	/* SMI_DCM */
+	0x2020100, 0xFFFFFFFF, 0x0000000B,	/* disp_ovl0 */
+	0x2020104, 0xFFFFFFFF, 0x0000000B,	/* disp_ovl0_2l */
+	0x2020108, 0xFFFFFFFF, 0x0000000B,	/* disp_rdma0 */
+	0x202010C, 0xFFFFFFFF, 0x00000004,	/* disp_wdma0 */
+	0x2020110, 0xFFFFFFFF, 0x00000005,	/* mdp_rdma0 */
+	0x2020114, 0xFFFFFFFF, 0x00000004,	/* mdp_wdma0 */
+	0x2020118, 0xFFFFFFFF, 0x00000002,	/* mdp_wrot0 */
+	0x2020200, 0xFFFFFFFF, 0x0000001C,	/* disp_ovl0 */
+	0x2020204, 0xFFFFFFFF, 0x00000005,	/* disp_ovl0_2l */
+	0x2020208, 0xFFFFFFFF, 0x0000001F,	/* disp_rdma0 */
+	0x202020C, 0xFFFFFFFF, 0x0000000A,
+	0x2020210, 0xFFFFFFFF, 0x00000005,
+	0x2020214, 0xFFFFFFFF, 0x00000001,
+	0x2020218, 0xFFFFFFFF, 0x00000005
 };
 const unsigned int *gs_smi_array_ptr = gs_smi_array;
-unsigned int gs_smi_array_len = 24;
+unsigned int gs_smi_array_len = 66;
 
 
 /* IMG is in the ISP subsys */
@@ -98,10 +118,12 @@ unsigned int gs_vdec_array_len = 3;
 const unsigned int gs_infra_array[] = {
 	/* Offset     Mask        Golden Setting Value */
 	0x01070, 0xFFFFFFFF, 0x40780603,	/* infra_bus_dcm */
-	0x01074, 0xFFFFFFFF, 0xB0380603	/* peri_bus_dcm */
+	0x01074, 0xFFFFFFFF, 0xB0380603,	/* peri_bus_dcm */
+	0x01078, 0xFFFFFFFF, 0x00000000,	/* MEM_DCM_CTRL */
+	0x0107C, 0xFFFFFFFF, 0x00000000		/* DFS_MEM_DCM_CTRL */
 };
 const unsigned int *gs_infra_array_ptr = gs_infra_array;
-unsigned int gs_infra_array_len = 6;
+unsigned int gs_infra_array_len = 12;
 
 
 /* ClockTop is always on */
