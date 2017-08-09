10d316a692109233b90fd8fe89a46f5b65a488b2
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 19:03:07 +0800

    [ALPS02279505] IDLE: Enable SODI function at Rainier
    [Detail] Enable SODI function at Rainier
    [Solution] Modify idle_switch to enable SODI at Rainier
    [Feature] SODI (Screen On Deep Idle)
    
    MTK-Commit-Id: 45c67e1853df120784b3476b25c03bda58928d81
    
    Change-Id: I85a136e6e7f06c206c7e9f7d265d401d7f8a16e6
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02279505

--- drivers/misc/mediatek/base/power/spm_v1/mt_idle.c
+++ drivers/misc/mediatek/base/power/spm_v1/mt_idle.c
@@ -384,7 +384,7 @@ static unsigned int slidle_condition_mask[NR_GRPS] = {
 /*Idle handler on/off*/
 static int idle_switch[NR_TYPES] = {
 	0,  /* dpidle switch */
-	0,  /* soidle switch */
+	1,  /* soidle switch */
 	0,  /* slidle switch */
 	1,  /* rgidle switch */
 };
