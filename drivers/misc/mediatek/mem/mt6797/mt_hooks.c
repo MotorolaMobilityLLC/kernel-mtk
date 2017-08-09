6aea14ff48b9ec325c85bc209e8366d3e2003164
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 04:32:48 +0800

    [ALPS02464821] arm/arm64: add a debug hook in undefinstr
    [Detail]
    Add a debug hook function in do_undefinstr to observe
    the behaviour of do_undefinstr.
    
    MTK-Commit-Id: 2d2ec44d6318e600a853bb3ebee886eca21baa4d
    
    Change-Id: I19b09fb786a5f034738d1183d7e799553a0a9335
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02464821
    Feature: Power Management

--- drivers/misc/mediatek/mem/mt6797/mt_hooks.c
+++ drivers/misc/mediatek/mem/mt6797/mt_hooks.c
@@ -2,6 +2,11 @@
 #include <linux/sizes.h>
 #include <linux/printk.h>
 #include <linux/mm.h>
+#include <linux/compat.h>
+#include <linux/init.h>
+#include <asm/traps.h>
+#include <asm/ptrace.h>
+#include <asm/cacheflush.h>
 #include <asm/pgtable.h>
 #include <asm/atomic.h>
 
@@ -29,4 +34,83 @@ void ioremap_debug_hook_func(phys_addr_t phys_addr, size_t size,
 	pr_err("ioremap to 0x1001AXXX more than once (%pa, %zx)\n",
 			&phys_addr, size);
 	BUG();
+}
+
+static DEFINE_PER_CPU(void *, __prev_undefinstr_pc);
+static DEFINE_PER_CPU(int, __prev_undefinstr_counter);
+
+/*
+ * return 0 if code is fixed
+ * return 1 if the undefined instruction cannot be fixed.
+ */
+int arm_undefinstr_retry(struct pt_regs *regs, unsigned int instr)
+{
+	void __user *pc;
+	struct thread_info *thread = current_thread_info();
+	u32 insn = __opcode_to_mem_arm(BUG_INSTR_VALUE);
+	u32 insn = 0;
+
+	pc = (void __user *)instruction_pointer(regs);
+
+	/*
+	 * Place the SIGILL ICache Invalidate after the Debugger
+	 * Undefined-Instruction Solution.
+	 */
+	if (((processor_mode(regs) == USR_MODE) ||
+				(processor_mode(regs) == SVC_MODE)) &&
+			(instr != insn)) {
+		void **prev_undefinstr_pc = &get_cpu_var(__prev_undefinstr_pc);
+		int *prev_undefinstr_counter = &get_cpu_var(__prev_undefinstr_counter);
+
+		pr_alert("USR_MODE/SVC_MODE Undefined Instruction Address curr:%p pc=%p:%p, instr: 0x%x\n",
+			(void *)current, (void *)pc, (void *)*prev_undefinstr_pc,
+			instr);
+		if (*prev_undefinstr_pc != pc) {
+			/*
+			 * If the current process or program counter is changed...
+			 * renew the counter.
+			 */
+			pr_alert("First Time Recovery curr:%p pc=%p:%p\n",
+				(void *)current, (void *)pc, (void *)*prev_undefinstr_pc);
+			*prev_undefinstr_pc = pc;
+			*prev_undefinstr_counter = 0;
+			put_cpu_var(__prev_undefinstr_pc);
+			put_cpu_var(__prev_undefinstr_counter);
+			__cpuc_flush_icache_all();
+			flush_cache_all();
+			/*
+			 * undo cpu_excp to cancel nest_panic code, see entry.S
+			 */
+			if (!user_mode(regs))
+				thread->cpu_excp--;
+			return 0;
+		} else if (*prev_undefinstr_counter < 2) {
+			pr_alert("%dth Time Recovery curr:%p pc=%p:%p\n",
+				*prev_undefinstr_counter + 1,
+				(void *)current, (void *)pc,
+				(void *)*prev_undefinstr_pc);
+			*prev_undefinstr_counter += 1;
+			put_cpu_var(__prev_undefinstr_pc);
+			put_cpu_var(__prev_undefinstr_counter);
+			__cpuc_flush_icache_all();
+			flush_cache_all();
+			/*
+			 * undo cpu_excp to cancel nest_panic code, see entry.S
+			 */
+			if (!user_mode(regs))
+				thread->cpu_excp--;
+			return 0;
+		}
+		*prev_undefinstr_counter += 1;
+
+		if (*prev_undefinstr_counter >= 2) {
+			*prev_undefinstr_pc = 0;
+			*prev_undefinstr_counter = 0;
+		}
+		put_cpu_var(__prev_undefinstr_pc);
+		put_cpu_var(__prev_undefinstr_counter);
+		pr_alert("Go to ARM Notify Die curr:%p pc=%p:%p\n",
+			(void *)current, (void *)pc, (void *)*prev_undefinstr_pc);
+	}
+	return 1;
 }
--- drivers/misc/mediatek/mem/mt6797/mt_hooks_64.c
+++ drivers/misc/mediatek/mem/mt6797/mt_hooks_64.c
@@ -2,6 +2,11 @@
 #include <linux/sizes.h>
 #include <linux/printk.h>
 #include <linux/mm.h>
+#include <linux/compat.h>
+#include <linux/init.h>
+#include <asm/traps.h>
+#include <asm/ptrace.h>
+#include <asm/cacheflush.h>
 #include <asm/pgtable.h>
 #include <asm/atomic.h>
 
@@ -28,4 +33,76 @@ void ioremap_debug_hook_func(phys_addr_t phys_addr, size_t size, pgprot_t prot)
 	pr_err("ioremap to 0x1001AXXX more than once (%pa, %zx)\n",
 			&phys_addr, size);
 	BUG();
+}
+
+static DEFINE_PER_CPU(void *, __prev_undefinstr_pc);
+static DEFINE_PER_CPU(int, __prev_undefinstr_counter);
+
+/*
+ * return 0 if code is fixed
+ * return 1 if the undefined instruction cannot be fixed.
+ */
+int arm_undefinstr_retry(struct pt_regs *regs, unsigned int instr)
+{
+	void __user *pc = (void __user *)instruction_pointer(regs);
+	struct thread_info *thread = current_thread_info();
+	/*
+	 * Place the SIGILL ICache Invalidate after the Debugger
+	 * Undefined-Instruction Solution.
+	 */
+	if ((user_mode(regs)) || processor_mode(regs) == PSR_MODE_EL1h) {
+		void **prev_undefinstr_pc = &get_cpu_var(__prev_undefinstr_pc);
+		int *prev_undefinstr_counter = &get_cpu_var(__prev_undefinstr_counter);
+
+		pr_alert("USR_MODE / SVC_MODE Undefined Instruction Address curr:%p pc=%p:%p, instr: 0x%x compat: %s\n",
+			(void *)current, (void *)pc, (void *)*prev_undefinstr_pc, instr,
+			is_compat_task() ? "yes" : "no");
+		if (*prev_undefinstr_pc != pc) {
+			/*
+			 * If the current process or program counter is changed...
+			 * renew the counter.
+			 */
+			pr_alert("First Time Recovery curr:%p pc=%p:%p\n",
+				(void *)current, (void *)pc, (void *)*prev_undefinstr_pc);
+			*prev_undefinstr_pc = pc;
+			*prev_undefinstr_counter = 0;
+			put_cpu_var(__prev_undefinstr_pc);
+			put_cpu_var(__prev_undefinstr_counter);
+			__flush_icache_all();
+			flush_cache_all();
+			/*
+			 * undo cpu_excp to cancel nest_panic code, see entry.S
+			 */
+			if (!user_mode(regs))
+				thread->cpu_excp--;
+			return 0;
+		} else if (*prev_undefinstr_counter < 2) {
+			pr_alert("%dth Time Recovery curr:%p pc=%p:%p\n",
+				*prev_undefinstr_counter + 1,
+				(void *)current, (void *)pc,
+				(void *)*prev_undefinstr_pc);
+			*prev_undefinstr_counter += 1;
+			put_cpu_var(__prev_undefinstr_pc);
+			put_cpu_var(__prev_undefinstr_counter);
+			__flush_icache_all();
+			flush_cache_all();
+			/*
+			 * undo cpu_excp to cancel nest_panic code, see entry.S
+			 */
+			if (!user_mode(regs))
+				thread->cpu_excp--;
+			return 0;
+		}
+		*prev_undefinstr_counter += 1;
+
+		if (*prev_undefinstr_counter >= 2) {
+			*prev_undefinstr_pc = 0;
+			*prev_undefinstr_counter = 0;
+		}
+		put_cpu_var(__prev_undefinstr_pc);
+		put_cpu_var(__prev_undefinstr_counter);
+		pr_alert("Go to ARM Notify Die curr:%p pc=%p:%p\n",
+			(void *)current, (void *)pc, (void *)*prev_undefinstr_pc);
+	}
+	return 1;
 }
