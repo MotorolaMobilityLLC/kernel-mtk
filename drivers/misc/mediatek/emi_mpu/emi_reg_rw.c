ea72171ef9091c81252a1804bfae7e1e8d838300
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 18:42:38 +0800

    [ALPS01979197] emi: emi mpu & bandwidth limited kernel driver porting
    [Detail] emi mpu & bandwidth limited kernel driver porting
    The emi mpu is memory protect unit which can setup hw promission to
    access memory.In SOC case, there are lots of hw need to access memory
    which like modem, wifi, cpu. The hw direct access memory which cant
    protect by mmu. The emi mpu can solve memory corruption quickly to let
    us know which hw device access memory incorrect.
    emi bandwidth limited is use to arrange hw access memory priority &
    usage amount.
    
    [Solution] emi mpu & bandwidth limited kernel driver porting
    
    [Feature] emi mpu
    
    MTK-Commit-Id: f1d896299aad3e667e69d9ead6b72b03ed4cef38
    
    Change-Id: Ic538195a8d69c96e10643e5ee01a0332c347f1a2
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS01979197

--- drivers/misc/mediatek/emi_mpu/emi_reg_rw.c
+++ drivers/misc/mediatek/emi_mpu/emi_reg_rw.c
@@ -22,6 +22,7 @@
 #include "mach/emi_mpu.h"
 #include "mach/mt_secure_api.h"
 
+static void __iomem *emi_base;
 
 #if defined(CONFIG_ARM_PSCI) || defined(CONFIG_MTK_PSCI)
 static int is_emi_mpu_reg(unsigned int offset)
@@ -41,6 +42,8 @@ void mt_emi_reg_write(unsigned int data, unsigned int offset)
 		return;
 	}
 #endif
+	if (emi_base)
+		mt_reg_sync_writel(data, emi_base + offset);
 }
 
 unsigned int mt_emi_reg_read(unsigned int offset)
@@ -49,14 +52,28 @@ unsigned int mt_emi_reg_read(unsigned int offset)
 	if (is_emi_mpu_reg(offset))
 		return (unsigned int)emi_mpu_smc_read(offset);
 #endif
-		return 0;
+	if (emi_base)
+		return readl((const void __iomem *)(emi_base + offset));
+
+	return 0;
 }
 
 int mt_emi_mpu_set_region_protection(unsigned int start,
-unsigned int end, unsigned int region_permission)
+				     unsigned int end,
+				     unsigned int region_permission)
 {
 #if defined(CONFIG_ARM_PSCI) || defined(CONFIG_MTK_PSCI)
-		return emi_mpu_smc_set(start, end, region_permission);
+	return emi_mpu_smc_set(start, end, region_permission);
 #endif
-		return 0;
+	return 0;
+}
+
+void mt_emi_reg_base_set(void *base)
+{
+	emi_base = base;
+}
+
+void *mt_emi_reg_base_get(void)
+{
+	return emi_base;
 }
