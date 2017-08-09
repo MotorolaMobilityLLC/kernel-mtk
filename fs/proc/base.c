a46db146a5f5fe3fb709b837d6e19c62ac2a0e3f
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 19:08:40 +0800

    [ALPS02294086] fs: oom_adj and oom_score_adj change round
    [Detail]
        oom_adj/oom_score_adj change dont consider decimal over 0.5
    round up, so oom_adj->oom_score_adj->oom_adj not right
    
    [Solution]
        add decimal round handle for oom_adj<->oom_score_adj
    
    [Feature] Others
    
    MTK-Commit-Id: 3567113ddb487d16f2b44abf0cda62836d91c9d6
    
    Change-Id: Ia0ef45c0d744003c9a04ab2105e4694c3ece23d2
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02294086

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -821,9 +821,8 @@ static ssize_t oom_adj_read(struct file *file, char __user *buf, size_t count,
 	if (lock_task_sighand(task, &flags)) {
 		if (task->signal->oom_score_adj == OOM_SCORE_ADJ_MAX)
 			oom_adj = OOM_ADJUST_MAX;
-		else
-			oom_adj = (task->signal->oom_score_adj * -OOM_DISABLE) /
-				  OOM_SCORE_ADJ_MAX;
+		else  /*modify for oom_score_adj->oom_adj round*/
+			oom_adj = ((task->signal->oom_score_adj * -OOM_DISABLE * 10)/OOM_SCORE_ADJ_MAX+5)/10;
 		unlock_task_sighand(task, &flags);
 	}
 	put_task_struct(task);
@@ -880,8 +879,8 @@ static ssize_t oom_adj_write(struct file *file, const char __user *buf,
 	 */
 	if (oom_adj == OOM_ADJUST_MAX)
 		oom_adj = OOM_SCORE_ADJ_MAX;
-	else
-		oom_adj = (oom_adj * OOM_SCORE_ADJ_MAX) / -OOM_DISABLE;
+	else   /*modify for oom_adj->oom_score_adj round*/
+		oom_adj = ((oom_adj * OOM_SCORE_ADJ_MAX * 10) / -OOM_DISABLE + 5)/10;
 
 	if (oom_adj < task->signal->oom_score_adj &&
 	    !capable(CAP_SYS_RESOURCE)) {
