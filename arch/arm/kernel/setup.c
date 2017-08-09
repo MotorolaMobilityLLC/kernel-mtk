db9fe7b22b6142e590b6a2814b09d749ab9424e8
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 22:25:49 +0800

    [ALPS02337513] Kernel: Add Processor info for compatibility
    [Detail]
    Add Processor info in /proc/cpuinfo for backward-compatibility
    
    [Solution]
    Add Processor info in /proc/cpuinfo
    
    [Feature] Others
    
    MTK-Commit-Id: 9a5f8f469ac07295f2536898b88970ed8d3d5876
    
    Change-Id: I8c69c98c1cf8113829a39f2a161195701f371540
    CR-Id: ALPS02337513
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -1040,6 +1040,11 @@ static int c_show(struct seq_file *m, void *v)
 	int i, j;
 	u32 cpuid;
 
+	pr_err("Dump cpuinfo\n");
+
+	seq_printf(m, "Processor\t: %s rev %d (%s)\n",
+		   cpu_name, read_cpuid_id() & 15, ELF_PLATFORM);
+
 	for_each_online_cpu(i) {
 		/*
 		 * glibc reads /proc/cpuinfo to determine the number of
