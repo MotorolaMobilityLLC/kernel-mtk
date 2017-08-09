7d56fb373bfce23d2e24e98a2b75da1e4942df09
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 20:12:56 +0800

    [ALPS02312980] lastpc: mt8173: Enable lastpc dump
    [Detail] Dump lastpc during bootup
    
    [Solution] Add lastpc driver for mt8173
    
    [Feature] lastpc
    
    MTK-Commit-Id: adf9236eac2b4e35f98d4547e6f9f233403cc042
    
    Change-Id: I252cc40f98e386f453174fc7e91ed202a9b20493
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02312980

--- drivers/misc/mediatek/debug_latch/lastpc/lastpc.c
+++ drivers/misc/mediatek/debug_latch/lastpc/lastpc.c
@@ -17,6 +17,7 @@ static const struct of_device_id lastpc_of_ids[] = {
 	{ .compatible = "mediatek,mt6580-mcucfg", },
 	{ .compatible = "mediatek,mt6735-mcucfg", },
 	{ .compatible = "mediatek,mt8163-mcucfg", },
+	{ .compatible = "mediatek,mt8173-mcucfg", },
 	{}
 };
 
