097348fbcc835fec64a521637b09eb5a841f38f0
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 14:56:59 +0800

    [ALPS02188244] cameraisp: ISP driver migration
    [Detail]
    Camera ISP D1 driver migration
    
    [Solution]
    Sync code from P4 ALPS_SW/TRUNK/L1
    
    [Feature]
    Camera ISP
    
    MTK-Commit-Id: c4f87d9c6661da8fd8d8097cbe7c4b5bb0757b6c
    
    Change-Id: I9da578504c4d603a844be1ff69d0655b870f4923
    CR-Id: ALPS02188244
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>

---
+++ drivers/misc/mediatek/cameraisp/src/mt6735/inc/mt_typedefs.h
@@ -0,0 +1,23 @@
+#ifndef _TYPEDEFS_H
+#define _TYPEDEFS_H
+
+#ifndef BOOL
+typedef unsigned char   BOOL;
+#endif
+
+typedef unsigned char   UINT8;
+typedef unsigned int    UINT32;
+typedef signed int      INT32;
+
+#if 0
+typedef unsigned short  UINT16;
+typedef unsigned short  USHORT;
+typedef signed char     INT8;
+typedef signed short    INT16;
+typedef unsigned int    DWORD;
+typedef void            VOID;
+typedef unsigned char   BYTE;
+typedef float           FLOAT;
+#endif
+
+#endif
