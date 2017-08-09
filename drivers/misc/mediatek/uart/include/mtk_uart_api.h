adec9a7edf144e407d435d8efe6bb4ec29d40174
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 14:28:03 +0800

    [ALPS02068907] UART: add exported header file
    [Detail]
    UART exports several APIs to other modules, but they dont
    have dedicated header file and makes other modules hard to
    reference them.
    
    [Solution]
    add a mtk_uart_api.h for these exported APIs.
    
    [Feature] Others
    
    MTK-Commit-Id: e246ba3dd90e7dcf077c30ba35cbe66562109792
    
    Change-Id: Idaf5f3c5326251b0faafcfff71e47d9b761508da
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02068907

---
+++ drivers/misc/mediatek/uart/include/mtk_uart_api.h
@@ -0,0 +1,10 @@
+#ifndef __MTK_UART_EXPORTED_API__
+#define __MTK_UART_EXPORTED_API__
+
+int request_uart_to_sleep(void);
+int request_uart_to_wakeup(void);
+
+void stop_log(void);
+void dump_uart_history(void);
+
+#endif /* __MTK_UART_EXPORTED_API__ */
