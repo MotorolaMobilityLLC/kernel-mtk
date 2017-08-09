bd102c675d4ce3d48f9eb7ada062cbf89b3f90e5
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 17:15:01 +0800

    [ALPS02246109] c2k ccci: export c2k API for sdio module
    [Detail]
    board.c is removed on kernel 3.18, so export related API in
    c2k_sdio_platform.c for sdio driver
    [Solution]
    add c2k_sdio_platform.c and header
    [Feature] Modem Interface Driver
    
    MTK-Commit-Id: 12c9abd7e0e80c63da01f0beaafd03b8e8835d56
    
    Change-Id: I9c2a96718c996c8bab23d8cc2b0486c30e9e52c8
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02246109

--- drivers/misc/mediatek/include/mt-plat/mt6735/include/mach/mt_c2k_sdio.h
+++ drivers/misc/mediatek/include/mt-plat/mt6735/include/mach/mt_c2k_sdio.h
@@ -2,29 +2,20 @@
 #define __MT_C2K_SDIO_H__
 
 #include <linux/pm.h>
-#include <linux/mmc/mmc.h>
 
-/* typedef void (*sdio_irq_handler_t)(void *); */
+#include <linux/mmc/sdio_func.h>
+
 typedef void (*pm_callback_t)(pm_message_t state, void *data);
 
-static inline void c2k_sdio_request_eirq(sdio_irq_handler_t irq_handler, void *data)
-{
-	pr_err("to be implemented\n");
-}
-
-static inline void c2k_sdio_enable_eirq(void)
-{
-	pr_err("to be implemented\n");
-}
-
-static inline void c2k_sdio_disable_eirq(void)
-{
-	pr_err("to be implemented\n");
-}
-
-static inline void c2k_sdio_register_pm(pm_callback_t pm_cb, void *data)
-{
-	pr_err("to be implemented\n");
-}
+extern void c2k_sdio_request_eirq(sdio_irq_handler_t irq_handler, void *data);
+extern void c2k_sdio_enable_eirq(void);
+extern void c2k_sdio_disable_eirq(void);
+extern void c2k_sdio_register_pm(pm_callback_t pm_cb, void *data);
+/*
+extern void c2k_sdio_install_eirq(void);
+extern void c2k_sdio_uninstall_eirq(void);
+extern void via_sdio_on (int sdio_port_num);
+extern void via_sdio_off (int sdio_port_num);
+*/
 
 #endif
