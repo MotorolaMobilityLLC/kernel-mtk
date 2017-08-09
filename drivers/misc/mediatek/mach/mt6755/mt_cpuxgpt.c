62e8096a8eccd0b227fe91c2e3ec00ebf6b53328
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 19:22:21 +0800

    [ALPS02108996] CPUXGPT: Add irq_percpu flag on request_irq
    [Detail]
    Add the irq_percpu flag to make CPUXGPT irq
    to restrict the irq distribution to percpu.
    
    [Solution]
    Add irq_percpu flag on request_irq().
    
    [Feature] null
    
    MTK-Commit-Id: 62bde844855f7f67a1616d4e2d8a6dceb3fa7daf
    
    Change-Id: I11e8c5dedde1521eebf05eaa4d6a030f8be5c183
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02108996

--- drivers/misc/mediatek/mach/mt6755/mt_cpuxgpt.c
+++ drivers/misc/mediatek/mach/mt6755/mt_cpuxgpt.c
@@ -393,7 +393,8 @@ int cpu_xgpt_register_timer(unsigned int id, irqreturn_t (*func)(int irq, void *
 /*don't trigger IRQ to CPU0 mt_gic_cfg_irq2cpu(irq_id,0,0);*/
 /*trigger IRQ to CPUx mt_gic_cfg_irq2cpu(irq_id,(irq_id - CPUXGPT_IRQID_BASE)%num_possible_cpus(),1); */
 
-		ret = request_irq(irq_id, (irq_handler_t)cpuxgpt_irq_handler[id], IRQF_TRIGGER_HIGH, name, NULL);
+		ret = request_irq(irq_id, (irq_handler_t)cpuxgpt_irq_handler[id],
+			IRQF_TRIGGER_HIGH | IRQF_PERCPU, name, NULL);
 		if (ret != 0) {
 			pr_err("%s:%s fail to register irq\n", __func__, name);
 			return ret;
