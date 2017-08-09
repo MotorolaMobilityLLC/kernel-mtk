3df71ad96b24cde99e177a4a6136628236ef3fd7
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 16:03:28 +0800

    [ALPS02223964] lcm : porting dual dsi output for panel
    [Detail] porting dual dsi output for panel
    
    [Solution] porting dual dsi output for panel
    
    [Feature] LCM
    
    MTK-Commit-Id: b45cf6b529091638d651bd5657bbb032daf030bf
    
    Change-Id: I17daac7e6a36dff435740e9bd93f4ada8b8560fd
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02223964

---
+++ drivers/misc/mediatek/lcm/r69429_wqxga_dsi_vdo/r69429_wqxga_dsi_vdo.h
@@ -0,0 +1,8 @@
+#ifndef _R69429_WQXGA_DSI_VDO_
+#define _R69429_WQXGA_DSI_VDO_
+
+extern unsigned int GPIO_LCD_PWR_EN;
+extern unsigned int GPIO_LCD_RST_EN;
+extern unsigned int GPIO_LCD_LED_EN;
+
+#endif
