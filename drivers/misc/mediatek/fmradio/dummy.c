8b5244eb95f46ffea0cac788049a7d1ab14e8b8f
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 07:08:38 +0800

    [ALPS02631772] FM Radio: fix build error of auto merge
    [Detail] sync FM code
    
    [Solution] sync FM code
    
    MTK-Commit-Id: ff1c941829d97ca56cb5c59d392fabcf1c36d820
    
    Change-Id: Idfed9f3f3a5e155d89fc88edbf5085e020948d5e
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02631772
    Feature: FM Radio

--- drivers/misc/mediatek/fmradio/dummy.c
+++
@@ -1,3 +0,0 @@
-/* dummy.c
- * This dummy file is just for meetting the build system's rule.
- */
--- drivers/misc/mediatek/fmradio/inc/fm_priv_log.h
+++
@@ -1,4 +0,0 @@
-#ifndef __FM_PRIV_LOG_H__
-#define __FM_PRIV_LOG_H__
-
-#endif /* __FM_PRIV_LOG_H__ */
