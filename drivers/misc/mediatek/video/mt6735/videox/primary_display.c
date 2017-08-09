371a2bdebed8ee17e117ac3a41d1448b82c0dcac
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 06:24:46 +0800

    [ALPS02358635] DISP:Revise sw noncc flow
    [Detail]
    The sw noncc set twice when reading lcm register in
    ESD check.
    
    [Solution]
    Move sw noncc when ESD check by reading TE in vdo mode.
    
    MTK-Commit-Id: 43f7b509af9480a6187fc9cbd45d1742b7c03dcb
    
    Change-Id: I55d4914f14d6e911286f0adbd437122eed92b5bc
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02358635
    Feature: Display Driver (DSI Interface)

--- drivers/misc/mediatek/video/mt6735/videox/primary_display.c
+++ drivers/misc/mediatek/video/mt6735/videox/primary_display.c
@@ -3582,6 +3582,66 @@ int primary_display_esd_check(void)
 		MMProfileLogEx(ddp_mmp_get_events()->esd_extte, MMProfileFlagStart, 0, 0);
 		if (primary_display_is_video_mode()) {
 			primary_display_switch_esd_mode(1);
+
+			/* use cmdq to pull DSI clk lane */
+			if (primary_display_cmdq_enabled()) {
+				_primary_path_lock(__func__);
+
+				/* 0.create esd check cmdq */
+				cmdqRecCreate(CMDQ_SCENARIO_DISP_ESD_CHECK,
+					      &(pgc->cmdq_handle_config_esd));
+				_primary_path_unlock(__func__);
+
+				primary_display_esd_cust_bycmdq(1);
+
+				/* 1.reset */
+				cmdqRecReset(pgc->cmdq_handle_config_esd);
+
+				/* wait stream eof first */
+				ret =
+				    cmdqRecWait(pgc->cmdq_handle_config_esd,
+						CMDQ_EVENT_DISP_RDMA0_EOF);
+
+				cmdqRecWait(pgc->cmdq_handle_config_esd,
+					    CMDQ_EVENT_MUTEX0_STREAM_EOF);
+
+				_primary_path_lock(__func__);
+				/* 2.stop dsi vdo mode */
+				dpmgr_path_build_cmdq(pgc->dpmgr_handle,
+						      pgc->cmdq_handle_config_esd,
+						      CMDQ_STOP_VDO_MODE);
+
+				/* 3.pull DSI clock lane */
+				DSI_sw_clk_trail_cmdq(0, pgc->cmdq_handle_config_esd);
+				DSI_manual_enter_HS(pgc->cmdq_handle_config_esd);
+
+				/* 4.start dsi vdo mode */
+				dpmgr_path_build_cmdq(pgc->dpmgr_handle,
+						      pgc->cmdq_handle_config_esd,
+						      CMDQ_START_VDO_MODE);
+
+
+				/* 5. trigger path */
+				cmdqRecClearEventToken(pgc->cmdq_handle_config_esd,
+						       CMDQ_EVENT_MUTEX0_STREAM_EOF);
+
+				if (gEnableSWTrigger == 1)
+					DISP_REG_SET(pgc->cmdq_handle_config_esd,
+						     DISP_REG_CONFIG_MUTEX_EN
+						     (DISP_OVL_SEPARATE_MUTEX_ID), 1);
+
+				dpmgr_path_trigger(pgc->dpmgr_handle, pgc->cmdq_handle_config_esd,
+						   CMDQ_ENABLE);
+
+				_primary_path_unlock(__func__);
+				cmdqRecFlush(pgc->cmdq_handle_config_esd);
+
+				primary_display_esd_cust_bycmdq(0);
+
+				cmdqRecDestroy(pgc->cmdq_handle_config_esd);
+				pgc->cmdq_handle_config_esd = NULL;
+			}
+
 			if (_need_register_eint()) {
 				MMProfileLogEx(ddp_mmp_get_events()->esd_extte, MMProfileFlagPulse, 1, 1);
 
