64091c12e86579b30b49b1ac8bfe0b1a6898f49b
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 05:54:03 +0800

    [ALPS02213055] Camera: File header check
    [Detail] File header check for kernel-3.18
    
    MTK-Commit-Id: d628270f1d5023cd0c4022ca06c04531d6ca0612
    
    Change-Id: Ie9e5abee12d46710ee1bc11166ea38092fba6643
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02213055
    Feature: [Android Default] AF (Auto Focus)

--- drivers/misc/mediatek/lens/common/lc898212xdaf/LC898212XDAF.c
+++ drivers/misc/mediatek/lens/common/lc898212xdaf/LC898212XDAF.c
@@ -1,5 +1,18 @@
 /*
- * BU6429AF voice coil motor driver
+* Copyright (C) 2016 MediaTek Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+*/
+
+/*
+ * LC898212XDAF voice coil motor driver
  *
  *
  */
