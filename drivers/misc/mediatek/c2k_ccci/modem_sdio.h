ee7941916c029375046eb735d6421251b8364ff1
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 15:24:45 +0800

    [ALPS02203174] USB:C2K USB ready for GIT
    [Detail]
    1st version for C2K USB part
    
    [Solution]
    N/A
    
    [Feature] USB
    
    MTK-Commit-Id: 2bcf5a78a0cf0a939be70c912ad7b6dc2c67d9fc
    
    Change-Id: I05771c8dd1f475a69d8de34116007ba206df13df
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02203174

---
+++ drivers/misc/mediatek/c2k_ccci/modem_sdio.h
@@ -0,0 +1,9 @@
+#ifndef MODEM_SDIO_H
+#define MODEM_SDIO_H
+#ifdef CONFIG_EVDO_DT_VIA_SUPPORT
+#define FLS_CH_ID			3		/*flashlessd channel id*/
+#else
+#define FLS_CH_ID			4	/*flashlessd channel id*/
+#endif
+#endif
+
