5fda6e4b902309c693dca149d797af1e15ea50bd
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 02:57:02 +0800

    [ALPS02297087] Trustonic: core migration
    [Detail]
    Enable core migration support for Trustonic TEE
    
    [Solution]
    Enable core migration in ppm module
    
    MTK-Commit-Id: 9aa49a1e4962a1c29130b89e975943004cd348cf
    
    Change-Id: If26a007bd929c0531f21dcd30c3918e65989c39f
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02297087
    Feature: Trustonic TEE (Trusted Execution Environment)

--- drivers/misc/mediatek/base/power/ppm_v1/src/mach/mt6797/mt_ppm_platform.h
+++ drivers/misc/mediatek/base/power/ppm_v1/src/mach/mt6797/mt_ppm_platform.h
@@ -32,10 +32,6 @@ extern unsigned int hps_get_hvytsk(unsigned int cluster_id);
 #define DYNAMIC_TABLE2REAL_PERCENTAGE	(58)
 #endif
 
-#ifdef CONFIG_TRUSTONIC_TEE_SUPPORT
-#define PPM_DISABLE_CLUSTER_MIGRATION	(1)
-#endif
-
 #define PPM_POWER_TABLE_CALIBRATION	(1)
 #ifdef PPM_POWER_TABLE_CALIBRATION
 #include "mt_static_power.h"
