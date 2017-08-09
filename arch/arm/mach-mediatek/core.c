8759dfcfc794c73e46f264abee1df6451774ba6f
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 14:14:10 +0800

    [ALPS02066172] Platform: files for 32-bit bringup
    [Detail]
    Basic files for 32-bit bringup
    
    [Solution]
    1. update arch/arm/Makefile and arch/arm/mach-mediatek/Makefile
    2. add dummy arch/arm/mach-mediatek/Makefile.boot and
    arch/arm/mach-mediatek/include/mach/uncompress.h
    3. add arch/arm/mach-mediatek/core.c for DT_MACHINE_START
    
    [Feature] Others
    
    MTK-Commit-Id: bc17fcc79d977f3c72e044f5143807e5d727e973
    
    Change-Id: I7abf7378fbc2acace449d1fdfa580c6a489913ff
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02066172

---
+++ arch/arm/mach-mediatek/core.c
@@ -0,0 +1,24 @@
+
+#include <linux/pm.h>
+#include <linux/bug.h>
+#include <linux/memblock.h>
+#include <asm/mach/arch.h>
+#include <asm/mach/time.h>
+#include <asm/mach/map.h>
+#include <asm/mach-types.h>
+#include <asm/hardware/cache-l2x0.h>
+#include <asm/smp_scu.h>
+#include <asm/page.h>
+#include <mach/irqs.h>
+#include <linux/irqchip.h>
+#include <linux/of_platform.h>
+
+#ifdef CONFIG_OF
+static const char *mt_dt_match[] __initconst = {
+	"mediatek,mt6735",
+	NULL
+};
+
+DT_MACHINE_START(MT6735_DT, "MT6735")
+	.dt_compat = mt_dt_match,
+MACHINE_END
