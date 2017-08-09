62ff24a53c8c3800b75bb96f6e50dcf686f916ee
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 06:44:05 +0800

    [ALPS02580584] lcm: mediatek: remove un-used code
    remove unused data(SSC data) from panel driver
    
    MTK-Commit-Id: c00a19d12d68fd8c2bfbcb86a9ce6942bed870d3
    
    Change-Id: I29cda3096c35cff91e82886f1dd32ef2621b9aba
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    Feature: lcm
    CR-Id: ALPS02580584
    (cherry picked from commit 53cef5734b03651369220c9dff78c685c83af53a)

--- drivers/gpu/drm/panel/panel-truly-tdo-bd0598u40004.c
+++ drivers/gpu/drm/panel/panel-truly-tdo-bd0598u40004.c
@@ -48,10 +48,6 @@ struct truly {
 	bool enabled;
 
 	int error;
-
-	bool ssc_disable;
-	int ssc_range;
-	int ssc_data;
 };
 
 #define truly_dcs_write_seq(ctx, seq...) \
@@ -841,9 +837,6 @@ static const struct panel_desc truly_panel = {
 static int truly_get_modes(struct drm_panel *panel)
 {
 	struct drm_display_mode *mode;
-	struct truly *ctx = panel_to_truly(panel);
-
-	default_mode.private = &ctx->ssc_data;
 
 	mode = drm_mode_duplicate(panel->drm, &default_mode);
 	if (!mode) {
@@ -884,16 +877,6 @@ static int truly_probe(struct mipi_dsi_device *dsi)
 	mipi_dsi_set_drvdata(dsi, ctx);
 
 	ctx->dev = dev;
-
-	/* Spread Spectrum Clock is option function for panel,
-	* some panel may not support, and need to disable it sometime,
-	* so please check panel first before you enable SSC.
-	*/
-
-	ctx->ssc_disable = false;	/* whether mipitx ssc is disable or not? */
-					/* e.g. false: enable ssc, true: disable */
-	ctx->ssc_range = 5;		/* mipitx ssc range must be 0 ~ 8 */
-	ctx->ssc_data = ctx->ssc_disable << 4 | ctx->ssc_range;
 
 	dsi->lanes = 4;
 	dsi->format = MIPI_DSI_FMT_RGB888;
