259cf0f683958affa2f84c642d441a942b9f0662
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 22:44:57 +0800

    [ALPS02346757] USB:Jade driving capability porting
    [Detail] Jade driving capability porting for HQA test
    
    [Solution] Jade driving capability porting for HQA test
    
    [Feature] USB
    
    MTK-Commit-Id: b35e7924db6779f0f1ebb2872aac401699dd1ba5
    
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    Change-Id: Iec4590ca1f29b29757c51e7d52762beecdda510b
    CR-Id: ALPS02346757

--- drivers/misc/mediatek/mu3d/drv/musb_core.c
+++ drivers/misc/mediatek/mu3d/drv/musb_core.c
@@ -2112,6 +2112,11 @@ allocate_instance(struct device *dev, struct musb_hdrc_config *config, void __io
 	/* memset(musb->xceiv, 0, sizeof(struct otg_transceiver)); */
 	/* musb->xceiv->state = OTG_STATE_B_IDLE; //initial its value */
 
+#ifdef CONFIG_DEBUG_FS
+	if (usb20_phy_init_debugfs())
+		os_printk(K_ERR, "usb20_phy_init_debugfs fail!\n");
+#endif
+
 	return musb;
 }
 
