7f96e34c9ff9516183c5001846f741bd04e09db2
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 07:15:00 +0800

    [ALPS02652162] ccci: remove sbp definition in kernel
    [Detail]
    Since SBP setting value gotten from project config,
    No need define SBP option in kernel config
    
    [Solution]
    Remove SBP definition in kernel
    
    MTK-Commit-Id: aeb22b7d4d6703b48407b3a69f250c8b9c334902
    
    Change-Id: I6aba07febe5df50b2fe51dd3ed91cdefb050330e
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02652162
    Feature: Modem Interface Driver

--- drivers/misc/mediatek/dual_ccci/include/ccci_common.h
+++ drivers/misc/mediatek/dual_ccci/include/ccci_common.h
@@ -328,9 +328,6 @@ int set_sim_type(int md_id, int data);
 int get_sim_type(int md_id, int *p_sim_type);
 int enable_get_sim_type(int md_id, unsigned int enable);
 void ccci_dump_md_register(int md_id);
-#ifdef CONFIG_MTK_MD_SBP_CUSTOM_VALUE
-int ccci_set_md_sbp(int md_id, unsigned int md_sbp);
-#endif	/*  CONFIG_MTK_MD_SBP_CUSTOM_VALUE */
 /* Generally, AP and MD has same share memory address after hw remapp.
  * however, if hardware remapp does not work, then need software remap,
  *This variable is used to fix md phy addr does not equeal with AP.
