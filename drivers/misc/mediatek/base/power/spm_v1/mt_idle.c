e1b2c7ec72d829d82e0150a870b263985beb1023
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 17:21:36 +0800

    [ALPS02253699] idle: Turn on low power idle
    [Detail] turn on SODI and deepidle
    
    [Solution] turn on SODI and deepidle
    
    [Feature] Power Management
    
    MTK-Commit-Id: 274af7bd9348c55c3b2ba1fa04ca0f4bdee6796a
    
    Change-Id: Iaaef3af9492482728b611c7e4973c59ad08c979e
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02253699

--- drivers/misc/mediatek/base/power/spm_v1/mt_idle.c
+++ drivers/misc/mediatek/base/power/spm_v1/mt_idle.c
@@ -302,8 +302,8 @@ static unsigned int slidle_condition_mask[NR_GRPS] = {
 #elif defined(CONFIG_ARCH_MT6735M)
 /* Idle handler on/off */
 static int idle_switch[NR_TYPES] = {
-	0,  /* dpidle switch */
-	0,  /* soidle switch */
+	1,  /* dpidle switch */
+	1,  /* soidle switch */
 	0,  /* slidle switch */
 	1,  /* rgidle switch */
 };
