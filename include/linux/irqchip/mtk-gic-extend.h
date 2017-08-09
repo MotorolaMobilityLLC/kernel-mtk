f491cd2354159661868de740d939b2cc4b2e7d3c
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 18:36:18 +0800

    [ALPS02097046] misc: mediatek: add gic extend driver
    [Detail]SPM can not work without mt_irq_xx API.
    [Solution]
    1.config MTK_GIC_EXT in arm64 kconfig.
    2.add ext_gic in misc/mediatek.
    3.add mtk-gic-extend.h for provide mt_irq_xx API.
    [Feature] ext_gic  driver
    
    MTK-Commit-Id: df668cdf9307932ddc622390eb88c3a50fd82eeb
    
    Change-Id: If9e3f65b70c94cfb29500d16d151e8e3e981aad1
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02097046
    Depend-Changelist:

---
+++ include/linux/irqchip/mtk-gic-extend.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2014 MediaTek Inc.
+ * Author: Maoguang.Meng <maoguang.meng@mediatek.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __MTK_GIC_EXTEND_H
+#define __MTK_GIC_EXTEND_H
+
+#define MT_EDGE_SENSITIVE	0
+#define MT_LEVEL_SENSITIVE	1
+#define MT_POLARITY_LOW		0
+#define MT_POLARITY_HIGH	1
+
+enum {
+	IRQ_MASK_HEADER = 0xF1F1F1F1,
+	IRQ_MASK_FOOTER = 0xF2F2F2F2
+};
+
+struct mtk_irq_mask {
+	unsigned int header;	/* for error checking */
+	__u32 mask0;
+	__u32 mask1;
+	__u32 mask2;
+	__u32 mask3;
+	__u32 mask4;
+	__u32 mask5;
+	__u32 mask6;
+	__u32 mask7;
+	__u32 mask8;
+	unsigned int footer;	/* for error checking */
+};
+
+void mt_irq_unmask_for_sleep(unsigned int irq);
+void mt_irq_mask_for_sleep(unsigned int irq);
+int mt_irq_mask_all(struct mtk_irq_mask *mask);
+int mt_irq_mask_restore(struct mtk_irq_mask *mask);
+void mt_irq_set_pending_for_sleep(unsigned int irq);
+extern void mt_irq_set_pending(unsigned int irq);
+extern unsigned int mt_irq_get_pending(unsigned int irq);
+#endif
+
