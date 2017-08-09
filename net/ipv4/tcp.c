fd7608f9e9ccf66a3d08e02486d448ecd0224c78
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 17:27:25 +0800

    [ALPS02248799] kernel bug fixed:fix tcp_nuke_addr
    [Detail]
    when tcp state is TIME_WAIT,enter tcp_done(), spin lock is locked
    in fact,stack can destroy TIME_WAIT socket
    [Solution]
    pass tcp_done to lock spin lock
    [Feature] [Android Default] Wi-Fi Driver for SoC
    
    MTK-Commit-Id: a00566b1b0afe7a6cd773f89dc589b51b734bcf3
    
    Change-Id: Ic351cf034544789af1fdc54cf470eac56de46d45
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02248799

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -3212,6 +3212,8 @@ restart:
 
 			if (sysctl_ip_dynaddr && sk->sk_state == TCP_SYN_SENT)
 				continue;
+			if (sk->sk_state == TCP_TIME_WAIT)
+				continue;
 			if (sock_flag(sk, SOCK_DEAD))
 				continue;
 
