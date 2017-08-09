497dd3a16af188e27b1b208ad55d7e6067374798
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 04:30:50 +0800

    [ALPS02264561] Vcore DVFS: return 0 for the repeat request
    [Detail]
    return 0 for the repeat request
    
    [Solution]
    return 0 for the repeat request
    
    MTK-Commit-Id: bf53a85e8037d5f1351e648501b6b35f6d67b49f
    
    Change-Id: Ia71ce6efa0793698a29a2b9d334ea2d6a7a596f7
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02264561
    Feature: VCORE DVFS

--- drivers/misc/mediatek/base/power/mt6797/mt_vcorefs_manager.c
+++ drivers/misc/mediatek/base/power/mt6797/mt_vcorefs_manager.c
@@ -192,7 +192,7 @@ static int kicker_request_compare(enum dvfs_kicker kicker, enum dvfs_opp opp)
 		if (vcorefs_get_curr_opp() == vcorefs_get_prev_opp()) {
 			vcorefs_crit("opp no change, kr_tb: %d, kr: %d, opp: %d\n",
 				    kicker_table[kicker], kicker, opp);
-			return -1;
+			return 0;
 		}
 	}
 
