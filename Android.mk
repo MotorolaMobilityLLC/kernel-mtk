23d0de2a58612b79eaec09e317ccd23752b92c06
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 20:08:35 +0800

    [ALPS00924681] build script: Add copyright and license for Android.mk
    [Detail]
    Android.mk and CleanSpec.mk are part of Android configuration,
    which are different from Kernel source files under GPL 2.0.
    So their copyright and license will be processed like what AOSP
    does for external/bison/Android.mk.
    
    [Solution]
    Android.mk and CleanSpec.mk will be released with
    MTK copyright under Apache License Version 2.0.
    
    [Feature] Build System(*)
    
    MTK-Commit-Id: 4846abf1b757d41a2bca24fba8737b48ccc425a2
    
    Change-Id: I5021f14533d13a879c23c99763b24f9fae6c0dea
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS00924681

--- Android.mk
+++ Android.mk
@@ -1,3 +1,17 @@
+# Copyright (c) 2015 MediaTek Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 LOCAL_PATH := $(call my-dir)
 KERNEL_ROOT_DIR := $(PWD)
 
@@ -152,7 +166,7 @@ else
 	python device/mediatek/build/build/tools/check_kernel_config.py -c $(MTK_TARGET_PROJECT_FOLDER)/ProjectConfig.mk -k $(KERNEL_CONFIG_FILE) -p $(MTK_PROJECT_NAME)
 endif
 
-ifneq ($(filter check-kernel-dotconfig,$(MAKECMDGOALS)),)
+ifneq ($(filter check-mtk-config check-kernel-dotconfig,$(MAKECMDGOALS)),)
 .PHONY: $(TARGET_KERNEL_CONFIG)
 endif
 check-kernel-dotconfig: $(TARGET_KERNEL_CONFIG)
