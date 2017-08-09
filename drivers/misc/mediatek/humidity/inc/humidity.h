d0e65f64ab8d7ffaccfeb61a2bf320b3bceb33d6
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 03:35:08 +0800

    [ALPS02439463] Sensor: humidity print too much log
    [Detail]
    Humidity printk too much.
    
    [Solution]
    Humidity printk too much, add switch in humidity.h
    
    MTK-Commit-Id: cd7c91842657bd02cbf5684fba5654d2f3747b50
    
    Change-Id: Ic7fdfe3211e3fe86d5ab3bfdb2ffccaac86e009b
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02439463
    Feature: Others

--- drivers/misc/mediatek/humidity/inc/humidity.h
+++ drivers/misc/mediatek/humidity/inc/humidity.h
@@ -27,10 +27,23 @@
 #include "humidity_factory.h"
 
 #define HMDY_TAG					"<HUMIDITY> "
-#define HMDY_FUN(f)				pr_debug(HMDY_TAG"%s\n", __func__)
 #define HMDY_ERR(fmt, args...)	pr_err(HMDY_TAG"%s %d : "fmt, __func__, __LINE__, ##args)
-#define HMDY_LOG(fmt, args...)	pr_debug(HMDY_TAG fmt, ##args)
 #define HMDY_VER(fmt, args...)	pr_debug(HMDY_TAG"%s: "fmt, __func__, ##args)
+
+#define HMDY_LOGLEVEL 0
+
+#if ((HMDY_LOGLEVEL) >= 0)
+#define HMDY_FUN(f)				pr_debug(HMDY_TAG"%s\n", __func__)
+#else
+#define HMDY_FUN(f)
+#endif
+
+#if ((HMDY_LOGLEVEL) >= 1)
+#define HMDY_LOG(fmt, args...)	pr_debug(HMDY_TAG fmt, ##args)
+#else
+#define HMDY_LOG(fmt, args...)
+#endif
+
 
 #define OP_HMDY_DELAY		0X01
 #define	OP_HMDY_ENABLE		0X02
