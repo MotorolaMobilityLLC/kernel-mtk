c974efbe2f515c406264ea5f44610adffec174ab
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 17:27:12 +0800

    [ALPS01842822] pbm:reduce log
    [Detail] add debug sysfs for
    enable/disable pbm log
    
    [Solution] add debug sysfs for
    enable/disable pbm log
    
    [Feature] Power Management
    
    MTK-Commit-Id: 43e0c07e60babd0c393edf39613d06838af3e04c
    
    Change-Id: I8c6f5db22730206feee223ecbe5eb90142ac4146
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS01842822

--- drivers/misc/mediatek/base/power/mt6735/mt_pbm.c
+++ drivers/misc/mediatek/base/power/mt6735/mt_pbm.c
@@ -1,6 +1,3 @@
-/**************************************
- * Power Budget Manager
- **************************************/
 #define pr_fmt(fmt)		"[PBM] " fmt
 
 #include <linux/kernel.h>
@@ -17,6 +14,7 @@
 #include <linux/sched/rt.h>
 #include <linux/platform_device.h>
 #include <linux/vmalloc.h>
+#include <linux/proc_fs.h>
 
 #include <mach/mt_pbm.h>
 #include <mach/upmu_sw.h>
@@ -27,9 +25,6 @@
 
 #ifndef DISABLE_PBM_FEATURE
 
-/**************************************
- * note
- **************************************/
 /* reference PMIC */
 /* extern kal_uint32 PMIC_IMM_GetOneChannelValue(kal_uint8 dwChannel, int deCount, int trimd); */
 /* #define DLPT_PRIO_PBM 0 */
@@ -41,9 +36,8 @@
 /* void mt_cpufreq_set_power_limit_by_pbm(unsigned int limited_power){} */
 /* void mt_gpufreq_set_power_limit_by_pbm(unsigned int limited_power){} */
 
-/**************************************
- * print message level
- **************************************/
+static bool mt_pbm_debug;
+
 #define pbm_emerg(fmt, args...)	pr_emerg(fmt, ##args)
 #define pbm_alert(fmt, args...)	pr_alert(fmt, ##args)
 #define pbm_crit(fmt, args...)	pr_crit(fmt, ##args)
@@ -51,7 +45,12 @@
 #define pbm_warn(fmt, args...)	pr_warn(fmt, ##args)
 #define pbm_notice(fmt, args...)	pr_debug(fmt, ##args)
 #define pbm_info(fmt, args...)	pr_debug(fmt, ##args)
-#define pbm_debug(fmt, args...)	pr_debug(fmt, ##args)
+
+#define pbm_debug(fmt, args...)	\
+	do {			\
+		if (mt_pbm_debug)		\
+			pr_crit(fmt, ##args);	\
+	} while (0)
 
 #define BIT_CHECK(a, b) ((a) & (1<<(b)))
 
@@ -144,23 +143,6 @@ int hpf_get_power_leakage(void)
 	return hpfmgr->loading_leakage;
 }
 
-unsigned long hpf_get_power_dlpt(void)
-{
-	struct hpf *hpfmgr = &hpf_ctrl;
-
-	return hpfmgr->loading_dlpt;
-}
-
-unsigned long hpf_get_power_md(void)
-{
-	struct hpf *hpfmgr = &hpf_ctrl;
-
-	if (hpfmgr->switch_md1 | hpfmgr->switch_md2)
-		return hpfmgr->loading_md;
-	else
-		return 0;
-}
-
 unsigned long hpf_get_power_cpu(void)
 {
 	struct hpf *hpfmgr = &hpf_ctrl;
@@ -184,6 +166,23 @@ unsigned long hpf_get_power_flash(void)
 
 	if (hpfmgr->switch_flash)
 		return hpfmgr->loading_flash;
+	else
+		return 0;
+}
+
+unsigned long hpf_get_power_dlpt(void)
+{
+	struct hpf *hpfmgr = &hpf_ctrl;
+
+	return hpfmgr->loading_dlpt;
+}
+
+unsigned long hpf_get_power_md(void)
+{
+	struct hpf *hpfmgr = &hpf_ctrl;
+
+	if (hpfmgr->switch_md1 | hpfmgr->switch_md2)
+		return hpfmgr->loading_md;
 	else
 		return 0;
 }
@@ -253,18 +252,26 @@ static void pbm_allocate_budget_manager(void)
 		mt_gpufreq_set_power_limit_by_pbm(togpu);
 	}
 
-	pbm_crit("(C/G)=%d,%d => (D/L/M/F/C/G)=%d,%d,%d,%d,%d,%d (Multi:%d),%d\n",
-		 cpu, gpu, dlpt, leakage, md, flash, tocpu, togpu, multiple, cpu_lower_bound);
+	if (mt_pbm_debug) {
+		pbm_debug("(C/G)=%d,%d => (D/L/M/F/C/G)=%d,%d,%d,%d,%d,%d (Multi:%d),%d\n",
+			 cpu, gpu, dlpt, leakage, md, flash, tocpu, togpu, multiple, cpu_lower_bound);
+	} else {
+		if ((cpu > tocpu) || (gpu > togpu))
+			pbm_crit("(C/G)=%d,%d => (D/L/M/F/C/G)=%d,%d,%d,%d,%d,%d (Multi:%d),%d\n",
+				 cpu, gpu, dlpt, leakage, md, flash, tocpu, togpu, multiple, cpu_lower_bound);
+	}
 }
 
 static bool pbm_func_enable_check(void)
 {
 	struct pbm *pwrctrl = &pbm_ctrl;
 
-	if (!pwrctrl->feature_en || !pwrctrl->pbm_drv_done)
+	if (!pwrctrl->feature_en || !pwrctrl->pbm_drv_done) {
+		pbm_crit("feature_en: %d, pbm_drv_done: %d\n", pwrctrl->feature_en, pwrctrl->pbm_drv_done);
 		return false;
-	else
-		return true;
+	}
+
+	return true;
 }
 
 static bool pbm_update_table_info(enum pbm_kicker kicker, struct mrp *mrpmgr)
@@ -328,33 +335,6 @@ static bool pbm_update_table_info(enum pbm_kicker kicker, struct mrp *mrpmgr)
 
 static void pbm_wake_up_thread(enum pbm_kicker kicker, struct mrp *mrpmgr)
 {
-#if 0
-	if (atomic_read(&kthread_nreq) <= 0) {
-		atomic_inc(&kthread_nreq);
-		wake_up_process(pbm_thread);
-
-		while (kicker == KR_FLASH && mrpmgr->switch_flash == 1) {
-			if (atomic_read(&kthread_nreq) == 0)
-				return;
-		}
-	} else {
-		if (kicker == KR_FLASH && mrpmgr->switch_flash == 1) {
-			/* while (atomic_read(&kthread_nreq) > 0){} */
-			while (1) {
-				if (atomic_read(&kthread_nreq) <= 0)
-					return;
-			}
-			atomic_inc(&kthread_nreq);
-			wake_up_process(pbm_thread);
-			/* while (atomic_read(&kthread_nreq) > 0){} */
-			while (1) {
-				if (atomic_read(&kthread_nreq) <= 0)
-					return;
-			}
-			return;	/* (atomic_read(&kthread_nreq) == 0) */
-		}
-	}
-#else
 	if (atomic_read(&kthread_nreq) <= 0) {
 		atomic_inc(&kthread_nreq);
 		wake_up_process(pbm_thread);
@@ -364,7 +344,6 @@ static void pbm_wake_up_thread(enum pbm_kicker kicker, struct mrp *mrpmgr)
 		if (atomic_read(&kthread_nreq) == 0)
 			return;
 	}
-#endif
 }
 
 static void mtk_power_budget_manager(enum pbm_kicker kicker, struct mrp *mrpmgr)
@@ -385,12 +364,12 @@ static void mtk_power_budget_manager(enum pbm_kicker kicker, struct mrp *mrpmgr)
 	pbm_wake_up_thread(kicker, mrpmgr);
 }
 
-/****************************************************
+/*
  * kicker: 0
  * who call : PMIC
  * i_max: mA
  * condition: persentage decrease 1%, then update i_max
- ****************************************************/
+ */
 void kicker_pbm_by_dlpt(unsigned int i_max)
 {
 	struct pbm *pwrctrl = &pbm_ctrl;
@@ -402,11 +381,11 @@ void kicker_pbm_by_dlpt(unsigned int i_max)
 		mtk_power_budget_manager(KR_DLPT, &mrpmgr);
 }
 
-/*****************************************************
+/*
  * kicker: 1
  * who call : MD
  * condition: on/off
- ****************************************************/
+ */
 void kicker_pbm_by_md(enum md_id id, bool status)
 {
 	struct pbm *pwrctrl = &pbm_ctrl;
@@ -419,12 +398,12 @@ void kicker_pbm_by_md(enum md_id id, bool status)
 		mtk_power_budget_manager(KR_MD, &mrpmgr);
 }
 
-/*****************************************************
+/*
  * kicker: 2
  * who call : CPU
  * loading: mW
  * condition: opp changed
- ****************************************************/
+ */
 void kicker_pbm_by_cpu(unsigned int loading, int core, int voltage)
 {
 	struct pbm *pwrctrl = &pbm_ctrl;
@@ -438,12 +417,12 @@ void kicker_pbm_by_cpu(unsigned int loading, int core, int voltage)
 		mtk_power_budget_manager(KR_CPU, &mrpmgr);
 }
 
-/*****************************************************
+/*
  * kicker: 3
  * who call : GPU
  * loading: mW
  * condition: opp changed
- ****************************************************/
+ */
 void kicker_pbm_by_gpu(bool status, unsigned int loading, int voltage)
 {
 	struct pbm *pwrctrl = &pbm_ctrl;
@@ -457,11 +436,11 @@ void kicker_pbm_by_gpu(bool status, unsigned int loading, int voltage)
 		mtk_power_budget_manager(KR_GPU, &mrpmgr);
 }
 
-/*****************************************************
+/*
  * kicker: 4
  * who call : Flash
  * condition: on/off
- ****************************************************/
+ */
 void kicker_pbm_by_flash(bool status)
 {
 	struct pbm *pwrctrl = &pbm_ctrl;
@@ -530,9 +509,119 @@ static int create_pbm_kthread(void)
 	return 0;
 }
 
+#if 1 /* CONFIG_PBM_PROC_FS */
+/*
+ * show current debug status
+ */
+static int mt_pbm_debug_proc_show(struct seq_file *m, void *v)
+{
+	if (mt_pbm_debug)
+		seq_puts(m, "pbm debug enabled\n");
+	else
+		seq_puts(m, "pbm debug disabled\n");
+
+	return 0;
+}
+
+/*
+ * enable debug message
+ */
+static ssize_t mt_pbm_debug_proc_write(struct file *file, const char __user *buffer,
+					   size_t count, loff_t *data)
+{
+	char desc[32];
+	int len = 0;
+	int debug = 0;
+
+	len = (count < (sizeof(desc) - 1)) ? count : (sizeof(desc) - 1);
+	if (copy_from_user(desc, buffer, len))
+		return 0;
+	desc[len] = '\0';
+
+	/* if (sscanf(desc, "%d", &debug) == 1) { */
+	if (kstrtoint(desc, 10, &debug) == 0) {
+		if (debug == 0)
+			mt_pbm_debug = 0;
+		else if (debug == 1)
+			mt_pbm_debug = 1;
+		else
+			pbm_warn("bad argument!! should be 0 or 1 [0: disable, 1: enable]\n");
+	} else
+		pbm_warn("bad argument!! should be 0 or 1 [0: disable, 1: enable]\n");
+
+	return count;
+}
+
+#define PROC_FOPS_RW(name)							\
+	static int mt_ ## name ## _proc_open(struct inode *inode, struct file *file)	\
+{									\
+	return single_open(file, mt_ ## name ## _proc_show, PDE_DATA(inode));	\
+}									\
+static const struct file_operations mt_ ## name ## _proc_fops = {		\
+	.owner		  = THIS_MODULE,				\
+	.open		   = mt_ ## name ## _proc_open,	\
+	.read		   = seq_read,					\
+	.llseek		 = seq_lseek,					\
+	.release		= single_release,				\
+	.write		  = mt_ ## name ## _proc_write,				\
+}
+
+#define PROC_FOPS_RO(name)							\
+	static int mt_ ## name ## _proc_open(struct inode *inode, struct file *file)	\
+{									\
+	return single_open(file, mt_ ## name ## _proc_show, PDE_DATA(inode));	\
+}									\
+static const struct file_operations mt_ ## name ## _proc_fops = {		\
+	.owner		= THIS_MODULE,				\
+	.open		= mt_ ## name ## _proc_open,	\
+	.read		= seq_read,					\
+	.llseek		= seq_lseek,					\
+	.release	= single_release,				\
+}
+
+#define PROC_ENTRY(name)	{__stringify(name), &mt_ ## name ## _proc_fops}
+
+PROC_FOPS_RW(pbm_debug);
+
+static int mt_pbm_create_procfs(void)
+{
+	struct proc_dir_entry *dir = NULL;
+	int i;
+
+	struct pentry {
+		const char *name;
+		const struct file_operations *fops;
+	};
+
+	const struct pentry entries[] = {
+		PROC_ENTRY(pbm_debug),
+	};
+
+	dir = proc_mkdir("pbm", NULL);
+
+	if (!dir) {
+		pbm_err("fail to create /proc/pbm @ %s()\n", __func__);
+		return -ENOMEM;
+	}
+
+	for (i = 0; i < ARRAY_SIZE(entries); i++) {
+		if (!proc_create
+		    (entries[i].name, S_IRUGO | S_IWUSR | S_IWGRP, dir, entries[i].fops))
+			pbm_err("@%s: create /proc/pbm/%s failed\n", __func__,
+				    entries[i].name);
+	}
+
+	return 0;
+}
+#endif	/* CONFIG_PBM_PROC_FS */
+
 static int __init pbm_module_init(void)
 {
 	int ret = 0;
+
+	#if 1 /* CONFIG_PBM_PROC_FS */
+	mt_pbm_create_procfs();
+	#endif
 
 	wake_lock_init(&pbm_wakelock, WAKE_LOCK_SUSPEND, "pbm");
 	register_dlpt_notify(&kicker_pbm_by_dlpt, DLPT_PRIO_PBM);
