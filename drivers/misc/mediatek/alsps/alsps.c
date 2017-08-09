ea6c5622498a4f8c82c26818d53d7c52b7bb035e
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 00:41:44 +0800

    [ALPS02352208] Sensor: Nesting level does not match indentation
    [Detail]
    The indentation of this code suggests it is nested when it is not.
    
    [Solution]
    Add necessary braces.
    
    [Feature] Sensor
    
    MTK-Commit-Id: e71afa86e709dec9d7b607ec1b0bb6774cb6be8b
    
    Change-Id: I49bb4f54888367c7ca0fafcee552a82693128ce6
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02352208

--- drivers/misc/mediatek/alsps/alsps.c
+++ drivers/misc/mediatek/alsps/alsps.c
@@ -196,7 +196,7 @@ static int als_real_enable(int enable)
 	if (1 == enable) {
 		if (true == cxt->is_als_active_data || true == cxt->is_als_active_nodata) {
 			err = cxt->als_ctl.enable_nodata(1);
-			if (err)
+			if (err) {
 				err = cxt->als_ctl.enable_nodata(1);
 				if (err) {
 					err = cxt->als_ctl.enable_nodata(1);
@@ -204,7 +204,8 @@ static int als_real_enable(int enable)
 						ALSPS_ERR("alsps enable(%d) err 3 timers = %d\n", enable, err);
 				}
 			}
-			ALSPS_LOG("alsps real enable\n");
+		}
+		ALSPS_LOG("alsps real enable\n");
 	}
 
 	if (0 == enable) {
