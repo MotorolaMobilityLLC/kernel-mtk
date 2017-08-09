9589ef22f284f6fb436ed401bddabe28b1b12c5c
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 06:36:57 +0800

    [ALPS02550176] DISP:Fix flight mode suspend power issue
    [Detail] Because the LCM power pin is not correctly
    turned off in QHD mode, we fix this flow.
    
    [Solution] Turn on LCM device tree in QHD mode.
    
    MTK-Commit-Id: 9eb11e4c68d65892deb5a2fbd05580dcf0b91b1a
    
    Change-Id: Icca81a9cb76bb7ce791dfdb0008696c981b02e66
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02550176
    Feature: BSP : Power Consumption(*)

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.c
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.c
@@ -990,6 +990,10 @@ unsigned char lcm_name_list[][128] = {
 	"r63417_fhd_dsi_cmd_truly_nt50358_drv",
 #endif
 
+#if defined(R63417_FHD_DSI_CMD_TRULY_NT50358_QHD)
+	"r63417_fhd_dsi_cmd_truly_nt50358_qhd_drv",
+#endif
+
 #if defined(R63417_FHD_DSI_VDO_TRULY_NT50358)
 	"r63417_fhd_dsi_vdo_truly_nt50358_drv",
 #endif
