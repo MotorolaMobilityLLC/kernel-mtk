adc62d13afc0f8ab707e66ee67ea250aa6cad7b0
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 06:47:48 +0800

    [ALPS02593109] IDLE: using tick broadcast framework
    [Detail]
    Enable USING_TICK_BROADCAST to notify broadcast tick module
    
    MTK-Commit-Id: 703ed12d7b4ca67d6665eae2a243e124e1c3ff35
    
    Change-Id: Iede8bf3ed58a04a5d33e397750d0c49b4c6cd6fb
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02593109
    Feature: SODI (Screen On Deep Idle)

--- drivers/cpuidle/cpuidle-mt67xx_v2.c
+++ drivers/cpuidle/cpuidle-mt67xx_v2.c
@@ -18,7 +18,7 @@
 
 #include "dt_idle_states.h"
 
-#if defined(CONFIG_ARCH_MT6755) || defined(CONFIG_ARCH_ELBRUS)
+#if defined(CONFIG_ARCH_MT6755) || defined(CONFIG_ARCH_MT6757) || defined(CONFIG_ARCH_ELBRUS)
 #define USING_TICK_BROADCAST
 #endif
 
