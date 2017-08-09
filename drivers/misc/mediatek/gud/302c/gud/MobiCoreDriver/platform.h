ab4901fa956c2ed1231b6b6f78432241dd0f0ac2
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 02:56:58 +0800

    [ALPS02297087] Trustonic : core migration
    [Detail]
    Add core migration support for Trustonic TEE
    
    [Solution]
    Integrate core migration patch from Trustonic
    
    MTK-Commit-Id: 6d649d32b917e7f9c98b866249648c77e08bc2a0
    
    Change-Id: I5a368b4dfcbb602ce48a63651b3838499635f36d
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02297087
    Feature: Trustonic TEE (Trusted Execution Environment)

--- drivers/misc/mediatek/gud/302c/gud/MobiCoreDriver/platform.h
+++ drivers/misc/mediatek/gud/302c/gud/MobiCoreDriver/platform.h
@@ -36,8 +36,8 @@
 
 #define TBASE_CORE_SWITCHER
 /* Values of MPIDR regs in  cpu0, cpu1, cpu2, cpu3*/
-#define CPU_IDS {0x0000, 0x0001, 0x0002, 0x0003, 0x0100, 0x0101, 0x0102, 0x0103}
-#define COUNT_OF_CPUS 8
+#define CPU_IDS {0x0000, 0x0001, 0x0002, 0x0003, 0x0100, 0x0101, 0x0102, 0x0103, 0x0200, 0x0201}
+#define COUNT_OF_CPUS (CONFIG_NR_CPUS)
 
 /* Enable Fastcall worker thread */
 #define MC_FASTCALL_WORKER_THREAD
