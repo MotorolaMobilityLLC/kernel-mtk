1279f83c1bb8aae9e18d27195f282be826d92539
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 17:30:10 +0800

    [ALPS02256856] LED:add the API announce for AAL
    [Detail] add the API announce for AAL usage.
    
    [Solution] add the backlight_brightness_set announce in leds_drv.c
    
    [Feature] [Android Default] Backlight
    
    MTK-Commit-Id: 5591df2a4f0f8ce9b12296de98dd88294dc39423
    
    Change-Id: I9f28e9d13e09e31f050e21053f5d75746e876912
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02256856

--- drivers/misc/mediatek/leds/leds_drv.h
+++ drivers/misc/mediatek/leds/leds_drv.h
@@ -10,4 +10,5 @@ int setMaxbrightness(int max_level, int enable);
 #endif
 
 extern int mt65xx_leds_brightness_set(enum mt65xx_led_type type, enum led_brightness level);
+extern int backlight_brightness_set(int level);
 extern int disp_bls_set_max_backlight(unsigned int level);
