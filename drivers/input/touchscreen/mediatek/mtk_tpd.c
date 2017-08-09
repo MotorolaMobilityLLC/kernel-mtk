24630a602a43c83c5785a29e17702c28596fe86e
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 01:46:37 +0800

    [ALPS02163835] touch: add gt9xx_hotknot driver for 2701 platform
    [Detail]
    add touch driver
    [Solution]
    sync code from l1.mp5
    [Feature] touchpanel
    
    MTK-Commit-Id: 852b4c47adb0b00fb41bf57d3f78e2260e4b51f5
    
    Change-Id: I965c378107116690a9e24d326938e6881de9bfb6
    CR-Id: ALPS02163835
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>

--- drivers/input/touchscreen/mediatek/mtk_tpd.c
+++ drivers/input/touchscreen/mediatek/mtk_tpd.c
@@ -59,6 +59,7 @@ struct of_device_id touch_of_match[] = {
 	{ .compatible = "mediatek,mt6797-touch", },
 	{ .compatible = "mediatek,mt8163-touch", },
 	{ .compatible = "mediatek,mt8127-touch", },
+	{ .compatible = "mediatek,mt2701-touch", },
 	{},
 };
 
