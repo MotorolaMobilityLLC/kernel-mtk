1333425186451dfadb01c5e462fc27a95587add3
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 22:44:08 +0800

    [ALPS02343685] disp: fix DAL_clean cause display hang
    [Detail] In some scenario like MD reset would clean assert_layer.
    which might cuase display hang.
    
    [Solution] porting display assert layer driver from MT6755
    
    [Feature] Telephony Recovery Mechanism
    
    MTK-Commit-Id: 0ee0d709b8ac947456f2d7abdd73dd525ded543b
    
    Change-Id: Icd092155cf3122d8344294d346dee42aad9b3acf
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02343685

--- drivers/misc/mediatek/video/mt6797/videox/disp_assert_layer.c
+++ drivers/misc/mediatek/video/mt6797/videox/disp_assert_layer.c
@@ -8,7 +8,7 @@
 #include "ddp_mmp.h"
 #include "disp_drv_platform.h"
 #include "disp_session.h"
-
+#include <linux/slab.h>
 #include <linux/string.h>
 #include <linux/semaphore.h>
 #include <asm/cacheflush.h>
@@ -35,52 +35,90 @@
 
 #define MAKE_TWO_RGB565_COLOR(high, low)  (((low) << 16) | (high))
 
-#define DAL_LOG(fmt, arg...) DISP_LOG_PRINT(ANDROID_LOG_INFO, "DAL", fmt, ##arg)
+DEFINE_SEMAPHORE(dal_sem);
+inline DAL_STATUS DAL_LOCK(void)
+{
+	if (down_interruptible(&dal_sem)) {
+		DISP_LOG_PRINT(ANDROID_LOG_WARN, "DAL", "Can't get semaphore in %s()\n",
+		__func__);
+		return DAL_STATUS_LOCK_FAIL;
+	}
+	return DAL_STATUS_OK;
+}
+
+#define DAL_UNLOCK() up(&dal_sem)
+
+
+inline MFC_STATUS DAL_CHECK_MFC_RET(MFC_STATUS expr)
+{
+	MFC_STATUS ret = expr;
+
+	if (MFC_STATUS_OK != ret) {
+		DISP_LOG_PRINT(ANDROID_LOG_WARN, "DAL",
+		"Warning: call MFC_XXX function failed in %s(), line: %d, ret: %x\n",
+		__func__, __LINE__, ret);
+		return ret;
+	}
+	return MFC_STATUS_OK;
+}
+
+
+inline DISP_STATUS DAL_CHECK_DISP_RET(DISP_STATUS expr)
+{
+	DISP_STATUS ret = (expr);
+
+	if (DISP_STATUS_OK != ret) {
+		DISP_LOG_PRINT(ANDROID_LOG_WARN, "DAL",
+		"Warning: call DISP_XXX function failed in %s(), line: %d, ret: %x\n",
+		__func__, __LINE__, ret);
+		return ret;
+	}
+	return DISP_STATUS_OK;
+}
+
+#define DAL_LOG(fmt, arg...) \
+	DISP_LOG_PRINT(ANDROID_LOG_INFO, "DAL", fmt, ##arg)
 /* --------------------------------------------------------------------------- */
 
 static MFC_HANDLE mfc_handle;
 static void *dal_fb_addr;
 static unsigned long dal_fb_pa;
 
-/* static BOOL dal_enable_when_resume = FALSE; */
+/*static BOOL dal_enable_when_resume = FALSE;*/
 static bool dal_disable_when_resume;
 static unsigned int dal_fg_color = RGB888_To_RGB565(DAL_COLOR_WHITE);
 static unsigned int dal_bg_color = RGB888_To_RGB565(DAL_COLOR_RED);
 static char dal_print_buffer[1024];
 
-bool dal_shown = 0;
+bool dal_shown = false;
 unsigned int isAEEEnabled = 0;
-
-DEFINE_SEMAPHORE(dal_sem);
 
 /* --------------------------------------------------------------------------- */
 
 
-UINT32 DAL_GetLayerSize(void)
+uint32_t DAL_GetLayerSize(void)
 {
 	/* avoid lcdc read buffersize+1 issue */
 	return DAL_WIDTH * DAL_HEIGHT * DAL_BPP + 4096;
 }
-
-#if 0 /* defined but not used */
-static DAL_STATUS DAL_SetRedScreen(UINT32 *addr)
+/*
+static DAL_STATUS DAL_SetRedScreen(uint32_t *addr)
 {
-	UINT32 i;
-	const UINT32 BG_COLOR = MAKE_TWO_RGB565_COLOR(DAL_BG_COLOR, DAL_BG_COLOR);
+	uint32_t i;
+	const uint32_t BG_COLOR = MAKE_TWO_RGB565_COLOR(DAL_BG_COLOR, DAL_BG_COLOR);
 
-	for (i = 0; i < DAL_GetLayerSize() / sizeof(UINT32); ++i)
+	for (i = 0; i < DAL_GetLayerSize() / sizeof(uint32_t); ++i)
 		*addr++ = BG_COLOR;
 	return DAL_STATUS_OK;
 }
-#endif
-
+*/
 DAL_STATUS DAL_SetScreenColor(DAL_COLOR color)
 {
-	UINT32 i;
-	UINT32 size;
-	UINT32 BG_COLOR;
+	uint32_t i;
+	uint32_t size;
+	uint32_t BG_COLOR;
 	MFC_CONTEXT *ctxt = NULL;
-	UINT32 offset;
+	uint32_t offset;
 	unsigned int *addr;
 	color = RGB888_To_RGB565(color);
 	BG_COLOR = MAKE_TWO_RGB565_COLOR(color, color);
@@ -94,7 +132,7 @@ DAL_STATUS DAL_SetScreenColor(DAL_COLOR color)
 	addr = (unsigned int *)(ctxt->fb_addr + offset);
 
 	size = DAL_GetLayerSize() - offset;
-	for (i = 0; i < size / sizeof(UINT32); ++i)
+	for (i = 0; i < size / sizeof(uint32_t); ++i)
 		*addr++ = BG_COLOR;
 	ctxt->screen_color = color;
 
@@ -104,22 +142,12 @@ EXPORT_SYMBOL(DAL_SetScreenColor);
 
 DAL_STATUS DAL_Init(unsigned long layerVA, unsigned long layerPA)
 {
-	MFC_STATUS ret;
-
 	pr_debug("%s, layerVA=0x%lx, layerPA=0x%lx\n", __func__, layerVA, layerPA);
 
 	dal_fb_addr = (void *)layerVA;
 	dal_fb_pa = layerPA;
-
-	ret = MFC_Open(&mfc_handle, dal_fb_addr, DAL_WIDTH, DAL_HEIGHT, DAL_BPP,
-				  DAL_FG_COLOR, DAL_BG_COLOR);
-	if (MFC_STATUS_OK != ret) {
-		DISP_LOG_PRINT(ANDROID_LOG_WARN, "DAL",
-			       "Warning: call MFC_XXX function failed in %s(), line: %d, ret: %x\n",
-			       __func__, __LINE__, ret);
-		return ret;
-	}
-
+	DAL_CHECK_MFC_RET(MFC_Open(&mfc_handle, dal_fb_addr,
+				   DAL_WIDTH, DAL_HEIGHT, DAL_BPP, DAL_FG_COLOR, DAL_BG_COLOR));
 	/* DAL_Clean(); */
 	DAL_SetScreenColor(DAL_COLOR_RED);
 
@@ -128,28 +156,14 @@ DAL_STATUS DAL_Init(unsigned long layerVA, unsigned long layerPA)
 
 DAL_STATUS DAL_SetColor(unsigned int fgColor, unsigned int bgColor)
 {
-	MFC_STATUS ret;
-
 	if (NULL == mfc_handle)
 		return DAL_STATUS_NOT_READY;
 
-	if (down_interruptible(&dal_sem)) {
-		DISP_LOG_PRINT(ANDROID_LOG_WARN, "DAL", "Can't get semaphore in %s()\n", __func__);
-		return DAL_STATUS_LOCK_FAIL;
-	}
-
+	DAL_LOCK();
 	dal_fg_color = RGB888_To_RGB565(fgColor);
 	dal_bg_color = RGB888_To_RGB565(bgColor);
-
-	ret = MFC_SetColor(mfc_handle, dal_fg_color, dal_bg_color);
-	if (MFC_STATUS_OK != ret) {
-		DISP_LOG_PRINT(ANDROID_LOG_WARN, "DAL",
-			       "Warning: call MFC_XXX function failed in %s(), line: %d, ret: %x\n",
-			       __func__, __LINE__, ret);
-		return ret;
-	}
-
-	up(&dal_sem);
+	DAL_CHECK_MFC_RET(MFC_SetColor(mfc_handle, dal_fg_color, dal_bg_color));
+	DAL_UNLOCK();
 
 	return DAL_STATUS_OK;
 }
@@ -159,12 +173,48 @@ DAL_STATUS DAL_Dynamic_Change_FB_Layer(unsigned int isAEEEnabled)
 {
 	return DAL_STATUS_OK;
 }
+static int show_dal_layer(int enable)
+{
+	disp_session_input_config *session_input;
+	disp_input_config *input;
+	int ret;
+
+	session_input = kzalloc(sizeof(*session_input), GFP_KERNEL);
+	if (!session_input)
+		return -ENOMEM;
+
+	session_input->setter = SESSION_USER_AEE;
+	session_input->config_layer_num = 1;
+	input = &session_input->config[0];
+
+	input->src_phy_addr = (void *)dal_fb_pa;
+	input->layer_id = primary_display_get_option("ASSERT_LAYER");
+	input->layer_enable = enable;
+	input->src_offset_x = 0;
+	input->src_offset_y = 0;
+	input->src_width = DAL_WIDTH;
+	input->src_height = DAL_HEIGHT;
+	input->tgt_offset_x = 0;
+	input->tgt_offset_y = 0;
+	input->tgt_width = DAL_WIDTH;
+	input->tgt_height = DAL_HEIGHT;
+	input->alpha = 0x80;
+	input->alpha_enable = 1;
+	input->next_buff_idx = -1;
+	input->src_pitch = DAL_WIDTH;
+	input->src_fmt = DAL_FORMAT;
+	input->next_buff_idx = -1;
+
+	ret = primary_display_config_input_multiple(session_input);
+	kfree(session_input);
+	return ret;
+}
+
 
 DAL_STATUS DAL_Clean(void)
 {
-	/* const UINT32 BG_COLOR = MAKE_TWO_RGB565_COLOR(DAL_BG_COLOR, DAL_BG_COLOR); */
+	/* const uint32_t BG_COLOR = MAKE_TWO_RGB565_COLOR(DAL_BG_COLOR, DAL_BG_COLOR); */
 	DAL_STATUS ret = DAL_STATUS_OK;
-	MFC_STATUS r;
 
 	static int dal_clean_cnt;
 	MFC_CONTEXT *ctxt = (MFC_CONTEXT *) mfc_handle;
@@ -174,69 +224,32 @@ DAL_STATUS DAL_Clean(void)
 
 
 	MMProfileLogEx(ddp_mmp_get_events()->dal_clean, MMProfileFlagStart, 0, 0);
-
-	if (down_interruptible(&dal_sem)) {
-		DISP_LOG_PRINT(ANDROID_LOG_WARN, "DAL", "Can't get semaphore in %s()\n", __func__);
-		return DAL_STATUS_LOCK_FAIL;
+	DAL_LOCK();
+	if (MFC_STATUS_OK != MFC_ResetCursor(mfc_handle)) {
+		pr_err("mfc_handle = %p\n", mfc_handle);
+		goto End;
 	}
-
-	r = MFC_ResetCursor(mfc_handle);
-	if (MFC_STATUS_OK != r) {
-		DISP_LOG_PRINT(ANDROID_LOG_WARN, "DAL",
-			       "Warning: call MFC_XXX function failed in %s(), line: %d, ret: %x\n",
-			       __func__, __LINE__, r);
-		return r;
-	}
-
 	ctxt->screen_color = 0;
 	DAL_SetScreenColor(DAL_COLOR_RED);
 
 
 	/* TODO: if dal_shown=false, and 3D enabled, mtkfb may disable UI layer, please modify 3D driver */
 	if (isAEEEnabled == 1) {
-		disp_session_input_config session_input;
-		disp_input_config *input;
-
-		memset((void *)&session_input, 0, sizeof(session_input));
-
-		session_input.setter = SESSION_USER_AEE;
-		session_input.config_layer_num = 1;
-		input = &session_input.config[0];
-
-		input->src_phy_addr = (void *)dal_fb_pa;
-		input->layer_id = primary_display_get_option("ASSERT_LAYER");
-		input->layer_enable = 0;
-		input->src_offset_x = 0;
-		input->src_offset_y = 0;
-		input->src_width = DAL_WIDTH;
-		input->src_height = DAL_HEIGHT;
-		input->tgt_offset_x = 0;
-		input->tgt_offset_y = 0;
-		input->tgt_width = DAL_WIDTH;
-		input->tgt_height = DAL_HEIGHT;
-		input->alpha = 0x80;
-		input->alpha_enable = 1;
-		input->next_buff_idx = -1;
-		input->src_pitch = DAL_WIDTH;
-		input->src_fmt = DAL_FORMAT;
-		input->next_buff_idx = -1;
-
-		ret = primary_display_config_input_multiple(&session_input);
-
+		show_dal_layer(0);
 		/* DAL disable, switch UI layer to default layer 3 */
 		pr_err("[DDP]* isAEEEnabled from 1 to 0, %d\n", dal_clean_cnt++);
 		isAEEEnabled = 0;
 		DAL_Dynamic_Change_FB_Layer(isAEEEnabled);	/* restore UI layer to DEFAULT_UI_LAYER */
 	}
 
-	dal_shown = 0;
-	dal_disable_when_resume = 0;
+	dal_shown = false;
+	dal_disable_when_resume = false;
 
 	primary_display_trigger(0, NULL, 0);
 
 
-/* End: */
-	up(&dal_sem);
+End:
+	DAL_UNLOCK();
 	MMProfileLogEx(ddp_mmp_get_events()->dal_clean, MMProfileFlagEnd, 0, 0);
 	return ret;
 }
@@ -245,15 +258,7 @@ EXPORT_SYMBOL(DAL_Clean);
 int is_DAL_Enabled(void)
 {
 	int ret = 0;
-
-	if (down_interruptible(&dal_sem)) {
-		DISP_LOG_PRINT(ANDROID_LOG_WARN, "DAL", "Can't get semaphore in %s()\n", __func__);
-		return DAL_STATUS_LOCK_FAIL;
-	}
-
 	ret = isAEEEnabled;
-
-	up(&dal_sem);
 	return ret;
 }
 
@@ -262,9 +267,7 @@ DAL_STATUS DAL_Printf(const char *fmt, ...)
 	va_list args;
 	uint i;
 	DAL_STATUS ret = DAL_STATUS_OK;
-	disp_session_input_config session_input;
-	disp_input_config *input;
-	MFC_STATUS r;
+
 
 	/* printk("[MTKFB_DAL] DAL_Printf mfc_handle=0x%08X, fmt=0x%08X\n", mfc_handle, fmt); */
 
@@ -277,12 +280,7 @@ DAL_STATUS DAL_Printf(const char *fmt, ...)
 		return DAL_STATUS_INVALID_ARGUMENT;
 
 	MMProfileLogEx(ddp_mmp_get_events()->dal_printf, MMProfileFlagStart, 0, 0);
-
-	if (down_interruptible(&dal_sem)) {
-		DISP_LOG_PRINT(ANDROID_LOG_WARN, "DAL", "Can't get semaphore in %s()\n", __func__);
-		return DAL_STATUS_LOCK_FAIL;
-	}
-
+	DAL_LOCK();
 	if (isAEEEnabled == 0) {
 		pr_err("[DDP] isAEEEnabled from 0 to 1, ASSERT_LAYER=%d, dal_fb_pa %lx\n",
 		       primary_display_get_option("ASSERT_LAYER"), dal_fb_pa);
@@ -290,66 +288,27 @@ DAL_STATUS DAL_Printf(const char *fmt, ...)
 		isAEEEnabled = 1;
 		DAL_Dynamic_Change_FB_Layer(isAEEEnabled);	/* default_ui_ layer coniig to changed_ui_layer */
 
-		r = MFC_Open(&mfc_handle, dal_fb_addr, DAL_WIDTH, DAL_HEIGHT,
-			       DAL_BPP, DAL_FG_COLOR, DAL_BG_COLOR);
-		if (MFC_STATUS_OK != r) {
-			DISP_LOG_PRINT(ANDROID_LOG_WARN, "DAL",
-				       "Warning: call MFC_XXX function failed in %s(), line: %d, ret: %x\n",
-				       __func__, __LINE__, r);
-			return r;
-		}
-
-		/* DAL_Clean(); */
-		memset((void *)&session_input, 0, sizeof(session_input));
-
-		session_input.setter = SESSION_USER_AEE;
-		session_input.config_layer_num = 1;
-		input = &session_input.config[0];
-
-		input->src_phy_addr = (void *)dal_fb_pa;
-		input->layer_id = primary_display_get_option("ASSERT_LAYER");
-		input->layer_enable = 1;
-		input->src_offset_x = 0;
-		input->src_offset_y = 0;
-		input->src_width = DAL_WIDTH;
-		input->src_height = DAL_HEIGHT;
-		input->tgt_offset_x = 0;
-		input->tgt_offset_y = 0;
-		input->tgt_width = DAL_WIDTH;
-		input->tgt_height = DAL_HEIGHT;
-		input->alpha = 0x80;
-		input->alpha_enable = 1;
-		input->next_buff_idx = -1;
-		input->src_pitch = DAL_WIDTH;
-		input->src_fmt = DAL_FORMAT;
-		input->next_buff_idx = -1;
-
-		ret = primary_display_config_input_multiple(&session_input);
+		DAL_CHECK_MFC_RET(MFC_Open(&mfc_handle, dal_fb_addr,
+					   DAL_WIDTH, DAL_HEIGHT, DAL_BPP,
+					   DAL_FG_COLOR, DAL_BG_COLOR));
+		show_dal_layer(1);
 	}
 	va_start(args, fmt);
 	i = vsprintf(dal_print_buffer, fmt, args);
 	BUG_ON(i >= ARRAY_SIZE(dal_print_buffer));
 	va_end(args);
-
-	r = MFC_Print(mfc_handle, dal_print_buffer);
-	if (MFC_STATUS_OK != r) {
-		DISP_LOG_PRINT(ANDROID_LOG_WARN, "DAL",
-			       "Warning: call MFC_XXX function failed in %s(), line: %d, ret: %x\n",
-			       __func__, __LINE__, r);
-		return r;
-	}
+	DAL_CHECK_MFC_RET(MFC_Print(mfc_handle, dal_print_buffer));
 
 	flush_cache_all();
 
 
 	if (!dal_shown)
-		dal_shown = 1;
+		dal_shown = true;
 
 	ret = primary_display_trigger(0, NULL, 0);
 
-/* End: */
 
-	up(&dal_sem);
+	DAL_UNLOCK();
 
 	MMProfileLogEx(ddp_mmp_get_events()->dal_printf, MMProfileFlagEnd, 0, 0);
 
@@ -369,7 +328,7 @@ DAL_STATUS DAL_OnDispPowerOn(void)
 #else
 unsigned int isAEEEnabled = 0;
 
-UINT32 DAL_GetLayerSize(void)
+uint32_t DAL_GetLayerSize(void)
 {
 	/* xuecheng, avoid lcdc read buffersize+1 issue */
 	return DAL_WIDTH * DAL_HEIGHT * DAL_BPP + 4096;
