adf834f3af49fc61e9adfccfbf1db33797477bb8
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 17:29:36 +0800

    [ALPS02210720] UART: add clock and GPIO support
    [Detail]
    UART in MT6797 is lack of clock and GPIO support
    in device tree.
    
    [Solution]
    1. add clock and pinctrl nodes for UART.
    2. fix compatible string
    
    [Feature] Others
    
    MTK-Commit-Id: 01d2dca1e2bf1c7a8930679b1e3ee5ca3285da92
    
    Change-Id: I2c2565aadd7d33dc49da43f60fe4b3725074ca3a
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02210720

--- drivers/misc/mediatek/uart/uart.c
+++ drivers/misc/mediatek/uart/uart.c
@@ -2740,6 +2740,7 @@ static const struct of_device_id apuart_of_ids[] = {
 	{.compatible = "mediatek,mt6735-uart",},
 	{.compatible = "mediatek,mt6755-uart",},
 	{.compatible = "mediatek,mt8173-uart",},
+	{.compatible = "mediatek,mt6797-uart",},
 	{}
 };
 #endif
