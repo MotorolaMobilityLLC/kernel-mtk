17a9f5379c269e5b92c520ed9b84772a44a07910
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 02:17:59 +0800

    [ALPS02242629] ECCCI: add in timestamp for MD register dump
    [Detail]
    Add in timestamp for MD register dump.
    
    [Solution]
    null
    
    [Feature] Modem Interface Driver
    
    MTK-Commit-Id: f4145cd06a66eb6702f145cdf1e748ad7382ee33
    
    Change-Id: I48a052ef66513fed75ba7117cd5dcf1ea68cdc31
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02242629

--- drivers/misc/mediatek/eccci/mt6797/cldma_platform.c
+++ drivers/misc/mediatek/eccci/mt6797/cldma_platform.c
@@ -261,13 +261,13 @@ void md_cd_dump_debug_register(struct ccci_modem *md)
 #if 1
 	/* 1. shared memory */
 	/* 2. TO PSM */
-	CCCI_MEM_LOG(md->index, TAG, "Dump MD TOPSM status 0x%x\n", MD_TOPSM_STATUS_BASE);
+	CCCI_MEM_LOG_TAG(md->index, TAG, "Dump MD TOPSM status 0x%x\n", MD_TOPSM_STATUS_BASE);
 	ccci_write32(md_reg->md_busreg1, 0x94, 0xE7C5);/* pre-action: permission */
 	ccci_util_mem_dump(md->index, CCCI_DUMP_MEM_DUMP,
 			md_ctrl->md_topsm_status, MD_TOPSM_STATUS_LENGTH);
 
 	/* 3. PC Monitor */
-	CCCI_MEM_LOG(md->index, TAG, "Dump MD PC monitor 0x%x\n", (MD_PC_MONITOR_BASE + 0x100));
+	CCCI_MEM_LOG_TAG(md->index, TAG, "Dump MD PC monitor 0x%x\n", (MD_PC_MONITOR_BASE + 0x100));
 	/* pre-action: Open Dbgsys clock */
 	md_addr = md_reg->md_ect_0;
 	reg_value = ccci_read32(md_addr, 4);
@@ -295,23 +295,23 @@ void md_cd_dump_debug_register(struct ccci_modem *md)
 	ccci_write32(md_reg->md_pc_mon2, 4, 0x1);	/* restart MD PCMon */
 
 	/* 4. MD RGU */
-	CCCI_MEM_LOG(md->index, TAG, "Dump MD RGU 0x%x\n", MD_RGU_BASE);
+	CCCI_MEM_LOG_TAG(md->index, TAG, "Dump MD RGU 0x%x\n", MD_RGU_BASE);
 	/* ccci_write32(md_reg->md_busreg1, 0x94, 0xE7C5); *//* pre-action */
 	ccci_util_mem_dump(md->index, CCCI_DUMP_MEM_DUMP, md_ctrl->md_rgu_base, 0x8B);
 	ccci_util_mem_dump(md->index, CCCI_DUMP_MEM_DUMP, (md_ctrl->md_rgu_base + 0x200), 0x60);
 	/* 5 OST */
-	CCCI_MEM_LOG(md->index, TAG, "Dump MD OST status %x\n", MD_OST_STATUS_BASE);
+	CCCI_MEM_LOG_TAG(md->index, TAG, "Dump MD OST status %x\n", MD_OST_STATUS_BASE);
 	/*ccci_write32(md_reg->md_busreg1, 0x94, 0xE7C5);*//* pre-action */
 	ccci_util_mem_dump(md->index, CCCI_DUMP_MEM_DUMP, md_ctrl->md_ost_status, MD_OST_STATUS_LENGTH);
 	/* 6. Bus status */
-	CCCI_MEM_LOG(md->index, TAG, "Dump MD Bus status %x\n", MD_BUS_STATUS_BASE);
+	CCCI_MEM_LOG_TAG(md->index, TAG, "Dump MD Bus status %x\n", MD_BUS_STATUS_BASE);
 	ccci_write32(md_reg->md_busreg1, 0x9C, 0x65);/* pre-action: permission */
 	ccci_util_mem_dump(md->index, CCCI_DUMP_MEM_DUMP, md_ctrl->md_bus_status, 0x38);
 	ccci_util_mem_dump(md->index, CCCI_DUMP_MEM_DUMP, (md_ctrl->md_bus_status + 0x100), 0x30);
 	ccci_util_mem_dump(md->index, CCCI_DUMP_MEM_DUMP, md_reg->md_busreg1, MD_BUSREG_DUMP_LEN1);
 	ccci_util_mem_dump(md->index, CCCI_DUMP_MEM_DUMP, md_reg->md_busreg2, MD_BUSREG_DUMP_LEN2);
 	/* 7. dump PLL */
-	CCCI_MEM_LOG(md->index, TAG, "Dump MD PLL\n");
+	CCCI_MEM_LOG_TAG(md->index, TAG, "Dump MD PLL\n");
 	ccci_util_mem_dump(md->index, CCCI_DUMP_MEM_DUMP, md_reg->md_clkSW, 0x4C);
 	ccci_util_mem_dump(md->index, CCCI_DUMP_MEM_DUMP, md_reg->md_clk_ctl01, MD_Clkctrl_DUMP_LEN01);
 	ccci_util_mem_dump(md->index, CCCI_DUMP_MEM_DUMP, md_reg->md_clk_ctl02, MD_Clkctrl_DUMP_LEN02);
@@ -329,12 +329,12 @@ void md_cd_dump_debug_register(struct ccci_modem *md)
 	ccci_util_mem_dump(md->index, CCCI_DUMP_MEM_DUMP, md_reg->md_clk_ctl14, MD_Clkctrl_DUMP_LEN14);
 	ccci_util_mem_dump(md->index, CCCI_DUMP_MEM_DUMP, md_reg->md_clk_ctl15, MD_Clkctrl_DUMP_LEN15);
 	/* 8. Bus REC */
-	CCCI_MEM_LOG(md->index, TAG, "Dump MD Bus REC%x\n", MD_BUSREC_DUMP_ADDR);
+	CCCI_MEM_LOG_TAG(md->index, TAG, "Dump MD Bus REC%x\n", MD_BUSREC_DUMP_ADDR);
 	ccci_write32(md_reg->md_busrec, 0x4, 0x1);/* pre-action */
 	ccci_util_mem_dump(md->index, CCCI_DUMP_MEM_DUMP, md_reg->md_busrec, MD_BUSREC_DUMP_LEN);
 	ccci_write32(md_reg->md_busrec, 0x4, 0x3);/* post-action */
 	/* 9. ECT: must after 4 TO PSM */
-	CCCI_MEM_LOG(md->index, TAG, "Dump MD ECT 0x%x\n", MD_ECT_DUMP_ADDR0);
+	CCCI_MEM_LOG_TAG(md->index, TAG, "Dump MD ECT 0x%x\n", MD_ECT_DUMP_ADDR0);
 	md_addr = md_reg->md_ect_0;
 	reg_value = ccci_read32(md_addr, 4);
 	reg_value |= (0x1 << 3);
