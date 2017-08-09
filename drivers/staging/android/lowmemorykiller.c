cc83f2f4e721ac61fca370ac16ccd9bd67b701ef
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 18:58:19 +0800

    [ALPS02285571] LMK: Fixed subtract swap pages from other_file twice
    [Detail]
    It is observed that lowmemory killer doesnt trigger properly.
    
    [Solution]
    Subtract swap pages only once.
    
    [Feature] FB (Face Beautify)
    
    MTK-Commit-Id: 126113c93050e0bd2e9a226f0917c8c2fbc9691a
    
    Change-Id: Idd0f87bd8a0184694f00990b35767ad04e56f5c0
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02285571

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -191,10 +191,6 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		return SHRINK_STOP;
 	}
 
-#ifdef CONFIG_ZRAM
-	other_file -= total_swapcache_pages();
-#endif
-
 #ifdef CONFIG_HIGHMEM
 	/*
 	* Check whether it is caused by low memory in normal zone!
