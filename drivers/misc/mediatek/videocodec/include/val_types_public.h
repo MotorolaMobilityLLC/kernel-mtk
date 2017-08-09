99f2b17c337f8a561e1fbd119d4e52791c0a209f
SW Integrator <swintegrator@medaitek.com>
2017-08-10 06:12:00 +0800

    [ALPS02560711] platform: modify 8160 defconfig and fix build error
    [Detail]
    1. disable CONFIG_MTK_SEC_VIDEO_PATH_SUPPORT
    2. add VAL_CHIP_NAME_MT8160 in val_types_public.h
    
    [Solution]
    
    MTK-Commit-Id: e581b15659f940bebce218fa6857aa5cff7e213b
    
    Change-Id: I4961e6674ecbddfe82b581b63d1240876b784782
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02560711
    Feature: Others

--- drivers/misc/mediatek/videocodec/include/val_types_public.h
+++ drivers/misc/mediatek/videocodec/include/val_types_public.h
@@ -96,6 +96,7 @@ typedef enum _VAL_CHIP_NAME_T {
 	VAL_CHIP_NAME_MT6755,                       /* / <Jade */
 	VAL_CHIP_NAME_MT6797,                       /* / <Everest */
 	VAL_CHIP_NAME_MT7623,                       /* / <7623 */
+	VAL_CHIP_NAME_MT8160,                       /* / <MT8160 */
 	VAL_CHIP_NAME_MAX = 0xFFFFFFFF              /* /< Max Value */
 } VAL_CHIP_NAME_T;
 
