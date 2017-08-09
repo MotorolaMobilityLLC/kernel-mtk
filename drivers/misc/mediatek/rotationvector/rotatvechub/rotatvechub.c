7f026a93c619ee14b115d3e7a6198f976efafaa2
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 03:26:07 +0800

    [ALPS02274235] sensor: add grv gmrv and rv scalar
    [Detail] add grv gmrv and rv sensor driver scalar
    
    [Solution] add grv gmrv and rv sensor driver scalar
    
    MTK-Commit-Id: fd1bdaee2ee6e82de5639017e4f36ca592e2f77d
    
    Change-Id: I8c7eeb38aba9d82c6da4867f71abefef811e189c
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02274235
    Feature: [Android Default] Acclerometer

--- drivers/misc/mediatek/rotationvector/rotatvechub/rotatvechub.c
+++ drivers/misc/mediatek/rotationvector/rotatvechub/rotatvechub.c
@@ -116,6 +116,7 @@ static int rotatvec_get_data(int *x, int *y, int *z, int *scalar, int *status)
 	*x = data.orientation_t.azimuth;
 	*y = data.orientation_t.pitch;
 	*z = data.orientation_t.roll;
+	*scalar = 1;
 	*status = data.orientation_t.status;
 	ROTVEC_LOG("recv ipi: timestamp: %lld, timestamp_gpt: %lld, x: %d, y: %d, z: %d!\n", time_stamp, time_stamp_gpt,
 		   *x, *y, *z);
