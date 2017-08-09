68933aae600b04ed083719d49f103754dd52dd9a
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 03:41:46 +0800

    [ALPS02403518] TEEI: MT6797
    [Detail] Used the same version (V1.0) for all platform.
    
    [Solution] Remove V1.1 folder and modified Makefile.
    
    MTK-Commit-Id: 0fa00676ad54bd9d98f8a18070c61310f966c534
    
    Change-Id: Iadbb80aa02564f0b222f2427c026527c3445238d
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02403518
    Feature: Microtrust TEEI

--- drivers/misc/mediatek/teei/V1.1/teei_fp/teei_fp.h
+++
@@ -1,6 +0,0 @@
-/*for socket*/
-
-#define fp_buffer_size 2048
-
-#define ARGS_BLOCK_SIZE 1024
-
--- drivers/misc/mediatek/teei/V1.1/tz_driver/teei_common_types.h
+++
@@ -1,5 +0,0 @@
-
-#ifndef SW_COMMON_TYPES_H
-#define SW_COMMON_TYPES_H
-define size_t (unsigned long)
-#endif
