bd981042181e490c5c7d890d3fd1955fc5551cb9
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 01:53:02 +0800

    [ALPS02213055] Camera: add lens driver lc898212xd
    [Detail] Bayer+Mono: add mono part
    
    [Solution] add lc898212xd_f
    
    [Feature] [Android Default] AF (Auto Focus)
    
    MTK-Commit-Id: f4bf6238b647cf0bb9ac458484feeb85986e239b
    
    Change-Id: Ic768d636d7b3da07e38985b7b8101b2a497b59a1
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02213055

---
+++ drivers/misc/mediatek/lens/common/lc898212xdaf_f/inc/LC898212XDAF_F.h
@@ -0,0 +1,8 @@
+#ifndef __LC898212XDAF_F__
+#define	__LC898212XDAF_F__
+
+extern void LC898212XDAF_F_MONO_init(unsigned char Hall_Off, unsigned char Hall_Bias);
+
+extern unsigned char LC898212XDAF_F_MONO_moveAF(short SsSmvEnd);
+
+#endif
