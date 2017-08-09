acdc63eb95059eb3cebedb9ebd26e08c04bbe198
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 15:36:10 +0800

    [ALPS02186168] Bluetooth: kernel driver update for M migration
    [Detail]
    Update bluetooth kernel driver
    
    [Solution]
    1. Fix coding style issues
    2. Delete phaseout macro and functions
    
    [Feature] MTK BT solution - advanced
    
    MTK-Commit-Id: 0ee3e964e68aee34a73f81f4bb7f1ec8fa027348
    
    Change-Id: I9ffac715b0ced4098c91a78141c3a5a848d313e2
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02186168

--- drivers/misc/mediatek/connectivity/common_detect/drv_init/bluetooth_drv_init.c
+++ drivers/misc/mediatek/connectivity/common_detect/drv_init/bluetooth_drv_init.c
@@ -25,7 +25,7 @@ int do_bluetooth_drv_init(int chip_id)
 	int i_ret = -1;
 
 	WMT_DETECT_INFO_FUNC("start to do bluetooth driver init\n");
-	/*i_ret = mtk_wcn_stpbt_drv_init();*/
+	i_ret = mtk_wcn_stpbt_drv_init();
 	WMT_DETECT_INFO_FUNC("finish bluetooth driver init, i_ret:%d\n", i_ret);
 	return i_ret;
 }
