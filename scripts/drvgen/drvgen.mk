6792d9f51f27cac9095b173db71f573390a45be5
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 15:51:38 +0800

    [ALPS02218261] Device tree: cust_xxx.dtsi standardization
    [Detail]
    Make D1/D2/D3/Rainier follow cust_xxx.dtsi standardization
    
    [Solution]
    Make D1/D2/D3/Rainier follow cust_xxx.dtsi standardization
    
    [Feature] device tree
    
    MTK-Commit-Id: 8891c6fbb4f23c1537f5df918cdfecd38cb4b9c4
    
    Change-Id: Id3a74cfe5b12cc0e04c165ddbf0f114d4f97c93a
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02218261

--- scripts/drvgen/drvgen.mk
+++ scripts/drvgen/drvgen.mk
@@ -10,19 +10,13 @@ ifndef DRVGEN_OUT
 DRVGEN_OUT := $(objtree)/$(DRVGEN_PATH)
 endif
 
-DRVGEN_CUSTOM_OUT := $(objtree)/arch/$(SRCARCH)/boot/dts/custom/
+DRVGEN_CUSTOM_OUT := $(objtree)/arch/$(SRCARCH)/boot/dts/
 
 export DRVGEN_OUT
 
 DRVGEN_OUT_PATH := $(DRVGEN_OUT)/inc
 
-  ALL_DRVGEN_FILE := cust_eint.dtsi
-  ALL_DRVGEN_FILE += cust_adc.dtsi
-  ALL_DRVGEN_FILE += cust_i2c.dtsi
-  ALL_DRVGEN_FILE += cust_md1_eint.dtsi
-  ALL_DRVGEN_FILE += cust_kpd.dtsi
-  ALL_DRVGEN_FILE += cust_clk_buf.dtsi
-  ALL_DRVGEN_FILE += cust_pmic.dtsi
+ALL_DRVGEN_FILE := cust.dtsi
 
 DWS_FILE := $(srctree)/$(DRVGEN_PATH)/codegen.dws
 ifneq ($(wildcard $(DWS_FILE)),)
@@ -38,48 +32,13 @@ DRVGEN_PREBUILT_CHECK := $(filter-out $(wildcard $(addprefix $(DRVGEN_PREBUILT_P
 drvgen: $(DRVGEN_FILE_LIST)
 ifneq ($(DRVGEN_PREBUILT_CHECK),)
 
-$(DRVGEN_OUT)/cust_eint.dtsi: $(DWS_FILE)
-	@mkdir -p $(dir $@)
-	@mkdir -p $(DRVGEN_CUSTOM_OUT)
-	@$(DRVGEN_TOOL) $(DWS_FILE) $(DRVGEN_CUSTOM_OUT) $(DRVGEN_OUT_PATH) eint_dtsi
 
-$(DRVGEN_OUT)/inc/pmic_drv.c: $(DWS_FILE)
-	@mkdir -p $(dir $@)
-	@$(DRVGEN_TOOL) $(DWS_FILE) $(DRVGEN_OUT_PATH) $(DRVGEN_OUT_PATH) pmic_c
-
-$(DRVGEN_OUT)/cust_i2c.dtsi: $(DWS_FILE)
-#	@mkdir -p $(dir $@)
+$(DRVGEN_OUT)/cust.dtsi: $(DWS_FILE)
 	@mkdir -p $(DRVGEN_CUSTOM_OUT)
-	@$(DRVGEN_TOOL) $(DWS_FILE) $(DRVGEN_CUSTOM_OUT) $(DRVGEN_OUT_PATH) i2c_dtsi
-
-$(DRVGEN_OUT)/cust_adc.dtsi: $(DWS_FILE)
-#	@mkdir -p $(dir $@)
-	@mkdir -p $(DRVGEN_CUSTOM_OUT)
-	@$(DRVGEN_TOOL) $(DWS_FILE) $(DRVGEN_CUSTOM_OUT) $(DRVGEN_OUT_PATH) adc_dtsi
-
-$(DRVGEN_OUT)/cust_md1_eint.dtsi: $(DWS_FILE)
-#	@mkdir -p $(dir $@)
-	@mkdir -p $(DRVGEN_CUSTOM_OUT)
-	@$(DRVGEN_TOOL) $(DWS_FILE) $(DRVGEN_CUSTOM_OUT) $(DRVGEN_OUT_PATH) md1_eint_dtsi
-
-$(DRVGEN_OUT)/cust_kpd.dtsi: $(DWS_FILE)
-#	@mkdir -p $(dir $@)
-	@mkdir -p $(DRVGEN_CUSTOM_OUT)
-	@$(DRVGEN_TOOL) $(DWS_FILE) $(DRVGEN_CUSTOM_OUT) $(DRVGEN_OUT_PATH) kpd_dtsi
-
-$(DRVGEN_OUT)/cust_clk_buf.dtsi: $(DWS_FILE)
-#	@mkdir -p $(dir $@)
-	@mkdir -p $(DRVGEN_CUSTOM_OUT)
-	@$(DRVGEN_TOOL) $(DWS_FILE) $(DRVGEN_CUSTOM_OUT) $(DRVGEN_OUT_PATH) clk_buf_dtsi
-
-$(DRVGEN_OUT)/cust_pmic.dtsi: $(DWS_FILE)
-#	@mkdir -p $(dir $@)
-	@mkdir -p $(DRVGEN_CUSTOM_OUT)
-	@$(DRVGEN_TOOL) $(DWS_FILE) $(DRVGEN_CUSTOM_OUT) $(DRVGEN_OUT_PATH) pmic_dtsi
+	@$(DRVGEN_TOOL) $(DWS_FILE) $(DRVGEN_CUSTOM_OUT) $(DRVGEN_OUT_PATH) cust_dtsi
 
 else
 $(DRVGEN_FILE_LIST): $(DRVGEN_OUT)/% : $(DRVGEN_PREBUILT_PATH)/%
-#	@mkdir -p $(dir $@)
 	@mkdir -p $(DRVGEN_CUSTOM_OUT)
 	cp -f $< $@
 endif
