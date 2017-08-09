19568b887f93eb2a9633d04d1274bae8f45e29db
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 03:31:43 +0800

    [ALPS02439918] GPU: fix a memory corruption in gpu_fault_wq
    [Detail] GPU: fix a memory corruption in gpu_fault_wq
    
    [Solution] GPU: fix a memory corruption in gpu_fault_wq
    
    MTK-Commit-Id: da7a0181a216dc90e6c13c15eb550de13387103f
    
    Change-Id: Ie103a3c705fb5130545ebfb0ad0e7d12c4998f06
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02439918
    Feature: DeskClock enhancement

--- drivers/misc/mediatek/gpu/gpu_mali/mali_midgard/mali-r7p0/drivers/gpu/arm/midgard/backend/gpu/mali_kbase_device_hw.c
+++ drivers/misc/mediatek/gpu/gpu_mali/mali_midgard/mali-r7p0/drivers/gpu/arm/midgard/backend/gpu/mali_kbase_device_hw.c
@@ -105,20 +105,18 @@ static void kbase_report_gpu_fault(struct kbase_device *kbdev, int multiple)
 			address);
 	if (multiple)
 		dev_warn(kbdev->dev, "There were multiple GPU faults - some have not been reported\n");
+
+	if (kbdev->gpu_fault_wq)
 	{
 		struct gpu_fault_event *event;
-		event = kmalloc(sizeof(event), GFP_ATOMIC);
+		event = kmalloc(sizeof(struct gpu_fault_event), GFP_ATOMIC);
 		event->gpu_fault_address = address;
 		INIT_WORK(&event->gpu_fault_work, kbasep_gpu_fault_worker);
-
-		if (kbdev->gpu_fault_wq)
-		{
-			queue_work(kbdev->gpu_fault_wq, &event->gpu_fault_work);
-		}
-		else
-		{
-			pr_err("kbdev->gpu_fault_wq = %p", kbdev->gpu_fault_wq);
-		}
+		queue_work(kbdev->gpu_fault_wq, &event->gpu_fault_work);
+	}
+	else
+	{
+		pr_err("kbdev->gpu_fault_wq = %p", kbdev->gpu_fault_wq);
 	}
 }
 
