a949c9a9fc6ab416a7abf1a371fada62855817d6
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 14:16:41 +0800

    [ALPS02099624] USB:MTK USB device K318 ready task
    [Detail]
    add mtk usb20 folder
    adjust dtsi and related config but currently disabled
    remove gadget folder coz android not ready yet
    currently got some workaround to avoid build error
    USB should work after Power module ready & gadget ready
    search "GIT K138 WORKAROUND" to check workaround
    
    [Solution]
    N/A
    
    [Feature] USB
    
    MTK-Commit-Id: bc4d6f4f20d446374f9ae1b5a3380fd01170786c
    
    Change-Id: Id27ebc2a43c4ebb364937a1bddddc802b4ce6b4c
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    Conflicts:
    CR-Id: ALPS02099624

---
+++ drivers/misc/mediatek/usb20/musb_qmu.h
@@ -0,0 +1,22 @@
+#ifndef _MUSB_QMU_H_
+#define _MUSB_QMU_H_
+
+#ifdef MUSB_QMU_SUPPORT
+#include "musb_core.h"		/* for struct musb */
+
+extern int musb_qmu_init(struct musb *musb);
+extern void musb_qmu_exit(struct musb *musb);
+extern void musb_kick_D_CmdQ(struct musb *musb, struct musb_request *request);
+extern void musb_disable_q_all(struct musb *musb);
+extern irqreturn_t musb_q_irq(struct musb *musb);
+extern void musb_flush_qmu(u32 ep_num, u8 isRx);
+extern void musb_restart_qmu(struct musb *musb, u32 ep_num, u8 isRx);
+extern bool musb_is_qmu_stop(u32 ep_num, u8 isRx);
+extern void musb_tx_zlp_qmu(struct musb *musb, u32 ep_num);
+
+/*FIXME, not good layer present */
+extern void mtk_qmu_enable(struct musb *musb, u8 EP_Num, u8 isRx);
+extern void __iomem *qmu_base;
+
+#endif
+#endif
