6253f42d4629e56681e0655ecce14dcfa375f209
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 14:15:37 +0800

    [ALPS02097178] ext4: allow multiple uid using reserved blocks
    [Detail]
    During booting up on Android phones, core services requires
    to write some data to /data. If there is no space left, the
    system will give a native exception(NE).
    
    [Solution]
    ext4 originally keeps only one reserved uid, change the
    statement to allow multiple uids, so that Android core
    services (uid<10010) can use the reserved blocks.
    
    For example, in init.($ro.hardware).rc
    exec /system/bin/tune2fs -O has_journal -u 10010 -r 4096
       /dev/block/platform/mtk-msdc.0/by-name/userdata
    
    [Feature] [Android Default] EXT4 File System
    
    MTK-Commit-Id: 507d5dcf3d85585d24f8697912972a440b8e19b3
    
    Change-Id: I74069d99f1ca3c2271c36ef4b502fca7a028b5b7
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02097178

--- fs/ext4/balloc.c
+++ fs/ext4/balloc.c
@@ -555,7 +555,7 @@ static int ext4_has_free_clusters(struct ext4_sb_info *sbi,
 		return 1;
 
 	/* Hm, nope.  Are (enough) root reserved clusters available? */
-	if (uid_eq(sbi->s_resuid, current_fsuid()) ||
+	if (uid_gte(sbi->s_resuid, current_fsuid()) ||
 	    (!gid_eq(sbi->s_resgid, GLOBAL_ROOT_GID) && in_group_p(sbi->s_resgid)) ||
 	    capable(CAP_SYS_RESOURCE) ||
 	    (flags & EXT4_MB_USE_ROOT_BLOCKS)) {
