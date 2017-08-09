a177f483e1e75ca214a1ecbd6e56159a68e2c0f7
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 22:52:26 +0800

    [ALPS02346515] bootprof: record begin/end timestamp
    [Detail]
    Need to record begin/end timestamp for bootprof
    
    [Solution]
    Add on/off timestamp to record bootprof begin/end
    
    [Feature] Power Management
    
    MTK-Commit-Id: 3405251117c2616f29c2758f4656fa9a00cb6321
    
    Change-Id: I23a7c0da85dee0c8a2f49aa92dba62cb25007fc0
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02346515
    (cherry picked from commit 4a2d875ba1918535a6e880caa9fde9203975a3bb)

--- drivers/misc/mediatek/mtprof/bootprof.c
+++ drivers/misc/mediatek/mtprof/bootprof.c
@@ -21,13 +21,13 @@ struct boot_log_struct {
 	u64 timestamp;
 	char event[BOOT_STR_SIZE];
 } mt_bootprof[BOOT_LOG_NUM];
-int boot_log_count = 0;
 
+static int boot_log_count;
 static DEFINE_MUTEX(mt_bootprof_lock);
 static int mt_bootprof_enabled;
 static int bootprof_lk_t, bootprof_pl_t;
+static u64 timestamp_on, timestamp_off;
 int boot_finish = 0;
-
 
 module_param_named(pl_t, bootprof_pl_t, int, S_IRUGO | S_IWUSR);
 module_param_named(lk_t, bootprof_lk_t, int, S_IRUGO | S_IWUSR);
@@ -67,10 +67,16 @@ static void mt_bootprof_switch(int on)
 {
 	mutex_lock(&mt_bootprof_lock);
 	if (mt_bootprof_enabled ^ on) {
+		unsigned long long ts = sched_clock();
+
+		pr_err("BOOTPROF:%10Ld.%06ld: %s\n", nsec_high(ts), nsec_low(ts), on ? "ON" : "OFF");
+
 		if (on) {
 			mt_bootprof_enabled = 1;
+			timestamp_on = ts;
 		} else {	/* boot up complete */
 			mt_bootprof_enabled = 0;
+			timestamp_off = ts;
 			boot_finish = 1;
 			/* log_store_bootup(); */
 			bootup_finish();
@@ -95,7 +101,7 @@ static ssize_t mt_bootprof_write(struct file *filp, const char *ubuf, size_t cnt
 			mt_bootprof_switch(0);
 		else if (buf[0] == '1')
 			mt_bootprof_switch(1);
-	return 1;
+		return 1;
 	}
 
 	buf[copy_size] = 0;
@@ -116,16 +122,20 @@ static int mt_bootprof_show(struct seq_file *m, void *v)
 	if (bootprof_pl_t > 0 && bootprof_lk_t > 0) {
 		SEQ_printf(m, "%10d        : %s\n", bootprof_pl_t, "preloader");
 		SEQ_printf(m, "%10d        : %s\n", bootprof_lk_t, "lk");
-		SEQ_printf(m, "%10d        : %s\n",
-			   gpt_boot_time() - bootprof_pl_t - bootprof_lk_t, "lk->Kernel");
 		SEQ_printf(m, "----------------------------------------\n");
 	}
+
+	SEQ_printf(m, "%10Ld.%06ld : ON\n",
+		   nsec_high(timestamp_on), nsec_low(timestamp_on));
 
 	for (i = 0; i < boot_log_count; i++) {
 		SEQ_printf(m, "%10Ld.%06ld : %s\n",
 			   nsec_high(mt_bootprof[i].timestamp), nsec_low(mt_bootprof[i].timestamp),
 			   mt_bootprof[i].event);
 	}
+
+	SEQ_printf(m, "%10Ld.%06ld : OFF\n",
+		   nsec_high(timestamp_off), nsec_low(timestamp_off));
 	SEQ_printf(m, "----------------------------------------\n");
 	return 0;
 }
