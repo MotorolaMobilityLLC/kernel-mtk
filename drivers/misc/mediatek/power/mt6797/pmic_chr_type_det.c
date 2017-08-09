a141f26aff63c62f40731f7995ba86f4768b5ef9
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 20:35:02 +0800

    [ALPS02285591] charger: port detection fix build error
    [Detail] fixed build error
    
    [Solution] marked with comments on error parts
    
    [Feature] Others
    
    MTK-Commit-Id: 4b414c071188683869ce9155b7cf769da8d5053b
    
    Change-Id: Iaa0827611b7b7b895f65e76c783e5040d916b41a
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02285591

--- drivers/misc/mediatek/power/mt6797/pmic_chr_type_det.c
+++ drivers/misc/mediatek/power/mt6797/pmic_chr_type_det.c
@@ -23,7 +23,7 @@
 #include <linux/seq_file.h>
 
 #include <asm/uaccess.h>
-
+#include <mt-plat/charging.h>
 #include <mt-plat/upmu_common.h>
 #include <mach/upmu_sw.h>
 #include <mach/upmu_hw.h>
@@ -51,16 +51,16 @@ int hw_charging_get_charger_type(void)
 
 #else
 
-static void hw_bc11_dump_register(void)
+/*static void hw_bc11_dump_register(void)
 {
-/*
+
     battery_log(BAT_LOG_FULL, "Reg[0x%x]=0x%x,Reg[0x%x]=0x%x\n",
 	MT6325_CHR_CON20, upmu_get_reg_value(MT6325_CHR_CON20),
 	MT6325_CHR_CON21, upmu_get_reg_value(MT6325_CHR_CON21)
 	);
-*/
-}
 
+}
+*/
 static void hw_bc11_init(void)
 {
 	msleep(200);
@@ -86,11 +86,11 @@ static void hw_bc11_init(void)
 
 	msleep(50);
 	/* mdelay(50); */
-
+/*
 	if (Enable_BATDRV_LOG == BAT_LOG_FULL) {
 		battery_log(BAT_LOG_FULL, "hw_bc11_init() \r\n");
 		hw_bc11_dump_register();
-	}
+	}*/
 
 }
 
@@ -113,10 +113,10 @@ static unsigned int hw_bc11_DCD(void)
 
 	wChargerAvail = bc11_get_register_value(PMIC_RGS_BC11_CMP_OUT);
 
-	if (Enable_BATDRV_LOG == BAT_LOG_FULL) {
+/*	if (Enable_BATDRV_LOG == BAT_LOG_FULL) {
 		battery_log(BAT_LOG_FULL, "hw_bc11_DCD() \r\n");
 		hw_bc11_dump_register();
-	}
+	}*/
 	/* RG_bc11_IPU_EN[1.0] = 00 */
 	bc11_set_register_value(PMIC_RG_BC11_IPU_EN, 0x0);
 	/* RG_bc11_IPD_EN[1.0] = 00 */
@@ -147,10 +147,10 @@ static unsigned int hw_bc11_stepA1(void)
 
 	wChargerAvail = bc11_get_register_value(PMIC_RGS_BC11_CMP_OUT);
 
-	if (Enable_BATDRV_LOG == BAT_LOG_FULL) {
+/*	if (Enable_BATDRV_LOG == BAT_LOG_FULL) {
 		battery_log(BAT_LOG_FULL, "hw_bc11_stepA1() \r\n");
 		hw_bc11_dump_register();
-	}
+	}*/
 	/* RG_bc11_IPD_EN[1.0] = 00 */
 	bc11_set_register_value(PMIC_RG_BC11_IPD_EN, 0x0);
 	/* RG_bc11_CMP_EN[1.0] = 00 */
@@ -178,10 +178,10 @@ static unsigned int hw_bc11_stepA2(void)
 
 	wChargerAvail = bc11_get_register_value(PMIC_RGS_BC11_CMP_OUT);
 
-	if (Enable_BATDRV_LOG == BAT_LOG_FULL) {
+/*	if (Enable_BATDRV_LOG == BAT_LOG_FULL) {
 		battery_log(BAT_LOG_FULL, "hw_bc11_stepA2() \r\n");
 		hw_bc11_dump_register();
-	}
+	}*/
 	/* RG_bc11_VSRC_EN[1:0]=00 */
 	bc11_set_register_value(PMIC_RG_BC11_VSRC_EN, 0x0);
 	/* RG_bc11_IPD_EN[1.0] = 00 */
@@ -209,10 +209,10 @@ static unsigned int hw_bc11_stepB2(void)
 
 	wChargerAvail = bc11_get_register_value(PMIC_RGS_BC11_CMP_OUT);
 
-	if (Enable_BATDRV_LOG == BAT_LOG_FULL) {
+/*	if (Enable_BATDRV_LOG == BAT_LOG_FULL) {
 		battery_log(BAT_LOG_FULL, "hw_bc11_stepB2() \r\n");
 		hw_bc11_dump_register();
-	}
+	}*/
 
 
 	if (!wChargerAvail) {
@@ -250,10 +250,10 @@ static void hw_bc11_done(void)
 
 	Charger_Detect_Release();
 
-	if (Enable_BATDRV_LOG == BAT_LOG_FULL) {
+/*	if (Enable_BATDRV_LOG == BAT_LOG_FULL) {
 		battery_log(BAT_LOG_FULL, "hw_bc11_done() \r\n");
 		hw_bc11_dump_register();
-	}
+	}*/
 
 }
 
@@ -273,10 +273,10 @@ int hw_charging_get_charger_type(void)
 		/********* Step A1 ***************/
 		if (1 == hw_bc11_stepA1()) {
 			CHR_Type_num = APPLE_2_1A_CHARGER;
-			battery_log(BAT_LOG_CRTI, "step A1 : Apple 2.1A CHARGER!\r\n");
+			/*battery_log(1, "step A1 : Apple 2.1A CHARGER!\r\n");*/
 		} else {
 			CHR_Type_num = NONSTANDARD_CHARGER;
-			battery_log(BAT_LOG_CRTI, "step A1 : Non STANDARD CHARGER!\r\n");
+			/*battery_log(1, "step A1 : Non STANDARD CHARGER!\r\n");*/
 		}
 	} else {
 	/********* Step A2 ***************/
@@ -285,14 +285,14 @@ int hw_charging_get_charger_type(void)
 			if (1 == hw_bc11_stepB2()) {
 				is_dcp_type = true;
 				CHR_Type_num = STANDARD_CHARGER;
-				battery_log(BAT_LOG_CRTI, "step B2 : STANDARD CHARGER!\r\n");
+				/*battery_log(1, "step B2 : STANDARD CHARGER!\r\n");*/
 			} else {
 				CHR_Type_num = CHARGING_HOST;
-				battery_log(BAT_LOG_CRTI, "step B2 :  Charging Host!\r\n");
+				/*battery_log(1, "step B2 :  Charging Host!\r\n");*/
 			}
 		} else {
 			CHR_Type_num = STANDARD_HOST;
-			battery_log(BAT_LOG_CRTI, "step A2 : Standard USB Host!\r\n");
+			/*battery_log(1, "step A2 : Standard USB Host!\r\n");*/
 		}
 
 	}
