e61d39644b535effdd726b97cf872d1cf7183ac6
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 21:45:04 +0800

    [ALPS02337589] aal:Disp pwm source clock setting
    [Detail]
    Select ULPOSC as disp pwm source clock for low power.
    
    [Solution]
    Config ULPOSC 29M as default disp pwm clock source setting.
    
    [Feature] Ambient Light Adaptive Luma
    
    MTK-Commit-Id: a0126f942221a3328646a84482c322239a0c9e8a
    
    Change-Id: Ic29fc58dae99315f74539cb237ca078f5296b454
    CR-Id: ALPS02337589
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>

--- drivers/misc/mediatek/video/mt6797/dispsys/ddp_pwm_mux.c
+++ drivers/misc/mediatek/video/mt6797/dispsys/ddp_pwm_mux.c
@@ -13,23 +13,26 @@ eDDP_CLK_ID disp_pwm_get_clkid(unsigned int clk_req)
 	eDDP_CLK_ID clkid = -1;
 
 	switch (clk_req) {
+	case 0:
+		clkid = ULPOSC_D8; /* ULPOSC 29M */
+		break;
 	case 1:
-		clkid = UNIVPLL2_D4;
+		clkid = ULPOSC_D2; /* ULPOSC 117M */
 		break;
 	case 2:
-		clkid = ULPOSC_D2;
+		clkid = UNIVPLL2_D4; /* PLL 104M */
 		break;
 	case 3:
-		clkid = ULPOSC_D3;
+		clkid = ULPOSC_D3; /* ULPOSC 78M */
 		break;
 	case 4:
-		clkid = ULPOSC_D8;
+		clkid = -1; /* Bypass config:default 26M */
 		break;
 	case 5:
-		clkid = ULPOSC_D10;
+		clkid = ULPOSC_D10; /* ULPOSC 23M */
 		break;
 	case 6:
-		clkid = ULPOSC_D4;
+		clkid = ULPOSC_D4; /* ULPOSC 58M */
 		break;
 	default:
 		clkid = -1;
