2f38631f565167abd2c99c916efd8705bca516cb
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 06:40:29 +0800

    [ALPS02607418] pmic: modify pmic irq init setting
    [Detail]
    pmic irq may disable and cannot enable
    
    [Solution]
    move PMIC_EINT_SETTING before pmic_thread_kthread run
    
    MTK-Commit-Id: b35b3db40e5cfca4cc45f0919b4e733de2cf34a2
    
    Change-Id: Ic8f484c177cd0335db37bc1d2c6fc9503de94658
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02607418
    Feature: Others
    (cherry picked from commit b604319cc5888ba86efd2eaf77acd09e1fac8e6e)

--- drivers/misc/mediatek/power/mt6797/pmic.c
+++ drivers/misc/mediatek/power/mt6797/pmic.c
@@ -4640,6 +4640,7 @@ static int pmic_mt_probe(struct platform_device *dev)
 	PMICLOG("[PMIC_EINT_SETTING] disable when CONFIG_MTK_FPGA\n");
 #else
 	/*PMIC Interrupt Service*/
+	PMIC_EINT_SETTING();
 	pmic_thread_handle = kthread_create(pmic_thread_kthread, (void *)NULL, "pmic_thread");
 	if (IS_ERR(pmic_thread_handle)) {
 		pmic_thread_handle = NULL;
@@ -4649,7 +4650,6 @@ static int pmic_mt_probe(struct platform_device *dev)
 		PMICLOG("[pmic_thread_kthread_mt6325] kthread_create Done\n");
 	}
 
-	PMIC_EINT_SETTING();
 	PMICLOG("[PMIC_EINT_SETTING] Done\n");
 #endif
 
