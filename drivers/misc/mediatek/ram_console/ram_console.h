ab600909c19e2dc4b02d6e9e7472600f5e6b59ad
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 14:46:03 +0800

    [ALPS02164827] ram_console: porting to kernel-3.18
    [Detail] Porting ram_console feature to GIT kernel-3.18
    
    [Solution] Porting ram_console code to GIT kernel-3.18 and enable feature
    
    [Feature] Others
    
    MTK-Commit-Id: 5ea1bbd49287ac7cd3a31ab1632c4835ac6a71ad
    
    Change-Id: Ieafd79df63f44a8a269072ca1425ad05401768d3
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02164827

---
+++ drivers/misc/mediatek/ram_console/ram_console.h
@@ -0,0 +1,10 @@
+#ifndef __MTK_RAM_CONSOLE_H__
+#define __MTK_RAM_CONSOLE_H__
+extern int card_dump_func_write(unsigned char *buf, unsigned int len, unsigned long long offset,
+				int dev);
+extern int boot_finish;
+extern struct file *expdb_open(void);
+#ifdef CONFIG_PSTORE
+extern void pstore_bconsole_write(struct console *con, const char *s, unsigned c);
+#endif
+#endif
