4ca42528e31c718610ec055eff654fdd827dfa9a
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 06:16:22 +0800

    [ALPS02579882] gpu_dvfs: mt6755: add kernel file header disclaimer
    [Detail]
    
    [Solution]
    
    MTK-Commit-Id: fbc18a41d1db6016e625493c8be03f5f64ea49e5
    
    Change-Id: I577480bb479cc2bfaebda1735cd9bf0154a0a3f3
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02579882
    Feature: GPU DVFS
    (cherry picked from commit 3b16def32f4642d5e2ae44d2dde27a7b4ce4152d)

--- drivers/misc/mediatek/base/power/mt6755/mt_gpufreq.c
+++ drivers/misc/mediatek/base/power/mt6755/mt_gpufreq.c
@@ -1,8 +1,14 @@
 /*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Copyright (C) 2015-2016 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  */
 
 #include <linux/kernel.h>
--- drivers/misc/mediatek/base/power/mt6755/mt_gpufreq_bringup.c
+++ drivers/misc/mediatek/base/power/mt6755/mt_gpufreq_bringup.c
@@ -1,4 +1,16 @@
-/**** public APIs *****/
+/*
+ * Copyright (C) 2015-2016 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
 unsigned int mt_gpufreq_voltage_enable_set(unsigned int enable)
 {
 	return 0;
