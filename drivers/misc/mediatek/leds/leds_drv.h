96fa9841b5628bce00bb9178b41d88848c77be90
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-09 14:21:11 +0800

    [ALPS02124193]LEDs : LEDs&Backlight driver porting
    [Detail] LEDs&Backlight driver porting in kernel 3.18
    
    [Solution] porting the LEDs&Backlight driver, but dont turn on
    			the CONFIG_MTK_LEDS as the pmic is not ready.
    
    [Feature] [Android Default] Backlight
    
    MTK-Commit-Id: 27fd1f4a3233255a7711628bc1ea85c348f7581b
    
    Change-Id: I6585f753f351b7b037ac6db8820eeaf8a020fe57
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02124193

---
+++ drivers/misc/mediatek/leds/leds_drv.h
@@ -0,0 +1,13 @@
+#include <linux/leds.h>
+#include <leds_hal.h>
+
+/****************************************************************************
+ * LED DRV functions
+ ***************************************************************************/
+
+#ifdef CONTROL_BL_TEMPERATURE
+int setMaxbrightness(int max_level, int enable);
+#endif
+
+extern int mt65xx_leds_brightness_set(enum mt65xx_led_type type, enum led_brightness level);
+extern int disp_bls_set_max_backlight(unsigned int level);
