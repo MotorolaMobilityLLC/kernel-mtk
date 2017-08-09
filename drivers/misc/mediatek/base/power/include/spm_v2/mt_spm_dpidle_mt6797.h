0080766d3f35b54a6936b2878c6668903252eb70
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 02:45:16 +0800

    [ALPS02403518] TEEI: MT6797
    [Detail] Microtrust TEEI for power saving.
    
    [Solution] Remover SEJ to enter idle/SDIO mode.
    
    MTK-Commit-Id: 36d99ca4bca6594004d338729c461a9f09a0cc8b
    
    Change-Id: I06083ef43230927d56c11c98d2a09c2d78bbd097
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02403518
    Feature: Microtrust TEEI

--- drivers/misc/mediatek/base/power/include/spm_v2/mt_spm_dpidle_mt6797.h
+++ drivers/misc/mediatek/base/power/include/spm_v2/mt_spm_dpidle_mt6797.h
@@ -3,6 +3,25 @@
 
 #include "mt_spm.h"
 
+#if defined(CONFIG_MICROTRUST_TEE_SUPPORT)
+#define WAKE_SRC_FOR_DPIDLE             \
+	(WAKE_SRC_R12_KP_IRQ_B |            \
+	WAKE_SRC_R12_APXGPT1_EVENT_B |      \
+	WAKE_SRC_R12_CONN2AP_SPM_WAKEUP_B | \
+	WAKE_SRC_R12_EINT_EVENT_B |         \
+	WAKE_SRC_R12_CONN_WDT_IRQ_B |       \
+	WAKE_SRC_R12_CCIF0_EVENT_B |        \
+	WAKE_SRC_R12_MD32_SPM_IRQ_B |       \
+	WAKE_SRC_R12_USB0_CDSC_B_AND_USB1_CSDC_B |           \
+	WAKE_SRC_R12_USB0_POWERDWN_B_AND_USB1_POWERDWN_B |       \
+	WAKE_SRC_R12_C2K_WDT_IRQ_B |        \
+	WAKE_SRC_R12_EINT_EVENT_SECURE_B |  \
+	WAKE_SRC_R12_CCIF1_EVENT_B |        \
+	WAKE_SRC_R12_AFE_IRQ_MCU_B |        \
+	WAKE_SRC_R12_SYS_CIRQ_IRQ_B |       \
+	WAKE_SRC_R12_MD1_WDT_B |            \
+	WAKE_SRC_R12_CLDMA_EVENT_B)
+#else
 #define WAKE_SRC_FOR_DPIDLE             \
 	(WAKE_SRC_R12_KP_IRQ_B |            \
 	WAKE_SRC_R12_APXGPT1_EVENT_B |      \
@@ -21,6 +40,7 @@
 	WAKE_SRC_R12_MD1_WDT_B |            \
 	WAKE_SRC_R12_CLDMA_EVENT_B |        \
 	WAKE_SRC_R12_SEJ_WDT_B_AND_SEJ_GPT_B)
+#endif /* #if defined(CONFIG_MICROTRUST_TEE_SUPPORT) */
 
 #endif /* __MT_SPM_DPIDLE_MT6797__H__ */
 
