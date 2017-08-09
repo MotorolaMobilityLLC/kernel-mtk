b693ff5faa95524988d0593b2f9084eba9fd2d58
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 05:54:26 +0800

    [ALPS02566671] MT6797: CPU Hotplug: code cleanup
    [Detail] cleanup code
    
    [Solution] cleanup code
    
    MTK-Commit-Id: 85798d2b516b4f7a6a414eaea2ba1fc29eaf4e48
    
    Change-Id: I39c0416a87f8cfb9af6171fb1660468d731a6cc4
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02566671
    Feature: CPU Hotplug

--- drivers/misc/mediatek/base/power/mt6797/mt_hotplug_strategy.h
+++ drivers/misc/mediatek/base/power/mt6797/mt_hotplug_strategy.h
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
 /**
 * @file    mt_hotplug_strategy.h
 * @brief   hotplug strategy(hps) - external header file
@@ -5,7 +18,6 @@
 
 #ifndef __MT_HOTPLUG_STRATEGY_H__
 #define __MT_HOTPLUG_STRATEGY_H__
-
 
 /*=============================================================*/
 /* Include files */
--- drivers/misc/mediatek/base/power/mt6797/mt_hotplug_strategy_ops_mt6797.c
+++ drivers/misc/mediatek/base/power/mt6797/mt_hotplug_strategy_ops_mt6797.c
@@ -1,7 +1,15 @@
-/**
-* @file    mt_hotplug_strategy_core.c
-* @brief   hotplug strategy(hps) - core
-*/
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
 
 /*============================================================================*/
 /* Include files */
