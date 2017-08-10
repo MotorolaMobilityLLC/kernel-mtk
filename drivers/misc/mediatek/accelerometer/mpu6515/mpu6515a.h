79708b5dbf5676f5783722130384db5f31039990
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 20:00:37 +0800

    [ALPS03400241] peripheral: correct the License announcement
    [Detail]
    correct the License announcement
    
    MTK-Commit-Id: 6e29e697fe606598973ed7cf6666ab477e75819f
    
    Change-Id: Ia0cc85d7cf93366ef01bf540d1f6212e8b761cfa
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS03400241
    Feature: Others

--- drivers/misc/mediatek/accelerometer/mpu6515/mpu6515a.h
+++ drivers/misc/mediatek/accelerometer/mpu6515/mpu6515a.h
@@ -1,3 +1,16 @@
+/* MPU6515 motion sensor driver
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
 #ifndef MPU6515A_H
 #define MPU6515A_H
 
