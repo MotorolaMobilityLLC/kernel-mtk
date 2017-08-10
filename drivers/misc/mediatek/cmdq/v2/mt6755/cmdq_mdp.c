a1ed84f1d7a0b72196491a1960e17fc052fea28b
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 20:07:01 +0800

    [ALPS03408072] cmdq: fix ke during dump
    Dump task by engine flag may lock before call,
    which cause spinlock->mutex->spinlock flow.
    
    [Solution]
    Design another function for dump case.
    
    MTK-Commit-Id: 79377ba7e2cd28445920b500d5c4f1bcb0e5805e
    
    Change-Id: I494f2eda41529e904cc842648f320579231a3c50
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS03408072
    Feature: CMDQ

--- drivers/misc/mediatek/cmdq/v2/mt6755/cmdq_mdp.c
+++ drivers/misc/mediatek/cmdq/v2/mt6755/cmdq_mdp.c
@@ -736,9 +736,13 @@ const char *cmdq_mdp_dispatch(uint64_t engineFlag)
 	int32_t status = 0;
 	const char *module = "MDP";
 
-	task.userDebugStr = kzalloc(debug_str_len, GFP_KERNEL);
+	task.userDebugStr = kzalloc(debug_str_len, GFP_ATOMIC);
+	if (!task.userDebugStr) {
+		CMDQ_ERR("fail to alloc debug buffer\n");
+		return "MDP";
+	}
 
-	status = cmdq_core_get_running_task_by_engine(engineFlag, debug_str_len, &task);
+	status = cmdq_core_get_running_task_by_engine_unlock(engineFlag, debug_str_len, &task);
 	if (status < 0) {
 		CMDQ_ERR("Failed: get task by engine flag: 0x%016llx, task flag: 0x%016llx\n",
 			engineFlag, task.engineFlag);
