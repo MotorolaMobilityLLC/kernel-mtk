6a20e8615dc960eba7657602571d52946368845b
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 18:31:15 +0800

    [ALPS03320965] timer: fix section build warning
    [Detail]
    fix section build warning
    
    MTK-Commit-Id: 213e27374f8bf11a148a6da834919ed1c4cde5cc
    
    Change-Id: Ie9ada2753ce30572daa5bde8fa4eb55a005edf0e
    CR-Id: ALPS03320965
    Feature: timer
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>

--- drivers/clocksource/cpuxgpt_mt6797.c
+++ drivers/clocksource/cpuxgpt_mt6797.c
@@ -64,7 +64,7 @@ static irqreturn_t __cpuxgpt7_irq_handler(int irq, void *dev_id);
 static irqreturn_t __cpuxgpt8_irq_handler(int irq, void *dev_id);
 static irqreturn_t __cpuxgpt9_irq_handler(int irq, void *dev_id);
 
-static const struct of_device_id cpuxgpt_addr_ids[] __initconst = {
+static const struct of_device_id cpuxgpt_addr_ids[] = {
 	{.compatible = "mediatek,mt6797-cpuxgpt"},
 	{},
 };
