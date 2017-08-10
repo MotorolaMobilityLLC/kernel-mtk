075bc00496c9501d9e355478c0d2405549efc1fd
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 09:53:48 +0800

    [ALPS02725226] c2k_ccci: fix build error
    [Detail] sdio request eirq function prototype changed
    
    [Solution] use new solution to fix
    
    MTK-Commit-Id: 633698f004ea9e31e441b7a6ef8f810f5fe2f501
    
    Change-Id: I843cabd8be5f89cf42f30ff7ca63d0385956cac8
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02725226
    Feature: Modem Interface Driver

--- drivers/misc/mediatek/include/mt-plat/mt6735/include/mach/mt_c2k_sdio.h
+++ drivers/misc/mediatek/include/mt-plat/mt6735/include/mach/mt_c2k_sdio.h
@@ -16,11 +16,10 @@
 
 #include <linux/pm.h>
 
-#include <linux/mmc/sdio_func.h>
-
+typedef void (msdc_c2k_irq_handler_t)(void *);
 typedef void (*pm_callback_t)(pm_message_t state, void *data);
 
-extern void c2k_sdio_request_eirq(sdio_irq_handler_t irq_handler, void *data);
+extern void c2k_sdio_request_eirq(msdc_c2k_irq_handler_t irq_handler, void *data);
 extern void c2k_sdio_enable_eirq(void);
 extern void c2k_sdio_disable_eirq(void);
 extern void c2k_sdio_register_pm(pm_callback_t pm_cb, void *data);
