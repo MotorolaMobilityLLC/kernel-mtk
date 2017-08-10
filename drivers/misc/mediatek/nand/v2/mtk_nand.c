115d648c00ec1aa81b8af254a71bfa74317ea29d
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 13:01:42 +0800

    [ALPS02798010] nand: fix nand build error
    [Detail]
    fix build error assignment from incompatible pointer type.
    
    [Solution]
    delete wrong static prefix.
    
    MTK-Commit-Id: 3ea88e06372c88e8ef0940f0beee0f0a229563fd
    
    Change-Id: I33e041b65e0220ab7ae8084ef5c73abba3ceff0c
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02798010
    Feature: NAND Boot Up

--- drivers/misc/mediatek/nand/v2/mtk_nand.c
+++ drivers/misc/mediatek/nand/v2/mtk_nand.c
@@ -5511,7 +5511,7 @@ int mtk_nand_block_markbad_hw(struct mtd_info *mtd, loff_t offset)
 	return ret;
 }
 
-static int mtk_nand_block_markbad(struct mtd_info *mtd, loff_t offset)
+int mtk_nand_block_markbad(struct mtd_info *mtd, loff_t offset)
 {
 	struct nand_chip *chip = mtd->priv;
 	u32 block = (u32) (offset >> chip->phys_erase_shift);
