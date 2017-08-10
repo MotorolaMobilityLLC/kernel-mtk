74741ac66159a48042f8bdd8b9d62b2f74c0c268
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 12:23:16 +0800

    [ALPS02743293] connectivity:wlan gen2 add GPLv2 header
    [Detail]
    add GPLv2 header and remove history
    
    [Solution]
    modify gen2 47 files
    
    MTK-Commit-Id: 295cb59690f30e6506bb015da112c1a88a18026d
    
    Change-Id: I0fe609110c97575e826fc5034a7578f6fc2a7bb8
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02743293
    Feature: [Android Default] Wi-Fi Driver for SoC
    (cherry picked from commit 10ae95ecceb19745aea8dbe95befddc2f2212881)

--- drivers/misc/mediatek/connectivity/wlan/gen2/os/linux/gl_p2p_kal.c
+++ drivers/misc/mediatek/connectivity/wlan/gen2/os/linux/gl_p2p_kal.c
@@ -1,11 +1,16 @@
 /*
-** Id: @(#) gl_p2p_cfg80211.c@@
+* Copyright (C) 2016 MediaTek Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
 */
 
-/*! \file   gl_p2p_kal.c
-    \brief
-
-*/
 
 /*******************************************************************************
 *                         C O M P I L E R   F L A G S
