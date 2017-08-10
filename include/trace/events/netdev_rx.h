2a9ab7d19fd330fafddf99b16ec6cb6cef67ad7e
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 10:19:24 +0800

    [ALPS02613781] ccmni: add GPL for netdev_rx.h
    [Detail]
    need GPL for kernel file
    [Solution]
    add GPL information for netdev_rx.h
    
    MTK-Commit-Id: c1000b07485d6bf31fc425f6555472e8e26aac27
    
    Change-Id: I74cbd56865703e8f6c9fb2fdc6713d3315471886
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02613781
    Feature: Modem Interface Driver
    (cherry picked from commit 70394b6123136f017df52b88605d3ff4c9355e1a)
    (cherry picked from commit 60e6ff53bbac2380ad4c332ff2acc94b3416bd9e)

--- include/trace/events/netdev_rx.h
+++ include/trace/events/netdev_rx.h
@@ -1,78 +1,24 @@
 /*
- * If TRACE_SYSTEM is defined, that will be the directory created
- * in the ftrace directory under /sys/kernel/debug/tracing/events/<system>
- *
- * The define_trace.h below will also look for a file name of
- * TRACE_SYSTEM.h where TRACE_SYSTEM is what is defined here.
- * In this case, it would look for sample.h
- *
- * If the header name will be different than the system name
- * (as in this case), then you can override the header name that
- * define_trace.h will look up by defining TRACE_INCLUDE_FILE
- *
- * This file is called trace-events-sample.h but we want the system
- * to be called "sample". Therefore we must define the name of this
- * file:
- *
- * #define TRACE_INCLUDE_FILE trace-events-sample
- *
- * As we do an the bottom of this file.
- *
- * Notice that TRACE_SYSTEM should be defined outside of #if
- * protection, just like TRACE_INCLUDE_FILE.
- */
+* Copyright (C) 2016 MediaTek Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+*/
+
 #undef TRACE_SYSTEM
 #define TRACE_SYSTEM netdev_rx
 
-/*
- * Notice that this file is not protected like a normal header.
- * We also must allow for rereading of this file. The
- *
- *  || defined(TRACE_HEADER_MULTI_READ)
- *
- * serves this purpose.
- */
 #if !defined(_TRACE_NETDEV_RX_H) || defined(TRACE_HEADER_MULTI_READ)
 #define _TRACE_NETDEV_RX_H
 
-/*
- * All trace headers should include tracepoint.h, until we finally
- * make it into a standard header.
- */
 #include <linux/tracepoint.h>
-/*
- * The TRACE_EVENT macro is broken up into 5 parts.
- *
- * name: name of the trace point. This is also how to enable the tracepoint.
- *   A function called trace_foo_bar() will be created.
- *
- * proto: the prototype of the function trace_foo_bar()
- *   Here it is trace_foo_bar(char *foo, int bar).
- *
- * args:  must match the arguments in the prototype.
- *    Here it is simply "foo, bar".
- *
- * struct:  This defines the way the data will be stored in the ring buffer.
- *    There are currently two types of elements. __field and __array.
- *    a __field is broken up into (type, name). Where type can be any
- *    type but an array.
- *    For an array. there are three fields. (type, name, size). The
- *    type of elements in the array, the name of the field and the size
- *    of the array.
- *
- *    __array( char, foo, 10) is the same as saying   char foo[10].
- *
- * fast_assign: This is a C like function that is used to store the items
- *    into the ring buffer.
- *
- * printk: This is a way to print out the data in pretty print. This is
- *    useful if the system crashes and you are logging via a serial line,
- *    the data can be printed to the console using this "printk" method.
- *
- * Note, that for both the assign and the printk, __entry is the handler
- * to the data structure in the ring buffer, and is defined by the
- * TP_STRUCT__entry.
- */
+
 TRACE_EVENT(netd_skb_rx,
 
 	TP_PROTO(unsigned long long *net_deta),
@@ -91,7 +37,6 @@ TRACE_EVENT(netd_skb_rx,
 										   __entry->net_deta[0], (unsigned int)__entry->net_deta[1], __entry->net_deta[2],__entry->net_deta[3],
 										   __entry->net_deta[4], __entry->net_deta[5],__entry->net_deta[6], __entry->net_deta[7])
 );
-
 
 TRACE_EVENT(rpsd_skb_rx,
 
@@ -114,38 +59,6 @@ TRACE_EVENT(rpsd_skb_rx,
 #endif
 
 /***** NOTICE! The #if protection ends here. *****/
-
-
-/*
- * There are several ways I could have done this. If I left out the
- * TRACE_INCLUDE_PATH, then it would default to the kernel source
- * include/trace/events directory.
- *
- * I could specify a path from the define_trace.h file back to this
- * file.
- *
- * #define TRACE_INCLUDE_PATH ../../samples/trace_events
- *
- * But the safest and easiest way to simply make it use the directory
- * that the file is in is to add in the Makefile:
- *
- * CFLAGS_trace-events-sample.o := -I$(src)
- *
- * This will make sure the current path is part of the include
- * structure for our file so that define_trace.h can find it.
- *
- * I could have made only the top level directory the include:
- *
- * CFLAGS_trace-events-sample.o := -I$(PWD)
- *
- * And then let the path to this directory be the TRACE_INCLUDE_PATH:
- *
- * #define TRACE_INCLUDE_PATH samples/trace_events
- *
- * But then if something defines "samples" or "trace_events" as a macro
- * then we could risk that being converted too, and give us an unexpected
- * result.
- */
 
 /*
  * TRACE_INCLUDE_FILE is not needed if the filename and TRACE_SYSTEM are equal
