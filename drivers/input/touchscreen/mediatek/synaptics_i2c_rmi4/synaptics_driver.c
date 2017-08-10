0b978ea9f56359c0ec2bc9f81328d93196a3519c
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 09:59:57 +0800

    [ALPS02723532] touch: 8173/8163 touch cannot work
    modify synaptics driver and ft5x0x driver
    modify module_init to late_initcall
    
    MTK-Commit-Id: aca1d82686ceb5b4bd477931cecc30181144dd7d
    
    Change-Id: I6681b8124db997ac92329caff4f08916544a8918
    CR-Id: ALPS02723532
    Feature: touch
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>

--- drivers/input/touchscreen/mediatek/synaptics_i2c_rmi4/synaptics_driver.c
+++ drivers/input/touchscreen/mediatek/synaptics_i2c_rmi4/synaptics_driver.c
@@ -2017,7 +2017,7 @@ static void __exit tpd_driver_exit(void)
 	TPD_DEBUG("Synaptics touch panel driver exit\n");
 	tpd_driver_remove(&tpd_device_driver);
 }
-module_init(tpd_driver_init);
+late_initcall(tpd_driver_init);
 module_exit(tpd_driver_exit);
 
 MODULE_DESCRIPTION("Mediatek Synaptics Driver");
