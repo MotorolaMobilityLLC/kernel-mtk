8a8252549c3ebdab8a489de6371ebf4f46c752c7
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 14:00:39 +0800

    [ALPS02694558] WMT:common part drvier structure refactor
    [Detail] wmt driver enhancement
    
    [Solution]
    1.add mtk copyright and GPLv2 license
    2.delete combo and conn_soc folder
    
    MTK-Commit-Id: 6a3efb1675a3a53cc3271b13d58119300830835d
    
    Change-Id: I0103ea7c521436cb97709e43dd4a7bcd018612bd
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02694558
    Feature: MTK BT Solution(*)

--- drivers/misc/mediatek/connectivity/common/common_main/mt6797/include/mtk_wcn_consys_hw.h
+++ drivers/misc/mediatek/connectivity/common/common_main/mt6797/include/mtk_wcn_consys_hw.h
@@ -1,7 +1,19 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
 /*! \file
-    \brief  Declaration of library functions
-
-    Any definitions in this file will be shared among GLUE Layer and internal Driver Stack.
+*    \brief  Declaration of library functions
+*
+*    Any definitions in this file will be shared among GLUE Layer and internal Driver Stack.
 */
 
 #ifndef _MTK_WCN_CONSYS_HW_H_
