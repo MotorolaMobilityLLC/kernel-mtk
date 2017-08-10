d92b864f17004a8bc29e0548969d95ad1d9472d0
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 13:34:13 +0800

    [ALPS02801176] GPIO: Submit GPIO code to MT6570
    [Detail]
    prepare GPIO kernel driver code to MT6570
    inlude GPIO lib and Pinctrl
    
    [Solution]
    Done
    
    MTK-Commit-Id: e4a53f68bbc4aacab3c04ea07fb20bf91d82cbe7
    
    Change-Id: I7fbe596754da22d23abf3faff2126298f0087079
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02801176
    Feature: Others

--- drivers/pinctrl/mediatek/pinctrl-mtk-common.h
+++ drivers/pinctrl/mediatek/pinctrl-mtk-common.h
@@ -1,16 +1,17 @@
 /*
- * Copyright (c) 2014 MediaTek Inc.
- * Author: Hongzhou.Yang <hongzhou.yang@mediatek.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+* Copyright (C) 2016 MediaTek Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+*/
+
+
 
 #ifndef __PINCTRL_MTK_COMMON_H
 #define __PINCTRL_MTK_COMMON_H
