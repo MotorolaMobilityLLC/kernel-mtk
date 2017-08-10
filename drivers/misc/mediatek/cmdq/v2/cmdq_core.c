038a0d00d0dc8f446c3946ac1099ee11b3008cee
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 14:08:08 +0800

    [ALPS02828772] cmdq: Enable consume again mechanism
    [Detail]
    Put task in wait list will timeout if no next task submit.
    
    [Solution]
    Enable consume again mechanism.
    
    MTK-Commit-Id: 13273bd4574d96cd08d3b414c6f70af939c673db
    
    Change-Id: I7d5a18d7f36aeeecf58ce4bd2302c55eda9f6686
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02828772
    Feature: CMDQ

--- drivers/misc/mediatek/cmdq/v2/cmdq_core.c
+++ drivers/misc/mediatek/cmdq/v2/cmdq_core.c
@@ -102,9 +102,7 @@ DumpFirstErrorStruct gCmdqFirstError;
 
 static DumpCommandBufferStruct gCmdqBufferDump;
 
-#ifdef CMDQ_SECURE_PATH_CONSUME_AGAIN
 static bool g_cmdq_consume_again;
-#endif
 
 /* use to generate [CMDQ_ENGINE_ENUM_id and name] mapping for status print */
 #define CMDQ_FOREACH_MODULE_PRINT(ACTION)\
@@ -3687,10 +3685,10 @@ static int32_t cmdq_core_acquire_thread(uint64_t engineFlag,
 			engineMustEnableClock =
 			    cmdq_core_get_actual_engine_flag_for_enable_clock(engineFlag, thread);
 		}
-#ifdef CMDQ_SECURE_PATH_CONSUME_AGAIN
+
 		if (CMDQ_INVALID_THREAD == thread && true == is_secure && CMDQ_SCENARIO_USER_MDP == scenario)
 			g_cmdq_consume_again = true;
-#endif
+
 		spin_unlock_irqrestore(&gCmdqThreadLock, flags);
 
 		if (CMDQ_INVALID_THREAD != thread) {
@@ -7630,12 +7628,10 @@ int32_t cmdqCoreWaitResultAndReleaseTask(TaskStruct *pTask, cmdqRegValueStruct *
 	cmdq_core_track_task_record(pTask, thread);
 	cmdq_core_release_thread(pTask);
 	cmdq_core_auto_release_task(pTask);
-#ifdef CMDQ_SECURE_PATH_CONSUME_AGAIN
 	if (true == g_cmdq_consume_again) {
 		cmdq_core_add_consume_task();
 		g_cmdq_consume_again = false;
 	}
-#endif
 	CMDQ_PROF_END(current->pid, __func__);
 
 	return status;
@@ -8132,9 +8128,7 @@ int32_t cmdqCoreInitialize(void)
 	/* MDP initialization setting */
 	cmdq_mdp_get_func()->mdpInitialSet();
 
-#ifdef CMDQ_SECURE_PATH_CONSUME_AGAIN
 	g_cmdq_consume_again = false;
-#endif
 
 	atomic_set(&g_cmdq_mem_monitor.monitor_mem_enable, 0);
 
