ae21e3713a3653f47724c9a44dc127ea685dd95f
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 19:12:52 +0800

    [ALPS03150888] IPV6: install MTK_IPV6_VZW_REQ6378 config
    [Detail] MTK_IPV6_VZW_REQ6378 config is installed to control
    the feature, RS triggering to extend ipv6 route lifetime.
    It is disabled by default.
    
    [Solution] install MTK_IPV6_VZW_REQ6378 to control VZWs feature.
    
    MTK-Commit-Id: b717df5bc13097328b2711ab7b869f17d201a5f1
    
    Change-Id: I968b0c84195f3725fe316397e3116a05f1855d70
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS03150888
    Feature: 2G/3G/4G FDD R10R11

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -1984,6 +1984,7 @@ struct rt6_info *rt6_get_dflt_router(const struct in6_addr *addr, struct net_dev
 	return rt;
 }
 
+#ifdef CONFIG_MTK_IPV6_VZW_REQ6378
 struct rt6_info *rt6_get_dflt_router_expires(struct net_device *dev)
 {
 	struct rt6_info *rt;
@@ -2006,6 +2007,7 @@ struct rt6_info *rt6_get_dflt_router_expires(struct net_device *dev)
 	read_unlock_bh(&table->tb6_lock);
 	return rt;
 }
+#endif
 
 struct rt6_info *rt6_add_dflt_router(const struct in6_addr *gwaddr,
 				     struct net_device *dev,
