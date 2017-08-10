d8cc1b7a213d808f5392e0d1a208d19b551be1a0
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 09:52:23 +0800

    [ALPS02642104] EEPROM/OTP : change license
    [Detail]
    
    [Solution]
    
    MTK-Commit-Id: bf941af20b420b43a0f8f1593a033844e8f58b39
    
    Change-Id: I8c4b88f4a144f367a328b26f75b7cac7ba5d75b8
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02642104
    Feature: CTS(*)

--- drivers/misc/mediatek/cam_cal/src/legacy/mt6755/s5k2p8_eeprom/s5k2p8_eeprom.c
+++ drivers/misc/mediatek/cam_cal/src/legacy/mt6755/s5k2p8_eeprom/s5k2p8_eeprom.c
@@ -1,4 +1,17 @@
 /*
+* Copyright (C) 2016 MediaTek Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+*/
+
+/*
  * Driver for CAM_CAL
  *
  *
@@ -643,7 +656,4 @@ module_init(CAM_CAL_i2C_init);
 module_exit(CAM_CAL_i2C_exit);
 
 MODULE_DESCRIPTION("S5K2P8 CAM_CAL driver");
-MODULE_AUTHOR("LukeHu <luke.hu@Mediatek.com>");
-MODULE_LICENSE("GPL");
-
-
+MODULE_AUTHOR("DreamYeh <Dream.Yeh@Mediatek.com>");
