cd8aed2139719f2e2d09610c050ff3e50bb8e293
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 14:40:52 +0800

    [ALPS02836285] accdet: add 4key support
    [Detail]
    accdet: add 4key support
    
    MTK-Commit-Id: b885b8c57e8bce33c8abd3a1548d0e8b536f9e23
    
    Change-Id: I162b505b91cdb4c2a41bd75338574e046dab798b
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02836285
    Feature: Headset Cable/Button Detection and Reaction

--- drivers/misc/mediatek/accdet/accdet_drv.c
+++ drivers/misc/mediatek/accdet/accdet_drv.c
@@ -56,6 +56,7 @@ static int accdet_remove(struct platform_device *dev)
 	return 0;
 }
 
+#ifdef CONFIG_PM
 static int accdet_suspend(struct device *device)
 {				/* wake up */
 	mt_accdet_suspend();
@@ -67,6 +68,7 @@ static int accdet_resume(struct device *device)
 	mt_accdet_resume();
 	return 0;
 }
+#endif
 
 /**********************************************************************
 //add for IPO-H need update headset state when resume
@@ -78,10 +80,19 @@ static int accdet_pm_restore_noirq(struct device *device)
 	mt_accdet_pm_restore_noirq();
 	return 0;
 }
+
+static const struct dev_pm_ops accdet_pm_ops = {
+	.suspend = accdet_suspend,
+	.resume = accdet_resume,
+	.restore_noirq = accdet_pm_restore_noirq,
+};
+#endif
+
 struct of_device_id accdet_of_match[] = {
 	{ .compatible = "mediatek,mt6735-accdet", },
 	{ .compatible = "mediatek,mt6755-accdet", },
 	{ .compatible = "mediatek,mt6757-accdet", },
+	{ .compatible = "mediatek,mt6570-accdet", },
 	{ .compatible = "mediatek,mt6580-accdet", },
 	{ .compatible = "mediatek,mt8173-accdet", },
 	{ .compatible = "mediatek,mt8163-accdet", },
@@ -90,13 +101,6 @@ struct of_device_id accdet_of_match[] = {
 	{ .compatible = "mediatek,elbrus-accdet", },
 	{},
 };
-
-static const struct dev_pm_ops accdet_pm_ops = {
-	.suspend = accdet_suspend,
-	.resume = accdet_resume,
-	.restore_noirq = accdet_pm_restore_noirq,
-};
-#endif
 
 static struct platform_driver accdet_driver = {
 	.probe = accdet_probe,
