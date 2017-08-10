6257ad03d1ff2829279430dee66cb50d7fb6c56e
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 18:18:17 +0800

    [ALPS03302380] DISP:Revise License announcement
    [Detail]
    Revise the License announcemen in debug.c
    
    [Solution]
    
    MTK-Commit-Id: 17e13a389d5f96b944283a82c78465ecc241dc16
    
    Change-Id: Ie3d4a73628f53e0a990b8a124666653a8db598a9
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS03302380
    Feature: others

--- drivers/misc/mediatek/video/mt6735/debug.h
+++ drivers/misc/mediatek/video/mt6735/debug.h
@@ -1,23 +1,14 @@
 /*
- * File: drivers/video/omap_new/debug.c
+ * Copyright (C) 2016 MediaTek Inc.
  *
- * Debug support for the omapfb driver
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  *
- * Copyright (C) 2004 Nokia Corporation
- * Author: Imre Deak <imre.deak@nokia.com>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
 
 #ifndef __MTKFB_DEBUG_H
