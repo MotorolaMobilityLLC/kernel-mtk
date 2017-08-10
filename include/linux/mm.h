d0222089e37df6f5d2ef5b2bf6c43588b070e5f1
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 15:02:56 +0800

    [ALPS02563038] mm: add get_user_pages_durable helper function
    Add get_user_pages_durable helper function so user could simple
    change calling get_user_pages API to this one.
    
    MTK-Commit-Id: bdab72abf0ce7219c9f5716278b337111c9042b8
    
    Change-Id: Ied3e3afe688939565d2649b099089a19eb19df81
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02563038
    Feature: CMA

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2134,6 +2134,24 @@ static inline long get_user_pages(struct task_struct *tsk, struct mm_struct *mm,
 				NULL);
 }
 
+static inline long get_user_pages_durable(struct task_struct *tsk,
+			struct mm_struct *mm, unsigned long start,
+			unsigned long nr_pages, int write, int force,
+			struct page **pages, struct vm_area_struct **vmas)
+{
+	int flags = FOLL_TOUCH | FOLL_DURABLE;
+
+	if (pages)
+		flags |= FOLL_GET;
+	if (write)
+		flags |= FOLL_WRITE;
+	if (force)
+		flags |= FOLL_FORCE;
+
+	return __get_user_pages(tsk, mm, start, nr_pages, flags, pages, vmas,
+				NULL);
+}
+
 #ifdef CONFIG_PROC_FS
 void vm_stat_account(struct mm_struct *, unsigned long, struct file *, long);
 #else
