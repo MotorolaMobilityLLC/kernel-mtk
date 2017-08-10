6c47c6312eb953d2506ff0ca544b7ded82bd5165
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 12:38:18 +0800

    [ALPS02696360] Audio : fix for license announce
    [Detail]
    fix for audio license announce file.
    [Solution]
    
    MTK-Commit-Id: f065f78eac2783bf79b6c0cd01df17dfc0bbdbff
    
    Change-Id: I952a3e540291b9b24ac9bedaeae782faa446eb81
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02696360
    Feature: Phone Sound

--- drivers/misc/mediatek/smartpa/mt6755/AudDrv_NXP_ioctl.h
+++ drivers/misc/mediatek/smartpa/mt6755/AudDrv_NXP_ioctl.h
@@ -1,29 +1,20 @@
-/*******************************************************************************
- *
- * Filename:
- * ---------
- *  auddrv_btcvsd_ioctl.h
- *
- * Project:
- * --------
- *   Android NXP IOCTL MSG
- *
- * Description:
- * ------------
- *   ioctl control message
- *
- * Author:
- * -------
- *
- *
- *
- *------------------------------------------------------------------------------
- *
- * .
- *
- *
- *
- *******************************************************************************/
+/*
+* Copyright (C) 2015 MediaTek Inc.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _AUDDRV_NXP_IOCTL_MSG_H
 #define _AUDDRV_NXP_IOCTL_MSG_H
 
