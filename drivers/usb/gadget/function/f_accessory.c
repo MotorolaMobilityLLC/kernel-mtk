a4701c95a88d63ee410581282a11935249fbb03d
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 21:25:11 +0800

    [ALPS03421428] usb:modify read data flow
    [Detail]
    modify read data flow to fix
    user wants to read data length and really request data length different issue
    
    MTK-Commit-Id: 170fddf1584fe1fa645e65189d9b8a683a53652f
    
    Change-Id: I4c8d32203971341b108ea06c49c70a550750ec2d
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS03421428
    Feature: CTS(*)

--- drivers/usb/gadget/function/f_accessory.c
+++ drivers/usb/gadget/function/f_accessory.c
@@ -571,6 +571,7 @@ static ssize_t acc_read(struct file *fp, char __user *buf,
 	struct acc_dev *dev = fp->private_data;
 	struct usb_request *req;
 	ssize_t r = count;
+	ssize_t data_length;
 	unsigned xfer;
 	int ret = 0;
 
@@ -583,6 +584,10 @@ static ssize_t acc_read(struct file *fp, char __user *buf,
 
 	if (count > BULK_BUFFER_SIZE)
 		count = BULK_BUFFER_SIZE;
+
+	data_length = count;
+	data_length += dev->ep_out->maxpacket - 1;
+	data_length -= data_length % dev->ep_out->maxpacket;
 
 	/* we will block until we're online */
 	pr_debug("acc_read: waiting for online\n");
@@ -601,7 +606,7 @@ static ssize_t acc_read(struct file *fp, char __user *buf,
 requeue_req:
 	/* queue a request */
 	req = dev->rx_req[0];
-	req->length = count;
+	req->length = data_length;
 	dev->rx_done = 0;
 	ret = usb_ep_queue(dev->ep_out, req, GFP_KERNEL);
 	if (ret < 0) {
@@ -633,6 +638,7 @@ copy_data:
 
 		pr_debug("rx %p %u\n", req, req->actual);
 		xfer = (req->actual < count) ? req->actual : count;
+
 		r = xfer;
 		if (copy_to_user(buf, req->buf, xfer))
 			r = -EFAULT;
