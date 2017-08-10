263199da4459e2a3fc53093d152de09253e15998
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 14:34:39 +0800

    [ALPS02822871] TUI:porting tui to android N for D3
    [Detail] change file license
    
    MTK-Commit-Id: 5186d2bff2a954b360331731ed0dacb3fff76c25
    
    Change-Id: I3c0c9b208593408c7c71ccc650ca38f4349c87cc
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02822871
    Feature: Trusted User Interface (TUI)

--- drivers/misc/mediatek/gud/302c/gud/TlcTui/tui-hal_mt6735.h
+++ drivers/misc/mediatek/gud/302c/gud/TlcTui/tui-hal_mt6735.h
@@ -1,13 +1,16 @@
 /*
-* Copyright (C) 2016 MediaTek Inc.
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 2 as
-* published by the Free Software Foundation.
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
-*/
+ * Copyright (c) 2014 TRUSTONIC LIMITED
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
 
 #ifndef __TUI_HAL_MT6735__H__
 #define __TUI_HAL_MT6735__H__
