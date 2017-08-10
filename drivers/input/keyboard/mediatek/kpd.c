92cac71650a69b1fa6df9d74fa49876bfab4bb4c
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 13:16:50 +0800

    [ALPS02768540] I2C: open i2c/kpd config for MT6570
    [Detail] open i2c,kpd kernel config.
    
    [Solution] modify dtsi,config file.
    
    MTK-Commit-Id: 7cd4565c3b97f9af4430be3e23b91c9ea1581469
    
    Change-Id: Id6b17bc4b9dad9ce054c17ec04c29d1120295292
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02768540
    Feature: Keypad

--- drivers/input/keyboard/mediatek/kpd.c
+++ drivers/input/keyboard/mediatek/kpd.c
@@ -71,6 +71,7 @@ static int kpd_pdrv_resume(struct platform_device *pdev);
 
 static const struct of_device_id kpd_of_match[] = {
 	{.compatible = "mediatek,mt6580-keypad"},
+	{.compatible = "mediatek,mt6570-keypad"},
 	{.compatible = "mediatek,mt6735-keypad"},
 	{.compatible = "mediatek,mt6755-keypad"},
 	{.compatible = "mediatek,mt6757-keypad"},
