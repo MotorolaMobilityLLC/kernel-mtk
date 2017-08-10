6f6ba7eb74f7c951f7cb071620f4d5958e02e6da
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 10:11:27 +0800

    [ALPS02616098] RPMB: use multi command to do operation for rpmb
    [Detail] using MTK api to check if device have already provisioned
    the RPMB key when 1500ms after boot complete. However,it is sometimes
    check failed
    
    [Solution] mmc:block:Add new ioctl to send multi commands certain
    eMMC devices allow vendor specific device information to be read
    via a sequence of vendor commands.These vendor commands must be
    issued in sequence of an atomic fashion. One way to support this
    would be to add an ioctl function for sending a sequence of
    commands to the device atomically as proposed here.These multi
    commands are simple array of the existing mmc_ioc_cmd structure.
    The structure passed via the ioctl uses a __u64 type to specify the
    number of commands(so that the structure is aligned on a 64-bit
    boundary) and a zero length array as a header for list of commands
    to be issued.The maximum number of commands that can be sent is
    determined by MMC_IOC_MAX_CMDS(which default to 255 and should be
    more than sufficient)
    
    MTK-Commit-Id: 11e8d7ca15868a27d0059ad7f18c7db13f149d23
    
    Change-Id: I9cb19f89ada205a5941856c0bf221637f36c3ab8
    Feature: RPMB access
    CR-Id: ALPS02616098
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>

--- include/uapi/linux/mmc/ioctl.h
+++ include/uapi/linux/mmc/ioctl.h
@@ -45,8 +45,25 @@ struct mmc_ioc_cmd {
 };
 #define mmc_ioc_cmd_set_data(ic, ptr) ic.data_ptr = (__u64)(unsigned long) ptr
 
+/**
+  *strcut mmc_ioc_multi_cmd - multi command information
+  *@num_of_cmds:Number of commands to send.Must be equal to or
+  *less than MMC_IOC_MAX_CMDS
+  *@cmds:Array of commands with length equal to 'num_of_cmds'
+*/
+struct mmc_ioc_multi_cmd {
+	__u64 num_of_cmds;
+	struct mmc_ioc_cmd cmds[0];
+};
+
 #define MMC_IOC_CMD _IOWR(MMC_BLOCK_MAJOR, 0, struct mmc_ioc_cmd)
-#define MMC_IOC_FFU_CMD _IOWR(MMC_BLOCK_MAJOR, 1, struct mmc_ioc_cmd)
+/*
+ *MMC_IOC_MULTI_CMD:Used to send an array of MMC commands described by
+ *the structure mmc_ioc_multi_cmd.The MMC driver will issue all
+ *commands in array in sequence to card
+*/
+#define MMC_IOC_MULTI_CMD _IOWR(MMC_BLOCK_MAJOR, 1, struct mmc_ioc_multi_cmd)
+#define MMC_IOC_FFU_CMD _IOWR(MMC_BLOCK_MAJOR, 2, struct mmc_ioc_cmd)
 
 /*
  * Since this ioctl is only meant to enhance (and not replace) normal access
@@ -55,5 +72,6 @@ struct mmc_ioc_cmd {
  * block device operations.
  */
 #define MMC_IOC_MAX_BYTES  (512L * 256)
+#define MMC_IOC_MAX_CMDS  255
 #define MMC_FFU_IOC_MAX_BYTES  (512L * 1024)
 #endif /* LINUX_MMC_IOCTL_H */
