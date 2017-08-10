f5d4c3d85ecec46e52a6e3d88dfa827b3ea45301
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 17:45:56 +0800

    [ALPS02967104] SPI: Modify SPI debug log API
    [Detail]
    Replace dev_alert by dev_info for SPI_INFO
    
    MTK-Commit-Id: c94881a3ffb730c3944f678b88fa66c02247ccdb
    
    Change-Id: I4416144b16e444f1237a8d921b3da5954b0a3757
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02967104
    Feature: Others

--- drivers/spi/mediatek/mt6735/spi.c
+++ drivers/spi/mediatek/mt6735/spi.c
@@ -139,7 +139,7 @@ void mt_spi_disable_master_clk(struct spi_device *ms)
 	#define SPI_DBG(fmt, args...)  pr_debug("mt-spi.c:%5d: <%s>" fmt,  __LINE__, __func__, ##args)
 
 #ifdef SPI_VERBOSE
-#define SPI_INFO(dev, fmt, args...)  dev_alert(dev, "spi.c:%5d: <%s>" fmt, __LINE__, __func__, ##args)
+#define SPI_INFO(dev, fmt, args...)  dev_info(dev, "spi.c:%5d: <%s>" fmt, __LINE__, __func__, ##args)
 static void spi_dump_reg(struct mt_spi_t *ms)
 {
 
