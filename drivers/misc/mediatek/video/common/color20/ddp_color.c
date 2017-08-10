cbb17c5e78ff7bc2bec791daf770ec58ecd3b800
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 14:45:26 +0800

    [ALPS02858990] PQ:Set Dependency of Color reg
    [Detail]
    For color contour issue, value of Y_LEV_ADJ should depend on
    wide_gamut_en.
    
    [Solution]
    Set the dependency of the following HW reg:
    wide_gamut_en = 0 -> Y_LEV_ADJ = 0x40
    wide_gamut_en = 1 -> Y_LEV_ADJ = 0x0
    
    MTK-Commit-Id: dc858ae2d94881bf991aed658b037c25249c89ea
    
    Change-Id: I01c5454c81bb5f92bd49544fc8bf4b398d63e167
    CR-Id:ALPS02858990
    Feature:Display Quality Enhancement
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>

--- drivers/misc/mediatek/video/common/color20/ddp_color.c
+++ drivers/misc/mediatek/video/common/color20/ddp_color.c
@@ -1103,6 +1103,7 @@ void DpEngine_COLORonConfig(DISP_MODULE_ENUM module, void *__cmdq)
 #if defined(CONFIG_ARCH_MT6797)
 	int i, j, reg_index;
 #endif
+	int wide_gamut_en = 0;
 
 	if (DISP_MODULE_COLOR1 == module) {
 		offset = C1_OFFSET;
@@ -1127,7 +1128,7 @@ void DpEngine_COLORonConfig(DISP_MODULE_ENUM module, void *__cmdq)
 #if defined(CONFIG_ARCH_MT6797)
 		_color_reg_mask(cmdq, DISP_COLOR_CFG_MAIN + offset, (1 << 21)
 						| (g_Color_Index.LSP_EN << 20)
-						| (g_Color_Index.S_GAIN_BY_Y_EN << 15) | (0 << 8)
+						| (g_Color_Index.S_GAIN_BY_Y_EN << 15) | (wide_gamut_en << 8)
 						| (0 << 7), 0x003081FF);
 #else
 		_color_reg_mask(cmdq, DISP_COLOR_CFG_MAIN + offset, (0 << 8) | (0 << 7)
@@ -1156,11 +1157,10 @@ void DpEngine_COLORonConfig(DISP_MODULE_ENUM module, void *__cmdq)
 	}
 
 	/* for partial Y contour issue */
-#if defined(CONFIG_ARCH_MT6797)
-	_color_reg_mask(cmdq, DISP_COLOR_LUMA_ADJ + offset, 0x40, 0x0000007F);
-#else
-	_color_reg_mask(cmdq, DISP_COLOR_LUMA_ADJ + offset, 0x0, 0x0000007F);
-#endif
+	if (wide_gamut_en == 0)
+		_color_reg_mask(cmdq, DISP_COLOR_LUMA_ADJ + offset, 0x40, 0x0000007F);
+	else if (wide_gamut_en == 1)
+		_color_reg_mask(cmdq, DISP_COLOR_LUMA_ADJ + offset, 0x0, 0x0000007F);
 
 	/* config parameter from customer color_index.h */
 	_color_reg_mask(cmdq, DISP_COLOR_G_PIC_ADJ_MAIN_1 + offset,
@@ -1417,6 +1417,7 @@ static void color_write_hw_reg(DISP_MODULE_ENUM module,
 #if defined(CONFIG_ARCH_MT6797)
 	int i, j, reg_index;
 #endif
+	int wide_gamut_en = 0;
 
 	if (DISP_MODULE_COLOR1 == module)
 		offset = C1_OFFSET;
@@ -1425,7 +1426,7 @@ static void color_write_hw_reg(DISP_MODULE_ENUM module,
 #if defined(CONFIG_ARCH_MT6797)
 		_color_reg_mask(cmdq, DISP_COLOR_CFG_MAIN + offset, (1 << 21)
 						| (g_Color_Index.LSP_EN << 20)
-						| (g_Color_Index.S_GAIN_BY_Y_EN << 15) | (0 << 8)
+						| (g_Color_Index.S_GAIN_BY_Y_EN << 15) | (wide_gamut_en << 8)
 						| (0 << 7), 0x003081FF);
 #else
 		_color_reg_mask(cmdq, DISP_COLOR_CFG_MAIN + offset, (0 << 8) | (0 << 7)
@@ -1455,11 +1456,10 @@ static void color_write_hw_reg(DISP_MODULE_ENUM module,
 	}
 
 	/* for partial Y contour issue */
-#if defined(CONFIG_ARCH_MT6797)
-	_color_reg_mask(cmdq, DISP_COLOR_LUMA_ADJ + offset, 0x40, 0x0000007F);
-#else
-	_color_reg_mask(cmdq, DISP_COLOR_LUMA_ADJ + offset, 0x0, 0x0000007F);
-#endif
+	if (wide_gamut_en == 0)
+		_color_reg_mask(cmdq, DISP_COLOR_LUMA_ADJ + offset, 0x40, 0x0000007F);
+	else if (wide_gamut_en == 1)
+		_color_reg_mask(cmdq, DISP_COLOR_LUMA_ADJ + offset, 0x0, 0x0000007F);
 
 	_color_reg_mask(cmdq, DISP_COLOR_G_PIC_ADJ_MAIN_1 + offset,
 		(color_reg->BRIGHTNESS << 16) | color_reg->CONTRAST, 0x07FF01FF);
