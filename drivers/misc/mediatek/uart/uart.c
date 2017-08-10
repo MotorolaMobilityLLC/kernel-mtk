259947594333ba9af1222ba2b243e5b17b9df9ec
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 14:16:19 +0800

    [ALPS02802763] uart: add pinctrl and remove log for mt6753
    [Detail]
    add pinctrl for uart part and remove uart useless log
    
    MTK-Commit-Id: 5f07c9033c2be00bba743b06715cd0a4ad781c19
    
    Change-Id: I89e400fd3286616748b827be5092ad80afeb97d5
    CR-Id: ALPS02802763
    Feature: uart
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>

--- drivers/misc/mediatek/uart/uart.c
+++ drivers/misc/mediatek/uart/uart.c
@@ -2435,7 +2435,6 @@ static int mtk_uart_syscore_suspend(void)
 		/* tx pin:  idle->high   power down->low */
 		mtk_uart_switch_tx_to_gpio(uart);
 		spin_unlock_irqrestore(&mtk_uart_bt_lock, flags);
-		pr_debug("[UART%d] Suspend(%d)!\n", uart->nport, ret);
 	}
 	return ret;
 }
@@ -2454,7 +2453,6 @@ static void mtk_uart_syscore_resume(void)
 		ret = uart_resume_port(&mtk_uart_drv, &uart->port);
 		spin_unlock_irqrestore(&mtk_uart_bt_lock, flags);
 		disable_irq(uart->port.irq);
-		pr_debug("[UART%d] Resume(%d)!\n", uart->nport, ret);
 	}
 }
 
@@ -2469,7 +2467,6 @@ static int mtk_uart_suspend(struct platform_device *pdev, pm_message_t state)
 		mtk_uart_save(uart);
 	if (uart && (uart->nport < UART_NR) && (uart != bt_port)) {
 		ret = uart_suspend_port(&mtk_uart_drv, &uart->port);
-		pr_debug("[UART%d] Suspend(%d)!\n", uart->nport, ret);
 		mtk_uart_switch_rx_to_gpio(uart);
 	}
 	return ret;
@@ -2484,7 +2481,6 @@ static int mtk_uart_resume(struct platform_device *pdev)
 	if (uart && (uart->nport < UART_NR) && (uart != bt_port)) {
 		mtk_uart_switch_to_rx(uart);
 		ret = uart_resume_port(&mtk_uart_drv, &uart->port);
-		pr_debug("[UART%d] Resume(%d)!\n", uart->nport, ret);
 	}
 	return ret;
 }
