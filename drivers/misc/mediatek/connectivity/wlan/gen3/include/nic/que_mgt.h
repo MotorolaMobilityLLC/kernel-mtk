3b87c1950d1d2b22821d6c9036901058d1916549
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 12:50:49 +0800

    [ALPS02695657] wlan: enhancement of arp recovery
    [Detail]
    1. when arp gateway which was obtained from dhcp, the arp monitor
    will start also
    
    [Solution]
    1. when arp gateway which was obtained from dhcp, the arp monitor
    will start also
    
    Conflicts:
    	drivers/misc/mediatek/connectivity/wlan/gen3/nic/que_mgt.c
    
    MTK-Commit-Id: 126569d6a7bfd9e9a6ddf85ad0819c0396438685
    
    Change-Id: I22e01e7da775b01f616461c7b7562c827962991c
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02695657
    Feature: [Android Default] Wi-Fi Driver for SoC

--- drivers/misc/mediatek/connectivity/wlan/gen3/include/nic/que_mgt.h
+++ drivers/misc/mediatek/connectivity/wlan/gen3/include/nic/que_mgt.h
@@ -932,6 +932,7 @@ VOID mqmHandleBaActionFrame(P_ADAPTER_T prAdapter, P_SW_RFB_T prSwRfb);
 VOID qmDetectArpNoResponse(P_ADAPTER_T prAdapter, P_MSDU_INFO_T prMsduInfo);
 VOID qmResetArpDetect(VOID);
 VOID qmHandleRxArpPackets(P_ADAPTER_T prAdapter, P_SW_RFB_T prSwRfb);
+VOID qmHandleRxDhcpPackets(P_ADAPTER_T prAdapter, P_SW_RFB_T prSwRfb);
 #endif
 
 /*******************************************************************************
