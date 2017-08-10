b120a2e68ad0bda16b0d31bb465e4b9fd2a3fe76
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 09:26:13 +0800

    [ALPS02696771] WMT:kernel file header disclaimer
    [Detail] the files written by MTK but no GPLv2 license
    
    [Solution] add mtk copyright and GPLv2 license
    
    MTK-Commit-Id: 7f53602e97770e580e1a8aa40085ce21a04b05b1
    
    Change-Id: I9ebfaa5fc2cac0ee5a13911d67041090df4cc554
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02696771
    Feature: Others

--- drivers/misc/mediatek/connectivity/common/combo/core/wmt_dbg.c
+++ drivers/misc/mediatek/connectivity/common/combo/core/wmt_dbg.c
@@ -1,11 +1,15 @@
-/*! \file
-    \brief brief description
-
-    Detailed descriptions here.
-
+/*
+* Copyright (C) 2016 MediaTek Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
 */
-
-
 
 /*******************************************************************************
 *                         C O M P I L E R   F L A G S
