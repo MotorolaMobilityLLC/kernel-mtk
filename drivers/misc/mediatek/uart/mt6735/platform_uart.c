5b1cffd2154c6a66defd134c3cbabcb47954fed4
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 18:16:27 +0800

    [ALPS03302383] UART: Adjust uart license
    [Detail]
    UART is open source driver code
    
    [Solution]
    Change license to GPL 2.0
    
    MTK-Commit-Id: 7acef2bad6fab39c1d0aca151af7df6c5cd82f79
    
    Change-Id: I36784e372800d8c951eb1bbd8af49ece687e872d
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS03302383
    Feature:UART

--- drivers/misc/mediatek/uart/mt6735/platform_uart.c
+++ drivers/misc/mediatek/uart/mt6735/platform_uart.c
@@ -1,20 +1,16 @@
 /*
- * (C) Copyright 2008
- * MediaTek <www.mediatek.com>
- * MingHsien Hsieh <minghsien.hsieh@mediatek.com>
+ * Copyright (C) 2017 MediaTek Inc.
  *
- * MTK UART Driver
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
+
 /*---------------------------------------------------------------------------*/
 #include <linux/tty.h>
 #include <linux/tty_flip.h>
