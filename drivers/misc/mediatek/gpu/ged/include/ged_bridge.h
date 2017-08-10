e10b3127fc1163e90142448c25c563f034d340ad
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 14:26:18 +0800

    [ALPS02842932] GPU: common: gralloc_extra - kernel
    [Detail] New module for gralloc_extra
    
    MTK-Commit-Id: dbe5a7510ac1e3a3a716f0ad402fddee7561e1e1
    
    Change-Id: I6c5a127c35dc8692aa74db5931b17999750b45fc
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02842932
    Feature: OpenGL|ES

--- drivers/misc/mediatek/gpu/ged/include/ged_bridge.h
+++ drivers/misc/mediatek/gpu/ged/include/ged_bridge.h
@@ -59,21 +59,33 @@ typedef enum
 #ifdef ENABLE_FRR_FOR_MT6XXX_PLATFORM
     GED_BRIDGE_COMMAND_VSYNC_WAIT,
 #endif
+
+	GED_BRIDGE_COMMAND_GE_ALLOC = 100,
+	GED_BRIDGE_COMMAND_GE_RETAIN,
+	GED_BRIDGE_COMMAND_GE_RELEASE,
+	GED_BRIDGE_COMMAND_GE_GET,
+	GED_BRIDGE_COMMAND_GE_SET,
 } GED_BRIDGE_COMMAND_ID;
 
-#define GED_BRIDGE_IO_LOG_BUF_GET			GED_IOWR(GED_BRIDGE_COMMAND_LOG_BUF_GET)
-#define GED_BRIDGE_IO_LOG_BUF_WRITE			GED_IOWR(GED_BRIDGE_COMMAND_LOG_BUF_WRITE)
-#define GED_BRIDGE_IO_LOG_BUF_RESET			GED_IOWR(GED_BRIDGE_COMMAND_LOG_BUF_RESET)
-#define GED_BRIDGE_IO_BOOST_GPU_FREQ		GED_IOWR(GED_BRIDGE_COMMAND_BOOST_GPU_FREQ)
+#define GED_BRIDGE_IO_LOG_BUF_GET           GED_IOWR(GED_BRIDGE_COMMAND_LOG_BUF_GET)
+#define GED_BRIDGE_IO_LOG_BUF_WRITE         GED_IOWR(GED_BRIDGE_COMMAND_LOG_BUF_WRITE)
+#define GED_BRIDGE_IO_LOG_BUF_RESET         GED_IOWR(GED_BRIDGE_COMMAND_LOG_BUF_RESET)
+#define GED_BRIDGE_IO_BOOST_GPU_FREQ        GED_IOWR(GED_BRIDGE_COMMAND_BOOST_GPU_FREQ)
 #define GED_BRIDGE_IO_MONITOR_3D_FENCE      GED_IOWR(GED_BRIDGE_COMMAND_MONITOR_3D_FENCE)
-#define GED_BRIDGE_IO_QUERY_INFO      GED_IOWR(GED_BRIDGE_COMMAND_QUERY_INFO)
-#define GED_BRIDGE_IO_NOTIFY_VSYNC       GED_IOWR(GED_BRIDGE_COMMAND_NOTIFY_VSYNC)
-#define GED_BRIDGE_IO_DVFS_PROBE       GED_IOWR(GED_BRIDGE_COMMAND_DVFS_PROBE)
-#define GED_BRIDGE_IO_DVFS_UM_RETURN GED_IOWR(GED_BRIDGE_COMMAND_DVFS_UM_RETURN)
-#define GED_BRIDGE_IO_EVENT_NOTIFY GED_IOWR(GED_BRIDGE_COMMAND_EVENT_NOTIFY)
+#define GED_BRIDGE_IO_QUERY_INFO            GED_IOWR(GED_BRIDGE_COMMAND_QUERY_INFO)
+#define GED_BRIDGE_IO_NOTIFY_VSYNC          GED_IOWR(GED_BRIDGE_COMMAND_NOTIFY_VSYNC)
+#define GED_BRIDGE_IO_DVFS_PROBE            GED_IOWR(GED_BRIDGE_COMMAND_DVFS_PROBE)
+#define GED_BRIDGE_IO_DVFS_UM_RETURN        GED_IOWR(GED_BRIDGE_COMMAND_DVFS_UM_RETURN)
+#define GED_BRIDGE_IO_EVENT_NOTIFY          GED_IOWR(GED_BRIDGE_COMMAND_EVENT_NOTIFY)
 #ifdef ENABLE_FRR_FOR_MT6XXX_PLATFORM
-#define GED_BRIDGE_IO_VSYNC_WAIT 			GED_IOWR(GED_BRIDGE_COMMAND_FSYNC_WAIT)
+#define GED_BRIDGE_IO_VSYNC_WAIT            GED_IOWR(GED_BRIDGE_COMMAND_FSYNC_WAIT)
 #endif
+
+#define GED_BRIDGE_IO_GE_ALLOC              GED_IOWR(GED_BRIDGE_COMMAND_GE_ALLOC)
+#define GED_BRIDGE_IO_GE_RETAIN             GED_IOWR(GED_BRIDGE_COMMAND_GE_RETAIN)
+#define GED_BRIDGE_IO_GE_RELEASE            GED_IOWR(GED_BRIDGE_COMMAND_GE_RELEASE)
+#define GED_BRIDGE_IO_GE_GET                GED_IOWR(GED_BRIDGE_COMMAND_GE_GET)
+#define GED_BRIDGE_IO_GE_SET                GED_IOWR(GED_BRIDGE_COMMAND_GE_SET)
 
 /*****************************************************************************
  *  LOG_BUF_GET
@@ -182,6 +194,7 @@ typedef struct GED_BRIDGE_OUT_QUERY_INFO_TAG
 {
     unsigned long   retrieve;
 } GED_BRIDGE_OUT_QUERY_INFO;
+
 /*****************************************************************************
  *  NOTIFY VSYNC
  *****************************************************************************/
@@ -203,13 +216,13 @@ typedef struct GED_BRIDGE_OUT_NOTIFY_VSYNC_TAG
  *  DVFS PROBE
  *****************************************************************************/
 
-/* Bridge in structure for SWVSYNCEVENT */
+/* Bridge in structure for DVFS_PROBE */
 typedef struct GED_BRIDGE_IN_DVFS_PROBE_TAG
 {
     int          pid;
 } GED_BRIDGE_IN_DVFS_PROBE;
 
-/* Bridge out structure for RECORDSWAPBUFFERS */
+/* Bridge out structure for DVFS_PROBE */
 typedef struct GED_BRIDGE_OUT_DVFS_PROBE_TAG
 {
     GED_ERROR eError;
@@ -248,6 +261,72 @@ typedef struct GED_BRIDGE_OUT_EVENT_NOTIFY_TAG
 {
     GED_ERROR eError;
 } GED_BRIDGE_OUT_EVENT_NOTIFY;
+
+/*****************************************************************************
+ *  GE - gralloc_extra functions
+ *****************************************************************************/
+
+/* Bridge in structure for GE_ALLOC */
+typedef struct GED_BRIDGE_IN_GE_ALLOC_TAG {
+	int region_num;
+	uint32_t region_sizes[0];
+} GED_BRIDGE_IN_GE_ALLOC;
+
+/* Bridge out structure for GE_ALLOC */
+typedef struct GED_BRIDGE_OUT_GE_ALLOC_TAG {
+	uint32_t ge_hnd;
+	GED_ERROR eError;
+} GED_BRIDGE_OUT_GE_ALLOC;
+
+/* Bridge in structure for GE_RETAIN */
+typedef struct GED_BRIDGE_IN_GE_RETAIN_TAG {
+	uint32_t ge_hnd;
+} GED_BRIDGE_IN_GE_RETAIN;
+
+/* Bridge out structure for GE_RETAIN */
+typedef struct GED_BRIDGE_OUT_GE_RETAIN_TAG {
+	int32_t ref;
+	GED_ERROR eError;
+} GED_BRIDGE_OUT_GE_RETAIN;
+
+/* Bridge in structure for GE_RELEASE */
+typedef struct GED_BRIDGE_IN_GE_RELEASE_TAG {
+	uint32_t ge_hnd;
+} GED_BRIDGE_IN_GE_RELEASE;
+
+/* Bridge out structure for GE_RELEASE */
+typedef struct GED_BRIDGE_OUT_GE_RELEASE_TAG {
+	int32_t ref;
+	GED_ERROR eError;
+} GED_BRIDGE_OUT_GE_RELEASE;
+
+/* Bridge in structure for GE_GET */
+typedef struct GED_BRIDGE_IN_GE_GET_TAG {
+	uint32_t ge_hnd;
+	int region_id;
+	int uint32_offset;
+	int uint32_size;
+} GED_BRIDGE_IN_GE_GET;
+
+/* Bridge out structure for GE_GET */
+typedef struct GED_BRIDGE_OUT_GE_GET_TAG {
+	GED_ERROR eError;
+	uint32_t data[0];
+} GED_BRIDGE_OUT_GE_GET;
+
+/* Bridge in structure for GE_SET */
+typedef struct GED_BRIDGE_IN_GE_SET_TAG {
+	uint32_t ge_hnd;
+	int region_id;
+	int uint32_offset;
+	int uint32_size;
+	uint32_t data[0];
+} GED_BRIDGE_IN_GE_SET;
+
+/* Bridge out structure for GE_SET */
+typedef struct GED_BRIDGE_OUT_GE_SET_TAG {
+	GED_ERROR eError;
+} GED_BRIDGE_OUT_GE_SET;
 
 /*****************************************************************************
  *  BRIDGE FUNCTIONS
@@ -292,6 +371,26 @@ int ged_bridge_dvfs_um_retrun(
 int ged_bridge_event_notify(
 		GED_BRIDGE_IN_EVENT_NOTIFY *psEVENT_NOTIFYINT, 
 		GED_BRIDGE_OUT_EVENT_NOTIFY *psEVENT_NOTIFYOUT);
+
+int ged_bridge_ge_alloc(
+		struct GED_BRIDGE_IN_GE_ALLOC_TAG  *psALLOC_IN,
+		struct GED_BRIDGE_OUT_GE_ALLOC_TAG *psALLOC_OUT);
+
+int ged_bridge_ge_retain(
+		struct GED_BRIDGE_IN_GE_RETAIN_TAG  *psRETAIN_IN,
+		struct GED_BRIDGE_OUT_GE_RETAIN_TAG *psRETAIN_OUT);
+
+int ged_bridge_ge_release(
+		struct GED_BRIDGE_IN_GE_RELEASE_TAG  *psRELEASE_IN,
+		struct GED_BRIDGE_OUT_GE_RELEASE_TAG *psRELEASE_OUT);
+
+int ged_bridge_ge_get(
+		struct GED_BRIDGE_IN_GE_GET_TAG  *psGET_IN,
+		struct GED_BRIDGE_OUT_GE_GET_TAG *psGET_OUT);
+
+int ged_bridge_ge_set(
+		struct GED_BRIDGE_IN_GE_SET_TAG  *psSET_IN,
+		struct GED_BRIDGE_OUT_GE_SET_TAG *psSET_OUT);
 
 #ifdef ENABLE_FRR_FOR_MT6XXX_PLATFORM
 int ged_bridge_vsync_wait(void);
--- drivers/misc/mediatek/gpu/ged/src/ged_main.c
+++ drivers/misc/mediatek/gpu/ged/src/ged_main.c
@@ -41,6 +41,7 @@
 #include "ged_vsync.h"
 #endif
 
+#include "ged_ge.h"
 
 #define GED_DRIVER_DEVICE_NAME "ged"
 
@@ -69,12 +70,18 @@ static void* gvIOCTLParamBuf = NULL;
  *****************************************************************************/
 static int ged_open(struct inode *inode, struct file *filp)
 {
+	filp->private_data = NULL;
 	GED_LOGE("%s:%d:%d\n", __func__, MAJOR(inode->i_rdev), MINOR(inode->i_rdev));
 	return 0;
 }
 
 static int ged_release(struct inode *inode, struct file *filp)
 {
+	if (filp->private_data) {
+		void (*free_func)(void *) = ((GED_FILE_PRIVATE_BASE *)filp->private_data)->free_func;
+
+		free_func(filp->private_data);
+	}
 	GED_LOGE("%s:%d:%d\n", __func__, MAJOR(inode->i_rdev), MINOR(inode->i_rdev));
 	return 0;
 }
@@ -94,7 +101,7 @@ static ssize_t ged_write(struct file *filp, const char __user *buf, size_t count
 	return 0;
 }
 
-static long ged_dispatch(GED_BRIDGE_PACKAGE *psBridgePackageKM)
+static long ged_dispatch(struct file *pFile, GED_BRIDGE_PACKAGE *psBridgePackageKM)
 {
 	int ret = -EFAULT;
 	void *pvInt, *pvOut;
@@ -116,7 +123,7 @@ static long ged_dispatch(GED_BRIDGE_PACKAGE *psBridgePackageKM)
 		}
 
 		// we will change the below switch into a function pointer mapping table in the future
-		switch(GED_GET_BRIDGE_ID(psBridgePackageKM->ui32FunctionID))
+		switch (GED_GET_BRIDGE_ID(psBridgePackageKM->ui32FunctionID))
 		{
 			case GED_BRIDGE_COMMAND_LOG_BUF_GET:
 				pFunc = (ged_bridge_func_type*)ged_bridge_log_buf_get;
@@ -153,6 +160,21 @@ static long ged_dispatch(GED_BRIDGE_PACKAGE *psBridgePackageKM)
 				pFunc = (ged_bridge_func_type*)ged_bridge_vsync_wait;
 				break;
 #endif
+			case GED_BRIDGE_COMMAND_GE_ALLOC:
+				pFunc = (ged_bridge_func_type *)ged_bridge_ge_alloc;
+				break;
+			case GED_BRIDGE_COMMAND_GE_RETAIN:
+				pFunc = (ged_bridge_func_type *)ged_bridge_ge_retain;
+				break;
+			case GED_BRIDGE_COMMAND_GE_RELEASE:
+				pFunc = (ged_bridge_func_type *)ged_bridge_ge_release;
+				break;
+			case GED_BRIDGE_COMMAND_GE_GET:
+				pFunc = (ged_bridge_func_type *)ged_bridge_ge_get;
+				break;
+			case GED_BRIDGE_COMMAND_GE_SET:
+				pFunc = (ged_bridge_func_type *)ged_bridge_ge_set;
+				break;
 			default:
 				GED_LOGE("Unknown Bridge ID: %u\n", GED_GET_BRIDGE_ID(psBridgePackageKM->ui32FunctionID));
 				break;
@@ -161,6 +183,40 @@ static long ged_dispatch(GED_BRIDGE_PACKAGE *psBridgePackageKM)
 		if (pFunc)
 		{
 			ret = pFunc(pvInt, pvOut);
+		}
+
+		switch (GED_GET_BRIDGE_ID(psBridgePackageKM->ui32FunctionID)) {
+		case GED_BRIDGE_COMMAND_GE_ALLOC:
+			{
+				GED_BRIDGE_OUT_GE_ALLOC *out = (GED_BRIDGE_OUT_GE_ALLOC *)pvOut;
+
+				if (out->eError == GED_OK) {
+					ged_ge_init_context(&pFile->private_data);
+					ged_ge_context_ref(pFile->private_data, out->ge_hnd);
+				}
+			}
+			break;
+		case GED_BRIDGE_COMMAND_GE_RETAIN:
+			{
+				GED_BRIDGE_IN_GE_RETAIN *in = (GED_BRIDGE_IN_GE_RETAIN *)pvInt;
+				GED_BRIDGE_OUT_GE_RETAIN *out = (GED_BRIDGE_OUT_GE_RETAIN *)pvOut;
+
+				if (out->eError == GED_OK) {
+					ged_ge_init_context(&pFile->private_data);
+					ged_ge_context_ref(pFile->private_data, in->ge_hnd);
+				}
+			}
+			break;
+		case GED_BRIDGE_COMMAND_GE_RELEASE:
+			{
+				GED_BRIDGE_IN_GE_RELEASE *in = (GED_BRIDGE_IN_GE_RELEASE *)pvInt;
+				GED_BRIDGE_OUT_GE_RELEASE *out = (GED_BRIDGE_OUT_GE_RELEASE *)pvOut;
+
+				if (out->eError == GED_OK)
+					ged_ge_context_deref(pFile->private_data, in->ge_hnd);
+
+			}
+			break;
 		}
 
 		if (psBridgePackageKM->i32OutBufferSize > 0)
@@ -196,7 +252,7 @@ static long ged_ioctl(struct file *pFile, unsigned int ioctlCmd, unsigned long a
 		goto unlock_and_return;
 	}
 
-	ret = ged_dispatch(psBridgePackageKM);
+	ret = ged_dispatch(pFile, psBridgePackageKM);
 
 unlock_and_return:
 	up(&ged_dal_sem);
@@ -242,7 +298,7 @@ static long ged_ioctl_compat(struct file *pFile, unsigned int ioctlCmd, unsigned
 	sBridgePackageKM64.i32InBufferSize = psBridgePackageKM32->i32InBufferSize;
 	sBridgePackageKM64.i32OutBufferSize = psBridgePackageKM32->i32OutBufferSize;
 
-	ret = ged_dispatch(&sBridgePackageKM64);
+	ret = ged_dispatch(pFile, &sBridgePackageKM64);
 
 unlock_and_return:
 	up(&ged_dal_sem);
@@ -309,6 +365,8 @@ static void ged_exit(void)
 
 	ged_debugFS_exit();
 
+	ged_ge_exit();
+
 	remove_proc_entry(GED_DRIVER_DEVICE_NAME, NULL);
 
 	if (gvIOCTLParamBuf)
@@ -371,11 +429,16 @@ static int ged_init(void)
 		goto ERROR;
 	}
 
-
 	err = ged_dvfs_system_init();
 	if (unlikely(err != GED_OK))
 	{
 		GED_LOGE("ged: failed to init common dvfs!\n");
+		goto ERROR;
+	}
+
+	err = ged_ge_init();
+	if (unlikely(err != GED_OK)) {
+		GED_LOGE("ged: failed to init gralloc_extra!\n");
 		goto ERROR;
 	}
 
