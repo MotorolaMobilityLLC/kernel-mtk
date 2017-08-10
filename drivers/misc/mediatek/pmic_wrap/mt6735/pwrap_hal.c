cfacce50f38852f6025215cc6830479cdc68e85a
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 10:27:24 +0800

    [ALPS02360345] pwrap: fix the code defect
    [Detail] fix the code defect
    
    [Solution] fix the code defect
    
    MTK-Commit-Id: 7c1fcd28ac4820cd8717813552b53dd64e417495
    
    Change-Id: If98e0a9e35e7cbb305f83239566ac2f7fd5c1334
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02360345
    Feature: Others

--- drivers/misc/mediatek/pmic_wrap/mt6735/pwrap_hal.c
+++ drivers/misc/mediatek/pmic_wrap/mt6735/pwrap_hal.c
@@ -539,7 +539,7 @@ static s32 pwrap_wacs2_hal(u32  write, u32  adr, u32  wdata, u32 *rdata)
 	/* check pmicaddr 0xa bit11 & bit10 ,bit 11 only can write1 bit 10 only can write 0 request by Wy Chuang */
 	if (0 != write && 0xa == adr) {
 
-		if (0 == (wdata & (1<<11)) || 1 == (wdata & (1<<10))) {
+		if (0 == (wdata & (1<<11)) || 1 == ((wdata>>10) & 0x1)) {
 			PWRAPERR(" pwrap_wacs2_hal check 0xa err pid=%d, wdata=0x%x\n", current->pid, wdata);
 			BUG_ON(1);
 		}
@@ -637,7 +637,7 @@ static s32 _pwrap_wacs2_nochk(u32 write, u32 adr, u32 wdata, u32 *rdata)
 
 	if (0 != write && 0xa == adr) {
 
-		if (0 == (wdata & (1<<11)) || 1 == (wdata & (1<<10))) {
+		if (0 == (wdata & (1<<11)) || 1 == ((wdata>>10) & 0x01)) {
 			PWRAPERR("_pwrap_wacs2_nochk check 0xa err pid=%d, wdata=0x%x\n", current->pid, wdata);
 			BUG_ON(1);
 		}
