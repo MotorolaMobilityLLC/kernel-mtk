636cb5bca812c96042a3a8a39ba0517e7b463649
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 13:38:38 +0800

    [ALPS02818120] Charger: Add License Header GPLv2
    [Detail]
    Add License Header GPLv2
    
    MTK-Commit-Id: 2dea71429f4762d21e0037cfe4939cb2259ef7cf
    
    Change-Id: Ic6d2d26742387f051eb13c1475a1d91f8870e5a6
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02818120
    Feature: Others

--- drivers/misc/mediatek/power/mt6757/rt9466.c
+++ drivers/misc/mediatek/power/mt6757/rt9466.c
@@ -1,13 +1,14 @@
 /*
- *  Driver for Richtek RT9466 E2 Charger
- *
- *  Copyright (C) 2015 Richtek Technology Corp.
- *  ShuFanLee <shufan_lee@richtek.com>
+ * Copyright (C) 2016 MediaTek Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
 
 #include <linux/module.h>
