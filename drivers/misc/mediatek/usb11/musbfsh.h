b8855f3a7824e9004c38c215e5371fe473edbe58
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 13:45:23 +0800

    [ALPS02821052] musbfsh.h: add Mediatek official GPLv2 license header.
    [Detail]
    Add Mediatek official GPLv2 license header.
    
    [Solution]
    Add Mediatek official GPLv2 license header.
    
    MTK-Commit-Id: edb6f8d3e9e9b6de6d67d1d4189505b991bb8b56
    
    Change-Id: Ic8afdd43a80cf7012254e3814f47b9945fcdd68e
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02821052
    Feature: Others

--- drivers/misc/mediatek/usb11/musbfsh.h
+++ drivers/misc/mediatek/usb11/musbfsh.h
@@ -1,6 +1,18 @@
 /*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+/*
  * This is used to for host and peripheral modes of the driver for
- * Inventra (Multidrop) Highspeed Dual-Role Controllers:  (M)HDRC.
+ * Inventra (Multidrop) Highspeed Dual-Role Controllers: (M)HDRC.
  *
  * Board initialization should put one of these into dev->platform_data,
  * probably on some platform_device named "musbfsh_hdrc".  It encapsulates
