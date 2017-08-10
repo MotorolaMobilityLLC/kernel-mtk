9f478538504646ee5041e2724e61657e0947e8fd
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 10:11:22 +0800

    [ALPS02736266] i2c: mt8173: modify start order for i2c dma.
    [Detail]
    i2c transfer error occurs if read register data after writing
    register addr of i2c device when i2c dcm is enabled.
    [Solution]
    enable dma after starting i2c transfer when reading.
    
    MTK-Commit-Id: 18939c136841a2ce7d81c79d12b8777d30a69be1
    
    Change-Id: I13aad1644ebea457df95c95e73f6179be86f57c9
    CR-Id: ALPS02736266
    Feature: i2c
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>

--- drivers/i2c/busses/i2c-mt65xx.c
+++ drivers/i2c/busses/i2c-mt65xx.c
@@ -262,9 +262,6 @@ static void mtk_i2c_init_hw(struct mtk_i2c *i2c)
 	else
 		writew(I2C_IO_CONFIG_OPEN_DRAIN, i2c->base + OFFSET_IO_CONFIG);
 
-	if (i2c->dev_comp->dcm)
-		writew(I2C_DCM_DISABLE, i2c->base + OFFSET_DCM_EN);
-
 	writew(i2c->timing_reg, i2c->base + OFFSET_TIMING);
 	writew(i2c->high_speed_reg, i2c->base + OFFSET_HS);
 
@@ -506,7 +503,8 @@ static int mtk_i2c_do_transfer(struct mtk_i2c *i2c, struct i2c_msg *msgs,
 		writel((msgs + 1)->len, i2c->pdmabase + OFFSET_RX_LEN);
 	}
 
-	writel(I2C_DMA_START_EN, i2c->pdmabase + OFFSET_EN);
+	if (i2c->op != I2C_MASTER_RD)
+		writel(I2C_DMA_START_EN, i2c->pdmabase + OFFSET_EN);
 
 	if (!i2c->auto_restart) {
 		start_reg = I2C_TRANSAC_START;
@@ -516,6 +514,9 @@ static int mtk_i2c_do_transfer(struct mtk_i2c *i2c, struct i2c_msg *msgs,
 			start_reg |= I2C_RS_MUL_CNFG;
 	}
 	writew(start_reg, i2c->base + OFFSET_START);
+
+	if (i2c->op == I2C_MASTER_RD)
+		writel(I2C_DMA_START_EN, i2c->pdmabase + OFFSET_EN);
 
 	ret = wait_for_completion_timeout(&i2c->msg_complete,
 					  i2c->adap.timeout);
