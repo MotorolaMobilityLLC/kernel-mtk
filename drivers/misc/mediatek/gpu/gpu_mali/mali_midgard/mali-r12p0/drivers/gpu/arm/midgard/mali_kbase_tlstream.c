a8b17539896e0ceedc0a807eb167b1fea33d79d6
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 12:21:29 +0800

    [ALPS02084647] GPU: change to r12 ddk
    [Detail] change gpu ddk version
    
    [Solution] change config.
    
    MTK-Commit-Id: 3e5a49a0999fe618fab1eb414aa6d85a1071dde6
    
    Change-Id: I875743fa254a32bab2810001e266130fe14680dc
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02084647
    Feature: OpenGL|ES

--- drivers/misc/mediatek/gpu/gpu_mali/mali_midgard/mali-r12p0/drivers/gpu/arm/midgard/mali_kbase_tlstream.c
+++ drivers/misc/mediatek/gpu/gpu_mali/mali_midgard/mali-r12p0/drivers/gpu/arm/midgard/mali_kbase_tlstream.c
@@ -1097,7 +1097,7 @@ static ssize_t kbasep_tlstream_read(
 
 	while (copy_len < size) {
 		enum tl_stream_type stype;
-		unsigned int        rb_idx_raw;
+		unsigned int        rb_idx_raw = 0;
 		unsigned int        rb_idx;
 		size_t              rb_size;
 
