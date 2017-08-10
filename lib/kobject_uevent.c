4389b6a5fbbad0aacf08d45571bf9a3cc552b43e
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 18:37:38 +0800

    [ALPS03115590] MSDC: add log for sending uevent about mmc partition
    [Detail] Add log for sending uevent about mmc partition
    
    [Solution]
    
    MTK-Commit-Id: e98d4a5223d21597325157236099c725b3297fda
    
    Change-Id: I93c523caac575f36677e015cc2aebc1d7f2ae768
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS03115590
    Feature: Driver

--- lib/kobject_uevent.c
+++ lib/kobject_uevent.c
@@ -293,6 +293,11 @@ int kobject_uevent_env(struct kobject *kobj, enum kobject_action action,
 		struct sk_buff *skb;
 		size_t len;
 
+		/* BEGIN MT6755 O debug */
+		if (!strncmp(devpath, "/devices/mtk-msdc.0", 19))
+			pr_err("[kobject_uevent] add uevent %s\n", devpath);
+		/* END */
+
 		if (!netlink_has_listeners(uevent_sock, 1))
 			continue;
 
@@ -319,6 +324,10 @@ int kobject_uevent_env(struct kobject *kobj, enum kobject_action action,
 							    kobj_bcast_filter,
 							    kobj);
 			/* ENOBUFS should be handled in userspace */
+			/* BEGIN MT6755 O debug */
+			if (!strncmp(devpath, "/devices/mtk-msdc.0", 19))
+				pr_err("[kobject_uevent] add uevent %s, ret %d\n", devpath, retval);
+			/* END */
 			if (retval == -ENOBUFS || retval == -ESRCH)
 				retval = 0;
 		} else
