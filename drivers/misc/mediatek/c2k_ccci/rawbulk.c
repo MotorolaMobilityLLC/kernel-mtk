0a05d758bb70a2e13fbc74f63a695016b44192bb
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 10:18:56 +0800

    [ALPS02353358] USB:fix coverity issue
    [Detail]
    add tmp_val to prevent overflow
    
    MTK-Commit-Id: 44b98b29b6d241408f5f072170e6900038fd9121
    
    Change-Id: I23a3a2ce89395929034ff02dad2cfbb4355d39f3
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02353358
    Feature: Others

--- drivers/misc/mediatek/c2k_ccci/rawbulk.c
+++ drivers/misc/mediatek/c2k_ccci/rawbulk.c
@@ -305,6 +305,7 @@ static ssize_t rawbulk_attr_store(struct device *dev, struct device_attribute *a
 	int ndowns;
 	int upsz;
 	int downsz;
+	long tmp_val;
 
 	struct rawbulk_function *fn;
 
@@ -343,7 +344,8 @@ static ssize_t rawbulk_attr_store(struct device *dev, struct device_attribute *a
 		if (idx == ATTR_ENABLE) {
 			int ret;
 
-			ret = kstrtol(buf, 0, (long *)&enable);
+			ret = kstrtol(buf, 0, (long *)&tmp_val);
+			enable = tmp_val;
 			C2K_NOTE("enable:%d\n", enable);
 
 #ifdef C2K_USB_UT
@@ -454,19 +456,22 @@ static ssize_t rawbulk_attr_store(struct device *dev, struct device_attribute *a
 			if (check_enable_state(fn)) {
 				int val, ret;
 
-				ret = kstrtol(buf, 0, (long *)&val);
+				ret = kstrtol(buf, 0, (long *)&tmp_val);
+				val = tmp_val;
 				modem_dtr_set(val, 1);
 			}
 		}
 	} else if (idx == ATTR_AUTORECONN) {
 		int val, ret;
 
-		ret = kstrtol(buf, 0, (long *)&val);
+		ret = kstrtol(buf, 0, (long *)&tmp_val);
+		val = tmp_val;
 		fn->autoreconn = !!val;
 	} else {
 		int val, ret;
 
-		ret = kstrtol(buf, 0, (long *)&val);
+		ret = kstrtol(buf, 0, (long *)&tmp_val);
+		val = tmp_val;
 		switch (idx) {
 		case ATTR_NUPS:
 			nups = val;
