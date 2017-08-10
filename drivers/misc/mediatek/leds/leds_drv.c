af3bd09eab4e23fc874ab9366e32f7c5f7c73b55
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 10:10:06 +0800

    [ALPS02738168] leds: add GPLv2
    [Detail]
    leds: add GPLv2
    
    MTK-Commit-Id: ef99fa4501ce26bd81d13466706e011ce52efc41
    
    Change-Id: I3dbd9b6e3714eeb7ddc4d9c73e8b97d2ee524bb5
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02738168
    Feature: Others

--- drivers/misc/mediatek/leds/leds_drv.c
+++ drivers/misc/mediatek/leds/leds_drv.c
@@ -1,9 +1,18 @@
 /*
- * drivers/leds/leds-mt65xx.c
+ * Copyright (C) 2015 MediaTek Inc.
  *
- * This file is subject to the terms and conditions of the GNU General Public
- * License.  See the file COPYING in the main directory of this archive for
- * more details.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+/*
+ * drivers/leds/leds-mt65xx.c
  *
  * mt65xx leds driver
  *
