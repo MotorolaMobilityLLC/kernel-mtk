f6ed1bcf768e93b9dd74c8b8705b4f308da0c1b1
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 21:24:45 +0800

    [ALPS03294924] TEEI: Remove cache invalidate and flush
    [Detail]
    Remove cache invalidate and flush in tz_driver.
    
    MTK-Commit-Id: 88fc9607e46f0ddb21d8c6babe3c89d152b26c2c
    
    Change-Id: I8175b1dbfcd549128ab1a5bb4d5084c21f0434b9
    CR-Id: ALPS03294924
    Feature: Microtrust TEEI
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>

--- drivers/misc/mediatek/teei/V1.0/tz_driver/include/teei_id.h
+++ drivers/misc/mediatek/teei/V1.0/tz_driver/include/teei_id.h
@@ -96,32 +96,7 @@ enum teei_cmd_type {
  * ***************************************************************/
 static inline void Flush_Dcache_By_Area(unsigned long start, unsigned long end)
 {
-
-
-	if (boot_soter_flag == START_STATUS) {
-#ifdef CONFIG_ARM64
-		__flush_dcache_area((void *)start, (end - start));
-#else
-	__asm__ __volatile__ ("dsb" : : : "memory"); /* dsb */
-
-	__asm__ __volatile__ (
-	    "1:  mcr p15, 0, %[i], c7, c14, 1\n" /* Clean and Invalidate Data Cache Line (using MVA) Register */
-	    "    add %[i], %[i], %[clsz]\n"
-	    "    cmp %[i], %[end]\n"
-	    "    blo 1b\n"
-	    :
-	    [i]    "=&r" (start)
-	    :        "0"   ((unsigned long)start & ~(Cache_line_size - 1)),
-	    [end]  "r"   (end),
-	    [clsz] "i"   (Cache_line_size)
-	    : "memory");
-
-	asm volatile ("mcr p15, 0, %0, c7, c5, 0" : : "r" (0) : "memory"); /* invalidate btc */
-
-	__asm__ __volatile__ ("dsb" : : : "memory"); /* dsb */
-#endif
-
-	}
+	/* NO NEED TO FLUSH IN THE NEW VERSION */
 }
 /******************************************************************
  * @brief:
@@ -187,47 +162,7 @@ static inline void __Invalidate_Dcache_By_Area(unsigned long start, unsigned lon
 
 static inline void Invalidate_Dcache_By_Area(unsigned long start, unsigned long end)
 {
-#if 0
-	__asm__ __volatile__ ("dsb" : : : "memory"); /* dsb */
-	__asm__ __volatile__ (
-	    "1:  mcr p15, 0, %[i], c7, c6, 1\n" /* Invalidate Data Cache Line (using MVA) Register */
-	    "    add %[i], %[i], %[clsz]\n"
-	    "    cmp %[i], %[end]\n"
-	    "    blo 1b\n"
-	    :
-	    [i]    "=&r" (start)
-	    :        "0"   ((unsigned long)start & (~(Cache_line_size - 1))),
-	    [end]  "r"   (end),
-	    [clsz] "i"   (Cache_line_size)
-	    : "memory");
-
-	asm volatile ("mcr p15, 0, %0, c7, c5, 0" : : "r" (0) : "memory"); /* invalidate btc */
-	__asm__ __volatile__ ("dsb" : : : "memory"); /* dsb */
-#endif
-
-#if 0
-	__asm__ volatile(
-	    "mrs	x3, ctr_el0\n\t"
-	    "lsr	x3, x3, #16\n\t"
-	    "and	x3, x3, #0xf\n\t"
-	    "mov	x2, #4\n\t"
-	    "lsl	x2, x2, x3\n\t"
-	    "sub	x3, x2, #1\n\t"
-	    "bic	%[start], %[start], x3\n\t"
-	    "1:	dc	ivac, %[start]\n\t"			/* invalidate D line / unified line */
-	    "add	%[start],%[start] , x2\n\t"
-	    "cmp	%[start], %[end]\n\t"
-	    "b.lo	1b\n\t"
-	    "dsb	sy\n\t"
-	    : :
-	    [start]  "r" (start),
-	    [end]  "r"  (end)
-	    : "memory");
-#endif
-
-	if (boot_soter_flag == START_STATUS)
-		__Invalidate_Dcache_By_Area(start, end);
-
+	/* NO NEED TO INVALIDATE IN THE NEW VERSION */
 }
 
 /* add end */
