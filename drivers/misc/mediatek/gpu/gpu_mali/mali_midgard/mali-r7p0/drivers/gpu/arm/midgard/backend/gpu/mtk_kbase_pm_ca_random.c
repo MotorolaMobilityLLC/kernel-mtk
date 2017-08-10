182cb2823797940146c5895d1f73e74906c52e55
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 15:07:22 +0800

    [ALPS02842932] GPU: add GPL header
    [Detail] MTK copyright & GPLv2 license statement
    
    MTK-Commit-Id: bdf3fc30e9f626695a2f54ec9aa8092345a382fb
    
    Change-Id: I0d32f13dff4692b6f06c8d5efd72c7d74665b128
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02842932
    Feature: OpenGL|ES

--- drivers/misc/mediatek/gpu/gpu_mali/mali_midgard/mali-r7p0/drivers/gpu/arm/midgard/backend/gpu/mtk_kbase_pm_ca_random.c
+++ drivers/misc/mediatek/gpu/gpu_mali/mali_midgard/mali-r7p0/drivers/gpu/arm/midgard/backend/gpu/mtk_kbase_pm_ca_random.c
@@ -1,6 +1,14 @@
-/**
- * @file mtk_kbase_pm_ca_random.c
- * A power policy implementing fixed core availability
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
 
 #include <mali_kbase.h>
