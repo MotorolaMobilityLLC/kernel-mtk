6ea58b7b9a133b30bcba2aedf21b4921ee530c66
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 09:59:38 +0800

    [ALPS02728467]  usb: fix data abort on usb_release_bos_descriptor
    [Detail]
        In function usb_reset_and_verify_device,
    "release the new BOS descriptor allocated  by hub_port_init()"
    is also need apply on re_enumerate to prevent from free
    old bos.
    
    [<ffffffc0000822bc>] do_mem_abort+0x3c/0xe8
    [<ffffffc000085c54>] el1_da+0x18/0x80
    [<ffffffc000830a34>] usb_release_bos_descriptor+0x20/0x38
    [<ffffffc00081eaf8>] usb_release_dev+0x28/0x5c
    [<ffffffc0003adb38>] device_release+0x34/0x98
    [<ffffffc000349648>] kobject_release+0x94/0x1a0
    
    [Solution]
       add check at re_enumerate to prevent from free old bos
    
    MTK-Commit-Id: caec5fa739bd1260910e215cf1a9f3e4e357c0e0
    
    Change-Id: I85f45597447b361e046dfeedddda9a227273abd3
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02728467
    Feature: [Android Default] USB OTG

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -5469,8 +5469,11 @@ done:
 	return 0;
 
 re_enumerate:
-	usb_release_bos_descriptor(udev);
-	udev->bos = bos;
+	/* release the new BOS descriptor allocated  by hub_port_init() */
+	if (udev->bos != bos) {
+		usb_release_bos_descriptor(udev);
+		udev->bos = bos;
+	}
 re_enumerate_no_bos:
 	/* LPM state doesn't matter when we're about to destroy the device. */
 	hub_port_logical_disconnect(parent_hub, port1);
