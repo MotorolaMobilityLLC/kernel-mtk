f250b1f26aff4653d79f5d310e3babffb1122e14
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 17:15:55 +0800

    [ALPS03015878] WLAN Gen2: Enable HT40 for 5G WiFi irect
    [Detail]
    Enable HT40 for 5G WiFi Direct.
    [Solution]
    Enable HT40 for 5G WiFi Direct.
    
    	drivers/misc/mediatek/connectivity/wlan/gen2/mgmt/cnm.c
    
    MTK-Commit-Id: 560e420a0e72fd66515ceb6427df17bfc30a9b91
    
    Change-Id: I7eddc2093bf46c6d58e0045642bf833906f126aa
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    Conflicts:
    CR-Id: ALPS03015878
    Feature: Wi-Fi Chipset Capability

--- drivers/misc/mediatek/connectivity/wlan/gen2/mgmt/rlm.c
+++ drivers/misc/mediatek/connectivity/wlan/gen2/mgmt/rlm.c
@@ -27,6 +27,8 @@
 ********************************************************************************
 */
 
+#define FIX_P2P_HT20	0
+
 /*******************************************************************************
 *                             D A T A   T Y P E S
 ********************************************************************************
@@ -1189,7 +1191,7 @@ VOID rlmProcessAssocRsp(P_ADAPTER_T prAdapter, P_SW_RFB_T prSwRfb, PUINT_8 pucIE
 	 *       shall be invoked afterwards.
 	 *       Update channel, bandwidth and protection mode by nicUpdateBss()
 	 */
-#if 1
+#if FIX_P2P_HT20
 	if (prStaRec->ucNetTypeIndex == NETWORK_TYPE_P2P_INDEX) {
 
 		DBGLOG(P2P, WARN, "Force P2P BW to 20\n");
