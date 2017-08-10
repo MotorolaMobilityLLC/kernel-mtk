b9dc07f5f5865a0a07237490ab728e3745f94d92
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 17:33:22 +0800

    [ALPS03168390] USB: fix KPOC
    [Detail]
    fix USB force on at KPOC
    
    MTK-Commit-Id: edb6d7a86290f3252dc59e5bc583e288318e6441
    
    Change-Id: I054801b55eb09a9af4c0547fd232832c9dc76b80
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS03168390
    Feature: USB

--- drivers/misc/mediatek/mu3d/drv/mt_usb.c
+++ drivers/misc/mediatek/mu3d/drv/mt_usb.c
@@ -258,14 +258,6 @@ bool usb_cable_connected(void)
 	CHARGER_TYPE chg_type = CHARGER_UNKNOWN;
 	bool connected = false, vbus_exist = false;
 
-#ifdef CONFIG_MTK_KERNEL_POWER_OFF_CHARGING
-	if (get_boot_mode() == KERNEL_POWER_OFF_CHARGING_BOOT
-			|| get_boot_mode() == LOW_POWER_OFF_CHARGING_BOOT) {
-		os_printk(K_INFO, "%s, in KPOC, force USB on\n", __func__);
-		return true;
-	}
-#endif
-
 	if (mu3d_force_on) {
 		/* FORCE USB ON */
 		chg_type = _mu3d_musb->charger_mode = STANDARD_HOST;
