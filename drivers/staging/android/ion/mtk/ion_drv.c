58cf7b501ffca0a39b6be49374a1d76e563149aa
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 21:11:48 +0800

    [ALPS03380915] ION:fix memtrack issue
    [Detail] add ion clients_summary file to debug
    
    MTK-Commit-Id: 62b750a6f566981f59277656b002c78121c9218d
    
    Change-Id: I0ecfb0354e8d708663d07c9fa313aa1def75c98b
    CR-Id:ALPS03380915
    Feature:others
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>

--- drivers/staging/android/ion/mtk/ion_drv.c
+++ drivers/staging/android/ion/mtk/ion_drv.c
@@ -722,6 +722,60 @@ int ion_device_destroy_heaps(struct ion_device *dev)
 	return 0;
 }
 
+/*for clients ion mm heap summary size*/
+static int ion_clients_summary_show(struct seq_file *s, void *unused)
+{
+	struct ion_device *dev = g_ion_device;
+	struct rb_node *n, *m;
+	int buffer_size = 0;
+
+	if (!down_read_trylock(&dev->lock))
+		return 0;
+	seq_printf(s, "%-16.s %-8.s %-8.s\n", "client_name", "pid", "size");
+	seq_puts(s, "------------------------------------------\n");
+	for (n = rb_first(&dev->clients); n; n = rb_next(n)) {
+		struct ion_client *client = rb_entry(n, struct ion_client, node);
+		{
+			 {
+				mutex_lock(&client->lock);
+				for (m = rb_first(&client->handles); m; m = rb_next(m)) {
+					struct ion_handle *handle = rb_entry(m, struct ion_handle, node);
+
+					if ((handle->buffer->heap->id == ION_HEAP_TYPE_MULTIMEDIA ||
+					     handle->buffer->heap->id == ION_HEAP_TYPE_MULTIMEDIA_FOR_CAMERA) &&
+						(handle->buffer->handle_count) != 0)
+						buffer_size +=
+						(int)(handle->buffer->size)/(handle->buffer->handle_count);
+				}
+				if (!buffer_size) {
+					mutex_unlock(&client->lock);
+					continue;
+				}
+				seq_printf(s, "%-16s %-8d %-8d\n", client->name, client->pid, buffer_size);
+				buffer_size = 0;
+			mutex_unlock(&client->lock);
+			}
+		}
+	}
+
+	seq_puts(s, "-------------------------------------------\n");
+	up_read(&dev->lock);
+
+	return 0;
+}
+
+static int ion_debug_client_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, ion_clients_summary_show, inode->i_private);
+}
+
+static const struct file_operations debug_client_fops = {
+	.open = ion_debug_client_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
 static int ion_drv_probe(struct platform_device *pdev)
 {
 	int i;
@@ -758,6 +812,8 @@ static int ion_drv_probe(struct platform_device *pdev)
 
 	/* debugfs_create_file("ion_profile", 0644, g_ion_device->debug_root, NULL, */
 	/* &debug_profile_fops); */
+	debugfs_create_file("clients_summary", 0644, g_ion_device->clients_debug_root, NULL,
+			    &debug_client_fops);
 	debugfs_create_symlink("ion_mm_heap", g_ion_device->debug_root, "./heaps/ion_mm_heap");
 
 	ion_history_init();
