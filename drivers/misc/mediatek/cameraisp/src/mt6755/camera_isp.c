e94d9cc9249865e3d7e5c4f5f4c3fd53a38e9345
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 14:16:00 +0800

    [ALPS02726865] ISP: add copyright header
    [Detail] add MTK Copyright and GPL v2 Copyright disclaim
    
    [Solution] add copyright header
    
    MTK-Commit-Id: cebbaf2d0ed573a9dbee80b52d3a907c69f9c5e4
    
    Change-Id: I431c9872fdd1fed612a510415e5b5e729f992a60
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02726865
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/cameraisp/src/mt6755/camera_isp.c
+++ drivers/misc/mediatek/cameraisp/src/mt6755/camera_isp.c
@@ -1,10 +1,21 @@
+/*
+* Copyright (C) 2016 MediaTek Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+*/
+
 /******************************************************************************
- * mt6593_isp.c	- MT6593 Linux ISP Device Driver
- *
- * Copyright 2008-2009 MediaTek	Co.,Ltd.
+ * camera_isp.c - MT6555 Linux ISP Device Driver
  *
  * DESCRIPTION:
- *	   This	file provid	the	other drivers ISP relative functions
+ *     This file provid the other drivers ISP relative functions
  *
  ******************************************************************************/
 #include <linux/types.h>
