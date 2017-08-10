3ae96baea61e1c1740e58c9d9de7d4f2ad22726a
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 18:16:36 +0800

    [ALPS03278271] GPU: Fix format string vulnerability
    Fix format string vulnerability
    
    MTK-Commit-Id: 1d9bb11e510eec9e964fa08ae43101ee55cbb233
    
    Change-Id: I9a31ebf05042847246415a6a0e987d1604e31ea5
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    Feature: OpenGL|ES
    CR-Id: ALPS03278271

--- drivers/misc/mediatek/gpu/ged/src/ged_bridge.c
+++ drivers/misc/mediatek/gpu/ged/src/ged_bridge.c
@@ -43,21 +43,7 @@ int ged_bridge_log_buf_write(
 		GED_BRIDGE_OUT_LOGBUFWRITE *psLogBufWriteOUT)
 {
 	psLogBufWriteOUT->eError = 
-		ged_log_buf_print2(psLogBufWriteIN->hLogBuf, psLogBufWriteIN->attrs, psLogBufWriteIN->acLogBuf);
-
-#if 0
-	if (ged_log_buf_write(
-				psLogBufWriteIN->hLogBuf, 
-				/*from user*/psLogBufWriteIN->acLogBuf,
-				GED_BRIDGE_IN_LOGBUF_SIZE) > 0)
-	{
-		psLogBufWriteOUT->eError = GED_OK;
-	}
-	else
-	{
-		psLogBufWriteOUT->eError = GED_ERROR_FAIL;
-	}
-#endif
+		ged_log_buf_print2(psLogBufWriteIN->hLogBuf, psLogBufWriteIN->attrs, "%s", psLogBufWriteIN->acLogBuf);
 	return 0;
 }
 //-----------------------------------------------------------------------------
