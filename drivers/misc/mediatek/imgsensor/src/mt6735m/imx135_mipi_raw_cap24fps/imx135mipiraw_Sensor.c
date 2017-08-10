df8f302d6107bdcc387bdf9c4139022c7ab93c56
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-10 09:48:50 +0800

    [ALPS02719889] cam: sensor log reduction
    [Detail] sensor log reduction
    
    [Solution] done
    
    MTK-Commit-Id: a368b132cd9cf9c1d4447637b7b4cbeb7fadfc2e
    
    Change-Id: I0d6182fb4864256a31a2906d6ee9c72af99d26be
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02719889
    Feature: Others

--- drivers/misc/mediatek/imgsensor/src/mt6735m/imx135_mipi_raw_cap24fps/imx135mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/mt6735m/imx135_mipi_raw_cap24fps/imx135mipiraw_Sensor.c
@@ -2216,7 +2216,7 @@ static kal_uint32 preview(MSDK_SENSOR_EXPOSURE_WINDOW_STRUCT *image_window,
     imgsensor.autoflicker_en = KAL_FALSE;
     spin_unlock(&imgsensor_drv_lock);
     preview_setting();
-    LOG_INF("L\n");
+
     return ERROR_NONE;
 }   /*  preview   */
 
@@ -2482,7 +2482,6 @@ static kal_uint32 get_resolution(MSDK_SENSOR_RESOLUTION_INFO_STRUCT *sensor_reso
     sensor_resolution->SensorCustom5Width  = imgsensor_info.custom5.grabwindow_width;
     sensor_resolution->SensorCustom5Height     = imgsensor_info.custom5.grabwindow_height;
 
-    LOG_INF("L\n");
     return ERROR_NONE;
 }   /*  get_resolution  */
 
@@ -2895,7 +2894,6 @@ static kal_uint32 feature_control(MSDK_SENSOR_FEATURE_ENUM feature_id,
     SENSOR_WINSIZE_INFO_STRUCT *wininfo;
     MSDK_SENSOR_REG_INFO_STRUCT *sensor_reg_data=(MSDK_SENSOR_REG_INFO_STRUCT *) feature_para;
 
-    LOG_INF("feature_id = %d\n", feature_id);
     switch (feature_id) {
         case SENSOR_FEATURE_GET_PERIOD:
             *feature_return_para_16++ = imgsensor.line_length;
