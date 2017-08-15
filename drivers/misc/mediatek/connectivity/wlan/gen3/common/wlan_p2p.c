9eac514c63fb486451e374069ae0c8df59bf19e1
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-15 13:56:29 +0800

    [ALPS03019499] Wi-Fi: Add AP uApsd support
    [Detail]
    We need to support Hotspot WMMPS to fullfill customers requirement
    
    [Solution]
    Hotspot WMMPS is not supported by default
    Driver need to send specific command to enable Hotspot uApsd.
    1. when "ApUapsd 1" detected in wifi.cfg
    2. when "iwpriv wlan0 set_sw_ctrl 0x10010103 1" issued to driver
    
    MTK-Commit-Id: f616a3182b12a1a58be9a63fc87c08d5650a4905
    
    Change-Id: Ie99e5149afab67890da4ecc217544b887c251aaf
    CR-Id: ALPS03019499
    Feature: Wi-Fi
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    (cherry picked from commit df112f725ec064611624b49a09c14f11d8b029d6)

--- drivers/misc/mediatek/connectivity/wlan/gen3/common/wlan_p2p.c
+++ drivers/misc/mediatek/connectivity/wlan/gen3/common/wlan_p2p.c
@@ -1381,7 +1381,7 @@ wlanoidSetUApsdParam(IN P_ADAPTER_T prAdapter,
 	prBssInfo = GET_BSS_INFO_BY_INDEX(prAdapter, prUapsdParam->ucBssIdx);
 	prPmProfSetupInfo = &prBssInfo->rPmProfSetupInfo;
 
-	kalMemZero(&rCmdUapsdParam, sizeof(CMD_CUSTOM_OPPPS_PARAM_STRUCT_T));
+	kalMemZero(&rCmdUapsdParam, sizeof(CMD_CUSTOM_UAPSD_PARAM_STRUCT_T));
 	rCmdUapsdParam.fgEnAPSD = prUapsdParam->fgEnAPSD;
 
 	rCmdUapsdParam.fgEnAPSD_AcBe = prUapsdParam->fgEnAPSD_AcBe;
@@ -1400,17 +1400,6 @@ wlanoidSetUApsdParam(IN P_ADAPTER_T prAdapter,
 	rCmdUapsdParam.ucMaxSpLen = prUapsdParam->ucMaxSpLen;
 	prPmProfSetupInfo->ucUapsdSp = prUapsdParam->ucMaxSpLen;
 
-#if 0
-	return wlanSendSetQueryCmd(prAdapter,
-				   CMD_ID_SET_UAPSD_PARAM,
-				   TRUE,
-				   FALSE,
-				   TRUE,
-				   nicCmdEventSetCommon,
-				   nicOidCmdTimeoutCommon,
-				   sizeof(CMD_CUSTOM_OPPPS_PARAM_STRUCT_T),
-				   (PUINT_8) &rCmdUapsdParam, pvSetBuffer, u4SetBufferLen);
-#else
 	return wlanoidSendSetQueryP2PCmd(prAdapter,
 					 CMD_ID_SET_UAPSD_PARAM,
 					 prBssInfo->ucBssIndex,
@@ -1419,10 +1408,8 @@ wlanoidSetUApsdParam(IN P_ADAPTER_T prAdapter,
 					 TRUE,
 					 NULL,
 					 nicOidCmdTimeoutCommon,
-					 sizeof(CMD_CUSTOM_OPPPS_PARAM_STRUCT_T),
+					 sizeof(CMD_CUSTOM_UAPSD_PARAM_STRUCT_T),
 					 (PUINT_8) &rCmdUapsdParam, pvSetBuffer, u4SetBufferLen);
-
-#endif
 }
 
 WLAN_STATUS
