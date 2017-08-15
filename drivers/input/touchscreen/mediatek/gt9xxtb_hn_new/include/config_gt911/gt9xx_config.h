b5340c7727e890447a4ef25eac13f689aa9b9c21
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-15 13:58:14 +0800

    [ALPS03395132] touch: remove some touch driver
    remove some old touch driver
    use new architecture driver instead of them
    fix GPL license issue
    
    MTK-Commit-Id: a2c145927ab26315968c14e393e23f50dc962c5f
    
    Change-Id: Ic2a491ab8a44fbb7c53e3513ba4c3bbf951513ff
    CR-Id: ALPS03395132
    Feature: touch
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>

--- drivers/input/touchscreen/mediatek/gt9xxtb_hn_new/include/config_gt911/gt9xx_config.h
+++ drivers/input/touchscreen/mediatek/gt9xxtb_hn_new/include/config_gt911/gt9xx_config.h
@@ -1,6 +1,6 @@
-/* drivers/input/touchscreen/gt9xx_config.h
+/* drivers/input/touchscreen/gt1x_generic.h
  *
- * 2010 - 2016 Goodix Technology.
+ * 2010 - 2014 Goodix Technology.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,16 +13,17 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
  *
+ * Version: 1.0
+ * Revision Record:
+ *      V1.0:  first release. 2014/09/28.
+ *
  */
+
 #ifndef _GT9XX_CONFIG_H_
 #define _GT9XX_CONFIG_H_
 
-/* ***************************PART2:TODO define********************************** */
-/* STEP_1(REQUIRED):Change config table. */
-/*TODO: puts the config info corresponded to your TP here, the following is just
-a sample config, send this config should cause the chip cannot work normally*/
-#define CTP_CFG_GROUP1 {\
-0x41, 0x20, 0x03, 0x00, 0x05, 0x05, 0x38, 0x06,\
+#define GTP_CFG_GROUP0 {\
+0x41, 0x20, 0x03, 0x00, 0x05, 0x05, 0x38, 0x00,\
 0x01, 0x0F, 0x28, 0x07, 0x55, 0x37, 0x03, 0x05,\
 0x00, 0x00, 0x00, 0x00, 0x11, 0x11, 0x05, 0x0C,\
 0x0E, 0x0F, 0x0B, 0x8C, 0x2E, 0x0E, 0x2F, 0x2D,\
@@ -33,8 +34,8 @@ a sample config, send this config should cause the chip cannot work normally*/
 0x2F, 0x00, 0x99, 0x39, 0x00, 0x85, 0x44, 0x00,\
 0x73, 0x52, 0x00, 0x73, 0x00, 0x00, 0x00, 0x00,\
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,\
-0x00, 0x00, 0x00, 0x1A, 0x0D, 0x03, 0x06, 0x00,\
-0x21, 0x43, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00,\
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,\
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,\
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,\
 0x02, 0x04, 0x06, 0x08, 0x0A, 0x0C, 0x0E, 0x10,\
 0x12, 0x14, 0x16, 0x18, 0x1A, 0x1C, 0x00, 0x00,\
@@ -45,42 +46,28 @@ a sample config, send this config should cause the chip cannot work normally*/
 0x08, 0x0A, 0x0C, 0x0F, 0x10, 0x12, 0x13, 0x14,\
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,\
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,\
-0x6E, 0x01,\
+0x0E, 0x01,\
 }
 
-/* default config for K82 */
+#define GTP_CFG_GROUP0_CHARGER {}
 
-#define CTP_CFG_GROUP1_CHARGER {\
-}
+#define GTP_CFG_GROUP1 {}
 
-/* TODO puts your group2 config info here,if need. */
-#define CTP_CFG_GROUP2 {\
-}
+#define GTP_CFG_GROUP1_CHARGER {}
 
-/* TODO puts your group2 config info here,if need. */
-#define CTP_CFG_GROUP2_CHARGER {\
-}
+#define GTP_CFG_GROUP2 {}
 
-/* TODO puts your group3 config info here,if need. */
-#define CTP_CFG_GROUP3 {\
-}
+#define GTP_CFG_GROUP2_CHARGER {}
 
-/* TODO puts your group3 config info here,if need. */
-#define CTP_CFG_GROUP3_CHARGER {\
-}
+#define GTP_CFG_GROUP3 {}
 
-/* TODO: define your config for Sensor_ID == 3 here, if needed */
-#define CTP_CFG_GROUP4 {\
-}
+#define GTP_CFG_GROUP3_CHARGER {}
 
-/* TODO: define your config for Sensor_ID == 4 here, if needed */
-#define CTP_CFG_GROUP5 {\
-}
+#define GTP_CFG_GROUP4 {}
 
-/* TODO: define your config for Sensor_ID == 5 here, if needed */
-#define CTP_CFG_GROUP6 {\
-}
+#define GTP_CFG_GROUP5 {}
 
+#define GTP_CFG_GROUP6 {}
 
-
-#endif /* _GT9XX_CONFIG_H_ */
+#define TPD_CALIBRATION_MATRIX {0, 2559, 0, -6557, 0, 5238784, 0, 0}
+#endif
