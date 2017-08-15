1488a5686d7818045d0c515f017fc3b1fdf14439
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-15 13:44:54 +0800

    [ALPS03400277] yaffs: fix license header
    [Detail]
    fix license header
    
    MTK-Commit-Id: af09bfd45ec8e86505c73031120523c6bc70be59
    
    Change-Id: I9ae7be1b943500728895341fa3b2310c28e305c4
    CR-Id: ALPS03400277
    Feature: Others
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>

--- fs/yaffs2/yaffs_guts.h
+++ fs/yaffs2/yaffs_guts.h
@@ -7,7 +7,7 @@
  * Created by Charles Manning <charles@aleph1.co.uk>
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 2.1 as
+ * it under the terms of the GNU General Public License version 2.1 as
  * published by the Free Software Foundation.
  *
  * Note: Only YAFFS headers are LGPL, YAFFS C code is covered by GPL.
