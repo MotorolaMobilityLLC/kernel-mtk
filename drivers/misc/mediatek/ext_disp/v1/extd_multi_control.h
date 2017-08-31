938860f16d09caf2e9ff48c431871a707213b055
rita.chan <rita.chan@mediatek.com>
2017-08-31 22:10:33 +0800

    [ALPS02767604] DISP: fix display driver disp_session.h
    [Detail] modify typedef coding style and fix session problem
    
    MTK-Commit-Id: 8120317c2990d03402478221ddb4ae1c950d17f2
    
    Change-Id: I40bab11e3b06f968d67d2fab805b479966f82559
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02767604
    Feature: Display Driver (DSI Interface)

--- drivers/misc/mediatek/ext_disp/v1/extd_multi_control.h
+++ drivers/misc/mediatek/ext_disp/v1/extd_multi_control.h
@@ -18,9 +18,9 @@
 #include "disp_session.h"
 
 void external_display_control_init(void);
-int external_display_config_input(disp_session_input_config *input, int idx, unsigned int session);
-int external_display_trigger(EXTD_TRIGGER_MODE trigger, unsigned int session);
+int external_display_config_input(struct disp_session_input_config *input, int idx, unsigned int session);
+int external_display_trigger(enum EXTD_TRIGGER_MODE trigger, unsigned int session);
 int external_display_wait_for_vsync(void *config, unsigned int session);
 int external_display_get_info(void *info, unsigned int session);
-int external_display_switch_mode(DISP_MODE mode, unsigned int *session_created, unsigned int session);
+int external_display_switch_mode(enum DISP_MODE mode, unsigned int *session_created, unsigned int session);
 #endif
