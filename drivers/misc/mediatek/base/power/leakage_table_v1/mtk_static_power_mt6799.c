884d4f9cb0dae7e2637e86f7eca73664ba4fbf68
rita.chan <rita.chan@mediatek.com>
2017-08-31 22:49:50 +0800

    [ALPS02894131] Leakage Table: Resolve multiple define
    [Detail]
    Move declaration to C file
    
    MTK-Commit-Id: ebdf5febefb746c084bd3034d59ac459bda60cbe
    
    Change-Id: Ifa02733676c829328afaf247ed6fc43d391cb2ba
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02894131
    Feature: Power Management

--- drivers/misc/mediatek/base/power/leakage_table_v1/mtk_static_power_mt6799.c
+++ drivers/misc/mediatek/base/power/leakage_table_v1/mtk_static_power_mt6799.c
@@ -1,48 +1,17 @@
 /*
- * Copyright (C) 2015 MediaTek Inc.
+ * Copyright (C) 2016 MediaTek Inc.
  *
- * This program is free software: you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
 
-#ifndef __MTK_STATIC_POWER_MTK6799_H__
-#define __MTK_STATIC_POWER_MTK6799_H__
-
-#define V_OF_FUSE		1100
-#define T_OF_FUSE		30
-#define FAB_INFO6		19
-#define FAB_INFO7		99
-
-#define DEF_BIG_LEAKAGE		56
-#define DEF_CPULL_LEAKAGE	36
-#define DEF_CPUL_LEAKAGE	53
-#define DEF_CCI_LEAKAGE		3
-#define DEF_GPU_LEAKAGE		93
-#define DEF_VCORE_LEAKAGE	94
-#define DEF_VMD1_LEAKAGE	26
-#define DEF_MODEM_LEAKAGE	27
-
-enum {
-	MTK_SPOWER_CPUBIG = 0,
-	MTK_SPOWER_CPUBIG_CLUSTER,
-	MTK_SPOWER_CPULL,
-	MTK_SPOWER_CPULL_CLUSTER,
-	MTK_SPOWER_CPUL,
-	MTK_SPOWER_CPUL_CLUSTER,
-	MTK_SPOWER_CPU_MAX,
-	MTK_SPOWER_CCI,
-	MTK_SPOWER_GPU,
-	MTK_SPOWER_VCORE,
-	MTK_SPOWER_VMD1,
-	MTK_SPOWER_MODEM,
-	MTK_SPOWER_MAX
-};
+#include <mtk_static_power.h>
 
 char *spower_name[] = {
 	"MTK_SPOWER_CPUBIG",
@@ -58,18 +27,6 @@ char *spower_name[] = {
 	"MTK_SPOWER_VMD1",
 	"MTK_SPOWER_MODEM",
 	"MTK_SPOWER_MAX"
-};
-
-enum {
-	MTK_BIG_LEAKAGE = 0,
-	MTK_LL_LEAKAGE,
-	MTK_L_LEAKAGE,
-	MTK_CCI_LEAKAGE,
-	MTK_GPU_LEAKAGE,
-	MTK_VCORE_LEAKAGE,
-	MTK_MD1_LEAKAGE,
-	MTK_MODEM_LEAKAGE,
-	MTK_LEAKAGE_MAX
 };
 
 char *leakage_name[] = {
@@ -142,4 +99,3 @@ int devinfo_table[] = {
 	1,      380,    804,    80,     177,    11,     34,     1689,
 };
 
-#endif
