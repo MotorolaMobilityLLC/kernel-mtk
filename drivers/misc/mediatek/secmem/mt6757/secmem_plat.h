7251b0e79505c49d57a77a33a2b115a6cb3fe218
rita.chan <rita.chan@mediatek.com>
2017-08-31 21:44:36 +0800

    [ALPS02745894] TrustonicTEE: Add support for MT6757
    [Detail]
    1. Enable gud driver when CONFIG_TRUSTONIC_TEE_SUPPORT=y
    2. Enable secmem driver when CONFIG_TRUSTONIC_TEE_SUPPORT=y
    3. Use set_cpus_allowed_ptr() instead of set_cpus_allowed()
       since the later was removed in kernel 4.4
    4. Fix all check_patch warnings in secmem driver.
    5. Remove legacy platform codes
    6. Remove 302a related codes
    
    MTK-Commit-Id: 220402330fbcb6efe9f0be3bfa28fd47b72860c6
    
    Change-Id: Ic1aadde5fe3afab08664f53da872df8d15657e2c
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02745894
    Feature: Trustonic TEE (Trusted Execution Environment)

---
+++ drivers/misc/mediatek/secmem/mt6757/secmem_plat.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef SECMEM_PLATFORM_H
+#define SECMEM_PLATFORM_H
+
+/* Enable the support of secure memory dump detail */
+#define SECMEM_DEBUG_DUMP
+
+/* Enable secure memory test interface */
+/* #define SECMEM_DEBUG_INTERFACE */
+
+/* Enable secure memory API */
+#define SECMEM_KERNEL_API
+
+#endif				/* end of SECMEM_PLATFORM_H */
