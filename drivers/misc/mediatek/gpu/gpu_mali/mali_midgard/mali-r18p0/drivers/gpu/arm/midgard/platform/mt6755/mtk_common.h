1e9549cf24776901edb7fd4608f104f62d34d5f4
rita.chan <rita.chan@mediatek.com>
2017-09-01 01:49:06 +0800

    [ALPS03380281] GPU: r18p0 kernel ddk
    [Detail] Add r18p0 gpu kernel driver
    
    [Notice]
    Bypass
        ENABLE_VCORE_DVFS_CONTROL
        ENABLE_AXI_1TO2_CONTROL
    For bring up
    file: drivers/misc/mediatek/gpu/gpu_mali/mali_midgard/mali-r18p0/drivers/gpu/arm/midgard/platform/mt6757/Kbuild
    
    MTK-Commit-Id: c89bd331f21605d51cbd71f68fcbbaccbf5147ee
    
    Change-Id: I690003ff173158d5a16bf5c82d2db05d90d637be
    CR-Id: ALPS03380281
    Feature: GPU
    Signed-off-by: rita.chan <rita.chan@mediatek.com>

---
+++ drivers/misc/mediatek/gpu/gpu_mali/mali_midgard/mali-r18p0/drivers/gpu/arm/midgard/platform/mt6755/mtk_common.h
@@ -0,0 +1,35 @@
+/*
+* Copyright (C) 2016 MediaTek Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+*/
+
+#ifndef __MTK_COMMON_H__
+#define __MTK_COMMON_H__
+
+/* MTK device context driver private data */
+struct mtk_config {
+
+    /* mtk mfg mapped register */
+		void __iomem *mfg_register;
+
+    /* main clock for gpu */
+		struct clk *clk_mfg;
+    /* smi clock */
+		struct clk *clk_smi_common;
+    /* mfg MTCMOS */
+		struct clk *clk_mfg_scp;
+    /* display MTCMOS */
+		struct clk *clk_display_scp;
+
+};
+
+
+#endif /* __MTK_COMMON_H__ */
