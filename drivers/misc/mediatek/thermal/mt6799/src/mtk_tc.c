f51e0e7ef04a47f726b57235a9cd488e6d26d1df
rita.chan <rita.chan@mediatek.com>
2017-08-31 23:28:11 +0800

    [ALPS02884509] Thermal: Add another set of calibration formulas
    [Detail]
    Some sample ICs have to use another set of calibration
    formulas. Let thermal driver report temperature accurately.
    
    MTK-Commit-Id: a225fdacc4b2c545c97c38496298d7decfff266f
    
    Change-Id: I2df361c8317de6533c5f33ee74369e8c9d316e30
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02884509
    Feature: Thermal Management

--- drivers/misc/mediatek/thermal/mt6799/src/mtk_tc.c
+++ drivers/misc/mediatek/thermal/mt6799/src/mtk_tc.c
@@ -152,6 +152,7 @@ static __s32 g_adc_cali_en_t;
 static __s32 g_o_slope;
 static __s32 g_o_slope_sign;
 static __s32 g_id;
+static __s32 g_ts_offset;
 
 static __s32 g_ge;
 static __s32 g_oe;
@@ -527,6 +528,9 @@ void tscpu_thermal_cal_prepare(void)
 	/*ID           (1b) *(0x11F10184)[9] */
 	g_id = ((temp0 & _BIT_(9)) >> 9);
 
+	/*TS_OFFSET    (1b) *(0x11F10188)[4] */
+	g_ts_offset = ((temp2 & _BIT_(4)) >> 4);
+
 	/*
 	*   Check ID bit
 	*   If ID=0 (TSMC sample)    , ignore O_SLOPE EFuse value and set O_SLOPE=0.
@@ -534,7 +538,6 @@ void tscpu_thermal_cal_prepare(void)
 	*/
 	if (g_id == 0)
 		g_o_slope = 0;
-
 
 	if (g_adc_cali_en_t == 1) {
 		/*thermal_enable = true; */
@@ -576,16 +579,29 @@ void tscpu_thermal_cal_prepare_2(__u32 ret)
 
 	g_gain = (10000 + g_ge);
 
-	format[0] = (g_o_vtsmcu1 + 3350 - g_oe);
-	format[1] = (g_o_vtsmcu2 + 3350 - g_oe);
-	format[2] = (g_o_vtsmcu3 + 3350 - g_oe);
-	format[3] = (g_o_vtsmcu4 + 3350 - g_oe);
-	format[4] = (g_o_vtsmcu5 + 3350 - g_oe);
-	format[5] = (g_o_vtsmcu6 + 3350 - g_oe);
-	format[6] = (g_o_vtsmcu7 + 3350 - g_oe);
-	format[7] = (g_o_vtsmcu6 + 3350 - g_oe);
-	format[8] = (g_o_vtsmcu7 + 3350 - g_oe);
-	format[9] = (g_o_vtsabb + 3350 - g_oe);
+	if (g_ts_offset == 0) {
+		format[0] = (g_o_vtsmcu1 + 3350 - g_oe);
+		format[1] = (g_o_vtsmcu2 + 3350 - g_oe);
+		format[2] = (g_o_vtsmcu3 + 3350 - g_oe);
+		format[3] = (g_o_vtsmcu4 + 3350 - g_oe);
+		format[4] = (g_o_vtsmcu5 + 3350 - g_oe);
+		format[5] = (g_o_vtsmcu6 + 3350 - g_oe);
+		format[6] = (g_o_vtsmcu7 + 3350 - g_oe);
+		format[7] = (g_o_vtsmcu6 + 3350 - g_oe);
+		format[8] = (g_o_vtsmcu7 + 3350 - g_oe);
+		format[9] = (g_o_vtsabb + 3350 - g_oe);
+	} else {
+		format[0] = (g_o_vtsmcu1 + 3150 - g_oe);
+		format[1] = (g_o_vtsmcu2 + 3150 - g_oe);
+		format[2] = (g_o_vtsmcu3 + 3150 - g_oe);
+		format[3] = (g_o_vtsmcu4 + 3150 - g_oe);
+		format[4] = (g_o_vtsmcu5 + 3150 - g_oe);
+		format[5] = (g_o_vtsmcu6 + 3150 - g_oe);
+		format[6] = (g_o_vtsmcu7 + 3150 - g_oe);
+		format[7] = (g_o_vtsmcu6 + 3150 - g_oe);
+		format[8] = (g_o_vtsmcu7 + 3150 - g_oe);
+		format[9] = (g_o_vtsabb + 3150 - g_oe);
+	}
 
 	for (i = 0; i < TS_ENUM_MAX; i++)
 		g_x_roomt[i] = (((format[i] * 10000) / 4096) * 10000) / g_gain;	/* x_roomt * 10000 */
