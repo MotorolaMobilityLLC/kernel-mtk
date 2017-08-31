fbcfe7780b8dde16e13b9b5b66e4a2c6fb7ef0ad
rita.chan <rita.chan@mediatek.com>
2017-09-01 00:36:23 +0800

    [ALPS03133813] MCDI: add any-core CPU criteria stat in kernel log
    [Detail]
    Add any-core CPU criteria stat in kernel log,
    print for every 5 secs
      - Pause count
      - Check power on/off status fail count
      - Check residency fail count
      - Pass any-core CPU criteria count
    
    MTK-Commit-Id: cdc981a31ae5885fbb9bca581348c7a8e28781e7
    
    Change-Id: I67c21710fda309fc86d91d3ac7f29a744d2e2790
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03133813
    Feature: All Core Always Online (ACAO-MCU Deep Idle)

--- drivers/misc/mediatek/base/power/mcdi/include/mtk_mcdi_governor.h
+++ drivers/misc/mediatek/base/power/mcdi/include/mtk_mcdi_governor.h
@@ -14,6 +14,14 @@
 #ifndef __MTK_MCDI_GOVERNOR_H__
 #define __MTK_MCDI_GOVERNOR_H__
 
+enum {
+	PAUSE_CNT	= 0,
+	CPU_ONOFF_STAT_FAIL_CNT,
+	RESIDENCY_FAIL_CNT,
+	CPU_COND_PASS_CNT,
+	NF_ANY_CORE_CPU_COND_INFO
+};
+
 int mcdi_governor_select(int cpu, int cluster_idx);
 void mcdi_governor_reflect(int cpu, int state);
 void mcdi_avail_cpu_cluster_update(void);
@@ -22,5 +30,6 @@ void set_mcdi_enable_status(bool enabled);
 void get_mcdi_enable_status(bool *enabled, bool *paused);
 int get_residency_latency_result(int cpu);
 void mcdi_state_pause(bool pause);
+void any_core_cpu_cond_get(unsigned long buf[NF_ANY_CORE_CPU_COND_INFO]);
 
 #endif /* __MTK_MCDI_GOVERNOR_H__ */
