789db2f609bd461ef35a5dde356c43371e6c3112
rita.chan <rita.chan@mediatek.com>
2017-09-01 00:17:51 +0800

    [ALPS03074833] cpuidle: Add spm resource item and user
    [Detail]
    1. Add SPM_RESOURCE_ALL to block deepidle/SODI
    2. Add UART user
    
    MTK-Commit-Id: 8775f0c6e24e83feb96272f6021b449a2b42f538
    
    Change-Id: I7224e51d256ac9c273410383cb8c4f19a07f2803
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03074833
    Feature: SODI (Screen On Deep Idle)

--- drivers/misc/mediatek/base/power/include/spm_v3/mtk_spm_resource_req_internal.h
+++ drivers/misc/mediatek/base/power/include/spm_v3/mtk_spm_resource_req_internal.h
@@ -17,6 +17,7 @@
 /* SPM resource request APIs: for internal use */
 
 void spm_resource_req_dump(void);
+void spm_resource_req_block_dump(void);
 unsigned int spm_get_resource_usage(void);
 bool spm_resource_req_init(void);
 void spm_resource_req_debugfs_init(struct dentry *spm_dir);
