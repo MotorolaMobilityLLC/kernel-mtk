0f13842d6059296af55a2c29c6e4274d52832c3d
rita.chan <rita.chan@mediatek.com>
2017-09-01 01:58:08 +0800

    [ALPS03431126] Lens: export symbol
    [Detail]
    
    [Solution]
    
    MTK-Commit-Id: 6844d77353846d8037ba3c838f0075fbe01b3e93
    
    Change-Id: Icc7c9acd0f94cd7b9dacc17549ae3a8751c92215
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03431126
    Feature: Others

--- drivers/misc/mediatek/lens/main/common/lc898214af/LC898214AF.c
+++ drivers/misc/mediatek/lens/main/common/lc898214af/LC898214AF.c
@@ -27,6 +27,7 @@
 
 #define AF_DRVNAME "LC898214AF_DRV"
 #define AF_I2C_SLAVE_ADDR        0xE4
+#define EEPROM_I2C_SLAVE_ADDR    0xA0
 
 #define AF_DEBUG
 #ifdef AF_DEBUG
@@ -51,6 +52,29 @@ static unsigned long g_u4CurrPosition;
 
 static signed short Hall_Max = 0x5800;
 static signed short Hall_Min = 0xA800;
+
+static int s4EEPROM_ReadReg(u16 addr, u16 *data)
+{
+	u8 u8data[2];
+	u8 pu_send_cmd[2] = { (u8) (addr >> 8), (u8) (addr & 0xFF) };
+
+	g_pstAF_I2Cclient->addr = (EEPROM_I2C_SLAVE_ADDR) >> 1;
+	if (i2c_master_send(g_pstAF_I2Cclient, pu_send_cmd, 2) < 0) {
+		LOG_INF("read I2C send failed!!\n");
+		return -1;
+	}
+	if (i2c_master_recv(g_pstAF_I2Cclient, u8data, 2) < 0) {
+		LOG_INF("EEPROM_ReadReg failed!!\n");
+		return -1;
+	}
+	LOG_INF("u8data[0] = 0x%x\n", u8data[0]);
+	LOG_INF("u8data[1] = 0x%x\n", u8data[1]);
+
+	*data = u8data[1] << 8 |  u8data[0];
+	LOG_INF("s4EEPROM_ReadReg 0x%x, 0x%x\n", addr, *data);
+
+	return 0;
+}
 
 static int s4AF_ReadReg(u8 a_uAddr, u8 *a_uData)
 {
@@ -137,6 +161,18 @@ static inline int moveAF(unsigned long a_u4Position)
 
 	if (*g_pAF_Opened == 1) {
 		unsigned char Temp;
+		unsigned short Data;
+		unsigned short Cnt;
+
+		s4EEPROM_ReadReg(0x0F63, &Data);
+
+		if (Data > 0 && Data < 0xFFFF)
+			Hall_Max = Data;
+
+		s4EEPROM_ReadReg(0x0F65, &Data);
+
+		if (Data > 0 && Data < 0xFFFF)
+			Hall_Min = Data;
 
 		s4AF_ReadReg(0xF0, &Temp);
 
@@ -147,12 +183,16 @@ static inline int moveAF(unsigned long a_u4Position)
 
 		s4AF_WriteReg(0, 0xE0, 0x1);
 
+		Cnt = 0;
+
 		while (1) {
 			msleep(20);
 
 			s4AF_ReadReg(0xE0, &Temp);
 
-			if (Temp == 0)
+			Cnt++;
+
+			if (Temp == 0 || Cnt > 3)
 				break;
 		}
 
