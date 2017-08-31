0e380f97d3d582cc8bc00ecfa25261ac0132c0f3
rita.chan <rita.chan@mediatek.com>
2017-08-31 22:48:43 +0800

    [ALPS02631259] USB:comment get version
    [Detail]
    get version for chip is not available now
    comment it first and denote FIXME
    
    MTK-Commit-Id: 010d0032c3d5a5e74fe1053bfb0767d3c54439d4
    
    Change-Id: Ib218009c787aca304ae1d52ecf8386b2cefd2852
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02631259
    Feature: USB 3.0 Device

--- drivers/misc/mediatek/mu3phy/mt6799/mtk-phy-asic.c
+++ drivers/misc/mediatek/mu3phy/mt6799/mtk-phy-asic.c
@@ -1228,10 +1228,14 @@ static int mt_usb_dts_probe(struct platform_device *pdev)
 	else
 		pr_err("ssusb_clk prepare fail\n");
 #endif
+
+	/* FIXME */
+#if 0
 	if (mt_get_chip_sw_ver() >= CHIP_SW_VER_02)
 		usb20_phy_rev6 = 1;
 	else
 		usb20_phy_rev6 = 0;
+#endif
 	return retval;
 }
 
