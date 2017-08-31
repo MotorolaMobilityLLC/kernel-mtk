7f618ba6e86df4d9e2ce61baf4e700b0d4448730
rita.chan <rita.chan@mediatek.com>
2017-09-01 01:46:10 +0800

    [ALPS03299404] Lens: coding style
    [Detail]
    
    [Solution]
    
    MTK-Commit-Id: 63616ec0e77fc07f398854ad0c5c584b0fa715bb
    
    Change-Id: I0a31a1df11cdb0289e88682e987997d7427c3386
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03299404
    Feature: Others

--- drivers/misc/mediatek/lens/main/common/lc898122af/Ois.h
+++ drivers/misc/mediatek/lens/main/common/lc898122af/Ois.h
@@ -20,25 +20,6 @@
 /* History                 : First edition                                         2009.07.30 Y.Tashita */
 /* ******************************************************************************** */
 
-#ifdef	OISINI
-#define	OISINI__
-#else
-#define	OISINI__		extern
-#endif
-
-
-
-
-
-
-
-#ifdef	OISCMD
-#define	OISCMD__
-#else
-#define	OISCMD__		extern
-#endif
-
-
 /* Define According To Usage */
 
 /****************************** Defineê‡ñæ ******************************/
@@ -393,120 +374,120 @@ struct stAdjPar {
 
 };
 
-OISCMD__ struct stAdjPar StAdjPar;	/* Execute Command Parameter */
+extern struct stAdjPar StAdjPar;	/* Execute Command Parameter */
 
-OISCMD__ unsigned char UcOscAdjFlg;	/* For Measure trigger */
+extern unsigned char UcOscAdjFlg;	/* For Measure trigger */
 #define	MEASSTR		0x01
 #define	MEASCNT		0x08
 #define	MEASFIX		0x80
 
-OISINI__ unsigned short UsCntXof;	/* OPTICAL Center Xvalue */
-OISINI__ unsigned short UsCntYof;	/* OPTICAL Center Yvalue */
+extern unsigned short UsCntXof;	/* OPTICAL Center Xvalue */
+extern unsigned short UsCntYof;	/* OPTICAL Center Yvalue */
 
-OISINI__ unsigned char UcPwmMod;	/* PWM MODE */
+extern unsigned char UcPwmMod;	/* PWM MODE */
 #define		PWMMOD_CVL	0x00	/* CVL PWM MODE */
 #define		PWMMOD_PWM	0x01	/* PWM MODE */
 
 #define		INIT_PWMMODE	PWMMOD_CVL	/* initial output mode */
 
-OISINI__ unsigned char UcCvrCod;	/* CverCode */
+extern unsigned char UcCvrCod;	/* CverCode */
 #define	CVER122		0x93	/* LC898122 */
 #define	CVER122A	0xA1	/* LC898122A */
 
 
 /* Prottype Declation */
-OISINI__ void IniSet(void);	/* Initial Top Function */
-OISINI__ void IniSetAf(void);	/* Initial Top Function */
+extern void IniSet(void);	/* Initial Top Function */
+extern void IniSetAf(void);	/* Initial Top Function */
 
-OISINI__ void ClrGyr(unsigned short, unsigned char);	/* Clear Gyro RAM */
+extern void ClrGyr(unsigned short, unsigned char);	/* Clear Gyro RAM */
 #define CLR_FRAM0			0x01
 #define CLR_FRAM1			0x02
 #define CLR_ALL_RAM		0x03
-OISINI__ void BsyWit(unsigned short, unsigned char);	/* Busy Wait Function */
-/* OISINI__ void WitTim_LC898122AF( unsigned short ) ;             // Wait */
-OISINI__ void MemClr(unsigned char *, unsigned short);	/* Memory Clear Function */
-OISINI__ void GyOutSignal(void);	/* Slect Gyro Output signal Function */
-OISINI__ void GyOutSignalCont(void);	/* Slect Gyro Output Continuos Function */
+extern void BsyWit(unsigned short, unsigned char);	/* Busy Wait Function */
+/* extern void WitTim_LC898122AF( unsigned short ) ;             // Wait */
+extern void MemClr(unsigned char *, unsigned short);	/* Memory Clear Function */
+extern void GyOutSignal(void);	/* Slect Gyro Output signal Function */
+extern void GyOutSignalCont(void);	/* Slect Gyro Output Continuos Function */
 #ifdef STANDBY_MODE
-OISINI__ void AccWit(unsigned char);	/* Acc Wait Function */
-OISINI__ void SelectGySleep(unsigned char);	/* Select Gyro Mode Function */
+extern void AccWit(unsigned char);	/* Acc Wait Function */
+extern void SelectGySleep(unsigned char);	/* Select Gyro Mode Function */
 #endif
 #ifdef	GAIN_CONT
-OISINI__ void AutoGainControlSw(unsigned char);	/* Auto Gain Control Sw */
+extern void AutoGainControlSw(unsigned char);	/* Auto Gain Control Sw */
 #endif
-OISINI__ void DrvSw(unsigned char UcDrvSw);	/* Driver Mode setting function */
-OISINI__ void AfDrvSw(unsigned char UcDrvSw);	/* AF Driver Mode setting function */
-OISINI__ void RamAccFixMod(unsigned char);	/* Ram Access Fix Mode setting function */
-OISINI__ void IniPtMovMod(unsigned char);	/* Pan/Tilt parameter setting by mode function */
-OISINI__ void ChkCvr(void);	/* Check Function */
+extern void DrvSw(unsigned char UcDrvSw);	/* Driver Mode setting function */
+extern void AfDrvSw(unsigned char UcDrvSw);	/* AF Driver Mode setting function */
+extern void RamAccFixMod(unsigned char);	/* Ram Access Fix Mode setting function */
+extern void IniPtMovMod(unsigned char);	/* Pan/Tilt parameter setting by mode function */
+extern void ChkCvr(void);	/* Check Function */
 
-OISCMD__ void SrvCon(unsigned char, unsigned char);	/* Servo ON/OFF */
-OISCMD__ unsigned short TneRun(void);	/* Hall System Auto Adjustment Function */
-OISCMD__ unsigned char RtnCen(unsigned char);	/* Return to Center Function */
-OISCMD__ void OisEna(void);	/* OIS Enable Function */
-OISCMD__ void OisEnaLin(void);	/* OIS Enable Function for Line adjustment */
-OISCMD__ void TimPro(void);	/* Timer Interrupt Process Function */
-OISCMD__ void S2cPro(unsigned char);	/* S2 Command Process Function */
+extern void SrvCon(unsigned char, unsigned char);	/* Servo ON/OFF */
+extern unsigned short TneRun(void);	/* Hall System Auto Adjustment Function */
+extern unsigned char RtnCen(unsigned char);	/* Return to Center Function */
+extern void OisEna(void);	/* OIS Enable Function */
+extern void OisEnaLin(void);	/* OIS Enable Function for Line adjustment */
+extern void TimPro(void);	/* Timer Interrupt Process Function */
+extern void S2cPro(unsigned char);	/* S2 Command Process Function */
 
 #ifdef	MN_3BSD05P1
 #define		DIFIL_S2		0x3F7FFE00
 #endif
-OISCMD__ void SetSinWavePara(unsigned char, unsigned char);	/* Sin wave Test Function */
+extern void SetSinWavePara(unsigned char, unsigned char);	/* Sin wave Test Function */
 #define		SINEWAVE	0
 #define		XHALWAVE	1
 #define		YHALWAVE	2
 #define		CIRCWAVE	255
-OISCMD__ unsigned char TneGvc(void);	/* Gyro VC Offset Adjust */
+extern unsigned char TneGvc(void);	/* Gyro VC Offset Adjust */
 
-OISCMD__ void SetZsp(unsigned char);	/* Set Zoom Step parameter Function */
-OISCMD__ void OptCen(unsigned char, unsigned short, unsigned short);	/* Set Optical Center adjusted value Function */
-OISCMD__ void StbOnnN(unsigned char, unsigned char);	/* Stabilizer For Servo On Function */
+extern void SetZsp(unsigned char);	/* Set Zoom Step parameter Function */
+extern void OptCen(unsigned char, unsigned short, unsigned short);	/* Set Optical Center adjusted value Function */
+extern void StbOnnN(unsigned char, unsigned char);	/* Stabilizer For Servo On Function */
 #ifdef	MODULE_CALIBRATION
-OISCMD__ unsigned char LopGan(unsigned char);	/* Loop Gain Adjust */
+extern unsigned char LopGan(unsigned char);	/* Loop Gain Adjust */
 #endif
 #ifdef STANDBY_MODE
-OISCMD__ void SetStandby(unsigned char);	/* Standby control      */
+extern void SetStandby(unsigned char);	/* Standby control      */
 #endif
 #ifdef	MODULE_CALIBRATION
-OISCMD__ unsigned short OscAdj(void);	/* OSC clock adjustment */
+extern unsigned short OscAdj(void);	/* OSC clock adjustment */
 #endif
 
 #ifdef	HALLADJ_HW
 #ifdef	MODULE_CALIBRATION
-OISCMD__ unsigned char LoopGainAdj(unsigned char);
+extern unsigned char LoopGainAdj(unsigned char);
 #endif
-OISCMD__ unsigned char BiasOffsetAdj(unsigned char, unsigned char);
+extern unsigned char BiasOffsetAdj(unsigned char, unsigned char);
 #endif
-OISCMD__ void GyrGan(unsigned char, unsigned long, unsigned long);	/* Set Gyro Gain Function */
-OISCMD__ void SetPanTiltMode(unsigned char);	/* Pan_Tilt control Function */
+extern void GyrGan(unsigned char, unsigned long, unsigned long);	/* Set Gyro Gain Function */
+extern void SetPanTiltMode(unsigned char);	/* Pan_Tilt control Function */
 #ifndef	HALLADJ_HW
-OISCMD__ unsigned long TnePtp(unsigned char, unsigned char);	/* Get Hall Peak to Peak Values */
+extern unsigned long TnePtp(unsigned char, unsigned char);	/* Get Hall Peak to Peak Values */
 #ifdef	MN_3BSD05P1
 #define		HALL_H_VAL	0x3F800000	/* 1.0 */
 #endif
-OISCMD__ unsigned char TneCen(unsigned char, union UnDwdVal);	/* Tuning Hall Center */
+extern unsigned char TneCen(unsigned char, union UnDwdVal);	/* Tuning Hall Center */
 #define		PTP_BEFORE		0
 #define		PTP_AFTER		1
 #endif
 #ifdef GAIN_CONT
-OISCMD__ unsigned char TriSts(void);	/* Read Status of Tripod mode Function */
+extern unsigned char TriSts(void);	/* Read Status of Tripod mode Function */
 #endif
-OISCMD__ unsigned char DrvPwmSw(unsigned char);	/* Select Driver mode Function */
+extern unsigned char DrvPwmSw(unsigned char);	/* Select Driver mode Function */
 #define		Mlnp		0	/* Linear PWM */
 #define		Mpwm		1	/* PWM */
 #ifdef	NEUTRAL_CENTER		/* Gyro VC Offset Adjust */
-OISCMD__ unsigned char TneHvc(void);	/* Hall VC Offset Adjust */
+extern unsigned char TneHvc(void);	/* Hall VC Offset Adjust */
 #endif				/* NEUTRAL_CENTER */
-OISCMD__ void SetGcf(unsigned char);	/* Set DI filter coefficient Function */
-OISCMD__ unsigned long UlH1Coefval;	/* H1 coefficient value */
+extern void SetGcf(unsigned char);	/* Set DI filter coefficient Function */
+extern unsigned long UlH1Coefval;	/* H1 coefficient value */
 #ifdef H1COEF_CHANGER
-OISCMD__ unsigned char UcH1LvlMod;	/* H1 level coef mode */
-OISCMD__ void SetH1cMod(unsigned char);	/* Set H1C coefficient Level chang Function */
+extern unsigned char UcH1LvlMod;	/* H1 level coef mode */
+extern void SetH1cMod(unsigned char);	/* Set H1C coefficient Level chang Function */
 #define		S2MODE		0x40
 #define		ACTMODE		0x80
 #define		MOVMODE		0xFF
 #endif
-OISCMD__ unsigned short RdFwVr(void);
+extern unsigned short RdFwVr(void);
 void RegWriteA_LC898122AF(unsigned short RegAddr, unsigned char RegData);
 void RegReadA_LC898122AF(unsigned short RegAddr, unsigned char *RegData);
 void RamWriteA_LC898122AF(unsigned short RamAddr, unsigned short RamData);
