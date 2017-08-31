9209abae2619208add241684f3da71731c442d08
rita.chan <rita.chan@mediatek.com>
2017-09-01 02:14:45 +0800

    [ALPS03378309] SPM: Modify spm resource init level
    [Detail]
    SPM resource need be initialized before USB init. Set subsys_initcall
    level for SPM resource init function.
    
    MTK-Commit-Id: 44472bb6833c9ea3369c7773f212b1f5e1ac739e
    
    Change-Id: I30e6ca7540c58d787bd418716bcb2faa997af65d
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03378309
    Feature: [Android Default] USB OTG
    (cherry picked from commit b2a7d55f1bfc588737e69d60b928fd6fd846ca62)

--- drivers/misc/mediatek/base/power/spm_v3/mtk_spm_init.c
+++ drivers/misc/mediatek/base/power/spm_v3/mtk_spm_init.c
@@ -29,10 +29,15 @@ static int __init mt_spm_init(void)
 	spm_module_init();
 	slp_module_init();
 #endif
-
-	spm_resource_req_init();
-
 	return 0;
 }
 
 late_initcall(mt_spm_init);
+
+static int __init mt_spm_resource_init(void)
+{
+	spm_resource_req_init();
+
+	return 0;
+}
+subsys_initcall(mt_spm_resource_init);
