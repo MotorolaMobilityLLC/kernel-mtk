32ec27930acf5c58169cb9d17711692aa46e5896
rita.chan <rita.chan@mediatek.com>
2017-09-01 00:09:44 +0800

    [ALPS03057375] sspm: reduce reserved memory
    [Detail]
    1. Reduce reserve mblock size for logger buffer.
    2. Increase reserve mblock size GPU DVFS.
    3. Remove 4M reserve mblock for ondiemet.
       (Ondiemet would reserve by itself)
    
    MTK-Commit-Id: 67db85a0aa251200f824d622bcfe30ec80fb1025
    
    Change-Id: I6f7ab15d171cad7336a9da65a4ffa292c779bb4d
    CR-Id: ALPS03057375
    Feature: TinySys
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    (cherry picked from commit 8bfea94684268cad540f9f40aea782560d62ecea)

--- drivers/misc/mediatek/sspm/mt6799/sspm_reservedmem_define.h
+++ drivers/misc/mediatek/sspm/mt6799/sspm_reservedmem_define.h
@@ -18,16 +18,15 @@ enum {
 	SSPM_MEM_ID = 0,
 	PTPOD_MEM_ID,
 	GPU_DVFS_MEM_ID,
-	/* debug only */
-	MET_MEM_ID,
 	NUMS_MEM_ID,
 };
 
 #ifdef _SSPM_INTERNAL_
+/* The total size of sspm_reserve_mblock should less equal than reserve-memory-sspm_share of device tree */
 static struct sspm_reserve_mblock sspm_reserve_mblock[NUMS_MEM_ID] = {
 	{
 		.num = SSPM_MEM_ID,
-		.size = 0x400000,
+		.size = 0x100400,
 	},
 	{
 		.num = PTPOD_MEM_ID,
@@ -35,11 +34,7 @@ static struct sspm_reserve_mblock sspm_reserve_mblock[NUMS_MEM_ID] = {
 	},
 	{
 		.num = GPU_DVFS_MEM_ID,
-		.size = 0x90,
-	},
-	{
-		.num = MET_MEM_ID,
-		.size = 0x400000,
+		.size = 0xA000,
 	},
 };
 #endif
