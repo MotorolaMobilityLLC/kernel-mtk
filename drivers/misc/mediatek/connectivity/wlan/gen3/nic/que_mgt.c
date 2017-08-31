d139916c019e6aed4d9e3c285fab8b7001ef26cd
rita.chan <rita.chan@mediatek.com>
2017-09-01 00:30:57 +0800

    [ALPS03171082] wlan: Handle packet with NULL StaRec and header translation
    [Detail]
    If AP send gragment frame packet,these packets will be dropped.
    [Solution]
    There are four types of packets:
    NULL StaRec & header translation will be droped,
    NULL StaRec & no header translation is not exist,
    StaRec & no header translation means fragment packet will be saved,
    StaRec & header translation means normal packet will be saved.
    
    MTK-Commit-Id: 4bc1f505e570f527169536abf5443b84aba6a5a7
    
    Change-Id: I320a4c2af3e3b9248a6cec387964c935016c39bd
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03171082
    Feature: wlan gen3

--- drivers/misc/mediatek/connectivity/wlan/gen3/nic/que_mgt.c
+++ drivers/misc/mediatek/connectivity/wlan/gen3/nic/que_mgt.c
@@ -2600,7 +2600,8 @@ P_SW_RFB_T qmHandleRxPackets(IN P_ADAPTER_T prAdapter, IN P_SW_RFB_T prSwRfbList
 
 		} else if (prCurrSwRfb->fgDataFrame) {
 			/* Check Class Error */
-			if (secCheckClassError(prAdapter, prCurrSwRfb, prCurrSwRfb->prStaRec) == TRUE) {
+			if (prCurrSwRfb->prStaRec && (secCheckClassError(prAdapter, prCurrSwRfb,
+									prCurrSwRfb->prStaRec) == TRUE)) {
 				P_RX_BA_ENTRY_T prReorderQueParm = NULL;
 
 				/* Invalid BA aggrement */
