67fe0fb0eb3ecd1ba5fb1860e4866582437cf973
rita.chan <rita.chan@mediatek.com>
2017-08-31 22:44:12 +0800

    [ALPS02856177] pmic: add ipi intf
    [Detail]
    Add IPI interface for sub pmic.
    
    MTK-Commit-Id: 836866c37712376b86a8c8f4d753cb8af87d5e5e
    
    Change-Id: I066bec5902d861dea00726b73936dd720ca63b7e
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02856177
    Feature: Power Management

---
+++ drivers/misc/mediatek/include/mt-plat/mt6799/include/mach/mtk_pmic_ipi.h
@@ -0,0 +1,44 @@
+/*
+* Copyright (C) 2016 MediaTek Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+*/
+/*****************************************************************************
+*
+* Filename:
+* ---------
+*   mt_pmic_ipi.h
+*
+* Project:
+* --------
+*   Android
+*
+* Description:
+* ------------
+*   pmic ipi header file
+*
+* Author:
+* -------
+*
+****************************************************************************/
+
+#ifndef _MT_PMIC_IPI_H_
+#define _MT_PMIC_IPI_H_
+
+void pmic_ipi_write_test(void);
+
+unsigned int pmic_ipi_read_interface(unsigned int RegNum, unsigned int *val, unsigned int MASK,
+					unsigned int SHIFT, unsigned char lock);
+unsigned int pmic_ipi_config_interface(unsigned int RegNum, unsigned int val, unsigned int MASK,
+					unsigned int SHIFT, unsigned char lock);
+unsigned int sub_pmic_ipi_interface(unsigned int type, unsigned int ctrl);
+
+#endif /* _MT_PMIC_IPI_H_*/
+
