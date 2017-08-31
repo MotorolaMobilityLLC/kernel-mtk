f05ae778503194b1799aa1729805fbbfc21b3cd5
rita.chan <rita.chan@mediatek.com>
2017-09-01 01:56:58 +0800

    [ALPS03396073] memcfg: fix coverity issue
    [Detail]
    Cast memtotal to unsigned long long to avoid overflow.
    
    MTK-Commit-Id: 4d2fa0438852f1b4dac2ef6dc017062a0363c2a6
    
    Change-Id: I1440046ef549045420ce31449acc1663c03129ef
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03396073
    Feature: Memory
    (cherry picked from commit 9f8e69fb7251f010c6c7b68dc37dbd2963ef3f14)

--- drivers/misc/mediatek/mem/mtk_memcfg_reserve_info.c
+++ drivers/misc/mediatek/mem/mtk_memcfg_reserve_info.c
@@ -196,7 +196,7 @@ static unsigned long long get_memtotal(void)
 	unsigned long long memtotal;
 
 	si_meminfo(&i);
-	memtotal = i.totalram << PAGE_SHIFT;
+	memtotal = (unsigned long long)i.totalram << PAGE_SHIFT;
 
 	return memtotal;
 }
