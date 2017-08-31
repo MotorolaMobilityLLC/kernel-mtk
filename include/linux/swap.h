65e12c79d431c52ce5eafcbb3dfb08336e93feb6
rita.chan <rita.chan@mediatek.com>
2017-08-31 23:57:09 +0800

    [ALPS02563038] mm: add API to shrink_all_memory without swap
    When shrinking lots of memory at once, eg. for large cma_alloc
    request, it is possible that memory swapped eariler will be freed
    right after. Shrinking without using swap in this case will
    accelerate the process.
    
    Add shrink_all_memory_no_swap api to shrink without using swap.
    Also, shrink_all_memory is only available when HIBERNATION is
    enabled. Make both functions available for all cases.
    
    MTK-Commit-Id: 92df1737d86df8e195c2affc88626eea7331b09c
    
    Change-Id: I73cc13e11850fc68c2ad09a8c3e2f0ff656230b3
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02563038
    Feature: CMA

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -328,6 +328,7 @@ extern unsigned long mem_cgroup_shrink_node_zone(struct mem_cgroup *mem,
 						struct zone *zone,
 						unsigned long *nr_scanned);
 extern unsigned long shrink_all_memory(unsigned long nr_pages);
+extern unsigned long shrink_all_memory_no_swap(unsigned long nr_pages);
 extern int vm_swappiness;
 extern int remove_mapping(struct address_space *mapping, struct page *page);
 extern unsigned long vm_total_pages;
