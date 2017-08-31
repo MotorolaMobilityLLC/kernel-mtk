0579717b740ff22365fc8799d76ff7f29ec57dd9
rita.chan <rita.chan@mediatek.com>
2017-08-31 21:24:32 +0800

    [ALPS02694810] cmdq: Enable cmdq v2driver
    [Detail]
    Use cmdq driver v2 for mt6757 platform.
    
    MTK-Commit-Id: f1d9a3f0681e65f1cba0ac3d3d255c95c3e1c9d7
    
    Change-Id: Ic797eb7fe8cd2ca0e17669ef384288da67712837
    CR-Id: ALPS02694810
    Feature: CMDQ
    Signed-off-by: rita.chan <rita.chan@mediatek.com>

---
+++ drivers/misc/mediatek/cmdq/v2/mt6757/cmdq_engine.h
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __CMDQ_ENGINE_H__
+#define __CMDQ_ENGINE_H__
+
+enum CMDQ_ENG_ENUM {
+	/* ISP */
+	CMDQ_ENG_ISP_IMGI = 0,
+	CMDQ_ENG_ISP_IMGO,	/* 1 */
+	CMDQ_ENG_ISP_IMG2O,	/* 2 */
+
+	/* MDP */
+	CMDQ_ENG_MDP_CAMIN,	/* 3 */
+	CMDQ_ENG_MDP_RDMA0,	/* 4 */
+	CMDQ_ENG_MDP_RDMA1,	/* 5 */
+	CMDQ_ENG_MDP_RSZ0,	/* 6 */
+	CMDQ_ENG_MDP_RSZ1,	/* 7 */
+	CMDQ_ENG_MDP_RSZ2,	/* 8 */
+	CMDQ_ENG_MDP_TDSHP0,	/* 9 */
+	CMDQ_ENG_MDP_COLOR0,	/* 10 */
+	CMDQ_ENG_MDP_WROT0,	/* 11 */
+	CMDQ_ENG_MDP_WROT1,	/* 12 */
+	CMDQ_ENG_MDP_WDMA,	/* 13 */
+
+	/* JPEG & VENC */
+	CMDQ_ENG_JPEG_ENC,	/* 14 */
+	CMDQ_ENG_VIDEO_ENC,	/* 15 */
+	CMDQ_ENG_JPEG_DEC,	/* 16 */
+	CMDQ_ENG_JPEG_REMDC,	/* 17 */
+
+	/* DISP */
+	CMDQ_ENG_DISP_UFOE,	/* 18 */
+	CMDQ_ENG_DISP_AAL,	/* 19 */
+	CMDQ_ENG_DISP_COLOR0,	/* 20 */
+	CMDQ_ENG_DISP_RDMA0,	/* 21 */
+	CMDQ_ENG_DISP_RDMA1,	/* 22 */
+	CMDQ_ENG_DISP_WDMA0,	/* 23 */
+	CMDQ_ENG_DISP_WDMA1,	/* 24 */
+	CMDQ_ENG_DISP_OVL0,	/* 25 */
+	CMDQ_ENG_DISP_OVL1,	/* 26 */
+	CMDQ_ENG_DISP_OVL2,	/* 27 */
+	CMDQ_ENG_DISP_GAMMA,	/* 28 */
+	CMDQ_ENG_DISP_DSI0_VDO,	/* 29 */
+	CMDQ_ENG_DISP_DSI0_CMD,	/* 30 */
+	CMDQ_ENG_DISP_DSI0,	/* 31 */
+	CMDQ_ENG_DISP_DPI,	/* 32 */
+	CMDQ_ENG_DISP_2L_OVL0,	/* 33 */
+	CMDQ_ENG_DISP_2L_OVL1,	/* 34 */
+	CMDQ_ENG_DISP_2L_OVL2,	/* 35 */
+
+	/* DPE */
+	CMDQ_ENG_DPE,	/* 36 */
+
+	/* temp: CMDQ internal usage */
+	CMDQ_ENG_CMDQ,
+	CMDQ_ENG_DISP_MUTEX,
+	CMDQ_ENG_MMSYS_CONFIG,
+
+	/* Dummy Engine */
+	CMDQ_ENG_MDP_TDSHP1,
+	CMDQ_ENG_MDP_MOUT0,
+	CMDQ_ENG_MDP_MOUT1,
+
+	CMDQ_ENG_DISP_COLOR1,
+	CMDQ_ENG_DISP_RDMA2,
+	CMDQ_ENG_DISP_MERGE,
+	CMDQ_ENG_DISP_SPLIT0,
+	CMDQ_ENG_DISP_SPLIT1,
+	CMDQ_ENG_DISP_DSI1_VDO,
+	CMDQ_ENG_DISP_DSI1_CMD,
+	CMDQ_ENG_DISP_DSI1,
+
+	CMDQ_MAX_ENGINE_COUNT	/* ALWAYS keep at the end */
+};
+
+#endif				/* __CMDQ_ENGINE_H__ */
