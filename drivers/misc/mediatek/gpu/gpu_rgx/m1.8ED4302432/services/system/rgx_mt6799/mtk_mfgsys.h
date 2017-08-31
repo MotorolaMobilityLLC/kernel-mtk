bbf2ae0341aed7888a0d81d4196c01ab9c1faf46
rita.chan <rita.chan@mediatek.com>
2017-08-31 23:54:28 +0800

    [ALPS03020488] GPU: Upgrade DDK to 1.8ED430
    [Detail]
    Upgrade GPU DDK to 1.8ED4302432
    
    MTK-Commit-Id: e6bf38e2d106197746815fcba4afd837d7aef606
    
    Change-Id: Iecc035ca18ec8e6cff14aa4e3048b03ce2962252
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03020488
    Feature: GPU

---
+++ drivers/misc/mediatek/gpu/gpu_rgx/m1.8ED4302432/services/system/rgx_mt6799/mtk_mfgsys.h
@@ -0,0 +1,39 @@
+#ifndef MTK_MFGSYS_H
+#define MTK_MFGSYS_H
+
+#include "servicesext.h"
+#include "rgxdevice.h"
+
+/* control APM is enabled or not  */
+#define MTK_PM_SUPPORT 1
+#define MTCMOS_CONTROL 1
+
+//extern to be used by PVRCore_Init in RGX DDK module.c 
+PVRSRV_ERROR MTKMFGSystemInit(void);
+
+void MTKMFGSystemDeInit(void);
+
+void MTKMFGEnableDVFSTimer(bool bEnable);
+
+/* below register interface in RGX sysconfig.c */
+PVRSRV_ERROR MTKDevPrePowerState(IMG_HANDLE hSysData, PVRSRV_DEV_POWER_STATE eNewPowerState,
+                                         PVRSRV_DEV_POWER_STATE eCurrentPowerState,
+									     IMG_BOOL bForced);
+
+PVRSRV_ERROR MTKDevPostPowerState(IMG_HANDLE hSysData, PVRSRV_DEV_POWER_STATE eNewPowerState,
+                                          PVRSRV_DEV_POWER_STATE eCurrentPowerState,
+									      IMG_BOOL bForced);
+
+PVRSRV_ERROR MTKSystemPrePowerState(PVRSRV_SYS_POWER_STATE eNewPowerState);
+
+PVRSRV_ERROR MTKSystemPostPowerState(PVRSRV_SYS_POWER_STATE eNewPowerState);
+
+void MTKRGXDeviceInit(PVRSRV_DEVICE_CONFIG *psDevConfig);
+
+extern int spm_mtcmos_ctrl_mfg0(int state);
+extern int spm_mtcmos_ctrl_mfg1(int state);
+extern int spm_mtcmos_ctrl_mfg2(int state);
+
+extern void switch_mfg_clk(int src);
+
+#endif
