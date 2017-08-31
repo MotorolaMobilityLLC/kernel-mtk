e761375e23bfedca14b5c187cfde5b59d4d10d50
rita.chan <rita.chan@mediatek.com>
2017-09-01 00:52:26 +0800

    [ALPS03148580] aurisys: remove high level log
    [Detail] do not use high level log
    
    [Solution] use pr_info & pr_notice instead
    
    MTK-Commit-Id: 8944ceca53fd38112c4d15af062e0a3fdbda0909
    
    Change-Id: Ibf61a22c05ee57ecdddf68bfe7d24bed9844e48a
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03148580
    Feature: Speech Capability

--- drivers/misc/mediatek/audio_ipi/common/utility/audio_log.h
+++ drivers/misc/mediatek/audio_ipi/common/utility/audio_log.h
@@ -17,10 +17,6 @@
 #include <linux/printk.h>
 
 
-/*#define FORCE_ENABLE_AUDIO_LOG*/
-
-#ifndef FORCE_ENABLE_AUDIO_LOG /* use pr_* to print audio log */
-
 #ifndef AUD_LOG_V
 #if 0
 #define AUD_LOG_V(x...) pr_debug(x)
@@ -33,45 +29,12 @@
 #define AUD_LOG_D pr_debug
 #endif
 
-#ifndef AUD_LOG_I
-#define AUD_LOG_I pr_info
-#endif
-
 #ifndef AUD_LOG_W
-#define AUD_LOG_W pr_warning
+#define AUD_LOG_W pr_info
 #endif
 
 #ifndef AUD_LOG_E
-#define AUD_LOG_E pr_err
-#endif
-
-#else /* force dump audio log (local debug only) */
-
-#ifndef AUD_LOG_V
-#if 0
-#define AUD_LOG_V(x...) pr_emerg(x)
-#else
-#define AUD_LOG_V(x...)
-#endif
-#endif
-
-
-#ifndef AUD_LOG_D
-#define AUD_LOG_D pr_emerg
-#endif
-
-#ifndef AUD_LOG_I
-#define AUD_LOG_I pr_emerg
-#endif
-
-#ifndef AUD_LOG_W
-#define AUD_LOG_W pr_emerg
-#endif
-
-#ifndef AUD_LOG_E
-#define AUD_LOG_E pr_emerg
-#endif
-
+#define AUD_LOG_E pr_notice
 #endif
 
 #endif /* end of AUDIO_LOG_H */
