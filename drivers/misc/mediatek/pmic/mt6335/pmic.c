b6903eff502ab5a652458a47adca72aa17998686
rita.chan <rita.chan@mediatek.com>
2017-08-31 22:47:31 +0800

    [ALPS02817681] PMIC: add PONSTS and POFFSTS log
    [Detail]
    1. add PONSTS and POFFSTS log
    2. enable dump boot status and clear them for pmic_pre_wdt_reset()
    
    MTK-Commit-Id: 88fad53b10a23b632c77fd293898f5bebbf2aba2
    
    Change-Id: I7feb384f82ab8a8b8c507a9b216cf8603cfe6f5d
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02817681
    Feature: Power Management

--- drivers/misc/mediatek/pmic/mt6335/pmic.c
+++ drivers/misc/mediatek/pmic/mt6335/pmic.c
@@ -405,51 +405,52 @@ unsigned int enable_vsram_vcore_hw_tracking(unsigned int en)
 /* dump exception reg in kernel and clean status */
 int pmic_dump_exception_reg(void)
 {
-#if 0	/* TBD */
 	int ret_val = 0;
 
-	pr_err("[PMIC_dump_exception_reg][pmic_status]\n");
 	kernel_dump_exception_reg();
 
 	/* clear UVLO off */
-	ret_val = pmic_config_interface(MT6351_TOP_RST_STATUS_CLR, 0xFFFF, 0xFFFF, 0);
+	ret_val = pmic_set_register_value(PMIC_TOP_RST_STATUS_CLR, 0xFFFF);
 
 	/* clear thermal shutdown 150 */
 	ret_val = pmic_set_register_value(PMIC_RG_STRUP_THR_CLR, 0x1);
 	udelay(200);
 	ret_val = pmic_set_register_value(PMIC_RG_STRUP_THR_CLR, 0x0);
 
-	/* clear power not good */
-	ret_val = pmic_set_register_value(PMIC_STRUP_PG_STATUS_CLR, 0x1);
+	/* clear power off status(POFFSTS) and PG status and BUCK OC status */
+	ret_val = pmic_set_register_value(PMIC_RG_POFFSTS_CLR, 0x1);
 	udelay(200);
-	ret_val = pmic_set_register_value(PMIC_STRUP_PG_STATUS_CLR, 0x0);
+	ret_val = pmic_set_register_value(PMIC_RG_POFFSTS_CLR, 0x0);
 
 	/* clear Long press shutdown */
 	ret_val = pmic_set_register_value(PMIC_CLR_JUST_RST, 0x1);
 	udelay(200);
 	ret_val = pmic_set_register_value(PMIC_CLR_JUST_RST, 0x0);
 	udelay(200);
-	kernel_output_reg(MT6351_STRUP_CON12);
+	pr_err(PMICTAG "[pmic_boot_status] JUST_PWRKEY_RST=0x%x\n",
+		pmic_get_register_value(PMIC_JUST_PWRKEY_RST));
 
-	/* clear WDTRST */
-	ret_val = pmic_config_interface(MT6351_TOP_RST_MISC_SET, 0x8, 0xFFFF, 0);
+	/* clear WDTRSTB_STATUS */
+	ret_val = pmic_set_register_value(PMIC_TOP_RST_MISC_SET, 0x8);
 	udelay(100);
-	ret_val = pmic_config_interface(MT6351_TOP_RST_MISC_CLR, 0x8, 0xFFFF, 0);
+	ret_val = pmic_set_register_value(PMIC_TOP_RST_MISC_CLR, 0x8);
 
 	/* clear BUCK OC */
-	ret_val = pmic_config_interface(MT6351_BUCK_OC_CON0, 0xFFFF, 0xFFFF, 0);
+	ret_val = pmic_config_interface(MT6335_BUCK_OC_CON0, 0xFF, 0xFF, 0);
 	udelay(200);
 
 	/* clear Additional(TBD) */
 
 	/* add mdelay for output the log in buffer */
-	mdelay(500);
+#if 0	/* TBD */
 	mdelay(500);
 	mdelay(500);
 	mdelay(500);
 	mdelay(500);
 #endif
-	return 0;
+	mdelay(500);
+
+	return ret_val;
 }
 
 void pmic_dump_register(struct seq_file *m)
@@ -486,18 +487,7 @@ int pmic_pre_wdt_reset(void)
 
 	preempt_disable();
 	local_irq_disable();
-#if 0	/* TBD */
-	/* for olympus pre wdt reset */
-	ret = pmic_set_register_value(PMIC_RG_VCORE_VSLEEP_SEL, 0x0);
-	ret = pmic_set_register_value(PMIC_RG_VSRAM_MD_VSLEEP_SEL, 0x0);
-	ret = pmic_set_register_value(PMIC_RG_VMODEM_VSLEEP_SEL, 0x0);
-	ret = pmic_set_register_value(PMIC_RG_VMD1_VSLEEP_SEL, 0x0);
-
-	ret = pmic_set_register_value(PMIC_BUCK_VCORE_VOSEL_SLEEP, 0x10);
-	ret = pmic_set_register_value(PMIC_BUCK_VMD1_VOSEL_SLEEP, 0x10);
-	ret = pmic_set_register_value(PMIC_BUCK_VMODEM_VOSEL_SLEEP, 0x10);
-	ret = pmic_set_register_value(PMIC_BUCK_VSRAM_MD_VOSEL_SLEEP, 0x10);
-#endif
+	pr_err(PMICTAG "[%s][pmic_boot_status]\n", __func__);
 	pmic_dump_exception_reg();
 #if DUMP_ALL_REG
 	pmic_dump_register();
