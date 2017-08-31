8db440145a5c6efefe2f1fd1d0e7d59a8f9f255a
rita.chan <rita.chan@mediatek.com>
2017-09-01 00:14:38 +0800

    [ALPS03102209] Upower: early porting
    [Detail]
    early porting
    
    MTK-Commit-Id: fa3a784fa519bb512c41620f9dfbb7334e223a49
    
    Change-Id: I2cfc991684556c4c44720fedf11f0b47e0b8a6ac
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03102209
    Feature: PTP-OD (Performance Thermal Power-Over Drive)

---
+++ drivers/misc/mediatek/base/power/upower_v2/inc/mtk_unified_power_internal.h
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+/**********************************************
+ * unified_power_internal.h
+ * This header file includes:
+ * 1. Externs of time profiling related APIs
+ **********************************************/
+#ifndef UNIFIED_POWER_INTERNAL_H
+#define UNIFIED_POWER_INTERNAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/spinlock.h>
+#include <linux/rcupdate.h>
+#include <linux/slab.h>
+#include <linux/errno.h>
+#include <linux/export.h>
+#include <linux/module.h>
+#include <linux/ktime.h>
+
+#define UPOWER_ENABLE (1)
+
+#ifdef CONFIG_MTK_TINYSYS_SSPM_SUPPORT
+	#define UPOWER_ENABLE_TINYSYS_SSPM (0)
+#else
+	#define UPOWER_ENABLE_TINYSYS_SSPM (0)
+#endif
+
+#define EARLY_PORTING_SPOWER /* will not get leakage from leakage driver */
+/* #define UPOWER_UT */
+/* #define UPOWER_PROFILE_API_TIME */
+#define UPOWER_RCU_LOCK
+
+/* for unified power driver internal use */
+#define UPOWER_LOG (1)
+#define UPOWER_TAG "[UPOWER]"
+
+#if UPOWER_LOG
+	#define upower_error(fmt, args...) pr_err(UPOWER_TAG fmt, ##args)
+	#define upower_debug(fmt, args...) pr_debug(UPOWER_TAG fmt, ##args)
+#else
+	#define upower_error(fmt, args...)
+	#define upower_debug(fmt, args...)
+#endif
+
+/*
+ * bit operation
+ */
+#undef  BIT
+#define BIT(bit)	(1U << (bit))
+
+#define MSB(range)	(1 ? range)
+#define LSB(range)	(0 ? range)
+/**
+ * Genearte a mask wher MSB to LSB are all 0b1
+ * @r:	Range in the form of MSB:LSB
+ */
+#define BITMASK(r)	\
+	(((unsigned) -1 >> (31 - MSB(r))) & ~((1U << LSB(r)) - 1))
+
+/**
+ * Set value at MSB:LSB. For example, BITS(7:3, 0x5A)
+ * will return a value where bit 3 to bit 7 is 0x5A
+ * @r:	Range in the form of MSB:LSB
+ */
+/* BITS(MSB:LSB, value) => Set value at MSB:LSB  */
+#define BITS(r, val)	((val << LSB(r)) & BITMASK(r))
+
+#define GET_BITS_VAL(_bits_, _val_)   (((_val_) & (BITMASK(_bits_))) >> ((0) ? _bits_))
+
+#ifdef UPOWER_RCU_LOCK
+extern void upower_read_lock(void);
+extern void upower_read_unlock(void);
+#endif
+
+#ifdef UPOWER_PROFILE_API_TIME
+enum {
+	GET_PWR,
+	GET_TBL_PTR,
+	UPDATE_TBL_PTR,
+
+	TEST_NUM
+};
+extern void upower_get_start_time_us(unsigned int type);
+extern void upower_get_diff_time_us(unsigned int type);
+extern void print_diff_results(unsigned int type);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
