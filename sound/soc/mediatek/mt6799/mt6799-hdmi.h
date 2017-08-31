06636dde2e2c88a405c6dcae01c1673f6ad1fb16
rita.chan <rita.chan@mediatek.com>
2017-09-01 01:20:26 +0800

    [ALPS03125973] Audio: HDMI porting
    [Detail]
    Patch 6799 HDMI factory issue.
    
    MTK-Commit-Id: 7f273e25f5c666055af0649fe3dda4e6a9fbae06
    
    Change-Id: I5cd2a5ae1e281bfa729b406c47defa0995ac4c06
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03125973
    Feature: Driver

--- sound/soc/mediatek/mt6799/mt6799-hdmi.h
+++ sound/soc/mediatek/mt6799/mt6799-hdmi.h
@@ -48,6 +48,7 @@
  ******************************************************************************/
 #define _ANX7805_SLIMPORT_CHANNEL 2
 #define _ANX7625_SLIMPORT_CHANNEL 8
+
 #define HDMI_USE_CHANNELS_MIN     1
 #define HDMI_USE_CHANNELS_MAX     8
 
