212da69378268cc1fa8a3f1f2c5dc2ef58b8252c
rita.chan <rita.chan@mediatek.com>
2017-09-01 00:04:39 +0800

    [ALPS02324135] scp: revert no used flp reserved memory
    [Detail] Revert FLP_MEM_ID reserved 4KB memory
    
    MTK-Commit-Id: d1e6e39135ceb5ea9aada6562c7a3c6346c9f717
    
    Change-Id: I4768c04c684a757d4b3b576c52cd8d6822921366
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02324135
    Feature: Others

--- drivers/misc/mediatek/scp/mt6799/scp_reservedmem_define.h
+++ drivers/misc/mediatek/scp/mt6799/scp_reservedmem_define.h
@@ -34,6 +34,12 @@ static scp_reserve_mblock_t scp_reserve_mblock[] = {
 		.size = 0x400000,/*4MB*/
 	},
 	{
+		.num = FLP_MEM_ID,
+		.start_phys = 0x0,
+		.start_virt = 0x0,
+		.size = 0x1000,/*4KB*/
+	},
+	{
 		.num = RTOS_MEM_ID,
 		.start_phys = 0x0,
 		.start_virt = 0x0,
