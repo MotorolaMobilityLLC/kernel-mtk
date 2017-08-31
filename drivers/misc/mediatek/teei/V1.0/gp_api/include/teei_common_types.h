7c03eea56060f10fb5f8d8c765ab49ac7850997a
rita.chan <rita.chan@mediatek.com>
2017-09-01 01:24:05 +0800

    [ALPS03308416] TEEI: kernel license
    [Detail]
    Add related license description.
    
    MTK-Commit-Id: a05e575216c095df232a5aa4670703cc3f0b6e4e
    
    Change-Id: Idbf87793dac9c519b0b56f2d6c2165714c14a9ab
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03308416
    Feature: Microtrust TEEI

--- drivers/misc/mediatek/teei/V1.0/gp_api/include/teei_common_types.h
+++
@@ -1,4 +0,0 @@
-#ifndef SW_COMMON_TYPES_H
-#define SW_COMMON_TYPES_H
-/*typedef unsigned int size_t;*/
-#endif
--- drivers/misc/mediatek/teei/V1.0/gp_api/include/utos_version.h
+++
@@ -1,4 +0,0 @@
-#ifndef __UTOS_VERSION_H__
-#define __UTOS_VERSION_H__
-#define UTOS_VERSION  "BS.mt6797evb.D.2.2.1"
-#endif
