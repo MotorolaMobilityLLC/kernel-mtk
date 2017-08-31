ca4984c33d26f927db364f2ea63749781e0d4f06
SW Integrator <swintegrator@medaitek.com>
2017-08-31 21:06:42 +0800

    [ALPS02664443] Platform: Add makefile for mediatek folder
    [Detail]
        Add makefile for mediatek drivers and fix build break issue
    [Solution]
        1. Before driver porting disable all of mediatek drivers
        2. Add a dummy driver for build break issue
    
    MTK-Commit-Id: aafda0a6f45e123b866edf768dc7db6bf55e4138
    
    Change-Id: Ibd37c01a11e9ca678f87e7f5b3353af8b46c781e
    Feature: Others
    CR-Id: ALPS02664443
    Signed-off-by: rita.chan <rita.chan@mediatek.com>

---
+++ drivers/misc/mediatek/dummy/dummy.c
@@ -0,0 +1,24 @@
+/*
+ * (C) Copyright 2016
+ * MediaTek <www.mediatek.com>
+ * MingHsien Hsieh <minghsien.hsieh@mediatek.com>
+ *
+ * MTK Dummy Driver
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+void dummy_func(void)
+{
+	/*Dummy driver for build pass purpose only*/
+	;
+}
+
