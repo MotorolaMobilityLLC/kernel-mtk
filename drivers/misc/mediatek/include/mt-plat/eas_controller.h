ac34c50e73619c678744ebe113f78c10abc433a0
rita.chan <rita.chan@mediatek.com>
2017-08-31 23:21:25 +0800

    [ALPS02801839] PerfService: EAS controller
    [Detail]
    1. EAS controller receives multiple boost value for foreground
    2. EAS controller chose the biggest boost value if multiple boost value
    inputs
    3. every input will have a kiker
      ex. PerfService kicker: KIR_PERF, FBC kicker: KIR_FBC; if KIR_PERF kick
      100 and KIR_FBC kick 20, EAS controller will chose boost value = 100 as
      foreground boost value
    4. usage:
      a. KIR_PERF: echo xx > /proc/perfmgr/perfserv_fg_boost
        xx: boost value, if xx > 100 --> boost value = 100
    	if xx < 0 --> release KIR_PERF
      b. KIR_FBC:
        #include "eas_controller.h"
        perfmgr_kick_fg_boost(KIR_FBC, xx);
        xx: boost value, if xx > 100 --> boost value = 100
    	if xx < 0 --> release KIR_FBC
    5. debug:
      echo xx > /proc/perfmgr/debug_fg_boost
        xx: boost value for debug, if xx > 100 --> boost value for debug = 100
        if xx < 0 --> release debug
        debug node will have highest priority
    
    MTK-Commit-Id: dacc8de120f7d4880d23cab8f640bf9a0db574ef
    
    Change-Id: I2a443db0d0e839b4c4084cfcfc4b8f421052584a
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02801839
    Feature: Others

---
+++ drivers/misc/mediatek/include/mt-plat/eas_controller.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#define CGROUP_FG 1
+#define KIR_PERF 0
+#define KIR_FBC 1
+#define MAX_KIR 2
+
+extern int boost_value_for_GED_idx(int, int);
+int perfmgr_kick_fg_boost(int kicker, int value);
+void init_perfmgr_eas_controller(void);
