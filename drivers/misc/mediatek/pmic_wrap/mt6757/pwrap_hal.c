ecbf34f3d2c8e8193033662cd9b98e045e9e11cc
rita.chan <rita.chan@mediatek.com>
2017-08-31 22:16:32 +0800

    [ALPS02761172] pmic_wrap: export pwrap_dump_all_register API
    [Solution]
    1. in order to debug for vcore dvfs, add pwrap_dump_all_register
    to check wrapper status.
    
    MTK-Commit-Id: c7c0b7903e5736beec0065b0c8c8294234bb702d
    
    Change-Id: I7e225d51fbfcedbbaffcfa6c4c47cc16b21c0ad4
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02761172
    Feature: DVFS (Dynamic Voltage and Frequency Scaling)

--- drivers/misc/mediatek/pmic_wrap/mt6757/pwrap_hal.c
+++ drivers/misc/mediatek/pmic_wrap/mt6757/pwrap_hal.c
@@ -1,15 +1,15 @@
 /*
-* Copyright (C) 2016 MediaTek Inc.
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 2 as
-* published by the Free Software Foundation.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
-*/
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
 
 /******************************************************************************
  * pwrap_hal.c - Linux pmic_wrapper Driver,hardware_dependent driver
@@ -123,7 +123,7 @@ static void pwrap_ut(u32 ut_test)
 
 #ifdef PMIC_WRAP_NO_PMIC
 /*-pwrap debug--------------------------------------------------------------------------*/
-static inline void pwrap_dump_all_register(void)
+void pwrap_dump_all_register(void)
 {
 	PWRAPLOG("Nothing to do\n");
 }
@@ -270,7 +270,7 @@ static inline void pwrap_dump_pmic_register(void)
 {
 }
 
-static inline void pwrap_dump_all_register(void)
+void pwrap_dump_all_register(void)
 {
 	pwrap_dump_ap_register();
 	pwrap_dump_pmic_register();
