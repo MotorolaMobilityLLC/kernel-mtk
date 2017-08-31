f876118e5b9bd86296aa4ee80ea491283e1f29a1
rita.chan <rita.chan@mediatek.com>
2017-08-31 22:11:10 +0800

    [ALPS02764792] ZMC: add zone_movable_cma modules
    [Detail] add zone_movable_cma modules
    
    MTK-Commit-Id: 64f14abac57658ada82b52e1e28e5ae89397f213
    
    Change-Id: I467cd95caf2bd29abc60dae4bb0866941d98631e
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02764792
    Feature: Others

--- drivers/misc/mediatek/include/mt-plat/mtk_meminfo.h
+++ drivers/misc/mediatek/include/mt-plat/mtk_meminfo.h
@@ -13,6 +13,7 @@
 
 #ifndef __MTK_MEMINFO_H__
 #define __MTK_MEMINFO_H__
+#include <linux/cma.h>
 
 /* physical offset */
 extern phys_addr_t get_phys_offset(void);
@@ -27,6 +28,26 @@ extern phys_addr_t get_zone_movable_cma_size(void);
 extern phys_addr_t memory_lowpower_cma_base(void);
 extern phys_addr_t memory_lowpower_cma_size(void);
 #endif /* end CONFIG_MTK_MEMORY_LOWPOWER */
+
+#ifdef CONFIG_ZONE_MOVABLE_CMA
+extern phys_addr_t zmc_base(void);
+extern struct page *zmc_cma_alloc(struct cma *cma, int count, unsigned int align);
+extern bool zmc_cma_release(struct cma *cma, struct page *pages, int count);
+
+extern int zmc_register_client(struct notifier_block *nb);
+extern int zmc_unregister_client(struct notifier_block *nb);
+extern int zmc_notifier_call_chain(unsigned long val, void *v);
+
+struct single_cma_registration {
+	phys_addr_t size;
+	phys_addr_t align;
+	const char *name;
+	void (*init)(struct cma *);
+};
+
+#define ZMC_EVENT_ALLOC_MOVABLE 0x01
+#endif
+
 enum dcs_status {
 	DCS_NORMAL,
 	DCS_LOWPOWER,
