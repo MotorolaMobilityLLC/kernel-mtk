a6867d82c1d6029964b79fcee8d345cae99a2263
rita.chan <rita.chan@mediatek.com>
2017-09-01 01:48:13 +0800

    [ALPS03386292] m4u:remove used code
    [Detail]
    m4u need not control smi clock, remove these code.
    
    MTK-Commit-Id: 9413e8bc3959af141771513e3137ca80ff7adb33
    
    Change-Id: Idafca1b1ef4bc44533b213e0e861a5a7d14a04d0
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03386292
    Feature: Others

--- drivers/misc/mediatek/m4u/mt6757/m4u_hw.c
+++ drivers/misc/mediatek/m4u/mt6757/m4u_hw.c
@@ -1097,13 +1097,8 @@ void smi_common_clock_on(void)
 	enable_clock(MT_CG_MMSYS_SMI_COMMON, "smi_common");
 	/* m4uHw_set_field_by_mask(0, 0xf4000108, 0x1, 0x1); */
 #else
-	int ret = 0;
-
 	M4UMSG("error: smi_common_clock_on not support");
 	return;
-	clk_enable(gM4uDev->smi_clk[SMI_COMMON]);
-	if (ret)
-		M4UMSG("error: prepare clk %s fail!.\n", smi_clk_name[SMI_COMMON]);
 #endif
 }
 EXPORT_SYMBOL(smi_common_clock_on);
@@ -1116,7 +1111,6 @@ void smi_common_clock_off(void)
 #else
 	M4UMSG("error: smi_common_clock_off not support");
 	return;
-	clk_disable(gM4uDev->smi_clk[SMI_COMMON]);
 #endif
 }
 EXPORT_SYMBOL(smi_common_clock_off);
