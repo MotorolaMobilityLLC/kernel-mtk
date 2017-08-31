b1aa61d22611d520a7fa0d650f89d134a63c3565
rita.chan <rita.chan@mediatek.com>
2017-08-31 23:20:33 +0800

    [ALPS02839895] sched: fix stune max_power and new_power issue
    [Detail]
    1. fix max_power & min_power issue
    2. FIX: STUNE: update usage of boosted task utilisation on CPU selection
    
    [Solution]
    1. Because system only eight cores online when init, we compute
       the min/max power consumption of all possible clusters and CPUs.
    2. FIX: STUNE: update usage of boosted task utilisation on CPU selection
    
    MTK-Commit-Id: 38102d6757d78f62e7e6ff920eb5102980027a49
    
    Change-Id: Ic99114befef87370abb9b933c9a89c9cbe4ccb2b
    CR-Id:ALPS02839895
    Feature:Sched
    Signed-off-by: rita.chan <rita.chan@mediatek.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5700,6 +5700,8 @@ static int energy_aware_wake_cpu(struct task_struct *p, int target)
 	struct sched_group *sg, *sg_target;
 	int target_max_cap = INT_MAX;
 	int target_cpu = task_cpu(p);
+	unsigned long min_util;
+	unsigned long new_util;
 	int i;
 
 	sd = rcu_dereference(per_cpu(sd_ea, task_cpu(p)));
@@ -5734,13 +5736,21 @@ static int energy_aware_wake_cpu(struct task_struct *p, int target)
 	} while (sg = sg->next, sg != sd->groups);
 
 	/* Find cpu with sufficient capacity */
+	min_util = boosted_task_util(p);
 	for_each_cpu_and(i, tsk_cpus_allowed(p), sched_group_cpus(sg_target)) {
 		/*
 		 * p's blocked utilization is still accounted for on prev_cpu
 		 * so prev_cpu will receive a negative bias due to the double
 		 * accounting. However, the blocked utilization may be zero.
 		 */
-		int new_util = cpu_util(i) + boosted_task_util(p);
+		new_util = cpu_util(i) + task_util(p);
+
+		/*
+		 * Ensure minimum capacity to grant the required boost.
+		 * The target CPU can be already at a capacity level higher
+		 * than the one required to boost the task.
+		 */
+		new_util = max(min_util, new_util);
 
 		if (new_util > capacity_orig_of(i))
 			continue;
