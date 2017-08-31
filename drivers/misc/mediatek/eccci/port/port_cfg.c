2b27cc02b809791d34602c1bb8b9bb440c55346a
rita.chan <rita.chan@mediatek.com>
2017-09-01 02:30:11 +0800

    [ALPS03359784] eccci: add ccci_bip port
    [Detail]
    add new ccci port for MD new feature
    [Solution]
    add ccci port in driver
    
    MTK-Commit-Id: 5f5a4dc827f0b35f98a762abed451918649ecf51
    
    Change-Id: Icb9f4bca897daf3fb7ce78fc06b3002fc7288fca
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03359784
    Feature: CDMA2000 1xRTT & EVDO

--- drivers/misc/mediatek/eccci/port/port_cfg.c
+++ drivers/misc/mediatek/eccci/port/port_cfg.c
@@ -149,6 +149,8 @@ static struct port_t md1_ccci_ports[] = {
 		&char_port_ops, 25, "ccci_c2k_agps",},
 	{CCCI_XCAP_TX, CCCI_XCAP_RX, 1, 1, 0xFF, 0xFF, MD1_NORMAL_HIF, PORT_F_WITH_CHAR_NODE,
 		&char_port_ops, 26, "ccci_ss_xcap",},
+	{CCCI_BIP_TX, CCCI_BIP_RX, 1, 1, 0xFF, 0xFF, MD1_NORMAL_HIF, PORT_F_WITH_CHAR_NODE,
+		&char_port_ops, 27, "ccci_bip",},
 
 /* IPC char port minor= minor idx + CCCI_IPC_MINOR_BASE(100) */
 	{CCCI_IPC_TX, CCCI_IPC_RX, 1, 1, 0xFF, 0xFF, MD1_NORMAL_HIF, PORT_F_WITH_CHAR_NODE,
