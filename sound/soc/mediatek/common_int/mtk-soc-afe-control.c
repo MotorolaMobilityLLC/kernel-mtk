c1afa5e517cf9121b81409e2d8202e9a6a5c9fbe
rita.chan <rita.chan@mediatek.com>
2017-09-01 01:42:29 +0800

    [ALPS03355273] audio: Fix echo ref noise issue
    [Detail]
    The word_size_align() will reduce the size of copy_to_user, it
    will cause the periodic noise
    
    [Solution]
    Dont do the word_size_align() in mtk_mem_ulblk_copy, the pointer
    alignment already applied in interrupt handler.
    
    MTK-Commit-Id: 368687bd8dacf517829895912af6a5bda4c8ff91
    
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    Feature: MagiVoIP
    Change-Id: Ibc036d6560497a39eff897662362d6a37ee19d9b
    CR-Id: ALPS03355273

--- sound/soc/mediatek/common_int/mtk-soc-afe-control.c
+++ sound/soc/mediatek/common_int/mtk-soc-afe-control.c
@@ -4398,7 +4398,7 @@ static int mtk_mem_ulblk_copy(struct snd_pcm_substream *substream,
 
 	PRINTK_AUD_UL1("mtk_capture_pcm_copy pos = %lucount = %lu\n ", pos, count);
 	/* get total bytes to copy */
-	count = word_size_align(audio_frame_to_bytes(substream, count));
+	count = audio_frame_to_bytes(substream, count);
 
 	/* check which memif nned to be write */
 	pVUL_MEM_ConTrol = pMemControl;
