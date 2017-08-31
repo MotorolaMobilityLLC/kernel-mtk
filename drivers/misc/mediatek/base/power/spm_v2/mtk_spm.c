2f6c0f1fab5f9d7b14add8ccb45f6d017c8c5a32
rita.chan <rita.chan@mediatek.com>
2017-09-01 01:24:23 +0800

    [ALPS02724026] SPM: fix build warning
    [Detail]
    Add __init tag for spm module initial function
    
    MTK-Commit-Id: 5d10cf591f6d8c20f19c868ad0baa6ac46b2ce3e
    
    Change-Id: Ieaca78a7e908657675554d31f065f673def778dd
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02724026
    Feature: SODI (Screen On Deep Idle)

--- drivers/misc/mediatek/base/power/spm_v2/mtk_spm.c
+++ drivers/misc/mediatek/base/power/spm_v2/mtk_spm.c
@@ -617,7 +617,7 @@ static void spm_register_init(void)
 	spin_unlock_irqrestore(&__spm_lock, flags);
 }
 
-int spm_module_init(void)
+int __init spm_module_init(void)
 {
 	int r = 0;
 #if defined(CONFIG_MACH_MT6757) || defined(CONFIG_MACH_KIBOPLUS)
