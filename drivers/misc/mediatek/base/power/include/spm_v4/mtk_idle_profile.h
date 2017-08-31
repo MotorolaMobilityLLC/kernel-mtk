6848863c01716eb741f7eda46766e8887be924a8
rita.chan <rita.chan@mediatek.com>
2017-09-01 01:44:31 +0800

    [ALPS03362118] idle: spm_v4: refine logging freq
    [Detail]
    -. refine logging freq to avoid kernel log too much
    
    MTK-Commit-Id: d278e3fe2d04a173597653de4da50260a723983b
    
    Change-Id: Ib00f11dedc8bb0fe5cf8d5110a7d6d60288bc592
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03362118
    Feature: System Performance
    (cherry picked from commit 8e42dfc5a841c0a0d27993c0ad9288cc1a50bcc7)

--- drivers/misc/mediatek/base/power/include/spm_v4/mtk_idle_profile.h
+++ drivers/misc/mediatek/base/power/include/spm_v4/mtk_idle_profile.h
@@ -67,12 +67,12 @@ bool mtk_idle_select_state(int type, int reason);
 void mtk_idle_block_setting(int type, unsigned long *cnt, unsigned long *block_cnt, unsigned int *block_mask);
 void mtk_idle_twam_init(void);
 
-static inline long int idle_get_current_time_ms(void)
+static inline u64 idle_get_current_time_ms(void)
 {
-	struct timeval t;
+	u64 idle_current_time = sched_clock();
 
-	do_gettimeofday(&t);
-	return ((t.tv_sec & 0xFFF) * 1000000 + t.tv_usec) / 1000;
+	do_div(idle_current_time, 1000000);
+	return idle_current_time;
 }
 
 void mtk_idle_recent_ratio_get(int *window_length_ms, struct mtk_idle_recent_ratio *ratio);
