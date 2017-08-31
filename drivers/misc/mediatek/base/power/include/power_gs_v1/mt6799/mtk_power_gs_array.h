f4bd2ac5f1108678ac40e473b09d91af42fee32b
rita.chan <rita.chan@mediatek.com>
2017-09-01 00:22:27 +0800

    [ALPS03137736] power_gs: update mt6799 low power golden setting
    [Detail]
    -. Update CG/DCM/PMIC power_gs
    
    MTK-Commit-Id: 1ba68a8a0b1b2242b08fab1fe74f6de0f3d80cbc
    
    Change-Id: Ib83a74807ac4afdc7082ba70ba74cb14dca9d43a
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03137736
    Feature: Power Management
    (cherry picked from commit 09b8622484089a0fc4d82378d5bd2c22e9f544f6)

--- drivers/misc/mediatek/base/power/include/power_gs_v1/mt6799/mtk_power_gs_array.h
+++ drivers/misc/mediatek/base/power/include/power_gs_v1/mt6799/mtk_power_gs_array.h
@@ -17,115 +17,228 @@
 extern void mt_power_gs_sp_dump(void);
 extern unsigned int golden_read_reg(unsigned int addr);
 
-extern const unsigned int *MT6335_PMIC_REG_gs_flightmode_suspend_mode;
+extern unsigned int *MT6335_PMIC_REG_gs_flightmode_suspend_mode_v2;
+extern unsigned int MT6335_PMIC_REG_gs_flightmode_suspend_mode_len_v2;
+
+extern unsigned int *MT6335_PMIC_REG_gs_flightmode_suspend_mode_32kless_v2;
+extern unsigned int MT6335_PMIC_REG_gs_flightmode_suspend_mode_32kless_len_v2;
+
+extern unsigned int *MT6335_PMIC_REG_gs_suspend_mode_v2;
+extern unsigned int MT6335_PMIC_REG_gs_suspend_mode_len_v2;
+
+extern unsigned int *MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_v2;
+extern unsigned int MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_len_v2;
+
+extern unsigned int *MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_32kless_v2;
+extern unsigned int MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_32kless_len_v2;
+
+extern unsigned int *MT6335_PMIC_REG_gs_sodi3P0_v2;
+extern unsigned int MT6335_PMIC_REG_gs_sodi3P0_len_v2;
+
+extern unsigned int *MT6335_PMIC_REG_gs_sodi3P0_32kless_v2;
+extern unsigned int MT6335_PMIC_REG_gs_sodi3P0_32kless_len_v2;
+
+extern unsigned int *CG_Golden_Setting_tcl_gs_sodi_v2;
+extern unsigned int CG_Golden_Setting_tcl_gs_sodi_len_v2;
+
+extern unsigned int *CG_Golden_Setting_tcl_gs_dpidle_v2;
+extern unsigned int CG_Golden_Setting_tcl_gs_dpidle_len_v2;
+
+extern unsigned int *CG_Golden_Setting_tcl_gs_suspend_v2;
+extern unsigned int CG_Golden_Setting_tcl_gs_suspend_len_v2;
+
+extern unsigned int *CG_Golden_Setting_tcl_gs_vp_v2;
+extern unsigned int CG_Golden_Setting_tcl_gs_vp_len_v2;
+
+extern unsigned int *CG_Golden_Setting_tcl_gs_topck_name_v2;
+extern unsigned int CG_Golden_Setting_tcl_gs_topck_name_len_v2;
+
+extern unsigned int *CG_Golden_Setting_tcl_gs_paging_v2;
+extern unsigned int CG_Golden_Setting_tcl_gs_paging_len_v2;
+
+extern unsigned int *CG_Golden_Setting_tcl_gs_mp3_play_v2;
+extern unsigned int CG_Golden_Setting_tcl_gs_mp3_play_len_v2;
+
+extern unsigned int *CG_Golden_Setting_tcl_gs_pdn_ao_v2;
+extern unsigned int CG_Golden_Setting_tcl_gs_pdn_ao_len_v2;
+
+extern unsigned int *CG_Golden_Setting_tcl_gs_idle_v2;
+extern unsigned int CG_Golden_Setting_tcl_gs_idle_len_v2;
+
+extern unsigned int *CG_Golden_Setting_tcl_gs_clkon_v2;
+extern unsigned int CG_Golden_Setting_tcl_gs_clkon_len_v2;
+
+extern unsigned int *CG_Golden_Setting_tcl_gs_talk_v2;
+extern unsigned int CG_Golden_Setting_tcl_gs_talk_len_v2;
+
+extern unsigned int *CG_Golden_Setting_tcl_gs_connsys_v2;
+extern unsigned int CG_Golden_Setting_tcl_gs_connsys_len_v2;
+
+extern unsigned int *CG_Golden_Setting_tcl_gs_datalink_v2;
+extern unsigned int CG_Golden_Setting_tcl_gs_datalink_len_v2;
+
+extern unsigned int *CG_Golden_Setting_tcl_gs_vr_v2;
+extern unsigned int CG_Golden_Setting_tcl_gs_vr_len_v2;
+
+extern unsigned int *CG_Golden_Setting_tcl_gs_flight_v2;
+extern unsigned int CG_Golden_Setting_tcl_gs_flight_len_v2;
+
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_sodi_v2;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_sodi_len_v2;
+
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_dpidle_v2;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_dpidle_len_v2;
+
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_suspend_v2;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_suspend_len_v2;
+
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_vp_v2;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_vp_len_v2;
+
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_topck_name_v2;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_topck_name_len_v2;
+
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_paging_v2;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_paging_len_v2;
+
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_mp3_play_v2;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_mp3_play_len_v2;
+
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_pdn_ao_v2;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_pdn_ao_len_v2;
+
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_idle_v2;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_idle_len_v2;
+
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_talk_v2;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_talk_len_v2;
+
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_connsys_v2;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_connsys_len_v2;
+
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_dcm_off_v2;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_dcm_off_len_v2;
+
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_datalink_v2;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_datalink_len_v2;
+
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_vr_v2;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_vr_len_v2;
+
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_flight_v2;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_flight_len_v2;
+
+/* --- v2 end --- */
+
+extern unsigned int *MT6335_PMIC_REG_gs_flightmode_suspend_mode;
 extern unsigned int MT6335_PMIC_REG_gs_flightmode_suspend_mode_len;
 
-extern const unsigned int *MT6335_PMIC_REG_gs_flightmode_suspend_mode_32kless;
+extern unsigned int *MT6335_PMIC_REG_gs_flightmode_suspend_mode_32kless;
 extern unsigned int MT6335_PMIC_REG_gs_flightmode_suspend_mode_32kless_len;
 
-extern const unsigned int *MT6335_PMIC_REG_gs_suspend_mode;
+extern unsigned int *MT6335_PMIC_REG_gs_suspend_mode;
 extern unsigned int MT6335_PMIC_REG_gs_suspend_mode_len;
 
-extern const unsigned int *MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode;
+extern unsigned int *MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode;
 extern unsigned int MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_len;
 
-extern const unsigned int *MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_32kless;
+extern unsigned int *MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_32kless;
 extern unsigned int MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_32kless_len;
 
-extern const unsigned int *MT6335_PMIC_REG_gs_sodi3P0;
+extern unsigned int *MT6335_PMIC_REG_gs_sodi3P0;
 extern unsigned int MT6335_PMIC_REG_gs_sodi3P0_len;
 
-extern const unsigned int *MT6335_PMIC_REG_gs_sodi3P0_32kless;
+extern unsigned int *MT6335_PMIC_REG_gs_sodi3P0_32kless;
 extern unsigned int MT6335_PMIC_REG_gs_sodi3P0_32kless_len;
 
-extern const unsigned int *CG_Golden_Setting_tcl_gs_sodi;
+extern unsigned int *CG_Golden_Setting_tcl_gs_sodi;
 extern unsigned int CG_Golden_Setting_tcl_gs_sodi_len;
 
-extern const unsigned int *CG_Golden_Setting_tcl_gs_dpidle;
+extern unsigned int *CG_Golden_Setting_tcl_gs_dpidle;
 extern unsigned int CG_Golden_Setting_tcl_gs_dpidle_len;
 
-extern const unsigned int *CG_Golden_Setting_tcl_gs_suspend;
+extern unsigned int *CG_Golden_Setting_tcl_gs_suspend;
 extern unsigned int CG_Golden_Setting_tcl_gs_suspend_len;
 
-extern const unsigned int *CG_Golden_Setting_tcl_gs_vp;
+extern unsigned int *CG_Golden_Setting_tcl_gs_vp;
 extern unsigned int CG_Golden_Setting_tcl_gs_vp_len;
 
-extern const unsigned int *CG_Golden_Setting_tcl_gs_topck_name;
+extern unsigned int *CG_Golden_Setting_tcl_gs_topck_name;
 extern unsigned int CG_Golden_Setting_tcl_gs_topck_name_len;
 
-extern const unsigned int *CG_Golden_Setting_tcl_gs_paging;
+extern unsigned int *CG_Golden_Setting_tcl_gs_paging;
 extern unsigned int CG_Golden_Setting_tcl_gs_paging_len;
 
-extern const unsigned int *CG_Golden_Setting_tcl_gs_mp3_play;
+extern unsigned int *CG_Golden_Setting_tcl_gs_mp3_play;
 extern unsigned int CG_Golden_Setting_tcl_gs_mp3_play_len;
 
-extern const unsigned int *CG_Golden_Setting_tcl_gs_pdn_ao;
+extern unsigned int *CG_Golden_Setting_tcl_gs_pdn_ao;
 extern unsigned int CG_Golden_Setting_tcl_gs_pdn_ao_len;
 
-extern const unsigned int *CG_Golden_Setting_tcl_gs_idle;
+extern unsigned int *CG_Golden_Setting_tcl_gs_idle;
 extern unsigned int CG_Golden_Setting_tcl_gs_idle_len;
 
-extern const unsigned int *CG_Golden_Setting_tcl_gs_clkon;
+extern unsigned int *CG_Golden_Setting_tcl_gs_clkon;
 extern unsigned int CG_Golden_Setting_tcl_gs_clkon_len;
 
-extern const unsigned int *CG_Golden_Setting_tcl_gs_talk;
+extern unsigned int *CG_Golden_Setting_tcl_gs_talk;
 extern unsigned int CG_Golden_Setting_tcl_gs_talk_len;
 
-extern const unsigned int *CG_Golden_Setting_tcl_gs_connsys;
+extern unsigned int *CG_Golden_Setting_tcl_gs_connsys;
 extern unsigned int CG_Golden_Setting_tcl_gs_connsys_len;
 
-extern const unsigned int *CG_Golden_Setting_tcl_gs_datalink;
+extern unsigned int *CG_Golden_Setting_tcl_gs_datalink;
 extern unsigned int CG_Golden_Setting_tcl_gs_datalink_len;
 
-extern const unsigned int *CG_Golden_Setting_tcl_gs_vr;
+extern unsigned int *CG_Golden_Setting_tcl_gs_vr;
 extern unsigned int CG_Golden_Setting_tcl_gs_vr_len;
 
-extern const unsigned int *CG_Golden_Setting_tcl_gs_flight;
+extern unsigned int *CG_Golden_Setting_tcl_gs_flight;
 extern unsigned int CG_Golden_Setting_tcl_gs_flight_len;
 
-extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_sodi;
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_sodi;
 extern unsigned int AP_DCM_Golden_Setting_tcl_gs_sodi_len;
 
-extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_dpidle;
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_dpidle;
 extern unsigned int AP_DCM_Golden_Setting_tcl_gs_dpidle_len;
 
-extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_suspend;
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_suspend;
 extern unsigned int AP_DCM_Golden_Setting_tcl_gs_suspend_len;
 
-extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_vp;
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_vp;
 extern unsigned int AP_DCM_Golden_Setting_tcl_gs_vp_len;
 
-extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_topck_name;
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_topck_name;
 extern unsigned int AP_DCM_Golden_Setting_tcl_gs_topck_name_len;
 
-extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_paging;
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_paging;
 extern unsigned int AP_DCM_Golden_Setting_tcl_gs_paging_len;
 
-extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_mp3_play;
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_mp3_play;
 extern unsigned int AP_DCM_Golden_Setting_tcl_gs_mp3_play_len;
 
-extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_pdn_ao;
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_pdn_ao;
 extern unsigned int AP_DCM_Golden_Setting_tcl_gs_pdn_ao_len;
 
-extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_idle;
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_idle;
 extern unsigned int AP_DCM_Golden_Setting_tcl_gs_idle_len;
 
-extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_talk;
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_talk;
 extern unsigned int AP_DCM_Golden_Setting_tcl_gs_talk_len;
 
-extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_connsys;
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_connsys;
 extern unsigned int AP_DCM_Golden_Setting_tcl_gs_connsys_len;
 
-extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_dcm_off;
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_dcm_off;
 extern unsigned int AP_DCM_Golden_Setting_tcl_gs_dcm_off_len;
 
-extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_datalink;
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_datalink;
 extern unsigned int AP_DCM_Golden_Setting_tcl_gs_datalink_len;
 
-extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_vr;
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_vr;
 extern unsigned int AP_DCM_Golden_Setting_tcl_gs_vr_len;
 
-extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_flight;
+extern unsigned int *AP_DCM_Golden_Setting_tcl_gs_flight;
 extern unsigned int AP_DCM_Golden_Setting_tcl_gs_flight_len;
 
 #endif
--- drivers/misc/mediatek/base/power/power_gs_v1/mt6799/mtk_power_gs_compare.c
+++ drivers/misc/mediatek/base/power/power_gs_v1/mt6799/mtk_power_gs_compare.c
@@ -13,57 +13,131 @@
 
 #include "mtk_power_gs.h"
 #include "mtk_power_gs_array.h"
-#include "mt-plat/mtk_rtc.h"
+#include <mt-plat/mtk_rtc.h>
+#include <mt-plat/mtk_chip.h>
+
+static unsigned int chip_sw_ver;
+
+static unsigned int *cg_suspend;
+static unsigned int cg_suspend_len;
+static unsigned int *dcm_suspend;
+static unsigned int dcm_suspend_len;
+
+static unsigned int *cg_sodi;
+static unsigned int cg_sodi_len;
+static unsigned int *dcm_sodi;
+static unsigned int dcm_sodi_len;
+
+static unsigned int *cg_dpidle;
+static unsigned int cg_dpidle_len;
+static unsigned int *dcm_dpidle;
+static unsigned int dcm_dpidle_len;
+
+static unsigned int *pmic_suspend;
+static unsigned int pmic_suspend_len;
+static unsigned int *pmic_suspend_32kless;
+static unsigned int pmic_suspend_32kless_len;
+
+static unsigned int *pmic_sodi;
+static unsigned int pmic_sodi_len;
+static unsigned int *pmic_sodi_32kless;
+static unsigned int pmic_sodi_32kless_len;
+
+static unsigned int *pmic_dpidle;
+static unsigned int pmic_dpidle_len;
+static unsigned int *pmic_dpidle_32kless;
+static unsigned int pmic_dpidle_32kless_len;
+
+void mt_power_gs_internal_init(void)
+{
+	chip_sw_ver = mt_get_chip_sw_ver();
+
+	if (chip_sw_ver >= CHIP_SW_VER_02) {
+		cg_suspend = CG_Golden_Setting_tcl_gs_suspend_v2;
+		cg_suspend_len = CG_Golden_Setting_tcl_gs_suspend_len_v2;
+		dcm_suspend = AP_DCM_Golden_Setting_tcl_gs_suspend_v2;
+		dcm_suspend_len = AP_DCM_Golden_Setting_tcl_gs_suspend_len_v2;
+		pmic_suspend = MT6335_PMIC_REG_gs_flightmode_suspend_mode_v2;
+		pmic_suspend_len = MT6335_PMIC_REG_gs_flightmode_suspend_mode_len_v2;
+		pmic_suspend_32kless = MT6335_PMIC_REG_gs_flightmode_suspend_mode_32kless_v2;
+		pmic_suspend_32kless_len = MT6335_PMIC_REG_gs_flightmode_suspend_mode_32kless_len_v2;
+		cg_sodi = CG_Golden_Setting_tcl_gs_sodi_v2;
+		cg_sodi_len = CG_Golden_Setting_tcl_gs_sodi_len_v2;
+		dcm_sodi = AP_DCM_Golden_Setting_tcl_gs_sodi_v2;
+		dcm_sodi_len = AP_DCM_Golden_Setting_tcl_gs_sodi_len_v2;
+		pmic_sodi = MT6335_PMIC_REG_gs_sodi3P0_v2;
+		pmic_sodi_len = MT6335_PMIC_REG_gs_sodi3P0_len_v2;
+		pmic_sodi_32kless = MT6335_PMIC_REG_gs_sodi3P0_32kless_v2;
+		pmic_sodi_32kless_len = MT6335_PMIC_REG_gs_sodi3P0_32kless_len_v2;
+		cg_dpidle = CG_Golden_Setting_tcl_gs_dpidle_v2;
+		cg_dpidle_len = CG_Golden_Setting_tcl_gs_dpidle_len_v2;
+		dcm_dpidle = AP_DCM_Golden_Setting_tcl_gs_dpidle_v2;
+		dcm_dpidle_len = AP_DCM_Golden_Setting_tcl_gs_dpidle_len_v2;
+		pmic_dpidle = MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_v2;
+		pmic_dpidle_len = MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_len_v2;
+		pmic_dpidle_32kless = MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_32kless_v2;
+		pmic_dpidle_32kless_len = MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_32kless_len_v2;
+	} else {
+		cg_suspend = CG_Golden_Setting_tcl_gs_suspend;
+		cg_suspend_len = CG_Golden_Setting_tcl_gs_suspend_len;
+		dcm_suspend = AP_DCM_Golden_Setting_tcl_gs_suspend;
+		dcm_suspend_len = AP_DCM_Golden_Setting_tcl_gs_suspend_len;
+		pmic_suspend = MT6335_PMIC_REG_gs_flightmode_suspend_mode;
+		pmic_suspend_len = MT6335_PMIC_REG_gs_flightmode_suspend_mode_len;
+		pmic_suspend_32kless = MT6335_PMIC_REG_gs_flightmode_suspend_mode_32kless;
+		pmic_suspend_32kless_len = MT6335_PMIC_REG_gs_flightmode_suspend_mode_32kless_len;
+		cg_sodi = CG_Golden_Setting_tcl_gs_sodi;
+		cg_sodi_len = CG_Golden_Setting_tcl_gs_sodi_len;
+		dcm_sodi = AP_DCM_Golden_Setting_tcl_gs_sodi;
+		dcm_sodi_len = AP_DCM_Golden_Setting_tcl_gs_sodi_len;
+		pmic_sodi = MT6335_PMIC_REG_gs_sodi3P0;
+		pmic_sodi_len = MT6335_PMIC_REG_gs_sodi3P0_len;
+		pmic_sodi_32kless = MT6335_PMIC_REG_gs_sodi3P0_32kless;
+		pmic_sodi_32kless_len = MT6335_PMIC_REG_gs_sodi3P0_32kless_len;
+		cg_dpidle = CG_Golden_Setting_tcl_gs_dpidle;
+		cg_dpidle_len = CG_Golden_Setting_tcl_gs_dpidle_len;
+		dcm_dpidle = AP_DCM_Golden_Setting_tcl_gs_dpidle;
+		dcm_dpidle_len = AP_DCM_Golden_Setting_tcl_gs_dpidle_len;
+		pmic_dpidle = MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode;
+		pmic_dpidle_len = MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_len;
+		pmic_dpidle_32kless = MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_32kless;
+		pmic_dpidle_32kless_len = MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_32kless_len;
+	}
+}
 
 void mt_power_gs_table_init(void)
 {
-	mt_power_gs_base_remap_init("Suspend ", "CG  ",
-			    CG_Golden_Setting_tcl_gs_suspend,
-			    CG_Golden_Setting_tcl_gs_suspend_len);
-	mt_power_gs_base_remap_init("Suspend ", "DCM ",
-			    AP_DCM_Golden_Setting_tcl_gs_suspend,
-			    AP_DCM_Golden_Setting_tcl_gs_suspend_len);
-	mt_power_gs_base_remap_init("DPIdle ", "CG  ",
-			    CG_Golden_Setting_tcl_gs_dpidle,
-			    CG_Golden_Setting_tcl_gs_dpidle_len);
-	mt_power_gs_base_remap_init("DPIdle ", "DCM ",
-			    AP_DCM_Golden_Setting_tcl_gs_dpidle,
-			    AP_DCM_Golden_Setting_tcl_gs_dpidle_len);
-	mt_power_gs_base_remap_init("SODI ", "CG  ",
-			    CG_Golden_Setting_tcl_gs_sodi,
-			    CG_Golden_Setting_tcl_gs_sodi_len);
-	mt_power_gs_base_remap_init("SODI ", "DCM ",
-			    AP_DCM_Golden_Setting_tcl_gs_sodi,
-			    AP_DCM_Golden_Setting_tcl_gs_sodi_len);
+	mt_power_gs_base_remap_init("Suspend ", "CG  ", cg_suspend, cg_suspend_len);
+	mt_power_gs_base_remap_init("Suspend ", "DCM ", dcm_suspend, dcm_suspend_len);
+	mt_power_gs_base_remap_init("DPIdle ", "CG  ", cg_dpidle, cg_dpidle_len);
+	mt_power_gs_base_remap_init("DPIdle ", "DCM ", dcm_dpidle, dcm_dpidle_len);
+	mt_power_gs_base_remap_init("SODI ", "CG  ", cg_sodi, cg_sodi_len);
+	mt_power_gs_base_remap_init("SODI ", "DCM ", dcm_sodi, dcm_sodi_len);
 }
 
 void mt_power_gs_suspend_compare(unsigned int dump_flag)
 {
+	pr_warn("Power_gs: ver_%d\n", chip_sw_ver);
+
 	if (dump_flag & GS_PMIC) {
 		if (crystal_exist_status() == true) {
 			/* 32k */
-			mt_power_gs_compare("Suspend ", "6335",
-				    MT6335_PMIC_REG_gs_flightmode_suspend_mode,
-				    MT6335_PMIC_REG_gs_flightmode_suspend_mode_len);
+			mt_power_gs_compare("Suspend ", "6335", pmic_suspend, pmic_suspend_len);
 		} else {
 			/* 32k-less */
 			pr_warn("Power_gs: %s in 32k-less\n", __func__);
 			mt_power_gs_compare("Suspend ", "6335",
-				    MT6335_PMIC_REG_gs_flightmode_suspend_mode_32kless,
-				    MT6335_PMIC_REG_gs_flightmode_suspend_mode_32kless_len);
+				    pmic_suspend_32kless,
+				    pmic_suspend_32kless_len);
 		}
 	}
 
 	if (dump_flag & GS_CG) {
-		mt_power_gs_compare("Suspend ", "CG  ",
-				    CG_Golden_Setting_tcl_gs_suspend,
-				    CG_Golden_Setting_tcl_gs_suspend_len);
+		mt_power_gs_compare("Suspend ", "CG  ", cg_suspend, cg_suspend_len);
 	}
 
 	if (dump_flag & GS_DCM) {
-		mt_power_gs_compare("Suspend ", "DCM ",
-				    AP_DCM_Golden_Setting_tcl_gs_suspend,
-				    AP_DCM_Golden_Setting_tcl_gs_suspend_len);
+		mt_power_gs_compare("Suspend ", "DCM ", dcm_suspend, dcm_suspend_len);
 	}
 
 	mt_power_gs_sp_dump();
@@ -71,31 +145,27 @@ void mt_power_gs_suspend_compare(unsigned int dump_flag)
 
 void mt_power_gs_dpidle_compare(unsigned int dump_flag)
 {
+	pr_warn("Power_gs: ver_%d\n", chip_sw_ver);
+
 	if (dump_flag & GS_PMIC) {
 		if (crystal_exist_status() == true) {
 			/* 32k */
-			mt_power_gs_compare("DPIdle  ", "6335",
-				    MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode,
-				    MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_len);
+			mt_power_gs_compare("DPIdle  ", "6335", pmic_dpidle, pmic_dpidle_len);
 		} else {
 			/* 32k-less */
 			pr_warn("Power_gs: %s in 32k-less\n", __func__);
 			mt_power_gs_compare("DPIdle  ", "6335",
-				    MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_32kless,
-				    MT6335_PMIC_REG_gs_early_suspend_deep_idle_mode_32kless_len);
+				    pmic_dpidle_32kless,
+				    pmic_dpidle_32kless_len);
 		}
 	}
 
 	if (dump_flag & GS_CG) {
-		mt_power_gs_compare("DPIdle ", "CG  ",
-				    CG_Golden_Setting_tcl_gs_dpidle,
-				    CG_Golden_Setting_tcl_gs_dpidle_len);
+		mt_power_gs_compare("DPIdle ", "CG  ", cg_dpidle, cg_dpidle_len);
 	}
 
 	if (dump_flag & GS_DCM) {
-		mt_power_gs_compare("DPIdle ", "DCM ",
-				    AP_DCM_Golden_Setting_tcl_gs_dpidle,
-				    AP_DCM_Golden_Setting_tcl_gs_dpidle_len);
+		mt_power_gs_compare("DPIdle ", "DCM ", dcm_dpidle, dcm_dpidle_len);
 	}
 
 	mt_power_gs_sp_dump();
@@ -103,31 +173,27 @@ void mt_power_gs_dpidle_compare(unsigned int dump_flag)
 
 void mt_power_gs_sodi_compare(unsigned int dump_flag)
 {
+	pr_warn("Power_gs: ver_%d\n", chip_sw_ver);
+
 	if (dump_flag & GS_PMIC) {
 		if (crystal_exist_status() == true) {
 			/* 32k */
-			mt_power_gs_compare("SODI  ", "6335",
-				    MT6335_PMIC_REG_gs_sodi3P0,
-				    MT6335_PMIC_REG_gs_sodi3P0_len);
+			mt_power_gs_compare("SODI  ", "6335", pmic_sodi, pmic_sodi_len);
 		} else {
 			/* 32k-less */
 			pr_warn("Power_gs: %s in 32k-less\n", __func__);
 			mt_power_gs_compare("SODI  ", "6335",
-				    MT6335_PMIC_REG_gs_sodi3P0_32kless,
-				    MT6335_PMIC_REG_gs_sodi3P0_32kless_len);
+				    pmic_sodi_32kless,
+				    pmic_sodi_32kless_len);
 		}
 	}
 
 	if (dump_flag & GS_CG) {
-		mt_power_gs_compare("SODI ", "CG  ",
-				    CG_Golden_Setting_tcl_gs_sodi,
-				    CG_Golden_Setting_tcl_gs_sodi_len);
+		mt_power_gs_compare("SODI ", "CG  ", cg_sodi, cg_sodi_len);
 	}
 
 	if (dump_flag & GS_DCM) {
-		mt_power_gs_compare("SODI ", "DCM ",
-				    AP_DCM_Golden_Setting_tcl_gs_sodi,
-				    AP_DCM_Golden_Setting_tcl_gs_sodi_len);
+		mt_power_gs_compare("SODI ", "DCM ", dcm_sodi, dcm_sodi_len);
 	}
 
 	mt_power_gs_sp_dump();
