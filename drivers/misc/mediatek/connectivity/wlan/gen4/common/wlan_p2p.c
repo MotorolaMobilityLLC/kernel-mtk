860d58822c7b04ae98297a2289eebb34072f0b3f
rita.chan <rita.chan@mediatek.com>
2017-08-31 23:00:58 +0800

    [ALPS02395187] wlan: wifi report roaming result fail
    [Detail]
      mt6632 patch back
      Handle dead code for wlan_p2p.c
    [Solution]
    
    MTK-Commit-Id: 7d0affea0ecb2cbac22501fcebce61a4e05258bc
    
    Change-Id: Iafc073e23628bb6822e8e958857ad631286b2231
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02395187
    Feature: wifi

--- drivers/misc/mediatek/connectivity/wlan/gen4/common/wlan_p2p.c
+++ drivers/misc/mediatek/connectivity/wlan/gen4/common/wlan_p2p.c
@@ -962,12 +962,12 @@ wlanoidGetP2PSDRequest(IN P_ADAPTER_T prAdapter,
 		       IN PVOID pvQueryBuffer, IN UINT_32 u4QueryBufferLen, OUT PUINT_32 pu4QueryInfoLen)
 {
 	WLAN_STATUS rWlanStatus = WLAN_STATUS_SUCCESS;
-	PUINT_8 pucPacketBuffer = NULL, pucTA = NULL;
-/* PUINT_8 pucChannelNum = NULL; */
-	PUINT_16 pu2PacketLength = NULL;
-	P_WLAN_MAC_HEADER_T prWlanHdr = (P_WLAN_MAC_HEADER_T) NULL;
-	UINT_8 ucVersionNum = 0;
-/* UINT_8 ucChannelNum = 0, ucSeqNum = 0; */
+	/*PUINT_8 pucPacketBuffer = NULL, pucTA = NULL;*/
+	/* PUINT_8 pucChannelNum = NULL; */
+	/*PUINT_16 pu2PacketLength = NULL;*/
+	/*P_WLAN_MAC_HEADER_T prWlanHdr = (P_WLAN_MAC_HEADER_T) NULL;*/
+	/*UINT_8 ucVersionNum = 0;*/
+	/* UINT_8 ucChannelNum = 0, ucSeqNum = 0; */
 
 	ASSERT(prAdapter);
 	ASSERT(pu4QueryInfoLen);
@@ -1009,7 +1009,7 @@ wlanoidGetP2PSDRequest(IN P_ADAPTER_T prAdapter,
 	*pu4QueryInfoLen = 0;
 	return rWlanStatus;
 #endif
-
+/*
 	prWlanHdr = (P_WLAN_MAC_HEADER_T) pucPacketBuffer;
 
 	kalMemCopy(pucTA, prWlanHdr->aucAddr2, MAC_ADDR_LEN);
@@ -1023,6 +1023,7 @@ wlanoidGetP2PSDRequest(IN P_ADAPTER_T prAdapter,
 	}
 
 	return rWlanStatus;
+*/
 }				/* end of wlanoidGetP2PSDRequest() */
 
 /*----------------------------------------------------------------------------*/
@@ -1049,11 +1050,11 @@ wlanoidGetP2PSDResponse(IN P_ADAPTER_T prAdapter,
 			IN PVOID pvQueryBuffer, IN UINT_32 u4QueryBufferLen, OUT PUINT_32 pu4QueryInfoLen)
 {
 	WLAN_STATUS rWlanStatus = WLAN_STATUS_SUCCESS;
-	P_WLAN_MAC_HEADER_T prWlanHdr = (P_WLAN_MAC_HEADER_T) NULL;
+	/*P_WLAN_MAC_HEADER_T prWlanHdr = (P_WLAN_MAC_HEADER_T) NULL;*/
 	/* UINT_8 ucSeqNum = 0, */
-	UINT_8 ucVersionNum = 0;
-	PUINT_8 pucPacketContent = (PUINT_8) NULL, pucTA = (PUINT_8) NULL;
-	PUINT_16 pu2PacketLength = (PUINT_16) NULL;
+	/*UINT_8 ucVersionNum = 0;*/
+	/*PUINT_8 pucPacketContent = (PUINT_8) NULL, pucTA = (PUINT_8) NULL;*/
+	/*PUINT_16 pu2PacketLength = (PUINT_16) NULL;*/
 
 	ASSERT(prAdapter);
 	ASSERT(pu4QueryInfoLen);
@@ -1097,6 +1098,7 @@ wlanoidGetP2PSDResponse(IN P_ADAPTER_T prAdapter,
 	*pu4QueryInfoLen = 0;
 	return rWlanStatus;
 #endif
+/*
 	prWlanHdr = (P_WLAN_MAC_HEADER_T) pucPacketContent;
 
 	kalMemCopy(pucTA, prWlanHdr->aucAddr2, MAC_ADDR_LEN);
@@ -1109,6 +1111,7 @@ wlanoidGetP2PSDResponse(IN P_ADAPTER_T prAdapter,
 	}
 
 	return rWlanStatus;
+*/
 }				/* end of wlanoidGetP2PSDResponse() */
 
 /*----------------------------------------------------------------------------*/
@@ -1463,10 +1466,10 @@ wlanoidQueryP2pOpChannel(IN P_ADAPTER_T prAdapter,
 		rResult = WLAN_STATUS_INVALID_DATA;
 		break;
 #endif
-
+/*
 		*pu4QueryInfoLen = sizeof(UINT_8);
 		rResult = WLAN_STATUS_SUCCESS;
-
+*/
 	} while (FALSE);
 
 	return rResult;
