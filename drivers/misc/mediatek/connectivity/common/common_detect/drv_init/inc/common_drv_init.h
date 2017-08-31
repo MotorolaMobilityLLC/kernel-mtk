df8a02feca28cb98a36ef34471dea7eed671c82c
rita.chan <rita.chan@mediatek.com>
2017-08-31 22:25:25 +0800

    [ALPS02694558] wmt:common part drvier structure refactor
    [Detail] Unify combo and conn_soc common driver to one driver
    
    [Solution]
    1.wmt driver use a new folder structure named comm_main in android N and
    later version
    2.delete wmt_stp_exp.c, all of function in it be removed in wmt_exp.c
    and stp_exp.c
    
    MTK-Commit-Id: c963c9f11a210c0ba059ab299bd9165417f98d15
    
    Change-Id: I20d90e59ed006ba0b6d1117f657480516f3a5483
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02694558
    Feature: MTK BT Solution(*)

--- drivers/misc/mediatek/connectivity/common/common_detect/drv_init/inc/common_drv_init.h
+++ drivers/misc/mediatek/connectivity/common/common_detect/drv_init/inc/common_drv_init.h
@@ -16,16 +16,10 @@
 #define _COMMON_DRV_INIT_H_
 extern int do_common_drv_init(int chip_id);
 
-/*defined in common part driver*/
-#ifdef MTK_WCN_COMBO_CHIP_SUPPORT
-extern int mtk_wcn_combo_common_drv_init(void);
+extern int mtk_wcn_common_drv_init(void);
 extern int mtk_wcn_hif_sdio_drv_init(void);
 extern int mtk_wcn_stp_uart_drv_init(void);
 extern int mtk_wcn_stp_sdio_drv_init(void);
-#endif
 
-#ifdef MTK_WCN_SOC_CHIP_SUPPORT
-extern int mtk_wcn_soc_common_drv_init(void);
-#endif
 
 #endif
