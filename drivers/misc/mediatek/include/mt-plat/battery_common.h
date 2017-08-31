4481e45e23a5e5ff11254ec9c047f56cb264f867
rita.chan <rita.chan@mediatek.com>
2017-09-01 01:28:17 +0800

    [ALPS03321171] battery: fix incorrect UI_SOC
    [Detail]
    when use CDP adapter , UI_SOC will show 50%
    at begin .
    init UI_SOC before CDP polling
    
    [Solution]
    
    MTK-Commit-Id: 968074d4d32d93ad3a2a07a449997f480a2826c8
    
    Change-Id: I88a2b37e29334f0ffc3d209ec498cfe158ecdece
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03321171
    Feature: Fuel Gauge

--- drivers/misc/mediatek/include/mt-plat/battery_common.h
+++ drivers/misc/mediatek/include/mt-plat/battery_common.h
@@ -381,7 +381,7 @@ extern PMU_STATUS do_jeita_state_machine(void);
 #ifdef CONFIG_MTK_POWER_EXT_DETECT
 extern kal_bool bat_is_ext_power(void);
 #endif
-
+extern signed int gFG_capacity_by_c;
 extern int g_platform_boot_mode;
 extern bool mt_usb_is_device(void);
 #if defined(CONFIG_USB_MTK_HDRC) || defined(CONFIG_USB_MU3D_DRV)
