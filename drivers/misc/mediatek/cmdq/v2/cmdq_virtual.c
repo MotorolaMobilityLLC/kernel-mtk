ffe3b756d239dd7f41c2ff8c9cb9f9b38e69c8e8
rita.chan <rita.chan@mediatek.com>
2017-08-31 22:08:50 +0800

    [ALPS02764967] CMDQ: 8167 cmdq driver
    [Detail]
    add 8167 cmdq driver for display driver early porting
    
    MTK-Commit-Id: 133b339ea311babf2b9dcff85b2e81a0a497b397
    
    Change-Id: I85c2574095366732cbae3b3328269adbe2f14790
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02764967
    Feature: Driver

--- drivers/misc/mediatek/cmdq/v2/cmdq_virtual.c
+++ drivers/misc/mediatek/cmdq/v2/cmdq_virtual.c
@@ -887,7 +887,7 @@ int cmdq_virtual_dump_smi(const int showSmiDump)
 {
 	int isSMIHang = 0;
 
-#if defined(CMDQ_CONFIG_SMI) && !defined(CONFIG_MTK_FPGA)
+#if defined(CMDQ_CONFIG_SMI) && !defined(CONFIG_MTK_FPGA) && !defined(CONFIG_MTK_SMI_VARIANT)
 	isSMIHang =
 	    smi_debug_bus_hanging_detect_ext(SMI_DBG_DISPSYS | SMI_DBG_VDEC | SMI_DBG_IMGSYS |
 					     SMI_DBG_VENC | SMI_DBG_MJC, showSmiDump, showSmiDump);
