b628049daef854b3958817d76db70de9f2a10be8
rita.chan <rita.chan@mediatek.com>
2017-08-31 21:16:20 +0800

    [ALPS02677544] arm64: cpuinfo: backward-compatibility support
    [Detail]
    Imporve backward-compatibility for thrid-party applications
    by patching the "Hardware", "Processor", and "model name"
    information in /proc/cpuinfo.
    
    Besides, this change also add a message to notify that some
    process reads /proc/cpuinfo as a hint for debugging.
    
    MTK-Commit-Id: 1e83211bf9475437951db04629f8f08c477b90ae
    
    Change-Id: Ia3f7c5f8e20bd551262d7e876d300497b67d1f22
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02677544
    Feature: Others

--- arch/arm64/kernel/cpuinfo.c
+++ arch/arm64/kernel/cpuinfo.c
@@ -19,6 +19,7 @@
 #include <asm/cpu.h>
 #include <asm/cputype.h>
 #include <asm/cpufeature.h>
+#include <asm/elf.h>
 
 #include <linux/bitops.h>
 #include <linux/bug.h>
@@ -48,6 +49,9 @@ static char *icache_policy_str[] = {
 };
 
 unsigned long __icache_flags;
+
+/* machine descriptor for arm64 device */
+static const char *machine_desc_str;
 
 static const char *const hwcap_str[] = {
 	"fp",
@@ -98,9 +102,18 @@ static const char *const compat_hwcap2_str[] = {
 };
 #endif /* CONFIG_COMPAT */
 
+/* setup machine descriptor */
+void machine_desc_set(const char *str)
+{
+	machine_desc_str = str;
+}
+
 static int c_show(struct seq_file *m, void *v)
 {
 	int i, j;
+
+	/* a hint message to notify that some process reads /proc/cpuinfo */
+	pr_err("Dump cpuinfo\n");
 
 	for_each_online_cpu(i) {
 		struct cpuinfo_arm64 *cpuinfo = &per_cpu(cpu_data, i);
@@ -112,6 +125,14 @@ static int c_show(struct seq_file *m, void *v)
 		 * "processor".  Give glibc what it expects.
 		 */
 		seq_printf(m, "processor\t: %d\n", i);
+
+		/*
+		 * backward-compatibility for thrid-party applications:
+		 * Since the cpu_info->cpu_name is deprecated, print "AArch64 Processor" instead
+		 * (As the defined string in arch/arm64/kernel/cputable.c for legacy kernel)
+		 */
+		seq_printf(m, "Processor\t: AArch64 Processor rev %d (%s)\n", MIDR_REVISION(midr), ELF_PLATFORM);
+		seq_printf(m, "model name\t: AArch64 Processor rev %d (%s)\n", MIDR_REVISION(midr), ELF_PLATFORM);
 
 		seq_printf(m, "BogoMIPS\t: %lu.%02lu\n",
 			   loops_per_jiffy / (500000UL/HZ),
@@ -148,6 +169,9 @@ static int c_show(struct seq_file *m, void *v)
 		seq_printf(m, "CPU part\t: 0x%03x\n", MIDR_PARTNUM(midr));
 		seq_printf(m, "CPU revision\t: %d\n\n", MIDR_REVISION(midr));
 	}
+
+	/* backward-compatibility for thrid-party applications */
+	seq_printf(m, "Hardware\t: %s\n", machine_desc_str);
 
 	return 0;
 }
