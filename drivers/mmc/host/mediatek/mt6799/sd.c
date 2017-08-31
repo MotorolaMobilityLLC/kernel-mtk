031db58cbbbde32eb53cb92eaee16d3f0579ef6d
rita.chan <rita.chan@mediatek.com>
2017-08-31 23:52:42 +0800

    [ALPS03013890] SDIO : fix sdc busy sanity fail
    [Detail]
    When suspend/resume, 6632 will re-init, and sometimes fail because sdc
    keep busy.
    
    [Solution]
    Init 6632 use low speed mode, so that HW DVFS cannot enable.
    If HW DVFS enable in low speed mode will casue DVFS timeout and CMD CRC.
    So Init 6632 must disable HW DVFS. But when DVFS disable, sdc may busy
    wait dvfs request pull down and cause problem. Driver need check sdc busy
    again to make sure sdc not busy after disable HW DVFS.
    
    MTK-Commit-Id: 720b10cefce73b39d03be8c3b8d9012c3c44af19
    
    Change-Id: I12126eb38cba8ad6bc91c5ba6eed118611e81d47
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03013890
    Feature: Others

--- drivers/mmc/host/mediatek/mt6799/sd.c
+++ drivers/mmc/host/mediatek/mt6799/sd.c
@@ -889,6 +889,19 @@ static void msdc_init_hw(struct msdc_host *host)
 	/* Disable HW DVFS */
 	MSDC_SET_FIELD(MSDC_CFG, MSDC_CFG_DVFS_EN, 0);
 	MSDC_SET_FIELD(MSDC_CFG, MSDC_CFG_DVFS_HW, 0);
+	while (sdc_is_busy()) {
+		MSDC_SET_FIELD(MSDC_CFG, MSDC_CFG_DVFS_EN, 1);
+		MSDC_SET_FIELD(MSDC_CFG, MSDC_CFG_DVFS_HW, 1);
+
+		pr_err("%s: Wait sdc busy before stop DVFS!\n", __func__);
+		while (sdc_is_busy())
+			;
+		/* Reset */
+		msdc_reset_hw(host->id);
+
+		MSDC_SET_FIELD(MSDC_CFG, MSDC_CFG_DVFS_EN, 0);
+		MSDC_SET_FIELD(MSDC_CFG, MSDC_CFG_DVFS_HW, 0);
+	}
 
 	/* Reset */
 	msdc_reset_hw(host->id);
