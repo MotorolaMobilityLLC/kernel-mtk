f98898f2c8df5ecd3e298a1f18441c88d2a2b63d
rita.chan <rita.chan@mediatek.com>
2017-08-31 22:37:44 +0800

    [ALPS02721249] ISP: add copyright header
    [Detail] add MTK Copyright and GPL v2 Copyright disclaim
    
    [Solution] add copyright header
    
    MTK-Commit-Id: 42b1f8277ca2fad6247431355b9fdb3e498c92e9
    
    Change-Id: I00ee8ac0ad13917601532b48a75e461d408a45e6
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02721249
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/cameraisp/src/mt6757/camera_isp.c
+++ drivers/misc/mediatek/cameraisp/src/mt6757/camera_isp.c
@@ -1,7 +1,18 @@
+/*
+* Copyright (C) 2016 MediaTek Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+*/
+
 /******************************************************************************
- * camera_isp.c - MT6797 Linux ISP Device Driver
- *
- * Copyright 2008-2016 MediaTek Co.,Ltd.
+ * camera_isp.c - MT6757 Linux ISP Device Driver
  *
  * DESCRIPTION:
  *     This file provid the other drivers ISP relative functions
