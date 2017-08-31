21488a23ec5320a412645ddceaef55aa557bae59
rita.chan <rita.chan@mediatek.com>
2017-08-31 23:54:49 +0800

    [ALPS02853817] wlan: fix coverity CID:359940
    [Detail]
    Incorrect expression: Array compared against 0.
    [Solution]
    Modify to check if bssid is empty.
    
    MTK-Commit-Id: 18444d051f7f4cc24b9ceab9f32406e304ce3d21
    
    Change-Id: I23e3c935fcf8e753c748edb874421237dd3c3775
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02853817
    Feature: Wi-Fi Driver for MT6632

--- drivers/misc/mediatek/connectivity/wlan/gen4/os/linux/gl_cfg80211.c
+++ drivers/misc/mediatek/connectivity/wlan/gen4/os/linux/gl_cfg80211.c
@@ -653,7 +653,7 @@ int mtk_cfg80211_scan(struct wiphy *wiphy, struct cfg80211_scan_request *request
 	if (request->n_ssids == 0) {
 		rScanRequest.u4SsidNum = 0;
 	} else if (request->n_ssids <= (SCN_SSID_MAX_NUM + 1)) {
-		if ((request->ssids[request->n_ssids - 1].ssid == NULL)
+		if ((request->ssids[request->n_ssids - 1].ssid[0] == 0)
 			|| (request->ssids[request->n_ssids - 1].ssid_len == 0))
 			request->n_ssids--; /* remove the rear NULL SSID if this is a wildcard scan*/
 
