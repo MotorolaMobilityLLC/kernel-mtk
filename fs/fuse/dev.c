577912c4eb91bb56c34acd266a4c6ebf7fe133eb
rita.chan <rita.chan@mediatek.com>
2017-08-31 22:43:46 +0800

    [ALPS02850124] fuse: add trace to FUSE_INIT command
    [Detail]
    Vold was sometimes stucked at mount() when inserting a USB OTG
    card reader. Volds mount command was initiated by appfuse, and
    the user space FUSE daemon is appfuse service.
    
    [Solution]
    Add trace to FUSE_INIT command to clarify the problem.
    
    MTK-Commit-Id: fd677482d7d3e8865723a0366c0bfc7672d184e8
    
    Change-Id: Iea2b13e95fab27b58174f912fc8aee44645772ba
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02850124
    Feature: [Android Default] USB OTG

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -5,7 +5,7 @@
   This program can be distributed under the terms of the GNU GPL.
   See the file COPYING.
 */
-
+#define DEBUG 1
 #include "fuse_i.h"
 #include "mtk_fuse.h"
 
@@ -608,13 +608,21 @@ void fuse_request_send_background_locked(struct fuse_conn *fc,
 	fc->num_background++;
 	if (fc->num_background == fc->max_background)
 		fc->blocked = 1;
+
+	if (req->in.h.opcode == FUSE_INIT)
+		pr_info("FUSE_INIT: fuse_request_send_background_locked() -> set_bdi_congested()\n");
+
 	if (fc->num_background == fc->congestion_threshold &&
 	    fc->bdi_initialized) {
 		set_bdi_congested(&fc->bdi, BLK_RW_SYNC);
 		set_bdi_congested(&fc->bdi, BLK_RW_ASYNC);
 	}
 	list_add_tail(&req->list, &fc->bg_queue);
+	if (req->in.h.opcode == FUSE_INIT)
+		pr_info("FUSE_INIT: fuse_request_send_background_locked() -> flush_bg_queue() start\n");
 	flush_bg_queue(fc);
+	if (req->in.h.opcode == FUSE_INIT)
+		pr_info("FUSE_INIT: fuse_request_send_background_locked() -> flush_bg_queue() end\n");
 }
 
 void fuse_request_send_background_ex(struct fuse_conn *fc, struct fuse_req *req,
@@ -625,9 +633,13 @@ void fuse_request_send_background_ex(struct fuse_conn *fc, struct fuse_req *req,
 	FUSE_IOLOG_START();
 	spin_lock(&fc->lock);
 	if (fc->connected) {
+		if (req->in.h.opcode == FUSE_INIT)
+			pr_info("FUSE_INIT: fuse_request_send_background_ex(): Connected, send request\n");
 		fuse_request_send_background_locked(fc, req);
 		spin_unlock(&fc->lock);
 	} else {
+		if (req->in.h.opcode == FUSE_INIT)
+			pr_info("FUSE_INIT: fuse_request_send_background_ex(): Not connected, return -ENOTCONN\n");
 		spin_unlock(&fc->lock);
 		req->out.h.error = -ENOTCONN;
 		req->end(fc, req);
