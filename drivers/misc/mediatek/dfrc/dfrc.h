354adc539eeefa68d979c705eebd6b3fe4906f8e
rita.chan <rita.chan@mediatek.com>
2017-08-31 23:20:48 +0800

    [ALPS02877208] DFRC: adjust the parameter of ioctl
    [Detail] DFRC has to control APP frame rate now, so we adjust the
             original ioctl parameter to identify APP.
    
    [Solution] adjust the parameter of ioctl
    
    MTK-Commit-Id: 73d4b383abd645126dbc82d55e816713a34eb70b
    
    Change-Id: I5afd4885582c04e49b6bd2c05ef88c8cc949a825
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02877208
    Feature: Display Quality Enhancement

--- drivers/misc/mediatek/dfrc/dfrc.h
+++ drivers/misc/mediatek/dfrc/dfrc.h
@@ -22,9 +22,7 @@
 #define DFRC_ALLOW_TOUCH 0x02
 
 int dfrc_allow_rrc_adjust_fps(void);
-void dfrc_set_rrc_touch_fps(int fps);
-void dfrc_set_rrc_video_fps(int fps);
 
-long dfrc_set_kernel_policy(int api, int fps, int mode);
+long dfrc_set_kernel_policy(int api, int fps, int mode, int target_pid, void *gl_context_id);
 
 #endif
