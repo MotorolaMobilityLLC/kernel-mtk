1eee430daf2033a91e88c979357d5674e652e01e
rita.chan <rita.chan@mediatek.com>
2017-09-01 00:57:13 +0800

    [ALPS03280834] WLAN-P2P: EAP packet are dropped when class error check
    [Detail]
    EAP packet are dropped when qmHandleRxPackets class error check for AIS
    which caused p2p connection failure.
    [Solution]
    When data packet dont do header translation in FW/HW, AIS finish this
    function for connected state in driver and drop the packet for
    disconnected state, but P2P keep original operation.
    
    MTK-Commit-Id: 21f9012e105659e924802c776110af78f094acd8
    
    Change-Id: I99f67bf2e576cf372f67f69accc3a674059ad707
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03280834
    Feature: Wi-Fi Chipset Capability
    (cherry picked from commit 14e6574678a8eb5b41b4fd2616821646a73bf306)

--- drivers/misc/mediatek/connectivity/wlan/gen4/mgmt/privacy.c
+++ drivers/misc/mediatek/connectivity/wlan/gen4/mgmt/privacy.c
@@ -240,9 +240,7 @@ BOOL secCheckClassError(IN P_ADAPTER_T prAdapter, IN P_SW_RFB_T prSwRfb, IN P_ST
 	prRxStatus = prSwRfb->prRxStatus;
 
 #if 1
-	if (!prStaRec || ((prRxStatus->u2StatusFlag & RXS_DW2_RX_CLASSERR_BITMAP) == RXS_DW2_RX_CLASSERR_VALUE)
-		|| (prAdapter->prAisBssInfo->eConnectionState == PARAM_MEDIA_STATE_DISCONNECTED)) {
-
+	if (!prStaRec || ((prRxStatus->u2StatusFlag & RXS_DW2_RX_CLASSERR_BITMAP) == RXS_DW2_RX_CLASSERR_VALUE)) {
 		DBGLOG(RSN, ERROR,
 		       "RX_CLASSERR: prStaRec=%p StatusFlag=0x%x, PktTYpe=0x%x, WlanIdx=%d StaRecIdx=%d eDst=%d\n",
 		       prStaRec, prRxStatus->u2StatusFlag, prRxStatus->u2PktTYpe,
