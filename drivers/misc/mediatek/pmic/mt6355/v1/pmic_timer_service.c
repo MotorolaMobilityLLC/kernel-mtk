741d747a888098aa1c4b90812883ae0437727798
rita.chan <rita.chan@mediatek.com>
2017-09-01 00:12:20 +0800

    [ALPS03095212] battery: merge gm30 code for mt6759
    [Detail]
    sync gm30 code with mt6759
    
    MTK-Commit-Id: df33d6e8ee7a750163d295da827b6ae92b7485ec
    
    Change-Id: I74b4ae7530c6c902b00970ef41e37ddee22b8976
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03095212
    Feature: Fuel Gauge

--- drivers/misc/mediatek/pmic/mt6355/v1/pmic_timer_service.c
+++ drivers/misc/mediatek/pmic/mt6355/v1/pmic_timer_service.c
@@ -15,11 +15,42 @@
 #include <linux/list.h>
 #include <linux/device.h>
 #include <linux/delay.h>
+#include <linux/kthread.h>
+#include <linux/wakelock.h>
 
+static struct list_head fgtimer_head = LIST_HEAD_INIT(fgtimer_head);
+static struct mutex fgtimer_lock;
+static unsigned long reset_time;
+static spinlock_t slock;
+static struct wake_lock wlock;
+static wait_queue_head_t wait_que;
+static bool fgtimer_thread_timeout;
+static int ftlog_level = 1;
 
-struct list_head fgtimer_head = LIST_HEAD_INIT(fgtimer_head);
-struct mutex fgtimer_lock;
-unsigned long reset_time;
+#define FTLOG_ERROR_LEVEL   1
+#define FTLOG_DEBUG_LEVEL   2
+#define FTLOG_TRACE_LEVEL   3
+
+#define ft_err(fmt, args...)   \
+do {									\
+	if (ftlog_level >= FTLOG_ERROR_LEVEL) {			\
+		pr_err(fmt, ##args); \
+	}								   \
+} while (0)
+
+#define ft_debug(fmt, args...)   \
+do {									\
+	if (ftlog_level >= FTLOG_DEBUG_LEVEL) {		\
+		pr_debug(fmt, ##args); \
+	}								   \
+} while (0)
+
+#define ft_trace(fmt, args...)\
+do {									\
+	if (ftlog_level >= FTLOG_TRACE_LEVEL) {			\
+		pr_debug(fmt, ##args);\
+	}						\
+} while (0)
 
 
 void mutex_fgtimer_lock(void)
@@ -44,13 +75,13 @@ void fgtimer_dump_list(void)
 	struct list_head *phead = &fgtimer_head;
 	struct fgtimer *ptr;
 
-	pr_err("dump list start\n");
+	ft_debug("dump list start\n");
 	list_for_each(pos, phead) {
 		ptr = container_of(pos, struct fgtimer, list);
-		pr_err("dump list name:%s time:%ld %ld int:%d\n", dev_name(ptr->dev),
+		ft_debug("dump list name:%s time:%ld %ld int:%d\n", dev_name(ptr->dev),
 		ptr->stime, ptr->endtime, ptr->interval);
 	}
-	pr_err("dump list end\n");
+	ft_debug("dump list end\n");
 }
 
 
@@ -69,7 +100,7 @@ void fgtimer_after_reset(void)
 
 	mutex_fgtimer_lock();
 	battery_meter_set_fg_timer_interrupt(0);
-	pr_err("fgtimer_reset\n");
+	ft_debug("fgtimer_reset\n");
 	fgtimer_dump_list();
 	list_for_each(pos, phead) {
 		ptr = container_of(pos, struct fgtimer, list);
@@ -109,13 +140,13 @@ void fgtimer_start(struct fgtimer *timer, int sec)
 	timer->interval = sec;
 	now = timer->stime;
 
-	pr_err("fgtimer_start dev:%s name:%s %ld %ld %d\n",
+	ft_debug("fgtimer_start dev:%s name:%s %ld %ld %d\n",
 	dev_name(timer->dev), timer->name, timer->stime, timer->endtime,
 	timer->interval);
 
 
 	if (list_empty(&timer->list) != true) {
-		pr_err("fgtimer_start dev:%s name:%s time:%ld %ld int:%d is not empty\n",
+		ft_debug("fgtimer_start dev:%s name:%s time:%ld %ld int:%d is not empty\n",
 		dev_name(timer->dev), timer->name,
 		timer->stime, timer->endtime, timer->interval);
 		list_del_init(&timer->list);
@@ -149,7 +180,7 @@ void fgtimer_start(struct fgtimer *timer, int sec)
 void fgtimer_stop(struct fgtimer *timer)
 {
 
-	pr_err("fgtimer_stop node:%s %ld %ld %d\n",
+	ft_debug("fgtimer_stop node:%s %ld %ld %d\n",
 	dev_name(timer->dev), timer->stime, timer->endtime,
 	timer->interval);
 
@@ -159,6 +190,7 @@ void fgtimer_stop(struct fgtimer *timer)
 
 }
 
+static struct timespec sstime[10];
 void fg_time_int_handler(void)
 {
 	unsigned int time;
@@ -166,11 +198,11 @@ void fg_time_int_handler(void)
 	struct list_head *phead = &fgtimer_head;
 	struct fgtimer *ptr;
 
+	get_monotonic_boottime(&sstime[0]);
 	time = battery_meter_get_fg_time();
-	pr_err("[fg_time_int_handler] time:%d\n", time);
+	ft_debug("[fg_time_int_handler] time:%d\n", time);
 
-	mutex_fgtimer_lock();
-
+	get_monotonic_boottime(&sstime[1]);
 	for (pos = phead->next; pos != phead;) {
 		struct list_head *ptmp;
 
@@ -179,13 +211,14 @@ void fg_time_int_handler(void)
 			ptmp = pos;
 			pos = pos->next;
 			list_del_init(ptmp);
-			pr_err("[fg_time_int_handler] %s %ld %ld %d timeout\n", dev_name(ptr->dev),
+			ft_debug("[fg_time_int_handler] %s %ld %ld %d timeout\n", dev_name(ptr->dev),
 			ptr->stime, ptr->endtime, ptr->interval);
 			if (ptr->callback)
 				ptr->callback(ptr);
 		} else
 			pos = pos->next;
 	}
+	get_monotonic_boottime(&sstime[2]);
 
 	pos = fgtimer_head.next;
 	if (list_empty(pos) != true) {
@@ -196,18 +229,69 @@ void fg_time_int_handler(void)
 			new_sec = ptr->endtime - time;
 			if (new_sec <= 0)
 				new_sec = 1;
-			pr_err("[fg_time_int_handler] next is %s %ld %ld %d now:%d new_sec:%d\n", dev_name(ptr->dev),
+			ft_debug("[fg_time_int_handler] next is %s %ld %ld %d now:%d new_sec:%d\n", dev_name(ptr->dev),
 				ptr->stime, ptr->endtime, ptr->interval, time, new_sec);
 			battery_meter_set_fg_timer_interrupt(new_sec);
 		}
 	}
-	mutex_fgtimer_unlock();
+	get_monotonic_boottime(&sstime[3]);
+	sstime[0] = timespec_sub(sstime[1], sstime[0]);
+	sstime[1] = timespec_sub(sstime[2], sstime[1]);
+	sstime[2] = timespec_sub(sstime[3], sstime[2]);
+}
 
+static int fgtime_thread(void *arg)
+{
+	unsigned long flags;
+	struct timespec stime, endtime, duraction;
+
+	while (1) {
+		wait_event(wait_que, (fgtimer_thread_timeout == true));
+		fgtimer_thread_timeout = false;
+		get_monotonic_boottime(&stime);
+		mutex_fgtimer_lock();
+		fg_time_int_handler();
+
+		spin_lock_irqsave(&slock, flags);
+		wake_unlock(&wlock);
+		spin_unlock_irqrestore(&slock, flags);
+
+		mutex_fgtimer_unlock();
+		get_monotonic_boottime(&endtime);
+		duraction = timespec_sub(endtime, stime);
+
+		if ((duraction.tv_nsec / 1000000) > 50)
+			ft_err("fgtime_thread time:%d ms %d %d %d\n", (int)(duraction.tv_nsec / 1000000),
+				(int)(sstime[0].tv_nsec / 1000000),
+				(int)(sstime[1].tv_nsec / 1000000),
+				(int)(sstime[2].tv_nsec / 1000000));
+	}
+
+	return 0;
+}
+
+
+void wake_up_fgtimer(void)
+{
+	unsigned long flags;
+
+	spin_lock_irqsave(&slock, flags);
+	if (wake_lock_active(&wlock) == 0)
+		wake_lock(&wlock);
+	spin_unlock_irqrestore(&slock, flags);
+
+	fgtimer_thread_timeout = true;
+	wake_up(&wait_que);
 }
 
 void fgtimer_service_init(void)
 {
-	pr_err("fgtimer_service_init\n");
+	ft_debug("fgtimer_service_init\n");
 	mutex_init(&fgtimer_lock);
-	pmic_register_interrupt_callback(FG_TIME_NO, fg_time_int_handler);
+	spin_lock_init(&slock);
+	wake_lock_init(&wlock, WAKE_LOCK_SUSPEND, "fg timer wakelock");
+	init_waitqueue_head(&wait_que);
+	kthread_run(fgtime_thread, NULL, "fg_timer_thread");
+
+	pmic_register_interrupt_callback(FG_TIME_NO, wake_up_fgtimer);
 }
