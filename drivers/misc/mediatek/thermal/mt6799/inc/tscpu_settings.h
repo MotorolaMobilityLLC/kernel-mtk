d591598392f79879ffb97b7b091164b2b2b3ffd7
rita.chan <rita.chan@mediatek.com>
2017-09-01 01:27:27 +0800

    [ALPS03337248] thermal: fix some thermal driver build warning
    [Detail]
    remove typedef to avoid thermal driver build warning
    
    MTK-Commit-Id: fd9407b4407cbd64734ecb47bd57a9e1a8a78a7e
    
    Change-Id: I97f6d71bd6cbd32d0041e34873a3c29953e08a63
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03337248
    Feature: Thermal Management

--- drivers/misc/mediatek/thermal/mt6799/inc/tscpu_settings.h
+++ drivers/misc/mediatek/thermal/mt6799/inc/tscpu_settings.h
@@ -220,12 +220,12 @@ do {                                    \
  * Structures
  *=============================================================
  */
-typedef enum {
+enum thermal_controller_name {
 	THERMAL_CONTROLLER0 = 0,
 	THERMAL_CONTROLLER1,
 	THERMAL_CONTROLLER2,
 	THERMAL_CONTROLLER_NUM
-} thermal_controller_name;
+};
 
 struct thermal_controller_speed {
 	unsigned int tempMonCtl1;
