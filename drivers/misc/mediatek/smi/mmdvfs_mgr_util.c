b836138103f9c3c763b178b3f45ab307e9aa85b8
rita.chan <rita.chan@mediatek.com>
2017-08-31 22:50:50 +0800

    [ALPS02695216] MMDVFS: bug fix in mmdvfs_get_lcd_resolution
    [Detail] The function failed if display driver has not started
    
    [Solution] Use CONFIG_LCM_WIDTH and CONFIG_LCM_HEIGHT
    
    MTK-Commit-Id: c10f3f178dba04687487baba5895664af054742a
    
    Change-Id: I0ea68be3f41bc05f4082db33b985b6b38a67b29f
    CR-Id: ALPS02695216
    Feature: MMDVFS
    Signed-off-by: rita.chan <rita.chan@mediatek.com>

--- drivers/misc/mediatek/smi/mmdvfs_mgr_util.c
+++ drivers/misc/mediatek/smi/mmdvfs_mgr_util.c
@@ -17,8 +17,24 @@
 
 mmdvfs_lcd_size_enum mmdvfs_get_lcd_resolution(void)
 {
-	int lcd_resolution = DISP_GetScreenWidth() * DISP_GetScreenHeight();
 	mmdvfs_lcd_size_enum result = MMDVFS_LCD_SIZE_HD;
+	long lcd_resolution = 0;
+	long lcd_w = 0;
+	long lcd_h = 0;
+	int convert_err = -EINVAL;
+
+#if defined(CONFIG_LCM_WIDTH) && defined(CONFIG_LCM_HEIGHT)
+	convert_err = kstrtoul(CONFIG_LCM_WIDTH, 10, &lcd_w);
+	if (!convert_err)
+		convert_err = kstrtoul(CONFIG_LCM_HEIGHT, 10, &lcd_h);
+#endif	/* CONFIG_LCM_WIDTH, CONFIG_LCM_HEIGHT */
+
+	if (convert_err) {
+		lcd_w = DISP_GetScreenWidth();
+		lcd_h = DISP_GetScreenHeight();
+	}
+
+	lcd_resolution = lcd_w * lcd_h;
 
 	if (lcd_resolution <= MMDVFS_DISPLAY_SIZE_HD)
 		result = MMDVFS_LCD_SIZE_HD;
