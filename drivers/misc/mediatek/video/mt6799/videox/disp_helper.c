8b92f746eec4f81d1b38a3bd71daacdec7ec9947
rita.chan <rita.chan@mediatek.com>
2017-08-31 23:56:31 +0800

    [ALPS03018785] DISP:Fix bring-up failed issue
    [Detail] Because smart overlay would introduce
    CMDQ timeout in bring-up stage.
    
    [Solution] Turn it off temporally.
    
    MTK-Commit-Id: 7e52ee6b4d981294241c77cd198906f5a2ec94a7
    
    Change-Id: Icf572ffb012105e952459d5cf5cd1b6ea98ce894
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03018785
    Feature: Display Partial Update

--- drivers/misc/mediatek/video/mt6799/videox/disp_helper.c
+++ drivers/misc/mediatek/video/mt6799/videox/disp_helper.c
@@ -372,7 +372,7 @@ void disp_helper_option_init(void)
 
 	disp_helper_set_option(DISP_OPT_BYPASS_OVL, 0);
 	disp_helper_set_option(DISP_OPT_FPS_CALC_WND, 10);
-	disp_helper_set_option(DISP_OPT_SMART_OVL, 1);
+	disp_helper_set_option(DISP_OPT_SMART_OVL, 0);
 	disp_helper_set_option(DISP_OPT_DYNAMIC_DEBUG, 0);
 	disp_helper_set_option(DISP_OPT_HRT, 1);
 
