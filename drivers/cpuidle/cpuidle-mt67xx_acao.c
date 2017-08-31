7715af183d8a749338d552e07f52a3e75f942b16
rita.chan <rita.chan@mediatek.com>
2017-09-01 00:21:19 +0800

    [ALPS03133813] MCDI: introduce MT6763 MCDI driver
    [Detail]
    1. Use MENU governor when CONFIG_MTK_ACAO_SUPPORT=yes
    2. Add CPUidle driver which supports rgidle & MCDI
    3. Add MCDI governor to check if
        (1) deepidle/SODI can enter
        (2) Cluster can be powered OFF
    
        According to predict_us in MENU governor
    
    MTK-Commit-Id: 3f52804ee5c363b2d86e5e27d80caf9c242d454f
    
    Change-Id: I7cf7b58442c65f2aca179869791b4115eee36065
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03133813
    Feature: All Core Always Online (ACAO-MCU Deep Idle)

---
+++ drivers/cpuidle/cpuidle-mt67xx_acao.c
@@ -0,0 +1,141 @@
+/*
+ * Copyright (C) 2017 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/cpuidle.h>
+#include <linux/cpumask.h>
+#include <linux/cpu_pm.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/slab.h>
+
+#include <asm/cpuidle.h>
+#include <asm/suspend.h>
+
+/* #define USING_TICK_BROADCAST */
+
+int __attribute__((weak)) rgidle_enter(int cpu)
+{
+	return 1;
+}
+
+int __attribute__((weak)) mcdi_enter(int cpu)
+{
+	return 1;
+}
+
+static int mtk_rgidle_enter(struct cpuidle_device *dev,
+			      struct cpuidle_driver *drv, int index)
+{
+	rgidle_enter(smp_processor_id());
+	return index;
+}
+
+static int mtk_mcidle_enter(struct cpuidle_device *dev,
+			      struct cpuidle_driver *drv, int index)
+{
+	mcdi_enter(smp_processor_id());
+	return index;
+}
+
+static struct cpuidle_driver mt67xx_acao_cpuidle_driver = {
+	.name             = "mt67xx_acao_cpuidle",
+	.owner            = THIS_MODULE,
+	.states[0] = {
+		.enter			= mtk_rgidle_enter,
+		.exit_latency		= 1,
+		.target_residency	= 1,
+		.name			= "rgidle",
+		.desc			= "wfi"
+	},
+	.states[1] = {
+		.enter			= mtk_mcidle_enter,
+		.exit_latency		= 300,
+		.target_residency	= 1000,
+#ifdef USING_TICK_BROADCAST
+		.flags			= CPUIDLE_FLAG_TIMER_STOP,
+#endif
+		.name			= "mcdi",
+		.desc			= "mcdi",
+	},
+	.state_count = 2,
+	.safe_state_index = 0,
+};
+
+int __init mt67xx_acao_cpuidle_init(void)
+{
+	int cpu, ret;
+	struct cpuidle_driver *drv = &mt67xx_acao_cpuidle_driver;
+	struct cpuidle_device *dev;
+
+	/*
+	 * Initialize idle states data, starting at index 1.
+	 * This driver is DT only, if no DT idle states are detected (ret == 0)
+	 * let the driver initialization fail accordingly since there is no
+	 * reason to initialize the idle driver if only wfi is supported.
+	 */
+#if 0
+	ret = dt_init_idle_driver(drv, mt67xx_v2_idle_state_match, 1);
+	if (ret <= 0)
+		return ret ? : -ENODEV;
+#endif
+
+	ret = cpuidle_register_driver(drv);
+	if (ret) {
+		pr_err("Failed to register cpuidle driver\n");
+		return ret;
+	}
+
+	/*
+	 * Call arch CPU operations in order to initialize
+	 * idle states suspend back-end specific data
+	 */
+	for_each_possible_cpu(cpu) {
+		ret = arm_cpuidle_init(cpu);
+
+		/*
+		 * Skip the cpuidle device initialization if the reported
+		 * failure is a HW misconfiguration/breakage (-ENXIO).
+		 */
+		if (ret == -ENXIO)
+			continue;
+
+		if (ret) {
+			pr_err("CPU %d failed to init idle CPU ops\n", cpu);
+			goto out_fail;
+		}
+
+		dev = kzalloc(sizeof(*dev), GFP_KERNEL);
+		if (!dev)
+			goto out_fail;
+		dev->cpu = cpu;
+
+		ret = cpuidle_register_device(dev);
+		if (ret) {
+			pr_err("Failed to register cpuidle device for CPU %d\n",
+			       cpu);
+			kfree(dev);
+			goto out_fail;
+		}
+	}
+
+	return 0;
+
+out_fail:
+	while (--cpu >= 0) {
+		dev = per_cpu(cpuidle_devices, cpu);
+		cpuidle_unregister_device(dev);
+		kfree(dev);
+	}
+
+	cpuidle_unregister_driver(drv);
+
+	return ret;
+}
+
+device_initcall(mt67xx_acao_cpuidle_init);
---
+++ drivers/misc/mediatek/base/power/mcdi/mcdi_v1/mtk_mcdi.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2017 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#ifndef __MTK_MCDI_H__
+#define __MTK_MCDI_H__
+
+#define NF_CPU              8
+#define NF_CLUSTER          2
+
+extern unsigned int get_menu_predict_us(void);
+
+#endif /* __MTK_MCDI_H__ */
