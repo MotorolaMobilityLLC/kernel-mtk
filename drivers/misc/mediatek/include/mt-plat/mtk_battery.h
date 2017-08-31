7552c78d5ba39745013926c1cbdf3acdcf9df6a0
rita.chan <rita.chan@mediatek.com>
2017-08-31 23:45:47 +0800

    [ALPS02995394] battery: fixed print too much error
    [Detail]
    1. Remove some debug message
    2. adjust debug level
    
    MTK-Commit-Id: ddaaeaf3ddc60627a1f30008438bd51412cebe0e
    
    Change-Id: I86de2e28d0167aaae65948ed4ba1128c1c6f3082
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02995394
    Feature: Others

--- drivers/misc/mediatek/include/mt-plat/mtk_battery.h
+++ drivers/misc/mediatek/include/mt-plat/mtk_battery.h
@@ -45,13 +45,21 @@
 /* ============================================================ */
 /* typedef and Struct*/
 /* ============================================================ */
+#if 0
 #define BMLOG_ERROR_LEVEL   3
 #define BMLOG_WARNING_LEVEL 4
 #define BMLOG_NOTICE_LEVEL  5
 #define BMLOG_INFO_LEVEL    6
 #define BMLOG_DEBUG_LEVEL   7
 #define BMLOG_TRACE_LEVEL   8
-
+#else
+#define BMLOG_ERROR_LEVEL   3
+#define BMLOG_WARNING_LEVEL 8
+#define BMLOG_NOTICE_LEVEL  8
+#define BMLOG_INFO_LEVEL    8
+#define BMLOG_DEBUG_LEVEL   8
+#define BMLOG_TRACE_LEVEL   8
+#endif
 
 #define bm_err(fmt, args...)   \
 do {									\
