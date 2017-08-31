7f9cfcfd483254656f37a5a914c15136632aea14
rita.chan <rita.chan@mediatek.com>
2017-09-01 00:58:18 +0800

    [ALPS03282021] mm: change vmstat_shepherd to use system_unbound_wq
    Let vmstat_shepherd use system_unbound_wq.
    
    MTK-Commit-Id: 81f7c4ee1d5b4dda6843e339ff71b0dac0ec19ae
    
    Change-Id: I9296c7744f2005250576dcd78d697fea893f9e5c
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03282021
    Feature: Memory Optimization

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1473,7 +1473,7 @@ static void vmstat_shepherd(struct work_struct *w)
 
 	put_online_cpus();
 
-	schedule_delayed_work(&shepherd,
+	queue_delayed_work(system_unbound_wq, &shepherd,
 		round_jiffies_relative(sysctl_stat_interval));
 
 }
@@ -1491,7 +1491,7 @@ static void __init start_shepherd_timer(void)
 	cpumask_copy(cpu_stat_off, cpu_online_mask);
 
 	vmstat_wq = alloc_workqueue("vmstat", WQ_FREEZABLE|WQ_MEM_RECLAIM, 0);
-	schedule_delayed_work(&shepherd,
+	queue_delayed_work(system_unbound_wq, &shepherd,
 		round_jiffies_relative(sysctl_stat_interval));
 }
 
