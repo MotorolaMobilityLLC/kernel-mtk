511951d839b7343235e1d67a05c731e44d9d5ab4
rita.chan <rita.chan@mediatek.com>
2017-08-31 22:09:23 +0800

    [ALPS02771273] usb : xhci driver porting
    [Detail]
      1. add mtk xhci driver
      2. add iddig dualrole driver
    
    MTK-Commit-Id: dc6bb31cba55bdfef8d275108c3a2c00ddca4fe2
    
    Change-Id: Id7f09fd3b9edf96f17d0c792d3fbe0c8239ee721
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02771273
    Feature: [Android Default] USB OTG

---
+++ drivers/misc/mediatek/xhci/xhci-mtk-driver.c
@@ -0,0 +1,212 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/delay.h>
+#include <linux/uaccess.h>
+#include <linux/dma-mapping.h>
+#include <linux/platform_device.h>
+#include <linux/wakelock.h>
+#include <linux/irq.h>
+#include <linux/switch.h>
+#include <linux/sched.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+#include <linux/of_irq.h>
+#include <linux/jiffies.h>
+#include <linux/list.h>
+#include <linux/atomic.h>
+
+#include <xhci-mtk-driver.h>
+#ifdef CONFIG_PROJECT_PHY
+#include <mtk-phy-asic.h>
+#endif
+#include <mt-plat/battery_meter.h>
+
+#define RET_SUCCESS 0
+#define RET_FAIL 1
+
+static struct wake_lock mtk_xhci_wakelock;
+
+enum dualrole_state {
+	DUALROLE_DEVICE,
+	DUALROLE_HOST,
+};
+
+static enum dualrole_state mtk_dualrole_stat = DUALROLE_DEVICE;
+static struct switch_dev mtk_otg_state;
+
+u32 xhci_debug_level = K_ALET | K_CRIT | K_ERR | K_WARNIN;
+
+module_param(xhci_debug_level, int, 0644);
+
+bool mtk_is_charger_4_vol(void)
+{
+#if defined(CONFIG_USBIF_COMPLIANCE) || defined(CONFIG_POWER_EXT)
+	return false;
+#else
+	int vol = battery_meter_get_charger_voltage();
+
+	mtk_xhci_mtk_printk(K_DEBUG, "voltage(%d)\n", vol);
+
+	return (vol > 4000) ? true : false;
+#endif
+}
+
+static void mtk_enable_otg_mode(void)
+{
+	set_chr_enable_otg(0x1);
+	set_chr_boost_current_limit(1500);
+}
+
+static void mtk_disable_otg_mode(void)
+{
+	set_chr_enable_otg(0x0);
+}
+
+static int mtk_xhci_hcd_init(void)
+{
+	int retval;
+
+	retval = xhci_mtk_register_plat();
+	if (retval < 0) {
+		pr_err("Problem registering platform driver.\n");
+		return retval;
+	}
+
+	return 0;
+}
+
+static void mtk_xhci_wakelock_init(void)
+{
+	wake_lock_init(&mtk_xhci_wakelock, WAKE_LOCK_SUSPEND, "xhci.wakelock");
+#ifdef CONFIG_USB_C_SWITCH
+	typec_host_driver.priv_data = NULL;
+	register_typec_switch_callback(&typec_host_driver);
+#endif
+}
+
+static void mtk_xhci_wakelock_lock(void)
+{
+	if (!wake_lock_active(&mtk_xhci_wakelock))
+		wake_lock(&mtk_xhci_wakelock);
+	mtk_xhci_mtk_printk(K_DEBUG, "xhci_wakelock_lock done\n");
+}
+
+static void mtk_xhci_wakelock_unlock(void)
+{
+	if (wake_lock_active(&mtk_xhci_wakelock))
+		wake_unlock(&mtk_xhci_wakelock);
+	mtk_xhci_mtk_printk(K_DEBUG, "xhci_wakelock_unlock done\n");
+}
+
+static void mtk_xhci_hcd_cleanup(void)
+{
+	xhci_mtk_unregister_plat();
+}
+
+int mtk_xhci_driver_load(void)
+{
+	int ret = 0;
+
+	/* recover clock/power setting and deassert reset bit of mac */
+#ifdef CONFIG_PROJECT_PHY
+	usb_phy_recover(0);
+	usb20_pll_settings(true, true);
+#endif
+	ret = mtk_xhci_hcd_init();
+	if (ret) {
+		ret = -ENXIO;
+		goto _err;
+	}
+	mtk_xhci_wakelock_lock();
+#ifndef CONFIG_USBIF_COMPLIANCE
+	switch_set_state(&mtk_otg_state, 1);
+#endif
+	mtk_dualrole_stat = DUALROLE_HOST;
+	mtk_enable_otg_mode();
+
+	return 0;
+
+_err:
+#ifdef CONFIG_PROJECT_PHY
+	usb_phy_savecurrent(1);
+#endif
+	mtk_dualrole_stat = DUALROLE_DEVICE;
+
+	return ret;
+}
+
+static void mtk_xhci_disPortPower(void)
+{
+	mtk_disable_otg_mode();
+	/* TODO IT */
+}
+
+void mtk_xhci_driver_unload(void)
+{
+	mtk_xhci_hcd_cleanup();
+	mtk_xhci_disPortPower();
+	/* close clock/power setting and assert reset bit of mac */
+#ifdef CONFIG_PROJECT_PHY
+	usb20_pll_settings(true, false);
+	usb_phy_savecurrent(1);
+#endif
+
+#ifndef CONFIG_USBIF_COMPLIANCE
+	switch_set_state(&mtk_otg_state, 0);
+#endif
+	mtk_xhci_wakelock_unlock();
+	mtk_dualrole_stat = DUALROLE_DEVICE;
+}
+
+void mtk_xhci_switch_init(void)
+{
+	mtk_otg_state.name = "otg_state";
+	mtk_otg_state.index = 0;
+	mtk_otg_state.state = 0;
+
+#ifndef CONFIG_USBIF_COMPLIANCE
+	if (switch_dev_register(&mtk_otg_state))
+		mtk_xhci_mtk_printk(K_DEBUG, "switch_dev_register fail\n");
+	else
+		mtk_xhci_mtk_printk(K_DEBUG, "switch_dev register success\n");
+#endif
+}
+
+bool mtk_is_host_mode(void)
+{
+	return (mtk_dualrole_stat == DUALROLE_HOST) ? true : false;
+}
+
+#ifdef CONFIG_USBIF_COMPLIANCE
+
+#else
+static int __init xhci_hcd_init(void)
+{
+	mtk_xhci_wakelock_init();
+	mtk_xhci_switch_init();
+	return 0;
+}
+
+late_initcall(xhci_hcd_init);
+
+static void __exit xhci_hcd_cleanup(void)
+{
+}
+
+module_exit(xhci_hcd_cleanup);
+#endif
+
