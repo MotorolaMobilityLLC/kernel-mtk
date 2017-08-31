f0d8c8d82d46e6fd775ca2ca35192d88bbb715f7
rita.chan <rita.chan@mediatek.com>
2017-08-31 23:25:46 +0800

    [ALPS02963383] wlan: Thoughput Issue
    [Detail]
    1. mt6757 config error, so kalboostCPU not function.
    
    [Solution]
    1. Fix config
    2. update CPU control flow in kalBoostCpu()
    
    MTK-Commit-Id: 5beee293f24e6823813051e6c3f96e3be49f79c5
    
    Change-Id: Ib0cca07c8ffd70672f0808896fe82d21e9ba46b9
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02963383
    Feature: MTBF(*)
    (cherry picked from commit ed4a7e3ed36604c91922d9840aec8a9c9f4740ee)

--- drivers/misc/mediatek/connectivity/wlan/gen2/os/linux/plat/mt6757/plat_priv.c
+++ drivers/misc/mediatek/connectivity/wlan/gen2/os/linux/plat/mt6757/plat_priv.c
@@ -9,11 +9,19 @@
 * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
 */
 
-#include "mach/mt_ppm_api.h"
+#include "mach/mtk_ppm_api.h"
 #include "gl_typedef.h"
 INT32 kalBoostCpu(UINT_32 core_num)
 {
 	pr_warn("enter kalBoostCpu, core_num:%d\n", core_num);
-	mt_ppm_sysboost_core(BOOST_BY_WIFI, core_num);
+	/* mt_ppm_sysboost_core(BOOST_BY_WIFI, core_num); */
+	if (core_num == 0) {
+		mt_ppm_sysboost_set_core_limit(BOOST_BY_WIFI, 0, -1, -1);
+		mt_ppm_sysboost_set_core_limit(BOOST_BY_WIFI, 1, -1, -1);
+	} else {
+		mt_ppm_sysboost_set_core_limit(BOOST_BY_WIFI, 0, 4, 4);
+		mt_ppm_sysboost_set_core_limit(BOOST_BY_WIFI, 1, core_num, 4);
+	}
 	return 0;
 }
+
