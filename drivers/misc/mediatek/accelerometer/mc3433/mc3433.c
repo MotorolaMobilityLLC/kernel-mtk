99af61bfaec8bd2b89e22e1429c5c561c104fc92
rita.chan <rita.chan@mediatek.com>
2017-09-01 01:46:30 +0800

    [ALPS03280283] sensor: modify GPL header
    modify g-sensor driver GPL header
    
    MTK-Commit-Id: 6a91c3dc9c71efb6ae8f5dfdaf391a241f2f7f0b
    
    Change-Id: Ibe18333c22c71c43a24c5de8c9cfab960484f3cb
    CR-Id: ALPS03280283
    Feature: sensor
    Signed-off-by: rita.chan <rita.chan@mediatek.com>

--- drivers/misc/mediatek/accelerometer/mc3433/mc3433.c
+++ drivers/misc/mediatek/accelerometer/mc3433/mc3433.c
@@ -1,21 +1,15 @@
-﻿ /*****************************************************************************
- * mc3433 Sensor Driver
+﻿/*
+ * Copyright (C) 2016 MediaTek Inc.
  *
- * This software is licensed under the terms of the GNU General Public
- * License version 2, as published by the Free Software Foundation, and
- * may be copied, distributed, and modified under those terms.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- *
- *****************************************************************************/
-
-/*****************************************************************************
- *** HEADER FILES
- *****************************************************************************/
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
 
 #include "cust_acc.h"
 #include "accel.h"
