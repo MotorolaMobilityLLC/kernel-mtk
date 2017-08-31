3c17edac3f80c9dd327fffae2dc133b547403a17
rita.chan <rita.chan@mediatek.com>
2017-09-01 00:12:14 +0800

    [ALPS03094098] RGU: Avoid MRDUMP incorrectly being triggered
    [Detail]
    Control "MRDUMP triggered by EINT" feature by device tree.
    
    MTK-Commit-Id: 08f6425ca372497e1fa306298aaac334b940c6a0
    
    Change-Id: I7227502292b630468f713059139d11bf892161f4
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03094098
    Feature: Power Management

--- drivers/watchdog/mediatek/wdt/mt6757/mtk_wdt.c
+++ drivers/watchdog/mediatek/wdt/mt6757/mtk_wdt.c
@@ -830,7 +830,7 @@ static int mtk_wdt_probe(struct platform_device *dev)
 #endif
 
 	node = of_find_compatible_node(NULL, NULL, "mediatek, mrdump_ext_rst-eint");
-	if (node) {
+	if (node && of_device_is_available(node)) {
 		of_property_read_u32_array(node, "debounce", ints, ARRAY_SIZE(ints));
 		ext_debugkey_io = ints[0];
 	}
