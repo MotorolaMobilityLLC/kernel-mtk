bd7a38d8111c8ece34ff62e35266f60d64642363
rita.chan <rita.chan@mediatek.com>
2017-08-31 22:14:17 +0800

    [ALPS02743747] cmdq: Fix handle to mva issue
    [Detail]
    Translate pa to mva API check pa plus size, which is size of base address.
    But cmdq use base+offset, which will out of range after plus size.
    
    [Solution]
    Do block offset the base pa before translate to mva.
    And plus buffer offset after translate pa to mva.
    
    MTK-Commit-Id: 462d87add6a17ec269cb1c001d6b78accb3c5032
    
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    Change-Id: I94ac0ea60d4a50ccf0ca79625f43ceeddfbda652
    CR-Id: ALPS02743747
    Feature: CMDQ
    (cherry picked from commit 7223e009eb303dbab24f9aab27503bc88f2aae70)

--- drivers/misc/mediatek/cmdq/v2/cmdq_def.h
+++ drivers/misc/mediatek/cmdq/v2/cmdq_def.h
@@ -313,11 +313,27 @@ struct cmdqSecAddrMetadataStruct {
 	/* [IN]_d, index of instruction. Update its arg_b value to real PA/MVA in secure world */
 	uint32_t instrIndex;
 
+	/*
+	 * Note: Buffer and offset
+	 *
+	 *   -------------
+	 *   |     |     |
+	 *   -------------
+	 *   ^     ^  ^  ^
+	 *   A     B  C  D
+	 *
+	 *	A: baseHandle
+	 *	B: baseHandle + blockOffset
+	 *  C: baseHandle + blockOffset + offset
+	 *	A~B or B~D: size
+	 */
+
 	enum CMDQ_SEC_ADDR_METADATA_TYPE type;	/* [IN] addr handle type */
 	uint32_t baseHandle;	/* [IN]_h, secure address handle */
-	uint32_t offset;	/* [IN]_b, buffser offset to secure handle */
-	uint32_t size;		/* buffer size */
-	uint32_t port;		/* hw port id (i.e. M4U port id) */
+	uint32_t blockOffset;	/* [IN]_b, block offset from handle(PA) to current block(plane) */
+	uint32_t offset;		/* [IN]_b, buffser offset to secure handle */
+	uint32_t size;			/* buffer size */
+	uint32_t port;			/* hw port id (i.e. M4U port id) */
 };
 
 struct cmdqSecDataStruct {
