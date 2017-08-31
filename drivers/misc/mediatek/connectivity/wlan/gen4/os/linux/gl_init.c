54d22aeb0c42a254835ba75fe2cf061f6945f494
rita.chan <rita.chan@mediatek.com>
2017-08-31 23:58:36 +0800

    [ALPS02907848] wlan: create a proc of setCAM
    [Detail]
    In VOIP case, disable powersave maybe improve VOIP delay.
    [Solution]
    Provide a API:
    In /proc/net/wlan, when wifi is on, we create a proc of setCAM.
    echo CAM 1 > setCAM means disable powersave.
    echo CAM 0 > setCAM means recover original state.
    
    MTK-Commit-Id: 46b7427313f2ebe6b3b27540d4c64e6ac8ffb782
    
    Change-Id: Ic530e335c26ecf5caece1c09405df93cacb7411b
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02907848
    Feature: others

--- drivers/misc/mediatek/connectivity/wlan/gen4/os/linux/gl_init.c
+++ drivers/misc/mediatek/connectivity/wlan/gen4/os/linux/gl_init.c
@@ -1968,6 +1968,63 @@ VOID wlanGetConfig(P_ADAPTER_T prAdapter)
 	}			/* pucConfigBuf */
 }
 
+VOID nicConfigProcSetCamCfgWrite(BOOLEAN enabled)
+{
+	struct net_device *prDev = NULL;
+	P_GLUE_INFO_T prGlueInfo = NULL;
+	P_ADAPTER_T prAdapter = NULL;
+	PARAM_POWER_MODE ePowerMode;
+	UINT_8 ucBssIndex;
+	CMD_PS_PROFILE_T rPowerSaveMode;
+
+	/* 4 <1> Sanity Check */
+	if ((u4WlanDevNum == 0) && (u4WlanDevNum > CFG_MAX_WLAN_DEVICES)) {
+		DBGLOG(INIT, ERROR, "wlanLateResume u4WlanDevNum==0 invalid!!\n");
+		return;
+	}
+
+	prDev = arWlanDevInfo[u4WlanDevNum - 1].prDev;
+	if (!prDev)
+		return;
+
+	prGlueInfo = *((P_GLUE_INFO_T *) netdev_priv(prDev));
+	if (!prGlueInfo)
+		return;
+
+	prAdapter = prGlueInfo->prAdapter;
+	if ((!prAdapter) || (!prAdapter->prAisBssInfo))
+		return;
+
+	ucBssIndex = prAdapter->prAisBssInfo->ucBssIndex;
+	if (ucBssIndex >= BSS_INFO_NUM)
+		return;
+	rPowerSaveMode.ucBssIndex = ucBssIndex;
+
+	if (enabled) {
+		prAdapter->rWlanInfo.fgEnSpecPwrMgt = TRUE;
+		ePowerMode = Param_PowerModeCAM;
+		rPowerSaveMode.ucPsProfile = (UINT_8) ePowerMode;
+		DBGLOG(INIT, INFO, "Enable CAM BssIndex:%d, PowerMode:%d\n",
+		       ucBssIndex, rPowerSaveMode.ucPsProfile);
+	} else {
+		prAdapter->rWlanInfo.fgEnSpecPwrMgt = FALSE;
+		rPowerSaveMode.ucPsProfile =
+				prAdapter->rWlanInfo.arPowerSaveMode[ucBssIndex].ucPsProfile;
+		DBGLOG(INIT, INFO, "Disable CAM BssIndex:%d, PowerMode:%d\n",
+		       ucBssIndex, rPowerSaveMode.ucPsProfile);
+	}
+
+	wlanSendSetQueryCmd(prAdapter,
+			    CMD_ID_POWER_SAVE_MODE,
+			    TRUE,
+			    FALSE,
+			    FALSE,
+			    NULL,
+			    NULL,
+			    sizeof(CMD_PS_PROFILE_T),
+			    (PUINT_8) & rPowerSaveMode,
+			    NULL, 0);
+}
 
 /*----------------------------------------------------------------------------*/
 /*!
