66c8c498706611d510eda538f7d39064b5a92a28
rita.chan <rita.chan@mediatek.com>
2017-08-31 23:56:09 +0800

    [ALPS02822358] PPM: change 1LL min freq to 741M
    [Detail] change 1LL min freq from 1200M to 741M
    
    MTK-Commit-Id: 4e47af1169a0cb3f96304852029d6011d23dfd44
    
    Change-Id: I2cab118a425cb9844e5f89d01846d5adce3f0cc0
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02822358
    Feature: CPU Hotplug

--- drivers/misc/mediatek/base/power/ppm_v2/src/mach/mt6799/mtk_ppm_platform.h
+++ drivers/misc/mediatek/base/power/ppm_v2/src/mach/mt6799/mtk_ppm_platform.h
@@ -67,7 +67,7 @@ extern unsigned int hps_get_bigtsk(unsigned int cluster_id);
 
 #define PPM_DISABLE_BIG_FOR_LP_MODE	(1)
 /*#define PPM_DISABLE_LL_ONLY		(1)*/
-#define PPM_1LL_MIN_FREQ		(1200000)
+#define PPM_1LL_MIN_FREQ		(741000)
 
 /* for COBRA algo */
 #define PPM_COBRA_USE_CORE_LIMIT	(1)
