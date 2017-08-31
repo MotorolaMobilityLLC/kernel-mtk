8205e023b8b43c6bfe8fcd85ce67e842f1af385b
rita.chan <rita.chan@mediatek.com>
2017-09-01 01:28:04 +0800

    [ALPS03185881] WIFI: WFD Disconnect (DBDC)
    [Detail]
    Abort DBDC band different with REQ DBDC band
    due to DBDC Switch send before driver abort
    and FW not switch done before abort send.
    [Solution]
    Abort with band_auto
    FW will auto find the correct REQ DBDC band
    even DBDC switch status unsync between driver & FW.
    
    MTK-Commit-Id: 9c709facb575b073fce481b866e7093407106405
    
    Change-Id: Idc29d391cd6c03a9a868a5e5c9dcadffc57325c9
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03185881
    Feature: [Android Default] Wi-Fi Driver for SoC
    (cherry picked from commit 16cf3c05bd716c339f1e944f27f012208166f785)

--- drivers/misc/mediatek/connectivity/wlan/gen4/mgmt/p2p_func.c
+++ drivers/misc/mediatek/connectivity/wlan/gen4/mgmt/p2p_func.c
@@ -1167,7 +1167,7 @@ VOID p2pFuncReleaseCh(IN P_ADAPTER_T prAdapter, IN UINT_8 ucBssIdx, IN P_P2P_CHN
 		prMsgChRelease->ucBssIndex = ucBssIdx;
 		prMsgChRelease->ucTokenID = prChnlReqInfo->ucSeqNumOfChReq++;
 #if CFG_SUPPORT_DBDC
-		prMsgChRelease->eDBDCBand = (prAdapter->aprBssInfo[ucBssIdx])->eDBDCBand;
+		prMsgChRelease->eDBDCBand = ENUM_BAND_AUTO;
 
 		DBGLOG(P2P, INFO,
 			"p2pFuncReleaseCh: P2P abort channel on band %u.\n",
@@ -1216,7 +1216,7 @@ VOID p2pFuncAcquireCh(IN P_ADAPTER_T prAdapter, IN UINT_8 ucBssIdx, IN P_P2P_CHN
 		prMsgChReq->ucRfCenterFreqSeg1 = prChnlReqInfo->ucCenterFreqS1;
 		prMsgChReq->ucRfCenterFreqSeg2 = prChnlReqInfo->ucCenterFreqS2;
 #if CFG_SUPPORT_DBDC
-		prMsgChReq->eDBDCBand = (prAdapter->aprBssInfo[ucBssIdx])->eDBDCBand;
+		prMsgChReq->eDBDCBand = ENUM_BAND_AUTO;
 
 		DBGLOG(P2P, INFO,
 		   "p2pFuncAcquireCh: P2P Request channel on band %u, tokenID: %d, cookie: 0x%llx.\n",
