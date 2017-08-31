97649c95e3d8a176615f107df12ef8ffaecf4c46
rita.chan <rita.chan@mediatek.com>
2017-09-01 00:19:36 +0800

    [ALPS03128088] Flashlight: update device for MT6759
    [Detail]
    1. Update flashlight device
    
    MTK-Commit-Id: 56db5de021a189c5b9cae42837c64e8a72dbf66e
    
    Change-Id: I8abff68be27802846ccdf1f27579b35dfbd76b71
    CR-Id: ALPS03128088
    Feature: [Android Default] Flashlight
    Signed-off-by: rita.chan <rita.chan@mediatek.com>

--- drivers/misc/mediatek/flashlight/flashlight-device.c
+++ drivers/misc/mediatek/flashlight/flashlight-device.c
@@ -42,14 +42,14 @@
 #elif defined(mt6759)
 const struct flashlight_device_id flashlight_id[] = {
 	/* {"NAME", TYPE, CT, PART} */
-	{"flashlights-rt5081", 0, 0, 0},
-	{"flashlights-rt5081", 0, 1, 0},
-	{"flashlights-none", 1, 0, 0},
-	{"flashlights-none", 1, 1, 0},
-	{"flashlights-none", 0, 0, 1},
-	{"flashlights-none", 0, 1, 1},
-	{"flashlights-none", 1, 0, 1},
-	{"flashlights-none", 1, 1, 1},
+	{0, 0, 0, "flashlights-rt5081", 0, 0},
+	{0, 1, 0, "flashlights-rt5081", 1, 0},
+	{1, 0, 0, "flashlights-none", -1, 0},
+	{1, 1, 0, "flashlights-none", -1, 0},
+	{0, 0, 1, "flashlights-none", -1, 0},
+	{0, 1, 1, "flashlights-none", -1, 0},
+	{1, 0, 1, "flashlights-none", -1, 0},
+	{1, 1, 1, "flashlights-none", -1, 0},
 };
 #elif defined(mt6799)
 const struct flashlight_device_id flashlight_id[] = {
