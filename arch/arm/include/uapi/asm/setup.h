47d26d9f4813ff843b8ca51110d1be4c02fabd03
rita.chan <rita.chan@mediatek.com>
2017-08-31 21:15:05 +0800

    [ALPS02687587] mem: add mem module
    Add mem module to provide basic memory APIs
    
    MTK-Commit-Id: 69007391c41ac783de12a1d6fac0b7d23c0c6cf1
    
    Change-Id: I4c67d46b5b6f572d72b8dda78d06ebfce8d9cf20
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02687587
    Feature: Large DRAM

--- arch/arm/include/uapi/asm/setup.h
+++ arch/arm/include/uapi/asm/setup.h
@@ -143,6 +143,29 @@ struct tag_memclk {
 	__u32 fmemclk;
 };
 
+/* general memory descriptor */
+struct mem_desc {
+	u64 start;
+	u64 size;
+};
+
+/* mblock is used by CPU */
+struct mblock {
+	u64 start;
+	u64 size;
+	u32 rank;	/* rank the mblock belongs to */
+};
+
+struct mblock_info {
+	u32 mblock_num;
+	struct mblock mblock[4];
+};
+
+struct dram_info {
+	u32 rank_num;
+	struct mem_desc rank_info[4];
+};
+
 struct tag {
 	struct tag_header hdr;
 	union {
