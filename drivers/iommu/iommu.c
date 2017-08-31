65dc1ca4a4895c149647aa152d23864246591adc
rita.chan <rita.chan@mediatek.com>
2017-08-31 22:57:55 +0800

    [ALPS02767202] DMA: cal dm mapping after device attached
    [Detail]
    The interface iommu_group_create_direct_mappings should be called
    after device attached, for mtk iommu, the domain_finalize was called
    in device attatch, the mtk iommu iopgt ops was allocated and initialized
    in domain_finalize, the iommu_group_create_direct_mappings would call
    the map interface to implement the map. If its earlier than device
    attach, there would be NULL dereference.
    
    [Solution]
    Call iommu_group_create_direct_mappings after device attached.
    
    MTK-Commit-Id: 9c2f9f75ff64ccc0a8dcf8a70ae1b5c463e2419e
    
    Change-Id: I4ad9bca05f7589f10b5b64485b2be9e88d2a949c
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02767202
    Feature: DMA

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -427,13 +427,12 @@ rename:
 
 	dev->iommu_group = group;
 
-	iommu_group_create_direct_mappings(group, dev);
-
 	mutex_lock(&group->mutex);
 	list_add_tail(&device->list, &group->devices);
 	if (group->domain)
 		__iommu_attach_device(group->domain, dev);
 	mutex_unlock(&group->mutex);
+	iommu_group_create_direct_mappings(group, dev);
 
 	/* Notify any listeners about change to group. */
 	blocking_notifier_call_chain(&group->notifier,
