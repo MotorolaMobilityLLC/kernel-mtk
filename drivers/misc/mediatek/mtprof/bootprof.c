853e06d2dcc0c8ddd29d05428297e0e327d4cdc3
rita.chan <rita.chan@mediatek.com>
2017-09-01 00:08:18 +0800

    [ALPS03076506] bootprof: Add first logo duration
    [Detail] add first logo duration of LK into bootprof
    
    [Solution] add first logo duration of LK in bootprof
    
    MTK-Commit-Id: 4bb996eac0cb377972bfde28ee55052e242e2181
    
    Change-Id: I749f822b300b967b1951150e2739f6ed4ea96e92
    CR-Id: ALPS03076506
    Feature: Others
    Signed-off-by: rita.chan <rita.chan@mediatek.com>

--- drivers/misc/mediatek/mtprof/bootprof.c
+++ drivers/misc/mediatek/mtprof/bootprof.c
@@ -50,12 +50,13 @@ static struct log_t *bootprof[BUF_COUNT];
 static unsigned long log_count;
 static DEFINE_MUTEX(bootprof_lock);
 static bool enabled;
-static int bootprof_lk_t, bootprof_pl_t;
+static int bootprof_lk_t, bootprof_pl_t, bootprof_logo_t;
 static u64 timestamp_on, timestamp_off;
 bool boot_finish;
 
 module_param_named(pl_t, bootprof_pl_t, int, S_IRUGO | S_IWUSR);
 module_param_named(lk_t, bootprof_lk_t, int, S_IRUGO | S_IWUSR);
+module_param_named(logo_t, bootprof_logo_t, int, S_IRUGO | S_IWUSR);
 
 #define MSG_SIZE 128
 
@@ -241,6 +242,8 @@ static int mt_bootprof_show(struct seq_file *m, void *v)
 
 	if (bootprof_pl_t > 0 && bootprof_lk_t > 0) {
 		SEQ_printf(m, "%10d        : %s\n", bootprof_pl_t, "preloader");
+		if (bootprof_logo_t > 0)
+			SEQ_printf(m, "%10d        : %s\n", bootprof_logo_t, "first logo");
 		SEQ_printf(m, "%10d        : %s\n", bootprof_lk_t, "lk");
 		/* SEQ_printf(m, "%10d        : %s\n",
 		 * gpt_boot_time() - bootprof_pl_t - bootprof_lk_t, "lk->Kernel");
