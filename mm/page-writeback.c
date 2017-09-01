279c8e5e88d4b17c85c69fc1a28d33364d12539f
rita.chan <rita.chan@mediatek.com>
2017-09-01 15:53:15 +0800

    [ALPS03073362] mm: check vmstat snapshot before congestion_wait
    Check snapshot of global vmstat for the following 2 cases,
          1. too_many_isolated in shrink_inactive_list
          2. throttle_vm_writeout
    
    MTK-Commit-Id: bccff33dbbc3bcdf36630ba01fbd85cbc2463e5f
    
    Change-Id: If12be016b330dece530186702af9420dc7cddf53
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03073362
    Feature: Memory Optimization

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -1621,6 +1621,22 @@ void balance_dirty_pages_ratelimited(struct address_space *mapping)
 }
 EXPORT_SYMBOL(balance_dirty_pages_ratelimited);
 
+/*
+ * Check vmstat snapshot for throttling of vm_writeout
+ */
+static unsigned long check_vm_writeout_snapshot(void)
+{
+	struct zone *z;
+	unsigned long nr_vm_writeout = 0;
+
+	for_each_populated_zone(z) {
+		nr_vm_writeout += zone_page_state_snapshot(z, NR_WRITEBACK);
+		nr_vm_writeout += zone_page_state_snapshot(z, NR_UNSTABLE_NFS);
+	}
+
+	return nr_vm_writeout;
+}
+
 void throttle_vm_writeout(gfp_t gfp_mask)
 {
 	unsigned long background_thresh;
@@ -1639,6 +1655,14 @@ void throttle_vm_writeout(gfp_t gfp_mask)
                 if (global_page_state(NR_UNSTABLE_NFS) +
 			global_page_state(NR_WRITEBACK) <= dirty_thresh)
                         	break;
+
+		/*
+		 * Take a deeper look at NR_WRITEBACK & NR_UNSTABLE_NFS
+		 * before entering congestion_wait.
+		 */
+		if (check_vm_writeout_snapshot() <= dirty_thresh)
+			break;
+
                 congestion_wait(BLK_RW_ASYNC, HZ/10);
 
 		/*
