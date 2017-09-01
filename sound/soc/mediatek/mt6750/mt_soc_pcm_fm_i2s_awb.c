e4d84b4c265d5aeade0052bee129638a28120f42
rita.chan <rita.chan@mediatek.com>
2017-09-01 16:19:24 +0800

    [ALPS02349266] ASoC: fix coding defect
    [Detail]
    16537: Structurally dead code
    
    MTK-Commit-Id: 5ae962ce1f1905a28ed839d01d37dccb2cadef94
    
    Change-Id: I75c9d77ccaa7ad33a72179b94b2a92ab256907ad
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS02349266
    Feature: Phone Sound.

--- sound/soc/mediatek/mt6750/mt_soc_pcm_fm_i2s_awb.c
+++ sound/soc/mediatek/mt6750/mt_soc_pcm_fm_i2s_awb.c
@@ -193,12 +193,9 @@ static int mtk_fm_i2s_awb_alsa_stop(struct snd_pcm_substream *substream)
 	return 0;
 }
 
-static kal_int32 Previous_Hw_cur;
 static snd_pcm_uframes_t mtk_awb_pcm_pointer(struct snd_pcm_substream
 					     *substream)
 {
-	kal_int32 HW_memory_index = 0;
-	kal_int32 HW_Cur_ReadIdx = 0;
 	kal_uint32 Frameidx = 0;
 	AFE_BLOCK_T *Awb_Block = &(FM_I2S_AWB_Control_context->rBlock);
 
@@ -208,17 +205,6 @@ static snd_pcm_uframes_t mtk_awb_pcm_pointer(struct snd_pcm_substream
 		/* get total bytes to copysinewavetohdmi */
 		Frameidx = audio_bytes_to_frame(substream , Awb_Block->u4WriteIdx);
 		return Frameidx;
-
-		HW_Cur_ReadIdx = Align64ByteSize(Afe_Get_Reg(AFE_AWB_CUR));
-		if (HW_Cur_ReadIdx == 0) {
-			pr_warn("[Auddrv] mtk_awb_pcm_pointer  HW_Cur_ReadIdx ==0\n");
-			HW_Cur_ReadIdx = Awb_Block->pucPhysBufAddr;
-		}
-		HW_memory_index = (HW_Cur_ReadIdx - Awb_Block->pucPhysBufAddr);
-		Previous_Hw_cur = HW_memory_index;
-		PRINTK_AUD_AWB("[Auddrv] mtk_awb_pcm_pointer =0x%x HW_memory_index = 0x%x\n",
-			       HW_Cur_ReadIdx, HW_memory_index);
-		return audio_bytes_to_frame(substream, Previous_Hw_cur);
 	}
 	return 0;
 }
