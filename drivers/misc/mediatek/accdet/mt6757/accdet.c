29704d7d3e34a3b3ddb1de7f043b4e993e6f1006
rita.chan <rita.chan@mediatek.com>
2017-09-14 15:46:11 +0800

    [ALPS03501705] Accdet: headset icon seldom doesnt disappear
    [Detail]
    when plug 4-pole out ,if AB=3 & AB=0 happens, 3-pole plug in
    will be incorrectly reported; then 3-pole plug-out is reported.
    no 4-pole plug-out, so icon doesnt disappear.
    
    [Solution]
    alwayse do 4-pole plug-out when 3-pole plug-out.
    
    MTK-Commit-Id: fae617e4ef58da6738dbee53755a431413c673e8
    
    Change-Id: Iaa0be9f10008b86114f6129d29228b56868b7394
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03501705
    Feature: Headset Cable/Button Detection and Reaction

--- drivers/misc/mediatek/accdet/mt6757/accdet.c
+++ drivers/misc/mediatek/accdet/mt6757/accdet.c
@@ -627,12 +627,20 @@ static void send_accdet_status_event(int cable, int status)
 	switch (cable) {
 	case HEADSET_NO_MIC:
 		input_report_switch(kpd_accdet_dev, SW_HEADPHONE_INSERT, status);
+		/* when plug 4-pole out, if both AB=3 AB=0 happen,3-pole plug in will be incorrectly reported,
+		* then 3-pole plug-out is reported, if no mantory 4-pole plug-out,icon would be visible.
+		*/
+		if (status == 0)
+			input_report_switch(kpd_accdet_dev, SW_MICROPHONE_INSERT, status);
+
 		input_report_switch(kpd_accdet_dev, SW_JACK_PHYSICAL_INSERT, status);
 		input_sync(kpd_accdet_dev);
 		ACCDET_DEBUG("HEADPHONE(3-pole) %s\n", status?"PlugIn":"PlugOut");
 		break;
 	case HEADSET_MIC:
-		input_report_switch(kpd_accdet_dev, SW_HEADPHONE_INSERT, status);
+		/* when plug 4-pole out, 3-pole plug out should also be reported for slow plug-in case */
+		if (status == 0)
+			input_report_switch(kpd_accdet_dev, SW_HEADPHONE_INSERT, status);
 		input_report_switch(kpd_accdet_dev, SW_MICROPHONE_INSERT, status);
 		input_report_switch(kpd_accdet_dev, SW_JACK_PHYSICAL_INSERT, status);
 		input_sync(kpd_accdet_dev);
