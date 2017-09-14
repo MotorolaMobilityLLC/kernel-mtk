83b106fa5e96fff9fdd09a808d5ae87f953270bc
rita.chan <rita.chan@mediatek.com>
2017-09-14 15:41:34 +0800

    [ALPS03441898] wlan gen2: Notify FW PM abort when deauth TX done
    1. Notify FW power management abort only when deauth TX done.
    2. Setup a timer for deauth frame.
    
    MTK-Commit-Id: 400e0288aeae85296c13029a137bc57b35b2beca
    
    Change-Id: I890ac4921e68f7c90c5e679dd54f36053ea36223
    CR-Id: ALPS03441898
    Feature: Wi-Fi Direct
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    (cherry picked from commit 698e821959dac98d4989c04221862c020c8e482f)

--- drivers/misc/mediatek/connectivity/wlan/gen2/nic/nic.c
+++ drivers/misc/mediatek/connectivity/wlan/gen2/nic/nic.c
@@ -1492,6 +1492,7 @@ WLAN_STATUS nicUpdateBss(IN P_ADAPTER_T prAdapter, IN ENUM_NETWORK_TYPE_INDEX_T 
 	else
 		rCmdSetBssInfo.ucStaRecIdxOfAP = STA_REC_INDEX_NOT_FOUND;
 
+	DBGLOG(NIC, INFO, "nicUpdateBss eNetworkTypeIdx: %d\n", eNetworkTypeIdx);
 	u4Status = wlanSendSetQueryCmd(prAdapter,
 				       CMD_ID_SET_BSS_INFO,
 				       TRUE,
@@ -1639,6 +1640,7 @@ WLAN_STATUS nicPmIndicateBssAbort(IN P_ADAPTER_T prAdapter, IN ENUM_NETWORK_TYPE
 
 	rCmdIndicatePmBssAbort.ucNetTypeIndex = (UINT_8) eNetworkTypeIdx;
 
+	DBGLOG(NIC, INFO, "nicPmIndicateBssAbort eNetTypeIndex:%d\n", eNetworkTypeIdx);
 	return wlanSendSetQueryCmd(prAdapter,
 				   CMD_ID_INDICATE_PM_BSS_ABORT,
 				   TRUE,
