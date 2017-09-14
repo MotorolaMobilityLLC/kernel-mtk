768949b3522336910a540317e8ac4f1b62c9ff96
rita.chan <rita.chan@mediatek.com>
2017-09-14 15:46:18 +0800

    [ALPS03503530] Biosensor: update biosensor driver
    [Detail]
    Revise driver for sensors-1.0 architecture
    Support factory test and calibration
    
    MTK-Commit-Id: 22b25e53fb1fc4a5d7bf9a25a9962eadbf9d55d5
    
    Change-Id: I107083f99628f3bdbaa30397c831923b52955a90
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03503530
    Feature: Biosensor

--- drivers/misc/mediatek/sensors-1.0/biometric/mt6381/ppg_control_lib_setting.h
+++ drivers/misc/mediatek/sensors-1.0/biometric/mt6381/ppg_control_lib_setting.h
@@ -22,46 +22,29 @@
 #define PPG_CTRL_VER_CODE_1     1
 #define PPG_CTRL_VER_CODE_2     0
 #define PPG_CTRL_VER_CODE_3     0
-#define PPG_CTRL_VER_CODE_4     3
+#define PPG_CTRL_VER_CODE_4     4
 
 /* Configure */
 #define PPG_CTRL_CH_MAX 2
 
-/* TIA */
-#define PPG_TIA_GAIN_500K 0
-#define PPG_TIA_GAIN_250K 1
-#define PPG_TIA_GAIN_100K 2
-#define PPG_TIA_GAIN_050K 3
-#define PPG_TIA_GAIN_025K 4
-#define PPG_TIA_GAIN_010K 5
-#define PPG_TIA_GAIN_001M 6
-
 /* DC edge */
 #define PPG_DC_POS_EDGE     30240 /* The real PPG saturation edge (1476mV) */
 #define PPG_DC_NEG_EDGE    -32260 /* The real PPG saturation edge (-1575mV) */
-#define PPG_DC_MAXP         27000 /* upper tuning bound */
-#define PPG_DC_MAXN        -29000 /* lower tuning bound */
+#define PPG_DC_MAXP         20000 /* upper tuning bound */
+#define PPG_DC_MAXN        -20000 /* lower tuning bound */
 
 /* minimal/maximal range */
-#define PPG_MAX_LED_CURRENT     96  /* IC MAX: 255 */
+#define PPG_MAX_LED_CURRENT     80  /* IC MAX: 255 */
 #define PPG_MIN_LED_CURRENT     8   /* IC MIN: 1 */
-#define PPG_MAX_TIA_GAIN        PPG_TIA_GAIN_100K /* IC MAX: 1M */
-#define PPG_MIN_TIA_GAIN        PPG_TIA_GAIN_100K /* IC MIN: 10K */
-#define PPG_MAX_AMBDAC_CURRENT  7   /* IC MAX: 7 -> 6.5uA */
-#define PPG_MIN_AMBDAC_CURRENT  1   /* IC MIN: 0uA */
 
 /* initial setting */
-#define PPG_INIT_LED1       40 /*0x25 */
+#define PPG_INIT_LED1       40 /* 0x25 */
 #define PPG_INIT_LED2       40 /* 0x2A */
-#define PPG_INIT_AMBDAC1    PPG_MAX_AMBDAC_CURRENT /* LED phase */
-#define PPG_INIT_AMBDAC2    PPG_MIN_AMBDAC_CURRENT /* AMB phase */
-#define PPG_INIT_TIA1       PPG_TIA_GAIN_100K
-#define PPG_INIT_TIA2       PPG_TIA_GAIN_100K
 
 /* ppg_control parameter */
 #define PPG_CTRL_FS_OPERATE 16
 #define PPG_CTRL_BUF_SIZE (PPG_CTRL_FS_OPERATE) /* 1.0-sec buffer */
-#define PPG_SATURATE_HANDLE_COUNT ((PPG_CTRL_FS_OPERATE >> 2) - 1)
+#define PPG_SATURATE_HANDLE_COUNT (PPG_CTRL_FS_OPERATE >> 2)
 
 #define PPG_DC_ENLARGE_BOUND (PPG_DC_POS_EDGE>>2)
 #define PPG_AC_TARGET        256
@@ -75,7 +58,7 @@
 #define PPG_LED_STEP_MIN    2
 
 /* Timer */
-#define PPG_CONTROL_TIME_LIMIT 14
+#define PPG_CONTROL_TIME_LIMIT 5
 #define PPG_CONTROL_ALWAYS_ON (-1)
 
 /* States */
