1d0bd38260bd6f64c73b747eed4a29087b248cd0
rita.chan <rita.chan@mediatek.com>
2017-09-14 15:22:00 +0800

    [ALPS03470412] FMRadio: YGPS signal will lost when FM off
    [Detail]
    When GPS is running in background, and FM is playing in foreground.
    Currently, stop FM radio but GPS signal will be lost immediately.
    
    [Solution]
    DO NOT clear top_clk_en_adie bit when FM is in power down stage
    
    MTK-Commit-Id: 302e30048c9294f69a514024ed5208d8ce23e937
    
    Change-Id: Idaadc23fb98733d612becb3bc5ba79869374d956
    Signed-off-by: rita.chan <rita.chan@mediatek.com>
    CR-Id: ALPS03470412
    Feature: FM Radio
    (cherry picked from commit d98fe36da9b3bb1805db8c5f325ce6bac558a085)

--- drivers/misc/mediatek/connectivity/fmradio/mt6627/pub/mt6627_fm_lib.c
+++ drivers/misc/mediatek/connectivity/fmradio/mt6627/pub/mt6627_fm_lib.c
@@ -875,6 +875,7 @@ static signed int mt6627_PowerDown(void)
 	unsigned int tem = 0;
 #if	defined(MT6625_FM)
 	unsigned int host_reg = 0;
+	WCN_DBG(FM_DBG | CHIP, "pwr down seq, but not clear top_clk_en_adie\n");
 #endif
 
 	WCN_DBG(FM_DBG | CHIP, "pwr down seq\n");
@@ -912,25 +913,28 @@ static signed int mt6627_PowerDown(void)
 	fm_reg_write(FM_MAIN_EXTINTRMASK, 0x00);
 
 #if	defined(MT6625_FM)
-	ret = fm_host_reg_read(0x80101030, &host_reg);
+	/* ret = fm_host_reg_read(0x80101030, &host_reg);
 	if (ret) {
 		WCN_DBG(FM_ALT | CHIP, " pwroff read 0x80100030 failed\n");
 		return ret;
 	}
+	WCN_DBG(FM_DBG | CHIP, "read host reg 0x80101030=%x\n", host_reg);
 	ret = fm_host_reg_write(0x80101030, host_reg & (~(0x1 << 1)));
 	if (ret) {
 		WCN_DBG(FM_ALT | CHIP, " pwroff disable top_ck_en_adie failed\n");
 		return ret;
 	}
+	*/
 
 	ret = fm_host_reg_read(0x80000224, &host_reg);
 	if (ret) {
-		WCN_DBG(FM_ALT | CHIP, " pwrup read 0x80000224 failed\n");
+		WCN_DBG(FM_ALT | CHIP, " pwroff read 0x80000224 failed\n");
 		return ret;
 	}
+	WCN_DBG(FM_DBG | CHIP, "read host reg 0x80000224=%x\n", host_reg);
 	ret = fm_host_reg_write(0x80000224, host_reg & (~(1 << 16)));
 	if (ret) {
-		WCN_DBG(FM_ALT | CHIP, " pwrup conn_srcclkena switch failed\n");
+		WCN_DBG(FM_ALT | CHIP, " pwroff conn_srcclkena switch failed\n");
 		return ret;
 	}
 #endif
