e4c42c8c9d67a0a1aaa0e0ae8c4ebdd63f032c8f
mtk05337 <chieh-jay.liu@mediatek.com>
2017-09-27 09:31:40 +0800

    [ALPS03479100] DFD: make DFD enable
    [Detail]
    Enable DFD with EFUSE check
    
    MTK-Commit-Id: 016b3a5498d475a00cfbc594447b374ab7d6a170
    
    Change-Id: I3777c97b101f57ffa87a54955ab7d248f0f59ffa
    Signed-off-by: mtk05337 <chieh-jay.liu@mediatek.com>
    CR-Id: ALPS03479100
    Feature: Others

--- drivers/misc/mediatek/dfd/mt6757/plat_dfd.c
+++ drivers/misc/mediatek/dfd/mt6757/plat_dfd.c
@@ -11,7 +11,26 @@
  * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
 
+#include <mt-plat/mtk_devinfo.h>
+#include <mt-plat/mtk_platform_debug.h>
+#include "../dfd.h"
+#include <linux/printk.h>
+
+static unsigned int is_kibop;
+
 unsigned int check_dfd_support(void)
 {
+	unsigned int segment;
+
+	is_kibop = 0;
+	segment = (get_devinfo_with_index(30) & 0x000000E0) >> 5;
+	if (segment == 0x3 || segment == 0x7) {
+		is_kibop = 1;
+		pr_notice("[dfd] Kibo+ enable DFD\n");
+		return 1;
+	}
+
+	pr_notice("[dfd] Kibo disable DFD,0x%x\n", segment);
 	return 0;
+
 }
