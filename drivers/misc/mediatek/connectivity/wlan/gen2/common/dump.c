5a5d7ac27ee4aa6e5541b6ba0fac6d3c285e4666
Wayne-TW <wayne-tw.chen@mediatek.com>
2017-09-27 09:47:01 +0800

    [ALPS03510294] wlan: log too much when roaming
    [Detail]
    When Roamming fail before ingoring, driver will
    have a for loop to continuously print logs.
    
    [Solution]
    align the debug method of kernel-4.4 and remove
    the logs printing.
    
    MTK-Commit-Id: 7070fae76e80143520bee78ed0b84584585f12a5
    
    CR-Id: ALPS03510294
    Feature: Wifi Driver
    Signed-off-by: Wayne-TW <wayne-tw.chen@mediatek.com>
    (cherry picked from commit 9cb3070c29c25cf20684367b0c532f2f92a7ac0b)
    
    Change-Id: I2138db0a51a1db659f229d999c15432afecb036f

--- drivers/misc/mediatek/connectivity/wlan/gen2/common/dump.c
+++ drivers/misc/mediatek/connectivity/wlan/gen2/common/dump.c
@@ -89,13 +89,9 @@ VOID dumpMemory8IEOneLine(IN PUINT_8 pString, IN PUINT_8 pucStartAddr, IN UINT_3
 	if (u4Length > CFG_IE_BUFFER_SIZE)
 		u4Length = CFG_IE_BUFFER_SIZE;
 
-	for (i = 0 ; i < u4Length ; i++) {
+	for (i = 0 ; i < u4Length ; i++)
 		offsetMsg += kalSnprintf(pucMsg + offsetMsg, BUF_MSG_LENGTH - offsetMsg, "%02x,", pucStartAddr[i]);
-		if (offsetMsg >= BUF_MSG_LENGTH) {
-			LOG_FUNC("%s\n", pucMsg);
-			offsetMsg = 0;
-		}
-	}
+
 	offsetMsg += kalSnprintf(pucMsg + offsetMsg, BUF_MSG_LENGTH - offsetMsg, "%s", "]");
 
 	LOG_FUNC("%s\n", pucMsg);
