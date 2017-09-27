c28a055ca6868d02c885245d2bbee1d223302537
mtk08242 <kiki.ou@mediatek.com>
2017-09-27 09:46:20 +0800

    [ALPS03443740] wlan: kernel patch for abort scan
    [Detail]
    cfg80211: Add support for aborting an ongoing scan
    Implement new functionality for aborting an ongoing scan.
    
    Add NL80211_CMD_ABORT_SCAN to the nl80211 interface. After
    aborting the scan, driver shall provide the scan status by
    calling cfg80211_scan_done().
    
    [Solution]
    Add NL80211_CMD_ABORT_SCAN to the nl80211 interface. After
    aborting the scan, driver shall provide the scan status by
    calling cfg80211_scan_done().
    
    MTK-Commit-Id: 9dc874801c43699caa06b8e5d79b16d6d3778eac
    
    Change-Id: I4512f8c9b88261a1a196983ca1d487cf5cbdc899
    Signed-off-by: Kiki Ou <kiki.ou@mediatek.com>
    CR-Id: ALPS03443740
    Feature: Wi-Fi Chipset Capability

--- include/uapi/linux/nl80211.h
+++ include/uapi/linux/nl80211.h
@@ -738,6 +738,10 @@
  *	before removing a station entry entirely, or before disassociating
  *	or similar, cleanup will happen in the driver/device in this case.
  *
+ * @NL80211_CMD_ABORT_SCAN: Stop an ongoing scan. Returns -ENOENT if a scan is
+ *	not running. The driver indicates the status of the scan through
+ *	cfg80211_scan_done().
+ *
  * @NL80211_CMD_MAX: highest used command number
  * @__NL80211_CMD_AFTER_LAST: internal use
  */
@@ -911,6 +915,19 @@ enum nl80211_commands {
 
 	NL80211_CMD_ADD_TX_TS,
 	NL80211_CMD_DEL_TX_TS,
+
+	NL80211_CMD_GET_MPP,
+
+	NL80211_CMD_JOIN_OCB,
+	NL80211_CMD_LEAVE_OCB,
+
+	NL80211_CMD_CH_SWITCH_STARTED_NOTIFY,
+
+	NL80211_CMD_TDLS_CHANNEL_SWITCH,
+	NL80211_CMD_TDLS_CANCEL_CHANNEL_SWITCH,
+
+	NL80211_CMD_WIPHY_REG_CHANGE,
+	NL80211_CMD_ABORT_SCAN,
 
 	/* add new commands above here */
 
