ce6ba5b29102f389112c58cc312949f59eda9e3f
Frank Lin <frank.lin@mediatek.com>
2017-09-27 09:48:10 +0800

    [ALPS03520813] hps: fix syzkaller security hole
    [Detail]
    * hps would crash to trigger hardware reboot(HWT) if the procfs
      node of hps have been written outrange value
    
    [Solution]
    * ensure the value written from userland
    * the platforms to be patched
      hps/v1
      hps/v2
      mt6757
      mt6735
    
    MTK-Commit-Id: 4017a6f08dd1c985575552b74d4f33b8f9144f65
    
    Change-Id: I9a838f57b07505de019f66f5cfa15206331cb1ae
    CR-Id: ALPS03520813
    Feature: CPU Hotplug
    Signed-off-by: Frank Lin <frank.lin@mediatek.com>

--- drivers/misc/mediatek/base/power/mt6735/mt_hotplug_strategy_procfs.c
+++ drivers/misc/mediatek/base/power/mt6735/mt_hotplug_strategy_procfs.c
@@ -1,16 +1,16 @@
 /*
- * Copyright (c) 2015 MediaTek Inc.
+ * Copyright (C) 2016 MediaTek Inc.
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
@@ -30,37 +30,87 @@ static int hps_proc_uint_show(struct seq_file *m, void *v)
 	return 0;
 }
 
-static ssize_t hps_proc_uint_write(
-		struct file *file, const char __user *buffer,
-		size_t count, loff_t *pos,
-		func_void before_write, func_void after_write)
+static int hps_sanitize_var(unsigned int *hpsvar, unsigned int newv)
 {
-	int len = 0;
-	char desc[32];
-	unsigned int var;
-	unsigned int *pv;
+	int rc = -EINVAL;
 
-	pv = (unsigned int *)((struct seq_file *)file->private_data)->private;
-	len = min(count, sizeof(desc) - 1);
-	if (copy_from_user(desc, buffer, len))
-		return 0;
-	desc[len] = '\0';
-
-	if (!kstrtouint(desc, 0, &var)) {
-		if (before_write)
-			before_write();
-
-		*pv = var;
-
-		if (after_write)
-			after_write();
-
-		return count;
+	if (hpsvar == &hps_ctxt.enabled) {
+		if (newv >= 0 && newv <= 1)
+			rc = 0;
+	} else if (hpsvar == &hps_ctxt.suspend_enabled) {
+		if (newv >= 0 && newv <= 1)
+			rc = 0;
+	} else if (hpsvar == &hps_ctxt.cur_dump_enabled) {
+		if (newv >= 0 && newv <= 1)
+			rc = 0;
+	} else if (hpsvar == &hps_ctxt.stats_dump_enabled) {
+		if (newv >= 0 && newv <= 1)
+			rc = 0;
+	} else if (hpsvar == &hps_ctxt.up_threshold) {
+		if (newv > 0)
+			rc = 0;
+	} else if (hpsvar == &hps_ctxt.up_times) {
+		if (newv > 0 && newv <= (unsigned int)MAX_CPU_UP_TIMES)
+			rc = 0;
+	} else if (hpsvar == &hps_ctxt.down_threshold) {
+		if (newv > 0)
+			rc = 0;
+	} else if (hpsvar == &hps_ctxt.down_times) {
+		if (newv > 0 && newv <= (unsigned int)MAX_CPU_DOWN_TIMES)
+			rc = 0;
+	} else if (hpsvar == &hps_ctxt.input_boost_enabled) {
+		if (newv >= 0 && newv <= 1)
+			rc = 0;
+	} else if (hpsvar == &hps_ctxt.input_boost_cpu_num) {
+		/* NOTE: what is the maximum? */
+		if (newv > 0)
+			rc = 0;
+	} else if (hpsvar == &hps_ctxt.rush_boost_enabled) {
+		if (newv >= 0 && newv <= 1)
+			rc = 0;
+	} else if (hpsvar == &hps_ctxt.rush_boost_threshold) {
+		if (newv > 0)
+			rc = 0;
+	} else if (hpsvar == &hps_ctxt.rush_boost_times) {
+		if (newv > 0)
+			rc = 0;
+	} else if (hpsvar == &hps_ctxt.tlp_times) {
+		if (newv > 0 && newv <= (unsigned int)MAX_TLP_TIMES)
+			rc = 0;
+	} else {
+		hps_warn("Unknown hps procfs node\n");
+		rc = -EINVAL;
 	}
 
-	hps_warn("%s(): bad argument\n", __func__);
+	return rc;
+}
 
-	return -EINVAL;
+static ssize_t hps_proc_uint_write(struct file *file, const char __user *buffer,
+				   size_t count, loff_t *pos,
+				   func_void before_write, func_void after_write)
+{
+	unsigned int var;
+	unsigned int *pv;
+	int rc;
+
+	rc = kstrtouint_from_user(buffer, count, 0, &var);
+	if (rc)
+		return rc;
+
+	pv = ((struct seq_file *)file->private_data)->private;
+	rc = hps_sanitize_var(pv, var);
+	if (rc)
+		return rc;
+
+	if (before_write)
+		before_write();
+
+	*pv = var;
+
+	if (after_write)
+		after_write();
+
+	return count;
 }
 
 static void lock_hps_ctxt(void)
