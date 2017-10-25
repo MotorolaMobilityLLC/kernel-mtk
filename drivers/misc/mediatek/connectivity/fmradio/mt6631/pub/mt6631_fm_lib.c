3026fd4905fb29905850cf99d06ef1fc016a0187
Black Chen <black-ch.chen@mediatek.com>
2017-10-25 16:09:51 +0800

    [ALPS02815148] FMRadio: fix converity issue
    [Detail]
    [Coverity Scanned Code Defect] Logically dead code
    91053
    
    [Solution]
    remove dead code
    
    MTK-Commit-Id: afd2b055f9dff54d8e8f413825b0444851f720f3
    
    Change-Id: I3de25aebd7012f0ef14745036b1ad7e412fbfae5
    Signed-off-by: Black Chen <black-ch.chen@mediatek.com>
    CR-Id: ALPS02815148
    Feature: FM Radio

--- drivers/misc/mediatek/connectivity/fmradio/mt6631/pub/mt6631_fm_lib.c
+++ drivers/misc/mediatek/connectivity/fmradio/mt6631/pub/mt6631_fm_lib.c
@@ -734,11 +734,6 @@ static signed int mt6631_pwrup_DSP_download(struct fm_patch_tbl *patch_tbl)
 	mt6631_hw_info.patch_ver = (signed int) tmp_reg;
 	WCN_DBG(FM_NTC | CHIP, "Patch version: 0x%08x\n", mt6631_hw_info.patch_ver);
 
-	if (ret == 1) {
-		dsp_buf[4] = 0x00;	/* if we found rom version undefined, we should disable patch */
-		dsp_buf[5] = 0x00;
-	}
-
 	ret = fm_download_patch((const unsigned char *)dsp_buf, patch_len, IMG_COEFFICIENT);
 	if (ret) {
 		WCN_DBG(FM_ALT | CHIP, " Download DSP coefficient failed\n");
