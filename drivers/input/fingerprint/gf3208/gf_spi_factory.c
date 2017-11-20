19f96b40e10bf578e5a806bf319e912c9f41894e
wuzhipeng <wuzhipeng@longcheer.com>
2017-11-20 16:28:37 +0800

    [Purp][Marino][Fingerprint] 添加指纹驱动及dts配置
    Change-Id: Ic07ea997d01020721b5910b3492be6e3bd941748

---
+++ drivers/input/fingerprint/gf3208/gf_spi_factory.c
@@ -0,0 +1,27 @@
+#include <linux/device.h>
+#include <linux/input.h>
+#include <linux/mutex.h>
+#include <linux/io.h>
+#include <linux/gpio.h>
+#include <linux/fb.h>
+#include <linux/clk.h>
+#include <net/sock.h>
+#include <linux/spi/spi.h>
+#include <linux/spi/spidev.h>
+
+/* MTK header */
+#include "mtk_spi.h"
+#include "mtk_spi_hal.h"
+#include "mtk_gpio.h"
+#include "mach/gpio_const.h"
+
+#include "gf_spi_tee.h"
+
+int  gf_ioctl_spi_init_cfg_cmd(struct mt_chip_conf *mcc, unsigned long arg){
+    return 0;
+}
+
+int gf_ioctl_transfer_raw_cmd(struct gf_device *gf_dev, unsigned long arg,unsigned int bufsiz){
+    return 0;
+}
+
