d092ca1e30e96b087cb1bf5072348707d34343c5
Qiangming Xia <qiangming.xia@mediatek.com>
2017-12-07 09:29:19 +0800

    [ALPS03632156] sensor: Fix sensor cts issue
    [Detail] Fix Rotationgriffin cts test fail.
    Improve magnetic sensor data timestamp precision
    
    [Solution] Use akm09911 continues mode
    
    MTK-Commit-Id: 61637c36ae3c99de8140b81a0d31a511d031fb28
    
    Change-Id: Ifb5635381e1b4d850ed01e7c16251dc325715a70
    Signed-off-by: Qiangming Xia <qiangming.xia@mediatek.com>
    CR-Id: ALPS03632156
    Feature: CTS(*)

--- drivers/misc/mediatek/sensors-1.0/magnetometer/akm09911/akm09911.c
+++ drivers/misc/mediatek/sensors-1.0/magnetometer/akm09911/akm09911.c
@@ -41,6 +41,11 @@
 #endif
 
 static DECLARE_WAIT_QUEUE_HEAD(open_wq);
+#define AKM_CONTINUOUS 1
+
+#if AKM_CONTINUOUS
+#define AKM_CONTINUOUS_MODE
+#endif
 
 static short akmd_delay = AKM09911_DEFAULT_DELAY;
 static int factory_mode;
@@ -316,6 +321,19 @@ static long AKECS_SetMode_SngMeasure(void)
 	/* Set data */
 	return AKI2C_TxData(buffer, 2);
 }
+#ifdef AKM_CONTINUOUS_MODE
+static long AKECS_SetMode_CntMeasure(char mode)
+{
+	char buffer[2];
+
+	/* Set measure mode */
+	buffer[0] = AK09911_REG_CNTL2;
+	buffer[1] = mode;	/* 16 bit mode */
+
+	/* Set data */
+	return AKI2C_TxData(buffer, 2);
+}
+#endif
 
 static long AKECS_SetMode_SelfTest(void)
 {
@@ -1173,8 +1191,10 @@ static ssize_t show_sensordata_value(struct device_driver *ddri, char *buf)
 	char sensordata[SENSOR_DATA_SIZE];
 	char strbuf[AKM09911_BUFSIZE];
 
+#ifndef AKM_CONTINUOUS_MODE
 	AKECS_SetMode_SngMeasure();
 	mdelay(10);
+#endif
 	AKECS_GetData(sensordata, SENSOR_DATA_SIZE);
 
 	sprintf(strbuf, "%d %d %d %d %d %d %d %d %d\n", sensordata[0], sensordata[1], sensordata[2],
@@ -1446,7 +1466,11 @@ static int akm09911_enable(int en)
 	factory_mode = 1;
 	if (value == 1) {
 		f_obj->enable = true;
+#ifdef AKM_CONTINUOUS_MODE
+		err = AKECS_SetMode_CntMeasure(AK09911_MODE_CNT_MEASURE_3);
+#else
 		err = AKECS_SetMode(AK09911_MODE_SNG_MEASURE);
+#endif
 		if (err < 0) {
 			MAGN_ERR("%s:AKECS_SetMode Error.\n", __func__);
 			return err;
@@ -1512,8 +1536,10 @@ static int akm09911_get_data(int *x, int *y, int *z, int *status)
 	char strbuf[SENSOR_DATA_SIZE];
 	int16_t data[3];
 
+#ifndef AKM_CONTINUOUS_MODE
 	AKECS_SetMode_SngMeasure();
 	mdelay(10);
+#endif
 
 	AKECS_GetData(strbuf, SENSOR_DATA_SIZE);
 	data[0] = (int16_t)(strbuf[1] | (strbuf[2] << 8));
