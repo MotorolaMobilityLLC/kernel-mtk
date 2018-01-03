6dcf628e0d7c37aab73f5ff05d1f860ce61d78c7
ligang <ligang@longcheer.com>
2018-01-03 17:24:01 +0800

    [L3500][gsensor][LXF_L3501_A01-6] update gsensor direction macro
    [rootcause] gsensor direction error
    [solution] fix gsensor direction
    [sideeffect] N/A
    [other] N/A
    
    Change-Id: Ia4ff25e3a2bbcd1314d9db5db52eca5f3c794ae6

--- drivers/misc/mediatek/sensors-1.0/accelerometer/bmi160_acc/bmi160_acc.c
+++ drivers/misc/mediatek/sensors-1.0/accelerometer/bmi160_acc/bmi160_acc.c
@@ -4130,7 +4130,7 @@ static int bmi160_acc_i2c_probe(struct i2c_client *client, const struct i2c_devi
     memset(obj, 0, sizeof(struct bmi160_acc_i2c_data));
 
     obj->hw = hw;
-#ifdef LCT_3500
+#ifdef CONFIG_LCT_3500
     obj->hw->direction = 1;
 #endif
     err = hwmsen_get_convert(obj->hw->direction, &obj->cvt);
