bb8e369cd44481f27f1dfc366bd51c856ef45783
Chinwen Chang <chinwen.chang@mediatek.com>
2018-06-03 05:31:03 +0800

    [ALPS02750725] memory-lowpower: update memory-lowpower driver
    This patch contains the following 2 changes,
         1. Refine acquire_memory/release_memory in
         	memory-lowpower-task.
         2. Refine the calculation of segment size in
         	mtkpasr driver.
    
    MTK-Commit-Id: c8d84582ccf22d78f3e6d13612f99415c6bab121
    
    Change-Id: I9904db403df8953dcc50af5f47daef49015b2c5c
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>
    CR-Id: ALPS02750725
    Feature: PASR (Partial Array Self-Refresh)

--- drivers/misc/mediatek/memory-lowpower/memory-lowpower.c
+++ drivers/misc/mediatek/memory-lowpower/memory-lowpower.c
@@ -38,6 +38,19 @@ static DEFINE_MUTEX(memory_lowpower_mutex);
 
 static unsigned long cma_usage_count;
 
+#define MEMORY_LOWPOWER_FULLNESS
+#ifdef MEMORY_LOWPOWER_FULLNESS
+/*
+ * Memory-lowpower needs to occupy the last portion of DRAM
+ * physically. But this may conflict with DRAM dummy read/write
+ * which will reserve a small size of aforementioned range.
+ * Because this feature doesn't care about the correctness of
+ * contents in the reserved range, so let memory-lowpower count
+ * it for fullness. (in bytes)
+ */
+static phys_addr_t grab_lastsize;
+#endif
+
 /*
  * Check whether memory_lowpower is initialized
  */
@@ -59,7 +72,11 @@ phys_addr_t memory_lowpower_cma_base(void)
  */
 phys_addr_t memory_lowpower_cma_size(void)
 {
+#ifndef MEMORY_LOWPOWER_FULLNESS
 	return (phys_addr_t)cma_get_size(cma);
+#else
+	return (phys_addr_t)cma_get_size(cma) + grab_lastsize;
+#endif
 }
 
 /*
@@ -67,13 +84,17 @@ phys_addr_t memory_lowpower_cma_size(void)
  * @count: Requested number of pages.
  * @align: Requested alignment of pages (in PAGE_SIZE order).
  * @pages: Pointer indicates allocated cma buffer.
- * It returns 0 is success, otherwise returns -1
+ * It returns 0 in success, otherwise returns -1
  */
 int get_memory_lowpower_cma_aligned(int count, unsigned int align, struct page **pages)
 {
 	int ret = 0;
 
 	mutex_lock(&memory_lowpower_mutex);
+
+#ifdef MEMORY_LOWPOWER_FULLNESS
+	count = min_t(unsigned long, (cma_get_size(cma) >> PAGE_SHIFT) - cma_usage_count, count);
+#endif
 
 	*pages = cma_alloc(cma, count, align);
 	if (*pages == NULL) {
@@ -92,13 +113,17 @@ int get_memory_lowpower_cma_aligned(int count, unsigned int align, struct page *
  * put_memory_lowpwer_cma_aligned - free aligned cma memory belongs to lowpower cma
  * @count: Requested number of pages.
  * @pages: Pointer indicates allocated cma buffer.
- * It returns 0 is success, otherwise returns -ENOMEM
+ * It returns 0 in success, otherwise returns -1
  */
 int put_memory_lowpower_cma_aligned(int count, struct page *pages)
 {
-	int ret;
+	int ret = 0;
 
 	mutex_lock(&memory_lowpower_mutex);
+
+#ifdef MEMORY_LOWPOWER_FULLNESS
+	count = min_t(unsigned long, cma_usage_count, count);
+#endif
 
 	if (pages) {
 		ret = cma_release(cma, pages, count);
@@ -106,23 +131,25 @@ int put_memory_lowpower_cma_aligned(int count, struct page *pages)
 			pr_err("%s incorrect pages: %p(%lx)\n",
 					__func__,
 					pages, page_to_pfn(pages));
-			return -EINVAL;
+			ret = -1;
+		} else {
+			cma_usage_count -= count;
+			ret = 0;
 		}
-
-		cma_usage_count -= count;
 	}
 
 	mutex_unlock(&memory_lowpower_mutex);
 
-	return 0;
+	return ret;
 }
 
 /*
- * get_memory_lowpwer_cma - allocate all cma memory belongs to lowpower cma
- *
+ * get_memory_lowpwer_cma - allocate all cma memory belongs to lowpower cma.
+ * It returns 0 in success, otherwise returns -1
  */
 int get_memory_lowpower_cma(void)
 {
+	int ret = 0;
 	int count = cma_get_size(cma) >> PAGE_SHIFT;
 
 	if (cma_pages) {
@@ -137,23 +164,24 @@ int get_memory_lowpower_cma(void)
 	if (cma_pages) {
 		pr_debug("%s:%d ok\n", __func__, __LINE__);
 		cma_usage_count += count;
-	} else
+	} else {
 		pr_alert("lowpower cma allocation failed\n");
+		ret = -1;
+	}
 
 	mutex_unlock(&memory_lowpower_mutex);
 
 out:
-	return 0;
+	return ret;
 }
 
 /*
- * put_memory_lowpwer_cma - free all cma memory belongs to lowpower cma
- *
- * It returns 0 is success, otherwise returns -ENOMEM
+ * put_memory_lowpwer_cma - free all cma memory belongs to lowpower cma.
+ * It returns 0 in success, otherwise returns -1
  */
 int put_memory_lowpower_cma(void)
 {
-	int ret;
+	int ret = 0;
 	int count = cma_get_size(cma) >> PAGE_SHIFT;
 
 	mutex_lock(&memory_lowpower_mutex);
@@ -164,26 +192,54 @@ int put_memory_lowpower_cma(void)
 			pr_err("%s incorrect pages: %p(%lx)\n",
 					__func__,
 					cma_pages, page_to_pfn(cma_pages));
-			return -EINVAL;
+			ret = -1;
+		} else {
+			cma_usage_count -= count;
+			cma_pages = 0;
+			ret = 0;
 		}
-
-		cma_usage_count -= count;
-
-		cma_pages = 0;
 	}
 
 	mutex_unlock(&memory_lowpower_mutex);
 
-	return 0;
+	return ret;
 }
+#ifdef MEMORY_LOWPOWER_FULLNESS
+#define TEST_AND_RESERVE_MEMBLOCK(base, size) (!memblock_is_region_reserved(base, size) && \
+						memblock_reserve(base, size) == 0)
+/* Grab the last page block for fullness */
+static void memory_lowpower_fullness(phys_addr_t base, phys_addr_t size)
+{
+	phys_addr_t pageblock_size = 1 << (pageblock_order + PAGE_SHIFT);
+	phys_addr_t expected_lastaddr, got_lastaddr = (phys_addr_t)ULLONG_MAX;
+
+	/* If the hole is not the size of 1 pageblock, just return */
+	if (memblock_end_of_DRAM() - (base + size) != pageblock_size) {
+		pr_alert("%s, no need to do fullness\n", __func__);
+		return;
+	}
+
+	expected_lastaddr = base + size;
+
+	if (TEST_AND_RESERVE_MEMBLOCK(expected_lastaddr, pageblock_size) ||
+		TEST_AND_RESERVE_MEMBLOCK(expected_lastaddr, pageblock_size - PAGE_SIZE))
+		got_lastaddr = expected_lastaddr;
+
+	if (expected_lastaddr == got_lastaddr) {
+		pr_alert("%s, success to grab the \"last pageblock\"\n", __func__);
+		grab_lastsize = pageblock_size;
+	} else
+		pr_alert("%s, failed to grab the last pageblock\n", __func__);
+}
+#endif
 
 static int memory_lowpower_init(struct reserved_mem *rmem)
 {
 	int ret;
 
 	pr_alert("%s, name: %s, base: 0x%pa, size: 0x%pa\n",
-			__func__, rmem->name,
-			&rmem->base, &rmem->size);
+		 __func__, rmem->name,
+		 &rmem->base, &rmem->size);
 
 	/* init cma area */
 	ret = cma_init_reserved_mem(rmem->base, rmem->size, 0, &cma);
@@ -193,11 +249,15 @@ static int memory_lowpower_init(struct reserved_mem *rmem)
 		return 1;
 	}
 
+#ifdef MEMORY_LOWPOWER_FULLNESS
+	/* try to grab the last pageblock */
+	memory_lowpower_fullness(rmem->base, rmem->size);
+#endif
+
 	return 0;
 }
-
 RESERVEDMEM_OF_DECLARE(memory_lowpower, "mediatek,memory-lowpower",
-		memory_lowpower_init);
+			memory_lowpower_init);
 
 #ifdef CONFIG_ZONE_MOVABLE_CMA
 static int __init memory_lowpower_sanity_test(void)
