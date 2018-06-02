d0c31fe35c79bdf3b14991afb20d4627d6db22cd
Elvin Lin <elvin.lin@mediatek.com>
2018-06-03 05:15:15 +0800

    [ALPS03526219] charger: add mediatek charging driver
    [Detail]
    Add mediatek charging driver
    1. mediatek charging framework
    2. switch charging algorithm
    3. PD charging
    
    MTK-Commit-Id: fe0012ed03fa449c3305ee1f73ba3640e12ffee4
    
    Change-Id: I07cceaf1e2742602c4284a01c63c2d3a68a4dec1
    Signed-off-by: Elvin Lin <elvin.lin@mediatek.com>
    CR-Id: ALPS03526219
    Feature: Charger

---
+++ drivers/power/supply/mediatek/charger/mtk_switch_charging.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#ifndef _MTK_SWITCH_CHARGER_H
+#define _MTK_SWITCH_CHARGER_H
+
+/*****************************************************************************
+ *  Switch Charging State
+ ****************************************************************************/
+
+#define CHR_CC (0x0001)
+#define CHR_BATFULL (0x0002)
+#define CHR_ERROR (0x0003)
+#define CHR_PE30 (0x0004)
+
+struct switch_charging_alg_data {
+	int state;
+	bool disable_charging;
+	struct mutex ichg_aicr_access_mutex;
+
+	unsigned int total_charging_time;
+	unsigned int pre_cc_charging_time;
+	unsigned int cc_charging_time;
+	unsigned int cv_charging_time;
+	unsigned int full_charging_time;
+	struct timespec charging_begin_time;
+};
+
+#endif /* End of _MTK_SWITCH_CHARGER_H */
