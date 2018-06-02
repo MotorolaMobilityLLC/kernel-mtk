043555b2c49007fe9b0821f7134e7c313a96d652
mtk07406 <richard-yc.wang@mediatek.com>
2018-06-03 04:16:43 +0800

    [ALPS03384803] GPU: submit vendor DDK source code
    [Detail]
    submit vendor DDK source code, Makefile and config.
    1. DDK version : bifrost r5p0
    2. GPU : ARM G71
    
    MTK-Commit-Id: e1f69564cc6ff56cd619c6a68e2b695fec286860
    
    Change-Id: I95e6dbbfb7362474f7fd321d545e2f43a1cef5d3
    Signed-off-by: mtk07406 <richard-yc.wang@mediatek.com>
    CR-Id: ALPS03384803
    Feature: OpenGL|ES

---
+++ drivers/misc/mediatek/gpu/gpu_mali/mali_midgard/mali-r7p0/drivers/gpu/arm/midgard/platform/mt6763/mtk_common.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#ifndef __MTK_COMMON_H__
+#define __MTK_COMMON_H__
+
+/* MTK device context driver private data */
+struct mtk_config {
+
+    /* mtk mfg mapped register */
+    volatile void *mfg_register;
+
+    struct clk *clk_mfg;
+    struct clk *clk_mfg_async;
+
+    struct clk *clk_mfg_core0;
+    struct clk *clk_mfg_core1;
+
+    struct clk *clk_mfg_main;
+#ifdef ENABLE_MFG_PERF
+    struct clk *clk_mfg_f52m_sel;
+    bool mfg_perf_enabled;
+#endif
+
+};
+
+#ifdef ENABLE_MFG_PERF
+/* MTK HAL mtk_enable_gpu_perf_monitor implementation */
+bool _mtk_enable_gpu_perf_monitor(bool enable);
+#endif
+
+#define MTKCLK_prepare_enable(clk) \
+	if (config->clk) { \
+		if (clk_prepare_enable(config->clk)) \
+		pr_alert("MALI: clk_prepare_enable failed when enabling" #clk); }
+
+#define MTKCLK_disable_unprepare(clk) \
+	if (config->clk) { \
+		clk_disable_unprepare(config->clk); }
+
+
+
+#endif /* __MTK_COMMON_H__ */
