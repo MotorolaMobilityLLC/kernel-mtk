2fe9e91735d7c1b280a5407a36839fc81d8090a1
Qiang guo <qiang.guo@mediatek.com>
2018-06-03 07:19:55 +0800

    [ALPS02752408] wlan: fixed KE at cfg80211_get_bss_channel
    [Detail]
    Process:
    1. turn on wifi but prAdapter->fgEnable5GBand is false by
    wlanLoadManufactureData falied.
    And prWdev->wiphy->bands[IEEE80211_BAND_5GHZ] set to NULL
    2. turn off wifi.
    3. turn on wifi and prAdapter->fgEnable5GBand is true
    by wlanLoadManufactureData successed. But we do not set
    prWdev->wiphy->bands[IEEE80211_BAND_5GHZ] to &mtk_band_5ghz.
    4. Scan result has the 5G ap. 5. Kerenel exception.
    [Solution]
    Set prWiphy->band[....5gh]rWdev->wiphy->bands[IEEE80211_BAND_5GHZ]
    to &mtk_band_5ghz while turn on wifi
    
    MTK-Commit-Id: 1f2282e3340be8558672cd485d48bca623f45ddc
    
    Change-Id: I541b36790a232d879da56ab822fbce1e456ae8d1
    Signed-off-by: Qiang guo <qiang.guo@mediatek.com>
    CR-Id: ALPS02752408
    Feature: Wi-Fi Scanner

--- drivers/misc/mediatek/connectivity/wlan/gen2/os/linux/gl_init.c
+++ drivers/misc/mediatek/connectivity/wlan/gen2/os/linux/gl_init.c
@@ -3033,6 +3033,8 @@ bailout:
 #endif
 		if (prAdapter->fgEnable5GBand == FALSE)
 			prWdev->wiphy->bands[IEEE80211_BAND_5GHZ] = NULL;
+		else
+			prWdev->wiphy->bands[IEEE80211_BAND_5GHZ] = &mtk_band_5ghz;
 
 		prGlueInfo->main_thread = kthread_run(tx_thread, prGlueInfo->prDevHandler, "tx_thread");
 		kalSetHalted(FALSE);
