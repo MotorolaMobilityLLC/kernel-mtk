fc39a9b343c8356be8e629d491735863848569cf
Tingting Lei <tingting.lei@mediatek.com>
2018-06-03 06:28:30 +0800

    [ALPS02720695] Hotspot: Canada regulation update
    [Detail]
    Industry Canada has updated their radio standard specification for
    Wi-Fi equipments operating on 5GHz band.
    Since Canadian Weather Radar operates in frequency 5.600~5.650GHz
    (CH120, 124 and 128), Industry Canada recommends no Wi-Fi operations
    on these channels to avoid interference.
    
    [Solution]
    Move COUNTRY_CODE_CA from country group 1 to a new separated group 19
    without CH120~128 in UNII-2C.
    Increase total group number and postpone the default group index.
    
    MTK-Commit-Id: 4454cc2efda843e07f262d40855be9ef669f9e7f
    
    Change-Id: I4ab83c220aced368ce79b1f7a7c0c9b0f2906418
    Signed-off-by: Tingting Lei <tingting.lei@mediatek.com>
    CR-Id: ALPS02720695
    Feature: Wi-Fi Driver

--- drivers/misc/mediatek/connectivity/wlan/gen2/mgmt/rlm_domain.c
+++ drivers/misc/mediatek/connectivity/wlan/gen2/mgmt/rlm_domain.c
@@ -137,9 +137,9 @@ static const UINT_16 g_u2CountryGroup0[] = {
 };
 
 static const UINT_16 g_u2CountryGroup1[] = {
-	COUNTRY_CODE_AS, COUNTRY_CODE_AI, COUNTRY_CODE_BM, COUNTRY_CODE_CA,
-	COUNTRY_CODE_KY, COUNTRY_CODE_GU, COUNTRY_CODE_FM, COUNTRY_CODE_PR,
-	COUNTRY_CODE_US, COUNTRY_CODE_VI
+	COUNTRY_CODE_AS, COUNTRY_CODE_AI, COUNTRY_CODE_BM, COUNTRY_CODE_KY,
+	COUNTRY_CODE_GU, COUNTRY_CODE_FM, COUNTRY_CODE_PR, COUNTRY_CODE_US,
+	COUNTRY_CODE_VI
 };
 
 static const UINT_16 g_u2CountryGroup2[] = {
@@ -230,7 +230,9 @@ static const UINT_16 g_u2CountryGroup17[] = { COUNTRY_CODE_MP };
 
 static const UINT_16 g_u2CountryGroup18[] = { COUNTRY_CODE_TW };
 
-static const UINT_16 g_u2CountryGroup19[] = {
+static const UINT_16 g_u2CountryGroup19[] = { COUNTRY_CODE_CA };
+
+static const UINT_16 g_u2CountryGroup20[] = {
 	COUNTRY_CODE_CK, COUNTRY_CODE_CU, COUNTRY_CODE_TL, COUNTRY_CODE_FO,
 	COUNTRY_CODE_GI, COUNTRY_CODE_GG, COUNTRY_CODE_IR, COUNTRY_CODE_IM,
 	COUNTRY_CODE_JE, COUNTRY_CODE_KP, COUNTRY_CODE_MH, COUNTRY_CODE_NU,
@@ -238,14 +240,14 @@ static const UINT_16 g_u2CountryGroup19[] = {
 	COUNTRY_CODE_SS, COUNTRY_CODE_SD, COUNTRY_CODE_SY
 };
 
-static const UINT_16 g_u2CountryGroup20[] = {
+static const UINT_16 g_u2CountryGroup21[] = {
 	COUNTRY_CODE_DF, COUNTRY_CODE_FF
 	/* When country code is not found and no matched NVRAM setting,
 	 * the default group will be used.
 	 */
 };
 
-static const UINT_16 g_u2CountryGroup21[] = {
+static const UINT_16 g_u2CountryGroup22[] = {
 	COUNTRY_CODE_UDF
 };
 
@@ -595,23 +597,23 @@ DOMAIN_INFO_ENTRY arSupportedRegDomains[] = {
 	{
 	 (PUINT_16) g_u2CountryGroup19, sizeof(g_u2CountryGroup19) / 2,
 	 {
-	  {81, BAND_2G4, CHNL_SPAN_5, 1, 13, FALSE}
-	  ,			/* CH_SET_2G4_1_13 */
+	  {81, BAND_2G4, CHNL_SPAN_5, 1, 11, FALSE}
+	  ,			/* CH_SET_2G4_1_11 */
 
 	  {115, BAND_5G, CHNL_SPAN_20, 36, 4, FALSE}
 	  ,			/* CH_SET_UNII_LOW_36_48 */
 	  {118, BAND_5G, CHNL_SPAN_20, 52, 4, TRUE}
 	  ,			/* CH_SET_UNII_MID_52_64 */
-	  {121, BAND_5G, CHNL_SPAN_20, 100, 12, TRUE}
-	  ,			/* CH_SET_UNII_WW_100_144 */
+	  {121, BAND_5G, CHNL_SPAN_20, 100, 5, TRUE}
+	  ,			/* CH_SET_UNII_WW_100_116 */
+	  {121, BAND_5G, CHNL_SPAN_20, 132, 4, TRUE}
+	  ,			/* CH_SET_UNII_WW_132_144 */
 	  {125, BAND_5G, CHNL_SPAN_20, 149, 5, FALSE}
-	  ,			/* CH_SET_UNII_UPPER_149_165 */
-	  {0, BAND_NULL, 0, 0, 0, FALSE}
+				/* CH_SET_UNII_UPPER_149_165 */
 	 }
 	}
 	,
 	{
-	 /* Note: Default group if no matched country code */
 	 (PUINT_16) g_u2CountryGroup20, sizeof(g_u2CountryGroup20) / 2,
 	 {
 	  {81, BAND_2G4, CHNL_SPAN_5, 1, 13, FALSE}
@@ -630,8 +632,27 @@ DOMAIN_INFO_ENTRY arSupportedRegDomains[] = {
 	}
 	,
 	{
-	 /* Note: User defined group for customer configured regulation */
+	 /* Note: Default group if no matched country code */
 	 (PUINT_16) g_u2CountryGroup21, sizeof(g_u2CountryGroup21) / 2,
+	 {
+	  {81, BAND_2G4, CHNL_SPAN_5, 1, 13, FALSE}
+	  ,			/* CH_SET_2G4_1_13 */
+
+	  {115, BAND_5G, CHNL_SPAN_20, 36, 4, FALSE}
+	  ,			/* CH_SET_UNII_LOW_36_48 */
+	  {118, BAND_5G, CHNL_SPAN_20, 52, 4, TRUE}
+	  ,			/* CH_SET_UNII_MID_52_64 */
+	  {121, BAND_5G, CHNL_SPAN_20, 100, 12, TRUE}
+	  ,			/* CH_SET_UNII_WW_100_144 */
+	  {125, BAND_5G, CHNL_SPAN_20, 149, 5, FALSE}
+	  ,			/* CH_SET_UNII_UPPER_149_165 */
+	  {0, BAND_NULL, 0, 0, 0, FALSE}
+	 }
+	}
+	,
+	{
+	 /* Note: User defined group for customer configured regulation */
+	 (PUINT_16) g_u2CountryGroup22, sizeof(g_u2CountryGroup22) / 2,
 	 {
 	  {81, BAND_2G4, CHNL_SPAN_5, 1, 12, FALSE}
 	  ,			/* CH_SET_2G4_1_12 */
@@ -729,7 +750,7 @@ SUBBAND_CHANNEL_T g_rRlmSubBand[] = {
 /*----------------------------------------------------------------------------*/
 P_DOMAIN_INFO_ENTRY rlmDomainGetDomainInfo(P_ADAPTER_T prAdapter)
 {
-#define REG_DOMAIN_DEF_IDX             20 /* Default regulatory domain */
+#define REG_DOMAIN_DEF_IDX             21 /* Default regulatory domain */
 #define REG_DOMAIN_GROUP_NUM  \
 	(sizeof(arSupportedRegDomains) / sizeof(DOMAIN_INFO_ENTRY))
 
