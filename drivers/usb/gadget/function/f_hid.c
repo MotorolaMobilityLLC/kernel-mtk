8da4dfe4f87f012fd9b0201f870571a674670c76
Arvin Wang <arvin.wang@mediatek.com>
2018-06-03 06:00:51 +0800

    [ALPS03538038] usb: revert f_hid use alloc_ep_req()
    [Detail]
        Allocating request in bind() function causes a NULL pointer
    dereference.
    
    [Solution]
        This reverts
    commit ba1582f22231 ("usb: gadget: f_hid: use alloc_ep_req()").
    
    MTK-Commit-Id: 231638a9a6fa2f2349c093e411d10fcd6e67fb2e
    
    Change-Id: I860abab2cdbb9732d3a0170d108bc85a2d38b3cc
    Signed-off-by: Arvin Wang <arvin.wang@mediatek.com>
    CR-Id: ALPS03538038
    Feature: USB 3.0 Device

--- drivers/usb/gadget/function/f_hid.c
+++ drivers/usb/gadget/function/f_hid.c
@@ -678,8 +678,12 @@ static int hidg_bind(struct usb_configuration *c, struct usb_function *f)
 
 	/* preallocate request and buffer */
 	status = -ENOMEM;
-	hidg->req = alloc_ep_req(hidg->in_ep, hidg->report_length);
+	hidg->req = usb_ep_alloc_request(hidg->in_ep, GFP_KERNEL);
 	if (!hidg->req)
+		goto fail;
+
+	hidg->req->buf = kmalloc(hidg->report_length, GFP_KERNEL);
+	if (!hidg->req->buf)
 		goto fail;
 
 	/* set descriptor dynamic values */
