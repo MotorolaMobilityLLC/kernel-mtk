f7ac57e5160f7a86ed8e294db8809faebecc11ff
Miles Chen <miles.chen@mediatek.com>
2018-06-03 02:39:14 +0800

    [ALPS02739226] mem: add debug hook infrastructure
    Add debug hook infrastructure. Weak functions are not allowed since
    kernel V3.19, so we re-factor the debug hook infrastructure.
    
    MTK-Commit-Id: d5bcc0bb774c20e0037963c982814e1c56b806e6
    
    Change-Id: I5e5ac90c2a2ac4f2001530e1c220ae6a928584f4
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS02739226
    Feature: Large DRAM

---
+++ drivers/misc/mediatek/mem/mtk_common_hooks.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+#include <linux/printk.h>
+#include <linux/ptrace.h>
+#include <linux/uaccess.h>
+
+int mem_fault_debug_hook(struct pt_regs *regs)
+{
+	void __user *pc = (void __user *)instruction_pointer(regs);
+	char buf[16];
+	int ret = -1;
+	u32 *p;
+
+	if (!copy_from_user(buf, pc, 16)) {
+		p = (u32 *)buf;
+		if ((*(p + 1) == 0x75666475) && (*(p + 2) == 0x00006477)) {
+			regs->ARM_pc += 12;
+			ret = 0;
+		}
+	}
+
+	return ret;
+}
