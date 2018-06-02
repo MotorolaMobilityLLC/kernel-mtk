c6b6783f6170351fa975764041af6aa22ca0d059
Charles Yen <charles.yen@mediatek.com>
2018-06-03 06:37:55 +0800

    [ALPS02767162] PQ:N0 Refactor
    [Detail]
    PQ Refactor for N0 projects
    
    [Solution]
    1.Modify PQ structure
    2.Change writing of some Color registers  to mask writing
    
    MTK-Commit-Id: 5b26d37d8b2e495e667e1f29b3648762eb949abc
    
    Change-Id: I744f3d7224c52e12439850481d7a16abd957d60a
    CR-Id:ALPS02767162
    Feature:Display Quality Enhancement
    Signed-off-by: Charles Yen <charles.yen@mediatek.com>

--- drivers/misc/mediatek/video/common/color20/ddp_color.h
+++ drivers/misc/mediatek/video/common/color20/ddp_color.h
@@ -94,7 +94,7 @@ enum {
 #define MIRAVISION_HW_VERSION       (0)
 #endif
 
-#define MIRAVISION_SW_VERSION       (2)	/* 1:Android Lollipop, 2:Android Marshmallow */
+#define MIRAVISION_SW_VERSION       (3)	/* 3:Android N*/
 #define MIRAVISION_SW_FEATURE_VIDEO_DC  (0x1)
 #define MIRAVISION_SW_FEATURE_AAL       (0x2)
 #define MIRAVISION_SW_FEATURE_PQDS       (0x4)
