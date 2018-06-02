85dcf4277643d61b8cf0929c7b58789b9a1c5322
Seiya Wang <seiya.wang@mediatek.com>
2018-06-03 02:20:40 +0800

    [ALPS03481189] mt8173: bringup 8173 projects
    Add ARCH_MT8173 for MediaTek MT8173
    Add GPL license header in mt8173-power.h
    Add mtk-gic-extend.c for MT8173 usage
    
    MTK-Commit-Id: 1e230cbf86942de80838fc6174eb3338571b9f85
    
    Change-Id: Ie467cbe768bb290b80bfbaaddd502d616611ff4f
    CR-Id: ALPS03481189
    Feature: Others
    Signed-off-by: Seiya Wang <seiya.wang@mediatek.com>

--- include/dt-bindings/power/mt8173-power.h
+++ include/dt-bindings/power/mt8173-power.h
@@ -1,5 +1,18 @@
-#ifndef _DT_BINDINGS_POWER_MT8183_POWER_H
-#define _DT_BINDINGS_POWER_MT8183_POWER_H
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _DT_BINDINGS_POWER_MT8173_POWER_H
+#define _DT_BINDINGS_POWER_MT8173_POWER_H
 
 #define MT8173_POWER_DOMAIN_VDEC	0
 #define MT8173_POWER_DOMAIN_VENC	1
@@ -12,4 +25,4 @@
 #define MT8173_POWER_DOMAIN_MFG_2D	8
 #define MT8173_POWER_DOMAIN_MFG		9
 
-#endif /* _DT_BINDINGS_POWER_MT8183_POWER_H */
+#endif /* _DT_BINDINGS_POWER_MT8173_POWER_H */
