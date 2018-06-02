98fc4dd755e7cd8759234deb56fb36f1331b895c
Shun-Chih Yu <shun-chih.yu@mediatek.com>
2018-06-03 02:52:40 +0800

    [ALPS02661288] plat_sram_flag: porting for mt6757
    [Detail]
    add interface to set and dump plat_sram_flag0,
    plat_sram_flag1, plat_sram_flag2 for lastpc-v2, dfd,
    and etb driver
    
    MTK-Commit-Id: caf0a00c84a7f5e9547bb53a255f706f70cca2a7
    
    Change-Id: I1d104baa855f561f21b3b0d15a310e4bec94433d
    Signed-off-by: Shun-Chih Yu <shun-chih.yu@mediatek.com>
    CR-Id: ALPS02661288
    Feature: Others

--- drivers/misc/mediatek/include/mt-plat/mtk_platform_debug.h
+++ drivers/misc/mediatek/include/mt-plat/mtk_platform_debug.h
@@ -14,10 +14,15 @@
 #ifndef __MTK_PLATFORM_DEBUG_H__
 #define __MTK_PLATFORM_DEBUG_H__
 
-#if CONFIG_MTK_PLAT_SRAM_FLAG
+#ifdef CONFIG_MTK_PLAT_SRAM_FLAG
 /* plat_sram_flag */
 extern int set_sram_flag_lastpc_valid(void);
+extern int set_sram_flag_dfd_valid(void);
 extern int set_sram_flag_etb_user(unsigned int etb_id, unsigned int user_id);
+#endif
+
+#ifdef CONFIG_MTK_DFD_INTERNAL_DUMP
+extern int dfd_setup(void);
 #endif
 
 #endif /* __MTK_PLATFORM_DEBUG_H__ */
