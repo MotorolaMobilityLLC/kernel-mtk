646ddeb65cab1b264030fced21c4f81a7f7dce95
Xianpu Liang <xianpu.liang@mediatek.com>
2018-06-03 07:41:51 +0800

    [ALPS02524755] wlan: three patch for kernel-4.4
    1. Add a new interface to control TDLS power save for user space
    2. change beacon tiemout defer timer to 5s
    3. stop timer when scan module uninit
    
    MTK-Commit-Id: 010c5344c55cec004442f403b4930b29fd6df450
    
    Change-Id: I6be77408589296c44213aad9d4525bb0b5e9c4c6
    Signed-off-by: Xianpu Liang <xianpu.liang@mediatek.com>
    CR-Id: ALPS02524755
    Feature: CrossMount

--- drivers/misc/mediatek/connectivity/wlan/gen3/common/wlan_oid.c
+++ drivers/misc/mediatek/connectivity/wlan/gen3/common/wlan_oid.c
@@ -11078,3 +11078,28 @@ WLAN_STATUS wlanoidQueryCfgRead(IN P_ADAPTER_T prAdapter,
 	return rStatus;
 }
 #endif
+
+WLAN_STATUS
+wlanoidDisableTdlsPs(IN P_ADAPTER_T prAdapter,
+			 IN PVOID pvSetBuffer, IN UINT_32 u4SetBufferLen, OUT PUINT_32 pu4SetInfoLen)
+{
+	struct CMD_TDLS_PS_T rTdlsPs;
+
+	if (!prAdapter || !pvSetBuffer)
+		return WLAN_STATUS_INVALID_DATA;
+
+	rTdlsPs.ucIsEnablePs = *(PUINT_8)pvSetBuffer - '0';
+	DBGLOG(OID, INFO, "enable tdls ps %d\n", rTdlsPs.ucIsEnablePs);
+	wlanSendSetQueryCmd(prAdapter,
+							CMD_ID_TDLS_PS,
+							TRUE,
+							FALSE,
+							FALSE,
+							NULL,
+							nicOidCmdTimeoutCommon,
+							sizeof(rTdlsPs),
+							(PUINT_8)&rTdlsPs,
+							NULL,
+							0);
+	return WLAN_STATUS_SUCCESS;
+}
