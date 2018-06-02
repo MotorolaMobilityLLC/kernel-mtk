a4785d0ad39591c01099e849829f2ede72e83c7b
Heiping Lei <Heiping.Lei@mediatek.com>
2018-06-03 06:20:47 +0800

    [ALPS02670441] connectivity: add common soc related code
    [Detail] 1. add connectivity kernel driver module.
    
    MTK-Commit-Id: 4158d08f0295f274074d34a73f52ba638e0c4516
    
    Change-Id: Ice3147c927f8de8658538174a8062c0eaca813cc
    Signed-off-by: Heiping Lei <Heiping.Lei@mediatek.com>
    CR-Id: ALPS02670441
    Feature: [Android Default] WMT Driver for SoC

--- drivers/misc/mediatek/connectivity/common/conn_soc/linux/pri/wmt_dev.c
+++ drivers/misc/mediatek/connectivity/common/conn_soc/linux/pri/wmt_dev.c
@@ -1450,6 +1450,8 @@ INT32 wmt_dev_read_file(PUINT8 pName, const PPUINT8 ppBufPtr, INT32 offset, INT3
 	INT32 file_len;
 	INT32 read_len;
 	PVOID pBuf;
+	char __user *p = NULL;
+	loff_t pos = 0;
 
 	/* struct cred *cred = get_task_cred(current); */
 	const struct cred *cred = get_current_cred();
@@ -1461,7 +1463,17 @@ INT32 wmt_dev_read_file(PUINT8 pName, const PPUINT8 ppBufPtr, INT32 offset, INT3
 	*ppBufPtr = NULL;
 
 	fd = filp_open(pName, O_RDONLY, 0);
-	if (!fd || IS_ERR(fd) || !fd->f_op || !fd->f_op->read) {
+	if (!fd || IS_ERR(fd) || !fd->f_op) {
+
+		if (!fd)
+			WMT_ERR_FUNC("%s %d\n", __func__, __LINE__);
+		if (IS_ERR(fd))
+			WMT_ERR_FUNC("%s %d\n", __func__, __LINE__);
+		if (!fd->f_op)
+			WMT_ERR_FUNC("%s %d\n", __func__, __LINE__);
+		if (!fd->f_op->read)
+			WMT_ERR_FUNC("%s %d\n", __func__, __LINE__);
+
 		WMT_ERR_FUNC("failed to open or read!(0x%p, %d, %d, %d)\n", fd, PTR_ERR(fd), cred->fsuid, cred->fsgid);
 		if (IS_ERR(fd))
 			WMT_ERR_FUNC("error code:%d\n", PTR_ERR(fd));
@@ -1486,8 +1498,10 @@ INT32 wmt_dev_read_file(PUINT8 pName, const PPUINT8 ppBufPtr, INT32 offset, INT3
 				fd->f_pos = offset;
 			}
 		}
+		p = (__force void __user *)pBuf;
 
-		read_len = fd->f_op->read(fd, pBuf + padSzBuf, file_len, &fd->f_pos);
+		/*read_len = fd->f_op->read(fd, p + padSzBuf, file_len, &fd->f_pos);*/
+		read_len = __vfs_read(fd, p + padSzBuf, file_len, &pos);
 		if (read_len != file_len)
 			WMT_WARN_FUNC("read abnormal: read_len(%d), file_len(%d)\n", read_len, file_len);
 
