47630b6c17718f53c30e3bbbfb49794188c36c8e
Major Lee <major.lee@mediatek.com>
2018-06-03 00:46:25 +0800

    [ALPS02660690] spm: introduce mtk_spm_init
    [Detail]
    Introduce spm init driver, mtk_spm_init.
    
    MTK-Commit-Id: b0ffbc33cd75b957adb4baa607c5cb821e186860
    
    Change-Id: I02a7774731e1f99b414501b44121feb5723078f9
    Signed-off-by: Major Lee <major.lee@mediatek.com>
    CR-Id: ALPS02660690
    Feature: Power Management

---
+++ drivers/misc/mediatek/base/power/spm_v2/mtk_spm_init.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+#include "mtk_spm.h"
+#include "mtk_sleep.h"
+
+
+static int __init mtk_spm_init(void)
+{
+#if defined(CONFIG_MACH_MT6757)
+#if !defined(CONFIG_FPGA_EARLY_PORTING)
+	/* cpu dormant driver init */
+	/* mt_cpu_dormant_init(); */
+
+	spm_module_init();
+	/* slp_module_init(); */
+#endif
+#endif
+
+	return 0;
+}
+
+arch_initcall(mtk_spm_init);
