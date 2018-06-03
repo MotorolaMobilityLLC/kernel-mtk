5675fc52cab5f533a65f4187ef8e5ee399133df7
LucasWei <lucas-sr.wei@mediatek.com>
2018-06-03 14:22:55 +0800

    [ALPS03027350] mm/filemap: mark movable zone as none dirtyable pages
    [Detail]
    We revert __GFP_CMA flag patch to block file system make file cached
    in movable zone but blocking cma memory reclaim.
    Consider the side effect of removing this patch - too many dirty pages
    without writeback. We shrink system dirtyable pages and pull in
    writeback timing.
    
    [Solution]
    Mark movable zone as none dirtyable pages at global_dirtyable_pages()
    
    MTK-Commit-Id: 0418c6283ca8ecf7feae0e7065930f963cc8479b
    
    Change-Id: I74867352db8d18066da12e579c94ddb1ca087d2f
    Signed-off-by: LucasWei <lucas-sr.wei@mediatek.com>
    CR-Id: ALPS03027350
    Feature: MTBF(*)

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -340,6 +340,30 @@ static unsigned long global_dirtyable_memory(void)
 	if (!vm_highmem_is_dirtyable)
 		x -= highmem_dirtyable_memory(x);
 
+	/*
+	 * movable zone is consider as memory backup pool,
+	 * remove movable zone from dirtyable memory count
+	 */
+	if (IS_ENABLED(CONFIG_ZONE_MOVABLE_CMA)) {
+		int node;
+		unsigned long dirtyable_memory = 0;
+
+		for_each_node(node) {
+			struct zone *z = &NODE_DATA(node)->node_zones[ZONE_MOVABLE];
+
+			dirtyable_memory += zone_dirtyable_memory(z);
+		}
+
+		if ((long)dirtyable_memory < 0)
+			dirtyable_memory = 0;
+
+		/*
+		 * Make sure that the number of movable pages is never larger
+		 * than the number of the total dirtyable memory.
+		 */
+		x -= min(x, dirtyable_memory);
+	}
+
 	return x + 1;	/* Ensure that we never return 0 */
 }
 
