845265c1f4da489622ba16d3170d9f3f3c8023d1
James Lo <james.lo@mediatek.com>
2018-06-03 16:36:57 +0800

    [ALPS03163436] HPS:Disable hotplug at suspend.
    [Detail]
    Add vinson config to disable hotplug
    at suspend stage.
    
    MTK-Commit-Id: 678b3171744d89142c5bbaf7d1024c0b1f667e23
    
    Change-Id: Id6128ac03182ecdea49d8c5f78c4853768adbd94
    CR-Id:ALPS03163436
    Feature:hps
    Signed-off-by: James Lo <james.lo@mediatek.com>

--- drivers/misc/mediatek/base/power/hps_v3/mtk_hps_main.c
+++ drivers/misc/mediatek/base/power/hps_v3/mtk_hps_main.c
@@ -444,7 +444,7 @@ static int hps_probe(struct platform_device *pdev)
  */
 static int hps_suspend(struct device *dev)
 {
-#ifndef CONFIG_MACH_MT6763
+#if !defined(CONFIG_MACH_MT6763) && !defined(CONFIG_MACH_MT6758)
 	int cpu = 9;
 #endif
 	int ret = 0;
@@ -463,7 +463,7 @@ suspend_end:
 	hps_warn("state: %u, enabled: %u, suspend_enabled: %u, rush_boost_enabled: %u, ret: %d\n",
 		 hps_ctxt.state, hps_ctxt.enabled,
 		 hps_ctxt.suspend_enabled, hps_ctxt.rush_boost_enabled, ret);
-#ifndef CONFIG_MACH_MT6763
+#if !defined(CONFIG_MACH_MT6763) && !defined(CONFIG_MACH_MT6758)
 	/* offline big cores only */
 	cpu_hotplug_enable();
 	for (cpu = 9; cpu >= 8; cpu--) {
