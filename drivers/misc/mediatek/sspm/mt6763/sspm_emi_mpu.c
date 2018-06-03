82c0d5079127890c4504f93ba351d6d0af5f40a8
MingLun Tsai <minglun.tsai@mediatek.com>
2018-06-03 16:18:14 +0800

    [ALPS03140339] sspm: enable emi mpu protection
    [Detail]
    enalbe emi mpu protection for sspm
    
    MTK-Commit-Id: b9645b4217d739f7a86835572474a41fc2bda295
    
    Change-Id: I6f4178e211d512f20bf069380a4aeb17e301672d
    Signed-off-by: MingLun Tsai <minglun.tsai@mediatek.com>
    CR-Id: ALPS03140339
    Feature: TinySys

--- drivers/misc/mediatek/sspm/mt6763/sspm_emi_mpu.c
+++ drivers/misc/mediatek/sspm/mt6763/sspm_emi_mpu.c
@@ -24,14 +24,17 @@ void sspm_set_emi_mpu(phys_addr_t base, phys_addr_t size)
 {
 	unsigned long long shr_mem_phy_start, shr_mem_phy_end;
 	int shr_mem_mpu_id;
-	unsigned int shr_mem_mpu_attr;
+	unsigned long long shr_mem_mpu_attr;
 
 	shr_mem_mpu_id = SSPM_MPU_REGION_ID;
 	shr_mem_phy_start = base;
 	shr_mem_phy_end = base + size - 1;
-	shr_mem_mpu_attr = SET_ACCESS_PERMISSON(FORBIDDEN, FORBIDDEN, FORBIDDEN, FORBIDDEN,
-			NO_PROTECTION, FORBIDDEN, FORBIDDEN, NO_PROTECTION);
-	pr_debug("[SSPM] MPU Start protect SSPM Share region<%d:%08llx:%08llx> %x\n",
+	shr_mem_mpu_attr = SET_ACCESS_PERMISSON(UNLOCK,
+			FORBIDDEN, FORBIDDEN, FORBIDDEN, FORBIDDEN,
+			FORBIDDEN, FORBIDDEN, FORBIDDEN, NO_PROTECTION,
+			FORBIDDEN, FORBIDDEN, FORBIDDEN, FORBIDDEN,
+			FORBIDDEN, FORBIDDEN, FORBIDDEN, NO_PROTECTION);
+	pr_debug("[SSPM] MPU Start protect SSPM Share region<%d:%08llx:%08llx> %llx\n",
 			shr_mem_mpu_id, shr_mem_phy_start, shr_mem_phy_end, shr_mem_mpu_attr);
 
 	emi_mpu_set_region_protection(
