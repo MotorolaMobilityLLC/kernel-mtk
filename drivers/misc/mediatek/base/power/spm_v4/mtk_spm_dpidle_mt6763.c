fe1dde3ee3ca3913ca1ee83e097b2e6d47f8f815
Yun-Chen Hsieh <yun-chen.hsieh@mediatek.com>
2018-06-03 15:57:38 +0800

    [ALPS03159776] idle: mt6763: update dpidle low power setting flow
    [Detail]
    -. update pmic wrap cmd table for dpidle
    -. update dpidle pmic low power setting flow
    
    MTK-Commit-Id: 455dcc7f2274b552341a85fb593c65c691eeeb7d
    
    Change-Id: Ica5b815018fd1a53776130da13dee9f990a71cae
    Signed-off-by: Yun-Chen Hsieh <yun-chen.hsieh@mediatek.com>
    CR-Id: ALPS03159776
    Feature: Deep Idle

--- drivers/misc/mediatek/base/power/spm_v4/mtk_spm_dpidle_mt6763.c
+++ drivers/misc/mediatek/base/power/spm_v4/mtk_spm_dpidle_mt6763.c
@@ -24,46 +24,12 @@
 
 void spm_dpidle_pre_process(unsigned int operation_cond, struct pwr_ctrl *pwrctrl)
 {
-#if 0
 #ifndef CONFIG_MTK_TINYSYS_SSPM_SUPPORT
-	unsigned int value = 0;
-	unsigned int vcore_lp_mode = 0;
-
-	/* set PMIC wrap table for Vproc/Vsram voltage decreased */
-	/* VSRAM_DVFS1 */
-	pmic_read_interface_nolock(PMIC_RG_VSRAM_DVFS1_VOSEL_ADDR,
-								&value,
-								PMIC_RG_VSRAM_DVFS1_VOSEL_MASK,
-								PMIC_RG_VSRAM_DVFS1_VOSEL_SHIFT);
-
-	mt_spm_pmic_wrap_set_cmd(PMIC_WRAP_PHASE_ALLINONE,
-								IDX_ALL_1_VSRAM_NORMAL,
-								value);
-
-	/* VSRAM_DVFS2 */
-	pmic_read_interface_nolock(PMIC_RG_VSRAM_DVFS2_VOSEL_ADDR,
-								&value,
-								PMIC_RG_VSRAM_DVFS2_VOSEL_MASK,
-								PMIC_RG_VSRAM_DVFS2_VOSEL_SHIFT);
-
-	mt_spm_pmic_wrap_set_cmd(PMIC_WRAP_PHASE_ALLINONE,
-								IDX_ALL_2_VSRAM_NORMAL,
-								value);
-
-	/* TODO: setup PMIC low power mode */
-	/* spm_pmic_power_mode(PMIC_PWR_DEEPIDLE, 0, 0); */
-
 	/* set PMIC WRAP table for deepidle power control */
 	mt_spm_pmic_wrap_set_phase(PMIC_WRAP_PHASE_ALLINONE);
 
-	vcore_lp_mode = !!(operation_cond & DEEPIDLE_OPT_VCORE_LP_MODE);
-	pmic_config_interface_nolock(PMIC_RG_BUCK_VCORE_HW2_OP_EN_ADDR,
-									vcore_lp_mode,
-									PMIC_RG_BUCK_VCORE_HW2_OP_EN_MASK,
-									PMIC_RG_BUCK_VCORE_HW2_OP_EN_SHIFT);
-
-	wk_auxadc_bgd_ctrl(0);
-#endif
+	/* setup PMIC low power mode */
+	spm_pmic_power_mode(PMIC_PWR_DEEPIDLE, 0, 0);
 #endif
 
 	__spm_sync_pcm_flags(pwrctrl);
@@ -72,13 +38,9 @@ void spm_dpidle_pre_process(unsigned int operation_cond, struct pwr_ctrl *pwrctr
 
 void spm_dpidle_post_process(void)
 {
-#if 0
 #ifndef CONFIG_MTK_TINYSYS_SSPM_SUPPORT
 	/* set PMIC WRAP table for normal power control */
 	mt_spm_pmic_wrap_set_phase(PMIC_WRAP_PHASE_ALLINONE);
-
-	wk_auxadc_bgd_ctrl(1);
-#endif
 #endif
 }
 
