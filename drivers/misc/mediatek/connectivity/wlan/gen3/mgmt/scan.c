38968b26f1185b575fa89df95417642201f17b68
Juju Sung <juju.sung@mediatek.com>
2018-06-03 21:34:52 +0800

    [ALPS03507153] DTS: Fix node deployment fail
    [Detail]
    1. Fix DTS in new kernel scheme
    2. Fix GCC warning to error issue
       - Refine space to for the 2nd next "if condition"
    
    MTK-Commit-Id: 8480a8aeea3b7ce14fb5e31df3db65c824632b83
    
    Change-Id: Ifaecd6a2ffd248d29649b7f557bd65d496921052
    Feature: DTS
    CR-Id:ALPS03507153
    Signed-off-by: Juju Sung <juju.sung@mediatek.com>

--- drivers/misc/mediatek/connectivity/wlan/gen3/mgmt/scan.c
+++ drivers/misc/mediatek/connectivity/wlan/gen3/mgmt/scan.c
@@ -1939,7 +1939,7 @@ UINT_8 nicChannelNum2Index(IN UINT_8 ucChannelNum)
 	} else
 		ucindex = 0;
 
-		return ucindex;
+	return ucindex;
 }
 
 /*----------------------------------------------------------------------------*/
