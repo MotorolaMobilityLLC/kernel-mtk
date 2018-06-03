7dbba0c7daa73a529b5f2990294bfecfd30c3e1b
Hector Yuan <hector.yuan@mediatek.com>
2018-06-03 22:11:53 +0800

    [ALPS03526773] DVFS: Build in EEM driver
    [Detail] Build in EEM driver
    
    MTK-Commit-Id: f9efabcfc46061b0be7096417ab9f14a394c6973
    
    Change-Id: I81ccd6961271964f737e1b52d1a3ba2bb4faa14e
    Signed-off-by: Hector Yuan <hector.yuan@mediatek.com>
    CR-Id: ALPS03526773
    Feature: DVFS (Dynamic Voltage and Frequency Scaling)
    (cherry picked from commit de842778ee0dc614b57dee7b849660c4dbe4d212)

--- drivers/misc/mediatek/base/power/eem_v2/mt6771/inc/mtk_eem_config.h
+++ drivers/misc/mediatek/base/power/eem_v2/mt6771/inc/mtk_eem_config.h
@@ -15,6 +15,7 @@
 #define _MTK_EEM_CONFIG_H_
 
 /* CONFIG (SW related) */
+#define EEM_NOT_READY	(1)
 #define CONFIG_EEM_SHOWLOG	(0)
 #define EN_ISR_LOG		(1)
 #define EEM_BANK_SOC		(0) /* use voltage bin, so disable it */
@@ -22,7 +23,7 @@
 #define DUMP_DATA_TO_DE		(1)
 #define EEM_ENABLE		(1) /* enable; after pass HPT mini-SQC */
 #define EEM_FAKE_EFUSE		(0)
-#define UPDATE_TO_UPOWER	(1)
+#define UPDATE_TO_UPOWER	(0)
 #define EEM_LOCKTIME_LIMIT	(3000)
 #define ENABLE_EEMCTL0		(1)
 
@@ -31,6 +32,14 @@
 #define SET_PMIC_VOLT (1)
 #define SET_PMIC_VOLT_TO_DVFS (1)
 #define LOG_INTERVAL	(2LL * NSEC_PER_SEC)
+
+enum mt_cpu_dvfs_id {
+	MT_CPU_DVFS_LL,
+	MT_CPU_DVFS_L,
+	MT_CPU_DVFS_CCI,
+
+	NR_MT_CPU_DVFS,
+};
 
 /* FIX ME */
 #define DEVINFO_IDX_0 50
@@ -43,6 +52,9 @@
 #define DEVINFO_IDX_7 57
 #define DEVINFO_IDX_8 58
 #define DEVINFO_IDX_9 131
+#define DEVINFO_IDX_16 132
+#define DEVINFO_IDX_17 133
+#define DEVINFO_IDX_18 134
 
 /* Fake EFUSE */
 #define DEVINFO_0 0xFF00
@@ -64,12 +76,18 @@
 #define DEVINFO_8 0x550055
 /* L_HIGH */
 #define DEVINFO_9 0x10bd3c1b
+/* LL */
+#define DEVINFO_16 0x10bd3c1b
+/* L + LL */
+#define DEVINFO_17 0x550055
+/* L */
+#define DEVINFO_18 0x10bd3c1b
 
 /*****************************************
 * eem sw setting
 ******************************************
 */
-#define NR_HW_RES_FOR_BANK	(10) /* real eem banks for efuse */
+#define NR_HW_RES_FOR_BANK	(13) /* real eem banks for efuse */
 #define EEM_INIT01_FLAG		(0x0F) /* should be 0x0F */
 
 #define NR_FREQ 16
@@ -139,4 +157,47 @@
 #define EEM_CTL0_CCI (0x00100003)
 #define EEM_CTL0_GPU (0x00050001)
 #endif
+
+#if EEM_FAKE_EFUSE		/* select PTP secure mode based on efuse config. */
+#define SEC_MOD_SEL			0xF0		/* non secure  mode */
+#else
+#define SEC_MOD_SEL			0x00		/* Secure Mode 0 */
+/* #define SEC_MOD_SEL			0x10	*/	/* Secure Mode 1 */
+/* #define SEC_MOD_SEL			0x20	*/	/* Secure Mode 2 */
+/* #define SEC_MOD_SEL			0x30	*/	/* Secure Mode 3 */
+/* #define SEC_MOD_SEL			0x40	*/	/* Secure Mode 4 */
+#endif
+
+#if SEC_MOD_SEL == 0x00
+#define SEC_DCBDET 0xCC
+#define SEC_DCMDET 0xE6
+#define SEC_BDES 0xF5
+#define SEC_MDES 0x97
+#define SEC_MTDES 0xAC
+#elif SEC_MOD_SEL == 0x10
+#define SEC_DCBDET 0xE5
+#define SEC_DCMDET 0xB
+#define SEC_BDES 0x31
+#define SEC_MDES 0x53
+#define SEC_MTDES 0x68
+#elif SEC_MOD_SEL == 0x20
+#define SEC_DCBDET 0x39
+#define SEC_DCMDET 0xFE
+#define SEC_BDES 0x18
+#define SEC_MDES 0x8F
+#define SEC_MTDES 0xB4
+#elif SEC_MOD_SEL == 0x30
+#define SEC_DCBDET 0xDF
+#define SEC_DCMDET 0x18
+#define SEC_BDES 0x0B
+#define SEC_MDES 0x7A
+#define SEC_MTDES 0x52
+#elif SEC_MOD_SEL == 0x40
+#define SEC_DCBDET 0x36
+#define SEC_DCMDET 0xF1
+#define SEC_BDES 0xE2
+#define SEC_MDES 0x80
+#define SEC_MTDES 0x41
+#endif
+
 #endif
