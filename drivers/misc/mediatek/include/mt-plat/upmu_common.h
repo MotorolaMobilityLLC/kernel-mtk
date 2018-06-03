f6a2dbc2bfc0cedfd6e5d8a9b413fa0fa70e1bb5
Jeter Chen <jeter.chen@mediatek.com>
2018-06-03 17:27:55 +0800

    [ALPS03247963] PMIC: remove typedef for CheckPatch
    [Detail]
    remove a part of typedef in PMIC for CheckPatch
    
    MTK-Commit-Id: 29f1280fc6ae06f9f52c0e6b6595d9230db40b28
    
    Change-Id: Icd53cec06c35da487afedd7dbef909eeea2bd30d
    Signed-off-by: Jeter Chen <jeter.chen@mediatek.com>
    CR-Id: ALPS03247963
    Feature: Power Management

--- drivers/misc/mediatek/include/mt-plat/upmu_common.h
+++ drivers/misc/mediatek/include/mt-plat/upmu_common.h
@@ -127,19 +127,6 @@ enum MT65XX_POWER_VOL_TAG {
 	VOL_3600 = 3600
 };
 
-#ifndef CONFIG_MTK_PMIC_NEW_ARCH
-typedef struct {
-	unsigned long dwPowerCount;
-	bool bDefault_on;
-	char name[MAX_MOD_NAME];
-	char mod_name[MAX_DEVICE][MAX_MOD_NAME];
-} DEVICE_POWER;
-
-typedef struct {
-	DEVICE_POWER Power[MT65XX_POWER_COUNT_END];
-} ROOTBUS_HW;
-#endif
-
 /*
  * PMIC Exported Function for power service
  */
@@ -164,6 +151,7 @@ extern unsigned int pmic_config_interface_nospinlock(unsigned int RegNum,
 	unsigned int SHIFT);
 extern unsigned short pmic_set_register_value(PMU_FLAGS_LIST_ENUM flagname, unsigned int val);
 extern unsigned short pmic_get_register_value(PMU_FLAGS_LIST_ENUM flagname);
+extern unsigned short pmic_set_register_value_nolock(PMU_FLAGS_LIST_ENUM flagname, unsigned int val);
 extern unsigned short pmic_get_register_value_nolock(PMU_FLAGS_LIST_ENUM flagname);
 extern unsigned short pmic_set_register_value_nospinlock(PMU_FLAGS_LIST_ENUM flagname, unsigned int val);
 extern unsigned short bc11_set_register_value(PMU_FLAGS_LIST_ENUM flagname, unsigned int val);
@@ -173,17 +161,11 @@ extern unsigned int upmu_get_reg_value(unsigned int reg);
 extern void pmic_lock(void);
 extern void pmic_unlock(void);
 
-#ifdef CONFIG_MTK_PMIC_CHIP_MT6335
-extern void pmic_enable_interrupt(PMIC_IRQ_ENUM intNo, unsigned int en, char *str);
-extern void pmic_mask_interrupt(PMIC_IRQ_ENUM intNo, char *str);
-extern void pmic_unmask_interrupt(PMIC_IRQ_ENUM intNo, char *str);
-extern void pmic_register_interrupt_callback(PMIC_IRQ_ENUM intNo, void (EINT_FUNC_PTR) (void));
-#else
 extern void pmic_enable_interrupt(unsigned int intNo, unsigned int en, char *str);
 extern void pmic_mask_interrupt(unsigned int intNo, char *str);
 extern void pmic_unmask_interrupt(unsigned int intNo, char *str);
 extern void pmic_register_interrupt_callback(unsigned int intNo, void (EINT_FUNC_PTR) (void));
-#endif
+
 extern unsigned short is_battery_remove_pmic(void);
 
 extern void pmic_auxadc_init(void);
