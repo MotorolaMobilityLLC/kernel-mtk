1bd64f9510d77557ced28499a0222dd10462f6cc
Sam-KY Lin <sam-ky.lin@mediatek.com>
2018-06-03 22:07:40 +0800

    [ALPS03481973] VCODEC: Fix Build Fail for Zion O1 Migration
    [Detail] Add missing folder
    
    MTK-Commit-Id: 0f1838f430a80c59f8497ca0a7f37dfb7548bfa6
    
    Change-Id: I045165b62e7858712faa6d70d9441e7bcdca12bd
    Signed-off-by: Sam-KY Lin <sam-ky.lin@mediatek.com>
    CR-Id: ALPS03481973
    Feature: Others

--- drivers/misc/mediatek/videocodec/mt6739/common/drv/src/drv_base.c
+++ drivers/misc/mediatek/videocodec/mt6739/common/drv/src/drv_base.c
@@ -34,10 +34,10 @@
 /* spinlock : OalHWContextLock */
 VAL_VCODEC_OAL_HW_CONTEXT_T oal_hw_context[VCODEC_MULTIPLE_INSTANCE_NUM];
 /* mutex : NonCacheMemoryListLock */
-VAL_NON_CACHE_MEMORY_LIST_T grNonCacheMemoryList[VCODEC_MULTIPLE_INSTANCE_NUM_x_10];
+struct VAL_NON_CACHE_MEMORY_LIST_T grNonCacheMemoryList[VCODEC_MULTIPLE_INSTANCE_NUM_x_10];
 
 /* For both hybrid and pure HW */
-VAL_VCODEC_HW_LOCK_T grVcodecHWLock;	/* mutex : HWLock */
+struct VAL_VCODEC_HW_LOCK_T grVcodecHWLock;	/* mutex : HWLock */
 
 VAL_UINT32_T gu4LockDecHWCount;	/* spinlock : LockDecHWCountLock */
 VAL_UINT32_T gu4LockEncHWCount;	/* spinlock : LockEncHWCountLock */
