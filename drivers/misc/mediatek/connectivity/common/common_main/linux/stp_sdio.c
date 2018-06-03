48504fd7e019ea50d361906d95a709523b6ea88c
Dennis Lin <dennis.lin@mediatek.com>
2018-06-03 21:05:57 +0800

    [ALPS03495980] wmt: Wifi on/off timeout false alarm filtering
    [Detail]
    In the case when system performance is poor, turning on Wifi might
    take more than 4 seconds to complete due to the context switch
    overhead in the Wifi firmware patch download procedure. This problem
    typically happens in eng-load where system performance is generally
    poor and should not be our top concern.
    
    [Solution]
    Monitor wmtd threads exectuable and runnable ratio, extend timeout
    when wmtd thread is unable to acquire enough CPU resource to run.
    
    MTK-Commit-Id: 4aebcd5c783829beaebb0d8c2b60e021a60efacf
    
    Change-Id: I261d4b2ec0245a5b43a0139b7bbb887e80cb93c1
    Signed-off-by: Dennis Lin <dennis.lin@mediatek.com>
    CR-Id: ALPS03495980
    Feature: Connectivity - Driver
    (cherry picked from commit b91a6014389b2d2641131bd788c9d39167062e12)

--- drivers/misc/mediatek/connectivity/common/common_main/linux/stp_sdio.c
+++ drivers/misc/mediatek/connectivity/common/common_main/linux/stp_sdio.c
@@ -667,7 +667,7 @@ INT32 stp_sdio_own_ctrl(SDIO_PS_OP op)
 		STPSDIO_LOUD_FUNC("before op(%d)\n", op);
 		osal_ftrace_print("own set start, wakeup_flag(%d), sleep_flag(%d), wait signal",
 				gp_info->wakeup_flag, gp_info->sleep_flag);
-		ret = osal_wait_for_signal_timeout(pOsalSignal);
+		ret = osal_wait_for_signal_timeout(pOsalSignal, NULL);
 		if (!ret)
 			STPSDIO_ERR_FUNC("OWN_SET fail, done(%d), sleep(%d), wakeup(%d)\n",
 				pOsalSignal->comp.done, gp_info->sleep_flag, gp_info->wakeup_flag);
@@ -683,7 +683,7 @@ INT32 stp_sdio_own_ctrl(SDIO_PS_OP op)
 		osal_ftrace_print("own clear start, wakeup_flag(%d), sleep_flag(%d), wait signal",
 				gp_info->wakeup_flag, gp_info->sleep_flag);
 		STPSDIO_LOUD_FUNC("before op(%d)\n", op);
-		ret = osal_wait_for_signal_timeout(pOsalSignal);
+		ret = osal_wait_for_signal_timeout(pOsalSignal, NULL);
 		if (!ret)
 			STPSDIO_ERR_FUNC("OWN_CLER fail, done(%d), sleep(%d), wakeup(%d)\n",
 				pOsalSignal->comp.done, gp_info->sleep_flag, gp_info->wakeup_flag);
