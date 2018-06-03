dae9398dec45ac1fb32a77f9d491bd828e256237
Light Hsieh <light.hsieh@mediatek.com>
2018-06-03 15:41:01 +0800

    [ALPS03115590] MSDC2: SDIO porting
    [Detail]
    1. Correct clock source frequency definition
    2. Disable HWDVFS, SDR104, SDR50, DDR50 for bring-up SDIO at safe speed
    
    [Solution]
    
    MTK-Commit-Id: 3d12d0f4c9f20e721afdcb5f267ef48d4910b975
    
    Change-Id: I170f0802e457ce37164f91409424fe74a9f18a90
    Signed-off-by: Light Hsieh <light.hsieh@mediatek.com>
    CR-Id: ALPS03115590
    Feature: Driver

--- include/dt-bindings/mmc/mt6763-msdc.h
+++ include/dt-bindings/mmc/mt6763-msdc.h
@@ -27,22 +27,16 @@
 #define MSDC50_CLKSRC_200MHZ    (2)
 #define MSDC50_CLKSRC_156MHZ    (3)
 #define MSDC50_CLKSRC_182MHZ    (4)
-/* #define MSDC50_CLKSRC_156MHZ    (5) */
-#define MSDC50_CLKSRC_100MHZ    (6)
-#define MSDC50_CLKSRC_624MHZ    (7)
-#define MSDC50_CLKSRC_312MHZ    (8)
+#define MSDC50_CLKSRC_312MHZ    (5)
 
 #define MSDC30_CLKSRC_26MHZ     (0)
 #define MSDC30_CLKSRC_208MHZ    (1)
-#define MSDC30_CLKSRC_100MHZ    (2)
-#define MSDC30_CLKSRC_156MHZ    (3)
-#define MSDC30_CLKSRC_182MHZ    (4)
-/* #define MSDC30_CLKSRC_156MHZ    (5) */
-#define MSDC30_CLKSRC_178MHZ    (6)
-#define MSDC30_CLKSRC_200MHZ    (7)
+#define MSDC30_CLKSRC_182MHZ    (2)
+/* #define MSDC30_CLKSRC_156MHZ    (3) */
+#define MSDC30_CLKSRC_200MHZ    (4)
 
 #define MSDC_SMPL_RISING        (0)
 #define MSDC_SMPL_FALLING       (1)
 #define MSDC_SMPL_SEPARATE      (2)
 
-#endif /* _DT_BINDINGS_MMC_MT6757_H */
+#endif /* _DT_BINDINGS_MMC_MT6763_H */
