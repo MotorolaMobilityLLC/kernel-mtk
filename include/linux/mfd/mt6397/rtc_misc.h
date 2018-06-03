c1d3b47db799e7efae635b801d7e4b688a6d7d07
Tianping Fang <Tianping.Fang@mediatek.com>
2018-06-03 14:18:03 +0800

    [ALPS02947669] RTC: add 32k control function
    [Detail]
    Add 32k control function,this doesnt follow the upstream standard
    and just add 32k turnkey feature/API to upstream RTC driver.
    
    MTK-Commit-Id: 7684d4eab0173f0e731f7ce2e9c1597913a58b4a
    
    Change-Id: I321395d1253405ca80a9b2bb728398881a1a10ad
    Signed-off-by: Tianping Fang <Tianping.Fang@mediatek.com>
    CR-Id: ALPS02947669
    Feature: RTC

--- include/linux/mfd/mt6397/rtc_misc.h
+++ include/linux/mfd/mt6397/rtc_misc.h
@@ -14,6 +14,15 @@
 
 #ifndef __MT6397_RTC_MISC_H__
 #define __MT6397_RTC_MISC_H__
+#include <linux/types.h>
+
+typedef enum {
+	RTC_GPIO_USER_WIFI = 8,
+	RTC_GPIO_USER_GPS = 9,
+	RTC_GPIO_USER_BT = 10,
+	RTC_GPIO_USER_FM = 11,
+	RTC_GPIO_USER_PMIC = 12,
+} rtc_gpio_user_t;
 
 #ifdef CONFIG_MT6397_MISC
 extern void mtk_misc_mark_fast(void);
@@ -22,6 +31,8 @@ extern bool mtk_misc_low_power_detected(void);
 extern bool mtk_misc_crystal_exist_status(void);
 extern int mtk_misc_set_spare_fg_value(u32 val);
 extern u32 mtk_misc_get_spare_fg_value(void);
+extern void rtc_gpio_enable_32k(rtc_gpio_user_t user);
+extern void rtc_gpio_disable_32k(rtc_gpio_user_t user);
 #else
 #define mtk_misc_mark_fast()			do {} while (0)
 #define mtk_misc_mark_recovey()			do {} while (0)
@@ -29,6 +40,8 @@ extern u32 mtk_misc_get_spare_fg_value(void);
 #define mtk_misc_crystal_exist_status()		do {} while (0)
 #define mtk_misc_set_spare_fg_value(val)	do {} while (0)
 #define mtk_misc_get_spare_fg_value()		do {} while (0)
+#define rtc_gpio_enable_32k(user)		do {} while (0)
+#define rtc_gpio_disable_32k(user)		do {} while (0)
 #endif
 #endif /* __MT6397_RTC_MISC_H__ */
 
