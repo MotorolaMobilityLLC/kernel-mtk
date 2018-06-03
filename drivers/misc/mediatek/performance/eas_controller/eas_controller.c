7269cc60c943d9021111b58a223754b0a7a65a2f
Poting Chen <poting.chen@mediatek.com>
2018-06-03 16:28:06 +0800

    [ALPS03209968] Perfmgr: add driver
    [Detail] add driver
    1. FBC
    2. Boost Controller
    3. Fliper
    
    MTK-Commit-Id: 50acc55a438fc77a4bc70f804a83688a0d7fbc28
    
    Change-Id: I148f4390b1421d730170bc5a767e2091170719b9
    Signed-off-by: Poting Chen <poting.chen@mediatek.com>
    CR-Id: ALPS03209968
    Feature: DVFS (Dynamic Voltage and Frequency Scaling)

--- drivers/misc/mediatek/performance/eas_controller/eas_controller.c
+++ drivers/misc/mediatek/performance/eas_controller/eas_controller.c
@@ -26,7 +26,7 @@ static int debug;
 #ifdef CONFIG_SCHED_TUNE
 static int update_eas_boost_value(int kicker, int cgroup_idx, int value)
 {
-	int final_boost_value = -1, final_boost_value_1 = -1, final_boost_value_2 = -1;
+	int final_boost_value = 0, final_boost_value_1 = 0, final_boost_value_2 = 0;
 	int boost_1[MAX_KIR], boost_2[MAX_KIR];
 	int i;
 
@@ -50,16 +50,14 @@ static int update_eas_boost_value(int kicker, int cgroup_idx, int value)
 
 	if (final_boost_value > 3000)
 		current_fg_boost_value = 3000;
-	else if (final_boost_value < 0)
-		current_fg_boost_value = -1;
+	else if (final_boost_value < -100)
+		current_fg_boost_value = -100;
 	else
 		current_fg_boost_value = final_boost_value;
 
 	if (!debug) {
 		if (current_fg_boost_value > 0)
 			boost_value_for_GED_idx(CGROUP_FG, current_fg_boost_value);
-		else
-			boost_value_for_GED_idx(CGROUP_FG, 0);
 	}
 
 	mutex_unlock(&boost_eas);
@@ -105,8 +103,8 @@ static ssize_t perfmgr_perfserv_fg_boost_write(struct file *filp, const char *ub
 
 	if (data > 3000)
 		boost_value[KIR_PERF] = 3000;
-	else if (data < 0)
-		boost_value[KIR_PERF] = -1;
+	else if (data < -100)
+		boost_value[KIR_PERF] = -100;
 	else
 		boost_value[KIR_PERF] = data;
 
@@ -179,8 +177,8 @@ static ssize_t perfmgr_debug_fg_boost_write(struct file *filp, const char *ubuf,
 	if (data > 3000) {
 		fg_debug_boost_value = 3000;
 		debug = 1;
-	} else if (data < 0) {
-		fg_debug_boost_value = -1;
+	} else if (data < -100) {
+		fg_debug_boost_value = -100;
 		debug = 0;
 	} else {
 		fg_debug_boost_value = data;
@@ -229,11 +227,11 @@ void perfmgr_eas_boost_init(void)
 	proc_create("current_fg_boost", 0644, boost_dir, &perfmgr_current_fg_boost_fops);
 	proc_create("debug_fg_boost", 0644, boost_dir, &perfmgr_debug_fg_boost_fops);
 
-	current_fg_boost_value = -1;
+	current_fg_boost_value = 0;
 	for (i = 0; i < MAX_KIR; i++)
-		boost_value[i] = -1;
+		boost_value[i] = 0;
 
-	fg_debug_boost_value = -1;
+	fg_debug_boost_value = 0;
 	debug = 0;
 }
 
