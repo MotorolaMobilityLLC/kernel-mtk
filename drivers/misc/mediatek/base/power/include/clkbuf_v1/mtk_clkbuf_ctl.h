d3d6164826ea5367cee29e12be7b5213ac674096
Roy-CC.Chen <roy-cc.chen@mediatek.com>
2018-06-03 10:16:48 +0800

    [ALPS02861262] clkbuf: mt6799: introduce clock buffer driver
    [Detail]
    1. introduce clock buffer driver for mt6799
    2. Disable driver for bring up
    
    MTK-Commit-Id: bc1f7feaba8734c5787831a5f673a84ec4469cad
    
    Change-Id: Ia7ddb4be26027246622d89b699190447b0147269
    Signed-off-by: Roy-CC.Chen <roy-cc.chen@mediatek.com>
    CR-Id: ALPS02861262
    Feature: Power Management
    (cherry picked from commit 5699ada8ed9377687f539689527563c0acbeb943)

---
+++ drivers/misc/mediatek/base/power/include/clkbuf_v1/mtk_clkbuf_ctl.h
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+/**
+* @file    mtk_clk_buf_ctl.c
+* @brief   Driver for clock buffer control
+*
+*/
+#ifndef __MTK_CLK_BUF_CTL_H__
+#define __MTK_CLK_BUF_CTL_H__
+
+#ifdef __KERNEL__
+#include <linux/kernel.h>
+#include <linux/mutex.h>
+#ifdef CONFIG_MTK_LEGACY
+#include <cust_clk_buf.h>
+#endif
+#else /* LK */
+#include <cust_clk_buf.h>
+#endif /* __KERNEL__ */
+
+#ifdef __KERNEL__
+#ifndef CONFIG_MTK_LEGACY
+typedef enum {
+	CLOCK_BUFFER_DISABLE,
+	CLOCK_BUFFER_SW_CONTROL,
+	CLOCK_BUFFER_HW_CONTROL,
+} MTK_CLK_BUF_STATUS;
+
+typedef enum {
+	CLK_BUF_DRIVING_CURR_AUTO_K = -1,
+	CLK_BUF_DRIVING_CURR_0,
+	CLK_BUF_DRIVING_CURR_1,
+	CLK_BUF_DRIVING_CURR_2,
+	CLK_BUF_DRIVING_CURR_3
+} MTK_CLK_BUF_DRIVING_CURR;
+#endif
+#endif /* __KERNEL__ */
+
+typedef enum {
+	CLK_BUF_SW_DISABLE = 0,
+	CLK_BUF_SW_ENABLE  = 1,
+} CLK_BUF_SWCTRL_STATUS_T;
+
+/* clk_buf_id: users of clock buffer */
+enum clk_buf_id {
+	CLK_BUF_BB_MD		= 0,
+	CLK_BUF_CONN,
+	CLK_BUF_NFC,
+	CLK_BUF_RF,
+	CLK_BUF_AUDIO,
+	CLK_BUF_CHG,
+	CLK_BUF_UFS,
+	CLK_BUF_INVALID
+};
+
+/* xo_id: clock buffer list */
+enum xo_id {
+	XO_SOC	= 0,
+	XO_WCN,
+	XO_NFC,
+	XO_CEL,
+	XO_AUD,		/* Disabled */
+	XO_PD,		/* AUDIO_CHG */
+	XO_EXT,		/* UFS */
+	XO_NUMBER
+};
+
+#define CLKBUF_NUM      XO_NUMBER
+
+#define STA_CLK_ON      1
+#define STA_CLK_OFF     0
+
+int clk_buf_init(void);
+bool clk_buf_ctrl(enum clk_buf_id id, bool onoff);
+void clk_buf_get_swctrl_status(CLK_BUF_SWCTRL_STATUS_T *status);
+void clk_buf_get_rf_drv_curr(void *rf_drv_curr);
+void clk_buf_set_by_flightmode(bool is_flightmode_on);
+void clk_buf_save_afc_val(unsigned int afcdac);
+void clk_buf_write_afcdac(void);
+void clk_buf_control_bblpm(bool on);
+bool is_clk_buf_under_flightmode(void);
+bool is_clk_buf_from_pmic(void);
+
+#ifdef __KERNEL__
+extern struct mutex clk_buf_ctrl_lock;
+#endif /* __KERNEL__ */
+
+#endif
+
