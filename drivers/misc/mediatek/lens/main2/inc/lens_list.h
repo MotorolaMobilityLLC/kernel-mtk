06f20b2378aca552a23344616eeefc70f18ff9c8
mtk10485 <sam.hung@mediatek.com>
2018-06-03 15:56:43 +0800

    [ALPS03148010] Camera: lens driver improvment
    [Detail] 1. Add PMIC power control
             2. factory mode - lens testing
             3. CCU - driver i2c format
    
    [Solution]
    
    MTK-Commit-Id: a55aefed050ab71f38fb733b4af1115271576aeb
    
    Change-Id: Ia6cc028ade1712dd2c0db031679973461225e714
    Signed-off-by: mtk10485 <sam.hung@mediatek.com>
    CR-Id: ALPS03148010
    Feature: [Android Default] AF (Auto Focus)

--- drivers/misc/mediatek/lens/main2/inc/lens_list.h
+++ drivers/misc/mediatek/lens/main2/inc/lens_list.h
@@ -15,7 +15,6 @@
 
 #define _LENS_LIST_H
 
-#ifdef CONFIG_MTK_LENS_AK7371AF_SUPPORT
 #define AK7371AF_SetI2Cclient AK7371AF_SetI2Cclient_Main2
 #define AK7371AF_Ioctl AK7371AF_Ioctl_Main2
 #define AK7371AF_Release AK7371AF_Release_Main2
@@ -24,15 +23,12 @@ extern void AK7371AF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient, spinlock_t
 extern long AK7371AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command, unsigned long a_u4Param);
 extern int AK7371AF_Release(struct inode *a_pstInode, struct file *a_pstFile);
 extern int AK7371AF_PowerDown(void);
-#endif
 
-#ifdef CONFIG_MTK_LENS_LC898212XDAF_SUPPORT
 #define LC898212XDAF_F_SetI2Cclient LC898212XDAF_F_SetI2Cclient_Main2
 #define LC898212XDAF_F_Ioctl LC898212XDAF_F_Ioctl_Main2
 #define LC898212XDAF_F_Release LC898212XDAF_F_Release_Main2
 extern void LC898212XDAF_F_SetI2Cclient(struct i2c_client *pstAF_I2Cclient, spinlock_t *pAF_SpinLock, int *pAF_Opened);
 extern long LC898212XDAF_F_Ioctl(struct file *a_pstFile, unsigned int a_u4Command, unsigned long a_u4Param);
 extern int LC898212XDAF_F_Release(struct inode *a_pstInode, struct file *a_pstFile);
-#endif
 
 #endif
