44504377f2b79024ab14661a3ebce69e2a158af2
Samuel Hsieh <samuel.hsieh@mediatek.com>
2018-06-03 12:50:03 +0800

    [ALPS03789045] clkbuf: mt6761 clock buffer driver porting
    [Detail]
    mt6761 clock buffer driver porting
    
    MTK-Commit-Id: f41b5e95ce23d086b567dac9aa289fa4e13d2c5e
    
    Change-Id: I9a54d030cbe4b952a65a42962ea5df2a976ac7ee
    Signed-off-by: Samuel Hsieh <samuel.hsieh@mediatek.com>
    CR-Id: ALPS03789045
    Feature: Others
    (cherry picked from commit ff9d723524d2190cdd412c51a675c3492fd4accb)

--- drivers/misc/mediatek/base/power/include/mtk_clkbuf_ctl.h
+++ drivers/misc/mediatek/base/power/include/mtk_clkbuf_ctl.h
@@ -42,7 +42,8 @@
 #elif defined(CONFIG_MACH_ELBRUS) || defined(CONFIG_MACH_MT6799) || \
 	defined(CONFIG_MACH_MT6758) || defined(CONFIG_MACH_MT6759) || \
 	defined(CONFIG_MACH_MT6763) || defined(CONFIG_MACH_MT6739) || \
-	defined(CONFIG_MACH_MT6775) || defined(CONFIG_MACH_MT6765)
+	defined(CONFIG_MACH_MT6775) || defined(CONFIG_MACH_MT6765) || \
+	defined(CONFIG_MACH_MT6761)
 
 #include "clkbuf_v1/mtk_clkbuf_ctl.h"
 
