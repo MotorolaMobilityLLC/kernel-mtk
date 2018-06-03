14b9a58447351398e26ff9c56014f8d096e3607f
JY Lan <jy.lan@mediatek.com>
2018-06-04 00:39:18 +0800

    [ALPS03736054] gyroscope: add gyroscope temperature
    [Detail]
    add proprietary gyroscope temperature for fusion sensor usage.
    
    MTK-Commit-Id: afc96f0c156138eff0e0bcb2f07602135975f176
    
    Change-Id: I7457a3d3528e83c1cf667b8057330b650b7e6a53
    Signed-off-by: JY Lan <jy.lan@mediatek.com>
    CR-Id: ALPS03736054
    Feature: Fused Location

--- drivers/misc/mediatek/sensors-1.0/sensorfusion/uncali_gyrohub/uncali_gyrohub.c
+++ drivers/misc/mediatek/sensors-1.0/sensorfusion/uncali_gyrohub/uncali_gyrohub.c
@@ -67,6 +67,7 @@ static int uncali_gyro_recv_data(struct data_unit_t *event, void *reserved)
 {
 	int err = 0;
 	int value[6] = {0};
+	int value_temp[6] = {0};
 
 #if defined CONFIG_MTK_SCP_SENSORHUB_V1
 	value[0] = event->uncalibrated_gyro_t.x;
@@ -82,10 +83,17 @@ static int uncali_gyro_recv_data(struct data_unit_t *event, void *reserved)
 	value[3] = event->uncalibrated_gyro_t.x_bias;
 	value[4] = event->uncalibrated_gyro_t.y_bias;
 	value[5] = event->uncalibrated_gyro_t.z_bias;
+	value_temp[0] = event->uncalibrated_gyro_t.temperature;
+	value_temp[1] = event->uncalibrated_gyro_t.temp_result;
 #endif
-	if (event->flush_action == DATA_ACTION)
+	if (event->flush_action == DATA_ACTION) {
 		err = uncali_gyro_data_report(value, event->uncalibrated_gyro_t.status,
 			(int64_t)event->time_stamp);
+		/*pr_debug("report gyro_temp : %d, %d\n", value_temp[0], value_temp[1]);*/
+		if (value_temp[0] != 0)
+			uncali_gyro_temperature_data_report(value_temp, event->uncalibrated_gyro_t.status,
+			(int64_t)event->time_stamp);
+	}
 	else if (event->flush_action == FLUSH_ACTION)
 		err = uncali_gyro_flush_report();
 
