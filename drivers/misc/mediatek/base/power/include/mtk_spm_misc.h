04c59862c15a2a7fca704a88b3589bd28d9b846a
Max Yu <max.yu@mediatek.com>
2018-06-03 21:39:38 +0800

    [ALPS03512406] SPM: mt6771 use include file(spm_v4)
    [Detail]
    mt6771 use include file(spm_v4)
    
    MTK-Commit-Id: 2901f43fbdf4a1365f91a8889860e2c34e4efc57
    
    Change-Id: I6d9f19f6d2ca9e8b8816518325ffd81ad8af767f
    Signed-off-by: Max Yu <max.yu@mediatek.com>
    CR-Id: ALPS03512406
    Feature: VCORE DVFS

--- drivers/misc/mediatek/base/power/include/mtk_spm_misc.h
+++ drivers/misc/mediatek/base/power/include/mtk_spm_misc.h
@@ -22,7 +22,7 @@
 
 #include "spm_v3/mtk_spm_misc.h"
 
-#elif defined(CONFIG_MACH_MT6763)
+#elif defined(CONFIG_MACH_MT6763) || defined(CONFIG_MACH_MT6771)
 
 #include "spm_v4/mtk_spm_misc.h"
 
--- drivers/misc/mediatek/base/power/include/mtk_spm_vcore_dvfs.h
+++ drivers/misc/mediatek/base/power/include/mtk_spm_vcore_dvfs.h
@@ -26,7 +26,7 @@
 
 #include "spm_v3/mtk_spm_vcore_dvfs.h"
 
-#elif defined(CONFIG_MACH_MT6763) || defined(CONFIG_MACH_MT6739)
+#elif defined(CONFIG_MACH_MT6763) || defined(CONFIG_MACH_MT6739) || defined(CONFIG_MACH_MT6771)
 
 #include "spm_v4/mtk_spm_vcore_dvfs.h"
 
