478b55e62a6cc3a02fa586df6d151976b74d8152
Cheng-En Chung <cheng-en.chung@mediatek.com>
2018-06-03 22:12:07 +0800

    [ALPS03530322] QoS: Add sspm ipi in kernel
    [Detail]
    Add sspm ipi in kernel
    
    MTK-Commit-Id: f04e206408b75645ec6befaa04feee830fb374b1
    
    Change-Id: I5e59562a97cac14ec94b48fb14c0f6a22517fb81
    Signed-off-by: Cheng-En Chung <cheng-en.chung@mediatek.com>
    CR-Id: ALPS03530322
    Feature: VCORE DVFS

---
+++ drivers/misc/mediatek/base/power/include/mtk_spm_vcore_dvfs_ipi.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2017 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#ifndef __MTK_SPM_VCORE_DVFS_IPI_H__
+#define __MTK_SPM_VCORE_DVFS_IPI_H__
+
+#if defined(CONFIG_MACH_MT6775)
+
+#include "spm_v3/mtk_spm_vcore_dvfs_ipi_mt6775.h"
+
+#endif
+
+#endif
