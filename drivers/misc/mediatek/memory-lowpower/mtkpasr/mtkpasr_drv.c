3f44bc35e0b5cfd5d0d35a95be84c7a3dc0b98b2
Chinwen Chang <chinwen.chang@mediatek.com>
2018-06-03 12:33:12 +0800

    [ALPS02946564] pasr: move vcorefs control out from CONFIG_MTK_DCS
    Move vcorefs control out from CONFIG_MTK_DCS for mt6799.
    
    MTK-Commit-Id: 6bc0bf87578a62bd943b2bd1afc323c9ac443669
    
    Change-Id: I0e285077feb86367617a3fe52f32773b788764a5
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>
    CR-Id: ALPS02946564
    Feature: PASR (Partial Array Self-Refresh)

--- drivers/misc/mediatek/memory-lowpower/mtkpasr/mtkpasr_drv.c
+++ drivers/misc/mediatek/memory-lowpower/mtkpasr/mtkpasr_drv.c
@@ -27,8 +27,11 @@
 /* Header file for DRAMC PASR configuration */
 #include <mtk_dramc.h>
 
-#ifdef CONFIG_MTK_DCS
+#ifdef MTK_PASR_VCORE_DVFS_CONTROL
 #include <mtk_vcorefs_manager.h>
+#endif
+
+#ifdef CONFIG_MTK_DCS
 #include <mt-plat/mtk_meminfo.h>
 #endif
 
@@ -250,15 +253,21 @@ static int mtkpasr_config(int times, get_range_t func)
 		if (mtkpasr_banks[i].free == (mtkpasr_banks[i].end_pfn - mtkpasr_banks[i].start_pfn))
 			mtkpasr_on |= (1 << mtkpasr_banks[i].segment);
 
+#ifdef MTK_PASR_VCORE_DVFS_CONTROL
+	vcorefs_request_dvfs_opp(KIR_PASR, OPP_0);
+#endif
+
 #ifndef CONFIG_MTK_DCS
 	/* APMCU flow */
 	MTKPASR_PRINT("%s: PASR[0x%lx]\n", __func__, mtkpasr_on);
 	if (enter_pasr_dpd_config(mtkpasr_on & 0xFF, mtkpasr_on >> 0x8) != 0)
 		MTKPASR_PRINT("%s: failed to program DRAMC!\n", __func__);
 #else
-	vcorefs_request_dvfs_opp(KIR_PASR, OPP_0);
 	/* Channel based PASR configuration */
 	enable_dcs_pasr();
+#endif
+
+#ifdef MTK_PASR_VCORE_DVFS_CONTROL
 	vcorefs_request_dvfs_opp(KIR_PASR, OPP_UNREQ);
 #endif
 
@@ -284,11 +293,17 @@ static int mtkpasr_restore(void)
 
 	MTKPASR_PRINT("%s:+\n", __func__);
 
+#ifdef MTK_PASR_VCORE_DVFS_CONTROL
+	vcorefs_request_dvfs_opp(KIR_PASR, OPP_0);
+#endif
+
 #ifndef CONFIG_MTK_DCS
 	restore_pasr();
 #else
-	vcorefs_request_dvfs_opp(KIR_PASR, OPP_0);
 	disable_dcs_pasr();
+#endif
+
+#ifdef MTK_PASR_VCORE_DVFS_CONTROL
 	vcorefs_request_dvfs_opp(KIR_PASR, OPP_UNREQ);
 #endif
 
