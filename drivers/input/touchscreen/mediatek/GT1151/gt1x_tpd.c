4fbe91aa7fa758338425fac4a4ebd66083ea708a
Jane Zhang <Jane.Zhang@mediatek.com>
2018-06-03 10:40:25 +0800

    [ALPS03491722] touch: remove update
    [Detail]
    remove update
    
    MTK-Commit-Id: bacd0ad8ba7f229c5d9d43bd269203f95c9bbd92
    
    Change-Id: I5e1dc0c3ea7e6d950af124d36e42fe0f9ec850a5
    CR-Id: ALPS03491722
    Feature: Others
    Signed-off-by: Jane Zhang <Jane.Zhang@mediatek.com>

--- drivers/input/touchscreen/mediatek/GT1151/gt1x_tpd.c
+++ drivers/input/touchscreen/mediatek/GT1151/gt1x_tpd.c
@@ -52,7 +52,7 @@ static int tpd_eint_mode = 1;
 static struct task_struct *thread;
 static struct task_struct *update_thread;
 static struct task_struct *probe_thread;
-static struct notifier_block pm_notifier_block;
+/* static struct notifier_block pm_notifier_block; */
 static int tpd_polling_time = 50;
 static DECLARE_WAIT_QUEUE_HEAD(waiter);
 static DECLARE_WAIT_QUEUE_HEAD(pm_waiter);
@@ -636,7 +636,7 @@ static int tpd_registration(void *client)
 	gt1x_esd_switch(SWITCH_ON);
 #endif
 
-#ifdef CONFIG_GTP_AUTO_UPDATE
+#if 0
 
 	update_thread = kthread_run(gt1x_auto_update_proc,
 					(void *)NULL, "gt1x_auto_update");
