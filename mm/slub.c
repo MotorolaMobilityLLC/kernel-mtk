e122150d3193de8f43b378d985d9a50caa20ac94
Miles Chen <miles.chen@mediatek.com>
2018-06-03 12:35:17 +0800

    [ALPS03774024] slub: revert "modify COMPAT_SLUB"
    This reverts commit 8184ca45cabe ("mem: modify COMPAT_SLUB")
    We bisected that this commit may cause a booting issue,
    so revert it.
    
    MTK-Commit-Id: 98b570b087a76ffbafa4937d70de041385b1a0fa
    
    Change-Id: Iecd9662cde367b5b89f42b4a6992a87b25e8e2c2
    CR-Id: ALPS03774024
    Feature: Monkey Test(*)
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

--- mm/slub.c
+++ mm/slub.c
@@ -41,9 +41,7 @@
 
 #ifdef CONFIG_ARM64
 #ifdef CONFIG_MTK_MEMCFG
-#ifndef CONFIG_RANDOMIZE_BASE
 #define MTK_COMPACT_SLUB_TRACK
-#endif
 #endif
 #endif
 
@@ -205,12 +203,7 @@ static inline bool kmem_cache_has_cpu_partial(struct kmem_cache *s)
 /*
  * Tracking user of a slab.
  */
-#ifdef CONFIG_RANDOMIZE_BASE
-#define TRACK_ADDRS_COUNT 4
-#else
 #define TRACK_ADDRS_COUNT 8
-#endif
-
 struct track {
 	unsigned long addr;	/* Called from address */
 #ifdef CONFIG_STACKTRACE
@@ -3734,7 +3727,6 @@ static void list_slab_objects(struct kmem_cache *s, struct page *page,
 				     sizeof(long), GFP_ATOMIC);
 	if (!map)
 		return;
-	slab_err(s, page, text, s->name);
 	slab_lock(page);
 
 	get_map(s, page, map);
@@ -3747,6 +3739,7 @@ static void list_slab_objects(struct kmem_cache *s, struct page *page,
 	}
 	slab_unlock(page);
 	kfree(map);
+	slab_err(s, page, text, s->name);
 #endif
 }
 
