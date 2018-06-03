5278a5c5b8a9b602aa5c07c0d29c6230343d80b4
mtk15426 <chih-wei.chiou@mediatek.com>
2018-06-03 11:18:52 +0800

    [ALPS03664463] spm: remove lowbattery irq from backup list
    [Detail]
    The lowbattery interrupt(irq 108) is de-feature in mt6765.
    It wont be initialize as edge trigger interrupt.
    If we backup the lowbattery interrupt as edge trigger
    then the interrupt enable bit and pending status will be set.
    
    It will let the system cant go to idle because isr check.
    
    [Solution]
    Remove lowbattery interrupt from backup list
    
    MTK-Commit-Id: e94ccef682952defe34302aaf8f4ff8c9e4d759d
    
    Change-Id: Id8f3562966710f0b3a74dba2dedab55fb3108f0b
    Signed-off-by: mtk15426 <chih-wei.chiou@mediatek.com>
    CR-Id: ALPS03664463
    Feature: SODI (Screen On Deep Idle)

--- drivers/misc/mediatek/base/power/spm/mt6765/mtk_spm_irq_edge.h
+++ drivers/misc/mediatek/base/power/spm/mt6765/mtk_spm_irq_edge.h
@@ -29,7 +29,6 @@ static struct edge_trigger_irq_list list[] = {
 	{ "mediatek,infracfg_ao",   0,	0 },
 	{ "mediatek,kp",            0,  WAKE_SRC_R12_KP_IRQ_B },
 	{ "mediatek,mdcldma",       3,  WAKE_SRC_R12_CLDMA_EVENT_B },
-	{ "mediatek,auxadc",        0,  WAKE_SRC_R12_LOWBATTERY_IRQ_B },
 };
 
 #endif /* __MTK_SPM_IRQ_EDGE_H__ */
