878cf7d99b460bce690fb59b9332e1e032e79e7d
luise.chen <luise.chen@mediatek.com>
2018-06-03 19:46:20 +0800

    [ALPS03091602] GED driver: Add GFX perf ftrace log - 2
    [Detail]
    Add more info for EAT perf log
    [Solution]
    Add more info for EAT perf log
    
    MTK-Commit-Id: 54d935e01460bb54b2dad76f74118471f59299b4
    
    Change-Id: I83bd8b0d7f33adb2feacbc2e07c58db05ee1c19d
    CR-Id: ALPS03091602
    Feature: Gaming Low Power Mode
    Signed-off-by: luise.chen <luise.chen@mediatek.com>

--- drivers/misc/mediatek/gpu/ged/include/ged_log.h
+++ drivers/misc/mediatek/gpu/ged/include/ged_log.h
@@ -64,7 +64,7 @@ void ged_log_trace_end(void);
 
 void ged_log_trace_counter(char *name, int count);
 
-void ged_log_perf_trace_counter(char *name, int count);
+void ged_log_perf_trace_counter(char *name, long long count, int pid, unsigned long frameID);
 
 void ged_log_dump(GED_LOG_BUF_HANDLE hLogBuf);
 
