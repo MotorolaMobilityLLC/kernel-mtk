4db5b5d82a38bda50577110d901e30bf6e44dc24
Johnson Lin <johnson.lin@mediatek.com>
2018-06-03 16:04:41 +0800

    [ALPS03167256] MRDUMP_KEY: support DTS config
    [Detail]
    MRDUMP_KEY could be triggered either from
    EINT or SYSRST, and the action is either
    FIQ interrupt or reboot immediately
    Add source and mode config into DTS node
    as below example
    source:"SYSRST" or "EINT"
    mode:"IRQ" or "RST"
    
           mrdump_ext_rst: mrdump_ext_rst {
                   compatible = "mediatek, mrdump_ext_rst-eint";
                   source = "EINT";
                   mode = "IRQ";
                   status = "okay";
           };
    eache platform will have its own configuration
    
    MTK-Commit-Id: 6234381360495f38a38693d489d85ae8e4acd45c
    
    Change-Id: I59cd387ea0b76142e21b45531bb3fe624468724d
    Signed-off-by: Johnson Lin <johnson.lin@mediatek.com>
    CR-Id: ALPS03167256
    Feature: MRDUMP_KEY

--- drivers/misc/mediatek/aee/mrdump/mrdump_key_setup.c
+++ drivers/misc/mediatek/aee/mrdump/mrdump_key_setup.c
@@ -13,25 +13,74 @@
 
 #include <linux/kconfig.h>
 #include <linux/module.h>
+#ifdef CONFIG_OF
+#include <linux/of.h>
+#endif
 #ifdef CONFIG_MTK_WD_KICKER
 #include <mach/wd_api.h>
+
+enum MRDUMP_RST_SOURCE {
+	MRDUMP_SYSRST,
+	MRDUMP_EINT};
+enum MRDUMP_NOTIFY_MODE {
+	MRDUMP_IRQ,
+	MRDUMP_RST};
+
 static int __init mrdump_key_init(void)
 {
 	int res;
 	struct wd_api *wd_api = NULL;
-#if defined(CONFIG_FPGA_EARLY_PORTING)
-	return 0;
-#endif
+	enum MRDUMP_RST_SOURCE source = MRDUMP_EINT;
+	enum MRDUMP_NOTIFY_MODE mode = MRDUMP_IRQ;
+	struct device_node *node;
+	const char *source_str, *mode_str;
+	char node_name[] = "mediatek, mrdump_ext_rst-eint";
+
+	node = of_find_compatible_node(NULL, NULL, node_name);
+	if (node) {
+		if (!of_property_read_string(node, "source", &source_str)) {
+			if (strcmp(source_str, "SYSRST") == 0)
+				source = MRDUMP_SYSRST;
+		} else {
+			pr_alert("MRDUMP_KEY: no source property, use legacy value EINT");
+		}
+
+		if (!of_property_read_string(node, "mode", &mode_str)) {
+			if (strcmp(mode_str, "RST") == 0)
+				mode = MRDUMP_RST;
+		} else {
+			pr_alert("MRDUMP_KEY: no mode property, use legacy value IRQ");
+		}
+	} else {
+		pr_alert("%s:MRDUMP_KEY node %s is not exist\n"
+			"MRDUMP_KEY is disabled\n", __func__, node_name);
+		goto out;
+	}
+
 	res = get_wd_api(&wd_api);
 	if (res < 0) {
-		pr_alert("%s: get wd api error %d\n", __func__, res);
+		pr_alert("%s: get_wd_api failed:%d\n"
+				"disable MRDUMP_KEY\n", __func__, res);
 	} else {
-		res = wd_api->wd_debug_key_sysrst_config(1, 0);
-		if (res == -1)
-			pr_alert("%s: MRDUMP_KEY not supported\n", __func__);
-		else
-			pr_alert("%s: MRDUMP_KEY enabled\n", __func__);
+		if (source == MRDUMP_SYSRST) {
+			res = wd_api->wd_debug_key_sysrst_config(1, mode);
+			if (res == -1)
+				pr_alert("%s: sysrst failed\n", __func__);
+			else
+				pr_alert("%s: enable MRDUMP_KEY\n", __func__);
+		} else if (source == MRDUMP_EINT) {
+			res = wd_api->wd_debug_key_eint_config(1, mode);
+			if (res == -1)
+				pr_alert("%s: eint failed\n", __func__);
+			else
+				pr_alert("%s: enabled MRDUMP_KEY\n", __func__);
+		} else {
+			pr_alert("%s:source %d is not match\n"
+				"disable MRDUMP_KEY\n", __func__, source);
+		}
 	}
+out:
+	of_node_put(node);
 	return 0;
 }
 
