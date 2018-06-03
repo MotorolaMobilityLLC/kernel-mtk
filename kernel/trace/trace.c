eb832c3881ead5cd4a6821a0f9de93ebf8883cfb
Andress Kuo <andress.kuo@mediatek.com>
2018-06-04 00:15:03 +0800

    [ALPS03743706] ftrace: Fix crash when it fails to alloc ring buffer
    Double free of the ring buffer happens when it fails to alloc new
    ring buffer instance for max_buffer if TRACER_MAX_TRACE is configured.
    The root cause is that the pointer is not set to NULL after the buffer
    is freed in allocate_trace_buffers(), and the freeing of the ring
    buffer is invoked again later if the pointer is not equal to Null,
    as:
    
    instance_mkdir()
        |-allocate_trace_buffers()
            |-allocate_trace_buffer(tr, &tr->trace_buffer...)
    	|-allocate_trace_buffer(tr, &tr->max_buffer...)
    
              // allocate fail(-ENOMEM),first free
              // and the buffer pointer is not set to null
            |-ring_buffer_free(tr->trace_buffer.buffer)
    
           // out_free_tr
        |-free_trace_buffers()
            |-free_trace_buffer(&tr->trace_buffer);
    
    	      //if trace_buffer is not null, free again
    	    |-ring_buffer_free(buf->buffer)
                    |-rb_free_cpu_buffer(buffer->buffers[cpu])
                        // ring_buffer_per_cpu is null, and
                        // crash in ring_buffer_per_cpu->pages
    
    MTK-Commit-Id: f19112f41e0fe946baacd779ef608a03590d9707
    
    Change-Id: If2c10e7d6c3d5a8f1d327ba547506e0c0fd06b29
    CR-Id: ALPS03743706
    Feature: ftrace
    Signed-off-by: Andress Kuo <andress.kuo@mediatek.com>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -6781,7 +6781,9 @@ static int allocate_trace_buffers(struct trace_array *tr, int size)
 	if (WARN_ON(ret)) {
 		pr_debug("[ftrace]allocate_trace_buffers size %d\n", size);
 		ring_buffer_free(tr->trace_buffer.buffer);
+		tr->trace_buffer.buffer = NULL;
 		free_percpu(tr->trace_buffer.data);
+		tr->trace_buffer.data = NULL;
 		return -ENOMEM;
 	}
 	tr->allocated_snapshot = allocate_snapshot;
