3bd5a5a39a6fcef07df57bda532bdb2fe1455b15
Max Yu <max.yu@mediatek.com>
2018-06-03 15:18:15 +0800

    [ALPS03113085] Vcore DVFS: Bianco early porting
    [Detail]
    Bianco early porting
    
    [Solution]
    Bianco early porting
    
    MTK-Commit-Id: 001775b4e86e80cf5423c5bb39ce1b9ce4facc61
    
    Change-Id: I4a3a1c6d3b633b01bc340c73cb46f4e6d01af47b
    Signed-off-by: Max Yu <max.yu@mediatek.com>
    CR-Id: ALPS03113085
    Feature: VCORE DVFS

---
+++ drivers/misc/mediatek/base/power/include/spm_v3/mtk_spm_vcore_dvfs_mt6763.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#ifndef __MTK_SPM_VCORE_DVFS_H__
+#define __MTK_SPM_VCORE_DVFS_H__
+
+#include "mtk_spm.h"
+#include <mtk_vcorefs_manager.h>
+
+/* Feature will disable both of DVS/DFS are 0 */
+#define SPM_VCORE_DVS_EN       0
+#define SPM_DDR_DFS_EN         0
+#define SPM_MM_CLK_EN          0
+
+#define SPM_DVFS_TIMEOUT       1000	/* 1ms */
+
+enum dvfsrc_channel {
+	DVFSRC_CHANNEL_1 = 1,
+	DVFSRC_CHANNEL_2,
+	DVFSRC_CHANNEL_3,
+	DVFSRC_CHANNEL_4,
+	NUM_DVFSRC_CHANNEL,
+};
+
+enum dvfsrc_hrt_kir {
+	DVFSRC_HRT_MM,
+	DVFSRC_HRT_CAM,
+	DVFSRC_HRT_MD,
+	NUM_DVFSRC_HRT_KIR,
+};
+
+enum vcorefs_smc_cmd {
+	VCOREFS_SMC_CMD_0,
+	VCOREFS_SMC_CMD_1,
+	NUM_VCOREFS_SMC_CMD,
+};
+
+extern void spm_go_to_vcorefs(int spm_flags);
+extern int spm_set_vcore_dvfs(struct kicker_config *krconf);
+extern void spm_vcorefs_init(void);
+extern int spm_dvfs_flag_init(void);
+extern char *spm_vcorefs_dump_dvfs_regs(char *p);
+extern u32 spm_vcorefs_get_MD_status(void);
+extern int spm_vcorefs_pwarp_cmd(void);
+extern void spm_dvfsrc_set_channel_bw(enum dvfsrc_channel);
+extern void spm_dvfsrc_hrt_bw_config(enum dvfsrc_hrt_kir hrt_kir, bool config);
+extern int spm_vcorefs_get_opp(void);
+
+#endif /* __MTK_SPM_VCORE_DVFS_H__ */
