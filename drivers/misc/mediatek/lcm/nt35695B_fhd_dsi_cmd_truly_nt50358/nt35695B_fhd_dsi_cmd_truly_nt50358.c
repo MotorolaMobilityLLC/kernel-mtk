7e526ae67a3c2d257d3ca4471d65943b9f6ec56d
k zhang <k.zhang@mediatek.com>
2018-06-03 19:12:58 +0800

    [ALPS02927026] display: add lcm driver for NT35695B with AUO lcd
    [Detail]
    1. add lcm driver for NT35695B with AUO lcd
    2. To dynamic use different LCM driver, we need to modify i2c init
      method to dynamicaly init. This patch modifies some orignal
      nt35695 LCM drivers to dynamic init i2c functions.
    
    MTK-Commit-Id: 616d2a915f9ff555c010ae8ffcdfc94dba8ec9d0
    
    Change-Id: I6b72aced1e356c43e521d0e0068e2a687a84e0b8
    Signed-off-by: k zhang <k.zhang@mediatek.com>
    CR-Id: ALPS02927026
    Feature: Display Driver (DSI Interface)

--- drivers/misc/mediatek/lcm/nt35695B_fhd_dsi_cmd_truly_nt50358/nt35695B_fhd_dsi_cmd_truly_nt50358.c
+++ drivers/misc/mediatek/lcm/nt35695B_fhd_dsi_cmd_truly_nt50358/nt35695B_fhd_dsi_cmd_truly_nt50358.c
@@ -112,7 +112,7 @@ static const struct of_device_id lcm_of_match[] = {
 #endif
 
 /*static struct i2c_client *tps65132_i2c_client;*/
-struct i2c_client *tps65132_i2c_client;
+static struct i2c_client *tps65132_i2c_client;
 
 /*****************************************************************************
  * Function Prototype
@@ -168,7 +168,7 @@ static int tps65132_remove(struct i2c_client *client)
 
 /*static int tps65132_write_bytes(unsigned char addr, unsigned char value)*/
 #if !defined(CONFIG_ARCH_MT6797)
-int tps65132_write_bytes(unsigned char addr, unsigned char value)
+static int tps65132_write_bytes(unsigned char addr, unsigned char value)
 {
 	int ret = 0;
 	struct i2c_client *client = tps65132_i2c_client;
@@ -183,31 +183,33 @@ int tps65132_write_bytes(unsigned char addr, unsigned char value)
 }
 #endif
 
-static int __init tps65132_iic_init(void)
+static int tps65132_iic_init(void)
 {
+	static int inited;
+	int ret;
+
+	if (inited)
+		return 0;
+
+	inited = 1;
+
 	LCM_LOGI("tps65132_iic_init\n");
-#if defined(CONFIG_MTK_LEGACY)
-	i2c_register_board_info(TPS_I2C_BUSNUM, &tps65132_board_info, 1);
-#endif
-	LCM_LOGI("tps65132_iic_init2\n");
-	i2c_add_driver(&tps65132_iic_driver);
-	LCM_LOGI("tps65132_iic_init success\n");
+	ret = i2c_add_driver(&tps65132_iic_driver);
+
+	if (ret)
+		LCM_LOGI("error: lcm tps65132_iic_init fail !!!\n");
+	else
+		LCM_LOGI("tps65132_iic_init success\n");
 	return 0;
 }
 
-static void __exit tps65132_iic_exit(void)
+#if 0
+static void tps65132_iic_exit(void)
 {
 	LCM_LOGI("tps65132_iic_exit\n");
 	i2c_del_driver(&tps65132_iic_driver);
 }
-
-
-module_init(tps65132_iic_init);
-module_exit(tps65132_iic_exit);
-
-MODULE_AUTHOR("Mike LIU");
-MODULE_DESCRIPTION("MTK TPS65132 I2C Driver");
-MODULE_LICENSE("GPL");
+#endif
 #endif
 #endif
 
@@ -1650,6 +1652,8 @@ static void lcm_init(void)
 	cmd = 0x00;
 	data = 0x0E;
 
+	tps65132_iic_init();
+
 	SET_RESET_PIN(0);
 
 #ifndef CONFIG_FPGA_EARLY_PORTING
@@ -1762,7 +1766,7 @@ static void lcm_update(unsigned int x, unsigned int y, unsigned int width, unsig
 
 static unsigned int lcm_compare_id(void)
 {
-	unsigned int id = 0;
+	unsigned int id = 0, version_id = 0;
 	unsigned char buffer[2];
 	unsigned int array[16];
 
@@ -1777,11 +1781,14 @@ static unsigned int lcm_compare_id(void)
 	dsi_set_cmdq(array, 1, 1);
 
 	read_reg_v2(0xF4, buffer, 2);
-	id = buffer[0];		/* we only need ID */
+	id = buffer[0];     /* we only need ID */
 
-	LCM_LOGI("%s,nt35695 debug: nt35695 id = 0x%08x\n", __func__, id);
+	read_reg_v2(0xDB, buffer, 1);
+	version_id = buffer[0];
 
-	if (id == LCM_ID_NT35695)
+	LCM_LOGI("%s,nt35695_id=0x%08x,version_id=0x%x\n", __func__, id, version_id);
+
+	if (id == LCM_ID_NT35695 && version_id == 0x81)
 		return 1;
 	else
 		return 0;
