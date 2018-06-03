27de8ced8413c21b02ade0051877a45856751992
chien-wei hsu <chien-wei.hsu@mediatek.com>
2018-06-03 16:42:14 +0800

    [ALPS02773746] Sound: Fix mt6799 kernel warning
    [Detail]
    Fix kernel warning of mt6799 and mt6337
    
    MTK-Commit-Id: 060ddcbf58271aea6c842b22a7aa636915df1e16
    
    Change-Id: Ib0b1c5be2fe0c0b38add884b9cef2f55c2b5f308
    Signed-off-by: chien-wei hsu <chien-wei.hsu@mediatek.com>
    CR-Id: ALPS02773746
    Feature: Driver

--- sound/soc/mediatek/codec/mt6337/mtk-auddrv-ana.h
+++ sound/soc/mediatek/codec/mt6337/mtk-auddrv-ana.h
@@ -63,12 +63,6 @@
 /*****************************************************************************
  *                  R E G I S T E R       D E F I N I T I O N
  *****************************************************************************/
-typedef	uint8 kal_uint8;
-typedef	int8 kal_int8;
-typedef	uint32 kal_uint32;
-typedef	int32 kal_int32;
-typedef	uint64 kal_uint64;
-typedef	int64 kal_int64;
 
 #define PMIC_REG_BASE                    (0x8000)
 #define PMIC_AUDIO_SYS_TOP_REG_BASE      (0xE000)
