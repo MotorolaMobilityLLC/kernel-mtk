b7e7be4c0a922848d3c03456317f06f4235ec2e6
Jane Zhang <Jane.Zhang@mediatek.com>
2018-06-03 12:51:05 +0800

    [ALPS03753015] timer: flush cpu base lock after unlocking
    [Detail]
    
    MTK PATCH to fix ARM v8.0 live spinlock issue.
    
    Flush lock value here if timer cancelling or deletion
    is not finished.
    
    In this case, Other CPU may need to get cpu_base spinlock
    to update running timer information. Flush lock value specifically
    after unlocking to promise that other CPU can see correct
    lock value to avoid starvation or unfair spinlock competition.
    
    MTK-Commit-Id: 7dca09116e6932669a94eb50a27f0d0fe3a2b360
    
    Change-Id: I3573eb9668d928ce72966b12f81cad86827426e1
    Signed-off-by: Jane Zhang <Jane.Zhang@mediatek.com>
    CR-Id: ALPS03753015
    Feature: Others
    (cherry picked from commit fd02c0503e947124d62662d5875132d2257ba7a4)

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -43,6 +43,7 @@
 #include <linux/slab.h>
 #include <linux/compat.h>
 
+#include <asm/cacheflush.h>
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
 #include <asm/div64.h>
@@ -1224,6 +1225,33 @@ int try_to_del_timer_sync(struct timer_list *timer)
 		ret = detach_if_pending(timer, base, true);
 	}
 	spin_unlock_irqrestore(&base->lock, flags);
+
+#if defined(CONFIG_SMP) && !defined(CONFIG_ARM64_LSE_ATOMICS)
+
+#ifndef dmac_flush_range
+#define dmac_flush_range __dma_flush_range
+#endif
+
+	/*
+	 * MTK PATCH to fix ARM v8.0 live spinlock issue.
+	 *
+	 * Flush lock value here if timer deletion is not finished.
+	 *
+	 * In this case, Other CPU may need to get cpu_base spinlock
+	 * to update running timer information. Flush lock value here
+	 * to promise that other CPU can see correct lock value to avoid
+	 * starvation or unfair spinlock competition.
+	 */
+	if (ret == -1 && irqs_disabled()) {
+#ifdef CONFIG_ARM64
+		__dma_flush_area((void *)&base->lock,
+			sizeof(spinlock_t));
+#else
+		dmac_flush_range((void *)&base->lock,
+			(void *)&base->lock + sizeof(spinlock_t) - 1);
+#endif
+	}
+#endif
 
 	return ret;
 }
