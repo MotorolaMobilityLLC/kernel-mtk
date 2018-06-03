436a4a431fae0cce1d8c8e915248c53192e9d690
KuanFu Yeh <kuanfu.yeh@mediatek.com>
2018-06-03 23:43:28 +0800

    [ALPS03660698] CCU: fix printk too much
    Dont block wait irq ioctl even if CCU is powered-off.
    
    MTK-Commit-Id: d61660fc65e77b461e1067d51a1a8b5b0ea52057
    
    Change-Id: I5c79be8d24fc73e33daf88ed1f0252e32de97a7b
    CR-Id: ALPS03660698
    Feature: [Android Default] AE (Auto Exposure)
    Signed-off-by: KuanFu Yeh <kuanfu.yeh@mediatek.com>

--- drivers/misc/mediatek/ccu/src/2.0/ccu_drv.c
+++ drivers/misc/mediatek/ccu/src/2.0/ccu_drv.c
@@ -587,7 +587,7 @@ static long ccu_ioctl(struct file *flip, unsigned int cmd, unsigned long arg)
 
 	LOG_DBG("ccu_ioctl+, cmd:%d\n", cmd);
 
-	if ((cmd != CCU_IOCTL_SET_POWER) && (cmd != CCU_IOCTL_FLUSH_LOG)) {
+	if ((cmd != CCU_IOCTL_SET_POWER) && (cmd != CCU_IOCTL_FLUSH_LOG) && (cmd != CCU_IOCTL_WAIT_IRQ)) {
 		powert_stat = ccu_query_power_status();
 		if (powert_stat == 0) {
 			LOG_WARN("ccuk: ioctl without powered on\n");
