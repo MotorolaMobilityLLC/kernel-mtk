8638ed4cb0c86d5c8b1b20a530f11099d162f98f
mtk12444 <sagy.shih@mediatek.com>
2018-06-03 14:24:00 +0800

    [ALPS03893654] DRAMC: add LP3 1333
    [Detail]
    1. Add LP3 1333 data rate
    2. Select 1333/1200 by efuse
    
    MTK-Commit-Id: 71a7ac72beded90e7b12305fc4469dbe03acdae8
    
    Change-Id: I8bf1d8cf998baf301a96ca47565ebcfe7d5f6a83
    Signed-off-by: mtk12444 <sagy.shih@mediatek.com>
    CR-Id: ALPS03893654
    Feature: DRAM
    (cherry picked from commit 89cde39c52288990bc4a1dc46906319c0d112bb7)

--- drivers/misc/mediatek/dramc/mt6761/mtk_dramc.c
+++ drivers/misc/mediatek/dramc/mt6761/mtk_dramc.c
@@ -43,6 +43,9 @@
 
 #include <mt-plat/aee.h>
 #include <mt-plat/mtk_chip.h>
+#include <mt-plat/mtk_devinfo.h>
+
+static int enable_lp3_1200;
 
 void __iomem *SYS_TIMER_BASE_ADDR;
 void __iomem *DRAMC_AO_CHA_BASE_ADDR;
@@ -1201,6 +1204,8 @@ unsigned int get_dram_data_rate(void)
 			u4DataRate = 1600;
 		else if (u4DataRate == 1534)
 			u4DataRate = 1534;
+		else if (u4DataRate == 1326)
+			u4DataRate = 1333;
 		else if (u4DataRate == 1196)
 			u4DataRate = 1200;
 		else
@@ -1287,7 +1292,7 @@ int dram_steps_freq(unsigned int step)
 		break;
 	case 2:
 		if (DRAM_TYPE == TYPE_LPDDR3)
-			freq = 1200;
+			freq = (enable_lp3_1200) ? 1200 : 1333;
 		else if ((DRAM_TYPE == TYPE_LPDDR4) ||
 				(DRAM_TYPE == TYPE_LPDDR4X))
 			freq = 1534;
@@ -1765,6 +1770,8 @@ static int dram_probe(struct platform_device *pdev)
 		dramc_info("fail to create read_dram_data_rate sysfs files\n");
 		return ret;
 	}
+
+	enable_lp3_1200 = get_devinfo_with_index(133) & 0x1;
 
 	if (dram_can_support_fh())
 		dramc_info("dram can support DFS\n");
