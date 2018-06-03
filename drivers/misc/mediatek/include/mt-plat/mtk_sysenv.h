84ed74ebf3077fcfa2e0749857b18773f7762be5
Linger Lee <linger.lee@mediatek.com>
2018-06-03 22:29:47 +0800

    [ALPS03547981] sysenv: turn on sysenv for tlc project
    [Detail]
    Turn on sysenv and fix build error
    
    MTK-Commit-Id: a53e4306e902aeca307937ba04679db27c0d4c79
    
    Change-Id: I43f7e19456007b95cbde2d37d01d1b7b74e8521c
    CR-Id: ALPS03547981
    Feature: MTK NAND Translation Layer (MNTL)
    Signed-off-by: Linger Lee <linger.lee@mediatek.com>

--- drivers/misc/mediatek/include/mt-plat/mtk_sysenv.h
+++ drivers/misc/mediatek/include/mt-plat/mtk_sysenv.h
@@ -13,7 +13,6 @@
 
 #ifndef __ENV_H__
 #define __ENV_H__
-#if defined(CONFIG_MTK_EMMC_SUPPORT) || defined(CONFIG_MTK_UFS_BOOTING) || defined(CONFIG_MMC_MTK)
 #include <linux/ioctl.h>
 
 #define CFG_ENV_SIZE		0x4000     /*16KB*/
@@ -42,54 +41,4 @@ struct env_ioctl {
 extern int set_env(char *name, char *value);
 extern char *get_env(const char *name);
 
-#else
-#include <linux/errno.h>	/* error codes */
-#include <linux/types.h>	/* size_t */
-#include <linux/proc_fs.h>  /*proc*/
-#include <linux/ioctl.h> /*ioctl*/
-#include <linux/module.h>
-
-#define CFG_ENV_SIZE 0x4000 /* (16KB) */
-#define CFG_ENV_OFFSET 0x20000  /* (128KB) */
-
-#define CFG_ENV_DATA_SIZE (CFG_ENV_SIZE-sizeof(g_env.checksum)-sizeof(g_env.sig)-sizeof(g_env.sig_1))
-#define CFG_ENV_DATA_OFFSET (sizeof(g_env.sig))
-#define CFG_ENV_SIG_1_OFFSET (CFG_ENV_SIZE - sizeof(g_env.checksum)-sizeof(g_env.sig_1))
-#define CFG_ENV_CHECKSUM_OFFSET (CFG_ENV_SIZE - sizeof(g_env.checksum))
-
-#define ENV_PART PART_MISC
-
-#define ENV_SIG "ENV_v1"
-
-#define DATA_FREE_SIZE_TH_DEFAULT (50*1024*1024)
-
-#ifdef CONFIG_MTK_SHARED_SDCARD
-
-#define LIMIT_SDCARD_SIZE
-
-extern long long data_free_size_th;
-
-#endif
-
-struct env_struct {
-	char sig[8]; /* "ENV_v1" */
-	char *env_data;
-	char sig_1[8];  /* "ENV_v1" */
-	int checksum; /* checksum for env_data */
-} env_t;
-
-#define ENV_MAGIC	 'e'
-#define ENV_READ		_IOW(ENV_MAGIC, 1, int)
-#define ENV_WRITE		_IOW(ENV_MAGIC, 2, int)
-
-struct env_ioctl {
-	char *name;
-	int name_len;
-	char *value;
-	int value_len;
-};
-
-extern int set_env(char *name, char *value);
-extern char *get_env(char *name);
-#endif
 #endif
