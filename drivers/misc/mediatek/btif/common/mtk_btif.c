6bbeaaa1c59249dfab434bbb23440f099466a0c7
Robbin Chiu <robbin.chiu@mediatek.com>
2018-06-03 23:05:53 +0800

    [ALPS03612606] WMT: Fix btif_file_open
    [Solution]
    Enable the function for btif file open
    
    MTK-Commit-Id: e26453f493ad9edb9d5a345d9b568a715369fe0d
    
    Change-Id: I9aca834c103b283938d4cbbae334622ea3bad4e9
    CR-Id: ALPS03612606
    Feature: Vulnerability Scan
    Signed-off-by: Robbin Chiu <robbin.chiu@mediatek.com>

--- drivers/misc/mediatek/btif/common/mtk_btif.c
+++ drivers/misc/mediatek/btif/common/mtk_btif.c
@@ -89,8 +89,6 @@ static int _btif_dma_write(p_mtk_btif p_btif,
 
 static unsigned int btif_bbs_wr_direct(p_btif_buf_str p_bbs,
 				unsigned char *p_buf, unsigned int buf_len);
-static unsigned int btif_bbs_read(p_btif_buf_str p_bbs,
-			   unsigned char *p_buf, unsigned int buf_len);
 static unsigned int btif_bbs_write(p_btif_buf_str p_bbs,
 			    unsigned char *p_buf, unsigned int buf_len);
 static void btif_dump_bbs_str(unsigned char *p_str, p_btif_buf_str p_bbs);
@@ -617,54 +615,13 @@ static int btif_file_release(struct inode *pinode, struct file *pfile)
 static ssize_t btif_file_read(struct file *pfile,
 			      char __user *buf, size_t count, loff_t *f_ops)
 {
-	int i_ret = 0;
-	int rd_len = 0;
-
-	BTIF_INFO_FUNC("++\n");
-	down(&rd_mtx);
-	rd_len = btif_bbs_read(&(g_btif[0].btif_buf), rd_buf, sizeof(rd_buf));
-	while (rd_len == 0) {
-		if (pfile->f_flags & O_NONBLOCK)
-			break;
-
-		wait_event(btif_wq, rx_notify_flag != 0);
-		rx_notify_flag = 0;
-		rd_len =
-		    btif_bbs_read(&(g_btif[0].btif_buf), rd_buf,
-				  sizeof(rd_buf));
-	}
-
-	if (rd_len == 0)
-		i_ret = 0;
-	else if ((rd_len > 0) && (copy_to_user(buf, rd_buf, rd_len) == 0))
-		i_ret = rd_len;
-	else
-		i_ret = -EFAULT;
-
-	up(&rd_mtx);
-	BTIF_INFO_FUNC("--, i_ret:%d\n", i_ret);
-	return i_ret;
+	return -EFAULT;
 }
 
 ssize_t btif_file_write(struct file *filp,
 			const char __user *buf, size_t count, loff_t *f_pos)
 {
-	int i_ret = 0;
-	int copy_size = 0;
-
-	copy_size = count > sizeof(wr_buf) ? sizeof(wr_buf) : count;
-
-	BTIF_INFO_FUNC("++\n");
-	down(&wr_mtx);
-	if (copy_from_user(&wr_buf[0], &buf[0], copy_size))
-		i_ret = -EFAULT;
-	else
-		i_ret = btif_send_data(&g_btif[0], wr_buf, copy_size);
-
-	up(&wr_mtx);
-	BTIF_INFO_FUNC("--, i_ret:%d\n", i_ret);
-
-	return i_ret;
+	return -EFAULT;
 }
 #ifdef CONFIG_COMPAT
 long btif_compat_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
@@ -2633,64 +2590,6 @@ unsigned int btif_bbs_write(p_btif_buf_str p_bbs,
 	}
 
 	return wr_len;
-}
-
-unsigned int btif_bbs_read(p_btif_buf_str p_bbs,
-			   unsigned char *p_buf, unsigned int buf_len)
-{
-	unsigned int rd_len = 0;
-	unsigned int ava_len = 0;
-	unsigned int wr_idx = p_bbs->wr_idx;
-
-	ava_len = BBS_COUNT_CUR(p_bbs, wr_idx);
-	if (ava_len >= 4096) {
-		BTIF_WARN_FUNC("ava_len too long, size(%d)\n", ava_len);
-		btif_dump_bbs_str("Rx buffer tooo long", p_bbs);
-	}
-	if (ava_len != 0) {
-		if (buf_len >= ava_len) {
-			rd_len = ava_len;
-			if (wr_idx >= (p_bbs)->rd_idx) {
-				memcpy(p_buf, BBS_PTR(p_bbs,
-						      p_bbs->rd_idx),
-				       ava_len);
-				(p_bbs)->rd_idx = wr_idx;
-			} else {
-				unsigned int tail_len = BBS_SIZE(p_bbs) -
-				    (p_bbs)->rd_idx;
-				memcpy(p_buf, BBS_PTR(p_bbs,
-						      p_bbs->rd_idx),
-				       tail_len);
-				memcpy(p_buf + tail_len, BBS_PTR(p_bbs,
-								 0), ava_len - tail_len);
-				(p_bbs)->rd_idx = wr_idx;
-			}
-		} else {
-			rd_len = buf_len;
-			if (wr_idx >= (p_bbs)->rd_idx) {
-				memcpy(p_buf, BBS_PTR(p_bbs,
-						      p_bbs->rd_idx),
-				       rd_len);
-				(p_bbs)->rd_idx = (p_bbs)->rd_idx + rd_len;
-			} else {
-				unsigned int tail_len = BBS_SIZE(p_bbs) -
-				    (p_bbs)->rd_idx;
-				if (tail_len >= rd_len) {
-					memcpy(p_buf, BBS_PTR(p_bbs, p_bbs->rd_idx),
-					       rd_len);
-					(p_bbs)->rd_idx =
-					    ((p_bbs)->rd_idx + rd_len) & (BBS_MASK(p_bbs));
-				} else {
-					memcpy(p_buf, BBS_PTR(p_bbs, p_bbs->rd_idx), tail_len);
-					memcpy(p_buf + tail_len,
-					       (p_bbs)->p_buf, rd_len - tail_len);
-					(p_bbs)->rd_idx = rd_len - tail_len;
-				}
-			}
-		}
-	}
-	mb();
-	return rd_len;
 }
 
 unsigned int btif_bbs_wr_direct(p_btif_buf_str p_bbs,
