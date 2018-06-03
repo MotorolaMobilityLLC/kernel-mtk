c7e024836f0da51a2d3d121abf37753fb5314357
LucasWei <lucas-sr.wei@mediatek.com>
2018-06-03 14:24:07 +0800

    [ALPS03578208] arm/cpufreq: Fix build fail for duplicated function
    [Detail]
    function `cpufreq_scale_freq_capacity` duplicated at:
    kernel/sched/cpufreq_schedplus.c:239:
    	unsigned long cpufreq_scale_freq_capacity
    arch/arm/kernel/topology_dts.c:326
    	unsigned long arch_scale_freq_capacity
    
    due to define arch_scale_freq_capacity to
    cpufreq_scale_freq_capacity.
    
    [Solutions]
    Extern both function to original function and define
    arch_scale_freq_capacity as its own name.
    
    MTK-Commit-Id: b8282b5f41e12cea26501756e7d47d9e9ed44f4b
    
    Change-Id: If23fa507b709c1d78077313638333cc5f48c0692
    Signed-off-by: LucasWei <lucas-sr.wei@mediatek.com>
    CR-Id: ALPS03578208
    Feature: Energy Aware Scheduling Plus (EAS+)

--- arch/arm/include/asm/topology.h
+++ arch/arm/include/asm/topology.h
@@ -31,9 +31,17 @@ void store_cpu_topology(unsigned int cpuid);
 const struct cpumask *cpu_coregroup_mask(int cpu);
 
 #ifdef CONFIG_CPU_FREQ
-#define arch_scale_freq_capacity cpufreq_scale_freq_capacity
+#define arch_scale_freq_capacity arch_scale_freq_capacity
+
+extern unsigned long arch_scale_freq_capacity(struct sched_domain *sd, int cpu);
+extern unsigned long
+cpufreq_scale_freq_capacity(struct sched_domain *sd, int cpu);
 #define arch_scale_max_freq_capacity cpufreq_scale_max_freq_capacity
+extern unsigned long
+cpufreq_scale_max_freq_capacity(struct sched_domain *sd, int cpu);
 #define arch_scale_min_freq_capacity cpufreq_scale_min_freq_capacity
+extern unsigned long
+cpufreq_scale_min_freq_capacity(struct sched_domain *sd, int cpu);
 #endif
 
 #define arch_scale_cpu_capacity scale_cpu_capacity
