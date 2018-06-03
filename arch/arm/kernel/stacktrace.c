2aaf489fab4e73cafb21bf5b45a307cf24626c38
Xinghua Yang <xinghua.yang@mediatek.com>
2018-06-03 09:59:18 +0800

    [ALPS03455820] kernel: force armeabi-v7a unwind other task
    [Detail]
    for armeabi-v7a, unwind other tasks backtrace doesnt work at all
    hang detect issue need backtrace to analysis and debug
    
    [Solution]
    force armeabi-v7a unwind other task
    
    MTK-Commit-Id: e79a58503ac499e2be00823d6a4f77e2daa5295d
    
    Change-Id: I028d0037ec5335919f3de86b960b324bbfd18564
    Signed-off-by: Xinghua Yang <xinghua.yang@mediatek.com>
    CR-Id: ALPS03455820
    Feature: GMS Package
    (cherry picked from commit d21e2c9ddf12827395ba8d900c1b728816997ac7)

--- arch/arm/kernel/stacktrace.c
+++ arch/arm/kernel/stacktrace.c
@@ -40,6 +40,9 @@ int notrace unwind_frame(struct stackframe *frame)
 	frame->sp = *(unsigned long *)(fp - 8);
 	frame->pc = *(unsigned long *)(fp - 4);
 
+	if (ALIGN(frame->fp, THREAD_SIZE) != ALIGN(fp, THREAD_SIZE))
+		return -EINVAL;
+
 	return 0;
 }
 #endif
@@ -118,6 +121,7 @@ static noinline void __save_stack_trace(struct task_struct *tsk,
 	data.no_sched_functions = nosched;
 
 	if (tsk != current) {
+#if 0
 #ifdef CONFIG_SMP
 		/*
 		 * What guarantees do we have here that 'tsk' is not
@@ -127,6 +131,7 @@ static noinline void __save_stack_trace(struct task_struct *tsk,
 		if (trace->nr_entries < trace->max_entries)
 			trace->entries[trace->nr_entries++] = ULONG_MAX;
 		return;
+#endif
 #else
 		frame.fp = thread_saved_fp(tsk);
 		frame.sp = thread_saved_sp(tsk);
