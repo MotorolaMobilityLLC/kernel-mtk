5b0632d2573b61037a79487a03e5d5cef65f4dd5
Hector Yuan <hector.yuan@mediatek.com>
2018-06-03 11:49:06 +0800

    [ALPS02806286] DVFS: Enable schedule idx
    [Detail] Enable schedule idx
    
    MTK-Commit-Id: 33db31072912f2e7acea886db62034648ef876e3
    
    Change-Id: I599f0bce8c6e797931b65fb552ba2a936d39345a
    Signed-off-by: Hector Yuan <hector.yuan@mediatek.com>
    CR-Id: ALPS02806286
    Feature: DVFS (Dynamic Voltage and Frequency Scaling)

--- drivers/misc/mediatek/base/power/cpufreq_v1/src/mtk_cpufreq_api.c
+++ drivers/misc/mediatek/base/power/cpufreq_v1/src/mtk_cpufreq_api.c
@@ -11,6 +11,7 @@
 * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
 */
 #include "mtk_cpufreq_internal.h"
+#include "mtk_cpufreq_hybrid.h"
 
 #ifdef CONFIG_HYBRID_CPU_DVFS
 enum mt_cpu_dvfs_id group(unsigned int cpu)
@@ -28,10 +29,9 @@ int mt_cpufreq_set_by_schedule_load(unsigned int cpu, enum cpu_dvfs_sched_type s
 {
 #ifdef CONFIG_HYBRID_CPU_DVFS
 	enum mt_cpu_dvfs_id id = group(cpu);
-	struct mt_cpu_dvfs *p = id_to_cpu_dvfs(id);
 
-	if (freq > mt_cpufreq_get_freq_by_idx(p, 15) &&
-		freq < mt_cpufreq_get_freq_by_idx(p, 0) && state < NUM_SCHE_TYPE)
+	if (freq > mt_cpufreq_get_freq_by_idx(id, 15) &&
+		freq < mt_cpufreq_get_freq_by_idx(id, 0) && state < NUM_SCHE_TYPE)
 		cpuhvfs_set_cpu_load_freq(cpu, state, freq);
 #endif
 
