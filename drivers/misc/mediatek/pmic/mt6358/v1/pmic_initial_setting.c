7979b0bd874e2857e4306a89ed150cc10fb33d87
Jeter Chen <jeter.chen@mediatek.com>
2018-06-03 23:36:32 +0800

    [ALPS03501192] PMIC: VIO28 no LP mode
    [Detail]
    disable VIO28 LP mode controlled by SRCLKEN0/2
    
    MTK-Commit-Id: 72ef190ce72c591d6d81c8f0a449e361fdcc01e8
    
    Change-Id: I58f73a186dd343f4b1419f272ef30a233cbfbbe5
    Signed-off-by: Jeter Chen <jeter.chen@mediatek.com>
    CR-Id: ALPS03501192
    Feature: MT6771 Platform Feature

--- drivers/misc/mediatek/pmic/mt6358/v1/pmic_initial_setting.c
+++ drivers/misc/mediatek/pmic/mt6358/v1/pmic_initial_setting.c
@@ -140,7 +140,7 @@ void PMIC_LP_INIT_SETTING(void)
 	pmic_ldo_va12_lp(SRCLKEN0, 1, HW_LP);
 	pmic_ldo_vaux18_lp(SRCLKEN0, 1, HW_LP);
 	pmic_ldo_vaud28_lp(SRCLKEN0, 1, HW_LP);
-	pmic_ldo_vio28_lp(SRCLKEN0, 1, HW_LP);
+	pmic_ldo_vio28_lp(SW, 1, SW_ON);
 	pmic_ldo_vio18_lp(SW, 1, SW_ON);
 	pmic_ldo_vfe28_lp(SRCLKEN1, 1, HW_OFF);
 	pmic_ldo_vdram2_lp(SRCLKEN0, 1, HW_LP);
@@ -182,7 +182,7 @@ void PMIC_LP_INIT_SETTING(void)
 	pmic_ldo_va12_lp(SRCLKEN2, 1, HW_LP);
 	pmic_ldo_vaux18_lp(SRCLKEN2, 1, HW_LP);
 	pmic_ldo_vaud28_lp(SW, 1, SW_ON);
-	pmic_ldo_vio28_lp(SRCLKEN2, 1, HW_LP);
+	pmic_ldo_vio28_lp(SW, 1, SW_ON);
 	pmic_ldo_vio18_lp(SW, 1, SW_ON);
 	pmic_ldo_vfe28_lp(SRCLKEN1, 1, HW_OFF);
 	pmic_ldo_vdram2_lp(SRCLKEN2, 1, HW_LP);
