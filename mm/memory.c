510b3a552f2d58f00dacebc10c2024c3f68ddd87
Vandana Salve <vsalve@nvidia.com>
2018-06-03 14:19:09 +0800

    [ALPS02563038] mm: use NON-MOVABLE pages when FOLL_DURABLE flag is set
    mm: get_user_pages: use NON-MOVABLE pages when FOLL_DURABLE flag is set
    
    Ensure that newly allocated pages, which are faulted in
    in FOLL_DURABLE mode comes from non-movalbe pageblocks,
    to workaround migration failures with CMA
    
    MTK-Commit-Id: 309277aad648f5a1d855597abcd2a7c8f1025dc7
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Change-Id: I76d2185cc7e77992db585a71efaa06a5c0105a76
    Signed-off-by: Vandana Salve <vsalve@nvidia.com>
    Reviewed-by: Riham Haidar <rhaidar@nvidia.com>
    Tested-by: Riham Haidar <rhaidar@nvidia.com>
    Signed-off-by: Yingjoe Chen <yingjoe.chen@mediatek.com>
    CR-Id: ALPS02563038
    Feature: CMA

--- mm/memory.c
+++ mm/memory.c
@@ -2115,7 +2115,7 @@ static inline int wp_page_reuse(struct mm_struct *mm,
  */
 static int wp_page_copy(struct mm_struct *mm, struct vm_area_struct *vma,
 			unsigned long address, pte_t *page_table, pmd_t *pmd,
-			pte_t orig_pte, struct page *old_page)
+			pte_t orig_pte, struct page *old_page, gfp_t gfp)
 {
 	struct page *new_page = NULL;
 	spinlock_t *ptl = NULL;
@@ -2129,11 +2129,11 @@ static int wp_page_copy(struct mm_struct *mm, struct vm_area_struct *vma,
 		goto oom;
 
 	if (is_zero_pfn(pte_pfn(orig_pte))) {
-		new_page = alloc_zeroed_user_highpage_movable(vma, address);
+		new_page = alloc_zeroed_user_highpage(gfp, vma, address);
 		if (!new_page)
 			goto oom;
 	} else {
-		new_page = alloc_page_vma(GFP_HIGHUSER_MOVABLE, vma, address);
+		new_page = alloc_page_vma(gfp, vma, address);
 		if (!new_page)
 			goto oom;
 		cow_user_page(new_page, old_page, address, vma);
@@ -2340,10 +2340,14 @@ static int wp_page_shared(struct mm_struct *mm, struct vm_area_struct *vma,
  */
 static int do_wp_page(struct mm_struct *mm, struct vm_area_struct *vma,
 		unsigned long address, pte_t *page_table, pmd_t *pmd,
-		spinlock_t *ptl, pte_t orig_pte)
+		spinlock_t *ptl, pte_t orig_pte, unsigned int flags)
 	__releases(ptl)
 {
 	struct page *old_page;
+	gfp_t gfp = GFP_HIGHUSER_MOVABLE;
+
+	if (IS_ENABLED(CONFIG_CMA) && (flags & FAULT_FLAG_NO_CMA))
+		gfp &= ~(__GFP_MOVABLE | __GFP_CMA);
 
 	old_page = vm_normal_page(vma, address, orig_pte);
 	if (!old_page) {
@@ -2361,7 +2365,7 @@ static int do_wp_page(struct mm_struct *mm, struct vm_area_struct *vma,
 
 		pte_unmap_unlock(page_table, ptl);
 		return wp_page_copy(mm, vma, address, page_table, pmd,
-				    orig_pte, old_page);
+				    orig_pte, old_page, gfp);
 	}
 
 	/*
@@ -2408,7 +2412,7 @@ static int do_wp_page(struct mm_struct *mm, struct vm_area_struct *vma,
 
 	pte_unmap_unlock(page_table, ptl);
 	return wp_page_copy(mm, vma, address, page_table, pmd,
-			    orig_pte, old_page);
+			    orig_pte, old_page, gfp);
 }
 
 static void unmap_mapping_range_vma(struct vm_area_struct *vma,
@@ -2651,7 +2655,7 @@ static int do_swap_page(struct mm_struct *mm, struct vm_area_struct *vma,
 	}
 
 	if (flags & FAULT_FLAG_WRITE) {
-		ret |= do_wp_page(mm, vma, address, page_table, pmd, ptl, pte);
+		ret |= do_wp_page(mm, vma, address, page_table, pmd, ptl, pte, flags);
 		if (ret & VM_FAULT_ERROR)
 			ret &= VM_FAULT_ERROR;
 		goto out;
@@ -3041,11 +3045,15 @@ static int do_cow_fault(struct mm_struct *mm, struct vm_area_struct *vma,
 	spinlock_t *ptl;
 	pte_t *pte;
 	int ret;
+	gfp_t gfp = GFP_HIGHUSER_MOVABLE | __GFP_CMA;
 
 	if (unlikely(anon_vma_prepare(vma)))
 		return VM_FAULT_OOM;
 
-	new_page = alloc_page_vma(GFP_HIGHUSER_MOVABLE | __GFP_CMA, vma, address);
+	if (IS_ENABLED(CONFIG_CMA) && (flags & FAULT_FLAG_NO_CMA))
+		gfp &= ~(__GFP_MOVABLE | __GFP_CMA);
+
+	new_page = alloc_page_vma(gfp, vma, address);
 	if (!new_page)
 		return VM_FAULT_OOM;
 
@@ -3365,7 +3373,7 @@ static int handle_pte_fault(struct mm_struct *mm,
 	if (flags & FAULT_FLAG_WRITE) {
 		if (!pte_write(entry))
 			return do_wp_page(mm, vma, address,
-					pte, pmd, ptl, entry);
+					pte, pmd, ptl, entry, flags);
 		entry = pte_mkdirty(entry);
 	}
 	entry = pte_mkyoung(entry);
