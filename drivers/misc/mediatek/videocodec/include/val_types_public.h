0b4f5ac17187ca3f37f8ecd92c0cb5b3fbeb83f0
Gary Wu <gary.wu@mediatek.com>
2018-06-03 16:59:04 +0800

    [ALPS03251683] Videocodec: Add MT6739 chipname
    Add MT6739 chipname
    
    MTK-Commit-Id: e10dc5a769798238210c982180424396c09b9f70
    
    Change-Id: I3fcc6def0390cd4905607ebf5cb84545effedf84
    CR-Id: ALPS03251683
    Feature: Video Framework
    Signed-off-by: Gary Wu <gary.wu@mediatek.com>

--- drivers/misc/mediatek/videocodec/include/val_types_public.h
+++ drivers/misc/mediatek/videocodec/include/val_types_public.h
@@ -101,7 +101,8 @@ typedef enum _VAL_CHIP_NAME_T {
 	VAL_CHIP_NAME_MT8160,                       /* / <MT8160 */
 	VAL_CHIP_NAME_ELBRUS,                       /* /< ELBRUS */
 	VAL_CHIP_NAME_MT6758,                       /* / <KIBOPLUS */
-	VAL_CHIP_NAME_MT6763,
+	VAL_CHIP_NAME_MT6763,                       /* /< BIANCO */
+	VAL_CHIP_NAME_MT6739,                       /* /< ZION */
 	VAL_CHIP_NAME_MAX = 0xFFFFFFFF              /* /< Max Value */
 } VAL_CHIP_NAME_T;
 
