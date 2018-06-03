a208b37e447c40786c89196d03f4d8c5ac60b7d6
tai-hua.tseng <tai-hua.tseng@mediatek.com>
2018-06-03 23:20:20 +0800

    [ALPS03595015] DISP: enable partial update
    [Detail]
    1. move partial update option to dts
    2. enable k71v1_64_bsp_op01 partial update option
    
    MTK-Commit-Id: 4092fbdf4b7a7758834ae92ce8fb5483f9e9720e
    
    Change-Id: Iae23b9993ccd4a45581dfe0390c0420200edd401
    CR-Id:ALPS03595015
    Feature: MT6771 Platform Feature
    Signed-off-by: tai-hua.tseng <tai-hua.tseng@mediatek.com>

--- drivers/misc/mediatek/video/mt6771/videox/disp_helper.c
+++ drivers/misc/mediatek/video/mt6771/videox/disp_helper.c
@@ -363,9 +363,7 @@ void disp_helper_option_init(void)
 	disp_helper_set_option(DISP_OPT_HRT, 1);
 
 	/* display partial update */
-#ifdef CONFIG_MTK_CONSUMER_PARTIAL_UPDATE_SUPPORT
-	disp_helper_set_option(DISP_OPT_PARTIAL_UPDATE, 0);
-#endif
+	disp_helper_set_option(DISP_OPT_PARTIAL_UPDATE, 1);
 	disp_helper_set_option(DISP_OPT_CV_BYSUSPEND, 0);
 	disp_helper_set_option(DISP_OPT_DELAYED_TRIGGER, 1);
 	disp_helper_set_option(DISP_OPT_SHADOW_REGISTER, 0);
