9dc06e0ae79dd629e3628fb426cb77a37bc3bc84
Xiaobin Wei <xiaobin.wei@mediatek.com>
2018-06-03 23:46:16 +0800

    [ALPS03496943] [mt6763]DSI: Add 0.5 MHz support for MIPI PLL
    [Detail] Because 495MHz MIPI PLL has de-sense with GPS/BlueTooth
    module, has to change MIPI clock to 514.5MHz, so need 0.5MHz
    support for MIPI PLL configure.
    
    [Solution] add data_rate = PLL_CLOCK*2 and use it instead of
    PLL_CLOCK*2; If the request pll is 514.5, then:
    1. PLL_CLOCK = 514
    2. data_rate = 1029
    
    MTK-Commit-Id: a94c4df2374d7f672ca19cfdc773387b27a96d9a
    
    Change-Id: I37a77bdf0388feb4f00f093c04634800d30dcf56
    Signed-off-by: Xiaobin Wei <xiaobin.wei@mediatek.com>
    CR-Id: ALPS03496943
    Feature: Others
    (cherry picked from commit c3e2c03b0832c79f26bcc51a36089cb280ae22c8)

--- drivers/misc/mediatek/video/mt6763/dispsys/ddp_dsi.c
+++ drivers/misc/mediatek/video/mt6763/dispsys/ddp_dsi.c
@@ -549,7 +549,9 @@ void DSI_exit_ULPS(enum DISP_MODULE_ENUM module)
 	int ret = 0;
 	unsigned int lane_num_bitvalue = 0;
 	/* wake_up_prd *1024*cycle time > 1ms */
-	int wake_up_prd = (_dsi_context[i].dsi_params.PLL_CLOCK * 2 * 1000) / (1024 * 8) + 0x1;
+	unsigned int data_rate = _dsi_context[i].dsi_params.data_rate != 0 ? _dsi_context[i].dsi_params.data_rate :
+							_dsi_context[i].dsi_params.PLL_CLOCK * 2;
+	int wake_up_prd = (data_rate * 1000) / (1024 * 8) + 0x1;
 	struct t_condition_wq *waitq;
 
 	for (i = DSI_MODULE_BEGIN(module); i <= DSI_MODULE_END(module); i++) {
@@ -1180,7 +1182,7 @@ void DSI_PHY_clk_change(enum DISP_MODULE_ENUM module, struct cmdqRecStruct *cmdq
 #if 0
 	int i = 0;
 	unsigned int j = 0;
-	unsigned int data_Rate = dsi_params->PLL_CLOCK*2;
+	unsigned int data_Rate = dsi_params->data_rate != 0 ? dsi_params->data_rate : dsi_params->PLL_CLOCK * 2;
 
 	unsigned int pcw_ratio = 0;
 	unsigned int S2Qdiv    = 0;
@@ -1319,7 +1321,7 @@ static void _DSI_PHY_clk_setting(enum DISP_MODULE_ENUM module, struct cmdqRecStr
 {
 	int i = 0;
 	unsigned int j = 0;
-	unsigned int data_Rate = dsi_params->PLL_CLOCK * 2;
+	unsigned int data_Rate = dsi_params->data_rate != 0 ? dsi_params->data_rate : dsi_params->PLL_CLOCK * 2;
 	unsigned int pcw_ratio = 0;
 	unsigned int pcw = 0;
 	unsigned int posdiv    = 0;
@@ -1649,7 +1651,13 @@ void DSI_PHY_TIMCONFIG(enum DISP_MODULE_ENUM module, struct cmdqRecStruct *cmdq,
 	return;
 #endif
 	lane_no = dsi_params->LANE_NUM;
-	if (dsi_params->PLL_CLOCK != 0) {
+	if (dsi_params->data_rate != 0) {
+		ui = 1000 / dsi_params->data_rate + 0x01;
+		cycle_time = 8000 / dsi_params->data_rate + 0x01;
+		DISP_LOG_PRINT(ANDROID_LOG_INFO, "DSI",
+			"[DISP] - kernel - DSI_PHY_TIMCONFIG, Cycle Time = %d(ns), Unit Interval = %d(ns). , lane# = %d\n",
+			cycle_time, ui, lane_no);
+	} else if (dsi_params->PLL_CLOCK != 0) {
 		ui = 1000 / (dsi_params->PLL_CLOCK * 2) + 0x01;
 		cycle_time = 8000 / (dsi_params->PLL_CLOCK * 2) + 0x01;
 		DISP_LOG_PRINT(ANDROID_LOG_INFO, "DSI",
