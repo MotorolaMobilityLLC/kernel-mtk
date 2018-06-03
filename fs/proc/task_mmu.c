863be809fcdb4049d6799dadfdf8a1807b27a6c6
Yun Hsiang <yun.hsiang@mediatek.com>
2018-06-03 13:40:34 +0800

    [ALPS02996621] fs: print vma name in show_map_vma
    [Detail]
    Print vma name in show_map_vma
    
    MTK-Commit-Id: 6a7408ca01d3cc237a1871fab067b044b83ac0f4
    
    Change-Id: I18cac69b362bdb0ad78d4436d3828898661a307c
    Signed-off-by: Yun Hsiang <yun.hsiang@mediatek.com>
    CR-Id: ALPS02996621
    Feature: Others

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -393,6 +393,11 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 
 		if (is_stack(priv, vma, is_pid))
 			name = "[stack]";
+
+		if (vma_get_anon_name(vma)) {
+			seq_pad(m, ' ');
+			seq_print_vma_name(m, vma);
+		}
 	}
 
 done:
