a9334316f0ea5d9873dd6222270f0bbbeb3ec314
mtk12211 <shang.zhou@mediatek.com>
2018-06-03 10:19:30 +0800

    [ALPS03571241] DISP: Porting ili9881c LCM driver
    [Detail]
    Porting ili9881c LCM driver.
    
    MTK-Commit-Id: b762843df02e810902bf93a1ab44eab7d0e3a41f
    
    Change-Id: I17643dc176b26140dc61860713d41cbd0aa96680
    Signed-off-by: mtk12211 <shang.zhou@mediatek.com>
    CR-Id: ALPS03571241
    Feature: Display Driver (DSI Interface)

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
@@ -277,6 +277,7 @@ extern struct LCM_DRIVER nt35695B_fhd_dsi_cmd_auo_nt50358_lcm_drv;
 extern struct LCM_DRIVER nt35695B_fhd_dsi_vdo_auo_nt50358_lcm_drv;
 extern struct LCM_DRIVER nt35695B_fhd_dsi_cmd_auo_nt50358_720p_lcm_drv;
 extern struct LCM_DRIVER nt35695B_fhd_dsi_cmd_auo_nt50358_qhd_lcm_drv;
+extern struct LCM_DRIVER ili9881c_hdp_dsi_vdo_ilitek_rt5081_lcm_drv;
 extern struct LCM_DRIVER nt51021_wuxga_dsi_vdo_mt8173_lcm_drv;
 
 #ifdef BUILD_LK
