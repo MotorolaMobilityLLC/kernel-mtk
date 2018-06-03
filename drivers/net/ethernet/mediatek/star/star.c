bdb7d3836b72b9d9ecb4b1b1d8e937acfadf8011
Zhiyong Tao <zhiyong.tao@mediatek.com>
2018-06-03 19:28:54 +0800

    [ALPS03409722] ethernet: correct the License announcement
    [Detail]
    add correct the License announcement.
    
    MTK-Commit-Id: d6ea0a0b73357b8c84d1a53b4273a522d3286c57
    
    Change-Id: Ic4928e5e7dcdaf4c80c916786d6613f6edec1255
    Signed-off-by: Zhiyong Tao <zhiyong.tao@mediatek.com>
    CR-Id: ALPS03409722
    Feature: ethernet

--- drivers/net/ethernet/mediatek/star/star.c
+++ drivers/net/ethernet/mediatek/star/star.c
@@ -1,10 +1,15 @@
 /* Mediatek STAR MAC network driver.
  *
- * Copyright (c) 2016-2017 Mediatek Corporation
+ * Copyright (c) 2016-2017 MediaTek Inc.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation.
+ * program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
 
 #include <linux/clk.h>
