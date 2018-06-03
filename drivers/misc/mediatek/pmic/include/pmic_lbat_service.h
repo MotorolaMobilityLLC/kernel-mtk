065c72949ebba14bbd8c06add77888e205f1274c
Jeter Chen <jeter.chen@mediatek.com>
2018-06-03 12:40:16 +0800

    [ALPS03790062] PMIC: add LBAT service SW de-bounce support
    [Detail]
    use timer_list to support SW de-bounce on LBAT service
    
    MTK-Commit-Id: 65f1042203a14628acedd4af62ac6f4ed9610b2e
    
    Change-Id: If1ea9a045f22171902fff5fe00ceeba57481d79f
    Signed-off-by: Jeter Chen <jeter.chen@mediatek.com>
    CR-Id: ALPS03790062
    Feature: Power Management

--- drivers/misc/mediatek/pmic/include/pmic_lbat_service.h
+++ drivers/misc/mediatek/pmic/include/pmic_lbat_service.h
@@ -14,27 +14,38 @@
 #ifndef __PMIC_LBAT_SERVICE_H__
 #define __PMIC_LBAT_SERVICE_H__
 
-struct lbat_dbg_st {
-	unsigned int dbg_id;
-};
+#include <linux/timer.h>
+
+struct lbat_thd_t;
 
 struct lbat_user {
 	char name[30];
-	unsigned int hv_thd;
-	unsigned int lv1_thd;
-	unsigned int lv2_thd;
+	struct lbat_thd_t *hv_thd;
+	struct lbat_thd_t *lv1_thd;
+	struct lbat_thd_t *lv2_thd;
 	void (*callback)(unsigned int);
+	unsigned int deb_cnt;
+	struct lbat_thd_t *deb_thd_ptr;
+	unsigned int hv_deb_prd;
+	unsigned int hv_deb_times;
+	unsigned int lv_deb_prd;
+	unsigned int lv_deb_times;
+	struct timer_list deb_timer;
+	struct work_struct deb_work;
 };
 
 /* extern function */
 extern int lbat_service_init(void);
 extern int lbat_user_register(struct lbat_user *user, const char *name,
-	unsigned int hv_thd, unsigned int lv1_thd,
-	unsigned int lv2_thd, void (*callback)(unsigned int));
-extern void lbat_min_en_setting(int en_val);
-extern void lbat_max_en_setting(int en_val);
+	unsigned int hv_thd_volt, unsigned int lv1_thd_volt,
+	unsigned int lv2_thd_volt, void (*callback)(unsigned int));
+extern int lbat_user_set_debounce(struct lbat_user *user,
+	unsigned int hv_deb_prd, unsigned int hv_deb_cnt,
+	unsigned int lv_deb_prd, unsigned int lv_deb_cnt);
 extern void lbat_suspend(void);
 extern void lbat_resume(void);
+extern unsigned int lbat_read_raw(void);
+extern unsigned int lbat_read_volt(void);
 extern void lbat_dump_reg(void);
 extern int lbat_debug_init(struct dentry *debug_dir);
 
