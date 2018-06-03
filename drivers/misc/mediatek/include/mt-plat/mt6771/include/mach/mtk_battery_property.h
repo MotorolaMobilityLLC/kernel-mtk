2a252589f1a35c65d218e65551db9f51c3377aad
Timo Liao <timo.liao@mediatek.com>
2018-06-03 22:38:30 +0800

    [ALPS03521311] battery: change lsb setting
    [Detail]
    1. change mt6358 lsb setting
    2. disable_fg for develop
    
    MTK-Commit-Id: 8985892673b105212c37a3f7bb4c57f1ca7ffc3c
    
    Change-Id: I056dab3c92b14a96653e20df0ecd4d467f436f15
    Signed-off-by: Timo Liao <timo.liao@mediatek.com>
    CR-Id: ALPS03521311
    Feature: Fuel Gauge

--- drivers/misc/mediatek/include/mt-plat/mt6771/include/mach/mtk_battery_property.h
+++ drivers/misc/mediatek/include/mt-plat/mt6771/include/mach/mtk_battery_property.h
@@ -149,15 +149,15 @@
 #define UI_FAST_TRACKING_GAP 300
 #define KEEP_100_PERCENT_MINSOC 9000
 
-#define UNIT_FGCURRENT     (314331)		/* mt6356 314.331 uA */
-#define UNIT_FGCAR         (89409)		/* charge_lsb 157166 * 2^11 / 3600 */
-#define R_VAL_TEMP_2         (2)			/* MT6335 use 3, old chip use 4 */
-#define R_VAL_TEMP_3         (3)			/* MT6335 use 3, old chip use 4 */
+#define UNIT_FGCURRENT     (381470)		/* mt6355 381.470 uA */
+#define UNIT_FGCAR         (108507)		/* CHARGE_LSB 190735 * 2^11 / 3600 */
+#define R_VAL_TEMP_2         (2)		/* MT6335 use 3, old chip use 4 */
+#define R_VAL_TEMP_3         (3)		/* MT6335 use 3, old chip use 4 */
 #define UNIT_TIME          (50)
-#define UNIT_FGCAR_ZCV     (157166)     /* unit 2^0 LSB */
-#define UNIT_FG_IAVG		(157166)
-#define CAR_TO_REG_FACTOR  (0x5979)		/* 3600 * 1000 * 1000 / 157166 */
-#define CAR_TO_REG_SHIFT (3)	/*coulomb interrupt lsb might be different with coulomb lsb */
+#define UNIT_FGCAR_ZCV     (190735)		/* CHARGE_LSB = 190.735 uAs ,unit 2^0 LSB */
+#define UNIT_FG_IAVG		(190735)	/* IAVG LSB: 190.735 uA */
+#define CAR_TO_REG_FACTOR  (0x49BA)		/* 3600 * 1000 * 1000 / CHARGE_LSB */
+#define CAR_TO_REG_SHIFT (5)	/*coulomb interrupt lsb might be different with coulomb lsb */
 
 #define SHUTDOWN_CONDITION_LOW_BAT_VOLT
 #define BATTERY_TMP_TO_DISABLE_GM30 -50
