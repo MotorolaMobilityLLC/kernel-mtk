7525cbaa6ae57c6df1db22a16cb6ae84f15273d1
Jonathan Wu <jonathan.wu@mediatek.com>
2018-06-03 19:46:18 +0800

    [ALPS03402542] wifi:WMM-AC/VE patch sync and turn on
    [Detail]
    1.Turn on WMM-AC/VE
    2.Patch sync from GEN3
     2.1 [ALPS03343374] wlan: Fix kernel panic
     2.2 [ALPS03354441] wlan: Error handling for abnormal RM request
     2.3 [ALPS03392598] wlan: Fix some VoE bug
    
    MTK-Commit-Id: 6d98833deacee7b113eced4de8e0130ae9732815
    
    Change-Id: I16ed20f078c7fedbf550bd6a7101ea48abecd95f
    Signed-off-by: Jonathan Wu <jonathan.wu@mediatek.com>
    CR-Id: ALPS03402542
    Feature: Voice Enterprise

--- drivers/misc/mediatek/connectivity/wlan/gen2/mgmt/rsn.c
+++ drivers/misc/mediatek/connectivity/wlan/gen2/mgmt/rsn.c
@@ -2630,7 +2630,8 @@ UINT_32 rsnCalculateFTIELen(P_ADAPTER_T prAdapter,
 	ENUM_PARAM_AUTH_MODE_T eAuthMode = prAdapter->rWifiVar.rConnSettings.eAuthMode;
 	struct FT_IES *prFtIEs = &prAdapter->prGlueInfo->rFtIeForTx;
 
-	if (!prFtIEs->prFTIE || (eAuthMode != AUTH_MODE_WPA2_FT && eAuthMode != AUTH_MODE_WPA2_FT_PSK))
+	if (!IS_BSS_INDEX_VALID(eNetTypeIndex) || !IS_BSS_AIS(GET_BSS_INFO_BY_INDEX(prAdapter, eNetTypeIndex)) ||
+		!prFtIEs->prFTIE || (eAuthMode != AUTH_MODE_WPA2_FT && eAuthMode != AUTH_MODE_WPA2_FT_PSK))
 		return 0;
 	return IE_SIZE(prFtIEs->prFTIE);
 }
@@ -2641,8 +2642,10 @@ VOID rsnGenerateFTIE(IN P_ADAPTER_T prAdapter, IN OUT P_MSDU_INFO_T prMsduInfo)
 	struct FT_IES *prFtIEs = &prAdapter->prGlueInfo->rFtIeForTx;
 	PUINT_8 pucBuffer = (PUINT_8)prMsduInfo->prPacket + prMsduInfo->u2FrameLength;
 	UINT_32 ucFtIeSize = 0;
+	UINT_8 ucBssIdx = prMsduInfo->ucNetworkType;
 
-	if (!prFtIEs->prFTIE || (eAuthMode != AUTH_MODE_WPA2_FT && eAuthMode != AUTH_MODE_WPA2_FT_PSK))
+	if (!IS_BSS_INDEX_VALID(ucBssIdx) || !IS_BSS_AIS(GET_BSS_INFO_BY_INDEX(prAdapter, ucBssIdx)) ||
+		!prFtIEs->prFTIE || (eAuthMode != AUTH_MODE_WPA2_FT && eAuthMode != AUTH_MODE_WPA2_FT_PSK))
 		return;
 	ucFtIeSize = IE_SIZE(prFtIEs->prFTIE);
 	prMsduInfo->u2FrameLength += ucFtIeSize;
