dd88ae57af30e55bc6be647332115df8ffe6cb0d
Morven-CF Yeh <morven-cf.yeh@mediatek.com>
2018-06-03 19:28:10 +0800

    [ALPS03368521] spm: disable aee_kernel_warning()
    [Detail] disable aee_kernel_warning() when pcm assert happened
    
    MTK-Commit-Id: 02cbab60b48e81e820c1556fca42173227cdf964
    
    Change-Id: I143b08acde0a7ac22d144f6bcb530fbde887df9a
    Signed-off-by: Morven-CF Yeh <morven-cf.yeh@mediatek.com>
    CR-Id: ALPS03368521
    Feature: SRLTE
    (cherry picked from commit c7df5144e98e4502e077851fe7c9f60d1b03f28d)

--- drivers/misc/mediatek/base/power/spm_v4/mtk_spm_internal.c
+++ drivers/misc/mediatek/base/power/spm_v4/mtk_spm_internal.c
@@ -204,15 +204,17 @@ unsigned int __spm_output_wake_reason(const struct wake_status *wakesta,
 
 	if (wakesta->assert_pc != 0) {
 		/* add size check for vcoredvfs */
-		spm_print(suspend, "PCM ASSERT AT 0x%x (%s), r13 = 0x%x, debug_flag = 0x%x 0x%x\n",
+		spm_crit2("PCM ASSERT AT 0x%x (%s), r13 = 0x%x, debug_flag = 0x%x 0x%x\n",
 			  wakesta->assert_pc, scenario, wakesta->r13,
 			  wakesta->debug_flag, wakesta->debug_flag1);
 
+#if 0
 		if (!(wakesta->debug_flag1 & SPM_DBG1_DRAM_SREF_ACK_TO))
 			aee_kernel_warning("SPM Warning",
 					"PCM ASSERT AT 0x%x (%s), r13 = 0x%x, debug_flag = 0x%x 0x%x\n",
 					wakesta->assert_pc, scenario, wakesta->r13,
 					wakesta->debug_flag, wakesta->debug_flag1);
+#endif
 
 		return WR_PCM_ASSERT;
 	}
