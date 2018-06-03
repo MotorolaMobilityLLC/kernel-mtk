b56a7b17d3da38577c5746c756244fa425d6380f
Yirong Wang <yirong.wang@mediatek.com>
2018-06-03 18:29:01 +0800

    [ALPS03326366] dramc: Fix double free
    Fix double free in dramc function
    
    MTK-Commit-Id: 0b66ca552b916c38e310e134cb8f5c7d55b86af1
    
    Change-Id: Iec9b21270b9ec83415e608a2cc4296a9417d400b
    CR-Id: ALPS03326366
    Signed-off-by: Yirong Wang <yirong.wang@mediatek.com>
    Feature: DRAMC
    (cherry picked from commit e588751db77b7b560bcf78da09d0f2510fc92905)

--- drivers/misc/mediatek/dramc/mt6763/mtk_dramc.c
+++ drivers/misc/mediatek/dramc/mt6763/mtk_dramc.c
@@ -730,7 +730,6 @@ int Binning_DRAM_complex_mem_test(void)
 
 	for (i = 0; i < size; i++) {
 		if (MEM32_BASE[i] != 0) {
-			vfree(ptr);
 			/* return -1; */
 			ret = -1;
 			goto fail;
@@ -741,7 +740,6 @@ int Binning_DRAM_complex_mem_test(void)
 	/* === Verify the tied bits (tied low) === */
 	for (i = 0; i < size; i++) {
 		if (MEM32_BASE[i] != 0xffffffff) {
-			vfree(ptr);
 			/* return -2; */
 			ret = -2;
 			goto fail;
@@ -756,7 +754,6 @@ int Binning_DRAM_complex_mem_test(void)
 	pattern8 = 0x00;
 	for (i = 0; i < len; i++) {
 		if (MEM8_BASE[i] != pattern8++) {
-			vfree(ptr);
 			/* return -3; */
 			ret = -3;
 			goto fail;
@@ -769,7 +766,6 @@ int Binning_DRAM_complex_mem_test(void)
 		if (MEM8_BASE[i] == pattern8)
 			MEM16_BASE[j] = pattern8;
 		if (MEM16_BASE[j] != pattern8) {
-			vfree(ptr);
 			/* return -4; */
 			ret = -4;
 			goto fail;
@@ -784,7 +780,6 @@ int Binning_DRAM_complex_mem_test(void)
 	pattern16 = 0x00;
 	for (i = 0; i < (len >> 1); i++) {
 		if (MEM16_BASE[i] != pattern16++) {
-			vfree(ptr);
 			/* return -5; */
 			ret = -5;
 			goto fail;
@@ -798,7 +793,6 @@ int Binning_DRAM_complex_mem_test(void)
 	pattern32 = 0x00;
 	for (i = 0; i < (len >> 2); i++) {
 		if (MEM32_BASE[i] != pattern32++) {
-			vfree(ptr);
 			/* return -6; */
 			ret = -6;
 			goto fail;
@@ -812,7 +806,6 @@ int Binning_DRAM_complex_mem_test(void)
 	/* === Read Check then Fill Memory with a5a5a5a5 Pattern === */
 	for (i = 0; i < size; i++) {
 		if (MEM32_BASE[i] != 0x44332211) {
-			vfree(ptr);
 			/* return -7; */
 			ret = -7;
 			goto fail;
@@ -825,7 +818,6 @@ int Binning_DRAM_complex_mem_test(void)
 	/* 00 Byte Pattern at offset 0h === */
 	for (i = 0; i < size; i++) {
 		if (MEM32_BASE[i] != 0xa5a5a5a5) {
-			vfree(ptr);
 			/* return -8; */
 			ret = -8;
 			goto fail;
@@ -838,7 +830,6 @@ int Binning_DRAM_complex_mem_test(void)
 	/* 00 Byte Pattern at offset 2h === */
 	for (i = 0; i < size; i++) {
 		if (MEM32_BASE[i] != 0xa5a5a500) {
-			vfree(ptr);
 			/* return -9; */
 			ret = -9;
 			goto fail;
@@ -851,7 +842,6 @@ int Binning_DRAM_complex_mem_test(void)
 	/* 00 Byte Pattern at offset 1h === */
 	for (i = 0; i < size; i++) {
 		if (MEM32_BASE[i] != 0xa500a500) {
-			vfree(ptr);
 			/* return -10; */
 			ret = -10;
 			goto fail;
@@ -864,7 +854,6 @@ int Binning_DRAM_complex_mem_test(void)
 	/* 00 Byte Pattern at offset 3h === */
 	for (i = 0; i < size; i++) {
 		if (MEM32_BASE[i] != 0xa5000000) {
-			vfree(ptr);
 			/* return -11; */
 			ret = -11;
 			goto fail;
@@ -877,7 +866,6 @@ int Binning_DRAM_complex_mem_test(void)
 	/* Word Pattern at offset 1h == */
 	for (i = 0; i < size; i++) {
 		if (MEM32_BASE[i] != 0x00000000) {
-			vfree(ptr);
 			/* return -12; */
 			ret = -12;
 			goto fail;
@@ -890,7 +878,6 @@ int Binning_DRAM_complex_mem_test(void)
 	/* Word Pattern at offset 0h == */
 	for (i = 0; i < size; i++) {
 		if (MEM32_BASE[i] != 0xffff0000) {
-			vfree(ptr);
 			/* return -13; */
 			ret = -13;
 			goto fail;
@@ -901,7 +888,6 @@ int Binning_DRAM_complex_mem_test(void)
     /*===  Read Check === */
 	for (i = 0; i < size; i++) {
 		if (MEM32_BASE[i] != 0xffffffff) {
-			vfree(ptr);
 			/* return -14; */
 			ret = -14;
 			goto fail;
@@ -921,7 +907,6 @@ int Binning_DRAM_complex_mem_test(void)
 		value = MEM_BASE[i];
 
 		if (value != PATTERN1) {
-			vfree(ptr);
 			/* return -15; */
 			ret = -15;
 			goto fail;
@@ -933,7 +918,6 @@ int Binning_DRAM_complex_mem_test(void)
 	for (i = 0; i < size; i++) {
 		value = MEM_BASE[i];
 		if (value != PATTERN2) {
-			vfree(ptr);
 			/* return -16; */
 			ret = -16;
 			goto fail;
@@ -945,7 +929,6 @@ int Binning_DRAM_complex_mem_test(void)
 	for (i = 0; i < size; i++) {
 		value = MEM_BASE[i];
 		if (value != PATTERN1) {
-			vfree(ptr);
 			/* return -17; */
 			ret = -17;
 			goto fail;
@@ -957,7 +940,6 @@ int Binning_DRAM_complex_mem_test(void)
 	for (i = 0; i < size; i++) {
 		value = MEM_BASE[i];
 		if (value != PATTERN2) {
-			vfree(ptr);
 			/* return -18; */
 			ret = -18;
 			goto fail;
@@ -969,7 +951,6 @@ int Binning_DRAM_complex_mem_test(void)
 	for (i = 0; i < size; i++) {
 		value = MEM_BASE[i];
 		if (value != PATTERN1) {
-			vfree(ptr);
 			/* return -19; */
 			ret = -19;
 			goto fail;
@@ -1015,7 +996,6 @@ int Binning_DRAM_complex_mem_test(void)
 	for (i = 0; i < size; i++) {
 		value = MEM_BASE[i];
 		if (value != 0x12345678) {
-			vfree(ptr);
 			/* return -20; */
 			ret = -20;
 			goto fail;
@@ -1033,7 +1013,6 @@ int Binning_DRAM_complex_mem_test(void)
 		if (i < size * 4 - 1)
 			MEM8_BASE[waddr8] = pattern8 + 1;
 		if (MEM8_BASE[raddr8] != pattern8) {
-			vfree(ptr);
 			/* return -21; */
 			ret = -21;
 			goto fail;
@@ -1048,7 +1027,6 @@ int Binning_DRAM_complex_mem_test(void)
 		if (i < size * 2 - 1)
 			MEM16_BASE[i + 1] = pattern16 + 1;
 		if (MEM16_BASE[i] != pattern16) {
-			vfree(ptr);
 			/* return -22; */
 			ret = -22;
 			goto fail;
@@ -1062,7 +1040,6 @@ int Binning_DRAM_complex_mem_test(void)
 		if (i < size - 1)
 			MEM32_BASE[i + 1] = pattern32 + 1;
 		if (MEM32_BASE[i] != pattern32) {
-			vfree(ptr);
 			/* return -23; */
 			ret = -23;
 			goto fail;
