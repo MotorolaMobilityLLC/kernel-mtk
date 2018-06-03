a869a2e377cabbe9d4320a43ab4f633c7a6e1c17
Vincent Lin <vincent.lin@mediatek.com>
2018-06-03 12:31:55 +0800

    [ALPS03774707] UDI: driver init
    [Detail]
    cervino udi driver init
    
    [Solution]
    cervino udi driver init
    
    MTK-Commit-Id: 86f430d6f91736f2abd0df98932a3122ba1edff7
    
    Change-Id: I298969eb7eb98cbfeafe2b2efb4fa0028398bc96
    Signed-off-by: Vincent Lin <vincent.lin@mediatek.com>
    CR-Id: ALPS03774707
    Feature: PTP-OD (Performance Thermal Power-Over Drive)

--- drivers/misc/mediatek/base/power/include/mtk_udi.h
+++ drivers/misc/mediatek/base/power/include/mtk_udi.h
@@ -14,7 +14,11 @@
 #ifndef _MTK_UDI_H_
 #define _MTK_UDI_H_
 
-#if defined(CONFIG_MACH_MT6775)
+#if defined(CONFIG_MACH_MT6765)
+
+#include "udi_v1/mtk_udi_mt6765.h"
+
+#elif defined(CONFIG_MACH_MT6775)
 
 #include "udi_v1/mtk_udi_mt6775.h"
 
