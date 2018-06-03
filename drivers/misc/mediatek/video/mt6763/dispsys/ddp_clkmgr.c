30f1c1efd154bdc16feec61d9765eb9e8fc61907
Xinshuang Dong <xinshuang.dong@mediatek.com>
2018-06-03 18:22:32 +0800

    [ALPS03306464] Display: fix the ovl abnormal issue.
    [Detail] 1. when lk->kernel, the ovl0/ovl0_2L cg should keep on.
    
    MTK-Commit-Id: 7b03fe2800fd3c1b2b5a4e7990ce9144c27e13b1
    
    Change-Id: I5f7bdf6689607e1982c7d5ff0d87c955a8a82f2d
    Signed-off-by: Xinshuang Dong <xinshuang.dong@mediatek.com>
    CR-Id: ALPS03306464
    Feature: Power Management
    (cherry picked from commit 62722807c6f6be9d3538bba7d57fefda45695d28)

--- drivers/misc/mediatek/video/mt6763/dispsys/ddp_clkmgr.c
+++ drivers/misc/mediatek/video/mt6763/dispsys/ddp_clkmgr.c
@@ -626,22 +626,36 @@ void ddp_clk_force_on(unsigned int on)
 
 int ddp_ovl_dcm_reset(void)
 {
+	unsigned int reg = 0;
 	DDPDBG("ddp_ovl_dcm_reset\n");
 	/* hw workaround : begin */
 	/* if the mmsys_clk restart, we should do it for all the ovl modules*/
 	/*  gce event 32/33/34 workaround */
-	ddp_clk_prepare_enable(DISP0_DISP_OVL1_2L);
-	ddp_clk_prepare_enable(DISP0_DISP_OVL0_2L);
-	ddp_clk_prepare_enable(DISP0_DISP_OVL0);
-	DISP_REG_SET(NULL, DISP_REG_CONFIG_MMSYS_HW_DCM_DIS_SET0, 1<<16);/*ovl1_2l dcm*/
-	DISP_REG_SET(NULL, DISP_REG_CONFIG_MMSYS_HW_DCM_DIS_SET0, 1<<15);/*ovl0_2l dcm*/
-	DISP_REG_SET(NULL, DISP_REG_CONFIG_MMSYS_HW_DCM_DIS_SET0, 1<<14);/*ovl0 dcm*/
-	DISP_REG_SET(NULL, DISP_REG_CONFIG_MMSYS_HW_DCM_DIS_CLR0, 1<<16);
-	DISP_REG_SET(NULL, DISP_REG_CONFIG_MMSYS_HW_DCM_DIS_CLR0, 1<<15);
-	DISP_REG_SET(NULL, DISP_REG_CONFIG_MMSYS_HW_DCM_DIS_CLR0, 1<<14);
-	ddp_clk_disable_unprepare(DISP0_DISP_OVL1_2L);
-	ddp_clk_disable_unprepare(DISP0_DISP_OVL0_2L);
-	ddp_clk_disable_unprepare(DISP0_DISP_OVL0);
+
+	reg = DISP_REG_GET(DISP_REG_CONFIG_MMSYS_CG_CON0);
+
+	if ((reg & (1 << 20)) != 0) {
+		/*if the ovl cg is already on, skip it.*/
+		ddp_clk_prepare_enable(DISP0_DISP_OVL0);
+		DISP_REG_SET(NULL, DISP_REG_CONFIG_MMSYS_HW_DCM_DIS_SET0, 1<<14);/*ovl0 dcm*/
+		DISP_REG_SET(NULL, DISP_REG_CONFIG_MMSYS_HW_DCM_DIS_CLR0, 1<<14);
+		ddp_clk_disable_unprepare(DISP0_DISP_OVL0);
+	}
+
+	if ((reg & (1 << 21)) != 0) {
+		ddp_clk_prepare_enable(DISP0_DISP_OVL0_2L);
+		DISP_REG_SET(NULL, DISP_REG_CONFIG_MMSYS_HW_DCM_DIS_SET0, 1<<15);/*ovl0_2l dcm*/
+		DISP_REG_SET(NULL, DISP_REG_CONFIG_MMSYS_HW_DCM_DIS_CLR0, 1<<15);
+		ddp_clk_disable_unprepare(DISP0_DISP_OVL0_2L);
+	}
+
+	if ((reg & (1 << 22)) != 0) {
+		ddp_clk_prepare_enable(DISP0_DISP_OVL1_2L);
+		DISP_REG_SET(NULL, DISP_REG_CONFIG_MMSYS_HW_DCM_DIS_SET0, 1<<16);/*ovl1_2l dcm*/
+		DISP_REG_SET(NULL, DISP_REG_CONFIG_MMSYS_HW_DCM_DIS_CLR0, 1<<16);
+		ddp_clk_disable_unprepare(DISP0_DISP_OVL1_2L);
+	}
+
 	/* hw workaround : end */
 
 	return 0;
