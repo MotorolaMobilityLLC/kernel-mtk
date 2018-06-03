ce065ede7bd7556e99b6c6558670af19d3dd35fc
mtk09688 <ting-fang.hou@mediatek.com>
2018-06-03 14:53:13 +0800

    [ALPS03013509] Audio: mt6759 mtkaif calibration
    [Detail]
    Modify mtkaif calibration function
    
    [Solution]
    Modify mtkaif calibration function
    - apply mt6759 delay phase setting
    - add mt6759 choose phases policy
    
    MTK-Commit-Id: 23fab59e1f867a9c8c8cd4763005d2c5dc5ef111
    
    Change-Id: Ib25b74b4786c118a6b9c1bd8d456e728068a9525
    CR-Id: ALPS03013509
    Feature: Driver
    Signed-off-by: Ting-Fang Hou <ting-fang.hou@mediatek.com>

--- sound/soc/mediatek/codec/mt6355/mtk-soc-codec-63xx.h
+++ sound/soc/mediatek/codec/mt6355/mtk-soc-codec-63xx.h
@@ -82,7 +82,7 @@ void OpenTrimBufferHardware(bool bEnable);
 
 /* mtk audio interface calibration function */
 void mtkaif_calibration_set_loopback(bool enable);
-void mtkaif_calibration_set_phase(int mode1, int mode2);
+void mtkaif_calibration_set_phase(int mode1);
 
 /* mtk dpd function */
 void mtk_read_dpd_parameter(int impedance, struct mtk_dpd_param *dpd_param);
