b7eff8c3d60dd319afe4519a7119570c35159557
Poting Chen <poting.chen@mediatek.com>
2018-06-03 11:00:42 +0800

    [ALPS03667890] kernel: performance migration
    [Detail] add boost controller
    
    MTK-Commit-Id: 5bc6681e7c571d212b071e018bb5a9b1bbed5181
    
    Change-Id: I812557ed8ac87abaedbd4e1f81016023a425624b
    Signed-off-by: Poting Chen <poting.chen@mediatek.com>
    CR-Id: ALPS03667890
    Feature: VCORE DVFS

--- drivers/misc/mediatek/include/mt-plat/legacy_controller.h
+++ drivers/misc/mediatek/include/mt-plat/legacy_controller.h
@@ -13,12 +13,14 @@
 
 #include <mtk_ppm_api.h>
 
-#define PPM_KIR_PERF 0
-#define PPM_KIR_FBC 1
-#define PPM_MAX_KIR 2
+#define PPM_KIR_PERF  0
+#define PPM_KIR_FBC   1
+#define PPM_KIR_WIFI  2
+#define PPM_KIR_BOOT  3
+#define PPM_KIR_TOUCH 4
+#define PPM_MAX_KIR   5
 
-extern char *ppm_copy_from_user_for_proc(const char __user *buffer
-					, size_t count);
+extern unsigned int mt_cpufreq_get_freq_by_idx(int id, int idx);
 int update_userlimit_cpu_freq(int kicker, int num_cluster
 				, struct ppm_limit_data *freq_limit);
 int update_userlimit_cpu_core(int kicker, int num_cluster
