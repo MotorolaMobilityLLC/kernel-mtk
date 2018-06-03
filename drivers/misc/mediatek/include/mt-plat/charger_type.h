3c8697a58de836488527ce2d35ddefefc4a0a4d7
Arvin Wang <arvin.wang@mediatek.com>
2018-06-03 09:10:47 +0800

    [ALPS03561644] usb : to fix build fail
    [Detail]
    charger_type.h:30:6: error: CONFIG_USB_MTK_EXTCON is not defined,
     evaluates to 0 [-Werror=undef]
      || (CONFIG_USB_MTK_EXTCON)
    
    MTK-Commit-Id: 92eb84f4b4eeecc63b914c8edaa55320d5d939b3
    
    Change-Id: I6e09d0a45c0bab1be87eef78dd5fe7299227103f
    Signed-off-by: Arvin Wang <arvin.wang@mediatek.com>
    CR-Id: ALPS03561644
    Feature: USB 3.0 Device

--- drivers/misc/mediatek/include/mt-plat/charger_type.h
+++ drivers/misc/mediatek/include/mt-plat/charger_type.h
@@ -27,7 +27,7 @@ enum charger_type {
 };
 
 #if defined(CONFIG_USB_MTK_HDRC) || defined(CONFIG_USB_MU3D_DRV) \
-	|| (CONFIG_USB_MTK_EXTCON)
+	|| defined(CONFIG_USB_MTK_EXTCON)
 extern void mt_usb_connect(void);
 extern void mt_usb_disconnect(void);
 #else
