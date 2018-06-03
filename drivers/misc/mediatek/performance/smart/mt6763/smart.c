ec4acf9d7a91bb11a283c804be91ec9b684dbf55
Ian-Y Chen <ian-y.chen@mediatek.com>
2018-06-03 22:10:05 +0800

    [ALPS03532345] smart: refine smart detection
    [Detail] Refine smart detection for o1
    
    [Solution] Remove redundant flow
    
    MTK-Commit-Id: 6bdd55e3ff636c52ee15417d4acd3b5375462a6a
    
    Change-Id: Icfcbe9ced06f280e32989a9c3e7ca3f183a9ea28
    Signed-off-by: Ian-Y Chen <ian-y.chen@mediatek.com>
    CR-Id: ALPS03532345
    Feature: System Performance

--- drivers/misc/mediatek/performance/smart/mt6763/smart.c
+++ drivers/misc/mediatek/performance/smart/mt6763/smart.c
@@ -85,19 +85,8 @@ static unsigned long app_btask_thresh;
 static unsigned long app_up_times;
 static unsigned long app_down_times;
 
-static int native_is_running;   /* Is native running */
-static unsigned long native_btask_thresh;
-static unsigned long native_up_times;
-static unsigned long native_down_times;
-static unsigned long java_btask_thresh;
-static unsigned long java_up_times;
-
 static unsigned long app_up_count;
 static unsigned long app_down_count;
-static unsigned long native_up_count;
-static unsigned long native_down_count;
-static unsigned long native_pid;
-static unsigned long java_up_count;
 
 static unsigned long turbo_util_thresh;
 static int turbo_mode_enable;
@@ -240,13 +229,8 @@ static ssize_t mt_app_is_sports_write(struct file *filp, const char *ubuf,
 		ret = 0;
 		if (arg == 0) {
 			app_is_sports = 0;
-			native_up_count = 0;
-			native_down_count = 0;
-			java_up_count = 0;
 		} else if (arg == 1) {
 			app_is_sports = 1;
-			app_up_count = 0;
-			app_down_count = 0;
 		}
 	} else
 		ret = -EINVAL;
@@ -392,159 +376,6 @@ static ssize_t mt_app_down_times_write(struct file *filp, const char *ubuf,
 static int mt_app_down_times_show(struct seq_file *m, void *v)
 {
 	seq_printf(m, "%lu\n", app_down_times);
-	return 0;
-}
-
-static ssize_t mt_native_is_running_write(struct file *filp, const char *ubuf,
-		size_t cnt, loff_t *data)
-{
-	int ret;
-	unsigned long arg;
-
-	if (!kstrtoul_from_user(ubuf, cnt, 0, &arg)) {
-		ret = 0;
-		if (arg == 0)
-			native_is_running = 0;
-		else if (arg == 1)
-			native_is_running = 1;
-	} else
-		ret = -EINVAL;
-
-	return (ret < 0) ? ret : cnt;
-}
-
-static int mt_native_is_running_show(struct seq_file *m, void *v)
-{
-	if (native_is_running)
-		SEQ_printf(m, "1\n");
-	else
-		SEQ_printf(m, "0\n");
-	return 0;
-}
-
-static ssize_t mt_native_btask_thresh_write(struct file *filp, const char *ubuf,
-		size_t cnt, loff_t *data)
-{
-	int ret;
-	unsigned long arg;
-
-	if (!kstrtoul_from_user(ubuf, cnt, 0, &arg)) {
-		ret = 0;
-		native_btask_thresh = arg;
-	} else
-		ret = -EINVAL;
-
-	return (ret < 0) ? ret : cnt;
-}
-
-static int mt_native_btask_thresh_show(struct seq_file *m, void *v)
-{
-	seq_printf(m, "%lu\n", native_btask_thresh);
-	return 0;
-}
-
-static ssize_t mt_native_up_times_write(struct file *filp, const char *ubuf,
-		size_t cnt, loff_t *data)
-{
-	int ret;
-	unsigned long arg;
-
-	if (!kstrtoul_from_user(ubuf, cnt, 0, &arg)) {
-		ret = 0;
-		native_up_times = arg;
-	} else
-		ret = -EINVAL;
-
-	return (ret < 0) ? ret : cnt;
-}
-
-static int mt_native_up_times_show(struct seq_file *m, void *v)
-{
-	seq_printf(m, "%lu\n", native_up_times);
-	return 0;
-}
-
-static ssize_t mt_native_down_times_write(struct file *filp, const char *ubuf,
-		size_t cnt, loff_t *data)
-{
-	int ret;
-	unsigned long arg;
-
-	if (!kstrtoul_from_user(ubuf, cnt, 0, &arg)) {
-		ret = 0;
-		native_down_times = arg;
-	} else
-		ret = -EINVAL;
-
-	return (ret < 0) ? ret : cnt;
-}
-
-static int mt_native_down_times_show(struct seq_file *m, void *v)
-{
-	seq_printf(m, "%lu\n", native_down_times);
-	return 0;
-}
-
-static ssize_t mt_native_pid_write(struct file *filp, const char *ubuf,
-		size_t cnt, loff_t *data)
-{
-	int ret;
-	unsigned long arg;
-
-	if (!kstrtoul_from_user(ubuf, cnt, 0, &arg)) {
-		ret = 0;
-		native_pid = arg;
-	} else
-		ret = -EINVAL;
-
-	return (ret < 0) ? ret : cnt;
-}
-
-static int mt_native_pid_show(struct seq_file *m, void *v)
-{
-	seq_printf(m, "%lu\n", native_pid);
-	return 0;
-}
-
-static ssize_t mt_java_btask_thresh_write(struct file *filp, const char *ubuf,
-		size_t cnt, loff_t *data)
-{
-	int ret;
-	unsigned long arg;
-
-	if (!kstrtoul_from_user(ubuf, cnt, 0, &arg)) {
-		ret = 0;
-		java_btask_thresh = arg;
-	} else
-		ret = -EINVAL;
-
-	return (ret < 0) ? ret : cnt;
-}
-
-static int mt_java_btask_thresh_show(struct seq_file *m, void *v)
-{
-	seq_printf(m, "%lu\n", java_btask_thresh);
-	return 0;
-}
-
-static ssize_t mt_java_up_times_write(struct file *filp, const char *ubuf,
-		size_t cnt, loff_t *data)
-{
-	int ret;
-	unsigned long arg;
-
-	if (!kstrtoul_from_user(ubuf, cnt, 0, &arg)) {
-		ret = 0;
-		java_up_times = arg;
-	} else
-		ret = -EINVAL;
-
-	return (ret < 0) ? ret : cnt;
-}
-
-static int mt_java_up_times_show(struct seq_file *m, void *v)
-{
-	seq_printf(m, "%lu\n", java_up_times);
 	return 0;
 }
 
@@ -809,97 +640,6 @@ static const struct file_operations mt_app_down_times_fops = {
 	.release = single_release,
 };
 
-static int mt_native_is_running_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, mt_native_is_running_show, inode->i_private);
-}
-
-static const struct file_operations mt_native_is_running_fops = {
-	.open = mt_native_is_running_open,
-	.write = mt_native_is_running_write,
-	.read = seq_read,
-	.llseek = seq_lseek,
-	.release = single_release,
-};
-
-static int mt_native_btask_thresh_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, mt_native_btask_thresh_show, inode->i_private);
-}
-
-static const struct file_operations mt_native_btask_thresh_fops = {
-	.open = mt_native_btask_thresh_open,
-	.write = mt_native_btask_thresh_write,
-	.read = seq_read,
-	.llseek = seq_lseek,
-	.release = single_release,
-};
-
-static int mt_native_up_times_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, mt_native_up_times_show, inode->i_private);
-}
-
-static const struct file_operations mt_native_up_times_fops = {
-	.open = mt_native_up_times_open,
-	.write = mt_native_up_times_write,
-	.read = seq_read,
-	.llseek = seq_lseek,
-	.release = single_release,
-};
-
-static int mt_native_down_times_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, mt_native_down_times_show, inode->i_private);
-}
-
-static const struct file_operations mt_native_down_times_fops = {
-	.open = mt_native_down_times_open,
-	.write = mt_native_down_times_write,
-	.read = seq_read,
-	.llseek = seq_lseek,
-	.release = single_release,
-};
-
-static int mt_native_pid_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, mt_native_pid_show, inode->i_private);
-}
-
-static const struct file_operations mt_native_pid_fops = {
-	.open = mt_native_pid_open,
-	.write = mt_native_pid_write,
-	.read = seq_read,
-	.llseek = seq_lseek,
-	.release = single_release,
-};
-
-static int mt_java_btask_thresh_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, mt_java_btask_thresh_show, inode->i_private);
-}
-
-static const struct file_operations mt_java_btask_thresh_fops = {
-	.open = mt_java_btask_thresh_open,
-	.write = mt_java_btask_thresh_write,
-	.read = seq_read,
-	.llseek = seq_lseek,
-	.release = single_release,
-};
-
-static int mt_java_up_times_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, mt_java_up_times_show, inode->i_private);
-}
-
-static const struct file_operations mt_java_up_times_fops = {
-	.open = mt_java_up_times_open,
-	.write = mt_java_up_times_write,
-	.read = seq_read,
-	.llseek = seq_lseek,
-	.release = single_release,
-};
-
 static int mt_turbo_util_thresh_open(struct inode *inode, struct file *file)
 {
 	return single_open(file, mt_turbo_util_thresh_show, inode->i_private);
@@ -992,27 +732,19 @@ int smart_enter_turbo_mode(void)
 void mt_smart_update_sysinfo(unsigned int cur_loads, unsigned int cur_tlp, unsigned int btask, unsigned int htask)
 {
 	int ret, prev_enable;
-	char event_hps[9] = "DETECT=4"; /* HPS*/
-	char event_tlp[9] = "DETECT=6"; /* TLP*/
 	char event_running[9]  = "DETECT=8"; /* running */
 	char event_act_up[9]   = "ACTION=1"; /* up   */
 	char event_act_down[9] = "ACTION=0"; /* down */
-	char *envp_hps[3] = { event_hps, event_act_up, NULL };
-	char *envp_tlp[3] = { event_tlp, event_act_up, NULL };
 	char *envp_up[3] = { event_running, event_act_up, NULL };
 	char *envp_down[3] = { event_running, event_act_down, NULL };
-	char event_native[9]      = "DETECT=9"; /* running */
-	char *envp_native_up[3] = { event_native, event_act_up, NULL };
-	char *envp_native_down[3] = { event_native, event_act_down, NULL };
-	int  pid = 0;
 	unsigned long ll_util, ll_cap;
 
 	if (!uevent_enable)
 		return;
 
 	if (log_enable)
-		pr_debug(TAG"get_sysinfo sports:%d, native:%d, cur_load:%d, cur_tlp:%d, btask:%d, htask:%d\n",
-		app_is_sports, native_is_running, cur_loads, cur_tlp, btask, htask);
+		pr_debug(TAG"get_sysinfo sports:%d, cur_load:%d, cur_tlp:%d, btask:%d, htask:%d\n",
+		app_is_sports, cur_loads, cur_tlp, btask, htask);
 
 	if (app_is_sports == 1) { /* foreground app enters sports mode  */
 
@@ -1081,75 +813,12 @@ void mt_smart_update_sysinfo(unsigned int cur_loads, unsigned int cur_tlp, unsig
 				}
 			}
 		}
-	} else if (native_is_running == 1) { /* native program is running */
-#ifdef CONFIG_MTK_ACAO_SUPPORT
-		if (turbo_mode_enable)
-			unset_cpu_isolation(ISO_TURBO);
-#endif
-		turbo_mode_enable = 0; /* reset */
-
-		if (htask != native_btask_thresh)
-			native_down_count++;
-		else
-			native_down_count = 0;
-
-		if (native_down_count >= native_down_times) {
-			pr_debug(TAG"get_sysinfo EXIT!!!!!\n");
-			native_is_running = 0;
-			native_down_count = 0; /* reset */
-			if (smart_context_obj) { /* native is stop */
-				ret = kobject_uevent_env(&smart_context_obj->mdev.this_device->kobj,
-				KOBJ_CHANGE, envp_native_down);
-				if (ret)
-					pr_debug(TAG"kobject_uevent error:%d\n", ret);
-			}
-		}
 	} else {
 #ifdef CONFIG_MTK_ACAO_SUPPORT
 		if (turbo_mode_enable)
 			unset_cpu_isolation(ISO_TURBO);
 #endif
 		turbo_mode_enable = 0; /* reset */
-
-		if (htask == native_btask_thresh) { /* detect native program */
-			native_up_count++;
-			java_up_count = 0;
-			if (native_up_count >= native_up_times) { /* native */
-				pr_debug(TAG"get_sysinfo native MATCH!!!!!\n");
-				native_is_running = 1;
-				native_up_count = 0; /* reset */
-				sched_max_util_task(NULL, &pid, NULL, NULL);	/* the heavist task */
-				native_pid = pid;
-				if (smart_context_obj) { /* native is running */
-					ret = kobject_uevent_env(&smart_context_obj->mdev.this_device->kobj,
-					KOBJ_CHANGE, envp_native_up);
-					if (ret)
-						pr_debug(TAG"kobject_uevent error:%d\n", ret);
-				}
-			}
-		} else if (htask >= java_btask_thresh) {
-			java_up_count++;
-			native_up_count = 0;
-			/*if (java_up_count == java_up_times - 1) { */
-			if (java_up_count == 1) { /* check TLP */
-				if (smart_context_obj) {
-					ret = kobject_uevent_env(&smart_context_obj->mdev.this_device->kobj,
-					KOBJ_CHANGE, envp_tlp);
-					if (ret)
-						pr_debug(TAG"kobject_uevent error:%d\n", ret);
-				}
-			} else if (java_up_count == java_up_times) { /* multi-core case */
-				if (smart_context_obj) {
-					ret = kobject_uevent_env(&smart_context_obj->mdev.this_device->kobj,
-					KOBJ_CHANGE, envp_hps);
-					if (ret)
-						pr_debug(TAG"kobject_uevent error:%d\n", ret);
-				}
-			}
-		} else {
-			native_up_count = 0;
-			java_up_count = 0;
-		}
 	}
 }
 
@@ -1220,6 +889,8 @@ static int _smart_timer_callback(unsigned long data)
 {
 	unsigned long flags;
 
+	mod_timer(&tsmart.tmr_list, (jiffies + msecs_to_jiffies(SMART_TIMER_INTERVAL_MS)));
+
 	spin_lock_irqsave(&tsmart.smart_lock, flags);
 	tsmart.smart_event = 0;
 	spin_unlock_irqrestore(&tsmart.smart_lock, flags);
@@ -1253,7 +924,7 @@ static int tsmart_thread(void *ptr)
 		smart_get_sysinfo(&cur_loads, &cur_tlp, &btask, &htask);
 		mt_smart_update_sysinfo(cur_loads, cur_tlp, btask, htask);
 
-		mod_timer(&tsmart.tmr_list, (jiffies + msecs_to_jiffies(SMART_TIMER_INTERVAL_MS)));
+		/*mod_timer(&tsmart.tmr_list, (jiffies + msecs_to_jiffies(SMART_TIMER_INTERVAL_MS)));*/
 	}
 	return 0;
 }
@@ -1267,12 +938,6 @@ static int init_smart_obj(void)
 	/* dev init */
 	struct smart_context *obj = kzalloc(sizeof(*obj), GFP_KERNEL);
 
-#if 0
-	if (!obj) {
-		pr_debug(TAG"kzalloc error\n");
-		return -1;
-	}
-#endif
 
 	mutex_init(&obj->s_op_mutex);
 	smart_context_obj = obj;
@@ -1284,13 +949,6 @@ static int init_smart_obj(void)
 		return -2;
 	}
 
-#if 0
-	ret = sysfs_create_group(&smart_context_obj->mdev.this_device->kobj, &smart_attribute_group);
-	if (ret < 0) {
-		pr_debug(TAG"sysfs_create_group error:%d\n", ret);
-		return -3;
-	}
-#endif
 	ret = kobject_uevent(&smart_context_obj->mdev.this_device->kobj, KOBJ_ADD);
 	if (ret) {
 		pr_debug(TAG"kobject_uevent error:%d\n", ret);
@@ -1354,27 +1012,6 @@ int __init init_smart(void)
 	pe = proc_create("app_down_times", 0644, smart_dir, &mt_app_down_times_fops);
 	if (!pe)
 		return -ENOMEM;
-	pe = proc_create("native_is_running", 0644, smart_dir, &mt_native_is_running_fops);
-	if (!pe)
-		return -ENOMEM;
-	pe = proc_create("native_btask_thresh", 0644, smart_dir, &mt_native_btask_thresh_fops);
-	if (!pe)
-		return -ENOMEM;
-	pe = proc_create("native_up_times", 0644, smart_dir, &mt_native_up_times_fops);
-	if (!pe)
-		return -ENOMEM;
-	pe = proc_create("native_down_times", 0644, smart_dir, &mt_native_down_times_fops);
-	if (!pe)
-		return -ENOMEM;
-	pe = proc_create("native_pid", 0644, smart_dir, &mt_native_pid_fops);
-	if (!pe)
-		return -ENOMEM;
-	pe = proc_create("java_btask_thresh", 0644, smart_dir, &mt_java_btask_thresh_fops);
-	if (!pe)
-		return -ENOMEM;
-	pe = proc_create("java_up_times", 0644, smart_dir, &mt_java_up_times_fops);
-	if (!pe)
-		return -ENOMEM;
 	pe = proc_create("turbo_util_thresh", 0644, smart_dir, &mt_turbo_util_thresh_fops);
 	if (!pe)
 		return -ENOMEM;
@@ -1387,15 +1024,6 @@ int __init init_smart(void)
 	app_up_times = 10;       /* 40ms * 10 = 400ms */
 	app_down_times = 100;    /* 40ms * 10 = 4000ms */
 
-	native_is_running = 0;
-	native_btask_thresh = 1;  /* btask should == 1 */
-	native_up_times = 200;    /* 40ms * 200 = 8000ms */
-	native_down_times = 2000; /* 40ms * 2000 = 80000ms */
-	native_pid = 0;
-
-	java_btask_thresh = 8;    /* btask should >= 8 */
-	java_up_times = 3;        /* 40ms * 3 = 120ms */
-
 	turbo_util_thresh = 80;   /* util <= 80% */
 
 	log_enable = 0;    /* debug log */
@@ -1405,7 +1033,10 @@ int __init init_smart(void)
 	force_isolate = 0; /* test mode: force turbo mode isolation on */
 
 	/* check turbo mode: bit 3 of index 54 */
-	turbo_support = (get_devinfo_with_index(54) & (1 << 3)) ? 1 : 0;
+	if (get_devinfo_with_index(30) == 0x10) /* 63n */
+		turbo_support = 0;
+	else
+		turbo_support = (get_devinfo_with_index(54) & (1 << 3)) ? 1 : 0;
 
 	mark_addr = kallsyms_lookup_name("tracing_mark_write");
 
