6eaf6cc8b2e5eafa1a22aa201239bc157c64d7f8
Xianpu Liang <xianpu.liang@mediatek.com>
2018-06-03 12:48:57 +0800

    [ALPS02524755] wlan: New API to control TDLS power save
    [Detail]
    TDLS power save will cause too much latency, so real time application
    may be affected.
    for example, the audio play back is not smoothly when run crossmount
    [Solution]
    Add a new API to disable TDLS power save
    
    MTK-Commit-Id: 2596500a2de85fc8bb816097f0c5af5586df17a6
    
    Change-Id: I22d30500063bd179d2fcbac9609efbaa54f2aea1
    Signed-off-by: Xianpu Liang <xianpu.liang@mediatek.com>
    CR-Id: ALPS02524755
    Feature: CrossMount

--- drivers/misc/mediatek/connectivity/wlan/gen4/os/linux/gl_cfg80211.c
+++ drivers/misc/mediatek/connectivity/wlan/gen4/os/linux/gl_cfg80211.c
@@ -2109,6 +2109,10 @@ int mtk_cfg80211_testmode_cmd(IN struct wiphy *wiphy, IN struct wireless_dev *wd
 			i4Status = mtk_cfg80211_testmode_hs20_cmd(wiphy, data, len);
 			break;
 #endif /* CFG_SUPPORT_PASSPOINT */
+		case TESTMODE_CMD_ID_STR_CMD:
+			i4Status = mtk_cfg80211_process_str_cmd(prGlueInfo,
+					(PUINT_8)(prParams+1), len - sizeof(*prParams));
+			break;
 
 		default:
 			i4Status = -EINVAL;
@@ -2664,6 +2668,29 @@ int mtk_cfg80211_tdls_oper(struct wiphy *wiphy, struct net_device *dev,
 		 /* FALSE,    //6628 -> 6630  fgIsP2pOid-> x */
 		 &u4BufLen);
 	return 0;
+}
+
+INT_32 mtk_cfg80211_process_str_cmd(P_GLUE_INFO_T prGlueInfo, PUINT_8 cmd, INT_32 len)
+{
+	WLAN_STATUS rStatus = WLAN_STATUS_SUCCESS;
+	UINT_32 u4SetInfoLen = 0;
+
+	if (strnicmp(cmd, "tdls-ps ", 8) == 0) {
+#if CFG_SUPPORT_TDLS
+		rStatus = kalIoctl(prGlueInfo,
+					   wlanoidDisableTdlsPs,
+					   (PVOID)(cmd+8), 1, FALSE, FALSE, TRUE, &u4SetInfoLen);
+#else
+		DBGLOG(REQ, WARN, "not support tdls\n");
+		return -EOPNOTSUPP;
+#endif
+	} else
+		return -EOPNOTSUPP;
+
+	if (rStatus == WLAN_STATUS_SUCCESS)
+		return 0;
+
+	return -EINVAL;
 }
 
 #endif
