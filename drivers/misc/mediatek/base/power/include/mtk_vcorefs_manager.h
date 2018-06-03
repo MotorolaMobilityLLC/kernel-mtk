2b3610ffa3cd00621c31a650a570c24dbed2c4ac
Howard Hsu <howard.hsu@mediatek.com>
2018-06-03 16:50:57 +0800

    [ALPS03244342] VCORE DVFS: new project early porting
    [Detail] early porting vcorefs driver for MT6758
    
    [Solution] early porting vcorefs driver
    
    MTK-Commit-Id: 172bf615a6e3a7f1ca71ed846a4edb8e12573d00
    
    Change-Id: Ic31d3d8b1da5fb3c3b3e76681f8c2e7cc2e3ae1f
    Signed-off-by: Howard Hsu <howard.hsu@mediatek.com>
    CR-Id: ALPS03244342
    Feature: VCORE DVFS

--- drivers/misc/mediatek/base/power/include/mtk_vcorefs_manager.h
+++ drivers/misc/mediatek/base/power/include/mtk_vcorefs_manager.h
@@ -18,7 +18,8 @@
 
 #include "vcorefs_v1/mtk_vcorefs_manager_mt6757.h"
 
-#elif defined(CONFIG_MACH_MT6799) || defined(CONFIG_MACH_MT6763) || defined(CONFIG_MACH_MT6759)
+#elif defined(CONFIG_MACH_MT6799) || defined(CONFIG_MACH_MT6763)    \
+	|| defined(CONFIG_MACH_MT6759) || defined(CONFIG_MACH_MT6758)
 
 #include "vcorefs_v3/mtk_vcorefs_manager.h"
 
