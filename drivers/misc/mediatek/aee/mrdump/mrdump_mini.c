67995aec3b46058712c1cad0586cb20e4d253e10
Chunlei Wang <chunlei.wang@mediatek.com>
2018-06-03 17:11:24 +0800

    [ALPS03264284] Hang: Enhance device hang detect feature.
    [Detail]
    Add hang detect info to buffer.
    
    [Solution]
    Add hang detect info to buffer.
    Modify kernel log level.
    
    MTK-Commit-Id: d5394f95a49733f2c4415433c24c2244a9280369
    
    Change-Id: If57f288c04db53d3008959a1afdfb61f05d3f216
    Signed-off-by: Chunlei Wang <chunlei.wang@mediatek.com>
    CR-Id: ALPS03264284
    Feature: Android Exception Engine(AEE)

--- drivers/misc/mediatek/aee/mrdump/mrdump_mini.c
+++ drivers/misc/mediatek/aee/mrdump/mrdump_mini.c
@@ -563,6 +563,9 @@ static void mrdump_mini_build_elf_misc(void)
 	get_kernel_log_buffer(&misc.vaddr, &misc.size, &misc.start);
 	mrdump_mini_add_misc(misc.vaddr, misc.size, misc.start, "_KERNEL_LOG_");
 	memset_io(&misc, 0, sizeof(struct mrdump_mini_elf_misc));
+	get_hang_detect_buffer(&misc.vaddr, &misc.size, &misc.start);
+	mrdump_mini_add_misc(misc.vaddr, misc.size, misc.start, "_HANG_DETECT_");
+	memset_io(&misc, 0, sizeof(struct mrdump_mini_elf_misc));
 	get_disp_err_buffer(&misc.vaddr, &misc.size, &misc.start);
 	mrdump_mini_add_misc(misc.vaddr, misc.size, misc.start, "_DISP_ERR_");
 	memset_io(&misc, 0, sizeof(struct mrdump_mini_elf_misc));
