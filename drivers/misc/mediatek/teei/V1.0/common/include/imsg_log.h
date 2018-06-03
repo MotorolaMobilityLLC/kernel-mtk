205f46e2a9f5886a24f43e493792505b2b37546c
Calvin Liao <calvin.liao@mediatek.com>
2018-06-03 18:34:54 +0800

    [ALPS03308416] TEEI: kernel license
    [Detail]
    Add related license description.
    
    MTK-Commit-Id: 9e0cbede605b7d244add0c9d52cc635fba30efa0
    
    Change-Id: I6fcf1ff872480b462bc33adbb432b66f19275ca1
    Signed-off-by: Calvin Liao <calvin.liao@mediatek.com>
    CR-Id: ALPS03308416
    Feature: Microtrust TEEI

--- drivers/misc/mediatek/teei/V1.0/common/include/imsg_log.h
+++ drivers/misc/mediatek/teei/V1.0/common/include/imsg_log.h
@@ -1,11 +1,15 @@
 /*
  * Copyright (c) 2015-2016 MICROTRUST Incorporated
- * All rights reserved
+ * All Rights Reserved.
  *
- * This file and software is confidential and proprietary to MICROTRUST Inc.
- * Unauthorized copying of this file and software is strictly prohibited.
- * You MUST NOT disclose this file and software unless you get a license
- * agreement from MICROTRUST Incorporated.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  */
 #ifndef _ISEE_IMSG_LOG_H_
 #define _ISEE_IMSG_LOG_H_
