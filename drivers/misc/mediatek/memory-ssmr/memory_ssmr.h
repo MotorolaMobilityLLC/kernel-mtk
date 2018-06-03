a1bc101330b3b29425a11721871b398b7ff93033
James Hsu <james.hsu@mediatek.com>
2018-06-03 13:12:14 +0800

    [ALPS03872294] SSMR: Modify feature configuration
    [Detail]
    1. Modify feature size configuration architecture.
    2. Enhance UT interface and offline latency.
    3. Provide a general function for SSVP user.
    4. Reset SSVP feature config in dts.
    
    MTK-Commit-Id: fabaf108293db27c64f199a1f1b83030f46cbf9a
    
    Change-Id: I43353238751c502de68afb7dddbea8918e21ab72
    Signed-off-by: James Hsu <james.hsu@mediatek.com>
    CR-Id: ALPS03872294
    Feature: Others

--- drivers/misc/mediatek/memory-ssmr/memory_ssmr.h
+++ drivers/misc/mediatek/memory-ssmr/memory_ssmr.h
@@ -1,65 +1,41 @@
 /*
- * Copyright (C) 2016 MediaTek Inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
- */
+* Copyright (C) 2018 MediaTek Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+*/
 #ifndef __MEMORY_SSMR_H__
 #define __MEMORY_SSMR_H__
 
-#define SVP_REGION_IOC_MAGIC		'S'
-
-#define SVP_REGION_ACQUIRE _IOR(SVP_REGION_IOC_MAGIC, 6, int)
-#define SVP_REGION_RELEASE _IOR(SVP_REGION_IOC_MAGIC, 8, int)
-
-void show_pte(struct mm_struct *mm, unsigned long addr);
-
-#define UPPER_LIMIT32 (1ULL << 32)
-#define UPPER_LIMIT64 (1ULL << 63)
-
-extern int _tui_region_offline(phys_addr_t *pa, unsigned long *size,
-		u64 upper_limit);
-
-int tui_region_offline64(phys_addr_t *pa, unsigned long *size)
-{
-	return _tui_region_offline(pa, size, UPPER_LIMIT64);
-}
-EXPORT_SYMBOL(tui_region_offline64);
-
-int tui_region_offline(phys_addr_t *pa, unsigned long *size)
-{
-	return _tui_region_offline(pa, size, UPPER_LIMIT32);
-}
-EXPORT_SYMBOL(tui_region_offline);
-
-extern int _secmem_region_offline(phys_addr_t *pa, unsigned long *size,
-		u64 upper_limit);
-
-int secmem_region_offline64(phys_addr_t *pa, unsigned long *size)
-{
-	return _secmem_region_offline(pa, size, UPPER_LIMIT64);
-}
-EXPORT_SYMBOL(secmem_region_offline64);
-
-int secmem_region_offline(phys_addr_t *pa, unsigned long *size)
-{
-	return _secmem_region_offline(pa, size, UPPER_LIMIT32);
-}
-EXPORT_SYMBOL(secmem_region_offline);
-
-#ifdef CONFIG_MTK_ION
-extern void ion_sec_heap_dump_info(void);
-#else
-static inline void ion_sec_heap_dump_info(void)
-{
-	pr_info("%s is not supported\n", __func__);
-}
+enum ssmr_feature_type {
+	SSMR_FEAT_SVP,
+#ifdef CONFIG_MTK_IRIS_SUPPORT
+	SSMR_FEAT_IRIS,
 #endif
+#ifdef CONFIG_MTK_CAM_SECURITY_SUPPORT
+	SSMR_FEAT_2D_FR,
+#endif
+#ifdef CONFIG_TRUSTONIC_TRUSTED_UI
+	SSMR_FEAT_TUI,
+#endif
+#ifdef CONFIG_MTK_SEC_VIDEO_PATH_SUPPORT
+	SSMR_FEAT_WFD,
+#endif
+#ifdef CONFIG_MTK_PROT_MEM_SUPPORT
+	SSMR_FEAT_PROT_SHAREDMEM,
+#endif
+	__MAX_NR_SSMR_FEATURES,
+};
+
+extern int ssmr_offline(phys_addr_t *pa, unsigned long *size, bool is_64bit,
+		unsigned int feat);
+extern int ssmr_online(unsigned int feat);
+
 
 #endif
