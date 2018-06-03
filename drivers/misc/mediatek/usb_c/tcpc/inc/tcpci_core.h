a867bb12bf9c0d58d79d1126270a8dcde6652bd7
Vend.Gene.Chen <vend_gene_chen001@mediatek.com>
2018-06-03 23:34:47 +0800

    [ALPS03521190] PMIC: set MT6370 pd notifier time check default off
    [Detail]
    set MT6370 pd notifier time check default off
    
    MTK-Commit-Id: d7ace3edd1b0e97a884191c384c8edfc4ca9d496
    
    Change-Id: I41b2352e41fc621949a962082669d4f2fbd0eedf
    Signed-off-by: Vend.Gene.Chen <vend_gene_chen001@mediatek.com>
    CR-Id: ALPS03521190
    Feature: Power Management

--- drivers/misc/mediatek/usb_c/tcpc/inc/tcpci_core.h
+++ drivers/misc/mediatek/usb_c/tcpc/inc/tcpci_core.h
@@ -77,7 +77,11 @@
 #define PE_EVT_INFO_VDM_DIS		0
 #define PE_DBG_RESET_VDM_DIS	1
 
+#ifdef CONFIG_PD_BEGUG_ON
 #define PD_BUG_ON(x)	WARN_ON(x)
+#else
+#define PD_BUG_ON(x)
+#endif
 
 struct tcpc_device;
 
