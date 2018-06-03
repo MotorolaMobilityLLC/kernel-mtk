f261b80c6ef90833bba46fcbf1dc5748b018143d
Anmin Hsu <anmin.hsu@mediatek.com>
2018-06-03 22:02:06 +0800

    [ALPS03251467] sspm: add mt6771 driver
    [Detail]
    1. Add driver header (tempory using legacy ipi pin)
    2. Reserved shared memory of dts
    3. Enable SSPM_PLT_CONFIG
    
    MTK-Commit-Id: bfec357ceb0f45f1726cbeb3b87c51e18c36c780
    
    Change-Id: I85f745468a4053067a7ee1e796b855eb55bcca51
    Signed-off-by: Anmin Hsu <anmin.hsu@mediatek.com>
    CR-Id: ALPS03251467
    Feature: TinySys

---
+++ drivers/misc/mediatek/sspm/mt6771/sspm_ipi_timeout_cb.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2011-2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <linux/module.h>       /* needed by all modules */
+#include <mt-plat/sync_write.h>
+#include "sspm_define.h"
+
+/* debug API */
+__weak void dump_emi_outstanding(void) {}
+__weak void mtk_spm_dump_debug_info(void) {}
+__weak void usb_dump_debug_register(void) {}
+
+/* platform callback when ipi timeout */
+void sspm_ipi_timeout_cb(int ipi_id)
+{
+
+}
+
