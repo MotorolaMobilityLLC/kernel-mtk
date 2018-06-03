daa1d79b6aa6eaa19548c7fc78cc98758f179d4e
ziming sun <ziming.sun@mediatek.com>
2018-06-03 14:58:18 +0800

    [ALPS02369398] wlan: wlanRemove fail
    [Detail]
    wlanRemove called by chip reset maybe failed
    due to pending kalIoctl.
    [Solution]
    Complete all kalIoctl when wlanRemove.
    
    MTK-Commit-Id: b7260a64eccda3814eda933319714dc6cbcf325c
    
    Change-Id: Ic41c8e42d36b30efd496af492f2773234b3aeaec
    Signed-off-by: ziming sun <ziming.sun@mediatek.com>
    CR-Id: ALPS02369398
    Feature: Wi-Fi Driver for MT6630

--- drivers/misc/mediatek/connectivity/wlan/gen4/os/linux/gl_init.c
+++ drivers/misc/mediatek/connectivity/wlan/gen4/os/linux/gl_init.c
@@ -2593,6 +2593,10 @@ static VOID wlanRemove(VOID)
 	prAdapter = prGlueInfo->prAdapter;
 	kalPerMonDestroy(prGlueInfo);
 
+	/* complete possible pending oid, which may block wlanRemove some time and then whole chip reset may failed */
+	if (kalIsResetting())
+		kalOidCmdClearance(prGlueInfo);
+
 #if CFG_ENABLE_BT_OVER_WIFI
 	if (prGlueInfo->rBowInfo.fgIsNetRegistered) {
 		bowNotifyAllLinkDisconnected(prGlueInfo->prAdapter);
