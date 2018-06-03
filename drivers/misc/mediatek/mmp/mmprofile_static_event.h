4c37924eb144538ceffedee7f4e3ab9898f63501
Yiqi Kang <yiqi.kang@mediatek.com>
2018-06-03 09:26:57 +0800

    [ALPS02822738] mmprofile: coding style failed with CamelCase naming
    [Detail] kernel driver mmp/ uses CamelCase naming which breaks kernel
    coding style.
    
    [Solution] to keep compatibility, use Marcos to avoid build error,
    while modify the function / type / constant naming according to
    kernel coding style.
    
    MTK-Commit-Id: 8a29d9876137822f77eb75cdd755a63a141b2a8d
    
    Change-Id: I3ad39e2cc7b2ebe1f70f43a51028ba0328dc29e1
    Signed-off-by: Yiqi Kang <yiqi.kang@mediatek.com>
    CR-Id: ALPS02822738
    Feature: Others

--- drivers/misc/mediatek/mmp/mmprofile_static_event.h
+++ drivers/misc/mediatek/mmp/mmprofile_static_event.h
@@ -14,26 +14,35 @@
 #ifndef __MMPROFILE_STATIC_EVENT_H__
 #define __MMPROFILE_STATIC_EVENT_H__
 
+#define MMP_InvalidEvent MMP_INVALID_EVENT
+#define MMP_RootEvent MMP_ROOT_EVENT
+#define MMP_TouchPanelEvent MMP_TOUCH_PANEL_EVENT
+#define MMP_MaxStaticEvent MMP_MAX_STATIC_EVENT
+#define MMP_StaticEvents mmp_static_events
 
 typedef enum {
-	MMP_InvalidEvent = 0,
-	MMP_RootEvent = 1,
+	MMP_INVALID_EVENT = 0,
+	MMP_ROOT_EVENT = 1,
 	/* User defined static events begin */
-	MMP_TouchPanelEvent,
+	MMP_TOUCH_PANEL_EVENT,
 	/* User defined static events end. */
-	MMP_MaxStaticEvent
-} MMP_StaticEvents;
+	MMP_MAX_STATIC_EVENT
+} mmp_static_events;
 
 #ifdef MMPROFILE_INTERNAL
-typedef struct {
-	MMP_StaticEvents event;
-	char *name;
-	MMP_StaticEvents parent;
-} MMP_StaticEvent_t;
+#define MMP_StaticEvent_t mmp_static_event_t
 
-static MMP_StaticEvent_t MMProfileStaticEvents[] = {
-	{MMP_RootEvent, "Root_Event", MMP_InvalidEvent},
-	{MMP_TouchPanelEvent, "TouchPanel_Event", MMP_RootEvent},
+typedef struct {
+	mmp_static_events event;
+	char *name;
+	mmp_static_events parent;
+} mmp_static_event_t;
+
+#define MMProfileStaticEvents mmprofile_static_events
+
+static mmp_static_event_t mmprofile_static_events[] = {
+	{MMP_ROOT_EVENT, "Root_Event", MMP_INVALID_EVENT},
+	{MMP_TOUCH_PANEL_EVENT, "TouchPanel_Event", MMP_ROOT_EVENT},
 };
 
 #endif
