94c3ee5a5f0588ab91549e2ca99957299d7f591f
Yun Hsiang <yun.hsiang@mediatek.com>
2018-06-03 16:49:33 +0800

    [ALPS03220551] drs: separate drs driver from memory-lowpower
    [Detail]
    DRS does not depend on memory-lowpower.
    Separate DRS from memory-lowpower and use sysfs to
    implement DRS monitor.
    
    MTK-Commit-Id: 5bf22263e697b6330028fd76b1797d98d180358e
    
    Change-Id: I0365b9cea0bb40f5a2cbe01cc0376c4e980b88f1
    Signed-off-by: Yun Hsiang <yun.hsiang@mediatek.com>
    CR-Id: ALPS03220551
    Feature: DRS (Dynamic Rank Switch)

--- drivers/misc/mediatek/mtkdrs/mtkdrs_drv.c
+++ drivers/misc/mediatek/mtkdrs/mtkdrs_drv.c
@@ -19,6 +19,7 @@
 #include <linux/uaccess.h>
 #include <linux/kthread.h>
 #include <mt-plat/mtk_meminfo.h>
+#include <mach/emi_mpu.h>
 
 static ssize_t mtkdrs_status_show(struct device *dev,
 		struct device_attribute *attr, char *buf)
@@ -33,7 +34,24 @@ static ssize_t mtkdrs_status_show(struct device *dev,
 static ssize_t mtkdrs_debug_show(struct device *dev,
 		struct device_attribute *attr, char *buf)
 {
-	return 0;
+	int n = 0;
+	unsigned long long cnt;
+	int ch;
+
+	for (ch = 0; ch < 4; ch++) {
+		cnt = get_drs_all_self_cnt(ch);
+		n += snprintf(buf + n, PAGE_SIZE - n, "get_drs_all_self_cnt %d: %llu ps\n", ch, cnt);
+	}
+	for (ch = 0; ch < 4; ch++) {
+		cnt = get_drs_rank1_self_cnt(ch);
+		n += snprintf(buf + n, PAGE_SIZE - n, "get_drs_rank1_self_cnt %d: %llu ps\n", ch, cnt);
+	}
+	for (ch = 0; ch < 4; ch++) {
+		cnt = get_drs_rank_prd(ch);
+		n += snprintf(buf + n, PAGE_SIZE - n, "get_drs_rank_prd %d: %llu\n ps", ch, cnt);
+	}
+
+	return n;
 }
 
 static DEVICE_ATTR(status, S_IRUGO, mtkdrs_status_show, NULL);
@@ -52,7 +70,21 @@ struct attribute_group mtkdrs_attr_group = {
 
 static int __init mtkdrs_init(void)
 {
-	return 0;
+	int ret;
+
+	ret = DRS_enable();
+	if (ret) {
+		pr_warn("DRS enable failed\n");
+		return ret;
+	}
+
+	ret = sysfs_create_group(power_kobj, &mtkdrs_attr_group);
+	if (ret) {
+		pr_warn("create sysfs mtkdrs group failed\n");
+		return ret;
+	}
+
+	return ret;
 }
 
 static void __exit mtkdrs_exit(void) { }
