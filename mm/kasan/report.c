e43758927786fee39498312a007b8372d4bf9237
Walter <walter-zh.wu@mediatek.com>
2018-06-03 12:31:57 +0800

    [ALPS03775122] kasan: trigger KE when KAsan detect memory corruption
    [Detail]
    trigger KE and get kasan DB
    
    MTK-Commit-Id: 3ea4eabf77acfb4280a73bef26347e3101247d24
    
    Change-Id: I7ffbeffaf6a233f1d2ce6d61e17e660abe205e72
    Signed-off-by: Walter-ZH.Wu <walter-zh.wu@mediatek.com>
    CR-Id: ALPS03775122
    Feature: Memory Optimization

--- mm/kasan/report.c
+++ mm/kasan/report.c
@@ -291,10 +291,8 @@ static void kasan_report_error(struct kasan_access_info *info)
 	}
 
 	kasan_end_report(&flags);
-	/* trigger KERNEL_API_DUMP DB */
-	aee_kernel_warning_api(__FILE__, __LINE__, DB_OPT_DEFAULT,
-				"KASAN_detect_corruption",
-				"KASAN_detect_corruption");
+	/* trigger KE to get the KAsan corruption message */
+	BUG();
 }
 
 void kasan_report(unsigned long addr, size_t size,
