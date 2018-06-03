2c91406d57189434daadd66eeec94a1fe0f70112
Ian-Y Chen <ian-y.chen@mediatek.com>
2018-06-04 00:13:30 +0800

    [ALPS03695295] perfmgr: control migration_cost
    [Detail] Provide interface to control sysctl_sched_migration_cost
    
    [Solution] Add /proc/perfmgr/eas/m_sched_migrate_cost_n
    
    MTK-Commit-Id: c96b5ad76f7d5f5895592b35626d14736e762aca
    
    Change-Id: I75b5c41bbcc5fff5afd2443a56a2f9c151250a33
    Signed-off-by: Ian-Y Chen <ian-y.chen@mediatek.com>
    CR-Id: ALPS03695295
    Feature: Others

--- drivers/misc/mediatek/performance/eas_controller/include/eas_ctrl_plat.h
+++ drivers/misc/mediatek/performance/eas_controller/include/eas_ctrl_plat.h
@@ -22,6 +22,8 @@ static inline void update_pwd_tbl(void) { }
 static inline int reduce_stall(int bv, int thres, int flag) { return -1; }
 #endif
 
+/* extern sysctl_sched_migration_cost for /proc/sys/kernel/sched_migration_cost_ns */
+extern unsigned int sysctl_sched_migration_cost;
 
 #ifdef CONFIG_MACH_MT6771
 void eas_ctrl_extend_notify_init(void);
