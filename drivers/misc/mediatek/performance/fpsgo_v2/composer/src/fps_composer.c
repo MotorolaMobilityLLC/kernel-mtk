c41d3f4534eb75f379cd5418c77a6773f586076d
ChiaYu Ku <ChiaYu.Ku@mediatek.com>
2018-06-04 00:39:26 +0800

    [ALPS03747803] FPSGO: introduce FTEH
    [Detail]
    FTEH: frame time error handling
    an error handling module in case that FPSGO can not get correct
    sleep time so that it uses the big ceiling frequency.
    FTEH is used to detect this scenario, when detected, free run.
    
    MTK-Commit-Id: 042d338387733811cd28abdd8e8b39dcb8306ac4
    
    Change-Id: I293a3ab0c46227713814353f111551e2b823f6d5
    Signed-off-by: ChiaYu Ku <ChiaYu.Ku@mediatek.com>
    CR-Id:ALPS03747803
    Feature:FPSGO

--- drivers/misc/mediatek/performance/fpsgo_v2/composer/src/fps_composer.c
+++ drivers/misc/mediatek/performance/fpsgo_v2/composer/src/fps_composer.c
@@ -310,6 +310,7 @@ void fpsgo_ctrl2comp_enqueue_start(int pid,
 		xgf_ret = fpsgo_comp2xgf_qudeq_notify(pid, XGF_QUEUE_START, &slptime);
 			if (xgf_ret != XGF_SLPTIME_OK)
 				pr_debug(COMP_TAG"%s xgf_ret:%d", __func__, xgf_ret);
+			f_render->sleep_time = slptime;
 		fpsgo_comp2fbt_enq_start(f_render, enqueue_start_time);
 		fpsgo_comp2fbt_frame_start(f_render, enqueue_start_time, slptime);
 		fpsgo_comp2fstb_queue_time_update(pid, f_render->frame_type,
