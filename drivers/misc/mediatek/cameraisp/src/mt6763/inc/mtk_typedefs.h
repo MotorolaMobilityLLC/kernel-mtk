3fed03f4b7d442d1d6f761de4e0f745d8f839c97
Tony Tsai <Tony.Tsai@mediatek.com>
2018-06-03 15:09:31 +0800

    [ALPS03095458] ISP:kernel isp driver for bianco
    [Detail] add camera isp for bianco project.
    
    [Solution]
    
    MTK-Commit-Id: 71a7358a9ef774ef6c8315e2726853daf2f3aff5
    
    Change-Id: I6adcc1aa2e837dc7d72a4f2e9cee6f1dfd412fa3
    Signed-off-by: Tony Tsai <Tony.Tsai@mediatek.com>
    CR-Id: ALPS03095458
    Feature: [Android Default] Camera Application Basic Functions

---
+++ drivers/misc/mediatek/cameraisp/src/mt6763/inc/mtk_typedefs.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _TYPEDEFS_H
+#define _TYPEDEFS_H
+
+#ifndef BOOL
+typedef unsigned char   BOOL;
+#endif
+
+typedef unsigned char   UINT8;
+typedef unsigned int    UINT32;
+typedef signed int      INT32;
+
+#if 0
+typedef unsigned short  UINT16;
+typedef unsigned short  USHORT;
+typedef signed char     INT8;
+typedef signed short    INT16;
+typedef unsigned int    DWORD;
+typedef void            VOID;
+typedef unsigned char   BYTE;
+typedef float           FLOAT;
+#endif
+
+#endif
