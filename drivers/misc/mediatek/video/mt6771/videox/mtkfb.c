56f26cd283b9438ce874c2a2fd42fcbde1449166
mtk14518 <freya.li@mediatek.com>
2018-06-03 23:43:59 +0800

    [ALPS03662821] DISP: Invalidate ioctl params from userspace
    [Detail]
    KE at mtkfb_pan_display_impl while execute IOCTL_fuzzer32
    var.xoffset, var.yoffset is too large and overflow frame buffer
    
    [Solution]
    invalidate params from userspace
    
    MTK-Commit-Id: 1a65e92c492551a16a6f26d281141a14da8737df
    
    Change-Id: I648421b5c1c830b358435653c719c18036054053
    Signed-off-by: mtk14518 <freya.li@mediatek.com>
    CR-Id: ALPS03662821
    Feature: Vulnerability Scan

--- drivers/misc/mediatek/video/mt6771/videox/mtkfb.c
+++ drivers/misc/mediatek/video/mt6771/videox/mtkfb.c
@@ -1555,6 +1555,12 @@ static int mtkfb_ioctl(struct fb_info *info, unsigned int cmd, unsigned long arg
 			if (copy_from_user(&var, argp, sizeof(var)))
 				return -EFAULT;
 
+			/* invalidate params from userspace */
+			if (var.xres > MTK_FB_XRES || var.yres > MTK_FB_YRES ||
+				var.xres_virtual > MTK_FB_XRESV || var.yres_virtual > MTK_FB_YRESV ||
+				var.xoffset > MTK_FB_XRES || var.yoffset > MTK_FB_YRESV * (MTK_FB_PAGES - 1))
+				return -EFAULT;
+
 			info->var.yoffset = var.yoffset;
 
 			/* check var.yoffset passed by user space */
