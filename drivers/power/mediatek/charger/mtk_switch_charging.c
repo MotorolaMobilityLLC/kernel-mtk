c8468ba00442a2dd6dab3d41a55ea1a1d94af01d
Elvin Lin <elvin.lin@mediatek.com>
2018-06-04 00:04:03 +0800

    [ALPS03708979] charger: Add ICHG setting for TYPEC_CC_VOLT_SNK_DFT
    [Detail]
    If it is type-C adpater and rp level is TYPEC_CC_VOLT_SNK_DFT,
    set AICR and ICHG to 500mA.
    
    MTK-Commit-Id: 6a2cf649b7af19b15fbb2ebfb414ea9e0c3d6331
    
    Change-Id: I5e8020b5ddc3799d24593a71ca012963b7057e68
    Signed-off-by: Elvin Lin <elvin.lin@mediatek.com>
    CR-Id: ALPS03708979
    Feature: pump express charging. (fast charging)

--- drivers/power/mediatek/charger/mtk_switch_charging.c
+++ drivers/power/mediatek/charger/mtk_switch_charging.c
@@ -151,6 +151,10 @@ static void swchg_select_charging_current_limit(struct charger_manager *info)
 		} else if (tcpm_inquire_typec_remote_rp_curr(info->tcpc) == 1500) {
 			pdata->input_current_limit = 1500000;
 			pdata->charging_current_limit = 2000000;
+		} else {
+			/* for TYPEC_CC_VOLT_SNK_DFT */
+			pdata->input_current_limit = 500000;
+			pdata->charging_current_limit = 500000;
 		}
 
 		chr_err("type-C:%d current:%d\n",
