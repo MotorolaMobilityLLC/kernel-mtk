8df51b8b71598c6f2d069a645a1fcb45b7d3f40a
wen.su <wen.su@mediatek.com>
2018-06-03 22:25:33 +0800

    [ALPS03516249] PMIC: disable vsram hw tracking
    [Detail]
    Disable vsram hw tracking to avoid bit-flip.
    
    MTK-Commit-Id: c2bb0ac3ba0a6613d735f82924b5fc70ed0bfe8d
    
    Change-Id: Ib5f061dc67c357e4f320017fc326ee7a56c6c132
    Signed-off-by: wen.su <wen.su@mediatek.com>
    CR-Id: ALPS03516249
    Feature: Power Management

--- drivers/misc/mediatek/pmic/mt6355/v1/pmic.c
+++ drivers/misc/mediatek/pmic/mt6355/v1/pmic.c
@@ -317,8 +317,10 @@ int pmic_tracking_init(void)
 	/* 0.65V */
 	pmic_set_register_value(PMIC_RG_VSRAM_VCORE_VOSEL_SLEEP_LB, 0x28);
 #endif
-
-#if defined(CONFIG_MACH_MT6758) || defined(CONFIG_MACH_MT6759) || defined(CONFIG_MACH_MT6775)
+#if defined(CONFIG_MACH_MT6775)
+	return 0;
+#endif
+#if defined(CONFIG_MACH_MT6758) || defined(CONFIG_MACH_MT6759)
 	ret = enable_vsram_vcore_hw_tracking(1);
 	PMICLOG("Enable VSRAM_VCORE hw tracking\n");
 #else
