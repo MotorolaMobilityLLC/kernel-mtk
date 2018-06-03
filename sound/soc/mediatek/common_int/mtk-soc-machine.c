be1625b72cbb2f657bea22c7cac9a0fbf8d38b1e
KaiChieh Chuang <kaichieh.chuang@mediatek.com>
2018-06-03 18:30:11 +0800

    [ALPS03326328] ASoC: mt6763 fix code defect
    [Detail]
    fix code defect
    coverity: 16246
    
    MTK-Commit-Id: abbf95888add739b516731edc11b4b5d8927933e
    
    Change-Id: I84ac6e05e991a7e3952db4f0854f19d5c0d3b3ee
    Signed-off-by: KaiChieh Chuang <kaichieh.chuang@mediatek.com>
    CR-Id: ALPS03326328
    Feature: Code Inspection

--- sound/soc/mediatek/common_int/mtk-soc-machine.c
+++ sound/soc/mediatek/common_int/mtk-soc-machine.c
@@ -189,8 +189,9 @@ static char const PareGetkeyAna[] = "Getanareg";
 static ssize_t mt_soc_debug_write(struct file *f, const char __user *buf,
 				  size_t count, loff_t *offset)
 {
+#define MAX_DEBUG_WRITE_INPUT 256
 	int ret = 0;
-	char InputString[256];
+	char InputString[MAX_DEBUG_WRITE_INPUT];
 	char *token1 = NULL;
 	char *token2 = NULL;
 	char *token3 = NULL;
@@ -202,15 +203,21 @@ static ssize_t mt_soc_debug_write(struct file *f, const char __user *buf,
 	unsigned long regvalue = 0;
 	char delim[] = " ,";
 
-	memset_io((void *)InputString, 0, 256);
+	memset_io((void *)InputString, 0, MAX_DEBUG_WRITE_INPUT);
 
-	if (count > 256)
-		count = 256;
+	if (count > MAX_DEBUG_WRITE_INPUT)
+		count = MAX_DEBUG_WRITE_INPUT;
 
 	if (copy_from_user((InputString), buf, count))
-		pr_warn("copy_from_user mt_soc_debug_write count = %zu, temp = %s\n", count, InputString);
+		pr_warn("%s(), copy_from_user fail, mt_soc_debug_write count = %zu, temp = %s\n",
+			__func__, count, InputString);
 
-	temp = kstrdup(InputString, GFP_KERNEL);
+	temp = kstrndup(InputString, MAX_DEBUG_WRITE_INPUT, GFP_KERNEL);
+	if (!temp) {
+		pr_warn("%s(), kstrdup fail\n", __func__);
+		goto exit;
+	}
+
 	pr_debug("copy_from_user mt_soc_debug_write count = %zu, temp = %s, pointer = %p\n",
 		count, InputString, InputString);
 	token1 = strsep(&temp, delim);
@@ -284,6 +291,9 @@ static ssize_t mt_soc_debug_write(struct file *f, const char __user *buf,
 		}
 	}
 	AudDrv_Clk_Off();
+
+	kfree(temp);
+exit:
 	return count;
 }
 
