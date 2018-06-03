09bfff373ecd1eae235ac4779d098b32f9dfe5fd
Timo Liao <timo.liao@mediatek.com>
2018-06-03 23:08:44 +0800

    [ALPS03580106] battery: multi gauge 0%
    [Detail]
    1. add new feature: multi gauge 0%.
    2. gauge 0% will change if tempture is change
    
    MTK-Commit-Id: 0418e63336c11a05f189a51d1cd0c675a51d58f8
    
    Change-Id: Iab98fabcc696742fab1607c7aff80801ccfade63
    Signed-off-by: Timo Liao <timo.liao@mediatek.com>
    CR-Id: ALPS03580106
    Feature: Fuel Gauge

--- drivers/misc/mediatek/include/mt-plat/mt6771/include/mach/mtk_battery_property.h
+++ drivers/misc/mediatek/include/mt-plat/mt6771/include/mach/mtk_battery_property.h
@@ -166,6 +166,8 @@
 #define BATTERY_TMP_TO_ENABLE_NAFG -20
 /* #define GM30_DISABLE_NAFG */
 
+#define MULTI_TEMP_GAUGE0 0	/* different temp using different gauge 0% */
+
 #define OVER_DISCHARGE_LEVEL 100
 #define UISOC_UPDATE_TYPE 2
 /*
