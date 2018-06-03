0dcea8eb62c29ecf5686485170f44a7e3cf6cf8f
robin.chen <robin.chen@mediatek.com>
2018-06-03 10:21:28 +0800

    [ALPS03571241] DISP: create fence KE
    [Detail]
    Display driver calling create fence and
    system KE.
    
    [Solution]
    Sync the timeline_fence_release patch from
    sw_sync.c.
    
    MTK-Commit-Id: 54f25f74da3f4f784b9bd0f6f8c56b9d185eb5f8
    
    Change-Id: I736820f2a6d28b4c591292697f74853b79847fba
    Signed-off-by: robin.chen <robin.chen@mediatek.com>
    CR-Id: ALPS03571241
    Feature: Display Driver (DSI Interface)

--- drivers/misc/mediatek/sync/mtk_sync.c
+++ drivers/misc/mediatek/sync/mtk_sync.c
@@ -141,8 +141,10 @@ static void timeline_fence_release(struct fence *fence)
 		unsigned long flags;
 
 		spin_lock_irqsave(fence->lock, flags);
-		if (!list_empty(&pt->link))
+		if (!list_empty(&pt->link)) {
 			list_del(&pt->link);
+			rb_erase(&pt->node, &parent->pt_tree);
+		}
 		spin_unlock_irqrestore(fence->lock, flags);
 	}
 
