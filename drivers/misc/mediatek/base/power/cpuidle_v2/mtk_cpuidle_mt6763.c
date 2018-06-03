c040dd79441355b669676b6a9490d95f4d6a88f9
Cheng-En Chung <cheng-en.chung@mediatek.com>
2018-06-03 16:27:18 +0800

    [ALPS03209683] MTK_CPUIDLE: Fix Build Error
    [Detail]
    Fix Build Error
    
    MTK-Commit-Id: 77305f11e167179839e3220e3b8e7be6f3cb6dea
    
    Change-Id: I2e751b692b6cb5162ad7a377bf09a3adf48b4106
    Signed-off-by: Cheng-En Chung <cheng-en.chung@mediatek.com>
    CR-Id: ALPS03209683
    Feature: Power Management

--- drivers/misc/mediatek/base/power/cpuidle_v2/mtk_cpuidle_mt6763.c
+++ drivers/misc/mediatek/base/power/cpuidle_v2/mtk_cpuidle_mt6763.c
@@ -18,11 +18,6 @@ char *irq_match[] = {
 	"mediatek,kp",
 	"mediatek,mt6763-consys",
 	"mediatek,mdcldma",
-#ifdef CONFIG_MTK_MD3_SUPPORT
-#if CONFIG_MTK_MD3_SUPPORT /* Using this to check >0 */
-	"mediatek,ap2c2k_ccif",
-#endif
-#endif
 	""
 };
 
@@ -32,11 +27,6 @@ int wake_src_irq[] = {
 	WAKE_SRC_R12_KP_IRQ_B,
 	WAKE_SRC_R12_CONN2AP_SPM_WAKEUP_B,
 	WAKE_SRC_R12_MD1_WDT_B,
-#ifdef CONFIG_MTK_MD3_SUPPORT
-#if CONFIG_MTK_MD3_SUPPORT /* Using this to check >0 */
-	WAKE_SRC_R12_C2K_WDT_IRQ_B,
-#endif
-#endif
 	0
 };
 
@@ -44,11 +34,6 @@ int irq_offset[] = {
 	0,
 	1,
 	3,
-#ifdef CONFIG_MTK_MD3_SUPPORT
-#if CONFIG_MTK_MD3_SUPPORT /* Using this to check >0 */
-	1,
-#endif
-#endif
 	0
 };
 
--- drivers/misc/mediatek/base/power/include/cpuidle_v2/mtk_cpuidle_mt6763.h
+++ drivers/misc/mediatek/base/power/include/cpuidle_v2/mtk_cpuidle_mt6763.h
@@ -18,11 +18,6 @@ enum {
 	KP_IRQ_NR = 0,
 	CONN_WDT_IRQ_NR,
 	MD1_WDT_IRQ_NR,
-#ifdef CONFIG_MTK_MD3_SUPPORT
-#if CONFIG_MTK_MD3_SUPPORT /* Using this to check >0 */
-	C2K_WDT_IRQ_NR,
-#endif
-#endif
 	IRQ_NR_MAX
 };
 
