91db30c8bc43321e62e37573d923c70de2935c0c
Brian-SY Yang <brian-sy.yang@mediatek.com>
2018-06-03 14:05:54 +0800

    [ALPS02822358] PPM: add dummy function for new API
    [Detail] add dummy function implement for new API to avoid build error
    if PPM is not built-in.
    
    MTK-Commit-Id: 354c5d212d12ddb6fef33994b8ad0524bf9ea869
    
    Change-Id: Ib48ef148d485d11c5b9b543b6c099acb83e0097b
    Signed-off-by: Brian-SY Yang <brian-sy.yang@mediatek.com>
    CR-Id: ALPS02822358
    Feature: CPU Hotplug

--- drivers/misc/mediatek/base/power/ppm_v2/src/mtk_ppm_dummy.c
+++ drivers/misc/mediatek/base/power/ppm_v2/src/mtk_ppm_dummy.c
@@ -80,6 +80,23 @@ unsigned int mt_ppm_thermal_get_cur_power(void)
 	return 0;
 }
 
+/* User limit policy */
+unsigned int mt_ppm_userlimit_cpu_core(unsigned int cluster_num, struct ppm_limit_data *data)
+{
+	return 0;
+}
+
+unsigned int mt_ppm_userlimit_cpu_freq(unsigned int cluster_num, struct ppm_limit_data *data)
+{
+	return 0;
+}
+
+/* Force limit policy */
+unsigned int mt_ppm_forcelimit_cpu_core(unsigned int cluster_num, struct ppm_limit_data *data)
+{
+	return 0;
+}
+
 /* PTPOD policy */
 void mt_ppm_ptpod_policy_activate(void)
 {
