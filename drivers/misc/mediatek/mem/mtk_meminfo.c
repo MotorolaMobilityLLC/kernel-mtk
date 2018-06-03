f37b094e790fac7a20df6e0a114c57c1b30d60f2
Yun Hsiang <yun.hsiang@mediatek.com>
2018-06-03 22:04:09 +0800

    [ALPS03505954] mm: add vmap_reserved_memory
    [Detail]
    Add vmap_reserved_memory
    
    MTK-Commit-Id: 62dc25e25e28dc16e4140c7218c915c19b70766a
    
    Change-Id: I29957477bc9d02c0f7581e3c192dcda023a43af9
    Signed-off-by: Yun Hsiang <yun.hsiang@mediatek.com>
    CR-Id: ALPS03505954
    Feature: Others

--- drivers/misc/mediatek/mem/mtk_meminfo.c
+++ drivers/misc/mediatek/mem/mtk_meminfo.c
@@ -13,8 +13,12 @@
 
 #include <asm/page.h>
 #include <asm/setup.h>
+#include <asm/pgtable.h>
 #include <linux/module.h>
 #include <linux/of_fdt.h>
+#include <linux/vmalloc.h>
+#include <linux/mm_types.h>
+#include <linux/page-flags.h>
 #include <mt-plat/mtk_memcfg.h>
 #include <mt-plat/mtk_meminfo.h>
 
@@ -141,3 +145,43 @@ phys_addr_t get_zone_movable_cma_size(void)
 #endif /* end CONFIG_MTK_MEMORY_LOWPOWER */
 	return 0;
 }
+
+/**
+ *	vmap_reserved_mem  -  map reserved memory into virtually contiguous space
+ *	@start:		start of reserved memory
+ *	@size:		size of reserved memory
+ *	@prot:		page protection for the mapping
+ */
+void *vmap_reserved_mem(phys_addr_t start, phys_addr_t size, pgprot_t prot)
+{
+	long i;
+	long page_count;
+	unsigned long pfn;
+	void *vaddr = NULL;
+	phys_addr_t addr = start;
+	struct page *page;
+	struct page **pages;
+
+	page_count = DIV_ROUND_UP(size, PAGE_SIZE);
+	pages = vmalloc(page_count * sizeof(struct page *));
+
+	for (i = 0; i < page_count; i++) {
+		pfn = __phys_to_pfn(addr);
+		if (unlikely(!pfn_valid(pfn)))
+			goto out_free;
+
+		page = pfn_to_page(pfn);
+		if (unlikely(!PageReserved(page)))
+			goto out_free;
+
+		pages[i] = page;
+		addr += PAGE_SIZE;
+	}
+
+	vaddr = vmap(pages, page_count, VM_MAP, prot);
+
+out_free:
+	vfree(pages);
+	return vaddr;
+}
+EXPORT_SYMBOL(vmap_reserved_mem);
