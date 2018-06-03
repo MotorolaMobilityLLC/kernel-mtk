a755d61541e587493264a3cafdc37298d16b4010
Changqing Sun <changqing.sun@mediatek.com>
2018-06-03 12:37:36 +0800

    [ALPS02898330] MDT:DataUsage Module Merge
    [Detail] Merge Datausage Part for MDT
    
    MTK-Commit-Id: 0a66b06b7756d5f72fc1407707c8a1376e68e079
    
    Change-Id: I53642d29e70e7737541d9d4539c965f0f2de1b29
    CR-Id: ALPS02898330
    Feature: USB MD direct tethering
    Signed-off-by: Changqing Sun <changqing.sun@mediatek.com>

--- include/linux/netfilter/xt_qtaguid.h
+++ include/linux/netfilter/xt_qtaguid.h
@@ -10,4 +10,26 @@
 #define XT_QTAGUID_SOCKET XT_OWNER_SOCKET
 #define xt_qtaguid_match_info xt_owner_match_info
 
+#ifdef CONFIG_MTK_MD_DIRECT_TETHERING_SUPPORT
+/*MD Tethering ShareMemory Data*/
+struct mdt_data_t {
+	uint64_t total_tx_bytes;
+	uint64_t total_tx_pkts;
+	uint64_t tx_tcp_bytes;
+	uint64_t tx_tcp_pkts;
+	uint64_t tx_udp_bytes;
+	uint64_t tx_udp_pkts;
+	uint64_t tx_others_bytes;
+	uint64_t tx_others_pkts;
+	uint64_t total_rx_bytes;
+	uint64_t total_rx_pkts;
+	uint64_t rx_tcp_bytes;
+	uint64_t rx_tcp_pkts;
+	uint64_t rx_udp_bytes;
+	uint64_t rx_udp_pkts;
+	uint64_t rx_others_bytes;
+	uint64_t rx_others_pkts;
+};
+
+#endif
 #endif /* _XT_QTAGUID_MATCH_H */
