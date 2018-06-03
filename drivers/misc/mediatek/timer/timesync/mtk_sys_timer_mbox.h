a00afb5855bb3a13888444795a4a0ff5f8273f37
Stanley Chu <stanley.chu@mediatek.com>
2018-06-03 21:38:49 +0800

    [ALPS03489452] timer: support timesync framework by sys_timer
    [Detail]
    1. add sys_timer module.
    2. support timesync mechanism between kernel time
    (sched_clock) and sys_timer tick.
    
    Timesync framework is aimed to synchronize "sched_clock"
    between AP and sub-system, for example, SSPM.
    
    This change uses SRAM-based framework for timesync:
    We uses sub-SRAM (SRAM in sspm) to store the latest time
    base synchronized and updated by AP. Then sub-system can use
    it as the base to calculate "sched_clock".
    
    MTK-Commit-Id: 0b4c8fca90b8488af84fbaedab4f4296bc700ed8
    
    Change-Id: Id0d818e2f56b1c4f3245e3d6c6376cc3db3033d1
    Signed-off-by: Stanley Chu <stanley.chu@mediatek.com>
    CR-Id: ALPS03489452
    Feature: Others

---
+++ drivers/misc/mediatek/timer/timesync/mtk_sys_timer_mbox.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2017 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#ifndef __MTK_SYS_TIMER_MBOX_H__
+#define __MTK_SYS_TIMER_MBOX_H__
+
+#define SYS_TIMER_MBOX                      3
+
+/* MBOX: AP write, SSPM read */
+#define SYS_TIMER_MBOX_TICK_H               16
+#define SYS_TIMER_MBOX_TICK_L               17
+#define SYS_TIMER_MBOX_TS_H                 18
+#define SYS_TIMER_MBOX_TS_L                 19
+#define SYS_TIMER_MBOX_DEBUG_TS_H           20
+#define SYS_TIMER_MBOX_DEBUG_TS_L           21
+
+#endif /* __MTK_SYS_TIMER_MBOX_H__ */
+
