f6f3a5fdecc2bf5bb4e4cad984a384918d3bfcb7
Jimmy Kuo <jimmy.kuo@mediatek.com>
2018-06-03 14:06:18 +0800

    [ALPS02853817] wlan: Coverity 359940 Array compared against 0
    [Detail]
    Incorrect expression: Array compared against 0
    
    [Solution]
    Remove Incorrect expression
    
    MTK-Commit-Id: eff3ebf07de58f1916517af84026129c7a03121e
    
    Change-Id: Ib7a69a08d7e2224844f80cf4a2601a6f05e01138
    Signed-off-by: Jimmy Kuo <jimmy.kuo@mediatek.com>
    CR-Id: ALPS02853817
    Feature: Others

--- drivers/misc/mediatek/connectivity/wlan/gen2/os/linux/gl_cfg80211.c
+++ drivers/misc/mediatek/connectivity/wlan/gen2/os/linux/gl_cfg80211.c
@@ -744,8 +744,7 @@ int mtk_cfg80211_scan(struct wiphy *wiphy, struct cfg80211_scan_request *request
 	if (request->n_ssids == 0)
 		rScanRequest.u4SsidNum = 0;
 	else if (request->n_ssids <= (SCN_SSID_MAX_NUM + 1)) {
-		if ((request->ssids[request->n_ssids - 1].ssid == NULL)
-			|| (request->ssids[request->n_ssids - 1].ssid_len == 0))
+		if (request->ssids[request->n_ssids - 1].ssid_len == 0)
 			request->n_ssids--; /* remove the rear NULL SSID if this is a wildcard scan*/
 
 		if (request->n_ssids == (SCN_SSID_MAX_NUM + 1)) /* remove the rear SSID if this is a specific scan */
