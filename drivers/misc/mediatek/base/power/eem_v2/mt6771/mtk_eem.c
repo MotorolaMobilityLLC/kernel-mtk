c3df8c5948bb334611b6db391ab7cd46f931c2de
Hector Yuan <hector.yuan@mediatek.com>
2018-06-04 00:10:52 +0800

    [ALPS03738009] DVFS: Modify OPP table to 1V
    [Detail] Modify OPP table to 1V
    
    MTK-Commit-Id: f4f9a99c9752d2ecb1fcf99fb086837cdafca47f
    
    Change-Id: I5c07757aa358f1a8825e15944a654a9e5a2cef5a
    Signed-off-by: Hector Yuan <hector.yuan@mediatek.com>
    CR-Id: ALPS03738009
    Feature: DVFS

--- drivers/misc/mediatek/base/power/eem_v2/mt6771/mtk_eem.c
+++ drivers/misc/mediatek/base/power/eem_v2/mt6771/mtk_eem.c
@@ -1225,6 +1225,7 @@ static void eem_init_det(struct eem_det *det, struct eem_devinfo *devinfo)
 		det->EEMMONEN	= devinfo->CPU_2L_MONEN;
 		det->MTDES	= devinfo->CPU_2L_MTDES;
 		det->SPEC	= devinfo->CPU_2L_SPEC;
+		det->volt_offset = MARGIN_ADD_OFF;
 		det->features = FEA_INIT01 | FEA_INIT02;
 
 		if (mt_cpufreq_get_cpu_level() > 0)
@@ -1240,9 +1241,8 @@ static void eem_init_det(struct eem_det *det, struct eem_devinfo *devinfo)
 		det->EEMMONEN	= devinfo->CPU_L_MONEN;
 		det->MTDES	= devinfo->CPU_L_MTDES;
 		det->SPEC	= devinfo->CPU_L_SPEC;
+		det->volt_offset = MARGIN_ADD_OFF;
 		det->features = FEA_INIT01 | FEA_INIT02;
-		if (eem_devinfo.FT_PGM <= 3)
-			det->volt_offset = MARGIN_ADD_OFF;
 
 		if (mt_cpufreq_get_cpu_level() > 0)
 			det->DVTFIXED = 0x9;
