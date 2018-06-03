eef00b755532a7fe565a1188fdc2683ba5fc56f6
mtk11156 <vincent.lin@mediatek.com>
2018-06-03 14:45:16 +0800

    [ALPS03021026] UDI: driver init for mt6759
    [Detail]
    UDI driver init for mt6759
    
    [Solution]
    UDI driver init for mt6759
    
    MTK-Commit-Id: 184cdd67b4fe8eefef4b0382262fd1e3060aba21
    
    Change-Id: I4dfa5e14f9d1db9cd72259d7f9092902339d5c87
    Signed-off-by: mtk11156 <vincent.lin@mediatek.com>
    CR-Id: ALPS03021026
    Feature: PTP-OD (Performance Thermal Power-Over Drive)

---
+++ drivers/misc/mediatek/base/power/include/mtk_udi.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _MTK_UDI_H_
+#define _MTK_UDI_H_
+
+#if defined(CONFIG_MACH_MT6759)
+
+#include "udi_v1/mtk_udi_mt6759.h"
+
+#elif defined(CONFIG_MACH_MT6799)
+
+#include "../mt6799/mtk_udi.h"
+
+#else
+
+#error NO corresponding project of mtk_udi.h header file can be found!!!
+
+#endif
+
+#endif /* _MTK_UDI_H_ */
+
