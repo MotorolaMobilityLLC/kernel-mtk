95a80d16d51e112bba69b9e21479c553748ef4da
Kuan Hsuan Huang <ks.huang@mediatek.com>
2018-06-03 10:49:09 +0800

    [ALPS02868554] Display: AUO laneswap 6799 driver
    [Detail] New driver added
    
    [Solution] Added AUO laneswap 6799 driver
    
    MTK-Commit-Id: b980feeb849a1b5078ee9103c96e7035b32a75f3
    
    Change-Id: I4a622c96a63240d66372218b99d608dcc7f0d8e9
    Signed-off-by: Kuan Hsuan Huang <ks.huang@mediatek.com>
    CR-Id: ALPS02868554
    Feature: Display Driver (DSI Interface)

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.c
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.c
@@ -975,6 +975,14 @@ LCM_DRIVER *lcm_driver_list[] = {
 #if defined(NT35595_FHD_DSI_CMD_TRULY_NT50358_720P_EXTERN)
 	&nt35595_fhd_dsi_cmd_truly_nt50358_720p_extern_lcm_drv,
 #endif
+
+#if defined(NT35695_FHD_DSI_CMD_AUO_NT50358_LANESWAP_MT6799)
+	&nt35695_fhd_dsi_cmd_auo_nt50358_laneswap_mt6799_lcm_drv,
+#endif
+
+#if defined(NT35695_FHD_DSI_VDO_AUO_NT50358_LANESWAP_MT6799)
+	&nt35695_fhd_dsi_vdo_auo_nt50358_laneswap_mt6799_lcm_drv,
+#endif
 };
 
 unsigned char lcm_name_list[][128] = {
