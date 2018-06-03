32dfe68412d4c15616284ed30d2c5bdb0deddb3b
andrew.yang <andrew.yang@mediatek.com>
2018-06-03 09:24:49 +0800

    [ALPS02826935] Sensor: Add MTK copyright&GPLv2 license statement
    [Detail]
    Add MTK copyright&GPLv2 license statement for the files that are
    added by MTK.
    
    MTK-Commit-Id: 1ef6d8fb383069055f909397a3d6ae30f3a358fb
    
    Change-Id: I20fb5a14e084e784c432bebe701f1b83372e97f8
    Signed-off-by: andrew.yang <andrew.yang@mediatek.com>
    CR-Id: ALPS02826935
    Feature: [Android Default] Acclerometer

--- drivers/misc/mediatek/magnetometer/maghub/maghub.h
+++ drivers/misc/mediatek/magnetometer/maghub/maghub.h
@@ -1,6 +1,16 @@
 /*
- * Definitions for akm09912 compass chip.
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
+
 #ifndef MAGHUB_H
 #define MAGHUB_H
 
