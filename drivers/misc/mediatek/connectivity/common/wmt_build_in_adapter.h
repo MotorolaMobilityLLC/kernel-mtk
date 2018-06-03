188dbb5a4380dc4b5d58c2a49f120c1fd551c67f
Xiufeng Wu <xiufeng.wu@mediatek.com>
2018-06-03 13:13:10 +0800

    [ALPS03786042] wmt: fix Coverity
    [Detail]
    fix Coverity: Uninitialized pointer read:4061640
    
    MTK-Commit-Id: f2e90820b60a30d373671d0cf6a379f849855f78
    
    Change-Id: I81c58735b54d38a33c771f40f4d370b8b1dac9ac
    Signed-off-by: Xiufeng Wu <xiufeng.wu@mediatek.com>
    CR-Id: ALPS03786042
    Feature: Driver
    (cherry picked from commit c4c42b07fab21eaf77f5ecc3006246339cf6b3db)

--- drivers/misc/mediatek/connectivity/common/wmt_build_in_adapter.h
+++ drivers/misc/mediatek/connectivity/common/wmt_build_in_adapter.h
@@ -21,15 +21,9 @@
  * Bridging from platform -> wmt_drv.ko
  ******************************************************************************/
 typedef int (*wmt_bridge_thermal_query_cb)(void);
-#if MTK_WCN_CMB_FOR_SDIO_1V_AUTOK
-typedef int (*wmt_bridge_1vautok_for_dvfs_cb)(void);
-#endif
 
 struct wmt_platform_bridge {
 	wmt_bridge_thermal_query_cb thermal_query_cb;
-#if MTK_WCN_CMB_FOR_SDIO_1V_AUTOK
-	wmt_bridge_1vautok_for_dvfs_cb autok_cb;
-#endif
 };
 
 void wmt_export_platform_bridge_register(struct wmt_platform_bridge *cb);
