8214338f4d14b808ca5d324175186edc1988bdca
Ma Shi <shi.ma@mediatek.com>
2018-06-03 13:17:27 +0800

    [ALPS03883536] msdc: add low io info in aee dump
    [Detail]
    add low io info in aee dump
    
    MTK-Commit-Id: 441c0b77afd223c20adf17a767547cc045be13e3
    
    Change-Id: I395dd8e2a9a8aade709b84fcf7eec37ee917aa7a
    Signed-off-by: Ma Shi <shi.ma@mediatek.com>
    CR-Id: ALPS03883536
    Feature: eMMC boot up

--- drivers/misc/mediatek/aee/mrdump/mrdump_mini_extra_misc.h
+++ drivers/misc/mediatek/aee/mrdump/mrdump_mini_extra_misc.h
@@ -24,5 +24,5 @@
  */
 /* e.g. */
 /* EXTRA_MISC(test_func, "TESTNAME", (4 * 1024)) */
-EXTRA_MISC(get_msdc_aee_buffer, "MSDC", (30 * 1024))
+EXTRA_MISC(get_msdc_aee_buffer, "MSDC", (512 * 1024))
 EXTRA_MISC(get_blockio_aee_buffer, "BLOCKIO", (300 * 1024))
