101f57d87e4224e0b605de0c1b39245359674773
Ives Hung <ives.hung@mediatek.com>
2018-06-03 18:23:44 +0800

    [ALPS03311899] Wlan gen2: Disable GSCN
    [Detail]
    To disable GSCN feature to avoid invoking from FW
    
    [Solution]
    Disable GSCN through config
    
    MTK-Commit-Id: 5bd3562a9a2e1c2e33f634bf361be7330e23d054
    
    Change-Id: Ic6427bc84561556a29622c524f47542a828ca9a3
    Signed-off-by: Ives Hung <ives.hung@mediatek.com>
    CR-Id: ALPS03311899
    Feature: MTBF(*)

--- drivers/misc/mediatek/connectivity/wlan/gen2/include/config.h
+++ drivers/misc/mediatek/connectivity/wlan/gen2/include/config.h
@@ -831,7 +831,7 @@
 
 #define CFG_SUPPORT_SCN_PSCN	1
 #if CFG_SUPPORT_SCN_PSCN
-#define CFG_SUPPORT_GSCN	1	/* GSCN can be disabled here */
+#define CFG_SUPPORT_GSCN	0	/* GSCN can be disabled here */
 #else
 #define CFG_SUPPORT_GSCN	0
 #endif
