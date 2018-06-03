fe454ff30040f52b6e30bd3d1a62d9db5f5c4a13
andrew.yang <andrew.yang@mediatek.com>
2018-06-03 21:37:46 +0800

    [ALPS03505985] alarmtimer: limit log rate
    [Detail]
    Limit log rate to 5 logs per second.
    
    MTK-Commit-Id: 72934f544ccbc969b6d64659dd7ffa26c2e86cdc
    
    Change-Id: Ia1a04bf5f65a7043d571171fc637020f16d7e934
    Signed-off-by: andrew.yang <andrew.yang@mediatek.com>
    CR-Id: ALPS03505985
    Feature: FM Radio
    (cherry picked from commit bf652ff6f7346f21e19a0c366129f7e6a6b37dd3)

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -25,6 +25,7 @@
 #include <linux/posix-timers.h>
 #include <linux/workqueue.h>
 #include <linux/freezer.h>
+#include <linux/ratelimit.h>
 
 /**
  * struct alarm_base - Alarm timer bases
@@ -137,10 +138,15 @@ static inline void alarmtimer_rtc_timer_init(void) { }
  */
 static void alarmtimer_enqueue(struct alarm_base *base, struct alarm *alarm)
 {
+	static DEFINE_RATELIMIT_STATE(ratelimit, HZ - 1, 5);
+
 	if (alarm->state & ALARMTIMER_STATE_ENQUEUED)
 		timerqueue_del(&base->timerqueue, &alarm->node);
 
-	pr_notice("%s, %lld\n", __func__, alarm->node.expires.tv64);
+	if (__ratelimit(&ratelimit)) {
+		ratelimit.begin = jiffies;
+		pr_notice("%s, %lld\n", __func__, alarm->node.expires.tv64);
+	}
 
 	timerqueue_add(&base->timerqueue, &alarm->node);
 	alarm->state |= ALARMTIMER_STATE_ENQUEUED;
