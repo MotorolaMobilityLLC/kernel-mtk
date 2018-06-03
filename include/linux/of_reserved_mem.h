2aee08bbf286d1a679bebad49769a78935de7e2f
Yun Hsiang <yun.hsiang@mediatek.com>
2018-06-03 15:20:01 +0800

    [ALPS03116196] memcg: refactor memory layout for MblockV2
    [Detail]
    All memory reserved before kernel are recorded in dts.
    Memory layout now get the information from reserved_mem and
    display it.
    
    MTK-Commit-Id: a1e878b3579c8b110964e5359d67a9cf2ea5e79f
    
    Change-Id: I888b4bc828add8f7a85b87182418b09be05fc54d
    Signed-off-by: Yun Hsiang <yun.hsiang@mediatek.com>
    CR-Id: ALPS03116196
    Feature: 4GB_DRAM

--- include/linux/of_reserved_mem.h
+++ include/linux/of_reserved_mem.h
@@ -27,6 +27,9 @@ typedef int (*reservedmem_of_init_fn)(struct reserved_mem *rmem);
 #define RESERVEDMEM_OF_DECLARE(name, compat, init)			\
 	_OF_DECLARE(reservedmem, name, compat, init, reservedmem_of_init_fn)
 
+int get_reserved_mem_count(void);
+struct reserved_mem get_reserved_mem(int num);
+
 #ifdef CONFIG_OF_RESERVED_MEM
 int of_reserved_mem_device_init(struct device *dev);
 void of_reserved_mem_device_release(struct device *dev);
