e465e6db3a87193880b16aed6f32850e2730a0a8
Chinwen Chang <chinwen.chang@mediatek.com>
2018-06-03 16:38:03 +0800

    [ALPS03221406] zram: add lz4k support
    Add lz4k compression algorithm for zram.
    
    MTK-Commit-Id: 6917c961006cb82791276e7ab733ec4284797148
    
    Change-Id: Ibdbf3c8d6bf3984d38b730e43db8beb5c58775e2
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>
    CR-Id: ALPS03221406
    Feature: Memory Optimization

---
+++ include/linux/lz4k.h
@@ -0,0 +1,40 @@
+
+#ifndef __LZ4K_H__
+#define __LZ4K_H__
+
+#include <linux/types.h>
+#include <linux/lzo.h>
+
+#define LZ4K_TAG 1261722188	/* "LZ4K" */
+
+#ifndef CONFIG_64BIT
+#define LZ4K_MEM_COMPRESS LZO1X_MEM_COMPRESS
+#else
+#define LZ4K_MEM_COMPRESS (LZO1X_MEM_COMPRESS << 1)
+#endif
+
+int lz4k_compress(const unsigned char *src, size_t src_len,
+		  unsigned char *dst, size_t *dst_len, void *wrkmem);
+
+int lz4k_decompress_safe(const unsigned char *src, size_t src_len,
+			 unsigned char *dst, size_t *dst_len);
+
+#ifdef CONFIG_UBIFS_FS
+int lz4k_decompress_ubifs(const unsigned char *src, size_t src_len,
+			  unsigned char *dst, size_t *dst_len);
+#endif				/* CONFIG_UBIFS_FS */
+
+
+#if 0
+
+extern int lz4k_compress_ubifs(const unsigned char *in, size_t in_len, unsigned char *out,
+		size_t *out_len, void *wrkmem);
+
+extern int lz4k_decompress_ubifs(const unsigned char *in, size_t in_len,
+		unsigned char *out, size_t *out_len);
+
+extern int lz4k_compress(const unsigned char *in, size_t in_len, unsigned char *out,
+		size_t *out_len, void *wrkmem);
+#endif
+
+#endif				/* __LZ4K_H__ */
