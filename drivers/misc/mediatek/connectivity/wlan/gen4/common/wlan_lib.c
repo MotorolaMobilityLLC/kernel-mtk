b4071c75a901fecb8f14346cbf58286e86866556
Tingting Lei <tingting.lei@mediatek.com>
2018-06-03 14:16:48 +0800

    [ALPS03006810] wlan: scan AP speed is worse than benchmark
    [Detail]
    Driver set CH36~48, CH52~64, CH100~140 all as passive scan channels
    for country code TW, FW can only do passive scan (listen Beacon) on
    these channels, cannot send Probe Request, which results in worse
    scan performance.
    
    [Solution]
    Correct the passive scan channel table for country code TW, only set
    CH100~140 as passive scan channels.
    
    MTK-Commit-Id: be7c8d31e2f2fd4ff256fba03fe0752697400552
    
    Change-Id: Ieabe8a652f6a24dd998565e284b604305011905f
    Signed-off-by: Tingting Lei <tingting.lei@mediatek.com>
    CR-Id: ALPS03006810
    Feature: Wi-Fi Driver

--- drivers/misc/mediatek/connectivity/wlan/gen4/common/wlan_lib.c
+++ drivers/misc/mediatek/connectivity/wlan/gen4/common/wlan_lib.c
@@ -5246,7 +5246,7 @@ WLAN_STATUS wlanLoadManufactureData(IN P_ADAPTER_T prAdapter, IN P_REG_INFO_T pr
 #endif
 
 	/* 6. Set domain and channel information to chip */
-	rlmDomainSendCmd(prAdapter, FALSE);
+	rlmDomainSendCmd(prAdapter);
 
 	/* Update supported channel list in channel table */
 	wlanUpdateChannelTable(prAdapter->prGlueInfo);
@@ -7074,9 +7074,9 @@ VOID wlanCfgSetCountryCode(IN P_ADAPTER_T prAdapter)
 		prAdapter->rWifiVar.rConnSettings.u2CountryCode =
 		    (((UINT_16) aucValue[0]) << 8) | ((UINT_16) aucValue[1]);
 
-		/* Force to re-search country code in country domains */
+		/* Force to re-search country code in regulatory domains */
 		prAdapter->prDomainInfo = NULL;
-		rlmDomainSendCmd(prAdapter, FALSE);
+		rlmDomainSendCmd(prAdapter);
 
 		/* Update supported channel list in channel table based on current country domain */
 		wlanUpdateChannelTable(prAdapter->prGlueInfo);
