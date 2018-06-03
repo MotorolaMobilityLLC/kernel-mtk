077f3a0d8aaaa5aa03c3c1151d49b88572950206
Andy <chia-hao.hsu@mediatek.com>
2018-06-03 09:51:10 +0800

    [ALPS03471622] Kernel: add platform driver for new chip
    [Detail]
    add platform driver for new chip
    
    MTK-Commit-Id: 73944c6b40584f59f3e158c4590f070d62cbd211
    
    Change-Id: Iccdc29a2ae5258de4825243f050739a2ae9f69a0
    CR-Id: ALPS03471622
    Feature: n/a
    Signed-off-by: Andy <chia-hao.hsu@mediatek.com>

---
+++ drivers/misc/mediatek/sspm/mt6765/sspm_reservedmem_define.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+#ifndef _SSPM_RESERVEDMEM_DEFINE_H_
+#define _SSPM_RESERVEDMEM_DEFINE_H_
+#include <sspm_reservedmem.h>
+
+enum {
+	SSPM_MEM_ID = 0,
+	PWRAP_MEM_ID,
+	PMIC_MEM_ID,
+	UPD_MEM_ID,
+	NUMS_MEM_ID,
+};
+
+#define SSPM_PLT_LOGGER_BUF_LEN 0x100000
+
+#ifdef _SSPM_INTERNAL_
+/* The total size of sspm_reserve_mblock should less equal than
+ * reserve-memory-sspm_share of device tree
+ */
+static struct sspm_reserve_mblock sspm_reserve_mblock[NUMS_MEM_ID] = {
+	{
+		.num = SSPM_MEM_ID,
+		.size = 0x400 + SSPM_PLT_LOGGER_BUF_LEN,
+		/* logger header + timesync header + 1M log buffer */
+	},
+	{
+		.num = PWRAP_MEM_ID,
+		.size = 0x300,  // 768 bytes
+	},
+	{
+		.num = PMIC_MEM_ID,
+		.size = 0xC000, // 3K
+	},
+	{
+		.num = UPD_MEM_ID,
+		.size = 0x1800, // 6K
+	},
+    /* TO align 64K, total is 1M+64K. The remaining size = 0x2100,   */
+};
+#endif
+#endif
