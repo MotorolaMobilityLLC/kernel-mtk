c58250012db5166627a987f285266334e964589b
Junzhi Zhao <junzhi.zhao@mediatek.com>
2018-06-03 12:02:42 +0800

    [ALPS02899233] HDMI: Modify HDMI_EDID_T define
    [Problem]
    HDMI_EDID_INFO_T is not compatible with userspace, thus the
    ioctl is unmatched, then the edid cannot be parsed to usespace.
    
    [Solution]
    Modify the HDMI_EDID_INFO_T to HDMI_EDID_T
    
    [Test]
    Test ok on tb8167m1 platform.
    
    MTK-Commit-Id: 8001be9caa61248c2e0b9c525259daa5adb85d8d
    
    Change-Id: I7a16483f38957ab0f30c926dbd58c7ef27560710
    CR-Id: ALPS02899233
    Feature: HDMI
    Signed-off-by: Junzhi Zhao <junzhi.zhao@mediatek.com>

--- drivers/misc/mediatek/ext_disp/v1/extd_hdmi.c
+++ drivers/misc/mediatek/ext_disp/v1/extd_hdmi.c
@@ -1445,7 +1445,7 @@ int hdmi_get_capability(void *info)
 int hdmi_get_edid(void *edid_info)
 {
 	int ret = 0;
-	struct HDMI_EDID_INFO_T pv_get_info;
+	HDMI_EDID_T pv_get_info;
 
 	memset(&pv_get_info, 0, sizeof(pv_get_info));
 
