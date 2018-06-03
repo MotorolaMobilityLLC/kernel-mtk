37b460098acb8c759c236a2bf3f141336910a4de
Brian-SY Yang <brian-sy.yang@mediatek.com>
2018-06-03 09:13:31 +0800

    [ALPS03567198] PPM: export sysboost symbol
    [Detail] export sysboost symbol for wlan ko
    
    MTK-Commit-Id: f5e788d8e6a8c2825bee42288a30957786def109
    
    Change-Id: Idd1b237e4e0d93e0fd0ae914fc595b40e31424df
    Signed-off-by: Brian-SY Yang <brian-sy.yang@mediatek.com>
    CR-Id: ALPS03567198
    Feature: Wi-Fi Chipset Capability

--- drivers/misc/mediatek/base/power/ppm_v3/src/mtk_ppm_policy_sys_boost.c
+++ drivers/misc/mediatek/base/power/ppm_v3/src/mtk_ppm_policy_sys_boost.c
@@ -160,6 +160,7 @@ void mt_ppm_sysboost_core(enum ppm_sysboost_user user, unsigned int core_num)
 {
 	/* phase-out */
 }
+EXPORT_SYMBOL(mt_ppm_sysboost_core);
 
 void mt_ppm_sysboost_freq(enum ppm_sysboost_user user, unsigned int freq)
 {
@@ -218,12 +219,14 @@ void mt_ppm_sysboost_freq(enum ppm_sysboost_user user, unsigned int freq)
 
 	mt_ppm_main();
 }
+EXPORT_SYMBOL(mt_ppm_sysboost_freq);
 
 void mt_ppm_sysboost_set_core_limit(enum ppm_sysboost_user user,
 	unsigned int cluster, int min_core, int max_core)
 {
 	/* phase-out */
 }
+EXPORT_SYMBOL(mt_ppm_sysboost_set_core_limit);
 
 void mt_ppm_sysboost_set_freq_limit(enum ppm_sysboost_user user,
 	unsigned int cluster, int min_freq, int max_freq)
@@ -285,6 +288,7 @@ void mt_ppm_sysboost_set_freq_limit(enum ppm_sysboost_user user,
 
 	mt_ppm_main();
 }
+EXPORT_SYMBOL(mt_ppm_sysboost_set_freq_limit);
 
 static void ppm_sysboost_update_limit_cb(void)
 {
