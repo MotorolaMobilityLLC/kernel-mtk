49a9ab1e3fd76d946a126c07e12a3b63e40f21a0
Miles Chen <miles.chen@mediatek.com>
2018-06-14 18:04:34 +0800

    [ALPS03938839] build: fix use of logical && with constant operand
    Fix clang build error:
    
    error: use of logical && with constant operand
    [-Werror,-Wconstant-logical-operand]
    
    MTK-Commit-Id: e818ccc6107e5d422b4fda8a28f38f12fcd41507
    
    Change-Id: I2dead9eb15fb29b790bdd14d23a308f77f4bb20c
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS03938839
    Feature: [Module]Kernel Maintenance

--- drivers/misc/mediatek/m4u/mt6771/m4u_hw.c
+++ drivers/misc/mediatek/m4u/mt6771/m4u_hw.c
@@ -1308,7 +1308,7 @@ int m4u_config_port_array(struct m4u_port_array *port_array)
 
 			config_larb[larb] |= (1 << larb_port);
 			regNew[larb][larb_port] = value =
-				(!!(port_array->ports[port] && M4U_PORT_ATTR_VIRTUAL));
+				!!port_array->ports[port] && !!M4U_PORT_ATTR_VIRTUAL;
 
 #ifdef M4U_TEE_SERVICE_ENABLE
 			{
