ae70a8285501f595a1a6c6bd7468d0cfa716170e
Perry Hsu <perry.hsu@mediatek.com>
2018-06-28 23:06:48 +0800

    [ALPS03837536] f2fs: add support to hardware inline encryption
    [Detail]
    Add support to file based hardware inline encryption (HIE).
    Implement and register the HIE callback functions that
    attach keys to bio, and retrieve keys from user key payload.
    
    Since the file data is crypted by the host hardware IP,
    and transparent to the software stack. Theres no need to
    decrypt pages on read complete function, or encrypt them
    before write.
    
    In addition, forced buffer I/O is also unnecessary for HIE
    crypted files.
    
    MTK-Commit-Id: f87683f8d56ba1194980bf394da303af4cd4a85a
    
    Change-Id: I31b065f90852310c8df0924067f54535ce7387cb
    Signed-off-by: Perry Hsu <perry.hsu@mediatek.com>
    CR-Id: ALPS03837536
    Feature: [Android Default] F2FS File System

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -2785,6 +2785,8 @@ int f2fs_sync_fs(struct super_block *sb, int sync);
 extern __printf(3, 4)
 void f2fs_msg(struct super_block *sb, const char *level, const char *fmt, ...);
 int sanity_check_ckpt(struct f2fs_sb_info *sbi);
+int f2fs_set_bio_ctx(struct inode *inode, struct bio *bio);
+int f2fs_set_bio_ctx_fio(struct f2fs_io_info *fio, struct bio *bio);
 
 /*
  * hash.c
@@ -3369,7 +3371,8 @@ static inline bool f2fs_may_encrypt(struct inode *inode)
 
 static inline bool f2fs_force_buffered_io(struct inode *inode, int rw)
 {
-	return (f2fs_post_read_required(inode) ||
+	return ((f2fs_post_read_required(inode) &&
+			fscrypt_is_sw_encrypt(inode)) ||
 			(rw == WRITE && test_opt(F2FS_I_SB(inode), LFS)) ||
 			F2FS_I_SB(inode)->s_ndevs);
 }
