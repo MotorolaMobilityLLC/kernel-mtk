64c1b758d4c76e508dba8f7896ce538ab41cf57e
Chang-An Chen <chang-an.chen@mediatek.com>
2018-06-28 23:06:13 +0800

    [ALPS03957481] Touch: fix possible NULL dereference
    Fix possible NULL dereference in GT911 driver
    
    MTK-Commit-Id: 8bb1b2e7c440502668db73d8bcbaf27e7a921c04
    
    Change-Id: I1b8cff82f7da85c2b6d556150a0d163448e9640e
    Signed-off-by: Chang-An Chen <chang-an.chen@mediatek.com>
    CR-Id: ALPS03957481
    Feature: [Android Default] CapTouch

--- drivers/input/touchscreen/mediatek/GT911/gt9xx_driver.c
+++ drivers/input/touchscreen/mediatek/GT911/gt9xx_driver.c
@@ -710,7 +710,7 @@ s32 gtp_read_version(struct i2c_client *client, u16 *version)
 	if (version)
 		*version = (buf[7] << 8) | buf[6];
 
-	tpd_info.vid = *version;
+	tpd_info.vid = (buf[7] << 8) | buf[6];
 	tpd_info.pid = 0x00;
 
 	/* for gt9xx series */
