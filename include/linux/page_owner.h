254c64ddc5ffd8ff502e8f2e3af052664a4cc39f
James Hsu <james.hsu@mediatek.com>
2018-07-12 19:59:06 +0800

    [ALPS03997706] SSVP: revert debug patch
    [Detail]
    Revert debug patch for build error
    
    MTK-Commit-Id: a76133967446f436d8b953ee10a3f94ec1db93e0
    
    Change-Id: If345a5bc77b7458701903c46e75dfba01f10e53d
    Signed-off-by: James Hsu <james.hsu@mediatek.com>
    CR-Id: ALPS03997706
    Feature: [Module]SSMR (Shareable Secure Memory Region)

--- include/linux/page_owner.h
+++ include/linux/page_owner.h
@@ -17,16 +17,6 @@ extern void __dump_page_owner(struct page *page);
 extern void pagetypeinfo_showmixedcount_print(struct seq_file *m,
 					pg_data_t *pgdat, struct zone *zone);
 
-extern int __dump_pfn_backtrace(unsigned long pfn);
-
-static inline int dump_pfn_backtrace(unsigned long pfn)
-{
-	if (static_branch_unlikely(&page_owner_inited))
-		return __dump_pfn_backtrace(pfn);
-	else
-		return -1;
-}
-
 static inline void reset_page_owner(struct page *page, unsigned int order)
 {
 	if (static_branch_unlikely(&page_owner_inited))
@@ -69,13 +59,6 @@ struct HandleCount {
 	struct hlist_node node;
 };
 
-struct BtEntry {
-	struct list_head list;
-	size_t nr_entries;
-	size_t allocations;
-	unsigned long backtrace[8];
-};
-
 #endif
 #else
 static inline void reset_page_owner(struct page *page, unsigned int order)
@@ -94,11 +77,6 @@ static inline void copy_page_owner(struct page *oldpage, struct page *newpage)
 }
 static inline void set_page_owner_migrate_reason(struct page *page, int reason)
 {
-}
-
-static inline int dump_pfn_backtrace(unsigned long pfn)
-{
-	return -1;
 }
 
 static inline void dump_page_owner(struct page *page)
