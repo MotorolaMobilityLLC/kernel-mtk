3668baa5a39d12e8089e660c6e24916b01a40f99
Jeter Chen <jeter.chen@mediatek.com>
2018-07-12 19:30:39 +0800

    [ALPS03916858] PMIC: do not trigger AEE warning when VIBR OC
    [Detail]
    do not trigger AEE warning when VIBR OC
    since VIBR OC will be handled by vibrator
    
    MTK-Commit-Id: c322bb50039eb2167e80fa17559eb1bf9a44c1ab
    
    Change-Id: I31e86c5b6b7a6d9b352c12f042ccdb96cf680e63
    Signed-off-by: Jeter Chen <jeter.chen@mediatek.com>
    CR-Id: ALPS03916858
    Feature: Power Management
    (cherry picked from commit 0898db538c89e448e5ac09d651eb8ad8db7ca0bd)

--- drivers/misc/mediatek/pmic/mt6356/v1/pmic_irq.c
+++ drivers/misc/mediatek/pmic/mt6356/v1/pmic_irq.c
@@ -592,14 +592,19 @@ void register_all_oc_interrupts(void)
 		case INT_VSIM1_OC:
 		case INT_VSIM2_OC:
 		case INT_VMCH_OC:
+		case INT_VCAMA_OC:
+		case INT_VCAMD_OC:
 		case INT_VCAMIO_OC:
+		case INT_VIBR_OC:
 			PMICLOG("[PMIC_INT] non-enabled OC: %d\n", oc_interrupt);
 			break;
+#if 0
 		case INT_VCAMA_OC:
 		case INT_VCAMD_OC:
 			PMICLOG("[PMIC_INT] OC:%d should be enabled after power on\n", oc_interrupt);
 			pmic_register_oc_interrupt_callback(oc_interrupt);
 			break;
+#endif
 		default:
 			pmic_register_oc_interrupt_callback(oc_interrupt);
 			pmic_enable_interrupt(oc_interrupt, 1, "PMIC");
