c8f849fd072f855a902113eebe49c243538364d9
hongxu.zhao <hongxu.zhao@mediatek.com>
2018-07-12 19:23:41 +0800

    [ALPS03859736] Timer: refine timesync for camera
    [Detail]
    1. fix bug in multi access condition
    2. add timesync to boot for camera
    
    MTK-Commit-Id: 00686d8ed82a383f219688567760e404e1830af7
    
    Change-Id: Ib66b82515e06b2355e9e6982db515e79f83773ec
    CR-Id: ALPS03859736
    Feature: Sensor Hub
    Signed-off-by: hongxu.zhao <hongxu.zhao@mediatek.com>

--- drivers/misc/mediatek/timer/timesync/archcounter_timesync.h
+++ drivers/misc/mediatek/timer/timesync/archcounter_timesync.h
@@ -16,5 +16,6 @@
 
 void archcounter_timesync_init(uint8_t status);
 uint64_t archcounter_timesync_to_monotonic(uint64_t hwclock);
+uint64_t archcounter_timesync_to_boot(uint64_t hwclock);
 
 #endif
