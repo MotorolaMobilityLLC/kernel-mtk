7d034a3e008b6e5d668ff16b679243b2bca5b723
Cui Li <cui.li@mediatek.com>
2018-07-27 00:02:00 +0800

    [ALPS03892298] m4u:fixed BCR build error
    [Detail]
    fixed build error
    
    MTK-Commit-Id: 6d0174e5486398ac2887d700336cb730e5474e5a
    
    Change-Id: Id06fb4e011b0fa58b8f6da07a275b7834addcf03
    Signed-off-by: Cui Li <cui.li@mediatek.com>
    CR-Id: ALPS03892298
    Feature: GenieZone

--- drivers/misc/mediatek/m4u/mt6765/m4u_priv.h
+++ drivers/misc/mediatek/m4u/mt6765/m4u_priv.h
@@ -30,7 +30,11 @@
 #define M4UINFO(string, args...) pr_info("[M4U] "string, ##args)
 
 #if defined(CONFIG_TRUSTONIC_TEE_SUPPORT) && defined(CONFIG_MTK_TEE_GP_SUPPORT)
+#if defined(CONFIG_MTK_SEC_VIDEO_PATH_SUPPORT)
 #define M4U_TEE_SERVICE_ENABLE
+#elif defined(CONFIG_MTK_CAM_SECURITY_SUPPORT)
+#define M4U_TEE_SERVICE_ENABLE
+#endif
 #endif
 
 #include "m4u_hw.h"
