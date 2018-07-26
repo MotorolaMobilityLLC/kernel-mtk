898e8f35749babe0ff72063e90351a0fc3c2b458
Andy Teng <andy.teng@mediatek.com>
2018-07-27 00:11:15 +0800

    [ALPS04032522] kernel: fix typedef redefinition error
    Fix clang build errors:
    error: redefinition of typedef pm_callback_t is a C11 feature
    [-Werror,-Wtypedef-redefinition]
    
    MTK-Commit-Id: f2eb42cbcedd9f2ab60454f1c9bb1b6a1a223a60
    
    Change-Id: Id3e80cb668b6205cc1d24794c48be3ee02da4e3d
    Signed-off-by: Andy Teng <andy.teng@mediatek.com>
    CR-Id: ALPS04032522
    Feature: SDIO

--- drivers/mmc/host/mediatek/ComboA/mtk_sd.h
+++ drivers/mmc/host/mediatek/ComboA/mtk_sd.h
@@ -146,7 +146,10 @@ enum {
 #define REQ_CRC_STATUS_ERR (0x1 << 7)
 
 typedef void (*sdio_irq_handler_t)(void *);  /* external irq handler */
+#ifndef CONFIG_MTK_COMBO_COMM
+/* prevent type redefinition in mtk_wcn_cmb_stub.h */
 typedef void (*pm_callback_t)(pm_message_t state, void *data);
+#endif
 
 #define MSDC_CD_PIN_EN      (1 << 0)  /* card detection pin is wired   */
 #define MSDC_WP_PIN_EN      (1 << 1)  /* write protection pin is wired */
