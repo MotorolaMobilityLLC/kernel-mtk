054f2e678931e6d160eb0d2c252efcc757fddeaa
SW Integrator <swintegrator@mediatek.com>
2018-07-27 00:11:00 +0800

    [ALPS04027410] sspm: change memory type
    Change memory type to wc for clang
    
    MTK-Commit-Id: fb9601da755cc5434c7609c007eb6d0d785ccd77
    
    Change-Id: Ifd3b71abe673d6b03a79202e0f719ac3b82b5edc
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS04027410
    Feature: TinySys

--- drivers/misc/mediatek/sspm/sspm_reservedmem.c
+++ drivers/misc/mediatek/sspm/sspm_reservedmem.c
@@ -130,7 +130,7 @@ int sspm_reserve_memory_init(void)
 		return -1;
 
 	accumlate_memory_size = 0;
-	sspm_mem_base_virt = (phys_addr_t)(uintptr_t)ioremap_nocache(sspm_mem_base_phys, sspm_mem_size);
+	sspm_mem_base_virt = (phys_addr_t)(uintptr_t)ioremap_wc(sspm_mem_base_phys, sspm_mem_size);
 	pr_debug("[SSPM]reserve mem: virt:0x%llx - 0x%llx (0x%llx)\n",
 			(unsigned long long)sspm_mem_base_virt,
 			(unsigned long long)sspm_mem_base_virt + (unsigned long long)sspm_mem_size,
