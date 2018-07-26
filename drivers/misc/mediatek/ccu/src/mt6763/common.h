24adf88dd4a0e42a7ab2b0bb6d822f44396f0f69
SW Integrator <swintegrator@mediatek.com>
2018-07-27 00:10:48 +0800

    [ALPS04027410] kernel: fix type redefinition build error
    Fix clang build errors:
    
    error: redefinition of typedef uint8_t is a C11 feature
    [-Werror,-Wtypedef-redefinition]
    error: redefinition of typedef MUINT32 is a C11 feature
    [-Werror,-Wtypedef-redefinition]
    error: redefinition of typedef uint32_t is a C11 feature
    [-Werror,-Wtypedef-redefinition]
    error: redefinition of typedef int32_t is a C11 feature
    [-Werror,-Wtypedef-redefinition]
    .
    
    MTK-Commit-Id: 5ff703a8165f0f6cf2ba84c0b99a1dcb15d03d54
    
    Change-Id: I1f35f2d98e5e66e48ffecf1b54e51ba3ebe67caf
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS04027410
    Feature: [Module]Kernel Maintenance

--- drivers/misc/mediatek/ccu/src/mt6763/common.h
+++ drivers/misc/mediatek/ccu/src/mt6763/common.h
@@ -15,15 +15,8 @@
 #define __COMMON_H__
 
 typedef unsigned char UINT8;
-typedef unsigned char uint8_t;
-
 typedef unsigned short UINT16;
-
-typedef unsigned int MUINT32;
 typedef unsigned int UINT32;
-typedef unsigned int uint32_t;
 typedef unsigned int m_uint32;
-
-typedef int int32_t;
 
 #endif				/* __COMMON_H__*/
