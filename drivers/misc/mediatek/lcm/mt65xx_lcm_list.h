c900bf124a6469db473cc8cd27a51f19c1e75d46
mtk12211 <shang.zhou@mediatek.com>
2018-08-10 23:55:41 +0800

    [ALPS03913391] DISP: Porting ili9881h lcm driver
    Porting ili9881h lcm driver.
    
    MTK-Commit-Id: 41f39a4ab79df7c6ab4e40539ad801df03e2dd36
    
    Change-Id: I4081734a6f3482f9ddcdc5035da6af6dc90593b1
    Signed-off-by: mtk12211 <shang.zhou@mediatek.com>
    CR-Id: ALPS03913391
    Feature: Display Driver (DSI Interface)

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
@@ -292,6 +292,8 @@ extern struct LCM_DRIVER ili9881c_hdp_dsi_vdo_ilitek_rt5081_lcm_drv_ebbg;
 extern struct LCM_DRIVER hx8394f_hd720_dsi_vdo_tianma_lcm_drv;
 extern struct LCM_DRIVER nt36672_fhdp_dsi_vdo_auo_lcm_drv;
 extern struct LCM_DRIVER nt36672_fhdp_dsi_vdo_auo_laneswap_lcm_drv;
+extern struct LCM_DRIVER ili9881h_hdp_dsi_vdo_ilitek_rt5081_19_9_lcm_drv;
+
 #ifdef BUILD_LK
 extern void mdelay(unsigned long msec);
 #endif
