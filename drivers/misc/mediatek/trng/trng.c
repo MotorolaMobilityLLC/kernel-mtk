eaf6cb23d3cd1f093581d14bf4434a19805a0111
Vink Shen <vink.shen@mediatek.com>
2018-08-22 16:28:14 +0800

    [ALPS04017312] trng: add crng randomness
    Enable TRNG to add entropy in CRNG
    
    MTK-Commit-Id: b76d43aebcfc422051c3aecab651fb84ced0dd51
    
    Change-Id: Ib17bada4612adec68614f35514b396d8d0999155
    Signed-off-by: Vink Shen <vink.shen@mediatek.com>
    CR-Id: ALPS04017312
    Feature: Power Management

--- drivers/misc/mediatek/trng/trng.c
+++ drivers/misc/mediatek/trng/trng.c
@@ -18,6 +18,13 @@
 #include <linux/seq_file.h>
 #include "trng.h"
 
+#include <linux/delay.h>
+#include <linux/hw_random.h>
+#include <linux/kthread.h>
+#include <linux/random.h>
+#include <linux/syscalls.h>
+
+#ifdef TRNG_DUMP
 #define BIT_STREAM_LEN	500000
 
 static int mtk_trng_dump(struct seq_file *m, void *v)
@@ -39,8 +46,10 @@ static int mtk_trng_dump(struct seq_file *m, void *v)
 				MTK_SIP_KERNEL_GET_RND,
 				TRNG_MAGIC,
 				0, 0, 0,
-				(void *)(val+1), (void *)(val+2), (void *)(val+3)
-				);
+				(void *)(val+1),
+				(void *)(val+2),
+				(void *)(val+3)
+			);
 
 		for (i = 0 ; i < 4 ; i++) {
 			pr_info("val[%d] = 0x%08x\n", i, (uint32_t)val[i]);
@@ -65,16 +74,52 @@ static const struct file_operations trng_fops = {
 	.llseek = seq_lseek,
 	.release = single_release
 };
+#endif
 
+int add_crng_entropy(void *data)
+{
+	int index;
+	size_t trng[4];
+	uint32_t buf[4];
+	(void) data;
+
+	msleep(1000);
+	while (sys_getrandom((char *)&index, sizeof(index), GRND_NONBLOCK)
+	== -EAGAIN) {
+		trng[0] = mt_secure_call_ret4(MTK_SIP_KERNEL_GET_RND,
+				TRNG_MAGIC, 0, 0, 0, (void *)(trng+1),
+				(void *)(trng+2), (void *)(trng+3));
+
+		for (index = 0; index < 4; ++index)
+			buf[index] = (uint32_t)trng[index];
+
+		/* Assume quality: 512/1024 = 0.5 */
+		add_hwgenerator_randomness((const char *)buf, sizeof(buf),
+			sizeof(buf) << 2);
+		msleep(1000);
+	}
+
+	return 0;
+}
 
 /*
  * trng_init: module init function.
  */
 static int __init trng_init(void)
 {
-	parent = proc_mkdir(dirname, NULL);
+	struct task_struct *task;
 
+	pr_info("[TRNG] module init\n");
+	task = kthread_create(add_crng_entropy, NULL, "trng_entropy");
+	if (!IS_ERR(task))
+		wake_up_process(task);
+	else
+		pr_notice("[TRNG] Fail to create kthread trng_entropy\n");
+
+#ifdef TRNG_DUMP
+	parent = proc_mkdir(dirname, NULL);
 	proc_create("rnd_dump", 0664, parent, &trng_fops);
+#endif
 
 	return 0;
 }
