696d68027d267c9397fed24302c5b7a526a530aa
Peter Wang <peter.wang@mediatek.com>
2018-08-31 10:43:04 +0800

    [ALPS04007212] UFS : Add pmic and clock setting"
    This reverts commit d4ce6cef75ff7dd7f209c94dbec8a4e211700c3f.
    
    MTK-Commit-Id: 99be96cc300d903304fe175d63701e822cbac2bf
    
    Change-Id: I2c54bfd6810b9f0421e741dc31dbaea8d27e81f5
    Signed-off-by: Peter Wang <peter.wang@mediatek.com>
    CR-Id: ALPS04007212
    Feature: UFS Booting

--- drivers/scsi/ufs/mediatek/mt6779/ufs-mtk-platform.h
+++ drivers/scsi/ufs/mediatek/mt6779/ufs-mtk-platform.h
@@ -17,10 +17,10 @@
 
 #ifndef CONFIG_FPGA_EARLY_PORTING
 /* If CLKBUF function not ready, comment this define */
-#define CLKBUF_READY
+/* #define CLKBUF_READY */ /* Peter enable after clkbuf ready */
 
 /* If UPMU function not ready, comment this define */
-#define UPMU_READY
+/* #define UPMU_READY */ /* Peter enable after upmu ready */
 #endif
 
 /* Peter enable and test after P ready */
