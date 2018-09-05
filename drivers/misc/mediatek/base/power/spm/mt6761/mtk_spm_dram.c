dd471a72eb6b818a3bee34c25dc678535d1c90e4
Arvin Wang <arvin.wang@mediatek.com>
2018-09-05 16:27:47 +0800

    [ALPS03974462] spm : add LP4 support
    Support LP4 DDR Type SPM support.
    
    MTK-Commit-Id: 94a2df24b4d4e0c33bffc010d8fafd5e444bd280
    
    Change-Id: I9e7ba4f30dc7ad9a8821c3c2104a50718b2400a1
    Signed-off-by: Arvin Wang <arvin.wang@mediatek.com>
    CR-Id: ALPS03974462
    Feature: Power Management
    (cherry picked from commit aff5c058dce289683d2c4efb0052488e21811d58)

--- drivers/misc/mediatek/base/power/spm/mt6761/mtk_spm_dram.c
+++ drivers/misc/mediatek/base/power/spm/mt6761/mtk_spm_dram.c
@@ -170,9 +170,13 @@ static void spm_dram_type_check(void)
 		spmfw_idx = SPMFW_LP4X_2CH_3733;
 	else if (ddr_type == TYPE_LPDDR4X && ddr_hz == 3200)
 		spmfw_idx = SPMFW_LP4X_2CH_3200;
+	else if (ddr_type == TYPE_LPDDR4 && ddr_hz == 3200)
+		spmfw_idx = SPMFW_LP4X_2CH_3200;
 	else if (ddr_type == TYPE_LPDDR3 && ddr_hz == 1866)
 		spmfw_idx = SPMFW_LP3_1CH_1866;
-	pr_info("#@# %s(%d) __spmfw_idx 0x%x\n", __func__, __LINE__, spmfw_idx);
+
+	pr_info("#@# %s(%d) __spmfw_idx 0x%x, ddr=[%d][%d]\n",
+		__func__, __LINE__, spmfw_idx, ddr_type, ddr_hz);
 }
 #endif /* CONFIG_MTK_DRAMC */
 
