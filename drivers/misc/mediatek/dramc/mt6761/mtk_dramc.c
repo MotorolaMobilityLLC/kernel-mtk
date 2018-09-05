aa8443d05bf678b7a1532945587215e6bd361ece
mtk12444 <sagy.shih@mediatek.com>
2018-09-05 16:25:13 +0800

    [ALPS03974462] DRAMC: add DDR2400 support
    Add DDR2400 support
    
    MTK-Commit-Id: 8cced4087e185a4840a4a13dcc5de4821a2f163a
    
    Change-Id: I0c793acdc48ef2aa882fd295e6a152beb1aaa18b
    Signed-off-by: mtk12444 <sagy.shih@mediatek.com>
    CR-Id: ALPS03974462
    Feature: DRAM
    (cherry picked from commit ae2d3d67c6f84beb9e3c25e8674836110e4a04f1)
    (cherry picked from commit 7840d96d9c08c6ac4014777571eb18c652d54a9a)

--- drivers/misc/mediatek/dramc/mt6761/mtk_dramc.c
+++ drivers/misc/mediatek/dramc/mt6761/mtk_dramc.c
@@ -46,6 +46,7 @@
 #include <mt-plat/mtk_devinfo.h>
 
 static int enable_lp3_1333;
+static unsigned int lp4_highest_freq;
 
 void __iomem *SYS_TIMER_BASE_ADDR;
 void __iomem *DRAMC_AO_CHA_BASE_ADDR;
@@ -1281,9 +1282,10 @@ int dram_steps_freq(unsigned int step)
 	case 0:
 		if (DRAM_TYPE == TYPE_LPDDR3)
 			freq = 1866;
-		else if ((DRAM_TYPE == TYPE_LPDDR4) ||
-				(DRAM_TYPE == TYPE_LPDDR4X))
+		else if (DRAM_TYPE == TYPE_LPDDR4X)
 			freq = 3200;
+		else if (DRAM_TYPE == TYPE_LPDDR4)
+			freq = lp4_highest_freq;
 		break;
 	case 1:
 		if (DRAM_TYPE == TYPE_LPDDR3)
@@ -1735,6 +1737,11 @@ static int dram_probe(struct platform_device *pdev)
 	dramc_info("Dram Data Rate = %d\n", get_dram_data_rate());
 	dramc_info("shuffle_status = %d\n", get_shuffle_status());
 	dramc_info("MR mode = %d\n", dram_mr_mode);
+
+	if (DRAM_TYPE == TYPE_LPDDR4)
+		lp4_highest_freq = get_dram_data_rate();
+	else
+		lp4_highest_freq = 0;
 
 #ifdef SW_TX_TRACKING
 	dram_sw_tx = (readl(PDEF_DRAMC0_CHA_REG_0C8) >> 24) & 0x1;
