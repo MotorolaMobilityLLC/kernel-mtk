a8f4267015e0282693d78f81476834e1051d321c
mtk10485 <Sam.Hung@mediatek.com>
2018-09-19 16:12:55 +0800

    [ALPS04102488] Lens: get driver folder name
    [Detail] for CCU parser requer
    
    MTK-Commit-Id: a78f40ccb7d02f184e3820c122ac5e771cd1afbe
    
    Change-Id: I048037abc4c40ee3a3926868b70da0304eaa4e34
    Signed-off-by: mtk10485 <Sam.Hung@mediatek.com>
    CR-Id: ALPS04102488
    Feature: [Android Default] AF (contrast AF)

--- drivers/misc/mediatek/lens/sub/common/ad5823af/AD5823AF.c
+++ drivers/misc/mediatek/lens/sub/common/ad5823af/AD5823AF.c
@@ -144,7 +144,6 @@ static int initAF(void)
 	if (*g_pAF_Opened == 1) {
 
 	int i4RetValue = 0;
-
 	char Mode[2] = {(char)(0x02), (char)(0x01)};
 	char MoveTime[2] = {(char)(0x03), (char)(0x4B)};
 	char VCMMSB[2] = {(char)(0x04), (char)(0x05)};
@@ -281,11 +280,14 @@ int AD5823AF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
 
 int AD5823AF_GetFileName(unsigned char *pFileName)
 {
-	char *FileString = (strrchr(__FILE__, '/') + 1);
+	char FilePath[512];
+	char *FileString;
 
-	strncpy(pFileName, FileString, AF_MOTOR_NAME);
-	FileString = strchr(pFileName, '.');
+	sprintf(FilePath, "%s", __FILE__);
+	FileString = strrchr(FilePath, '/');
 	*FileString = '\0';
+	FileString = (strrchr(FilePath, '/') + 1);
+	strncpy(pFileName, FileString, AF_MOTOR_NAME);
 	LOG_INF("FileName : %s\n", pFileName);
 
 	return 1;
