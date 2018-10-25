004058775a33d2a657592028fa0666290de68428
daohong.qin <daohong.qin@tinno.com>
2018-10-25 16:27:44 +0800

    <REQ><HAAPGO-273><p161bn: add rear camera s5k3l6 driver>
    Change-Id: Idd936249001494878c278916acdf5fcc0f464076

---
+++ drivers/misc/mediatek/imgsensor/src/mt6765/camera_project/p161bn/v1/imgsensor_sensor_list.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2017 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#include "kd_imgsensor.h"
+#include "imgsensor_sensor_list.h"
+
+/* Add Sensor Init function here
+ * Note:
+ * 1. Add by the resolution from ""large to small"", due to large sensor
+ *    will be possible to be main sensor.
+ *    This can avoid I2C error during searching sensor.
+ * 2. This should be the same as
+ *     mediatek\custom\common\hal\imgsensor\src\sensorlist.cpp
+ */
+struct IMGSENSOR_INIT_FUNC_LIST kdSensorList[MAX_NUM_OF_SUPPORT_SENSOR] = {
+#if defined(S5K3L6_SUNWIN_P161BN_MIPI_RAW)
+       {S5K3L6_SUNWIN_P161BN_SENSOR_ID, 
+	SENSOR_DRVNAME_S5K3L6_SUNWIN_P161BN_MIPI_RAW,
+	S5K3L6_SUNWIN_P161BN_MIPI_RAW_SensorInit},
+#endif
+	/*  ADD sensor driver before this line */
+	{0, {0}, NULL}, /* end of list */
+};
+/* e_add new sensor driver here */
+
