ec554a119aa1bba0d5009ef514e8a4e26f5d4de7
Auto Li <auto.li@mediatek.com>
2018-10-31 10:01:54 +0800

    [ALPS04130177] EEPROM: Add 6761/6765 main3
    Add 6761/6765 main3
    
    MTK-Commit-Id: 9c892fed62dd5d53852379085dcf778ee81e1ca1
    
    Change-Id: If5da1b5ffe7d29cd6b57289d3683a910aad999b9
    Signed-off-by: Auto Li <auto.li@mediatek.com>
    CR-Id: ALPS04130177
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/cam_cal/src/common/v1/eeprom_i2c_common_driver.c
+++ drivers/misc/mediatek/cam_cal/src/common/v1/eeprom_i2c_common_driver.c
@@ -27,7 +27,6 @@
 #include "cam_cal.h"
 #include "cam_cal_define.h"
 #include "cam_cal_list.h"
-/*#include <asm/system.h>  // for SM*/
 #include <linux/dma-mapping.h>
 #ifdef CONFIG_COMPAT
 /* 64 bit */
@@ -144,77 +143,4 @@ unsigned int Common_read_region(struct i2c_client *client, unsigned int addr,
 	else
 		return 0;
 }
-
-
-
-
-/************************************************************
- * I2C read function (Custom)
- * Customer's driver can put on here
- * Below is an example
- ************************************************************/
- #define PAGE_SIZE_ 256
-static int iReadRegI2C(u8 *a_pSendData, u16 a_sizeSendData,
-		u8 *a_pRecvData, u16 a_sizeRecvData, u16 i2cId)
-{
-	int  i4RetValue = 0;
-
-	spin_lock(&g_spinLock);
-	g_pstI2CclientG->addr = (i2cId >> 1);
-
-	spin_unlock(&g_spinLock);
-	i4RetValue = i2c_master_send(g_pstI2CclientG,
-		a_pSendData, a_sizeSendData);
-	if (i4RetValue != a_sizeSendData) {
-		pr_debug("I2C send failed!!, Addr = 0x%x\n", a_pSendData[0]);
-		return -1;
-	}
-	i4RetValue = i2c_master_recv(g_pstI2CclientG,
-		(char *)a_pRecvData, a_sizeRecvData);
-	if (i4RetValue != a_sizeRecvData) {
-		pr_debug("I2C read failed!!\n");
-		return -1;
-	}
-	return 0;
-}
-
-static int custom_read_region(u32 addr, u8 *data, u16 i2c_id, u32 size)
-{
-	u8 *buff = data;
-	u32 size_to_read = size;
-
-	int ret = 0;
-
-	while (size_to_read > 0) {
-		u8 page = addr / PAGE_SIZE_;
-		u8 offset = addr % PAGE_SIZE_;
-		char *Buff = data;
-
-		if (iReadRegI2C(&offset, 1, (u8 *)Buff, 1,
-			i2c_id + (page << 1)) < 0) {
-			pr_debug("fail addr=0x%x 0x%x, P=%d, offset=0x%x",
-				addr, *Buff, page, offset);
-			break;
-		}
-		addr++;
-		buff++;
-		size_to_read--;
-		ret++;
-	}
-	pr_debug("addr =%x size %d data read = %d\n", addr, size, ret);
-	return ret;
-}
-
-
-
-unsigned int Custom_read_region(struct i2c_client *client, unsigned int addr,
-				unsigned char *data, unsigned int size)
-{
-	g_pstI2CclientG = client;
-	if (custom_read_region(addr, data, g_pstI2CclientG->addr, size) == 0)
-		return size;
-	else
-		return 0;
-}
-
 
