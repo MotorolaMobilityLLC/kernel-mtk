9cc505ae31eb24beae21c75269a0d09601513f73
Auto Li <auto.li@mediatek.com>
2018-11-13 10:27:01 +0800

    [ALPS04130177] EEPROM: Main3 support
    [Detail] Main3 support
    
    [Solution] done
    
    MTK-Commit-Id: 6e782ef8f399a87bafc2f817e7040978f156d4de
    
    Change-Id: Ibd2add76e5381dc4d361c9bcaf6b1ef430ea6319
    Signed-off-by: Auto Li <auto.li@mediatek.com>
    CR-Id: ALPS04130177
    Feature: [Android Default] Camera Application Basic Functions
    (cherry picked from commit 7d4a9a232e058d85e98ae99861b032b422c835d6)

--- drivers/misc/mediatek/cam_cal/inc/cam_cal_define.h
+++ drivers/misc/mediatek/cam_cal/inc/cam_cal_define.h
@@ -1,15 +1,15 @@
 /*
-* Copyright (C) 2016 MediaTek Inc.
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 2 as
-* published by the Free Software Foundation.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
-*/
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
 #ifndef _CAM_CAL_DATA_H
 #define _CAM_CAL_DATA_H
 
@@ -23,7 +23,14 @@ struct stCAM_CAL_INFO_STRUCT {
 	u32 u4Offset;
 	u32 u4Length;
 	u32 sensorID;
-	u32 deviceID;/* MAIN = 0x01, SUB  = 0x02, MAIN_2 = 0x04, SUB_2 = 0x08 */
+	/*
+	 * MAIN = 0x01,
+	 * SUB  = 0x02,
+	 * MAIN_2 = 0x04,
+	 * SUB_2 = 0x08,
+	 * MAIN_3 = 0x10,
+	 */
+	u32 deviceID;
 	u8 *pu1Params;
 };
 
@@ -39,4 +46,3 @@ struct COMPAT_stCAM_CAL_INFO_STRUCT {
 #endif
 
 #endif/*_CAM_CAL_DATA_H*/
-
