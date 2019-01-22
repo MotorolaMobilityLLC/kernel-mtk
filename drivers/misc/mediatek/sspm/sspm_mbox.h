b52fa29d1ecdf35f2023381dd875bade9bc5a987
jiachiam.liu <jiachiam.liu@mediatek.com>
2019-01-22 22:26:54 +0800

    [ALPS03041477] sspm: ipi code merge from alps-mp-n0.mp8
    [Detail]
    1. CL:611760 - enhance and fix IPI timeout check
    2. CL:613474 - print pin name when ipi timeout
    3. CL:614933 - fix incorrect ipi polling ack
    4. CL:618943 - enhance IPI timeout
    5. CL:627713 - add sspm_ipi_lock_spm_scenario in ipi send
    6. CL:629018 - remove old IPI API
    
    MTK-Commit-Id: acc9c56c224525d4143e4ea220d119822462f357
    
    Change-Id: Ibdaa7d0a5327adce5775cf877f8e68b5865156ec
    Signed-off-by: jiachiam.liu <jiachiam.liu@mediatek.com>
    CR-Id: ALPS03041477
    Feature: TinySys

--- drivers/misc/mediatek/sspm/sspm_mbox.h
+++ drivers/misc/mediatek/sspm/sspm_mbox.h
@@ -26,6 +26,7 @@ typedef unsigned int (*sspm_ipi_isr)(unsigned int mbox, void __iomem *base, unsi
 
 unsigned int sspm_mbox_size(int mbox);
 extern int sspm_mbox_send(unsigned int mbox, unsigned int slot, unsigned int irq, void *data, unsigned int len);
+extern uint32_t *sspm_mbox_addr(unsigned int mbox, unsigned int slot);
 extern int sspm_mbox_read(unsigned int mbox, unsigned int slot, void *data, unsigned int len);
 extern unsigned int sspm_mbox_init(unsigned int mode, unsigned int count, sspm_ipi_isr ipi_isr_cb);
 extern int sspm_mbox_polling(unsigned int mbox, unsigned int irq, unsigned int slot,
