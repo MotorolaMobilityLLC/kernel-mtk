7d0789957436b05440517381ebcc2734af9d0745
Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
2019-01-22 20:54:00 +0800

    [ALPS02743747] cmdq: Fix handle to mva issue
    [Detail]
    Translate pa to mva API check pa plus size, which is size of base address.
    But cmdq use base+offset, which will out of range after plus size.
    
    [Solution]
    Do block offset the base pa before translate to mva.
    And plus buffer offset after translate pa to mva.
    
    MTK-Commit-Id: 5cff9ca98075009f2e432fbae25889766fa5f097
    
    Signed-off-by: Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
    Change-Id: I94ac0ea60d4a50ccf0ca79625f43ceeddfbda652
    CR-Id: ALPS02743747
    Feature: CMDQ
    (cherry picked from commit 7223e009eb303dbab24f9aab27503bc88f2aae70)

--- drivers/misc/mediatek/cmdq/v2/inc/cmdq_sec_dr/cmdq_sec_iwc_common.h
+++ drivers/misc/mediatek/cmdq/v2/inc/cmdq_sec_dr/cmdq_sec_iwc_common.h
@@ -42,11 +42,27 @@ struct iwcCmdqAddrMetadata_t {
 	/* [IN]_d, index of instruction. Update its arg_b value to real PA/MVA in secure world */
 	uint32_t instrIndex;
 
-	uint32_t type; /* [IN] addr handle type*/
-	uint32_t baseHandle; /* [IN]_h, secure address handle */
-	uint32_t offset;     /* [IN]_b, buffser offset to secure handle */
-	uint32_t size;       /* buffer size */
-	uint32_t port;       /* hw port id (i.e. M4U port id)*/
+	/*
+	 * Note: Buffer and offset
+	 *
+	 *   -------------
+	 *   |     |     |
+	 *   -------------
+	 *   ^     ^  ^  ^
+	 *   A     B  C  D
+	 *
+	 *	A: baseHandle
+	 *	B: baseHandle + blockOffset
+	 *	C: baseHandle + blockOffset + offset
+	 *	A~B or B~D: size
+	 */
+
+	uint32_t type;			/* [IN] addr handle type*/
+	uint32_t baseHandle;	/* [IN]_h, secure address handle */
+	uint32_t blockOffset;	/* [IN]_b, block offset from handle(PA) to current block(plane) */
+	uint32_t offset;		/* [IN]_b, buffser offset to secure handle */
+	uint32_t size;			/* buffer size */
+	uint32_t port;			/* hw port id (i.e. M4U port id)*/
 };
 
 struct iwcCmdqDebugConfig_t {
