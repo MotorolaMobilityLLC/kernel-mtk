8fde6bfebe68a8ab16638c0e5bee20d6781bb1c7
Elvin Zhang <elvin.zhang@mediatek.com>
2019-01-23 00:07:30 +0800

    [ALPS03909713] DISP: Set LCM density and Physical parameter
    [Detail]
    Set LCM density and Physical width/height
    according to TDO-HD0546K40305v0.1 pdf
    
    MTK-Commit-Id: 6b2cc54ece43732f6ba4f6ebf144d912b8fb4bc9
    
    Change-Id: Idf46f6498419f51efbd6c1dc51265633bbf47c11
    Signed-off-by: Elvin Zhang <elvin.zhang@mediatek.com>
    CR-Id: ALPS03909713
    Feature: [Module]Display Driver

--- drivers/misc/mediatek/lcm/ili9881c_hd_dsi_vdo_ilitek_nt50358/ili9881c_hd_dsi_vdo_ilitek_nt50358.c
+++ drivers/misc/mediatek/lcm/ili9881c_hd_dsi_vdo_ilitek_nt50358/ili9881c_hd_dsi_vdo_ilitek_nt50358.c
@@ -215,6 +215,7 @@ static const unsigned char LCD_MODULE_ID = 0x01;
 #define LCM_DSI_CMD_MODE									0
 #define FRAME_WIDTH										(720)
 #define FRAME_HEIGHT									(1440)
+#define LCM_DENSITY										(320)
 
 #define LCM_PHYSICAL_WIDTH									(64800)
 #define LCM_PHYSICAL_HEIGHT									(129600)
@@ -555,6 +556,7 @@ static void lcm_get_params(LCM_PARAMS *params)
 	params->physical_height = LCM_PHYSICAL_HEIGHT/1000;
 	params->physical_width_um = LCM_PHYSICAL_WIDTH;
 	params->physical_height_um = LCM_PHYSICAL_HEIGHT;
+	params->density = LCM_DENSITY;
 
 
 #if (LCM_DSI_CMD_MODE)
