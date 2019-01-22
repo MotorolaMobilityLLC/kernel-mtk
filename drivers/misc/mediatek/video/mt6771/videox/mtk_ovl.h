d2c32bb80c2988f6a47849e49d951f89840dface
robin.chen <robin.chen@mediatek.com>
2019-01-23 00:05:34 +0800

    [ALPS03717940] DISP: Revise WDMA recovery flow
    [Detail]
    -The WDMA recovery would be ignored during extension mode since the
    original flow always use main display path handle to control hw engine.
    Fix it by detecting the owner(main or 2nd DISP) of the wdma and then
    use corresponding path handle.
    -Remove idle leave api due to the engine power only be turned off in
    DL mode during screen idle mode. We dont have to consider if wdma is
    power off at the recovery stage.
    
    MTK-Commit-Id: c62cad1ca603614da6377fbb4e1411f665d8ee0f
    
    Change-Id: I252c6a9723067528756c884293bf0b7a4473ab59
    CR-Id: ALPS03717940
    Feature: Display
    Signed-off-by: robin.chen <robin.chen@mediatek.com>

--- drivers/misc/mediatek/video/mt6771/videox/mtk_ovl.h
+++ drivers/misc/mediatek/video/mt6771/videox/mtk_ovl.h
@@ -29,6 +29,7 @@ int ovl2mem_get_max_layer(void);
 void ovl2mem_suspend(void);
 void ovl2mem_resume(void);
 void *mtk_ovl_get_dpmgr_handle(void);
-
+void ovl2mem_path_lock(const char *caller);
+void ovl2mem_path_unlock(const char *caller);
 
 #endif
