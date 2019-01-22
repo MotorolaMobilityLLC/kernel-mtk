37309fe9164f111f194eb81b5f46b3f17ccfbfea
Linger Lee <linger.lee@mediatek.com>
2019-01-23 00:01:16 +0800

    [ALPS03547981] mntl: insert mntl module
    [Detail]
    Get ko reserved memory address & size
    insert module after NAND driver inited
    
    MTK-Commit-Id: 832bf5a4e0b2c45bf9c9952089cb9f3da8099cd6
    
    Change-Id: I5dd114cbb506986e56a687677b320a4d19873d63
    CR-Id: ALPS03547981
    Feature: MTK NAND Translation Layer (MNTL)
    Signed-off-by: Linger Lee <linger.lee@mediatek.com>

--- drivers/misc/mediatek/nand/v1/mtk_nand_fs.h
+++ drivers/misc/mediatek/nand/v1/mtk_nand_fs.h
@@ -19,6 +19,7 @@ extern int g_i4Interrupt;
 extern bool DDR_INTERFACE;
 extern int g_i4InterruptRdDMA;
 extern int g_i4Homescreen;
+extern int init_module_mem(void *buf, int size);
 int mtk_nand_fs_init(void);
 
 void mtk_nand_fs_exit(void);
