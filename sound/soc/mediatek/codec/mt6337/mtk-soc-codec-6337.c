bf0646e5218accbfd4cf4f95291b8d84f7458809
chien-wei hsu <chien-wei.hsu@mediatek.com>
2019-01-22 22:28:31 +0800

    [ALPS02773746] Sound: HPDET with current correction
    [Detail]
    current correction value is saved in Efuse.
    Read the efuse value, and calculate the headphone
    impedance with current correction.
    it should be more accurate.
    
    MTK-Commit-Id: be57a5db9c6ff1cae54915b6bef21f7049c10e1c
    
    Change-Id: Ie459c5b3a016991c55d0d7e5dabda58e132b3b29
    Signed-off-by: chien-wei hsu <chien-wei.hsu@mediatek.com>
    CR-Id: ALPS02773746
    Feature: Phone Sound
    (cherry picked from commit 79fb63acf1094847b3348673cf5ea0f8300191c3)

--- sound/soc/mediatek/codec/mt6337/mtk-soc-codec-6337.c
+++ sound/soc/mediatek/codec/mt6337/mtk-soc-codec-6337.c
@@ -6348,59 +6348,14 @@ static const struct snd_kcontrol_new mt6331_UL_Codec_controls[] = {
 
 int Audio_Read_Efuse_HP_Impedance_Current_Calibration(void)
 {
-	int ret;
+	int value = 0, sign = 0;
 
-	/* Initialize to zero*/
-	ret = 0;
+	sign  = (Ana_Get_Reg(OTP_DOUT_256_271) >> 7) & 0x1;
+	value = Ana_Get_Reg(OTP_DOUT_256_271) & 0x7f;
+	value = sign ? -value : value;
 
-#ifdef EFUSE_HP_IMPEDANCE
-	pr_warn("+%s\n", __func__);
-
-	/* 1. enable efuse ctrl engine clock */
-	Ana_Set_Reg(TOP_CKHWEN_CON0_CLR, 0x0040, 0x0040);
-	Ana_Set_Reg(TOP_CKPDN_CON3_CLR, 0x0004, 0x0004);
-
-	/* 2. set RG_OTP_RD_SW */
-	Ana_Set_Reg(0x0C16, 0x0001, 0x0001);
-
-	/* 3. Select which row to read in MACRO_1 */
-	/* set RG_OTP_PA for bit#786 ~ #793*/
-	Ana_Set_Reg(0x0C00, 0x0022, 0x003F);
-
-	/* 4. Toggle RG_OTP_RD_TRIG */
-	ret = Ana_Get_Reg(0x0C10);
-	if (ret == 0)
-		Ana_Set_Reg(0x0C10, 0x0001, 0x0001);
-	else
-		Ana_Set_Reg(0x0C10, 0x0000, 0x0001);
-
-	/* 5. Polling RG_OTP_RD_BUSY */
-	ret = 1;
-	while (ret == 1) {
-		usleep_range(100, 200);
-		ret = Ana_Get_Reg(0x0C1A);
-		ret = ret & 0x0001;
-		pr_warn("%s polling 0xC1A=0x%x\n", __func__, ret);
-	}
-
-	/* Need to delay at least 1ms for 0xC1A and than can read */
-	usleep_range(500, 1000);
-
-	/* 6. Read RG_OTP_DOUT_SW */
-	/* BIT#786 ~ #793 */
-	ret = Ana_Get_Reg(0x0C18);
-	pr_warn("%s HPoffset : efuse=0x%x\n", __func__, ret);
-	ret = (ret >> 2) & 0x00FF;
-	if (ret >= 64)
-		ret = ret - 128;
-
-	/* 7. Disables efuse_ctrl egine clock */
-	Ana_Set_Reg(TOP_CKPDN_CON3_SET, 0x0004, 0x0004);
-	Ana_Set_Reg(TOP_CKHWEN_CON0_SET, 0x0040, 0x0040);
-#endif
-
-	pr_warn("-%s EFUSE: %d\n", __func__, ret);
-	return ret;
+	pr_aud("%s(), EFUSE: %d\n", __func__, value);
+	return value;
 }
 EXPORT_SYMBOL(Audio_Read_Efuse_HP_Impedance_Current_Calibration);
 static const struct snd_soc_dapm_widget mt6331_dapm_widgets[] = {
