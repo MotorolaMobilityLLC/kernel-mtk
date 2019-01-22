b34932b91223fab566a2cbd3cd12d6e72b28a0db
Ian-Y Chen <ian-y.chen@mediatek.com>
2019-01-23 00:05:08 +0800

    [ALPS03688158] fbc: add touch boost
    [Detail] Implement touch boost for mt6771
    
    [Solution] Add KIR_TOUCH in legacy controller and EAS controller
    
    MTK-Commit-Id: 134a349213916198fe3559aff036d9551efac656
    
    Change-Id: I99972cad17197c21fe21c8dcd58e505317f4336d
    Signed-off-by: Ian-Y Chen <ian-y.chen@mediatek.com>
    CR-Id: ALPS03688158
    Feature: APP IOT(*)

--- drivers/misc/mediatek/include/mt-plat/eas_controller.h
+++ drivers/misc/mediatek/include/mt-plat/eas_controller.h
@@ -18,10 +18,11 @@
 #define CGROUP_RT 4
 #define NR_CGROUP 5
 
-#define EAS_KIR_PERF 0
-#define EAS_KIR_FBC 1
-#define EAS_KIR_BOOT 2
-#define EAS_MAX_KIR 3
+#define EAS_KIR_PERF  0
+#define EAS_KIR_FBC   1
+#define EAS_KIR_BOOT  2
+#define EAS_KIR_TOUCH 3
+#define EAS_MAX_KIR   4
 
 extern int boost_write_for_perf_idx(int, int);
 int update_eas_boost_value(int kicker, int cgroup_idx, int value);
