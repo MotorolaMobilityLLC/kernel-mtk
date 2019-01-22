39b7d58cbaffaf4a2a7769f16c9ef554db0e4a6f
Chinwen Chang <chinwen.chang@mediatek.com>
2019-01-22 20:56:15 +0800

    [ALPS02750725] zmc: add allocation priority to zmc driver
    This patch modified zmc driver with the following changes,
         1. priority check for allocation
         2. fix the computation of shrunk_size
         3. fullness support
    
    MTK-Commit-Id: 96a2bcdb9201f0f5afa29585f967c13aa1addf25
    
    Change-Id: Iecc34eda072a3da81804e779f21fe16e200f823f
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>
    CR-Id: ALPS02750725
    Feature: PASR (Partial Array Self-Refresh)

--- drivers/misc/mediatek/zone_movable_cma/single_cma.h
+++ drivers/misc/mediatek/zone_movable_cma/single_cma.h
@@ -15,6 +15,6 @@
 
 
 phys_addr_t zmc_base(void);
-struct page *zmc_cma_alloc(struct cma *cma, int count, unsigned int align);
+struct page *zmc_cma_alloc(struct cma *cma, int count, unsigned int align, struct single_cma_registration *p);
 bool zmc_cma_release(struct cma *cma, struct page *pages, int count);
 #endif
