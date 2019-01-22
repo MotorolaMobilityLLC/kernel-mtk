44de376d116e988661238c8a7d47db7759e00c4c
Josh-YC Liu <josh-yc.liu@mediatek.com>
2019-01-23 00:06:10 +0800

    [ALPS03899206] PWM: Add Reset HW register
    Due to some SW flow, it will cause some HW situtation.
    Add reset DISP_PWM HW register when enable/disable DISP_PWM.
    
    MTK-Commit-Id: 9067851eef645ef37ee9b292dbe87baaeb47c436
    
    Change-Id: I2fb9ca34c866efd3db6d2194efd7d356e9861220
    Signed-off-by: Josh-YC Liu <josh-yc.liu@mediatek.com>
    CR-Id: ALPS03899206
    Feature: Ambient Light Adaptive Luma

--- drivers/misc/mediatek/video/common/pwm10/ddp_pwm.c
+++ drivers/misc/mediatek/video/common/pwm10/ddp_pwm.c
@@ -171,7 +171,10 @@ static void disp_pwm_backlight_status(enum disp_pwm_id_t id, unsigned int is_pow
 
 	if (g_pwm_led_mode == MT65XX_LED_MODE_CUST_BLS_PWM) {
 		/* Read PWM value from register */
-		high_width = DISP_REG_GET(reg_base + DISP_PWM_CON_1_OFF) >> 16;
+		if (DISP_REG_GET(reg_base + DISP_PWM_EN_OFF) > 0)
+			high_width = DISP_REG_GET(reg_base + DISP_PWM_CON_1_OFF) >> 16;
+		else
+			high_width = 0;
 	} else {
 		/* Set dummy backlight value */
 		if (is_power_on == true)
@@ -184,13 +187,13 @@ static void disp_pwm_backlight_status(enum disp_pwm_id_t id, unsigned int is_pow
 		PWM_NOTICE("backlight is on (%d), ddp_pwm power:(%d), pwm id: (%d)",
 			high_width, is_power_on, index);
 		/* Change status when backlight turns on */
-		atomic_set(&g_pwm_is_power_on[index], is_power_on);
+		atomic_set(&g_pwm_is_power_on[index], 1);
 	} else if (is_power_on == false) {
 		PWM_NOTICE("backlight is off, ddp_pwm power:(%d), pwm id: (%d)",
 			is_power_on, index);
 		/* Save vlaue before clock off */
 		atomic_set(&g_pwm_value_before_power_off[index], high_width);
-		atomic_set(&g_pwm_is_power_on[index], is_power_on);
+		atomic_set(&g_pwm_is_power_on[index], 0);
 	}
 #endif
 }
@@ -204,10 +207,13 @@ static void disp_pwm_query_backlight(char *debug_output)
 	int index = index_of_pwm(DISP_PWM0);
 	unsigned int high_width;
 
-	if (atomic_read(&g_pwm_is_power_on[index]) != 1) {
+	if (atomic_read(&g_pwm_is_power_on[index]) == 1) {
 		if (g_pwm_led_mode == MT65XX_LED_MODE_CUST_BLS_PWM) {
 			/* Read PWM value from register */
-			high_width = DISP_REG_GET(reg_base + DISP_PWM_CON_1_OFF) >> 16;
+			if (DISP_REG_GET(reg_base + DISP_PWM_EN_OFF) > 0)
+				high_width = DISP_REG_GET(reg_base + DISP_PWM_CON_1_OFF) >> 16;
+			else
+				high_width = 0;
 		} else {
 			/* Set dummy backlight value */
 			high_width = 1023;
@@ -549,12 +555,17 @@ int disp_pwm_set_backlight_cmdq(enum disp_pwm_id_t id, int level_1024, void *cmd
 		level_1024 = disp_pwm_level_remap(id, level_1024);
 
 		reg_base = pwm_get_reg_base(id);
-		DISP_REG_MASK(cmdq, reg_base + DISP_PWM_CON_1_OFF, level_1024 << 16, 0x1fff << 16);
 
-		if (level_1024 > 0)
+		if (level_1024 > 0) {
+			DISP_REG_MASK(cmdq, reg_base + DISP_PWM_CON_1_OFF, level_1024 << 16, 0x1fff << 16);
+
 			disp_pwm_set_enabled(cmdq, id, 1);
-		else
+		} else {
+			/* Avoid to set 0 */
+			DISP_REG_MASK(cmdq, reg_base + DISP_PWM_CON_1_OFF, 1 << 16, 0x1fff << 16);
+
 			disp_pwm_set_enabled(cmdq, id, 0);	/* To save power */
+		}
 
 		DISP_REG_MASK(cmdq, reg_base + DISP_PWM_COMMIT_OFF, 1, ~0);
 		DISP_REG_MASK(cmdq, reg_base + DISP_PWM_COMMIT_OFF, 0, ~0);
