1f9337684987d5ca62e56e1d06eae92a6c3a8d6d
Wayne-TW <wayne-tw.chen@mediatek.com>
2019-01-22 23:05:30 +0800

    [ALPS03513502] wlan: kernel modules decoupling
    [Detail]
    To reduce the effort of patch sync
    
    [Solution]
    1.Used compile option to differentiate kernel version
    2.Build wlan_drv & wlan chardev as a kernel module (ko)
    
    MTK-Commit-Id: 6a25f14ecb6f1c643a8184ff04f708f81650b719
    
    Change-Id: Ifc3e27b2a0efcb2d2bf832aa335af563c466ae9d
    CR-Id: ALPS03513502
    Feature: Wifi Driver
    Signed-off-by: Wayne-TW Chen <wayne-tw.chen@mediatek.com>

--- drivers/misc/mediatek/connectivity/common/common_main/core/wmt_lib.c
+++ drivers/misc/mediatek/connectivity/common/common_main/core/wmt_lib.c
@@ -2005,6 +2005,7 @@ UINT8 *wmt_lib_get_fwinfor_from_emi(UINT8 section, UINT32 offset, UINT8 *buf, UI
 
 	return 0;
 }
+EXPORT_SYMBOL(wmt_lib_get_fwinfor_from_emi);
 
 INT32 wmt_lib_merge_if_flag_ctrl(UINT32 enable)
 {
---
+++ drivers/misc/mediatek/connectivity/common/connectivity_build_in_adapter.c
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#ifdef DFT_TAG
+#undef DFT_TAG
+#endif
+#define DFT_TAG "[WMT-CONSYS-HW]"
+
+
+#include <linux/version.h>
+#include <linux/types.h>
+#include <linux/printk.h>
+#include <linux/export.h>
+
+/*device tree mode*/
+#ifdef CONFIG_OF
+#include <linux/of.h>
+#include <linux/of_irq.h>
+#include <linux/irqreturn.h>
+#include <linux/of_address.h>
+#endif
+
+#include <linux/platform_device.h>
+#include <linux/pm_runtime.h>
+#include <linux/of_reserved_mem.h>
+
+#include "connectivity_build_in_adapter.h"
+#include "mtk_spm_resource_req.h"
+
+#ifdef CONFIG_ARCH_MT6570
+#define CPU_BOOST y
+#endif
+#ifdef CONFIG_ARCH_MT6755
+#define CPU_BOOST y
+#endif
+#ifdef CONFIG_MACH_MT6757
+#define CPU_BOOST y
+#endif
+#ifdef CONFIG_MACH_MT6763
+#define CPU_BOOST y
+#endif
+
+#ifdef CPU_BOOST
+#include "mach/mtk_ppm_api.h"
+#endif
+
+#if 0
+
+phys_addr_t gConEmiPhyBase;
+EXPORT_SYMBOL(gConEmiPhyBase);
+
+/*Reserved memory by device tree!*/
+
+int reserve_memory_consys_fn(struct reserved_mem *rmem)
+{
+	WMT_PLAT_WARN_FUNC(DFT_TAG "[W]%s: name: %s,base: 0x%llx,size: 0x%llx\n",
+		__func__, rmem->name, (unsigned long long)rmem->base,
+		(unsigned long long)rmem->size);
+	gConEmiPhyBase = rmem->base;
+	return 0;
+}
+
+RESERVEDMEM_OF_DECLARE(reserve_memory_test, "mediatek,consys-reserve-memory", reserve_memory_consys_fn);
+#endif
+
+void connectivity_export_show_stack(struct task_struct *tsk, unsigned long *sp)
+{
+	show_stack(tsk, sp);
+}
+EXPORT_SYMBOL(connectivity_export_show_stack);
+
+void connectivity_export_tracing_record_cmdline(struct task_struct *tsk)
+{
+	tracing_record_cmdline(tsk);
+}
+EXPORT_SYMBOL(connectivity_export_tracing_record_cmdline);
+
+#ifdef CPU_BOOST
+void connectivity_export_mt_ppm_sysboost_freq(enum ppm_sysboost_user user, unsigned int freq)
+{
+	mt_ppm_sysboost_freq(user, freq);
+}
+EXPORT_SYMBOL(connectivity_export_mt_ppm_sysboost_freq);
+
+void connectivity_export_mt_ppm_sysboost_core(enum ppm_sysboost_user user, unsigned int core_num)
+{
+	mt_ppm_sysboost_core(user, core_num);
+}
+EXPORT_SYMBOL(connectivity_export_mt_ppm_sysboost_core);
+
+void KERNEL_spm_resource_req(unsigned int user, unsigned int req_mask)
+{
+	spm_resource_req(user, req_mask);
+}
+EXPORT_SYMBOL(KERNEL_spm_resource_req);
+
+void KERNEL_mt_ppm_sysboost_set_core_limit(enum ppm_sysboost_user user, unsigned int cluster,
+					int min_core, int max_core)
+{
+	mt_ppm_sysboost_set_core_limit(user, cluster, min_core, max_core);
+}
+EXPORT_SYMBOL(KERNEL_mt_ppm_sysboost_set_core_limit);
+#endif
