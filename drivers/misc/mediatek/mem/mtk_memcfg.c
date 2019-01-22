e3830a044f4afb203e64decfbfda41a491b15c42
Yun Hsiang <yun.hsiang@mediatek.com>
2019-01-22 20:50:22 +0800

    [ALPS02757360] memcfg: remove unused information.
    [Detail]
    Remove unused information for reducing buffer usage.
    
    MTK-Commit-Id: ac65bca6a21e748242b631f1fa40b59907500fda
    
    Change-Id: Id215f306615a99eb963b5db64924f3c544f81fd9
    Signed-off-by: Yun Hsiang <yun.hsiang@mediatek.com>
    CR-Id: ALPS02757360
    Feature: 4GB_DRAM

--- drivers/misc/mediatek/mem/mtk_memcfg.c
+++ drivers/misc/mediatek/mem/mtk_memcfg.c
@@ -664,14 +664,6 @@ static int dt_scan_memory(unsigned long node, const char *uname,
 			"orig_dram_info", NULL);
 	if (dram_info) {
 		for (i = 0; i < dram_info->rank_num; i++) {
-			MTK_MEMCFG_LOG_AND_PRINTK(
-				"[debug]orig_dram rank[%d]   :   0x%08llx - 0x%08llx (0x%llx)\n",
-				i,
-				dram_info->rank_info[i].start,
-				dram_info->rank_info[i].start +
-				dram_info->rank_info[i].size - 1,
-				dram_info->rank_info[i].size
-				);
 			phone_dram_sz += dram_info->rank_info[i].size;
 		}
 	}
@@ -681,15 +673,6 @@ static int dt_scan_memory(unsigned long node, const char *uname,
 			"mblock_info", NULL);
 	if (mblock_info) {
 		for (i = 0; i < mblock_info->mblock_num; i++) {
-			MTK_MEMCFG_LOG_AND_PRINTK(
-				"[debug]mblock[%d][r%d]  :   0x%08llx - 0x%08llx (0x%llx)\n",
-				i,
-				mblock_info->mblock[i].rank,
-				mblock_info->mblock[i].start,
-				mblock_info->mblock[i].start +
-				mblock_info->mblock[i].size - 1,
-				mblock_info->mblock[i].size
-				);
 			dram_sz += mblock_info->mblock[i].size;
 		}
 	}
