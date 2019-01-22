2e10c15abae2cac3d2736f474da37e305265be64
hongxu.zhao <hongxu.zhao@mediatek.com>
2019-01-23 00:07:37 +0800

    [ALPS03957495] sensor: fix snprintf usage bug
    [Detail]
    fix snprintf usage bug
    
    MTK-Commit-Id: 79f22c5c01f0b679e164db0588ddb700bdb19b7b
    
    Change-Id: Ib0a0d790e8af4ad2070e38125d851602a6d1c772
    CR-Id: ALPS03957495
    Feature: Sensor Hub
    Signed-off-by: hongxu.zhao <hongxu.zhao@mediatek.com>

--- drivers/misc/mediatek/sensors-1.0/step_counter/stepsignhub/stepsignhub.c
+++ drivers/misc/mediatek/sensors-1.0/step_counter/stepsignhub/stepsignhub.c
@@ -39,7 +39,7 @@ struct step_chub_ipi_data {
 static struct step_chub_ipi_data obj_ipi_data;
 static ssize_t show_step_cds_value(struct device_driver *ddri, char *buf)
 {
-	return snprintf(buf, PAGE_SIZE, "%s\n", buf);
+	return snprintf(buf, PAGE_SIZE, "%d\n", 0);
 }
 
 static ssize_t store_trace_value(struct device_driver *ddri, const char *buf, size_t count)
