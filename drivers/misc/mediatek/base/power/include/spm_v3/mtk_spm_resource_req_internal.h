f4d91173f1914dc82c3414e7477053da4a49299f
Aaron Tian <aaron.tian@mediatek.com>
2019-01-22 21:17:29 +0800

    [ALPS02837889] SPM: add resource request API in spm_v3
    [Detail]
    Subsystem drivers can request SPM that system resource have to used.
    (e.g., DRAM, 26M clock)
    Thus, SPM will NOT power OFF the related resources
    
    [Solution]
    Add resource request API used for subsystem
    
    MTK-Commit-Id: d8a268729f32cfff275333623a60e8e8999a3714
    
    Change-Id: Ifd70e744ed6a149dc0122a37d98cfcd812a27832
    Signed-off-by: Aaron Tian <aaron.tian@mediatek.com>
    CR-Id: ALPS02837889
    Feature: Deep Idle

---
+++ drivers/misc/mediatek/base/power/include/spm_v3/mtk_spm_resource_req_internal.h
@@ -0,0 +1,9 @@
+#ifndef __MTK_SPM_RESOURCE_REQ_INTERNAL_H__
+#define __MTK_SPM_RESOURCE_REQ_INTERNAL_H__
+
+/* SPM resource request APIs: for internal use */
+
+void spm_resource_req_dump(void);
+unsigned int spm_get_resource_usage(void);
+
+#endif /* __MTK_SPM_RESOURCE_REQ_INTERNAL_H__ */
