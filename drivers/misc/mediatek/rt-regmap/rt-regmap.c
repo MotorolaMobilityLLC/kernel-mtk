3d6262c9301c04c75f6a4776bea575f61954ec17
Vend.Jeff.Chang <vend_jeff_chang001@mediatek.com>
2019-01-22 21:27:58 +0800

    [ALPS02797869] PMIC: rt-regmap remove filp_open
    [Detail]
    rt-regmap remove filp_open
    
    MTK-Commit-Id: 3d6707e2ceb524d2a8ddd44e68cf52017889eeed
    
    Change-Id: I8aea64a3b4b5994975f5d904bf061030aa19a94d
    Signed-off-by: Vend.Jeff.Chang <vend_jeff_chang001@mediatek.com>
    CR-Id: ALPS02797869
    Feature: Power Management

--- drivers/misc/mediatek/rt-regmap/rt-regmap.c
+++ drivers/misc/mediatek/rt-regmap/rt-regmap.c
@@ -1209,51 +1209,12 @@ static void rt_regmap_cache_release(struct rt_regmap_device *rd)
 }
 
 #ifdef CONFIG_DEBUG_FS
-static void rt_check_dump_config_file(struct rt_regmap_device *rd,
-				long int *reg_dump, int *cnt, char *type)
-{
-	char *token, *buf, *tmp_type;
-	char PATH[64];
-	mm_segment_t fs;
-	struct file *fp;
-	int ret, tmp_cnt = 0;
-
-	buf = devm_kzalloc(&rd->dev, 64*sizeof(char), GFP_KERNEL);
-	snprintf(PATH, 64, "/sdcard/%s_dump_config.txt", rd->props.name);
-	fp = filp_open(PATH, O_RDONLY, 0);
-	if (IS_ERR(fp)) {
-		pr_info("There is no Dump config file in sdcard\n");
-		devm_kfree(&rd->dev, buf);
-	} else {
-		fs = get_fs();
-		set_fs(get_ds());
-		fp->f_op->read(fp, buf, 64, &fp->f_pos);
-		set_fs(fs);
-
-		tmp_type = token = strsep(&buf, " ");
-		token = strsep(&buf, " ");
-		while (token != NULL) {
-			ret = kstrtoul(token, 16, &reg_dump[tmp_cnt]);
-			if (ret == 0)
-				tmp_cnt++;
-			token = strsep(&buf, " ");
-		}
-		filp_close(fp, NULL);
-		*cnt = tmp_cnt;
-		memcpy(type, tmp_type, 16);
-		devm_kfree(&rd->dev, buf);
-	}
-}
-
 static void rt_show_regs(struct rt_regmap_device *rd, struct seq_file *seq_file)
 {
-	int i = 0, k = 0, ret, count = 0, cnt = 0;
+	int i = 0, k = 0, ret, count = 0;
 	unsigned char regval[512];
-	long int reg_dump[64] = {0};
 	const rt_register_map_t *rm = rd->props.rm;
-	char type[16];
 
-	rt_check_dump_config_file(rd, reg_dump, &cnt, type);
 	down(&rd->semaphore);
 	for (i = 0; i < rd->props.register_num; i++) {
 		ret = rd->regmap_ops.regmap_block_read(rd, rm[i]->addr,
