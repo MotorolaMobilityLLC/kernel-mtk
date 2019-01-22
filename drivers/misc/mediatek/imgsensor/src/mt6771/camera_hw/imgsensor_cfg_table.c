0c95628731d5520dfe67d0cbbba127e6510c1bed
John Wei <john.wei@mediatek.com>
2019-01-23 00:04:33 +0800

    [ALPS03651850] cam: add s5k3p8sx
    [Detail]
    
    [Solution]
    
    MTK-Commit-Id: 80a46b4092f5f038fd77613495198f9ae2997c84
    
    Change-Id: I483186eafd66c15effc587decba3dac721d2d27b
    Signed-off-by: John Wei <john.wei@mediatek.com>
    CR-Id: ALPS03651850
    Feature: Camera Feature

--- drivers/misc/mediatek/imgsensor/src/mt6771/camera_hw/imgsensor_cfg_table.c
+++ drivers/misc/mediatek/imgsensor/src/mt6771/camera_hw/imgsensor_cfg_table.c
@@ -257,6 +257,22 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
 		},
 	},
 #endif
+#if defined(S5K3P8SX_MIPI_RAW)
+	{
+		SENSOR_DRVNAME_S5K3P8SX_MIPI_RAW,
+		{
+			{PDN, Vol_Low, 0},
+			{RST, Vol_Low, 0},
+			{SensorMCLK, Vol_High, 1},
+			{DVDD, Vol_1000, 0},
+			{AVDD, Vol_2800, 0},
+			{DOVDD, Vol_1800, 0},
+			{AFVDD, Vol_2800, 5},
+			{PDN, Vol_High, 0},
+			{RST, Vol_High, 2},
+		},
+	},
+#endif
 #if defined(S5K3M2_MIPI_RAW)
 	{
 		SENSOR_DRVNAME_S5K3M2_MIPI_RAW,
