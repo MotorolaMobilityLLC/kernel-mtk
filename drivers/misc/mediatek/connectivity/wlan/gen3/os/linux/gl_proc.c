5111285105d3dc61293af79ca1d78de6519b2f39
ziming sun <ziming.sun@mediatek.com>
2019-01-22 22:34:58 +0800

    [ALPS03094500] wlan: patch back from whitney
    [Detail]
    ALPS03094500 CL:658813
    ALPS03108008 CL:667051
    ALPS02907848 CL:603076
    
    MTK-Commit-Id: 418f7eaaa0a5f2010f60398599ed062f7824598d
    
    Change-Id: Ia439d4e08354a1da3f57f9af9f2a6fc30cdb6cdf
    Signed-off-by: ziming sun <ziming.sun@mediatek.com>
    CR-Id: ALPS03094500
    Feature: 2G/3G/4G R12

--- drivers/misc/mediatek/connectivity/wlan/gen3/os/linux/gl_proc.c
+++ drivers/misc/mediatek/connectivity/wlan/gen3/os/linux/gl_proc.c
@@ -59,6 +59,7 @@
 #define PROC_RX_STATISTICS                      "rx_statistics"
 #define PROC_TX_STATISTICS                      "tx_statistics"
 #define PROC_DBG_LEVEL_NAME                     "dbgLevel"
+#define PROC_SET_CAM				"setCAM"
 
 #define PROC_MCR_ACCESS_MAX_USER_INPUT_LEN      20
 #define PROC_RX_STATISTICS_MAX_USER_INPUT_LEN   10
@@ -191,6 +192,59 @@ static const struct file_operations dbglevel_ops = {
 	.owner = THIS_MODULE,
 	.read = procDbgLevelRead,
 	.write = procDbgLevelWrite,
+};
+
+static ssize_t procSetCamCfgWrite(struct file *file, const char __user *buffer, size_t count, loff_t *data)
+{
+#define MODULE_NAME_LEN_1 5
+
+	UINT_32 u4CopySize = sizeof(aucProcBuf);
+	UINT_8 *temp = &aucProcBuf[0];
+	BOOLEAN fgSetCamCfg = FALSE;
+	UINT_8 aucModule[MODULE_NAME_LEN_1];
+	UINT_32 u4Enabled;
+	UINT_8 aucModuleArray[MODULE_NAME_LEN_1] = "CAM";
+	BOOLEAN fgParamValue = TRUE;
+
+	kalMemSet(aucProcBuf, 0, u4CopySize);
+	if (u4CopySize >= count + 1)
+		u4CopySize = count;
+
+	if (copy_from_user(aucProcBuf, buffer, u4CopySize)) {
+		pr_err("error of copy from user\n");
+		return -EFAULT;
+	}
+	aucProcBuf[u4CopySize] = '\0';
+	temp = &aucProcBuf[0];
+	while (temp) {
+		/* pick up a string and teminated after meet : */
+		if (sscanf(temp, "%s %d", aucModule, &u4Enabled) != 2)  {
+			pr_info("read param fail, aucModule=%s\n", aucModule);
+			fgParamValue = FALSE;
+			break;
+		}
+
+		if (kalStrnCmp(aucModule, aucModuleArray, MODULE_NAME_LEN_1) == 0) {
+			if (u4Enabled)
+				fgSetCamCfg = TRUE;
+			else
+				fgSetCamCfg = FALSE;
+		}
+		temp = kalStrChr(temp, ',');
+		if (!temp)
+			break;
+		temp++; /* skip ',' */
+	}
+
+	if (fgParamValue)
+		nicConfigProcSetCamCfgWrite(fgSetCamCfg);
+
+	return count;
+}
+
+static const struct file_operations proc_set_cam_ops = {
+	.owner = THIS_MODULE,
+	.write = procSetCamCfgWrite,
 };
 
 /*******************************************************************************
@@ -496,6 +550,7 @@ INT_32 procUninitProcFs(VOID)
 INT_32 procRemoveProcfs(VOID)
 {
 	remove_proc_entry(PROC_MCR_ACCESS, gprProcRoot);
+	remove_proc_entry(PROC_SET_CAM, gprProcRoot);
 #if CFG_SUPPORT_DEBUG_FS
 	remove_proc_entry(PROC_ROAM_PARAM, gprProcRoot);
 	remove_proc_entry(PROC_COUNTRY, gprProcRoot);
@@ -516,6 +571,14 @@ INT_32 procCreateFsEntry(P_GLUE_INFO_T prGlueInfo)
 		DBGLOG(INIT, ERROR, "Unable to create /proc entry\n\r");
 		return -1;
 	}
+
+	prEntry = proc_create(PROC_SET_CAM, 0664, gprProcRoot, &proc_set_cam_ops);
+	if (prEntry == NULL) {
+		DBGLOG(INIT, ERROR, "Unable to create /proc entry SetCAM\n\r");
+		return -1;
+	}
+	proc_set_user(prEntry, KUIDT_INIT(PROC_UID_SHELL), KGIDT_INIT(PROC_GID_WIFI));
+
 #if CFG_SUPPORT_DEBUG_FS
 	prEntry = proc_create(PROC_ROAM_PARAM, 0664, gprProcRoot, &roam_ops);
 	if (prEntry == NULL) {
