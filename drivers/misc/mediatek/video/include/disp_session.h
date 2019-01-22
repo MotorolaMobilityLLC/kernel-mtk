20cad28177ea9830b6553a47e99d7d47fcacfd34
mtk13530 <elon.hsu@mediatek.com>
2019-01-23 00:03:03 +0800

    [ALPS03595015] disp: add IOCTL for display self refresh
    [Detail]
    Add IOCTL for display self refresh
    
    MTK-Commit-Id: 4a5420a5b213bf1ca1a7ed5f986ed5ac300a57ea
    
    Change-Id: I70072e12e97bb4e63a8c7a4ee613d84bd801aec3
    Signed-off-by: mtk13530 <elon.hsu@mediatek.com>
    CR-Id: ALPS03595015
    Feature: MT6771 Platform Feature

--- drivers/misc/mediatek/video/include/disp_session.h
+++ drivers/misc/mediatek/video/include/disp_session.h
@@ -468,6 +468,13 @@ enum DISP_UT_ERROR {
 	DISP_UT_ERROR_RDMA = 0x00000004,
 	DISP_UT_ERROR_CMDQ_TIMEOUT = 0x00000008,
 };
+
+enum DISP_SELF_REFRESH_TYPE {
+	REFRESH_FOR_ANTI_LATENCY2 = 0,
+	REFRESH_FOR_SWITCH_DECOUPLE,
+	REFRESH_FOR_SWITCH_DECOUPLE_MIRROR,
+};
+
 /* IOCTL commands. */
 #define DISP_IOW(num, dtype)     _IOW('O', num, dtype)
 #define DISP_IOR(num, dtype)     _IOR('O', num, dtype)
@@ -503,7 +510,8 @@ enum DISP_UT_ERROR {
 #define	DISP_IOCTL_SET_SCENARIO					DISP_IOW(223, struct disp_scenario_config_t)
 #define	DISP_IOCTL_WAIT_ALL_JOBS_DONE				DISP_IOW(224, unsigned int)
 #define	DISP_IOCTL_SCREEN_FREEZE				DISP_IOW(225, unsigned int)
-#define DISP_IOCTL_GET_UT_RESULT			DISP_IOW(226, unsigned int)
+#define DISP_IOCTL_GET_UT_RESULT				DISP_IOW(226, unsigned int)
+#define DISP_IOCTL_WAIT_DISP_SELF_REFRESH			DISP_IOW(227, unsigned int)
 #ifdef __KERNEL__
 
 int disp_mgr_get_session_info(struct disp_session_info *info);
