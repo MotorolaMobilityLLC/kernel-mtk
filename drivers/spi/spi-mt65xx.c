e29506fdb4e533c209514b842e291b6fc95ee8d6
Freddy Hsin <freddy.hsin@mediatek.com>
2019-01-23 00:01:12 +0800

    [ALPS03524111] spi: add basic spi driver for mt6771
    [Detail]
    Modify and enable clock sources for each SPI channel in dts
    Enable upstream SPI driver in kernel config
    Add MT6771 of_device_id in driver source
    
    MTK-Commit-Id: 248088b833b5ad3db96149b2a151ac03fabd8b98
    
    Change-Id: I0cbc2b2effe6b7270892bd5aa0af090c754dc0b5
    Signed-off-by: Freddy Hsin <freddy.hsin@mediatek.com>
    CR-Id: ALPS03524111
    Feature: [Android Default] CapTouch

--- drivers/spi/spi-mt65xx.c
+++ drivers/spi/spi-mt65xx.c
@@ -128,6 +128,11 @@ static const struct mtk_spi_compatible mt6775_compat = {
 	.adjust_reg = true,
 	.dma_8gb_v1 = true,
 };
+static const struct mtk_spi_compatible mt6771_compat = {
+	.need_pad_sel = true,
+	.adjust_reg = true,
+	.dma_8gb_v2 = true,
+};
 static const struct mtk_spi_compatible mt6739_compat = {
 	.need_pad_sel = true,
 	.adjust_reg = true,
@@ -161,6 +166,9 @@ static const struct of_device_id mtk_spi_of_match[] = {
 	},
 	{ .compatible = "mediatek,mt6775-spi",
 		.data = (void *)&mt6775_compat,
+	},
+	{ .compatible = "mediatek,mt6771-spi",
+		.data = (void *)&mt6771_compat,
 	},
 	{ .compatible = "mediatek,mt8135-spi",
 		.data = (void *)&mtk_common_compat,
