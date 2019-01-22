40b26c4ad7a92be7d64c0d868aafccdb67584496
YC <yu-chang.wang@mediatek.com>
2019-01-22 21:14:29 +0800

    [ALPS02681814] USB: add GPL
    [Detail]
    add Mediatek GPL
    
    MTK-Commit-Id: 4d19b8a1ae2b0045735c2aee9ae1355d09a192bb
    
    Change-Id: I6839165b184d38ba2ced41d4e26e67273d60a630
    Signed-off-by: YC <yu-chang.wang@mediatek.com>
    CR-Id: ALPS02681814
    Feature: UMS (Usb Mass Storage)

--- drivers/misc/mediatek/mu3d/drv/mtk_usb.c
+++ drivers/misc/mediatek/mu3d/drv/mtk_usb.c
@@ -1,12 +1,15 @@
 /*
- * MUSB OTG controller driver for Blackfin Processors
- *
- * Copyright 2006-2008 Analog Devices Inc.
- *
- * Enter bugs at http://blackfin.uclinux.org/
- *
- * Licensed under the GPL-2 or later.
- */
+* Copyright (C) 2016 MediaTek Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+*/
 
 #include <linux/module.h>
 #include <linux/kernel.h>
