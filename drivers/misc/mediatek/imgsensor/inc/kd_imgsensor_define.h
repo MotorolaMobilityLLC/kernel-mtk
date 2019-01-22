6d753340047eddf926ec189fbda8156a6845d00a
John.Wei <john.wei@mediatek.com>
2019-01-22 23:01:40 +0800

    [ALPS03249793] cam: stream on/off
    [Detail] for speed up camera lunch time
    
    [Solution]
    
    MTK-Commit-Id: 104273dc53e419ad104b6015f4c9e5bdbcf80364
    
    Change-Id: I63ee8f6a651a97c96a1c89428bb969490eb39bb6
    Signed-off-by: John.Wei <john.wei@mediatek.com>
    CR-Id: ALPS03249793
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/imgsensor/inc/kd_imgsensor_define.h
+++ drivers/misc/mediatek/imgsensor/inc/kd_imgsensor_define.h
@@ -488,6 +488,7 @@ typedef struct {
 	MUINT32 SensorMIPIDeskew;
 	MUINT16 SensorHorFOV;
 	MUINT16 SensorVerFOV;
+	MUINT16 SensorOrientation;
 } ACDK_SENSOR_INFO_STRUCT, *PACDK_SENSOR_INFO_STRUCT;
 
 typedef struct {
@@ -593,6 +594,7 @@ typedef struct {
 	MUINT32 SensorMIPIDeskew;
 	MUINT16 SensorHorFOV;
 	MUINT16 SensorVerFOV;
+	MUINT16 SensorOrientation;
 
 } ACDK_SENSOR_INFO2_STRUCT, *PACDK_SENSOR_INFO2_STRUCT;
 
