8dcdb32b698cb423531da71b707d8513d28e8951
Yun-Chen Hsieh <yun-chen.hsieh@mediatek.com>
2019-01-22 22:56:11 +0800

    [ALPS03139400] idle: fix build error in md2ap wakeup source
    [Detail]
    -. fix build error when ECCCI driver enabled
    -. update md2ap wakeup source naming for suspend and dpidle
    
    MTK-Commit-Id: 83d7aa5f4d66c0b1d1addba9318508b1a2f5c177
    
    Change-Id: I532ede83477f255a8f911e8dce684f6c89ee2e76
    Signed-off-by: Yun-Chen Hsieh <yun-chen.hsieh@mediatek.com>
    CR-Id: ALPS03139400
    Feature: Deep Idle
    (cherry picked from commit 8ac7f4500798bdaf8a43e10d116fbd1c157037de)

--- drivers/misc/mediatek/base/power/spm_v4/mtk_spm_sleep.c
+++ drivers/misc/mediatek/base/power/spm_v4/mtk_spm_sleep.c
@@ -419,7 +419,7 @@ static wake_reason_t spm_output_wake_reason(struct wake_status *wakesta, struct 
 
 #if !defined(CONFIG_FPGA_EARLY_PORTING)
 #ifdef CONFIG_MTK_ECCCI_DRIVER
-	if (wakesta->r12 & WAKE_SRC_R12_MD2AP_PEER_WAKEUP_EVENT)
+	if (wakesta->r12 & WAKE_SRC_R12_MD2AP_PEER_EVENT_B)
 		exec_ccci_kern_func_by_md_id(0, ID_GET_MD_WAKEUP_SRC, NULL, 0);
 	if (wakesta->r12 & WAKE_SRC_R12_CCIF1_EVENT_B)
 		exec_ccci_kern_func_by_md_id(2, ID_GET_MD_WAKEUP_SRC, NULL, 0);
