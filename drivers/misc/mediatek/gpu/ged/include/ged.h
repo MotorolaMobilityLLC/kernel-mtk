2d53484b03b18e4ae847e9b039dcf56cb12467ae
luise.chen <luise.chen@mediatek.com>
2019-01-22 22:15:01 +0800

    [ALPS02721089] GED_driver: API for setting target fps to the main renderer
    [Detail]
    int ged_set_target_fps(unsigned int target_fps, int mode)
    1. target_fps: FPS target
    2. mode: 0x1 for default, 0x2 for FRR mode, 0x4 for ARR mode
    3. ret: 1 for sucecess, 0 for failure
    [Solution]
    int ged_set_target_fps(unsigned int target_fps, int mode)
    1. target_fps: FPS target
    2. mode: 0x1 for default, 0x2 for FRR mode, 0x4 for ARR mode
    3. ret: 1 for sucecess, 0 for failure
    
    MTK-Commit-Id: c677fcafb41a94fccab9b8079d56bbfae5befaa2
    
    Change-Id: I88ec4a171e441384293a78283d9451ff49702c50
    CR-Id: ALPS02721089
    Feature: Gaming Low Power Mode
    Signed-off-by: luise.chen <luise.chen@mediatek.com>

--- drivers/misc/mediatek/gpu/ged/include/ged.h
+++ drivers/misc/mediatek/gpu/ged/include/ged.h
@@ -22,7 +22,7 @@ typedef enum GED_NOTIFICATION_TYPE_TAG {
 } GED_NOTIFICATION_TYPE;
 
 void ged_notification(GED_NOTIFICATION_TYPE eType);
-void ged_set_target_fps(unsigned int target_fps, int mode);
+int ged_set_target_fps(unsigned int target_fps, int mode);
 unsigned int ged_get_cur_fps(void);
 void ged_get_latest_perf_state(long long *t_cpu_remained,
 				long long *t_gpu_remained,
