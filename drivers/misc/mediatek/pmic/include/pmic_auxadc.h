62d20fc3a92d6e859997d24848eaca1bdebac0e9
Jeter Chen <jeter.chen@mediatek.com>
2019-01-22 22:57:36 +0800

    [ALPS02986467] PMIC: add pmic_auxadc callback for low power scenario
    [Detail]
    1. Add pmic_auxadc callback for suspend/SODI/deep idle
    and set mt6356 workaround setting.
    This setting will turn off BAT TEMP background detection in Low Power,
    Because VBIF28 will be disabled.
    2. Fix KCRS
    
    MTK-Commit-Id: d0a6dd50a4dd77480a8925d34b125b29add05205
    
    Change-Id: If2847f603f39a30f7c690b14755ae03aaebcd806
    Signed-off-by: Jeter Chen <jeter.chen@mediatek.com>
    CR-Id: ALPS02986467
    Feature: Power Management

---
+++ drivers/misc/mediatek/pmic/include/pmic_auxadc.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#ifndef _PMIC_AUXADC_H_
+#define _PMIC_AUXADC_H_
+
+extern void pmic_auxadc_suspend(void);
+extern void pmic_auxadc_resume(void);
+
+#endif				/* _PMIC_AUXADC_H_ */
