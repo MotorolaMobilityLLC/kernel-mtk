f84aa6a7ccca5171cbdd0e1d5c7c266e301ff607
Josepchi Tsai <josepchi.tsai@mediatek.com>
2019-01-23 00:05:31 +0800

    [ALPS03738776] Cam: Fix power on sequence
    [Detail]
    
    [Solution]
    
    MTK-Commit-Id: 7d613e90759bf2f6eed0126cf0ae5aa3cbe4af4d
    
    Change-Id: I493021835b84ab4d1d769058c88bb58049f84edf
    CR-Id: ALPS03738776
    Feature: Camera Application
    Signed-off-by: Josepchi Tsai <josepchi.tsai@mediatek.com>

--- drivers/misc/mediatek/imgsensor/src/mt6771/camera_hw/imgsensor_cfg_table.c
+++ drivers/misc/mediatek/imgsensor/src/mt6771/camera_hw/imgsensor_cfg_table.c
@@ -147,14 +147,14 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
 	{
 		SENSOR_DRVNAME_IMX350_MIPI_RAW,
 		{
-			{SensorMCLK, Vol_High, 0},
+			{PDN, Vol_Low, 0},
+			{RST, Vol_Low, 0},
 			{AVDD, Vol_2800, 0},
 			{DOVDD, Vol_1800, 0},
-			{DVDD, Vol_1100, 0},
-			{PDN, Vol_Low, 0},
+			{DVDD, Vol_1200, 5},
+			{SensorMCLK, Vol_High, 5},
 			{PDN, Vol_High, 0},
-			{RST, Vol_Low, 0},
-			{RST, Vol_High, 1}
+			{RST, Vol_High, 5}
 		},
 	},
 #endif
@@ -206,6 +206,22 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
 		},
 	},
 #endif
+#if defined(IMX386_MIPI_MONO)
+	{
+		SENSOR_DRVNAME_IMX386_MIPI_MONO,
+		{
+			{AVDD, Vol_2800, 0},
+			{DVDD, Vol_1100, 0},
+			{DOVDD, Vol_1800, 0},
+			{AFVDD, Vol_2800, 1},
+			{SensorMCLK, Vol_High, 1},
+			{PDN, Vol_Low, 0},
+			{PDN, Vol_High, 0},
+			{RST, Vol_Low, 0},
+			{RST, Vol_High, 5},
+		},
+	},
+#endif
 #if defined(IMX376_MIPI_RAW)
 	{
 		SENSOR_DRVNAME_IMX376_MIPI_RAW,
@@ -221,7 +237,6 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
 		},
 	},
 #endif
-
 #if defined(IMX338_MIPI_RAW)
 	{
 		SENSOR_DRVNAME_IMX338_MIPI_RAW,
