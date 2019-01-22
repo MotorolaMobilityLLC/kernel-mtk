1fbe04deccce2b57eb0fbc1a92f159d73cc8b314
Howard Hsu <howard.hsu@mediatek.com>
2019-01-22 20:52:33 +0800

    [ALPS02790149] VCORE DVFS: create vcorefs_v1
    [Detail] Create vcorefs_v1 folder for vcore dvfs
             manager/governor driver.
    
    [Solution] Create vcorefs_v1 for vcorefs dvfs manager
    
    MTK-Commit-Id: 63ceeaf7f215b0103d116e6c4ac3f1d3eeb86b12
    
    Change-Id: Idf490183ae53c524697b2c5a96fab2eee31df27f
    Signed-off-by: Howard Hsu <howard.hsu@mediatek.com>
    CR-Id: ALPS02790149
    Feature: VCORE DVFS

---
+++ drivers/misc/mediatek/base/power/include/mtk_vcorefs_governor.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _COMMON_MT_VCOREFS_GOVERNOR_H
+#define _COMMON_MT_VCOREFS_GOVERNOR_H
+
+#if defined(CONFIG_MACH_MT6757) || defined(CONFIG_MACH_KIBOPLUS)
+
+#include "vcorefs_v1/mtk_vcorefs_governor_mt6757.h"
+
+#endif
+
+#endif /* _COMMON_MT_VCOREFS_MANAGER_H */
+
