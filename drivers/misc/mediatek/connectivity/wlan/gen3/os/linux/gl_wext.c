d4acd036ff968aea7e3cb2f23f6308905bfcc4cc
Qiang guo <qiang.guo@mediatek.com>
2019-01-22 20:53:16 +0800

    [ALPS02596277] wlan: fix security Vulnerability
    [Detail]
    1.Wifi kernelspace do not use copyfromuser function to access the
    userspace memory.
    2.Copy from user do not check the length from userspace.
    3.Do not check the local buffer overflow.
    [Solution]
    1.Wifi kernelspace use copyfromuser function to access the userspace
    memory.
    2.Check the data length from userspace before using copyfromuser
    function
    3.Check the local buffer overflow.
    
    MTK-Commit-Id: 561ae1275a9c7cbebbb3757a21719fbd10fb7f1f
    
    Change-Id: I80e26f301bbaed53b825da2ae43c2536f7179473
    Signed-off-by: Qiang guo <qiang.guo@mediatek.com>
    CR-Id: ALPS02596277
    Feature: Wi-Fi Driver for MT6630

--- drivers/misc/mediatek/connectivity/wlan/gen3/os/linux/gl_wext.c
+++ drivers/misc/mediatek/connectivity/wlan/gen3/os/linux/gl_wext.c
@@ -59,7 +59,8 @@ const long channel_freq[] = {
 
 #define NUM_CHANNELS (ARRAY_SIZE(channel_freq))
 
-#define MAX_SSID_LEN    32
+#define MAX_SSID_LEN		32
+#define COUNTRY_CODE_LEN	10	/* country code length */
 
 /*******************************************************************************
  *                             D A T A   T Y P E S
@@ -1826,7 +1827,7 @@ wext_get_essid(IN struct net_device *prNetDev,
 
 	kalMemFree(prSsid, VIR_MEM_TYPE, sizeof(PARAM_SSID_T));
 
-	return 0;
+	return rStatus;
 }				/* wext_get_essid */
 
 #if 0
@@ -2876,22 +2877,24 @@ static int wext_set_country(IN struct net_device *prNetDev, IN struct iw_point *
 	P_GLUE_INFO_T prGlueInfo;
 	WLAN_STATUS rStatus;
 	UINT_32 u4BufLen;
-	UINT_8 aucCountry[2];
+	UINT_8 aucCountry[COUNTRY_CODE_LEN];
 
 	ASSERT(prNetDev);
 
 	/* prData->pointer should be like "COUNTRY US", "COUNTRY EU"
 	 * and "COUNTRY JP"
 	 */
-	if (GLUE_CHK_PR2(prNetDev, prData) == FALSE || !prData->pointer || prData->length < 10)
+	if (GLUE_CHK_PR2(prNetDev, prData) == FALSE || !prData->pointer || prData->length < COUNTRY_CODE_LEN)
 		return -EINVAL;
 
 	prGlueInfo = *((P_GLUE_INFO_T *) netdev_priv(prNetDev));
 
-	aucCountry[0] = *((PUINT_8)prData->pointer + 8);
-	aucCountry[1] = *((PUINT_8)prData->pointer + 9);
+	if (copy_from_user(aucCountry, prData->pointer, COUNTRY_CODE_LEN))
+		return -EFAULT;
 
-	rStatus = kalIoctl(prGlueInfo, wlanoidSetCountryCode, &aucCountry[0], 2, FALSE, FALSE, TRUE, &u4BufLen);
+	rStatus = kalIoctl(prGlueInfo,
+			   wlanoidSetCountryCode,
+			   &aucCountry[COUNTRY_CODE_LEN-2], 2, FALSE, FALSE, TRUE, &u4BufLen);
 	if (rStatus != WLAN_STATUS_SUCCESS) {
 		DBGLOG(REQ, ERROR, "Set country code error: %x\n", rStatus);
 		return -EFAULT;
@@ -3152,7 +3155,8 @@ int wext_support_ioctl(IN struct net_device *prDev, IN struct ifreq *prIfReq, IN
 
 #if 1
 	case SIOCSIWESSID:	/* 0x8B1A, set SSID (network name) */
-		if (iwr->u.essid.length > IW_ESSID_MAX_SIZE) {
+		u4ExtraSize = iwr->u.essid.length;
+		if (u4ExtraSize > IW_ESSID_MAX_SIZE) {
 			ret = -E2BIG;
 			break;
 		}
@@ -3167,7 +3171,7 @@ int wext_support_ioctl(IN struct net_device *prDev, IN struct ifreq *prIfReq, IN
 			break;
 		}
 
-		if (copy_from_user(prExtraBuf, iwr->u.essid.pointer, iwr->u.essid.length))
+		if (copy_from_user(prExtraBuf, iwr->u.essid.pointer, u4ExtraSize))
 			ret = -EFAULT;
 		else
 			ret = wext_set_essid(prDev, NULL, &iwr->u.essid, prExtraBuf);
@@ -3179,18 +3183,19 @@ int wext_support_ioctl(IN struct net_device *prDev, IN struct ifreq *prIfReq, IN
 #endif
 
 	case SIOCGIWESSID:	/* 0x8B1B, get SSID */
+		u4ExtraSize = iwr->u.essid.length;
 		if (!iwr->u.essid.pointer) {
 			ret = -EINVAL;
 			break;
 		}
 
-		if (iwr->u.essid.length < IW_ESSID_MAX_SIZE) {
-			DBGLOG(INIT, INFO, "[wifi] iwr->u.essid.length:%d too small\n", iwr->u.essid.length);
+		if (u4ExtraSize != IW_ESSID_MAX_SIZE && u4ExtraSize != IW_ESSID_MAX_SIZE + 1) {
+			DBGLOG(INIT, INFO, "[wifi] iwr->u.essid.length:%d error\n", iwr->u.essid.length);
 			ret = -E2BIG;	/* let caller try larger buffer */
 			break;
 		}
 
-		prExtraBuf = kalMemAlloc(IW_ESSID_MAX_SIZE, VIR_MEM_TYPE);
+		prExtraBuf = kalMemAlloc(IW_ESSID_MAX_SIZE + 1, VIR_MEM_TYPE);
 		if (!prExtraBuf) {
 			ret = -ENOMEM;
 			break;
@@ -3204,7 +3209,7 @@ int wext_support_ioctl(IN struct net_device *prDev, IN struct ifreq *prIfReq, IN
 				ret = -EFAULT;
 		}
 
-		kalMemFree(prExtraBuf, VIR_MEM_TYPE, IW_ESSID_MAX_SIZE);
+		kalMemFree(prExtraBuf, VIR_MEM_TYPE, IW_ESSID_MAX_SIZE + 1);
 		prExtraBuf = NULL;
 
 		break;
@@ -3247,22 +3252,22 @@ int wext_support_ioctl(IN struct net_device *prDev, IN struct ifreq *prIfReq, IN
 #if 1
 	case SIOCSIWENCODE:	/* 0x8B2A, set encoding token & mode */
 		/* Only DISABLED case has NULL pointer and length == 0 */
+		u4ExtraSize = iwr->u.encoding.length;
 		if (iwr->u.encoding.pointer) {
-			if (iwr->u.encoding.length > 16) {
+			if (u4ExtraSize > 16) {
 				ret = -E2BIG;
 				break;
 			}
 
-			u4ExtraSize = iwr->u.encoding.length;
 			prExtraBuf = kalMemAlloc(u4ExtraSize, VIR_MEM_TYPE);
 			if (!prExtraBuf) {
 				ret = -ENOMEM;
 				break;
 			}
 
-			if (copy_from_user(prExtraBuf, iwr->u.encoding.pointer, iwr->u.encoding.length))
+			if (copy_from_user(prExtraBuf, iwr->u.encoding.pointer, u4ExtraSize))
 				ret = -EFAULT;
-		} else if (iwr->u.encoding.length != 0) {
+		} else if (u4ExtraSize != 0) {
 			ret = -EINVAL;
 			break;
 		}
@@ -3294,22 +3299,22 @@ int wext_support_ioctl(IN struct net_device *prDev, IN struct ifreq *prIfReq, IN
 		if (iwr->u.data.pointer) {
 			P_GLUE_INFO_T prGlueInfo = *((P_GLUE_INFO_T *) netdev_priv(prDev));
 
+			u4ExtraSize = iwr->u.data.length;
 			if (1 /* wlanQueryWapiMode(prGlueInfo->prAdapter) */) {
 				/* Fixed length structure */
 #if CFG_SUPPORT_WAPI
-				if (iwr->u.data.length > 42 /* The max wapi ie buffer */) {
+				if (u4ExtraSize > 42 /* The max wapi ie buffer */) {
 					ret = -EINVAL;
 					break;
 				}
 #endif
-				u4ExtraSize = iwr->u.data.length;
 				if (u4ExtraSize) {
 					prExtraBuf = kalMemAlloc(u4ExtraSize, VIR_MEM_TYPE);
 					if (!prExtraBuf) {
 						ret = -ENOMEM;
 						break;
 					}
-					if (copy_from_user(prExtraBuf, iwr->u.data.pointer, iwr->u.data.length))
+					if (copy_from_user(prExtraBuf, iwr->u.data.pointer, u4ExtraSize))
 						ret = -EFAULT;
 					else
 						wext_support_ioctl_SIOCSIWGENIE(prGlueInfo, prExtraBuf, u4ExtraSize);
@@ -3333,13 +3338,18 @@ int wext_support_ioctl(IN struct net_device *prDev, IN struct ifreq *prIfReq, IN
 	case SIOCSIWENCODEEXT:	/* 0x8B34, set extended encoding token & mode */
 		if (iwr->u.encoding.pointer) {
 			u4ExtraSize = iwr->u.encoding.length;
+			if (u4ExtraSize > sizeof(struct iw_encode_ext)) {
+				ret = -EINVAL;
+				break;
+			}
+
 			prExtraBuf = kalMemAlloc(u4ExtraSize, VIR_MEM_TYPE);
 			if (!prExtraBuf) {
 				ret = -ENOMEM;
 				break;
 			}
 
-			if (copy_from_user(prExtraBuf, iwr->u.encoding.pointer, iwr->u.encoding.length))
+			if (copy_from_user(prExtraBuf, iwr->u.encoding.pointer, u4ExtraSize))
 				ret = -EFAULT;
 		} else if (iwr->u.encoding.length != 0) {
 			ret = -EINVAL;
--- drivers/misc/mediatek/connectivity/wlan/gen3/os/linux/gl_wext_priv.c
+++ drivers/misc/mediatek/connectivity/wlan/gen3/os/linux/gl_wext_priv.c
@@ -44,7 +44,9 @@
 *                              C O N S T A N T S
 ********************************************************************************
 */
-#define NUM_SUPPORTED_OIDS      (sizeof(arWlanOidReqTable) / sizeof(WLAN_REQ_ENTRY))
+#define NUM_SUPPORTED_OIDS	(sizeof(arWlanOidReqTable) / sizeof(WLAN_REQ_ENTRY))
+#define	CMD_OID_BUF_LENGTH	4096
+
 
 /*******************************************************************************
 *                  F U N C T I O N   D E C L A R A T I O N S
@@ -96,7 +98,7 @@ reqExtSetAcpiDevicePowerState(IN P_GLUE_INFO_T prGlueInfo,
 *                       P R I V A T E   D A T A
 ********************************************************************************
 */
-static UINT_8 aucOidBuf[4096] = { 0 };
+static UINT_8 aucOidBuf[CMD_OID_BUF_LENGTH] = { 0 };
 
 /* OID processing table */
 /*
@@ -1071,8 +1073,10 @@ int
 priv_set_ints(IN struct net_device *prNetDev,
 	      IN struct iw_request_info *prIwReqInfo, IN union iwreq_data *prIwReqData, IN char *pcExtra)
 {
-	UINT_32 u4SubCmd, u4BufLen;
+	UINT_16 i = 0;
+	UINT_32 u4SubCmd, u4BufLen, u4CmdLen;
 	P_GLUE_INFO_T prGlueInfo;
+	INT_32  setting[4] = {0};
 	int status = 0;
 	WLAN_STATUS rStatus = WLAN_STATUS_SUCCESS;
 	P_SET_TXPWR_CTRL_T prTxpwr;
@@ -1087,19 +1091,16 @@ priv_set_ints(IN struct net_device *prNetDev,
 	prGlueInfo = *((P_GLUE_INFO_T *) netdev_priv(prNetDev));
 
 	u4SubCmd = (UINT_32) prIwReqData->data.flags;
+	u4CmdLen = (UINT_32) prIwReqData->data.length;
 
 	switch (u4SubCmd) {
 	case PRIV_CMD_SET_TX_POWER:
 		{
-			INT_32 *setting = prIwReqData->data.pointer;
-			UINT_16 i;
+			if (u4CmdLen > 4)
+				return -EINVAL;
+			if (copy_from_user(setting, prIwReqData->data.pointer, u4CmdLen))
+				return -EFAULT;
 
-#if 0
-			DBGLOG(INIT, INFO, "Tx power num = %d\n", prIwReqData->data.length);
-
-			DBGLOG(INIT, INFO,
-				"Tx power setting = %d %d %d %d\n", setting[0], setting[1], setting[2], setting[3]);
-#endif
 			prTxpwr = &prGlueInfo->rTxPwr;
 			if (setting[0] == 0 && prIwReqData->data.length == 4 /* argc num */) {
 				/* 0 (All networks), 1 (legacy STA), 2 (Hotspot AP), 3 (P2P), 4 (BT over Wi-Fi) */
@@ -1242,7 +1243,6 @@ priv_set_struct(IN struct net_device *prNetDev,
 	/* WLAN_STATUS rStatus = WLAN_STATUS_SUCCESS; */
 	UINT_32 u4CmdLen = 0;
 	P_NDIS_TRANSPORT_STRUCT prNdisReq;
-	PUINT_32 pu4IntBuf = NULL;
 
 	P_GLUE_INFO_T prGlueInfo = NULL;
 	UINT_32 u4BufLen = 0;
@@ -1328,13 +1328,20 @@ priv_set_struct(IN struct net_device *prNetDev,
 	case PRIV_CMD_WSC_PROBE_REQ:
 		{
 			/* retrieve IE for Probe Request */
+			u4CmdLen = prIwReqData->data.length;
+			if (u4CmdLen > GLUE_INFO_WSCIE_LENGTH) {
+				DBGLOG(REQ, ERROR, "Input data length is invalid %u\n", u4CmdLen);
+				return -EINVAL;
+			}
+
 			if (prIwReqData->data.length > 0) {
-				if (copy_from_user(prGlueInfo->aucWSCIE, prIwReqData->data.pointer,
-						   prIwReqData->data.length)) {
+				if (copy_from_user(prGlueInfo->aucWSCIE,
+						   prIwReqData->data.pointer,
+						   u4CmdLen)) {
 					status = -EFAULT;
 					break;
 				}
-				prGlueInfo->u2WSCIELen = prIwReqData->data.length;
+				prGlueInfo->u2WSCIELen = u4CmdLen;
 			} else {
 				prGlueInfo->u2WSCIELen = 0;
 			}
@@ -1342,11 +1349,16 @@ priv_set_struct(IN struct net_device *prNetDev,
 		break;
 #endif
 	case PRIV_CMD_OID:
-		if (copy_from_user(&aucOidBuf[0], prIwReqData->data.pointer, prIwReqData->data.length)) {
+		u4CmdLen = prIwReqData->data.length;
+		if (u4CmdLen > CMD_OID_BUF_LENGTH) {
+			DBGLOG(REQ, ERROR, "Input data length is invalid %u\n", u4CmdLen);
+			return -EINVAL;
+		}
+		if (copy_from_user(&aucOidBuf[0], prIwReqData->data.pointer, u4CmdLen)) {
 			status = -EFAULT;
 			break;
 		}
-		if (!kalMemCmp(&aucOidBuf[0], pcExtra, prIwReqData->data.length)) {
+		if (!kalMemCmp(&aucOidBuf[0], pcExtra, u4CmdLen)) {
 			/* ToDo:: DBGLOG */
 			DBGLOG(REQ, INFO, "pcExtra buffer is valid\n");
 		} else {
@@ -1366,13 +1378,17 @@ priv_set_struct(IN struct net_device *prNetDev,
 		break;
 
 	case PRIV_CMD_SW_CTRL:
-		pu4IntBuf = (PUINT_32) prIwReqData->data.pointer;
+		u4CmdLen = prIwReqData->data.length;
 		prNdisReq = (P_NDIS_TRANSPORT_STRUCT) &aucOidBuf[0];
 
-		/* kalMemCopy(&prNdisReq->ndisOidContent[0], prIwReqData->data.pointer, 8); */
+		if (u4CmdLen > sizeof(prNdisReq->ndisOidContent)) {
+			DBGLOG(REQ, ERROR, "Input data length is invalid %u\n", u4CmdLen);
+			return -EINVAL;
+		}
+
 		if (copy_from_user(&prNdisReq->ndisOidContent[0],
 					prIwReqData->data.pointer,
-					prIwReqData->data.length)) {
+					u4CmdLen)) {
 			status = -EFAULT;
 			break;
 		}
