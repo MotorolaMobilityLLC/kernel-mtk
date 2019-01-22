0da29590c5613df4478794944a7a3de98bbc4416
chang hao chen <chang-hao.chen@mediatek.com>
2019-01-22 22:56:31 +0800

    [ALPS03075750] SPM: disable suspend
    [Detail] disable SPM suspend
    
    MTK-Commit-Id: 438c32d0fc947d54f66d89a9a230778cc4af93be
    
    Change-Id: I99f086f4bcead12cbf4192444af52102014dc9ef
    Signed-off-by: Chang-Hao Chen <Chang-Hao.Chen@mediatek.com>
    Feature: Power Management
    CR-Id: ALPS03075750

--- drivers/misc/mediatek/base/power/spm_v3/mtk_spm_init.c
+++ drivers/misc/mediatek/base/power/spm_v3/mtk_spm_init.c
@@ -27,7 +27,9 @@ static int __init mt_spm_init(void)
 #if !defined(CONFIG_FPGA_EARLY_PORTING)
 	mtk_cpuidle_init();
 	spm_module_init();
+#ifndef CONFIG_MACH_MT6759
 	slp_module_init();
+#endif
 #endif
 
 	spm_resource_req_init();
