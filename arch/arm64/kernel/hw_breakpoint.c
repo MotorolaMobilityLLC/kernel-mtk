0ea1a21edb4ca19acfdc919ab2f748c1259fed1e
mtk05337 <chieh-jay.liu@mediatek.com>
2019-01-22 20:48:39 +0800

    [ALPS02750637] kernel: add WP/BP driver
    [Detail]
    Enable WP/BP driver for enhancing debug utility
    
    [Solution]
    Enable WP/BP after fininsh the verfication
    
    MTK-Commit-Id: d9be04cf7583d02457a72fcf6362a553b6651e69
    
    Change-Id: I903f43a4bb0f856ebfe892438f51e17390713b20
    CR-Id: ALPS02750637
    Feature: Others
    Signed-off-by: mtk05337 <chieh-jay.liu@mediatek.com>

--- arch/arm64/kernel/hw_breakpoint.c
+++ arch/arm64/kernel/hw_breakpoint.c
@@ -851,6 +851,10 @@ void hw_breakpoint_thread_switch(struct task_struct *next)
  */
 static void hw_breakpoint_reset(void *unused)
 {
+#ifdef CONFIG_MEDIATEK_SOLUTION
+	/* mediatek will use our own operations for hw breakpoint/watchpoint */
+	return;
+#else
 	int i;
 	struct perf_event **slots;
 	/*
@@ -879,7 +883,7 @@ static void hw_breakpoint_reset(void *unused)
 			write_wb_reg(AARCH64_DBG_REG_WCR, i, 0UL);
 			write_wb_reg(AARCH64_DBG_REG_WVR, i, 0UL);
 		}
-	}
+#endif	}
 }
 
 static int hw_breakpoint_reset_notify(struct notifier_block *self,
