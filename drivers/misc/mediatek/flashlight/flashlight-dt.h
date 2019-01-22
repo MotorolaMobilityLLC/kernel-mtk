98ff58ab4f94b3f3a8ed560649bfe60f67c882fd
Simon.Wang <simon-tch.wang@mediatek.com>
2019-01-22 22:57:41 +0800

    [ALPS03138548] flashlight: update device tree.
    [Detail]
    1. Update flashlight device tree.
    
    [Solution]
    
    MTK-Commit-Id: 608146f4a36dc9967a3f8f5be31d3c7bebfd7e57
    
    Change-Id: Ib36d0b51e71dc3a1bbe9380b6bdb48ce9348f588
    Signed-off-by: Simon.Wang <simon-tch.wang@mediatek.com>
    CR-Id: ALPS03138548
    Feature: [Android Default] Flashlight

--- drivers/misc/mediatek/flashlight/flashlight-dt.h
+++ drivers/misc/mediatek/flashlight/flashlight-dt.h
@@ -14,14 +14,12 @@
 #ifndef _FLASHLIGHT_DT_H
 #define _FLASHLIGHT_DT_H
 
-#ifdef mt6757
+#define DUMMY_DTNAME      "mediatek,flashlights_dummy_gpio"
+#define DUMMY_DTNAME_I2C  "mediatek,flashlights_dummy_i2c"
 #define LM3643_DTNAME     "mediatek,flashlights_lm3643"
 #define LM3643_DTNAME_I2C "mediatek,strobe_main"
+#define RT4505_DTNAME_I2C "mediatek,flashlights_rt4505_i2c"
 #define RT5081_DTNAME     "mediatek,flashlights_rt5081"
-#endif
-
-#ifdef mt6799
 #define MT6336_DTNAME     "mediatek,flashlights_mt6336"
-#endif
 
 #endif /* _FLASHLIGHT_DT_H */
