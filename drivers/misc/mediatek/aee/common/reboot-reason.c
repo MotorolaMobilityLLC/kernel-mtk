b7a22c8f5e18fc4144d96ef064b7acb0ad9a56f4
Xinghua Yang <xinghua.yang@mediatek.com>
2019-01-22 22:56:52 +0800

    [ALPS03156845] aee: add timestamp information for nested panic
    [Detail]
    nested panic timestamp information is useful for analysis
    
    [Solution]
    add timestamp information for nested panic
    
    MTK-Commit-Id: e772beaddb688dc07cd332161372a4340e7494df
    
    Change-Id: I4f8d0a9092c85dbff31561caaf0a24a57207a20a
    Signed-off-by: Xinghua Yang <xinghua.yang@mediatek.com>
    CR-Id: ALPS03156845
    Feature: Android Exception Engine(AEE)

--- drivers/misc/mediatek/aee/common/reboot-reason.c
+++ drivers/misc/mediatek/aee/common/reboot-reason.c
@@ -337,6 +337,18 @@ __weak int aee_rr_curr_fiq_step(void)
 }
 #endif
 
+#define TS_MAX_LEN 64
+static const char *get_timestamp_string(char *buf, int bufsize)
+{
+	u64 ts;
+	unsigned long rem_nsec;
+
+	ts = local_clock();
+	rem_nsec = do_div(ts, 1000000000);
+	snprintf(buf, bufsize, "[%5lu.%06lu]",
+		       (unsigned long)ts, rem_nsec / 1000);
+	return buf;
+}
 asmlinkage void aee_stop_nested_panic(struct pt_regs *regs)
 {
 	struct thread_info *thread = current_thread_info();
@@ -351,6 +363,7 @@ asmlinkage void aee_stop_nested_panic(struct pt_regs *regs)
 	int prev_fiq_step = aee_rr_curr_fiq_step();
 	/* everytime enter nested_panic flow, add 8 */
 	static int step_base = -8;
+	char tsbuf[TS_MAX_LEN] = {0};
 
 	step_base = step_base < 48 ? step_base + 8 : 56;
 
@@ -361,7 +374,8 @@ asmlinkage void aee_stop_nested_panic(struct pt_regs *regs)
 	aee_rec_step_nested_panic(step_base + 2);
 	/*nested panic may happens more than once on many/single cpus */
 	if (atomic_read(&nested_panic_time) < 3)
-		aee_nested_printf("\nCPU%dpanic%d@%d\n", cpu, nested_panic_time, prev_fiq_step);
+		aee_nested_printf("\nCPU%dpanic%d@%d-%s\n", cpu, nested_panic_time, prev_fiq_step,
+				  get_timestamp_string(tsbuf, TS_MAX_LEN));
 	atomic_inc(&nested_panic_time);
 
 	switch (atomic_read(&nested_panic_time)) {
