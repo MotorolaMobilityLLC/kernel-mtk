f044388c4d1ee8e09aed97a3da20c914a213dadb
Andy <chia-hao.hsu@mediatek.com>
2019-01-23 00:08:24 +0800

    [ALPS03944023] Kernel: Remove unnecessary directive
    [Detail]
    Remove unnecessary directives
    
    MTK-Commit-Id: dd119464754121a71cc170cf55d8587d5f3011eb
    
    Change-Id: I92a4654d7895ca0c97a624e625105fc4c5fa2f44
    CR-Id: ALPS03944023
    Feature: Large DRAM
    Signed-off-by: Andy <chia-hao.hsu@mediatek.com>

--- scripts/drvgen/drvgen.mk
+++ scripts/drvgen/drvgen.mk
@@ -69,8 +69,6 @@ $(DRVGEN_FILE_LIST): $(DRVGEN_TOOL) $(DWS_FILE) $(DRVGEN_FIG) $(PROJ_DTS_FILES)
 		fi \
 	done
 
-ifeq ($(strip $(CONFIG_MTK_DTBO_FEATURE)), y)
-
 dtbo_check: $(MAIN_DTB_NAMES) $(PROJ_DTB_NAMES)
 	for i in $(PROJ_DTB_FILES); do \
 		$(srctree)/scripts/dtc/ufdt_apply_overlay $(MAIN_DTB_FILES) $$i $$i.merge;\
@@ -92,7 +90,5 @@ $(objtree)/dtboimg.cfg: FORCE
 	else \
 		rm $@.tmp; \
 	fi
-
-endif
 
 endif#MTK_PLATFORM
