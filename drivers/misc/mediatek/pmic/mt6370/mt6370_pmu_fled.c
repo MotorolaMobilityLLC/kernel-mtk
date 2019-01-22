0c10b2e50eee74976af32a5cf3c733659d537ad3
Wilma wu <wilma.wu@mediatek.com>
2019-01-23 00:09:03 +0800

    [ALPS03899386] pmic: fix mt6370/mt6371 flash led issue
    adjust delay for mode switching.
    
    MTK-Commit-Id: 9fadc75faa69f7b9d194248604c2cdbf1fd94912
    
    Change-Id: I40f437f3f7bc715774c94e0256313cee11a6973c
    Signed-off-by: Wilma wu <wilma.wu@mediatek.com>
    CR-Id: ALPS03899386
    Feature: Power Management

--- drivers/misc/mediatek/pmic/mt6370/mt6370_pmu_fled.c
+++ drivers/misc/mediatek/pmic/mt6370/mt6370_pmu_fled.c
@@ -289,11 +289,13 @@ static int mt6370_fled_set_mode(struct rt_fled_dev *info,
 			break;
 		ret |= mt6370_pmu_reg_clr_bit(fi->chip,
 			MT6370_PMU_REG_FLEDEN, MT6370_STROBE_EN_MASK);
-		ret |= mt6370_pmu_reg_set_bit(fi->chip,
-				MT6370_PMU_REG_FLEDEN, fi->id == MT6370_FLED1 ? 0x02 : 0x01);
+		udelay(500);
+		ret |= mt6370_pmu_reg_set_bit(fi->chip, MT6370_PMU_REG_FLEDEN,
+				fi->id == MT6370_FLED1 ? 0x02 : 0x01);
 		ret |= mt6370_pmu_reg_set_bit(fi->chip,
 				MT6370_PMU_REG_FLEDEN, MT6370_TORCH_EN_MASK);
-		dev_info(fi->dev, "set to torch mode\n");
+		udelay(500);
+		dev_info(fi->dev, "set to torch mode with 500 us delay\n");
 		mt6370_global_mode = mode;
 		if (fi->id == MT6370_FLED1)
 			mt6370_fled_on |= 1 << MT6370_FLED1;
@@ -304,12 +306,12 @@ static int mt6370_fled_set_mode(struct rt_fled_dev *info,
 		ret = mt6370_pmu_reg_clr_bit(fi->chip,
 			MT6370_PMU_REG_FLEDEN, MT6370_STROBE_EN_MASK);
 		udelay(400);
-		ret |= mt6370_pmu_reg_set_bit(fi->chip,
-			MT6370_PMU_REG_FLEDEN, fi->id == MT6370_FLED1 ? 0x02 : 0x01);
+		ret |= mt6370_pmu_reg_set_bit(fi->chip, MT6370_PMU_REG_FLEDEN,
+			fi->id == MT6370_FLED1 ? 0x02 : 0x01);
 		ret |= mt6370_pmu_reg_set_bit(fi->chip,
 			MT6370_PMU_REG_FLEDEN, MT6370_STROBE_EN_MASK);
-		udelay(400);
-		dev_info(fi->dev, "set to flash mode\n");
+		mdelay(5);
+		dev_info(fi->dev, "set to flash mode with 400/4500 us delay\n");
 		mt6370_global_mode = mode;
 		if (fi->id == MT6370_FLED1)
 			mt6370_fled_on |= 1 << MT6370_FLED1;
