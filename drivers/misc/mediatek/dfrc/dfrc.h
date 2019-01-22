990124ff7b4bc559432c62746e7d04eb1649706a
JihCheng Chiu <jih-cheng.chiu@mediatek.com>
2019-01-22 22:14:39 +0800

    [ALPS02877208] DFRC: adjusting the rule of setting FRR
    [Detail] Change the method of control FRR
    
    [Solution] FRR will query the setting from DFRC, so creating a table
               to save all setting.
    
    MTK-Commit-Id: 4d54bf4e16ac9c6716ef80fb6d39ca7c4673b644
    
    Change-Id: Ia28aa0e58e271be994199e2decb3ab541c824926
    Signed-off-by: JihCheng Chiu <jih-cheng.chiu@mediatek.com>
    CR-Id: ALPS02877208
    Feature: Display Quality Enhancement

--- drivers/misc/mediatek/dfrc/dfrc.h
+++ drivers/misc/mediatek/dfrc/dfrc.h
@@ -21,8 +21,14 @@
 #define DFRC_ALLOW_VIDEO 0x01
 #define DFRC_ALLOW_TOUCH 0x02
 
+#define DFRC_DRV_ALL_PID -1
+#define DFRC_DRV_ALL_CID 0
+
+#define DFRC_DRV_FPS_NON_ASSIGN 0
+
 int dfrc_allow_rrc_adjust_fps(void);
 
 long dfrc_set_kernel_policy(int api, int fps, int mode, int target_pid, unsigned long long gl_context_id);
+long dfrc_get_frr_setting(int pid, unsigned long long gl_context_id, int *fps, int *mode);
 
 #endif
