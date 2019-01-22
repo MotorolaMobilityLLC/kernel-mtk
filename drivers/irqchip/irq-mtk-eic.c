97d8e14f92c7ea9caa7c39fb5921fb61b1891138
Lovefool Tai <lovefool.tai@mediatek.com>
2019-01-22 20:08:31 +0800

    [ALPS02706484] eint: implement irq_disable
    [Detail] implemnet irq_disable in eint driver
    
    [Solution] implemnet irq_disable in eint driver
    
    MTK-Commit-Id: 76254cdb52036060a9df7950fec15ae37f9b02c2
    
    Change-Id: Iff7b369b962aea806769231955bb4b5d8fe5b380
    Signed-off-by: Lovefool Tai <lovefool.tai@mediatek.com>
    CR-Id: ALPS02706484
    Feature: eint

--- drivers/irqchip/irq-mtk-eic.c
+++ drivers/irqchip/irq-mtk-eic.c
@@ -1893,8 +1893,10 @@ static struct irq_chip mt_irq_eint = {
 	.name = "mt-eint",
 	.irq_mask = mt_eint_irq_mask,
 	.irq_unmask = mt_eint_irq_unmask,
+	.irq_disable = mt_eint_irq_mask,
 	.irq_ack = mt_eint_irq_ack,
 	.irq_set_type = mt_eint_irq_set_type,
+	.flags = IRQCHIP_SKIP_SET_WAKE,
 };
 
 int mt_eint_domain_xlate_onetwocell(struct irq_domain *d,
