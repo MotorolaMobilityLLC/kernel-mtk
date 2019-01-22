6bb914317471b351006e4ac37e38d5ef15066222
Adrian Huang <adrian.huang@mediatek.com>
2019-01-23 00:05:29 +0800

    [ALPS03738009] EEM/Picachu: Introduce Picachu module
    [Detail]
    Introduce the Picachu kernel module. This module retrieves
    the data from preloader Picachu, and applied the data to
    eem driver if needed.
    
    MTK-Commit-Id: 89a5e044b92ad426c070d5e48ccb4a10968cd0b7
    
    Change-Id: I1c878f854c5d9e568654eb5e15cc789733b81a7b
    Signed-off-by: Adrian Huang <adrian.huang@mediatek.com>
    CR-Id: ALPS03738009
    Feature: PTP-OD (Performance Thermal Power-Over Drive)

--- drivers/misc/mediatek/base/power/eem_v2/mt6771/inc/mtk_eem.h
+++ drivers/misc/mediatek/base/power/eem_v2/mt6771/inc/mtk_eem.h
@@ -204,6 +204,8 @@ extern int mt_eem_status(enum eem_det_id id);
 extern unsigned int get_efuse_status(void);
 extern unsigned int mt_eem_is_enabled(void);
 
+extern void eem_set_pi_efuse(enum eem_ctrl_id id, unsigned int pi_efuse);
+
 /* DRCC */
 extern unsigned int drcc_offset_done;
 #endif
