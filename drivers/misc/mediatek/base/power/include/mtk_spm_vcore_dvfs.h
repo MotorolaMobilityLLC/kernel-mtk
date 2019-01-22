b3e56db97cc380483e9af82ffb24c836c581a53a
Howard Hsu <howard.hsu@mediatek.com>
2019-01-22 20:51:11 +0800

    [ALPS02790149] VCORE DVFS: fix new plat compile error
    [Detail]fix compile error for new platform
    
    [Solution]fix compile error for new platform
    
    MTK-Commit-Id: 7cb41fcbe9db73ecfc72781a80aa43d98c7557ef
    
    Change-Id: Iba353091872c84f69064538c38a21d581311f472
    Signed-off-by: Howard Hsu <howard.hsu@mediatek.com>
    CR-Id: ALPS02790149
    Feature: VCORE DVFS

--- drivers/misc/mediatek/base/power/include/mtk_spm_vcore_dvfs.h
+++ drivers/misc/mediatek/base/power/include/mtk_spm_vcore_dvfs.h
@@ -18,7 +18,7 @@
 
 #include "spm_v2/mt_spm_vcore_dvfs_mt6755.h"
 
-#elif defined(CONFIG_MACH_MT6757)
+#elif defined(CONFIG_MACH_MT6757) || defined(CONFIG_MACH_KIBOPLUS)
 
 #include "spm_v2/mtk_spm_vcore_dvfs_mt6757.h"
 
