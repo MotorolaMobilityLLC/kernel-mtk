b5a89abefcf2c201546e2704dfc6ef0ac69ef45b
Johnson Lin <johnson.lin@mediatek.com>
2019-01-23 00:03:48 +0800

    [ALPS03638332] mrdump_key: add pmic control
    [Detail]
    SYSRST mode is supported by PMIC
    Its enabled in preloader before
    Moving it to kernel in mrdump_key init
    flow and control by DTS
    
    MTK-Commit-Id: 2a25abc8b180a36775dc5e616696a59ecff0f447
    
    Change-Id: Ic40348cb782670365396484fcad29e2bf0ffb28a
    Signed-off-by: Johnson Lin <johnson.lin@mediatek.com>
    CR-Id: ALPS03638332
    Feature: Memory RAM Dump (MRDUMP)

--- drivers/misc/mediatek/aee/mrdump/mrdump_key_setup.c
+++ drivers/misc/mediatek/aee/mrdump/mrdump_key_setup.c
@@ -18,6 +18,9 @@
 #endif
 #ifdef CONFIG_MTK_WD_KICKER
 #include <mach/wd_api.h>
+#ifdef CONFIG_MTK_PMIC_COMMON
+#include <mt-plat/upmu_common.h>
+#endif
 
 enum MRDUMP_RST_SOURCE {
 	MRDUMP_SYSRST,
@@ -26,12 +29,22 @@ enum MRDUMP_NOTIFY_MODE {
 	MRDUMP_IRQ,
 	MRDUMP_RST};
 
+#ifdef CONFIG_MTK_PMIC_COMMON
+enum MRDUMP_LONG_PRESS_MODE {
+	LONG_PRESS_NONE,
+	LONG_PRESS_SHUTDOWN};
+#endif
+
 static int __init mrdump_key_init(void)
 {
 	int res;
 	struct wd_api *wd_api = NULL;
 	enum MRDUMP_RST_SOURCE source = MRDUMP_EINT;
 	enum MRDUMP_NOTIFY_MODE mode = MRDUMP_IRQ;
+#ifdef CONFIG_MTK_PMIC_COMMON
+	enum MRDUMP_LONG_PRESS_MODE long_press_mode = LONG_PRESS_NONE;
+	const char *long_press;
+#endif
 	struct device_node *node;
 	const char *source_str, *mode_str, *interrupts;
 	char node_name[] = "mediatek, mrdump_ext_rst-eint";
@@ -39,9 +52,19 @@ static int __init mrdump_key_init(void)
 	node = of_find_compatible_node(NULL, NULL, node_name);
 	if (node) {
 		if (!of_property_read_string(node, "source", &source_str)) {
-			if (strcmp(source_str, "SYSRST") == 0)
+			if (strcmp(source_str, "SYSRST") == 0) {
 				source = MRDUMP_SYSRST;
-			else if (!of_property_read_string(node, "interrupts", &interrupts))
+#ifdef CONFIG_MTK_PMIC_COMMON
+				if (!of_property_read_string(node, "long_press",
+					&long_press)) {
+					if (strcmp(long_press, "SHUTDOWN") == 0)
+						long_press_mode = LONG_PRESS_SHUTDOWN;
+					else
+						long_press_mode = LONG_PRESS_NONE;
+				}
+#endif
+			} else if (!of_property_read_string(node, "interrupts",
+				&interrupts))
 				source = MRDUMP_EINT;
 			else {
 				pr_notice("mrdump_key:EINT disabled,no dws configuration found\n");
@@ -53,9 +76,8 @@ static int __init mrdump_key_init(void)
 		if (!of_property_read_string(node, "mode", &mode_str)) {
 			if (strcmp(mode_str, "RST") == 0)
 				mode = MRDUMP_RST;
-		} else {
+		} else
 			pr_notice("MRDUMP_KEY: no mode property, use legacy value IRQ");
-		}
 	} else {
 		pr_notice("%s:MRDUMP_KEY node %s is not exist\n"
 			"MRDUMP_KEY is disabled\n", __func__, node_name);
@@ -72,13 +94,24 @@ static int __init mrdump_key_init(void)
 			if (res == -1)
 				pr_notice("%s: sysrst failed\n", __func__);
 			else
-				pr_notice("%s: enable MRDUMP_KEY\n", __func__);
+				pr_notice("%s: enable MRDUMP_KEY SYSRST mode\n", __func__);
+#ifdef CONFIG_MTK_PMIC_COMMON
+			pr_notice("%s: configure PMIC for smart reset\n", __func__);
+			if (long_press_mode == LONG_PRESS_SHUTDOWN) {
+				pr_notice("long_press_mode = SHUTDOWN\n");
+				pmic_enable_smart_reset(1, 1);
+			} else {
+				pr_notice("long_press_mode = NONE\n");
+				pmic_enable_smart_reset(1, 0);
+			}
+#endif
+
 		} else if (source == MRDUMP_EINT) {
 			res = wd_api->wd_debug_key_eint_config(1, mode);
 			if (res == -1)
 				pr_notice("%s: eint failed\n", __func__);
 			else
-				pr_notice("%s: enabled MRDUMP_KEY\n", __func__);
+				pr_notice("%s: enabled MRDUMP_KEY EINT mode\n", __func__);
 		} else {
 			pr_notice("%s:source %d is not match\n"
 				"disable MRDUMP_KEY\n", __func__, source);
