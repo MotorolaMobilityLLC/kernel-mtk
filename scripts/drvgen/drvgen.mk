527b8e8f2140c2d4f292ee03c701138f791cdbdb
SW Integrator <swintegrator@medaitek.com>
2019-01-22 19:58:25 +0800

    [ALPS02664443] Platform: Add drvgen script
    [Detail]
        Add drvgen script
    
    MTK-Commit-Id: cc9e9fc22f6743bb717b337423281cadd7743869
    
    Change-Id: I1090719b1f90a3864aa88c2e5733aec7034a4da7
    Feature: Others
    CR-Id: ALPS02664443
    Signed-off-by: Roger Hsu <roger.hsu@mediatek.com>

---
+++ scripts/drvgen/drvgen.mk
@@ -0,0 +1,25 @@
+ifdef MTK_PLATFORM
+DRVGEN_PATH := drivers/misc/mediatek/dws/$(MTK_PLATFORM)
+
+ifndef DRVGEN_OUT
+DRVGEN_OUT := $(objtree)/arch/$(SRCARCH)/boot/dts
+endif
+export DRVGEN_OUT
+
+ALL_DRVGEN_FILE := cust.dtsi
+
+DWS_FILE := $(srctree)/$(DRVGEN_PATH)/$(MTK_PROJECT).dws
+ifneq ($(wildcard $(DWS_FILE)),)
+DRVGEN_FILE_LIST := $(addprefix $(DRVGEN_OUT)/,$(ALL_DRVGEN_FILE))
+else
+DRVGEN_FILE_LIST :=
+endif
+DRVGEN_TOOL := $(srctree)/tools/dct/DrvGen
+
+.PHONY: drvgen
+drvgen: $(DRVGEN_FILE_LIST)
+$(DRVGEN_OUT)/cust.dtsi: $(DRVGEN_TOOL) $(DWS_FILE)
+	@mkdir -p $(dir $@)
+	$(DRVGEN_TOOL) $(DWS_FILE) $(dir $@) $(dir $@) cust_dtsi
+
+endif#MTK_PLATFORM
