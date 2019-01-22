ca3361f27871801a454e48d574f2f56efd36cbee
Joey Pan <joey.pan@mediatek.com>
2019-01-23 00:04:22 +0800

    [ALPS03625145] DISP:Fix VP BW report too low issue
    [Detail] Because the BW calculation should base on
    output frame rate in RDMA path, we revise the related
    settings.
    
    [Solution] Revise the BW calculation setting.
    
    MTK-Commit-Id: 1c6e877e65f9de94bab296aaa51a187cbb4a3b64
    
    Change-Id: Iac18465f513a4a23bc24a79f65613ccd285bfbb0
    Signed-off-by: Joey Pan <joey.pan@mediatek.com>
    CR-Id: ALPS03625145
    Feature: MT6771 Platform Feature

--- drivers/misc/mediatek/video/mt6771/dispsys/ddp_drv.h
+++ drivers/misc/mediatek/video/mt6771/dispsys/ddp_drv.h
@@ -358,6 +358,6 @@ struct device *disp_get_device(void);
 #define DISP_IOCTL_SET_TPLAY_HANDLE    _IOW(DISP_IOCTL_MAGIC, 200, unsigned int)
 
 int disp_get_ovl_bandwidth(unsigned int in_fps, unsigned int out_fps, unsigned int *bandwidth);
-int disp_get_rdma_bandwidth(unsigned int in_fps, unsigned int *bandwidth);
+int disp_get_rdma_bandwidth(unsigned int out_fps, unsigned int *bandwidth);
 
 #endif
