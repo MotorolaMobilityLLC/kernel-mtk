9a97f62c1ca4a4585d4a125a42023a3cc59ce862
Josepchi Tsai <josepchi.tsai@mediatek.com>
2019-01-22 21:17:59 +0800

    [ALPS02836388] Cam: Merge 2L7 from alps-mp-m1.mp3
    [Detail]
    Merge 2L7 from alps-mp-m1.mp3
    
    [Solution]
    None
    
    MTK-Commit-Id: 23797efeda3a2d652fb4e31fadd065f149262904
    
    Change-Id: I69e6250c3a21ab9145430fb50a22526bcc6bcd55
    Signed-off-by: Josepchi Tsai <josepchi.tsai@mediatek.com>
    CR-Id: ALPS02836388
    Feature: Camera Feature

--- drivers/misc/mediatek/imgsensor/inc/kd_camera_feature.h
+++ drivers/misc/mediatek/imgsensor/inc/kd_camera_feature.h
@@ -19,6 +19,11 @@
 #define FTYPE_ENUM(_enums...)  _enums
 #endif              /* FTYPE_ENUM */
 
+#ifndef FID_TO_TYPE_ENUM
+#define FID_TO_TYPE_ENUM(_fid, _enums) \
+	typedef enum { _enums/*, OVER_NUM_OF_##_fid*/ }
+#endif              /* FID_TO_TYPE_ENUM */
+
 #include "kd_camera_feature_id.h"
 #include "kd_camera_feature_enum.h"
 
