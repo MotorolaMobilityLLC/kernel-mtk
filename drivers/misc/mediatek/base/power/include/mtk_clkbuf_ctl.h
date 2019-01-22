c804cb627b87f37531b116d15ae15cfc70d969d9
Ethan Chang <ethan.chang@mediatek.com>
2019-01-23 00:01:05 +0800

    [ALPS03524680] Clock buffer: early porting
    [Detail]
    MT6775 Clock buffer early poring
    
    MTK-Commit-Id: 892fe1e8de63adb7ac322c7f3745133186b1c57d
    
    Change-Id: I42ad142bdc637a413f9c1f2e2b7f81f16abba1d5
    Signed-off-by: Ethan Chang <ethan.chang@mediatek.com>
    CR-Id: ALPS03524680
    Feature: Power Management
    (cherry picked from commit 0c5d5582e96b2a58403059535ba6ccb01c69749e)

--- drivers/misc/mediatek/base/power/include/mtk_clkbuf_ctl.h
+++ drivers/misc/mediatek/base/power/include/mtk_clkbuf_ctl.h
@@ -40,7 +40,8 @@
 
 #elif defined(CONFIG_MACH_ELBRUS) || defined(CONFIG_MACH_MT6799) || \
 	defined(CONFIG_MACH_MT6758) || defined(CONFIG_MACH_MT6759) || \
-	defined(CONFIG_MACH_MT6763) || defined(CONFIG_MACH_MT6739)
+	defined(CONFIG_MACH_MT6763) || defined(CONFIG_MACH_MT6739) || \
+	defined(CONFIG_MACH_MT6775)
 
 #include "clkbuf_v1/mtk_clkbuf_ctl.h"
 
