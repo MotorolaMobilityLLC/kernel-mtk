1a962e5ec8f306ed29e089806fe8fdd4a296641c
Auto Li <auto.li@mediatek.com>
2019-01-22 22:00:25 +0800

    [ALPS02992197] CAM: Add IMX398
    [Detail] First porting to support IMX398
    
    [Solution] done
    
    MTK-Commit-Id: 83e6e275b0443a84533ca5b0c37ad4bbb1ef4f80
    
    Change-Id: Ie0992cd02fa8d6229e8bfcfc28a41078f275191b
    Signed-off-by: Auto Li <auto.li@mediatek.com>
    CR-Id: ALPS02992197
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/imgsensor/inc/kd_imgsensor.h
+++ drivers/misc/mediatek/imgsensor/inc/kd_imgsensor.h
@@ -113,6 +113,7 @@ NSFeature :  : RAWSensorInfo < _id >  :  : getFlickerPara \
 ********************************************************************************/
 /* SENSOR CHIP VERSION */
 /*IMX*/
+#define IMX398_SENSOR_ID                        0x0398
 #define IMX268_SENSOR_ID                        0x0268
 #define IMX386_SENSOR_ID                        0x0386
 #define IMX362_SENSOR_ID                        0x0362
@@ -292,6 +293,7 @@ NSFeature :  : RAWSensorInfo < _id >  :  : getFlickerPara \
 #define CAMERA_HW_DEVNAME                       "kd_camera_hw"
 /* SENSOR DEVICE DRIVER NAME */
 /*IMX*/
+#define SENSOR_DRVNAME_IMX398_MIPI_RAW          "imx398mipiraw"
 #define SENSOR_DRVNAME_IMX268_MIPI_RAW          "imx268mipiraw"
 #define SENSOR_DRVNAME_IMX386_MIPI_RAW          "imx386mipiraw"
 #define SENSOR_DRVNAME_IMX362_MIPI_RAW          "imx362mipiraw"
