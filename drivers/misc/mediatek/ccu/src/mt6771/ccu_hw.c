a0f83ac760f7274e7463c41853a7e65435176303
mtk12363 <song-tseng.ho@mediatek.com>
2019-01-23 00:04:19 +0800

    [ALPS03661710] CCU: Fix DEVAPC error
    [Detail]
    1. Would not dump i2c reg.
    
    MTK-Commit-Id: 444eada85c2b902d03081e5150f1610c3fb711b4
    
    Change-Id: Id8a8108a105c89b9806b552c9ca7ff7f947e2a6e
    Signed-off-by: mtk12363 <song-tseng.ho@mediatek.com>
    CR-Id: ALPS03661710
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/ccu/src/mt6771/ccu_hw.c
+++ drivers/misc/mediatek/ccu/src/mt6771/ccu_hw.c
@@ -237,7 +237,6 @@ irqreturn_t ccu_isr_handler(int irq, void *dev_id)
 				g_LogBufIdx = 0xFFFFFFFF;	/* -1*/
 
 				wake_up_interruptible(&ccuInfo.WaitQueueHead);
-				ccu_i2c_dump_errr();
 				LOG_ERR("wakeup ccuInfo.WaitQueueHead done\n");
 				break;
 			}
@@ -252,8 +251,6 @@ irqreturn_t ccu_isr_handler(int irq, void *dev_id)
 				g_LogBufIdx = -2;
 
 				wake_up_interruptible(&ccuInfo.WaitQueueHead);
-				if ((receivedCcuCmd.in_data_ptr == 0xEE)  || (receivedCcuCmd.in_data_ptr == 0xDD))
-					ccu_i2c_dump_errr();
 				LOG_ERR("wakeup ccuInfo.WaitQueueHead done\n");
 				break;
 			}
