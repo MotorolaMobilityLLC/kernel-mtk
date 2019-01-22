765aad641a52b700c38e73bc265af0b24b0f95c9
Chun-Hung Wu <chun-hung.wu@mediatek.com>
2019-01-22 21:18:18 +0800

    [ALPS02685397] ufs: Add MT6799 kernel driver
    [Detail]
    Support ufs kernel driver for MT6799.
    
    	arch/arm64/configs/fpga6799_64_ufs_debug_defconfig
    	arch/arm64/configs/fpga6799_64_ufs_defconfig
    
    [Detail]
    
    [Solution]
    
    MTK-Commit-Id: f67d1bc79dd25052c7c7cd5f1f9a28eaef1347e4
    
    Change-Id: Ie044b1cd9d7100b82dfd3848b822ab54e4c0ff92
    Signed-off-by: Chun-Hung Wu <chun-hung.wu@mediatek.com>
    Conflicts:
    CR-Id: ALPS02685397
    Feature: UFS Booting

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -174,6 +174,13 @@ struct ufshcd_lrb {
 	int task_tag;
 	u8 lun; /* UPIU LUN id field is only 8-bit wide */
 	bool intr_cmd;
+
+#ifdef CONFIG_MTK_UFS_HW_CRYPTO
+	u32 crypto_en;
+	u32 crypto_cfgid;
+	u32 crypto_dunl;
+	u32 crypto_dunu;
+#endif
 };
 
 /**
@@ -532,6 +539,11 @@ struct ufs_hba {
 	struct devfreq *devfreq;
 	struct ufs_clk_scaling clk_scaling;
 	bool is_sys_suspended;
+
+#ifdef CONFIG_MTK_UFS_BOOTING
+	u32 manu_id;               /* record vendor id for vendor-specific configurations */
+	unsigned int dev_quirks;
+#endif
 };
 
 /* Returns true if clocks can be gated. Otherwise false */
@@ -787,5 +799,33 @@ static inline void ufshcd_vops_dbg_register_dump(struct ufs_hba *hba)
 	if (hba->vops && hba->vops->dbg_register_dump)
 		hba->vops->dbg_register_dump(hba);
 }
+
+#ifdef CONFIG_MTK_UFS_BOOTING
+void  ufshcd_enable_intr(struct ufs_hba *hba, u32 intrs);
+int   ufshcd_get_req_rsp(struct utp_upiu_rsp *ucd_rsp_ptr);
+int   ufshcd_get_rsp_upiu_result(struct utp_upiu_rsp *ucd_rsp_ptr);
+int   ufshcd_get_tr_ocs(struct ufshcd_lrb *lrbp);
+int   ufshcd_query_attr(struct ufs_hba *hba,
+							enum query_opcode opcode,
+							enum attr_idn idn,
+							u8 index,
+							u8 selector,
+							u32 *attr_val);
+int   ufshcd_query_flag(struct ufs_hba *hba, enum query_opcode opcode, enum flag_idn idn, bool *flag_res);
+int   ufshcd_query_descriptor(struct ufs_hba *hba,
+							enum query_opcode opcode,
+							enum desc_idn idn,
+							u8 index,
+							u8 selector,
+							u8 *desc_buf,
+							int *buf_len);
+int   ufshcd_read_device_desc(struct ufs_hba *hba, u8 *buf, u32 size);
+int   ufshcd_read_desc(struct ufs_hba *hba, enum desc_idn desc_id, int desc_index, u8 *buf, u32 size);
+int   ufshcd_read_string_desc(struct ufs_hba *hba, int desc_index, u8 *buf, u32 size, bool ascii);
+void  ufshcd_send_command(struct ufs_hba *hba, unsigned int task_tag);
+int   ufshcd_send_uic_cmd(struct ufs_hba *hba, struct uic_command *uic_cmd);
+int   ufshcd_uic_change_pwr_mode(struct ufs_hba *hba, u8 mode);
+void  ufshcd_utrl_clear(struct ufs_hba *hba, u32 pos);
+#endif
 
 #endif /* End of Header */
