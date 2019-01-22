6e4bfa510b06f0fd97218c7579948c5b21a1932c
Irene Luo <irene.luo@mediatek.com>
2019-01-22 23:01:57 +0800

    [ALPS03327703] MCDI: driver porting
    [Detail]
    Copy original code from mt6763
    
    MTK-Commit-Id: c1243c2ca9810d064957bc72f544ae0576240b7e
    
    Change-Id: I6261672e823ec6fbf959ef74da1796bdcc6fecf3
    Signed-off-by: Irene Luo <irene.luo@mediatek.com>
    CR-Id: ALPS03327703
    Feature: All Core Always Online (ACAO-MCU Deep Idle)

---
+++ drivers/misc/mediatek/base/power/mcdi/include/mcdi_v2/mtk_mcdi.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2017 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#ifndef __MTK_MCDI_H__
+#define __MTK_MCDI_H__
+
+#define NF_CPU                  8
+#define NF_CLUSTER              2
+#define CPU_PWR_STAT_MASK       0x000000FF
+#define CLUSTER_PWR_STAT_MASK   0x00030000
+
+enum {
+	ALL_CPU_IN_CLUSTER = 0,
+	CPU_CLUSTER,
+	CPU_IN_OTHER_CLUSTER,
+	NF_PWR_STAT_MAP_TYPE
+};
+
+extern u32 aee_rr_rec_mcdi_val(int id, u32 val);
+
+int cluster_idx_get(int cpu);
+unsigned int get_menu_predict_us(void);
+bool mcdi_task_pause(bool paused);
+unsigned int mcdi_mbox_read(int id);
+void mcdi_mbox_write(int id, unsigned int val);
+void update_avail_cpu_mask_to_mcdi_controller(unsigned int cpu_mask);
+bool is_cpu_pwr_on_event_pending(void);
+unsigned int get_pwr_stat_check_map(int type, int idx);
+int mcdi_get_mcdi_idle_state(int idx);
+void mcdi_status_init(void);
+void mcdi_of_init(void);
+
+#endif /* __MTK_MCDI_H__ */
---
+++ drivers/misc/mediatek/base/power/mcdi/mcdi_v2/mtk_mcdi_state.c
@@ -0,0 +1,196 @@
+/*
+ * Copyright (C) 2017 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#include <linux/types.h>
+
+#include <mtk_mcdi.h>
+#include <mtk_mcdi_state.h>
+
+enum {
+	MCDI_STATE_TABLE_SET_0      = 0,
+	MCDI_STATE_TABLE_SET_1      = 1,
+	MCDI_STATE_TABLE_SET_2      = 2,
+	NF_MCDI_STATE_TABLE_TYPE    = 3
+};
+
+/*
+ * Used for mcdi_governor
+ * only use exit_latency & target_residency
+ */
+static struct cpuidle_driver mt67xx_acao_mcdi_state[NF_MCDI_STATE_TABLE_TYPE] = {
+	[0] = {
+		.name             = "mt67xx_acao_mcdi_set_0",
+		.owner            = THIS_MODULE,
+		.states[0] = {
+			.enter              = NULL,
+			.exit_latency       = 1,
+			.target_residency   = 1,
+			.name               = "wfi",
+			.desc               = "wfi"
+		},
+		.states[1] = {
+			.enter              = NULL,
+			.exit_latency       = 300,
+			.target_residency   = 4500,
+			.name               = "cpu_off",
+			.desc               = "cpu_off",
+		},
+		.states[2] = {
+			.enter              = NULL,
+			.exit_latency       = 600,
+			.target_residency   = 4500,
+			.name               = "cluster_off",
+			.desc               = "cluster_off",
+		},
+		.states[3] = {
+			.enter              = NULL,
+			.exit_latency       = 1200,
+			.target_residency   = 5000,
+			.name               = "sodi",
+			.desc               = "sodi",
+		},
+		.states[4] = {
+			.enter              = NULL,
+			.exit_latency       = 1200,
+			.target_residency   = 5000,
+			.name               = "dpidle",
+			.desc               = "dpidle",
+		},
+		.states[5] = {
+			.enter              = NULL,
+			.exit_latency       = 5000,
+			.target_residency   = 10500,
+			.name               = "sodi3",
+			.desc               = "sodi3",
+		},
+		.state_count = 6,
+		.safe_state_index = 0,
+	},
+	[1] = {
+		.name             = "mt67xx_acao_mcdi_set_1",
+		.owner            = THIS_MODULE,
+		.states[0] = {
+			.enter              = NULL,
+			.exit_latency       = 1,
+			.target_residency   = 1,
+			.name               = "wfi",
+			.desc               = "wfi"
+		},
+		.states[1] = {
+			.enter              = NULL,
+			.exit_latency       = 300,
+			.target_residency   = 1500,
+			.name               = "cpu_off",
+			.desc               = "cpu_off",
+		},
+		.states[2] = {
+			.enter              = NULL,
+			.exit_latency       = 600,
+			.target_residency   = 1500,
+			.name               = "cluster_off",
+			.desc               = "cluster_off",
+		},
+		.states[3] = {
+			.enter              = NULL,
+			.exit_latency       = 1200,
+			.target_residency   = 5000,
+			.name               = "sodi",
+			.desc               = "sodi",
+		},
+		.states[4] = {
+			.enter              = NULL,
+			.exit_latency       = 1200,
+			.target_residency   = 5000,
+			.name               = "dpidle",
+			.desc               = "dpidle",
+		},
+		.states[5] = {
+			.enter              = NULL,
+			.exit_latency       = 5000,
+			.target_residency   = 10500,
+			.name               = "sodi3",
+			.desc               = "sodi3",
+		},
+		.state_count = 6,
+		.safe_state_index = 0,
+	},
+	[2] = {
+		.name             = "mt67xx_acao_mcdi_set_2",
+		.owner            = THIS_MODULE,
+		.states[0] = {
+			.enter              = NULL,
+			.exit_latency       = 1,
+			.target_residency   = 1,
+			.name               = "wfi",
+			.desc               = "wfi"
+		},
+		.states[1] = {
+			.enter              = NULL,
+			.exit_latency       = 300,
+			.target_residency   = 850,
+			.name               = "cpu_off",
+			.desc               = "cpu_off",
+		},
+		.states[2] = {
+			.enter              = NULL,
+			.exit_latency       = 600,
+			.target_residency   = 1500,
+			.name               = "cluster_off",
+			.desc               = "cluster_off",
+		},
+		.states[3] = {
+			.enter              = NULL,
+			.exit_latency       = 1200,
+			.target_residency   = 5000,
+			.name               = "sodi",
+			.desc               = "sodi",
+		},
+		.states[4] = {
+			.enter              = NULL,
+			.exit_latency       = 1200,
+			.target_residency   = 5000,
+			.name               = "dpidle",
+			.desc               = "dpidle",
+		},
+		.states[5] = {
+			.enter              = NULL,
+			.exit_latency       = 5000,
+			.target_residency   = 10500,
+			.name               = "sodi3",
+			.desc               = "sodi3",
+		},
+		.state_count = 6,
+		.safe_state_index = 0,
+	}
+};
+
+static int mcdi_state_table_idx_map[NF_CPU] = {
+	MCDI_STATE_TABLE_SET_0,
+	MCDI_STATE_TABLE_SET_0,
+	MCDI_STATE_TABLE_SET_0,
+	MCDI_STATE_TABLE_SET_0,
+	MCDI_STATE_TABLE_SET_1,
+	MCDI_STATE_TABLE_SET_1,
+	MCDI_STATE_TABLE_SET_1,
+	MCDI_STATE_TABLE_SET_2
+};
+
+struct cpuidle_driver *mcdi_state_tbl_get(int cpu)
+{
+	int tbl_idx = 0;
+
+	tbl_idx = (cpu >= 0 && cpu < NF_CPU) ? mcdi_state_table_idx_map[cpu] : 0;
+
+	return &mt67xx_acao_mcdi_state[tbl_idx];
+}
+
