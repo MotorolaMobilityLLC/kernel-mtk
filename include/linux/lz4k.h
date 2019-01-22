219157d364351e5ae9cdfb2bc5baa56c75ac1e78
Chinwen Chang <chinwen.chang@mediatek.com>
2019-01-22 22:57:59 +0800

    [ALPS03221406] lz4k: let lz4k support zsm
    Let LZ4K support ZSM.
    
    MTK-Commit-Id: 5166f120410bfda8cd6cee2ba033cd76f68df83e
    
    Change-Id: Ia9c0ad2eddd3bd7557ea1b9afea8cbb7b68dbd2e
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>
    CR-Id: ALPS03221406
    Feature: Memory Optimization

--- include/linux/lz4k.h
+++ include/linux/lz4k.h
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
 
 #ifndef __LZ4K_H__
 #define __LZ4K_H__
@@ -18,7 +31,8 @@ int lz4k_compress(const unsigned char *src, size_t src_len,
 
 int lz4k_decompress_safe(const unsigned char *src, size_t src_len,
 			 unsigned char *dst, size_t *dst_len);
-
+int lz4k_compress_zram(const unsigned char *src, size_t src_len,
+		  unsigned char *dst, size_t *dst_len, void *wrkmem, int *checksum);
 #ifdef CONFIG_UBIFS_FS
 int lz4k_decompress_ubifs(const unsigned char *src, size_t src_len,
 			  unsigned char *dst, size_t *dst_len);
