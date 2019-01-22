b1e1d9dbb8cde0b996c49a855b09b5718fd7055e
Jacky Chen <ming-fan.chen@mediatek.com>
2019-01-22 23:01:48 +0800

    [ALPS03301704] SMI: mt6758 met porting
    [Detail] mt6758 met smi porting
    
    MTK-Commit-Id: d07716a69aa7e2e8518af5fd022bd8f70932eae2
    
    Change-Id: I216c090ab3009f624db100ceb8885754e58e9a68
    Signed-off-by: Jacky Chen <ming-fan.chen@mediatek.com>
    CR-Id: ALPS03301704
    Feature: SMI

--- drivers/misc/mediatek/smi/met/met_smi_configuration.h
+++ drivers/misc/mediatek/smi/met/met_smi_configuration.h
@@ -42,8 +42,22 @@
 #define SMI_LARB_NUMBER 4
 #define smi_met_SMI_LARB_NUMBER 4
 #define smi_met_SMI_COMM_NUMBER 1
+#elif defined(SMI_MET_VIN)
+#define SMI_MET_LARB0_PORT_NUM 7
+#define SMI_MET_LARB1_PORT_NUM 6
+#define SMI_MET_LARB2_PORT_NUM 3
+#define SMI_MET_LARB3_PORT_NUM 5
+#define SMI_MET_LARB4_PORT_NUM 10
+#define SMI_MET_LARB5_PORT_NUM 12
+#define SMI_MET_LARB6_PORT_NUM 21
+#define SMI_MET_LARB7_PORT_NUM 11
+#define SMI_MET_COMMON_PORT_NUM 9
+#define SMI_MET_TOTAL_MASTER_NUM 9
+#define SMI_COMM_NUMBER 1
+#define SMI_LARB_NUMBER 8
+#define smi_met_SMI_LARB_NUMBER 8
+#define smi_met_SMI_COMM_NUMBER 1
 #endif
-
 
 extern struct chip_smi smi_map[SMI_MET_TOTAL_MASTER_NUM];
 #endif
