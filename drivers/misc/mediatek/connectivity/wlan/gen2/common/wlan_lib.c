c1b3a920916535b2e7603eb8f8fd7a161ac14beb
Clovis Zhuang <clovis.zhuang@mediatek.com>
2019-01-22 21:05:58 +0800

    [ALPS02814192] WLAN Gen2: Porting patches to kernel 4.4 (1)
    [Detail]
    Porting WLAN Gen2 patches from kernel 3.18 to 4.4
    [Solution]
    [ALPS02659924] wlan: fix security Vulnerability by Saker Hsia · 3 months
    [ALPS02693269] wlan: correct wifi ramcode path by xianpu.liang · 3 month
    [ALPS02693269] wlan: correct wifi ramcode path by Xianpu Liang · 3 month
    [ALPS02638242] wlan: ppr2: patch back by Wenwei Han · 3 months ago
    [ALPS02603618] wlan: Support FCC TX power control by Qiang guo · 4 month
    
    MTK-Commit-Id: 41064997d4d2b8f269a6ca193aec6438c1769b02
    
    Change-Id: I9a1599a046d6e5bed2a960bf2430aaeb52f8dff1
    Signed-off-by: Clovis Zhuang <clovis.zhuang@mediatek.com>
    CR-Id: ALPS02814192
    Feature: [Android Default] Wi-Fi Driver for SoC

--- drivers/misc/mediatek/connectivity/wlan/gen2/common/wlan_lib.c
+++ drivers/misc/mediatek/connectivity/wlan/gen2/common/wlan_lib.c
@@ -3548,6 +3548,9 @@ WLAN_STATUS wlanLoadManufactureData(IN P_ADAPTER_T prAdapter, IN P_REG_INFO_T pr
 #if CFG_SUPPORT_RDD_TEST_MODE
 	CMD_RDD_CH_T rRddParam;
 #endif
+#if CFG_SUPPORT_FCC_DYNAMIC_TX_PWR_ADJUST
+	CMD_FCC_TX_PWR_ADJUST FccTxPwrAdjust = {0x00};
+#endif
 
 	ASSERT(prAdapter);
 
@@ -3577,6 +3580,27 @@ WLAN_STATUS wlanLoadManufactureData(IN P_ADAPTER_T prAdapter, IN P_REG_INFO_T pr
 			nicUpdateTxPower(prAdapter, (P_CMD_TX_PWR_T) (&(prRegInfo->rTxPwr)));
 		}
 	}
+
+#if CFG_SUPPORT_FCC_DYNAMIC_TX_PWR_ADJUST
+	/* Tx Power Adjust for FCC/CE Certification */
+	FccTxPwrAdjust.fgFccTxPwrAdjust = 1;	/* 1:enable; 0:disable */
+	FccTxPwrAdjust.Offset_CCK = 14;		/* drop 7dB */
+	FccTxPwrAdjust.Offset_HT20 = 16;	/* drop 8dB */
+	FccTxPwrAdjust.Offset_HT40 = 14;	/* drop 7dB*/
+	FccTxPwrAdjust.Channel_CCK[0] = 11;	/* [0] for start channel */
+	FccTxPwrAdjust.Channel_CCK[1] = 13;	/* [1] for ending channel */
+	FccTxPwrAdjust.Channel_HT20[0] = 11;	/* [0] for start channel */
+	FccTxPwrAdjust.Channel_HT20[1] = 13;	/* [1] for ending channel */
+	FccTxPwrAdjust.Channel_HT40[0] = 7;	/* [0] for start channel,engineer mode ch9(2452) */
+	FccTxPwrAdjust.Channel_HT40[1] = 9;	/* [1] for ending channel,engineer mode ch11(2462) */
+
+	wlanSendSetQueryCmd(prAdapter,
+			    CMD_ID_SET_FCC_TX_PWR_CERT,
+			    TRUE,
+			    FALSE,
+			    FALSE, NULL, NULL, sizeof(CMD_FCC_TX_PWR_ADJUST), (PUINT_8) (&FccTxPwrAdjust), NULL, 0);
+
+#endif
 
 	/* 3. Check if needs to support 5GHz */
 	/* if(prRegInfo->ucEnable5GBand) { // Frank workaround */
