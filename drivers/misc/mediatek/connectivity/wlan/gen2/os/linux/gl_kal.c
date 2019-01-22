2e90b8a70a746640b76db16f2f10f97cc4c3d78f
Wayne-TW <wayne-tw.chen@mediatek.com>
2019-01-22 23:05:07 +0800

    [ALPS02906522] wlan: log too much whck alloc fail
    [Detail]
    361464: Unchecked return value
    driver will print too much logs when allocating packets fail
    
    [Solution]
    change one fail one log mechanism to many fails one log
    and add return value check
    
    MTK-Commit-Id: 8dc43335f03972e04eb332d3547341ac33328428
    
    Change-Id: I544ba9ccb8e88c87afb64105579b2c144cdfb164
    CR-Id: ALPS02906522
    Feature: Wifi Driver
    Signed-off-by: Wayne-TW Chen <wayne-tw.chen@mediatek.com>
    (cherry picked from commit 7d0ba8c64db47610dd4d17b418d50a0229913f1e)

--- drivers/misc/mediatek/connectivity/wlan/gen2/os/linux/gl_kal.c
+++ drivers/misc/mediatek/connectivity/wlan/gen2/os/linux/gl_kal.c
@@ -678,8 +678,6 @@ PVOID kalPacketAlloc(IN P_GLUE_INFO_T prGlueInfo, IN UINT_32 u4Size, OUT PPUINT_
 
 	if (prSkb)
 		*ppucData = (PUINT_8) (prSkb->data);
-	else
-		DBGLOG(RX, WARN, "kalPacketAlloc fail!!\n");
 #if DBG
 	{
 		PUINT_32 pu4Head = (PUINT_32) &prSkb->cb[0];
