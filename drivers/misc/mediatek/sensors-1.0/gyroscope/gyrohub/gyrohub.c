ef70a1e622f8a1c00db45fba3e6af0f549e7162f
Lomen Wu <lomen.wu@mediatek.com>
2019-01-23 00:08:01 +0800

    [ALPS03897641] Sensor: for cts case not blocking flush event
    [Detail]
    for cts case not blocking flush event
    
    MTK-Commit-Id: ee4e62dde9665f75d604718a271fa55c8bccf414
    
    Change-Id: I4dcb9ce34e6b4f14fac49c2e0b7d0c8d1532133c
    Signed-off-by: Lomen Wu <lomen.wu@mediatek.com>
    CR-Id: ALPS03897641
    Feature: Sensor Hub
    (cherry picked from commit 9598a8eed3d1f1d9597c13170562af35f151a38e)

--- drivers/misc/mediatek/sensors-1.0/gyroscope/gyrohub/gyrohub.c
+++ drivers/misc/mediatek/sensors-1.0/gyroscope/gyrohub/gyrohub.c
@@ -465,7 +465,7 @@ static int gyro_recv_data(struct data_unit_t *event, void *reserved)
 
 	memset(&data, 0, sizeof(struct gyro_data));
 
-	if (event->flush_action == DATA_ACTION && READ_ONCE(obj->android_enable) == true) {
+	if (event->flush_action == DATA_ACTION) {
 		if (READ_ONCE(obj->android_enable) == false)
 			return 0;
 		data.x = event->gyroscope_t.x;
@@ -475,7 +475,7 @@ static int gyro_recv_data(struct data_unit_t *event, void *reserved)
 		data.timestamp = (int64_t)event->time_stamp;
 		data.reserved[0] = event->reserve[0];
 		err = gyro_data_report(&data);
-	} else if (event->flush_action == FLUSH_ACTION && READ_ONCE(obj->android_enable) == true) {
+	} else if (event->flush_action == FLUSH_ACTION) {
 		if (READ_ONCE(obj->android_enable) == false)
 			return 0;
 		err = gyro_flush_report();
