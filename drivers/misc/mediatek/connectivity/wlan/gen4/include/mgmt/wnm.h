9d3ba8e9e5040be861d24b06d8e0f71f39d3aab8
Wenwei Han <wenwei.han@mediatek.com>
2019-01-22 21:59:51 +0800

    [ALPS02548285] wlan: passpoint r2 enable wnm frame report
    [Detail]
    wnm notification action  frame was not supported
    
    [Solution]
    Add wnm notification action frame support. when received wnm
    notification action frame, notify it to supplicant
    
    MTK-Commit-Id: 2ed4d5c85a4ef03463707dfd2ba1a1d1a1100e15
    
    Change-Id: Idfe5e166fa8c050093842fd541215418fafab084
    Signed-off-by: Wenwei Han <wenwei.han@mediatek.com>
    CR-Id: ALPS02548285
    Feature: [Android Default] Wi-Fi Driver for SoC

--- drivers/misc/mediatek/connectivity/wlan/gen4/include/mgmt/wnm.h
+++ drivers/misc/mediatek/connectivity/wlan/gen4/include/mgmt/wnm.h
@@ -115,8 +115,6 @@ VOID wnmWNMAction(IN P_ADAPTER_T prAdapter, IN P_SW_RFB_T prSwRfb);
 
 VOID wnmReportTimingMeas(IN P_ADAPTER_T prAdapter, IN UINT_8 ucStaRecIndex, IN UINT_32 u4ToD, IN UINT_32 u4ToA);
 
-#define WNM_UNIT_TEST 1
-
 #if WNM_UNIT_TEST
 VOID wnmTimingMeasUnitTest1(P_ADAPTER_T prAdapter, UINT_8 ucStaRecIndex);
 #endif
