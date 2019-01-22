c07ed79bba26978688dc44afd7467b93396b570d
Andy Teng <andy.teng@mediatek.com>
2019-01-22 22:59:23 +0800

    [ALPS03280542] msdc: 6757 mp merge back to trunk
    [Detail]
    MP code merge back to trunk.
    
    [Solution]
    Update trunk to latest MP code.
    
    MTK-Commit-Id: 3871e4199dd86ceeaaee8db8dd1db92beb3971cf
    
    Change-Id: I8156a78f759a5713b42f67dbb33bee06b5d73e35
    Signed-off-by: Andy Teng <andy.teng@mediatek.com>
    CR-Id: ALPS03280542
    Feature: eMMC Boot Up

--- drivers/mmc/host/mediatek/rpmb/drrpmb_gp/public/drrpmb_gp_Api.h
+++ drivers/mmc/host/mediatek/rpmb/drrpmb_gp/public/drrpmb_gp_Api.h
@@ -1,15 +1,14 @@
 /*
- * Copyright (c) 2013-2016 TRUSTONIC LIMITED
- * All Rights Reserved.
+ * Copyright (C) 2016 MediaTek Inc.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
 
 /**
