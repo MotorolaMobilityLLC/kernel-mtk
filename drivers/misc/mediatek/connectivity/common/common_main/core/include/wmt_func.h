5c62e268be3353805d83fa7e0cc54b09c7ef6ddd
Zhao Chen <zhao.chen@mediatek.com>
2019-01-22 23:02:08 +0800

    [ALPS03203303] wmt:disable gps/fm related api in 8167p5
    [Detail]
    Ioctl test may call gps or fm on/off function
    in 8167p5, it may cause exception.
    
    [Solution]
    Disable fm and gps on/off function if not supported
    
    MTK-Commit-Id: 3c4a42f3bc6a54874057a66ecf13c9d616a4c29d
    
    Change-Id: I1bf34bd592bd6a7ad9e8f75509864521e9dada95
    Signed-off-by: Zhao Chen <zhao.chen@mediatek.com>
    CR-Id: ALPS03203303
    Feature: Vulnerability Scan
    (cherry picked from commit e0eb797d7cee57e9d5a4f66583800e628691064e)

--- drivers/misc/mediatek/connectivity/common/common_main/core/include/wmt_func.h
+++ drivers/misc/mediatek/connectivity/common/common_main/core/include/wmt_func.h
@@ -40,13 +40,13 @@
 #endif
 
 
-#if 1				/* defined(CONFIG_MTK_COMBO_FM) */
+#if defined(CONFIG_MTK_COMBO_FM)
 #define CFG_FUNC_FM_SUPPORT 1
 #else
 #define CFG_FUNC_FM_SUPPORT 0
 #endif
 
-#if 1				/* defined(CONFIG_MTK_COMBO_GPS) */
+#if defined(CONFIG_MTK_COMBO_GPS)
 #define CFG_FUNC_GPS_SUPPORT 1
 #else
 #define CFG_FUNC_GPS_SUPPORT 0
