f572d81e1b202ea46ad35dc231aafb757e1ad5ec
chipeng chang <chipeng.chang@mediatek.com>
2019-01-22 20:14:28 +0800

    [ALPS02696360] Audio: add smart pa driver
    [Detail]
    add audio smart pa driver for nxp external device.
    [Solution]
    
    MTK-Commit-Id: 499922daeac8d2c7b83eee5adbf85e7048c3fa83
    
    Change-Id: I2841316e91a4406964e21953c456f06791f726be
    Signed-off-by: chipeng chang <chipeng.chang@mediatek.com>
    CR-Id: ALPS02696360
    Feature: Phone Sound

---
+++ drivers/misc/mediatek/smartpa/auddrv_nxp_ioctl.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ *
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*******************************************************************************
+ *
+ * Filename:
+ * ---------
+ *  auddrv_NXP_ioctl.h
+ *
+ * Project:
+ * --------
+ *   Android NXP IOCTL MSG
+ *
+ * Description:
+ * ------------
+ *   ioctl control message
+ *
+ * Author:
+ * -------
+ *
+ *
+ *
+ *------------------------------------------------------------------------------
+ *
+ * .
+ *
+ *
+ *
+ *******************************************************************************/
+#ifndef _AUDDRV_NXP_IOCTL_MSG_H
+#define _AUDDRV_NXP_IOCTL_MSG_H
+
+
+/*****************************************************************************
+*                     C O M P I L E R   F L A G S
+******************************************************************************
+*/
+
+
+/*****************************************************************************
+*                          C O N S T A N T S
+******************************************************************************
+*/
+
+/* below is control message */
+#define AUD_DRV_NXP_IOC_MAGIC 'C'
+
+/*****************************************************************************
+*                         D A T A   T Y P E S
+******************************************************************************
+*/
+
+/*****************************************************************************
+*                        F U N C T I O N   D E F I N I T I O N
+******************************************************************************
+*/
+
+
+#endif
