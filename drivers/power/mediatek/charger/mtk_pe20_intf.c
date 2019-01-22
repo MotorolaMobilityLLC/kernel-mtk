2b2a68abed5f2e7add14f5ceba8538278cf7a47d
shufan lee <vend_shufan_lee001@mediatek.com>
2019-01-22 23:04:31 +0800

    [ALPS03482499] Charger: Make pe20_intf.c Can Cherry Pick From n1.mp16
    [Detail]
    1. Modify log to make file can merge patch from n1.mp16
    
    MTK-Commit-Id: 681dc7330022d2c5b20bbea64fb9c4f1658ab8d2
    
    Change-Id: I7ae6f360d0843572b5fe6ceb3e45e8bd7d3f1232
    Signed-off-by: shufan lee <vend_shufan_lee001@mediatek.com>
    CR-Id: ALPS03482499
    Feature: Charger

--- drivers/power/mediatek/charger/mtk_pe20_intf.c
+++ drivers/power/mediatek/charger/mtk_pe20_intf.c
@@ -120,7 +120,7 @@ static int pe20_set_mivr(struct charger_manager *pinfo, int uV)
 	if (pinfo->chg2_dev) {
 		ret = charger_dev_set_mivr(pinfo->chg2_dev, uV);
 		if (ret < 0)
-			pr_notice("%s: chg2 failed, ret = %d\n", __func__, ret);
+			pr_err("%s: chg2 failed, ret = %d\n", __func__, ret);
 	}
 
 	return ret;
