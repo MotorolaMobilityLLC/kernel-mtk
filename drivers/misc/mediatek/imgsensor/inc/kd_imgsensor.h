639c430efc58f5367cfdeccad2dd13f1550d2057
John.Wei <john.wei@mediatek.com>
2019-01-22 23:02:01 +0800

    [ALPS03328819] cam: sync code
    [Detail] from mp8 & fix coding style
    
    [Solution]
    
    MTK-Commit-Id: a82db4b9de611d086d71921b5dfe00a26f54f236
    
    Change-Id: I3dc06ece196f3a90cd18d254430b7543189ea4c7
    Signed-off-by: John.Wei <john.wei@mediatek.com>
    CR-Id: ALPS03328819
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/imgsensor/inc/kd_imgsensor.h
+++ drivers/misc/mediatek/imgsensor/inc/kd_imgsensor.h
@@ -185,7 +185,9 @@
 #define S5K3L8_SENSOR_ID                        0x30C8
 #define S5K3M3_SENSOR_ID                        0x30D3
 #define S5K2X8_SENSOR_ID                        0x2188
+#define S5K2P7_SENSOR_ID                        0x2107
 #define S5K2P8_SENSOR_ID                        0x2108
+#define S5K3P3_SENSOR_ID                        0x3103
 #define S5K3P3SX_SENSOR_ID                      0x3103
 #define S5K3P8_SENSOR_ID                        0x3108
 #define S5K3P8STECH_SENSOR_ID                   0xf3108
@@ -373,6 +375,7 @@
 #define SENSOR_DRVNAME_S5K3L8_MIPI_RAW          "s5k3l8_mipi_raw"
 #define SENSOR_DRVNAME_S5K3M3_MIPI_RAW          "s5k3m3_mipi_raw"
 #define SENSOR_DRVNAME_S5K2X8_MIPI_RAW          "s5k2x8_mipi_raw"
+#define SENSOR_DRVNAME_S5K2P7_MIPI_RAW          "s5k2p7_mipi_raw"
 #define SENSOR_DRVNAME_S5K2P8_MIPI_RAW          "s5k2p8_mipi_raw"
 #define SENSOR_DRVNAME_S5K3P3SX_MIPI_RAW        "s5k3p3sx_mipi_raw"
 #define SENSOR_DRVNAME_S5K3P3_MIPI_RAW          "s5k3p3_mipi_raw"
