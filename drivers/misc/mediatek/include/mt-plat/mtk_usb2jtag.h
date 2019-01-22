699a3cb548e9132d92db59ae97d8689de28f10dc
mtk05337 <chieh-jay.liu@mediatek.com>
2019-01-22 22:24:49 +0800

    [ALPS03031077] USB2JTAG: USB2JTAG revision
    [Detail] USB2JTAG violated the rule of coding standard
    1. change mt_xxx() to mtk_xxx()
    2. delete redundant comments.
    
    [Solution] Make USB2JTAG meet the rule of coding standard
    
    MTK-Commit-Id: 6d41d8d4d272bcc9fbc32a4f3ccacdd5e55a0207
    
    Change-Id: I5e56b8891ed6ecb0a3b95400eb451ef83c8f27e3
    Signed-off-by: mtk05337 <chieh-jay.liu@mediatek.com>
    CR-Id: ALPS03031077
    Feature: Others

--- drivers/misc/mediatek/include/mt-plat/mtk_usb2jtag.h
+++ drivers/misc/mediatek/include/mt-plat/mtk_usb2jtag.h
@@ -13,6 +13,6 @@
 
 #ifndef __MTK_USB2JTAG_H_
 #define __MTK_USB2JTAG_H_
-int mt_usb2jtag_resume(void);
+int mtk_usb2jtag_resume(void);
 unsigned int usb2jtag_mode(void);
 #endif
--- drivers/misc/mediatek/usb2jtag/usb2jtag_v1.h
+++ drivers/misc/mediatek/usb2jtag/usb2jtag_v1.h
@@ -14,8 +14,8 @@
 #ifndef __USB2JTAG_H_
 #define __USB2JTAG_H_
 unsigned int usb2jtag_mode(void);
-struct mt_usb2jtag_driver *get_mt_usb2jtag_drv(void);
-struct mt_usb2jtag_driver {
+struct mtk_usb2jtag_driver *get_mtk_usb2jtag_drv(void);
+struct mtk_usb2jtag_driver {
 	int	(*usb2jtag_init)(void);
 	int	(*usb2jtag_resume)(void);
 	int	(*usb2jtag_suspend)(void);
