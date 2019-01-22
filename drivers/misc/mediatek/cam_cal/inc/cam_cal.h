1fa5a3e38ce11909a4ff3eae460e19b4b4bd3002
Dream Yeh <dream.yeh@mediatek.com>
2019-01-22 22:57:46 +0800

    [ALPS03183860] EEPROM : remove warning message
    [Detail] To remove all warning message when build kernel
    
    [Solution]
    
    MTK-Commit-Id: 5e38de8492d9ffff4f95095ca2431f057786dbdb
    
    Change-Id: I5aeb5ae903062f8c6606203f60350fdb2028d31f
    Signed-off-by: Dream Yeh <dream.yeh@mediatek.com>
    CR-Id: ALPS03183860
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/cam_cal/inc/cam_cal.h
+++ drivers/misc/mediatek/cam_cal/inc/cam_cal.h
@@ -34,13 +34,13 @@
 ********************************************************************************/
 
 /*CAM_CAL write*/
-#define CAM_CALIOC_S_WRITE           _IOW(CAM_CALAGIC, 0, stCAM_CAL_INFO_STRUCT)
+#define CAM_CALIOC_S_WRITE           _IOW(CAM_CALAGIC, 0, struct stCAM_CAL_INFO_STRUCT)
 /*CAM_CAL read*/
-#define CAM_CALIOC_G_READ            _IOWR(CAM_CALAGIC, 5, stCAM_CAL_INFO_STRUCT)
+#define CAM_CALIOC_G_READ            _IOWR(CAM_CALAGIC, 5, struct stCAM_CAL_INFO_STRUCT)
 
 #ifdef CONFIG_COMPAT
-#define COMPAT_CAM_CALIOC_S_WRITE    _IOW(CAM_CALAGIC, 0, COMPAT_stCAM_CAL_INFO_STRUCT)
-#define COMPAT_CAM_CALIOC_G_READ    _IOWR(CAM_CALAGIC, 5, COMPAT_stCAM_CAL_INFO_STRUCT)
+#define COMPAT_CAM_CALIOC_S_WRITE    _IOW(CAM_CALAGIC, 0, struct COMPAT_stCAM_CAL_INFO_STRUCT)
+#define COMPAT_CAM_CALIOC_G_READ    _IOWR(CAM_CALAGIC, 5, struct COMPAT_stCAM_CAL_INFO_STRUCT)
 #endif
 #endif /*_CAM_CAL_H*/
 
