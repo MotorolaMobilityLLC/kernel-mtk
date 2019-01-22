e59cca46ddbd321444d054569dc04b6af47c9030
Kendrick Hsu <kendrick.hsu@mediatek.com>
2019-01-23 00:03:41 +0800

    [ALPS03631095] cmdq: revise cpr size
    reduce cpr size to avoid touch invalid address
    
    MTK-Commit-Id: a04a8f2bb7202f49eb431be3484d70a68e11e406
    
    Change-Id: I939f2ac156159be239a4fc29a854cee78f016687
    Signed-off-by: Kendrick Hsu <kendrick.hsu@mediatek.com>
    CR-Id: ALPS03631095
    Feature: CMDQ

--- drivers/misc/mediatek/cmdq/v3/cmdq_record.c
+++ drivers/misc/mediatek/cmdq/v3/cmdq_record.c
@@ -2160,6 +2160,7 @@ s32 cmdq_task_create_delay_thread_sram(void **pp_delay_thread_buffer, u32 *buffe
 
 	cmdq_op_finalize_command(handle, true);
 
+	*buffer_size = handle->blockSize;
 	if (handle->blockSize <= 0 || handle->pBuffer == NULL) {
 		CMDQ_ERR("REC: create delay thread fail, block_size = %d\n", handle->blockSize);
 		cmdq_task_destroy(handle);
@@ -2196,7 +2197,6 @@ s32 cmdq_task_create_delay_thread_sram(void **pp_delay_thread_buffer, u32 *buffe
 	if (*pp_delay_thread_buffer != NULL)
 		kfree(*pp_delay_thread_buffer);
 	*pp_delay_thread_buffer = p_new_buffer;
-	*buffer_size = handle->blockSize;
 
 	cmdq_task_destroy(handle);
 	return 0;
