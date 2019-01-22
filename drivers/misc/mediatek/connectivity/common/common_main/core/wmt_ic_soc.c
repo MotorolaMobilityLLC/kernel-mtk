bba155eb6126300e642abb6f389b2952f05c7c8d
Danny Shih <danny.shih@mediatek.com>
2019-01-22 23:04:28 +0800

    [ALPS03314972] WMT: mt6739 early porting
    [Detail] Add MT6739 related code for Consys common part driver
    
    [Solution]
    Modify SOC chipid & PMIC chipid
    Modify wmt driver
    Modify Kconfig
    
    MTK-Commit-Id: 2d000945c28b354fec4204412057084973f6d544
    
    Change-Id: Ie7a8bf6963a6b0711fda7bed976063b88ffc8680
    Feature: WMT
    Signed-off-by: Danny Shih <danny.shih@mediatek.com>
    CR-Id: ALPS03314972

--- drivers/misc/mediatek/connectivity/common/common_main/core/wmt_ic_soc.c
+++ drivers/misc/mediatek/connectivity/common/common_main/core/wmt_ic_soc.c
@@ -1082,8 +1082,7 @@ static INT32 mtk_wcn_soc_sw_init(P_WMT_HIF_CONF pWmtHifConf)
 
 #if CFG_WMT_PATCH_DL_OPTM
 	if (wmt_ic_ops_soc.icId == 0x0279 ||
-		wmt_ic_ops_soc.icId == 0x0507 ||
-		wmt_ic_ops_soc.icId == 0x0668) {
+		wmt_ic_ops_soc.icId == 0x0699) {
 		iRet = wmt_core_init_script(set_mcuclk_table_3, osal_array_size(set_mcuclk_table_3));
 		if (iRet)
 			WMT_ERR_FUNC("set_mcuclk_table_3 fail(%d)\n", iRet);
@@ -1109,8 +1108,7 @@ static INT32 mtk_wcn_soc_sw_init(P_WMT_HIF_CONF pWmtHifConf)
 
 #if CFG_WMT_PATCH_DL_OPTM
 	if (wmt_ic_ops_soc.icId == 0x0279 ||
-		wmt_ic_ops_soc.icId == 0x0507 ||
-		wmt_ic_ops_soc.icId == 0x0688) {
+		wmt_ic_ops_soc.icId == 0x0699) {
 		iRet = wmt_core_init_script(set_mcuclk_table_4, osal_array_size(set_mcuclk_table_4));
 		if (iRet)
 			WMT_ERR_FUNC("set_mcuclk_table_4 fail(%d)\n", iRet);
@@ -1170,6 +1168,7 @@ static INT32 mtk_wcn_soc_sw_init(P_WMT_HIF_CONF pWmtHifConf)
 		(wmt_ic_ops_soc.icId == 0x0321) ||
 		(wmt_ic_ops_soc.icId == 0x0335) ||
 		(wmt_ic_ops_soc.icId == 0x0688) ||
+		(wmt_ic_ops_soc.icId == 0x0699) ||
 		(wmt_ic_ops_soc.icId == 0x0337)) {
 		wmt_stp_wifi_lte_coex();
 		WMT_DBG_FUNC("wmt_stp_wifi_lte_coex done!\n");
@@ -1545,8 +1544,7 @@ static INT32 mtk_wcn_soc_gps_sync_ctrl(WMT_IC_PIN_STATE state, UINT32 flag)
 	 * may cause bus hang
 	 */
 	if (wmt_ic_ops_soc.icId != 0x0279 &&
-		wmt_ic_ops_soc.icId != 0x0507 &&
-		wmt_ic_ops_soc.icId != 0x0688) {
+		wmt_ic_ops_soc.icId != 0x0699) {
 		if (state == WMT_IC_PIN_MUX)
 			uVal = 0x1 << 28;
 		else
@@ -1783,8 +1781,7 @@ static INT32 wmt_stp_wifi_lte_coex(VOID)
 			    wmt_core_init_script(set_wifi_lte_coex_table_1, osal_array_size(set_wifi_lte_coex_table_1));
 			WMT_DBG_FUNC("wmt_core:set_wifi_lte_coex_table_1 %s(%d)\n", iRet ? "fail" : "ok", iRet);
 		} else if (wmt_ic_ops_soc.icId == 0x0279 ||
-				wmt_ic_ops_soc.icId == 0x0507 ||
-				wmt_ic_ops_soc.icId == 0x0688) {
+				wmt_ic_ops_soc.icId == 0x0699) {
 			/* add WMT_COXE_CONFIG_EXT_COMPONENT_OPCODE command for 2G4 eLNA demand*/
 			if (pWmtGenConf->coex_wmt_ext_component) {
 				WMT_INFO_FUNC("coex_wmt_ext_component:0x%x\n", pWmtGenConf->coex_wmt_ext_component);
@@ -2223,8 +2220,7 @@ static INT32 mtk_wcn_soc_patch_dwn(UINT32 index)
 		WMT_PATCH_P_ADDRESS_CMD[9] = 0x0b;
 	}
 
-	if (wmt_ic_ops_soc.icId == 0x0507 ||
-		wmt_ic_ops_soc.icId == 0x0688) {
+	if (wmt_ic_ops_soc.icId == 0x0699) {
 		/* ROMv4 patch RAM base */
 		WMT_PATCH_ADDRESS_CMD[8] = 0x18;
 		WMT_PATCH_ADDRESS_CMD[9] = 0x05;
