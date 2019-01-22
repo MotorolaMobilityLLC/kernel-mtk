58cf9d026c759bb6e2ed3d5ac687abe3685218dd
Seiya Wang <seiya.wang@mediatek.com>
2019-01-23 00:08:13 +0800

    [ALPS04017777] mtee: enable counter
    Enable tz counter
    
    MTK-Commit-Id: 0e3f071c482ea2661ef651ad406be861d1c4fa52
    
    Change-Id: Idade642f277f7721d25ce79051d19425e224a5e4
    CR-Id: ALPS04017777
    Feature: MTK In-House TEE (Trusted Execution Environment)
    Signed-off-by: Seiya Wang <seiya.wang@mediatek.com>
    (cherry picked from commit 38c582ea6913e3db807b88bcd8cb5193b23af022)

--- drivers/misc/mediatek/mtee/tz_counter.h
+++ drivers/misc/mediatek/mtee/tz_counter.h
@@ -14,10 +14,8 @@
 #ifndef __KREE_INC_COUNTER
 #define __KREE_INC_COUNTER
 
-#ifdef CONFIG_ARM64
 /* enable counter implementation */
 #define ENABLE_INC_ONLY_COUNTER
-#endif
 
 int update_counter_thread(void *data);
 
