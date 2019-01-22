3a5fc4a73de13c146cb5a3ef87ab341ed18ebe6e
hs.chien <hs.chien@mediatek.com>
2019-01-22 22:35:22 +0800

    [ALPS03117058] RTC: Fix RTC get time for in/out suspend/resume scenario
    [Detail]
    When get RTC time, should take care SEC is carried or not.
    
    [Solution]
    RTC_INT_CNT should be reloaded before read this register. Or it will keep
    old value.
    
    MTK-Commit-Id: 3c39e57443b7a92cf2b415022134503e9c234260
    
    Change-Id: I07860517c842ea5cafc13d9a556e0cc4b073ed55
    CR-Id: ALPS03117058
    Feature: RTC
    Signed-off-by: HS Chien <hs.chien@mediatek.com>

--- drivers/misc/mediatek/rtc/mtk_rtc_hal_common.c
+++ drivers/misc/mediatek/rtc/mtk_rtc_hal_common.c
@@ -153,13 +153,13 @@ u16 hal_rtc_get_spare_register(rtc_spare_enum cmd)
 
 static void rtc_get_tick(struct rtc_time *tm)
 {
+	tm->tm_cnt = rtc_read(RTC_INT_CNT);
 	tm->tm_sec = rtc_read(RTC_TC_SEC);
 	tm->tm_min = rtc_read(RTC_TC_MIN);
 	tm->tm_hour = rtc_read(RTC_TC_HOU);
 	tm->tm_mday = rtc_read(RTC_TC_DOM);
 	tm->tm_mon = rtc_read(RTC_TC_MTH);
 	tm->tm_year = rtc_read(RTC_TC_YEA);
-	tm->tm_cnt = rtc_read(RTC_INT_CNT);
 }
 
 void hal_rtc_get_tick_time(struct rtc_time *tm)
@@ -170,6 +170,8 @@ void hal_rtc_get_tick_time(struct rtc_time *tm)
 	rtc_write(RTC_BBPU, bbpu);
 	rtc_write_trigger();
 	rtc_get_tick(tm);
+	bbpu = rtc_read(RTC_BBPU) | RTC_BBPU_KEY | RTC_BBPU_RELOAD;
+	rtc_write(RTC_BBPU, bbpu);
 	if (rtc_read(RTC_INT_CNT) < tm->tm_cnt) {	/* SEC has carried */
 		rtc_get_tick(tm);
 	}
