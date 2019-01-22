544347e9109a932d3fedfecc3dd5fbbd589563d1
jiachiam.liu <jiachiam.liu@mediatek.com>
2019-01-23 00:01:22 +0800

    [ALPS03379927] sspm: update EMI MPU API
    [Detail]
    Use new EMI MPU API for mt6771 & mt6775
    
    MTK-Commit-Id: ad309e6b864f87130550aa22e7d52a58dfaa4a9d
    
    Change-Id: I01248f076f1455fee59e94f8ed8060b90271945f
    CR-Id: ALPS03379927
    Feature: TinySys
    Signed-off-by: jiachiam.liu <jiachiam.liu@mediatek.com>

--- drivers/misc/mediatek/sspm/mt6771/sspm_emi_mpu.c
+++ drivers/misc/mediatek/sspm/mt6771/sspm_emi_mpu.c
@@ -18,30 +18,23 @@
 
 
 #if SSPM_EMI_PROTECTION_SUPPORT
-#include <emi_mpu.h>
+#include <mt_emi_api.h>
 
 void sspm_set_emi_mpu(phys_addr_t base, phys_addr_t size)
 {
-	unsigned long long shr_mem_phy_start, shr_mem_phy_end;
-	int shr_mem_mpu_id;
-	unsigned long long shr_mem_mpu_attr;
+	struct emi_region_info_t region_info;
 
-	shr_mem_mpu_id = SSPM_MPU_REGION_ID;
-	shr_mem_phy_start = base;
-	shr_mem_phy_end = base + size - 1;
-	shr_mem_mpu_attr = SET_ACCESS_PERMISSON(UNLOCK,
+	region_info.region = SSPM_MPU_REGION_ID;
+	region_info.start = base;
+	region_info.end = base + size - 1;
+	SET_ACCESS_PERMISSION(region_info.apc, UNLOCK,
 			FORBIDDEN, FORBIDDEN, FORBIDDEN, FORBIDDEN,
 			FORBIDDEN, FORBIDDEN, FORBIDDEN, NO_PROTECTION,
 			FORBIDDEN, FORBIDDEN, FORBIDDEN, FORBIDDEN,
 			FORBIDDEN, FORBIDDEN, FORBIDDEN, NO_PROTECTION);
-	pr_debug("[SSPM] MPU Start protect SSPM Share region<%d:%08llx:%08llx> %llx\n",
-			shr_mem_mpu_id, shr_mem_phy_start, shr_mem_phy_end, shr_mem_mpu_attr);
+	pr_debug("[SSPM] MPU Start protect SSPM Share region<%d:%08llx:%08llx> %x, %x\n",
+			region_info.apc[1], region_info.apc[0]);
 
-	emi_mpu_set_region_protection(
-			shr_mem_phy_start,  /*START_ADDR */
-			shr_mem_phy_end,    /*END_ADDR */
-			shr_mem_mpu_id,     /*region */
-			shr_mem_mpu_attr);
-
+	emi_mpu_set_protection(&region_info);
 }
 #endif
