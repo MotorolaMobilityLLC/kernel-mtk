b28abf5866b9ac878b1a0a8c14b38f801de1613f
Cheng-En Chung <cheng-en.chung@mediatek.com>
2019-01-22 21:28:06 +0800

    [ALPS02894131] Leakage Table: Porting and Enable
    [Detail]
    Porting and Enable Leakage Table in MT6799
    
    MTK-Commit-Id: a3575c625f8f2b35d19d02955f11dfce5e8d407b
    
    Change-Id: Ibf19e9a42c1388c57bfdac1d027dd8ff9800967f
    Signed-off-by: Cheng-En Chung <cheng-en.chung@mediatek.com>
    CR-Id: ALPS02894131
    Feature: Power Management

---
+++ drivers/misc/mediatek/base/power/include/leakage_table_v1/mtk_static_power.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#ifndef __MTK_STATIC_POWER_H__
+#define __MTK_STATIC_POWER_H__
+
+#if defined(CONFIG_MACH_MT6799)
+#include "mtk_static_power_mtk6799.h"
+#endif
+
+/* #define MTK_SPOWER_UT */
+extern u32 get_devinfo_with_index(u32 index);
+
+/*
+ * @argument
+ * dev: the enum of MT_SPOWER_xxx
+ * voltage: the operating voltage, mV.
+ * degree: the Tj. (degree C)
+ * @return
+ *  -1, means sptab is not yet ready.
+ *  other value: the mW of leakage value.
+ */
+extern int mt_spower_get_leakage(int dev, unsigned int voltage, int degree);
+extern int mt_spower_get_efuse_lkg(int dev);
+
+extern int mt_spower_init(void);
+
+
+#endif
