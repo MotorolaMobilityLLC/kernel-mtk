cc0e740f426572520b587a2e7c624e883ef90804
mtk01157 <cj.yang@mediatek.com>
2019-01-22 21:32:02 +0800

    [ALPS02902389] GPU: mt8167 GPU bring-up
    [Detail]
    update gpu bring-up kernel part, add
    1. mt8167 gpu platform dependence code
    2. enable HW GPU in kernel config
    3. add gpu dvfs code
    4. add gpu device tree
    
    MTK-Commit-Id: 681f733eec4187708ea8d6f5021d0b8838347022
    
    Change-Id: I75112425a4d82a9e3c9138c431a4d951430469a5
    Signed-off-by: mtk01157 <cj.yang@mediatek.com>
    CR-Id: ALPS02902389
    Feature: GPU

--- drivers/misc/mediatek/gpu/ged/src/ged_dvfs.c
+++ drivers/misc/mediatek/gpu/ged/src/ged_dvfs.c
@@ -202,11 +202,16 @@ unsigned long ged_query_info( GED_INFO eType)
 		case GED_LATEST_START:
 			return g_ulPreCalResetTS_us;
 		case GED_FPS: {
+#if defined(CONFIG_ARCH_MT6755) || defined(CONFIG_ARCH_MT6757) || \
+	defined(CONFIG_ARCH_MT6797)
 			struct disp_session_info info;
 			memset(&info, 0, sizeof(info));
 			info.session_id = MAKE_DISP_SESSION(DISP_SESSION_PRIMARY, 0);
 			disp_mgr_get_session_info(&info);
 			return info.updateFPS;
+#else
+			return 0;
+#endif
 		}
 		default:
 			return 0;
