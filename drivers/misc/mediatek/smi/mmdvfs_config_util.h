f9cb4004a3a01b8c11ea45fc4a3e00304f09c6ac
Frederic Chen <frederic.chen@mediatek.com>
2019-01-22 21:54:48 +0800

    [ALPS02959910] MMDVFS: initial version of configurable profile
    [Detail] Support scenario, voltage and clks configurtion
    
    [Solution] Added configurtion headers and adaptors
    
    MTK-Commit-Id: b700a94c45eec0328cfd2621e53080f1dbcceee5
    
    Change-Id: I121e1334d29dbdd351b7a18d94131b0c96bdc71d
    Signed-off-by: Frederic Chen <frederic.chen@mediatek.com>
    CR-Id: ALPS02959910
    Feature: MMDVFS

---
+++ drivers/misc/mediatek/smi/mmdvfs_config_util.h
@@ -0,0 +1,187 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+
+#ifndef __MMDVFS_CONFIG_UTIL_H__
+#define __MMDVFS_CONFIG_UTIL_H__
+
+#include "mtk_smi.h"
+#include "mmdvfs_mgr.h"
+
+
+#define MMDVFS_CLK_CONFIG_BY_MUX    (0)
+#define MMDVFS_CLK_CONFIG_HOPPING   (1)
+#define MMDVFS_CLK_CONFIG_NONE      (2)
+
+/* System OPP number limitation (Max opp support)*/
+#define MMDVFS_OPP_NUM_LIMITATION		(6)    /* Max total MMDVFS opps of the profile support */
+#define MMDVFS_CLK_OPP_NUM_LIMITATION		(6) /* Max total CLK opps of the profile support */
+#define MMDVFS_VCORE_OPP_NUM_LIMITATION		(6) /* Max total CLK opps of the profile support */
+
+/* MMDVFS OPPs (External interface, Vcore and MMCLK combined)*/
+#define MMDVFS_FINE_STEP_UNREQUEST (-1)
+#define MMDVFS_FINE_STEP_OPP0 (0)
+#define MMDVFS_FINE_STEP_OPP1 (1)
+#define MMDVFS_FINE_STEP_OPP2 (2)
+#define MMDVFS_FINE_STEP_OPP3 (3)
+#define MMDVFS_FINE_STEP_OPP4 (4)
+#define MMDVFS_FINE_STEP_OPP5 (5)
+
+
+
+/* MMCLK opps (internal used in MMDVFS) */
+#define MMDVFS_MMCLK_OPP0 (0)
+#define MMDVFS_MMCLK_OPP1 (1)
+#define MMDVFS_MMCLK_OPP2 (2)
+#define MMDVFS_MMCLK_OPP3 (3)
+#define MMDVFS_MMCLK_OPP4 (4)
+#define MMDVFS_MMCLK_OPP5 (5)
+
+
+/* Defined the max size of MMDVFS CLK MUX table */
+#define MMDVFS_CLK_MUX_NUM_LIMITATION (15)
+
+/* CLK MUX ID */
+/* Define the internal index of each MM CLK */
+#define MMDVFS_CLK_MUX_TOP_SMI0_2X_SEL  (0)
+#define MMDVFS_CLK_MUX_TOP_MM_SEL   (1)
+#define MMDVFS_CLK_MUX_TOP_CAM_SEL  (2)
+#define MMDVFS_CLK_MUX_TOP_IMG_SEL  (3)
+#define MMDVFS_CLK_MUX_TOP_VENC_SEL (4)
+#define MMDVFS_CLK_MUX_TOP_VDEC_SEL (5)
+#define MMDVFS_CLK_MUX_TOP_MJC_SEL  (6)
+#define MMDVFS_CLK_MUX_TOP_DSP_SEL  (7)
+#define MMDVFS_CLK_MUX_TOP_VPU_IF_SEL  (8)
+#define MMDVFS_CLK_MUX_TOP_VPU_SEL  (9)
+#define MMDVFS_CLK_MUX_NUM (10)
+
+
+/* Configuration Header */
+struct mmdvfs_cam_property {
+	int sensor_size;
+	int feature_flag;
+	int fps;
+};
+
+struct mmdvfs_video_property {
+	int width;
+	int height;
+	int codec;
+};
+
+struct mmdvfs_profile {
+	const char *profile_name;
+	int smi_scenario_id;
+	struct mmdvfs_cam_property cam_limit;
+	struct mmdvfs_video_property video_limit;
+};
+
+/* HW Configuration Management */
+
+struct mmdvfs_clk_mux_desc {
+	void *ccf_handle;
+	char *ccf_name; /* For debugging*/
+};
+
+struct mmdvfs_clk_source_desc {
+	void *ccf_handle;
+	char *ccf_name; /* For debugging*/
+	int requested_clk; /* For debugging*/
+};
+
+struct mmdvfs_clk_hw_map {
+	int config_method; /* 0: don't set, 1: clk_mux, 2: pll hopping */
+	struct mmdvfs_clk_mux_desc clk_mux;
+	int pll_id;
+	int total_step;
+	/* DSS value of each step for this clk */
+	int step_hopping_dds_map[MMDVFS_CLK_OPP_NUM_LIMITATION];
+	/* CLK MUX of each step for this clk */
+	int step_clk_source_id_map[MMDVFS_CLK_OPP_NUM_LIMITATION];
+};
+
+struct mmdvfs_clk_hw_configuration {
+	int config_method; /* 0: don't set, 1: clk_mux, 2: pll hopping */
+	struct mmdvfs_clk_mux_desc clk_mux;
+	int pll_id;
+	int total_steps;
+	int step_hopping_dds_map[MMDVFS_CLK_MUX_NUM_LIMITATION];
+	int step_clk_source_id_map[MMDVFS_CLK_MUX_NUM_LIMITATION];
+};
+
+/* Record vcore and step of clks to be requested for a single MMDVFS step */
+struct mmdvfs_hw_configurtion {
+	int vcore_step;
+	int clk_steps[MMDVFS_CLK_MUX_NUM_LIMITATION];
+	int total_clks;
+};
+
+/* For a single mmdvfs step's profiles and associated hardware configuration */
+struct mmdvfs_step_to_profile_mapping {
+	int mmdvfs_step;
+	struct mmdvfs_profile *profiles;
+	int total_profiles;
+	struct mmdvfs_hw_configurtion hw_config;
+};
+
+struct mmdvfs_adaptor {
+	int enable_vcore;
+	int enable_clk_mux;
+	int enable_pll_hopping;
+	struct mmdvfs_clk_source_desc *mmdvfs_clk_sources;
+	int mmdvfs_clk_sources_num;
+	struct mmdvfs_clk_hw_map *mmdvfs_clk_hw_maps;
+	int mmdvfs_clk_hw_maps_num;
+	struct mmdvfs_step_to_profile_mapping *step_profile_mappings;
+	int step_num;
+	void (*profile_dump_func)(struct mmdvfs_adaptor *self);
+	void (*single_hw_configuration_dump_func)(struct mmdvfs_adaptor *self,
+	struct mmdvfs_hw_configurtion *hw_configuration);
+	void (*hw_configuration_dump_func)(struct mmdvfs_adaptor *self);
+	int (*determine_mmdvfs_step)(struct mmdvfs_adaptor *self,
+	int smi_scenario, struct mmdvfs_cam_property *cam_setting,
+	struct mmdvfs_video_property *codec_setting);
+	int (*apply_hw_configurtion_by_step)(struct mmdvfs_adaptor *self, int mmdvfs_step, int current_step);
+	int (*apply_vcore_hw_configurtion_by_step)(struct mmdvfs_adaptor *self, int mmdvfs_step);
+	int (*apply_clk_hw_configurtion_by_step)(struct mmdvfs_adaptor *self, int mmdvfs_step);
+	int (*get_cam_sys_clk)(struct mmdvfs_adaptor *self, int mmdvfs_step);
+	/* static members */
+	void (*single_profile_dump_func)(struct mmdvfs_profile *profile);
+
+};
+
+
+struct mmdvfs_step_util {
+	/* Record each scenaio's opp */
+	int mmdvfs_scenario_mmdvfs_opp[MMDVFS_SCEN_COUNT];
+	int total_scenario;
+	/* Record the currency status of each opps*/
+	int mmdvfs_concurrency_of_opps[MMDVFS_OPP_NUM_LIMITATION];
+	int total_opps;
+	int *mmclk_oop_to_legacy_step;
+	int mmclk_oop_to_legacy_step_num;
+	int *legacy_step_to_oop;
+	int legacy_step_to_oop_num;
+	void (*init)(struct mmdvfs_step_util *self);
+	int (*get_legacy_mmclk_step_from_mmclk_opp)(
+	struct mmdvfs_step_util *self, int mmclk_step);
+	int (*get_opp_from_legacy_step)(struct mmdvfs_step_util *self,
+	int legacy_step);
+	int (*set_step)(struct mmdvfs_step_util *self, int step, int client_id);
+};
+
+extern struct mmdvfs_adaptor *g_mmdvfs_adaptor;
+extern struct mmdvfs_step_util *g_mmdvfs_step_util;
+void mmdvfs_config_util_init(void);
+
+#endif /* __MMDVFS_CONFIG_UTIL_H__ */
---
+++ drivers/misc/mediatek/smi/mmdvfs_profile_adaptor.c
@@ -0,0 +1,572 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/uaccess.h>
+#include <linux/clk.h>
+#include <mtk_vcorefs_manager.h>
+
+#include "mmdvfs_config_mt6799.h"
+#include "mtk_smi.h"
+#include "mmdvfs_mgr.h"
+
+
+
+/* Class: mmdvfs_step_util */
+static int mmdvfs_get_legacy_mmclk_step_from_mmclk_opp(
+struct mmdvfs_step_util *self, int mmclk_step);
+static int mmdvfs_get_opp_from_legacy_step(struct mmdvfs_step_util *self,
+int legacy_step);
+static void mmdvfs_step_util_init(struct mmdvfs_step_util *self);
+static int mmdvfs_step_util_set_step(struct mmdvfs_step_util *self, int step,
+int client_id);
+
+struct mmdvfs_step_util mmdvfs_step_util_obj = {
+	{0},
+	MMDVFS_SCEN_COUNT,
+	{0},
+	MMDVFS_OPP_MAX,
+	mmdvfs_legacy_step_to_opp,
+	MMDVFS_VOLTAGE_COUNT,
+	mmdvfs_mmclk_opp_to_legacy_mmclk_step,
+	MMDVFS_OPP_MAX,
+	mmdvfs_step_util_init,
+	mmdvfs_get_legacy_mmclk_step_from_mmclk_opp,
+	mmdvfs_get_opp_from_legacy_step,
+	mmdvfs_step_util_set_step
+};
+
+/* Class: mmdvfs_adaptor */
+static void mmdvfs_single_profile_dump(struct mmdvfs_profile *profile);
+static void mmdvfs_profile_dump(struct mmdvfs_adaptor *self);
+static void mmdvfs_single_hw_configuration_dump(struct mmdvfs_adaptor *self,
+struct mmdvfs_hw_configurtion *hw_configuration);
+static void mmdvfs_hw_configuration_dump(struct mmdvfs_adaptor *self);
+static int mmdvfs_determine_step(struct mmdvfs_adaptor *self, int smi_scenario,
+struct mmdvfs_cam_property *cam_setting,
+struct mmdvfs_video_property *codec_setting);
+static int mmdvfs_apply_hw_configurtion_by_step(struct mmdvfs_adaptor *self, int mmdvfs_step, int current_step);
+static int mmdvfs_apply_vcore_hw_configurtion_by_step(struct mmdvfs_adaptor *self, int mmdvfs_step);
+static int mmdvfs_apply_clk_hw_configurtion_by_step(struct mmdvfs_adaptor *self, int mmdvfs_step);
+static int mmdvfs_get_cam_sys_clk(struct mmdvfs_adaptor *self, int mmdvfs_step);
+
+static int is_camera_profile_matched(struct mmdvfs_cam_property *cam_setting,
+struct mmdvfs_cam_property *profile_property);
+
+static int is_video_profile_matched(
+struct mmdvfs_video_property *video_setting,
+struct mmdvfs_video_property *profile_property);
+
+struct mmdvfs_adaptor mmdvfs_adaptor_obj = {
+	0, 0, 0,
+	mmdvfs_clk_sources_setting, MMDVFS_CLK_SOURCE_NUM,
+	mmdvfs_clk_hw_map_setting, MMDVFS_CLK_MUX_NUM,
+	mmdvfs_step_to_profile_mappings_setting, MMDVFS_OPP_MAX,
+	mmdvfs_profile_dump,
+	mmdvfs_single_hw_configuration_dump,
+	mmdvfs_hw_configuration_dump,
+	mmdvfs_determine_step,
+	mmdvfs_apply_hw_configurtion_by_step,
+	mmdvfs_apply_vcore_hw_configurtion_by_step,
+	mmdvfs_apply_clk_hw_configurtion_by_step,
+	mmdvfs_get_cam_sys_clk,
+	mmdvfs_single_profile_dump,
+};
+
+/* Member function implementation */
+static int mmdvfs_apply_hw_configurtion_by_step(struct mmdvfs_adaptor *self,
+int mmdvfs_step, const int current_step)
+{
+	MMDVFSMSG("current = %d, target = %d\n", current_step, mmdvfs_step);
+	if (current_step == -1 || ((mmdvfs_step != -1) && (mmdvfs_step
+	<= current_step))) {
+		MMDVFSMSG("Apply Vcore setting(%d --> %d):\n", current_step,
+		mmdvfs_step);
+		MMDVFSMSG("current = %d, target = %d\n", current_step, mmdvfs_step);
+		self->apply_vcore_hw_configurtion_by_step(self, mmdvfs_step);
+		MMDVFSMSG("Apply CLK setting:\n");
+		self->apply_clk_hw_configurtion_by_step(self, mmdvfs_step);
+	} else {
+		MMDVFSMSG("Apply CLK setting:\n");
+		self->apply_clk_hw_configurtion_by_step(self, mmdvfs_step);
+		MMDVFSMSG("Apply Vcore setting:\n");
+		self->apply_vcore_hw_configurtion_by_step(self, mmdvfs_step);
+	}
+
+	return 0;
+}
+
+static int mmdvfs_apply_vcore_hw_configurtion_by_step(
+struct mmdvfs_adaptor *self, int mmdvfs_step)
+{
+	struct mmdvfs_hw_configurtion *hw_config_ptr = NULL;
+	int vcore_step = 0;
+
+	if (self->enable_vcore == 0) {
+		MMDVFSMSG("Doesn't change Vcore setting: vcore module is disable in MMDVFS\n");
+		return -1;
+	}
+
+	/* Check unrequest step */
+	if (mmdvfs_step == MMDVFS_FINE_STEP_UNREQUEST) {
+		vcore_step = OPP_UNREQ;
+	} else {
+		/* Check if the step is legall */
+		if (mmdvfs_step < 0 || mmdvfs_step >= self->step_num
+		|| (self->step_profile_mappings + mmdvfs_step) == NULL)
+			return -1;
+
+		/* Get hw configurtion fot the step */
+		hw_config_ptr
+		= &(self->step_profile_mappings + mmdvfs_step)->hw_config;
+
+		/* Check if mmdvfs_hw_configurtion is found */
+		if (hw_config_ptr == NULL)
+			return -1;
+
+		/* Get vcore step */
+		vcore_step = hw_config_ptr->vcore_step;
+	}
+	vcorefs_request_dvfs_opp(KIR_MM, vcore_step);
+	/* Set vcore step */
+	MMDVFSMSG("Set vcore step: %d\n", vcore_step);
+
+	return 0;
+
+}
+
+static int mmdvfs_apply_clk_hw_configurtion_by_step(
+struct mmdvfs_adaptor *self, int mmdvfs_step_request)
+{
+	struct mmdvfs_hw_configurtion *hw_config_ptr = NULL;
+	int clk_idx = 0;
+	int mmdvfs_step = -1;
+
+	if (self->enable_clk_mux == 0) {
+		MMDVFSMSG("Doesn't change clk mux setting: clk_mux module is disable in MMDVFS\n");
+		return -1;
+	}
+
+	/* Check unrequest step and reset the mmdvfs step to the lowest one */
+	if (mmdvfs_step_request == -1)
+		mmdvfs_step = self->step_num - 1;
+	else
+		mmdvfs_step = mmdvfs_step_request;
+
+	/* Check if the step is legall */
+	if (mmdvfs_step < 0 || mmdvfs_step >= self->step_num
+	|| (self->step_profile_mappings + mmdvfs_step) == NULL)
+		return -1;
+
+	/* Get hw configurtion fot the step */
+	hw_config_ptr = &(self->step_profile_mappings + mmdvfs_step)->hw_config;
+
+	/* Check if mmdvfs_hw_configurtion is found */
+	if (hw_config_ptr == NULL)
+		return -1;
+
+	MMDVFSMSG("CLK SWITCH: total = %d\n", hw_config_ptr->total_clks);
+
+	/* Get each clk and setp it accord to config method */
+	for (clk_idx = 0; clk_idx < hw_config_ptr->total_clks; clk_idx++) {
+		/* Get the clk step setting of each mm clks */
+		int clk_step = hw_config_ptr->clk_steps[clk_idx];
+		/* Get the specific clk descriptor */
+		struct mmdvfs_clk_hw_map *clk_hw_map_ptr =
+		&(self->mmdvfs_clk_hw_maps[clk_idx]);
+
+		if (clk_step < 0 || clk_step >= clk_hw_map_ptr->total_step) {
+			MMDVFSMSG("invalid clk step (%d) for %s\n", clk_step,
+			clk_hw_map_ptr->clk_mux.ccf_name);
+		} else {
+			int clk_mux_mask = get_mmdvfs_clk_mux_mask();
+
+			if (!((1 << clk_idx) & clk_mux_mask)) {
+				MMDVFSMSG("CLK %d(%s) swich is not enabled\n",
+				clk_idx, clk_hw_map_ptr->clk_mux.ccf_name);
+				continue;
+			}
+			/* Apply the clk setting, only support mux now */
+			if (clk_hw_map_ptr->config_method
+			== MMDVFS_CLK_CONFIG_BY_MUX) {
+				/* Get clk source */
+				int
+				clk_source_id =
+				clk_hw_map_ptr->step_clk_source_id_map[clk_step];
+
+				if (clk_source_id < 0 || clk_source_id
+				>= self->mmdvfs_clk_sources_num)
+					MMDVFSMSG(
+					"invalid clk source id: %d, step:%d, mux:%s\n",
+					clk_source_id, mmdvfs_step,
+					clk_hw_map_ptr->clk_mux.ccf_name);
+				else {
+					int ccf_ret = -1;
+
+					MMDVFSMSG(
+					"Change %s source to %s, expect clk = %d\n",
+					clk_hw_map_ptr->clk_mux.ccf_name,
+					self->mmdvfs_clk_sources[clk_source_id].ccf_name,
+					self->mmdvfs_clk_sources[clk_source_id].requested_clk);
+
+					if (clk_hw_map_ptr->clk_mux.ccf_handle == NULL ||
+						self->mmdvfs_clk_sources[clk_source_id].ccf_handle == NULL) {
+						MMDVFSMSG("CCF handle can't be NULL during MMDVFS\n");
+						continue;
+					}
+
+					ccf_ret =
+						clk_prepare_enable((struct clk *)clk_hw_map_ptr->clk_mux.ccf_handle);
+					MMDVFSMSG("clk_prepare_enable: handle = %lx\n",
+					((unsigned long)clk_hw_map_ptr->clk_mux.ccf_handle));
+
+					if (ccf_ret) {
+						MMDVFSMSG("Failed to prepare clk: %s\n",
+						clk_hw_map_ptr->clk_mux.ccf_name);
+						return -1;
+					}
+
+					ccf_ret = clk_set_parent((struct clk *)clk_hw_map_ptr->clk_mux.ccf_handle,
+						(struct clk *)self->mmdvfs_clk_sources[clk_source_id].ccf_handle);
+					MMDVFSMSG("clk_set_parent: handle = (%lx,%lx), src id = %dn",
+					((unsigned long)clk_hw_map_ptr->clk_mux.ccf_handle),
+					((unsigned long)self->mmdvfs_clk_sources[clk_source_id].ccf_handle),
+					clk_source_id);
+
+
+					if (ccf_ret) {
+						MMDVFSMSG("Failed to set parent:%s,%s\n",
+						clk_hw_map_ptr->clk_mux.ccf_name,
+						self->mmdvfs_clk_sources[clk_source_id].ccf_name);
+						return -1;
+					}
+
+					clk_disable_unprepare((struct clk *)clk_hw_map_ptr->clk_mux.ccf_handle);
+					MMDVFSMSG("clk_disable_unprepare: handle = %lx\n",
+					((unsigned long)clk_hw_map_ptr->clk_mux.ccf_handle));
+				}
+			}
+		}
+	}
+	return 0;
+
+}
+
+static int mmdvfs_get_cam_sys_clk(struct mmdvfs_adaptor *self, int mmdvfs_step)
+{
+	return self->step_profile_mappings[mmdvfs_step].hw_config.clk_steps[MMDVFS_CLK_MUX_TOP_CAM_SEL];
+}
+
+/* single_profile_dump_func: */
+static void mmdvfs_single_profile_dump(struct mmdvfs_profile *profile)
+{
+	if (profile == NULL) {
+		MMDVFSMSG("mmdvfs_single_profile_dump: NULL profile found\n");
+		return;
+	}
+	MMDVFSMSG("%s, %d, (%d,%d,%d), (%d,%d,%d)\n", profile->profile_name,
+	profile->smi_scenario_id, profile->cam_limit.sensor_size,
+	profile->cam_limit.feature_flag, profile->cam_limit.fps,
+	profile->video_limit.width, profile->video_limit.height,
+	profile->video_limit.codec);
+}
+
+/* Profile Matching Util */
+static void mmdvfs_profile_dump(struct mmdvfs_adaptor *self)
+{
+	int i = 0;
+
+	struct mmdvfs_step_to_profile_mapping *profile_mapping =
+	self->step_profile_mappings;
+	if (profile_mapping == NULL)
+		MMDVFSMSG(
+		"mmdvfs_profile_dump: step_profile_mappings can't be NULL\n");
+	MMDVFSMSG("MMDVFS DUMP (%d):\n", profile_mapping->mmdvfs_step);
+	for (i = 0; i < profile_mapping->total_profiles; i++) {
+		struct mmdvfs_profile *profile = profile_mapping->profiles + i;
+
+		mmdvfs_single_profile_dump(profile);
+	}
+}
+
+static void mmdvfs_single_hw_configuration_dump(struct mmdvfs_adaptor *self,
+struct mmdvfs_hw_configurtion *hw_configuration)
+{
+
+	int i = 0;
+	const int clk_soure_total = self->mmdvfs_clk_sources_num;
+
+	struct mmdvfs_clk_source_desc *clk_sources = self->mmdvfs_clk_sources;
+
+	struct mmdvfs_clk_hw_map *clk_hw_map = self->mmdvfs_clk_hw_maps;
+
+	if (clk_hw_map == NULL) {
+		MMDVFSMSG(
+		"single_hw_configuration_dump: mmdvfs_clk_hw_maps can't be NULL\n");
+		return;
+	}
+
+	if (hw_configuration == NULL) {
+		MMDVFSMSG(
+		"single_hw_configuration_dump: hw_configuration can't be NULL\n");
+		return;
+	}
+
+	MMDVFSMSG("Vcore step: %d\n", hw_configuration->vcore_step);
+
+	for (i = 0; i < hw_configuration->total_clks; i++) {
+		char *ccf_clk_source_name = "NONE";
+		char *ccf_clk_mux_name = "NONE";
+		int requested_clk = -1;
+		int clk_step = -1;
+		int clk_source_id = -1;
+
+		struct mmdvfs_clk_hw_map *map_item = clk_hw_map + i;
+
+		clk_step = hw_configuration->clk_steps[i];
+
+		if (map_item != NULL && map_item->clk_mux.ccf_name != NULL) {
+			clk_source_id
+			= map_item->step_clk_source_id_map[clk_step];
+			ccf_clk_mux_name = map_item->clk_mux.ccf_name;
+		}
+
+		if (map_item->config_method != MMDVFS_CLK_CONFIG_NONE
+		&& clk_source_id < clk_soure_total && clk_source_id >= 0) {
+			ccf_clk_source_name
+			= clk_sources[clk_source_id].ccf_name;
+			requested_clk
+			= clk_sources[clk_source_id].requested_clk;
+		}
+
+		MMDVFSMSG("\t%s, %s, %dMhz\n", map_item->clk_mux.ccf_name,
+		ccf_clk_source_name, requested_clk);
+	}
+}
+
+static int mmdvfs_determine_step(struct mmdvfs_adaptor *self, int smi_scenario,
+struct mmdvfs_cam_property *cam_setting,
+struct mmdvfs_video_property *codec_setting)
+{
+	/* Find the matching scenario from OPP 0 to Max OPP */
+	int opp_index = 0;
+	int profile_index = 0;
+	struct mmdvfs_step_to_profile_mapping *profile_mappings =
+	self->step_profile_mappings;
+	const int opp_max_num = self->step_num;
+
+	if (profile_mappings == NULL)
+		MMDVFSMSG(
+		"mmdvfs_mmdvfs_step: step_profile_mappings can't be NULL\n");
+
+	for (opp_index = 0; opp_index < opp_max_num; opp_index++) {
+		struct mmdvfs_step_to_profile_mapping *mapping_ptr =
+		profile_mappings + opp_index;
+
+		for (profile_index = 0; profile_index
+		< mapping_ptr->total_profiles; profile_index++) {
+			/* Check if the scenario matches any profile */
+			struct mmdvfs_profile *profile_prt =
+			mapping_ptr->profiles + profile_index;
+			if (smi_scenario == profile_prt->smi_scenario_id) {
+				/* Check cam setting */
+				if (is_camera_profile_matched(cam_setting,
+				&profile_prt->cam_limit) == 0)
+					/* Doesn't match the camera property condition, skip this run */
+					continue;
+				if (is_video_profile_matched(codec_setting,
+				&profile_prt->video_limit) == 0)
+					/* Doesn't match the video property condition, skip this run */
+					continue;
+				/* Complete match, return the opp index */
+				mmdvfs_single_profile_dump(profile_prt);
+				return opp_index;
+			}
+		}
+	}
+
+	/* If there is no profile matched, return -1 (no dvfs request)*/
+	return -1;
+}
+
+/* Show each setting of opp */
+static void mmdvfs_hw_configuration_dump(struct mmdvfs_adaptor *self)
+{
+	int i = 0;
+	struct mmdvfs_step_to_profile_mapping *mapping =
+	self->step_profile_mappings;
+
+	if (mapping == NULL) {
+		MMDVFSMSG(
+		"mmdvfs_hw_configuration_dump: mmdvfs_clk_hw_maps can't be NULL");
+		return;
+	}
+
+	MMDVFSMSG("All OPP configurtion dump\n");
+	for (i = 0; i < self->step_num; i++) {
+		struct mmdvfs_step_to_profile_mapping *mapping_item = mapping
+		+ i;
+		MMDVFSMSG("MMDVFS OPP %d:\n", i);
+		if (mapping_item != NULL)
+			self->single_hw_configuration_dump_func(self,
+			&mapping_item->hw_config);
+	}
+}
+
+static int is_camera_profile_matched(struct mmdvfs_cam_property *cam_setting,
+struct mmdvfs_cam_property *profile_property)
+{
+	int is_match = 1;
+
+	/* Null pointer check: */
+	/* If the scenario doesn't has cam_setting, then there is no need to check the cam property  */
+	if (!cam_setting || !profile_property) {
+		is_match = 1;
+	} else {
+
+		/* Check the minium sensor resolution */
+		if (!(cam_setting->sensor_size >= profile_property->sensor_size))
+			is_match = 0;
+
+		/* Check the minium sensor resolution */
+		if (!(cam_setting->fps >= profile_property->fps))
+			is_match = 0;
+
+		/* Check the if the feature match */
+		/* Not match if there is no featue matching the profile's one */
+		/* 1 ==> don't change */
+		/* 0 ==> set is_match to 0 */
+		if (profile_property->feature_flag != 0
+		&& !(cam_setting->feature_flag & profile_property->feature_flag))
+			is_match = 0;
+	}
+
+	return is_match;
+}
+
+static int is_video_profile_matched(
+struct mmdvfs_video_property *video_setting,
+struct mmdvfs_video_property *profile_property)
+{
+	int is_match = 1;
+	/* Null pointer check: */
+	/* If the scenario doesn't has video_setting, then there is no need to check the video property */
+
+	if (!video_setting || !profile_property)
+		is_match = 1;
+	else {
+		if (!(video_setting->height * video_setting->width
+		>= profile_property->height * profile_property->width))
+	/* Check the minium sensor resolution */
+		is_match = 0;
+	}
+
+	return is_match;
+}
+
+static void mmdvfs_step_util_init(struct mmdvfs_step_util *self)
+{
+	int idx = 0;
+
+	for (idx = 0; idx < self->total_scenario; idx++)
+		self->mmdvfs_scenario_mmdvfs_opp[idx] = MMDVFS_FINE_STEP_UNREQUEST;
+
+	for (idx = 0; idx < self->total_opps; idx++)
+		self->mmdvfs_concurrency_of_opps[idx] = 0;
+
+}
+
+/* updat the step members only (HW independent part) */
+/* return the final step */
+static int mmdvfs_step_util_set_step(struct mmdvfs_step_util *self, int step,
+int client_id)
+{
+	int scenario_idx = 0;
+	int opp_idx = 0;
+	int final_opp = -1;
+
+	/* check step range here */
+	if (step < -1 || step >= self->total_opps)
+		return -1;
+
+	/* check invalid scenario */
+	if (client_id < 0 || client_id >= self->total_scenario)
+		return -1;
+
+	self->mmdvfs_scenario_mmdvfs_opp[client_id] = step;
+
+	/* Reset the concurrency fileds before the calculation */
+	for (opp_idx = 0; opp_idx < self->total_opps; opp_idx++)
+		self->mmdvfs_concurrency_of_opps[opp_idx] = 0;
+
+	for (scenario_idx = 0; scenario_idx < self->total_scenario; scenario_idx++) {
+		for (opp_idx = 0; opp_idx < self->total_opps; opp_idx++) {
+			if (self->mmdvfs_scenario_mmdvfs_opp[scenario_idx]
+			== opp_idx)
+				self->mmdvfs_concurrency_of_opps[opp_idx] |= 1
+				<< scenario_idx;
+		}
+	}
+
+	for (opp_idx = 0; opp_idx < self->total_opps; opp_idx++) {
+		if (self->mmdvfs_concurrency_of_opps[opp_idx] != 0) {
+			final_opp = opp_idx;
+			break;
+		}
+	}
+
+	return final_opp;
+}
+
+static int mmdvfs_get_opp_from_legacy_step(struct mmdvfs_step_util *self,
+int legacy_step)
+{
+	if (self->legacy_step_to_oop == NULL || legacy_step < 0 || legacy_step
+	>= self->legacy_step_to_oop_num)
+		return -1;
+	else
+		return self->legacy_step_to_oop_num + legacy_step;
+}
+
+static int mmdvfs_get_legacy_mmclk_step_from_mmclk_opp(
+struct mmdvfs_step_util *self, int mmclk_step)
+{
+	int step_ret = -1;
+
+	if (self->mmclk_oop_to_legacy_step == NULL || mmclk_step < 0
+	|| mmclk_step >= self->mmclk_oop_to_legacy_step_num) {
+		step_ret = -1;
+	} else {
+		int *step_ptr = self->mmclk_oop_to_legacy_step + mmclk_step;
+
+		if (step_ptr != NULL)
+			step_ret = *step_ptr;
+		else
+			step_ret = -1;
+	}
+	return step_ret;
+}
+
+struct mmdvfs_adaptor *g_mmdvfs_adaptor = &mmdvfs_adaptor_obj;
+struct mmdvfs_step_util *g_mmdvfs_step_util = &mmdvfs_step_util_obj;
+
+void mmdvfs_config_util_init(void)
+{
+	MMDVFSMSG("g_mmdvfs_adaptor init\n");
+	g_mmdvfs_adaptor->profile_dump_func(g_mmdvfs_adaptor);
+	MMDVFSMSG("g_mmdvfs_step_util init\n");
+	g_mmdvfs_step_util->init(g_mmdvfs_step_util);
+}
+
+
+
+
