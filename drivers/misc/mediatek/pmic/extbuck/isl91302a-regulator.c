0018d29939c8aecddbb4f8749dfd9f5acb902343
Vend.Jeff.Chang <vend_jeff_chang001@mediatek.com>
2019-01-22 21:59:03 +0800

    [ALPS02986439] PMIC: Add log for debug this CR
    [Detail]
    Add log for debug this CR
    
    MTK-Commit-Id: 04df4aabc79ba931c2a7b745a98f6dc8341f825c
    
    Change-Id: Ia96dd1495b9fecd31548f7f679b74535267c2505
    Signed-off-by: Vend.Jeff.Chang <vend_jeff_chang001@mediatek.com>
    CR-Id: ALPS02986439
    Feature: System Performance

--- drivers/misc/mediatek/pmic/extbuck/isl91302a-regulator.c
+++ drivers/misc/mediatek/pmic/extbuck/isl91302a-regulator.c
@@ -265,6 +265,7 @@ static int isl91302a_enable_ipi(struct mtk_simple_regulator_desc *mreg_desc)
 	unsigned int ret = 0;
 	unsigned char buck_id;
 
+	pr_err("isl91302a_enable_ipi ");
 	buck_id = mreg_desc->rdesc.id;
 	if (buck_id != 1) {
 		pr_err("%s only support proc2\n", __func__);
@@ -283,6 +284,7 @@ static int isl91302a_disable_ipi(struct mtk_simple_regulator_desc *mreg_desc)
 	unsigned int ret = 0;
 	unsigned char buck_id;
 
+	pr_err("isl91302a_disable_ipi ");
 	buck_id = mreg_desc->rdesc.id;
 	if (buck_id != 1) {
 		pr_err("%s only support proc2\n", __func__);
@@ -301,6 +303,7 @@ static int isl91302a_is_enabled_ipi(struct mtk_simple_regulator_desc *mreg_desc)
 	unsigned int ret = 0;
 	unsigned char buck_id;
 
+	pr_err("isl91302a_is_enabled_ipi ");
 	buck_id = mreg_desc->rdesc.id;
 	if (buck_id != 1) {
 		pr_err("%s only support proc2\n", __func__);
