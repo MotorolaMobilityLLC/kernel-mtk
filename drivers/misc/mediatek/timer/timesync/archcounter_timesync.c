caeb0197b9c7edbf1309d1b451a8023ee7ab2628
hongxu.zhao <hongxu.zhao@mediatek.com>
2019-01-23 00:08:01 +0800

    [ALPS03859736] Timer: refine timesync for camera
    [Detail]
    1. fix bug in multi access condition
    2. add timesync to boot for camera
    
    MTK-Commit-Id: 00686d8ed82a383f219688567760e404e1830af7
    
    Change-Id: Ib66b82515e06b2355e9e6982db515e79f83773ec
    CR-Id: ALPS03859736
    Feature: Sensor Hub
    Signed-off-by: hongxu.zhao <hongxu.zhao@mediatek.com>

--- drivers/misc/mediatek/timer/timesync/archcounter_timesync.c
+++ drivers/misc/mediatek/timer/timesync/archcounter_timesync.c
@@ -29,7 +29,8 @@ struct moving_average {
 	uint8_t tail;
 };
 
-static struct moving_average moving_average_algo;
+static struct moving_average moving_average_algo_mono;
+static struct moving_average moving_average_algo_boot;
 static DEFINE_SPINLOCK(moving_average_lock);
 
 /* arch counter is 13M, mult is 161319385, shift is 21 */
@@ -51,19 +52,18 @@ static uint64_t arch_counter_to_ns(uint64_t cyc)
 }
 
 static void moving_average_filter(struct moving_average *filter,
-	uint64_t monotonic_time, uint64_t archcounter_time)
+	uint64_t base_time, uint64_t archcounter_time)
 {
 	int i = 0;
 	int32_t avg;
 	int64_t ret_avg = 0;
 
-	if (monotonic_time < filter->last_time + FILTER_FREQ)
+	if (base_time < filter->last_time + FILTER_FREQ)
 		return;
 
-	spin_lock(&moving_average_lock);
-	filter->last_time = monotonic_time;
+	filter->last_time = base_time;
 
-	filter->input[filter->tail++] = monotonic_time - archcounter_time;
+	filter->input[filter->tail++] = base_time - archcounter_time;
 	filter->tail &= (FILTER_DATAPOINTS - 1);
 	if (filter->cnt < FILTER_DATAPOINTS)
 		filter->cnt++;
@@ -72,7 +72,6 @@ static void moving_average_filter(struct moving_average *filter,
 		avg += (int32_t)(filter->input[i] - filter->input[0]);
 	ret_avg = (avg / filter->cnt) + filter->input[0];
 	WRITE_ONCE(filter->output, ret_avg);
-	spin_unlock(&moving_average_lock);
 }
 
 static uint64_t get_filter_output(struct moving_average *filter)
@@ -89,27 +88,57 @@ static void filter_algo_init(struct moving_average *filter)
 
 void archcounter_timesync_init(uint8_t status)
 {
-	if (status)
-		filter_algo_init(&moving_average_algo);
+	if (status) {
+		filter_algo_init(&moving_average_algo_mono);
+		filter_algo_init(&moving_average_algo_boot);
+	}
 }
 
 uint64_t archcounter_timesync_to_monotonic(uint64_t hwclock)
 {
 	unsigned long flags = 0;
-	uint64_t monotonic_time = 0;
+	uint64_t base_time = 0;
 	uint64_t archcounter_time = 0;
 	uint64_t reslut_time = 0;
 
+	spin_lock(&moving_average_lock);
+
 	local_irq_save(flags);
-	monotonic_time = ktime_to_ns(ktime_get());
+	base_time = ktime_to_ns(ktime_get());
 	archcounter_time = arch_counter_to_ns(arch_counter_get_cntvct());
 	local_irq_restore(flags);
 
-	moving_average_filter(&moving_average_algo,
-		monotonic_time, archcounter_time);
+	moving_average_filter(&moving_average_algo_mono,
+		base_time, archcounter_time);
 
 	reslut_time = arch_counter_to_ns(hwclock) +
-		get_filter_output(&moving_average_algo);
+		get_filter_output(&moving_average_algo_mono);
+
+	spin_unlock(&moving_average_lock);
+	return reslut_time;
+}
+
+uint64_t archcounter_timesync_to_boot(uint64_t hwclock)
+{
+	unsigned long flags = 0;
+	uint64_t base_time = 0;
+	uint64_t archcounter_time = 0;
+	uint64_t reslut_time = 0;
+
+	spin_lock(&moving_average_lock);
+
+	local_irq_save(flags);
+	base_time = ktime_get_boot_ns();
+	archcounter_time = arch_counter_to_ns(arch_counter_get_cntvct());
+	local_irq_restore(flags);
+
+	moving_average_filter(&moving_average_algo_boot,
+		base_time, archcounter_time);
+
+	reslut_time = arch_counter_to_ns(hwclock) +
+		get_filter_output(&moving_average_algo_boot);
+
+	spin_unlock(&moving_average_lock);
 	return reslut_time;
 }
 
@@ -122,23 +151,39 @@ struct work_struct timesync_test_work;
 static void timesync_test_work_func(struct work_struct *work)
 {
 	unsigned long flags = 0;
-	uint64_t monotonic_time = 0;
+	uint64_t base_time = 0;
 	uint64_t archcounter_time = 0;
 	uint64_t algo_offset = 0;
 
 	local_irq_save(flags);
-	monotonic_time = ktime_to_ns(ktime_get());
+	base_time = ktime_to_ns(ktime_get());
 	archcounter_time = arch_counter_to_ns(arch_counter_get_cntvct());
 	local_irq_restore(flags);
 
-	moving_average_filter(&moving_average_algo,
-		monotonic_time, archcounter_time);
-	algo_offset = get_filter_output(&moving_average_algo);
+	moving_average_filter(&moving_average_algo_mono,
+		base_time, archcounter_time);
+	algo_offset = get_filter_output(&moving_average_algo_mono);
 
 	pr_debug("[archcounter_timesync] monotonic=%lld, archcounter=%lld\n",
-		monotonic_time, archcounter_time);
+		base_time, archcounter_time);
 	pr_debug("[archcounter_timesync] raw_offset=%lld\n",
-		monotonic_time - archcounter_time);
+		base_time - archcounter_time);
+	pr_debug("[archcounter_timesync] algo_offset=%lld\n", algo_offset);
+
+
+	local_irq_save(flags);
+	base_time = ktime_get_boot_ns();
+	archcounter_time = arch_counter_to_ns(arch_counter_get_cntvct());
+	local_irq_restore(flags);
+
+	moving_average_filter(&moving_average_algo_mono,
+		base_time, archcounter_time);
+	algo_offset = get_filter_output(&moving_average_algo_mono);
+
+	pr_debug("[archcounter_timesync] boot=%lld, archcounter=%lld\n",
+		base_time, archcounter_time);
+	pr_debug("[archcounter_timesync] raw_offset=%lld\n",
+		base_time - archcounter_time);
 	pr_debug("[archcounter_timesync] algo_offset=%lld\n", algo_offset);
 }
 
@@ -154,7 +199,8 @@ static int __init archcounter_timesync_entry(void)
 {
 	pr_debug("[archcounter_timesync] archcounter_timesync_entry\n");
 
-	filter_algo_init(&moving_average_algo);
+	filter_algo_init(&moving_average_algo_mono);
+	filter_algo_init(&moving_average_algo_boot);
 
 #ifdef CONFIG_TIMESYNC_TEST
 	INIT_WORK(&timesync_test_work, timesync_test_work_func);
