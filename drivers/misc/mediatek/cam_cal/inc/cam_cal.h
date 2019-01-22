bf539288f5174019f344996084d7be8383cdad1f
Auto Li <auto.li@mediatek.com>
2019-01-23 00:09:06 +0800

    [ALPS04130177] EEPROM: Main3 support
    [Detail] Main3 support
    
    [Solution] done
    
    MTK-Commit-Id: 6e782ef8f399a87bafc2f817e7040978f156d4de
    
    Change-Id: Ibd2add76e5381dc4d361c9bcaf6b1ef430ea6319
    Signed-off-by: Auto Li <auto.li@mediatek.com>
    CR-Id: ALPS04130177
    Feature: [Android Default] Camera Application Basic Functions
    (cherry picked from commit 7d4a9a232e058d85e98ae99861b032b422c835d6)

--- drivers/misc/mediatek/cam_cal/inc/cam_cal.h
+++ drivers/misc/mediatek/cam_cal/inc/cam_cal.h
@@ -1,15 +1,16 @@
 /*
-* Copyright (C) 2016 MediaTek Inc.
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 2 as
-* published by the Free Software Foundation.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
-*/
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ ***************/
+
 #ifndef _CAM_CAL_H
 #define _CAM_CAL_H
 
@@ -29,18 +30,19 @@
 /*X means "switch G and S atomically"*/
 /*H means "switch T and Q atomically"*/
 
-/*******************************************************************************
-*
-********************************************************************************/
+/**********************************************
+ *
+ **********************************************/
 
 /*CAM_CAL write*/
-#define CAM_CALIOC_S_WRITE           _IOW(CAM_CALAGIC, 0, struct stCAM_CAL_INFO_STRUCT)
+#define CAM_CALIOC_S_WRITE _IOW(CAM_CALAGIC, 0, struct stCAM_CAL_INFO_STRUCT)
 /*CAM_CAL read*/
-#define CAM_CALIOC_G_READ            _IOWR(CAM_CALAGIC, 5, struct stCAM_CAL_INFO_STRUCT)
+#define CAM_CALIOC_G_READ _IOWR(CAM_CALAGIC, 5, struct stCAM_CAL_INFO_STRUCT)
 
 #ifdef CONFIG_COMPAT
-#define COMPAT_CAM_CALIOC_S_WRITE    _IOW(CAM_CALAGIC, 0, struct COMPAT_stCAM_CAL_INFO_STRUCT)
-#define COMPAT_CAM_CALIOC_G_READ    _IOWR(CAM_CALAGIC, 5, struct COMPAT_stCAM_CAL_INFO_STRUCT)
+#define COMPAT_CAM_CALIOC_S_WRITE \
+	_IOW(CAM_CALAGIC, 0, struct COMPAT_stCAM_CAL_INFO_STRUCT)
+#define COMPAT_CAM_CALIOC_G_READ \
+	_IOWR(CAM_CALAGIC, 5, struct COMPAT_stCAM_CAL_INFO_STRUCT)
 #endif
 #endif /*_CAM_CAL_H*/
-
