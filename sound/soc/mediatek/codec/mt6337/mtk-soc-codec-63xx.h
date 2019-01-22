3680ba1ae18da539c1227c065a1c56a8f57ef7fa
chien-wei hsu <chien-wei.hsu@mediatek.com>
2019-01-22 21:28:50 +0800

    [ALPS02594228] AUDIO: Add DPD module
    [Detail]
    1. add read dpd parameter from efuse when codec initialize.
    2. add function to calculate the dpd variable.
    3. add api to get the dpd variable to AP side.
    4. add platform api to set dpd module.
    5. add mixer control to turn on dpd module.
    
    MTK-Commit-Id: 67bdf9d6188bc1dee9f3ee24ef61bef8e3d36293
    
    Change-Id: I020a983577153dd83d142dd31b812eff6f42301c
    CR-Id: ALPS02594228
    Feature: Audio Playback(*)
    Signed-off-by: chien-wei hsu <chien-wei.hsu@mediatek.com>

--- sound/soc/mediatek/codec/mt6337/mtk-soc-codec-63xx.h
+++ sound/soc/mediatek/codec/mt6337/mtk-soc-codec-63xx.h
@@ -53,6 +53,20 @@ struct mtk_hpdet_param {
 	int resistance_second_threshold;
 };
 
+/* DPD parameter */
+struct mtk_dpd_param {
+	int efuse_on;
+	int version;
+	int a2_lch;
+	int a3_lch;
+	int a4_lch;
+	int a5_lch;
+	int a2_rch;
+	int a3_rch;
+	int a4_rch;
+	int a5_rch;
+};
+
 void audckbufEnable(bool enable);
 void OpenClassAB(void);
 void OpenAnalogHeadphone(bool bEnable);
@@ -76,5 +90,8 @@ void OpenTrimBufferHardware(bool bEnable);
 /* mtk audio interface calibration function */
 void mtkaif_calibration_set_loopback(bool enable);
 void mtkaif_calibration_set_phase(int mode1, int mode2);
+
+/*mtk dpd function*/
+void mtk_read_dpd_parameter(int impedance, struct mtk_dpd_param *dpd_param);
 #endif
 
