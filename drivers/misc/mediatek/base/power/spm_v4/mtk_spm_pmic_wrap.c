8d614bea67e2a080871d6a442067dec53a912ed6
Morven-CF Yeh <morven-cf.yeh@mediatek.com>
2019-01-22 22:56:12 +0800

    [ALPS03113444] spm: modify spm header file
    [Detail] 1. modify spm header file
    2. add vcore/vsram_others voltage control
    3. add sw_flag1 and debug_flag1
    
    MTK-Commit-Id: ce7c18bb3dddfc0bca8a81645da94e7de112d936
    
    Change-Id: Iec103973a3a8eea19afcb448d9b7d7628ff26a31
    Signed-off-by: Morven-CF Yeh <morven-cf.yeh@mediatek.com>
    CR-Id: ALPS03113444
    Feature: Power Management

--- drivers/misc/mediatek/base/power/spm_v4/mtk_spm_pmic_wrap.c
+++ drivers/misc/mediatek/base/power/spm_v4/mtk_spm_pmic_wrap.c
@@ -43,14 +43,22 @@
  */
 #define _BIT_(_bit_)                    (unsigned)(1 << (_bit_))
 #define _BITS_(_bits_, _val_)         \
-	  ((((unsigned) -1 >> (31 - ((1) ? _bits_))) & ~((1U << ((0) ? _bits_)) - 1)) & ((_val_)<<((0) ? _bits_)))
+	((((unsigned) -1 >> (31 - ((1) ? _bits_))) & ~((1U << ((0) ? _bits_)) - 1)) & ((_val_)<<((0) ? _bits_)))
 #define _BITMASK_(_bits_)               (((unsigned) -1 >> (31 - ((1) ? _bits_))) & ~((1U << ((0) ? _bits_)) - 1))
 #define _GET_BITS_VAL_(_bits_, _val_)   (((_val_) & (_BITMASK_(_bits_))) >> ((0) ? _bits_))
+
+/*
+ * PMIC_WRAP
+ */
+#define VCORE_BASE_UV 50000 /* PMIC MT6356 */
+#define VOLT_TO_PMIC_VAL(volt)  (((volt) - VCORE_BASE_UV + 625 - 1) / 625) /* ((((volt) - 700 * 100 + 625 - 1) / 625) */
+#define PMIC_VAL_TO_VOLT(pmic)  (((pmic) * 625) + VCORE_BASE_UV) /* (((pmic) * 625) / 100 + 700) */
 
 #define DEFAULT_VOLT_VSRAM      (100000)
 #define DEFAULT_VOLT_VCORE      (100000)
 #define NR_PMIC_WRAP_CMD (16)	/* 16 pmic wrap cmd */
 #define MAX_RETRY_COUNT (100)
+#define SPM_DATA_SHIFT (16)
 
 struct pmic_wrap_cmd {
 	unsigned long cmd_addr;
@@ -73,28 +81,25 @@ static struct pmic_wrap_setting pw = {
 	.phase = NR_PMIC_WRAP_PHASE,	/* invalid setting for init */
 	.addr = {{0, 0} },
 
-	/* FIXME: */
-#if 0
 	.set[PMIC_WRAP_PHASE_ALLINONE] = {
-		._[IDX_ALL_1_VSRAM_PWR_ON]      = {MT6335_LDO_VSRAM_DVFS1_CON0, _BITS_(0:0, 1),},
-		._[IDX_ALL_1_VSRAM_SHUTDOWN]    = {MT6335_LDO_VSRAM_DVFS1_CON0, _BITS_(0:0, 0),},
-		._[IDX_ALL_1_VSRAM_NORMAL]      = {MT6335_LDO_VSRAM_DVFS1_CON1, VOLT_TO_PMIC_VAL(80000),},
-		._[IDX_ALL_1_VSRAM_SLEEP]       = {MT6335_LDO_VSRAM_DVFS1_CON1, VOLT_TO_PMIC_VAL(65000),},
-		._[IDX_ALL_DPIDLE_LEAVE]        = {MT6335_TOP_SPI_CON0, _BITS_(0:0, 1),},
-		._[IDX_ALL_DPIDLE_ENTER]        = {MT6335_TOP_SPI_CON0, _BITS_(0:0, 0),},
-		._[IDX_ALL_RESERVE_6]           = {0, 0,},
-		._[IDX_ALL_VCORE_SUSPEND]       = {MT6335_BUCK_VCORE_CON1, VOLT_TO_PMIC_VAL(56800),},
-		._[IDX_ALL_VCORE_LEVEL0]        = {MT6335_BUCK_VCORE_CON1, VOLT_TO_PMIC_VAL(65000),},
-		._[IDX_ALL_VCORE_LEVEL1]        = {MT6335_BUCK_VCORE_CON1, VOLT_TO_PMIC_VAL(70000),},
-		._[IDX_ALL_VCORE_LEVEL2]        = {MT6335_BUCK_VCORE_CON1, VOLT_TO_PMIC_VAL(75000),},
-		._[IDX_ALL_VCORE_LEVEL3]        = {MT6335_BUCK_VCORE_CON1, VOLT_TO_PMIC_VAL(80000),},
-		._[IDX_ALL_2_VSRAM_PWR_ON]      = {MT6335_LDO_VSRAM_DVFS2_CON0, _BITS_(0:0, 1),},
-		._[IDX_ALL_2_VSRAM_SHUTDOWN]    = {MT6335_LDO_VSRAM_DVFS2_CON0, _BITS_(0:0, 0),},
-		._[IDX_ALL_2_VSRAM_NORMAL]      = {MT6335_LDO_VSRAM_DVFS2_CON1, VOLT_TO_PMIC_VAL(80000),},
-		._[IDX_ALL_2_VSRAM_SLEEP]       = {MT6335_LDO_VSRAM_DVFS2_CON1, VOLT_TO_PMIC_VAL(65000),},
+		._[CMD_0]    = {MT6356_BUCK_VCORE_ELR0, VOLT_TO_PMIC_VAL(70000),},
+		._[CMD_1]    = {MT6356_BUCK_VCORE_ELR0, VOLT_TO_PMIC_VAL(80000),},
+		._[CMD_2]    = {0, 0,},
+		._[CMD_3]    = {0, 0,},
+		._[CMD_4]    = {MT6356_LDO_VSRAM_CON3, _BITS_(14:8, VOLT_TO_PMIC_VAL(80000)),},
+		._[CMD_5]    = {MT6356_LDO_VSRAM_CON3, _BITS_(14:8, VOLT_TO_PMIC_VAL(90000)),},
+		._[CMD_6]    = {MT6356_TOP_SPI_CON0, _BITS_(0:0, 1),},
+		._[CMD_7]    = {MT6356_TOP_SPI_CON0, _BITS_(0:0, 0),},
+		._[CMD_8]    = {MT6356_BUCK_VPROC_CON0, _BITS_(1:0, 3),},
+		._[CMD_9]    = {MT6356_BUCK_VPROC_CON0, _BITS_(1:0, 1),},
+		._[CMD_10]   = {MT6356_LDO_VSRAM_PROC_CON0, _BITS_(1:0, 3),},
+		._[CMD_11]   = {MT6356_LDO_VSRAM_PROC_CON0, _BITS_(1:0, 1),},
+		._[CMD_12]   = {MT6356_BUCK_VPROC_CON0, _BITS_(1:0, 0),},
+		._[CMD_13]   = {MT6356_BUCK_VPROC_CON0, _BITS_(1:0, 1),},
+		._[CMD_14]   = {MT6356_LDO_VSRAM_PROC_CON0, _BITS_(1:0, 0),},
+		._[CMD_15]   = {MT6356_LDO_VSRAM_PROC_CON0, _BITS_(1:0, 1),},
 		.nr_idx = NR_IDX_ALL,
 	},
-#endif
 };
 
 static DEFINE_SPINLOCK(pmic_wrap_lock);
@@ -104,22 +109,22 @@ static DEFINE_SPINLOCK(pmic_wrap_lock);
 void _mt_spm_pmic_table_init(void)
 {
 	struct pmic_wrap_cmd pwrap_cmd_default[NR_PMIC_WRAP_CMD] = {
-		{(unsigned long)PMIC_WRAP_DVFS_ADR0, (unsigned long)PMIC_WRAP_DVFS_WDATA0,},
-		{(unsigned long)PMIC_WRAP_DVFS_ADR1, (unsigned long)PMIC_WRAP_DVFS_WDATA1,},
-		{(unsigned long)PMIC_WRAP_DVFS_ADR2, (unsigned long)PMIC_WRAP_DVFS_WDATA2,},
-		{(unsigned long)PMIC_WRAP_DVFS_ADR3, (unsigned long)PMIC_WRAP_DVFS_WDATA3,},
-		{(unsigned long)PMIC_WRAP_DVFS_ADR4, (unsigned long)PMIC_WRAP_DVFS_WDATA4,},
-		{(unsigned long)PMIC_WRAP_DVFS_ADR5, (unsigned long)PMIC_WRAP_DVFS_WDATA5,},
-		{(unsigned long)PMIC_WRAP_DVFS_ADR6, (unsigned long)PMIC_WRAP_DVFS_WDATA6,},
-		{(unsigned long)PMIC_WRAP_DVFS_ADR7, (unsigned long)PMIC_WRAP_DVFS_WDATA7,},
-		{(unsigned long)PMIC_WRAP_DVFS_ADR8, (unsigned long)PMIC_WRAP_DVFS_WDATA8,},
-		{(unsigned long)PMIC_WRAP_DVFS_ADR9, (unsigned long)PMIC_WRAP_DVFS_WDATA9,},
-		{(unsigned long)PMIC_WRAP_DVFS_ADR10, (unsigned long)PMIC_WRAP_DVFS_WDATA10,},
-		{(unsigned long)PMIC_WRAP_DVFS_ADR11, (unsigned long)PMIC_WRAP_DVFS_WDATA11,},
-		{(unsigned long)PMIC_WRAP_DVFS_ADR12, (unsigned long)PMIC_WRAP_DVFS_WDATA12,},
-		{(unsigned long)PMIC_WRAP_DVFS_ADR13, (unsigned long)PMIC_WRAP_DVFS_WDATA13,},
-		{(unsigned long)PMIC_WRAP_DVFS_ADR14, (unsigned long)PMIC_WRAP_DVFS_WDATA14,},
-		{(unsigned long)PMIC_WRAP_DVFS_ADR15, (unsigned long)PMIC_WRAP_DVFS_WDATA15,},
+		{(unsigned long)SPM_DVFS_CMD0, (unsigned long)SPM_DVFS_CMD0,},
+		{(unsigned long)SPM_DVFS_CMD1, (unsigned long)SPM_DVFS_CMD1,},
+		{(unsigned long)SPM_DVFS_CMD2, (unsigned long)SPM_DVFS_CMD2,},
+		{(unsigned long)SPM_DVFS_CMD3, (unsigned long)SPM_DVFS_CMD3,},
+		{(unsigned long)SPM_DVFS_CMD4, (unsigned long)SPM_DVFS_CMD4,},
+		{(unsigned long)SPM_DVFS_CMD5, (unsigned long)SPM_DVFS_CMD5,},
+		{(unsigned long)SPM_DVFS_CMD6, (unsigned long)SPM_DVFS_CMD6,},
+		{(unsigned long)SPM_DVFS_CMD7, (unsigned long)SPM_DVFS_CMD7,},
+		{(unsigned long)SPM_DVFS_CMD8, (unsigned long)SPM_DVFS_CMD8,},
+		{(unsigned long)SPM_DVFS_CMD9, (unsigned long)SPM_DVFS_CMD9,},
+		{(unsigned long)SPM_DVFS_CMD10, (unsigned long)SPM_DVFS_CMD10,},
+		{(unsigned long)SPM_DVFS_CMD11, (unsigned long)SPM_DVFS_CMD11,},
+		{(unsigned long)SPM_DVFS_CMD12, (unsigned long)SPM_DVFS_CMD12,},
+		{(unsigned long)SPM_DVFS_CMD13, (unsigned long)SPM_DVFS_CMD13,},
+		{(unsigned long)SPM_DVFS_CMD14, (unsigned long)SPM_DVFS_CMD14,},
+		{(unsigned long)SPM_DVFS_CMD15, (unsigned long)SPM_DVFS_CMD15,},
 	};
 
 	memcpy(pw.addr, pwrap_cmd_default, sizeof(pwrap_cmd_default));
@@ -142,10 +147,9 @@ void mt_spm_pmic_wrap_set_phase(enum pmic_wrap_phase_id phase)
 
 	pw.phase = phase;
 
-	for (i = 0; i < pw.set[phase].nr_idx; i++) {
-		spm_write(pw.addr[i].cmd_addr, pw.set[phase]._[i].cmd_addr);
-		spm_write(pw.addr[i].cmd_wdata, pw.set[phase]._[i].cmd_wdata);
-	}
+	for (i = 0; i < pw.set[phase].nr_idx; i++)
+		spm_write(pw.addr[i].cmd_addr,
+				(pw.set[phase]._[i].cmd_addr << SPM_DATA_SHIFT) | (pw.set[phase]._[i].cmd_wdata));
 
 	spm_pwrap_warn("pmic table init: done\n");
 
@@ -165,62 +169,10 @@ void mt_spm_pmic_wrap_set_cmd(enum pmic_wrap_phase_id phase, int idx, unsigned i
 	pw.set[phase]._[idx].cmd_wdata = cmd_wdata;
 
 	if (pw.phase == phase)
-		spm_write(pw.addr[idx].cmd_wdata, cmd_wdata);
+		spm_write(pw.addr[idx].cmd_wdata, (pw.addr[idx].cmd_wdata << SPM_DATA_SHIFT) | cmd_wdata);
 
 	pmic_wrap_unlock(flags);
 }
 EXPORT_SYMBOL(mt_spm_pmic_wrap_set_cmd);
-
-#if 0
-void mt_spm_pmic_wrap_set_cmd_full(enum pmic_wrap_phase_id phase, int idx, unsigned int cmd_addr,
-				   unsigned int cmd_wdata)
-{
-	unsigned long flags;
-
-	WARN_ON(phase >= NR_PMIC_WRAP_PHASE);
-	WARN_ON(idx >= pw.set[phase].nr_idx);
-
-	pmic_wrap_lock(flags);
-
-	pw.set[phase]._[idx].cmd_addr = cmd_addr;
-	pw.set[phase]._[idx].cmd_wdata = cmd_wdata;
-
-	if (pw.phase == phase) {
-		spm_write(pw.addr[idx].cmd_addr, cmd_addr);
-		spm_write(pw.addr[idx].cmd_wdata, cmd_wdata);
-	}
-
-	pmic_wrap_unlock(flags);
-}
-EXPORT_SYMBOL(mt_spm_pmic_wrap_set_cmd_full);
-
-void mt_spm_pmic_wrap_get_cmd_full(enum pmic_wrap_phase_id phase, int idx, unsigned int *p_cmd_addr,
-				   unsigned int *p_cmd_wdata)
-{
-	unsigned long flags;
-
-	WARN_ON(phase >= NR_PMIC_WRAP_PHASE);
-	WARN_ON(idx >= pw.set[phase].nr_idx);
-
-	pmic_wrap_lock(flags);
-
-	*p_cmd_addr = pw.set[phase]._[idx].cmd_addr;
-	*p_cmd_wdata = pw.set[phase]._[idx].cmd_wdata;
-
-	pmic_wrap_unlock(flags);
-}
-EXPORT_SYMBOL(mt_cpufreq_get_pmic_cmd_full);
-#endif
-
-int mt_spm_pmic_wrap_init(void)
-{
-	if (pw.addr[0].cmd_addr == 0)
-		_mt_spm_pmic_table_init();
-
-	/* init PMIC_WRAP & volt */
-	mt_spm_pmic_wrap_set_phase(PMIC_WRAP_PHASE_ALLINONE);
-
-	return 0;
-}
 
 MODULE_DESCRIPTION("SPM-PMIC_WRAP Driver v0.1");
