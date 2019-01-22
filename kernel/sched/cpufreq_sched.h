a35e0a248ba82b2b5e44d93c76bb743d9ffd0ed4
Hector Yuan <hector.yuan@mediatek.com>
2019-01-22 21:41:56 +0800

    [ALPS02806286] DVFS: Simplify Schedule assist algo
    [Detail] Simplify Schedule assist algo
    
    MTK-Commit-Id: 038a167fb608f25fd7dd6b11d0e4a01f58fe85f5
    
    Change-Id: I489091f3c4008f9701d2d5673dfdcc9b68cd3ad3
    Signed-off-by: Hector Yuan <hector.yuan@mediatek.com>
    CR-Id: ALPS02806286
    Feature: DVFS (Dynamic Voltage and Frequency Scaling)

--- kernel/sched/cpufreq_sched.h
+++ kernel/sched/cpufreq_sched.h
@@ -1,2 +1,3 @@
 
-extern int mt_cpufreq_set_by_schedule_load(unsigned int cpu, enum cpu_dvfs_sched_type state, unsigned int freq);
+/* extern int mt_cpufreq_set_by_schedule_load(unsigned int cpu, enum cpu_dvfs_sched_type state, unsigned int freq); */
+extern int  mt_cpufreq_set_by_schedule_load_cluster(int cid, unsigned int freq);
