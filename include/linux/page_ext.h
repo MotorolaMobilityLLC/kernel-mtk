d122626e3d33293c4bf29fbd52d2fa417d4b7581
LucasWei <lucas-sr.wei@mediatek.com>
2019-01-22 22:20:40 +0800

    [ALPS03021784] page_owner: Slim page_owner memory overhead
    [Detail]
    For performance consideration, page owner is implemented with
    lock free. Each page extensions have a stand along data strucutre
    for backtrack of page allocation.
    This patch compress page alloction backtrace. Leverage on compressing,
    page owner can dump alloction usage with summarize data structure.
    
    [Solution]
    Compress page owner with hash table and link list.
    The implemation with a fixed memory area to store page alloction
    backtrace.
    
    MTK-Commit-Id: ede0e8378329bf99a0cde84e0c741a6897cc7772
    
    Change-Id: I22268723f0725aa1fb8ae780b2cffc03b5aac866
    Signed-off-by: LucasWei <lucas-sr.wei@mediatek.com>
    CR-Id: ALPS03021784
    Feature: Resolution switch

--- include/linux/page_ext.h
+++ include/linux/page_ext.h
@@ -3,6 +3,7 @@
 
 #include <linux/types.h>
 #include <linux/stacktrace.h>
+#include <linux/page_owner.h>
 
 struct pglist_data;
 struct page_ext_operations {
@@ -44,8 +45,12 @@ struct page_ext {
 #ifdef CONFIG_PAGE_OWNER
 	unsigned int order;
 	gfp_t gfp_mask;
+#ifdef CONFIG_PAGE_OWNER_SLIM
+	BtEntry *entry;
+#else
 	unsigned int nr_entries;
 	unsigned long trace_entries[8];
+#endif
 #endif
 };
 
--- mm/page_owner.c
+++ mm/page_owner.c
@@ -5,12 +5,213 @@
 #include <linux/bootmem.h>
 #include <linux/stacktrace.h>
 #include <linux/page_owner.h>
+#include <linux/memblock.h>
 #include "internal.h"
 
+#ifdef CONFIG_DEBUG_PAGE_OWNER_DEFAULT_OFF
 static bool page_owner_disabled = true;
+#else
+static bool page_owner_disabled;
+#endif
+
 bool page_owner_inited __read_mostly;
 
 static void init_early_allocated_pages(void);
+
+#ifdef CONFIG_PAGE_OWNER_SLIM
+
+#define BOOT_ENTRIES (4 * 1024)
+
+unsigned long bitmap[BITS_TO_LONGS(BOOT_ENTRIES)];
+
+BtTable gBtTable;
+static BtEntry memory_base[BOOT_ENTRIES];
+
+static DEFINE_SPINLOCK(page_owner_spin_lock);
+
+static inline size_t get_hash(unsigned long *backtrace, size_t nr_entries)
+{
+	size_t hash = 0;
+	size_t i;
+
+	if (backtrace == NULL)
+		return 0;
+
+	for (i = 0 ; i < nr_entries ; i++)
+		hash = hash << 5 ^ hash >> 1 ^ backtrace[i] >> 2;
+
+	return hash;
+}
+
+BtEntry *alloc_entry(void)
+{
+	int index;
+
+	index = bitmap_find_free_region(bitmap, BOOT_ENTRIES, 0);
+	if (index == -ENOMEM)
+		return NULL;
+
+	bitmap_set(bitmap, index, 1);
+	return &memory_base[index];
+}
+
+static int backtrace_cmp(unsigned long *b1, unsigned long *b2, size_t n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		if (b1[i] != b2[i])
+			return b1[i] - b2[i];
+
+	return 0;
+}
+
+static BtEntry *find_entry(BtTable *table, int slot,
+		unsigned long *backtrace, size_t nr_entry)
+{
+	BtEntry *entry;
+
+	list_for_each_entry(entry, &table->list[slot], list) {
+		/*
+		 * See if the entry matches exactly.
+		 */
+		if (entry->nr_entries == nr_entry) {
+			if (!backtrace_cmp(backtrace, entry->backtrace, nr_entry))
+				return entry;
+		}
+	}
+
+	return NULL;
+}
+
+static void free_entry(BtEntry *entry)
+{
+	int index;
+
+	list_del(&entry->list);
+
+	gBtTable.count--;
+	index = (int)(entry - &memory_base[0]);
+	bitmap_clear(bitmap, index, 1);
+}
+
+void release_backtrace(BtEntry *entry)
+{
+	unsigned long flags;
+
+	spin_lock_irqsave(&page_owner_spin_lock, flags);
+	entry->allocations--;
+
+	if (entry->allocations == 0)
+		free_entry(entry);
+
+	spin_unlock_irqrestore(&page_owner_spin_lock, flags);
+}
+
+BtEntry *record_backtrace(unsigned long *backtrace, size_t nr_entry)
+{
+	size_t hash = get_hash(backtrace, nr_entry);
+	size_t slot = hash % BT_HASH_TABLE_SIZE;
+	BtEntry *entry = NULL;
+	unsigned long flags;
+
+	spin_lock_irqsave(&page_owner_spin_lock, flags);
+	entry = find_entry(&gBtTable, slot, backtrace, nr_entry);
+
+	if (entry != NULL)
+		entry->allocations++;
+	else {
+		entry = alloc_entry();
+
+		if (entry == NULL) {
+			pr_alert("[ERROR]alloc fails\n");
+			pr_alert("No backtrace memory available, disable page owner tracking.\n");
+			page_owner_inited = false;
+			spin_unlock_irqrestore(&page_owner_spin_lock, flags);
+			return NULL;
+		}
+
+		entry->allocations = 1;
+		entry->nr_entries = nr_entry;
+
+		memcpy(entry->backtrace, backtrace, nr_entry * sizeof(unsigned long));
+
+
+		list_add(&entry->list, &gBtTable.list[slot]);
+
+
+		gBtTable.count++;
+	}
+
+	spin_unlock_irqrestore(&page_owner_spin_lock, flags);
+
+	return entry;
+}
+
+static ssize_t
+read_page_owner_slim(struct file *file, char __user *buf, size_t count, loff_t *ppos)
+{
+	BtEntry *entry;
+	unsigned long flags;
+	int ret = 0;
+	int index;
+	char *kbuf;
+
+	if (!page_owner_inited)
+		return -EINVAL;
+
+	index = *ppos;
+
+	if (index >= BOOT_ENTRIES)
+		return 0;
+
+	kbuf = kmalloc(count, GFP_KERNEL);
+
+	if (!kbuf)
+		return -ENOMEM;
+
+	spin_lock_irqsave(&page_owner_spin_lock, flags);
+	/* search memory_base for allocated entries */
+	for (; index < BOOT_ENTRIES; index++) {
+		entry = &memory_base[index];
+		if (entry->allocations > 0) {
+			ret = snprintf(kbuf, count,
+					"%8zu %pS %p %p %p %p %p %p\n", entry->allocations,
+					(void *) entry->backtrace[2], (void *) entry->backtrace[2],
+					(void *) entry->backtrace[3], (void *) entry->backtrace[4],
+					(void *) entry->backtrace[5], (void *) entry->backtrace[6],
+					(void *) entry->backtrace[7]);
+			break;
+		}
+	}
+
+	spin_unlock_irqrestore(&page_owner_spin_lock, flags);
+
+	if (ret >= count) {
+		pr_info("Insufficiant buffer\n");
+		ret = -1;
+		goto error;
+	}
+
+	if (copy_to_user(buf, kbuf, ret)) {
+		ret = -EFAULT;
+		goto error;
+	}
+
+	*ppos = index + 1;
+	kfree(kbuf);
+	return ret;
+
+error:
+	kfree(kbuf);
+	return ret;
+}
+
+static const struct file_operations proc_page_owner_slim_operations = {
+	.read		= read_page_owner_slim,
+};
+
+#endif /* CONFIG_PAGE_OWNER_SLIM */
 
 static int early_page_owner_param(char *buf)
 {
@@ -34,8 +235,26 @@ static bool need_page_owner(void)
 
 static void init_page_owner(void)
 {
+#ifdef CONFIG_PAGE_OWNER_SLIM
+	int i;
+#endif
+
 	if (page_owner_disabled)
 		return;
+
+#ifdef CONFIG_PAGE_OWNER_SLIM
+
+	if (memory_base == NULL) {
+		pr_err("PAGE OWNER SLIM: early init memblock_alloc fail.\n");
+		pr_info("Disable page owner tracking.\n");
+		page_owner_disabled = true;
+		return;
+	}
+
+	pr_info("Enable [PAGE_OWNER_SLIM]\n");
+	for (i = 0; i < BT_HASH_TABLE_SIZE; i++)
+		INIT_LIST_HEAD(&gBtTable.list[i]);
+#endif
 
 	page_owner_inited = true;
 	init_early_allocated_pages();
@@ -54,24 +273,46 @@ void __reset_page_owner(struct page *page, unsigned int order)
 	for (i = 0; i < (1 << order); i++) {
 		page_ext = lookup_page_ext(page + i);
 		__clear_bit(PAGE_EXT_OWNER, &page_ext->flags);
+#ifdef CONFIG_PAGE_OWNER_SLIM
+		if (page_ext && page_ext->entry) {
+			release_backtrace(page_ext->entry);
+			page_ext->entry = NULL;
+		}
+#endif
 	}
 }
 
 void __set_page_owner(struct page *page, unsigned int order, gfp_t gfp_mask)
 {
 	struct page_ext *page_ext = lookup_page_ext(page);
+#ifdef CONFIG_PAGE_OWNER_SLIM
+	BtEntry *entry;
+	unsigned long trace_entries[8];
+	struct stack_trace trace = {
+		.nr_entries = 0,
+		.entries = &trace_entries[0],
+		.max_entries = ARRAY_SIZE(trace_entries),
+		.skip = 3,
+	};
+#else
 	struct stack_trace trace = {
 		.nr_entries = 0,
 		.max_entries = ARRAY_SIZE(page_ext->trace_entries),
 		.entries = &page_ext->trace_entries[0],
 		.skip = 3,
 	};
+#endif
 
 	save_stack_trace(&trace);
 
+#ifdef CONFIG_PAGE_OWNER_SLIM
+	entry = record_backtrace(trace.entries, trace.nr_entries);
+	page_ext->entry = entry;
+#else
+	page_ext->nr_entries = trace.nr_entries;
+#endif
 	page_ext->order = order;
 	page_ext->gfp_mask = gfp_mask;
-	page_ext->nr_entries = trace.nr_entries;
 
 	__set_bit(PAGE_EXT_OWNER, &page_ext->flags);
 }
@@ -90,10 +331,18 @@ print_page_owner(char __user *buf, size_t count, unsigned long pfn,
 	int ret;
 	int pageblock_mt, page_mt;
 	char *kbuf;
+#ifdef CONFIG_PAGE_OWNER_SLIM
+	BtEntry *entry = page_ext->entry;
+	struct stack_trace trace = {
+		.nr_entries = entry->nr_entries,
+		.entries = &entry->backtrace[0],
+	};
+#else
 	struct stack_trace trace = {
 		.nr_entries = page_ext->nr_entries,
 		.entries = &page_ext->trace_entries[0],
 	};
+#endif
 
 	kbuf = kmalloc(count, GFP_KERNEL);
 	if (!kbuf)
@@ -314,6 +563,13 @@ static int __init pageowner_init(void)
 			NULL, &proc_page_owner_operations);
 	if (IS_ERR(dentry))
 		return PTR_ERR(dentry);
+
+#ifdef CONFIG_PAGE_OWNER_SLIM
+	dentry = debugfs_create_file("page_owner_slim", S_IRUSR, NULL,
+			NULL, &proc_page_owner_slim_operations);
+	if (IS_ERR(dentry))
+		return PTR_ERR(dentry);
+#endif
 
 	return 0;
 }
