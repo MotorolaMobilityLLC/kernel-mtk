2de3db11ee45ffb7fae72dc2c5635cb881093c64
mtk03113 <nicholas.tang@mediatek.com>
2019-01-22 22:12:21 +0800

    [ALPS02881094] MRDUMP : Export kimage vaddr
    [Detail]
    New kimage virtual address relocation will cause mrdump-gdb failed to load
    master page table.
    
    [Solution]
    Export master page table with PA
    
    MTK-Commit-Id: fd512472453e28f1a8cc682ae60bd341deeae77a
    
    Change-Id: I4c4cd4ac9e60ddf89a21aadcaf93c0346a8e5087
    CR-Id: ALPS02881094
    Feature: Android Exception Engine(AEE)
    Signed-off-by: mtk03113 <nicholas.tang@mediatek.com>

--- drivers/misc/mediatek/aee/mrdump/mrdump_control.c
+++ drivers/misc/mediatek/aee/mrdump/mrdump_control.c
@@ -138,6 +138,10 @@ void mrdump_cblock_init(void)
 	machdesc_p->page_offset = (uint64_t)PAGE_OFFSET;
 	machdesc_p->high_memory = (uintptr_t)high_memory;
 
+#if defined(KIMAGE_VADDR)
+	machdesc_p->kimage_vaddr = KIMAGE_VADDR;
+#endif
+
 	machdesc_p->vmalloc_start = (uint64_t)VMALLOC_START;
 	machdesc_p->vmalloc_end = (uint64_t)VMALLOC_END;
 
@@ -145,7 +149,7 @@ void mrdump_cblock_init(void)
 	machdesc_p->modules_end = (uint64_t)MODULES_END;
 
 	machdesc_p->phys_offset = (uint64_t)PHYS_OFFSET;
-	machdesc_p->master_page_table = (uintptr_t)&swapper_pg_dir;
+	machdesc_p->master_page_table = (uintptr_t)__pa(&swapper_pg_dir);
 
 #if defined(CONFIG_SPARSEMEM_VMEMMAP)
 	machdesc_p->memmap = (uintptr_t)vmemmap;
