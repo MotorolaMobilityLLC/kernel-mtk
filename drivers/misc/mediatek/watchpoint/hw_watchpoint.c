27b336057afb698bfc6aac26e578529c931264ab
Hanks Chen <hanks.chen@mediatek.com>
2019-01-22 22:27:53 +0800

    [ALPS03074266] Platform: remove useless code
    [Detail]
    Kernel-4.4 remove useless code - watchpoint
    
    MTK-Commit-Id: 321afd8c56b9614a4e0fba4d319cece7dba3c361
    
    Change-Id: I13a5819a14b8f835a5a08b65574365b23aa2fa86
    Signed-off-by: Hanks Chen <hanks.chen@mediatek.com>
    CR-Id: ALPS03074266
    Feature: Others

--- drivers/misc/mediatek/watchpoint/hw_watchpoint.c
+++ drivers/misc/mediatek/watchpoint/hw_watchpoint.c
@@ -145,11 +145,8 @@ void reset_watchpoint(void)
 	int j;
 	int i;
 	unsigned int args;
-#ifdef CONFIG_ARCH_MT6580
-	int offset = 2;
-#else
 	int offset = 4;
-#endif
+
 	for_each_online_cpu(j) {
 		args = cs_cpu_read(wp_tracer.debug_regs[j], EDSCR);
 		pr_debug("[MTK WP] Reset flow cpu %d, EDSCR 0x%x\n", j, args);
@@ -215,11 +212,8 @@ int add_hw_watchpoint(struct wp_event *wp_event)
 	int ret, i, j;
 	unsigned long flags;
 	unsigned int ctl;
-#ifdef CONFIG_ARCH_MT6580
-	int offset = 2;
-#else
 	int offset = 4;
-#endif
+
 	if (!wp_event)
 		return -EINVAL;
 
@@ -290,11 +284,8 @@ int del_hw_watchpoint(struct wp_event *wp_event)
 {
 	unsigned long flags;
 	int i, j;
-#ifdef CONFIG_ARCH_MT6580
-	int offset = 2;
-#else
 	int offset = 4;
-#endif
+
 	if (!wp_event)
 		return -EINVAL;
 
@@ -339,12 +330,7 @@ static int watchpoint_handler(unsigned long addr, unsigned int esr, struct pt_re
  * and WVRs in both aarch32 and aarch64 mode, we have no choices
  * but referring to Chip name to configure the offset.
  */
-
-#ifdef CONFIG_ARCH_MT6580
-	int offset = 2;
-#else
 	int offset = 4;
-#endif
 	/* Notes
 	 * wfar is the watched data address which is accessed . and it is from FAR_EL1
 	 */
