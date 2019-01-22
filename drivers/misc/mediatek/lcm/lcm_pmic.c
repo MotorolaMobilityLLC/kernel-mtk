8a886b61428ee33a2ab21035bec5e15c319a6290
mtk81025 <wenying.wang@mediatek.com>
2019-01-22 22:56:09 +0800

    [ALPS02809148] [mt6759] display: porting for rt5081
    [Detail]
    1. porting for rt5081 removing always on 5v
    
    MTK-Commit-Id: 7a19a103f6701623fd9988ac520711abb9de676a
    
    Change-Id: Ied217a374a8191566af96aff6043d282d7815a97
    Signed-off-by: mtk81025 <wenying.wang@mediatek.com>
    CR-Id: ALPS02809148
    Feature: Display Driver (DSI Interface)

--- drivers/misc/mediatek/lcm/lcm_pmic.c
+++ drivers/misc/mediatek/lcm/lcm_pmic.c
@@ -11,4 +11,126 @@
  * GNU General Public License for more details.
  */
 
+#include <linux/regulator/mediatek/mtk_regulator.h>
+#include <linux/string.h>
+#include <linux/kernel.h>
+
+#ifdef CONFIG_RT5081_PMU_DSV
+static struct mtk_regulator disp_bias_pos;
+static struct mtk_regulator disp_bias_neg;
+static int regulator_inited;
+
+int display_bias_regulator_init(void)
+{
+	int ret = 0;
+
+	if (regulator_inited)
+		return ret;
+
+	/* please only get regulator once in a driver */
+	ret = mtk_regulator_get(NULL, "dsv_pos", &disp_bias_pos);
+	if (ret < 0) { /* handle return value */
+		pr_err("get dsv_pos fail, ret = %d\n", ret);
+		return ret;
+	}
+
+	ret = mtk_regulator_get(NULL, "dsv_neg", &disp_bias_neg);
+	if (ret < 0) { /* handle return value */
+		pr_err("get dsv_pos fail, ret = %d\n", ret);
+		return ret;
+	}
+
+	regulator_inited = 1;
+	return ret; /* must be 0 */
+
+}
+EXPORT_SYMBOL(display_bias_regulator_init);
+
+int display_bias_enable(void)
+{
+	int ret = 0;
+	int retval = 0;
+
+	if (!regulator_inited)
+		return ret;
+
+	/* set voltage with min & max*/
+	ret = mtk_regulator_set_voltage(&disp_bias_pos, 5400000, 5400000);
+	if (ret < 0)
+		pr_err("set voltage disp_bias_pos fail, ret = %d\n", ret);
+	retval |= ret;
+
+	ret = mtk_regulator_set_voltage(&disp_bias_neg, 5400000, 5400000);
+	if (ret < 0)
+		pr_err("set voltage disp_bias_neg fail, ret = %d\n", ret);
+	retval |= ret;
+
+#if 0
+	/* get voltage */
+	ret = mtk_regulator_get_voltage(&disp_bias_pos);
+	if (ret < 0)
+		pr_err("get voltage disp_bias_pos fail\n");
+	pr_debug("pos voltage = %d\n", ret);
+
+	ret = mtk_regulator_get_voltage(&disp_bias_neg);
+	if (ret < 0)
+		pr_err("get voltage disp_bias_neg fail\n");
+	pr_debug("neg voltage = %d\n", ret);
+#endif
+	/* enable regulator */
+	ret = mtk_regulator_enable(&disp_bias_pos, true);
+	if (ret < 0)
+		pr_err("enable regulator disp_bias_pos fail, ret = %d\n", ret);
+	retval |= ret;
+
+	ret = mtk_regulator_enable(&disp_bias_neg, true);
+	if (ret < 0)
+		pr_err("enable regulator disp_bias_neg fail, ret = %d\n", ret);
+	retval |= ret;
+
+	return retval;
+}
+EXPORT_SYMBOL(display_bias_enable);
+
+int display_bias_disable(void)
+{
+	int ret = 0;
+	int retval = 0;
+
+	if (!regulator_inited)
+		return ret;
+
+	ret = mtk_regulator_enable(&disp_bias_neg, false);
+	if (ret < 0)
+		pr_err("disable regulator disp_bias_neg fail, ret = %d\n", ret);
+	retval |= ret;
+
+	ret = mtk_regulator_enable(&disp_bias_pos, false);
+	if (ret < 0)
+		pr_err("disable regulator disp_bias_pos fail, ret = %d\n", ret);
+	retval |= ret;
+
+	return retval;
+}
+EXPORT_SYMBOL(display_bias_disable);
+
+#else
+int display_bias_regulator_init(void)
+{
+	return 0;
+}
+EXPORT_SYMBOL(display_bias_regulator_init);
+
+int display_bias_enable(void)
+{
+	return 0;
+}
+EXPORT_SYMBOL(display_bias_enable);
+
+int display_bias_disable(void)
+{
+	return 0;
+}
+EXPORT_SYMBOL(display_bias_disable);
+#endif
 
