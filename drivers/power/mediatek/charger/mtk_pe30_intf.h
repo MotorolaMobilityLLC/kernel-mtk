a67358e8aff2eff2334e42b0a486e8fa50927b5a
Wy Chuang <wy.chuang@mediatek.com>
2019-01-22 22:23:31 +0800

    [ALPS02926934] charger: move charger setting from public to driver
    [Detail]
    PE 3.0 is special feature , move all setting to driver to avoid
    leaking information.
    
    MTK-Commit-Id: 04cdb8c1f01526d8e1f418593caadd3aef6ea690
    
    Change-Id: I55a3de92252dd539fa77de6aed353ef36cc48b17
    Signed-off-by: Wy Chuang <wy.chuang@mediatek.com>
    CR-Id: ALPS02926934
    Feature: Charger

--- drivers/power/mediatek/charger/mtk_pe30_intf.h
+++ drivers/power/mediatek/charger/mtk_pe30_intf.h
@@ -16,6 +16,45 @@
 #ifndef __MTK_PE_30_INTF_H
 #define __MTK_PE_30_INTF_H
 
+/* pe3.0 */
+#define CV_LIMIT 6000	/* vbus upper bound */
+#define BAT_UPPER_BOUND 4350	/* battery upper bound */
+#define BAT_LOWER_BOUND 3000	/* battery low bound */
+
+#define CC_SS_INIT 1500	/*init charging current (mA)*/
+#define CC_INIT 5000	/*max charging current (mA)*/
+#define CC_INIT_BAD_CABLE1 4000	/*charging current(mA) for bad Cable1*/
+#define CC_INIT_BAD_CABLE2 3000	/*charging current(mA) for bad Cable2*/
+#define CC_INIT_R 230	/*good cable max impedance*/
+#define CC_INIT_BAD_CABLE1_R 280	/*bad cable1 max impedance*/
+#define CC_INIT_BAD_CABLE2_R 360	/*bad cable2 max impedance*/
+
+#define CC_NORMAL 4000000	/*normal charging ibus OC threshold (uA)*/
+#define CC_MAX 6500000	/*PE3.0 ibus OC threshold (uA)*/
+#define CC_END 2000	/*PE3.0 min charging current (mA)*/
+#define CC_STEP 200	/*CC state charging current step (mA)*/
+#define CC_SS_STEP 500	/*soft start state charging current step (mA)*/
+#define CC_SS_STEP2 200	/*soft start state charging current step (mA),when battery voltage > CC_SS_STEP2*/
+#define CC_SS_STEP3 100	/*soft start state charging current step (mA),when battery voltage > CC_SS_STEP3*/
+#define CV_SS_STEP2 4000	/*battery voltage threshold for CC_SS_STEP2*/
+#define CV_SS_STEP3 4200	/*battery voltage threshold for CC_SS_STEP3*/
+#define CC_SS_BLANKING 100	/*polling duraction for init/soft start state(ms)*/
+#define CC_BLANKING 1000	/*polling duraction for CC state(ms)*/
+#define CHARGER_TEMP_MAX 120	/*max charger IC temperature*/
+#define TA_TEMP_MAX 80	/*max adapter temperature*/
+#define VBUS_OV_GAP 100
+#define FOD_CURRENT 200 /*FOD current threshold*/
+#define R_VBAT_MIN 40	/*min R_VBAT*/
+#define R_SW_MIN 20	/*min R_SW*/
+#define PE30_MAX_CHARGING_TIME 5400	/*PE3.0 max chargint time (sec)*/
+#if defined(CONFIG_MTK_JEITA_STANDARD_SUPPORT)
+#define BATTERY_TEMP_MIN TEMP_T2_THRESHOLD
+#define BATTERY_TEMP_MAX TEMP_T3_THRESHOLD
+#else
+#define BATTERY_TEMP_MIN 0	/*PE3.0 min battery temperature*/
+#define BATTERY_TEMP_MAX 50	/*PE3.0 man battery temperature*/
+#endif
+
 struct mtk_pe30_cap {
 	int cur;
 	int vol;
