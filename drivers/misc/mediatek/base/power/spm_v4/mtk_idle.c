19ed81d2579f2323ec66f05959e40feba18f1ec1
JM Lai <jm.lai@mediatek.com>
2019-01-22 22:57:20 +0800

    [ALPS03116931] spm: turn off xo_ufs when ufs in hibernate for MT6763
    [Detail]
    clkbuf is ready, turn off xo_ufs when ufs is in hibernate.
    
    MTK-Commit-Id: 74c7aef130498504dd00f22f58ff2eb13d49d1d6
    
    Change-Id: Ibb779346390326becb349305b07f3b8c2cbd54e0
    Signed-off-by: JM Lai <jm.lai@mediatek.com>
    CR-Id: ALPS03116931
    Feature: SODI (Screen On Deep Idle)

--- drivers/misc/mediatek/base/power/spm_v4/mtk_idle.c
+++ drivers/misc/mediatek/base/power/spm_v4/mtk_idle.c
@@ -1005,8 +1005,7 @@ unsigned int ufs_cb_before_xxidle(void)
 	bool ufs_in_hibernate = false;
 
 	ufs_in_hibernate = !ufs_mtk_deepidle_hibern8_check();
-	/* FIXME: Don't turn on/off XO_UFS before clkbuf ready */
-	/* op_cond = ufs_in_hibernate ? DEEPIDLE_OPT_XO_UFS_ON_OFF : 0; */
+	op_cond = ufs_in_hibernate ? DEEPIDLE_OPT_XO_UFS_ON_OFF : 0;
 
 	return op_cond;
 #else
