f2571c8924eb3987b4edcc08b5a72baaf67691f0
Calvin Liao <calvin.liao@mediatek.com>
2019-01-22 23:01:57 +0800

    [ALPS03308416] TEEI: kernel license
    [Detail]
    Add related license description.
    
    MTK-Commit-Id: 849ccd2e8f5855dfbcc45994d516855ba65fcfee
    
    Change-Id: Idbf87793dac9c519b0b56f2d6c2165714c14a9ab
    Signed-off-by: Calvin Liao <calvin.liao@mediatek.com>
    CR-Id: ALPS03308416
    Feature: Microtrust TEEI

--- drivers/misc/mediatek/teei/V1.0/tz_vfs/fp_vendor.c
+++ drivers/misc/mediatek/teei/V1.0/tz_vfs/fp_vendor.c
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2015-2017 MICROTRUST Incorporated
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
 #include "fp_vendor.h"
 #include<linux/types.h>
 #include "../tz_driver/include/nt_smc_call.h"
