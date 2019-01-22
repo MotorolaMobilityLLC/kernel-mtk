95ea75f92b4bd60227758dedd4bdfda865f17ed9
Xinghua Yang <xinghua.yang@mediatek.com>
2019-01-22 23:01:46 +0800

    [ALPS02692772] aee: enable fulldump
    [Detail]
    enable fulldump for nested panic and ipanic
    
    [Solution]
    enable fulldump for nested panic and ipanic
    
    MTK-Commit-Id: c547df1817368b71c8ce0e1aa9d08589bbb1b2c9
    
    Change-Id: If5a9f86bee0d982aeb6dcb78df9ac2ce83502148
    Signed-off-by: Xinghua Yang <xinghua.yang@mediatek.com>
    CR-Id: ALPS02692772
    Feature: Others

--- drivers/misc/mediatek/aee/ipanic/ipanic_rom.c
+++ drivers/misc/mediatek/aee/ipanic/ipanic_rom.c
@@ -507,6 +507,7 @@ int ipanic(struct notifier_block *this, unsigned long event, void *ptr)
 	int dt;
 	int errno;
 	struct ipanic_header *ipanic_hdr;
+	struct pt_regs saved_regs;
 
 	memset(&dumper, 0x0, sizeof(struct kmsg_dumper));
 #ifdef CONFIG_MTK_RAM_CONSOLE
@@ -514,6 +515,8 @@ int ipanic(struct notifier_block *this, unsigned long event, void *ptr)
 	aee_rr_rec_exp_type(2);
 #endif
 	bust_spinlocks(1);
+	mrdump_mini_save_regs(&saved_regs);
+	__mrdump_create_oops_dump(AEE_REBOOT_MODE_KERNEL_PANIC, &saved_regs, "Kernel Panic");
 	spin_lock_irq(&ipanic_lock);
 	aee_disable_api();
 	mrdump_mini_ke_cpu_regs(NULL);
@@ -574,12 +577,23 @@ void ipanic_recursive_ke(struct pt_regs *regs, struct pt_regs *excp_regs, int cp
 {
 	int errno;
 	struct kmsg_dumper dumper;
+	struct pt_regs saved_regs;
 
 #ifdef CONFIG_MTK_RAM_CONSOLE
 	aee_rr_rec_exp_type(3);
 #endif
 	aee_nested_printf("minidump\n");
 	bust_spinlocks(1);
+	if (excp_regs != NULL) {
+		__mrdump_create_oops_dump(AEE_REBOOT_MODE_NESTED_EXCEPTION, excp_regs, "Kernel NestedPanic");
+	} else if (regs != NULL) {
+		aee_nested_printf("previous excp_regs NULL\n");
+		__mrdump_create_oops_dump(AEE_REBOOT_MODE_NESTED_EXCEPTION, regs, "Kernel NestedPanic");
+	} else {
+		aee_nested_printf("both NULL\n");
+		mrdump_mini_save_regs(&saved_regs);
+		__mrdump_create_oops_dump(AEE_REBOOT_MODE_NESTED_EXCEPTION, &saved_regs, "Kernel NestedPanic");
+	}
 	inner_dcache_flush_all();
 #ifdef __aarch64__
 	inner_dcache_disable();
