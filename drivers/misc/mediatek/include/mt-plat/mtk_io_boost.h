b630953df5949b66a64e1e9e3816cc3cce82b37f
Stanley Chu <stanley.chu@mediatek.com>
2019-01-23 00:07:20 +0800

    [ALPS03880333] io-boost: fix potential deadlock issue
    [Detail]
    Avoid I/O threads opening task file (in sysfs) themselves because
    file opening may trigger I/O and then deadlock happens.
    
    Use a specific kthread instead for task initialization.
    
    MTK-Commit-Id: bff18f474b16fe0f41e8449943d02076594cafee
    
    Change-Id: Ib06b02f988b3a5762af504a5ce0e0dca087e3961
    Signed-off-by: Stanley Chu <stanley.chu@mediatek.com>
    CR-Id: ALPS03880333
    Feature: System Performance

--- drivers/misc/mediatek/include/mt-plat/mtk_io_boost.h
+++ drivers/misc/mediatek/include/mt-plat/mtk_io_boost.h
@@ -14,8 +14,7 @@
 #ifndef __MTK_IO_BOOST_H
 #define __MTK_IO_BOOST_H
 
-extern int mtk_io_boost_add_tid(int tid);
-extern int mtk_io_boost_test_and_add_tid(int tid, bool *done);
+extern int mtk_iobst_register_tid(int tid);
 
 #endif
 
