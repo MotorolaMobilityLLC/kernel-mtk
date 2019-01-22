dd1c73938f0aabbf4ba78a079c085459d6a43638
Josepchi Tsai <josepchi.tsai@mediatek.com>
2019-01-23 00:06:01 +0800

    [ALPS03778565] Cam: Fix IMX386
    [Detail]
    
    [Solution]
    
    MTK-Commit-Id: a7f327092a48b4f5b80760c389eea5a58992b10b
    
    Change-Id: Id44e22cf783bbe8425cec12aa6453e003a056691
    CR-Id: ALPS03778565
    Feature: Others
    Signed-off-by: Josepchi Tsai <josepchi.tsai@mediatek.com>

--- drivers/misc/mediatek/imgsensor/src/mt6771/imx386_mipi_raw/imx386mipi_sensor.h
+++ drivers/misc/mediatek/imgsensor/src/mt6771/imx386_mipi_raw/imx386mipi_sensor.h
@@ -129,5 +129,6 @@ struct imgsensor_info_struct {
 extern int iReadRegI2C(u8 *a_pSendData, u16 a_sizeSendData, u8 *a_pRecvData, u16 a_sizeRecvData, u16 i2cId);
 extern int iWriteRegI2C(u8 *a_pSendData, u16 a_sizeSendData, u16 i2cId);
 extern int iBurstWriteReg(u8 *pData, u32 bytes, u16 i2cId);
+extern int iBurstWriteReg_multi(u8 *pData, u32 bytes, u16 i2cId, u16 transfer_length, u16 timing);
 
 #endif
