492b43a627ae1a80b0f85893bb70b1427ceee5d6
Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
2019-01-22 22:57:55 +0800

    [ALPS03162263] cmdq: check command structure
    1. Check request register count from ioctl,
    to protect correct memory allocate.
    2. Check command size in valid range.
    
    MTK-Commit-Id: 6565ed26017d653b646d45e91b27a5d5b13309a6
    
    Change-Id: Icc2f6ac46b2a0ade3a10e8b59d3b981a2d3a8b78
    Signed-off-by: Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
    CR-Id: ALPS03162263
    Feature: CMDQ

--- drivers/misc/mediatek/cmdq/v2/cmdq_core.h
+++ drivers/misc/mediatek/cmdq/v2/cmdq_core.h
@@ -112,6 +112,7 @@ else								\
 #define CMDQ_THREAD_SEC_MDP				14
 
 /* max count of regs */
+#define CMDQ_MAX_COMMAND_SIZE		(0x10000)
 #define CMDQ_MAX_DUMP_REG_COUNT		(2048)
 #define CMDQ_MAX_WRITE_ADDR_COUNT	(PAGE_SIZE / sizeof(u32))
 
