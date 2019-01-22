96ef51ac761ba77fc78687410c41b1af62345ce9
mtk12379 <jing-ting.wu@mediatek.com>
2019-01-22 20:15:07 +0800

    [ALPS02433942] sched: add sched debug info
    [Detail]
    1. add sched debug AEE info at HWT
    2. add aee_kernel_exception in  __schedule_bug.
    3. change raw_spin_lock_irqsave to raw_spin_trylock_n_irqsave
    4. use aee_sram_fiq_log() print sched debug AEE info to SYS_LAST_KMSG
    
    [Solution]
    add source code
    
    MTK-Commit-Id: 0de10cb616b67c376faaa15332fc8917c9cf6062
    
    Change-Id: I5cf73af6429d961d39f4d3b75cfca3034e2515bf
    CR-Id:ALPS02433942
    Feature:Sched
    Signed-off-by: mtk12379 <jing-ting.wu@mediatek.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -83,6 +83,8 @@
 #include <asm/paravirt.h>
 #endif
 
+#include <mt-plat/aee.h>
+
 #include "sched.h"
 #include "../workqueue_internal.h"
 #include "../smpboot.h"
@@ -3057,6 +3059,7 @@ static noinline void __schedule_bug(struct task_struct *prev)
 #endif
 	dump_stack();
 	add_taint(TAINT_WARN, LOCKDEP_STILL_OK);
+	aee_kernel_exception("Sched exception", "__schedule_bug: call schedule() when preempt disable");
 }
 
 /*
