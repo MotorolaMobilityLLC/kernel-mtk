6333b279582a60575dae48d0a75ffe7aa9a2b8f1
CCJ Yeh <ccj.yeh@mediatek.com>
2019-01-23 14:00:45 +0800

    [ALPS04143887] CPU Hotplug: Resolve Syzkaller Scan issue
    Resolve Syzkaller Scan issue
    
    MTK-Commit-Id: 186c5c5a4e4b83c8a6b9cdb198eac580403d2c46
    
    Change-Id: I7e7110b1a307bd75b6567a6e941e1f75000bc977
    Signed-off-by: CCJ Yeh <ccj.yeh@mediatek.com>
    CR-Id: ALPS04143887
    Feature: Vulnerability Scan

--- drivers/misc/mediatek/base/power/hps_v3/mtk_hps_core.c
+++ drivers/misc/mediatek/base/power/hps_v3/mtk_hps_core.c
@@ -372,6 +372,7 @@ ACAO_HPS_START:
 	aee_rr_rec_hps_cb_footprint(2);
 	aee_rr_rec_hps_cb_fp_times((u64) ktime_to_ms(ktime_get()));
 	/*Debug message dump*/
+	cpu_onoff = 0;
 	for (i = 0 ; i < 8 ; i++) {
 
 		if (i >= setup_max_cpus)
