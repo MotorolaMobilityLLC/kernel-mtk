6b940ae35ecfad50167e3b4617df2071a7403080
Shane Chien <Shane.Chien@mediatek.com>
2019-02-28 00:14:00 +0800

    [ALPS03633174] ASoC: APLL sequence fix in hifi playback
    [Detail]
    Add apll kcontrol for dl2 pcm to control apll.
    
    MTK-Commit-Id: 9391b78bf5280ad01a8f4a81b372aa5729275e0b
    
    Change-Id: Ica9675a561e75b3357b9497eee1c2566d25b1518
    Signed-off-by: Shane Chien <Shane.Chien@mediatek.com>
    CR-Id: ALPS03633174
    Feature: Driver

--- sound/soc/mediatek/common_int/mtk-soc-pcm-deep-buffer-dl.c
+++ sound/soc/mediatek/common_int/mtk-soc-pcm-deep-buffer-dl.c
@@ -108,10 +108,10 @@ static int deep_buffer_dl_hdoutput_get(struct snd_kcontrol *kcontrol,
 static int deep_buffer_dl_hdoutput_set(struct snd_kcontrol *kcontrol,
 				       struct snd_ctl_elem_value *ucontrol)
 {
-	pr_debug("%s()\n", __func__);
+	/* pr_debug("%s()\n", __func__); */
 	if (ucontrol->value.enumerated.item[0] >
 	    ARRAY_SIZE(deep_buffer_dl_HD_output)) {
-		pr_err("return -EINVAL\n");
+		pr_warn("%s(), return -EINVAL\n", __func__);
 		return -EINVAL;
 	}
 
