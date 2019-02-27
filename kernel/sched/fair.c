c3dcd6af9b65ba548d8fc7445b0a3125b8a0d112
YT Chang (張雅婷) <yt.chang@mediatek.com>
2019-02-28 02:35:08 +0800

    [ALPS03980620] sched: revert disable CONFIG_SCHED_DEBUG in user load
    [Detail]
    revert disable CONFIG_SCHED_DEBUG in user load
    Due to VTS fail
    VtsKernelProcFileApi#testProcSchedLatencyNS fail
    /proc/sys/kernel/sched_latency_ns: File does not exist.
    VtsKernelProcFileApi#testProcSchedTunableScaling fail
    /proc/sys/kernel/sched_tunable_scaling: File does not exist.
    VtsKernelProcFileApi#testProcSchedWakeupGranularityNS fail
    /proc/sys/kernel/sched_wakeup_granularity_ns: File does not exist.
    
    MTK-Commit-Id: 8bb0b5c339e0aa9d5ad8d27b54d14e4cabfcda35
    
    Change-Id: I4689aa519a5b0a075de680ebe11147044fbb7ffe
    Signed-off-by: YT Chang <yt.chang@mediatek.com>
    CR-Id: ALPS03980620
    Feature: Energy Aware Scheduling Plus (EAS+)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -111,7 +111,7 @@ unsigned int sysctl_sched_wakeup_granularity = 1000000UL;
 unsigned int normalized_sysctl_sched_wakeup_granularity = 1000000UL;
 
 const_debug unsigned int sched_cache_hot_migration_cost = 500000UL;
-__read_mostly unsigned int sysctl_sched_migration_cost = 500000UL;
+const_debug unsigned int sysctl_sched_migration_cost = 500000UL;
 
 /*
  * The exponential sliding  window over which load is averaged for shares
