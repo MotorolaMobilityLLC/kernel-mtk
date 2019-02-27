5c6a4fc010a682883e9320c955f7011713046c50
luise.chen <luise.chen@mediatek.com>
2019-02-28 02:29:54 +0800

    [ALPS03800366] GPU DVFS: Add merged patch
    [Detail]
    Patch list:
    ALPS03574894 - GPU DVFS: Make FB-DVFS effective only on main renderer
    ALPS03779915 - GPU: Boost GPU freq.
    
    MTK-Commit-Id: b354e4b447b26da123901c29c74d0f5f30cdc3d2
    
    Change-Id: I90c76a1a7cef83a7edfffbffa2c218c68230a7c3
    CR-Id: ALPS03800366
    Feature: GPU DVFS
    Signed-off-by: luise.chen <luise.chen@mediatek.com>

--- drivers/misc/mediatek/gpu/ged/src/ged_bridge.c
+++ drivers/misc/mediatek/gpu/ged/src/ged_bridge.c
@@ -59,7 +59,9 @@ int ged_bridge_boost_gpu_freq(
 		GED_BRIDGE_OUT_BOOSTGPUFREQ *psBoostGpuFreqOUT)
 {
 #if 1
-	psBoostGpuFreqOUT->eError = mtk_boost_gpu_freq() ? GED_OK : GED_ERROR_FAIL;
+	psBoostGpuFreqOUT->eError = (mtk_set_bottom_gpu_freq(
+		psBoostGpuFreqIN->eGPUFreqLevel) == true) ?
+		GED_OK : GED_ERROR_FAIL;
 #else
 	unsigned int ui32Count;
 	if (mtk_custom_get_gpu_freq_level_count(&ui32Count))
