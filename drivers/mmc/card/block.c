3a53f5acc3e9313c2d8a9bc332e8d28ce3eeffe5
Ma Shi <shi.ma@mediatek.com>
2019-02-28 00:22:11 +0800

    [ALPS03725524] fs: mt6765: add hie for ext4/msdc
    [Detail]
    add hie for ext4/msdc
    
    MTK-Commit-Id: acf1c03226809cee44282c1ba86676eaeb9d5542
    
    Change-Id: I496740b4cf9050cd9c4897ce614ec0909ccaf6c4
    Signed-off-by: Ma Shi <shi.ma@mediatek.com>
    CR-Id: ALPS03725524
    Feature: [Android Default] EXT4 File System

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -2250,12 +2250,6 @@ static void mmc_blk_rw_rq_prep(struct mmc_queue_req *mqrq,
 
 	mqrq->mmc_active.mrq = &brq->mrq;
 	mqrq->mmc_active.err_check = mmc_blk_err_check;
-#ifdef CONFIG_MTK_HW_FDE
-	if (mqrq->req->bio) {
-		brq->mrq.bi_hw_fde = mqrq->req->bio->bi_hw_fde;
-		brq->mrq.bi_key_idx = mqrq->req->bio->bi_key_idx;
-	}
-#endif
 
 #ifdef CONFIG_MTK_EMMC_CQ_SUPPORT
 	if (card->ext_csd.cmdq_mode_en) {
@@ -2289,11 +2283,12 @@ static void mmc_blk_rw_rq_prep(struct mmc_queue_req *mqrq,
 	}
 #endif
 
-#ifdef CONFIG_MTK_HW_FDE
-	if (mqrq->req->bio) {
-		brq->mrq.bi_hw_fde = mqrq->req->bio->bi_hw_fde;
-		brq->mrq.bi_key_idx = mqrq->req->bio->bi_key_idx;
-	}
+#if defined(CONFIG_MTK_HW_FDE) || defined(CONFIG_HIE)
+	if (mqrq->req->bio)
+		brq->mrq.req = mqrq->req;
+
+	/* request is from mmc layer */
+	brq->mrq.is_mmc_req = true;
 #endif
 
 	mmc_queue_bounce_pre(mqrq);
