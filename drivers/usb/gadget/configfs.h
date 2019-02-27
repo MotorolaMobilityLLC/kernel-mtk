fb7ed27f51deb67e830a3949f999871dbfc19bd8
Arvin Wang <arvin.wang@mediatek.com>
2019-02-28 00:29:18 +0800

    [ALPS03561644] usb : add configfs support
    [Detail]
       add musb driver configfs support
    
    MTK-Commit-Id: 06d86ee92c95fd4b7e3e315854895917eccc7816
    
    Change-Id: I6843d275f19270dfd69f00d672f376f4031fb900
    Signed-off-by: Arvin Wang <arvin.wang@mediatek.com>
    CR-Id: ALPS03561644
    Feature: USB 3.0 Device

--- drivers/usb/gadget/configfs.h
+++ drivers/usb/gadget/configfs.h
@@ -16,6 +16,7 @@ static inline struct usb_os_desc *to_usb_os_desc(struct config_item *item)
 {
 	return container_of(to_config_group(item), struct usb_os_desc, group);
 }
+extern int acm_shortcut(void);
 
 extern struct device *create_function_device(char *name);
 
