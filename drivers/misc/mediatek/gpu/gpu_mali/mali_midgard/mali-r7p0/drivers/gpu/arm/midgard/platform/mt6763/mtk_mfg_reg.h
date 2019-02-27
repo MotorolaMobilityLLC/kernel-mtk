fa5d8f5ecabe4d85c59d9628e15e76ccc105696a
mtk07406 <richard-yc.wang@mediatek.com>
2019-02-27 22:01:46 +0800

    [ALPS03384803] GPU: submit vendor DDK source code
    [Detail]
    submit vendor DDK source code, Makefile and config.
    1. DDK version : bifrost r5p0
    2. GPU : ARM G71
    
    MTK-Commit-Id: e1f69564cc6ff56cd619c6a68e2b695fec286860
    
    Change-Id: I95e6dbbfb7362474f7fd321d545e2f43a1cef5d3
    Signed-off-by: mtk07406 <richard-yc.wang@mediatek.com>
    CR-Id: ALPS03384803
    Feature: OpenGL|ES

---
+++ drivers/misc/mediatek/gpu/gpu_mali/mali_midgard/mali-r7p0/drivers/gpu/arm/midgard/platform/mt6763/mtk_mfg_reg.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#ifndef __MTK_MFG_REG_H__
+#define __MTK_MFG_REG_H__
+
+/* MFG reg */
+#define MFG_DEBUG_SEL   0x180
+#define MFG_DEBUG_A 0x184
+#define MFG_DEBUG_IDEL	(1 << 2)
+
+/* MFG Power Monitor */
+#define MFG_PERF_EN_00 0x3C0
+#define MFG_PERF_EN_01 0x3C4
+#define MFG_PERF_EN_02 0x3C8
+#define MFG_PERF_EN_03 0x3CC
+#define MFG_PERF_EN_04 0x3D0
+#define MFG_1to2_CFG_CON_00 0x8F0
+#define MFG_1to2_CFG_CON_01 0x8F4
+
+extern volatile void *g_MFG_base;
+
+#define base_write32(addr, value)	writel(value, addr)
+#define base_read32(addr)			readl(addr)
+#define MFG_write32(addr, value)	base_write32(g_MFG_base+addr, value)
+#define MFG_read32(addr)			base_read32(g_MFG_base+addr)
+
+#endif /* __MTK_MFG_REG_H__ */
