068c04ac29280ca74d93391846bd9383bf106327
Hanks Chen <hanks.chen@mediatek.com>
2019-02-28 01:30:11 +0800

    [ALPS03702583] platform: porting platform debug info
    [Detail]
    1) porting platform debug info for lk dump
    2) update sram flag driver
    
    MTK-Commit-Id: 07486567458f06cee9b8d77917378d84862eba65
    
    Change-Id: I1649a461cfe0b53280ffbc40fbb406101f628cc3
    Signed-off-by: Hanks Chen <hanks.chen@mediatek.com>
    CR-Id: ALPS03702583
    Feature: Others

--- drivers/misc/mediatek/debug_latch/plat_dbg_info/plat_sram_flag.h
+++ drivers/misc/mediatek/debug_latch/plat_dbg_info/plat_sram_flag.h
@@ -15,11 +15,16 @@
 #define __PLAT_SRAM_FLAG_H__
 
 /* each flag is a word */
-#define PLAT_FLAG0	0
-#define PLAT_FLAG1	1
-#define PLAT_FLAG2	2
-#define PLAT_FLAG3	3
+struct plat_sram_flag {
+	unsigned int plat_magic;
+	unsigned int plat_sram_flag0;
+	unsigned int plat_sram_flag1;
+	unsigned int plat_sram_flag2;
+};
 
+#define DEF_PLAT_SRAM_FLAG PLAT_SRAM_FLAG_T
+
+#define PLAT_SRAM_FLAG_KEY	0xDB45
 /*
  * PLAT_FLAG0:
  * bit[0:0] = lastpc_valid,
@@ -67,4 +72,8 @@
 int set_sram_flag_lastpc_valid(void);
 int set_sram_flag_dfd_valid(void);
 int set_sram_flag_etb_user(unsigned int utb_id, unsigned int user_id);
+
+extern void __iomem *get_dbg_info_base(unsigned int key);
+extern unsigned int get_dbg_info_size(unsigned int key);
+
 #endif
