7858e3a247f671183eb6f0e437d1827df4ce483e
Howard Hsu <howard.hsu@mediatek.com>
2019-02-28 02:30:37 +0800

    [ALPS03784003] VCORE DVFS: update LP4 DVFS table
    [Detail] update LP4 DVFS table
    
    [Solution] upadte LP4 DVFS table
    
    MTK-Commit-Id: b700e09b644ee90369df58d0c17c7890793c1e95
    
    Change-Id: I25a123f9d584e81b89818fc42323bf44866c808c
    Signed-off-by: Howard Hsu <howard.hsu@mediatek.com>
    CR-Id: ALPS03784003
    Feature: VCORE DVFS
    (cherry picked from commit 3aeedee2a309b3e7a588ecba9950c9514525ca71)

--- drivers/devfreq/helio-dvfsrc-opp-mt6765.c
+++ drivers/devfreq/helio-dvfsrc-opp-mt6765.c
@@ -133,35 +133,35 @@ void dvfsrc_opp_level_mapping(void)
 
 	switch (spm_get_spmfw_idx()) {
 	case SPMFW_LP4X_2CH_3200:
-		set_vcore_opp(VCORE_DVFS_OPP_0, VCORE_OPP_0);
+		set_vcore_opp(VCORE_DVFS_OPP_0, VCORE_OPP_UNREQ);
 		set_vcore_opp(VCORE_DVFS_OPP_1, VCORE_OPP_0);
 		set_vcore_opp(VCORE_DVFS_OPP_2, VCORE_OPP_0);
 		set_vcore_opp(VCORE_DVFS_OPP_3, VCORE_OPP_0);
-		set_vcore_opp(VCORE_DVFS_OPP_4, VCORE_OPP_1);
-		set_vcore_opp(VCORE_DVFS_OPP_5, VCORE_OPP_1);
-		set_vcore_opp(VCORE_DVFS_OPP_6, VCORE_OPP_1);
-		set_vcore_opp(VCORE_DVFS_OPP_7, VCORE_OPP_1);
+		set_vcore_opp(VCORE_DVFS_OPP_4, VCORE_OPP_0);
+		set_vcore_opp(VCORE_DVFS_OPP_5, VCORE_OPP_0);
+		set_vcore_opp(VCORE_DVFS_OPP_6, VCORE_OPP_0);
+		set_vcore_opp(VCORE_DVFS_OPP_7, VCORE_OPP_0);
 		set_vcore_opp(VCORE_DVFS_OPP_8, VCORE_OPP_1);
 		set_vcore_opp(VCORE_DVFS_OPP_9, VCORE_OPP_1);
 		set_vcore_opp(VCORE_DVFS_OPP_10, VCORE_OPP_1);
 		set_vcore_opp(VCORE_DVFS_OPP_11, VCORE_OPP_1);
-		set_vcore_opp(VCORE_DVFS_OPP_12, VCORE_OPP_2);
+		set_vcore_opp(VCORE_DVFS_OPP_12, VCORE_OPP_1);
 		set_vcore_opp(VCORE_DVFS_OPP_13, VCORE_OPP_2);
 		set_vcore_opp(VCORE_DVFS_OPP_14, VCORE_OPP_2);
 		set_vcore_opp(VCORE_DVFS_OPP_15, VCORE_OPP_3);
 
-		set_ddr_opp(VCORE_DVFS_OPP_0, DDR_OPP_0);
+		set_ddr_opp(VCORE_DVFS_OPP_0, DDR_OPP_UNREQ);
 		set_ddr_opp(VCORE_DVFS_OPP_1, DDR_OPP_0);
 		set_ddr_opp(VCORE_DVFS_OPP_2, DDR_OPP_0);
-		set_ddr_opp(VCORE_DVFS_OPP_3, DDR_OPP_0);
+		set_ddr_opp(VCORE_DVFS_OPP_3, DDR_OPP_1);
 		set_ddr_opp(VCORE_DVFS_OPP_4, DDR_OPP_0);
 		set_ddr_opp(VCORE_DVFS_OPP_5, DDR_OPP_1);
 		set_ddr_opp(VCORE_DVFS_OPP_6, DDR_OPP_0);
 		set_ddr_opp(VCORE_DVFS_OPP_7, DDR_OPP_1);
-		set_ddr_opp(VCORE_DVFS_OPP_8, DDR_OPP_2);
-		set_ddr_opp(VCORE_DVFS_OPP_9, DDR_OPP_0);
-		set_ddr_opp(VCORE_DVFS_OPP_10, DDR_OPP_1);
-		set_ddr_opp(VCORE_DVFS_OPP_11, DDR_OPP_2);
+		set_ddr_opp(VCORE_DVFS_OPP_8, DDR_OPP_1);
+		set_ddr_opp(VCORE_DVFS_OPP_9, DDR_OPP_1);
+		set_ddr_opp(VCORE_DVFS_OPP_10, DDR_OPP_2);
+		set_ddr_opp(VCORE_DVFS_OPP_11, DDR_OPP_1);
 		set_ddr_opp(VCORE_DVFS_OPP_12, DDR_OPP_2);
 		set_ddr_opp(VCORE_DVFS_OPP_13, DDR_OPP_2);
 		set_ddr_opp(VCORE_DVFS_OPP_14, DDR_OPP_2);
