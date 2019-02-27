d379a92f4bb0a5185001645dcdb933710d9acd41
haizhen.wang <haizhen.wang@mediatek.com>
2019-02-28 02:31:56 +0800

    [ALPS03796844] [DISP]: print system timer tick when underflow
    [Detail]
    print the low 32 bit of system timer tick
    when RDMA underflow to 20 times and when DSI underrun
    
    [Solution]
    
    MTK-Commit-Id: a76e49aec12853b897add15f9e1358433c558dae
    
    Change-Id: I63bb2ba89ac97e0a2166b2883e880a9d5b18638e
    Signed-off-by: haizhen.wang <haizhen.wang@mediatek.com>
    CR-Id: ALPS03796844
    Feature: Display Driver (DSI Interface)

--- drivers/misc/mediatek/video/mt6761/videox/disp_helper.c
+++ drivers/misc/mediatek/video/mt6761/videox/disp_helper.c
@@ -419,6 +419,7 @@ void disp_helper_option_init(void)
 	disp_helper_set_option(DISP_OPT_OVL_SBCH, 1);
 
 	disp_helper_set_option(DISP_OPT_DSI_UNDERRUN_AEE, 1);
+	disp_helper_set_option(DISP_OPT_RDMA_UNDERFLOW_AEE, 1);
 }
 
 int disp_helper_get_option_list(char *stringbuf, int buf_len)
