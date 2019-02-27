da1311efb13b7bafe8f052d3f27e961e2e1907b7
Ivan-YH Chen <ivan-yh.chen@mediatek.com>
2019-02-28 02:32:01 +0800

    [ALPS03887500] charger: use bc1.2 for rp 56k
    [Detail]
    Use BC1.2 to select charging current for Rp 56k case.
    
    MTK-Commit-Id: 1c774b010ef255d2a4886a964c75e46c4f853fca
    
    Change-Id: Idc3846d0749a1233de9f3ebe124de0de1f07b6ca
    Signed-off-by: Ivan-YH Chen <ivan-yh.chen@mediatek.com>
    CR-Id: ALPS03887500
    Feature: pump express charging. (fast charging)

--- drivers/power/supply/mediatek/charger/mtk_dual_switch_charging.c
+++ drivers/power/supply/mediatek/charger/mtk_dual_switch_charging.c
@@ -161,7 +161,7 @@ dual_swchg_select_charging_current_limit(struct charger_manager *info)
 			pdata->input_current_limit = 1500000;
 			pdata->charging_current_limit = 2000000;
 		} else {
-			/* for TYPEC_CC_VOLT_SNK_DFT */
+			chr_err("type-C: inquire rp error\n");
 			pdata->input_current_limit = 500000;
 			pdata->charging_current_limit = 500000;
 		}
