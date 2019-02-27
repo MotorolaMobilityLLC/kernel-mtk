c95370cba83c548a63637c8347f2a1fb5e2e16a8
Morven-CF Yeh <morven-cf.yeh@mediatek.com>
2019-02-28 04:03:36 +0800

    [ALPS03869853] cm_mgr: add 10ms stall ratio value
    add 10ms stall ratio value to sram for perf usage
    
    MTK-Commit-Id: e08c8529dd51e176397b7b8f3fb43e30597abeab
    
    Change-Id: Ie70ac84f39c301ce5a5f92453a1bc83cfe6495a5
    Signed-off-by: Morven-CF Yeh <morven-cf.yeh@mediatek.com>
    CR-Id: ALPS03869853
    Feature: Power Management

--- drivers/devfreq/helio-dvfsrc.h
+++ drivers/devfreq/helio-dvfsrc.h
@@ -75,10 +75,7 @@ struct helio_dvfsrc {
 #define QOS_MM_BW		(QOS_TOTAL_BW_BUF_SIZE * 4 + 0xC)
 #define QOS_OTHER_BW		(QOS_TOTAL_BW_BUF_SIZE * 4 + 0x10)
 
-#define QOS_CM_GPU_ONOFF	(0x70)
-#define QOS_CM_GPU_OPP		(0x74)
-#define QOS_CM_RESERVE_2	(0x78)
-#define QOS_CM_RESERVE_3	(0x7C)
+#define QOS_CM_STALL_RATIO(idx) (QOS_SRAM_BASE_MAPPED + idx * 4 + 0x60)
 
 /* PMIC */
 #define vcore_pmic_to_uv(pmic)	\
