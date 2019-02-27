2a02ba6e3f95e2df00bfe9e420dc4d3a6f94a6d5
MY Chuang <my.chuang@mediatek.com>
2019-02-28 04:06:45 +0800

    [ALPS04292325] mrdump: fix build error of redifition
    [Detail]
    Build error:
    1. static declaration of aee_rr_curr_exp_type follows
       non-static declaration
    2. conflicting types for aee_sram_fiq_save_bin
    3. attribute declaration must precede definition
    4. redefinition of aee_sram_fiq_log
    
    [Solution]
    clean up repetitive codes.
    
    MTK-Commit-Id: 00c4f2d41355593d18ce90ee736a8d0e654cead5
    
    Change-Id: I85820543ebb58925238828330f6a010a33a1b224
    Signed-off-by: MY Chuang <my.chuang@mediatek.com>
    CR-Id: ALPS04292325
    Feature: Memory RAM Dump (MRDUMP)
    (cherry picked from commit 712b9001542c796f1e69a17a043e7d1b0c6562f5)

--- drivers/misc/mediatek/aee/mrdump/mrdump_panic.c
+++ drivers/misc/mediatek/aee/mrdump/mrdump_panic.c
@@ -23,9 +23,6 @@
 #include <linux/module.h>
 #include <linux/delay.h>
 #include <mrdump.h>
-#ifdef CONFIG_MTK_RAM_CONSOLE
-#include <mt-plat/mtk_ram_console.h>
-#endif
 #include <linux/reboot.h>
 #include "mrdump_panic.h"
 #ifdef CONFIG_MTK_WATCHDOG
@@ -393,10 +390,6 @@ int aee_in_nested_panic(void)
 }
 static inline void aee_rec_step_nested_panic(int step)
 {
-}
-__weak int aee_rr_curr_fiq_step(void)
-{
-	return -1;
 }
 #endif
 
