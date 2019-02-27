d4101da92a280c2dae6d0cfca15e6dd9b9d722e0
MY Chuang <my.chuang@mediatek.com>
2019-02-28 00:25:54 +0800

    [ALPS03724110] mrdump: revise mini-ramdump write out sram content
    [Detail]
    Due to mrdumps control block restructure, revive mini-ramdump write
    out SRAM content
    
    MTK-Commit-Id: f6cc9d3fbca2f814e2efd60f5b3f55a8527be8f4
    
    Change-Id: Ia5d2d63aa5da2f6bf54350494120af326387a451
    Signed-off-by: MY Chuang <my.chuang@mediatek.com>
    CR-Id: ALPS03724110
    Feature: Memory RAM Dump (MRDUMP)

--- drivers/misc/mediatek/aee/mrdump/mrdump_private.h
+++ drivers/misc/mediatek/aee/mrdump/mrdump_private.h
@@ -60,6 +60,7 @@ struct mrdump_platform {
 
 struct pt_regs;
 
+extern struct mrdump_rsvmem_block mrdump_sram_cb;
 extern struct mrdump_control_block *mrdump_cblock;
 extern const unsigned long kallsyms_addresses[] __weak;
 extern const u8 kallsyms_names[] __weak;
