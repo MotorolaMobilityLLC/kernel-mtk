68bb6d61860852030cb44e71d0d8756def23da0c
mtk10485 <Sam.Hung@mediatek.com>
2019-02-28 00:36:44 +0800

    [ALPS03668928] VCM: modify AF init flow
    [Detail] reset AF_Opend flag
    
    [Solution]
    
    MTK-Commit-Id: 758828633710513f27e86b51972f1c7a7229cb7a
    
    Change-Id: I7fcc454f7ab772d1335e41917b6f9cc265a5050b
    Signed-off-by: mtk10485 <Sam.Hung@mediatek.com>
    CR-Id: ALPS03668928
    Feature: [Android Default] AF (contrast AF)

--- drivers/misc/mediatek/lens/main/main_lens.c
+++ drivers/misc/mediatek/lens/main/main_lens.c
@@ -354,6 +354,12 @@ static long AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
 			(__user struct stAF_MotorName *)(a_u4Param));
 		break;
 
+	case AFIOC_S_SETDRVINIT:
+		spin_lock(&g_AF_SpinLock);
+		g_s4AF_Opened = 1;
+		spin_unlock(&g_AF_SpinLock);
+		break;
+
 	case AFIOC_S_SETPOWERDOWN:
 		AF_PowerDown();
 		i4RetValue = 1;
