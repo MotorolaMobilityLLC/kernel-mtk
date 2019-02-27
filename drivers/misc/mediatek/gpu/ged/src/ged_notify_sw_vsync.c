63c7f3456b60dfd7d50298089630f96e5af165a7
Vincent Sung <vincent.sung@mediatek.com>
2019-02-28 04:03:13 +0800

    [ALPS03869337] GPU: Force FWDump when packing db
    [Solution]
    Port form kernel-4.4
    
    MTK-Commit-Id: 783b1c09883878c4425e101be8ac737e242dfda3
    
    Change-Id: I56d7e0f33e814094dd01ab90154a511d74bf0a4d
    CR-Id: ALPS03869337
    Feature: OpenGL|ES
    Signed-off-by: Vincent Sung <vincent.sung@mediatek.com>

--- drivers/misc/mediatek/gpu/ged/src/ged_notify_sw_vsync.c
+++ drivers/misc/mediatek/gpu/ged/src/ged_notify_sw_vsync.c
@@ -63,7 +63,8 @@ extern void (*mtk_gpu_sodi_exit_fp)(void);
 
 static struct workqueue_struct* g_psNotifyWorkQueue = NULL;
 #if defined(CONFIG_MACH_MT8167) || defined(CONFIG_MACH_MT8173)\
-|| defined(CONFIG_MACH_MT6739) || defined(CONFIG_MACH_MT6761)
+|| defined(CONFIG_MACH_MT6739) || defined(CONFIG_MACH_MT6761)\
+|| defined(CONFIG_MACH_MT6765)
 static struct workqueue_struct *g_psDumpFW;
 #endif
 
@@ -79,7 +80,8 @@ typedef struct GED_NOTIFY_SW_SYNC_TAG
 } GED_NOTIFY_SW_SYNC;
 
 #if defined(CONFIG_MACH_MT8167) || defined(CONFIG_MACH_MT8173)\
-|| defined(CONFIG_MACH_MT6739) || defined(CONFIG_MACH_MT6761)
+|| defined(CONFIG_MACH_MT6739) || defined(CONFIG_MACH_MT6761)\
+|| defined(CONFIG_MACH_MT6765)
 struct GED_DUMP_FW {
 	struct work_struct	sWork;
 };
@@ -573,7 +575,8 @@ void ged_sodi_stop(void)
 
 
 #if defined(CONFIG_MACH_MT8167) || defined(CONFIG_MACH_MT8173)\
-|| defined(CONFIG_MACH_MT6739) || defined(CONFIG_MACH_MT6761)
+|| defined(CONFIG_MACH_MT6739) || defined(CONFIG_MACH_MT6761)\
+|| defined(CONFIG_MACH_MT6765)
 static void ged_dump_fw_handle(struct work_struct *psWork)
 {
 	struct GED_DUMP_FW *psNotify
@@ -611,7 +614,8 @@ GED_ERROR ged_notify_sw_vsync_system_init(void)
 	}
 
 #if defined(CONFIG_MACH_MT8167) || defined(CONFIG_MACH_MT8173)\
-|| defined(CONFIG_MACH_MT6739) || defined(CONFIG_MACH_MT6761)
+|| defined(CONFIG_MACH_MT6739) || defined(CONFIG_MACH_MT6761)\
+|| defined(CONFIG_MACH_MT6765)
 	g_psDumpFW = NULL;
 	g_psDumpFW = create_workqueue("ged_dump_fw_log");
 
@@ -643,7 +647,8 @@ void ged_notify_sw_vsync_system_exit(void)
 	}
 
 #if defined(CONFIG_MACH_MT8167) || defined(CONFIG_MACH_MT8173)\
-|| defined(CONFIG_MACH_MT6739) || defined(CONFIG_MACH_MT6761)
+|| defined(CONFIG_MACH_MT6739) || defined(CONFIG_MACH_MT6761)\
+|| defined(CONFIG_MACH_MT6765)
 	if (g_psDumpFW != NULL) {
 		flush_workqueue(g_psDumpFW);
 
