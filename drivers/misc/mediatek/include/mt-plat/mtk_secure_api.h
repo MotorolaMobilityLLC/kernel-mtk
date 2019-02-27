1a6b9b2af7ea792f274adf1a87f0646f8c45920c
Fred Cheng <fred.cheng@mediatek.com>
2019-02-27 22:34:48 +0800

    [ALPS03509508] SCP DVFS: add SMC ID
    [Detail]
    add SMC ID of SCP DVFS
    
    MTK-Commit-Id: 7e4ae066a16e35c77fe9cd02242e7f50b45aa61f
    
    Change-Id: I1a8e2bf8e18000dbd91d7565aaeb7b221459860d
    Signed-off-by: Fred Cheng <fred.cheng@mediatek.com>
    CR-Id: ALPS03509508
    Feature: TinySys

--- drivers/misc/mediatek/include/mt-plat/mtk_secure_api.h
+++ drivers/misc/mediatek/include/mt-plat/mtk_secure_api.h
@@ -105,6 +105,10 @@
 #define MTK_SIP_KERNEL_EMIMPU_CLEAR \
 	(0x82000261 | MTK_SIP_SMC_AARCH_BIT)
 
+/* SCP DVFS related SMC call */
+#define MTK_SIP_KERNEL_SCP_DVFS_CTRL \
+	(0x82000290 | MTK_SIP_SMC_AARCH_BIT)
+
 extern size_t mt_secure_call_all(size_t function_id,
 	size_t arg0, size_t arg1, size_t arg2,
 	size_t arg3, size_t *r1, size_t *r2, size_t *r3);
