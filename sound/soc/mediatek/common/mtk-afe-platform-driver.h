055e47850b545c789b1b7b2b4e6ffc7ba294cd12
KaiChieh Chuang <kaichieh.chuang@mediatek.com>
2019-02-28 02:34:03 +0800

    [ALPS03619966] ASoC: add mt3967 driver
    add mt3967 driver
    
    MTK-Commit-Id: 1b220f23f30d8c5a21966606b9851f8fcd6727e7
    
    Change-Id: I628cc68ec6ac57e2a769f2282fc30af543851ce8
    Signed-off-by: KaiChieh Chuang <kaichieh.chuang@mediatek.com>
    CR-Id: ALPS03619966
    Feature: Phone Sound

--- sound/soc/mediatek/common/mtk-afe-platform-driver.h
+++ sound/soc/mediatek/common/mtk-afe-platform-driver.h
@@ -19,5 +19,12 @@
 
 extern const struct snd_soc_platform_driver mtk_afe_pcm_platform;
 
+struct mtk_base_afe;
+struct snd_soc_platform;
+
+int mtk_afe_combine_sub_dai(struct mtk_base_afe *afe);
+int mtk_afe_add_sub_dai_control(struct snd_soc_platform *platform);
+
+unsigned int word_size_align(unsigned int in_size);
 #endif
 
