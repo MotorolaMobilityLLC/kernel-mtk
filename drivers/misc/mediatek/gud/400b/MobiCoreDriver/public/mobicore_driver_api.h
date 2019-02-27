9b9b43621535d8f100dbddb3296e6375f3acf7bc
Vink Shen <vink.shen@mediatek.com>
2019-02-28 04:03:52 +0800

    [ALPS03829322] Trustonic TEE: add gud driver ready API
    Add gud driver ready API to let other modules know.
    
    MTK-Commit-Id: 8b1f6b81a77704f76b4f55d07a62f378c6ea7bdf
    
    Change-Id: I16770b48fd7e7426eee73f90f9e290057f9c465c
    Signed-off-by: Vink Shen <vink.shen@mediatek.com>
    CR-Id: ALPS03829322
    Feature: Trustonic TEE (Trusted Execution Environment)

--- drivers/misc/mediatek/gud/400b/MobiCoreDriver/public/mobicore_driver_api.h
+++ drivers/misc/mediatek/gud/400b/MobiCoreDriver/public/mobicore_driver_api.h
@@ -463,4 +463,6 @@ __MC_CLIENT_LIB_API enum mc_result mc_get_session_error_code(
 	struct mc_session_handle	*session,
 	s32				*exit_code);
 
+__MC_CLIENT_LIB_API bool is_mobicore_ready(void);
+
 #endif /* _MOBICORE_DRIVER_API_H_ */
