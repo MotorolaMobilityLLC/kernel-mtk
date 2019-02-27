7051779cf24ce0ee7e43c3e927416735a04fc5f3
Eddie Hung <eddie.hung@mediatek.com>
2019-02-28 02:34:54 +0800

    [ALPS03880102] USB: merge f_fs patchs
    [Detail]
    Merge below known issue patchs for f_fs
    
    USB: f_fs: Allow epfile to be opened only once
    USB: f_fs: Add memory barrier before atomic operations
    USB: f_fs: Fix disconnect check during ongoing IO
    USB: f_fs: Check error status before doing epfile I/O
    USB: f_fs: Fail stale read IOs after disconnect
    USB: gadget: f_fs: Return error if TX req is queued during device offline
    USB: f_fs: Set ffs->func to NULL after disabling endpoint in set_alt()
    USB: f_fs: Fix epfile crash during composition switch
    
    These patchs are from https://source.codeaurora.org/.
    
    MTK-Commit-Id: 6e32e95d65d11e5a4f0808bb457a489ba62b390d
    
    Change-Id: If0b37406fe98fb005bb3f7f9dfe1c7dee75e150a
    Signed-off-by: Eddie Hung <eddie.hung@mediatek.com>
    CR-Id: ALPS03880102
    Feature: [Android Default] MTP/PTP (Media File Transfer / Picture File Transfer)
    (cherry picked from commit 49acd6f63c292cc1e1bf361d6d0ea966a864ecb5)

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -956,6 +956,11 @@ static ssize_t ffs_epfile_io(struct file *file, struct ffs_io_data *io_data)
 	int halt;
 	char *cnxn_data = NULL;
 
+	/* to get updated error atomic variable value */
+	smp_mb__before_atomic();
+	if (atomic_read(&epfile->error))
+		return -ENODEV;
+
 	/* Are we still active? */
 	if (WARN_ON(epfile->ffs->state != FFS_ACTIVE))
 		return -ENODEV;
@@ -966,9 +971,26 @@ static ssize_t ffs_epfile_io(struct file *file, struct ffs_io_data *io_data)
 		if (file->f_flags & O_NONBLOCK)
 			return -EAGAIN;
 
-		ret = wait_event_interruptible(epfile->wait, (ep = epfile->ep));
-		if (ret)
-			return -EINTR;
+		/* Don't wait on write if device is offline */
+		if (!io_data->read)
+			return -ENODEV;
+
+		/* to get updated error atomic variable value */
+		smp_mb__before_atomic();
+
+		/*
+		 * if ep is disabled, this fails all current IOs
+		 * and wait for next epfile open to happen
+		 */
+		if (!atomic_read(&epfile->error)) {
+			ret = wait_event_interruptible(epfile->wait,
+					(ep = epfile->ep));
+			if (ret < 0)
+				return -EINTR;
+		}
+
+		if (!ep)
+			return -ENODEV;
 	}
 
 	/* Do we halt? */
@@ -1102,8 +1124,10 @@ static ssize_t ffs_epfile_io(struct file *file, struct ffs_io_data *io_data)
 		if (!io_data->read)
 			write_out++;
 
-		if (unlikely(ret < 0))
+		if (unlikely(ret < 0)) {
+			ret = -EIO;
 			goto error_lock;
+		}
 
 		spin_unlock_irq(&epfile->ffs->eps_lock);
 
@@ -1114,13 +1138,37 @@ static ssize_t ffs_epfile_io(struct file *file, struct ffs_io_data *io_data)
 			 * status. usb_ep_dequeue API should guarantee no race
 			 * condition with req->complete callback.
 			 */
-			usb_ep_dequeue(ep->ep, req);
-			interrupted = ep->status < 0;
+			spin_lock_irq(&epfile->ffs->eps_lock);
+			interrupted = true;
+			/*
+			 * While we were acquiring lock endpoint got
+			 * disabled (disconnect) or changed
+			 (composition switch) ?
+			 */
+			if (epfile->ep == ep) {
+				usb_ep_dequeue(ep->ep, req);
+				interrupted = ep->status < 0;
+			}
+			spin_unlock_irq(&epfile->ffs->eps_lock);
 		}
 
-		if (interrupted)
+		if (interrupted) {
 			ret = -EINTR;
-		else if (io_data->read && ep->status > 0) {
+			goto error_mutex;
+		}
+
+		ret = -ENODEV;
+		spin_lock_irq(&epfile->ffs->eps_lock);
+		/*
+		 * While we were acquiring lock endpoint got
+		 * disabled (disconnect) or changed
+		 * (composition switch) ?
+		 */
+		if (epfile->ep == ep)
+			ret = ep->status;
+		spin_unlock_irq(&epfile->ffs->eps_lock);
+
+		if (io_data->read && ret > 0) {
 			cnxn_data = req->buf;
 			if (req->actual == 24) {
 				if (cnxn_data[0] == 0x43 && cnxn_data[1] == 0x4e
@@ -1141,8 +1189,7 @@ static ssize_t ffs_epfile_io(struct file *file, struct ffs_io_data *io_data)
 			}
 			ret = __ffs_epfile_read_data(epfile, data, ep->status,
 						     &io_data->data);
-		} else
-			ret = ep->status;
+		}
 		goto error_mutex;
 	} else if (!(req = usb_ep_alloc_request(ep->ep, GFP_ATOMIC))) {
 		ret = -ENOMEM;
@@ -1191,8 +1238,18 @@ ffs_epfile_open(struct inode *inode, struct file *file)
 	if (WARN_ON(epfile->ffs->state != FFS_ACTIVE))
 		return -ENODEV;
 
+	/* to get updated opened atomic variable value */
+	smp_mb__before_atomic();
+	if (atomic_read(&epfile->opened)) {
+		pr_info("%s(): ep(%s) is already opened.\n",
+					__func__, epfile->name);
+		return -EBUSY;
+	}
+
+	atomic_set(&epfile->opened, 1);
 	file->private_data = epfile;
 	ffs_data_opened(epfile->ffs);
+	atomic_set(&epfile->error, 0);
 
 	return 0;
 }
@@ -1308,8 +1365,11 @@ ffs_epfile_release(struct inode *inode, struct file *file)
 
 	ENTER();
 
+	atomic_set(&epfile->opened, 0);
 	__ffs_epfile_read_buffer_free(epfile);
+	atomic_set(&epfile->error, 1);
 	ffs_data_closed(epfile->ffs);
+	file->private_data = NULL;
 
 	return 0;
 }
@@ -1698,6 +1758,8 @@ static void ffs_data_get(struct ffs_data *ffs)
 {
 	ENTER();
 
+	/* to get updated ref atomic variable value */
+	smp_mb__before_atomic();
 	atomic_inc(&ffs->ref);
 }
 
@@ -1705,6 +1767,8 @@ static void ffs_data_opened(struct ffs_data *ffs)
 {
 	ENTER();
 
+	/* to get updated ref atomic variable value */
+	smp_mb__before_atomic();
 	atomic_inc(&ffs->ref);
 	if (atomic_add_return(1, &ffs->opened) == 1 &&
 			ffs->state == FFS_DEACTIVATED) {
@@ -1717,6 +1781,8 @@ static void ffs_data_put(struct ffs_data *ffs)
 {
 	ENTER();
 
+	/* to get updated ref atomic variable value */
+	smp_mb__before_atomic();
 	if (unlikely(atomic_dec_and_test(&ffs->ref))) {
 		pr_info("%s(): freeing\n", __func__);
 		ffs_data_clear(ffs);
@@ -1731,6 +1797,8 @@ static void ffs_data_closed(struct ffs_data *ffs)
 {
 	ENTER();
 
+	/* to get updated opened atomic variable value */
+	smp_mb__before_atomic();
 	if (atomic_dec_and_test(&ffs->opened)) {
 		if (ffs->no_disconnect) {
 			ffs->state = FFS_DEACTIVATED;
@@ -1746,6 +1814,9 @@ static void ffs_data_closed(struct ffs_data *ffs)
 			ffs_data_reset(ffs);
 		}
 	}
+
+	/* to get updated opened atomic variable value */
+	smp_mb__before_atomic();
 	if (atomic_read(&ffs->opened) < 0) {
 		ffs->state = FFS_CLOSING;
 		ffs_data_reset(ffs);
@@ -1937,6 +2008,10 @@ static void ffs_func_eps_disable(struct ffs_function *func)
 
 	spin_lock_irqsave(&func->ffs->eps_lock, flags);
 	do {
+
+		if (epfile)
+			atomic_set(&epfile->error, 1);
+
 		/* pending requests get nuked */
 		if (likely(ep->ep))
 			usb_ep_disable(ep->ep);
@@ -3296,8 +3371,10 @@ static int ffs_func_set_alt(struct usb_function *f,
 			return intf;
 	}
 
-	if (ffs->func)
+	if (ffs->func) {
 		ffs_func_eps_disable(ffs->func);
+		ffs->func = NULL;
+	}
 
 	if (ffs->state == FFS_DEACTIVATED) {
 		ffs->state = FFS_CLOSING;
@@ -3625,6 +3702,7 @@ static void ffs_func_unbind(struct usb_configuration *c,
 		if (ep->ep && ep->req)
 			usb_ep_free_request(ep->ep, ep->req);
 		ep->req = NULL;
+		ep->ep = NULL;
 		++ep;
 	} while (--count);
 	spin_unlock_irqrestore(&func->ffs->eps_lock, flags);
@@ -3860,6 +3938,9 @@ static void ffs_closed(struct ffs_data *ffs)
 	else
 		goto done;
 
+
+	/* to get updated refcount atomic variable value */
+	smp_mb__before_atomic();
 	if (opts->no_configfs || !opts->func_inst.group.cg_item.ci_parent
 	    || !atomic_read(&opts->func_inst.group.cg_item.ci_kref.refcount))
 		goto done;
