d5b7257007dd33f945232de24df0984b4e3eab3e
Chinwen Chang <chinwen.chang@mediatek.com>
2019-02-28 04:04:44 +0800

    [ALPS04091267] mlog: record the memory usage of camerahalserver
    In newer version of Android, camerahalserver is a memory hungry
    process instead of mediaserver. So let mlog start to record its
    memory usage for issue analysis.
    
    MTK-Commit-Id: a25cdaf38b16d7ff97b8a9a6146907e2a16164f0
    
    Change-Id: Ib188b077889fb4ef9767330912f2d62f9dba5b10
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>
    CR-Id: ALPS04091267
    Feature: Memory Optimization
    (cherry-pick from aaa96c5db9a761b94e4824359da3415c80c53362)

--- drivers/misc/mediatek/mlog/mlog_logger.c
+++ drivers/misc/mediatek/mlog/mlog_logger.c
@@ -766,10 +766,9 @@ static void mlog_procinfo(void)
 			goto unlock_continue;
 
 		/*
-		 * 1. mediaserver is a suspect in many ANR/FLM cases.
-		 * 2. procesname is "mediaserver" not "/system/bin/mediaserver"
+		 * camerahalserver is a suspect in many ANR/FLM cases.
 		 */
-		if (strncmp("mediaserver", p->comm, TASK_COMM_LEN) == 0)
+		if (strncmp("camerahalserver", p->comm, TASK_COMM_LEN) == 0)
 			goto collect_proc_mem_info;
 
 		/* skip root user */
