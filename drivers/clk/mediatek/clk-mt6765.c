2f2539d3193e48b416ef8ba18ab67ef466c6704a
Owen Chen <owen.chen@mediatek.com>
2019-02-28 02:32:13 +0800

    [ALPS03842280] clkmgr: disp pwm clk on
    [Detail]
    need to enable disp pwm in initial clk driver
    setting.
    
    [Solution]
    driver update.
    
    MTK-Commit-Id: 0dca64b74ede860eeda98b41c953d9df3ffd671f
    
    Change-Id: I5a86871178f55dec6933d4cad79112b7d4305dd0
    CR-Id: ALPS03842280
    Feature: [Module]Clock Management
    Signed-off-by: Owen Chen <owen.chen@mediatek.com>
    (cherry picked from commit 3cd611c23532c4c392e70cdfc0e1355fdbc675ef)

--- drivers/clk/mediatek/clk-mt6765.c
+++ drivers/clk/mediatek/clk-mt6765.c
@@ -430,7 +430,7 @@ void __iomem *venc_gcon_base;
 #define INFRA_CG1		0x00000004
 #define INFRA_CG2		0x983fff00
 #define INFRA_CG3		0x86947E16
-#define INFRA_CG4		0x2ffc06dd
+#define INFRA_CG4		0x2ffc06d9
 #define INFRA_CG5		0x00020fe7
 
 #define MM_DISABLE_CG		0x0c840040 /* un-gating in preloader */
@@ -439,7 +439,7 @@ void __iomem *venc_gcon_base;
 #define INFRA_CG1		0x00000004
 #define INFRA_CG2		0x98ffff7f
 #define INFRA_CG3		0x879c7f96
-#define INFRA_CG4		0x2ffc87d9
+#define INFRA_CG4		0x2ffc87dd
 #define INFRA_CG5		0x00038fff
 
 #define MM_DISABLE_CG		0x3fffffff /* un-gating in preloader */
