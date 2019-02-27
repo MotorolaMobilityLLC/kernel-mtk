a76f1ceaa2530161b0912325a271e2df47c81b52
jiachiam.liu <jiachiam.liu@mediatek.com>
2019-02-28 04:04:34 +0800

    [ALPS04030668] sspm: add mt6779 driver
    Add mt6779 platform code
    
    MTK-Commit-Id: 784e1955f5509fc3fa989d940b6a692026e26077
    
    Change-Id: If9825ca11c572e5b6fcbb760c04db89a9cc0b842
    Signed-off-by: jiachiam.liu <jiachiam.liu@mediatek.com>
    CR-Id: ALPS04030668
    Feature: TinySys
    (cherry-pick from 65a954da5bffac04680529b9721bd9a27a24322a)

---
+++ drivers/misc/mediatek/sspm/mt6779/sspm_ipi_timeout_cb.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2011-2015 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#include <linux/module.h>       /* needed by all modules */
+#include <mt-plat/sync_write.h>
+#include "sspm_define.h"
+
+
+/* platform callback when ipi timeout */
+void sspm_ipi_timeout_cb(int ipi_id)
+{
+
+}
+
