695dbd189376c064a7ea254ee8cf9110f75795d7
Dream Yeh <dream.yeh@mediatek.com>
2019-02-28 04:03:14 +0800

    [ALPS04016820] EEPROM : code flow improve
    [Detail] To improve EEPROM read flow
    
    [Solution]
    1. To provide Customer EEPROM driver function
    2. Check EEPROM header only on userspace
    3. To remove redundant code
    
    MTK-Commit-Id: 8142e91c1923c27752fbeea9069a1c94ab5fc855
    
    Change-Id: I7bd90088ee29ce7ba65787cda8eb378f152ee895
    Signed-off-by: Dream Yeh <dream.yeh@mediatek.com>
    CR-Id: ALPS04016820
    Feature: Cshot (Continuous Shot)
    (cherry picked from commit 0ff27a5482f1403caa4a71993eb35fc3967540a5)

--- drivers/misc/mediatek/cam_cal/src/cam_cal_list.h
+++ drivers/misc/mediatek/cam_cal/src/cam_cal_list.h
@@ -38,45 +38,20 @@
 typedef unsigned int (*cam_cal_cmd_func) (struct i2c_client *client,
 	unsigned int addr, unsigned char *data, unsigned int size);
 
-typedef unsigned int (*cam_cal_check_func) (struct i2c_client *client,
-					    cam_cal_cmd_func readCamCalData);
-
-enum CAM_CAL_CMD_TYPE {
-	CMD_NONE = 0,
-	CMD_AUTO,
-	CMD_MAIN,
-	CMD_MAIN2,
-	CMD_SUB,
-	CMD_SUB2,
-	CMD_BRCB032GWZ,
-	CMD_CAT24C16,
-	CMD_GT24C32A,
-	CMD_NUM
-};
-
 struct stCAM_CAL_LIST_STRUCT {
 	unsigned int sensorID;
 	unsigned int slaveID;
-	enum CAM_CAL_CMD_TYPE cmdType;
-	cam_cal_check_func checkFunc;
-};
-
-struct stCAM_CAL_FUNC_STRUCT {
-	enum CAM_CAL_CMD_TYPE cmdType;
 	cam_cal_cmd_func readCamCalData;
 };
 
 
 unsigned int cam_cal_get_sensor_list
 		(struct stCAM_CAL_LIST_STRUCT **ppCamcalList);
-unsigned int cam_cal_get_func_list
-		(struct stCAM_CAL_FUNC_STRUCT **ppCamcalFuncList);
-unsigned int cam_cal_check_mtk_cid
-		(struct i2c_client *client, cam_cal_cmd_func readCamCalData);
-unsigned int cam_cal_check_double_eeprom
-		(struct i2c_client *client, cam_cal_cmd_func readCamCalData);
 
-/*defined on kd_sensorlist.c*/
-void CAMERA_HW_Get_i2C_BusNum(unsigned int *I2C_Bus_Num);
+
+/* Common EEPROM read i2C function */
+unsigned int Common_read_region(struct i2c_client *client, unsigned int addr,
+				unsigned char *data, unsigned int size);
+
 
 #endif				/* __CAM_CAL_LIST_H */
