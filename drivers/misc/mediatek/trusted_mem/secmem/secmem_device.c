29b8691571383ead0dce6d44fbb286578cdd70f7
Coboy Chen <coboy.chen@mediatek.com>
2019-02-28 02:35:00 +0800

    [ALPS03975924] tmem: Fix Coverity Issues
    Fix below coverity defect IDs:
    4272354, 4272412, 4272464, 4275813, 4275815, 4275818,
    4272396, 4272459.
    
    MTK-Commit-Id: 168536ca83024be4cc0d1c2dfca8bde5aa1a215d
    
    Change-Id: I5b7d7aca15754de610f95c35fb7a7ee9657e4dcf
    Signed-off-by: Coboy Chen <coboy.chen@mediatek.com>
    CR-Id: ALPS03975924
    Feature: Secure Video Path (SVP)

--- drivers/misc/mediatek/trusted_mem/secmem/secmem_device.c
+++ drivers/misc/mediatek/trusted_mem/secmem/secmem_device.c
@@ -80,7 +80,8 @@ static int __init secmem_init(void)
 	get_tee_peer_ops(&t_device->peer_ops);
 	get_tee_peer_priv_data(TEE_MEM_SVP, &t_device->peer_priv);
 
-	sprintf(t_device->name, "%s", SECMEM_SVP_DEVICE_NAME);
+	snprintf(t_device->name, MAX_DEVICE_NAME_LEN, "%s",
+		 SECMEM_SVP_DEVICE_NAME);
 #if defined(CONFIG_MTK_SSMR) || (defined(CONFIG_CMA) && defined(CONFIG_MTK_SVP))
 	t_device->ssmr_feature_id = SSMR_FEAT_SVP;
 #endif
