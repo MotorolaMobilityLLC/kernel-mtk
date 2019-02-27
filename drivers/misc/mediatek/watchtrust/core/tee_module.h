7c65d8a1aaf78e01322bfef189278f0b60e3e36d
Calvin Liao <calvin.liao@mediatek.com>
2019-02-28 02:31:56 +0800

    [ALPS03729884] TEE: Modify Watchdata driver
    [Detail]
    Modify Watchdata driver from Watchdata TEE.
    
    MTK-Commit-Id: 95e2131c1d9644e41e4409bf6330ad94d385c9b8
    
    Change-Id: I6587f688ff50c509cd0013a1638e2013592791ea
    CR-Id: ALPS03729884
    Feature: Watchdata TEE
    Signed-off-by: Calvin Liao <calvin.liao@mediatek.com>

---
+++ drivers/misc/mediatek/watchtrust/core/tee_module.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2016-2018, Watchdata Limited
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License Version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __TEE_CORE_H__
+#define __TEE_CORE_H__
+
+int tee_core_init(void);
+void tee_core_exit(void);
+
+#endif
