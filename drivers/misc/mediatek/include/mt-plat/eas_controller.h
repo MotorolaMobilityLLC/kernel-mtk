61ca97cf1154eaf0249caea3d1dc2ab02bc13b74
mtk15471 <paula.hong@mediatek.com>
2019-02-28 01:28:41 +0800

    [ALPS03765624] Fix codescan: FUNCTION_ARGUMENTS
    [Detail]
    function definition argument int should also have an identifier name
    
    [Solutions]
    Give argument to  boost_write_for_perf_idx funvtion
    
    MTK-Commit-Id: 9cc83057fb7bc2fe096f74ce7029b398c80952ab
    
    Change-Id: I3a6ce78f39407968687ef7d6ccb650ace5e76d65
    Signed-off-by: mtk15471 <paula.hong@mediatek.com>
    CR-Id: ALPS03765624
    Feature: System Performance

--- drivers/misc/mediatek/include/mt-plat/eas_controller.h
+++ drivers/misc/mediatek/include/mt-plat/eas_controller.h
@@ -10,20 +10,24 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  */
+enum {
+	CGROUP_ROOT = 0,
+	CGROUP_FG,
+	CGROUP_BG,
+	CGROUP_TA,
+	CGROUP_RT,
+	NR_CGROUP
+};
 
-#define CGROUP_ROOT 0
-#define CGROUP_FG 1
-#define CGROUP_BG 2
-#define CGROUP_TA 3
-#define CGROUP_RT 4
-#define NR_CGROUP 5
+enum {
+	EAS_KIR_PERF = 0,
+	EAS_KIR_FBC,
+	EAS_KIR_BOOT,
+	EAS_KIR_TOUCH,
+	EAS_MAX_KIR
+};
 
-#define EAS_KIR_PERF  0
-#define EAS_KIR_FBC   1
-#define EAS_KIR_BOOT  2
-#define EAS_KIR_TOUCH 3
-#define EAS_MAX_KIR   4
+extern int boost_write_for_perf_idx(int group_idx, int boost_value);
 
-extern int boost_write_for_perf_idx(int, int);
 int update_eas_boost_value(int kicker, int cgroup_idx, int value);
 void init_perfmgr_eas_controller(void);
