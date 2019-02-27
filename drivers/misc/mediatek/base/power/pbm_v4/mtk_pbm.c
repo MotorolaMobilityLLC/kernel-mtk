e7d67679a6c3a2f75e03de4186ac9d352bb57516
Samuel Hsieh <samuel.hsieh@mediatek.com>
2019-02-28 02:32:25 +0800

    [ALPS03922123] DLPT: set task running before mutex_lock
    [Detail]
    To avoid warning: do not call blocking ops when !TASK_RUNNING;
    set task running before mutex_lock() on pbm thread
    
    [Solution]
    set task running before mutex_lock() on pbm thread
    
    MTK-Commit-Id: 850c2ff60e960c14b5bc6d6965092e9759113803
    
    Change-Id: I54632b45da638eb9c295b5650aa2be812667f47e
    Signed-off-by: Samuel Hsieh <samuel.hsieh@mediatek.com>
    CR-Id: ALPS03922123
    Feature: Dynamic load power throttling
    (cherry picked from commit 0b58f348d47efa9aa519883398debadf650b275c)

--- drivers/misc/mediatek/base/power/pbm_v4/mtk_pbm.c
+++ drivers/misc/mediatek/base/power/pbm_v4/mtk_pbm.c
@@ -519,6 +519,8 @@ static int pbm_thread_handle(void *data)
 			continue;
 		}
 
+		set_current_state(TASK_RUNNING);
+
 		mutex_lock(&pbm_mutex);
 		if (g_dlpt_need_do == 1) {
 			if (g_dlpt_stop == 0) {
@@ -540,8 +542,6 @@ static int pbm_thread_handle(void *data)
 		atomic_dec(&kthread_nreq);
 		mutex_unlock(&pbm_mutex);
 	}
-
-	__set_current_state(TASK_RUNNING);
 
 	return 0;
 }
