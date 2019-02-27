1f41b48f2f9aa46babf7c9c75e38c571824426e7
Perry Hsu <perry.hsu@mediatek.com>
2019-02-28 04:05:22 +0800

    [ALPS04101184] mdla: disable debug log by default.
    MDLA debug log is disabled by default, and can be enabled by
    echo 1 > /d/mdla/klog.
    
    MTK-Commit-Id: 9310341d5a89ad195f52d9a585e1dae176f02c88
    
    Change-Id: Ia0633dcb789e43bcf605f5c1a183cdb04917f20a
    Signed-off-by: Perry Hsu <perry.hsu@mediatek.com>
    CR-Id: ALPS04101184
    Feature: MDLA Platform
    (cherry picked from commit 9468e28d96c99997f89e90bc855451f3aa53a801)
    (cherry-pick from 0809dd9fc1b5fd0c051968a2c96086cdbab711f7)

--- drivers/misc/mediatek/mdla/1.0/mdla_ion.c
+++ drivers/misc/mediatek/mdla/1.0/mdla_ion.c
@@ -11,6 +11,8 @@
  * GNU General Public License for more details.
  */
 
+#include "mdla_debug.h"
+
 #include <linux/types.h>
 #include <ion.h>
 #include <mtk/ion_drv.h>
@@ -21,7 +23,6 @@
 
 #include "mdla_ioctl.h"
 #include "mdla_ion.h"
-#include "mdla_debug.h"
 
 static struct ion_client *ion_client;
 
@@ -29,14 +30,12 @@ void mdla_ion_init(void)
 {
 	ion_client = ion_client_create(g_ion_device, "mdla");
 
-	// TODO: remove debug
 	mdla_debug("%s: ion_client_create(): %p\n", __func__, ion_client);
 }
 
 void mdla_ion_exit(void)
 {
 	if (ion_client) {
-		// TODO: remove debug
 		mdla_debug("%s: %p\n", __func__, ion_client);
 		ion_client_destroy(ion_client);
 		ion_client = NULL;
@@ -58,19 +57,16 @@ int mdla_ion_kmap(unsigned long arg)
 	if (copy_from_user(&ion_data, (void * __user) arg, sizeof(ion_data)))
 		return -EFAULT;
 
-	// TODO: remove debug
 	mdla_debug("%s: share fd: %d\n", __func__, ion_data.fd);
 
 	hndl = ion_import_dma_buf_fd(ion_client, ion_data.fd);
 
 	if (IS_ERR_OR_NULL(hndl)) {
-		// TODO: remove debug
 		mdla_debug("%s: ion_import_dma_buf_fd(): failed: %d\n",
 			__func__, PTR_ERR(hndl));
 		return -EINVAL;
 	}
 
-	// TODO: remove debug
 	mdla_debug("%s: ion_import_dma_buf_fd(): %p\n", __func__, hndl);
 
 	/*	set memory port */
@@ -80,7 +76,6 @@ int mdla_ion_kmap(unsigned long arg)
 	ret = ion_kernel_ioctl(ion_client, ION_CMD_MULTIMEDIA,
 		(unsigned long) &mm_data);
 	if (ret < 0) {
-		// TODO: remove debug
 		mdla_debug("%s: ion_kernel_ioctl(%p, %p): %d\n",
 			__func__, ion_client, hndl, ret);
 		return -EINVAL;
@@ -90,14 +85,13 @@ int mdla_ion_kmap(unsigned long arg)
 	kva = ion_map_kernel(ion_client, hndl);
 	if (IS_ERR_OR_NULL(kva)) {
 		mdla_debug("%s: ion_map_kernel(%p, %p): %d\n",
-			__func__, ion_client, hndl, ret); // TODO: remove debug
+			__func__, ion_client, hndl, ret);
 		return -EINVAL;
 	}
 
 	/*  Get the phyiscal address (mva) to the buffer */
 	ret = ion_phys(ion_client, hndl, &mva, &ion_data.len);
 	if (ret < 0) {
-		// TODO: remove debug
 		mdla_debug("%s: ion_phys(%p, %p): %d\n",
 			__func__, ion_client, hndl, ret);
 
@@ -111,7 +105,6 @@ int mdla_ion_kmap(unsigned long arg)
 	if (copy_to_user((void * __user) arg, &ion_data, sizeof(ion_data)))
 		return -EFAULT;
 
-	// TODO: remove debug
 	mdla_debug("%s: mva=%llxh, kva=%llxh, kernel_handle=%llxh\n",
 		__func__, ion_data.mva, ion_data.kva, ion_data.khandle);
 
@@ -130,7 +123,6 @@ int mdla_ion_kunmap(unsigned long arg)
 	if (copy_from_user(&ion_data, (void * __user) arg, sizeof(ion_data)))
 		return -EFAULT;
 
-	// TODO: remove debug
 	mdla_debug("%s: mva=%llxh, kva=%llxh, kernel_handle=%llxh\n",
 		__func__, ion_data.mva, ion_data.kva, ion_data.khandle);
 
@@ -155,12 +147,10 @@ void mdla_ion_sync(u64 hndl)
 	ret = ion_kernel_ioctl(ion_client, ION_CMD_SYSTEM,
 		(unsigned long)&sys_data);
 
-	// TODO: remove debug
 	mdla_debug("%s: ion_kernel_ioctl kernel_handle=%llx\n",
 		__func__, (unsigned long long)hndl);
 
 	if (ret) {
-		// TODO: remove debug
 		mdla_debug("%s: ion_kernel_ioctl(hndl=%llx): %d failed\n",
 			__func__, (unsigned long long)hndl, ret);
 	}
