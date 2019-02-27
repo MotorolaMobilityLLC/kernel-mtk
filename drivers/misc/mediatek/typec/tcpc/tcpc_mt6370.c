c794c5c5810298624b2bc00172fd0fe048630d47
Jeremy Chou <jeremy.chou@mediatek.com>
2019-02-28 02:35:26 +0800

    [ALPS03931752] TCPC: fix HRESET fail when system suspend.
    [Detail]
        HRESET could not complete due to i2c being suspended.
    
    [Solution]
        If HRESET is not completed yet, prevent system from suspending.
    
    MTK-Commit-Id: db86d51711129486128388adf70f21de725c1851
    
    Change-Id: I1899e6f1661c298776d02544fd70185acc6cdf35
    Signed-off-by: Jeremy Chou <jeremy.chou@mediatek.com>
    CR-Id: ALPS03931752
    Feature: pump express charging. (fast charging)
    (cherry picked from commit ae6bc3758d357a548a22d5fabff47139e2e484ac)

--- drivers/misc/mediatek/typec/tcpc/tcpc_mt6370.c
+++ drivers/misc/mediatek/typec/tcpc/tcpc_mt6370.c
@@ -1627,8 +1627,16 @@ static int mt6370_i2c_suspend(struct device *dev)
 
 	if (client) {
 		chip = i2c_get_clientdata(client);
-		if (chip)
+		if (chip) {
+			if (chip->tcpc->pd_wait_hard_reset_complete) {
+				pr_info("%s PD WAITING HRESET COMPLETE(%d) - NO SUSPEND\n",
+					__func__, chip->tcpc->pd_wait_hard_reset_complete);
+				return -EAGAIN;
+			} else
+				pr_info("%s PD WAITING HRESET COMPLETE(%d) - SUSPEND\n",
+					__func__, chip->tcpc->pd_wait_hard_reset_complete);
 			down(&chip->suspend_lock);
+		}
 	}
 
 	return 0;
