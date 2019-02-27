d898def69329efc4ede20bffc46cfc1e74d880d4
Ma Shi <shi.ma@mediatek.com>
2019-02-28 02:31:08 +0800

    [ALPS03682387] msdc: workarounds for weak vendors
    [Detail]
    1. disable cmdq when OTA;
    2. send PON when alarm awake in charge mode;
    3. fix bug of cmd6(flush cache) in reinit emmc flow;
    4. fix bug of calculating wrong time to not ready task after
    reinit emmc;
    
    MTK-Commit-Id: 5f91021392d2c429c850d244ea67cb7c76ec630f
    
    Change-Id: I7aacba2e5f14b45c0e0bb5cbb61ce6c776c6b20d
    Signed-off-by: Ma Shi <shi.ma@mediatek.com>
    CR-Id: ALPS03682387
    Feature: eMMC boot up
    (cherry picked from commit 3f296a85f107e5a77a3de85d889ad3b913e39aa4)

--- drivers/misc/mediatek/rtc/include/mtk_rtc_hal.h
+++ drivers/misc/mediatek/rtc/include/mtk_rtc_hal.h
@@ -34,4 +34,5 @@ extern void hal_rtc_clear_alarm(struct rtc_time *tm);
 extern void hal_rtc_set_lp_irq(void);
 extern void hal_rtc_save_pwron_time(bool enable,
 	struct rtc_time *tm, bool logo);
+extern int mmc_charge_shutdown(void);
 #endif
