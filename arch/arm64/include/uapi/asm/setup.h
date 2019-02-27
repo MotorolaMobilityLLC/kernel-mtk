9b75ccbff060e139c56b9be0ec2ebf59bfe284dd
Yun Hsiang <yun.hsiang@mediatek.com>
2019-02-27 23:35:38 +0800

    [ALPS03625188] memcfg: add memcfg module
    [Detail]
    Add the following module.
    
    Entries in /proc/mtk_memcfg/
    memory_layout
    reserve_memot
    total_reserve
    oom_trigger
    frag_trigger
    
    API
    mtk_meminfo
    
    MTK-Commit-Id: 434db2b7d966993f5ea122b201fe6bbc5325624e
    
    Change-Id: Ie44a9d68f2b6470a5d7c2c9cd9c4f11a7ccd0cd0
    Signed-off-by: Yun Hsiang <yun.hsiang@mediatek.com>
    CR-Id: ALPS03625188
    Feature: Others

--- arch/arm64/include/uapi/asm/setup.h
+++ arch/arm64/include/uapi/asm/setup.h
@@ -23,4 +23,15 @@
 
 #define COMMAND_LINE_SIZE	2048
 
+/* general memory descriptor */
+struct mem_desc {
+	u64 start;
+	u64 size;
+};
+
+struct dram_info {
+	u32 rank_num;
+	struct mem_desc rank_info[4];
+};
+
 #endif
