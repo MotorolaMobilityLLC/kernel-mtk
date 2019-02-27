6408a8b53520190105debc4750176bd53e0a23b8
Irene Luo <irene.luo@mediatek.com>
2019-02-28 01:25:35 +0800

    [ALPS03757562] MCDI: do Not send ipi to iso CPUs on PM_QoS update
    [Detail]
    When other driver(s) updates PM_QoS `CPU_DMA_LATENCY` target,
    cpuidle driver will wakeup all online CPUs, to let those CPUs
    update new `CPU_DMA_LATENCY` target.
    
    To prevent wakeup isolated CPUs, we Only wakeup all
    "online and non-isolated" CPUs.
    
    MTK-Commit-Id: 3e66b9628beaece113277912f79027fdc49ca991
    
    Change-Id: I6120b5ef93cf8e7ae638ae69de6c87c06114625e
    Signed-off-by: Irene Luo <irene.luo@mediatek.com>
    CR-Id: ALPS03757562
    Feature: MCU Deep Idle

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -6,6 +6,15 @@
  *               Adam Belay <abelay@novell.com>
  *
  * This code is licenced under the GPL.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
 
 #include <linux/clockchips.h>
@@ -34,6 +43,29 @@ LIST_HEAD(cpuidle_detected_devices);
 static int enabled_devices;
 static int off __read_mostly;
 static int initialized __read_mostly;
+
+/**
+ * wake_up_avail_idle_cpus - break non-iso cpus out of idle
+ * wake_up_avail_idle_cpus try to break non-iso cpus which is in idle state
+ * even including idle polling cpus, for non-idle or iso cpus,
+ * we will do nothing for them.
+ */
+void wake_up_avail_idle_cpus(void)
+{
+	int cpu;
+
+	preempt_disable();
+	for_each_online_cpu(cpu) {
+		if (cpu == smp_processor_id())
+			continue;
+
+		if (cpu_isolated(cpu))
+			continue;
+
+		wake_up_if_idle(cpu);
+	}
+	preempt_enable();
+}
 
 int cpuidle_disabled(void)
 {
@@ -623,7 +655,14 @@ EXPORT_SYMBOL_GPL(cpuidle_register);
 static int cpuidle_latency_notify(struct notifier_block *b,
 		unsigned long l, void *v)
 {
-	wake_up_all_idle_cpus();
+	/*
+	 * MTK patch:
+	 * only wakeup idle CPUs which is `online & !isolated`,
+	 * when PM_QoS CPU_DMA_LATENCY is updated
+	 */
+/*	wake_up_all_idle_cpus(); */
+	wake_up_avail_idle_cpus();
+
 	return NOTIFY_OK;
 }
 
