fcb6f1fb24d8028e3db4324dac8b42201fc05088
albert-zl.huang <albert-zl.huang@mediatek.com>
2019-02-28 00:05:29 +0800

    [ALPS03621329] spm: add "md_wdt_irq_b" to spm edge trigger list
    [Detail]
    add "md_wdt_irq_b" to spm edge trigger list at MT6765
    
    MTK-Commit-Id: 287c7972a530412ccdfdd4c66a1c3c71699ef9c3
    
    Change-Id: Iba1ef4b0fbf7f2d58955009a711af6a2c47f263a
    Signed-off-by: albert-zl.huang <albert-zl.huang@mediatek.com>
    CR-Id: ALPS03621329
    Feature: Power Management

--- drivers/misc/mediatek/base/power/spm/mt6765/mtk_spm_irq_edge.h
+++ drivers/misc/mediatek/base/power/spm/mt6765/mtk_spm_irq_edge.h
@@ -28,7 +28,7 @@ struct edge_trigger_irq_list {
 static struct edge_trigger_irq_list list[] = {
 	{ "mediatek,infracfg_ao",   0,	0 },
 	{ "mediatek,kp",            0,  WAKE_SRC_R12_KP_IRQ_B },
-	//FIXME { "mediatek,mdcldma",       3,  WAKE_SRC_R12_CLDMA_EVENT_B },
+	{ "mediatek,mdcldma",       3,  WAKE_SRC_R12_CLDMA_EVENT_B },
 	{ "mediatek,auxadc",        0,  WAKE_SRC_R12_LOWBATTERY_IRQ_B },
 };
 
