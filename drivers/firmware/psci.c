8c473c0dd9be49b7836dfc3cd919b6a60e526fd8
ZH Chen <zh.chen@mediatek.com>
2019-02-28 02:30:24 +0800

    [ALPS03801065] psci: skip reset handler registration
    [Detail]
    Skip PSCI reset handler registration in MTK platforms
    since there might be racing between - psci_sys_reset
    (registered as arm_pm_restart) and - mtk_arch_reset_handle
    (registered as member of retart_hanlder_list)
    
    Skip registration to ensure MTK proprietary reset
    handler will be used.
    
    Long-term plan may be using standard PSCI interface
    for reboot in MTK platforms.
    
    MTK-Commit-Id: a6b7f90ac3f89d6fda72fc28a987cd8220bb1f69
    
    Change-Id: I95ed4375614dcc1a738322312b83fbf31b9e3b4b
    Signed-off-by: ZH Chen <zh.chen@mediatek.com>
    CR-Id: ALPS03801065
    Feature: Headset Cable/Button Detection and Reaction

--- drivers/firmware/psci.c
+++ drivers/firmware/psci.c
@@ -237,15 +237,19 @@ static int get_set_conduit_method(struct device_node *np)
 	return 0;
 }
 
+#if (!defined(CONFIG_MTK_WATCHDOG) && !defined(CONFIG_MACH_MT8167))
 static void psci_sys_reset(enum reboot_mode reboot_mode, const char *cmd)
 {
 	invoke_psci_fn(PSCI_0_2_FN_SYSTEM_RESET, 0, 0, 0);
 }
+#endif
 
+#if (!defined(CONFIG_MACH_MT6757) && !defined(CONFIG_MACH_MT8167))
 static void psci_sys_poweroff(void)
 {
 	invoke_psci_fn(PSCI_0_2_FN_SYSTEM_OFF, 0, 0, 0);
 }
+#endif
 
 static int __init psci_features(u32 psci_func_id)
 {
@@ -518,9 +522,13 @@ static void __init psci_0_2_set_functions(void)
 
 	psci_ops.migrate_info_type = psci_migrate_info_type;
 
+#if (!defined(CONFIG_MTK_WATCHDOG) && !defined(CONFIG_MACH_MT8167))
 	arm_pm_restart = psci_sys_reset;
+#endif
 
+#if (!defined(CONFIG_MACH_MT6757) && !defined(CONFIG_MACH_MT8167))
 	pm_power_off = psci_sys_poweroff;
+#endif
 }
 
 /*
