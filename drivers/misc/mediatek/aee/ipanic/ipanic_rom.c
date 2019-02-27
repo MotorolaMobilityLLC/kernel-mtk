7a7c93e62f5ae9114e5ec0aafb04aa10348132d2
Ji Zhang <ji.zhang@mediatek.com>
2019-02-28 02:31:44 +0800

    [ALPS03881807] aee: add more kernel offset dump
    [Detail]
    Add kernel offset dump in:
    1. HWT
    2. panic
    3. nested panic
    
    MTK-Commit-Id: 9e9e15d1ae87cdc1e110a43dd3b93265b01bb952
    
    Change-Id: I316edc44835d64db87bb800a786e10d25b784a14
    Signed-off-by: Ji Zhang <ji.zhang@mediatek.com>
    CR-Id: ALPS03881807
    Feature: Others

--- drivers/misc/mediatek/aee/ipanic/ipanic_rom.c
+++ drivers/misc/mediatek/aee/ipanic/ipanic_rom.c
@@ -79,6 +79,7 @@ int ipanic(struct notifier_block *this, unsigned long event, void *ptr)
 	struct kmsg_dumper dumper;
 	struct pt_regs saved_regs;
 
+	show_kaslr();
 	memset(&dumper, 0x0, sizeof(struct kmsg_dumper));
 #ifdef CONFIG_MTK_RAM_CONSOLE
 	aee_rr_rec_fiq_step(AEE_FIQ_STEP_KE_IPANIC_START);
@@ -101,6 +102,7 @@ void ipanic_recursive_ke(struct pt_regs *regs, struct pt_regs *excp_regs,
 {
 	struct pt_regs saved_regs;
 
+	show_kaslr();
 #ifdef CONFIG_MTK_RAM_CONSOLE
 	aee_rr_rec_exp_type(3);
 #endif
@@ -146,32 +148,13 @@ void ipanic_zap_console_sem(void)
 	console_unlock();
 }
 
-#ifdef CONFIG_RANDOMIZE_BASE
-static u64 show_kaslr(void)
-{
-	u64 const kaslr_offset = kimage_vaddr - KIMAGE_VADDR;
-
-	pr_notice("Kernel Offset: 0x%llx from 0x%lx\n", kaslr_offset,
-			KIMAGE_VADDR);
-	return kaslr_offset;
-}
-#else
-static u64 show_kaslr(void)
-{
-	pr_notice("Kernel Offset: disabled\n");
-	return 0;
-}
-#endif
-
 static int ipanic_die(struct notifier_block *self, unsigned long cmd, void *ptr)
 {
 	struct die_args *dargs = (struct die_args *)ptr;
-	u64 kaslr_offset;
 
-	kaslr_offset = show_kaslr();
+	show_kaslr();
 	print_modules();
 #ifdef CONFIG_MTK_RAM_CONSOLE
-	aee_rr_rec_kaslr_offset(kaslr_offset);
 	aee_rr_rec_exp_type(2);
 	aee_rr_rec_fiq_step(AEE_FIQ_STEP_KE_IPANIC_DIE);
 #endif
