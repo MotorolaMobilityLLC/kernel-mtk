77e3a00b43c3188118a7dd8e9b5e7125d160c7b5
YT Chang <yt.chang@mediatek.com>
2019-02-27 23:39:05 +0800

    [ALPS03272733] sched: dont use SCHED_WARN_ON to prevent deadlock
    [Detail]
    SCHED_WARN_ON will use printk and in scheduler
    use printk will cause deadlock
    dont use SCHED_WARN_ON to prevent deadlock
    
    MTK-Commit-Id: 8e81b24f7ce8e194c261bd83e9e881f7a937a5bd
    
    Change-Id: I10463af47bbe99ca94669e1c77941628375d1e9d
    Signed-off-by: YT Chang <yt.chang@mediatek.com>
    CR-Id: ALPS03272733
    Feature: Sched
    (cherry picked from commit 00151e2da37f3a66fcc0656bca72ffdae2462dc2)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -279,7 +279,7 @@ static inline struct rq *rq_of(struct cfs_rq *cfs_rq)
 
 static inline struct task_struct *task_of(struct sched_entity *se)
 {
-	SCHED_WARN_ON(!entity_is_task(se));
+	/* SCHED_WARN_ON(!entity_is_task(se)); */
 	return container_of(se, struct task_struct, se);
 }
 
