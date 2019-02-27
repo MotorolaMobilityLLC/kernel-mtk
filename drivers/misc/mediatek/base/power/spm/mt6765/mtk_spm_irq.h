e67dc48748354d3b122ccecf9de5cb96a0423f09
albert-zl.huang <albert-zl.huang@mediatek.com>
2019-02-28 02:32:06 +0800

    [ALPS03901142] suspend: add wakeup reason
    [Detail] add last_resume_reason
    
    MTK-Commit-Id: f999d32361126845ecae2331abce2737b3d498fe
    
    Change-Id: If4c308ec4360fb0d0edbfe4f7e3fb0523562ef72
    Signed-off-by: albert-zl.huang <albert-zl.huang@mediatek.com>
    CR-Id: ALPS03901142
    Feature: Power Management
    (cherry picked from commit 189b7064de86caac246505b56f02a698d4fa011d)

--- drivers/misc/mediatek/base/power/spm/mt6765/mtk_spm_irq.h
+++ drivers/misc/mediatek/base/power/spm/mt6765/mtk_spm_irq.h
@@ -20,5 +20,6 @@ int mtk_spm_irq_register(unsigned int spm_irq_0);
 /* for mtk_spm_idle.c and mtk_spm_suspend.c */
 void mtk_spm_irq_backup(void);
 void mtk_spm_irq_restore(void);
+unsigned int mtk_spm_get_irq_0(void);
 
 #endif /* __MTK_SPM_IRQ_H__ */
--- drivers/misc/mediatek/base/power/spm/mt6765/mtk_spm_suspend.c
+++ drivers/misc/mediatek/base/power/spm/mt6765/mtk_spm_suspend.c
@@ -15,6 +15,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/delay.h>
+#include <linux/wakeup_reason.h>
 #include <asm/setup.h>
 
 #if defined(CONFIG_MTK_WATCHDOG) && defined(CONFIG_MTK_WD_KICKER)
@@ -211,6 +212,8 @@ static unsigned int spm_output_wake_reason(struct wake_status *wakesta)
 		NULL, 0);
 #endif
 #endif
+	log_wakeup_reason(mtk_spm_get_irq_0());
+
 	return wr;
 }
 
