b6ee78bc3bb7ff8826746e95f32458a12efe5350
Johnson Lin <johnson.lin@mediatek.com>
2019-02-28 02:34:06 +0800

    [ALPS03941249] mrdump_key: support PWR and HOME key
    [Detail]
    support DTS configuration to enable
    POWER + HOME(Volum up) key combination
    
    MTK-Commit-Id: cfd18a7d3cb840cd4934035f5fef514128cc7839
    
    Change-Id: I8f8cc5f9c0fddbca579693269e76f80379ba8735
    Signed-off-by: Johnson Lin <johnson.lin@mediatek.com>
    CR-Id: ALPS03941249
    Feature: Memory RAM Dump (MRDUMP)

--- drivers/misc/mediatek/aee/mrdump/mrdump_key_setup.c
+++ drivers/misc/mediatek/aee/mrdump/mrdump_key_setup.c
@@ -36,6 +36,10 @@ enum MRDUMP_NOTIFY_MODE {
 enum MRDUMP_LONG_PRESS_MODE {
 	LONG_PRESS_NONE,
 	LONG_PRESS_SHUTDOWN};
+enum MRDUMP_LONG_PRESS_KEY_MODE {
+	KEY_NONE,
+	POWER_ONLY,
+	POWER_HOME};
 #endif
 
 static int __init mrdump_key_init(void)
@@ -49,7 +53,10 @@ static int __init mrdump_key_init(void)
 #ifdef CONFIG_MTK_PMIC_COMMON
 	enum MRDUMP_LONG_PRESS_MODE long_press_mode
 			= LONG_PRESS_NONE;
+	enum MRDUMP_LONG_PRESS_KEY_MODE key_combine_mode
+			= KEY_NONE;
 	const char *long_press;
+	const char *key_combine;
 #endif
 	struct device_node *node;
 	const char *source_str, *mode_str, *interrupts;
@@ -74,8 +81,21 @@ static int __init mrdump_key_init(void)
 				&long_press)) {
 				if (strcmp(long_press, "SHUTDOWN") == 0)
 					long_press_mode = LONG_PRESS_SHUTDOWN;
-				else
+				else if (strcmp(long_press, "NONE") == 0)
 					long_press_mode = LONG_PRESS_NONE;
+				else
+					pr_info("long_press=%s not supported\n",
+					long_press);
+			}
+
+			if (!of_property_read_string(node, "key_combination",
+				&key_combine)) {
+				if (strcmp(key_combine, "POWER_ONLY") == 0)
+					key_combine_mode = POWER_ONLY;
+				else if (strcmp(key_combine, "POWER_HOME") == 0)
+					key_combine_mode = POWER_HOME;
+				else
+					key_combine_mode = KEY_NONE;
 			}
 #endif
 		}
@@ -113,6 +133,22 @@ static int __init mrdump_key_init(void)
 		} else {
 			pr_notice("long_press_mode = NONE\n");
 			pmic_enable_smart_reset(1, 0);
+		}
+
+		if (key_combine_mode == POWER_ONLY) {
+			pmic_config_interface(PMIC_RG_PWRKEY_RST_EN_ADDR
+			, 0x1, PMIC_RG_PWRKEY_RST_EN_MASK,
+			PMIC_RG_PWRKEY_RST_EN_SHIFT);
+			pr_info("key_combine=POWER_ONLY\n");
+
+		} else if (key_combine_mode == POWER_HOME) {
+			pmic_config_interface(PMIC_RG_HOMEKEY_RST_EN_ADDR,
+			0x1, PMIC_RG_HOMEKEY_RST_EN_MASK,
+			PMIC_RG_HOMEKEY_RST_EN_SHIFT);
+			pr_info("key_combine=POWER_HOME\n");
+		} else {
+			pr_info("key_combine_mode=%d not aply\n"
+					, key_combine_mode);
 		}
 #endif
 
