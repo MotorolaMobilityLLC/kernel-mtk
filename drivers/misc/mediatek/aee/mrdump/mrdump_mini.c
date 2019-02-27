c1bb7a9aed2596d93b86a6f3f1723a5b7e8242a5
Xinghua Yang <xinghua.yang@mediatek.com>
2019-02-28 02:29:27 +0800

    [ALPS03748913] aee: fix nested panic blocked issue
    [Detail]
    nested panic flow will be blocked when kernel module config is enabled
    and this issue only happens for non-aarch64 case
    also backtrace information should be processed for the first priority to
    reduce lost possibility
    
    [Solution]
    preprocessing to avoid translation fault in nested panic flow
    move backward module info saving work
    
    MTK-Commit-Id: 6becc88673729108e6bd5d16152e11f672fe2223
    
    Change-Id: I73087da16c9df0621ffaffab71381f78264da9f0
    Signed-off-by: Xinghua Yang <xinghua.yang@mediatek.com>
    CR-Id: ALPS03748913
    Feature: Power Management

--- drivers/misc/mediatek/aee/mrdump/mrdump_mini.c
+++ drivers/misc/mediatek/aee/mrdump/mrdump_mini.c
@@ -752,10 +752,10 @@ void mrdump_mini_ke_cpu_regs(struct pt_regs *regs)
 		mrdump_mini_save_regs(regs);
 	}
 	cpu = get_HW_cpuid();
-	mrdump_modules_info(NULL, -1);
 	mrdump_mini_cpu_regs(cpu, regs, current, 1);
 	mrdump_mini_add_loads();
 	mrdump_mini_build_task_info(regs);
+	mrdump_modules_info(NULL, -1);
 	if (once == 0) {
 		once = 1;
 		mrdump_mini_add_extra_misc((unsigned long)extra_members,
