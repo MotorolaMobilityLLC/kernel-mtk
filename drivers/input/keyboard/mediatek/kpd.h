377c1a3c95f8f1d369035ebd46670e79228adcf5
Qiangming Xia <qiangming.xia@mediatek.com>
2019-02-27 21:50:20 +0800

    [ALPS03484972] Keyboard: Migration keyboard driver
    [Detail]
    1. Remove some unused feature.
    2. Compatible new wakelock API.eg.support CONFIG_PM _WAKELOCKS.
    3. Coding style.
    
    [Solution] Modify keyboard driver
    
    MTK-Commit-Id: 19b12bf17675221f27e43d8aabcfa94e1ac77e61
    
    Change-Id: I6f280406e73984d12e32c6e96c7e861779135ce4
    Signed-off-by: Qiangming Xia <qiangming.xia@mediatek.com>
    CR-Id: ALPS03484972
    Feature: Keypad

--- drivers/input/keyboard/mediatek/kpd.h
+++ drivers/input/keyboard/mediatek/kpd.h
@@ -1,19 +1,14 @@
 /*
- * (C) Copyright 2009
- * MediaTek <www.MediaTek.com>
- *
- * MT6516 Sensor IOCTL & data structure
+ * Copyright (C) 2016 MediaTek Inc.
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
 
 #ifndef __KPD_H__
@@ -37,20 +32,40 @@
 #include <linux/delay.h>
 
 struct keypad_dts_data {
-	u16 kpd_key_debounce;
-	u16 kpd_use_extend_type;
-	u16 kpd_hw_map_num;
-	u16 kpd_hw_init_map[KPD_NUM_KEYS];
+	u32 kpd_key_debounce;
+	u32 kpd_sw_pwrkey;
+	u32 kpd_hw_pwrkey;
+	u32 kpd_sw_rstkey;
+	u32 kpd_hw_rstkey;
+	u32 kpd_use_extend_type;
+	u32 kpd_hw_map_num;
+	u32 kpd_hw_init_map[KPD_NUM_KEYS];
+	u32 kpd_hw_dl_key1;
+	u32 kpd_hw_dl_key2;
+	u32 kpd_hw_dl_key3;
+	u32 kpd_hw_recovery_key;
+	u32 kpd_hw_factory_key;
 };
+
 extern struct keypad_dts_data kpd_dts_data;
+extern int kpd_klog_en;
 
-#define KPD_DEBUG	0
+#define KPD_DEBUG	1
 #define SET_KPD_KCOL		_IO('k', 29)
-
 #define KPD_SAY		"kpd: "
+
 #if KPD_DEBUG
-#define kpd_print(fmt, arg...)	pr_debug(KPD_SAY fmt, ##arg)
-#define kpd_info(fmt, arg...)	pr_debug(KPD_SAY fmt, ##arg)
+#define kpd_print(fmt, arg...)	do { \
+	if (kpd_klog_en) \
+		pr_debug(KPD_SAY fmt, ##arg); \
+	} while (0)
+#define kpd_info(fmt, arg...)	do { \
+	if (kpd_klog_en) \
+		pr_info(KPD_SAY fmt, ##arg); \
+	} while (0)
+#define kpd_notice(fmt, arg...)	 \
+			pr_notice(KPD_SAY fmt, ##arg)
+
 #else
 #define kpd_print(fmt, arg...)	do {} while (0)
 #define kpd_info(fmt, arg...)	do {} while (0)
