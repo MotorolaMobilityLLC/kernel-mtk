447665477600c1be3fa986bb7c56a71795de255c
Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
2019-02-27 22:09:21 +0800

    [ALPS03520309] cmdq: enable cmdq driver
    1. Enable cmdq driver in kernel.
    2. Change max length to 80.
    3. Modify log sytles.
    
    MTK-Commit-Id: 80f4073dcc15d77c32ba32621f84b8e123aa3301
    
    Change-Id: Ica67cf0293198a8fddcb256a9f7047a9c52eeba9
    Signed-off-by: Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
    CR-Id: ALPS03520309
    Feature: CMDQ

---
+++ drivers/misc/mediatek/cmdq/v3/cmdq_mdp_common.h
@@ -0,0 +1,178 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __CMDQ_MDP_COMMON_H__
+#define __CMDQ_MDP_COMMON_H__
+
+#include "cmdq_def.h"
+#include "cmdq_core.h"
+
+#include <linux/types.h>
+
+/* dump mmsys config */
+typedef void (*CmdqDumpMMSYSConfig) (void);
+
+/* VENC callback function */
+typedef s32(*CmdqVEncDumpInfo) (u64 engineFlag, int level);
+
+/* query MDP clock is on  */
+typedef bool(*CmdqMdpClockIsOn) (enum CMDQ_ENG_ENUM engine);
+
+/* enable MDP clock  */
+typedef void (*CmdqEnableMdpClock) (bool enable, enum CMDQ_ENG_ENUM engine);
+
+/* Common Clock Framework */
+typedef void (*CmdqMdpInitModuleClk) (void);
+
+/* MDP callback function */
+typedef s32(*CmdqMdpClockOn) (u64 engineFlag);
+
+typedef s32(*CmdqMdpDumpInfo) (u64 engineFlag, int level);
+
+typedef s32(*CmdqMdpResetEng) (u64 engineFlag);
+
+typedef s32(*CmdqMdpClockOff) (u64 engineFlag);
+
+/* MDP Initialization setting */
+typedef void(*CmdqMdpInitialSet) (void);
+
+/* Initialization & de-initialization MDP base VA */
+typedef void (*CmdqMdpInitModuleBaseVA) (void);
+
+typedef void (*CmdqMdpDeinitModuleBaseVA) (void);
+
+/* MDP engine dump */
+typedef void (*CmdqMdpDumpRSZ) (const unsigned long base, const char *label);
+
+typedef void (*CmdqMdpDumpTDSHP) (const unsigned long base, const char *label);
+
+/* test MDP clock function */
+typedef u32(*CmdqMdpRdmaGetRegOffsetSrcAddr) (void);
+
+typedef u32(*CmdqMdpWrotGetRegOffsetDstAddr) (void);
+
+typedef u32(*CmdqMdpWdmaGetRegOffsetDstAddr) (void);
+
+typedef void (*CmdqTestcaseClkmgrMdp) (void);
+
+typedef const char*(*CmdqDispatchModule) (u64 engineFlag);
+
+typedef void (*CmdqTrackTask) (const struct TaskStruct *pTask);
+
+typedef const char *(*CmdqPraseErrorModByEngFlag) (
+	const struct TaskStruct *task);
+
+typedef u64 (*CmdqMdpGetEngineGroupBits) (u32 engine_group);
+
+struct cmdqMDPFuncStruct {
+	CmdqDumpMMSYSConfig dumpMMSYSConfig;
+	CmdqVEncDumpInfo vEncDumpInfo;
+	CmdqMdpInitModuleBaseVA initModuleBaseVA;
+	CmdqMdpDeinitModuleBaseVA deinitModuleBaseVA;
+	CmdqMdpClockIsOn mdpClockIsOn;
+	CmdqEnableMdpClock enableMdpClock;
+	CmdqMdpInitModuleClk initModuleCLK;
+	CmdqMdpDumpRSZ mdpDumpRsz;
+	CmdqMdpDumpTDSHP mdpDumpTdshp;
+	CmdqMdpClockOn mdpClockOn;
+	CmdqMdpDumpInfo mdpDumpInfo;
+	CmdqMdpResetEng mdpResetEng;
+	CmdqMdpClockOff mdpClockOff;
+	CmdqMdpInitialSet mdpInitialSet;
+	CmdqMdpRdmaGetRegOffsetSrcAddr rdmaGetRegOffsetSrcAddr;
+	CmdqMdpWrotGetRegOffsetDstAddr wrotGetRegOffsetDstAddr;
+	CmdqMdpWdmaGetRegOffsetDstAddr wdmaGetRegOffsetDstAddr;
+	CmdqTestcaseClkmgrMdp testcaseClkmgrMdp;
+	CmdqDispatchModule dispatchModule;
+	CmdqTrackTask trackTask;
+	CmdqPraseErrorModByEngFlag parseErrModByEngFlag;
+	CmdqMdpGetEngineGroupBits getEngineGroupBits;
+	CmdqErrorResetCB errorReset;
+};
+
+/* track MDP task */
+#define DEBUG_STR_LEN 1024 /* debug str length */
+#define MDP_MAX_TASK_NUM 5 /* num of tasks to be keep */
+#define MDP_MAX_PLANE_NUM 3 /* max color format plane num */
+/* each plane has 2 info address and size */
+#define MDP_PORT_BUF_INFO_NUM (MDP_MAX_PLANE_NUM * 2)
+#define MDP_BUF_INFO_STR_LEN 8 /* each buf info length */
+/* dispatch key format is MDP_(ThreadName) */
+#define MDP_DISPATCH_KEY_STR_LEN (TASK_COMM_LEN + 5)
+
+struct cmdqMDPTaskStruct {
+	char callerName[TASK_COMM_LEN];
+	char userDebugStr[DEBUG_STR_LEN];
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void cmdq_mdp_virtual_function_setting(void);
+void cmdq_mdp_map_mmsys_VA(void);
+long cmdq_mdp_get_module_base_VA_MMSYS_CONFIG(void);
+void cmdq_mdp_unmap_mmsys_VA(void);
+struct cmdqMDPFuncStruct *cmdq_mdp_get_func(void);
+
+void cmdq_mdp_enable(u64 engineFlag, enum CMDQ_ENG_ENUM engine);
+
+int cmdq_mdp_loop_reset(enum CMDQ_ENG_ENUM engine,
+	const unsigned long resetReg,
+	const unsigned long resetStateReg,
+	const u32 resetMask,
+	const u32 resetValue, const bool pollInitResult);
+
+void cmdq_mdp_loop_off(enum CMDQ_ENG_ENUM engine,
+	const unsigned long resetReg,
+	const unsigned long resetStateReg,
+	const u32 resetMask,
+	const u32 resetValue, const bool pollInitResult);
+
+const char *cmdq_mdp_get_rsz_state(const u32 state);
+
+void cmdq_mdp_dump_venc(const unsigned long base, const char *label);
+void cmdq_mdp_dump_rdma(const unsigned long base, const char *label);
+void cmdq_mdp_dump_rot(const unsigned long base, const char *label);
+void cmdq_mdp_dump_color(const unsigned long base, const char *label);
+void cmdq_mdp_dump_wdma(const unsigned long base, const char *label);
+
+void cmdq_mdp_check_TF_address(unsigned int mva, char *module);
+const char *cmdq_mdp_parse_error_module_by_hwflag(
+	const struct TaskStruct *task);
+
+/* Platform dependent function */
+
+s32 cmdqMdpClockOn(u64 engineFlag);
+
+s32 cmdqMdpDumpInfo(u64 engineFlag, int level);
+
+s32 cmdqVEncDumpInfo(u64 engineFlag, int level);
+
+s32 cmdqMdpResetEng(u64 engineFlag);
+
+s32 cmdqMdpClockOff(u64 engineFlag);
+
+u32 cmdq_mdp_rdma_get_reg_offset_src_addr(void);
+u32 cmdq_mdp_wrot_get_reg_offset_dst_addr(void);
+u32 cmdq_mdp_wdma_get_reg_offset_dst_addr(void);
+
+void testcase_clkmgr_mdp(void);
+
+/* Platform virtual function setting */
+void cmdq_mdp_platform_function_setting(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif				/* __CMDQ_MDP_COMMON_H__ */
---
+++ drivers/misc/mediatek/cmdq/v3/cmdq_sec.h
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __CMDQ_SEC_H__
+#define __CMDQ_SEC_H__
+
+#include "cmdq_core.h"
+
+#if defined(CMDQ_SECURE_PATH_SUPPORT)
+#include "tee_client_api.h"
+#include "cmdq_sec_iwc_common.h"
+
+#if defined(CONFIG_TRUSTONIC_TEE_SUPPORT)
+#include "mobicore_driver_api.h"
+#endif /* CONFIG_TRUSTONIC_TEE_SUPPORT */
+
+#endif /* CMDQ_SECURE_PATH_SUPPORT */
+
+/* error code for CMDQ */
+#define CMDQ_ERR_NULL_SEC_CTX_HANDLE (6000)
+#define CMDQ_ERR_SEC_CTX_SETUP (6001)
+#define CMDQ_ERR_SEC_CTX_TEARDOWN (6002)
+
+/* inter-world communication state */
+enum CMDQ_IWC_STATE_ENUM {
+	IWC_INIT = 0,
+	IWC_CONTEXT_INITED = 1,
+	IWC_WSM_ALLOCATED = 2,
+	IWC_SES_OPENED = 3,
+	IWC_SES_MSG_PACKAGED = 4,
+	IWC_SES_TRANSACTED = 5,
+	IWC_SES_ON_TRANSACTED = 6,
+	IWC_END_OF_ENUM = 7,
+};
+
+/* CMDQ secure context struct
+ * note it is not global data, each process has its own CMDQ sec context
+ */
+struct cmdqSecContextStruct {
+	struct list_head listEntry;
+
+	/* basic info */
+	u32 tgid;		/* tgid of process context */
+	u32 referCount;	/* reference count for open cmdq device node */
+
+	/* iwc state */
+	enum CMDQ_IWC_STATE_ENUM state;
+
+	/* iwc information */
+	void *iwcMessage;	/* message buffer */
+#if defined(CMDQ_SECURE_PATH_SUPPORT)
+#if defined(CMDQ_GP_SUPPORT)
+
+#if defined(CONFIG_TRUSTONIC_TEE_SUPPORT)
+	/* Universally Unique Identifier of secure tl/dr */
+	struct TEEC_UUID uuid;
+#else
+	TEEC_UUID uuid;	/* Universally Unique Identifier of secure tl/dr */
+#endif
+	struct TEEC_Context gp_context; /* basic context */
+	struct TEEC_Session sessionHandle; /* session handle */
+	struct TEEC_SharedMemory shared_mem; /* shared memory */
+#else
+	/* Universally Unique Identifier of secure tl/dr */
+	struct mc_uuid_t uuid;
+	struct mc_session_handle sessionHandle;	/* session handle */
+	/* true if someone has opened mobicore device in this
+	 * prpocess context
+	 */
+	u32 openMobicoreByOther;
+#endif
+#endif
+};
+
+s32 cmdq_sec_init_allocate_resource_thread(void *data);
+
+/* Create and destroy non-cachable shared memory,
+ * used to share data for CMDQ driver between NWd and SWd
+ *
+ * Be careful that we should not disvlose any information about secure buffer
+ * address of
+ */
+s32 cmdq_sec_create_shared_memory(struct cmdqSecSharedMemoryStruct **pHandle,
+	const u32 size);
+s32 cmdq_sec_destroy_shared_memory(struct cmdqSecSharedMemoryStruct *handle);
+
+/* Callback to fill message buffer for secure task
+ *
+ * Params:
+ *     init32_t command id
+ *     void*	pornter of TaskStruct
+ *     s32  CMDQ HW thread id
+ *     void*    the inter-world communication buffer
+ * Return:
+ *     >=0 for success;
+ */
+typedef s32(*CmdqSecFillIwcCB) (s32, void *, s32, void *);
+
+
+/* Entry secure world to handle secure path jobs
+ * .submit task
+ * .cancel error task
+ */
+s32 cmdq_sec_exec_task_async_unlocked(struct TaskStruct *pTask, s32 thread);
+s32 cmdq_sec_cancel_error_task_unlocked(struct TaskStruct *pTask, s32 thread,
+	struct cmdqSecCancelTaskResultStruct *pResult);
+s32 cmdq_sec_allocate_path_resource_unlocked(bool throwAEE);
+
+/* secure path control */
+void cmdq_sec_lock_secure_path(void);
+void cmdq_sec_unlock_secure_path(void);
+
+void cmdqSecInitialize(void);
+void cmdqSecDeInitialize(void);
+
+void cmdqSecEnableProfile(const bool enable);
+
+/* function declaretion */
+struct cmdqSecContextStruct *cmdq_sec_context_handle_create(u32 tgid);
+
+#endif				/* __DDP_CMDQ_SEC_H__ */
