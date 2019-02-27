b2958f4a58ec93abffecf7824615ab3b8406b7b3
Owen Chen <owen.chen@mediatek.com>
2019-02-28 02:29:02 +0800

    [ALPS03779604] clkmgr: print enable clks
    [Detail]
    add enable clks log if mtcmos stuck in while loop
    over 100000 times.
    
    [Solution]
    debug usage.
    
    MTK-Commit-Id: fda6c4b964eafd6b8cc9ad967361ce21e0ef799b
    
    Change-Id: Ic493888ac7006a51c9b9b37f95810b14c5c3afc2
    CR-Id: ALPS03779604
    Feature: Clock Manager
    Signed-off-by: Owen Chen <owen.chen@mediatek.com>

--- drivers/clk/mediatek/clk-mt6765-pg.h
+++ drivers/clk/mediatek/clk-mt6765-pg.h
@@ -62,4 +62,6 @@ extern unsigned int mt_get_ckgen_freq(unsigned int ID);
 #ifdef CONFIG_MTK_RAM_CONSOLE
 extern void aee_rr_rec_clk(int id, u32 val);
 #endif
+
+extern void print_enabled_clks_once(void);
 #endif/* __DRV_CLK_MT6758_PG_H */
