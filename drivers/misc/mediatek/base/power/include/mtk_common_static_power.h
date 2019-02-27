2564a06fdbb153ea69cff20bd90f61798db9a77b
charles.yang <charles.yang@mediatek.com>
2019-02-28 04:05:01 +0800

    [ALPS04079665] PTPOD: Introduce PTPOD, UPOWER, SPOWER driver
    [Detail] Introduce driver
    
    MTK-Commit-Id: b86cfaef3f74bcfb96fea002d7af563a17ca9ca4
    
    Change-Id: I4853f0190b5aa2b884ef63162678dbd037163686
    Signed-off-by: charles.yang <charles.yang@mediatek.com>
    CR-Id: ALPS04079665
    Feature: PTP-OD (Performance Thermal Power-Over Drive)
    (cherry-pick from 7126c6cf1e1442b143bf22c5670d6372c30901fd)

--- drivers/misc/mediatek/base/power/include/mtk_common_static_power.h
+++ drivers/misc/mediatek/base/power/include/mtk_common_static_power.h
@@ -14,10 +14,7 @@
 #ifndef _COMMON_MTK_STATIC_POWER_H
 #define _COMMON_MTK_STATIC_POWER_H
 
-#if defined(CONFIG_MACH_MT6759) || defined(CONFIG_MACH_MT6763) || \
-	defined(CONFIG_MACH_MT6758) || defined(CONFIG_MACH_MT6739) || \
-	defined(CONFIG_MACH_MT6765) || defined(CONFIG_MACH_MT6761) || \
-	defined(CONFIG_MACH_MT3967)
+#if defined(CONFIG_MTK_STATIC_POWER)
 #include "leakage_table_v2/mtk_static_power.h"
 #endif
 
