3f533955770934a032183f0bd7819e90c7d526ba
mtk12211 <shang.zhou@mediatek.com>
2019-02-28 00:39:29 +0800

    [ALPS03571241] DISP: Enable PQ
    [Detail]
    Enable PQ.
    
    MTK-Commit-Id: 93c743a82ef94b8900b7881ffd7db5d1cdee5864
    
    Change-Id: I29eb4f5ad04eb612b41f1290c509dde5ddc40c27
    Signed-off-by: mtk12211 <shang.zhou@mediatek.com>
    CR-Id: ALPS03571241
    Feature: Display Driver (DSI Interface)

--- drivers/misc/mediatek/video/mt6765/videox/disp_helper.c
+++ drivers/misc/mediatek/video/mt6765/videox/disp_helper.c
@@ -371,7 +371,7 @@ void disp_helper_option_init(void) /* SHANG: Need modify */
 	/* use RGB565 format for decouple mode intermediate buffer */
 	disp_helper_set_option(DISP_OPT_DECOUPLE_MODE_USE_RGB565, 0);
 
-	disp_helper_set_option(DISP_OPT_BYPASS_PQ, 1);
+	disp_helper_set_option(DISP_OPT_BYPASS_PQ, 0);
 	disp_helper_set_option(DISP_OPT_MUTEX_EOF_EN_FOR_CMD_MODE, 1);
 	disp_helper_set_option(DISP_OPT_ESD_CHECK_RECOVERY, 1);
 	disp_helper_set_option(DISP_OPT_ESD_CHECK_SWITCH, 1);
