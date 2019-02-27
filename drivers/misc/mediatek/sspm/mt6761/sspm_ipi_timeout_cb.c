4547e95e8ab3f0c4338307cf31e94f1322080559
HT Feng <ht.feng@mediatek.com>
2019-02-28 04:05:46 +0800

    [ALPS04143691] sspm: add callback when ipi timeout
    dump emi debug information when IPI timeout
    
    MTK-Commit-Id: a89483676ad91600c301efadec9588146c2132ed
    
    Change-Id: I011e9a65f9f956f78dcb4d7b377e94bb45fa3ac2
    CR-Id: ALPS04143691
    Signed-off-by: HT Feng <ht.feng@mediatek.com>
    Feature: [Android Default] Camera Application Basic Functions
    (cherry-pick from 676dc3fa8dc9636a0f896fbbabb9094cb41563bc)

--- drivers/misc/mediatek/sspm/mt6761/sspm_ipi_timeout_cb.c
+++ drivers/misc/mediatek/sspm/mt6761/sspm_ipi_timeout_cb.c
@@ -23,6 +23,8 @@ __weak void usb_dump_debug_register(void) {}
 /* platform callback when ipi timeout */
 void sspm_ipi_timeout_cb(int ipi_id)
 {
-
+	/* for debug EMI use */
+	pr_info("%s: dump_emi_outstanding\n", __func__);
+	dump_emi_outstanding();
 }
 
