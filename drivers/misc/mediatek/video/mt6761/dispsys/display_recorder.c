4f11ec95d722000aef86591d59e241e04e0da191
haizhen.wang <haizhen.wang@mediatek.com>
2019-02-28 02:30:48 +0800

    [ALPS03819502] [DISP]: Add log for esd check
    [Detail]
    Sync from Cervino
    1.Print dsi irq mmp log.
    2.Add esd check mmp log.
    3.In vdo mode, remove kick and blocking flush in recovery flow.
    
    [Solution]
    
    MTK-Commit-Id: 2ddedc8967dded320966347265df793d61d839ea
    
    Change-Id: I4eb63e37adb3954a3003c0a49708f9f0890d92ae
    Signed-off-by: haizhen.wang <haizhen.wang@mediatek.com>
    CR-Id: ALPS03819502
    Feature: [Android Default] Factory Reset Protection (Kill Switch)

--- drivers/misc/mediatek/video/mt6761/dispsys/display_recorder.c
+++ drivers/misc/mediatek/video/mt6761/dispsys/display_recorder.c
@@ -139,6 +139,10 @@ static mmp_event dprec_mmp_event_spy(enum DPREC_LOGGER_ENUM l)
 		return ddp_mmp_get_events()->session_wait_vsync;
 	case DPREC_LOGGER_DISPMGR_CACHE_SYNC:
 		return ddp_mmp_get_events()->primary_cache_sync;
+	case DPREC_LOGGER_ESD_RECOVERY:
+		return ddp_mmp_get_events()->esd_recovery;
+	case DPREC_LOGGER_ESD_CMDQ:
+		return ddp_mmp_get_events()->esd_cmdq;
 /*	case DPREC_LOGGER_DSI_EXT_TE:
  *		return ddp_mmp_get_events()->dsi_te;
  */
