c52a821655bb6db7a3d27d859bd0bd8e5d6123dc
Cheng-Jung Ho <Cheng-Jung.Ho@mediatek.com>
2019-02-27 22:22:36 +0800

    [ALPS03511539] Video Codec: Enable 6758 video codec driver
    Sync video codec common header with 8173 and enable 6758 videocodec kernel
    driver.
    
    MTK-Commit-Id: d7d3d424a4faa3eee37e24f5765ca567317f70b3
    
    Feature: Video Codec
    Change-Id: Ic163f2a0c2b0b7521ef56dfeb0d0bf0bdb037272
    Signed-off-by: Cheng-Jung Ho <Cheng-Jung.Ho@mediatek.com>
    CR-Id: ALPS03511539

--- drivers/misc/mediatek/videocodec/include/kernel/val_api_private.h
+++ drivers/misc/mediatek/videocodec/include/kernel/val_api_private.h
@@ -23,57 +23,62 @@ extern "C" {
 #include "libmtk_cipher_export.h"
 
 
-VAL_RESULT_T eValInit(VAL_HANDLE_T *a_phHalHandle);
-VAL_RESULT_T eValDeInit(VAL_HANDLE_T *a_phHalHandle);
+enum VAL_RESULT_T eValInit(unsigned long *a_phHalHandle);
+enum VAL_RESULT_T eValDeInit(unsigned long *a_phHalHandle);
 
-VAL_RESULT_T eVideoIntMemAlloc(VAL_INTMEM_T *a_prParam,
-				VAL_UINT32_T a_u4ParamSize);
-VAL_RESULT_T eVideoIntMemFree(VAL_INTMEM_T *a_prParam,
-				VAL_UINT32_T a_u4ParamSize);
+enum VAL_RESULT_T eVideoIntMemAlloc
+	(struct VAL_INTMEM_T *a_prParam, unsigned int a_u4ParamSize);
+enum VAL_RESULT_T eVideoIntMemFree
+	(struct VAL_INTMEM_T *a_prParam, unsigned int a_u4ParamSize);
 
-VAL_RESULT_T eVideoCreateEvent(VAL_EVENT_T *a_prParam,
-				VAL_UINT32_T a_u4ParamSize);
-VAL_RESULT_T eVideoSetEvent(VAL_EVENT_T *a_prParam,
-				VAL_UINT32_T a_u4ParamSize);
-VAL_RESULT_T eVideoCloseEvent(VAL_EVENT_T *a_prParam,
-				VAL_UINT32_T a_u4ParamSize);
-VAL_RESULT_T eVideoWaitEvent(VAL_EVENT_T *a_prParam,
-				VAL_UINT32_T a_u4ParamSize);
+enum VAL_RESULT_T eVideoCreateEvent
+	(struct VAL_EVENT_T *a_prParam, unsigned int a_u4ParamSize);
+enum VAL_RESULT_T eVideoSetEvent
+	(struct VAL_EVENT_T *a_prParam, unsigned int a_u4ParamSize);
+enum VAL_RESULT_T eVideoCloseEvent
+	(struct VAL_EVENT_T *a_prParam, unsigned int a_u4ParamSize);
+enum VAL_RESULT_T eVideoWaitEvent
+	(struct VAL_EVENT_T *a_prParam, unsigned int a_u4ParamSize);
 
-VAL_RESULT_T eVideoCreateMutex(VAL_MUTEX_T *a_prParam,
-				VAL_UINT32_T a_u4ParamSize);
-VAL_RESULT_T eVideoCloseMutex(VAL_MUTEX_T *a_prParam,
-				VAL_UINT32_T a_u4ParamSize);
-VAL_RESULT_T eVideoWaitMutex(VAL_MUTEX_T *a_prParam,
-				VAL_UINT32_T a_u4ParamSize);
-VAL_RESULT_T eVideoReleaseMutex(VAL_MUTEX_T *a_prParam,
-				VAL_UINT32_T a_u4ParamSize);
+enum VAL_RESULT_T eVideoCreateMutex
+	(struct VAL_MUTEX_T *a_prParam, unsigned int a_u4ParamSize);
+enum VAL_RESULT_T eVideoCloseMutex
+	(struct VAL_MUTEX_T *a_prParam, unsigned int a_u4ParamSize);
+enum VAL_RESULT_T eVideoWaitMutex
+	(struct VAL_MUTEX_T *a_prParam, unsigned int a_u4ParamSize);
+enum VAL_RESULT_T eVideoReleaseMutex
+	(struct VAL_MUTEX_T *a_prParam, unsigned int a_u4ParamSize);
 
-VAL_RESULT_T eVideoMMAP(VAL_MMAP_T *a_prParam, VAL_UINT32_T a_u4ParamSize);
-VAL_RESULT_T eVideoUnMMAP(VAL_MMAP_T *a_prParam, VAL_UINT32_T a_u4ParamSize);
+enum VAL_RESULT_T eVideoMMAP
+	(struct VAL_MMAP_T *a_prParam, unsigned int a_u4ParamSize);
+enum VAL_RESULT_T eVideoUnMMAP
+	(struct VAL_MMAP_T *a_prParam, unsigned int a_u4ParamSize);
 
-VAL_RESULT_T eVideoInitLockHW(VAL_VCODEC_OAL_HW_REGISTER_T *prParam, int size);
-VAL_RESULT_T eVideoDeInitLockHW(VAL_VCODEC_OAL_HW_REGISTER_T *prParam,
-				int size);
+enum VAL_RESULT_T eVideoInitLockHW
+	(struct VAL_VCODEC_OAL_HW_REGISTER_T *prParam, int size);
+enum VAL_RESULT_T eVideoDeInitLockHW
+	(struct VAL_VCODEC_OAL_HW_REGISTER_T *prParam, int size);
 
-VAL_RESULT_T eVideoVCodecCoreLoading(int CPUid, int *Loading);
-VAL_RESULT_T eVideoVCodecCoreNumber(int *CPUNums);
+enum VAL_RESULT_T eVideoVCodecCoreLoading(int CPUid, int *Loading);
+enum VAL_RESULT_T eVideoVCodecCoreNumber(int *CPUNums);
 
-VAL_RESULT_T eVideoConfigMCIPort(VAL_UINT32_T u4PortConfig,
-			VAL_UINT32_T *pu4PortResult, VAL_MEM_CODEC_T eMemCodec);
+enum VAL_RESULT_T eVideoConfigMCIPort
+	(unsigned int u4PortConfig, unsigned int *pu4PortResult,
+	enum VAL_MEM_CODEC_T eMemCodec);
 
+unsigned int eVideoHwM4UEnable(char bEnable);
 /* MTK_SEC_VIDEO_PATH_SUPPORT */
-VAL_UINT32_T eVideoHwM4UEnable(VAL_BOOL_T bEnable);
-VAL_UINT32_T eVideoLibDecrypt(VIDEO_ENCRYPT_CODEC_T a_eVIDEO_ENCRYPT_CODEC);
+unsigned int eVideoLibDecrypt
+	(enum VIDEO_ENCRYPT_CODEC_T a_eVIDEO_ENCRYPT_CODEC);
 
 /* for DirectLink Meta Mode + */
-VAL_RESULT_T eVideoAllocMetaHandleList(VAL_HANDLE_T *a_MetaHandleList);
-VAL_RESULT_T eVideoGetBufInfoFromMetaHandle(
-	VAL_HANDLE_T a_MetaHandleList,
-	VAL_VOID_T *a_pvInParam,
-	VAL_VOID_T *a_pvOutParam
+enum VAL_RESULT_T eVideoAllocMetaHandleList(unsigned long *a_MetaHandleList);
+enum VAL_RESULT_T eVideoGetBufInfoFromMetaHandle(
+	unsigned long a_MetaHandleList,
+	void *a_pvInParam,
+	void *a_pvOutParam
 );
-VAL_RESULT_T eVideoFreeMetaHandleList(VAL_HANDLE_T a_MetaHandleList);
+enum VAL_RESULT_T eVideoFreeMetaHandleList(unsigned long a_MetaHandleList);
 /* for DirectLink Meta Mode - */
 
 #ifdef __cplusplus
