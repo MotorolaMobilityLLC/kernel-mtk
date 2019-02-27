c89b3341efd507db5e5f221dfe27e5eac7328c42
Brian-SY Yang <brian-sy.yang@mediatek.com>
2019-02-27 22:08:17 +0800

    [ALPS03524972] PPM: add init version
    [Detail] add PPM driver for MT6758 but default only built in dummy
    driver
    
    MTK-Commit-Id: 5cf8ce867453bda5eba0b81a37b3630ca87516ae
    
    Change-Id: I0b984dc4095e495090760050bba4ef8362aaf363
    Signed-off-by: Brian-SY Yang <brian-sy.yang@mediatek.com>
    CR-Id: ALPS03524972
    Feature: CPU Hotplug

---
+++ drivers/misc/mediatek/base/power/include/mtk_ppm_api.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _MTK_PPM_API_H_
+#define _MTK_PPM_API_H_
+
+#if defined(CONFIG_MACH_MT6758)
+#include "ppm_v3/mtk_ppm_api.h"
+#endif
+
+#endif /* _MTK_PPM_API_H_ */
+
