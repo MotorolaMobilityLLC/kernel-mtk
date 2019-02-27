11edfebf56634cd3b90293092ee385969a254e2c
Morven-CF Yeh <morven-cf.yeh@mediatek.com>
2019-02-28 02:31:18 +0800

    [ALPS03837059] cm_mgr: enable cm_mgr
    [Detail] set cm_mgr_enable to 1
    
    MTK-Commit-Id: c2ea072cb2e3b362741c6222d6560c511ce6061a
    
    Change-Id: I9a096e91665fae6d8b1342b28350e6b45982eab4
    Signed-off-by: Morven-CF Yeh <morven-cf.yeh@mediatek.com>
    CR-Id: ALPS03837059
    Feature: Power Management

--- drivers/misc/mediatek/base/power/cm_mgr_v1/mt6765/mtk_cm_mgr_platform.h
+++ drivers/misc/mediatek/base/power/cm_mgr_v1/mt6765/mtk_cm_mgr_platform.h
@@ -26,6 +26,7 @@
 /* #define USE_IDLE_NOTIFY */
 #define USE_NEW_CPU_OPP
 /* #define USE_CM_MGR_AT_SSPM */
+/* #define USE_SINGLE_CLUSTER */
 
 #define CM_MGR_EMI_OPP	2
 #define CM_MGR_LOWER_OPP 9
@@ -33,6 +34,9 @@
 #define CM_MGR_CPU_CLUSTER 2
 #define CM_MGR_CPU_COUNT 8
 #define CM_MGR_CPU_LIMIT 4
+
+#define CLUSTER0_MASK   0x0f
+#define CLUSTER1_MASK   0xf0
 
 #define VCORE_ARRAY_SIZE CM_MGR_EMI_OPP
 #define CM_MGR_CPU_ARRAY_SIZE (CM_MGR_CPU_CLUSTER * CM_MGR_EMI_OPP)
