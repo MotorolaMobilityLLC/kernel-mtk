3d8e3ff61c73405ffd246af0a75bc90b88b692db
Yirong Wang <yirong.wang@mediatek.com>
2019-02-27 23:47:56 +0800

    [ALPS03610244] DTBO: check dtb overlay when build time
    Check if dtb overlay could work on built dtb, otherwise
    generate a build fail
    
    Conflicts:
    	arch/arm64/configs/tb8173p1_k49_defconfig
    	arch/arm64/configs/tb8173p1_k49_debug_defconfig
    
    MTK-Commit-Id: 3cef95efe9e87dd97465a24a70ac9d76ba89d273
    
    Change-Id: I77dc4fa4924257d0e94746f1d3bb026058abc24d
    CR-Id: ALPS03610244
    Feature: Devicetree Overlay
    Signed-off-by: Yirong Wang <yirong.wang@mediatek.com>

--- scripts/drvgen/drvgen.mk
+++ scripts/drvgen/drvgen.mk
@@ -12,29 +12,32 @@
 ifdef MTK_PLATFORM
 DRVGEN_PATH := drivers/misc/mediatek/dws/$(MTK_PLATFORM)
 
+ifeq ($(strip $(CONFIG_ARM64)), y)
+MAIN_DT_NAMES := $(subst $\",,$(CONFIG_BUILD_ARM64_APPENDED_DTB_IMAGE_NAMES))
+else
+MAIN_DT_NAMES := $(subst $\",,$(CONFIG_BUILD_ARM_APPENDED_DTB_IMAGE_NAMES))
+endif
+
 ifeq ($(strip $(CONFIG_MTK_DTBO_FEATURE)), y)
 
 ifeq ($(strip $(CONFIG_ARM64)), y)
-DT_NAMES := $(subst $\",,$(CONFIG_BUILD_ARM64_DTB_OVERLAY_IMAGE_NAMES))
+PROJ_DT_NAMES := $(subst $\",,$(CONFIG_BUILD_ARM64_DTB_OVERLAY_IMAGE_NAMES))
 else
-DT_NAMES := $(subst $\",,$(CONFIG_BUILD_ARM_DTB_OVERLAY_IMAGE_NAMES))
+PROJ_DT_NAMES := $(subst $\",,$(CONFIG_BUILD_ARM_DTB_OVERLAY_IMAGE_NAMES))
 endif
 
 else #DTBO is not enabled
 
-ifeq ($(strip $(CONFIG_ARM64)), y)
-DT_NAMES := $(subst $\",,$(CONFIG_BUILD_ARM64_APPENDED_DTB_IMAGE_NAMES))
-else
-DT_NAMES := $(subst $\",,$(CONFIG_BUILD_ARM_APPENDED_DTB_IMAGE_NAMES))
-endif
+PROJ_DT_NAMES := $(MAIN_DT_NAMES)
 
 endif #CONFIG_MTK_DTBO_FEATURE
 
-DTB_FILES := $(addsuffix .dtb,$(addprefix $(objtree)/arch/$(SRCARCH)/boot/dts/, $(DT_NAMES)))
-DTS_FILES := $(addsuffix .dts,$(addprefix $(srctree)/arch/$(SRCARCH)/boot/dts/, $(DT_NAMES)))
+MAIN_DTB_FILES := $(addsuffix .dtb,$(addprefix $(objtree)/arch/$(SRCARCH)/boot/dts/, $(MAIN_DT_NAMES)))
+PROJ_DTB_FILES := $(addsuffix .dtb,$(addprefix $(objtree)/arch/$(SRCARCH)/boot/dts/, $(PROJ_DT_NAMES)))
+PROJ_DTS_FILES := $(addsuffix .dts,$(addprefix $(srctree)/arch/$(SRCARCH)/boot/dts/, $(PROJ_DT_NAMES)))
 
-export DTB_FILES
-export DTS_FILES
+export PROJ_DTB_FILES
+export PROJ_DTS_FILES
 
 
 ifndef DRVGEN_OUT
@@ -52,7 +55,7 @@ DWS_FILE := $(srctree)/$(DRVGEN_PATH)/$(MTK_PROJECT).dws
 ifneq ($(wildcard $(DWS_FILE)),)
 DRVGEN_FILE_LIST := $(addprefix $(DRVGEN_OUT)/,$(ALL_DRVGEN_FILE))
 ifeq ($(strip $(CONFIG_MTK_DTBO_FEATURE)), y)
-DRVGEN_FILE_LIST += $(DTB_FILES)
+DRVGEN_FILE_LIST += $(PROJ_DTB_FILES)
 endif
 else
 DRVGEN_FILE_LIST :=
@@ -63,8 +66,8 @@ DRVGEN_FIG := $(wildcard $(dir $(DRVGEN_TOOL))config/*.fig)
 .PHONY: drvgen
 drvgen: $(DRVGEN_FILE_LIST)
 ifeq ($(strip $(CONFIG_MTK_DTBO_FEATURE)), y)
-$(DRVGEN_FILE_LIST): $(DRVGEN_TOOL) $(DWS_FILE) $(DRVGEN_FIG) $(DTS_FILES)
-	for i in $(DTS_FILES); do \
+$(DRVGEN_FILE_LIST): $(DRVGEN_TOOL) $(DWS_FILE) $(DRVGEN_FIG) $(PROJ_DTS_FILES)
+	for i in $(PROJ_DTS_FILES); do \
 		base_prj=`grep -m 1 "#include [<\"].*\/cust\.dtsi[>\"]" $$i | sed 's/#include [<"]//g' | sed 's/\/cust\.dtsi[>"]//g'`;\
 		prj_path=$(DRVGEN_OUT)/$$base_prj ;\
 		dws_path=$(srctree)/$(DRVGEN_PATH)/$$base_prj.dws ;\
@@ -82,13 +85,16 @@ endif
 ifeq ($(strip $(CONFIG_MTK_DTBO_FEATURE)), y)
 
 DTB_OVERLAY_IMAGE_TAGERT := $(DRVGEN_OUT)/odmdtbo.img
-$(DTB_OVERLAY_IMAGE_TAGERT) : PRIVATE_DTB_OVERLAY_OBJ:=$(DTB_FILES)
+$(DTB_OVERLAY_IMAGE_TAGERT) : PRIVATE_DTB_OVERLAY_OBJ:=$(PROJ_DTB_FILES)
+$(DTB_OVERLAY_IMAGE_TAGERT) : PRIVATE_MAIN_DTB_OVERLAY_OBJ:=$(MAIN_DTB_FILES)
 $(DTB_OVERLAY_IMAGE_TAGERT) : PRIVATE_MULTIPLE_DTB_OVERLAY_OBJ:=$(DRVGEN_OUT)/$(MTK_PROJECT).mdtb
 $(DTB_OVERLAY_IMAGE_TAGERT) : PRIVATE_DTB_OVERLAY_HDR:=$(srctree)/scripts/odmdtbo.cfg
+$(DTB_OVERLAY_IMAGE_TAGERT) : PRIVATE_MULTIPLE_DTB_OVERLAY_APPLY:=$(srctree)/scripts/dtc/ufdt_apply_overlay
 $(DTB_OVERLAY_IMAGE_TAGERT) : PRIVATE_MKIMAGE_TOOL:=$(srctree)/scripts/mkimage
 $(DTB_OVERLAY_IMAGE_TAGERT) : $(PRIVATE_MULTIPLE_DTB_OVERLAY_OBJ) dtbs $(PRIVATE_DTB_OVERLAY_HDR) | $(PRIVATE_MKIMAGE_TOOL)
 	@echo Singing the generated overlay dtbo.
 	cat $(PRIVATE_DTB_OVERLAY_OBJ) > $(PRIVATE_MULTIPLE_DTB_OVERLAY_OBJ) || (rm -f $(PRIVATE_MULTIPLE_DTB_OVERLAY_OBJ); false)
+	$(PRIVATE_MULTIPLE_DTB_OVERLAY_APPLY) $(PRIVATE_MAIN_DTB_OVERLAY_OBJ) $(PRIVATE_DTB_OVERLAY_OBJ) $(PRIVATE_DTB_OVERLAY_OBJ).merge
 	$(PRIVATE_MKIMAGE_TOOL) $(PRIVATE_MULTIPLE_DTB_OVERLAY_OBJ) $(PRIVATE_DTB_OVERLAY_HDR)  > $@
 .PHONY: odmdtboimage
 odmdtboimage : $(DTB_OVERLAY_IMAGE_TAGERT) dtbs
