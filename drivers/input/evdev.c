6673e89866c8b7522ffe743b4c6f131969091aa0
Miles Chen <miles.chen@mediatek.com>
2019-02-28 04:06:23 +0800

    [ALPS04222717] input: use vzalloc()
    OOM panic in evdev_open(). We found this in arm32 kernel.
    There are two issues here: normal zone memory fragmentation and
    heavy normal zone memory usage.
    
    Use vzalloc() instead kzalloc() to solve allocation fail
    due to memory fragmentation. vzalloc() uses highmem memory instead
    of normal memory to reduce memory pressure in normal zone.
    
    MTK-Commit-Id: cfe741a826f9f10db959f82fc3c600aa483a3270
    
    Change-Id: Iedc5b74fccdc02a4461fd948e381ef4f2085f1d2
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS04222717
    Feature: [Module]Kernel Memory Management
    (cherry-pick from b5cdb1ce126b0cacc2c03618a81e8a8fdbdf8d2a)

--- drivers/input/evdev.c
+++ drivers/input/evdev.c
@@ -502,9 +502,7 @@ static int evdev_open(struct inode *inode, struct file *file)
 	struct evdev_client *client;
 	int error;
 
-	client = kzalloc(size, GFP_KERNEL | __GFP_NOWARN);
-	if (!client)
-		client = vzalloc(size);
+	client = vzalloc(size);
 	if (!client)
 		return -ENOMEM;
 
