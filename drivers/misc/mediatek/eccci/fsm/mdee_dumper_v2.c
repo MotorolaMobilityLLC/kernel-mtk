263e89c490700f94f2e78e44c04a4b195ff1ab4c
guanglai guo <guanglai.guo@mediatek.com>
2019-02-28 04:03:43 +0800

    [ALPS04013498] CCCI: fix KE issue about NULL pointer
    parameter detail of aed_md_exception_api should not be
    assigned NULL pointer, if so, then KE happens.
    so add fixed string to fill detail when checked detail will
    be assigned NULL pointer.
    
    MTK-Commit-Id: 47928db49ad2363b29009dda4fd4dff844d5c723
    
    Change-Id: I88f689a0532f816dc8b132d841c47376bb8348a8
    Signed-off-by: guanglai guo <guanglai.guo@mediatek.com>
    CR-Id: ALPS04013498
    Feature: Modem Interface Driver

--- drivers/misc/mediatek/eccci/fsm/mdee_dumper_v2.c
+++ drivers/misc/mediatek/eccci/fsm/mdee_dumper_v2.c
@@ -40,6 +40,7 @@ static void ccci_aed_v2(struct ccci_fsm_ee *mdee, unsigned int dump_flag,
 	int md_img_len = 0;
 	int info_str_len = 0;
 	char *buff;		/*[AED_STR_LEN]; */
+	char buf_fail[] = "Fail alloc mem for exception\n";
 	char *img_inf;
 	struct mdee_dumper_v2 *dumper = mdee->dumper_obj;
 	int md_id = mdee->md_id;
@@ -90,15 +91,26 @@ static void ccci_aed_v2(struct ccci_fsm_ee *mdee, unsigned int dump_flag,
 		md_img_addr = (void *)mem_layout->md_bank0.base_ap_view_vir;
 		md_img_len = MD_IMG_DUMP_SIZE;
 	}
+	if (buff == NULL) {
 #if defined(CONFIG_MTK_AEE_FEATURE)
-	if (md_dbg_dump_flag & (1 << MD_DBG_DUMP_SMEM))
-		aed_md_exception_api(ex_log_addr, ex_log_len,
-			md_img_addr, md_img_len, buff, db_opt);
-	else
-		aed_md_exception_api(NULL, 0, md_img_addr, md_img_len,
-			buff, db_opt);
+		if (md_dbg_dump_flag & (1 << MD_DBG_DUMP_SMEM))
+			aed_md_exception_api(ex_log_addr, ex_log_len,
+				md_img_addr, md_img_len, buf_fail, db_opt);
+		else
+			aed_md_exception_api(NULL, 0, md_img_addr, md_img_len,
+				buf_fail, db_opt);
 #endif
-	kfree(buff);
+	} else {
+#if defined(CONFIG_MTK_AEE_FEATURE)
+		if (md_dbg_dump_flag & (1 << MD_DBG_DUMP_SMEM))
+			aed_md_exception_api(ex_log_addr, ex_log_len,
+				md_img_addr, md_img_len, buff, db_opt);
+		else
+			aed_md_exception_api(NULL, 0, md_img_addr, md_img_len,
+				buff, db_opt);
+#endif
+		kfree(buff);
+	}
 }
 
 static void mdee_output_debug_info_to_buf(struct ccci_fsm_ee *mdee,
