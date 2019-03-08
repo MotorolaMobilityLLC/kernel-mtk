9cc139466c5c72ba5b4f0bca2332573c48e31a5f
weiweij <weiweij@motorola.com>
2019-03-08 15:55:26 +0800

    driver/charger: add moto_chg_tcmd driver file
    Adding this driver is for moto chg tcmd's kernel interface.
    By this driver, all chg tcmd's kernel file point would be just
    in the folder of
    /sys/devices/platform/moto_chg_tcmd/
    So after then, tcmd app do not  need to change file access folder's name.
    It will more convinient for all MTK projects with different charger IC.
    
    Change-Id: I7d0346ab6a4ac56017083015eb21a5c1a3b08935
    Signed-off-by: weiweij <weiweij@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1317896
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

---
+++ include/linux/power/moto_chg_tcmd.h
@@ -0,0 +1,39 @@
+#ifndef __MOTO_CHG_TCMD_H__
+#define __MOTO_CHG_TCMD_H__
+
+enum MOTO_CHG_TCMD_CLIENT_ID {
+	MOTO_CHG_TCMD_CLIENT_CHG,
+	MOTO_CHG_TCMD_CLIENT_BAT,
+	MOTO_CHG_TCMD_ClIENT_MAX
+};
+
+struct moto_chg_tcmd_client {
+	void *data;
+	int client_id;
+
+#ifdef USE_LIST_HEAD
+	struct list_head list;
+#endif
+
+	int (*reg_read)(void *input, unsigned char reg, unsigned char *val);//hex
+	int (*reg_write)(void *input, unsigned char reg, unsigned char val);//hex
+
+	int (*get_chg_current)(void *input, int* val);//unit mA
+	int (*set_chg_current)(void *input, int val);//unit mA
+	int (*set_chg_enable)(void *input, int val);
+
+	int (*get_usb_current)(void *input, int* val);//unit mA
+	int (*set_usb_current)(void *input, int val);//unit mA
+	int (*set_usb_enable)(void *input, int val);
+
+	int (*get_usb_voltage)(void *input, int* val);//unit mV
+
+	int (*get_bat_temp)(void *input, int* val);//unit C
+	int (*get_bat_voltage)(void *input, int* val);//unit mV
+	int (*get_bat_ocv)(void *input, int* val);//unit mV
+	int (*get_bat_id)(void *input, int* val);
+};
+
+int moto_chg_tcmd_register(struct moto_chg_tcmd_client *client);
+
+#endif
