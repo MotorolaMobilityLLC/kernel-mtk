d59e638a1e842ba1e742febb7dfc033d8367d539
Miles Chen <miles.chen@mediatek.com>
2019-03-15 11:09:37 +0800

    [ALPS04314047] DISP: fix build break when CONFIG_PM_AUTOSLEEP=n
    Fix the following buidl break when CONFIG_PM_AUTOSLEEP=n
    
    ../drivers/misc/mediatek/video/mt6761/videox/mtkfb.c:2840:13:
    error: unused function mtkfb_late_resume [-Werror,-Wunused-function]
    static void mtkfb_late_resume(void)
    ../drivers/misc/mediatek/video/mt6761/videox/mtkfb.c:2820:13:
    error: unused function mtkfb_early_suspend [-Werror,-Wunused-function]
    static void mtkfb_early_suspend(void)
    
    MTK-Commit-Id: 2b7f168481209254fbb91d6682937099ee02b14f
    
    Change-Id: I585bbf12f442cc213ecfd567e785765654c4b1b1
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS04314047
    Feature: [Module]Kernel Memory Management

--- drivers/misc/mediatek/video/mt6765/videox/mtkfb.c
+++ drivers/misc/mediatek/video/mt6765/videox/mtkfb.c
@@ -194,8 +194,10 @@ static int mtkfb_get_overlay_layer_info(
 #ifdef CONFIG_OF
 static int _parse_tag_videolfb(void);
 #endif
+#if defined(CONFIG_PM_AUTOSLEEP)
 static void mtkfb_late_resume(void);
 static void mtkfb_early_suspend(void);
+#endif
 
 
 void mtkfb_log_enable(int enable)
@@ -2817,6 +2819,7 @@ void mtkfb_clear_lcm(void)
 {
 }
 
+#if defined(CONFIG_PM_AUTOSLEEP)
 static void mtkfb_early_suspend(void)
 {
 	int ret = 0;
@@ -2856,6 +2859,7 @@ static void mtkfb_late_resume(void)
 	DISPMSG("[FB Driver] leave late_resume\n");
 
 }
+#endif
 
 /*---------------------------------------------------------------------------*/
 #ifdef CONFIG_PM
