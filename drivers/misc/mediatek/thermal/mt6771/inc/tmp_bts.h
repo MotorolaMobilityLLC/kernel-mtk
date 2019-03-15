de18bd63921564b8747bfd98c89e61b54da6a98f
gengpeijun <wtts11@motorola.com>
2019-03-15 22:00:40 +0800

    thermal/kernel: enable charger_thermal
    thermal/kernel:
    choose AUX_IN2_NTC channel to enable charger_thermal
    
    Change-Id: I1122d672faae17136a83fe858691bb815121e50a
    Signed-off-by: gengpeijun <wtts11@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1321557
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: chen <wtts2@motorola.com>
    Reviewed-by: Lu Chai <chailu1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1322048
    Reviewed-by: Yongbin Chen <chenyb1@motorola.com>
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>

--- drivers/misc/mediatek/thermal/mt6771/inc/tmp_bts.h
+++ drivers/misc/mediatek/thermal/mt6771/inc/tmp_bts.h
@@ -20,6 +20,7 @@
 
 #define AUX_IN0_NTC (0)
 #define AUX_IN1_NTC (1)
+#define AUX_IN2_NTC (2)
 
 #define BTS_RAP_PULL_UP_R				390000		/* 390K,pull up resister */
 #define BTS_TAP_OVER_CRITICAL_LOW		4397119		/* base on 100K NTC temp default value -40 deg */
@@ -32,6 +33,12 @@
 #define BTSMDPA_RAP_PULL_UP_VOLTAGE		1800		/* 1.8V ,pull up voltage */
 #define BTSMDPA_RAP_NTC_TABLE			7			/* default is NCP15WF104F03RC(100K) */
 #define BTSMDPA_RAP_ADC_CHANNEL			AUX_IN1_NTC	/* default is 1 */
+
+#define BTSCHARGER_RAP_PULL_UP_R			390000		/* 390K,pull up resister */
+#define BTSCHARGER_TAP_OVER_CRITICAL_LOW	4397119		/* base on 100K NTC temp default value -40 deg */
+#define BTSCHARGER_RAP_PULL_UP_VOLTAGE		1800		/* 1.8V ,pull up voltage */
+#define BTSCHARGER_RAP_NTC_TABLE			7			/* default is NCP15WF104F03RC(100K) */
+#define BTSCHARGER_RAP_ADC_CHANNEL			AUX_IN2_NTC	/* default is 2 */
 
 extern int IMM_GetOneChannelValue(int dwChannel, int data[4], int *rawdata);
 extern int IMM_IsAdcInitReady(void);
