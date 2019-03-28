474732369ac799cdd91ac5c7b2b235ffde418f7c
Miles Chen <miles.chen@mediatek.com>
2019-03-28 11:50:44 +0800

    [ALPS04332181] mm/page_owner: fix a mismerge
    While applying "UPSTREAM: mm/page_owner: clamp read count to PAGE_SIZE",
    there is a mistake. The change should be in print_page_owner(),
    not in read_page_owner_slim().
    
    MTK-Commit-Id: c947b872f89f8d5a84bf3d5b0c7899e85c81c517
    
    Change-Id: I758e570b07b09df35e0d5516fb05f01d26318d78
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS04332181
    Feature: [Module]Kernel Memory Management

--- mm/page_owner.c
+++ mm/page_owner.c
@@ -146,7 +146,6 @@ read_page_owner_slim(struct file *file, char __user *buf, size_t count,
 	if (index >= HANDLE_ENTRIES)
 		return 0;
 
-	count = min_t(size_t, count, PAGE_SIZE);
 	kbuf = kmalloc(count, GFP_KERNEL);
 	if (!kbuf)
 		return -ENOMEM;
@@ -589,6 +588,7 @@ print_page_owner(char __user *buf, size_t count, unsigned long pfn,
 	};
 
 
+	count = min_t(size_t, count, PAGE_SIZE);
 	kbuf = kmalloc(count, GFP_KERNEL);
 	if (!kbuf)
 		return -ENOMEM;
