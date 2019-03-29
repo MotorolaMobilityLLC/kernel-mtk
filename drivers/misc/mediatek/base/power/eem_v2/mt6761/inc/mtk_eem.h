bdf2a09c90fcf14ee7000d66ed2f5d86abf3bf39
Angus Lin <angus.lin@mediatek.com>
2019-03-29 13:08:07 +0800

    [ALPS04328372] EEM: Fix compile error
    [Detail]
    Fix compile error
    
    MTK-Commit-Id: ab14caef5477524d0df8bca5d00e249889fe9143
    
    Change-Id: I20228959a18ccbe9c17ff9957335eaf0fbd56efc
    Signed-off-by: Angus Lin <angus.lin@mediatek.com>
    CR-Id: ALPS04328372
    Feature: PTP-OD (Performance Thermal Power-Over Drive)

--- drivers/misc/mediatek/base/power/eem_v2/mt6761/inc/mtk_eem.h
+++ drivers/misc/mediatek/base/power/eem_v2/mt6761/inc/mtk_eem.h
@@ -195,7 +195,7 @@ extern int mt_eem_status(enum eem_det_id id);
 extern unsigned int get_efuse_status(void);
 extern unsigned int mt_eem_is_enabled(void);
 
-extern void eem_set_pi_efuse(enum eem_ctrl_id id, unsigned int pi_efuse);
+extern void eem_set_pi_efuse(enum eem_det_id id, unsigned int pi_efuse);
 
 /* DRCC */
 extern unsigned int drcc_offset_done;
