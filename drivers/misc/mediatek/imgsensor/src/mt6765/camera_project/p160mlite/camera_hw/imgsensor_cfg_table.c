9677ae80be820832b3f4a5b3859b4cc144f3d396
dashao.su <dashao.su@tinno.com>
2019-04-11 11:39:03 +0800

    <FUNC><YXXJAPK-207><camera:func:Modify camera SP2509V power-on and power-down sequence.>
    Change-Id: I08a7125394b9e3165a857a87e693a8527b7ec184

--- drivers/misc/mediatek/imgsensor/src/mt6765/camera_project/p160mlite/camera_hw/imgsensor_cfg_table.c
+++ drivers/misc/mediatek/imgsensor/src/mt6765/camera_project/p160mlite/camera_hw/imgsensor_cfg_table.c
@@ -182,13 +182,13 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
 	{
 		SENSOR_DRVNAME_SP2509V_SUNWIN_P161BN_MIPI_RAW,
 		{
-                        {PDN, Vol_High, 0},
-                        {RST, Vol_Low, 0},
-                        {DOVDD, Vol_1800, 5},
-                        {AVDD, Vol_High, 5},
-                        {SensorMCLK, Vol_High, 1},
-                        {PDN, Vol_Low, 10},
-                        {RST, Vol_High, 20}
+                        {PDN, Vol_High, 0, Vol_High, 0},
+                        {RST, Vol_Low, 0, Vol_Low, 0},
+                        {DOVDD, Vol_1800, 5, Vol_Low, 0},
+                        {AVDD, Vol_High, 5, Vol_Low, 0},
+                        {SensorMCLK, Vol_High, 1, Vol_Low, 0},
+                        {PDN, Vol_Low, 10, Vol_High,0},
+                        {RST, Vol_High, 20, Vol_Low, 0}
 		},
 	},
 #endif
