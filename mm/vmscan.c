20bead4bb7b415f96300251c0e57ea4666c00c0a
Chinwen Chang <chinwen.chang@mediatek.com>
2019-04-13 16:37:03 +0800

    [ALPS04323145] mm: remove needless code and enlarge swappiness for gmo
    This patch includes the following modifications,
         1. Remove needless flow in memory reclaim path.
         2. Enlarge the upper bound of swappiness for GMO projects.
    
    MTK-Commit-Id: 243631d5511394e224c8dab2014533c2b0844390
    
    Change-Id: Ife304799a62fa32cd10523082fdc8e62cbb95b4f
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>
    CR-Id: ALPS04323145
    Feature: Memory Optimization

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2094,54 +2094,6 @@ static unsigned long shrink_list(enum lru_list lru, unsigned long nr_to_scan,
 	return shrink_inactive_list(nr_to_scan, lruvec, sc, lru);
 }
 
-#ifdef CONFIG_MTK_GMO_RAM_OPTIMIZE
-/* threshold of swapin and out */
-static unsigned int swpinout_threshold = 12000;
-module_param_named(threshold, swpinout_threshold, uint, 0644);
-static bool swap_is_allowed(void)
-{
-	static unsigned long prev_time, last_thrashing_time;
-	static unsigned long prev_swpinout;
-	static bool no_thrashing = true;
-	int cpu;
-	unsigned long swpinout = 0;
-
-	if (prev_time == 0)
-		prev_time = jiffies;
-
-	/* take 1s break */
-	if (!no_thrashing && time_before(jiffies, last_thrashing_time + HZ))
-		return false;
-
-	/* detect at 8Hz */
-	if (time_after(jiffies, prev_time + (HZ >> 3))) {
-		for_each_online_cpu(cpu) {
-			struct vm_event_state *this =
-					&per_cpu(vm_event_states, cpu);
-
-			swpinout += this->event[PSWPIN] + this->event[PSWPOUT];
-		}
-
-		if (((swpinout - prev_swpinout) * HZ /
-		    (jiffies - prev_time + 1)) > swpinout_threshold) {
-			last_thrashing_time = jiffies;
-			no_thrashing = false;
-		} else {
-			no_thrashing = true;
-		}
-
-		prev_swpinout = swpinout;
-		prev_time = jiffies;
-	}
-
-	/* Only kswapd is allowed to do more jobs */
-	if (!current_is_kswapd())
-		return false;
-
-	return no_thrashing;
-}
-#endif
-
 enum scan_balance {
 	SCAN_EQUAL,
 	SCAN_FRACT,
@@ -2376,121 +2328,6 @@ out:
 	}
 }
 
-#ifdef CONFIG_ZONE_MOVABLE_CMA
-/*
- * Migration callback that allocates free pages from movable zone.
- */
-static struct page *alloc_movable_target(struct page *page,
-		unsigned long private, int **resultp)
-{
-	struct page *newpage;
-
-	newpage = alloc_page(__GFP_HIGHMEM | __GFP_MOVABLE | __GFP_NORETRY |
-			__GFP_NOWARN);
-
-	if (newpage != NULL &&
-			zone_idx(page_zone(newpage)) < OPT_ZONE_MOVABLE_CMA) {
-		if (put_page_testzero(newpage))
-			free_hot_cold_page(newpage, true);
-		else
-			WARN_ON(1);
-
-		return NULL;
-	}
-
-	return newpage;
-}
-#endif /* CONFIG_ZONE_MOVABLE_CMA */
-
-/*
- * Migrate pages of anon LRU to movable zone.
- */
-static unsigned long migrate_lru_pages(unsigned long *nr, enum lru_list lru,
-		struct lruvec *lruvec, struct scan_control *sc)
-{
-#ifdef CONFIG_ZONE_MOVABLE_CMA
-	LIST_HEAD(page_list);
-	struct pglist_data *pgdat = lruvec_pgdat(lruvec);
-	struct zone *src_zone;
-	unsigned long nr_to_scan = SWAP_CLUSTER_MAX;
-	unsigned long nr_scanned, nr_taken;
-	int err;
-	unsigned long flags;
-
-	/* Check whether ZONE_MOVABLE_CMA zone is empty */
-	if (global_page_state(NR_FREE_CMA_PAGES) == 0)
-		return 0;
-
-	/* Migrate active anon LRU when inactive < active */
-	src_zone = &pgdat->node_zones[sc->reclaim_idx];
-	if (zone_page_state(src_zone, NR_ZONE_INACTIVE_ANON) <
-			zone_page_state(src_zone, NR_ZONE_ACTIVE_ANON))
-		lru = LRU_ACTIVE_ANON;
-
-	/* If no available swap space, double the number of page migration */
-	if (get_nr_swap_pages() == 0)
-		nr_to_scan = SWAP_CLUSTER_MAX << 1;
-
-	nr_to_scan = min(zone_page_state(src_zone, NR_ZONE_LRU_BASE + lru),
-			nr_to_scan);
-
-	/* No pages for scanning */
-	if (nr_to_scan == 0)
-		return 0;
-
-	/* Update nr[lru] to avoid needless shrinking */
-	nr[lru] -= min(nr[lru], nr_to_scan);
-
-	/* Start isolation & migration */
-	spin_lock_irq(&pgdat->lru_lock);
-	nr_taken = isolate_lru_pages(nr_to_scan, lruvec, &page_list,
-			&nr_scanned, sc, 0, lru);
-
-	__mod_node_page_state(pgdat, NR_ISOLATED_ANON, nr_taken);
-	spin_unlock_irq(&pgdat->lru_lock);
-
-	err = 0;
-	if (!list_empty(&page_list)) {
-		/*
-		 * This can help reduce memory fragmentation,
-		 * so view it as MR_COMPACTION.
-		 */
-		err = migrate_pages(&page_list, alloc_movable_target,
-				NULL, 0, MIGRATE_SYNC, MR_COMPACTION);
-		if (err)
-			putback_movable_pages(&page_list);
-	}
-
-	/* Suppose no pages were migrated when we got -ENOMEM */
-	if (err == -ENOMEM) {
-		count_vm_event(ZMC_LRU_MIGRATION_NOMEM);
-		return 0;
-	}
-
-	/* Update the number of page migrated(approx. by nr_taken) */
-	if (nr_taken > 0) {
-		local_irq_save(flags);
-		__count_vm_events(ZMC_LRU_MIGRATED, nr_taken);
-		local_irq_restore(flags);
-	}
-
-	return (nr_taken - err);
-#else
-	/* do nothing */
-	return 0;
-#endif /* CONFIG_ZONE_MOVABLE_CMA */
-}
-
-#ifdef CONFIG_MTK_GMO_RAM_OPTIMIZE
-/*
- * When the length of inactive lru is smaller than 256(SWAP_CLUSTER_MAX << 3),
- * there is high risk to suffer from congestion wait.
- * For low-ram device, this value is suggested to be higher than 4 to keep away
- * from above situation while we have smaller sc->priority.
- */
-static int scan_anon_priority = 4;
-module_param_named(scan_anon_prio, scan_anon_priority, int, 0644);
-#endif
 /*
  * This is a basic per-node page freer.  Used by both kswapd and direct reclaim.
  */
@@ -2512,34 +2349,6 @@ static void shrink_node_memcg(struct pglist_data *pgdat, struct mem_cgroup *memc
 	/* Record the original scan target for proportional adjustments later */
 	memcpy(targets, nr, sizeof(nr));
 
-#ifdef CONFIG_MTK_GMO_RAM_OPTIMIZE
-	/*
-	 * sc->priority: 12, 11, 10,  9
-	 * (4)    shift:  4,  3,  2,  1
-	 *           nr:  2,  4,  8, 16
-	 * (5)    shift:  5,  4,  3,  2
-	 *           nr:  1,  2,  4,  8
-	 * (3)    shift:  3,  2,  1,  0
-	 *           nr:  4,  8, 16, 32
-	 */
-	if (swap_is_allowed() && sc->priority > 8 &&
-			nr[LRU_INACTIVE_ANON] == 0) {
-		int shift = scan_anon_priority - DEF_PRIORITY + sc->priority;
-
-		if (shift >= 0)
-			nr[LRU_INACTIVE_ANON] = SWAP_CLUSTER_MAX >> shift;
-		else
-			nr[LRU_INACTIVE_ANON] = 1;
-
-		nr[LRU_ACTIVE_ANON] = nr[LRU_INACTIVE_ANON];
-	}
-#else
-	/* Give a chance to migrate anon pages */
-	if (IS_ENABLED(CONFIG_MTK_MEMORY_LOWPOWER) &&
-			nr[LRU_INACTIVE_ANON] == 0)
-		nr[LRU_INACTIVE_ANON] = 1;
-#endif
-
 	/*
 	 * Global reclaiming within direct reclaim at DEF_PRIORITY is a normal
 	 * event that can occur when there is little memory pressure e.g.
@@ -2559,19 +2368,6 @@ static void shrink_node_memcg(struct pglist_data *pgdat, struct mem_cgroup *memc
 					nr[LRU_INACTIVE_FILE]) {
 		unsigned long nr_anon, nr_file, percentage;
 		unsigned long nr_scanned;
-
-		/*
-		 * Before shrinking inactive anon lru,
-		 * let us try to do migration.
-		 */
-		if (sc->reclaim_idx < OPT_ZONE_MOVABLE_CMA)
-			nr_reclaimed += migrate_lru_pages(nr,
-					LRU_INACTIVE_ANON, lruvec, sc);
-
-		/* Clear the number of anon lru for scanning to 0 earlier */
-		if (IS_ENABLED(CONFIG_ZONE_MOVABLE_CMA) &&
-				get_nr_swap_pages() == 0)
-			nr[LRU_INACTIVE_ANON] = nr[LRU_ACTIVE_ANON] = 0;
 
 		for_each_evictable_lru(lru) {
 			if (nr[lru]) {
