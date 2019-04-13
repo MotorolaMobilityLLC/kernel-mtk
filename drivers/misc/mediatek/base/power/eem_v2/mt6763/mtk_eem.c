a615f06e03bc1ef6170464759b0388cc1e091e96
Angus Lin <angus.lin@mediatek.com>
2019-04-13 16:44:55 +0800

    [ALPS04239425] EEM: Fix build error
    [Detail]
    Fix build error
    
    MTK-Commit-Id: 03bc841cdbf5bc2fe5c6a4fdf4dcea3b2f9bc20a
    
    Change-Id: I181d641799efcb8d8aec04fad952084533e7bfe6
    Signed-off-by: Angus Lin <angus.lin@mediatek.com>
    CR-Id: ALPS04239425
    Feature: PTP-OD (Performance Thermal Power-Over Drive)

--- drivers/misc/mediatek/base/power/eem_v2/mt6763/mtk_eem.c
+++ drivers/misc/mediatek/base/power/eem_v2/mt6763/mtk_eem.c
@@ -66,7 +66,6 @@
 /* local includes (kernel-4.4)*/
 #ifdef __KERNEL__
 	#include <mt-plat/mtk_chip.h>
-	#include <mt-plat/mt_gpio.h>
 	#include "upmu_common.h"
 	#include "mach/mtk_freqhopping.h"
 	#include "mtk_thermal.h"
