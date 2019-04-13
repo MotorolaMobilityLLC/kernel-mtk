a386a6c43bd70b111bb80af1becc9bacc95f6598
Damon Chu <damon.chu@mediatek.com>
2019-04-13 16:46:30 +0800

    [ALPS04345549] mmdvfs: add mt6763 support
    1. Add basic mt6763 porting
    2. Add scenario mechanism
    
    MTK-Commit-Id: e15037ec47815417d84a5b9e3af34a939aece176
    
    Change-Id: Ied21743ae7c6ca0fbb618ca9ab31cd319d29453e
    Feature:[Module]MM pipeline/MM DVFS
    CR-Id:ALPS04345549
    Signed-off-by: Damon Chu <damon.chu@mediatek.com>

--- drivers/misc/mediatek/smi/mmdvfs_config_mt6763.h
+++ drivers/misc/mediatek/smi/mmdvfs_config_mt6763.h
@@ -15,7 +15,7 @@
 #define __MMDVFS_CONFIG_MT6763_H__
 
 #include "mmdvfs_config_util.h"
-#include "mtk_vcorefs_manager.h"
+#include <mtk_vcorefs_manager.h>
 
 /* Part I MMSVFS HW Configuration (OPP)*/
 /* Define the number of mmdvfs, vcore and mm clks opps */
@@ -48,7 +48,7 @@ struct mmdvfs_clk_source_desc
  */
 /* Field decscription: */
 /* 1. config_method: MMDVFS_CLK_CONFIG_BY_MUX/ MMDVFS_CLK_CONFIG_PLL /
- * MMDVFS_CLK_CONFIG_NONE
+ * MMDVFS_CLK_NONE
  */
 /* 2. pll_id: MTK' PLL ID, please set -1 if PLL hopping is not used */
 /* 3. clk mux desc {hanlde, name}, the please handle -1 and it will be
@@ -59,61 +59,61 @@ struct mmdvfs_clk_source_desc
 /* 6. clk sources id of each steps: please set -1 if it is not used */
 struct mmdvfs_clk_hw_map
 	mt6763_mmdvfs_clk_hw_map_setting[MMDVFS_CLK_MUX_NUM] = {
-	  {MMDVFS_CLK_CONFIG_NONE,
+	  {MMDVFS_CLK_NONE,
 	  {NULL, "MMDVFS_CLK_MUX_TOP_SMI0_2X_SEL"},
 	  -1,
 	  2,
 	  {-1, -1},
 	  {-1, -1} },
-	 {MMDVFS_CLK_CONFIG_PLL_RATE,
+	 {MMDVFS_CLK_PLL_RATE,
 	  {NULL, "MMDVFS_CLK_TOP_MMPLL_CK"},
 	  9,
 	  2,
 	  {0x00204EC4, 0x001713B1},
 	  {-1, -1} },
-	 {MMDVFS_CLK_CONFIG_BY_MUX,
+	 {MMDVFS_CLK_BY_MUX,
 	  {NULL, "MMDVFS_CLK_MUX_TOP_CAM_SEL"},
 	  -1,
 	  2,
 	  {-1, -1},
 	  {MT6763_MMDVFS_CLK_TOP_TVDPLL_CK, MT6763_MMDVFS_CLK_TOP_MMPLL_CK} },
-	 {MMDVFS_CLK_CONFIG_NONE,
+	 {MMDVFS_CLK_NONE,
 	  {NULL, "MMDVFS_CLK_MUX_TOP_IMG_SEL"},
 	  -1,
 	  2,
 	  {-1, -1},
 	  {-1, -1} },
-	 {MMDVFS_CLK_CONFIG_NONE,
+	 {MMDVFS_CLK_NONE,
 	  {NULL, "MMDVFS_CLK_MUX_TOP_VENC_SEL"},
 	  -1,
 	  2,
 	  {-1, -1},
 	  {-1, -1} },
-	 {MMDVFS_CLK_CONFIG_NONE,
+	 {MMDVFS_CLK_NONE,
 	  {NULL, "MMDVFS_CLK_MUX_TOP_VDEC_SEL"},
 	  -1,
 	  2,
 	  {-1, -1},
 	  {-1, -1} },
-	 {MMDVFS_CLK_CONFIG_NONE,
+	 {MMDVFS_CLK_NONE,
 	  {NULL, "MMDVFS_CLK_MUX_TOP_MJC_SEL"},
 	  -1,
 	  2,
 	  {-1, -1},
 	  {-1, -1} },
-	 {MMDVFS_CLK_CONFIG_NONE,
+	 {MMDVFS_CLK_NONE,
 	  {NULL, "MMDVFS_CLK_MUX_TOP_VPU_IF_SEL"},
 	  -1,
 	  2,
 	  {-1, -1},
 	  {-1, -1} },
-	 {MMDVFS_CLK_CONFIG_NONE,
+	 {MMDVFS_CLK_NONE,
 	  {NULL, "MMDVFS_CLK_MUX_TOP_VPU_IF_SEL"},
 	  -1,
 	  2,
 	  {-1, -1},
 	  {-1, -1} },
-	 {MMDVFS_CLK_CONFIG_NONE,
+	 {MMDVFS_CLK_NONE,
 	  {NULL, "MMDVFS_CLK_MUX_TOP_VPU_SEL"},
 	  -1,
 	  2,
@@ -459,7 +459,7 @@ struct mmdvfs_profile
 /* 1. For a single mmdvfs step's profiles and associated hardware configuration
  */
 /* LP4 2-ch */
-struct mmdvfs_step_to_profile_mapping
+struct mmdvfs_step_profile
 	mt6763_step_profile[MT6763_MMDVFS_OPP_MAX] = {
 	{0,
 	 mt6763_mmdvfs_opp0_profiles,
@@ -500,7 +500,7 @@ struct mmdvfs_step_to_profile_mapping
 };
 
 /* LP4 1-ch */
-struct mmdvfs_step_to_profile_mapping
+struct mmdvfs_step_profile
 	mt6763_step_profile_lp4_1ch[MT6763_MMDVFS_OPP_MAX] = {
 	{0,
 	 mt6763_mmdvfs_opp0_profiles_lp4_1ch,
@@ -541,7 +541,7 @@ struct mmdvfs_step_to_profile_mapping
 };
 
 /* LP3 */
-struct mmdvfs_step_to_profile_mapping
+struct mmdvfs_step_profile
 	mt6763_step_profile_lp3[MT6763_MMDVFS_OPP_MAX] = {
 	{0,
 	 mt6763_mmdvfs_opp0_profiles_lp3,
@@ -608,14 +608,14 @@ int mt6763_mmdvfs_mmclk_opp_to_legacy_mmclk_step[MT6763_MMDVFS_OPP_MAX] = {
 
 /* Part V ISP DVFS configuration */
 #define MMDVFS_ISP_THRESHOLD_NUM 2
-int mt6763_mmdvs_isp_threshold_setting[MMDVFS_ISP_THRESHOLD_NUM] = {450, 300};
-int mt6763_mmdvs_isp_threshold_opp[MMDVFS_ISP_THRESHOLD_NUM] = {
+int mt6763_isp_threshold_setting[MMDVFS_ISP_THRESHOLD_NUM] = {450, 300};
+int mt6763_isp_threshold_opp[MMDVFS_ISP_THRESHOLD_NUM] = {
 	MMDVFS_FINE_STEP_OPP0, MMDVFS_FINE_STEP_OPP3};
 
-struct mmdvfs_threshold_setting
-	mt6763_mmdvfs_threshold_settings[MMDVFS_PM_QOS_SUB_SYS_NUM] = {
-	{MMDVFS_PM_QOS_SUB_SYS_CAMERA, mt6763_mmdvs_isp_threshold_setting,
-	 mt6763_mmdvs_isp_threshold_opp, MMDVFS_ISP_THRESHOLD_NUM,
+struct mmdvfs_thres_setting
+	mt6763_thres_handler[MMDVFS_PM_QOS_SUB_SYS_NUM] = {
+	{MMDVFS_PM_QOS_SUB_SYS_CAMERA, mt6763_isp_threshold_setting,
+	 mt6763_isp_threshold_opp, MMDVFS_ISP_THRESHOLD_NUM,
 	 MMDVFS_PMQOS_ISP},
 };
 
