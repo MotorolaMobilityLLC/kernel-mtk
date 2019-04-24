98ced081242b77962d180ec8c738c64a589254e7
zhouwl <zhouwl@lenovo.com>
2019-04-24 20:03:41 +0800

    ASoC: regmap debugfs
    Use Kconfig to control REGMAP_ALLOW_WRITE_DEBUGFS switch,
    This is standard method in kernel.
    
    Change-Id: I390dcd9d2272c4915ac6b2ae5c4374c4d1e4d7f2
    Signed-off-by: zhouwl <zhouwl@mt.com>
    Reviewed-on: https://gerrit.mot.com/1341793
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/base/regmap/regmap-debugfs.c
+++ drivers/base/regmap/regmap-debugfs.c
@@ -259,8 +259,7 @@ static ssize_t regmap_map_read_file(struct file *file, char __user *user_buf,
 				   count, ppos);
 }
 
-#undef REGMAP_ALLOW_WRITE_DEBUGFS
-#ifdef REGMAP_ALLOW_WRITE_DEBUGFS
+#ifdef CONFIG_REGMAP_ALLOW_WRITE_DEBUGFS
 /*
  * This can be dangerous especially when we have clients such as
  * PMICs, therefore don't provide any real compile time configuration option
@@ -595,7 +594,7 @@ void regmap_debugfs_init(struct regmap *map, const char *name)
 	if (map->max_register || regmap_readable(map, 0)) {
 		umode_t registers_mode;
 
-#if defined(REGMAP_ALLOW_WRITE_DEBUGFS)
+#if defined(CONFIG_REGMAP_ALLOW_WRITE_DEBUGFS)
 		registers_mode = 0600;
 #else
 		registers_mode = 0400;
