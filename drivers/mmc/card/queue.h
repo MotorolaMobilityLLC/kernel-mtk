a3a97abe1f67db2d89face12baad7c53d5f50d81
Chun-Hung Wu <chun-hung.wu@mediatek.com>
2019-04-27 09:50:12 +0800

    [ALPS04399165]  msdc: Fix null pointer dereference
    [Detail]
    Fix mmc_queue.flags type to long.
    
    MTK-Commit-Id: d9566b1e112bd40fb16d5bd8e2c20ff496e0a07d
    
    Change-Id: I2e46df44a4d8c9c035082b669c7f5fb79b5bcf38
    Signed-off-by: Chun-Hung Wu <chun-hung.wu@mediatek.com>
    CR-Id: ALPS04399165
    Feature: eMMC Boot Up

--- drivers/mmc/card/queue.h
+++ drivers/mmc/card/queue.h
@@ -63,7 +63,7 @@ struct mmc_queue {
 	struct mmc_card		*card;
 	struct task_struct	*thread;
 	struct semaphore	thread_sem;
-	unsigned int		flags;
+	unsigned long		flags;
 #define MMC_QUEUE_SUSPENDED	(1 << 0)
 #define MMC_QUEUE_NEW_REQUEST	(1 << 1)
 	void			*data;
