39c4a7cd9d3be183a2f33d0a492716a6543f6aef
Brian-SY Yang <brian-sy.yang@mediatek.com>
2019-04-27 09:31:49 +0800

    [ALPS03700045] CM_MGR: fix build error
    fix build error for k63
    
    MTK-Commit-Id: 060d17100c67369debe44e4b089aa8401172f3d7
    
    Change-Id: I1f360e31d7ff530ba0fd0e2a41b20561bc5441e5
    Signed-off-by: Brian-SY Yang <brian-sy.yang@mediatek.com>
    CR-Id: ALPS03700045
    Feature: Power Management

--- drivers/misc/mediatek/fiq_cache/mtk-fiq-cache.c
+++ drivers/misc/mediatek/fiq_cache/mtk-fiq-cache.c
@@ -18,15 +18,29 @@
 
 unsigned long *aee_rr_rec_fiq_cache_step_pa(void);
 
+#ifdef CONFIG_MACH_MT6763
+DEFINE_SPINLOCK(cache_lock);
+#else
 static DEFINE_MUTEX(cache_mutex);
+#endif
 
 void mt_fiq_cache_flush_all(void)
 {
+#ifdef CONFIG_MACH_MT6763
+	unsigned long flags;
+
+	trace_printk("[FIQ_CACHE] starts\n");
+	spin_lock_irqsave(&cache_lock, flags);
+	mt_secure_call(MTK_SIP_KERNEL_CACHE_FLUSH_FIQ, 0, 0, 0, 0);
+	spin_unlock_irqrestore(&cache_lock, flags);
+	trace_printk("[FIQ_CACHE] done\n");
+#else
 	mutex_lock(&cache_mutex);
 	trace_printk("[FIQ_CACHE] starts\n");
 	mt_secure_call(MTK_SIP_KERNEL_CACHE_FLUSH_FIQ, 0, 0, 0, 0);
 	trace_printk("[FIQ_CACHE] done\n");
 	mutex_unlock(&cache_mutex);
+#endif
 }
 
 static int __init fiq_cache_init(void)
