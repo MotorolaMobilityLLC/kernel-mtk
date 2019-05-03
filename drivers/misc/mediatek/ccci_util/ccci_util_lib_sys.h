4b9e15f8beba4e1c13f6a09c0c9744da5b39cda1
guanglai guo <guanglai.guo@mediatek.com>
2019-05-03 08:49:44 +0800

    [ALPS04332698] CCCI: add sys node for pin value config
    add pincfg sys to set pin value, for simulation of RF cable
    connection or not.
    
    MTK-Commit-Id: 7c53351beb6af473aaf73cd21094e51e602ea130
    
    Change-Id: I63dc7915b183d35524f307d91dddf1dda2b7bc0c
    Signed-off-by: guanglai guo <guanglai.guo@mediatek.com>
    CR-Id: ALPS04332698
    Feature: Modem Interface Driver

--- drivers/misc/mediatek/ccci_util/ccci_util_lib_sys.h
+++ drivers/misc/mediatek/ccci_util/ccci_util_lib_sys.h
@@ -15,3 +15,4 @@
 extern unsigned int ccci_debug_enable;
 int get_dump_buf_usage(char buf[], int size);
 extern void spm_ap_mdsrc_req(unsigned char);
+extern void inject_pin_status_event(int pin_value, char pin_name[]);
