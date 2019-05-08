328a27ddb176d62368fd5166c7003e40a3ef263c
Haijian Ma <mahj8@motorola.com>
2019-05-08 16:16:56 +0800

    charger: remove power off action in kernel
    remove power off action in kernel
    when kernel power off charGing
    power off action must be implement in charGing only mode userspace
    
    Change-Id: If60d7499b0e72924d94cd866431f35658d1ef0fd
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1350126
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Lu Chai <chailu1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1847188

--- drivers/power/supply/mediatek/charger/mtk_chg_type_det.c
+++ drivers/power/supply/mediatek/charger/mtk_chg_type_det.c
@@ -158,6 +158,7 @@ struct mt_charger {
 	enum charger_type chg_type;
 };
 
+#ifdef MTK_BASE
 static int mt_charger_online(struct mt_charger *mtk_chg)
 {
 	int ret = 0;
@@ -177,6 +178,7 @@ static int mt_charger_online(struct mt_charger *mtk_chg)
 
 	return ret;
 }
+#endif
 
 /* Power Supply Functions */
 static int mt_charger_get_property(struct power_supply *psy,
@@ -239,7 +241,9 @@ static int mt_charger_set_property(struct power_supply *psy,
 	switch (psp) {
 	case POWER_SUPPLY_PROP_ONLINE:
 		mtk_chg->chg_online = val->intval;
+		#ifdef MTK_BASE
 		mt_charger_online(mtk_chg);
+		#endif
 		return 0;
 	case POWER_SUPPLY_PROP_CHARGE_TYPE:
 		mtk_chg->chg_type = val->intval;
