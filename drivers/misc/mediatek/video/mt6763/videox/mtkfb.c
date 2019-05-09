daa16590a8bfff5859c76acd41354191be329cb0
shitian.ma <shitian.ma@mediatek.com>
2019-05-09 16:24:03 +0800

    [ALPS04415908] DSI: fix build error when HWC set power module
    Should be remove CONFIG_PM_AUTOSLEEP option in framebuffer module
    
    MTK-Commit-Id: 53b8d048e7a971dc19256f61a41fec672ca8e65c
    
    Change-Id: Ia10c12b15de9da20ca81381bd6f212826d193867
    CR-Id: ALPS04415908
    Feature: Display Driver (DSI Interface)
    Signed-off-by: shitian.ma <shitian.ma@mediatek.com>

--- drivers/misc/mediatek/video/mt6763/videox/mtkfb.c
+++ drivers/misc/mediatek/video/mt6763/videox/mtkfb.c
@@ -216,10 +216,8 @@ mtkfb_get_overlay_layer_info(struct fb_overlay_layer_info *layerInfo);
 #ifdef CONFIG_OF
 static int _parse_tag_videolfb(void);
 #endif
-#if defined(CONFIG_PM_AUTOSLEEP)
 static void mtkfb_late_resume(void);
 static void mtkfb_early_suspend(void);
-#endif
 
 void mtkfb_log_enable(int enable)
 {
@@ -305,7 +303,6 @@ exit:
 	return r;
 }
 
-#if defined(CONFIG_PM_AUTOSLEEP)
 #if defined(CONFIG_MTK_DUAL_DISPLAY_SUPPORT) &&                               \
 	(CONFIG_MTK_DUAL_DISPLAY_SUPPORT == 2)
 static int mtkfb1_blank(int blank_mode, struct fb_info *info)
@@ -378,7 +375,7 @@ static int mtkfb_blank(int blank_mode, struct fb_info *info)
 
 	return 0;
 }
-#endif
+
 
 int mtkfb_set_backlight_level(unsigned int level)
 {
@@ -1927,9 +1924,7 @@ static struct fb_ops mtkfb_ops = {
 #ifdef CONFIG_COMPAT
 	.fb_compat_ioctl = mtkfb_compat_ioctl,
 #endif
-#if defined(CONFIG_PM_AUTOSLEEP)
 	.fb_blank = mtkfb_blank,
-#endif
 };
 
 #if defined(CONFIG_MTK_DUAL_DISPLAY_SUPPORT) &&                               \
@@ -1951,9 +1946,7 @@ static struct fb_ops mtkfb1_ops = {
 #ifdef CONFIG_COMPAT
 	.fb_compat_ioctl = NULL,
 #endif
-#if defined(CONFIG_PM_AUTOSLEEP)
 	.fb_blank = mtkfb1_blank,
-#endif
 };
 #endif
 /*
@@ -2798,7 +2791,6 @@ int mtkfb_ipo_init(void)
 
 void mtkfb_clear_lcm(void) {}
 
-#if defined(CONFIG_PM_AUTOSLEEP)
 static void mtkfb_early_suspend(void)
 {
 	int ret = 0;
@@ -2838,7 +2830,6 @@ static void mtkfb_late_resume(void)
 
 	DISPMSG("[FB Driver] leave late_resume\n");
 }
-#endif
 
 /*---------------------------------------------------------------------------*/
 #ifdef CONFIG_PM
