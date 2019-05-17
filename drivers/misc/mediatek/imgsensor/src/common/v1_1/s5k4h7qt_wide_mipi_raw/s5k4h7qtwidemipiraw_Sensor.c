b718d07fd2f74ab4c5fbddd7ff88b14aac54e022
xuzr3 <xuzr3@lenovo.com>
2019-05-17 09:16:37 +0800

    Limal: Bringup 2nd camera module
    Add config for 2nd camera module.
    s5k3l6qt/ov02aqt/s5k4h7qt/s5k4h7qt_wide.
    
    Change-Id: I38e7d0bca34814f44a123a20af4bf37fdbf7834b
    Signed-off-by: chenll4 <chenll4@motorola.com>

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/s5k4h7qt_wide_mipi_raw/s5k4h7qtwidemipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/s5k4h7qt_wide_mipi_raw/s5k4h7qtwidemipiraw_Sensor.c
@@ -285,6 +285,7 @@ static kal_uint32 streaming_control(kal_bool enable)
 static void write_shutter(kal_uint32 shutter)
 {
 	kal_uint16 realtime_fps = 0;
+	int i = 0;
 
 	spin_lock(&imgsensor_drv_lock);
 	if (shutter > imgsensor.min_frame_length - imgsensor_info.margin)
@@ -349,7 +350,7 @@ static void write_shutter(kal_uint32 shutter)
 
 		write_cmos_sensor_8(0x0100, 0x01);
 
-		for (int i = 0; i < timeout; i++) {
+		for (i = 0; i < timeout; i++) {
 			mdelay(10);
 			framecnt = read_cmos_sensor_8(0x0005);
 			if ( framecnt == 0xFF) {
