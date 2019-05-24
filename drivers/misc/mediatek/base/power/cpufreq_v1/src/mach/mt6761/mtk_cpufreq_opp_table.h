2a461c31d782d10d78bf24865b3f472c0f1b9eb4
charles.yang <charles.yang@mediatek.com>
2019-05-24 15:50:08 +0800

    [ALPS04477670] CPUDVFS: 6761 add LITE opp & power table
    [Detail] CPUDVFS: 6761 add LITE opp & power table
    
    MTK-Commit-Id: 8fe771d50fc9798c0694501cf23b8306d890daed
    
    Change-Id: Ic314a4db15c472d5153441f4b7bd7873f87463da
    Signed-off-by: charles.yang <charles.yang@mediatek.com>
    CR-Id: ALPS04477670
    Feature: DVFS (Dynamic Voltage and Frequency Scaling)
    (cherry picked from commit 652024c567f1a6bcbff44ebaf8eb6e993ad755f1)

--- drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6761/mtk_cpufreq_opp_table.h
+++ drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6761/mtk_cpufreq_opp_table.h
@@ -122,6 +122,42 @@
 #define CPU_DVFS_VOLT14_VPROC_LL_FY2    80000           /* 10uV */
 #define CPU_DVFS_VOLT15_VPROC_LL_FY2    80000           /* 10uV */
 
+/* Lite */
+/* for DVFS OPP table LL */
+#define CPU_DVFS_FREQ0_LL_LITE    1800000    /* KHz */
+#define CPU_DVFS_FREQ1_LL_LITE    1767000    /* KHz */
+#define CPU_DVFS_FREQ2_LL_LITE    1734000    /* KHz */
+#define CPU_DVFS_FREQ3_LL_LITE    1700000    /* KHz */
+#define CPU_DVFS_FREQ4_LL_LITE    1667000    /* KHz */
+#define CPU_DVFS_FREQ5_LL_LITE    1633000    /* KHz */
+#define CPU_DVFS_FREQ6_LL_LITE    1533000    /* KHz */
+#define CPU_DVFS_FREQ7_LL_LITE    1466000    /* KHz */
+#define CPU_DVFS_FREQ8_LL_LITE    1400000    /* KHz */
+#define CPU_DVFS_FREQ9_LL_LITE    1308000    /* KHz */
+#define CPU_DVFS_FREQ10_LL_LITE   1216000    /* KHz */
+#define CPU_DVFS_FREQ11_LL_LITE   1125000    /* KHz */
+#define CPU_DVFS_FREQ12_LL_LITE   1056000    /* KHz */
+#define CPU_DVFS_FREQ13_LL_LITE    987000    /* KHz */
+#define CPU_DVFS_FREQ14_LL_LITE    918000    /* KHz */
+#define CPU_DVFS_FREQ15_LL_LITE    850000    /* KHz */
+
+#define CPU_DVFS_VOLT0_VPROC_LL_LITE     95000           /* 10uV */
+#define CPU_DVFS_VOLT1_VPROC_LL_LITE     93750           /* 10uV */
+#define CPU_DVFS_VOLT2_VPROC_LL_LITE     92500           /* 10uV */
+#define CPU_DVFS_VOLT3_VPROC_LL_LITE     91250           /* 10uV */
+#define CPU_DVFS_VOLT4_VPROC_LL_LITE     90000           /* 10uV */
+#define CPU_DVFS_VOLT5_VPROC_LL_LITE     88750           /* 10uV */
+#define CPU_DVFS_VOLT6_VPROC_LL_LITE     85000           /* 10uV */
+#define CPU_DVFS_VOLT7_VPROC_LL_LITE     82500           /* 10uV */
+#define CPU_DVFS_VOLT8_VPROC_LL_LITE     80000           /* 10uV */
+#define CPU_DVFS_VOLT9_VPROC_LL_LITE     77500           /* 10uV */
+#define CPU_DVFS_VOLT10_VPROC_LL_LITE    75000           /* 10uV */
+#define CPU_DVFS_VOLT11_VPROC_LL_LITE    72500           /* 10uV */
+#define CPU_DVFS_VOLT12_VPROC_LL_LITE    70625           /* 10uV */
+#define CPU_DVFS_VOLT13_VPROC_LL_LITE    68750           /* 10uV */
+#define CPU_DVFS_VOLT14_VPROC_LL_LITE    66875           /* 10uV */
+#define CPU_DVFS_VOLT15_VPROC_LL_LITE    65000           /* 10uV */
+
 /* DVFS OPP table */
 #define OPP_TBL(cluster, seg, lv, vol)	\
 static struct mt_cpu_freq_info opp_tbl_##cluster##_e##lv##_0[] = {        \
@@ -162,6 +198,7 @@ static struct mt_cpu_freq_info opp_tbl_##cluster##_e##lv##_0[] = {        \
 OPP_TBL(LL,  FY, 0, LL); /* opp_tbl_LL_e0_0  */
 OPP_TBL(LL,  SB, 1, LL); /* opp_tbl_LL_e1_0  */
 OPP_TBL(LL,  FY2, 2, LL); /* opp_tbl_LL_e2_0  */
+OPP_TBL(LL,  LITE, 3, LL); /* opp_tbl_LL_e2_0  */
 
 
 /* v0.3 */
@@ -173,6 +210,8 @@ struct opp_tbl_info opp_tbls[NR_MT_CPU_DVFS][NUM_CPU_LEVEL] = {
 		[CPU_LEVEL_1] = { opp_tbl_LL_e1_0,
 				ARRAY_SIZE(opp_tbl_LL_e1_0) },
 		[CPU_LEVEL_2] = { opp_tbl_LL_e2_0,
+				ARRAY_SIZE(opp_tbl_LL_e2_0) },
+		[CPU_LEVEL_3] = { opp_tbl_LL_e3_0,
 				ARRAY_SIZE(opp_tbl_LL_e2_0) },
 	},
 };
@@ -240,7 +279,26 @@ static struct mt_cpu_freq_method opp_tbl_method_LL_FY2[] = {
 	FP(2,	1),
 	FP(2,	1),
 };
-
+/* Lite */
+static struct mt_cpu_freq_method opp_tbl_method_LL_LITE[] = {
+	/* POS,	CLK */
+	FP(1,	1),
+	FP(1,	1),
+	FP(1,	1),
+	FP(1,	1),
+	FP(1,	1),
+	FP(1,	1),
+	FP(1,	1),
+	FP(2,	1),
+	FP(2,	1),
+	FP(2,	1),
+	FP(2,	1),
+	FP(2,	1),
+	FP(2,	1),
+	FP(2,	1),
+	FP(2,	1),
+	FP(2,	1),
+};
 /* v0.3 */
 struct opp_tbl_m_info opp_tbls_m[NR_MT_CPU_DVFS][NUM_CPU_LEVEL] = {
 	/* LL */
@@ -248,5 +306,6 @@ struct opp_tbl_m_info opp_tbls_m[NR_MT_CPU_DVFS][NUM_CPU_LEVEL] = {
 		[CPU_LEVEL_0] = { opp_tbl_method_LL_FY },
 		[CPU_LEVEL_1] = { opp_tbl_method_LL_SB },
 		[CPU_LEVEL_2] = { opp_tbl_method_LL_FY2 },
+		[CPU_LEVEL_3] = { opp_tbl_method_LL_LITE },
 	},
 };
