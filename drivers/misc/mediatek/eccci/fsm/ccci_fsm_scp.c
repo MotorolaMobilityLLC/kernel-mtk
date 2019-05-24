780693fe4d61de38286028e09fa9c2439664fc21
guanglai guo <guanglai.guo@mediatek.com>
2019-05-24 15:42:41 +0800

    [ALPS04415781] CCCI: add re-init mechanism for scp reboot
    when SCP reboot, notify SCPCCCI and MD to re-init the
    share memory.
    add while loop mechanism to get IPI channel.
    
    MTK-Commit-Id: 52e25e407f7108aeccd3883b047cd6b10819e131
    
    Change-Id: Ia0fc846560c727cfd363bf0dd888900e8b750399
    Signed-off-by: guanglai guo <guanglai.guo@mediatek.com>
    CR-Id: ALPS04415781
    Feature: Modem Interface Driver

--- drivers/misc/mediatek/eccci/fsm/ccci_fsm_scp.c
+++ drivers/misc/mediatek/eccci/fsm/ccci_fsm_scp.c
@@ -28,6 +28,7 @@ static struct ccci_skb_queue scp_ipi_rx_skb_list;
 static int ccci_scp_ipi_send(int md_id, int op_id, void *data)
 {
 	int ret = 0;
+	int ipi_status = 0;
 
 	if (atomic_read(&scp_state) == SCP_CCCI_STATE_INVALID) {
 		CCCI_ERROR_LOG(md_id, FSM,
@@ -45,8 +46,13 @@ static int ccci_scp_ipi_send(int md_id, int op_id, void *data)
 		"IPI send %d/0x%x, %d\n",
 		scp_ipi_tx_msg.op_id, scp_ipi_tx_msg.data[0],
 		(int)sizeof(struct ccci_ipi_msg));
-	if (scp_ipi_send(IPI_APCCCI, &scp_ipi_tx_msg,
-			sizeof(scp_ipi_tx_msg), 1, SCP_A_ID) != SCP_IPI_DONE) {
+	while (1) {
+		ipi_status = scp_ipi_send(IPI_APCCCI, &scp_ipi_tx_msg,
+			sizeof(scp_ipi_tx_msg), 1, SCP_A_ID);
+		if (ipi_status != SCP_IPI_BUSY)
+			break;
+	}
+	if (ipi_status == SCP_IPI_ERROR) {
 		CCCI_ERROR_LOG(md_id, FSM, "IPI send fail!\n");
 		ret = -CCCI_ERR_MD_NOT_READY;
 	}
@@ -68,8 +74,10 @@ static void ccci_scp_md_state_sync_work(struct work_struct *work)
 		switch (scp_ctl->md_id) {
 		case MD_SYS1:
 			while (count < SCP_BOOT_TIMEOUT/EVENT_POLL_INTEVAL) {
-				if (atomic_read(&scp_state) ==
-					SCP_CCCI_STATE_BOOTING)
+				if (atomic_read(&scp_state)
+					== SCP_CCCI_STATE_BOOTING
+					|| atomic_read(&scp_state)
+						== SCP_CCCI_STATE_RBREADY)
 					break;
 				count++;
 				msleep(EVENT_POLL_INTEVAL);
