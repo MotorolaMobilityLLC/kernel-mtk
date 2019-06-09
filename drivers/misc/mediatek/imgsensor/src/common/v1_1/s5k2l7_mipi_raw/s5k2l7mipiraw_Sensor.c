d21c65712898267163095cd2bb78f7c3e9ea78e0
Adogu Huang <adogu.huang@mediatek.com>
2019-06-09 13:27:48 +0800

    [ALPS04426362] imgsensor: fix: s5k2l7 driver build error
    Add undef macro to fix the build errors generated from ccu tool.
    
    Ref:
    libccu_s5k2l7_mipi_raw.c:1:2: error: #error "Violate CCU sensor
    driver coding RULE 4: using non defined marco, USE_OIS,
    ENABLE_WDR_AUTO_RATION, CAPTURE_WDR"
    
    MTK-Commit-Id: dfc1d49bd4eaa41118c3f9eed4873221e59ff11f
    
    Change-Id: I5a04c6c566d6b8d0fea68b5f2e21c3afade8cdcb
    CR-Id: ALPS04426362
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Adogu Huang <adogu.huang@mediatek.com>

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/s5k2l7_mipi_raw/s5k2l7mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/s5k2l7_mipi_raw/s5k2l7mipiraw_Sensor.c
@@ -63,7 +63,8 @@
 /* #define TEST_PATTERN_EN */
 /*WDR auto ration mode*/
 /* #define ENABLE_WDR_AUTO_RATION */
-
+#undef ENABLE_WDR_AUTO_RATION
+#undef CAPTURE_WDR
 
 #define MULTI_WRITE 1
 
@@ -360,6 +361,7 @@ static struct SENSOR_VC_INFO_STRUCT SENSOR_VC_INFO[5] = {
 
 
 /* #define USE_OIS */
+#undef USE_OIS
 #ifdef USE_OIS
 #define OIS_I2C_WRITE_ID 0x48
 #define OIS_I2C_READ_ID 0x49
