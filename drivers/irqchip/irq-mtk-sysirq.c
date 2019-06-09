f44af0264b5991d0ff118e5d802cc8a5aa86558b
Hanks Chen <hanks.chen@mediatek.com>
2019-06-09 14:04:52 +0800

    [ALPS04079752] irq: add irq mask and indirect access api
    [Detail]
    1) add irq mask api for gic dcm
    2) add debug info for mcusys mask
    3) add indirect access api for mask/polarity control
    
    MTK-Commit-Id: bc3152b3910cfedb4e2d1e846d41f66708433f5c
    
    Change-Id: Iefbcd47289486bb5a46eb2f228eae7f5c3cf4df4
    Signed-off-by: Hanks Chen <hanks.chen@mediatek.com>
    CR-Id: ALPS04079752
    Feature: [Module]Kernel Maintenance
    (cherry picked from commit 80f465906e2ab62b50517b25196ed2ad373c79dd)

--- drivers/irqchip/irq-mtk-sysirq.c
+++ drivers/irqchip/irq-mtk-sysirq.c
@@ -24,12 +24,90 @@
 
 struct mtk_sysirq_chip_data {
 	spinlock_t lock;
+#ifndef CONFIG_MTK_INDIRECT_ACCESS
 	u32 nr_intpol_bases;
 	void __iomem **intpol_bases;
+	void __iomem *int_msk_ctl0;
 	u32 *intpol_words;
 	u8 *intpol_idx;
 	u16 *which_word;
+#else
+	void __iomem *indirect_access;
+	void __iomem *direct_access_en;
+#endif
+	u32 need_unmask;
 };
+
+#ifdef CONFIG_MTK_INDIRECT_ACCESS
+static int mtk_sysirq_set_type(struct irq_data *data, unsigned int type)
+{
+	struct mtk_sysirq_chip_data *chip_data = data->chip_data;
+	void __iomem *base;
+	u32 value;
+	unsigned long flags;
+	int ret;
+
+	base = chip_data->indirect_access;
+
+	spin_lock_irqsave(&chip_data->lock, flags);
+	if (type == IRQ_TYPE_LEVEL_LOW || type == IRQ_TYPE_EDGE_FALLING) {
+		if (type == IRQ_TYPE_LEVEL_LOW)
+			type = IRQ_TYPE_LEVEL_HIGH;
+		else
+			type = IRQ_TYPE_EDGE_RISING;
+		value = 1;
+	} else
+		value = 0;
+
+	/* select spi id */
+	value |= (data->hwirq << 16);
+
+	/* select polarity control */
+	value |= (1 << 29);
+
+	writel(value, base);
+
+	data = data->parent_data;
+	ret = data->chip->irq_set_type(data, type);
+	spin_unlock_irqrestore(&chip_data->lock, flags);
+
+	return ret;
+}
+
+void _mt_irq_mask(struct irq_data *data)
+{
+	struct mtk_sysirq_chip_data *chip_data = data->chip_data;
+	void __iomem *base = chip_data->indirect_access;
+	unsigned int value;
+
+	/* set mask */
+	value = 1;
+	/* select spi id */
+	value |= (data->hwirq << 16);
+	/* select mask control */
+	value |= (1 << 30);
+
+	writel_relaxed(value, base);
+}
+
+void _mt_irq_unmask(struct irq_data *data)
+{
+	struct mtk_sysirq_chip_data *chip_data = data->chip_data;
+	void __iomem *base = chip_data->indirect_access;
+	unsigned int value;
+
+	/* set unmask */
+	value = 0;
+	/* select spi id */
+	value |= (data->hwirq << 16);
+	/* select mask control */
+	value |= (1 << 30);
+
+	writel_relaxed(value, base);
+}
+
+
+#else
 
 static int mtk_sysirq_set_type(struct irq_data *data, unsigned int type)
 {
@@ -65,6 +143,61 @@ static int mtk_sysirq_set_type(struct irq_data *data, unsigned int type)
 	return ret;
 }
 
+void _mt_irq_mask(struct irq_data *data)
+{
+	u32 offset, reg, value;
+	irq_hw_number_t hwirq = data->hwirq;
+	struct mtk_sysirq_chip_data *chip_data = data->chip_data;
+	void __iomem *base = chip_data->int_msk_ctl0;
+
+	offset = hwirq%32;
+	reg = ((hwirq)/32);
+
+	value = readl_relaxed(base + reg * 4);
+	value |= (1 << offset);	/* mask */
+	writel_relaxed(value, (base + reg * 4));
+
+}
+
+void _mt_irq_unmask(struct irq_data *data)
+{
+	u32 offset, reg, value;
+	irq_hw_number_t hwirq = data->hwirq;
+	struct mtk_sysirq_chip_data *chip_data = data->chip_data;
+	void __iomem *base = chip_data->int_msk_ctl0;
+
+	offset = hwirq%32;
+	reg = ((hwirq)/32);
+
+	value = readl_relaxed(base + reg * 4);
+	value &= ~(1 << offset);	/* unmask */
+	writel_relaxed(value, (base + reg * 4));
+}
+
+#endif
+
+static void mtk_irq_chip_mask_parent(struct irq_data *data)
+{
+	struct mtk_sysirq_chip_data *chip_data = data->chip_data;
+	struct irq_data *parent_data = data->parent_data;
+
+	parent_data->chip->irq_mask(parent_data);
+
+	if (chip_data->need_unmask == 1)
+		_mt_irq_mask(data);
+}
+
+static void mtk_irq_chip_unmask_parent(struct irq_data *data)
+{
+	struct mtk_sysirq_chip_data *chip_data = data->chip_data;
+	struct irq_data *parent_data = data->parent_data;
+
+	parent_data->chip->irq_unmask(parent_data);
+
+	if (chip_data->need_unmask == 1)
+		_mt_irq_unmask(data);
+}
+
 static struct irq_chip mtk_sysirq_chip = {
 	.name			= "MT_SYSIRQ",
 	.irq_mask		= irq_chip_mask_parent,
@@ -73,6 +206,7 @@ static struct irq_chip mtk_sysirq_chip = {
 	.irq_set_type		= mtk_sysirq_set_type,
 	.irq_retrigger		= irq_chip_retrigger_hierarchy,
 	.irq_set_affinity	= irq_chip_set_affinity_parent,
+	.flags                  = IRQCHIP_SET_TYPE_MASKED,
 };
 
 static int mtk_sysirq_domain_translate(struct irq_domain *d,
@@ -132,8 +266,13 @@ static int __init mtk_sysirq_of_init(struct device_node *node,
 {
 	struct irq_domain *domain, *domain_parent;
 	struct mtk_sysirq_chip_data *chip_data;
-	int ret, size, intpol_num = 0, nr_intpol_bases = 0, i = 0;
-
+	int ret, intpol_num = 0;
+	const char *need_unmask_str = NULL;
+#ifndef CONFIG_MTK_INDIRECT_ACCESS
+	int nr_intpol_bases = 0, i = 0, size;
+#else
+	const char *enable_indirect_str = NULL;
+#endif
 
 	domain_parent = irq_find_host(parent);
 	if (!domain_parent) {
@@ -145,6 +284,7 @@ static int __init mtk_sysirq_of_init(struct device_node *node,
 	if (!chip_data)
 		return -ENOMEM;
 
+#ifndef CONFIG_MTK_INDIRECT_ACCESS
 	while (of_get_address(node, i++, NULL, NULL))
 		nr_intpol_bases++;
 
@@ -215,18 +355,86 @@ static int __init mtk_sysirq_of_init(struct device_node *node,
 		chip_data->intpol_idx[i] = j;
 		chip_data->which_word[i] = word;
 	}
+#else
+	if (!of_property_read_string(node,
+				"enable_indirect", &enable_indirect_str)) {
+		if (enable_indirect_str &&
+				!strncmp(enable_indirect_str, "yes", 3)) {
+			unsigned int indirect_base, en_base;
+
+			chip_data->need_unmask = 1;
+			if (of_property_read_u32(node,
+					"indirect_base", &indirect_base))
+				return -EINVAL;
+			if (of_property_read_u32(node,
+					"indirect_en_base", &en_base))
+				return -EINVAL;
+			/* get int polarity number */
+			if (of_property_read_u32(node,
+					"intpol_num", &intpol_num))
+				return -EINVAL;
+
+			pr_info("[%s] indirect : 0x%x, indirect_en : 0x%x\n",
+					__func__, indirect_base, en_base);
+
+			chip_data->indirect_access =
+				ioremap(indirect_base, 0x4);
+			chip_data->direct_access_en = ioremap(en_base, 0x4);
+			if (!chip_data->indirect_access
+					|| !chip_data->direct_access_en)
+				return -EINVAL;
+		}
+
+	} else {
+		pr_notice("mtk_sysirq: indirect access not specified\n");
+		ret = -ENOMEM;
+		goto out_free_chip;
+	}
+#endif
+
+	chip_data->need_unmask = 0;
+	if (!of_property_read_string(node,
+				"need_unmask", &need_unmask_str)) {
+		if (need_unmask_str && !strncmp(need_unmask_str, "yes", 3)) {
+#ifndef CONFIG_MTK_INDIRECT_ACCESS
+			unsigned int tmp_base, offset;
+
+			if (of_property_read_u32(node,
+						"mask_base", &tmp_base))
+				return -EINVAL;
+			if (of_property_read_u32(node,
+						"mask_offset", &offset))
+				return -EINVAL;
+
+			pr_info("[%s] mask_base : 0x%x, offset : 0x%x\n",
+					__func__, tmp_base, offset);
+
+			chip_data->int_msk_ctl0 = ioremap(tmp_base, offset);
+			pr_info("[%s] mask_base : 0x%p\n",
+					__func__, chip_data->int_msk_ctl0);
+			if (!chip_data->int_msk_ctl0)
+				return -EINVAL;
+#endif
+			chip_data->need_unmask = 1;
+			/* hook mask callback */
+			mtk_sysirq_chip.irq_mask = mtk_irq_chip_mask_parent;
+			mtk_sysirq_chip.irq_unmask = mtk_irq_chip_unmask_parent;
+		}
+	}
+
 
 	domain = irq_domain_add_hierarchy(domain_parent, 0, intpol_num, node,
 					  &sysirq_domain_ops, chip_data);
 	if (!domain) {
 		ret = -ENOMEM;
-		goto out_free_which_word;
+		goto out_free_all;
 	}
 	spin_lock_init(&chip_data->lock);
 
 	return 0;
 
-out_free_which_word:
+out_free_all:
+#ifndef CONFIG_MTK_INDIRECT_ACCESS
 	kfree(chip_data->which_word);
 out_free_intpol_idx:
 	kfree(chip_data->intpol_idx);
@@ -237,6 +445,7 @@ out_free_intpol:
 	kfree(chip_data->intpol_bases);
 out_free_intpol_words:
 	kfree(chip_data->intpol_words);
+#endif
 out_free_chip:
 	kfree(chip_data);
 	return ret;
