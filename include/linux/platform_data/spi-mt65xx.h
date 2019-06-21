a804b8e35b7085c5f9e621c589bd802605aeb124
mtk15754 <wenqiang.zhang@mediatek.com>
2019-06-21 09:56:27 +0800

    [ALPS04419965] SPI: add chip_config
    1.add deassert_mode,setup_time,hold_time
    2.add chip_config user interface
    
    MTK-Commit-Id: 2a7cf64c4e7e1706a0ed2055cc5cb2d028f92790
    
    Change-Id: I2570cff98fd1def50f0b8b90531c66326b4d63b9
    Signed-off-by: mtk15754 <wenqiang.zhang@mediatek.com>
    CR-Id: ALPS04419965
    Feature: [Module]SPI (Serial Peripheral Interface Bus)

--- include/linux/platform_data/spi-mt65xx.h
+++ include/linux/platform_data/spi-mt65xx.h
@@ -18,5 +18,11 @@ struct mtk_chip_config {
 	u32 rx_mlsb;
 	u32 cs_pol;
 	u32 sample_sel;
+
+	//patch the parameter as customized
+	u32 cs_setuptime;
+	u32 cs_holdtime;
+	u32 cs_idletime;
+	u32 deassert_mode;
 };
 #endif
