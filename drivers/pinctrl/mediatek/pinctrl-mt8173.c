2cd758dc09a4f7b3bc8c5a24093bd0b7ea20b8f2
jg_poxu <jg_poxu@mediatek.com>
2019-06-21 09:56:08 +0800

    [ALPS04144899] mt8173: fix pinctrl debug issue
    fix pinctrl debug issue
    
    MTK-Commit-Id: 6e9c9e2574265d02bdd4d5db2f00d158b0225ede
    
    Change-Id: I94059db50985293b7b207e89467922a066de0e6d
    Signed-off-by: jg_poxu <jg_poxu@mediatek.com>
    CR-Id: ALPS04144899
    Feature: Configuration
    (cherry picked from commit 77e482ae780155e47db27eeb4f493cb1f61818ee)

--- drivers/pinctrl/mediatek/pinctrl-mt8173.c
+++ drivers/pinctrl/mediatek/pinctrl-mt8173.c
@@ -194,6 +194,24 @@ static int mt8173_ies_smt_set(struct regmap *regmap, unsigned int pin,
 	return -EINVAL;
 }
 
+static int mt8173_spec_ies_get(struct regmap *regmap, unsigned int pin)
+{
+	return mtk_spec_get_ies_smt_range(regmap, mt8173_ies_set,
+		ARRAY_SIZE(mt8173_ies_set), pin);
+}
+
+static int mt8173_spec_smt_get(struct regmap *regmap, unsigned int pin)
+{
+	return mtk_spec_get_ies_smt_range(regmap, mt8173_smt_set,
+		ARRAY_SIZE(mt8173_smt_set), pin);
+}
+
+static int mt8173_spec_pull_get(struct regmap *regmap, unsigned int pin)
+{
+	return mtk_spec_pull_get_samereg(regmap, mt8173_spec_pupd,
+		ARRAY_SIZE(mt8173_spec_pupd), pin);
+}
+
 static const struct mtk_drv_group_desc mt8173_drv_grp[] =  {
 	/* 0E4E8SR 4/8/12/16 */
 	MTK_DRV_GRP(4, 16, 1, 2, 4),
@@ -329,6 +347,9 @@ static const struct mtk_pinctrl_devdata mt8173_pinctrl_data = {
 	.n_pin_drv_grps = ARRAY_SIZE(mt8173_pin_drv),
 	.spec_pull_set = mt8173_spec_pull_set,
 	.spec_ies_smt_set = mt8173_ies_smt_set,
+	.spec_pull_get = mt8173_spec_pull_get,
+	.spec_ies_get = mt8173_spec_ies_get,
+	.spec_smt_get = mt8173_spec_smt_get,
 	.dir_offset = 0x0000,
 	.pullen_offset = 0x0100,
 	.pullsel_offset = 0x0200,
