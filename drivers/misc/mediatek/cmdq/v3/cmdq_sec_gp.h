da960f02ea91ab22b476b7674bbf4abdce9e6f72
Ming-Fan Chen <ming-fan.chen@mediatek.com>
2019-06-21 09:59:51 +0800

    [ALPS04313905] cmdq: update struct for svp
    fix cmdq build error: update struct for svp
    
    MTK-Commit-Id: 8184149b1fa1454e443623c7a67d9f88b347526b
    
    Change-Id: Ieb78023fc440aaa9789b771bc92e9641e38a365b
    CR-Id: ALPS04313905
    Feature: [Module]CMDQ Driver
    Signed-off-by: Ming-Fan Chen <ming-fan.chen@mediatek.com>

--- drivers/misc/mediatek/cmdq/v3/cmdq_sec_gp.h
+++ drivers/misc/mediatek/cmdq/v3/cmdq_sec_gp.h
@@ -27,12 +27,8 @@
 
 /* context for tee vendor */
 struct cmdq_sec_tee_context {
-#if defined(CONFIG_TRUSTONIC_TEE_SUPPORT)
 	/* Universally Unique Identifier of secure tl/dr */
 	struct TEEC_UUID uuid;
-#else
-	TEEC_UUID uuid; /* Universally Unique Identifier of secure tl/dr */
-#endif
 	struct TEEC_Context gp_context; /* basic context */
 	struct TEEC_Session session; /* session handle */
 	struct TEEC_SharedMemory shared_mem; /* shared memory */
