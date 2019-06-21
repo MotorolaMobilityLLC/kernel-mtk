3a94ecda477543c771af030a2a4716faf3aa893a
Leroy Lin <Leroy.Lin@mediatek.com>
2019-06-21 09:52:50 +0800

    [ALPS04422940] SCP: Filter log by modules and levels
    This patch allows the log filter to be set by a device node. The
    log of the specified module and level is printed.
    
    MTK-Commit-Id: b79b205d0179775e6eea1216baf6d79b17036193
    
    Change-Id: I1b5f1e23ccccb53607667afdc864db4128f21032
    Signed-off-by: Leroy Lin <Leroy.Lin@mediatek.com>
    CR-Id: ALPS04422940
    Feature: TinySys

--- drivers/misc/mediatek/scp/v01/scp_helper.c
+++ drivers/misc/mediatek/scp/v01/scp_helper.c
@@ -882,6 +882,40 @@ DEVICE_ATTR(recovery_flag, 0600, scp_recovery_flag_r, scp_recovery_flag_w);
 
 #endif
 
+
+/******************************************************************************
+ *****************************************************************************/
+static ssize_t scp_set_log_filter(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	enum scp_ipi_status ret;
+	uint32_t filter;
+	const unsigned int len = sizeof(filter);
+
+	if (sscanf(buf, "0x%08x", &filter) != 1)
+		return -EINVAL;
+
+	ret = scp_ipi_send(IPI_SCP_LOG_FILTER, &filter, len, 0, SCP_A_ID);
+	switch (ret) {
+	case SCP_IPI_DONE:
+		pr_notice("[SCP] Set log filter to 0x%08x\n", filter);
+		return count;
+
+	case SCP_IPI_BUSY:
+		pr_notice("[SCP] IPI busy. Set log filter failed!\n");
+		return -EBUSY;
+
+	case SCP_IPI_ERROR:
+	default:
+		pr_notice("[SCP] IPI error. Set log filter failed!\n");
+		return -EIO;
+	}
+}
+DEVICE_ATTR(log_filter, 0200, NULL, scp_set_log_filter);
+
+
+/******************************************************************************
+ *****************************************************************************/
 static struct miscdevice scp_device = {
 	.minor = MISC_DYNAMIC_MINOR,
 	.name = "scp",
@@ -987,6 +1021,10 @@ static int create_files(void)
 	if (unlikely(ret != 0))
 		return ret;
 #endif  // SCP_RECOVERY_SUPPORT
+
+	ret = device_create_file(scp_device.this_device, &dev_attr_log_filter);
+	if (unlikely(ret != 0))
+		return ret;
 
 	return 0;
 }
