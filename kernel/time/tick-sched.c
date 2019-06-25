5b703ee9836abe2bf8abac4a4725a7fbcb8e202f
Freddy Hsin <freddy.hsin@mediatek.com>
2019-06-25 09:13:43 +0800

    [ALPS04461303] timer: Fix collision between tick and other hrtimers
    Reference:
    https://github.com/torvalds/linux/commit/
    411fe24e6b7c283c3a1911450cdba6dd3aaea56e/
    
    Author: Ingo Molnar <mingo@kernel.org>
    
    The bug happened when a tick fired too early past its expected expiration:
    on IRQ exit the tick was scheduled again to the same deadline but skipped
    reprogramming because ts->next_tick still kept in cache the deadline.
    This has been fixed now with resetting ts->next_tick from the tick
    itself. Extra care has also been taken to prevent from obsolete values
    throughout CPU hotplug operations.
    
    When the tick is stopped and an interrupt occurs afterward, we check on
    that interrupt exit if the next tick needs to be rescheduled. If it
    doesnt need any update, we dont want to do anything.
    
    In order to check if the tick needs an update, we compare it against the
    clockevent device deadline. Now thats a problem because the clockevent
    device is at a lower level than the tick itself if it is implemented
    on top of hrtimer.
    
    Every hrtimer share this clockevent device. So comparing the next tick
    deadline against the clockevent device deadline is wrong because the
    device may be programmed for another hrtimer whose deadline collides
    with the tick. As a result we may end up not reprogramming the tick
    accidentally.
    
    In a worst case scenario under full dynticks mode, the tick stops firing
    as it is supposed to every 1hz, leaving /proc/stat stalled:
    
          Task in a full dynticks CPU
          ----------------------------
    
          * hrtimer A is queued 2 seconds ahead
          * the tick is stopped, scheduled 1 second ahead
          * tick fires 1 second later
          * on tick exit, nohz schedules the tick 1 second ahead but sees
            the clockevent device is already programmed to that deadline,
            fooled by hrtimer A, the tick isnt rescheduled.
          * hrtimer A is cancelled before its deadline
          * tick never fires again until an interrupt happens...
    
    In order to fix this, store the next tick deadline to the tick_sched
    local structure and reuse that value later to check whether we need to
    reprogram the clock after an interrupt.
    
    On the other hand, ts->sleep_length still wants to know about the next
    clock event and not just the tick, so we want to improve the related
    comment to avoid confusion.
    
    MTK-Commit-Id: 30bc00760652c321c73d3cc6bb905b218ac601a6
    
    Change-Id: Ic5891ea3718e8c04099e3026c32a5d4978c2522c
    Signed-off-by: Freddy Hsin <freddy.hsin@mediatek.com>
    CR-Id: ALPS04461303
    Feature: Power Management
    (cherry picked from commit 1e8051fe70e4dcea8ad865ceee0de937d903470b)
    (cherry picked from commit 3483b283d95131af46f5b8db17f07dc412fdd79a)

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -146,6 +146,13 @@ static void tick_sched_handle(struct tick_sched *ts, struct pt_regs *regs)
 		touch_softlockup_watchdog_sched();
 		if (is_idle_task(current))
 			ts->idle_jiffies++;
+
+		/*
+		 * In case the current tick fired too early past its expected
+		 * expiration, make sure we don't bypass the next clock
+		 * reprogramming to the same deadline.
+		 */
+		ts->next_tick.tv64 = 0;
 	}
 #endif
 	update_process_times(user_mode(regs));
@@ -765,6 +772,12 @@ static void tick_nohz_restart(struct tick_sched *ts, ktime_t now)
 		hrtimer_start_expires(&ts->sched_timer, HRTIMER_MODE_ABS_PINNED);
 	else
 		tick_program_event(hrtimer_get_expires(&ts->sched_timer), 1);
+
+	/*
+	 * Reset to make sure next tick stop doesn't get fooled by past
+	 * cached clock deadline.
+	 */
+	ts->next_tick.tv64 = 0;
 }
 
 static inline bool local_timer_softirq_pending(void)
@@ -891,8 +904,16 @@ static ktime_t tick_nohz_stop_sched_tick(struct tick_sched *ts,
 	tick.tv64 = expires;
 
 	/* Skip reprogram of event if its not changed */
-	if (ts->tick_stopped && (expires == dev->next_event.tv64))
-		goto out;
+	if (ts->tick_stopped && (expires == ts->next_tick.tv64)) {
+		/* Sanity check: make sure clockevent is actually programmed */
+		if (likely(dev->next_event.tv64 <= ts->next_tick.tv64))
+			goto out;
+		WARN_ON_ONCE(1);
+		pr_info_once("basemono: %llu ts->next_tick: %llu dev->next_event: %llu timer->active: %d timer->expires: %llu\n",
+			     basemono, ts->next_tick.tv64, dev->next_event.tv64,
+			     hrtimer_active(&ts->sched_timer),
+			     hrtimer_get_expires(&ts->sched_timer).tv64);
+	}
 
 	/*
 	 * nohz_stop_sched_tick can be called several times before
@@ -911,6 +932,8 @@ static ktime_t tick_nohz_stop_sched_tick(struct tick_sched *ts,
 		trace_tick_stop(1, TICK_DEP_MASK_NONE);
 	}
 
+	ts->next_tick.tv64 = tick.tv64;
+
 	/*
 	 * If the expiration time == KTIME_MAX, then we simply stop
 	 * the tick timer.
@@ -926,7 +949,10 @@ static ktime_t tick_nohz_stop_sched_tick(struct tick_sched *ts,
 	else
 		tick_program_event(tick, 1);
 out:
-	/* Update the estimated sleep length */
+	/*
+	 * Update the estimated sleep length until the next timer
+	 * (not only the tick).
+	 */
 	ts->sleep_length = ktime_sub(dev->next_event, now);
 	return tick;
 }
@@ -984,6 +1010,11 @@ static bool can_stop_idle_tick(int cpu, struct tick_sched *ts)
 	if (unlikely(!cpu_online(cpu))) {
 		if (cpu == tick_do_timer_cpu)
 			tick_do_timer_cpu = TICK_DO_TIMER_NONE;
+		/*
+		 * Make sure the CPU doesn't get fooled by obsolete tick
+		 * deadline if it comes back online later.
+		 */
+		ts->next_tick.tv64 = 0;
 		return false;
 	}
 
