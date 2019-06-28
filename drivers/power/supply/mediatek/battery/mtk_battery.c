237bcb77cdbd1e25127bc108aaa0e2bd5146a82f
chailu1 <chailu1@lenovo.com>
2019-06-28 16:19:07 +0800

    battery: Add /sys/class/power_supply/battery/age
    Add /sys/class/power_supply/battery/age for service requires.
    This node return battery aGing percentage.
    
    Change-Id: I42729b588deca8e98997560d812b4b66ac38ff5f
    Signed-off-by: chailu1 <chailu1@mt.com>
    Reviewed-on: https://gerrit.mot.com/1378237
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Wei Xu <xuwei9@mt.com>
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1847204
    Reviewed-by: Lu Chai <chailu1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>

--- drivers/power/supply/mediatek/battery/mtk_battery.c
+++ drivers/power/supply/mediatek/battery/mtk_battery.c
@@ -132,6 +132,7 @@ static enum power_supply_property battery_props[] = {
 	POWER_SUPPLY_PROP_TIME_TO_FULL_NOW,
 	POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN,
 	POWER_SUPPLY_PROP_CHARGE_RATE,
+	POWER_SUPPLY_PROP_AGE,
 };
 
 /* weak function */
@@ -504,6 +505,11 @@ static int battery_get_property(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_RATE:
 		val->intval = mmi_chrg_rate_check();
+		break;
+	case POWER_SUPPLY_PROP_AGE:
+		if (gm.aging_factor > 10000)
+			gm.aging_factor = 10000;
+		val->intval = gm.aging_factor /100;
 		break;
 
 	default:
