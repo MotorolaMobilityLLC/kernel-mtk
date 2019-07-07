85a7b0cea36d4c5e9ea597b78cc5c8c578557e2b
Chinwen Chang <chinwen.chang@mediatek.com>
2019-07-07 18:37:46 +0800

    [ALPS04578729] ANDROID: Avoid taking multiple locks in handle_lmk_event
    Conflicting lock events have been reported resulting from
    rcu_read_lock, mmap_sem (in get_cmdline) and lmk_event_lock.
    
    This CL avoids the possibility of these conditions by moving
    handle_lmk_event outside rcu_read_lock and invoking get_cmdline before
    lmk_event_lock is taken.
    
    Bug: 133479338, 133829075
    Signed-off-by: Jim Blackler <jimblackler@google.com>
    
    MTK-Commit-Id: 3f5986fa6058c20ea25f0ed37ad345ed12490157
    
    Change-Id: Ib811b3bad19cd631a4aedff0c5e5c9535d52bb70
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>
    CR-Id: ALPS04578729
    Feature: Memory Optimization

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -120,12 +120,24 @@ void handle_lmk_event(struct task_struct *selected, short min_score_adj)
 	struct lmk_event *event;
 	int res;
 	long rss_in_pages = -1;
+	char taskname[MAX_TASKNAME];
 	struct mm_struct *mm = get_task_mm(selected);
 
 	if (mm) {
 		rss_in_pages = get_mm_rss(mm);
 		mmput(mm);
 	}
+
+	res = get_cmdline(selected, taskname, MAX_TASKNAME - 1);
+
+	/* No valid process name means this is definitely not associated with a
+	 * userspace activity.
+	 */
+
+	if (res <= 0 || res >= MAX_TASKNAME)
+		return;
+
+	taskname[res] = '\0';
 
 	spin_lock(&lmk_event_lock);
 
@@ -141,18 +153,8 @@ void handle_lmk_event(struct task_struct *selected, short min_score_adj)
 	events = (struct lmk_event *)event_buffer.buf;
 	event = &events[head];
 
-	res = get_cmdline(selected, event->taskname, MAX_TASKNAME - 1);
+	memcpy(event->taskname, taskname, res + 1);
 
-	/* No valid process name means this is definitely not associated with a
-	 * userspace activity.
-	 */
-
-	if (res <= 0 || res >= MAX_TASKNAME) {
-		spin_unlock(&lmk_event_lock);
-		return;
-	}
-
-	event->taskname[res] = '\0';
 	event->pid = selected->pid;
 	event->uid = from_kuid_munged(current_user_ns(), task_uid(selected));
 	if (selected->group_leader)
@@ -680,8 +682,6 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		lowmem_trigger_warning(selected, selected_oom_score_adj);
 
 		rem += selected_tasksize;
-
-		handle_lmk_event(selected, min_score_adj);
 	} else {
 		if (p_state_is_found & LOWMEM_P_STATE_D)
 			lowmem_print(2,
@@ -701,6 +701,9 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		     sc->nr_to_scan, sc->gfp_mask, rem);
 	rcu_read_unlock();
 	spin_unlock(&lowmem_shrink_lock);
+
+	if (selected)
+		handle_lmk_event(selected, min_score_adj);
 
 	/* dump more memory info outside the lock */
 	if (selected && selected_oom_score_adj <= lowmem_no_warn_adj &&
