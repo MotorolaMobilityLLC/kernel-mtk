c83492b7348ad4d9480a5133adc6e5f1f6d52e69
Guangming Cao <guangming.cao@mediatek.com>
2019-07-07 18:34:43 +0800

    [ALPS04681893] ion: [cache sync] workaround
    workd around to avoid cache sync without address check
    
    wordaround for presanity issue.
    
    MTK-Commit-Id: 050443e78ebeb2eac130f8ff6eb149cc59bac26f
    
    Change-Id: I95c33d49d1eecff938884fd1771c7d51f40722ae
    Signed-off-by: Guangming Cao <guangming.cao@mediatek.com>
    CR-Id: ALPS04681893
    Feature: [Module]ION/M4U

--- drivers/staging/android/ion/mtk/ion_drv.c
+++ drivers/staging/android/ion/mtk/ion_drv.c
@@ -333,7 +333,9 @@ static long ion_sys_cache_sync(struct ion_client *client,
 		}
 
 #ifdef __ION_CACHE_SYNC_USER_VA_EN__
-		if (sync_type < ION_CACHE_CLEAN_BY_RANGE_USE_PA)
+		//if (sync_type < ION_CACHE_CLEAN_BY_RANGE_USE_PA)
+		//work aroud for cache sync issue
+		if (sync_type < ION_CACHE_CLEAN_ALL)
 #endif
 		{
 			struct ion_buffer *buffer;
