82dc11227852dd53bd1f9a5d4673fbcd91614f73
Eddie Hung <eddie.hung@mediatek.com>
2019-07-20 10:56:32 +0800

    [ALPS04510475] tcpc: change irq enable time
    [Detail]
    In order to fix some pd TA detection issue at kpoc mdoe,
    change delay time to 4s to let pd detection can more earlier.
    
    MTK-Commit-Id: b7a2cf7e7b5f7c6011308d8641a456d2f54c2561
    
    Change-Id: I415a69e165ec48c5cbd28aafbfaebc4f93895119
    CR-Id: ALPS04510475
    Feature: Power Delivery
    Signed-off-by: Eddie Hung <eddie.hung@mediatek.com>

--- drivers/misc/mediatek/typec/tcpc/tcpci_core.c
+++ drivers/misc/mediatek/typec/tcpc/tcpci_core.c
@@ -454,7 +454,7 @@ static int tcpc_device_irq_enable(struct tcpc_device *tcpc)
 		tcpci_init_alert_mask(tcpc);
 
 	schedule_delayed_work(
-		&tcpc->event_init_work, msecs_to_jiffies(10*1000));
+		&tcpc->event_init_work, msecs_to_jiffies(4*1000));
 
 	pr_info("%s : tcpc irq enable OK!\n", __func__);
 	return 0;
