d58b6c7df1787bf3207e11f305de225ac7bf2fe4
zhangyang <yang.zhang@ontim.cn>
2019-07-25 17:19:45 +0800

    [Kernel-LCM]Add ESD check
    Change-Id: I62e76c9e86ad0dcd65d0d310e9fc28ad61319144
    St-result:PASS

--- drivers/misc/mediatek/lcm/ontim_ILI9881C_hc_hlt/ontim_ILI9881C_hc_hlt.c
+++ drivers/misc/mediatek/lcm/ontim_ILI9881C_hc_hlt/ontim_ILI9881C_hc_hlt.c
@@ -478,16 +478,11 @@ static void lcm_get_params(LCM_PARAMS *params)
 	params->dsi.CLK_HS_POST = 36;
 
 	params->dsi.clk_lp_per_line_enable = 0;
-	params->dsi.esd_check_enable = 0;
+	params->dsi.esd_check_enable = 1;
 	params->dsi.customization_esd_check_enable = 0;
-	params->dsi.lcm_esd_check_table[0].cmd = 0x09;
-	params->dsi.lcm_esd_check_table[0].count = 3;
-	params->dsi.lcm_esd_check_table[0].para_list[0] = 0x80;
-	params->dsi.lcm_esd_check_table[0].para_list[1] = 0x03;
-	params->dsi.lcm_esd_check_table[0].para_list[2] = 0x06;
-	params->dsi.lcm_esd_check_table[1].cmd = 0x54;
-	params->dsi.lcm_esd_check_table[1].count = 1;
-	params->dsi.lcm_esd_check_table[1].para_list[0] = 0x24;
+	params->dsi.lcm_esd_check_table[0].cmd = 0x0A;
+	params->dsi.lcm_esd_check_table[0].count = 1;
+	params->dsi.lcm_esd_check_table[0].para_list[0] = 0x9C;
 
 }
 
