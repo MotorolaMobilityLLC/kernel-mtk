00553eccd250c6cc8d0a3e89e886bb538bf9212b
Megad Lu <megad.lu@mediatek.com>
2019-07-26 11:55:34 +0800

    [ALPS04702480] NFC : Update ST54H driver
    Add timings for ST54H SPI
    
    MTK-Commit-Id: 7105827585b64be13001ca9a522a4aa8dcb50b4c
    
    Change-Id: I38944116cb1a5845d65623fd81aaeb0dd6773499
    CR-Id: ALPS04702480
    Feature: NFC Chipset Capability
    Signed-off-by: Megad Lu <megad.lu@mediatek.com>

--- drivers/misc/mediatek/nfc/st21nfc/st21nfc.h
+++ drivers/misc/mediatek/nfc/st21nfc/st21nfc.h
@@ -24,14 +24,27 @@
  * PN544_SET_PWR(1): power on
  * PN544_SET_PWR(>1): power on with firmware download enabled
  */
-#define ST21NFC_GET_WAKEUP _IOR(ST21NFC_MAGIC, 0x01, unsigned int)
-#define ST21NFC_PULSE_RESET _IOR(ST21NFC_MAGIC, 0x02, unsigned int)
-#define ST21NFC_SET_POLARITY_RISING _IOR(ST21NFC_MAGIC, 0x03, unsigned int)
-#define ST21NFC_SET_POLARITY_FALLING _IOR(ST21NFC_MAGIC, 0x04, unsigned int)
-#define ST21NFC_SET_POLARITY_HIGH _IOR(ST21NFC_MAGIC, 0x05, unsigned int)
-#define ST21NFC_SET_POLARITY_LOW _IOR(ST21NFC_MAGIC, 0x06, unsigned int)
-#define ST21NFC_GET_POLARITY _IOR(ST21NFC_MAGIC, 0x07, unsigned int)
-#define ST21NFC_RECOVERY _IOR(ST21NFC_MAGIC, 0x08, unsigned int)
+#define ST21NFC_GET_WAKEUP _IO(ST21NFC_MAGIC, 0x01)
+#define ST21NFC_PULSE_RESET _IO(ST21NFC_MAGIC, 0x02)
+#define ST21NFC_SET_POLARITY_RISING _IO(ST21NFC_MAGIC, 0x03)
+#define ST21NFC_SET_POLARITY_FALLING _IO(ST21NFC_MAGIC, 0x04)
+#define ST21NFC_SET_POLARITY_HIGH _IO(ST21NFC_MAGIC, 0x05)
+#define ST21NFC_SET_POLARITY_LOW _IO(ST21NFC_MAGIC, 0x06)
+#define ST21NFC_GET_POLARITY _IO(ST21NFC_MAGIC, 0x07)
+#define ST21NFC_RECOVERY _IO(ST21NFC_MAGIC, 0x08)
+#define ST21NFC_USE_ESE _IOW(ST21NFC_MAGIC, 0x09, unsigned int)
+
+// Keep compatibility with older user applications.
+#define ST21NFC_LEGACY_GET_WAKEUP _IOR(ST21NFC_MAGIC, 0x01, unsigned int)
+#define ST21NFC_LEGACY_PULSE_RESET _IOR(ST21NFC_MAGIC, 0x02, unsigned int)
+#define ST21NFC_LEGACY_SET_POLARITY_RISING _IOR(ST21NFC_MAGIC, 0x03, \
+	unsigned int)
+#define ST21NFC_LEGACY_SET_POLARITY_FALLING _IOR(ST21NFC_MAGIC, 0x04, \
+	unsigned int)
+#define ST21NFC_LEGACY_SET_POLARITY_HIGH _IOR(ST21NFC_MAGIC, 0x05, unsigned int)
+#define ST21NFC_LEGACY_SET_POLARITY_LOW _IOR(ST21NFC_MAGIC, 0x06, unsigned int)
+#define ST21NFC_LEGACY_GET_POLARITY _IOR(ST21NFC_MAGIC, 0x07, unsigned int)
+#define ST21NFC_LEGACY_RECOVERY _IOR(ST21NFC_MAGIC, 0x08, unsigned int)
 
 struct st21nfc_platform_data {
 	unsigned int irq_gpio;
@@ -39,3 +52,10 @@ struct st21nfc_platform_data {
 	unsigned int reset_gpio;
 	unsigned int polarity_mode;
 };
+
+#define ST54SPI_CB_RESET_END 0
+#define ST54SPI_CB_RESET_START 1
+#define ST54SPI_CB_ESE_NOT_USED 2
+#define ST54SPI_CB_ESE_USED 3
+void st21nfc_register_st54spi_cb(void (*cb)(int, void *), void *data);
+void st21nfc_unregister_st54spi_cb(void);
