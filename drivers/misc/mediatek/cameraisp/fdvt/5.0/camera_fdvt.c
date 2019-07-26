1d0e917124d9c3c2eae39c24ff450c8bc5bc60da
Gasper Li <bo-jyun.li@mediatek.com>
2019-07-26 11:53:51 +0800

    [ALPS04710249] FDVT: Enhance code sercurity
    [Detail] Enhance code security of FDVT kernel driver
    
    MTK-Commit-Id: efda4b75219b11456b8317a295b43125c6cf4239
    
    Change-Id: Ice859fea8dc708ea5678ac4f5ce3232e13656a8e
    Signed-off-by: Gasper Li <bo-jyun.li@mediatek.com>
    CR-Id: ALPS04710249
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/cameraisp/fdvt/5.0/camera_fdvt.c
+++ drivers/misc/mediatek/cameraisp/fdvt/5.0/camera_fdvt.c
@@ -3494,7 +3494,7 @@ EXIT:
  *****************************************************************************/
 static signed int FDVT_mmap(struct file *pFile, struct vm_area_struct *pVma)
 {
-	long length = 0;
+	unsigned long length = 0;
 	unsigned int pfn = 0x0;
 
 	length = pVma->vm_end - pVma->vm_start;
