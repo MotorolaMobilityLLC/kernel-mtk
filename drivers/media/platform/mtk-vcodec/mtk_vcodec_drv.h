53069fff5bfe99a5e159fa096215ca2275d7cb21
Marcus Huang <marcus.huang@mediatek.com>
2019-07-26 11:55:41 +0800

    [ALPS04249111] HEIF capture: Add HEIF encoder
    Add HEIF encoder component.
    
    MTK-Commit-Id: 79b736db7684de826e0bb3bccb2f06fc0f0385b8
    
    Change-Id: Iaa1c6a87a4f578d125a66e63c81ffcc4ee7e803c
    Feature: HEVC Encoder
    CR-Id: ALPS04249111
    Signed-off-by: Marcus Huang <marcus.huang@mediatek.com>

--- drivers/media/platform/mtk-vcodec/mtk_vcodec_drv.h
+++ drivers/media/platform/mtk-vcodec/mtk_vcodec_drv.h
@@ -86,6 +86,7 @@ enum mtk_encode_param {
 	MTK_ENCODE_PARAM_PREPEND_SPSPPS_TO_IDR = (1 << 9),
 	MTK_ENCODE_PARAM_OPERATION_RATE = (1 << 10),
 	MTK_ENCODE_PARAM_BITRATE_MODE = (1 << 11),
+	MTK_ENCODE_PARAM_GRID_SIZE = (1 << 13),
 };
 
 /*
@@ -200,6 +201,7 @@ struct mtk_enc_params {
 	unsigned int    prependheader;
 	unsigned int    operationrate;
 	unsigned int    bitratemode;
+	unsigned int    heif_grid_size;
 };
 
 /*
@@ -237,6 +239,7 @@ struct venc_enc_param {
 	unsigned int prependheader;
 	unsigned int operationrate;
 	unsigned int bitratemode;
+	unsigned int heif_grid_size;
 	unsigned int sizeimage[MTK_VCODEC_MAX_PLANES];
 };
 
