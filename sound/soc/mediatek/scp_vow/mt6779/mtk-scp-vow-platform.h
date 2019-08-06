39409436ea9058710dfed11fd47366c58c86f99b
Che-Jui Chang <che-jui.chang@mediatek.com>
2019-08-06 23:20:22 +0800

    [ALPS04308537] vow: add vow feature and fix driver issues
    sync vow kernel driver code to new android branch
    1. fix coverity out of bound issue
    2. new pcm dump porting
    3. add vow recover handling when scp reset
    4. add O_LARGEFILE in filp_open. will let file system
       has no 2GB limitation.
    5. add protection in ioctl command to avoid free NULL pointer
    6. Logically dead code, remove these useless code
    7. vow 32-bit project build fail fixed
    8. add second mic power related setting
    9. bargein get irq id and change ioctl type
    
    MTK-Commit-Id: 4f34f5dd7c544bd742f2e32a1abc50b40d12c635
    
    Change-Id: I5e8baac868c178ab03733b4e25d13cb9f8f4cfe0
    Signed-off-by: Che-Jui Chang <che-jui.chang@mediatek.com>
    CR-Id: ALPS04308537
    Feature: MagiEar-Voice Wakeup

---
+++ sound/soc/mediatek/scp_vow/mt6779/mtk-scp-vow-platform.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ * Author Che-Jui Chang <Che-Jui.Chang@mediatek.com>
+ */
+
+#ifndef _MTK_SCP_VOW_PLATFORM_H
+#define _MTK_SCP_VOW_PLATFORM_H
+
+int get_scp_vow_memif_platform_id(void);
+
+#endif
