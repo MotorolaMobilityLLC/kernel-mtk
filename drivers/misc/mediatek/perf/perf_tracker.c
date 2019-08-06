fae1f471585168008be7f52d2d8229af334912fb
Frankie Chang <Frankie.Chang@mediatek.com>
2019-08-06 23:20:08 +0800

    [ALPS04401232] Perf: add fuel gauge info
    Read current(mA) and voltage(mV) of battery from PMIC.
    
    if read per delayed 8ms, estimated overhead is about
    20% CPU no matter different capacity.
    
    Start fuel gauge
     - echo 1 > /sys/devices/system/cpu/perf/fuel_gauge_enable
    
    Change polling interval
     - echo (delay ms) > /sys/device/system/cpu/perf/fuel_gauge_period
    
    Conflicts:
    	drivers/misc/mediatek/perf/perf_tracker.c
    
    MTK-Commit-Id: 28fa261be9fdf6da9df4197ca6ec6dd75623981a
    
    Change-Id: I0ba7171214ac421fbfb21164abdde30b8a304c0e
    CR-Id: ALPS04401232
    Feature: System Performance
    Signed-off-by: Frankie Chang <Frankie.Chang@mediatek.com>
    (cherry picked from commit 8dac0d7c43679bc97829fd36fc75de12f8693d3b)

--- drivers/misc/mediatek/perf/perf_tracker.c
+++ drivers/misc/mediatek/perf/perf_tracker.c
@@ -20,11 +20,30 @@
 #include <linux/swap.h>
 #include <mt-plat/mtk_blocktag.h>
 #include <helio-dvfsrc.h>
+#include <linux/jiffies.h>
 #ifdef CONFIG_MTK_QOS_FRAMEWORK
 #include <mtk_qos_sram.h>
 #endif
 #include <mt-plat/perf_tracker.h>
 #include <linux/topology.h>
+
+#include <linux/module.h>
+#ifndef __CHECKER__
+#define CREATE_TRACE_POINTS
+#include "perf_tracker_trace.h"
+#endif
+
+#include "perf_tracker.h"
+
+#ifdef CONFIG_MTK_GAUGE_VERSION
+#include <mt-plat/mtk_battery.h>
+
+static void fuel_gauge_handler(struct work_struct *work);
+
+static int fuel_gauge_enable;
+static int fuel_gauge_delay; /* ms */
+static DECLARE_DELAYED_WORK(fuel_gauge, fuel_gauge_handler);
+#endif
 
 static int perf_tracker_on, perf_tracker_init;
 static DEFINE_MUTEX(perf_ctl_mutex);
@@ -55,6 +74,25 @@ int __attribute__((weak)) mtk_btag_mictx_get_data(
 	struct mtk_btag_mictx_iostat_struct *io)
 {
 	return -1;
+}
+#endif
+
+#ifdef CONFIG_MTK_GAUGE_VERSION
+static void fuel_gauge_handler(struct work_struct *work)
+{
+	int curr, volt;
+
+	if (!fuel_gauge_enable)
+		return;
+
+	/* read current(mA) and valtage(mV) from pmic */
+	curr = battery_get_bat_current();
+	volt = battery_get_bat_voltage();
+
+	trace_fuel_gauge(curr, volt);
+
+	queue_delayed_work(system_power_efficient_wq,
+			&fuel_gauge, msecs_to_jiffies(fuel_gauge_delay));
 }
 #endif
 
@@ -160,6 +198,70 @@ void __perf_tracker(u64 wallclock,
 			);
 }
 
+#ifdef CONFIG_MTK_GAUGE_VERSION
+static ssize_t show_fuel_gauge_enable(struct kobject *kobj,
+		struct kobj_attribute *attr, char *buf)
+{
+	unsigned int len = 0, max_len = 4096;
+
+	len += snprintf(buf, max_len, "fuel_gauge_enable = %u\n",
+			fuel_gauge_enable);
+	return len;
+}
+
+static ssize_t store_fuel_gauge_enable(struct kobject *kobj,
+		struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	int tmp;
+
+	mutex_lock(&perf_ctl_mutex);
+
+	if (kstrtouint(buf, 10, &tmp) == 0)
+		fuel_gauge_enable = (tmp > 0) ? 1 : 0;
+
+	if (fuel_gauge_enable) {
+		/* default delay 8ms */
+		fuel_gauge_delay = (fuel_gauge_delay > 0) ?
+				fuel_gauge_delay : 8;
+
+		/* start fuel gauge tracking */
+		queue_delayed_work(system_power_efficient_wq,
+				&fuel_gauge,
+				msecs_to_jiffies(fuel_gauge_delay));
+	}
+
+	mutex_unlock(&perf_ctl_mutex);
+
+	return count;
+}
+
+static ssize_t show_fuel_gauge_period(struct kobject *kobj,
+		struct kobj_attribute *attr, char *buf)
+{
+	unsigned int len = 0, max_len = 4096;
+
+	len += snprintf(buf, max_len, "fuel_gauge_period = %u(ms)\n",
+				fuel_gauge_delay);
+	return len;
+}
+
+static ssize_t store_fuel_gauge_period(struct kobject *kobj,
+		struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	int tmp;
+
+	mutex_lock(&perf_ctl_mutex);
+
+	if (kstrtouint(buf, 10, &tmp) == 0)
+		if (tmp > 0) /* ms */
+			fuel_gauge_delay = tmp;
+
+	mutex_unlock(&perf_ctl_mutex);
+
+	return count;
+}
+#endif
+
 /*
  * make perf tracker on
  * /sys/devices/system/cpu/perf/enable
@@ -201,8 +303,21 @@ static ssize_t store_perf_enable(struct kobject *kobj,
 static struct kobj_attribute perf_enable_attr =
 __ATTR(enable, 0600, show_perf_enable, store_perf_enable);
 
+#ifdef CONFIG_MTK_GAUGE_VERSION
+static struct kobj_attribute perf_fuel_gauge_enable_attr =
+__ATTR(fuel_gauge_enable, 0600,
+	show_fuel_gauge_enable, store_fuel_gauge_enable);
+static struct kobj_attribute perf_fuel_gauge_period_attr =
+__ATTR(fuel_gauge_period, 0600,
+	show_fuel_gauge_period, store_fuel_gauge_period);
+#endif
+
 static struct attribute *perf_attrs[] = {
 	&perf_enable_attr.attr,
+#ifdef CONFIG_MTK_GAUGE_VERSION
+	&perf_fuel_gauge_enable_attr.attr,
+	&perf_fuel_gauge_period_attr.attr,
+#endif
 	NULL,
 };
 
