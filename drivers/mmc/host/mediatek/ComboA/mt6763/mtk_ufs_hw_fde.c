5a3b8d04c3dc2f2661f5222779f4d4b757d8eaf0
akhil <akhil.misra@mediatek.com>
2019-08-31 09:26:00 +0800

    [ALPS04741967] msdc: Change for F2FS
    Add support for Hardware Inline Encryption
    
    MTK-Commit-Id: 11683ca5fdbac29f3fe2e031d349511886392935
    
    Change-Id: I2282e4497f22dd3bb3be6408e7d2e634afd8d30e
    Signed-off-by: Akhil Misra <akhil.misra@mediatek.com>
    CR-Id: ALPS04741967
    Feature: [Android Default] F2FS File System

--- drivers/mmc/host/mediatek/ComboA/mt6763/mtk_ufs_hw_fde.c
+++ drivers/mmc/host/mediatek/ComboA/mt6763/mtk_ufs_hw_fde.c
@@ -11,6 +11,7 @@
  * GNU General Public License for more details.
  */
 
+#include <linux/hie.h>
 #ifdef CONFIG_MTK_HW_FDE
 #include "mtk_secure_api.h"
 #endif
@@ -34,15 +35,19 @@ enum {
 };
 
 static void msdc_crypto_switch_config(struct msdc_host *host,
-		u32 block_address, u32 dir)
+	struct request *req,
+	u32 block_address, u32 dir)
 {
 	void __iomem *base = host->base;
 	u32 aes_mode_current = 0, aes_sw_reg = 0;
 	u32 ctr[4] = {0};
 	unsigned long polling_tmo = 0;
+	u64 hw_hie_iv_num = 0;
 
 	/* 1. set ctr */
 	aes_sw_reg = MSDC_READ32(EMMC52_AES_EN);
+
+	hw_hie_iv_num = hie_get_iv(req);
 
 	if (aes_sw_reg & EMMC52_AES_SWITCH_VALID0) {
 		MSDC_GET_FIELD(EMMC52_AES_CFG_GP0,
@@ -60,14 +65,28 @@ static void msdc_crypto_switch_config(struct msdc_host *host,
 	case MSDC_CRYPTO_XTS_AES:
 	case MSDC_CRYPTO_AES_CBC_ESSIV:
 	case MSDC_CRYPTO_BITLOCKER:
+	{
+		if (hw_hie_iv_num) {
+			ctr[0] = hw_hie_iv_num & 0xffffffff;
+			ctr[1] = (hw_hie_iv_num >> 32) & 0xffffffff;
+		} else {
 		ctr[0] = block_address;
+		}
 		break;
+	}
 	case MSDC_CRYPTO_AES_ECB:
 	case MSDC_CRYPTO_AES_CBC:
 		break;
 	case MSDC_CRYPTO_AES_CTR:
+	{
+		if (hw_hie_iv_num) {
+			ctr[0] = hw_hie_iv_num & 0xffffffff;
+			ctr[1] = (hw_hie_iv_num >> 32) & 0xffffffff;
+		} else {
 		ctr[0] = block_address;
+		}
 		break;
+	}
 	case MSDC_CRYPTO_AES_CBC_MAC:
 		break;
 	default:
@@ -218,7 +237,7 @@ check_hw_crypto:
 		/* Check data addressw with 16bytes alignment */
 		WARN_ON((host->dma.gpd_addr & 0xf) ||
 		(host->dma.bd_addr & 0xf));
-		msdc_crypto_switch_config(host, blk_addr, dir);
+		msdc_crypto_switch_config(host, mq_rq->req, blk_addr, dir);
 	}
 }
 
