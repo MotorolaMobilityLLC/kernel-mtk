a548c88ac76662a989a5eaffb678ed3a8d9e2c8b
Kobe Wu <kobe-cp.wu@mediatek.com>
2019-08-31 09:28:04 +0800

    [ALPS04763968] sched_mon: fix uninitialized scalar variable
    Set an initial value to local variables.
    
    MTK-Commit-Id: f7d8feeaaceed2db25f02e9208774a54ba8c54bf
    
    Change-Id: Ia6aca787f2a0c3ee3d2a4d4e644f8fb1d5e12d64
    CR-Id: ALPS04763968
    Feature: [Module]Schedule Monitor
    Signed-off-by: Kobe Wu <kobe-cp.wu@mediatek.com>

--- drivers/misc/mediatek/mtprof/prof_main.c
+++ drivers/misc/mediatek/mtprof/prof_main.c
@@ -135,7 +135,7 @@ static ssize_t mt_pid_write(struct file *filp, const char *ubuf,
 	   size_t cnt, loff_t *data)
 {
 	char buf[10];
-	unsigned long val;
+	unsigned long val = 0;
 	int ret;
 	struct task_struct *tsk;
 
@@ -320,7 +320,7 @@ static int mt_signal_log_show(struct seq_file *m, void *v)
 static ssize_t mt_signal_log_write(struct file *filp, const char *ubuf,
 	   size_t cnt, loff_t *data)
 {
-	unsigned long val;
+	unsigned long val = 0;
 	unsigned long update;
 	int ret;
 
@@ -416,7 +416,7 @@ static int mt_fork_exit_log_show(struct seq_file *m, void *v)
 static ssize_t mt_fork_exit_log_write(struct file *filp, const char *ubuf,
 	   size_t cnt, loff_t *data)
 {
-	unsigned long val;
+	unsigned long val = 0;
 	unsigned long update;
 	int ret;
 
