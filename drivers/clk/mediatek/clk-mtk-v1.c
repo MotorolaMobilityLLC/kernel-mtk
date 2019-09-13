cf3352e9fdb35ce6a3f3a5ba0da766702ebd9470
mtk01761 <wendell.lin@mediatek.com>
2019-09-13 08:17:07 +0800

    [ALPS04779807] mt6779 clk: fix coverity
    fix Resource leak
    
    MTK-Commit-Id: b7c8bc5ee092d561e80c087b00da6c5969fea95a
    
    Change-Id: I2b65000a63ce31c6fdd5121ced860cfcb0418e6e
    Signed-off-by: mtk01761 <wendell.lin@mediatek.com>
    CR-Id: ALPS04779807
    Feature: Power Management

--- drivers/clk/mediatek/clk-mtk-v1.c
+++ drivers/clk/mediatek/clk-mtk-v1.c
@@ -59,6 +59,7 @@ struct clk *mtk_clk_register_mux(
 	struct clk_hw *gate_hw = NULL;
 	const struct clk_ops *gate_ops = NULL;
 	u32 mask = BIT(width) - 1;
+	int ret;
 
 #if MT_CCF_DEBUG
 	pr_debug("name: %s, num_parents: %d, gate_bit: %d\n",
@@ -78,8 +79,8 @@ struct clk *mtk_clk_register_mux(
 	if (gate_bit <= MAX_MUX_GATE_BIT) {
 		gate = kzalloc(sizeof(struct clk_gate), GFP_KERNEL);
 		if (!gate) {
-			kfree(mux);
-			return ERR_PTR(-ENOMEM);
+			ret = -ENOMEM;
+			goto err_out;
 		}
 
 		gate->reg = base_addr;
@@ -98,9 +99,14 @@ struct clk *mtk_clk_register_mux(
 		CLK_IGNORE_UNUSED);
 
 	if (IS_ERR(clk)) {
-		kfree(gate);
-		kfree(mux);
+		ret = PTR_ERR(clk);
+		goto err_out;
 	}
 
 	return clk;
+err_out:
+	kfree(gate);
+	kfree(mux);
+
+	return ERR_PTR(ret);
 }
