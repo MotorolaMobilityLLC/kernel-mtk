f64ced969c7909c2fb64eb82e9e7dc3167863f59
Andy Teng <andy.teng@mediatek.com>
2019-09-13 08:15:55 +0800

    [ALPS04705717] mmc: Fix coverity code defect
    Fix Coverity Scanned Code Defect
     Dereference after null check:8259945
       => remove WARN_ON)
     Uninitialized scalar variable:4434788
       => unsigned int cmd_tx = 0
     Uninitialized scalar variable:151333
       => u32 ocr = 0
    
    MTK-Commit-Id: 21563a3b932149b94dc99dc8704cda63e2a4bf00
    
    Change-Id: I97b65af1bdcd46a8154db1e303bb8592a8d389ff
    Signed-off-by: Andy Teng <andy.teng@mediatek.com>
    CR-Id: ALPS04705717
    Feature: eMMC Boot Up

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -2597,7 +2597,7 @@ static const struct mmc_bus_ops mmc_ops = {
 int mmc_attach_mmc(struct mmc_host *host)
 {
 	int err;
-	u32 ocr, rocr;
+	u32 ocr = 0, rocr;
 
 	BUG_ON(!host);
 	WARN_ON(!host->claimed);
