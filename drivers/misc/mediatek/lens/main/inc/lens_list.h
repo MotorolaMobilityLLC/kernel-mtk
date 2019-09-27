ed8f9956394b40654837782c379347c4621ab85a
Huadong.Ni <Huadong.Ni@mediatek.com>
2019-09-27 12:09:47 +0800

    [ALPS04800797] lens: delete lens kernel driver
    delete lens kernel driver
    
    MTK-Commit-Id: 60291f987cef9f9c115fc6b0ee27ac2733481cd0
    
    Change-Id: Idfb0f6ad65ed1d833b991613687b128fc96f9fe4
    Signed-off-by: Huadong.Ni <Huadong.Ni@mediatek.com>
    CR-Id: ALPS04800797
    Feature: [Android Default] AF (contrast AF)

--- drivers/misc/mediatek/lens/main/inc/lens_list.h
+++ drivers/misc/mediatek/lens/main/inc/lens_list.h
@@ -103,17 +103,6 @@ extern long DW9714AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
 extern int DW9714AF_Release(struct inode *a_pstInode, struct file *a_pstFile);
 extern int DW9714AF_GetFileName(unsigned char *pFileName);
 
-#define DW9763AF_SetI2Cclient DW9763AF_SetI2Cclient_Main
-#define DW9763AF_Ioctl DW9763AF_Ioctl_Main
-#define DW9763AF_Release DW9763AF_Release_Main
-#define DW9763AF_GetFileName DW9763AF_GetFileName_Main
-extern int DW9763AF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
-				 spinlock_t *pAF_SpinLock, int *pAF_Opened);
-extern long DW9763AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
-			   unsigned long a_u4Param);
-extern int DW9763AF_Release(struct inode *a_pstInode, struct file *a_pstFile);
-extern int DW9763AF_GetFileName(unsigned char *pFileName);
-
 #define FP5510E2AF_SetI2Cclient FP5510E2AF_SetI2Cclient_Main
 #define FP5510E2AF_Ioctl FP5510E2AF_Ioctl_Main
 #define FP5510E2AF_Release FP5510E2AF_Release_Main
