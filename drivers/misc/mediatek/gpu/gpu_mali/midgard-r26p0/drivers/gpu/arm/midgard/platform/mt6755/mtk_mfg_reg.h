655fe487a6f62bd1a7edefec36b9dcb5614f1e9f
mtk20693 <junyun.zhang@mediatek.com>
2019-09-27 12:04:00 +0800

    [ALPS04790548] GPU: r26p0 Kernel Driver
    Upload r26p0 kernel driver. Add kernel GPU folder
    
    MTK-Commit-Id: 8d42e0e346b98ab02af027be794e1ca1a9f568b1
    
    Change-Id: I1ba9bf7b0f970f11c26559776f5abccd34dcb9d2
    Signed-off-by: mtk20693 <junyun.zhang@mediatek.com>
    CR-Id: ALPS04790548
    Feature: OpenGL|ES

---
+++ drivers/misc/mediatek/gpu/gpu_mali/midgard-r26p0/drivers/gpu/arm/midgard/platform/mt6755/mtk_mfg_reg.h
@@ -0,0 +1,28 @@
+/*
+* Copyright (C) 2016 MediaTek Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+*/
+
+#ifndef __MTK_MFG_REG_H__
+#define __MTK_MFG_REG_H__
+
+/* MT6755 MFG reg */
+#define MFG_DEBUG_SEL   0x180
+#define MFG_DEBUG_A 0x184
+#define MFG_BUS_IDLE_BIT	(1 << 2)
+
+#define MFG_WRITE32(value, addr)	writel(value, addr)
+#define MFG_READ32(addr)	readl(addr)
+#define MFG_DEBUG_CTRL_REG(value)	((value) + MFG_DEBUG_SEL)
+#define MFG_DEBUG_STAT_REG(value)	((value) + MFG_DEBUG_A)
+
+#endif /* __MTK_MFG_REG_H__ */
+
