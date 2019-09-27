6e6f117b68155ed05ae03d1ee6cc4a95b88ec05d
Yuxian Xu <yuxian.xu@mediatek.com>
2019-09-27 12:09:10 +0800

    [ALPS04795421] phone hang: dump vold backtrace
    dump native backtrace of vold when phone hang, especially reboot hang.
    
    MTK-Commit-Id: 7bd3651432b8932102b2123b60992ed9749e6f1e
    
    Signed-off-by: Yuxian Xu <Yuxian.Xu@mediatek.com>
    Change-Id: I1f82c612a3bf7edfa34570c42dee99a0ce1403af
    CR-Id: ALPS04795421
    Feature: Android Exception Engine(AEE)

--- drivers/misc/mediatek/aee/aed/monitor_hang.c
+++ drivers/misc/mediatek/aee/aed/monitor_hang.c
@@ -1504,6 +1504,7 @@ static void hang_dump_backtrace(void)
 			(strcmp(p->comm, "debuggerd64") == 0) ||
 			(strcmp(p->comm, "mmcqd/1") == 0) ||
 			(strcmp(p->comm, "vdc") == 0) ||
+			(strcmp(p->comm, "vold") == 0) ||
 			(strcmp(p->comm, "debuggerd") == 0)) {
 			read_unlock(&tasklist_lock);
 			show_bt_by_pid(p->pid);
