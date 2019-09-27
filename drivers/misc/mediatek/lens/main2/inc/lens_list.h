f913494e58c8d70b3374465838c92418397c2a37
guozuoyu <zuoyu.guo@ontim.cn>
2019-09-27 17:41:19 +0800

    0358478[moto_e6s]合入主后摄s5k3p9sx main2 lens driver St-result:PASS
    Change-Id: I62cd0dbb945b4f7cf2ce5dfc9762997e44e16efe

--- drivers/misc/mediatek/lens/main2/inc/lens_list.h
+++ drivers/misc/mediatek/lens/main2/inc/lens_list.h
@@ -29,6 +29,20 @@ extern int AK7371AF_PowerDown(struct i2c_client *pstAF_I2Cclient,
 				int *pAF_Opened);
 extern int AK7371AF_GetFileName(unsigned char *pFileName);
 
+#define DW9714K_SetI2Cclient DW9714K_SetI2Cclient_Main2
+#define DW9714K_Ioctl DW9714K_Ioctl_Main2
+#define DW9714K_Release DW9714K_Release_Main2
+#define DW9714K_PowerDown DW9714K_PowerDown_Main2
+#define DW9714K_GetFileName DW9714K_GetFileName_Main2
+extern int DW9714K_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+				 spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long DW9714K_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+			   unsigned long a_u4Param);
+extern int DW9714K_Release(struct inode *a_pstInode, struct file *a_pstFile);
+extern int DW9714K_PowerDown(struct i2c_client *pstAF_I2Cclient,
+				int *pAF_Opened);
+extern int DW9714K_GetFileName(unsigned char *pFileName);
+
 #define LC898212XDAF_F_SetI2Cclient LC898212XDAF_F_SetI2Cclient_Main2
 #define LC898212XDAF_F_Ioctl LC898212XDAF_F_Ioctl_Main2
 #define LC898212XDAF_F_Release LC898212XDAF_F_Release_Main2
