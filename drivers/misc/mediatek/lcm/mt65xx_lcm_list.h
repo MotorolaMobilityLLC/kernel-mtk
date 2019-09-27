e5595c48b0fb95451eedfa4d72f37dd5783039d0
meng.wu <Wmeng.Wu@mediatek.com>
2019-09-27 15:08:17 +0800

    [ALPS04748646] lcm: porting back lcm driver from mt8766/mt8768
    [Detail]
    Add panel driver
    
    MTK-Commit-Id: 332d1ced5f5925502584e0aa270e82b571a47f7b
    
    Change-Id: I8102c996962a78f7cca9742380ad9eed59398223
    CR-Id: ALPS04748646
    Feature: [Module]Display Driver
    Signed-off-by: Huijuan Xie <huijuan.xie@mediatek.com>
    Signed-off-by: Jitao Shi <jitao.shi@mediatek.com>

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
@@ -24,6 +24,7 @@ extern struct LCM_DRIVER ontim_ft8006p_hdplus_dsi_vdo_truly_lcm_drv;
 extern struct LCM_DRIVER ontim_ft8006p_hdplus_dsi_vdo_hlt_lcm_drv;
 extern struct LCM_DRIVER otm1282a_hd720_dsi_vdo_60hz_lcm_drv;
 extern struct LCM_DRIVER otm1282a_hd720_dsi_vdo_lcm_drv;
+
 extern struct LCM_DRIVER nt35595_fhd_dsi_cmd_truly_8163_lcm_drv;
 extern struct LCM_DRIVER nt35523_wxga_dsi_vdo_8163_lcm_drv;
 extern struct LCM_DRIVER vvx10f008b00_wuxga_dsi_vdo_lcm_drv;
