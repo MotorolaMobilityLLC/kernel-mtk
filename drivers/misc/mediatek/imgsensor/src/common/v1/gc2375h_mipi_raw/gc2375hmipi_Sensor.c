d143ea778c153f8f658f33115905db933e3bce4e
majianghua <jianghua.ma@ontim.cn>
2019-10-03 13:59:49 +0800

    0359101:camera--info
    St-result： PASS
    
    camera ---info
    
    Change-Id: I8e35bc76bd9cac6353c784097296bf09c6b6744f

--- drivers/misc/mediatek/imgsensor/src/common/v1/gc2375h_mipi_raw/gc2375hmipi_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1/gc2375h_mipi_raw/gc2375hmipi_Sensor.c
@@ -568,6 +568,8 @@ static kal_uint32 set_test_pattern_mode(kal_bool enable)
 	return ERROR_NONE;
 }
 
+extern char backaux_cam_name[64];
+
 static kal_uint32 get_imgsensor_id(UINT32 *sensor_id)
 {
 	kal_uint8 i = 0;
@@ -580,6 +582,8 @@ static kal_uint32 get_imgsensor_id(UINT32 *sensor_id)
 		do {
 			*sensor_id = return_sensor_id();
 			if (*sensor_id == imgsensor_info.sensor_id) {
+				memset(backaux_cam_name, 0x00, sizeof(backaux_cam_name));
+                memcpy(backaux_cam_name, "2_2375h", 64);
 				LOG_INF("i2c write id: 0x%x, sensor id: 0x%x\n", imgsensor.i2c_write_id, *sensor_id);
 				return ERROR_NONE;
 			}
