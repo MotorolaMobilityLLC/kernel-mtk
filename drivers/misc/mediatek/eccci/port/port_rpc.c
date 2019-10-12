28ef3cb1348eb80161692fee177c7788d56bf8d1
xin xu <xin.xu@mediatek.com>
2019-10-12 12:07:27 +0800

    [ALPS04806686] ccci: switch parsing gpio value api by platform
    switch parsing gpio value api dynamically by platform:
    k57plus gpio format in device tree is different from other
    chips, add k57plus gpio value parsing api support.
    
    MTK-Commit-Id: 48da543b4393dde9a25616020ccfc6cb0c09b6cc
    
    Change-Id: Ie86e48d4d8b825c213956142e4ac2fc1878d5157
    Signed-off-by: xin xu <xin.xu@mediatek.com>
    CR-Id: ALPS04806686
    Feature: Modem Interface Driver

--- drivers/misc/mediatek/eccci/port/port_rpc.c
+++ drivers/misc/mediatek/eccci/port/port_rpc.c
@@ -130,8 +130,14 @@ static int get_gpio_id_from_dt(struct device_node *node,
 	int ret;
 
 	/* For new API, there is a shift between AP GPIO ID and MD GPIO ID */
+#ifdef ENABLE_GPIO_FORMAT_SPEC
+	ret = of_property_read_u32(node, gpio_name, &gpio_id);
+	md_view_gpio_id = gpio_id;
+#else
 	gpio_id = of_get_named_gpio(node, gpio_name, 0);
 	ret = of_property_read_u32_index(node, gpio_name, 1, &md_view_gpio_id);
+#endif
+
 	if (ret)
 		return ret;
 
