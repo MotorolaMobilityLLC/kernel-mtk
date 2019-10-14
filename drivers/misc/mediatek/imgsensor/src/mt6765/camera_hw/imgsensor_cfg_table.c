9c7de64bcfb14ea9896084e6f87c4510b4c12b43
wangzhitong <zhitong.wang@ontim.cn>
2019-10-14 18:43:02 +0800

    0359101:0160462 M6102 moto_e6s P0调试 后2M 摄像头PDN 没有按照规格书要求
    St-result： PASS
    
    rear2 camera pnd poweroff delay 6ms
    
    Change-Id: Ia95057dd22752f9549a523d92fa3ea3908eefd7d

--- drivers/misc/mediatek/imgsensor/src/mt6765/camera_hw/imgsensor_cfg_table.c
+++ drivers/misc/mediatek/imgsensor/src/mt6765/camera_hw/imgsensor_cfg_table.c
@@ -192,14 +192,15 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
 #if defined(GC2375H_MIPI_RAW)
 		 {SENSOR_DRVNAME_GC2375H_MIPI_RAW,
 		  {
-		   {PDN, Vol_High, 1,Vol_High,1},
-		   {RST, Vol_Low, 0},
-		   {DOVDD, Vol_1800, 0}, 
+		   {RST, Vol_Low, 1},
+		   {PDN, Vol_High, 6,Vol_Low, 0},
+		   {DOVDD, Vol_1800, 0},
 		  // {DVDD, Vol_1800, 0}, dovdd same
 		   {AVDD, Vol_2800, 0},
 		   {SensorMCLK, Vol_High, 0},
-		   {PDN, Vol_Low, 1},
+		   {PDN, Vol_Low,1,Vol_High, 0},
 		   {RST, Vol_High, 0},
+		   {PDN, Vol_Low, 1,Vol_High, 1},
 		   },
 		  },
 #endif
