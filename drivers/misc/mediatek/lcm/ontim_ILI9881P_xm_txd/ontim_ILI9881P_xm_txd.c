66e2a90b957ec6f55e44f38289bd23fd50a96246
zhangyang <yang.zhang@ontim.cn>
2019-10-17 10:14:50 +0800

    0359949:Repair the moto_e6_play forth LCM ESD issue 2/2
    Disable TE Check， Now TE pin is used for pwm
    
    Change-Id: I9b863e6384307aa402fc2628d1c3501872d2f108
    St-result:Factory test ok
    (cherry picked from commit 54331766856057b6f823c913aa724ee264388489)

--- drivers/misc/mediatek/lcm/ontim_ILI9881P_xm_txd/ontim_ILI9881P_xm_txd.c
+++ drivers/misc/mediatek/lcm/ontim_ILI9881P_xm_txd/ontim_ILI9881P_xm_txd.c
@@ -498,15 +498,15 @@ static void lcm_get_params(LCM_PARAMS *params)
 	params->dsi.ssc_disable = 0;
 	params->dsi.ssc_range = 3;
 	//params->dsi.HS_TRAIL = 15;
-	params->dsi.PLL_CLOCK = 336;    /* this value must be in MTK suggested table */
-	params->dsi.PLL_CK_CMD = 300;    
-	params->dsi.PLL_CK_VDO = 300;     
+	params->dsi.PLL_CLOCK = 336;/* this value must be in MTK suggested table */
+	params->dsi.PLL_CK_CMD = 300;
+	params->dsi.PLL_CK_VDO = 300;
 	params->dsi.CLK_HS_POST = 36;
 
 	params->dsi.clk_lp_per_line_enable = 0;
 	params->dsi.esd_check_enable = 1;
-	params->dsi.customization_esd_check_enable = 0;
-	
+	params->dsi.customization_esd_check_enable = 1;
+
 	params->dsi.lcm_esd_check_table[0].cmd = 0x09;
 	params->dsi.lcm_esd_check_table[0].count = 3;
 	params->dsi.lcm_esd_check_table[0].para_list[0] = 0x81;
