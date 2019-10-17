2c8f3609b64e9030ffea42ac7ea2996b85f04c1c
zhangyang <yang.zhang@ontim.cn>
2019-10-17 16:15:19 +0800

    0359999:Repair the splash screen&refresh rate 2/2
    Change-Id: Id60cef274a4fd7e7a68e034b1aa00307ec5c5ef6
    St-result:PASS

--- drivers/misc/mediatek/lcm/ontim_ft8006p_hdplus_dsi_vdo_truly/ontim_ft8006p_hdplus_dsi_vdo_truly.c
+++ drivers/misc/mediatek/lcm/ontim_ft8006p_hdplus_dsi_vdo_truly/ontim_ft8006p_hdplus_dsi_vdo_truly.c
@@ -214,7 +214,6 @@ static void lcm_get_params(struct LCM_PARAMS *params)
 	params->dsi.mode = SYNC_PULSE_VDO_MODE;
 	params->dsi.switch_mode = CMD_MODE;
 #endif
-	LCM_LOGI("lcm_get_params lcm_dsi_mode %d\n", lcm_dsi_mode);
 	params->dsi.switch_mode_enable = 0;
 	/* DSI */
 	/* Command mode setting */
@@ -229,15 +228,15 @@ static void lcm_get_params(struct LCM_PARAMS *params)
 	params->dsi.packet_size = 256;
 	/* video mode timing */
 	params->dsi.PS = LCM_PACKED_PS_24BIT_RGB888;
-	params->dsi.vertical_sync_active = 8; //old is 2,now is 4
-	params->dsi.vertical_backporch = 106; //old is 8,now is 100
-	params->dsi.vertical_frontporch = 130; //old is 24,now is 124
+	params->dsi.vertical_sync_active = 4; //old is 2,now is 4
+	params->dsi.vertical_backporch = 108; //old is 8,now is 100
+	params->dsi.vertical_frontporch = 132; //old is 24,now is 124
 	params->dsi.vertical_active_line = FRAME_HEIGHT;
-	params->dsi.horizontal_sync_active = 14; //old is 20,now is 8
-	params->dsi.horizontal_backporch = 25;//old is 60,now is 12
-	params->dsi.horizontal_frontporch = 45;//old is 60,now is 16
+	params->dsi.horizontal_sync_active = 20; //old is 20,now is 8
+	params->dsi.horizontal_backporch = 60;//old is 60,now is 12
+	params->dsi.horizontal_frontporch = 60;//old is 60,now is 16
 	params->dsi.horizontal_active_pixel = FRAME_WIDTH;
-	params->dsi.PLL_CLOCK = 270;    /* FrameRate = 60Hz */ /* this value must be in MTK suggested table */
+	params->dsi.PLL_CLOCK = 296;    /* FrameRate = 60Hz */ /* this value must be in MTK suggested table */
 #if 0
 	params->dsi.HS_TRAIL = 7;
 	params->dsi.HS_ZERO = 12;
