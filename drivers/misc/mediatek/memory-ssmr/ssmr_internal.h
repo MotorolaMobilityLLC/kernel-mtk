969f7d2dccd89f75d888580bdf6b457b55fd768d
James Hsu <james.hsu@mediatek.com>
2019-10-24 14:37:59 +0800

    [ALPS04712966] ssmr: add CONFIG_TRUSTONIC_TEE_SUPPORT for svp
    add CONFIG_TRUSTONIC_TEE_SUPPORT for checking SVP status
    
    MTK-Commit-Id: e345da3b5bd6e7f92bb04884fd569b35ed0a7ebd
    
    Change-Id: I4924456e2d6515475b3607553b7c29b6258c8f43
    Signed-off-by: James Hsu <james.hsu@mediatek.com>
    CR-Id: ALPS04712966
    Feature: [Module]SSMR (Shareable Secure Memory Region)

--- drivers/misc/mediatek/memory-ssmr/ssmr_internal.h
+++ drivers/misc/mediatek/memory-ssmr/ssmr_internal.h
@@ -15,6 +15,7 @@
 
 #if defined(CONFIG_MTK_SEC_VIDEO_PATH_SUPPORT) ||\
 	defined(CONFIG_MTK_TEE_GP_SUPPORT) ||\
+	defined(CONFIG_TRUSTONIC_TEE_SUPPORT) ||\
 	defined(CONFIG_MTK_IRIS_SUPPORT) ||\
 	defined(CONFIG_MTK_CAM_SECURITY_SUPPORT)
 #define SSMR_SECMEM_REGION_ENABLE
@@ -132,7 +133,8 @@ struct SSMR_Region {
 
 static struct SSMR_Feature _ssmr_feats[__MAX_NR_SSMR_FEATURES] = {
 #if defined(CONFIG_MTK_SEC_VIDEO_PATH_SUPPORT) ||\
-	defined(CONFIG_MTK_TEE_GP_SUPPORT)
+	defined(CONFIG_MTK_TEE_GP_SUPPORT) ||\
+	defined(CONFIG_TRUSTONIC_TEE_SUPPORT)
 	[SSMR_FEAT_SVP] = {
 		.dt_prop_name = "svp-size",
 		.feat_name = "svp",
