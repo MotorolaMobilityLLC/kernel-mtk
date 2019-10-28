42acab99493ca1cb028957e6989ee009264419cc
guoqiang <qiang.guo@ontim.cn>
2019-10-28 10:02:39 +0800

    0360084:add Secondary supply front camera driver
    St-result: PASS
    
    add Secondary supply front camera driver hi846
    
    Change-Id: I83d014d81c9237f3a2d2c356102ba64d7c6f1097

--- drivers/misc/mediatek/imgsensor/src/mt6765/camera_hw/imgsensor_cfg_table.c
+++ drivers/misc/mediatek/imgsensor/src/mt6765/camera_hw/imgsensor_cfg_table.c
@@ -177,6 +177,22 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
 		   },
 		  },
 #endif
+#if defined(BLACKJACK_TXD_HI846_MIPI_RAW)
+    {
+        SENSOR_DRVNAME_BLACKJACK_TXD_HI846_MIPI_RAW,
+        {
+            {SensorMCLK, Vol_High, 0},
+            {AVDD, Vol_2800, 1},
+            {DOVDD, Vol_1800, 1},
+            {DVDD, Vol_1200, 5},
+//            {AFVDD, Vol_2800, 1},
+//            {PDN, Vol_Low, 0},
+//            {PDN, Vol_High, 0},
+            {RST, Vol_Low, 10},
+            {RST, Vol_High, 1},
+        },
+    },
+#endif
 /*blackjack end*/
 #if defined(S5K3P9SXT_MIPI_RAW)
         {
