024f890ace2d0e95b4b4062d795ee890b219977b
chailu1 <chailu1@lenovo.com>
2019-11-01 17:15:52 +0800

    charger:remove mtk dynamic mivr and atm
    1. remove mtk atm mode
    2. remove dynamic mivr
    
    Change-Id: I4ab5f82d87196ef9b4917bd72f1e48e88a4d135e
    Signed-off-by: chailu1 <chailu1@mt.com>
    Reviewed-on: https://gerrit.mot.com/1447685
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/mediatek/charger/mtk_switch_charging.c
+++ drivers/power/supply/mediatek/charger/mtk_switch_charging.c
@@ -155,13 +155,13 @@ static void swchg_select_charging_current_limit(struct charger_manager *info)
 		pdata->input_current_limit = 200000; /* 200mA */
 		goto done;
 	}
-
+#ifdef MTK_BASE
 	if (info->atm_enabled == true && (info->chr_type == STANDARD_HOST ||
 	    info->chr_type == CHARGING_HOST)) {
 		pdata->input_current_limit = 100000; /* 100mA */
 		goto done;
 	}
-
+#endif
 	if (mtk_is_TA_support_pd_pps(info)) {
 		pdata->input_current_limit =
 			info->data.pe40_single_charger_input_current;
