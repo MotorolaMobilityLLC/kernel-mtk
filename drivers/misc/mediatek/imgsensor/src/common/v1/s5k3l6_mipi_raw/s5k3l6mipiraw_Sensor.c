c5685f7c6edce1c44fd3cd5ce49991cdb28f6f79
leixu <xu.lei@ontim.cn>
2019-11-06 20:16:48 +0800

    0361568:Support PDAF function for S5K3L6[1/2]
    Test case:
    Check pd information.
    Check bpci table information.
    Check PDAF OTP data.
    Check PDAF flow.
    
    St-result: PASS
    
    Change-Id: I7a10109066ee2fdacb4b8c4d1c0ab480a1f965a8

--- drivers/misc/mediatek/imgsensor/src/common/v1/s5k3l6_mipi_raw/s5k3l6mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1/s5k3l6_mipi_raw/s5k3l6mipiraw_Sensor.c
@@ -173,9 +173,9 @@ static struct SENSOR_WINSIZE_INFO_STRUCT imgsensor_winsize_info[5] =
  //for 3l6
 {
 
-	.i4OffsetX = 28,
+	.i4OffsetX = 24,
 
-	.i4OffsetY = 31,
+	.i4OffsetY = 24,
 
 	.i4PitchX = 64,
 
@@ -190,8 +190,10 @@ static struct SENSOR_WINSIZE_INFO_STRUCT imgsensor_winsize_info[5] =
 	.i4PosL = {{28,31},{80,31},{44,35},{64,35},{32,51},{76,51},{48,55},{60,55},{48,63},{60,63},{32,67},{76,67},{44,83},{64,83},{28,87},{80,87}},
 
 	.i4PosR = {{28,35},{80,35},{44,39},{64,39},{32,47},{76,47},{48,51},{60,51},{48,67},{60,67},{32,71},{76,71},{44,79},{64,79},{28,83},{80,83}},
+
 	.i4BlockNumX=65,
-    .i4BlockNumY=48,
+
+	.i4BlockNumY=48,
 };
 
 static kal_uint16 read_cmos_sensor_byte(kal_uint16 addr)
