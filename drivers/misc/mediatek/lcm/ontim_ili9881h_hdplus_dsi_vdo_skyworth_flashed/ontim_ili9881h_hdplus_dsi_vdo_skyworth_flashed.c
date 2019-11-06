ad467d864987a9e1ffee0242f740ac41ce0c6cba
zhangyang <yang.zhang@ontim.cn>
2019-11-06 15:50:43 +0800

    0361358:Add  moto_e6s skyworth flashed LCM
    Verify the flashed LCM ok
    
    Change-Id: I3db9fd708c837b2cd67e5c5cfcf6bd9e5b1e3142
    St-result:PASS

--- drivers/misc/mediatek/lcm/ontim_ili9881h_hdplus_dsi_vdo_skyworth_flashed/ontim_ili9881h_hdplus_dsi_vdo_skyworth_flashed.c
+++ drivers/misc/mediatek/lcm/ontim_ili9881h_hdplus_dsi_vdo_skyworth_flashed/ontim_ili9881h_hdplus_dsi_vdo_skyworth_flashed.c
@@ -267,7 +267,7 @@ static void lcm_get_params(struct LCM_PARAMS *params)
 	params->dsi.lcm_esd_check_table[0].count = 3;
 	params->dsi.lcm_esd_check_table[0].para_list[0] = 0x80;
 	params->dsi.lcm_esd_check_table[0].para_list[1] = 0x03;
-	params->dsi.lcm_esd_check_table[0].para_list[2] = 0x06;
+	params->dsi.lcm_esd_check_table[0].para_list[2] = 0x04;
 	params->dsi.lcm_esd_check_table[1].cmd = 0x54;
 	params->dsi.lcm_esd_check_table[1].count = 1;
 	params->dsi.lcm_esd_check_table[1].para_list[0] = 0x2c;
