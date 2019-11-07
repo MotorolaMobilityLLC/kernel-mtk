1b2a06223564af2e60d0d951ee2cb9ff72b9eb34
Jeremy Chou <jeremy.chou@mediatek.com>
2019-11-07 16:46:45 +0800

    [ALPS04857481] USB: Do not show AEE Warning when QMU error
    [Detail]
    Do not show AEE Warning when QMU error.
    Check error log instead.
    
    MTK-Commit-Id: 76ff05024c687da543281705005e2654828592d0
    
    Change-Id: I57aadddd43134d2a16e5647baf615711f1418f35
    Signed-off-by: Eddie Hung <eddie.hung@mediatek.com>
    Signed-off-by: Jeremy Chou <jeremy.chou@mediatek.com>
    CR-Id: ALPS04857481
    Feature: [Android Default] USB OTG

--- drivers/misc/mediatek/usb20/mtk_qmu.c
+++ drivers/misc/mediatek/usb20/mtk_qmu.c
@@ -395,8 +395,10 @@ void gpd_switch_to_dram(struct device *dev)
 				(void *)(uintptr_t)Tx_gpd_Offset[index],
 				(void *)(uintptr_t)Tx_gpd_Offset_dram);
 		QMU_ERR("%s\n", string);
+#if 0
 #ifdef CONFIG_MTK_AEE_FEATURE
 		aee_kernel_warning(string, string);
+#endif
 #endif
 	}
 
@@ -1641,10 +1643,11 @@ finished:
 			default:
 				break;
 			}
-
+#if 0
 #ifdef CONFIG_MTK_AEE_FEATURE
 			if (val && !skip_val)
 				aee_kernel_warning(string, string);
+#endif
 #endif
 		}
 #endif
@@ -1748,8 +1751,10 @@ finished:
 
 		sprintf(string, "USB20_HOST, TXQ<%d> ERR, CSR:%x", epnum, val);
 		QMU_ERR("%s\n", string);
+#if 0
 #ifdef CONFIG_MTK_AEE_FEATURE
 		aee_kernel_warning(string, string);
+#endif
 #endif
 	}
 
