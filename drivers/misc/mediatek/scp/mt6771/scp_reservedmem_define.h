6841ad0e8a31e1253eca0864a24a48768b5684a4
Leroy Lin <Leroy.Lin@mediatek.com>
2019-11-13 13:18:49 +0800

    [ALPS04854313] SCP: reduce SCP memory usage
    1. Reduce SCP reserved memory from 20 MB to 3.5 MB.
    2. Reduce SCP reserved memory even further to 3 MB for a specific project.
    
    MTK-Commit-Id: d96bab93e67a89e6facff9d4ac697efd33a500d6
    
    Change-Id: I862ae3d9a2c594583c892e9c5b4f1f9884359688
    Signed-off-by: Leroy Lin <Leroy.Lin@mediatek.com>
    CR-Id: ALPS04854313
    Feature: TinySys

--- drivers/misc/mediatek/scp/mt6771/scp_reservedmem_define.h
+++ drivers/misc/mediatek/scp/mt6771/scp_reservedmem_define.h
@@ -36,39 +36,27 @@ static struct scp_reserve_mblock scp_reserve_mblock[] = {
 		.num = SENS_MEM_ID,
 		.start_phys = 0x0,
 		.start_virt = 0x0,
-		.size = 0x800000,/*8MB*/
+		.size = 0x100000,  /* 1 MB */
 	},
 #ifdef CONFIG_MTK_AUDIO_TUNNELING_SUPPORT
 	{
 		.num = MP3_MEM_ID,
 		.start_phys = 0x0,
 		.start_virt = 0x0,
-		.size = 0x400000,/*4MB*/
+		.size = 0x400000,  /* 4 MB */
 	},
 #endif
 	{
 		.num = FLP_MEM_ID,
 		.start_phys = 0x0,
 		.start_virt = 0x0,
-		.size = 0x1000,/*4KB*/
-	},
-	{
-		.num = RTOS_MEM_ID,
-		.start_phys = 0x0,
-		.start_virt = 0x0,
-		.size = 0x100000,/*1MB*/
-	},
-	{
-		.num = SENS_MEM_DIRECT_ID,
-		.start_phys = 0x0,
-		.start_virt = 0x0,
-		.size = 0x2000,/*8KB*/
+		.size = 0x1000,  /* 4 KB */
 	},
 	{
 		.num = SCP_A_LOGGER_MEM_ID,
 		.start_phys = 0x0,
 		.start_virt = 0x0,
-		.size = 0x200000,/*2MB*/
+		.size = 0x180000,  /* 1.5 MB */
 	},
 	{
 		.num = AUDIO_IPI_MEM_ID,
@@ -81,7 +69,7 @@ static struct scp_reserve_mblock scp_reserve_mblock[] = {
 		.num = SPK_PROTECT_MEM_ID,
 		.start_phys = 0x0,
 		.start_virt = 0x0,
-		.size = 0x30000,/*192KB*/
+		.size = 0x30000,  /* 192 KB */
 	},
 #endif
 #ifdef CONFIG_MTK_VOW_BARGE_IN_SUPPORT
