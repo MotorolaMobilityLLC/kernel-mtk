c5b9232cfd716bc629865b33c552003ac2543829
wangzhongxin <zhongxin.wang@ontim.cn>
2019-11-18 18:55:10 +0800

    ROW-981: resovle focal tp jump point
    Description:
    focal moto_e6s version is 0xoc,moto_e6s is 0x12.
    
    Change-Id: I6a1d9fdbc1bbfb9d193d2842f3039bdfeb62c0f6
    
    St-result:pass

--- drivers/input/touchscreen/mediatek/focaltech_touch/focaltech_config.h
+++ drivers/input/touchscreen/mediatek/focaltech_touch/focaltech_config.h
@@ -230,13 +230,13 @@
  * define your own fw_file, the sample one to be replaced is invalid
  * NOTE: if FTS_GET_MODULE_NUM > 1, it's the fw corresponding with FTS_VENDOR_ID
  */
-#define FTS_UPGRADE_FW_FILE                      "include/firmware/blackjack_ft8006p_v11_all.i"
+#define FTS_UPGRADE_FW_FILE                      "include/firmware/blackjack_ft8006p_v12_all.i"
 
 /*
  * if FTS_GET_MODULE_NUM >= 2, fw corrsponding with FTS_VENDOR_ID2
  * define your own fw_file, the sample one is invalid
  */
-#define FTS_UPGRADE_FW2_FILE                     "include/firmware/fiji_ft8006p_v0b_all.i"
+#define FTS_UPGRADE_FW2_FILE                     "include/firmware/fiji_ft8006p_v0c_all.i"
 
 /*
  * if FTS_GET_MODULE_NUM >= 3, fw corrsponding with FTS_VENDOR_ID3
