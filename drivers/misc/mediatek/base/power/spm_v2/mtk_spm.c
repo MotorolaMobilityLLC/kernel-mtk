6e22f5d9a8f6ca2f8b69089bf30f12cbaf8c060d
sparsh <sparsh.birla@mediatek.com>
2019-11-22 09:35:54 +0800

    [ALPS04853122] SPM: Initialize irq type of edge irqs
    [Detail]
    Initialize irq type to avoid pending irqs.
    
    MTK-Commit-Id: 6c7d458068f4caa6311ccf2b337f2b9a65243efb
    
    Change-Id: Ie1d179d9e6ab7800840ebad8e9e9955e096ec96d
    Signed-off-by: Sparsh Birla <sparsh.birla@mediatek.com>
    CR-Id: ALPS04853122
    Feature: Power Management

--- drivers/misc/mediatek/base/power/spm_v2/mtk_spm.c
+++ drivers/misc/mediatek/base/power/spm_v2/mtk_spm.c
@@ -675,6 +675,8 @@ static void spm_register_init(void)
 int __init spm_module_init(void)
 {
 	int r = 0;
+	int i;
+	unsigned int irq_type;
 #if defined(CONFIG_MACH_MT6757) || defined(CONFIG_MACH_KIBOPLUS)
 #if !defined(CONFIG_FPGA_EARLY_PORTING)
 	u32 reg_val;
@@ -693,6 +695,14 @@ int __init spm_module_init(void)
 	if (spm_fs_init() != 0)
 		r = -EPERM;
 #endif
+
+	/* Note: Initialize irq type to avoid pending irqs */
+	for (i = 0; i < NF_EDGE_TRIG_IRQS; i++) {
+		if (edge_trig_irqs[i]) {
+			irq_type = irq_get_trigger_type(edge_trig_irqs[i]);
+			irq_set_irq_type(edge_trig_irqs[i], irq_type);
+		}
+	}
 
 #ifdef CONFIG_FAST_CIRQ_CLONE_FLUSH
 	set_wakeup_sources(edge_trig_irqs, NF_EDGE_TRIG_IRQS);
