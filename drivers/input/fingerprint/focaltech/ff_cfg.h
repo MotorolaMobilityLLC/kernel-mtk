64356fd156b5d2b413a1373985c9aedcde83f15b
dujiali <jiali.du@ontim.cn>
2019-12-10 20:44:03 +0800

    0365219:bjQ,fpsensor,bringup(driver3/7)
    St-result: PASS
    
    Description:
    1.fpsensor bring up for moto g⁸ power lite64 Android Q
    
    Change-Id: I445ca490d6b43f76eced67a854cc5a01fbcaf46b

---
+++ drivers/input/fingerprint/focaltech/ff_cfg.h
@@ -0,0 +1,23 @@
+  
+/******************************************************************************
+  Copyright (c), 1991-2007, My Company.
+  
+  文件: ff_cfg.h
+  描述: 
+  函数: //List main functions
+  版本:
+         1. Author : 
+            Date   : 2019-9-11
+            Changes: First create file
+ ******************************************************************************/
+
+
+#ifndef __FF_CFG_H__
+#define __FF_CFG_H__
+
+#define FF_CFG_BEANPOD_VERSION 300
+
+#endif /* __FF_CFG_H__ */
+
+
+
