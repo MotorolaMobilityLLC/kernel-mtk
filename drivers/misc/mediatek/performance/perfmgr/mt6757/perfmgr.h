1ee2e542e40f7600785d0e3034eb846eb9dbf25d
Ian-Y Chen <ian-y.chen@mediatek.com>
2019-12-19 11:04:01 +0800

    [ALPS04862730] perfmgr: add touch boost
    [Detail]
    Implement touch boost for mt6757
    
    MTK-Commit-Id: 4c029e8eb1843b192014984302800c0bbad7ba6f
    
    Change-Id: Idde78acaa015f5fabdb4d40bc44007257a043837
    Signed-off-by: Ian-Y Chen <ian-y.chen@mediatek.com>
    CR-Id: ALPS04862730
    Feature: [Module]PowerHAL

---
+++ drivers/misc/mediatek/performance/perfmgr/mt6757/perfmgr.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __PERFMGR_H__
+#define __PERFMGR_H__
+
+enum {
+	FORCELIMIT_TYPE_SP     = 0,
+	FORCELIMIT_TYPE_VR     = 1,
+	FORCELIMIT_TYPE_CPUSET = 2,
+	FORCELIMIT_TYPE_NUM    = 3,
+};
+
+struct forcelimit_data {
+	int min_core;
+	int max_core;
+};
+
+extern int init_perfmgr_touch(void);
+extern int perfmgr_touch_suspend(void);
+
+extern int  perfmgr_get_target_core(void);
+extern int  perfmgr_get_target_freq(void);
+
+extern void init_perfmgr_boost(void);
+extern void perfmgr_boost(int enable, int core, int freq);
+
+extern void perfmgr_forcelimit_cpuset_cancel(void);
+extern void perfmgr_forcelimit_cpu_core(int type, int cluster_num,
+	struct forcelimit_data *data);
+
+#endif				/* !__PERFMGR_H__ */
