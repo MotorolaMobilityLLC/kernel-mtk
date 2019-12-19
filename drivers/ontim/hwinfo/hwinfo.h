c3c4d3d73f23b050ddd7c36d0593992850df295e
shabei <bei.sha@ontim.cn>
2019-12-19 20:23:39 +0800

    0363370:add hwinfo of RF_GPIO
    Description:
    Add hwinfo of RF_GPIO for boot with RF line
    
    RootCause:
    Free text here...
    
    Analysis:
    Free text here...
    
    Solution:
    Free text here...
    
    Test Impact:
    Free text here...
    
    Side Effect:
    Free text here...
    
    St-result:PASS
    
    Change-Id: I980a1117c870a1ad17c285889250db259679fd8a

--- drivers/ontim/hwinfo/hwinfo.h
+++ drivers/ontim/hwinfo/hwinfo.h
@@ -56,7 +56,7 @@ KEYWORD(FRONT_CAM_EFUSE)        //camera  efuse
 KEYWORD(FRONTAUX_CAM_EFUSE)        //camera  efuse
 KEYWORD(CARD_HOLDER_PRESENT)  //card hold detect
 KEYWORD(CHARGER_IC_MFR) //houzn add
-
+KEYWORD(RF_GPIO)
 KEYWORD(current_cpuid)// current cpuid
 
 #ifdef KEYWORD_ENUM
