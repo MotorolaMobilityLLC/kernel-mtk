b4706d8246c292d2a6b5b00f473debe043bd8b32
guozuoyu <zuoyu.guo@ontim.cn>
2020-01-16 14:48:34 +0800

    0358478[moto_g8]porting camera,add mono camera for FF 2/4
    St-result:PASS
    
    sun_gc02m1b and tsp_gc02m1b
    
    Change-Id: If603aeb595bfc81bdb94e831ed76ebc905d5269c

--- drivers/misc/mediatek/imgsensor/src/mt6765/camera_hw/imgsensor_cfg_table.c
+++ drivers/misc/mediatek/imgsensor/src/mt6765/camera_hw/imgsensor_cfg_table.c
@@ -162,6 +162,48 @@ struct IMGSENSOR_HW_POWER_SEQ platform_power_sequence[] = {
 /* Legacy design */
 struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
 /*blackjack*/
+#if defined(BLACKJACK_SUN_GC02M1B_MIPI_RAW)
+    {SENSOR_DRVNAME_BLACKJACK_SUN_GC02M1B_MIPI_RAW,
+        {   
+            {RST, Vol_Low, 1},
+            {PDN, Vol_High, 6,Vol_Low, 0},
+            {DOVDD, Vol_1800, 0},
+            {AVDD, Vol_2800, 0},
+            {SensorMCLK, Vol_High, 0},
+            {PDN, Vol_Low,1,Vol_High, 0},
+            {RST, Vol_High, 0},
+            {PDN, Vol_Low, 1,Vol_High, 1},
+        },  
+    },  
+#endif
+#if defined(BLACKJACK_TSP_GC02M1B_MIPI_RAW)
+    {SENSOR_DRVNAME_BLACKJACK_TSP_GC02M1B_MIPI_RAW,
+        {   
+            {RST, Vol_Low, 1}, 
+            {PDN, Vol_High, 6,Vol_Low, 0}, 
+            {DOVDD, Vol_1800, 0}, 
+            {AVDD, Vol_2800, 0}, 
+            {SensorMCLK, Vol_High, 0}, 
+            {PDN, Vol_Low,1,Vol_High, 0}, 
+            {RST, Vol_High, 0}, 
+            {PDN, Vol_Low, 1,Vol_High, 1}, 
+        },  
+    },  
+#endif
+#if defined(BLACKJACK_SUN_GC02M1B_MIPI_RAW)
+    {SENSOR_DRVNAME_BLACKJACK_SUN_GC02M1B_MIPI_RAW,
+        {    
+            {RST, Vol_Low, 1},
+            {PDN, Vol_High, 6,Vol_Low, 0},
+            {DOVDD, Vol_1800, 0},
+            {AVDD, Vol_2800, 0},
+            {SensorMCLK, Vol_High, 0},
+            {PDN, Vol_Low,1,Vol_High, 0},
+            {RST, Vol_High, 0},
+            {PDN, Vol_Low, 1,Vol_High, 1},
+        },   
+    },   
+#endif
 #if defined(BLACKJACK_HLT_OV16A10_MIPI_RAW)
 		 {SENSOR_DRVNAME_BLACKJACK_HLT_OV16A10_MIPI_RAW,
 		  {
