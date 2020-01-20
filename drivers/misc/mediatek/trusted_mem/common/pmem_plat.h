3ecec77358cc7a102d045a14593953a639df8a71
Coboy Chen <coboy.chen@mediatek.com>
2020-01-20 20:38:53 +0800

    [ALPS04969629] tmem: remove mock support
    Code refactoring of removing mock support including
    mock device and a simple buddy memory manager. These
    code are not required anymore.
    
    MTK-Commit-Id: 746ab29e0fe051d3a04724e5b8a75b230871d823
    
    Change-Id: Ib77660411a179b0233fafbb9f01cacce230109b1
    Signed-off-by: Coboy Chen <coboy.chen@mediatek.com>
    CR-Id: ALPS04969629
    Feature: GenieZone

--- drivers/misc/mediatek/trusted_mem/common/pmem_plat.h
+++ drivers/misc/mediatek/trusted_mem/common/pmem_plat.h
@@ -16,29 +16,6 @@
 
 #include "private/tmem_utils.h"
 
-#ifdef PMEM_MOCK_OBJECT_SUPPORT
-/**********************************************************/
-/***************** MOCK SUPPORT ***************************/
-/**********************************************************/
-
-/* Use in development phase only (SSMR is mock) */
-#define PMEM_FORCE_MOCK_SSMR_SUPPORT (0)
-#if PMEM_FORCE_MOCK_SSMR_SUPPORT
-#define PMEM_MOCK_SSMR
-#endif
-
-/* Use in development phase only (MTEE is mock) */
-#define PMEM_FORCE_MOCK_MTEE_SUPPORT (0)
-#if PMEM_FORCE_MOCK_MTEE_SUPPORT
-#define PMEM_MOCK_MTEE
-#else
-#if !defined(CONFIG_MTK_GZ_KREE)
-#define PMEM_MOCK_MTEE
-#endif /* end of #if !defined(CONFIG_MTK_GZ_KREE) */
-#endif /* end of #if PMEM_FORCE_MOCK_MTEE_SUPPORT */
-
-#endif /* end of #ifdef PMEM_MOCK_OBJECT_SUPPORT */
-
 /**********************************************************/
 /**********************************************************/
 /**********************************************************/
@@ -47,25 +24,7 @@
 #define PMEM_MTEE_SESSION_KEEP_ALIVE
 #endif
 
-#define PMEM_ALIGNMENT_CHECK_ON_CLIENT_SIDE (0)
-#if PMEM_ALIGNMENT_CHECK_ON_CLIENT_SIDE
-#define PMEM_ALIGNMENT_CHECK
-#endif
-
-#define PMEM_MINIMAL_SIZE_CHECK_ON_CLIENT_SIDE (0)
-#if PMEM_MINIMAL_SIZE_CHECK_ON_CLIENT_SIDE
-#define PMEM_MIN_SIZE_CHECK
-#endif
-
 #define PMEM_64BIT_PHYS_SHIFT (10)
 #define PMEM_PHYS_LIMIT_MIN_ALLOC_SIZE (1 << PMEM_64BIT_PHYS_SHIFT)
-
-#if defined(PMEM_MOCK_MTEE)
-/* for mock buddy memmgr */
-#define PMEM_MIN_ALLOC_CHUNK_SIZE SIZE_1K
-#else
-/* for GZ */
-#define PMEM_MIN_ALLOC_CHUNK_SIZE SIZE_2M
-#endif
 
 #endif /* end of PMEM_PLAT_H */
