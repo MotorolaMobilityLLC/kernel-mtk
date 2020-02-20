bfbdfa85e841e9e4fd967e759422be8a9c873cd6
OSS3_ME5 <haizhen.wang@mediatek.com>
2020-02-20 10:19:23 +0800

    [ALPS04846574] [DISP]: add dynfps info
    add dynfps info
    
    MTK-Commit-Id: d99cf272016cba639becb3cc59752366cef57662
    
    Change-Id: I1d8aa2f7e8185a256f6eb13b8b325b795c7e4f31
    Signed-off-by: OSS3_ME5 <haizhen.wang@mediatek.com>
    CR-Id: ALPS04846574
    Feature: Display C-PHY
    (cherry picked from commit 32d8253b77d8722d86af04c071449b338d665752)

--- drivers/misc/mediatek/video/include/disp_session.h
+++ drivers/misc/mediatek/video/include/disp_session.h
@@ -342,6 +342,9 @@ struct disp_frame_cfg_t {
 	unsigned int hrt_idx;
 
 	bool hbm_en;
+
+	/*DynFPS*/
+	int active_config;
 };
 
 struct disp_session_info {
@@ -502,6 +505,9 @@ struct disp_layer_info {
 	int res_idx;
 	unsigned int hrt_weight;
 	unsigned int hrt_idx;
+
+	/*DynFPS*/
+	int active_config_id;
 };
 
 enum DISP_SCENARIO {
@@ -530,6 +536,21 @@ enum DISP_SELF_REFRESH_TYPE {
 	REFRESH_FOR_IDLE,
 	REFRESH_TYPE_NUM,
 };
+
+
+/*DynFPS start*/
+#define MULTI_CONFIG_NUM 2
+struct dyn_config_info {
+	unsigned int vsyncFPS;
+	unsigned int width;
+	unsigned int height;
+};
+
+struct multi_configs {
+	unsigned int config_num;
+	struct dyn_config_info dyn_cfgs[MULTI_CONFIG_NUM];
+};
+/*DynFPS end*/
 
 /* IOCTL commands. */
 #define DISP_IOW(num, dtype)     _IOW('O', num, dtype)
