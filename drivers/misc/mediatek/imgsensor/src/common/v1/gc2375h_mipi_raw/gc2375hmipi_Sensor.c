10b1fe0cda67fda3b14dfa49ff258f2c442133f3
majianghua <jianghua.ma@chino-e.com>
2020-04-13 14:40:55 +0800

    0385076: moto_g8--camera bring up
    St-result： PASS
    
        ov13855,gc5035,gc2375
    
    Change-Id: Ib9d15105b9aecc4174155335e68cb5494991dd23

--- drivers/misc/mediatek/imgsensor/src/common/v1/gc2375h_mipi_raw/gc2375hmipi_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1/gc2375h_mipi_raw/gc2375hmipi_Sensor.c
@@ -695,6 +695,9 @@ static kal_uint32 set_test_pattern_mode(kal_bool enable)
  * GLOBALS AFFECTED
  *
  *************************************************************************/
+
+extern char backaux_cam_name[64];
+
 static kal_uint32 get_imgsensor_id(UINT32 *sensor_id)
 {
 	kal_uint8 i = 0;
@@ -707,7 +710,9 @@ static kal_uint32 get_imgsensor_id(UINT32 *sensor_id)
 		do {
 			*sensor_id = return_sensor_id();
 			if (*sensor_id == imgsensor_info.sensor_id) {
-				cam_pr_debug("i2c write id: 0x%x, sensor id: 0x%x\n",
+					memset(backaux_cam_name, 0x00, sizeof(backaux_cam_name));
+					memcpy(backaux_cam_name, "2_ak57_jsl_gc2375h", 64);
+					cam_pr_debug("ak57_jsl_gc2375h i2c write id: 0x%x, sensor id: 0x%x\n",
 					imgsensor.i2c_write_id, *sensor_id);
 				return ERROR_NONE;
 			}
