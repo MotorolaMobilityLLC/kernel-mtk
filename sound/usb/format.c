66b2da6f46f7750ac5ec03360c00248aba773156
haolei <lei.hao@ontim.cn>
2020-04-22 16:09:48 +0800

    moto_g(8)_power_liteQ-1153:[moto_g(8)_power_lite][CTS][CTS Verifier_10.0-R3]USB Audio Peripheral Record Test fail St-result:PASS
    Change-Id: I3bcd614e719f4cc0e4b2040c683452b8e0fcd0da
    Signed-off-by: haolei <lei.hao@ontim.cn>

--- sound/usb/format.c
+++ sound/usb/format.c
@@ -199,6 +199,10 @@ static int parse_audio_format_rates_v1(struct snd_usb_audio *chip, struct audiof
 			     chip->usb_id == USB_ID(0x041e, 0x4068)))
 				rate = 8000;
 
+			/* AudioBox 22 VSL */
+			if (rate > 48000 && chip->usb_id == USB_ID(0x194f, 0x0101))
+				continue;
+
 			fp->rate_table[fp->nr_rates] = rate;
 			if (!fp->rate_min || rate < fp->rate_min)
 				fp->rate_min = rate;
@@ -256,6 +260,11 @@ static int parse_uac2_sample_rate_range(struct snd_usb_audio *chip,
 		}
 
 		for (rate = min; rate <= max; rate += res) {
+
+			/* AudioBox 22 VSL */
+			if (rate > 48000 && chip->usb_id == USB_ID(0x194f, 0x0101))
+				break;
+
 			if (fp->rate_table)
 				fp->rate_table[nr_rates] = rate;
 			if (!fp->rate_min || rate < fp->rate_min)
