e3e11f26fe0fdd497aa537e48b755c2d2315d9ca
Eddie Hung <eddie.hung@mediatek.com>
2020-04-29 22:14:00 +0800

    [ALPS05085228] usb: fix usb audio peripheral record test fail
    [Detail]
    Apply several patchs to fix this issue.
    
    1. There is a lot of dump_tx_ops log. it may cause performance issue.
    It is legacy code and can bypass it.
    
    2. Increase resource for usb QMU.
    
    3. Force switch Audiobox 22 VSL sample rate to 48k
    
    MTK-Commit-Id: db59de60cc554fa88e77eb19c5174ea6561d86f5
    
    Change-Id: I68e1210c2a42ae99e4eacddb6ff10357ea4acb72
    Signed-off-by: Eddie Hung <eddie.hung@mediatek.com>
    CR-Id: ALPS05085228
    Feature: [Android Default] USB OTG

--- sound/usb/format.c
+++ sound/usb/format.c
@@ -198,6 +198,10 @@ static int parse_audio_format_rates_v1(struct snd_usb_audio *chip, struct audiof
 			    (chip->usb_id == USB_ID(0x041e, 0x4064) ||
 			     chip->usb_id == USB_ID(0x041e, 0x4068)))
 				rate = 8000;
+			/* AudioBox 22 VSL */
+			if (rate > 48000 &&
+				chip->usb_id == USB_ID(0x194f, 0x0101))
+				continue;
 
 			fp->rate_table[fp->nr_rates] = rate;
 			if (!fp->rate_min || rate < fp->rate_min)
@@ -256,6 +260,11 @@ static int parse_uac2_sample_rate_range(struct snd_usb_audio *chip,
 		}
 
 		for (rate = min; rate <= max; rate += res) {
+			/* AudioBox 22 VSL */
+			if (rate > 48000 &&
+				chip->usb_id == USB_ID(0x194f, 0x0101))
+				break;
+
 			if (fp->rate_table)
 				fp->rate_table[nr_rates] = rate;
 			if (!fp->rate_min || rate < fp->rate_min)
@@ -382,7 +391,7 @@ static int parse_audio_format_i(struct snd_usb_audio *chip,
 		switch (chip->usb_id) {
 
 		case USB_ID(0x0763, 0x2003): /* M-Audio Audiophile USB */
-			if (chip->setup == 0x00 && 
+			if (chip->setup == 0x00 &&
 			    fp->altsetting == 6)
 				pcm_format = SNDRV_PCM_FORMAT_S16_BE;
 			else
