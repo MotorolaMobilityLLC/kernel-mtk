84a785be9ecb96b9479839b1e2ef6704ccdc4744
Ming-Fan Chen <ming-fan.chen@mediatek.com>
2020-05-22 10:15:22 +0800

    [ALPS05102504] cmdq: enhance protect
    Enhance code scan protection.
    
    MTK-Commit-Id: 40241f9aaa1a6dda26ab43f289c10f6fc9c218b2
    
    Change-Id: I4667c5aa46e23ea778aef0d6273dad6a32561572
    Signed-off-by: Ming-Fan Chen <ming-fan.chen@mediatek.com>
    CR-Id: ALPS05102504
    Feature: [Module]CMDQ Driver

--- drivers/misc/mediatek/cmdq/v3/cmdq_helper_ext.h
+++ drivers/misc/mediatek/cmdq/v3/cmdq_helper_ext.h
@@ -827,7 +827,8 @@ u32 cmdq_event_get_table_size(void);
 
 /* CMDQ core feature functions */
 
-bool cmdq_core_check_user_valid(void *src, u32 size);
+bool cmdq_core_check_user_valid(void *src, u32 size,
+	struct cmdqRecStruct *handle);
 
 void cmdq_core_deinit_group_cb(void);
 
