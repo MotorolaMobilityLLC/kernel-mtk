9894e6618b7486c8d5e6f265c97b1a70499fab05
wangzhitong <zhitong.wang@chino-e.com>
2020-06-23 15:50:48 +0800

    0402376:melta camera add motor & modify mid
    St-result： PASS
    
    melta camera add motor
    
    Change-Id: I0d3936e8af261c32a4bc00cf80b883b967e00044

--- drivers/misc/mediatek/lens/main/inc/lens_list.h
+++ drivers/misc/mediatek/lens/main/inc/lens_list.h
@@ -92,6 +92,14 @@ extern int BU63169AF_PowerDown(struct i2c_client *pstAF_I2Cclient,
 extern int BU63169AF_GetFileName(unsigned char *pFileName);
 #endif
 
+extern int DW9767AF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+				 spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long DW9767AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+			   unsigned long a_u4Param);
+extern int DW9767AF_Release(struct inode *a_pstInode, struct file *a_pstFile);
+extern int DW9767AF_GetFileName(unsigned char *pFileName);
+
+
 #define DW9714AF_SetI2Cclient DW9714AF_SetI2Cclient_Main
 #define DW9714AF_Ioctl DW9714AF_Ioctl_Main
 #define DW9714AF_Release DW9714AF_Release_Main
