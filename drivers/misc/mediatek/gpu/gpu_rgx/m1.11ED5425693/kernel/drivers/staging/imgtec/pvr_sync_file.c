f5d936ccfa2d80b46f2a96317a15b114c7467406
Huan Feng <huan.feng@mediatek.com>
2020-07-23 10:46:23 +0800

    [ALPS05100347]  GPU:Remove extra fence put
    [Detail]
    pvr_fence_destroy() will decrement the refcount first because its
    synchronous so it will reach 0 and the fence will be put into the
    deferred_free_list in pvr_fence.c and then freed with
    dma_fence_free() in a workqueue. Then the fput() is going to be
    executed from a different workqueue (than the one in pvr_fence.c)
    and decrement the refcount once more. By that time the memory of
    the fence would be probably already freed and this last fence_put()
    would overwrite a freed memory (or even possibly allocated by
    someone else).
    
    [Ticket]
    125418
    
    [Solution]
    Remove the extra dma_fence_put() called by pvr_fence_destroy().
    
    MTK-Commit-Id: 3b0f5814d5804c991346b7bc3ec1daf15d568a44
    
    Change-Id: I91137637d12ca498426839a7a2cb57eff49f8dc3
    CR-Id: ALPS05100347
    Feature: OpenGL|ES
    Signed-off-by: Huan Feng <huan.feng@mediatek.com>

--- drivers/misc/mediatek/gpu/gpu_rgx/m1.11ED5425693/kernel/drivers/staging/imgtec/pvr_sync_file.c
+++ drivers/misc/mediatek/gpu/gpu_rgx/m1.11ED5425693/kernel/drivers/staging/imgtec/pvr_sync_file.c
@@ -431,9 +431,6 @@ pvr_sync_rollback_fence_data(PVRSRV_FENCE fence_to_rollback,
 	fput(sync_file->file);
 
 	put_unused_fd(fence_to_rollback);
-
-	pvr_fence_destroy(pvr_fence);
-
 	return PVRSRV_OK;
 }
 
