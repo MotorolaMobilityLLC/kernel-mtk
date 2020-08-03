f24519937a7e8ad1819f6469556591e76854d2f7
wangzhitong <zhitong.wang@chino-e.com>
2020-08-03 19:16:32 +0800

    0402376:melta camera ov02b10 kernel
    St-result： PASS
    
    melta add ov02b10
    
    Change-Id: Ic785c131f79a0cf4da5f99f184108847b9355854

--- drivers/misc/mediatek/imgsensor/src/mt6765/camera_hw/imgsensor_cfg_table.c
+++ drivers/misc/mediatek/imgsensor/src/mt6765/camera_hw/imgsensor_cfg_table.c
@@ -491,6 +491,20 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
         },
     },
 #endif
+#if defined(MALTA_SUN_OV02B10_MIPI_RAW)
+    {SENSOR_DRVNAME_MALTA_SUN_OV02B10_MIPI_RAW,
+        {
+            {PDN,Vol_Low, 0},
+            {RST,Vol_Low, 0},
+            {DOVDD, Vol_1800, 0},
+            {AVDD, Vol_2800, 1},
+            {PDN,Vol_Low, 0,Vol_Low,0},
+            {SensorMCLK, Vol_High, 1},
+            {PDN,Vol_High, 5,Vol_High,0},
+            {RST, Vol_High, 4},
+        },
+    },
+#endif
 /* melta end*/
 #if defined(IMX398_MIPI_RAW)
 	{
