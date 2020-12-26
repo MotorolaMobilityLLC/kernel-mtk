5e4e7b8cc1d96d78f57c8cbd7e62dbdfd16a8c1d
rd.shitao.chen <shitao.chen@chino-e.com>
2020-12-26 09:53:10 +0800

    moto e(7)L-619:[moto e(7)_lite_32_64]update hlt_icn9911c tp fw to 15.0
    Change-Id: I61b076233db25cc0b4c0aab64bd5bdc476a73041
    St-result: PASS

--- drivers/input/touchscreen/mediatek/chipone_tddi_v131/cts_builtin_firmware.h
+++ drivers/input/touchscreen/mediatek/chipone_tddi_v131/cts_builtin_firmware.h
@@ -1,5 +1,5 @@
 static u8 icnl9911_driver_builtin_firmware[] = {
-#include"firmware/hlt/ICNL9911C_HLT_MOTO_MALTA_20201221_V020D.h"
+#include"firmware/hlt/ICNL9911C_HLT_MOTO_MALTA_20201225_V020F.h"
 };
 const static struct cts_firmware cts_driver_builtin_firmwares[] = {
     {
