2b4f6b2d8573e54e862542a93548874f99ab753c
Saurabh Jairath <saurabh.jairath@mediatek.com>
2021-01-21 14:01:30 +0800

    [ALPS05499581] disp: mt6763: remove ion mapmva heap usage
    1. ION will remove mapmva heap for security concern.
    2. Display action:
       i: [Frame Buffer] Remove use mapmva heap, use m4u intead of ION
      ii: [Screen Capture] Remove use ion to reuse vmalloc buffer,
                            alloc new by ION, set it, copy to dst buffer.
    
    MTK-Commit-Id: 977107f3456adecd82ec1945248a722a9784398b
    
    Change-Id: Id3091f951305c01dbc8bfe88e241da14d0043d45
    Signed-off-by: Saurabh Jairath <saurabh.jairath@mediatek.com>
    CR-Id: ALPS05499581
    Feature: [Module]Display Driver
    (cherry picked from commit 96647368d71cb3565f6a64fc0596cf4fae678036)
    (cherry picked from commit 1e6e7ab07adc909b9928c283ffb7e356de34f8ee)

--- drivers/misc/mediatek/video/mt6763/videox/debug.c
+++ drivers/misc/mediatek/video/mt6763/videox/debug.c
@@ -498,7 +498,6 @@ static int alloc_buffer_from_dma(size_t size, struct test_buf_info *buf_info)
 	unsigned long size_align;
 	unsigned int mva = 0;
 
-#ifndef CONFIG_MTK_IOMMU
 	size_align = round_up(size, PAGE_SIZE);
 
 	buf_info->buf_va = dma_alloc_coherent(disp_get_device(), size,
@@ -529,31 +528,6 @@ static int alloc_buffer_from_dma(size_t size, struct test_buf_info *buf_info)
 		if (ret)
 			pr_debug("m4u_alloc_mva returns fail: %d\n", ret);
 	}
-#else
-	struct ion_client *ion_display_client = NULL;
-	struct ion_handle *ion_display_handle = NULL;
-
-	size_align = round_up(size, PAGE_SIZE);
-	ion_display_client = disp_ion_create("disp_cap_ovl");
-	if (ion_display_client == NULL) {
-		pr_debug("primary capture:Fail to create ion\n");
-		ret = -1;
-		goto out;
-	}
-
-	ion_display_handle =
-	    disp_ion_alloc(ion_display_client, ION_HEAP_MULTIMEDIA_PA2MVA_MASK,
-			   buf_info->buf_pa, size_align);
-	if (ret != 0) {
-		pr_debug("primary capture:Fail to allocate buffer\n");
-		ret = -1;
-		goto out;
-	}
-	disp_ion_get_mva(ion_display_client, ion_display_handle,
-			 (unsigned int *)&mva, DISP_M4U_PORT_DISP_WDMA0);
-
-out:
-#endif
 	buf_info->buf_mva = mva;
 	DISPMSG("%s MVA is 0x%x PA is 0x%pa\n", __func__, mva,
 		&buf_info->buf_pa);
