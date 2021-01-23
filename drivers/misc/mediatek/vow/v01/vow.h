3d9e5e513bf58a86e228f6d89a460cd4e6fc66e1
Yueh Hsun Hsieh <ys.hsieh@mediatek.com>
2021-01-23 13:01:00 +0800

    [ALPS05507604] VOW: Add mutex lock in vmalloc/vfree
    Resend AP reg when call Read voice data
    
    MTK-Commit-Id: 87a9948da8956d5b8047b52ec1c373ad5421350a
    
    Change-Id: I12e71962b0b90aa469d4e1938ed2f2154b34c9da
    Signed-off-by: Yueh Hsun Hsieh <ys.hsieh@mediatek.com>
    CR-Id: ALPS05507604
    Feature: VOW (MagiEar-Voice Wakeup)

--- drivers/misc/mediatek/vow/v01/vow.h
+++ drivers/misc/mediatek/vow/v01/vow.h
@@ -103,6 +103,7 @@
 #define VOW_GET_ALEXA_ENGINE_VER      _IOW(VOW_IOC_MAGIC, 0x11, unsigned int)
 #define VOW_GET_GOOGLE_ENGINE_VER     _IOW(VOW_IOC_MAGIC, 0x12, unsigned int)
 #define VOW_GET_GOOGLE_ARCH           _IOW(VOW_IOC_MAGIC, 0x13, unsigned int)
+#define VOW_READ_VOICE_DATA           _IOW(VOW_IOC_MAGIC, 0x17, unsigned int)
 
 #ifdef CONFIG_MTK_VOW_BARGE_IN_SUPPORT
 
@@ -170,7 +171,6 @@ enum { /* dump_data_t */
  *****************************************************************************/
 enum vow_control_cmd_t {
 	VOWControlCmd_Init = 0,
-	VOWControlCmd_ReadVoiceData,
 	VOWControlCmd_EnableDebug,
 	VOWControlCmd_DisableDebug,
 	VOWControlCmd_EnableSeamlessRecord,
