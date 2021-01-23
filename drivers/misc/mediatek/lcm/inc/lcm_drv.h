421588a3cafc8a444f896d01859d5d36f94003a1
mtk12691 <wangting.wang@mediatek.com>
2021-01-23 13:00:52 +0800

    [ALPS05499663] disp: support HBM
    [Detail]
    1.add FOD in cmd mode, default off.
    2.add hbm delay to avoid setting highbright before vsync,which
    could be produce highlight flash in aod.
    
    MTK-Commit-Id: 3b2d734abb374bf8f3f600151a4812b2aea31e29
    
    Change-Id: Ie695ecbd79f943c4aaf6a086b94ed91579f7bc1e
    Signed-off-by: wangting.wang <wangting.wang@mediatek.com>
    CR-Id: ALPS05499663
    Feature: [Module]Display Driver

--- drivers/misc/mediatek/lcm/inc/lcm_drv.h
+++ drivers/misc/mediatek/lcm/inc/lcm_drv.h
@@ -534,6 +534,12 @@ struct dynamic_fps_info {
 	/*unsigned int idle_check_interval;*//*ms*/
 };
 
+struct vsync_trigger_time {
+	unsigned int fps;
+	unsigned int trigger_after_te;
+	unsigned int config_expense_time;
+};
+
 
 /*DynFPS*/
 enum DynFPS_LEVEL {
@@ -738,6 +744,7 @@ struct LCM_DSI_PARAMS {
 	/*for ARR*/
 	unsigned int dynamic_fps_levels;
 	struct dynamic_fps_info dynamic_fps_table[DYNAMIC_FPS_LEVELS];
+	struct vsync_trigger_time vsync_after_te[DFPS_LEVELS];
 
 #ifdef CONFIG_MTK_HIGH_FRAME_RATE
 	/****DynFPS start****/
@@ -793,6 +800,9 @@ struct LCM_PARAMS {
 	unsigned int min_luminance;
 	unsigned int average_luminance;
 	unsigned int max_luminance;
+
+	unsigned int hbm_en_time;
+	unsigned int hbm_dis_time;
 
 #ifdef CONFIG_MTK_HIGH_FRAME_RATE
 	enum LCM_Send_Cmd_Mode sendmode;
@@ -1030,6 +1040,12 @@ struct LCM_DRIVER {
 	void (*set_pwm_for_mix)(int enable);
 
 	void (*aod)(int enter);
+
+
+	bool (*get_hbm_state)(void);
+	bool (*get_hbm_wait)(void);
+	bool (*set_hbm_wait)(bool wait);
+	bool (*set_hbm_cmdq)(bool en, void *qhandle);
 
 	/* /////////////DynFPS///////////////////////////// */
 	void (*dfps_send_lcm_cmd)(void *cmdq_handle,
