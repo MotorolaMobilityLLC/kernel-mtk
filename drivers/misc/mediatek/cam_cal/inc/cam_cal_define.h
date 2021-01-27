71ff9003b32f5904f2de52ae097bcf69e6da1e15
shaozhuchao <shaozhuchao@wingtech.com>
2021-01-27 20:02:53 +0800

    Saipan: metadata,setting updata and camera rename[1/5]
    metadata,setting updata and camera rename:
    modify for  metadata,setting updata and camera rename
    
    Change-Id: Ide139e3fb0f2309a916f583decd9f9c88533a0ad
    Signed-off-by: shaozhuchao <shaozhuchao@wingtech.com>
    Reviewed-on: https://gerrit.mot.com/1863015
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Heng Chen <chenheng3@lenovo.com>
    Reviewed-by: Qiang Guo <guoq8@motorola.com>
    Reviewed-by: Shanghui Zhang <zhangsh@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/cam_cal/inc/cam_cal_define.h
+++ drivers/misc/mediatek/cam_cal/inc/cam_cal_define.h
@@ -11,6 +11,39 @@
 #include <linux/compat.h>
 #endif
 
+struct stCAM_CAL_DATAINFO_STRUCT{
+	u32 sensorID; // Sensor ID
+	u32 deviceID; // MAIN = 0x01, SUB  = 0x02, MAIN_2 = 0x04
+	u32 dataLength; //Data len
+	u32 sensorVendorid; // Module ID | Pos ID | Vcm ID | Len ID
+	u8  vendorByte[4]; // Module ID offset, Pos ID offset, Vcm ID offset,  Len ID offset
+	u8  *dataBuffer; //It's need malloc dataLength cache
+};
+
+typedef enum{
+	MODULE_ITEM = 0,
+	AWB_ITEM,
+	SEGMENT_ITEM,
+	AF_ITEM,
+	LSC_ITEM,
+	PDAF_ITEM,
+	PDAF_PROC2_ITEM,
+	SAIPAN_SUNNY_HI4821Q_XGC,
+	SAIPAN_SUNNY_HI4821Q_QGC,
+	SAIPAN_SUNNY_HI4821Q_PGC,
+	DUALCAM_ITEM,
+	TOTAL_ITEM,
+	MAX_ITEM,
+}stCAM_CAL_CHECKSUM_ITEM;
+
+struct stCAM_CAL_CHECKSUM_STRUCT{
+	stCAM_CAL_CHECKSUM_ITEM item;
+	u32 flagAdrees;
+	u32 startAdress;
+	u32 endAdress;
+	u32 checksumAdress;
+	u8  validFlag;
+};
 struct CAM_CAL_SENSOR_INFO {
 	u32 sensor_id;
 };
