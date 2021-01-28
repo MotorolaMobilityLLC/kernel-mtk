621752aab40f2b3fb4e8f9737b91889eeeeccdaa
mahj8 <mahj8@lenovo.com>
2021-01-28 11:06:43 +0800

    charger: modify tcmd batt id test api
    modify tcmd batt id api
    for better compatibility
    
    Change-Id: Icefe504c318b6e93f7ce3c019a4fea23eabedae1
    Signed-off-by: mahj8 <mahj8@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1863611
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/include/mt-plat/mt6768/include/mach/mtk_battery_table.h
+++ drivers/misc/mediatek/include/mt-plat/mt6768/include/mach/mtk_battery_table.h
@@ -22,6 +22,9 @@
 #define Q_MAX_L_CURRENT 0
 #define Q_MAX_H_CURRENT 10000
 
+/* Default use moto serial number feature */
+#define MTK_GET_BATTERY_ID_BY_SERIALNUMBER
+
 /* multiple battery profile compile options */
 /*#define MTK_GET_BATTERY_ID_BY_AUXADC*/
 
@@ -204,7 +207,7 @@ int g_temperature[MAX_TABLE] = {
 #define BIF_NTC_R 16000
 
 #if (BAT_NTC_10 == 1)
-struct FUELGAUGE_TEMPERATURE Fg_Temperature_Table[21] = {
+struct FUELGAUGE_TEMPERATURE Fg_Temperature_Table[23] = {
 		{-40, 195652},
 		{-35, 148171},
 		{-30, 113347},
@@ -225,7 +228,9 @@ struct FUELGAUGE_TEMPERATURE Fg_Temperature_Table[21] = {
 		{45, 4917},
 		{50, 4161},
 		{55, 3535},
-		{60, 3014}
+		{60, 3014},
+                {65, 2588},
+                {70, 2227}
 };
 #endif
 
