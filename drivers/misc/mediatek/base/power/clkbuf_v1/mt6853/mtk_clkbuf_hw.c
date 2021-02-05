bdf397fada145ae3bf4688069a17218cc275f0c4
wuwl10 <wuwl10@lenovo.com>
2021-02-05 17:55:24 +0800

    pmic: enable nfc clk
    1.Config the PMIC register to make sure
    NFC_CLK can be control by CLK_REQ signal.
    Patch from: ALPS05567322
    2. Config GPIO81 as INPUT for CLK_REQ
    
    Change-Id: I4c9566ff98d68c5a834277bb5d0aa76551b8be36
    Reviewed-on: https://gerrit.mot.com/1872452
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/base/power/clkbuf_v1/mt6853/mtk_clkbuf_hw.c
+++ drivers/misc/mediatek/base/power/clkbuf_v1/mt6853/mtk_clkbuf_hw.c
@@ -1704,7 +1704,7 @@ void clk_buf_post_init(void)
 #endif
 #endif
 
-#ifndef CONFIG_MTK_NFC_CLKBUF_ENABLE
+#if 0
 	/* no need to use XO_NFC if no NFC */
 	clk_buf_ctrl_internal(CLK_BUF_NFC, CLK_BUF_FORCE_OFF);
 	CLK_BUF3_STATUS = CLOCK_BUFFER_DISABLE;
