1769b74df61f56b7987f099d4197c98255df4ff8
zhanglinfeng <zhanglinfeng@wingtech.com>
2021-02-08 10:28:43 +0800

    Saipan: add front camera hi1336 otp code[1/2]
    add front camera hi1336 otp code:
    modify for front camera hi1336 otp
    
    Change-Id: Icdd0dcdd173b3fe7fa995ba2f4210b23ba86e84e
    Signed-off-by: zhanglinfeng <zhanglinfeng@wingtech.com>
    Reviewed-on: https://gerrit.mot.com/1873681
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Shanghui Zhang <zhangsh@motorola.com>
    Reviewed-by: Qiang Guo <guoq8@motorola.com>
    Reviewed-by: Wang Liu <liuwang2@lenovo.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/saipan_dmegc_hi1336_mipi_raw/saipan_dmegc_hi1336_otp.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2019 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#include <linux/videodev2.h>
+#include <linux/i2c.h>
+#include <linux/platform_device.h>
+#include <linux/delay.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
+#include <linux/fs.h>
+#include <asm/atomic.h>
+#include <linux/slab.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/syscalls.h>
+#include <linux/fcntl.h>
+#include <linux/types.h>
+
+//#include "kd_camera_hw.h"
+#include "kd_imgsensor.h"
+#include "kd_imgsensor_define.h"
+#include "kd_imgsensor_errcode.h"
+#include "kd_camera_typedef.h"
+#include "saipan_dmegc_hi1336_otp.h"
+
+kal_uint16 read_hi1336_eeprom(kal_uint32 addr)
+{
+	kal_uint16 get_byte=0;
+
+	char pu_send_cmd[2] = { (char)(addr >> 8), (char)(addr & 0xFF) };
+	iReadRegI2C(pu_send_cmd, 2, (u8*)&get_byte, 1, EEPROM_GT24P64M_ID);
+
+	return get_byte;
+}
