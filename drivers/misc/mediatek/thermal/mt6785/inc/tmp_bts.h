ee41ad72e189ffed23d9f5f72a7fd1f1777c5431
xuwei9 <xuwei9@lenovo.com>
2021-02-19 10:33:54 +0800

    thermal add new ntc
    Thermal add new ntc driver
    Refer to:https://gerrit.mot.com/#/c/1846605/
    
    Change-Id: I3b3573229e6ea31e0d25cbdc8fe21fb435fe5481
    Signed-off-by: xuwei9 <xuwei9@mt.com>
    Reviewed-on: https://gerrit.mot.com/1882062
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/thermal/mt6785/inc/tmp_bts.h
+++ drivers/misc/mediatek/thermal/mt6785/inc/tmp_bts.h
@@ -18,6 +18,10 @@
 
 #define AUX_IN0_NTC (0)
 #define AUX_IN1_NTC (1)
+#define AUX_IN2_NTC (2)
+#define AUX_IN3_NTC (3)
+#define AUX_IN4_NTC (4)
+#define AUX_IN5_NTC (5)
 
 #define BTS_RAP_PULL_UP_R		390000 /* 390K, pull up resister */
 
@@ -42,6 +46,59 @@
 #define BTSMDPA_RAP_NTC_TABLE		7 /* default is NCP15WF104F03RC(100K) */
 
 #define BTSMDPA_RAP_ADC_CHANNEL		AUX_IN1_NTC /* default is 1 */
+
+//aux2 -- aux5
+#define USB_RAP_PULL_UP_R               390000 /* 390K, pull up resister */
+
+#define USB_TAP_OVER_CRITICAL_LOW       4397119 /* base on 100K NTC temp
+                                                 * default value -40 deg
+                                                 */
+
+#define USB_RAP_PULL_UP_VOLTAGE         1800 /* 1.8V ,pull up voltage */
+
+#define USB_RAP_NTC_TABLE               7 /* default is NCP15WF104F03RC(100K) */
+
+#define USB_RAP_ADC_CHANNEL             AUX_IN2_NTC /* default is 0 */
+
+//bq ic
+#define MBTHERM_RAP_PULL_UP_R               390000 /* 390K, pull up resister */
+
+#define MBTHERM_TAP_OVER_CRITICAL_LOW       4397119 /* base on 100K NTC temp
+                                                 * default value -40 deg
+                                                 */
+
+#define MBTHERM_RAP_PULL_UP_VOLTAGE         1800 /* 1.8V ,pull up voltage */
+
+#define MBTHERM_RAP_NTC_TABLE               7 /* default is NCP15WF104F03RC(100K) */
+
+#define MBTHERM_RAP_ADC_CHANNEL             AUX_IN3_NTC /* default is 0 */
+
+
+#define WTCHARGER_RAP_PULL_UP_R               390000 /* 390K, pull up resister */
+
+#define WTCHARGER_TAP_OVER_CRITICAL_LOW       4397119 /* base on 100K NTC temp
+                                                 * default value -40 deg
+                                                 */
+
+#define WTCHARGER_RAP_PULL_UP_VOLTAGE         1800 /* 1.8V ,pull up voltage */
+
+#define WTCHARGER_RAP_NTC_TABLE               7 /* default is NCP15WF104F03RC(100K) */
+
+#define WTCHARGER_RAP_ADC_CHANNEL             AUX_IN4_NTC /* default is 0 */
+
+
+#define FLASHLIGHT_RAP_PULL_UP_R               390000 /* 390K, pull up resister */
+
+#define FLASHLIGHT_TAP_OVER_CRITICAL_LOW       4397119 /* base on 100K NTC temp
+                                                 * default value -40 deg
+                                                 */
+
+#define FLASHLIGHT_RAP_PULL_UP_VOLTAGE         1800 /* 1.8V ,pull up voltage */
+
+#define FLASHLIGHT_RAP_NTC_TABLE               7 /* default is NCP15WF104F03RC(100K) */
+
+#define FLASHLIGHT_RAP_ADC_CHANNEL             AUX_IN5_NTC /* default is 0 */
+
 
 extern int IMM_GetOneChannelValue(int dwChannel, int data[4], int *rawdata);
 extern int IMM_IsAdcInitReady(void);
