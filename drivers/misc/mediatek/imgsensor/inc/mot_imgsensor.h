38b1a94fd71da0d02d164bbd3e42d96c0b7b2ca0
Qiang Guo <guoqiang8@lenovo.com>
2021-02-25 18:24:33 +0800

    kyoto: bringup camera
    1. kernel define all sensor
    2. modify build rule
    3. add mot define file
    
    Change-Id: I33901c83c403d00a9cb6d389cafea46547c24423
    Signed-off-by: Qiang Guo <guoqiang8@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1887001
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Dawei Wang <wangdw10@motorola.com>
    Reviewed-by: Wang Liu <liuwang2@lenovo.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Reviewed-by: Shanghui Zhang <zhangsh@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/misc/mediatek/imgsensor/inc/mot_imgsensor.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2021 lucas <guoqiang8@lenovo.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _MOT_IMGSENSOR_H
+#define _MOT_IMGSENSOR_H
+
+#if defined(kyoto)
+typedef enum {
+        NO_ERRORS,
+        CRC_FAILURE,
+        LIMIT_FAILURE
+} calibration_status_t;
+
+typedef struct {
+        calibration_status_t mnf_status;
+        calibration_status_t af_status;
+        calibration_status_t awb_status;
+        calibration_status_t lsc_status;
+        calibration_status_t pdaf_status;
+        calibration_status_t dual_status;
+} RetStatus;
+int imgread_cam_cal_data(int sensorid,
+			const char* dump_file ,RetStatus *rStatus);
+#endif
+/*kyoto*/
+#define MOT_S5KHM2_SENSOR_ID                               (0x1ad2)
+#define MOT_OV32B40_SENSOR_ID                              (0x3242)
+#define MOT_OV02B1B_SENSOR_ID                              (0x002b)
+#define MOT_S5K4H7_SENSOR_ID                               (0x487b)
+#define SENSOR_DRVNAME_MOT_S5KHM2_MIPI_RAW                 "mot_s5khm2_mipi_raw"
+#define SENSOR_DRVNAME_MOT_OV32B40_MIPI_RAW                "mot_ov32b40_mipi_raw"
+#define SENSOR_DRVNAME_MOT_OV02B1B_MIPI_RAW                "mot_ov02b1b_mipi_raw"
+#define SENSOR_DRVNAME_MOT_S5K4H7_MIPI_RAW                 "mot_s5k4h7_mipi_raw"
+
+#endif
