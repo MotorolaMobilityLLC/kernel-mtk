1d813add3692fc91f7b1e9008bd244394dde849e
Qiang Guo <guoqiang8@lenovo.com>
2021-02-25 18:24:33 +0800

    kyoto: bringup camera
    1. kernel define all sensor
    2. modify build rule
    3. add mot define file
    
    Change-Id: I33901c83c403d00a9cb6d389cafea46547c24423
    Signed-off-by: Qiang Guo <guoqiang8@mt.com>
    Reviewed-on: https://gerrit.mot.com/1887001
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Dawei Wang <wangdw10@motorola.com>
    Reviewed-by: Wang Liu <liuwang2@mt.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Reviewed-by: Shanghui Zhang <zhangsh@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_sensor_list.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2021 lucas <guoqiang8@lenovo.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __MOT_SENSORLIST_H__
+#define __MOT_SENSORLIST_H__
+
+UINT32 SAIPAN_SHINE_HI846_MIPI_RAW_SensorInit(struct SENSOR_FUNCTION_STRUCT **pfFunc);
+UINT32 SAIPAN_SUNNY_HI4821Q_MIPI_RAW_SensorInit(struct SENSOR_FUNCTION_STRUCT **pfFunc);
+UINT32 SAIPAN_CXT_GC02M1B_MIPI_MONO_SensorInit(struct SENSOR_FUNCTION_STRUCT **pfFunc);
+UINT32 SAIPAN_CXT_GC02M1_MIPI_RAW_SensorInit(struct SENSOR_FUNCTION_STRUCT **pfFunc);
+
+UINT32 MOT_S5KHM2_MIPI_RAW_SensorInit(struct SENSOR_FUNCTION_STRUCT **pfFunc);
+UINT32 MOT_OV32B40_MIPI_RAW_SensorInit(struct SENSOR_FUNCTION_STRUCT **pfFunc);
+UINT32 MOT_OV02B1B_MIPI_RAW_SensorInit(struct SENSOR_FUNCTION_STRUCT **pfFunc);
+UINT32 MOT_S5K4H7_MIPI_RAW_SensorInit(struct SENSOR_FUNCTION_STRUCT **pfFunc);
+#endif
+
