dd60cdfc1fb68f6bc88751965cd0fd6e2e1d7878
tanhua1 <tanhua1@lenovo.com>
2021-03-01 15:32:50 +0800

    backlight: add lm3697 backlight driver
    add lm3697 backlight driver for
    lisbon
    
    Change-Id: Iede2f0c272d41598dc7a53c3bd0afcbcc2478a79
    Signed-off-by: tanhua1 <tanhua1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1889320
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lcm/panel_mot_mipi_vid_tianma_ili7807s_678_fhd/panel_mot_mipi_vid_tianma_ili7807s_678_fhd.c
+++ drivers/misc/mediatek/lcm/panel_mot_mipi_vid_tianma_ili7807s_678_fhd/panel_mot_mipi_vid_tianma_ili7807s_678_fhd.c
@@ -702,11 +702,30 @@ static void lcm_resume(void)
 	lcm_init();
 }
 
-extern int chargepump_set_backlight_level(int level);
+#if defined(CONFIG_LEDS_AW99703)
+extern int is_aw99703_chip_exist(void);
+extern int chargepump_set_backlight_level(unsigned int level);
+#endif
+#if defined(CONFIG_LEDS_LM3697)
+extern int is_lm3697_chip_exist(void);
+extern int lm3697_set_brightness_level(unsigned int level);
+#endif
+
 static void lcm_setbacklight(/*void *handle, */unsigned int level)
 {
 	pr_debug("lcm_setbacklight.");
-	chargepump_set_backlight_level(level);
+#if defined(CONFIG_LEDS_AW99703)
+	if (is_aw99703_chip_exist()) {
+		chargepump_set_backlight_level(level);
+		return;
+	}
+#endif
+#if defined(CONFIG_LEDS_LM3697)
+	if (is_lm3697_chip_exist()) {
+		lm3697_set_brightness_level(level);
+		return;
+    }
+#endif
 }
 
 struct LCM_DRIVER mipi_mot_vid_tianma_ili7807s_fhd_678_lcm_drv = {
