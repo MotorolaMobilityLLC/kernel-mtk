37a99f7274599f78efbe64a587728b5eccb87f4f
Qiang Guo <guoqiang8@lenovo.com>
2021-03-02 15:09:28 +0800

    kyoto: add main and main3 lens
    1.add main driver and config
    2.add main3 driver and config
    3.add flashlight config
    
    Change-Id: I87185f8cd6d47742fe289cb97adc021d6a9b07a6
    Signed-off-by: Qiang Guo <guoqiang8@mt.com>
    Reviewed-on: https://gerrit.mot.com/1890173
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shanghui Zhang <zhangsh@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Reviewed-by: Heng Chen <chenheng3@mt.com>
    Reviewed-by: Wang Liu <liuwang2@mt.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lens/main/main_lens.c
+++ drivers/misc/mediatek/lens/main/main_lens.c
@@ -59,7 +59,8 @@
 #define I2C_REGISTER_ID 0x28
 #endif
 
-#define PLATFORM_DRIVER_NAME "lens_actuator_main_af"
+//#define PLATFORM_DRIVER_NAME "lens_actuator_main_af"
+#define PLATFORM_DRIVER_NAME "camera_af_hw_node"
 #define AF_DRIVER_CLASS_NAME "actuatordrv_main_af"
 
 #if I2C_CONFIG_SETTING == 1
@@ -88,6 +89,8 @@ static struct stAF_OisPosInfo OisPosInfo;
 /* ------------------------- */
 
 static struct stAF_DrvList g_stAF_DrvList[MAX_NUM_OF_LENS] = {
+	{1, "MOT_DW9800VAF", MOT_DW9800VAF_SetI2Cclient, MOT_DW9800VAF_Ioctl,
+	MOT_DW9800VAF_Release, MOT_DW9800VAF_GetFileName, NULL},
 	{1, AFDRV_DW9718TAF, DW9718TAF_SetI2Cclient, DW9718TAF_Ioctl,
 	 DW9718TAF_Release, DW9718TAF_GetFileName, NULL},
 	{1, AFDRV_AK7371AF, AK7371AF_SetI2Cclient, AK7371AF_Ioctl,
@@ -258,6 +261,12 @@ static int af_pinctrl_set(int pin, int state)
 
 	return ret;
 }
+
+void mot_af_poweron(int state)
+{
+	af_pinctrl_set(AF_PINCTRL_PIN_HWEN, state);
+}
+EXPORT_SYMBOL(mot_af_poweron);
 
 /* PMIC */
 #if !defined(CONFIG_MTK_LEGACY)
