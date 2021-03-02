d1280504766ef7f8ad0c41fbac70239fef54f3bd
Qiang Guo <guoqiang8@lenovo.com>
2021-03-02 15:09:28 +0800

    kyoto: add main and main3 lens
    1.add main driver and config
    2.add main3 driver and config
    3.add flashlight config
    
    Change-Id: I87185f8cd6d47742fe289cb97adc021d6a9b07a6
    Signed-off-by: Qiang Guo <guoqiang8@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1890173
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shanghui Zhang <zhangsh@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Reviewed-by: Heng Chen <chenheng3@lenovo.com>
    Reviewed-by: Wang Liu <liuwang2@lenovo.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lens/main/inc/lens_list.h
+++ drivers/misc/mediatek/lens/main/inc/lens_list.h
@@ -173,6 +173,17 @@ extern long DW9800WAF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
 			   unsigned long a_u4Param);
 extern int DW9800WAF_Release(struct inode *a_pstInode, struct file *a_pstFile);
 extern int DW9800WAF_GetFileName(unsigned char *pFileName);
+
+#define MOT_DW9800VAF_SetI2Cclient MOT_DW9800VAF_SetI2Cclient_Main
+#define MOT_DW9800VAF_Ioctl MOT_DW9800VAF_Ioctl_Main
+#define MOT_DW9800VAF_Release MOT_DW9800VAF_Release_Main
+#define MOT_DW9800VAF_GetFileName MOT_DW9800VAF_GetFileName_Main
+extern int MOT_DW9800VAF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+				 spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long MOT_DW9800VAF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+			   unsigned long a_u4Param);
+extern int MOT_DW9800VAF_Release(struct inode *a_pstInode, struct file *a_pstFile);
+extern int MOT_DW9800VAF_GetFileName(unsigned char *pFileName);
 /* extern struct regulator *regulator_get_regVCAMAF(void); */
 
 #define DW9718AF_SetI2Cclient DW9718AF_SetI2Cclient_Main
