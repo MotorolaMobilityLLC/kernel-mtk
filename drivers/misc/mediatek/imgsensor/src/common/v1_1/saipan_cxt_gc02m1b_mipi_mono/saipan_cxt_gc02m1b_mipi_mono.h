535567bd1d808778f5fed08871dafa70530ac3c0
yangchangyun <yangchangyun@wingtech.com>
2021-03-03 13:52:45 +0800

    Saipan: depth camera driver modify[1/1]
    sensor FAE regularly checks and update the driver&setting:
    setting：The deleted part is duplicated. It already exists in the init part.
    driver：feature_control()&imgsensor_info_struct are repeated and commented out.
    Depth camera does not support PDAF, so delete them in the driver.
    
    Change-Id: I8411cc8a5788e0f8e8c2f4668d138b393420d95d
    Signed-off-by: yangchangyun <yangchangyun@wingtech.com>
    Signed-off-by: zhanglinfeng <zhanglinfeng@wingtech.com>
    Reviewed-on: https://gerrit.mot.com/1891332
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Qiang Guo <guoq8@motorola.com>
    Reviewed-by: Shanghui Zhang <zhangsh@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Reviewed-by: Wang Liu <liuwang2@mt.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/saipan_cxt_gc02m1b_mipi_mono/saipan_cxt_gc02m1b_mipi_mono.h
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/saipan_cxt_gc02m1b_mipi_mono/saipan_cxt_gc02m1b_mipi_mono.h
@@ -133,7 +133,6 @@ struct imgsensor_info_struct {
 	kal_uint32 max_gain;
 	kal_uint32 min_gain_iso;
 	kal_uint32 gain_step;
-	kal_uint32 exp_step;
 	kal_uint32 gain_type;
 	//-modify codes for factory mode of photo black screen
 	kal_uint8  isp_driving_current;	//mclk driving current
@@ -151,43 +150,7 @@ struct imgsensor_info_struct {
 };
 
 
-#if 0
-/* SENSOR PRIVATE STRUCT FOR CONSTANT */
-struct imgsensor_info_struct {
-	kal_uint32 sensor_id;
-	kal_uint32 checksum_value;
-	struct imgsensor_mode_struct pre;
-	struct imgsensor_mode_struct cap;
-	struct imgsensor_mode_struct cap1;
-	struct imgsensor_mode_struct normal_video;
-	struct imgsensor_mode_struct hs_video;
-	struct imgsensor_mode_struct slim_video;
-	kal_uint8  ae_shut_delay_frame;
-	kal_uint8  ae_sensor_gain_delay_frame;
-	kal_uint8  ae_ispGain_delay_frame;
-        kal_uint8  frame_time_delay_frame;
-	kal_uint8  ihdr_support;
-	kal_uint8  ihdr_le_firstline;
-	kal_uint8  sensor_mode_num;
-	kal_uint8  cap_delay_frame;
-	kal_uint8  pre_delay_frame;
-	kal_uint8  video_delay_frame;
-	kal_uint8  hs_video_delay_frame;
-	kal_uint8  slim_video_delay_frame;
-	kal_uint8  margin;
-	kal_uint32 min_shutter;
-	kal_uint32 max_frame_length;
-	kal_uint8  isp_driving_current;
-	kal_uint8  sensor_interface_type;
-	kal_uint8  mipi_sensor_type;
-	kal_uint8  mipi_settle_delay_mode;
-	kal_uint8  sensor_output_dataformat;
-	kal_uint8  mclk;
-	kal_uint8  mipi_lane_num;
-	kal_uint8  i2c_addr_table[5];
-	kal_uint32 i2c_speed;
-};
-#endif
+
 extern int iReadRegI2C(u8 *a_pSendData, u16 a_sizeSendData, u8 *a_pRecvData, u16 a_sizeRecvData, u16 i2cId);
 extern int iWriteRegI2C(u8 *a_pSendData, u16 a_sizeSendData, u16 i2cId);
 extern int iWriteReg(u16 a_u2Addr, u32 a_u4Data, u32 a_u4Bytes, u16 i2cId);
