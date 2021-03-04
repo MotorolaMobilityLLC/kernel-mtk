ed0da73eeaf482f7d392d7eb26c0290ccc46fb57
ontim_NiuZq <zhiqiang.niu@ontim.cn>
2021-03-04 13:25:27 +0800

    lisbon:[camera] add awb limit check
    Description:
    add awb limit check in kernel
    
    Change-Id: Iae40b6c502c17d0c349aa7f34c3e7808f309aae1
    Signed-off-by: ontim_NiuZq <zhiqiang.niu@ontim.cn>
    Reviewed-on: https://gerrit.mot.com/1892615
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Qiang Guo <guoq8@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Reviewed-by: Wang Liu <liuwang2@lenovo.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/mt6785/mot_lisbon_ov64b40_mipi_raw/ov64b40mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/mt6785/mot_lisbon_ov64b40_mipi_raw/ov64b40mipiraw_Sensor.c
@@ -1021,23 +1021,37 @@ static uint8_t mot_eeprom_util_calculate_awb_factors_limit(awb_t unit, awb_t gol
 	uint32_t r_g;
 	uint32_t b_g;
 	uint32_t golden_rg, golden_bg;
+	uint32_t gr_gb;
+	uint32_t golden_gr_gb;
 	uint32_t r_g_golden_min;
 	uint32_t r_g_golden_max;
 	uint32_t b_g_golden_min;
 	uint32_t b_g_golden_max;
 
-	r_g = unit.r_g * 1000;
-	b_g = unit.b_g*1000;
+	LOG_INF("unit.r_g = 0x%x, unit.b_g=0x%x,unit.gr_gb=0x%x \n",unit.r_g,unit.b_g,unit.gr_gb);
+	LOG_INF("golden.r_g = 0x%x, golden.b_g=0x%x,golden.gr_gb=0x%x \n",golden.r_g,golden.b_g,golden.gr_gb);
 
-	golden_rg = golden.r_g* 1000;
-	golden_bg = golden.b_g* 1000;
+
+	LOG_INF("limit golden  0x%x, 0x%x ,0x%x 0x%x \n",limit.r_g_golden_min,limit.r_g_golden_max,
+		limit.b_g_golden_min,limit.b_g_golden_max);
+
+
+	r_g = unit.r_g *1000;
+	b_g = unit.b_g*1000;
+	gr_gb = unit.gr_gb*100;
+
+	golden_rg = golden.r_g*1000;
+	golden_bg = golden.b_g*1000;
+	golden_gr_gb = golden.gr_gb*100;
 
 	r_g_golden_min = limit.r_g_golden_min*16384;
 	r_g_golden_max = limit.r_g_golden_max*16384;
 	b_g_golden_min = limit.b_g_golden_min*16384;
 	b_g_golden_max = limit.b_g_golden_max*16384;
+
 	LOG_INF("rg = %d, bg=%d,rgmin=%d,bgmax =%d\n",r_g,b_g,r_g_golden_min,r_g_golden_max);
 	LOG_INF("grg = %d, gbg=%d,bgmin=%d,bgmax =%d\n",golden_rg,golden_bg,b_g_golden_min,b_g_golden_max);
+
 	if (r_g < (golden_rg - r_g_golden_min) || r_g > (golden_rg + r_g_golden_max)) {
 		LOG_INF("Final RG calibration factors out of range!");
 		return 1;
@@ -1047,6 +1061,19 @@ static uint8_t mot_eeprom_util_calculate_awb_factors_limit(awb_t unit, awb_t gol
 		LOG_INF("Final BG calibration factors out of range!");
 		return 1;
 	}
+
+	LOG_INF("gr_gb = %d, golden_gr_gb=%d \n",gr_gb,golden_gr_gb);
+
+	if (gr_gb < AWB_GR_GB_MIN || gr_gb > AWB_GR_GB_MAX) {
+		LOG_INF("Final gr_gb calibration factors out of range!!!");
+		return 1;
+	}
+
+	if (golden_gr_gb < AWB_GR_GB_MIN || golden_gr_gb > AWB_GR_GB_MAX) {
+		LOG_INF("Final golden_gr_gb calibration factors out of range!!!");
+		return 1;
+	}
+
 	return 0;
 }
 
@@ -1064,18 +1091,18 @@ static calibration_status_t ov64b40_check_awb_data(void *data)
 		return CRC_FAILURE;
 	}
 
-	unit.r = to_uint16_swap(eeprom->awb_src_1_r)/64;
-	unit.gr = to_uint16_swap(eeprom->awb_src_1_gr)/64;
-	unit.gb = to_uint16_swap(eeprom->awb_src_1_gb)/64;
-	unit.b = to_uint16_swap(eeprom->awb_src_1_b)/64;
+	unit.r = to_uint16_swap(eeprom->awb_src_1_r);
+	unit.gr = to_uint16_swap(eeprom->awb_src_1_gr);
+	unit.gb = to_uint16_swap(eeprom->awb_src_1_gb);
+	unit.b = to_uint16_swap(eeprom->awb_src_1_b);
 	unit.r_g = to_uint16_swap(eeprom->awb_src_1_rg_ratio);
 	unit.b_g = to_uint16_swap(eeprom->awb_src_1_bg_ratio);
 	unit.gr_gb = to_uint16_swap(eeprom->awb_src_1_gr_gb_ratio);
 
-	golden.r = to_uint16_swap(eeprom->awb_src_1_golden_r)/64;
-	golden.gr = to_uint16_swap(eeprom->awb_src_1_golden_gr)/64;
-	golden.gb = to_uint16_swap(eeprom->awb_src_1_golden_gb)/64;
-	golden.b = to_uint16_swap(eeprom->awb_src_1_golden_b)/64;
+	golden.r = to_uint16_swap(eeprom->awb_src_1_golden_r);
+	golden.gr = to_uint16_swap(eeprom->awb_src_1_golden_gr);
+	golden.gb = to_uint16_swap(eeprom->awb_src_1_golden_gb);
+	golden.b = to_uint16_swap(eeprom->awb_src_1_golden_b);
 	golden.r_g = to_uint16_swap(eeprom->awb_src_1_golden_rg_ratio);
 	golden.b_g = to_uint16_swap(eeprom->awb_src_1_golden_bg_ratio);
 	golden.gr_gb = to_uint16_swap(eeprom->awb_src_1_golden_gr_gb_ratio);
