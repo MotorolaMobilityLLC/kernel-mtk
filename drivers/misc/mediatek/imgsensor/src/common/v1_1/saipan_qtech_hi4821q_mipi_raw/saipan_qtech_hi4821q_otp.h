05d0392109b7e41f472d25892718e7173c5c5f23
zhanglinfeng <zhanglinfeng@wingtech.com>
2021-03-08 13:34:27 +0800

    Saipan: Uneven color of picture when ISO<200[1/1]
    XGC/QGC/BPGC data is't used:
    modify otp apply fix TG size cause uneven color
    
    Change-Id: I0a39bdfa957c7f15f5e4f2a13f7f4f950fafedf1
    Signed-off-by: zhanglinfeng <zhanglinfeng@wingtech.com>
    Reviewed-on: https://gerrit.mot.com/1895101
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Wang Liu <liuwang2@mt.com>
    Reviewed-by: Qiang Guo <guoq8@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/saipan_qtech_hi4821q_mipi_raw/saipan_qtech_hi4821q_otp.h
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/saipan_qtech_hi4821q_mipi_raw/saipan_qtech_hi4821q_otp.h
@@ -25,7 +25,7 @@
 #include <linux/unistd.h>
 
 #define EEPROM_BL24SA64D_ID 0xA0
-#define SAIPAN_QTECH_HI4821Q_XGC_QGC_PGC_CALIB 0
+#define SAIPAN_QTECH_HI4821Q_XGC_QGC_BPGC_CALIB 1
 #define SAIPAN_QTECH_HI4821Q_OTP_DUMP 0
 
 #define SAIPAN_QTECH_HI4821Q_OTP_ENABLE 1
@@ -34,23 +34,19 @@
 kal_uint16 saipan_qtech_hi4821q_read_eeprom(kal_uint32 addr);
 #endif
 
-#if SAIPAN_QTECH_HI4821Q_XGC_QGC_PGC_CALIB
+#if SAIPAN_QTECH_HI4821Q_XGC_QGC_BPGC_CALIB
 //  OTP information setting
 #define XGC_BLOCK_X  9
 #define XGC_BLOCK_Y  7
 #define QGC_BLOCK_X  9
 #define QGC_BLOCK_Y  7
-#define PGC_BLOCK_X  13
-#define PGC_BLOCK_Y  11
+#define BPGC_BLOCK_X  13
+#define BPGC_BLOCK_Y  11
 
 // SRAM Information
 #define SRAM_XGC_START_ADDR_48M     0x43F0
 #define SRAM_QGC_START_ADDR_48M     0x4000
-#define SRAM_PGC_START_ADDR_48M     0x4000  //0x8980 (2020.9.14)
-
-u8* pgc_data_buffer = NULL;
-u8* qgc_data_buffer = NULL;
-u8* xgc_data_buffer = NULL;
+#define SRAM_BPGC_START_ADDR_48M     0x5068  //0x8980 (2020.9.14)
 
 #define PGC_DATA_SIZE 572
 #define QGC_DATA_SIZE 1008
@@ -61,8 +57,9 @@ extern void dumpEEPROMData(int u4Length,u8* pu1Params);
 #endif
 #endif
 
-#if SAIPAN_QTECH_HI4821Q_XGC_QGC_PGC_CALIB
-static void apply_sensor_Cali(void);
+#if SAIPAN_QTECH_HI4821Q_XGC_QGC_BPGC_CALIB
+	void apply_sensor_BPGC_Cali(void);
+	void apply_sensor_XGC_QGC_Cali(void);
 #endif
 
 #if SAIPAN_QTECH_HI4821Q_OTP_ENABLE
@@ -85,7 +82,7 @@ static struct stCAM_CAL_CHECKSUM_STRUCT st_rear_saipan_qtech_hi4821q_Checksum[11
 	{PDAF_PROC2_ITEM,0x0962,0x0962,0x0D4E,0x0D4F,0x55},
 	{SAIPAN_QTECH_HI4821Q_XGC,0x0D50,0x0D50,0x1005,0x1006,0x55},
 	{SAIPAN_QTECH_HI4821Q_QGC,0x1007,0x1007,0x13F7,0x13F8,0x55},
-	{SAIPAN_QTECH_HI4821Q_PGC,0x13F9,0x13F9,0x1635,0x1636,0x55},
+	{SAIPAN_QTECH_HI4821Q_BPGC,0x13F9,0x13F9,0x1635,0x1636,0x55},
 	{TOTAL_ITEM,0x0000,0x0000,0x1636,0x1637,0x55},
 	{MAX_ITEM,0xFFFF,0xFFFF,0xFFFF,0xFFFF,0x55},  // this line must haved
 };
