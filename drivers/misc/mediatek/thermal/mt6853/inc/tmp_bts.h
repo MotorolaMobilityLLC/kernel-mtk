a733b54ee5233160c9927706c84426999012db30
chailu1 <chailu1@lenovo.com>
2021-03-09 16:14:01 +0800

    therm: add new ntc for mt6853
    thermal/ntc
    add AUX_IN3_NTC AUX_IN4_NTC AUX_IN6_NTC
    
    Change-Id: Iec4201bbd8cbef20b5520deea32d40c80d124a7d
    Signed-off-by: chailu1 <chailu1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1896345
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/thermal/mt6853/inc/tmp_bts.h
+++ drivers/misc/mediatek/thermal/mt6853/inc/tmp_bts.h
@@ -19,6 +19,9 @@
 #define AUX_IN0_NTC (0)
 #define AUX_IN1_NTC (1)
 #define AUX_IN2_NTC (2)
+#define AUX_IN3_NTC (3)
+#define AUX_IN4_NTC (4)
+#define AUX_IN6_NTC (6)
 
 #define BTS_RAP_PULL_UP_R		100000 /* 100K, pull up resister */
 
@@ -56,6 +59,43 @@
 #define BTSNRPA_RAP_ADC_CHANNEL		AUX_IN2_NTC
 
 
+#define WTCHARGER_RAP_PULL_UP_R               100000 /* 100K, pull up resister */
+
+#define WTCHARGER_TAP_OVER_CRITICAL_LOW       4397119 /* base on 100K NTC temp
+							* default value -40 deg
+							*/
+
+#define WTCHARGER_RAP_PULL_UP_VOLTAGE         1800 /* 1.8V ,pull up voltage */
+
+#define WTCHARGER_RAP_NTC_TABLE               7 /* default is NCP15WF104F03RC(100K) */
+
+#define WTCHARGER_RAP_ADC_CHANNEL             AUX_IN3_NTC /* default is 6 */
+
+
+#define TYPEC_THERMAL_RAP_PULL_UP_R               100000 /* 100K, pull up resister */
+
+#define TYPEC_THERMAL_TAP_OVER_CRITICAL_LOW       4397119 /* base on 100K NTC temp
+                                                 * default value -40 deg
+                                                 */
+
+#define TYPEC_THERMAL_RAP_PULL_UP_VOLTAGE         1800 /* 1.8V ,pull up voltage */
+
+#define TYPEC_THERMAL_RAP_NTC_TABLE               7 /* default is NCP15WF104F03RC(100K) */
+
+#define TYPEC_THERMAL_RAP_ADC_CHANNEL             AUX_IN6_NTC /* default is 6 */
+
+
+#define MBTHERM_RAP_PULL_UP_R               100000 /* 100K, pull up resister */
+
+#define MBTHERM_TAP_OVER_CRITICAL_LOW       4397119 /* base on 100K NTC temp
+                                                 * default value -40 deg
+                                                 */
+
+#define MBTHERM_RAP_PULL_UP_VOLTAGE         1800 /* 1.8V ,pull up voltage */
+
+#define MBTHERM_RAP_NTC_TABLE               7 /* default is NCP15WF104F03RC(100K) */
+
+#define MBTHERM_RAP_ADC_CHANNEL             AUX_IN4_NTC /* default is 4 */
 
 extern int IMM_GetOneChannelValue(int dwChannel, int data[4], int *rawdata);
 extern int IMM_IsAdcInitReady(void);
