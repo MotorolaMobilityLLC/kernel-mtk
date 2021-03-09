8eef4bc1677173d4efa00cca43029bc04a451e4f
liujg6 <liujg6@lenovo.com>
2021-03-09 09:58:14 +0800

    defconfg: Add Moto ext_config feature
    Add ext_config feature to extend mtk's defconfig
    There make files can compose some kconfig files into one
    Propogate changes from (CR)
    
    Change-Id: Ib1e250cec5c8388080f04f6d68e4f64ce6f50bc1
    Reviewed-on: https://gerrit.mot.com/1895969
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xianhong Li <lixh8@mt.com>
    Submit-Approved: Jira Key

---
+++ defconfig.mk
@@ -0,0 +1,52 @@
+DEFCONFIGSRC			:= $(KERNEL_DIR)/arch/$(KERNEL_TARGET_ARCH)/configs
+LJAPDEFCONFIGSRC		:= ${DEFCONFIGSRC}/ext_config
+KERNEL_DEFCONFIG        := $(firstword $(KERNEL_DEFCONFIG))
+
+DEFCONFIG_BASENAME		:= $(subst _defconfig,,$(KERNEL_DEFCONFIG))
+ifneq ($(TARGET_BUILD_VARIANT), user)
+DEFCONFIG_BASENAME		:= $(subst _debug,,$(DEFCONFIG_BASENAME))
+endif
+TARGET_DEFCONFIG		:= $(KERNEL_OUT)/mapphone_defconfig
+PRODUCT_SPECIFIC_DEFCONFIGS	:= $(DEFCONFIGSRC)/$(KERNEL_DEFCONFIG) $(LJAPDEFCONFIGSRC)/moto-$(DEFCONFIG_BASENAME).config
+
+KERNEL_DEBUG_DEFCONFIG          := $(LJAPDEFCONFIGSRC)/debug-$(DEFCONFIG_BASENAME).config
+PRODUCT_KERNEL_DEBUG_DEFCONFIG  := $(LJAPDEFCONFIGSRC)/$(PRODUCT_DEBUG_DEFCONFIG)
+FACTORY_DEFCONFIG		:= $(LJAPDEFCONFIGSRC)/factory-$(DEFCONFIG_BASENAME).config
+
+# add debug config file for non-user build
+ifneq ($(TARGET_BUILD_VARIANT), user)
+ifneq ($(wildcard $(KERNEL_DEBUG_DEFCONFIG)),)
+PRODUCT_SPECIFIC_DEFCONFIGS += $(KERNEL_DEBUG_DEFCONFIG)
+endif
+
+# Add a product-specific debug defconfig, too
+ifneq ($(PRODUCT_DEBUG_DEFCONFIG),)
+PRODUCT_SPECIFIC_DEFCONFIGS += $(PRODUCT_KERNEL_DEBUG_DEFCONFIG)
+endif
+endif
+
+ifeq ($(TARGET_FACTORY_DEFCONFIG), true)
+PRODUCT_SPECIFIC_DEFCONFIGS += $(FACTORY_DEFCONFIG)
+endif
+
+# append all additional configs
+ifneq ($(KERNEL_EXTRA_CONFIG),)
+PRODUCT_SPECIFIC_DEFCONFIGS += $(KERNEL_EXTRA_CONFIG:%=$(LJAPDEFCONFIGSRC)/%.config)
+endif
+
+define do-make-defconfig
+	$(hide) mkdir -p $(dir $(1))
+	$(hide) printf "# This file was automatically generated from:\n" > $(1);
+	$(hide) $(foreach f, $(2), printf "#\t" >> $(1); echo $(f) >> $(1);)
+	$(hide) printf "\n" >> $(1)
+	$(hide) cat $(2) >> $(1)
+endef
+
+#
+# make combined defconfig file
+#---------------------------------------
+$(TARGET_DEFCONFIG): FORCE $(PRODUCT_SPECIFIC_DEFCONFIGS)
+	$(call do-make-defconfig,$@,$(PRODUCT_SPECIFIC_DEFCONFIGS))
+
+.PHONY: FORCE
+FORCE:
