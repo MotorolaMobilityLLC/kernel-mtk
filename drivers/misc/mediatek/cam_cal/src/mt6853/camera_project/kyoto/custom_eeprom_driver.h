4cb51ed9918edafeb922ae78598035bd01a9aef2
liuwang2 <liuwang2@lenovo.com>
2021-03-10 19:56:41 +0800

    kyoto: add calibration status & module id
    [detail]
    add calibration status & module id for front camera
    and main camera eeprom.
    
    Change-Id: If330b4b5318d3e75ab9803351694a8c6a35a0223
    Signed-off-by: liuwang2 <liuwang2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1897587
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Qiang Guo <guoq8@motorola.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Shenhuai Huang <huangsh4@motorola.com>
    Reviewed-by: Shanghui Zhang <zhangsh@motorola.com>
    Reviewed-by: Heng Chen <chenheng3@lenovo.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/cam_cal/src/mt6853/camera_project/kyoto/custom_eeprom_driver.h
+++ drivers/misc/mediatek/cam_cal/src/mt6853/camera_project/kyoto/custom_eeprom_driver.h
@@ -32,37 +32,39 @@
 #include "cam_cal_list.h"
 #include "kd_camera_feature.h"
 #include "kd_imgsensor.h"
+#include "kd_imgsensor_define.h"
 
 #define DEV_NAME_FMT_DEV "/dev/camera_eeprom%u"
 #define LOG_INF(format, args...) pr_info(PFX "[%s] " format, __func__, ##args)
-#define LOG_DBG(format, args...) pr_err(PFX "[%s] " format, __func__, ##args)
+#define LOG_DBG(format, args...) pr_debug(PFX "[%s] " format, __func__, ##args)
 #define LOG_ERR(format, args...) pr_err(PFX "[%s] " format, __func__, ##args)
 
 
 #define BLACK_LEVEL_SAMSUNG_10B_64 0x40
 
-#define AWB_R_MIN 200
-#define AWB_R_MAX 880
-#define AWB_GR_MIN 760
-#define AWB_GR_MAX 880
-#define AWB_GB_MIN 760
-#define AWB_GB_MAX 880
-#define AWB_B_MIN 200
-#define AWB_B_MAX 880
+#define AWB_R_MIN 200*64
+#define AWB_R_MAX 880*64
+#define AWB_GR_MIN 760*64
+#define AWB_GR_MAX 880*64
+#define AWB_GB_MIN 760*64
+#define AWB_GB_MAX 880*64
+#define AWB_B_MIN 200*64
+#define AWB_B_MAX 880*64
 
-#define S5K3L6_EEPROM_CRC_MANUFACTURING_SIZE (37)
-#define S5K3L6_EEPROM_CRC_AF_CAL_SIZE (24)
-
-#define S5K3L6_EEPROM_CRC_AWB_CAL_SIZE (43)
-#define S5K3L6_EEPROM_CRC_LSC_SIZE (1868)
-#define S5K3L6_EEPROM_CRC_PDAF_OUTPUT1_SIZE (496)
-#define S5K3L6_EEPROM_CRC_PDAF_OUTPUT2_SIZE (918)
+/*
+0.97 * 100* 16384
+1.03 * 100* 16384
+*/
+#define AWB_GR_GB_MIN 1589248
+#define AWB_GR_GB_MAX 1687552
 
 typedef struct {
+	uint8_t cie_rsv[6];
 	uint8_t awb_r_g_golden_min_limit[1];
 	uint8_t awb_r_g_golden_max_limit[1];
 	uint8_t awb_b_g_golden_min_limit[1];
 	uint8_t awb_b_g_golden_max_limit[1];
+	// golden data
 	uint8_t awb_src_1_golden_r[2];
 	uint8_t awb_src_1_golden_gr[2];
 	uint8_t awb_src_1_golden_gb[2];
@@ -70,6 +72,7 @@ typedef struct {
 	uint8_t awb_src_1_golden_rg_ratio[2];
 	uint8_t awb_src_1_golden_bg_ratio[2];
 	uint8_t awb_src_1_golden_gr_gb_ratio[2];
+	// unit data
 	uint8_t awb_src_1_r[2];
 	uint8_t awb_src_1_gr[2];
 	uint8_t awb_src_1_gb[2];
@@ -78,7 +81,6 @@ typedef struct {
 	uint8_t awb_src_1_bg_ratio[2];
 	uint8_t awb_src_1_gr_gb_ratio[2];
 	uint8_t awb_reserve[5];
-	uint8_t awb_crc16[2];
 } camcal_awb;
 
 typedef struct {
@@ -114,10 +116,25 @@ typedef enum {
 }EEPROM_CRC_TYPE_ENUM;
 
 typedef struct {
+	// Common manufacture info
+	u8 table_revision[1];
+	u8 cal_hw_revision[1];
+	u8 cal_sw_revision[1];
+	u8 mot_part_number[8];
+	u8 actuator_id[1];
+	u8 lens_id[1];
+	u8 manufacturer_id[2];
+	u8 factory_id[2];
+	u8 manufacture_line[1];
+	u8 manufacture_date[3];
+	u8 serial_number[16];
+} manufacture_info;
+
+typedef struct {
     UINT16 Include;
     UINT32 StartAddr;
     UINT32 BlockSize;
-    void (*doCalDataCheck)(u8 *pCamCaldata, UINT32 start_addr, UINT32 BlockSize, RetStatus *rStatus);
+    void (*doCalDataCheck)(u8 *pCamCaldata, UINT32 start_addr, UINT32 BlockSize, mot_calibration_info_t *mot_cal_info);
 } CALIBRATION_ITEM_STRUCT;
 
 typedef struct {
@@ -125,9 +142,9 @@ typedef struct {
 	const CALIBRATION_ITEM_STRUCT CalItemTbl[EEPROM_CRC_LIST];
 }MOT_EEPROM_CAL;
 
-static void mot_check_mnf_data(u8 *data, UINT32 StartAddr, UINT32 BlockSize, RetStatus *rStatus);
-static void mot_check_af_data(u8 *data, UINT32 StartAddr, UINT32 BlockSize, RetStatus *rStatus);
-static void mot_check_awb_data(u8 *data, UINT32 StartAddr, UINT32 BlockSize, RetStatus *rStatus);
-static void mot_check_lsc_data(u8 *data, UINT32 StartAddr, UINT32 BlockSize, RetStatus *rStatus);
-static void mot_check_pdaf_data(u8 *data, UINT32 StartAddr, UINT32 BlockSize, RetStatus *rStatus);
+static void mot_check_mnf_data(u8 *data, UINT32 StartAddr, UINT32 BlockSize, mot_calibration_info_t *mot_cal_info);
+static void mot_check_af_data(u8 *data, UINT32 StartAddr, UINT32 BlockSize, mot_calibration_info_t *mot_cal_info);
+static void mot_check_awb_data(u8 *data, UINT32 StartAddr, UINT32 BlockSize, mot_calibration_info_t *mot_cal_info);
+static void mot_check_lsc_data(u8 *data, UINT32 StartAddr, UINT32 BlockSize, mot_calibration_info_t *mot_cal_info);
+static void mot_check_pdaf_data(u8 *data, UINT32 StartAddr, UINT32 BlockSize, mot_calibration_info_t *mot_cal_info);
 #endif
