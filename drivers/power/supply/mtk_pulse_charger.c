f96190eeb8fedb7593b4b4360963bbffc15e803c
Qiong Xie <xieq2@motorola.com>
2021-03-12 05:42:30 -0600

    Revert "(CR):Charger: disable charging when ATM mode on."
    This reverts commit f04ab9456402c4411385655cc4893da8d51e90b0.
    
    Change-Id: Ia0a4284aa8141a091f3aaa942dd410a0e34f72ec
    Reviewed-on: https://gerrit.mot.com/1899642
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Jianguo Liu <liujg6@motorola.com>

--- drivers/power/supply/mtk_pulse_charger.c
+++ drivers/power/supply/mtk_pulse_charger.c
@@ -246,10 +246,8 @@ static void linear_chg_turn_on_charging(struct mtk_charger *info)
 		chr_err("[charger]Charger Error, turn OFF charging !\n");
 #ifdef SUPPORT_BOOTMODE
 	} else if ((get_boot_mode() == META_BOOT) ||
-			((get_boot_mode() == ADVMETA_BOOT)) ||
-			(!strcmp(atm_mode, "enable"))) {
+			((get_boot_mode() == ADVMETA_BOOT))) {
 		charging_enable = false;
-                //charger_manager_notifier(info, CHARGER_NOTIFY_STOP_CHARGING);
 		chr_err("[charger]In meta or advanced meta mode, disable charging\n");
 #endif
 	} else {
