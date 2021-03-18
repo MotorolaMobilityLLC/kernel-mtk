b9e3443e4242ba3e38bf2593025be46f24b8e590
huangsh4 <huangsh4@lenovo.com>
2021-03-18 22:53:42 +0800

    Ellis: bringup main/front/depth camera.
    Ellis: bringup main/front/depth camera.
    
    Change-Id: I320b88788453a79d8ca0da12d196d106633935ef
    Signed-off-by: huangsh4 <huangsh4@mt.com>
    Reviewed-on: https://gerrit.mot.com/1905032
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Shenhuai Huang <huangsh4@motorola.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Dawei Wang <wangdw10@motorola.com>
    Reviewed-by: Wang Liu <liuwang2@mt.com>
    Reviewed-by: Qiang Guo <guoq8@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>

--- drivers/misc/mediatek/imgsensor/inc/kd_imgsensor.h
+++ drivers/misc/mediatek/imgsensor/inc/kd_imgsensor.h
@@ -114,6 +114,11 @@
  *
  ************************************************************************/
 /* SENSOR CHIP VERSION */
+//add ellis sensor id begin
+#define MOT_ELLIS_HI1336_SENSOR_ID 0x1336
+#define MOT_ELLIS_SC500CS_SENSOR_ID 0xee27
+#define MOT_ELLIS_OV02B1B_SENSOR_ID 0x002b
+//add ellis sensor id end
 /*IMX*/
 #define IMX499_SENSOR_ID                        0x0499
 #define IMX486_SENSOR_ID                        0x0486
@@ -529,7 +534,11 @@
 #define SENSOR_DRVNAME_IMX135_MIPI_RAW_8MP      "imx135_mipi_raw_8mp"
 #define SENSOR_DRVNAME_OV13870_MIPI_RAW_5MP     "ov13870_mipi_raw_5mp"
 #define SENSOR_DRVNAME_OV8856_MIPI_RAW_5MP      "ov8856_mipi_raw_5mp"
-
+//add ellis sensor drvname begin
+#define SENSOR_DRVNAME_MOT_ELLIS_HI1336_MIPI_RAW "mot_ellis_hi1336_mipi_raw"
+#define SENSOR_DRVNAME_MOT_ELLIS_SC500CS_MIPI_RAW "mot_ellis_sc500cs_mipi_raw"
+#define SENSOR_DRVNAME_MOT_ELLIS_OV02B1B_MIPI_RAW "mot_ellis_ov02b1b_mipi_raw"
+//add ellis sensor drvname end
 /******************************************************************************
  *
  ******************************************************************************/
