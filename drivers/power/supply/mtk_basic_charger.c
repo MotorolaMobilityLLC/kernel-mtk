a474f98a3ee42ac323b69a14297c8c03cfe50bb6
xieqiong2 <xieqiong2@lenovo.com>
2021-03-19 18:48:43 +0800

    charge: Optimize charge process at factory mode.
    Setting charge process after charge module finish
    Otherwise will cause logic confusion at some scene.
    While check typeC adatper, add pointer check
    
    Change-Id: I823e66ed25a2b3d965e68562d572dae3159341ae
    Signed-off-by: xieqiong2 <xieqiong2@mt.com>
    Reviewed-on: https://gerrit.mot.com/1906038
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: liujian <liujian24@mt.com>

--- drivers/power/supply/mtk_basic_charger.c
+++ drivers/power/supply/mtk_basic_charger.c
@@ -87,6 +87,9 @@ static bool is_typec_adapter(struct mtk_charger *info)
 {
 	int rp;
 
+	if (info == NULL || info->pd_adapter == NULL)
+		return false;
+
 	rp = adapter_dev_get_property(info->pd_adapter, TYPEC_RP_LEVEL);
 	if (info->pd_type == MTK_PD_CONNECT_TYPEC_ONLY_SNK &&
 			rp != 500 &&
