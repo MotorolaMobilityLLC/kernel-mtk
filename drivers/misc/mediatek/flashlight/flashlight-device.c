dc853bcbf433bccf7f5a73fe416dba56b80afcd1
huangsh4 <huangsh4@lenovo.com>
2021-03-22 20:35:09 +0800

    Ellis: enable flashlight feature
    Ellis: enable flashlight feature.
    
    Change-Id: I59e570d68d49a48a0bf70ab45f4ecd4d74f4636c
    Signed-off-by: huangsh4 <huangsh4@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1906966
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Reviewed-by: Zhilong Wang <wangzl30@motorola.com>
    Reviewed-by: Shanghui Zhang <zhangsh@motorola.com>
    Reviewed-by: Qiang Guo <guoq8@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>

--- drivers/misc/mediatek/flashlight/flashlight-device.c
+++ drivers/misc/mediatek/flashlight/flashlight-device.c
@@ -90,14 +90,7 @@ const struct flashlight_device_id flashlight_id[] = {
 #else
 const struct flashlight_device_id flashlight_id[] = {
 	/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
-	{0, 0, 0, "flashlights-none", -1, 0},
-	{0, 1, 0, "flashlights-none", -1, 0},
-	{1, 0, 0, "flashlights-none", -1, 0},
-	{1, 1, 0, "flashlights-none", -1, 0},
-	{0, 0, 1, "flashlights-none", -1, 0},
-	{0, 1, 1, "flashlights-none", -1, 0},
-	{1, 0, 1, "flashlights-none", -1, 0},
-	{1, 1, 1, "flashlights-none", -1, 0},
+	{0, 0, 0, "flashlights_ocp8132", 0, 0},
 };
 #endif
 
