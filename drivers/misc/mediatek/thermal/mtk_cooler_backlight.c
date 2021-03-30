ab3816f549e3636959ff11e4d9048c30ee0ec4e6
huaqin62 <huaqin62@motorola.com>
2021-03-30 14:10:58 +0800

    thermal:backlight thermal control
    PROPAGATED_from (CR)
    
    thermalX
    backlight control by thermal
    
    Change-Id: Ibb845159787edebf98e0975058250ce2d8d9d952
    Signed-off-by: huaqin62 <huaqin62@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1912620
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Lu Chai <chailu1@motorola.com>
    Reviewed-by: Wei Xu <xuwei9@lenovo.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: Robert Cabral <robertc@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2191513

--- drivers/misc/mediatek/thermal/mtk_cooler_backlight.c
+++ drivers/misc/mediatek/thermal/mtk_cooler_backlight.c
@@ -64,7 +64,7 @@ static void mtk_cl_backlight_set_max_brightness_limit(void)
 				defined(CONFIG_LEDS_MTK_I2C)
 			setMaxBrightness("lcd-backlight", 70, 0);
 			#else
-			setMaxbrightness(178, 0);
+			setMaxbrightness(178, 1);
 			#endif
 			break;
 		case 2:
