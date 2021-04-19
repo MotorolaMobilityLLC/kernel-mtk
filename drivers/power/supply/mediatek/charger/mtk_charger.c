10b0f79779ef0218e5339e4a800c27d72e94ff82
mahj8 <mahj8@lenovo.com>
2021-04-19 10:49:22 +0800

    charGing: charGing state machine don't depend on battery soc
    charGing state machine use battery voltage as condition to switch state
    instead of battery soc.
    1.Sometimes batt soc have some error in low temperature and less
    then 100%, charGing can't auto stop when battery FULL.
    2.unit can't recharge after charge full.When unit charge full, it will get
    the forced capacity 100%, so chrg_step can't be switch to STEP_NORM
    from STEP_FULL.
    
    refer to https://gerrit.mot.com/1456841
    
    Change-Id: I4d0e4ff47f12dcbb2e5bbaa7fdd329ac73711b53
    Signed-off-by: mahj8 <mahj8@mt.com>
    Reviewed-on: https://gerrit.mot.com/1926738
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/mediatek/charger/mtk_charger.c
+++ drivers/power/supply/mediatek/charger/mtk_charger.c
@@ -2286,8 +2286,7 @@ static void mmi_charger_check_status(struct charger_manager *info)
 	} else if (mmi->pres_chrg_step == STEP_NORM) {
 		if (!zone->fcc_norm_ma)
 			mmi->pres_chrg_step = STEP_FLOAT;
-		else if ((batt_soc < 100) ||
-			 (batt_mv + HYST_STEP_MV) < max_fv_mv) {
+		else if ((batt_mv + HYST_STEP_MV/2) < max_fv_mv) {
 			mmi->chrg_taper_cnt = 0;
 			mmi->pres_chrg_step = STEP_NORM;
 		} else if (mmi_has_current_tapered(info, batt_ma,
@@ -2295,7 +2294,7 @@ static void mmi_charger_check_status(struct charger_manager *info)
 			mmi->pres_chrg_step = STEP_FULL;
 		}
 	} else if (mmi->pres_chrg_step == STEP_FULL) {
-		if (batt_soc <= 99) {
+		if (batt_mv < (max_fv_mv - HYST_STEP_MV * 2)) {
 			mmi->chrg_taper_cnt = 0;
 			mmi->pres_chrg_step = STEP_NORM;
 		}
