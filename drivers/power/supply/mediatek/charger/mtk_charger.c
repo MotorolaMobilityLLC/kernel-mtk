a180585416471b0f5fe2c1d16c77d184da3db7a4
chailu1 <chailu1@lenovo.com>
2021-05-09 20:02:44 +0800

    charging: add disable ichg and ibat api for adaptive charging
    add disable ichg and ibat api
    for adaptive charging feature
    
    porting from
    https://gerrit.mot.com/#/c/1921957/
    https://gerrit.mot.com/#/c/1935360/
    
    Change-Id: I59ea3c13a50ee5cfed8e0fd731a3c578a5ae13fe
    Signed-off-by: chailu1 <chailu1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1943605
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/mediatek/charger/mtk_charger.c
+++ drivers/power/supply/mediatek/charger/mtk_charger.c
@@ -65,6 +65,7 @@
 
 #include "mtk_charger_intf.h"
 #include "mtk_charger_init.h"
+#include <linux/qpnp_adaptive_charge.h>
 
 static char atm_mode[10];
 int __init atm_mode_init(char *s)
@@ -575,6 +576,34 @@ int charger_manager_force_charging_current(struct charger_consumer *consumer,
 	}
 	return -EBUSY;
 }
+
+void adaptive_charging_disable_ichg(bool on)
+{
+	struct charger_manager *info = pinfo;
+
+	if (info == NULL)
+		return;
+
+	info->mmi.adaptive_charging_disable_ichg = !!on;
+	pr_info("%s: adaptive charging disable ichg %d\n", __func__,
+		info->mmi.adaptive_charging_disable_ichg);
+	_wake_up_charger(info);
+}
+EXPORT_SYMBOL(adaptive_charging_disable_ichg);
+
+void adaptive_charging_disable_ibat(bool on)
+{
+	struct charger_manager *info = pinfo;
+
+	if (info == NULL)
+		return;
+
+	info->mmi.adaptive_charging_disable_ibat = !!on;
+	pr_info("%s: adaptive charging disable ibat %d\n", __func__,
+		info->mmi.adaptive_charging_disable_ibat);
+	_wake_up_charger(info);
+}
+EXPORT_SYMBOL(adaptive_charging_disable_ibat);
 
 int charger_manager_get_current_charging_type(struct charger_consumer *consumer)
 {
@@ -2467,6 +2496,14 @@ static void mmi_charger_check_status(struct charger_manager *info)
 		mmi->target_fcc,
 		mmi->target_usb,
 		mmi->demo_discharging);
+	pr_info("[%s]adaptive charging:disable_ibat = %d, "
+		"disable_ichg = %d, enable HZ = %d, "
+		"charging disable = %d\n",
+		__func__,
+		mmi->adaptive_charging_disable_ibat,
+		mmi->adaptive_charging_disable_ichg,
+		mmi->charging_enable_hz,
+		mmi->battery_charging_disable);
 end_check:
 
 	return;
@@ -2754,6 +2791,10 @@ void mmi_init(struct charger_manager *info)
 
 	info->mmi.is_factory_image = false;
 	info->mmi.charging_limit_modes = CHARGING_LIMIT_UNKNOWN;
+	info->mmi.adaptive_charging_disable_ibat = false;
+	info->mmi.adaptive_charging_disable_ichg = false;
+	info->mmi.charging_enable_hz = false;
+	info->mmi.battery_charging_disable = false;
 
 	rc = parse_mmi_dt(info, &info->pdev->dev);
 	if (rc < 0)
