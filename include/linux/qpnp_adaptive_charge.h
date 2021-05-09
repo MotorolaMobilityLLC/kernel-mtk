460e87c6dd51b3a9412517974ffbdabd64fabf22
chailu1 <chailu1@lenovo.com>
2021-05-09 20:02:44 +0800

    charGing: add disable ichg and ibat api for adaptive charGing
    add disable ichg and ibat api
    for adaptive charGing feature
    
    porting from
    https://gerrit.mot.com/#/c/1921957/
    https://gerrit.mot.com/#/c/1935360/
    
    Change-Id: I59ea3c13a50ee5cfed8e0fd731a3c578a5ae13fe
    Signed-off-by: chailu1 <chailu1@mt.com>
    Reviewed-on: https://gerrit.mot.com/1943605
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key

---
+++ include/linux/qpnp_adaptive_charge.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) 2015-2017, 2019-2020, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __MMI_ADAPTIVE_CHARGE_H
+#define __MMI_ADAPTIVE_CHARGE_H
+
+void adaptive_charging_disable_ichg(bool on);
+void adaptive_charging_disable_ibat(bool on);
+#endif
