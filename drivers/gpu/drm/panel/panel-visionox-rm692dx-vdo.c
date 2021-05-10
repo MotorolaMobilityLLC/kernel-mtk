0d76b8fbeb24123d383053f67a7f91dd657d95e0
wangyq13 <wangyq13@lenovo.com>
2021-05-10 10:52:34 +0800

    drm/panel: reduce the logs of backlight
    Kyoto enable Haab, so there are more backlight settings,
    more backlight kernel logs.
    Change to pr_debug to reduce backlight logs for performance.
    
    Change-Id: I1d88a5c4304c8561c302e00960627e49739a4ada
    Signed-off-by: wangyq13 <wangyq13@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1943766
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/panel-visionox-rm692dx-vdo.c
+++ drivers/gpu/drm/panel/panel-visionox-rm692dx-vdo.c
@@ -723,7 +723,7 @@ static int lcm_setbacklight_cmdq(void *dsi, dcs_write_gce cb,
 	if ((level != 0) && (level < 16))
 		level = 16;
 
-	pr_info("%s backlight = %d\n", __func__, level);
+	pr_debug("%s backlight = %d\n", __func__, level);
 
 	level = level * 2047  / 255;
 	bl_tb0[1] = ((level >> 8) & 0x7);
