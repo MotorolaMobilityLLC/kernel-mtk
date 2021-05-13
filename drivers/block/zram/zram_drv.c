27e458c4c3f9263a2025c9fa7251baf68d25d696
jixj <jixj@lenovo.com>
2021-05-13 17:34:13 +0800

    block/zram: Add "idle_percent" node
    PROPAGATED_from (CR)
    
    Add /sys/block/zram<id>/idle_percent to show idle page
    percent so that userspace can get usage of idle page
    
    Change-Id: I7645e8955377c4d4ab99f11d1a2deacce50b54a5
    Reviewed-on: https://gerrit.mot.com/1948361
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1957923
    Reviewed-by: Xiaojun Ji <jixj@motorola.com>
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Reviewed-by: Yonghui Jia <jiayh2@motorola.com>
    Signed-off-by: Levy Gabriel da Silva Galvao <levy@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2512621

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -331,6 +331,23 @@ static ssize_t idle_store(struct device *dev,
 	return len;
 }
 
+static ssize_t idle_percent_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct zram *zram = dev_to_zram(dev);
+	unsigned long nr_pages = zram->disksize >> PAGE_SHIFT;
+	unsigned long index = 0, idle_num = 0;
+
+	down_read(&zram->init_lock);
+	for (index = 0; index < nr_pages; index++) {
+		if (zram_test_flag(zram, index, ZRAM_IDLE))
+			idle_num++;
+	}
+	up_read(&zram->init_lock);
+
+	return scnprintf(buf, PAGE_SIZE, "%u\n", idle_num * 100 / nr_pages);
+}
+
 #ifdef CONFIG_ZRAM_WRITEBACK
 static ssize_t writeback_limit_enable_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t len)
@@ -1874,6 +1891,7 @@ static DEVICE_ATTR_WO(reset);
 static DEVICE_ATTR_WO(mem_limit);
 static DEVICE_ATTR_WO(mem_used_max);
 static DEVICE_ATTR_WO(idle);
+static DEVICE_ATTR_RO(idle_percent);
 static DEVICE_ATTR_RW(max_comp_streams);
 static DEVICE_ATTR_RW(comp_algorithm);
 #ifdef CONFIG_ZRAM_WRITEBACK
@@ -1891,6 +1909,7 @@ static struct attribute *zram_disk_attrs[] = {
 	&dev_attr_mem_limit.attr,
 	&dev_attr_mem_used_max.attr,
 	&dev_attr_idle.attr,
+	&dev_attr_idle_percent.attr,
 	&dev_attr_max_comp_streams.attr,
 	&dev_attr_comp_algorithm.attr,
 #ifdef CONFIG_ZRAM_WRITEBACK
