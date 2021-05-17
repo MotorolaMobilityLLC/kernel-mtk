ca2b7a284446f41d927d2a65a93341d935d0631f
hujing3 <hujing3@lenovo.com>
2021-05-17 11:19:27 +0800

    lcm:corfu: add EVT visionox+FT3519 panel
    Corfu/Corfu lite: formal EVT panel visionox+FT3519 support
    display driver bringup
    
    Change-Id: I18d519704c5b330d395859d7af22cdf60c19dcbb
    Signed-off-by: hujing3 <hujing3@mt.com>
    Reviewed-on: https://gerrit.mot.com/1950237
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Jianguo Liu <liujg6@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1963496
    Reviewed-by: Qiong Xie <xieq2@motorola.com>

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
@@ -16,6 +16,7 @@
 
 #include <lcm_drv.h>
 
+extern struct LCM_DRIVER mipi_mot_vid_visionox_ft3519_fhd_643_lcm_drv;
 extern struct LCM_DRIVER mipi_mot_vid_visionox_gt9885_fhd_647_lcm_drv;
 extern struct LCM_DRIVER otm1282a_hd720_dsi_vdo_60hz_lcm_drv;
 extern struct LCM_DRIVER otm1282a_hd720_dsi_vdo_lcm_drv;
