1faaf6badcc1a2c7fe741c778593abcd3d18797e
Mike SU <suzh1@lenovo.com>
2021-05-27 14:51:24 +0800

    MOTODESK-3589 NCM interface did NOT down
    Root cause: ncm_unbind did NOT gether_cleanup(net_dev)
    Solution: move setup net_dev from ncm_alloc to ncm_bind
    and move cleanup net_dev from ncm_free to ncm_unbind
    
    Change-Id: I2a918ac31697caf6c615073cfcef6a145e019fe1
    Reviewed-on: https://gerrit.mot.com/1961941
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yuecai Liu <liuyc20@motorola.com>
    Reviewed-by: Zonghua Liu <a17671@motorola.com>
    Reviewed-by: Xuejin Wang <wangxjn@motorola.com>
    Submit-Approved: Jira Key

--- drivers/usb/gadget/function/f_ncm.c
+++ drivers/usb/gadget/function/f_ncm.c
@@ -1441,7 +1441,6 @@ static int ncm_bind(struct usb_configuration *c, struct usb_function *f)
 		f->os_desc_n = 1;
 		f->os_desc_table[0].os_desc = &ncm_opts->ncm_os_desc;
 	}
-	/* End MOTODESK-2239 */
 	/*
 	 * in drivers/usb/gadget/configfs.c:configfs_composite_bind()
 	 * configurations are bound in sequence with list_for_each_entry,
@@ -1451,20 +1450,39 @@ static int ncm_bind(struct usb_configuration *c, struct usb_function *f)
 	 */
 	if (!ncm_opts->bound) {
 		mutex_lock(&ncm_opts->lock);
+		/* MOTODESK-2239 Use ncm interface name. suzh1 2021-02-05 */
+		ncm_opts->net = gether_setup_name_default("ncm");
+		if (IS_ERR(ncm_opts->net)) {
+			status = PTR_ERR(ncm_opts->net);
+			mutex_unlock(&ncm_opts->lock);
+			goto error;
+		}
 		gether_set_gadget(ncm_opts->net, cdev->gadget);
 		status = gether_register_netdev(ncm_opts->net);
 		mutex_unlock(&ncm_opts->lock);
-		if (status)
-			/* MOTODESK-2239 Add OS descriptor. suzh1 2021-02-05 */
-			goto fail;
+		if (status) {
+			free_netdev(ncm_opts->net);
+			goto error;
+		}
 		ncm_opts->bound = true;
 	}
+
+	/* export host's Ethernet address in CDC format */
+	status = gether_get_host_addr_cdc(ncm_opts->net, ncm->ethaddr,
+				      sizeof(ncm->ethaddr));
+	if (status < 12) { /* strlen("01234567890a") */
+		ERROR(cdev, "%s: failed to get host eth addr, err %d\n",
+		__func__, status);
+		status = -EINVAL;
+		goto netdev_cleanup;
+	}
+	ncm->port.ioport = netdev_priv(ncm_opts->net);
+
 	us = usb_gstrings_attach(cdev, ncm_strings,
 				 ARRAY_SIZE(ncm_string_defs));
-	/* Begin MOTODESK-2239 Add OS descriptor support. suzh1 2021-02-05 */
 	if (IS_ERR(us)) {
 		status = PTR_ERR(us);
-		goto fail;
+		goto netdev_cleanup;
 	}
 	/* End MOTODESK-2239 */
 	ncm_control_intf.iInterface = us[STRING_CTRL_IDX].id;
@@ -1567,16 +1585,18 @@ static int ncm_bind(struct usb_configuration *c, struct usb_function *f)
 	return 0;
 
 fail:
-	/* Begin MOTODESK-2239 Add OS descriptor support. suzh1 2021-02-05 */
-	kfree(f->os_desc_table);
-	f->os_desc_n = 0;
-	/* End MOTODESK-2239 */
-
 	if (ncm->notify_req) {
 		kfree(ncm->notify_req->buf);
 		usb_ep_free_request(ncm->notify, ncm->notify_req);
 	}
+/* Begin MOTODESK-2239 Add OS descriptor support. suzh1 2021-02-05 */
+netdev_cleanup:
+	gether_cleanup(netdev_priv(ncm_opts->net));
 
+error:
+	kfree(f->os_desc_table);
+	f->os_desc_n = 0;
+/* End MOTODESK-2239 */
 	ERROR(cdev, "%s: can't bind, err %d\n", f->name, status);
 
 	return status;
@@ -1624,8 +1644,6 @@ static void ncm_free_inst(struct usb_function_instance *f)
 	opts = container_of(f, struct f_ncm_opts, func_inst);
 	if (opts->bound)
 		gether_cleanup(netdev_priv(opts->net));
-	else
-		free_netdev(opts->net);
 	/* MOTODESK-2239 Add OS descriptor support. suzh1 2021-02-05 */
 	kfree(opts->ncm_interf_group);
 	kfree(opts);
@@ -1646,13 +1664,6 @@ static struct usb_function_instance *ncm_alloc_inst(void)
 
 	mutex_init(&opts->lock);
 	opts->func_inst.free_func_inst = ncm_free_inst;
-	/* MOTODESK-2239 Use ncm interface name. suzh1 2021-02-05 */
-	opts->net = gether_setup_name_default("ncm");
-	if (IS_ERR(opts->net)) {
-		struct net_device *net = opts->net;
-		kfree(opts);
-		return ERR_CAST(net);
-	}
 	INIT_LIST_HEAD(&opts->ncm_os_desc.ext_prop);
 
 	descs[0] = &opts->ncm_os_desc;
@@ -1688,8 +1699,12 @@ static void ncm_free(struct usb_function *f)
 static void ncm_unbind(struct usb_configuration *c, struct usb_function *f)
 {
 	struct f_ncm *ncm = func_to_ncm(f);
+	struct f_ncm_opts *opts = container_of(f->fi, struct f_ncm_opts,
+					func_inst);
 
 	DBG(c->cdev, "ncm unbind\n");
+
+	opts->bound = false;
 
 	hrtimer_cancel(&ncm->task_timer);
 	/* Begin MOTODESK-2239 Add OS descriptor support. suzh1 2021-02-05 */
@@ -1707,13 +1722,14 @@ static void ncm_unbind(struct usb_configuration *c, struct usb_function *f)
 
 	kfree(ncm->notify_req->buf);
 	usb_ep_free_request(ncm->notify, ncm->notify_req);
+
+	gether_cleanup(netdev_priv(opts->net));
 }
 
 static struct usb_function *ncm_alloc(struct usb_function_instance *fi)
 {
 	struct f_ncm		*ncm;
 	struct f_ncm_opts	*opts;
-	int status;
 
 	/* allocate and initialize one new instance */
 	ncm = kzalloc(sizeof(*ncm), GFP_KERNEL);
@@ -1723,20 +1739,9 @@ static struct usb_function *ncm_alloc(struct usb_function_instance *fi)
 	opts = container_of(fi, struct f_ncm_opts, func_inst);
 	mutex_lock(&opts->lock);
 	opts->refcnt++;
-
-	/* export host's Ethernet address in CDC format */
-	status = gether_get_host_addr_cdc(opts->net, ncm->ethaddr,
-				      sizeof(ncm->ethaddr));
-	if (status < 12) { /* strlen("01234567890a") */
-		kfree(ncm);
-		mutex_unlock(&opts->lock);
-		return ERR_PTR(-EINVAL);
-	}
 	ncm_string_defs[STRING_MAC_IDX].s = ncm->ethaddr;
-
 	spin_lock_init(&ncm->lock);
 	ncm_reset_values(ncm);
-	ncm->port.ioport = netdev_priv(opts->net);
 	mutex_unlock(&opts->lock);
 	ncm->port.is_fixed = true;
 	ncm->port.supports_multi_frame = true;
