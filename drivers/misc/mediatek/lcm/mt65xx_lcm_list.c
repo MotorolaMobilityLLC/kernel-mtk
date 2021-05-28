51164d7228e682eca27363b6ab8ee4d54b37f2d1
hujing3 <hujing3@lenovo.com>
2021-05-28 09:17:08 +0800

    kernel/lcm:corfu: add EVB visionox+gt9885 panel
    Corfu: EVB vision+gooidx panel
    display driver bringup
    
    Change-Id: Ia7a8e7de1c7f98ffd24d1334dc9bb3ba9e6e3d61
    Signed-off-by: hujing3 <hujing3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1963150
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.c
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.c
@@ -1327,6 +1327,10 @@ struct LCM_DRIVER *lcm_driver_list[] = {
 #if defined(DSI_PANEL_MOT_MIPI_VID_HLT_FT8006S_652_HDP)
 	&mipi_mot_vid_hlt_ft8006s_652_hdp_lcm_drv,
 #endif
+
+#if defined(DSI_PANEL_MOT_MIPI_VID_VISIONOX_GT9885_FHD)
+	&mipi_mot_vid_visionox_gt9885_fhd_647_lcm_drv,
+#endif
 };
 
 unsigned char lcm_name_list[][128] = {
