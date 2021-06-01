560952eacf8cc7463bddc78a299b6097d0057ad4
lugang <lugang@longcheer.com>
2021-06-01 17:39:00 +0800

    corfu: add camera for mainline in kernel
    add corfu camera
    for mainline
    
    Change-Id: Id9f6390b5f1d184f8dd45d7903d84c04920b012a
    Reviewed-on: https://gerrit.mot.com/1966379
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/inc/kd_imgsensor.h
+++ drivers/misc/mediatek/imgsensor/inc/kd_imgsensor.h
@@ -381,7 +381,10 @@
 //add kyoto sensor id end
 
 //add corfu sensor id begin
+#define MOT_CORFU_S5KGM1_QTECH_ID       0x08D1
 #define MOT_CORFU_HI1336_OFILM_ID       0x1336
+#define MOT_CORFU_S5K4H7_QTECH_ID       0x487C
+#define MOT_CORFU_GC02M1_TSP_ID         0x02e1
 //add corfu sensor id end
 //add coful sensor id begin
 #define MOT_COFUL_S5K4H7_QTECH_ID       0x487C
@@ -636,7 +639,10 @@
 //add kyoto sensor drvname end
 
 //add corfu sensor drvname begin
+#define SENSOR_DRVNAME_MOT_CORFU_S5KGM1_QTECH               "mot_corfu_s5kgm1_qtech"
 #define SENSOR_DRVNAME_MOT_CORFU_HI1336_OFILM               "mot_corfu_hi1336_ofilm"
+#define SENSOR_DRVNAME_MOT_CORFU_S5K4H7_QTECH               "mot_corfu_s5k4h7_qtech"
+#define SENSOR_DRVNAME_MOT_CORFU_GC02M1_TSP                 "mot_corfu_gc02m1_tsp"
 //add corfu sensor drvname end
 //add coful sensor drvname begin
 #define SENSOR_DRVNAME_MOT_COFUL_S5K4H7_QTECH               "mot_coful_s5k4h7_qtech"
