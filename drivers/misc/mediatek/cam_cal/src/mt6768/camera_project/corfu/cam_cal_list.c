7d8c5d3d8b5fed7ee528f435d25b4a776e773c24
xiaozhenyu <xiaozhenyu@longcheer.com>
2021-06-04 22:37:49 +0800

    corfu: q3 camera otp bring up <1/2>
    1. s5kgm1 eeprom (mnf af wb lsc pdaf)
    2. hi1336 eeprom (mnf wb lsc)
    3. s5k4h7 otp (mnf wb lsc)
    4. gc02m1 otp (wb)
    
    Change-Id: I65fdd32b2ef482669c41e6fb9f41a257302cd43a
    Signed-off-by: xiaozhenyu <xiaozhenyu@longcheer.com>
    Reviewed-on: https://gerrit.mot.com/1970753
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/misc/mediatek/cam_cal/src/mt6768/camera_project/corfu/cam_cal_list.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2018 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+#include <linux/kernel.h>
+#include "cam_cal_list.h"
+#include "eeprom_i2c_common_driver.h"
+#include "eeprom_i2c_custom_driver.h"
+#include "kd_imgsensor.h"
+
+extern unsigned int moto_s5k4h7_read_region(struct i2c_client *client, unsigned int addr,
+			unsigned char *data, unsigned int size);
+
+struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
+	/*Below is commom sensor */
+	{MOT_CORFU_S5KGM1_QTECH_ID, 0xA0, Common_read_region},
+	{MOT_CORFU_HI1336_OFILM_ID, 0xA2, Common_read_region},
+	// custom otp
+	{MOT_CORFU_S5K4H7_QTECH_ID, 0x5A, moto_s5k4h7_read_region},
+	{MOT_CORFU_GC02M1_TSP_ID, 0x6e, GC02M1_read_region},
+	/*  ADD before this line */
+	{0, 0, 0}       /*end of list */
+};
+
+unsigned int cam_cal_get_sensor_list(
+	struct stCAM_CAL_LIST_STRUCT **ppCamcalList)
+{
+	if (ppCamcalList == NULL)
+		return 1;
+
+	*ppCamcalList = &g_camCalList[0];
+	return 0;
+}
+
+
