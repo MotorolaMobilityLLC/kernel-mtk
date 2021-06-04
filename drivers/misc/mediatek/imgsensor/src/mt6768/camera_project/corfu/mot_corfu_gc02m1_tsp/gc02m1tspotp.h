f01e6d7d655a0f4e8dd16a3c4c03765002d0bf3f
xiaozhenyu <xiaozhenyu@longcheer.com>
2021-06-04 22:37:49 +0800

    corfu: q3 camera otp bring up <1/2>
    1. s5kgm1 eeprom (mnf af wb lsc pdaf)
    2. hi1336 eeprom (mnf wb lsc)
    3. s5k4h7 otp (mnf wb lsc)
    4. gc02m1 otp (wb)
    
    Change-Id: I65fdd32b2ef482669c41e6fb9f41a257302cd43a
    Signed-off-by: xiaozhenyu <xiaozhenyu@longcheer.com>
    Reviewed-on: https://gerrit.mot.com/1970753
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/mt6768/camera_project/corfu/mot_corfu_gc02m1_tsp/gc02m1tspotp.h
+++ drivers/misc/mediatek/imgsensor/src/mt6768/camera_project/corfu/mot_corfu_gc02m1_tsp/gc02m1tspotp.h
@@ -11,15 +11,25 @@
  * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
 
-#ifndef __S5K4H7QTECH_OTP_H__
-#define __S5K4H7QTECH_OTP_H__
+#ifndef __GC02M1TSP_OTP_H__
+#define __GC02M1TSP_OTP_H__
 
 extern int iReadRegI2C(u8 *a_pSendData, u16 a_sizeSendData,
 		       u8 *a_pRecvData, u16 a_sizeRecvData, u16 i2cId);
 extern int iWriteRegI2C(u8 *a_pSendData, u16 a_sizeSendData, u16 i2cId);
 
-bool S5K4H7QTECH_otp_update(void);
-unsigned char s5k4h7qtech_get_module_id(void);
+bool GC02M1TSP_otp_update(void);
+unsigned int gc02m1_read_all_data(mot_calibration_info_t * pOtpCalInfo);
 
+typedef struct gc02m1_group_data {
+	u8 wb_flag;
+	u8 wb_data[6];
+	u8 wb_checksum[2];
+} gc02m1_group_data_t;
+
+typedef struct gc02m1_otp {
+	struct gc02m1_group_data group_data[3];
+	u8 mtk_info[3][14];
+} gc02m1_otp_t;
 
 #endif
