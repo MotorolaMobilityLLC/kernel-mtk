75c9f786754b05ce916c0ecfb13c0b75fbdffb47
lugang <lugang@longcheer.com>
2021-06-04 19:11:14 +0800

    corfu: add ois function and optimize s5kgm1 driver
    add ois and optimize s5kgm1
    in mainline
    
    Change-Id: I089ad78567e648cde03e46f0582107ad8e8ddbf2
    Signed-off-by: lugang <lugang@longcheer.com>
    Reviewed-on: https://gerrit.mot.com/1970448
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lens/mtk/main/inc/lens_list.h
+++ drivers/misc/mediatek/lens/mtk/main/inc/lens_list.h
@@ -373,4 +373,15 @@ extern long WV511AAF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
 extern int WV511AAF_Release(struct inode *a_pstInode, struct file *a_pstFile);
 extern int WV511AAF_GetFileName(unsigned char *pFileName);
 
+#define DW9781CAF_SetI2Cclient DW9781CAF_SetI2Cclient_Main
+#define DW9781CAF_Ioctl DW9781CAF_Ioctl_Main
+#define DW9781CAF_Release DW9781CAF_Release_Main
+#define DW9781CAF_GetFileName DW9781CAF_GetFileName_Main
+extern int DW9781CAF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+				 spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long DW9781CAF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+			   unsigned long a_u4Param);
+extern int DW9781CAF_Release(struct inode *a_pstInode, struct file *a_pstFile);
+extern int DW9781CAF_GetFileName(unsigned char *pFileName);
+
 #endif
