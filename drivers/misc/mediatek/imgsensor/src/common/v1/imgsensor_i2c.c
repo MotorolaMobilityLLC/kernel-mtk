80363c8a2b99df1eb65007fb4b36b1eb5681c353
zufengmei <zufengmei@longcheer.com>
2021-06-06 17:12:29 +0800

    coful: camera bringup for dvt
    camera bringup
    add dvt
    
    Change-Id: Idc4e9250ec82d289edd0dadab96ca48bed15b437
    Signed-off-by: zufengmei <zufengmei@longcheer.com>
    Reviewed-on: https://gerrit.mot.com/1971250
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1/imgsensor_i2c.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1/imgsensor_i2c.c
@@ -13,6 +13,7 @@ static const struct i2c_device_id gi2c_dev_id[] = {
 	{IMGSENSOR_I2C_DRV_NAME_0, 0},
 	{IMGSENSOR_I2C_DRV_NAME_1, 0},
 	{IMGSENSOR_I2C_DRV_NAME_2, 0},
+	{IMGSENSOR_I2C_DRV_NAME_3, 0},
 	{}
 };
 
@@ -27,6 +28,10 @@ static const struct of_device_id gof_device_id_1[] = {
 };
 static const struct of_device_id gof_device_id_2[] = {
 	{ .compatible = IMGSENSOR_I2C_OF_DRV_NAME_2, },
+	{}
+};
+static const struct of_device_id gof_device_id_3[] = {
+	{ .compatible = IMGSENSOR_I2C_OF_DRV_NAME_3, },
 	{}
 };
 #endif
@@ -49,6 +54,13 @@ static int
 imgsensor_i2c_probe_2(struct i2c_client *client, const struct i2c_device_id *id)
 {
 	gi2c.inst[IMGSENSOR_I2C_DEV_2].pi2c_client = client;
+	return 0;
+}
+
+static int
+imgsensor_i2c_probe_3(struct i2c_client *client, const struct i2c_device_id *id)
+{
+	gi2c.inst[IMGSENSOR_I2C_DEV_3].pi2c_client = client;
 	return 0;
 }
 
@@ -90,6 +102,18 @@ static struct i2c_driver gi2c_driver[IMGSENSOR_I2C_DEV_MAX_NUM] = {
 		.owner = THIS_MODULE,
 #ifdef CONFIG_OF
 		.of_match_table = gof_device_id_2,
+#endif
+		},
+		.id_table = gi2c_dev_id,
+	},
+	{
+		.probe = imgsensor_i2c_probe_3,
+		.remove = imgsensor_i2c_remove,
+		.driver = {
+		.name = IMGSENSOR_I2C_DRV_NAME_3,
+		.owner = THIS_MODULE,
+#ifdef CONFIG_OF
+		.of_match_table = gof_device_id_3,
 #endif
 		},
 		.id_table = gi2c_dev_id,
