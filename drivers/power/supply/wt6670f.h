611e9fc2c1d76714da1f13587102237bfdea2690
liujian24 <liujian24@lenovo.com>
2021-06-09 15:22:37 +0800

    Corfu:Bringup Charger on Corfu
    WHY:Bringup charger on Corfu
    WHAT:Add related drivers and charger code
    
    Change-Id: Ica9a1ca0e03dab7bad8c19a2cabf4da95d20dd80
    Reviewed-on: https://gerrit.mot.com/1974763
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Geoffrey Lu <lubm1@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/power/supply/wt6670f.h
@@ -0,0 +1,19 @@
+
+
+struct wt6670f {
+	struct device *dev;
+	struct i2c_client *client;
+
+	int i2c_scl_pin;
+	int i2c_sda_pin;
+	int reset_pin;
+
+	int count;
+
+	struct mutex i2c_rw_lock;
+};
+
+
+int wt6670f_isp_flow(struct wt6670f *chip);
+
+
