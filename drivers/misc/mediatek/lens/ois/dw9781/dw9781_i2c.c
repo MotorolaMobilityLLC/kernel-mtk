f93b71939c61d33c553530f8eead6cd33951879f
chenglong1 <chenglong1@lenovo.com>
2021-06-11 17:18:54 +0800

    Corfu: OIS factory test kernel implementation.
    Currently we've implemented Hall test and Gyro offset calibration in kernel,
    also firmware tests reserved for future tests.
    Due to Hall test and Gyro offset calibration will last too long time to finish
    in single capture request, so add kernel work thread to let the tests run in
    background.
    
    Change-Id: I20b73b3f1bbea5b2072d4716a9bd9f3eba3cba1c
    Signed-off-by: chenglong1 <chenglong1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1960967
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xu Ji <jixu@motorola.com>
    Reviewed-by: Darong Huang <huangdra@motorola.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Reviewed-by: Zhilong Wang <wangzl30@motorola.com>
    Reviewed-by: Haotian Zhang <zhanght21@lenovo.com>
    Submit-Approved: Jira Key

---
+++ drivers/misc/mediatek/lens/ois/dw9781/dw9781_i2c.c
@@ -0,0 +1,83 @@
+#include "dw9781_i2c.h"
+
+//#define DW9781_DEBUG
+#ifdef DW9781_DEBUG
+#define LOG_INF(format, args...) \
+	printk(" [%s] " format, __func__, ##args)
+#else
+#define LOG_INF(format, args...)
+#endif
+#define DW9781_I2C_SLAVE_ADDR 0xE4
+
+static struct i2c_client *dw9781_i2c_client = NULL;
+
+void dw9781_set_i2c_client(struct i2c_client *i2c_client)
+{
+	dw9781_i2c_client = i2c_client;
+}
+
+int write_reg_16bit_value_16bit(unsigned short regAddr, unsigned short regData)
+{
+	int i4RetValue = 0;
+	struct i2c_msg msgs;
+	char puSendCmd[4] = { (char)(regAddr>>8), (char)(regAddr&0xff),
+	                      (char)(regData >> 8),
+	                      (char)(regData&0xFF) };
+
+	LOG_INF("DW9781 I2C read:%04x, data:%04x", regAddr, regData);
+
+	if (!dw9781_i2c_client) {
+		printk("[%s] FATAL: DW9781 i2c client is NULL!!!",__func__);
+		return -1;
+	}
+
+	msgs.addr  = DW9781_I2C_SLAVE_ADDR >> 1;
+	msgs.flags = 0;
+	msgs.len   = 4;
+	msgs.buf   = puSendCmd;
+
+	i4RetValue = i2c_transfer(dw9781_i2c_client->adapter, &msgs, 1);
+	if (i4RetValue != 1) {
+		printk("I2C send failed!!\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+int read_reg_16bit_value_16bit(unsigned short regAddr, unsigned short *regData)
+{
+	int i4RetValue = 0;
+	unsigned short readTemp = 0;
+	struct i2c_msg msg[2];
+	char puReadCmd[2] = { (char)(regAddr>>8), (char)(regAddr&0xff)};
+
+	LOG_INF("DW9781 I2C read:%04x", regAddr);
+
+	if (!dw9781_i2c_client) {
+		printk("[%s] FATAL: DW9781 i2c client is NULL!!!",__func__);
+		return -1;
+	}
+	dw9781_i2c_client->addr = DW9781_I2C_SLAVE_ADDR >> 1;
+
+	msg[0].addr = dw9781_i2c_client->addr;
+	msg[0].flags = 0;
+	msg[0].len = 2;
+	msg[0].buf = puReadCmd;
+
+	msg[1].addr = dw9781_i2c_client->addr;
+	msg[1].flags = I2C_M_RD;
+	msg[1].len = 2;
+	msg[1].buf = (u8*)&readTemp;
+
+	i4RetValue = i2c_transfer(dw9781_i2c_client->adapter, msg, ARRAY_SIZE(msg));
+
+	if (i4RetValue != 2) {
+		printk("DW9781 I2C read failed!!\n");
+		return -1;
+	}
+
+	*regData = ((readTemp>>8) & 0xff) | ((readTemp<<8) & 0xff00);
+
+	return 0;
+}
