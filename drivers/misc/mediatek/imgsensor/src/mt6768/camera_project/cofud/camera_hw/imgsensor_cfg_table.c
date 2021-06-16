c1b8918c75b4490c8bdd3cafba04f89d8aab8627
yangdesheng <yangdesheng@longcheer.com>
2021-06-16 15:26:00 +0800

    cofud: camera bringup for cofud <3/4>
    camera bringup
    add camera sensor code
    add eeprom
    add flashlight
    
    Change-Id: I8443df8101d3009da7d5ce17d14e187b2948599e
    Signed-off-by: yangdesheng <yangdesheng@longcheer.com>
    Reviewed-on: https://gerrit.mot.com/1981033
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/misc/mediatek/imgsensor/src/mt6768/camera_project/cofud/camera_hw/imgsensor_cfg_table.c
@@ -0,0 +1,215 @@
+/*
+ * Copyright (C) 2017 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#include "kd_imgsensor.h"
+
+#include "regulator/regulator.h"
+#include "gpio/gpio.h"
+/*#include "mt6306/mt6306.h"*/
+#include "mclk/mclk.h"
+
+
+
+#include "imgsensor_cfg_table.h"
+
+enum IMGSENSOR_RETURN
+	(*hw_open[IMGSENSOR_HW_ID_MAX_NUM])(struct IMGSENSOR_HW_DEVICE **) = {
+	imgsensor_hw_regulator_open,
+	imgsensor_hw_gpio_open,
+	/*imgsensor_hw_mt6306_open,*/
+	imgsensor_hw_mclk_open
+};
+
+struct IMGSENSOR_HW_CFG imgsensor_custom_config[] = {
+	{
+		IMGSENSOR_SENSOR_IDX_MAIN,
+		IMGSENSOR_I2C_DEV_0,
+		{
+			{IMGSENSOR_HW_ID_MCLK, IMGSENSOR_HW_PIN_MCLK},
+			{IMGSENSOR_HW_ID_REGULATOR, IMGSENSOR_HW_PIN_DVDD},
+			{IMGSENSOR_HW_ID_REGULATOR, IMGSENSOR_HW_PIN_DOVDD},
+			{IMGSENSOR_HW_ID_REGULATOR, IMGSENSOR_HW_PIN_AVDD},
+			{IMGSENSOR_HW_ID_GPIO, IMGSENSOR_HW_PIN_AFVDD},
+			{IMGSENSOR_HW_ID_REGULATOR, IMGSENSOR_HW_PIN_VDDA},
+			{IMGSENSOR_HW_ID_GPIO, IMGSENSOR_HW_PIN_RST},
+			{IMGSENSOR_HW_ID_NONE, IMGSENSOR_HW_PIN_NONE},
+		},
+	},
+	{
+		IMGSENSOR_SENSOR_IDX_SUB,
+		IMGSENSOR_I2C_DEV_1,
+		{
+			{IMGSENSOR_HW_ID_MCLK, IMGSENSOR_HW_PIN_MCLK},
+			{IMGSENSOR_HW_ID_REGULATOR, IMGSENSOR_HW_PIN_AVDD},
+			{IMGSENSOR_HW_ID_REGULATOR, IMGSENSOR_HW_PIN_DOVDD},
+			{IMGSENSOR_HW_ID_GPIO, IMGSENSOR_HW_PIN_AFVDD},
+			{IMGSENSOR_HW_ID_GPIO, IMGSENSOR_HW_PIN_DVDD},
+#ifdef MIPI_SWITCH
+			{IMGSENSOR_HW_ID_GPIO, IMGSENSOR_HW_PIN_MIPI_SWITCH_SEL},
+#endif
+			{IMGSENSOR_HW_ID_GPIO, IMGSENSOR_HW_PIN_RST},
+			{IMGSENSOR_HW_ID_NONE, IMGSENSOR_HW_PIN_NONE},
+		},
+	},
+	{
+		IMGSENSOR_SENSOR_IDX_MAIN2,
+		IMGSENSOR_I2C_DEV_2,
+		{
+			{IMGSENSOR_HW_ID_MCLK, IMGSENSOR_HW_PIN_MCLK},
+			{IMGSENSOR_HW_ID_REGULATOR, IMGSENSOR_HW_PIN_AVDD},
+			{IMGSENSOR_HW_ID_REGULATOR, IMGSENSOR_HW_PIN_DOVDD},
+			{IMGSENSOR_HW_ID_GPIO, IMGSENSOR_HW_PIN_DVDD},
+			{IMGSENSOR_HW_ID_GPIO, IMGSENSOR_HW_PIN_RST},
+			{IMGSENSOR_HW_ID_NONE, IMGSENSOR_HW_PIN_NONE},
+		},
+	},
+	{
+		IMGSENSOR_SENSOR_IDX_SUB2,
+		IMGSENSOR_I2C_DEV_0,
+		{
+			{IMGSENSOR_HW_ID_MCLK, IMGSENSOR_HW_PIN_MCLK},
+			{IMGSENSOR_HW_ID_REGULATOR, IMGSENSOR_HW_PIN_AVDD},
+			{IMGSENSOR_HW_ID_REGULATOR, IMGSENSOR_HW_PIN_DOVDD},
+			{IMGSENSOR_HW_ID_GPIO, IMGSENSOR_HW_PIN_DVDD},
+			{IMGSENSOR_HW_ID_GPIO, IMGSENSOR_HW_PIN_RST},
+#ifdef MIPI_SWITCH
+			{IMGSENSOR_HW_ID_GPIO, IMGSENSOR_HW_PIN_MIPI_SWITCH_SEL},
+#endif
+			{IMGSENSOR_HW_ID_NONE, IMGSENSOR_HW_PIN_NONE},
+		},
+	},
+	{
+		IMGSENSOR_SENSOR_IDX_MAIN3,
+		IMGSENSOR_I2C_DEV_2,
+		{
+			{IMGSENSOR_HW_ID_MCLK, IMGSENSOR_HW_PIN_MCLK},
+			{IMGSENSOR_HW_ID_REGULATOR, IMGSENSOR_HW_PIN_AVDD},
+			{IMGSENSOR_HW_ID_REGULATOR, IMGSENSOR_HW_PIN_DOVDD},
+			{IMGSENSOR_HW_ID_GPIO, IMGSENSOR_HW_PIN_RST},
+			{IMGSENSOR_HW_ID_NONE, IMGSENSOR_HW_PIN_NONE},
+		},
+	},
+
+	{IMGSENSOR_SENSOR_IDX_NONE}
+};
+
+struct IMGSENSOR_HW_POWER_SEQ platform_power_sequence[] = {
+#ifdef MIPI_SWITCH
+	{
+		PLATFORM_POWER_SEQ_NAME,
+		{
+			{
+				IMGSENSOR_HW_PIN_MIPI_SWITCH_EN,
+				IMGSENSOR_HW_PIN_STATE_LEVEL_0,
+				0,
+				IMGSENSOR_HW_PIN_STATE_LEVEL_HIGH,
+				0
+			},
+			{
+				IMGSENSOR_HW_PIN_MIPI_SWITCH_SEL,
+				IMGSENSOR_HW_PIN_STATE_LEVEL_0,
+				0,
+				IMGSENSOR_HW_PIN_STATE_LEVEL_0,
+				0
+			},
+		},
+		IMGSENSOR_SENSOR_IDX_SUB,
+	},
+	{
+		PLATFORM_POWER_SEQ_NAME,
+		{
+			{
+				IMGSENSOR_HW_PIN_MIPI_SWITCH_EN,
+				IMGSENSOR_HW_PIN_STATE_LEVEL_0,
+				0,
+				IMGSENSOR_HW_PIN_STATE_LEVEL_HIGH,
+				0
+			},
+			{
+				IMGSENSOR_HW_PIN_MIPI_SWITCH_SEL,
+				IMGSENSOR_HW_PIN_STATE_LEVEL_HIGH,
+				0,
+				IMGSENSOR_HW_PIN_STATE_LEVEL_0,
+				0
+			},
+		},
+		IMGSENSOR_SENSOR_IDX_SUB2,
+	},
+#endif
+
+	{NULL}
+};
+
+/* Legacy design */
+struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
+/* Add Corfu start */
+#if defined(MOT_COFUD_S5KJN1_MIPI_RAW)
+        {
+                SENSOR_DRVNAME_MOT_COFUD_S5KJN1_MIPI_RAW,
+                {
+			{SensorMCLK, Vol_High, 1},
+			{RST, Vol_Low, 1},
+			{DVDD, Vol_1100, 1},
+			{AVDD, Vol_2800, 1},
+			{DOVDD, Vol_1800, 1},
+			{VDDA, Vol_2800, 1},
+			{AFVDD, Vol_High, 1},
+			{RST, Vol_High, 1},
+                },
+        },
+#endif
+#if defined(MOT_CORFU_HI1336_OFILM)
+        {
+                SENSOR_DRVNAME_MOT_CORFU_HI1336_OFILM,
+                {
+                        {RST, Vol_Low, 1},
+                        {SensorMCLK, Vol_High, 1},
+                        {AVDD, Vol_2800, 1},
+                        {DOVDD, Vol_1800, 1},
+                        {AFVDD, Vol_High, 1},
+                        {DVDD, Vol_1100, 1},
+                        {RST, Vol_High, 5}
+                },
+        },
+#endif
+#if defined(MOT_COFUD_S5K4H7_MIPI_RAW)
+        {
+                SENSOR_DRVNAME_MOT_COFUD_S5K4H7_MIPI_RAW,
+                {
+                        {RST, Vol_Low, 1},
+                        {SensorMCLK, Vol_High, 1},
+                        {DVDD, Vol_1200, 1},
+                        {AVDD, Vol_2800, 1},
+                        {DOVDD, Vol_1800, 1},
+                        {RST, Vol_High, 5}
+                },
+        },
+#endif
+#if defined(MOT_COFUD_GC02M1_MIPI_RAW)
+        {
+                SENSOR_DRVNAME_MOT_COFUD_GC02M1_MIPI_RAW,
+                {
+                        {RST, Vol_Low, 1},
+                        {SensorMCLK, Vol_High, 1},
+                        {DVDD, Vol_High, 1},
+                        {DOVDD, Vol_1800, 1},
+                        {AVDD, Vol_2800, 1},
+                        {RST, Vol_High, 5}
+                },
+        },
+#endif
+/* Add Corfu end */
+	/* add new sensor before this line */
+	{NULL,},
+};
+
