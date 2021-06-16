8ffc97df7fc710cd5082cd1249c8750e138bf201
zhanglinfeng <zhanglinfeng@wingtech.com>
2021-06-16 15:49:26 +0800

    Tonga: update dual camera setting
    update ov02b1b settings and s5kjn1sq
    update type of AVDD
    
    Change-Id: Ia745ae7f2b9b7c6fd3c236c6bbbb8df22f41d585
    Reviewed-on: https://gerrit.mot.com/1981058
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1/mot_tonga_ov02b1b_mipi_raw/mot_tonga_ov02b1bmipiraw_Sensor.h
+++ drivers/misc/mediatek/imgsensor/src/common/v1/mot_tonga_ov02b1b_mipi_raw/mot_tonga_ov02b1bmipiraw_Sensor.h
@@ -26,9 +26,6 @@ enum{
 	IMGSENSOR_MODE_PREVIEW,
 	IMGSENSOR_MODE_CAPTURE,
 	IMGSENSOR_MODE_VIDEO,
-	IMGSENSOR_MODE_HIGH_SPEED_VIDEO,
-	IMGSENSOR_MODE_SLIM_VIDEO,
-	IMGSENSOR_MODE_CUSTOM1,
 };
 
 /* 表示几种（不同工作模式状态下）的sensor参数信息 */
@@ -95,15 +92,8 @@ struct imgsensor_info_struct {
 	/* capture scenario relative information */
 	struct imgsensor_mode_struct cap;
 
-	struct imgsensor_mode_struct cap1;
 	struct imgsensor_mode_struct normal_video;
 
-	/* high speed video scenario relative information */
-	struct imgsensor_mode_struct hs_video;
-
-	/* slim video for VT scenario relative information */
-	struct imgsensor_mode_struct slim_video;
-	struct imgsensor_mode_struct custom1; //custom1 scenario information
 	kal_uint8 ae_shut_delay_frame;	/* shutter delay frame for AE cycle */
 
 	/* sensor gain delay frame for AE cycle */
@@ -117,10 +107,6 @@ struct imgsensor_info_struct {
 	kal_uint8 pre_delay_frame;	/* enter preview delay frame num */
 	kal_uint8 video_delay_frame;	/* enter video delay frame num */
 
-	/* enter high speed video  delay frame num */
-	kal_uint8 hs_video_delay_frame;
-	kal_uint8 slim_video_delay_frame; /* enter slim video delay frame num */
-	kal_uint8  custom1_delay_frame;     //enter custom1 delay frame num
 	kal_uint8  frame_time_delay_frame;
 	kal_uint8 margin;	/* sensor framelength & shutter margin */
 	kal_uint32 min_shutter;	/* min shutter */
