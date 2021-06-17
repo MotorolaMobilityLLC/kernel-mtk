005c7d6888d45aedc1e1ab91bc72ed9be8d80819
lugang <lugang@longcheer.com>
2021-06-17 15:32:51 +0800

    corfu: update OIS DW9781 code
    update OIS
    DW9781C code
    
    Change-Id: I02eb77f7b816aa70ef2c83cbe814c47fd73c649b
    Signed-off-by: lugang <lugang@longcheer.com>
    Reviewed-on: https://gerrit.mot.com/1981445
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lens/mtk/ois/dw9781/dw9781_i2c.h
+++ drivers/misc/mediatek/lens/mtk/ois/dw9781/dw9781_i2c.h
@@ -7,4 +7,7 @@
 void dw9781_set_i2c_client(struct i2c_client *i2c_client);
 int write_reg_16bit_value_16bit(unsigned short regAddr, unsigned short regData);
 int read_reg_16bit_value_16bit(unsigned short regAddr, unsigned short *regData);
+void i2c_block_write_reg(unsigned short addr,unsigned short *fwContentPtr,int size);
+void i2c_block_read_reg(unsigned short addr,unsigned short *temp,int size);
+#define DATPKT_SIZE 256
 #endif
