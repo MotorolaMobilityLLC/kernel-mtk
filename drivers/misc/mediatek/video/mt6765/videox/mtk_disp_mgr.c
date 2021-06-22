72c4be7f1c643c4a13acb2ece1cc54772a7d5339
hujing3 <hujing3@lenovo.com>
2021-06-22 18:11:56 +0800

    disp/mt6765: use copy_to_user for ioctl to get lcm index
    display/mt6765:
    support color mode for multiple panels, need get lcm index.
    Use copy_to_user for ioctl, otherwise caller cannot get correct value.
    porting from (CR): https://gerrit.mot.com/1384025
    
    Change-Id: I0a177118d0b278cc3a650f5773f6000abb054657
    Signed-off-by: hujing3 <hujing3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1987899
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/video/mt6765/videox/mtk_disp_mgr.c
+++ drivers/misc/mediatek/video/mt6765/videox/mtk_disp_mgr.c
@@ -1563,6 +1563,7 @@ const char *_session_ioctl_spy(unsigned int cmd)
 long mtk_disp_mgr_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	int ret = -1;
+	void __user *argp = (void __user *)arg;
 	static DEFINE_RATELIMIT_STATE(ioctl_ratelimit, 1 * HZ, 10);
 
 	switch (cmd) {
@@ -1620,7 +1621,14 @@ long mtk_disp_mgr_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 		}
 	case DISP_IOCTL_GET_LCMINDEX:
 		{
-			return primary_display_get_lcm_index();
+			int lcm_index;
+			lcm_index = primary_display_get_lcm_index();
+			if (copy_to_user(argp, &lcm_index, sizeof(lcm_index))) {
+				DISPERR("mtk_disp_mgr_ioctl: handle get_lcm_index, copy_to_user_failed!\n");
+				return -EFAULT;
+			}
+			DISPDBG("%s: get lcm_index=%d\n", __func__, lcm_index);
+			return 0;
 		}
 	case DISP_IOCTL_QUERY_VALID_LAYER:
 		{
