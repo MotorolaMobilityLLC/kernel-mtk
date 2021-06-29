c0ce470bee81e6be949a2c51d0ce0b34e60d7c4a
tanhua1 <tanhua1@lenovo.com>
2021-06-29 17:16:21 +0800

    backlight: modify hbm backlight logic for mtk platform
    modify hbm backlight logic for mtk platform
    1. Set max brightness when hbm enable
    2. Ignore the brightness setting while hbm is enable
    3. Restore the brightness before hbm was enable while hbm disable
    
    Change-Id: I88e200ac74f9ca4faa0dce4c61ed595259f32c0d
    Signed-off-by: tanhua1 <tanhua1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1995651
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jing Hu <hujing3@lenovo.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/video/mt6785/videox/mtkfb.c
+++ drivers/misc/mediatek/video/mt6785/videox/mtkfb.c
@@ -94,6 +94,8 @@
 #include "mmpath.h"
 #include "mtkfb_params.h"
 
+#include "mtk_leds_drv.h"
+
 /* static variable */
 static u32 MTK_FB_XRES;
 static u32 MTK_FB_YRES;
@@ -239,6 +241,8 @@ int  mtkfb_set_cabc(int cabc_mode)
 int mtkfb_set_hbm(int hbm_state)
 {
     int cmd_type = PARAM_HBM;
+
+    hbm_brightness_set(hbm_state);
 
     if(primary_display_setlcm_cmd(&cmd_type, NULL, &hbm_state)) {
         pr_err("%s: set HBM %d failed.\n", __func__, hbm_state);
