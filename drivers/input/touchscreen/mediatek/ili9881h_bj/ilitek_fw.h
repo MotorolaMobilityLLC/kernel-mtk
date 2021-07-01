14c17b3271ccbfa016acbfae2f033ed3366fbd78
zhangshaohu <Shaohu.zhang@chino-e.com>
2021-07-01 14:28:18 +0800

    EKBLACKJQ-2917: resolve double-click wakeup screen ili9881h in kernel
    Description:
     1. add the new firmware.
     2. double-click interval change from 500ms to 150ms.
    
    St-result: PASS
    
    Change-Id: If2fc09c2518ff05ebb5e67cf82273af10a1ed98a
    Signed-off-by: zhangshaohu <Shaohu.zhang@chino-e.com>

--- drivers/input/touchscreen/mediatek/ili9881h_bj/ilitek_fw.h
+++ drivers/input/touchscreen/mediatek/ili9881h_bj/ilitek_fw.h
@@ -1,4 +1,4 @@
 static unsigned char CTPM_FW[] = {
-	#include "firmware/HLT_MDT_6517_LongV_M6206_0xA1_0x45_0x03_20200219.ili"
+    #include "firmware/HLT_MDT_6517_LongV_M6206_0xA1_0x45_0x04_0xA1_0x45_0x01_20210628.ili"
 };
 
