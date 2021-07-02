88ca7dc52d58b561e5b680f198118df8476d4ee1
liujg6 <liujg6@lenovo.com>
2021-07-02 15:51:56 +0800

    mt6765: update log level of charger and pmic
    Kernel is printing too much logs.
    Log level of charger and pmic is improper and we should disable
    pmic log compared with other platform, level down charger's log
    level refer to recent MTK's changes to battery log level..
    
    Change-Id: Id696b7e279827d77693708b8bbeb76d7ff8de519
    Reviewed-on: https://gerrit.mot.com/2000531
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jianguo Liu <liujg6@motorola.com>
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/thermal/mt6765/src/mtk_pmic_efuse.c
+++ drivers/misc/mediatek/thermal/mt6765/src/mtk_pmic_efuse.c
@@ -33,7 +33,7 @@
  *Local variable definition
  *=============================================================
  */
-int mtktspmic_debug_log = 1;
+int mtktspmic_debug_log;
 /* Cali */
 static __s32 g_o_vts;
 static __s32 g_o_vts_2;
