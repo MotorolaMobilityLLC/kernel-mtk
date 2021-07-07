9fc5d33cb1362329c7c397c8f2897bf07020e618
houdz1 <houdz1@lenovo.com>
2021-07-07 09:36:21 +0800

    kernel/lcm: tonga: enable esd check for panel
    Read the register of DDIC (0x0A),And check the value.
    If is it not x9C, then make esd recovery.
    
    Change-Id: Ia44225f1d4b7f0537f9fbbb4a49a51809f5b8a9a
    Signed-off-by: houdz1 <houdz1@mt.com>
    Reviewed-on: https://gerrit.mot.com/2004132
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lcm/dsi_panel_mot_mipi_vid_tm_ili9883a_652_hdp/dsi_panel_mot_mipi_vid_tm_ili9883a_652_hdp.c
+++ drivers/misc/mediatek/lcm/dsi_panel_mot_mipi_vid_tm_ili9883a_652_hdp/dsi_panel_mot_mipi_vid_tm_ili9883a_652_hdp.c
@@ -312,8 +312,8 @@ static void lcm_get_params(struct LCM_PARAMS *params)
 #endif
 
 	params->dsi.clk_lp_per_line_enable = 0;
-	params->dsi.esd_check_enable = 0;
-	params->dsi.customization_esd_check_enable = 0;
+	params->dsi.esd_check_enable = 1;
+	params->dsi.customization_esd_check_enable = 1;
 	params->dsi.lcm_esd_check_table[0].cmd = 0x0A;
 	params->dsi.lcm_esd_check_table[0].count = 1;
 	params->dsi.lcm_esd_check_table[0].para_list[0] = 0x9C;
