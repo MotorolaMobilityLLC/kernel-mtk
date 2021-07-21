88c9f87ecb43de869c315a688b0e25cbb4c73d51
liujian24 <liujian24@lenovo.com>
2021-07-21 21:05:54 +0800

    Coful:charging doesn't work by Apple adapter
    WHY:BQ25601 force charger second detect more times
    WHAT:Change second detect to once
    
    Change-Id: I79da8973d9d1eaddf9dcc11dcc98a13cdae4f713
    Reviewed-on: https://gerrit.mot.com/2020337
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: liujian <liujian24@lenovo.com>
    Reviewed-by: Shengchun Li <lisc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/mediatek/charger/bq25601.c
+++ drivers/power/supply/mediatek/charger/bq25601.c
@@ -1400,6 +1400,7 @@ static int bq25601_enable_chg_type_det(struct charger_device *chg_dev, bool en)
 #ifdef CONFIG_MOTO_CHG_BQ25601_SUPPORT
         int ret = 0;
         unsigned char val;
+	bool force_set = false;
 #endif
         int count = 0;
         struct bq25601_info *chip = dev_get_drvdata(&chg_dev->dev);
@@ -1460,7 +1461,6 @@ static int bq25601_enable_chg_type_det(struct charger_device *chg_dev, bool en)
         }
 #else
 //        msleep(500);
-        msleep(200);
 
         for(count = 0;count<8;count++){
                 ret = bq25601_read_byte(8, &val);
@@ -1469,7 +1469,10 @@ static int bq25601_enable_chg_type_det(struct charger_device *chg_dev, bool en)
                         break;
                 }else{
                         /*force second recognition*/
-                        bq25601_set_force_dpdm(0x1);
+			if(!force_set){
+                              bq25601_set_force_dpdm(0x1);
+			      force_set = true;
+			}
                         msleep(100);
                         pr_info("%s,charger is usb will test again\n",__func__);
                 }
