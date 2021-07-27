2cead8e9f53a576abe2e616a81e185be611fbfb0
longch69 <longch69@motorola.com>
2021-07-27 22:37:21 +0800

    Milan: s5k4h7 AF bringup [4/4]
    [Detail]
    s5k4h7 AF bringup
    
    Change-Id: I60227eef7abf792637eb07346179d9e2fe5515c2
    Signed-off-by: shendeguo <shendeguo@longcheer.com>
    Reviewed-on: https://gerrit.mot.com/2025632
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <wangzl37@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: <longch69@motorola.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lens/main3/inc/lens_list.h
+++ drivers/misc/mediatek/lens/main3/inc/lens_list.h
@@ -58,6 +58,20 @@ extern int MOT_DW9714VAF_PowerDown(struct i2c_client *pstAF_I2Cclient,
 				int *pAF_Opened);
 extern int MOT_DW9714VAF_GetFileName(unsigned char *pFileName);
 
+#define DW9714AF_SetI2Cclient DW9714AF_SetI2Cclient_Main3
+#define DW9714AF_Ioctl DW9714AF_Ioctl_Main3
+#define DW9714AF_Release DW9714AF_Release_Main3
+#define DW9714AF_PowerDown DW9714AF_PowerDown_Main3
+#define DW9714AF_GetFileName DW9714AF_GetFileName_Main3
+extern int DW9714AF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+				 spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long DW9714AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+			   unsigned long a_u4Param);
+extern int DW9714AF_Release(struct inode *a_pstInode, struct file *a_pstFile);
+extern int DW9714AF_PowerDown(struct i2c_client *pstAF_I2Cclient,
+				int *pAF_Opened);
+extern int DW9714AF_GetFileName(unsigned char *pFileName);
+
 extern void mot_af_poweron(int state);
 
 extern void AFRegulatorCtrl(int Stage);
