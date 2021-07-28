42c6aafdb8013cea961eb3c3693cf0362ac0623e
Chun-Jie Chen <chun-jie.chen@mediatek.com>
2021-07-28 12:59:42 +0800

    [ALPS05769179] soc: mediatek: fix mux clock coverity
    [Null pointer dereferences]
    fix cid:
    10798903
    
    MTK-Commit-Id: 0e69de415edbb9dde20d334e6c06dbaff85c2069
    
    Change-Id: I34d032c58a9123aeb25b8a9e3584ce50b3a1843c
    Signed-off-by: Chun-Jie Chen <chun-jie.chen@mediatek.com>
    CR-Id: ALPS05769179
    Feature: [Module]Clock Management

--- drivers/clk/mediatek/clk-mux.c
+++ drivers/clk/mediatek/clk-mux.c
@@ -204,7 +204,7 @@ static int mtk_mux_set_parent_setclr(struct clk_hw *hw, u8 index)
 		 * Workaround for mm dvfs. Poll mm rdma reg before
 		 * clkmux switching.
 		 */
-		if (!strcmp(__clk_get_name(hw->clk), "mm_sel"))
+		if (hw && hw->clk && !strcmp(__clk_get_name(hw->clk), "mm_sel"))
 			mm_polling(hw);
 #endif
 
