c9395dbac1dda0dbcdfab13226034f62460ca0b4
gang.feng <gang.feng@mediatek.com>
2021-08-04 21:13:45 +0800

    [ALPS05952018] Audio: refine the suspend state control
    push suspend msg flow has timing problem when another ipi msg send before
    state change to suspending, so move the suspending after msg state check.
    
    MTK-Commit-Id: a7928beffd0c6854de375dfa769ef8a90cd4d31b
    
    Change-Id: Ia80be262c3c0e14d955fb3031df04cd6c7388087
    CR-Id: ALPS05952018
    Feature: [Module]Audio OpenDSP
    Signed-off-by: gang.feng <gang.feng@mediatek.com>

--- drivers/misc/mediatek/adsp/mt6885/adsp_platform_driver.c
+++ drivers/misc/mediatek/adsp/mt6885/adsp_platform_driver.c
@@ -237,7 +237,6 @@ int adsp_core0_suspend(void)
 			ret = -EPIPE;
 			goto ERROR;
 		}
-		set_adsp_state(pdata, ADSP_SUSPENDING);
 
 		/* wait core suspend ack timeout 2s */
 		ret = wait_for_completion_timeout(&pdata->done, 2 * HZ);
@@ -320,7 +319,6 @@ int adsp_core1_suspend(void)
 			ret = -EPIPE;
 			goto ERROR;
 		}
-		set_adsp_state(pdata, ADSP_SUSPENDING);
 
 		/* wait core suspend ack timeout 2s */
 		ret = wait_for_completion_timeout(&pdata->done, 2 * HZ);
