71c628cd53d9d62f4d54130b206cdf508c2e97ff
lijun <lijun@longcheer.com>
2021-08-04 11:09:11 +0800

    Corfu/CorfuLite: modify hi1336 i2c speed
    [detail]
    modify hi1336 i2c speed
    
    Change-Id: I0835cfd768b219dcb7c2af2fa9ae45c714d2fc17
    Signed-off-by: lijun <lijun@longcheer.com>
    Reviewed-on: https://gerrit.mot.com/2032222
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Darong Huang <huangdra@motorola.com>
    Reviewed-by: <wangzl37@motorola.com>
    Reviewed-by: Shenhuai Huang <huangsh4@motorola.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1/mot_corfu_hi1336_ofilm/mot_corfu_hi1336_ofilm.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1/mot_corfu_hi1336_ofilm/mot_corfu_hi1336_ofilm.c
@@ -158,7 +158,7 @@ static struct imgsensor_info_struct imgsensor_info = {
 	.mclk = 24,
 	.mipi_lane_num = SENSOR_MIPI_4_LANE,
 	.i2c_addr_table = {0x46,0xff},
-	.i2c_speed = 400,
+	.i2c_speed = 1000,
 };
 
 static struct imgsensor_struct imgsensor = {
