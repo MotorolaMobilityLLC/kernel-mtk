91071e64c580c243120d8720f7aa5e1db7db285c
longch59 <longch59@motorola.com>
2021-08-09 03:55:23 -0500

    Revert "(CR) kernel/lcm: change to bypass PQ by MTK"
    - no need bypassPQ
    - Using bypassPQ will cause the functions that require PQ to be unavailable, so remove it first
    
    This reverts commit cb1f1317f7ad0483e7888f274b4ee28f09d42dd2.
    
    Change-Id: I42b59db82ac7db90501fe47506447b764ffe38ae
    Reviewed-on: https://gerrit.mot.com/2036612
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/video/mt6768/videox/primary_display.c
+++ drivers/misc/mediatek/video/mt6768/videox/primary_display.c
@@ -2328,11 +2328,8 @@ static int _DC_switch_to_DL_fast(int block)
 	_cmdq_insert_wait_frame_done_token_mira(pgc->cmdq_handle_config);
 
 	old_scenario = dpmgr_get_scenario(pgc->dpmgr_handle);
-#ifdef CONFIG_MTK_MT6382_BDG_BUF7
-	new_scenario = DDP_SCENARIO_PRIMARY_BYPASS_PQ_DISP;
-#else
 	new_scenario = DDP_SCENARIO_PRIMARY_DISP;
-#endif
+
 	dpmgr_modify_path_power_on_new_modules(pgc->dpmgr_handle,
 		new_scenario, 0);
 
@@ -2430,11 +2427,7 @@ static int _DC_switch_to_DL_sw_only(void)
 	_cmdq_insert_wait_frame_done_token_mira(pgc->cmdq_handle_config);
 
 	old_scenario = dpmgr_get_scenario(pgc->dpmgr_handle);
-#ifdef CONFIG_MTK_MT6382_BDG_BUF7
-	new_scenario = DDP_SCENARIO_PRIMARY_BYPASS_PQ_DISP;
-#else
 	new_scenario = DDP_SCENARIO_PRIMARY_DISP;
-#endif
 	dpmgr_modify_path_power_on_new_modules(pgc->dpmgr_handle,
 		new_scenario, 1);
 	dpmgr_modify_path(pgc->dpmgr_handle, new_scenario,
@@ -2561,11 +2554,7 @@ static int rdma_mode_switch_to_DL(struct cmdqRecStruct *handle, int block)
 	}
 
 	old_scenario = dpmgr_get_scenario(pgc->dpmgr_handle);
-#ifdef CONFIG_MTK_MT6382_BDG_BUF7
-	new_scenario = DDP_SCENARIO_PRIMARY_BYPASS_PQ_DISP;
-#else
 	new_scenario = DDP_SCENARIO_PRIMARY_DISP;
-#endif
 	dpmgr_modify_path_power_on_new_modules(pgc->dpmgr_handle,
 		new_scenario, 0);
 	dpmgr_modify_path(pgc->dpmgr_handle, new_scenario, handle,
@@ -2757,14 +2746,9 @@ static int _build_path_direct_link(void)
 
 	DISPFUNC();
 	pgc->mode = DIRECT_LINK_MODE;
-#ifdef CONFIG_MTK_MT6382_BDG_BUF7
-	pgc->dpmgr_handle = dpmgr_create_path(DDP_SCENARIO_PRIMARY_BYPASS_PQ_DISP,
-		pgc->cmdq_handle_config);
-#else
+
 	pgc->dpmgr_handle = dpmgr_create_path(DDP_SCENARIO_PRIMARY_DISP,
 		pgc->cmdq_handle_config);
-#endif
-
 	if (pgc->dpmgr_handle) {
 		DISPDBG("dpmgr create path SUCCESS(%p)\n", pgc->dpmgr_handle);
 	} else {
@@ -3805,13 +3789,7 @@ static int update_primary_intferface_module(void)
 	enum DISP_MODULE_ENUM interface_module;
 
 	interface_module = _get_dst_module_by_lcm(pgc->plcm);
-
-#ifdef CONFIG_MTK_MT6382_BDG_BUF7
-	ddp_set_dst_module(DDP_SCENARIO_PRIMARY_BYPASS_PQ_DISP, interface_module);
-#else
 	ddp_set_dst_module(DDP_SCENARIO_PRIMARY_DISP, interface_module);
-#endif
-
 	ddp_set_dst_module(DDP_SCENARIO_PRIMARY_RDMA0_COLOR0_DISP,
 		interface_module);
 	ddp_set_dst_module(DDP_SCENARIO_PRIMARY_RDMA0_DISP, interface_module);
