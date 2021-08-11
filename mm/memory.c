9b3d1693c1d6eec8e3b113fe26b73da58a1129db
liujg6 <liujg6@lenovo.com>
2021-08-11 13:05:48 +0800

    Revert "mm: fix the race between swapin_readahead and SWP_SYNCHRONOUS_IO path"
    This reverts commit feadaa23b87f7317bbd7a4d3263e582aab2983d4.
    
    Revert above patch and propogate memory.c changes from
    (CR)(gerrit.mot.com/#/c/1983989/3/mm/memory.c)
    
    Change-Id: I42301f7bce15833a05253517a5dd23911bf60ab0
    Reviewed-on: https://gerrit.mot.com/2038591
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiaojun Ji <jixj@motorola.com>
    Reviewed-by: Zhizhong Ge <gezz1@motorola.com>
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Submit-Approved: Jira Key

--- mm/memory.c
+++ mm/memory.c
@@ -3058,8 +3058,6 @@ int do_swap_page(struct vm_fault *vmf)
 	struct mem_cgroup *memcg;
 	struct vma_swap_readahead swap_ra;
 	swp_entry_t entry;
-	struct swap_info_struct *si;
-	bool skip_swapcache = false;
 	pte_t pte;
 	int locked;
 	int exclusive = 0;
@@ -3109,24 +3107,6 @@ int do_swap_page(struct vm_fault *vmf)
 	}
 	delayacct_set_flag(DELAYACCT_PF_SWAPIN);
 
-	/*
-	 * lookup_swap_cache below can fail and before the SWP_SYNCHRONOUS_IO
-	 * check is made, another process can populate the swapcache, delete
-	 * the swap entry and decrement the swap count. So decide on taking
-	 * the SWP_SYNCHRONOUS_IO path before the lookup. In the event of the
-	 * race described, the victim process will find a swap_count > 1
-	 * and can then take the readahead path instead of SWP_SYNCHRONOUS_IO.
-	 */
-	si = swp_swap_info(entry);
-	/* Moto huangzq2: check sync_io on each page if we enabled Zram wb.
-	 * Zram writeback will remove SWP_SYNCHRONOUS_IO flag as it has disk
-	 * IO operation on writeback page during swap in.
-	 */
-	if (si->flags & SWP_SYNCHRONOUS_IO && __swap_count(si, entry) == 1)
-		skip_swapcache = true;
-	else if (__swap_count(si, entry) == 1 && swap_slot_has_sync_io(entry))
-		skip_swapcache = true;
-
 	if (!page) {
 		page = lookup_swap_cache(entry, vma_readahead ? vma : NULL,
 					 vmf->address);
@@ -3134,7 +3114,20 @@ int do_swap_page(struct vm_fault *vmf)
 	}
 
 	if (!page) {
+		struct swap_info_struct *si = swp_swap_info(entry);
+		bool skip_swapcache = false;
+
+		/* Moto huangzq2: check sync_io on each page if we enabled Zram wb.
+		 * Zram writeback will remove SWP_SYNCHRONOUS_IO flag as it has disk
+		 * IO operation on writeback page during swap in.
+		 */
+		if (si->flags & SWP_SYNCHRONOUS_IO && __swap_count(si, entry) == 1)
+			skip_swapcache = true;
+		else if (__swap_count(si, entry) == 1 && swap_slot_has_sync_io(entry))
+			skip_swapcache = true;
+
 		if (skip_swapcache) {
+			/* skip swapcache */
 			page = alloc_page_vma(GFP_HIGHUSER_MOVABLE, vma, vmf->address);
 			if (page) {
 				__SetPageLocked(page);
