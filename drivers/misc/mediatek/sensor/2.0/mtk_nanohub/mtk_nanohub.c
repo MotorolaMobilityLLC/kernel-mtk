a9f89c4355fb7665490fbe65bba0a40360912206
Jiacheng Ou <oujc1@motorola.com>
2021-08-12 14:05:05 +0800

    mt6833 driver:pre-submit compatible params for sensor
    Austin:
    compatible stk3a5x and tmd2755 params
    
    Signed-off-by: Jiacheng Ou <oujc1@motorola.com>
    Change-Id: I4903d8233bb246e51bc4705c71cf6dd8811948a7
    Reviewed-on: https://gerrit.mot.com/2039735
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Lijun Chen <chenlj2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/sensor/2.0/mtk_nanohub/mtk_nanohub.c
+++ drivers/misc/mediatek/sensor/2.0/mtk_nanohub/mtk_nanohub.c
@@ -2775,12 +2775,22 @@ static ssize_t algo_params_store(struct device_driver *ddri,
 	pr_err("sensor_cfg_to_hub  als_custom %d ps_custom %d\n",sizeof(struct als_custom),sizeof(struct ps_custom));
 
 	err = mtk_nanohub_cfg_to_hub(ID_LIGHT, (uint8_t *)&motparams->alscustom, sizeof(struct als_custom));
-	pr_err("sensor_cfg_to_hub light type %d scale %d\n",motparams->alscustom.als_cali.alscfg, (uint32_t)(motparams->alscustom.als_cali.light_scale*1000));
 	if (err < 0)
-		pr_err("sensor_cfg_to_hub LTV fail\n");
+		pr_err("sensor_cfg_to_hub light fail\n");
 
 	err = mtk_nanohub_cfg_to_hub(ID_PROXIMITY, (uint8_t *)&motparams->pscustom, sizeof(struct ps_custom));
-	pr_err("sensor_cfg_to_hub proximity type %d cover %d\n",motparams->pscustom.ps_cali.pscfg, motparams->pscustom.ps_cali.ps_cover);
+#endif
+#ifdef CONFIG_MOTO_ALSPS_NVCFG
+	if(14 == motparams->als_nvcfg.alscfg) {
+		err = mtk_nanohub_cfg_to_hub(ID_LIGHT, (uint8_t *)&motparams->als_nvcfg, sizeof(struct mot_als_nvcfg));
+		if (err < 0)
+			pr_err("sensor_cfg_to_hub light fail\n");
+	}
+	if(15 == motparams->ps_nvcfg.pscfg) {
+		err = mtk_nanohub_cfg_to_hub(ID_PROXIMITY, (uint8_t *)&motparams->ps_nvcfg, sizeof(struct mot_ps_nvcfg));
+		if (err < 0)
+			pr_err("sensor_cfg_to_hub proximity fail\n");
+	}
 #endif
 	//if(get_boot_mode() == FACTORY_BOOT)
 	//	mtk_nanohub_selftest_to_hub(ID_PROXIMITY);
