7fc3756cd6c1b3a577e015093ad947176e18ff78
hujing3 <hujing3@lenovo.com>
2021-08-16 11:52:16 +0800

    driver/lcm/corfu: mipi clk update for RF
    corfu/coful/cofud:
    mipi clk update for RF
    
    Change-Id: Ia4a511b3b8fb5c32f9302c1aa18c20087388b47a
    Signed-off-by: hujing3 <hujing3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2043172
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shengchun Li <lisc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lcm/dsi_panel_mot_mipi_vid_visionox_ft3519_fhd/dsi_panel_mot_mipi_vid_visionox_ft3519_fhd.c
+++ drivers/misc/mediatek/lcm/dsi_panel_mot_mipi_vid_visionox_ft3519_fhd/dsi_panel_mot_mipi_vid_visionox_ft3519_fhd.c
@@ -256,7 +256,7 @@ static void lcm_get_params(struct LCM_PARAMS *params)
 	params->dsi.horizontal_active_pixel = FRAME_WIDTH;
 	params->dsi.ssc_disable = 1;
 #ifndef CONFIG_FPGA_EARLY_PORTING
-	params->dsi.PLL_CLOCK = 590;    /* this value must be in MTK suggested table */
+	params->dsi.PLL_CLOCK = 598;    /* this value must be in MTK suggested table */
 #else
 	params->dsi.pll_div1 = 0;
 	params->dsi.pll_div2 = 0;
