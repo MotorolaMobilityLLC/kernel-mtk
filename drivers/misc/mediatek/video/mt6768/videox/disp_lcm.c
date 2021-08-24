fcd8fd412ebd7cc771ea6771c866f059935a6e89
xiaozhenyu <xiaozhenyu@longcheer.com>
2021-08-24 16:45:24 +0800

    milan: add TP tap to wake power switch
    - modify gesture_open code
    - add lcm suspend power for TP tap to wake
    
    Change-Id: Id5907e6b8917176aebed14252075fabe218479b3
    Reviewed-on: https://gerrit.mot.com/2049560
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jing Hu <hujing3@lenovo.com>
    Reviewed-by: Shengchun Li <lisc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/video/mt6768/videox/disp_lcm.c
+++ drivers/misc/mediatek/video/mt6768/videox/disp_lcm.c
@@ -12,6 +12,7 @@
 #include "disp_drv_platform.h"
 #include "ddp_manager.h"
 #include "disp_lcm.h"
+#include "primary_display.h"
 
 #if defined(MTK_LCM_DEVICE_TREE_SUPPORT)
 #include <linux/of.h>
@@ -1417,6 +1418,21 @@ int disp_lcm_esd_recover(struct disp_lcm_handle *plcm)
 	return -1;
 }
 
+void is_touchscreen_gesture_open(int value)
+{
+	struct LCM_DRIVER *lcm_drv = NULL;
+
+	if (_is_lcm_inited(pgc->plcm)) {
+		lcm_drv = pgc->plcm->drv;
+		if (value == 1) {
+				lcm_drv->tp_gesture_status  = GESTURE_ON;
+		} else{
+				lcm_drv->tp_gesture_status = GESTURE_OFF;
+		}
+	}
+}
+EXPORT_SYMBOL(is_touchscreen_gesture_open);
+
 int disp_lcm_suspend(struct disp_lcm_handle *plcm)
 {
 	struct LCM_DRIVER *lcm_drv = NULL;
@@ -1430,10 +1446,16 @@ int disp_lcm_suspend(struct disp_lcm_handle *plcm)
 			DISPERR("FATAL ERROR, lcm_drv->suspend is null\n");
 			return -1;
 		}
-
-		if (lcm_drv->suspend_power)
-			lcm_drv->suspend_power();
-
+		DISPMSG("tp_gesture_status = %d\n",lcm_drv->tp_gesture_status);
+		if(lcm_drv->tp_gesture_status ){
+			if (lcm_drv->tp_gesture_status == GESTURE_OFF) {
+				if (lcm_drv->suspend_power)
+					lcm_drv->suspend_power();
+			}
+		}else{
+			if (lcm_drv->suspend_power)
+				lcm_drv->suspend_power();
+        }
 
 		return 0;
 	}
