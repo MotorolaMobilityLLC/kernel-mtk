d1dbf8cc774ed92b885acfeed4f258f22094085c
shaozhuchao <shaozhuchao@wingtech.com>
2021-08-25 15:22:53 +0800

    Tonga: fix camera fail during aGing
    The CPU write to register is delayed, causing CCU boot exception, p1fail
    add memory barrier between two write reg function, then perform the aGing test again
    
    Change-Id: Ib4b583fd4485f97bca2932fa97d4881025926b8a
    Reviewed-on: https://gerrit.mot.com/2050423
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Heng Chen <chenheng3@mt.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/ccu/src/mt6765/ccu_hw.c
+++ drivers/misc/mediatek/ccu/src/mt6765/ccu_hw.c
@@ -905,6 +905,7 @@ int ccu_run(void)
 	/*LOG_DBG("cache flushed 2\n");*/
 	/*3. Set CCU_A_RESET. CCU_HW_RST=0*/
 	ccu_write_reg(ccu_base, CCU_INFO23, 0x900d);
+	mb();
 	ccu_write_reg_bit(ccu_base, RESET, CCU_HW_RST, 0);
 
 	LOG_DBG("released CCU reset, wait for initial done, %x\n",
