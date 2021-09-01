c7e8cc2693ed0980355d4fda29cdbe1c773afea9
JeiFeng Lee <linger.lee@mediatek.com>
2021-09-01 21:27:50 +0800

    [ALPS05969552] perf_ioctl: remove useless ioctl
    eara_thrm is not used anymore
    Remove them for security
    
    MTK-Commit-Id: 19286d57706d95ceb861081122b28cf8c4029e8e
    
    Change-Id: I6fed326bccf5938023d956e722073456ceeca92b
    Signed-off-by: JeiFeng Lee <linger.lee@mediatek.com>
    CR-Id: ALPS05969552
    Feature: Thermal Management

--- drivers/misc/mediatek/performance/perf_ioctl/perf_ioctl.c
+++ drivers/misc/mediatek/performance/perf_ioctl/perf_ioctl.c
@@ -70,84 +70,6 @@ static unsigned long perfctl_copy_to_user(void __user *pvTo,
 	return ulBytes;
 }
 
-/*--------------------EARA THERMAL------------------------*/
-
-/*--------------------DEV OP------------------------*/
-static int eara_thrm_show(struct seq_file *m, void *v)
-{
-	return 0;
-}
-
-static int eara_thrm_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, eara_thrm_show, inode->i_private);
-}
-
-static int eara_thrm_release(struct inode *inode, struct file *file)
-{
-	return single_release(inode, file);
-}
-
-static long eara_thrm_ioctl(struct file *filp,
-		unsigned int cmd, unsigned long arg)
-{
-	ssize_t ret = 0;
-	struct _EARA_THRM_PACKAGE *msgKM = NULL,
-			*msgUM = (struct _EARA_THRM_PACKAGE *)arg;
-	struct _EARA_THRM_PACKAGE smsgKM;
-
-	msgKM = &smsgKM;
-
-	if (perfctl_copy_from_user(msgKM, msgUM,
-				sizeof(struct _EARA_THRM_PACKAGE))) {
-		ret = -EFAULT;
-		goto ret_ioctl;
-	}
-
-	switch (cmd) {
-	case EARA_ENABLE:
-		if (eara_enable_fp)
-			eara_enable_fp(msgKM->request);
-		else
-			ret = -1;
-		break;
-	case EARA_GETINFO:
-		if (eara_get_tfps_pair_fp)
-			eara_get_tfps_pair_fp(EARA_MAX_COUNT, msgKM->pair_pid,
-					msgKM->pair_bufid, msgKM->pair_tfps);
-		else
-			ret = -1;
-
-		perfctl_copy_to_user(msgUM, msgKM,
-				sizeof(struct _EARA_THRM_PACKAGE));
-		break;
-	case EARA_TDIFF:
-		if (eara_set_tfps_diff_fp)
-			eara_set_tfps_diff_fp(EARA_MAX_COUNT, msgKM->pair_pid,
-					msgKM->pair_bufid, msgKM->pair_diff);
-		else
-			ret = -1;
-		break;
-	default:
-		pr_debug(TAG "%s %d: unknown cmd %x\n",
-			__FILE__, __LINE__, cmd);
-		ret = -1;
-		goto ret_ioctl;
-	}
-
-ret_ioctl:
-	return ret;
-}
-
-static const struct file_operations eara_thrm_Fops = {
-	.unlocked_ioctl = eara_thrm_ioctl,
-	.compat_ioctl = eara_thrm_ioctl,
-	.open = eara_thrm_open,
-	.read = seq_read,
-	.llseek = seq_lseek,
-	.release = eara_thrm_release,
-};
-
 /*--------------------EARA------------------------*/
 static void perfctl_notify_fpsgo_nn_begin(
 	struct _EARA_NN_PACKAGE *msgKM,
@@ -619,15 +541,6 @@ int init_perfctl(struct proc_dir_entry *parent)
 	}
 
 	pe = proc_create("eara_sys_ioctl", 0664, parent, &earasys_Fops);
-	if (!pe) {
-		pr_debug(TAG"%s failed with %d\n",
-				"Creating file node ",
-				ret_val);
-		ret_val = -ENOMEM;
-		goto out_wq;
-	}
-
-	pe = proc_create("eara_thrm_ioctl", 0664, parent, &eara_thrm_Fops);
 	if (!pe) {
 		pr_debug(TAG"%s failed with %d\n",
 				"Creating file node ",
