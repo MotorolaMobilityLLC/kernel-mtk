a3a476c5c64bd0d9c3ecc6343d10747d05e5c461
Ruijie Sun <ruijie.sun@mediatek.com>
2021-09-14 15:45:02 +0800

    [ALPS06095834] DISP: fix enter/leave idle issue
    [Detail]:
    fix enter/leave idle issue
    
    MTK-Commit-Id: aa2cf6ed79683ac4ca25dd17c108300d60d137c2
    
    Change-Id: Ie56b8adf3bb2b743666c4c3cc30cd25e332ba368
    CR-Id: ALPS06095834
    Feature: Display Driver (DSI Interface)
    Signed-off-by: Yijun Sun <yijun.sun@mediatek.com>

--- drivers/misc/mediatek/video/mt6768/videox/disp_lowpower.c
+++ drivers/misc/mediatek/video/mt6768/videox/disp_lowpower.c
@@ -335,10 +335,7 @@ int primary_display_dsi_vfp_change(int state)
 	struct LCM_PARAMS *params;
 	unsigned int apply_vfp = 0;
 
-	if (bdg_is_bdg_connected() == 1)
-		cmdqRecCreate(CMDQ_SCENARIO_PRIMARY_DISP, &handle);
-	else
-		cmdqRecCreate(CMDQ_SCENARIO_DISP_ESD_CHECK, &handle);
+	cmdqRecCreate(CMDQ_SCENARIO_DISP_ESD_CHECK, &handle);
 
 	cmdqRecReset(handle);
 
@@ -385,8 +382,6 @@ int primary_display_dsi_vfp_change(int state)
 			dpmgr_path_build_cmdq(primary_get_dpmgr_handle(), handle,
 						CMDQ_STOP_VDO_MODE, 0);
 
-			cmdqRecClearEventToken(handle, CMDQ_EVENT_MUTEX0_STREAM_EOF);
-
 			dpmgr_path_ioctl(primary_get_dpmgr_handle(), handle,
 						DDP_DSI_PORCH_CHANGE, &apply_vfp);
 
@@ -396,8 +391,6 @@ int primary_display_dsi_vfp_change(int state)
 
 			ddp_mutex_set_sof_wait(dpmgr_path_get_mutex(primary_get_dpmgr_handle()),
 						handle, 0);
-
-			_blocking_flush();
 
 			cmdqRecFlush(handle);
 		} else {
