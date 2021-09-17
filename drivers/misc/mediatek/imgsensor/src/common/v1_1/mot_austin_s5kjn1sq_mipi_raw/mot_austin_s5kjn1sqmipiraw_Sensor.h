7955f89412663f839af4fa1eabc413324be66df8
wangzl37 <wangzl37@motorola.com>
2021-09-17 09:58:00 +0800

    austin: update s5kjn1 settings
    austin：
    --update s5kjn1 settings
    --disable pdaf
    
    Change-Id: Ic2fc439a7e24bd69c16fa52b3593481e511b7d2a
    Signed-off-by: wangzl37 <wangzl37@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2068243
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Darong Huang <huangdra@motorola.com>
    Reviewed-by: Xu Ji <jixu@motorola.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_austin_s5kjn1sq_mipi_raw/mot_austin_s5kjn1sqmipiraw_Sensor.h
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_austin_s5kjn1sq_mipi_raw/mot_austin_s5kjn1sqmipiraw_Sensor.h
@@ -32,7 +32,6 @@ enum IMGSENSOR_MODE {
 	IMGSENSOR_MODE_SLIM_VIDEO,
 #if S5KJN1SQ_CUSTOM_ENABLE
 	IMGSENSOR_MODE_CUSTOM1,
-	IMGSENSOR_MODE_CUSTOM2,
 #endif
 };
 
@@ -114,7 +113,6 @@ struct imgsensor_info_struct {
 	struct imgsensor_mode_struct slim_video;
 #if S5KJN1SQ_CUSTOM_ENABLE
 	struct imgsensor_mode_struct custom1;
-	struct imgsensor_mode_struct custom2;
 #endif
 	kal_uint8 ae_shut_delay_frame;	/* shutter delay frame for AE cycle */
 
@@ -136,7 +134,6 @@ struct imgsensor_info_struct {
 	kal_uint8 slim_video_delay_frame;
 #if S5KJN1SQ_CUSTOM_ENABLE
 	kal_uint8 custom1_delay_frame;
-	kal_uint8 custom2_delay_frame;
 #endif
 	kal_uint8  frame_time_delay_frame;
 
