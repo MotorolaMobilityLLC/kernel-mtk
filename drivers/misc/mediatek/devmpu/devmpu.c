af86e0ce9d1b61a59b001e83a48665923c825276
chunhui.li <chunhui.li@mediatek.com>
2021-09-30 20:30:21 +0800

    [ALPS06181369] devmpu: fix cpu pre-fetch issue
    devmpu should filter domain 0/read violation to avoid
    cpu pre-fetch issue when enable geniezone
    
    MTK-Commit-Id: 7382d32a43ac775d64851337317cf902b80f8682
    
    Signed-off-by: chunhui.li <chunhui.li@mediatek.com>
    CR-Id: ALPS06181369
    Feature: [Module]Device MPU
    Change-Id: I4fc746fec98f7ccb87496bdbc3a676dd93809d3d

--- drivers/misc/mediatek/devmpu/devmpu.c
+++ drivers/misc/mediatek/devmpu/devmpu.c
@@ -209,12 +209,12 @@ int devmpu_print_violation(uint64_t vio_addr, uint32_t vio_id,
 		 * to be consistent with EMI MPU violation handling
 		 */
 		vio_rw = (vio.is_write) ? 1 : 2;
+	}
 
 #ifdef CONFIG_MTK_ENABLE_GENIEZONE
-		if (vio_rw == 2 && vio_domain == 0)
-			return 0;
+	if (vio_rw == 2 && vio_domain == 0)
+		return 0;
 #endif
-	}
 
 	vio_axi_id = (vio_id >> 3) & 0x1FFF;
 	vio_port_id = vio_id & 0x7;
