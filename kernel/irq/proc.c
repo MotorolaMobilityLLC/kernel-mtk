e060b8ee5d0193015c6ae3fc878a1f2a902ad5c6
zjbhq11 <zjbhq11@motorola.com>
2021-10-11 10:42:23 +0800

    kernel/irq: resolve kernel panic issue
    kernel/irq:
    resolve kernel panic issue
    
    Change-Id: I4e0b8166a89826855afff6547ceea11c1b15affa
    Signed-off-by: zjbhq11 <zjbhq11@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2086828
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key

--- kernel/irq/proc.c
+++ kernel/irq/proc.c
@@ -596,10 +596,13 @@ int show_interrupts(struct seq_file *p, void *v)
 	if (desc->name)
 		seq_printf(p, "-%-8s", desc->name);
 
-	if (action) {
+	if (!IS_ERR_OR_NULL(action) && !IS_ERR_OR_NULL(action->name)) {
 		seq_printf(p, "  %s", action->name);
-		while ((action = action->next) != NULL)
-			seq_printf(p, ", %s", action->name);
+		while ((action = action->next) != NULL) {
+			if (!IS_ERR_OR_NULL(action) && !IS_ERR_OR_NULL(action->name)) {
+				seq_printf(p, ", %s", action->name);
+			}
+		}
 	}
 
 	seq_putc(p, '\n');
