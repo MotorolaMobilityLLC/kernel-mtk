5d6dfe3dec51d9b85003bc9688dfc6153d6b7e79
junren.su <junren.su@mediatek.com>
2021-10-15 19:56:26 +0800

    [ALPS06244953] ION: move buffer kmap from begin/end_cpu_access()
    [Detail]
    Since dma_buf_begin/end_cpu_access() calls always used to bracket
    dma_buf_kmap/kunmap calls, ION performed kmap/kunmap invocations for the
    buffer during dma_buf_begin/end_cpu_access() calls and cached the
    results with a kmap counter.
    However, dma_buf_begin/end_cpu_access() invocations can be
    triggered from the userspace using the DMA_BUF_IOC_SYNC ioctl as well.
    This means that a mapping that was created by a device driver using by a
    dma_buf_kmap() call or an ion_map_kernel() call could be unmapped
    from userspace if a client accidentally(or maliciously) invoked
    DMA_BUF_IOCTL_SYNC IOCTL with DMA_BUF_SYNC_END argument since this
    would inturn invoke dma_buf_end_cpu_access() which would then decrement
    the kmap counter and invoke kunmap() when the counter gets to 0.
    
    This patch moves the kmap/kunmap operations from the
    begin/end_cpu_access() DMA-BUF ops to the map/unmap DMA-BUF ops to
    prevent the issue.
    
    MTK-Commit-Id: 6bd6b6fab5188f452cd9750e647f07479e53caea
    
    CR-Id: ALPS06244953
    Signed-off-by: junren.su <junren.su@mediatek.com>
    Feature: ION
    Change-Id: I66d9ab32f0ca83daa233cfa8d665e1fde48c3ebf
    (cherry-pick from 2631e40e3a9a320dea851bdcd5a0ba07eb1f2df5)

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -1735,13 +1735,33 @@ static void ion_dma_buf_release(struct dma_buf *dmabuf)
 static void *ion_dma_buf_kmap(struct dma_buf *dmabuf, unsigned long offset)
 {
 	struct ion_buffer *buffer = dmabuf->priv;
+	void *vaddr;
 
-	return buffer->vaddr + offset * PAGE_SIZE;
+	if (!buffer->heap->ops->map_kernel) {
+		IONMSG("%s: map kernel is not implemented by this heap.\n",
+		       __func__);
+		return ERR_PTR(-ENOTTY);
+	}
+	mutex_lock(&buffer->lock);
+	vaddr = ion_buffer_kmap_get(buffer);
+	mutex_unlock(&buffer->lock);
+
+	if (IS_ERR(vaddr))
+		return vaddr;
+
+	return vaddr + offset * PAGE_SIZE;
 }
 
 static void ion_dma_buf_kunmap(struct dma_buf *dmabuf, unsigned long offset,
 			       void *ptr)
 {
+	struct ion_buffer *buffer = dmabuf->priv;
+
+	if (buffer->heap->ops->map_kernel) {
+		mutex_lock(&buffer->lock);
+		ion_buffer_kmap_put(buffer);
+		mutex_unlock(&buffer->lock);
+	}
 }
 
 #ifdef MTK_ION_DMABUF_SUPPORT
@@ -1749,17 +1769,8 @@ static int ion_dma_buf_begin_cpu_access(struct dma_buf *dmabuf,
 					enum dma_data_direction direction)
 {
 	struct ion_buffer *buffer = dmabuf->priv;
-	void *vaddr;
 	struct ion_dma_buf_attachment *a;
 
-	/*
-	 * TODO: Move this elsewhere because we don't always need a vaddr
-	 */
-	if (buffer->heap->ops->map_kernel) {
-		mutex_lock(&buffer->lock);
-		vaddr = ion_buffer_kmap_get(buffer);
-		mutex_unlock(&buffer->lock);
-	}
 	if (ion_iommu_heap_type(buffer) ||
 	    buffer->heap->type == (int)ION_HEAP_TYPE_SYSTEM) {
 		IONDBG("%s iommu device, to cache sync\n", __func__);
@@ -1783,12 +1794,6 @@ static int ion_dma_buf_end_cpu_access(struct dma_buf *dmabuf,
 	struct ion_buffer *buffer = dmabuf->priv;
 	struct ion_dma_buf_attachment *a;
 
-	if (buffer->heap->ops->map_kernel) {
-		mutex_lock(&buffer->lock);
-		ion_buffer_kmap_put(buffer);
-		mutex_unlock(&buffer->lock);
-	}
-
 	if (ion_iommu_heap_type(buffer) ||
 	    buffer->heap->type == (int)ION_HEAP_TYPE_SYSTEM) {
 		IONDBG("%s iommu device, to cache sync\n", __func__);
@@ -1809,30 +1814,12 @@ static int ion_dma_buf_end_cpu_access(struct dma_buf *dmabuf,
 static int ion_dma_buf_begin_cpu_access(struct dma_buf *dmabuf,
 					enum dma_data_direction direction)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
-	void *vaddr;
-
-	if (!buffer->heap->ops->map_kernel) {
-		pr_err("%s: map kernel is not implemented by this heap.\n",
-		       __func__);
-		return -ENODEV;
-	}
-
-	mutex_lock(&buffer->lock);
-	vaddr = ion_buffer_kmap_get(buffer);
-	mutex_unlock(&buffer->lock);
-	return PTR_ERR_OR_ZERO(vaddr);
+	return 0;
 }
 
 static int ion_dma_buf_end_cpu_access(struct dma_buf *dmabuf,
 				      enum dma_data_direction direction)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
-
-	mutex_lock(&buffer->lock);
-	ion_buffer_kmap_put(buffer);
-	mutex_unlock(&buffer->lock);
-
 	return 0;
 }
 #endif
