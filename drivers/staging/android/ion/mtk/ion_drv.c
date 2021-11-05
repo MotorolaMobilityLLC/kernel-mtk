88820485b9b3996dd4aacf88c565c8e53948a4dd
junren.su <junren.su@mediatek.com>
2021-11-05 11:26:17 +0800

    [ALPS06271385] ION:fix coverity issue
    [Detail]
    Invalid type in argument to printf format specifie.
    
    MTK-Commit-Id: 53015395e1801256e31709b91a98444ae4612513
    
    CR-Id: ALPS06271385
    Signed-off-by: junren.su <junren.su@mediatek.com>
    Feature: ION
    Change-Id: I40b337beb7a7b041a44e6dd1fce96a73681712e0
    (cherry picked from commit 44e9dc6c318edc2a85ed1e9d2c9a935ddf01d78b)

--- drivers/staging/android/ion/mtk/ion_drv.c
+++ drivers/staging/android/ion/mtk/ion_drv.c
@@ -596,7 +596,7 @@ static long ion_sys_ioctl(struct ion_client *client, unsigned int cmd,
 		    copy_from_user(&param, (void __user *)arg,
 				   sizeof(struct ion_sys_data));
 		if (ret_copy != 0) {
-			IONMSG("%s:err arg copy failed, ret_copy = %d. %s(%s),%d, k:%d\n",
+			IONMSG("%s:err arg copy failed, ret_copy = %lu. %s(%s),%d, k:%d\n",
 			       __func__, ret_copy, client->name, client->dbg_name,
 			       client->pid, from_kernel);
 			ret = -EFAULT;
