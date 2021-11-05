62b730d084feb707357d6bc268c97b1c46731675
Celine Liu <celine.liu@mediatek.com>
2021-11-05 11:27:20 +0800

    [ALPS06306315] adsp: change ipi irq flow
    [Detail]
    1. change ipi irq flow (change@mp8: 4573419)
    2. refine clock default value
    
    MTK-Commit-Id: ecb65f617fe5e23765468109e126ba732c7dde48
    
    Change-Id: I226e25329b56989a0271adc71f98b7bf2105907a
    Signed-off-by: Celine Liu <celine.liu@mediatek.com>
    CR-Id: ALPS06306315
    Feature: [Module]Audio OpenDSP
    (cherry picked from commit a1ce6c7d86c578cb6ff3e708372ceed191b5e1b6)

--- drivers/misc/mediatek/adsp/mt6785/adsp_clk.h
+++ drivers/misc/mediatek/adsp/mt6785/adsp_clk.h
@@ -8,9 +8,6 @@
 
 #include <linux/platform_device.h>
 
-#define CLK_DEFAULT_INIT_CK     CLK_TOP_ADSPPLL_D6
-#define CLK_DEFAULT_26M_CK      CLK_TOP_CLK26M
-
 enum adsp_clk {
 	CLK_ADSP_INFRA,
 	CLK_TOP_ADSP_SEL,
@@ -21,6 +18,9 @@ enum adsp_clk {
 	ADSP_CLK_NUM
 };
 
+
+#define CLK_DEFAULT_INIT_CK     CLK_TOP_ADSPPLL_D6
+#define CLK_DEFAULT_26M_CK      CLK_TOP_CLK26M
 void adsp_set_clock_freq(enum adsp_clk clk);
 int adsp_set_top_mux(enum adsp_clk clk);
 int adsp_enable_clock(void);
