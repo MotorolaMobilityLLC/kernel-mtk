7afa395a8fb4a78501319233d2b3f755abc7de31
mtk23564 <baron-pj.yen@mediatek.com>
2021-11-11 12:25:43 +0800

    [ALPS06137571] cam: fix coverity
    [Detail]
    fix 6s p-p coverity: dead_error_condition
    
    MTK-Commit-Id: 59491950da1ae98b8afd6a1c4956513099c97e7d
    
    Change-Id: I15dc4515a4c1ed378c6e62a6572eeb7888fcd37f
    Signed-off-by: mtk23564 <baron-pj.yen@mediatek.com>
    CR-Id: ALPS06137571
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/ov05a20_mipi_raw/ov05a20mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/ov05a20_mipi_raw/ov05a20mipiraw_Sensor.c
@@ -693,8 +693,6 @@ static void hdr_write_gain(kal_uint16 lgain, kal_uint16 sgain)
 	if (reg_lgain > 0x7c0) {
 		reg_d_lgain = reg_lgain*1024/1984;
 
-		if (reg_d_lgain < 0x400)// sensor 1xGain
-			reg_d_lgain = 0x400;
 		if (reg_d_lgain > 0x3fff)// sensor 16xGain
 			reg_d_lgain = 0x3fff;
 		/* long exposure */
@@ -710,8 +708,6 @@ static void hdr_write_gain(kal_uint16 lgain, kal_uint16 sgain)
 	} else {
 		if (reg_lgain < 0x80)// sensor 1xGain
 			reg_lgain = 0x80;
-		if (reg_lgain > 0x7c0)// sensor 15.5xGain
-			reg_lgain = 0x7c0;
 
 		/* binary to find A_Gain */
 		reg_a_gain = binary_find_AGain(AGain_table,
@@ -736,8 +732,6 @@ static void hdr_write_gain(kal_uint16 lgain, kal_uint16 sgain)
 	if (reg_sgain > 0x7c0) {
 		reg_d_sgain = reg_sgain*1024/1984;
 
-		if (reg_d_sgain < 0x400)// sensor 1xGain
-			reg_d_sgain = 0x400;
 		if (reg_d_sgain > 0x3fff)// sensor 16xGain
 			reg_d_sgain = 0x3fff;
 		/* short gain */
@@ -753,8 +747,6 @@ static void hdr_write_gain(kal_uint16 lgain, kal_uint16 sgain)
 	} else {
 		if (reg_sgain < 0x80)// sensor 1xGain
 			reg_sgain = 0x80;
-		if (reg_sgain > 0x7c0)// sensor 15.5xGain
-			reg_sgain = 0x7c0;
 
 		/* binary to find A_Gain */
 		reg_a_gain = binary_find_AGain(AGain_table,
