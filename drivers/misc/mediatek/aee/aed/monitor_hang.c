8b67114f582c501128c3835d5d9bea95121f4356
zhiyong.wang <zhiyong.wang@mediatek.com>
2021-11-11 12:25:29 +0800

    [ALPS06330988] hang: stop hang by adb stop
    The command "adb stop" kills system_server in which case hang_detect
    thread should stop detecting. "adb start" can wake up it.
    
    MTK-Commit-Id: 0485cdcff9a9eb73141d28ed47444be530ac1cf8
    
    Signed-off-by: zhiyong.wang <zhiyong.wang@mediatek.com>
    CR-Id: ALPS06330988
    Feature: Android Exception Engine(AEE)
    Change-Id: I4dc78958fc9fca5097aef124ae23b3b0f6a9fcee
    (cherry picked from commit c6b9232ccd608df43eb6b9d6f6e60f1e0508ecfc)
    (cherry picked from commit 75a92b315b37d2feec4c92c4a2b0fa4490df9a07)

--- drivers/misc/mediatek/aee/aed/monitor_hang.c
+++ drivers/misc/mediatek/aee/aed/monitor_hang.c
@@ -95,6 +95,7 @@ static bool Hang_Detect_first;
 #define HD_INTER 30
 
 static int hd_detect_enabled;
+static bool hd_zygote_stopped;
 static int hd_timeout = 0x7fffffff;
 static int hang_detect_counter = 0x7fffffff;
 static int dump_bt_done;
@@ -299,11 +300,44 @@ static ssize_t monitor_hang_read(struct file *filp, char __user *buf,
 static ssize_t monitor_hang_write(struct file *filp, const char __user *buf,
 		size_t count, loff_t *f_pos)
 {
+	char msg[8] = {0};
 
-	/* LOGV("%s\n", __func__); */
-	return 0;
+	if (count > 2) {
+		pr_info("hang_detect: invalid input\n");
+		return -EINVAL;
+	}
+
+	if (!buf) {
+		pr_info("hang_detect: invalid user buf\n");
+		return -EINVAL;
+	}
+
+	if (copy_from_user(msg, buf, count)) {
+		pr_info("hang_detect: failed to copy from user\n");
+		return -EFAULT;
+	}
+
+	if (strncmp(current->comm, "init", 4))
+		return  -EINVAL;
+
+	if (msg[0] == '0') {
+		hd_detect_enabled = 0;
+		hd_zygote_stopped = true;
+		pr_info("hang_detect: disable by stop cmd\n");
+	} else if (msg[0] == '1') {
+		if (hd_zygote_stopped) {
+			hd_detect_enabled = 1;
+			hd_zygote_stopped = false;
+			pr_info("hang_detect: enable by start cmd\n");
+		} else {
+			pr_info("hang_detect: zygote running\n");
+		}
+	} else {
+		pr_info("hang_detect: invalid control msg\n");
+	}
+
+	return count;
 }
-
 
 
 /*
