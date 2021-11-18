0b440188fbf67ed567ca91a24a1e213627788602
Wen Chen <wen.chen@mediatek.com>
2021-11-19 01:16:38 +0800

    [ALPS06287385] apusys: Fix null pointer access for reg_dump
    Skip iomem access while null pointer
    
    MTK-Commit-Id: 5493546c5bf41382b41f38b6722bd487e13647ac
    
    Signed-off-by: Wen Chen <wen.chen@mediatek.com>
    CR-Id: ALPS06287385
    Feature: [Module] APUSYS
    Change-Id: Ibf3f93fa003bbddf2436b7bf80dfe72978da110e
    (cherry picked from commit 0cd6e37bec5c9b61d18b6d81adcffa1bde89b7f4)

--- drivers/misc/mediatek/apusys/debug/debug_driver.c
+++ drivers/misc/mediatek/apusys/debug/debug_driver.c
@@ -313,7 +313,12 @@ static int debug_probe(struct platform_device *pdev)
 	}
 
 	pr_notice("debug probe done, dbglog_buf= 0x%p\n", dbglog_buf);
-	apusys_dump_init(&pdev->dev);
+	ret = apusys_dump_init(&pdev->dev);
+	if (ret) {
+		DBG_LOG_ERR("failed to create debug dump attr node(devinfo).\n");
+		goto out;
+	}
+
 	return 0;
 
 out:
