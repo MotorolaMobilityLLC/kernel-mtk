7cbde6d3de8c050da56403cc2dfef1d5e413a33f
mtk20335 <Rose-YH.Kuo@mediatek.com>
2021-11-19 01:16:30 +0800

    [ALPS06249716] cmdq: set cmdq_mbox_enable in UT
    set cmdq_mbox_enable in UT for idle
    
    MTK-Commit-Id: b24726ae950f2c570f82e7885189dabdb29ed7a4
    
    Signed-off-by: mtk20335 <Rose-YH.Kuo@mediatek.com>
    CR-Id: ALPS06249716
    Feature: [Module]CMDQ Driver
    Change-Id: I761eb1e9250ea37356e986d3c5aaa23c73687fc7
    (cherry picked from commit b9e3b05a06efeb0f6e393054e1f148e8bb1e3eb3)

--- drivers/misc/mediatek/cmdq/mailbox/cmdq-test.c
+++ drivers/misc/mediatek/cmdq/mailbox/cmdq-test.c
@@ -1173,6 +1173,8 @@ cmdq_test_trigger(struct cmdq_test *test, const s32 sec, const s32 id)
 		return;
 	}
 #endif
+	cmdq_mbox_enable(test->clt->chan);
+	cmdq_mbox_enable(test->loop->chan);
 	switch (id) {
 	case 0:
 		cmdq_test_mbox_write(test, sec, false);
@@ -1263,6 +1265,8 @@ cmdq_test_trigger(struct cmdq_test *test, const s32 sec, const s32 id)
 	default:
 		break;
 	}
+	cmdq_mbox_disable(test->loop->chan);
+	cmdq_mbox_disable(test->clt->chan);
 	cmdq_thread_timeout_restore(thread, backup);
 }
 
