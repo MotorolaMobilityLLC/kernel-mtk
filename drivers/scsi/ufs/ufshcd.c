177df1d6beaa7d3e1f5cc73d47f75e4b4783aeb4
fuxm1 <fuxm1@motorola.com>
2021-11-21 12:45:59 +0800

    ufs/hid: add manual_gc driver for Hynix HID
    PROPAGATED_from (CR)
    
    Remark:
    Hynix HID feature driver interface manual_gc added.
    memory list:
    4+64   H9HQ54ACPMMDAR-KEM(V6+D1y)
    6+128  H9HQ15AECMBDAR-KEM(V6+D1y)
    6+256  H9HQ21AECMADAR-KEM(V6+D1y)
    
    Propagate from: (CR)
    
    Change-Id: I8189facb119cd0c4631c3c09eb80a1acb54b1a11
    Signed-off-by: fuxm1 <fuxm1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2122817
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Chuanchun Zhu <zhucc2@motorola.com>
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/2495294

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -122,6 +122,11 @@ unsigned int ram_size;
 /* Default value of wait time before gating device ref clock */
 #define UFSHCD_REF_CLK_GATING_WAIT_US 0xFF /* microsecs */
 
+#if defined(CONFIG_SCSI_SKHID)
+/* for manual gc */
+#define UFSHCD_MANUAL_GC_HOLD_HIBERN8		2000	/* 2 seconds */
+#endif
+
 #define ufshcd_toggle_vreg(_dev, _vreg, _on)				\
 	({                                                              \
 		int _ret;                                               \
@@ -311,6 +316,9 @@ static irqreturn_t ufshcd_tmc_handler(struct ufs_hba *hba);
 #if defined(CONFIG_SCSI_SKHPB) || defined(CONFIG_SCSI_UFS_HPB)
 static int get_dram_info(struct ufs_hba *hba);
 static int get_storage_info(struct ufs_hba *hba);
+#endif
+#if defined(CONFIG_SCSI_SKHID)
+static void ufshcd_add_hid_info_sysfs_node(struct ufs_hba *hba);
 #endif
 static void ufshcd_async_scan(void *data, async_cookie_t cookie);
 static int ufshcd_reset_and_restore(struct ufs_hba *hba);
@@ -1777,6 +1785,51 @@ static void ufshcd_clkscaling_init_sysfs(struct ufs_hba *hba)
 	if (device_create_file(hba->dev, &hba->clk_scaling.enable_attr))
 		dev_err(hba->dev, "Failed to create sysfs for clkscale_enable\n");
 }
+
+#if defined(CONFIG_SCSI_SKHID)
+static enum hrtimer_restart ufshcd_mgc_hrtimer_handler(struct hrtimer *timer)
+{
+	struct ufs_hba *hba = container_of(timer, struct ufs_hba,
+					manual_gc.hrtimer);
+
+	queue_work(hba->manual_gc.mgc_workq, &hba->manual_gc.hibern8_work);
+	return HRTIMER_NORESTART;
+}
+
+static void ufshcd_mgc_hibern8_work(struct work_struct *work)
+{
+	struct ufs_hba *hba = container_of(work, struct ufs_hba,
+						manual_gc.hibern8_work);
+	pm_runtime_mark_last_busy(hba->dev);
+	pm_runtime_put_noidle(hba->dev);
+	/* bkops will be disabled when power down */
+}
+
+static void ufshcd_init_manual_gc(struct ufs_hba *hba)
+{
+	struct ufs_manual_gc *mgc = &hba->manual_gc;
+	char wq_name[sizeof("ufs_mgc_hibern8_work")];
+
+	mgc->state = MANUAL_GC_ENABLE;
+	mgc->hagc_support = true;
+	mgc->delay_ms = UFSHCD_MANUAL_GC_HOLD_HIBERN8;
+
+	hrtimer_init(&mgc->hrtimer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+	mgc->hrtimer.function = ufshcd_mgc_hrtimer_handler;
+
+	INIT_WORK(&mgc->hibern8_work, ufshcd_mgc_hibern8_work);
+	snprintf(wq_name, ARRAY_SIZE(wq_name), "ufs_mgc_hibern8_work_%d",
+			hba->host->host_no);
+	hba->manual_gc.mgc_workq = create_singlethread_workqueue(wq_name);
+}
+
+static void ufshcd_exit_manual_gc(struct ufs_hba *hba)
+{
+	hrtimer_cancel(&hba->manual_gc.hrtimer);
+	cancel_work_sync(&hba->manual_gc.hibern8_work);
+	destroy_workqueue(hba->manual_gc.mgc_workq);
+}
+#endif
 
 static void ufshcd_ungate_work(struct work_struct *work)
 {
@@ -3258,7 +3311,7 @@ static inline void ufshcd_init_query(struct ufs_hba *hba,
 	(*request)->upiu_req.selector = selector;
 }
 
-#if defined(CONFIG_SCSI_SKHPB)
+#if defined(CONFIG_SCSI_SKHPB) || defined(CONFIG_SCSI_SKHID)
 int ufshcd_query_flag_retry(struct ufs_hba *hba,
 	enum query_opcode opcode, enum flag_idn idn, bool *flag_res)
 #else
@@ -3432,14 +3485,20 @@ EXPORT_SYMBOL_GPL(ufshcd_query_attr);
  *
  * Returns 0 for success, non-zero in case of failure
 */
+#if defined(CONFIG_SCSI_SKHID)
+int ufshcd_query_attr_retry(struct ufs_hba *hba,
+	enum query_opcode opcode, enum attr_idn idn, u8 index, u8 selector,
+	u32 *attr_val)
+#else
 static int ufshcd_query_attr_retry(struct ufs_hba *hba,
 	enum query_opcode opcode, enum attr_idn idn, u8 index, u8 selector,
 	u32 *attr_val)
+#endif
 {
 	int ret = 0;
 	u32 retries;
 
-	 for (retries = QUERY_REQ_RETRIES; retries > 0; retries--) {
+	for (retries = QUERY_REQ_RETRIES; retries > 0; retries--) {
 		ret = ufshcd_query_attr(hba, opcode, idn, index,
 						selector, attr_val);
 		if (ret)
@@ -5786,8 +5845,11 @@ static inline int ufshcd_get_bkops_status(struct ufs_hba *hba, u32 *status)
  * to know whether auto bkops is enabled or disabled after this function
  * returns control to it.
  */
-static int ufshcd_bkops_ctrl(struct ufs_hba *hba,
-			     enum bkops_status status)
+#if defined(CONFIG_SCSI_SKHID)
+int ufshcd_bkops_ctrl(struct ufs_hba *hba, enum bkops_status status)
+#else
+static int ufshcd_bkops_ctrl(struct ufs_hba *hba,enum bkops_status status)
+#endif
 {
 	int err;
 	u32 curr_status = 0;
@@ -7533,7 +7595,9 @@ static int ufs_get_device_desc(struct ufs_hba *hba)
 
 	/* getting Specification Version in big endian format */
 	dev_info->wspecversion = desc_buf[DEVICE_DESC_PARAM_SPEC_VER] << 8 |
-				      desc_buf[DEVICE_DESC_PARAM_SPEC_VER + 1];
+                      desc_buf[DEVICE_DESC_PARAM_SPEC_VER + 1];
+
+	dev_info->b_wb_buffer_type = desc_buf[DEVICE_DESC_PARAM_WB_TYPE];
 
 	model_index = desc_buf[DEVICE_DESC_PARAM_PRDCT_NAME];
 	err = ufshcd_read_string_desc(hba, model_index,
@@ -8053,6 +8117,10 @@ static int ufshcd_probe_hba(struct ufs_hba *hba, bool async)
 #if defined(CONFIG_SCSI_SKHPB)
 	if (hba->dev_info.wmanufacturerid == UFS_VENDOR_SKHYNIX)
 		schedule_delayed_work(&hba->skhpb_init_work, 0);
+#endif
+
+#if defined(CONFIG_SCSI_SKHID)
+		ufshcd_add_hid_info_sysfs_node(hba);
 #endif
 
 	/*
@@ -9737,6 +9805,152 @@ static void ufshcd_add_hpb_info_sysfs_node(struct ufs_hba *hba)
 }
 #endif
 
+#if defined(CONFIG_SCSI_SKHID) && defined(CONFIG_SCSI_UFS_FEATURE)
+static ssize_t manual_gc_show(struct device *dev,
+			struct device_attribute *attr, char *buf)
+{
+	struct ufs_hba *hba = dev_get_drvdata(dev);
+	u32 status = MANUAL_GC_OFF;
+
+	if (hba->manual_gc.state == MANUAL_GC_DISABLE)
+		return scnprintf(buf, PAGE_SIZE, "%s", "disabled\n");
+
+	pm_runtime_get_sync(hba->dev);
+
+	//down_read(&hba->query_lock);
+	if (hba->manual_gc.hagc_support) {
+		int err = ufshcd_query_attr_retry(hba,
+			UPIU_QUERY_OPCODE_READ_ATTR,
+			QUERY_ATTR_IDN_MANUAL_GC_STATUS, 0, 0, &status);
+
+		hba->manual_gc.hagc_support = err ? false: true;
+	}
+	//up_read(&hba->query_lock);
+	pm_runtime_mark_last_busy(hba->dev);
+	pm_runtime_put_noidle(hba->dev);
+
+	if (!hba->manual_gc.hagc_support)
+		return scnprintf(buf, PAGE_SIZE, "%s", "bkops\n");
+	return scnprintf(buf, PAGE_SIZE, "%s",
+			status == MANUAL_GC_OFF ? "off\n" : "on\n");
+}
+
+static int manual_gc_enable(struct ufs_hba *hba, u32 *value)
+{
+	return ufshcd_query_attr_retry(hba,
+				UPIU_QUERY_OPCODE_WRITE_ATTR,
+				QUERY_ATTR_IDN_MANUAL_GC_CONT, 0, 0,
+				value);
+}
+
+static ssize_t manual_gc_store(struct device *dev,
+			struct device_attribute *attr,
+			const char *buf, size_t count)
+{
+	struct ufs_hba *hba = dev_get_drvdata(dev);
+	u32 value;
+	int err = 0;
+
+	if (kstrtou32(buf, 0, &value))
+		return -EINVAL;
+
+	if (value >= MANUAL_GC_MAX)
+		return -EINVAL;
+
+	if (value == MANUAL_GC_DISABLE || value == MANUAL_GC_ENABLE) {
+		hba->manual_gc.state = value;
+		return count;
+	}
+	if (hba->manual_gc.state == MANUAL_GC_DISABLE)
+		return count;
+
+	pm_runtime_get_sync(hba->dev);
+
+	if (hba->manual_gc.hagc_support)
+		hba->manual_gc.hagc_support =
+			manual_gc_enable(hba, &value) ? false : true;
+
+	if (!hba->manual_gc.hagc_support) {
+		enum query_opcode opcode = (value == MANUAL_GC_ON) ?
+						UPIU_QUERY_OPCODE_SET_FLAG:
+						UPIU_QUERY_OPCODE_CLEAR_FLAG;
+
+		err = ufshcd_bkops_ctrl(hba, (value == MANUAL_GC_ON) ?
+					BKOPS_STATUS_NON_CRITICAL:
+					BKOPS_STATUS_CRITICAL);
+		if (!hba->auto_bkops_enabled)
+			err = -EAGAIN;
+
+		/* flush wb buffer */
+		if (hba->dev_info.wspecversion >= 0x0310) {
+			u8 index = ufshcd_wb_get_query_index(hba);
+
+			ufshcd_query_flag_retry(hba, opcode,
+				QUERY_FLAG_IDN_WB_BUFF_FLUSH_DURING_HIBERN8,
+				index);
+			ufshcd_query_flag_retry(hba, opcode,
+				QUERY_FLAG_IDN_WB_BUFF_FLUSH_EN, index);
+		}
+	}
+
+	if (err || hrtimer_active(&hba->manual_gc.hrtimer)) {
+		pm_runtime_mark_last_busy(hba->dev);
+		pm_runtime_put_noidle(hba->dev);
+		return count;
+	} else {
+		/* pm_runtime_put_sync in delay_ms */
+		hrtimer_start(&hba->manual_gc.hrtimer,
+			ms_to_ktime(hba->manual_gc.delay_ms),
+			HRTIMER_MODE_REL);
+	}
+	return count;
+}
+
+static ssize_t manual_gc_hold_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct ufs_hba *hba = dev_get_drvdata(dev);
+
+	return snprintf(buf, PAGE_SIZE, "%lu\n", hba->manual_gc.delay_ms);
+}
+
+static ssize_t manual_gc_hold_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct ufs_hba *hba = dev_get_drvdata(dev);
+	unsigned long value;
+
+	if (kstrtoul(buf, 0, &value))
+		return -EINVAL;
+
+	hba->manual_gc.delay_ms = value;
+	return count;
+}
+static DEVICE_ATTR(manual_gc, 0644, manual_gc_show, manual_gc_store);
+static DEVICE_ATTR(manual_gc_hold, 0644, manual_gc_hold_show, manual_gc_hold_store);
+
+static struct attribute *ufs_hid_default_attributes[] = {
+        &dev_attr_manual_gc.attr,
+        &dev_attr_manual_gc_hold.attr,
+        NULL
+};
+
+static struct attribute_group ufs_hid_default_group = {
+        .attrs  = ufs_hid_default_attributes,
+};
+
+static void ufshcd_add_hid_info_sysfs_node(struct ufs_hba *hba)
+{
+        int err  = -ENOMEM;
+        struct device *dev = hba->dev;
+
+        err = sysfs_create_group(&dev->kobj, &ufs_hid_default_group);
+
+        if (err)
+               dev_err(hba->dev, "cannot create hid sysfs group err: %d\n", err);
+}
+#endif
+
 /**
  * ufshcd_shutdown - shutdown routine
  * @hba: per adapter instance
@@ -9796,6 +10010,10 @@ if (hba->dev_info.wmanufacturerid == UFS_VENDOR_SKHYNIX &&
 	ufs_bsg_remove(hba);
 	ufs_sysfs_remove_nodes(hba->dev);
 	scsi_remove_host(hba->host);
+		ufshcd_hba_stop(hba, true);
+#if defined(CONFIG_SCSI_SKHID)
+	ufshcd_exit_manual_gc(hba);
+#endif
 	/* disable interrupts */
 	ufshcd_disable_intr(hba, hba->intr_mask);
 	ufshcd_hba_stop(hba, true);
@@ -9977,6 +10195,9 @@ int ufshcd_init(struct ufs_hba *hba, void __iomem *mmio_base, unsigned int irq)
 
 	ufshcd_init_clk_scaling(hba);
 
+#if defined(CONFIG_SCSI_SKHID)
+	ufshcd_init_manual_gc(hba);
+#endif
 	/*
 	 * In order to avoid any spurious interrupt immediately after
 	 * registering UFS controller interrupt handler, clear any pending UFS
@@ -10084,6 +10305,9 @@ out_remove_scsi_host:
 	scsi_remove_host(hba->host);
 exit_gating:
 	ufshcd_exit_clk_scaling(hba);
+#if defined(CONFIG_SCSI_SKHID)
+	ufshcd_exit_manual_gc(hba);
+#endif
 	ufshcd_exit_clk_gating(hba);
 out_disable:
 	hba->is_irq_enabled = false;
