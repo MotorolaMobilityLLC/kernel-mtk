d342e6da556f2a56ae142c5a36a6f7ef069ffcb0
fuxm1 <fuxm1@motorola.com>
2021-11-21 14:25:07 +0800

    ufs/hid: compatible with Micron 4+64 not support HID
    Remark:
    compatible with 4+64 Micron lyra structure memory
    Micron 4+64 MT29VZZZAD8FQFSL-046 WJ.G8K
    
    propagate from: (CR)
    
    Change-Id: Idb984e028fd56d7b9307d33c77dc70cbeb3f1ca4
    Signed-off-by: fuxm1 <fuxm1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2122822
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Chuanchun Zhu <zhucc2@motorola.com>
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/scsi/ufs/ufsfeature.c
+++ drivers/scsi/ufs/ufsfeature.c
@@ -301,7 +301,7 @@ out:
 	return ret;
 }
 
-void ufsf_device_check(struct ufs_hba *hba)
+int ufsf_device_check(struct ufs_hba *hba)
 {
 	struct ufsf_feature *ufsf = &hba->ufsf;
 	int ret;
@@ -318,11 +318,11 @@ void ufsf_device_check(struct ufs_hba *hba)
 
 	ret = ufsf_read_dev_desc(ufsf, selector);
 	if (ret)
-		return;
+		return ret;
 
 	ret = ufsf_read_geo_desc(ufsf, selector);
 	if (ret)
-		return;
+		return ret;
 
 	seq_scan_lu(lun) {
 		ret = ufsf_read_unit_desc(ufsf, lun, selector);
@@ -330,7 +330,7 @@ void ufsf_device_check(struct ufs_hba *hba)
 			goto out_free_mem;
 	}
 
-	return;
+	return 0;
 out_free_mem:
 #if defined(CONFIG_UFSHPB)
     if (IS_RAM_SIZE_GREATER_THAN_4G(ram_size)) {
@@ -349,7 +349,7 @@ out_free_mem:
 	ufsf->tw_dev_info.tw_device = false;
 	atomic_set(&ufsf->tw_state, TW_NOT_SUPPORTED);
 #endif
-	return;
+	return 0;
 }
 
 static void ufsf_print_query_buf(unsigned char *field, int size)
