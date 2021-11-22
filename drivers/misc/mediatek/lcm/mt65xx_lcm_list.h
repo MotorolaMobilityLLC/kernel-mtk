42cbec9954cb45ef8aafe6e28b146cf8251030e1
hujing3 <hujing3@lenovo.com>
2021-11-22 14:02:11 +0800

    lcm:Ellis:4th djn panel display driver bringup
    Ellis: 4th djn panel
    display driver bringup
    
    Change-Id: I84096c28bdf86f9ee2073a146127faa3849f6990
    Reviewed-on: https://gerrit.mot.com/2123258
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Shengchun Li <lisc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
@@ -12,6 +12,7 @@
 extern struct LCM_DRIVER mipi_mot_vid_txd_ili7806s_hdp_652_lcm_drv;
 extern struct LCM_DRIVER mipi_mot_vid_tm_hx83112a_hdp_652_lcm_drv;
 extern struct LCM_DRIVER mipi_mot_vid_tm_nt36672a_hdp_652_lcm_drv;
+extern struct LCM_DRIVER mipi_mot_vid_djn_nt36525b_hdp_652_lcm_drv;
 extern struct LCM_DRIVER mipi_mot_vid_boe_nt36525c_652_hdp_lcm_drv;
 extern struct LCM_DRIVER mipi_mot_vid_tm_ili9883a_652_hdp_lcm_drv;
 extern struct LCM_DRIVER mipi_mot_vid_tm_icnl9916_652_hdp_lcm_drv;
