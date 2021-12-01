a60809713c6cfa587780731790630d025d94181e
ot_liangyu.qi <ot_liangyu.qi@mediatek.com>
2021-12-01 15:05:30 +0800

    [ALPS06394026] platform:fix code Inspection err
    fix code Inspection err for clk
    
    MTK-Commit-Id: a1d2947ce39fbfd7d693d4fdb5d04176278c8f80
    
    Signed-off-by: ot_liangyu.qi <ot_liangyu.qi@mediatek.com>
    CR-Id: ALPS06394026
    Feature: [Module]Clock Management
    Change-Id: Ia13d1b0747c9a85f72417655b0ee662e30806d1f

--- drivers/clk/mediatek/clkchk.c
+++ drivers/clk/mediatek/clkchk.c
@@ -153,6 +153,10 @@ static int mtk_clk_rate_change(struct notifier_block *nb,
 {
 	struct clk_notifier_data *ndata = data;
 	struct clk_hw *hw = __clk_get_hw(ndata->clk);
+	if (!hw) {
+		pr_notice("%s: hw is NULL", __func__);
+		return NOTIFY_BAD;
+	}
 	const char *clk_name = __clk_get_name(hw->clk);
 	int vcore_opp = get_vcore_opp();
 
@@ -220,6 +224,7 @@ static void print_enabled_clks(void)
 	const char * const *cn;
 	const char * const *off_pn;
 	const char *fix_clk = "clk26m";
+	struct clk_hw *c_hw;
 
 	if (!clkchk_cfg)
 		return;
@@ -230,7 +235,7 @@ static void print_enabled_clks(void)
 	for (; *cn; cn++) {
 		int valid = 0;
 		struct clk *c = __clk_lookup(*cn);
-		struct clk_hw *c_hw = __clk_get_hw(c);
+		c_hw = __clk_get_hw(c);
 		struct clk_hw *p_hw;
 		const char *c_name;
 		const char *p_name;
