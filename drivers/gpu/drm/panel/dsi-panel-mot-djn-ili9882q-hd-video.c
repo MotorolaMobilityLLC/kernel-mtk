cfa509318407c273ff87f98eac20788bbed774eb
wangyq13 <wangyq13@lenovo.com>
2021-12-15 14:26:05 +0800

    drm/panel: add lcm index for pq access
    drm/panel:
    Add lcm index, pq service can get lcm index using ioctl.
    
    Change-Id: Ib00a805bdeee38827947dfc88b81e3adac65f7a0
    Signed-off-by: wangyq13 <wangyq13@mt.com>
    Reviewed-on: https://gerrit.mot.com/2144922
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/dsi-panel-mot-djn-ili9882q-hd-video.c
+++ drivers/gpu/drm/panel/dsi-panel-mot-djn-ili9882q-hd-video.c
@@ -575,6 +575,7 @@ static struct mtk_panel_params ext_params = {
 		.switch_en = 1,
 		.vact_timing_fps = 60,
 	},
+	.lcm_index = 2,
 	.hbm_type = HBM_MODE_DCS_GPIO,
 	.max_bl_level = 2047,
 };
@@ -593,6 +594,7 @@ static struct mtk_panel_params ext_params_90hz = {
 		.switch_en = 1,
 		.vact_timing_fps = 90,
 	},
+	.lcm_index = 2,
 	.hbm_type = HBM_MODE_DCS_GPIO,
 	.max_bl_level = 2047,
 };
