1d28cd9814d6a68a37a01db62027b1f1996177f4
buyan1 <buyan1@motorola.com>
2021-12-15 20:29:43 +0800

    tesla: gc02m1b bringup
    details:
    Drive porting in kernel-5.10(kernel)
    
    Change-Id: Ib4a1812f7f17342d180c79673eaabc70a641ee91
    Signed-off-by: buyan1 <buyan1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2145407
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: <wangzl37@motorola.com>
    Reviewed-by: <guozy16@motorola.com>
    Reviewed-by: Shenhuai Huang <huangsh4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/inc/kd_imgsensor.h
+++ drivers/misc/mediatek/imgsensor/inc/kd_imgsensor.h
@@ -387,6 +387,7 @@
 #define OV16A10_SENSOR_ID                       0x561641
 #define GC02M1B_SENSOR_ID                       0x02e0
 #define SC800CS_LY_SENSOR_ID                    0xd126
+#define MOT_DUBAI_GC02M1B_SENSOR_ID             0x02E0
 
 /* CAMERA DRIVER NAME */
 #define CAMERA_HW_DEVNAME                       "kd_camera_hw"
@@ -637,6 +638,9 @@
 #define SENSOR_DRVNAME_OV13B10MAIN_MIPI_RAW         "ov13b10main_mipi_raw"
 #define SENSOR_DRVNAME_OV02B10_MIPI_RAW         "ov02b10_mipi_raw"
 
+//add austin sensor drvname begin
+#define SENSOR_DRVNAME_MOT_DUBAI_GC02M1B_MIPI_RAW   "mot_dubai_gc02m1b_mipi_raw"
+//add austin sensor drvname end
 /******************************************************************************
  *
  ******************************************************************************/
