33b07e0a8a674ad2759c2a0e042665610e2579ff
guozy16 <guozy16@motorola.com>
2021-12-17 10:00:05 +0800

    tesla: bring up wide camera hi1336
    details:
    bringup wide camera hi1336
    
    Change-Id: Ib5d349893b0251e74c66b30d623a42d8731ac844
    Signed-off-by: guozy16 <guozy16@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2147464
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/misc/mediatek/imgsensor/src-v4l2/common/mot_dubai_hi1336_mipi_raw/subdrv.mk
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2020 MediaTek Inc.
+
+imgsensor-objs += \
+	$(subdrv-rpath)/mot_dubai_hi1336mipiraw_Sensor.o \
+	$(subdrv-rpath)/mot_dubai_hi1336_eeprom.o
+
