435d474e4fa458041ead7c1a6e2a4a94cf05e5a1
houdz1 <houdz1@lenovo.com>
2021-12-21 17:07:29 +0800

    drm/mediatek: support for getting DYN_MIPI_CHANGE from dts
    Mtk define "helper-value" at mt6879.dts. This is a dts base,
    it can not be overlay by projects.
    We add a label "force_dyn_mipi_change"  to enable DYN_MIPI_CHANGE.
    
    Change-Id: I36bbac56b852bbe027a324bb916619ea28b443aa
    Signed-off-by: houdz1 <houdz1@mt.com>
    Reviewed-on: https://gerrit.mot.com/2150756
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_helper.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_helper.c
@@ -204,6 +204,9 @@ void mtk_drm_helper_init(struct device *dev, struct mtk_drm_helper **helper_opt)
 		mtk_drm_helper_set_opt_by_name(tmp_opt,
 				"MTK_DRM_OPT_PRIM_DUAL_PIPE", 0);
 
+	if (of_property_read_bool(dev->of_node, "force_dyn_mipi_change"))
+		mtk_drm_helper_set_opt_by_name(tmp_opt,
+				"MTK_DRM_OPT_DYN_MIPI_CHANGE", 1);
 
 	*helper_opt = tmp_opt;
 }
