d87563e44572f3a2d6c156f559bbbb50fc7146cc
duxu1 <duxu1@lenovo.com>
2021-12-21 18:06:30 +0800

    Increase shutdown hang timeout
    The hang timeout is changed by [ALPS06353839],
    increase the timeout to 270 seconds
    to avoid trigger hang crashdump in StsHostTestCases.
    
    Change-Id: I56a8f66affc8e059f4d1ab2c662979f2e27cd59e
    Signed-off-by: duxu1 <duxu1@mt.com>
    Reviewed-on: https://gerrit.mot.com/2150819
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Lihui Chen <chenlh4@mt.com>

--- drivers/misc/mediatek/aee/aed/monitor_hang.c
+++ drivers/misc/mediatek/aee/aed/monitor_hang.c
@@ -378,8 +378,8 @@ static long monitor_hang_ioctl(struct file *file, unsigned int cmd,
 		(!strncmp(current->comm, "init", 4))) {
 		reboot_flag = true;
 		hd_detect_enabled = 1;
-		hang_detect_counter = 5;
-		hd_timeout = 5;
+		hang_detect_counter = 9;
+		hd_timeout = 9;
 		pr_info("hang_detect: %s set reboot command.\n", current->comm);
 		return ret;
 	}
