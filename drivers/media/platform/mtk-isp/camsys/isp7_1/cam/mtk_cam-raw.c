69f09f86c0af800c7b3f7e7921532c139104a474
Roy Yang <roy.yang@mediatek.com>
2022-01-11 19:29:56 +0800

    [ALPS06319367] media: cam: fix qos bw issue
    1. fix qos bw coding issue
    2. fix fbc_cnt mask
    3. add smi dump after reset timeout
    
    MTK-Commit-Id: 1a528c37d8eafd8a2df2a2433e3cdeb966556192
    
    CR-Id: ALPS06319367
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: Ib72052a67467f0f8c7af81b2ebd882870247eb1b
    Signed-off-by: Roy Yang <roy.yang@mediatek.com>

--- drivers/media/platform/mtk-isp/camsys/isp7_1/cam/mtk_cam-raw.c
+++ drivers/media/platform/mtk-isp/camsys/isp7_1/cam/mtk_cam-raw.c
@@ -1239,6 +1239,8 @@ void reset(struct mtk_raw_device *dev)
 			 readl(dev->base + REG_DMA_SOFT_RST_STAT2),
 			 readl(dev->yuv_base + REG_DMA_SOFT_RST_STAT));
 
+		mtk_smi_dbg_hang_detect("camsys");
+
 		goto RESET_FAILURE;
 	}
 
@@ -2220,7 +2222,6 @@ static irqreturn_t mtk_irq_raw(int irq, void *data)
 		raw_dev->sof_count++;
 		irq_info.write_cnt = ((fbc_fho_ctl2 & WCNT_BIT_MASK) >> 8) - 1;
 		irq_info.fbc_cnt = (fbc_fho_ctl2 & CNT_BIT_MASK) >> 16;
-
 	}
 	/* Vsync interrupt */
 	if (irq_status & VS_INT_ST)
