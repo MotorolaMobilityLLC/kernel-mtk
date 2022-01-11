297e6d63cbc58e6f8dcf65e96c6b252284ca5e33
Vend.Gene.Chen <v_genech@mediatek.com>
2022-01-11 19:32:12 +0800

    [ALPS06339646] iio: adc: fix mt6375 auxadc lockdep
    fix mt6375 auxadc lockdep
    
    MTK-Commit-Id: 2e9c48add3dea0a212e8afe5eabbbfdc20009c95
    
    Signed-off-by: Vend.Gene.Chen <v_genech@mediatek.com>
    CR-Id: ALPS06339646
    Feature: Charger
    Change-Id: I2612a0999b48624f80e838ab19b2895afa0a3380

--- drivers/iio/adc/mt6375-auxadc.c
+++ drivers/iio/adc/mt6375-auxadc.c
@@ -82,6 +82,7 @@ struct mt6375_priv {
 	struct work_struct vbat0_work;
 	atomic_t vbat0_flag;
 	struct wakeup_source *vbat0_ws;
+	struct lock_class_key info_exist_key;
 };
 
 #define VBAT0_POLL_TIME_SEC	5
@@ -874,6 +875,8 @@ static int mt6375_auxadc_probe(struct platform_device *pdev)
 	device_init_wakeup(&pdev->dev, true);
 	platform_set_drvdata(pdev, priv);
 	priv->vbat0_ws = wakeup_source_register(&pdev->dev, "vbat0_ws");
+	lockdep_register_key(&priv->info_exist_key);
+	lockdep_set_class(&indio_dev->info_exist_lock, &priv->info_exist_key);
 
 	ret = mt6375_auxadc_parse_dt(priv);
 	if (ret) {
@@ -929,6 +932,7 @@ static int mt6375_auxadc_remove(struct platform_device *pdev)
 {
 	struct mt6375_priv *priv = platform_get_drvdata(pdev);
 
+	lockdep_unregister_key(&priv->info_exist_key);
 	auxadc_del_irq_chip(priv);
 	return 0;
 }
