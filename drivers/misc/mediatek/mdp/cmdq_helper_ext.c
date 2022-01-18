5c003143971b253ab67853fe9abdadb5b737ea8e
LiaoHui <ot_hui.liao@mediatek.com>
2022-01-18 14:48:25 +0800

    [ALPS06523887] MDP:Prevent numerical miscalculation
    Add the condition judgment of perf log.
    Prevent miscalculation of time value.
    
    MTK-Commit-Id: 64a01e3a05ff05529db59e88117171ba8c522239
    
    Signed-off-by: LiaoHui <ot_hui.liao@mediatek.com>
    CR-Id: ALPS06523887
    Feature: mdp driver
    Change-Id: I3ec9e1977b315d851877269977785cb913358505
    (cherry picked from commit 215d80bd1cc7d8e52f47cd8ca3de89cdfcbd91d5)

--- drivers/misc/mediatek/mdp/cmdq_helper_ext.c
+++ drivers/misc/mediatek/mdp/cmdq_helper_ext.c
@@ -4492,7 +4492,7 @@ s32 cmdq_pkt_wait_flush_ex_result(struct cmdqRecStruct *handle)
 
 	status = cmdq_pkt_wait_complete(handle->pkt);
 
-	if (handle->profile_exec) {
+	if (handle->profile_exec && cmdq_util_is_feature_en(CMDQ_LOG_FEAT_PERF)) {
 		u32 *va = cmdq_pkt_get_perf_ret(handle->pkt);
 
 		if (va) {
