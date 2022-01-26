71a3aef75423fa13695f672d9b54756907b468f7
chailu1 <chailu1@lenovo.com>
2022-01-26 18:20:48 +0800

    mux: add wls and usbc mux policy
    add wls charge/wls rev charge/usb charge/usb otg
    wls factory test mux policy
    
    Change-Id: I9447b9f161d6eafbb413ee500ecb34b8ac4d106f
    Signed-off-by: chailu1 <chailu1@mt.com>
    Reviewed-on: https://gerrit.mot.com/2179487
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jiacheng Ou <oujc1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/extcon/extcon-mtk-usb.c
+++ drivers/misc/mediatek/extcon/extcon-mtk-usb.c
@@ -33,6 +33,28 @@ static const unsigned int usb_extcon_cable[] = {
 	EXTCON_NONE,
 };
 
+static int mmi_mux_typec_otg_chan(enum mmi_mux_channel channel, bool on)
+{
+	struct mtk_charger *info = NULL;
+	struct power_supply *chg_psy = NULL;
+
+	chg_psy = power_supply_get_by_name("mtk-master-charger");
+	if (chg_psy == NULL || IS_ERR(chg_psy)) {
+		pr_err("%s Couldn't get chg_psy\n");
+		return 0;
+	} else {
+		info = (struct mtk_charger *)power_supply_get_drvdata(chg_psy);
+	}
+
+	pr_info("%s open typec OTG chan =%d, on = %d\n", __func__, channel, on);
+	if (info->algo.do_mux)
+		info->algo.do_mux(info, channel, on);
+	else
+		pr_err("%s get info->algo.do_mux fail", __func__);
+
+	return 0;
+}
+
 static void mtk_usb_extcon_update_role(struct work_struct *work)
 {
 	struct usb_role_info *role = container_of(to_delayed_work(work),
@@ -204,6 +226,7 @@ static int mtk_usb_extcon_set_vbus(struct mtk_extcon_info *extcon,
 	dev_info(dev, "vbus turn %s\n", is_on ? "on" : "off");
 
 	if (is_on) {
+		mmi_mux_typec_otg_chan(MMI_MUX_CHANNEL_TYPEC_OTG, true);
 		if (extcon->vbus_vol) {
 			ret = regulator_set_voltage(vbus,
 					extcon->vbus_vol, extcon->vbus_vol);
@@ -229,31 +252,10 @@ static int mtk_usb_extcon_set_vbus(struct mtk_extcon_info *extcon,
 		}
 	} else {
 		regulator_disable(vbus);
+		mmi_mux_typec_otg_chan(MMI_MUX_CHANNEL_TYPEC_OTG, false);
 	}
 
 	extcon->vbus_on = is_on;
-
-	return 0;
-}
-
-static int mmi_mux_typec_otg_chan(enum mmi_mux_channel channel, bool on)
-{
-	struct mtk_charger *info = NULL;
-	struct power_supply *chg_psy = NULL;
-
-	chg_psy = power_supply_get_by_name("mtk-master-charger");
-	if (chg_psy == NULL || IS_ERR(chg_psy)) {
-		pr_err("%s Couldn't get chg_psy\n");
-		return 0;
-	} else {
-		info = (struct mtk_charger *)power_supply_get_drvdata(chg_psy);
-	}
-
-	pr_info("%s open typec OTG chan =%d, on = %d\n", __func__, channel, on);
-	if (info->algo.do_mux)
-		info->algo.do_mux(info, channel, on);
-	else
-		pr_err("%s get info->algo.do_mux fail", __func__);
 
 	return 0;
 }
@@ -273,8 +275,6 @@ static int mtk_extcon_tcpc_notifier(struct notifier_block *nb,
 		dev_info(dev, "source vbus = %dmv\n",
 				 noti->vbus_state.mv);
 		vbus_on = (noti->vbus_state.mv) ? true : false;
-		if (vbus_on)
-			mmi_mux_typec_otg_chan(MMI_MUX_CHANNEL_TYPEC_OTG, true);
 		mtk_usb_extcon_set_vbus(extcon, vbus_on);
 		break;
 	case TCP_NOTIFY_TYPEC_STATE:
@@ -293,18 +293,14 @@ static int mtk_extcon_tcpc_notifier(struct notifier_block *nb,
 			noti->typec_state.new_state == TYPEC_ATTACHED_DBGACC_SNK)) {
 			dev_info(dev, "Type-C SINK plug in\n");
 			mtk_usb_extcon_set_role(extcon, USB_ROLE_DEVICE);
-		} else if ((noti->typec_state.old_state == TYPEC_ATTACHED_SNK ||
-			noti->typec_state.old_state == TYPEC_ATTACHED_NORP_SRC ||
-			noti->typec_state.old_state == TYPEC_ATTACHED_CUSTOM_SRC) &&
-			noti->typec_state.new_state == TYPEC_UNATTACHED) {
-			dev_info(dev, "Type-C SINK plug out\n");
-			mtk_usb_extcon_set_role(extcon, USB_ROLE_NONE);
 		} else if ((noti->typec_state.old_state == TYPEC_ATTACHED_SRC ||
+			noti->typec_state.old_state == TYPEC_ATTACHED_SNK ||
+			noti->typec_state.old_state == TYPEC_ATTACHED_NORP_SRC ||
+			noti->typec_state.old_state == TYPEC_ATTACHED_CUSTOM_SRC ||
 			noti->typec_state.old_state == TYPEC_ATTACHED_DBGACC_SNK) &&
 			noti->typec_state.new_state == TYPEC_UNATTACHED) {
-			dev_info(dev, "Type-C OTG/SRC plug out\n");
+			dev_info(dev, "Type-C plug out\n");
 			mtk_usb_extcon_set_role(extcon, USB_ROLE_NONE);
-			mmi_mux_typec_otg_chan(MMI_MUX_CHANNEL_TYPEC_OTG, false);
 		}
 		break;
 	case TCP_NOTIFY_DR_SWAP:
