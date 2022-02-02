4d393a47c44aba2b5e0fe9e499430378ae670b7f
chailu1 <chailu1@lenovo.com>
2022-02-02 14:58:36 +0800

    charge: disable kernel battery temperature 60C poweroff
    battery temperture 68C poweroff,This is finish by framework
    So disable kernel battery 60C poweroff or reset behavior.
    
    porting from https://gerrit.mot.com/1349477
    
    Change-Id: I543441443caa2c8ec9360871cefa60acbb72263d
    Signed-off-by: chailu1 <chailu1@mt.com>
    Reviewed-on: https://gerrit.mot.com/2183049
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jiacheng Ou <oujc1@motorola.com>
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/mtk_battery_table.h
+++ drivers/power/supply/mtk_battery_table.h
@@ -463,7 +463,7 @@ int g_temperature[MAX_TABLE] = {
 #define BIF_NTC_R 16000
 
 #if (BAT_NTC_10 == 1)
-struct fg_temp fg_temp_table[21] = {
+struct fg_temp fg_temp_table[23] = {
 		{-40, 195652},
 		{-35, 148171},
 		{-30, 113347},
@@ -484,7 +484,9 @@ struct fg_temp fg_temp_table[21] = {
 		{45, 4917},
 		{50, 4161},
 		{55, 3535},
-		{60, 3014}
+		{60, 3014},
+		{65, 2588},
+		{70, 2227}
 };
 #endif
 
