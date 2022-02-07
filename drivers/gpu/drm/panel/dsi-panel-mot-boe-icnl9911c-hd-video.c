063163ea8b40a9d78a1ceee2d7c983f107f4dbe8
wangyq13 <wangyq13@lenovo.com>
2022-02-07 14:48:37 +0800

    drm/panel: disable dimming before backlight is off
    drm/panel:
    Disable dimming before backlight is off, to make backlight is
    off quickly.
    
    Change-Id: Iafdf06b73b062cfa93810c2e0d76162100663844
    Signed-off-by: wangyq13 <wangyq13@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2184597
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/dsi-panel-mot-boe-icnl9911c-hd-video.c
+++ drivers/gpu/drm/panel/dsi-panel-mot-boe-icnl9911c-hd-video.c
@@ -540,6 +540,7 @@ static int lcm_setbacklight_cmdq(void *dsi, dcs_write_gce cb,
 	void *handle, unsigned int level)
 {
 	char bl_tb0[] = {0x51, 0xFF, 0x0E};
+	char dimming_tb[] = {0x53, 0x24};
 
 	pr_debug("icnl9911c level : %d\n", level);
 
@@ -548,6 +549,9 @@ static int lcm_setbacklight_cmdq(void *dsi, dcs_write_gce cb,
 
 	if (!cb)
 		return -1;
+
+	if (!level)
+		cb(dsi, handle, dimming_tb, ARRAY_SIZE(dimming_tb));
 
 	cb(dsi, handle, bl_tb0, ARRAY_SIZE(bl_tb0));
 
