d0dd2a83702982a9b5381b78f5709f54499c379c
huangsh4 <huangsh4@lenovo.com>
2022-02-09 16:45:40 +0800

    tesla: set to tesla_ois_fw_download
    camera:
    enable ois dw9871 fw download.
    
    Change-Id: I9012ad9e7a9b188c54047cfe608391072404ec36
    Signed-off-by: huangsh4 <huangsh4@mt.com>
    Reviewed-on: https://gerrit.mot.com/2168115
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Darong Huang <huangdra@motorola.com>
    Reviewed-by: Shenhuai Huang <huangsh4@motorola.com>
    Reviewed-by: <wangzl37@motorola.com>
    Reviewed-by: Xu Ji <jixu@motorola.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: <guozy16@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/misc/mediatek/lens/vcm/v4l2/mot_dubai_ak7377a/dw9781.h
@@ -0,0 +1,27 @@
+#ifndef __DW9781_H__
+#define __DW9781_H__
+
+#include "ois_ext_cmd.h"
+
+#define EOK 0
+
+/* gyro offset calibration */
+#define GYRO_OFFSET_CAL_OK 0x00
+#define GYRO_CAL_TIME_OVER 0xFF
+#define X_GYRO_OFFSET_SPEC_OVER_NG 0x0001
+#define X_GYRO_RAW_DATA_CHECK 0x0010
+#define Y_GYRO_OFFSET_SPEC_OVER_NG 0x0002
+#define Y_GYRO_RAW_DATA_CHECK 0x0020
+#define GYRO_OFST_CAL_OVERCNT 50
+
+int gyro_offset_calibrtion(void);
+motOISGOffsetResult *dw9781_get_gyro_offset_result(void);
+void calibration_save(void);
+void check_calibration_data(void);
+int gyro_offset_check_update(void);
+int square_motion_test (int radius, int accuracy, int deg_step, int w_time0,
+                               int w_time1, int w_time2, int ref_stroke, motOISExtData *pResult);
+void ois_reset(void);
+int ois_checksum(void);
+int dw9781c_download_ois_fw(void);
+#endif
---
+++ drivers/misc/mediatek/lens/vcm/v4l2/mot_dubai_ak7377a/dw9781_i2c.h
@@ -0,0 +1,13 @@
+#ifndef __DW9781_I2C_H__
+#define __DW9781_I2C_H__
+#include <linux/i2c.h>
+#include <linux/delay.h>
+#include <linux/uaccess.h>
+
+void dw9781_set_i2c_client(struct i2c_client *i2c_client);
+int write_reg_16bit_value_16bit(unsigned short regAddr, unsigned short regData);
+int read_reg_16bit_value_16bit(unsigned short regAddr, unsigned short *regData);
+void i2c_block_write_reg(unsigned short addr,unsigned short *fwContentPtr,int size);
+void i2c_block_read_reg(unsigned short addr,unsigned short *temp,int size);
+#define DATPKT_SIZE 256
+#endif
