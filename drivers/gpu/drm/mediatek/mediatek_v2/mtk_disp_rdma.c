bd4f34ef233c4989e3d5669dd87a5cddacb65b59
Roger Jeng <roger.jeng@mediatek.com>
2022-02-18 15:16:24 +0800

    [ALPS06531179] DISP: set disp RDMA 10bit
    [Detail]
    set disp RDMA 10bit always
    dither set 8bit/10bit setting by panel
    
    MTK-Commit-Id: a3ca3b7c47fa958af61fcad4b763818892ed2ad2
    
    Signed-off-by: Roger Jeng <roger.jeng@mediatek.com>
    Feature: display_mobile
    Change-Id: Iea40c874a61569f911667bd513c09d790edb8121
    CR-Id: ALPS06531179

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_rdma.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_rdma.c
@@ -45,6 +45,7 @@ int disp_met_set(void *data, u64 val);
 #define RDMA_ENGINE_EN BIT(0)
 #define RDMA_SOFT_RESET BIT(4)
 #define RDMA_MODE_MEMORY BIT(1)
+#define RDMA_RG_PIXEL_10_BIT BIT(3)
 #define DISP_REG_RDMA_SIZE_CON_0 0x0014
 #define RDMA_MATRIX_ENABLE BIT(17)
 #define RDMA_MATRIX_INT_MTX_SEL (7UL << 20)
@@ -174,6 +175,7 @@ int disp_met_set(void *data, u64 val);
 
 #define GLOBAL_CON_FLD_ENGINE_EN REG_FLD_MSB_LSB(0, 0)
 #define GLOBAL_CON_FLD_MODE_SEL REG_FLD_MSB_LSB(1, 1)
+#define GLOBAL_CON_FLD_PIXEL_10_BIT REG_FLD_MSB_LSB(3, 3)
 #define GLOBAL_CON_FLD_SMI_BUSY REG_FLD_MSB_LSB(12, 12)
 #define RDMA_BG_CON_0_LEFT REG_FLD_MSB_LSB(12, 0)
 #define RDMA_BG_CON_0_RIGHT REG_FLD_MSB_LSB(28, 16)
@@ -888,6 +890,12 @@ static void mtk_rdma_config(struct mtk_ddp_comp *comp,
 		mtk_rdma_write_mem_start_addr_cmdq(comp, 0, handle);
 	}
 
+	/* always set disp RDMA 10bit, no by panel(8bit/10bit) */
+	/* dither setting will set by panel */
+	mtk_ddp_write_mask(comp, RDMA_RG_PIXEL_10_BIT,
+		DISP_REG_RDMA_GLOBAL_CON, RDMA_RG_PIXEL_10_BIT,
+		handle);
+
 #ifdef IF_ZERO
 	/*
 	 * Enable FIFO underflow since DSI and DPI can't be blocked.
@@ -1265,12 +1273,13 @@ int mtk_rdma_analysis(struct mtk_ddp_comp *comp)
 	unsigned int fifo = readl(baddr + DISP_REG_RDMA_FIFO_CON);
 
 	global_ctrl = readl(DISP_REG_RDMA_GLOBAL_CON + baddr);
-	DDPDUMP("== %s ANALYSIS ==\n", mtk_dump_comp_str(comp));
-	DDPDUMP("en=%d,mode:%s,smi_busy:%d\n",
+	DDPDUMP("== %s ANALYSIS:0x%x ==\n", mtk_dump_comp_str(comp), comp->regs_pa);
+	DDPDUMP("en=%d,mode:%s,smi_busy:%d,10bit:%d\n",
 		REG_FLD_VAL_GET(GLOBAL_CON_FLD_ENGINE_EN, global_ctrl),
 		REG_FLD_VAL_GET(GLOBAL_CON_FLD_MODE_SEL, global_ctrl)
 				? "mem" : "DL",
-		REG_FLD_VAL_GET(GLOBAL_CON_FLD_SMI_BUSY, global_ctrl));
+		REG_FLD_VAL_GET(GLOBAL_CON_FLD_SMI_BUSY, global_ctrl),
+		REG_FLD_VAL_GET(GLOBAL_CON_FLD_PIXEL_10_BIT, global_ctrl));
 
 	DDPDUMP("wh(%dx%d),pitch=%d,addr=0x%x\n",
 		readl(DISP_REG_RDMA_SIZE_CON_0 + baddr) & 0xfff,
