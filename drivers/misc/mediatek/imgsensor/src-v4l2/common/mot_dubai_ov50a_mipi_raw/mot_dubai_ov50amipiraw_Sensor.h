78edac132754faebec9f7bd9c47144f257895347
wangzl37 <wangzl37@motorola.com>
2022-03-01 17:50:57 +0800

    tesla: update OV50A setting
    tesla:
    --OV50A add 1920x1080@60fps setting
    --OV50A add 1920x1080@120fps setting
    --OV50A add 1280x720@240fps setting
    --OV32C update test parttern setting
    
    Change-Id: I73a298d768a06098e405d5a5c9a2858acd468a15
    Signed-off-by: wangzl37 <wangzl37@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2204317
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Darong Huang <huangdra@motorola.com>
    Reviewed-by: <buyan1@motorola.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src-v4l2/common/mot_dubai_ov50a_mipi_raw/mot_dubai_ov50amipiraw_Sensor.h
+++ drivers/misc/mediatek/imgsensor/src-v4l2/common/mot_dubai_ov50a_mipi_raw/mot_dubai_ov50amipiraw_Sensor.h
@@ -37,6 +37,7 @@ enum IMGSENSOR_MODE {
 	IMGSENSOR_MODE_SLIM_VIDEO,
 	IMGSENSOR_MODE_CUSTOM1,
 	IMGSENSOR_MODE_CUSTOM2,
+	IMGSENSOR_MODE_CUSTOM3,
 };
 
 struct imgsensor_mode_struct {
@@ -99,6 +100,7 @@ struct imgsensor_info_struct {
 	struct imgsensor_mode_struct slim_video;
 	struct imgsensor_mode_struct custom1; //custom1 scenario information
 	struct imgsensor_mode_struct custom2; //custom2 scenario information
+	struct imgsensor_mode_struct custom3; //custom2 scenario information
 
 	kal_uint8  ae_shut_delay_frame;	//shutter delay frame for AE cycle
 	//sensor gain delay frame for AE cycle
@@ -115,6 +117,7 @@ struct imgsensor_info_struct {
 	kal_uint8  slim_video_delay_frame; //enter svideo delay frame num
 	kal_uint8  custom1_delay_frame;     //enter custom1 delay frame num
 	kal_uint8  custom2_delay_frame;     //enter custom2 delay frame num
+	kal_uint8  custom3_delay_frame;     //enter custom2 delay frame num
 	kal_uint8  frame_time_delay_frame;
 
 	kal_uint8  margin; //sensor framelength & shutter margin
