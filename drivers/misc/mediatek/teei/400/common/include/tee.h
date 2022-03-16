4a5c892cf7d3f9c13ff83501c357b50342ef105c
huifen wang <huifen.wang@mediatek.com>
2022-03-16 08:45:35 +0800

    [ALPS06641447] TEEI: fix sec vulnerability
    Fix security vulnerability in tee driver H# 1470761  and H#1469239
    
    MTK-Commit-Id: e96b9fde136f865def1065acb52958b692aedf40
    
    Signed-off-by: huifen wang <huifen.wang@mediatek.com>
    CR-Id: ALPS06641447
    Feature: Microtrust TEEI
    Change-Id: Ic2f937415a73a469208d1ad9e9e0d74cd7244444
    (cherry-pick from 061dfb9fac327a1b066f1eccf89347e74f4fdaaf)

--- drivers/misc/mediatek/teei/400/common/include/tee.h
+++ drivers/misc/mediatek/teei/400/common/include/tee.h
@@ -1,32 +1,16 @@
 /*
  * Copyright (c) 2015-2016, Linaro Limited
- * All rights reserved.
  * Copyright (c) 2015-2019, MICROTRUST Incorporated
- * All rights reserved.
- * Copyright (c) 2017-2018, GNU General Public License
- * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
  *
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #ifndef __TEE_H
@@ -478,4 +462,9 @@ struct tee_ioctl_capi_proxy_arg {
  * munmap(): unmaps previously shared memory
  */
 
+#define TEE_IOC_SHM_RELEASE _IOR(TEE_IOC_MAGIC, TEE_IOC_BASE + 12, \
+					struct tee_ioctl_shm_alloc_data)
+
+#define TEE_IOC_SHM_ID _IOR(TEE_IOC_MAGIC, TEE_IOC_BASE + 13, \
+					struct tee_ioctl_shm_alloc_data)
 #endif /*__TEE_H*/
