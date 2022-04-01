38a7b36f17b2048d8ebe5e6b7decb2a1816c7d82
hansx3 <hansx3@Lenovo.com>
2022-04-01 17:01:07 +0800

    maui: gc02m1 bring up [1]
    details:
    add sensor config
    copy from tonga
    
    Change-Id: I01ba3149f15b8f063ccfde16a83156076025eb8f
    Signed-off-by: hansx3 <hansx3@mt.com>
    Reviewed-on: https://gerrit.mot.com/2231987
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: XiaoFeng Wang <wangxf32@mt.com>
    Reviewed-by: <liuzy55@motorola.com>
    Tested-by: Jira Key
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/inc/kd_imgsensor.h
+++ drivers/misc/mediatek/imgsensor/inc/kd_imgsensor.h
@@ -147,6 +147,7 @@
 //add maui sensor id begin
 #define MOT_MAUI_HI1634B_SENSOR_ID 0x1634
 #define MOT_MAUI_S5K5E9_SENSOR_ID  0x559b
+#define MOT_MAUI_GC02M1_SENSOR_ID 0x02e0
 //add maui sensor id end
 /*IMX*/
 #define IMX766_SENSOR_ID                        0x0766
@@ -638,6 +639,7 @@
 //add maui sensor drvname begin
 #define SENSOR_DRVNAME_MOT_MAUI_HI1634B_MIPI_RAW "mot_maui_hi1634b_mipi_raw"
 #define SENSOR_DRVNAME_MOT_MAUI_S5K5E9_MIPI_RAW  "mot_maui_s5k5e9_mipi_raw"
+#define SENSOR_DRVNAME_MOT_MAUI_GC02M1_MIPI_RAW "mot_maui_gc02m1_mipi_raw"
 //add maui sensor drvname end
 /******************************************************************************
  *
