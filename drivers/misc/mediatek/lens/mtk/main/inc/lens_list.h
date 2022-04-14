928fcac6a85e1ca864472d2c6accf26f4394fce1
liuzy55 <liuzy55@motorola.com>
2022-04-14 15:11:09 +0800

    maui: AF GT9772 bringup[1]
    details:
    add AF gt9772 and configure
    
    Change-Id: Ice9cc63ff859ac80d29948de7172906d6ef68db5
    Signed-off-by: liuzy55 <liuzy55@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2242269
    Reviewed-by: Shenhuai Huang <huangsh4@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: XiaoFeng Wang <wangxf32@lenovo.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Tested-by: Jira Key
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lens/mtk/main/inc/lens_list.h
+++ drivers/misc/mediatek/lens/mtk/main/inc/lens_list.h
@@ -244,6 +244,20 @@ extern int GT9764AF_PowerDown(struct i2c_client *pstAF_I2Cclient,
 				int *pAF_Opened);
 extern int GT9764AF_GetFileName(unsigned char *pFileName);
 
+#define GT9772AF_SetI2Cclient GT9772AF_SetI2Cclient_Main
+#define GT9772AF_Ioctl GT9772AF_Ioctl_Main
+#define GT9772AF_Release GT9772AF_Release_Main
+#define GT9772AF_PowerDown GT9772AF_PowerDown_Main
+#define GT9772AF_GetFileName GT9772AF_GetFileName_Main
+extern int GT9772AF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+				spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long GT9772AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+				unsigned long a_u4Param);
+extern int GT9772AF_Release(struct inode *a_pstInode, struct file *a_pstFile);
+extern int GT9772AF_PowerDown(struct i2c_client *pstAF_I2Cclient,
+				int *pAF_Opened);
+extern int GT9772AF_GetFileName(unsigned char *pFileName);
+
 #define LC898122AF_SetI2Cclient LC898122AF_SetI2Cclient_Main
 #define LC898122AF_Ioctl LC898122AF_Ioctl_Main
 #define LC898122AF_Release LC898122AF_Release_Main
