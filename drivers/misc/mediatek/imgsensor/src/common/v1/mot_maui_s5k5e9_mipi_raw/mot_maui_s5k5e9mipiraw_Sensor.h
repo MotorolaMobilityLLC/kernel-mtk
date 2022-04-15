6468af0567345c9c6e7085df19e48ae892e48c1e
Xiaofeng Wang <wangxf32@lenovo.com>
2022-04-15 15:50:13 +0800

    maui: s5k5e9 eeprom
    details:
    add s5k5e9 eeprom GT24P64E
    
    Change-Id: Ib93f9c4dcbf3f7cebce86a8f97783694333b9ff5
    Signed-off-by: Xiaofeng Wang <wangxf32@mt.com>
    Reviewed-on: https://gerrit.mot.com/2243790
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: <hansx3@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1/mot_maui_s5k5e9_mipi_raw/mot_maui_s5k5e9mipiraw_Sensor.h
+++ drivers/misc/mediatek/imgsensor/src/common/v1/mot_maui_s5k5e9_mipi_raw/mot_maui_s5k5e9mipiraw_Sensor.h
@@ -124,6 +124,153 @@ typedef struct imgsensor_info_struct {
 
 } imgsensor_info_struct;
 
+typedef enum {
+	NO_ERRORS,
+	CRC_FAILURE,
+	LIMIT_FAILURE
+} calibration_status_t;
+
+#define AWB_R_MIN 200
+#define AWB_R_MAX 880
+#define AWB_GR_MIN 760
+#define AWB_GR_MAX 880
+#define AWB_GB_MIN 760
+#define AWB_GB_MAX 880
+#define AWB_B_MIN 200
+#define AWB_B_MAX 880
+
+struct mot_s5k5e9_eeprom_t {
+	uint8_t eeprom_table_version[1];
+	uint8_t cal_hw_ver[1];
+	uint8_t cal_sw_ver[1];
+	uint8_t mpn[8];
+	uint8_t actuator_id[1];
+	uint8_t lens_id[1];
+	uint8_t manufacturer_id[2];
+	uint8_t factory_id[2];
+	uint8_t manufacture_line[1];
+	uint8_t manufacture_date[3];
+	uint8_t serial_number[16];
+	uint8_t manufacture_crc16[2];
+
+	uint8_t af_reserve[26];
+	uint8_t cie_ev[2];
+	uint8_t cie_u[2];
+	uint8_t cie_v[2];
+	uint8_t awb_r_g_golden_min_limit[1];
+	uint8_t awb_r_g_golden_max_limit[1];
+	uint8_t awb_b_g_golden_min_limit[1];
+	uint8_t awb_b_g_golden_max_limit[1];
+	uint8_t awb_src_1_golden_r[2];
+	uint8_t awb_src_1_golden_gr[2];
+	uint8_t awb_src_1_golden_gb[2];
+	uint8_t awb_src_1_golden_b[2];
+	uint8_t awb_src_1_golden_rg_ratio[2];
+	uint8_t awb_src_1_golden_bg_ratio[2];
+	uint8_t awb_src_1_golden_gr_gb_ratio[2];
+	uint8_t awb_src_1_r[2];
+	uint8_t awb_src_1_gr[2];
+	uint8_t awb_src_1_gb[2];
+	uint8_t awb_src_1_b[2];
+	uint8_t awb_src_1_rg_ratio[2];
+	uint8_t awb_src_1_bg_ratio[2];
+	uint8_t awb_src_1_gr_gb_ratio[2];
+	uint8_t awb_reserve[5];
+	uint8_t awb_crc16[2];
+
+	uint8_t optical_center_x_1_r_channel[2];
+	uint8_t optical_center_y_1_r_channel[2];
+	uint8_t optical_center_x_1_gr_channel[2];
+	uint8_t optical_center_y_1_gr_channel[2];
+	uint8_t optical_center_x_1_gb_channel[2];
+	uint8_t optical_center_y_1_gb_channel[2];
+	uint8_t optical_center_x_1_b_channel[2];
+	uint8_t optical_center_y_1_b_channel[2];
+	uint8_t optical_center_crc16[2];
+
+	uint8_t sfr_distance1_reserve[3];
+	uint8_t sfr_ctr_distance1[2];
+	uint8_t sfr_field1_upper_left_distance1[2];
+	uint8_t sfr_field1_upper_right_distance1[2];
+	uint8_t sfr_field1_lower_right_distance1[2];
+	uint8_t sfr_field1_lower_left_distance1[2];
+	uint8_t sfr_field2_upper_left_distance1[2];
+	uint8_t sfr_field2_upper_right_distance1[2];
+	uint8_t sfr_field2_lower_right_distance1[2];
+	uint8_t sfr_field2_lower_left_distance1[2];
+	uint8_t sfr_field3_upper_left_distance1[2];
+	uint8_t sfr_field3_upper_right_distance1[2];
+	uint8_t sfr_field3_lower_right_distance1[2];
+	uint8_t sfr_field3_lower_left_distance1[2];
+	uint8_t sfr_field__distance1_reserve[8];
+	uint8_t sfr_field2_corner_to_corner_variation_distance1[2];
+	uint8_t sfr_distance2_reserve[3];
+	uint8_t sfr_ctr_distance2[2];
+	uint8_t sfr_field1_upper_left_distance2[2];
+	uint8_t sfr_field1_upper_right_distance2[2];
+	uint8_t sfr_field1_lower_right_distance2[2];
+	uint8_t sfr_field1_lower_left_distance2[2];
+	uint8_t sfr_field2_upper_left_distance2[2];
+	uint8_t sfr_field2_upper_right_distance2[2];
+	uint8_t sfr_field2_lower_right_distance2[2];
+	uint8_t sfr_field2_lower_left_distance2[2];
+	uint8_t sfr_field3_upper_left_distance2[2];
+	uint8_t sfr_field3_upper_right_distance2[2];
+	uint8_t sfr_field3_lower_right_distance2[2];
+	uint8_t sfr_field3_lower_left_distance2[2];
+	uint8_t sfr_field_distance2_reserve[8];
+	uint8_t sfr_field2_corner_to_corner_variation_distance2_reserve[2];
+	uint8_t sfr_distance3_reserve[3];
+	uint8_t sfr_ctr_distance3[2];
+	uint8_t sfr_field1_upper_left_distance3[2];
+	uint8_t sfr_field1_upper_right_distance3[2];
+	uint8_t sfr_field1_lower_right_distance3[2];
+	uint8_t sfr_field1_lower_left_distance3[2];
+	uint8_t sfr_field2_upper_left_distance3[2];
+	uint8_t sfr_field2_upper_right_distance3[2];
+	uint8_t sfr_field2_lower_right_distance3[2];
+	uint8_t sfr_field2_lower_left_distance3[2];
+	uint8_t sfr_field3_upper_left_distance3[2];
+	uint8_t sfr_field3_upper_right_distance3[2];
+	uint8_t sfr_field3_lower_right_distance3[2];
+	uint8_t sfr_field3_lower_left_distance3[2];
+	uint8_t sfr_field_distance3_reserve[8];
+	uint8_t sfr_field2_corner_to_corner_variation_distance3_reserve[2];
+	uint8_t sfr_crc16[2];
+
+	uint8_t lsc_calibration[1868];
+	uint8_t lsc_crc16[2];
+
+	uint8_t module_check_flag[1];
+	uint8_t otp_calibration_type[4];
+	uint8_t sensor_type[1];
+	uint8_t awb_af_calibration_information[1];
+	uint8_t one_table_size[2];
+	uint8_t posture_af_infinite_reserve[2];
+	uint8_t posture_af_macro_reserve[2];
+	uint8_t new_mtk_format_version[1];
+	uint8_t new_mtk_format_flag[3];
+	uint8_t af_temperature_reserve[1];
+	uint8_t mtk_necessary_crc16[2];
+};
+
+typedef struct {
+	uint16_t r;
+	uint16_t gr;
+	uint16_t gb;
+	uint16_t b;
+	uint16_t r_g;
+	uint16_t b_g;
+	uint16_t gr_gb;
+} awb_t;
+
+typedef struct {
+	uint8_t r_g_golden_min;
+	uint8_t r_g_golden_max;
+	uint8_t b_g_golden_min;
+	uint8_t b_g_golden_max;
+} awb_limit_t;
+
 extern int iReadRegI2C(u8 *a_pSendData, u16 a_sizeSendData, u8 * a_pRecvData, u16 a_sizeRecvData, u16 i2cId);
 extern int iWriteRegI2C(u8 *a_pSendData, u16 a_sizeSendData, u16 i2cId);
 extern void kdSetI2CSpeed(u16 i2cSpeed);
