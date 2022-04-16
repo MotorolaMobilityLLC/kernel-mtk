a39fd24a9ce0903c9ecb1b077e12a67b251ec452
houdz1 <houdz1@lenovo.com>
2022-04-16 21:37:16 +0800

    drm/mediatek: use mipi_dsi_dcs_grp_write_gce to send DDIC's command
    "mipi_dsi_dcs_write_gce" only send one command.
    "set panel feature" must send more than one commands.
    And maybe include the command for switch page.
    So we must send these commands with group.
    
    Change-Id: Ic1176008353843e94cc2221ae7f5300affcd3470
    Signed-off-by: houdz1 <houdz1@mt.com>
    Reviewed-on: https://gerrit.mot.com/2244232
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_panel_ext.h
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_panel_ext.h
@@ -569,7 +569,7 @@ struct mtk_panel_funcs {
 	int (*send_ddic_cmd_pack)(struct drm_panel *panel,
 		void *dsi_drv, dcs_write_gce_pack cb, void *handle);
 	int (*panel_feature_set)(struct drm_panel *panel, void *dsi_drv,
-			    dcs_write_gce cb, void *handle, struct panel_param_info param_info);
+			    dcs_grp_write_gce cb, void *handle, struct panel_param_info param_info);
 	int (*panel_feature_get)(struct drm_panel *panel, paramId_t param_id);
 };
 
