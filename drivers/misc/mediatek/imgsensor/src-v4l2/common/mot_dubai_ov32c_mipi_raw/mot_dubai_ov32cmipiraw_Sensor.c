de1c4d37d93bac1f2a4964a286fd43d5c5319af6
wangzl37 <wangzl37@motorola.com>
2022-04-24 10:04:58 +0800

    tesla: fix Camera ITS Test  sensor_fusion fail[1/3]
    tesla:
    --fix Camera ITS Test  sensor_fusion fail
    
    Change-Id: Iadf726eb96885bac6db21c29bd6bcbb26f15de20
    Signed-off-by: wangzl37 <wangzl37@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2251054
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: <buyan1@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 6af3d49006b7eb89c2970c89f3368646fc37f597)

--- drivers/misc/mediatek/imgsensor/src-v4l2/common/mot_dubai_ov32c_mipi_raw/mot_dubai_ov32cmipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src-v4l2/common/mot_dubai_ov32c_mipi_raw/mot_dubai_ov32cmipiraw_Sensor.c
@@ -1305,7 +1305,7 @@ static int feature_control(struct subdrv_ctx *ctx, MSDK_SENSOR_FEATURE_ENUM feat
 		break;
 	case SENSOR_FEATURE_GET_OFFSET_TO_START_OF_EXPOSURE:
 		*(MUINT32 *)(uintptr_t)(*(feature_data + 1))
-			= 2500000;
+			= 7248000;
 		break;
 	case SENSOR_FEATURE_GET_PERIOD_BY_SCENARIO:
 		switch (*feature_data) {
