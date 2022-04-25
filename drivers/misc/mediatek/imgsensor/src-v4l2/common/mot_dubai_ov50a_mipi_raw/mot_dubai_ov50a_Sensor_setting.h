35bba1004696ea261b925d9e91d908589ad03794
wangzl37 <wangzl37@motorola.com>
2022-04-25 13:53:03 +0800

    tesla: update OV50A setting
    tesla:
    --update OV50A setting
    
    Change-Id: Ic75e5f154639d46a28b6d1e3393b377d6fbc44e0
    Signed-off-by: wangzl37 <wangzl37@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2251765
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shenhuai Huang <huangsh4@motorola.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 3b44b04ce45d307a2d5b76e150b9cbe0e39074c7)

--- drivers/misc/mediatek/imgsensor/src-v4l2/common/mot_dubai_ov50a_mipi_raw/mot_dubai_ov50a_Sensor_setting.h
+++ drivers/misc/mediatek/imgsensor/src-v4l2/common/mot_dubai_ov50a_mipi_raw/mot_dubai_ov50a_Sensor_setting.h
@@ -27,7 +27,7 @@ kal_uint16 addr_data_pair_xtalk_mot_dubai_ov50a2q[] = {
 };
 
 kal_uint16 addr_data_pair_init_mot_dubai_ov50a2q[] = {
-#include"setting/OV50A40_Initial_CPHY_3Trio_1872Msps_20220222.h"
+#include"setting/OV50A40_Initial_CPHY_3Trio_1872Msps_20220422.h"
 };
 
 kal_uint16 addr_data_pair_init_mot_dubai_ov50a_20220312[] = {
@@ -35,37 +35,37 @@ kal_uint16 addr_data_pair_init_mot_dubai_ov50a_20220312[] = {
 };
 
 kal_uint16 addr_data_pair_preview_mot_dubai_ov50a2q[] = {
-#include"setting/OV50A40_4096x3072_4C2SCG_10bit_30fps_AG255_PDVC1_2048x768_20220121.h"
+#include"setting/OV50A40_4096x3072_4C2SCG_10bit_30fps_AG255_PDVC1_2048x768_20220422.h"
 };
 
 kal_uint16 addr_data_pair_capture_mot_dubai_ov50a2q[] = {
-#include"setting/OV50A40_4096x3072_4C2SCG_10bit_30fps_AG255_PDVC1_2048x768_20220121.h"
+#include"setting/OV50A40_4096x3072_4C2SCG_10bit_30fps_AG255_PDVC1_2048x768_20220422.h"
 };
 
 kal_uint16 addr_data_pair_video_mot_dubai_ov50a2q[] = {
-#include"setting/OV50A40_4096x3072_4C2SCG_10bit_30fps_AG255_PDVC1_2048x768_20220121.h"
+#include"setting/OV50A40_4096x3072_4C2SCG_10bit_30fps_AG255_PDVC1_2048x768_20220422.h"
 };
 
 kal_uint16 addr_data_pair_hs_video_mot_dubai_ov50a2q[] = {
-#include"setting/OV50A40_1920x1080_4C1SCG_10bit_120fps_AG255_20220118.h"
+#include"setting/OV50A40_1920x1080_4C1SCG_10bit_120fps_AG255_20220318.h"
 };
 
 kal_uint16 addr_data_pair_slim_video_mot_dubai_ov50a2q[] = {
-#include"setting/OV50A40_4096x3072_4C2SCG_10bit_30fps_AG255_PDVC1_2048x768_20220121.h"
+#include"setting/OV50A40_4096x3072_4C2SCG_10bit_30fps_AG255_PDVC1_2048x768_20220422.h"
 };
 
 kal_uint16 addr_data_pair_custom1[] = {
-#include"setting/OV50A40_8192x6144_10bit_20fps_AG64_20220118.h"
+#include"setting/OV50A40_8192x6144_10bit_20fps_AG64_20220418.h"
 };
 
 kal_uint16 addr_data_pair_custom2[] = {
-#include"setting/OV50A40_1920x1080_4C2SCG_10bit_60fps_AG255_PDVC1_3840x270_20220118.h"
+#include"setting/OV50A40_1920x1080_4C2SCG_10bit_60fps_AG255_PDVC1_1920x540_20220331.h"
 };
 kal_uint16 addr_data_pair_custom3[] = {
-#include"setting/OV50A40_1280x720_4C1SCG_10bit_240fps_AG255_20220118.h"
+#include"setting/OV50A40_1280x720_4C1SCG_10bit_240fps_AG255_20220318.h"
 };
 kal_uint16 addr_data_pair_custom4[] = {
-#include"setting/OV50A40_4096x3072_Cropping_10bit_30fps_AG64_PDVC1_2048x768_20220316.h"
+#include"setting/OV50A40_4096x3072_Cropping_10bit_30fps_AG64_PDVC1_2048x768_20220418.h"
 };
 #ifdef SEAMLESS_SWITCH_V2
 kal_uint16 addr_data_pair_seamless_switch_step1_mot_dubai_ov50a2q[] = {
