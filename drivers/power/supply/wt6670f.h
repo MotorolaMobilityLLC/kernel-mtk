02113fe02ad831e7f6906a5e1f975c0964224237
hq100 <hq100@motorola.com>
2022-05-09 18:14:03 +0800

    wt6670f bringup[4/4]
    1.add wf6670f driver
    2.Fix the problem of irq non-stop reporting
    
    Change-Id: I21af499e281c37d162faf639075413303b7a0fe2
    Reviewed-on: https://gerrit.mot.com/2263687
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Lei Chen <chenl18@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Tested-by: Jira Key
    Submit-Approved: Jira Key

---
+++ drivers/power/supply/wt6670f.h
@@ -0,0 +1,23 @@
+#define WT6670_FIRMWARE_VERSION  0x03
+
+struct wt6670f {
+	struct device *dev;
+	struct i2c_client *client;
+
+	int i2c_scl_pin;
+	int i2c_sda_pin;
+	int reset_pin;
+
+	int count;
+	int chg_type;
+	bool chg_ready;
+
+	struct mutex i2c_rw_lock;
+};
+
+enum {
+	QC3P_WT6670F,
+	QC3P_Z350,
+};
+
+int wt6670f_isp_flow(struct wt6670f *chip);
