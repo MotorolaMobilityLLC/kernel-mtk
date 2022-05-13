8e12997b47065a7b7500369a3bf6bb60577ee0a4
chenglong1 <chenglong1@lenovo.com>
2022-05-13 19:56:22 +0800

    Vicky: Add calibration status checking.
    Add EEPROM segments CRC checking and fill the calibration status.
    TODO:
    - Optimizing I2C read, using sequential read instead single byte read.
    - Share EEPROM data forEEPROM driver to avoid repeatly EEPROM data reading.
    
    Change-Id: I7060532e27b015fc63e62adf0be7f323f0da3595
    Signed-off-by: chenglong1 <chenglong1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2268454
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-by: XiaoFeng Wang <wangxf32@lenovo.com>
    Reviewed-by: Shenhuai Huang <huangsh4@motorola.com>

--- drivers/misc/mediatek/cam_cal/src/custom/cam_cal_list.c
+++ drivers/misc/mediatek/cam_cal/src/custom/cam_cal_list.c
@@ -19,6 +19,9 @@ struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
 	{GC5035_SENSOR_ID,  0x7E, Common_read_region},
 	{HI1339SUBOFILM_SENSOR_ID, 0xA2, Common_read_region},
 	{HI1339SUBTXD_SENSOR_ID, 0xA2, Common_read_region},
+	//Begin: Add EEPROM for Vicky
+	{MOT_VICKY_S5KHM6_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_16K},
+	//End
 	{OV48B_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_16K},
 	{IMX766_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_32K},
 	{IMX766DUAL_SENSOR_ID, 0xA2, Common_read_region, MAX_EEPROM_SIZE_16K},
