11d4086586239aa6cfa738039bade060241ff84d
Pina Chen <pina.chen@mediatek.com>
2022-05-14 13:33:55 +0800

    [ALPS06345809] vcodec: fix log too much
    Fix log too much by fix dvfss log level
    
    MTK-Commit-Id: 924d0d9dfe62c8ce9e697d60584dc3102fe28e9a
    
    Signed-off-by: Pina Chen <pina.chen@mediatek.com>
    CR-Id: ALPS06345809
    Feature: Video Driver
    Change-Id: Ib9edca72ded35b57276af587f5d51f69ce9a578f
    (cherry picked from commit ecdd0b8af78ad3e620a722c3797a59ca43ac0814)

--- drivers/media/platform/mtk-vcodec/v2/mtk_vcodec_dec_pm_plat.c
+++ drivers/media/platform/mtk-vcodec/v2/mtk_vcodec_dec_pm_plat.c
@@ -64,7 +64,7 @@ static bool mtk_dec_tput_init(struct mtk_vcodec_dev *dev)
 	dev->vdec_dvfs_params.normal_max_freq = nmax;
 	dev->vdec_dvfs_params.allow_oc = 0;
 
-	mtk_v4l2_debug(6, "[VDEC] tput op_th %d, tmin %u, tmax %u",
+	mtk_v4l2_debug(8, "[VDEC] tput op_th %d, tmin %u, tmax %u",
 		dev->vdec_dvfs_params.per_frame_adjust_op_rate,
 		dev->vdec_dvfs_params.min_freq,
 		dev->vdec_dvfs_params.normal_max_freq);
@@ -77,8 +77,8 @@ static bool mtk_dec_tput_init(struct mtk_vcodec_dev *dev)
 	cnt = of_property_count_u32_elems(pdev->dev.of_node, "throughput-table");
 	dev->vdec_tput_cnt = cnt / tp_item_num;
 
-	mtk_v4l2_debug(6, "[VDEC] tput table elements %u, %d per line",
-			cnt, tp_item_num, dev->vdec_tput);
+	mtk_v4l2_debug(8, "[VDEC] tput table elements %u, %d per line",
+			cnt, tp_item_num);
 	if (!dev->vdec_tput_cnt) {
 		mtk_v4l2_debug(0, "[VDEC] throughtput table not exist");
 		return false;
@@ -86,7 +86,7 @@ static bool mtk_dec_tput_init(struct mtk_vcodec_dev *dev)
 
 	dev->vdec_tput = vzalloc(sizeof(struct vcodec_perf) * dev->vdec_tput_cnt);
 
-	mtk_v4l2_debug(6, "[VDEC] vzalloc %zu x %d res %p",
+	mtk_v4l2_debug(8, "[VDEC] vzalloc %zu x %d res %p",
 			sizeof(struct vcodec_perf), dev->vdec_tput_cnt, dev->vdec_tput);
 	if (!dev->vdec_tput) {
 		mtk_v4l2_debug(0, "[VDEC] vzalloc vdec_tput table failed");
@@ -281,7 +281,7 @@ void mtk_prepare_vdec_emi_bw(struct mtk_vcodec_dev *dev)
 
 	for (i = 0; i < port_num; i++) {
 		dev->vdec_qos_req[i] = of_mtk_icc_get(&pdev->dev, path_strs[i]);
-		mtk_v4l2_debug(10, "[VDEC] qos port[%d] name %s", i, path_strs[i]);
+		mtk_v4l2_debug(16, "[VDEC] qos port[%d] name %s", i, path_strs[i]);
 	}
 #endif
 }
@@ -304,7 +304,7 @@ void set_vdec_opp(struct mtk_vcodec_dev *dev, u32 freq)
 		volt = dev_pm_opp_get_voltage(opp);
 		dev_pm_opp_put(opp);
 
-		mtk_v4l2_debug(0, "[VDEC] freq %u, voltage %d", freq, volt);
+		mtk_v4l2_debug(8, "[VDEC] freq %u, voltage %d", freq, volt);
 
 		ret = regulator_set_voltage(dev->vdec_reg, volt, INT_MAX);
 		if (ret) {
@@ -315,7 +315,7 @@ void set_vdec_opp(struct mtk_vcodec_dev *dev, u32 freq)
 
 void mtk_vdec_dvfs_begin_inst(struct mtk_vcodec_ctx *ctx)
 {
-	mtk_v4l2_debug(6, "[VDEC] ctx = %p",  ctx);
+	mtk_v4l2_debug(8, "[VDEC] ctx = %p",  ctx);
 
 	if (need_update(ctx)) {
 		update_freq(ctx->dev, MTK_INST_DECODER);
@@ -326,7 +326,7 @@ void mtk_vdec_dvfs_begin_inst(struct mtk_vcodec_ctx *ctx)
 
 void mtk_vdec_dvfs_end_inst(struct mtk_vcodec_ctx *ctx)
 {
-	mtk_v4l2_debug(6, "[VDEC] ctx = %p",  ctx);
+	mtk_v4l2_debug(8, "[VDEC] ctx = %p",  ctx);
 
 	if (remove_update(ctx)) {
 		update_freq(ctx->dev, MTK_INST_DECODER);
@@ -341,7 +341,7 @@ void mtk_vdec_pmqos_begin_inst(struct mtk_vcodec_ctx *ctx)
 	struct mtk_vcodec_dev *dev = 0;
 	u64 target_bw = 0;
 
-	mtk_v4l2_debug(6, "[VDEC] ctx = %p",  ctx);
+	mtk_v4l2_debug(8, "[VDEC] ctx = %p",  ctx);
 	dev = ctx->dev;
 
 	for (i = 0; i < dev->vdec_port_cnt; i++) {
@@ -351,13 +351,13 @@ void mtk_vdec_pmqos_begin_inst(struct mtk_vcodec_ctx *ctx)
 		if (dev->vdec_port_bw[i].port_type < VCODEC_PORT_LARB_SUM) {
 			mtk_icc_set_bw_not_update(dev->vdec_qos_req[i],
 					MBps_to_icc((u32)target_bw), 0);
-			mtk_v4l2_debug(6, "[VDEC] port %d bw %lu MB/s", i, (u32)target_bw);
+			mtk_v4l2_debug(8, "[VDEC] port %d bw %lu MB/s", i, (u32)target_bw);
 		} else if (dev->vdec_port_bw[i].port_type == VCODEC_PORT_LARB_SUM) {
 			mtk_icc_set_bw(dev->vdec_qos_req[i], 0, 0);
-			mtk_v4l2_debug(6, "[VDEC] port %d set larb %u bw",
+			mtk_v4l2_debug(8, "[VDEC] port %d set larb %u bw",
 					i, dev->vdec_port_bw[i].larb);
 		} else {
-			mtk_v4l2_debug(6, "[VDEC] unknown port type %d %d\n",
+			mtk_v4l2_debug(8, "[VDEC] unknown port type %d %d\n",
 				dev->vdec_port_bw[i].port_type);
 		}
 	}
@@ -369,7 +369,7 @@ void mtk_vdec_pmqos_end_inst(struct mtk_vcodec_ctx *ctx)
 	struct mtk_vcodec_dev *dev = 0;
 	u64 target_bw = 0;
 
-	mtk_v4l2_debug(6, "[VDEC] ctx = %p",  ctx);
+	mtk_v4l2_debug(8, "[VDEC] ctx = %p",  ctx);
 	dev = ctx->dev;
 
 	for (i = 0; i < dev->vdec_port_cnt; i++) {
@@ -383,13 +383,13 @@ void mtk_vdec_pmqos_end_inst(struct mtk_vcodec_ctx *ctx)
 		if (dev->vdec_port_bw[i].port_type < VCODEC_PORT_LARB_SUM) {
 			mtk_icc_set_bw_not_update(dev->vdec_qos_req[i],
 					MBps_to_icc((u32)target_bw), 0);
-			mtk_v4l2_debug(6, "[VDEC] port %d bw %lu MB/s", i, (u32)target_bw);
+			mtk_v4l2_debug(8, "[VDEC] port %d bw %lu MB/s", i, (u32)target_bw);
 		} else if (dev->vdec_port_bw[i].port_type == VCODEC_PORT_LARB_SUM) {
 			mtk_icc_set_bw(dev->vdec_qos_req[i], 0, 0);
-			mtk_v4l2_debug(6, "[VDEC] port %d set larb %u bw",
+			mtk_v4l2_debug(8, "[VDEC] port %d set larb %u bw",
 					i, dev->vdec_port_bw[i].larb);
 		} else {
-			mtk_v4l2_debug(6, "[VDEC] unknown port type %d",
+			mtk_v4l2_debug(8, "[VDEC] unknown port type %d",
 				dev->vdec_port_bw[i].port_type);
 		}
 	}
