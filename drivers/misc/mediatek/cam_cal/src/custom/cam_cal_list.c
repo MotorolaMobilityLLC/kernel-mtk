c64f918c977d7609faf36645b09a1db24658d0f5
huangsh4 <huangsh4@lenovo.com>
2022-05-17 02:38:15 +0800

    Vicky: enable s5k4h7 eeprom feature.
    camera:
    enable s5k4h7 eeprom feature.
    
    Change-Id: I16f11827f58620def383c156ad91a7e33a26556b
    Signed-off-by: huangsh4 <huangsh4@mt.com>
    Reviewed-on: https://gerrit.mot.com/2265984
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: <hansx3@motorola.com>
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/cam_cal/src/custom/cam_cal_list.c
+++ drivers/misc/mediatek/cam_cal/src/custom/cam_cal_list.c
@@ -11,12 +11,14 @@
 
 #define MAX_EEPROM_SIZE_32K 0x8000
 #define MAX_EEPROM_SIZE_16K 0x4000
-
+extern unsigned int mot_s5k4h7_read_region(struct i2c_client *client, unsigned int addr,
+			unsigned char *data, unsigned int size);
 struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
 	/*Below is commom sensor */
 	//Begin: Add EEPROM for Vicky
 	{MOT_VICKY_S5KHM6_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_16K},
 	{MOT_VICKY_HI1634Q_SENSOR_ID, 0xA2, Common_read_region, MAX_EEPROM_SIZE_16K},
+	{MOT_VICKY_S5K4H7_SENSOR_ID, 0x5A, mot_s5k4h7_read_region},  // otp
 	//End
 	{OV48B_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_16K},
 	{IMX766_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_32K},
