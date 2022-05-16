4399f005a9f58c042fdcc2e147ebeec1041b0cf9
liuzy55 <liuzy55@motorola.com>
2022-05-16 14:33:18 +0800

    vicky: front hi1634q eeprom bringup[1]
    details:
    add hi1634q eepromID list
    add hi1634q eeprom driver
    
    Change-Id: Id9e57075b041da63cf20fb8e6de824d0370336d1
    Signed-off-by: liuzy55 <liuzy55@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2269701
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: XiaoFeng Wang <wangxf32@lenovo.com>
    Reviewed-by: <hansx3@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_vicky_hi1634q_mipi_raw/mot_vicky_hi1634qmipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_vicky_hi1634q_mipi_raw/mot_vicky_hi1634qmipiraw_Sensor.c
@@ -54,6 +54,9 @@
 
 #undef VENDOR_EDIT
 
+#define LOG_INF(format, args...)    pr_err(PFX "[%s] " format, __func__, ##args)
+#define LOGE(format, args...)    pr_err(PFX "[%s] " format, __func__, ##args)
+
 #define USE_BURST_MODE 1
 
 //#define I2C_BUFFER_LEN 255 /* trans# max is 255, each 3 bytes */
@@ -622,6 +625,355 @@ static void slim_video_setting(void)
 	pr_debug("Hi-1634Q slim_video_setting end\n");
 }
 
+#define VICKY_HI1634Q_EEPROM_SLAVE_ADDR 0xA2
+#define VICKY_HI1634Q_SENSOR_IIC_SLAVE_ADDR 0x42
+#define VICKY_HI1634Q_EEPROM_SIZE  4389
+#define VICKY_HI1634Q_EEPROM_CRC_AWB_CAL_SIZE 43
+#define VICKY_HI1634Q_EEPROM_CRC_OPTICAL_CENTER_CAL_SIZE 16
+#define VICKY_HI1634Q_EEPROM_CRC_SFR_CAL_SIZE 117
+#define VICKY_HI1634Q_EEPROM_CRC_LSC_SIZE 1868
+#define VICKY_HI1634Q_EEPROM_CRC_NECESSARY_DATA_CAL_SIZE 18
+#define VICKY_HI1634Q_EEPROM_CRC_MANUFACTURING_SIZE 37
+#define VICKY_HI1634Q_MANUFACTURE_PART_NUMBER "28C85320"
+#define VICKY_HI1634Q_MPN_LENGTH 8
+static uint8_t VICKY_HI1634Q_eeprom[VICKY_HI1634Q_EEPROM_SIZE] = {0};
+static calibration_status_t mnf_status            = CRC_FAILURE;
+static calibration_status_t awb_status            = CRC_FAILURE;
+static calibration_status_t optical_center_status = CRC_FAILURE;
+static calibration_status_t sfr_status            = CRC_FAILURE;
+static calibration_status_t lsc_status            = CRC_FAILURE;
+static calibration_status_t necessary_data_status = CRC_FAILURE;
+static uint8_t crc_reverse_byte(uint32_t data)
+{
+	return ((data * 0x0802LU & 0x22110LU) |
+		(data * 0x8020LU & 0x88440LU)) * 0x10101LU >> 16;
+}
+static uint32_t convert_crc(uint8_t *crc_ptr)
+{
+	return (crc_ptr[0] << 8) | (crc_ptr[1]);
+}
+static uint16_t to_uint16_swap(uint8_t *data)
+{
+	uint16_t converted;
+	memcpy(&converted, data, sizeof(uint16_t));
+	return ntohs(converted);
+}
+static void VICKY_HI1634Q_read_data_from_eeprom(kal_uint8 slave, kal_uint32 start_add, uint32_t size)
+{
+	int i = 0;
+	spin_lock(&imgsensor_drv_lock);
+	imgsensor.i2c_write_id = slave;
+	spin_unlock(&imgsensor_drv_lock);
+	//read eeprom data
+	for (i = 0; i < size; i ++) {
+		VICKY_HI1634Q_eeprom[i] = read_cmos_sensor_8(start_add);
+		start_add ++;
+	}
+	spin_lock(&imgsensor_drv_lock);
+	imgsensor.i2c_write_id = VICKY_HI1634Q_SENSOR_IIC_SLAVE_ADDR;
+	spin_unlock(&imgsensor_drv_lock);
+}
+static int32_t eeprom_util_check_crc16(uint8_t *data, uint32_t size, uint32_t ref_crc)
+{
+	int32_t crc_match = 0;
+	uint16_t crc = 0x0000;
+	uint16_t crc_reverse = 0x0000;
+	uint32_t i, j;
+	uint32_t tmp;
+	uint32_t tmp_reverse;
+	/* Calculate both methods of CRC since integrators differ on
+	* how CRC should be calculated. */
+	for (i = 0; i < size; i++) {
+		tmp_reverse = crc_reverse_byte(data[i]);
+		tmp = data[i] & 0xff;
+		for (j = 0; j < 8; j++) {
+			if (((crc & 0x8000) >> 8) ^ (tmp & 0x80))
+				crc = (crc << 1) ^ 0x8005;
+			else
+				crc = crc << 1;
+			tmp <<= 1;
+			if (((crc_reverse & 0x8000) >> 8) ^ (tmp_reverse & 0x80))
+				crc_reverse = (crc_reverse << 1) ^ 0x8005;
+			else
+				crc_reverse = crc_reverse << 1;
+			tmp_reverse <<= 1;
+		}
+	}
+	crc_reverse = (crc_reverse_byte(crc_reverse) << 8) |
+		crc_reverse_byte(crc_reverse >> 8);
+	if (crc == ref_crc || crc_reverse == ref_crc)
+		crc_match = 1;
+	LOG_INF("REF_CRC 0x%x CALC CRC 0x%x CALC Reverse CRC 0x%x matches? %d\n",
+		ref_crc, crc, crc_reverse, crc_match);
+	return crc_match;
+}
+static uint8_t mot_eeprom_util_check_awb_limits(awb_t unit, awb_t golden)
+{
+	uint8_t result = 0;
+	if (unit.r < AWB_R_MIN || unit.r > AWB_R_MAX) {
+		LOG_INF("unit r out of range! MIN: %d, r: %d, MAX: %d",
+			AWB_R_MIN, unit.r, AWB_R_MAX);
+		result = 1;
+	}
+	if (unit.gr < AWB_GR_MIN || unit.gr > AWB_GR_MAX) {
+		LOG_INF("unit gr out of range! MIN: %d, gr: %d, MAX: %d",
+			AWB_GR_MIN, unit.gr, AWB_GR_MAX);
+		result = 1;
+	}
+	if (unit.gb < AWB_GB_MIN || unit.gb > AWB_GB_MAX) {
+		LOG_INF("unit gb out of range! MIN: %d, gb: %d, MAX: %d",
+			AWB_GB_MIN, unit.gb, AWB_GB_MAX);
+		result = 1;
+	}
+	if (unit.b < AWB_B_MIN || unit.b > AWB_B_MAX) {
+		LOG_INF("unit b out of range! MIN: %d, b: %d, MAX: %d",
+			AWB_B_MIN, unit.b, AWB_B_MAX);
+		result = 1;
+	}
+	if (golden.r < AWB_R_MIN || golden.r > AWB_R_MAX) {
+		LOG_INF("golden r out of range! MIN: %d, r: %d, MAX: %d",
+			AWB_R_MIN, golden.r, AWB_R_MAX);
+		result = 1;
+	}
+	if (golden.gr < AWB_GR_MIN || golden.gr > AWB_GR_MAX) {
+		LOG_INF("golden gr out of range! MIN: %d, gr: %d, MAX: %d",
+			AWB_GR_MIN, golden.gr, AWB_GR_MAX);
+		result = 1;
+	}
+	if (golden.gb < AWB_GB_MIN || golden.gb > AWB_GB_MAX) {
+		LOG_INF("golden gb out of range! MIN: %d, gb: %d, MAX: %d",
+			AWB_GB_MIN, golden.gb, AWB_GB_MAX);
+		result = 1;
+	}
+	if (golden.b < AWB_B_MIN || golden.b > AWB_B_MAX) {
+		LOG_INF("golden b out of range! MIN: %d, b: %d, MAX: %d",
+			AWB_B_MIN, golden.b, AWB_B_MAX);
+		result = 1;
+	}
+	return result;
+}
+static uint8_t mot_eeprom_util_calculate_awb_factors_limit(awb_t unit, awb_t golden,
+		awb_limit_t limit)
+{
+	uint32_t r_g;
+	uint32_t b_g;
+	uint32_t golden_rg, golden_bg;
+	uint32_t r_g_golden_min;
+	uint32_t r_g_golden_max;
+	uint32_t b_g_golden_min;
+	uint32_t b_g_golden_max;
+	r_g = unit.r_g * 1000;
+	b_g = unit.b_g*1000;
+	golden_rg = golden.r_g* 1000;
+	golden_bg = golden.b_g* 1000;
+	r_g_golden_min = limit.r_g_golden_min*16384;
+	r_g_golden_max = limit.r_g_golden_max*16384;
+	b_g_golden_min = limit.b_g_golden_min*16384;
+	b_g_golden_max = limit.b_g_golden_max*16384;
+	LOG_INF("rg = %d, bg=%d,rgmin=%d,bgmax =%d\n",r_g,b_g,r_g_golden_min,r_g_golden_max);
+	LOG_INF("grg = %d, gbg=%d,bgmin=%d,bgmax =%d\n",golden_rg,golden_bg,b_g_golden_min,b_g_golden_max);
+	if (r_g < (golden_rg - r_g_golden_min) || r_g > (golden_rg + r_g_golden_max)) {
+		LOG_INF("Final RG calibration factors out of range!");
+		return 1;
+	}
+	if (b_g < (golden_bg - b_g_golden_min) || b_g > (golden_bg + b_g_golden_max)) {
+		LOG_INF("Final BG calibration factors out of range!");
+		return 1;
+	}
+	return 0;
+}
+static calibration_status_t VICKY_HI1634Q_check_awb_data(void *data)
+{
+	struct mot_hi1634q_eeprom_t *eeprom = (struct mot_hi1634q_eeprom_t*)data;
+	awb_t unit;
+	awb_t golden;
+	awb_limit_t golden_limit;
+	if(!eeprom_util_check_crc16(eeprom->cie_ev,
+		VICKY_HI1634Q_EEPROM_CRC_AWB_CAL_SIZE,
+		convert_crc(eeprom->awb_crc16))) {
+		LOG_INF("AWB CRC Fails!");
+		return CRC_FAILURE;
+	}
+	unit.r = to_uint16_swap(eeprom->awb_src_1_r)/64;
+	unit.gr = to_uint16_swap(eeprom->awb_src_1_gr)/64;
+	unit.gb = to_uint16_swap(eeprom->awb_src_1_gb)/64;
+	unit.b = to_uint16_swap(eeprom->awb_src_1_b)/64;
+	unit.r_g = to_uint16_swap(eeprom->awb_src_1_rg_ratio);
+	unit.b_g = to_uint16_swap(eeprom->awb_src_1_bg_ratio);
+	unit.gr_gb = to_uint16_swap(eeprom->awb_src_1_gr_gb_ratio);
+	golden.r = to_uint16_swap(eeprom->awb_src_1_golden_r)/64;
+	golden.gr = to_uint16_swap(eeprom->awb_src_1_golden_gr)/64;
+	golden.gb = to_uint16_swap(eeprom->awb_src_1_golden_gb)/64;
+	golden.b = to_uint16_swap(eeprom->awb_src_1_golden_b)/64;
+	golden.r_g = to_uint16_swap(eeprom->awb_src_1_golden_rg_ratio);
+	golden.b_g = to_uint16_swap(eeprom->awb_src_1_golden_bg_ratio);
+	golden.gr_gb = to_uint16_swap(eeprom->awb_src_1_golden_gr_gb_ratio);
+	if (mot_eeprom_util_check_awb_limits(unit, golden)) {
+		LOG_INF("AWB CRC limit Fails!");
+		return LIMIT_FAILURE;
+	}
+	golden_limit.r_g_golden_min = eeprom->awb_r_g_golden_min_limit[0];
+	golden_limit.r_g_golden_max = eeprom->awb_r_g_golden_max_limit[0];
+	golden_limit.b_g_golden_min = eeprom->awb_b_g_golden_min_limit[0];
+	golden_limit.b_g_golden_max = eeprom->awb_b_g_golden_max_limit[0];
+	if (mot_eeprom_util_calculate_awb_factors_limit(unit, golden,golden_limit)) {
+		LOG_INF("AWB CRC factor limit Fails!");
+		return LIMIT_FAILURE;
+	}
+	LOG_INF("AWB CRC Pass");
+	return NO_ERRORS;
+}
+static calibration_status_t VICKY_HI1634Q_check_lsc_data(void *data)
+{
+	struct mot_hi1634q_eeprom_t *eeprom = (struct mot_hi1634q_eeprom_t*)data;
+	if (!eeprom_util_check_crc16(eeprom->mtk_lsc_calibration, VICKY_HI1634Q_EEPROM_CRC_LSC_SIZE,
+		convert_crc(eeprom->mtk_lsc_crc16))) {
+		LOG_INF("LSC CRC Fails!");
+		return CRC_FAILURE;
+	}
+	LOG_INF("LSC CRC Pass");
+	return NO_ERRORS;
+}
+static void VICKY_HI1634Q_eeprom_get_mnf_data(void *data,
+		mot_calibration_mnf_t *mnf)
+{
+	int ret;
+	struct mot_hi1634q_eeprom_t *eeprom = (struct mot_hi1634q_eeprom_t*)data;
+	ret = snprintf(mnf->table_revision, MAX_CALIBRATION_STRING, "0x%x",
+		eeprom->eeprom_table_version[0]);
+	if (ret < 0 || ret >= MAX_CALIBRATION_STRING) {
+		LOG_INF("snprintf of mnf->table_revision failed");
+		mnf->table_revision[0] = 0;
+	}
+	ret = snprintf(mnf->mot_part_number, MAX_CALIBRATION_STRING, "%c%c%c%c%c%c%c%c",
+		eeprom->mpn[0], eeprom->mpn[1], eeprom->mpn[2], eeprom->mpn[3],
+		eeprom->mpn[4], eeprom->mpn[5], eeprom->mpn[6], eeprom->mpn[7]);
+	if (ret < 0 || ret >= MAX_CALIBRATION_STRING) {
+		LOG_INF("snprintf of mnf->mot_part_number failed");
+		mnf->mot_part_number[0] = 0;
+	}
+	ret = snprintf(mnf->actuator_id, MAX_CALIBRATION_STRING, "0x%x", eeprom->actuator_id[0]);
+	if (ret < 0 || ret >= MAX_CALIBRATION_STRING) {
+		LOG_INF("snprintf of mnf->actuator_id failed");
+		mnf->actuator_id[0] = 0;
+	}
+	ret = snprintf(mnf->lens_id, MAX_CALIBRATION_STRING, "0x%x", eeprom->lens_id[0]);
+	if (ret < 0 || ret >= MAX_CALIBRATION_STRING) {
+		LOG_INF("snprintf of mnf->lens_id failed");
+		mnf->lens_id[0] = 0;
+	}
+	if (eeprom->manufacturer_id[0] == 'S' && eeprom->manufacturer_id[1] == 'U') {
+		ret = snprintf(mnf->integrator, MAX_CALIBRATION_STRING, "Sunny");
+	} else if (eeprom->manufacturer_id[0] == 'O' && eeprom->manufacturer_id[1] == 'F') {
+		ret = snprintf(mnf->integrator, MAX_CALIBRATION_STRING, "OFilm");
+	} else if (eeprom->manufacturer_id[0] == 'Q' && eeprom->manufacturer_id[1] == 'T') {
+		ret = snprintf(mnf->integrator, MAX_CALIBRATION_STRING, "Qtech");
+	} else {
+		ret = snprintf(mnf->integrator, MAX_CALIBRATION_STRING, "Unknown");
+		LOG_INF("unknown manufacturer_id");
+	}
+	if (ret < 0 || ret >= MAX_CALIBRATION_STRING) {
+		LOG_INF("snprintf of mnf->integrator failed");
+		mnf->integrator[0] = 0;
+	}
+	ret = snprintf(mnf->factory_id, MAX_CALIBRATION_STRING, "%c%c",
+		eeprom->factory_id[0], eeprom->factory_id[1]);
+	if (ret < 0 || ret >= MAX_CALIBRATION_STRING) {
+		LOG_INF("snprintf of mnf->factory_id failed");
+		mnf->factory_id[0] = 0;
+	}
+	ret = snprintf(mnf->manufacture_line, MAX_CALIBRATION_STRING, "%u",
+		eeprom->manufacture_line[0]);
+	if (ret < 0 || ret >= MAX_CALIBRATION_STRING) {
+		LOG_INF("snprintf of mnf->manufacture_line failed");
+		mnf->manufacture_line[0] = 0;
+	}
+	ret = snprintf(mnf->manufacture_date, MAX_CALIBRATION_STRING, "20%u/%u/%u",
+		eeprom->manufacture_date[0], eeprom->manufacture_date[1], eeprom->manufacture_date[2]);
+	if (ret < 0 || ret >= MAX_CALIBRATION_STRING) {
+		LOG_INF("snprintf of mnf->manufacture_date failed");
+		mnf->manufacture_date[0] = 0;
+	}
+	ret = snprintf(mnf->serial_number, MAX_CALIBRATION_STRING, "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+		eeprom->serial_number[0], eeprom->serial_number[1],
+		eeprom->serial_number[2], eeprom->serial_number[3],
+		eeprom->serial_number[4], eeprom->serial_number[5],
+		eeprom->serial_number[6], eeprom->serial_number[7],
+		eeprom->serial_number[8], eeprom->serial_number[9],
+		eeprom->serial_number[10], eeprom->serial_number[11],
+		eeprom->serial_number[12], eeprom->serial_number[13],
+		eeprom->serial_number[14], eeprom->serial_number[15]);
+	if (ret < 0 || ret >= MAX_CALIBRATION_STRING) {
+		LOG_INF("snprintf of mnf->serial_number failed");
+		mnf->serial_number[0] = 0;
+	}
+}
+static calibration_status_t VICKY_HI1634Q_check_optical_center_data(void *data)
+{
+	struct mot_hi1634q_eeprom_t *eeprom = (struct mot_hi1634q_eeprom_t*)data;
+	if (!eeprom_util_check_crc16(eeprom->optical_center_x_1_r_channel, VICKY_HI1634Q_EEPROM_CRC_OPTICAL_CENTER_CAL_SIZE,
+		convert_crc(eeprom->optical_center_crc16))) {
+		LOG_INF("optical center CRC Fails!");
+		return CRC_FAILURE;
+	}
+	LOG_INF("optical center CRC Pass");
+	return NO_ERRORS;
+}
+static calibration_status_t VICKY_HI1634Q_check_sfr_data(void *data)
+{
+	struct mot_hi1634q_eeprom_t *eeprom = (struct mot_hi1634q_eeprom_t*)data;
+	if (!eeprom_util_check_crc16(eeprom->sfr_distance1_reserve, VICKY_HI1634Q_EEPROM_CRC_SFR_CAL_SIZE,
+		convert_crc(eeprom->sfr_crc16))) {
+		LOG_INF("SFR CRC Fails!");
+		return CRC_FAILURE;
+	}
+	LOG_INF("SFR CRC Pass");
+	return NO_ERRORS;
+}
+static calibration_status_t VICKY_HI1634Q_check_necessary_data(void *data)
+{
+	struct mot_hi1634q_eeprom_t *eeprom = (struct mot_hi1634q_eeprom_t*)data;
+	if (!eeprom_util_check_crc16(eeprom->module_check_flag, VICKY_HI1634Q_EEPROM_CRC_NECESSARY_DATA_CAL_SIZE,
+		convert_crc(eeprom->mtk_necessary_crc16))) {
+		LOG_INF("mtk necessary CRC Fails!");
+		return CRC_FAILURE;
+	}
+	LOG_INF("mtk necessary CRC Pass");
+	return NO_ERRORS;
+}
+static calibration_status_t VICKY_HI1634Q_check_manufacturing_data(void *data)
+{
+	struct mot_hi1634q_eeprom_t *eeprom = (struct mot_hi1634q_eeprom_t*)data;
+    LOG_INF("Manufacturing eeprom->mpn = %s !",eeprom->mpn);
+#if 0
+	if(strncmp(eeprom->mpn, VICKY_HI1634Q_MANUFACTURE_PART_NUMBER, VICKY_HI1634Q_MPN_LENGTH) != 0) {
+		LOG_INF("Manufacturing part number (%s) check Fails!", eeprom->mpn);
+		return CRC_FAILURE;
+	}
+#endif
+	if (!eeprom_util_check_crc16(eeprom->eeprom_table_version, VICKY_HI1634Q_EEPROM_CRC_MANUFACTURING_SIZE,
+		convert_crc(eeprom->manufacture_crc16))) {
+		LOG_INF("Manufacturing CRC Fails!");
+		return CRC_FAILURE;
+	}
+	LOG_INF("Manufacturing CRC Pass");
+	return NO_ERRORS;
+}
+static void VICKY_HI1634Q_eeprom_format_calibration_data(void *data)
+{
+	if (NULL == data) {
+		LOG_INF("data is NULL");
+		return;
+	}
+	mnf_status            = VICKY_HI1634Q_check_manufacturing_data(data);
+	awb_status            = VICKY_HI1634Q_check_awb_data(data);
+	optical_center_status = VICKY_HI1634Q_check_optical_center_data(data);;
+	sfr_status            = VICKY_HI1634Q_check_sfr_data(data);;
+	lsc_status            = VICKY_HI1634Q_check_lsc_data(data);
+	necessary_data_status = VICKY_HI1634Q_check_necessary_data(data);
+	LOG_INF("status mnf:%d, awb:%d, optical_center:%d, sfr:%d, lsc:%d, necessary_data:%d",
+		mnf_status, awb_status, optical_center_status, sfr_status, lsc_status, necessary_data_status);
+}
 /*************************************************************************
  * FUNCTION
  *	get_imgsensor_id
@@ -652,6 +1004,8 @@ static kal_uint32 get_imgsensor_id(UINT32 *sensor_id)
 		do {
 			*sensor_id = return_sensor_id();
 			if (*sensor_id == imgsensor_info.sensor_id) {
+				VICKY_HI1634Q_read_data_from_eeprom(VICKY_HI1634Q_EEPROM_SLAVE_ADDR, 0x0000, VICKY_HI1634Q_EEPROM_SIZE);
+				VICKY_HI1634Q_eeprom_format_calibration_data((void *)VICKY_HI1634Q_eeprom);
 				pr_debug("i2c write id: 0x%x, sensor id: 0x%x\n", imgsensor.i2c_write_id, *sensor_id);
 				return ERROR_NONE;
 			}
@@ -1015,6 +1369,12 @@ static kal_uint32 get_info(enum MSDK_SCENARIO_ID_ENUM scenario_id,
 
 	sensor_info->FrameTimeDelayFrame =
 		imgsensor_info.frame_time_delay_frame;
+
+	sensor_info->calibration_status.mnf = mnf_status;
+	sensor_info->calibration_status.awb = awb_status;
+	sensor_info->calibration_status.lsc = lsc_status;
+	VICKY_HI1634Q_eeprom_get_mnf_data((void *) VICKY_HI1634Q_eeprom, &sensor_info->mnf_calibration);
+
 
 	switch (scenario_id) {
 	case MSDK_SCENARIO_ID_CAMERA_PREVIEW:
