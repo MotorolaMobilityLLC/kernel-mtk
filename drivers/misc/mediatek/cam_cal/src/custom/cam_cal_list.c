38ed64fd271cedb7fa4f6ed5769180cab4ef7a6b
kankan5 <kankan5@motorola.com>
2022-05-26 19:39:22 +0800

    vicky: s5kgm1 eeprom bring up[1/2]
    details:
    s5kgm1 eeprom bring up
    
    Change-Id: I28dbd18e4f1002008748f1e58d47ffc2973bc1f6
    Signed-off-by: kankan5 <kankan5@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2280819
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: <wangzl37@motorola.com>
    Reviewed-by: <hansx3@motorola.com>
    Reviewed-by: XiaoFeng Wang <wangxf32@mt.com>
    Reviewed-by: Shenhuai Huang <huangsh4@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/cam_cal/src/custom/cam_cal_list.c
+++ drivers/misc/mediatek/cam_cal/src/custom/cam_cal_list.c
@@ -19,6 +19,7 @@ struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
 	/*Below is commom sensor */
 	//Begin: Add EEPROM for Vicky
 	{MOT_VICKY_S5KHM6_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_16K},
+	{MOT_VICKY_S5KGM1_SENSOR_ID, 0xB0, Common_read_region, MAX_EEPROM_SIZE_16K},
 	{MOT_VICKY_HI1634Q_SENSOR_ID, 0xA2, Common_read_region, MAX_EEPROM_SIZE_16K},
 	{MOT_VICKY_S5K4H7_SENSOR_ID, 0x5A, mot_s5k4h7_read_region},  // otp
 	{MOT_VICKY_OV02B10_SENSOR_ID, 0x78, mot_ov02b10_read_region},  // otp
