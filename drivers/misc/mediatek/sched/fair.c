733ad354e57241305ec6b972c0d2675d68abe3ea
Jing-Ting Wu <Jing-Ting.Wu@mediatek.com>
2022-05-28 11:58:29 +0800

    [ALPS07310473] sched: add per-task latency_sensitive
    [Detail]
    Add per-task latency_sensitive aware group setting
    
    MTK-Commit-Id: 83e6d68cf47f0d17325901a5cbcd05b9267b3860
    
    CR-Id: ALPS07310473
    Feature: Energy Aware Scheduling Plus (EAS+)
    Signed-off-by: Jing-Ting Wu <Jing-Ting.Wu@mediatek.com>
    Change-Id: I6023fbc5895eca139b7ea0fb010cafb8c79dc155
    (cherry picked from commit ac30a922f89c9ace4bd082478ff8adcca2382c66)

--- drivers/misc/mediatek/sched/fair.c
+++ drivers/misc/mediatek/sched/fair.c
@@ -396,8 +396,10 @@ void mtk_find_energy_efficient_cpu(void *data, struct task_struct *p, int prev_c
 	rcu_read_lock();
 	if (!uclamp_min_ls)
 		latency_sensitive = uclamp_latency_sensitive(p);
-	else
-		latency_sensitive = p->uclamp_req[UCLAMP_MIN].value > 0 ? 1 : 0;
+	else {
+		latency_sensitive = (p->uclamp_req[UCLAMP_MIN].value > 0 ? 1 : 0) ||
+					uclamp_latency_sensitive(p);
+	}
 
 	pd = rcu_dereference(rd->pd);
 	if (!pd || READ_ONCE(rd->overutilized)) {
@@ -613,8 +615,10 @@ static struct task_struct *detach_a_hint_task(struct rq *src_rq, int dst_cpu)
 
 		if (!uclamp_min_ls)
 			latency_sensitive = uclamp_latency_sensitive(p);
-		else
-			latency_sensitive = p->uclamp_req[UCLAMP_MIN].value > 0 ? 1 : 0;
+		else {
+			latency_sensitive = (p->uclamp_req[UCLAMP_MIN].value > 0 ? 1 : 0) ||
+					uclamp_latency_sensitive(p);
+		}
 
 		if (latency_sensitive &&
 			task_util <= dst_capacity) {
@@ -642,8 +646,10 @@ inline bool is_task_latency_sensitive(struct task_struct *p)
 	rcu_read_lock();
 	if (!uclamp_min_ls)
 		latency_sensitive = uclamp_latency_sensitive(p);
-	else
-		latency_sensitive = p->uclamp_req[UCLAMP_MIN].value > 0 ? 1 : 0;
+	else {
+		latency_sensitive = (p->uclamp_req[UCLAMP_MIN].value > 0 ? 1 : 0) ||
+					uclamp_latency_sensitive(p);
+	}
 	rcu_read_unlock();
 
 	return latency_sensitive;
