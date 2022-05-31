84d92908cf9791512887d2d41a5d235b318b4c7c
chenglong1 <chenglong1@lenovo.com>
2022-05-31 11:48:57 +0800

    Vicky: Optimizing power sequence for S5KHM6.
    - Remove useless delay from power sequence, to optimize main camera startup delay.
    - Change DVDD voltage from 1.1V to 1.0V (typical voltage).
    
    Change-Id: I97542c7fdd4ae657b5411d6b04f35f5982c8824f
    Signed-off-by: chenglong1 <chenglong1@mt.com>
    Reviewed-on: https://gerrit.mot.com/2284675
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: XiaoFeng Wang <wangxf32@mt.com>
    Reviewed-by: <wangzl37@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Reviewed-by: Shenhuai Huang <huangsh4@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/imgsensor_pwr_seq.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/imgsensor_pwr_seq.c
@@ -15,14 +15,13 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
 	{
 		SENSOR_DRVNAME_MOT_VICKY_S5KHM6_MIPI_RAW,
 		{
-			{SensorMCLK, Vol_Low, 2},
-			{RST, Vol_Low, 5},
-			{DOVDD, Vol_1800, 6},
-			{AVDD, Vol_2200, 7},
-			{DVDD, Vol_1100, 8},
-			{AFVDD, Vol_2800, 7},
-			{RST, Vol_High, 9},
-			{SensorMCLK, Vol_High, 10}
+			{SensorMCLK, Vol_High, 1},
+			{RST, Vol_Low, 1},
+			{AFVDD, Vol_2800, 0},
+			{DOVDD, Vol_1800, 0},
+			{AVDD, Vol_2200, 0},
+			{DVDD, Vol_1000, 1},
+			{RST, Vol_High, 11},
 		},
 	},
 #endif
