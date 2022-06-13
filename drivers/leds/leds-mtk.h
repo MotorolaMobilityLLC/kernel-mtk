7a5ef160326c7e3c3972d7e5424be79852e6da8e
houdz1 <houdz1@lenovo.com>
2022-06-13 10:42:51 +0800

    driver/leds: support i2c backlight with backlight class
    For kernel5.10,  backlight driver is register to backlight class.
    So we must call backlight class API to set  backlight.
    
    Change-Id: I3e38fd326546299c26e59aa12edc817a1cbc5018
    Signed-off-by: houdz1 <houdz1@mt.com>
    Reviewed-on: https://gerrit.mot.com/2298382
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Submit-Approved: Jira Key

--- drivers/leds/leds-mtk.h
+++ drivers/leds/leds-mtk.h
@@ -30,6 +30,9 @@ struct mt_led_data {
 	int (*mtk_hw_brightness_set)(struct mt_led_data *m_data,
 		int brightness);
 	struct mutex	led_access;
+#ifdef CONFIG_BACKLIGHT_CLASS_DEVICE
+	struct backlight_device *i2c_bd;
+#endif
 };
 
 int mt_leds_parse_dt(struct mt_led_data *mdev, struct fwnode_handle *fwnode);
