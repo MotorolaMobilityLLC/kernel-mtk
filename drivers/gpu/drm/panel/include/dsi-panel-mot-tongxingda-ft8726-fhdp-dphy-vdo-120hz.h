dc70d40ec2037aec319d8490a779c044d6386dff
huangxinglue <wingte77@motorola.com>
2022-06-22 17:29:10 +0800

    drm/panel: Devonn: add the 2nd panel file
    Add the 2nd panel file.
    ft8726 + 1080x2400 + 6.497 + video mode
    
    Change-Id: Ia8486d10826062de2dff632c2c54bc62a48b442a
    Signed-off-by: huangxinglue <wingte77@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2309490
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Dezhen Hou <houdz1@motorola.com>
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/gpu/drm/panel/include/dsi-panel-mot-tongxingda-ft8726-fhdp-dphy-vdo-120hz.h
@@ -0,0 +1,87 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2021 MediaTek Inc.
+ */
+
+#ifndef PANEL_FT8726_FHDP_DPHY_VDO_120HZ_TONGXINGDA_HFP
+#define PANEL_FT8726_FHDP_DPHY_VDO_120HZ_TONGXINGDA_HFP
+
+
+#define REGFLAG_DELAY           0xFFFC
+#define REGFLAG_UDELAY          0xFFFB
+#define REGFLAG_END_OF_TABLE    0xFFFD
+#define REGFLAG_RESET_LOW       0xFFFE
+#define REGFLAG_RESET_HIGH      0xFFFF
+
+#define FRAME_WIDTH                 1080
+#define FRAME_HEIGHT                2400
+
+#define PHYSICAL_WIDTH              67716
+#define PHYSICAL_HEIGHT            150480
+
+#define DATA_RATE                   900
+#define HSA                         10
+#define HBP                         44
+#define VSA                         2
+#define VBP                         10
+
+/*Parameter setting for mode 0 Start*/
+#define MODE_0_FPS                  60
+#define MODE_0_VFP                  2456
+#define MODE_0_HFP                  26
+#define MODE_0_DATA_RATE            900
+/*Parameter setting for mode 0 End*/
+
+/*Parameter setting for mode 1 Start*/
+#define MODE_1_FPS                  90
+#define MODE_1_VFP                  834
+#define MODE_1_HFP                  26
+#define MODE_1_DATA_RATE            900
+/*Parameter setting for mode 1 End*/
+
+/*Parameter setting for mode 2 Start*/
+#define MODE_2_FPS                  120
+#define MODE_2_VFP                  20
+#define MODE_2_HFP                  26
+#define MODE_2_DATA_RATE            900
+/*Parameter setting for mode 2 End*/
+
+#define LFR_EN                      0
+/* DSC RELATED */
+
+
+#define DSC_ENABLE                  1
+#define DSC_VER                     11
+#define DSC_SLICE_MODE              1
+#define DSC_RGB_SWAP                0
+#define DSC_DSC_CFG                 34
+#define DSC_RCT_ON                  1
+#define DSC_BIT_PER_CHANNEL         8
+#define DSC_DSC_LINE_BUF_DEPTH      9
+#define DSC_BP_ENABLE               1
+#define DSC_BIT_PER_PIXEL           128
+//define DSC_PIC_HEIGHT
+//define DSC_PIC_WIDTH
+#define DSC_SLICE_HEIGHT            8
+#define DSC_SLICE_WIDTH             540
+#define DSC_CHUNK_SIZE              540
+#define DSC_XMIT_DELAY              512
+#define DSC_DEC_DELAY               616
+#define DSC_SCALE_VALUE             32
+#define DSC_INCREMENT_INTERVAL      187
+#define DSC_DECREMENT_INTERVAL      7
+#define DSC_LINE_BPG_OFFSET         12
+#define DSC_NFL_BPG_OFFSET          3511
+#define DSC_SLICE_BPG_OFFSET        3255
+#define DSC_INITIAL_OFFSET          6144
+#define DSC_FINAL_OFFSET            4336
+#define DSC_FLATNESS_MINQP          3
+#define DSC_FLATNESS_MAXQP          12
+#define DSC_RC_MODEL_SIZE           8192
+#define DSC_RC_EDGE_FACTOR          6
+#define DSC_RC_QUANT_INCR_LIMIT0    11
+#define DSC_RC_QUANT_INCR_LIMIT1    11
+#define DSC_RC_TGT_OFFSET_HI        3
+#define DSC_RC_TGT_OFFSET_LO        3
+
+#endif //end of PANEL_FT8726_FHDP_DPHY_VDO_120HZ_TONGXINGDA_HFP
