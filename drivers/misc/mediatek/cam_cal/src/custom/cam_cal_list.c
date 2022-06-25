d0b73fd084e201b7be2ab41b4a832f7bf17b0367
liujianguo <wingte76@motorola.com>
2022-06-25 15:45:11 +0800

    repair otp error
    repair otp error
    --repair ov02b10 otp error
    
    Change-Id: I5ba02ddf2a88bd120dbf3eb716a29f865eecb693
    Reviewed-on: https://gerrit.mot.com/2313295
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <wingte76@motorola.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/cam_cal/src/custom/cam_cal_list.c
+++ drivers/misc/mediatek/cam_cal/src/custom/cam_cal_list.c
@@ -17,7 +17,10 @@ extern unsigned int mot_s5k4h7_read_region(struct i2c_client *client, unsigned i
 extern unsigned int mot_ov02b10_read_region(struct i2c_client *client, unsigned int addr,
 			unsigned char *data, unsigned int size);
 #endif
+extern unsigned int mot_ov02b10_read_region(struct i2c_client *client, unsigned int addr,
+			unsigned char *data, unsigned int size);
 struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
+	{MOT_DEVONN_OV02B10_SENSOR_ID, 0x78, mot_ov02b10_read_region},  // otp
 	/*Below is commom sensor */
 	{HI1339_SENSOR_ID, 0xB0, Common_read_region},
 	{OV13B10LZ_SENSOR_ID, 0xB0, Common_read_region},
