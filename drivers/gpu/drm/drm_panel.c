bbe8e1a331455dea5080deb92b4316c4e877d015
huangxinglue <wingte77@motorola.com>
2022-07-07 20:28:01 +0800

    drm: Devonn: Add control api for tp single tap
    Add the function in drm path.
    Add the api for tp single tap to control display power sequence.
    
    Change-Id: Ib1dafbc117a5440db8230c1e98e4da37d6e13d73
    Signed-off-by: huangxinglue <wingte77@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2325938
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Dezhen Hou <houdz1@motorola.com>
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/drm_panel.c
+++ drivers/gpu/drm/drm_panel.c
@@ -115,6 +115,18 @@ int drm_panel_prepare(struct drm_panel *panel)
 }
 EXPORT_SYMBOL(drm_panel_prepare);
 
+static int tp_gesture_flag=0;
+int touch_set_state(int state, int panel_idx)
+{
+	if (state == 1) {
+		tp_gesture_flag = 1;
+	} else{
+		tp_gesture_flag = 0;
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL(touch_set_state);
 /**
  * drm_panel_unprepare - power off a panel
  * @panel: DRM panel
@@ -130,6 +142,9 @@ int drm_panel_unprepare(struct drm_panel *panel)
 {
 	if (!panel)
 		return -EINVAL;
+
+	if (panel->funcs && panel->funcs->set_gesture_flag)
+		panel->funcs->set_gesture_flag(tp_gesture_flag);
 
 	if (panel->funcs && panel->funcs->unprepare)
 		return panel->funcs->unprepare(panel);
