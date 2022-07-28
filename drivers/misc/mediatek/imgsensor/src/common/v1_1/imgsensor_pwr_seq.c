d747944bed4dcbd6e6dce9f85b3952be5f19e5a6
wigte87 <wingte87@motorola.com>
2022-07-28 17:27:51 +0800

    devonn: mod ov02b10 camera power sequence[1/1]
    devonn:
    -ov02b10 camera power sequence
    
    Change-Id: Ic7b712add9f1a56cc9f3f11c4c7e4b4dfa294225
    Reviewed-on: https://gerrit.mot.com/2347618
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <wangzl37@motorola.com>
    Reviewed-by: <wingte87@motorola.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Darong Huang <huangdra@motorola.com>
    Reviewed-by: Pengfei Qiao <qiaopf1@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/imgsensor_pwr_seq.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/imgsensor_pwr_seq.c
@@ -60,10 +60,10 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
 			SENSOR_DRVNAME_MOT_DEVONN_OV02B10_MIPI_RAW,
 		{
 			{RST, Vol_Low, 1},
-			{SensorMCLK, Vol_High, 0},
 			{DOVDD, Vol_High, 1},
 			{AVDD, Vol_High, 9},
 			{RST, Vol_High, 2},
+			{SensorMCLK, Vol_High, 0},
 		},
 	},
 #endif
