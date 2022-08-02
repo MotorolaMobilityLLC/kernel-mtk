beb0d426e0333b1bd03c9b715cc9be4b256257db
hq102 <hq102@motorola.com>
2022-08-02 16:26:03 +0800

    drvier:add COM_FG_METER_RESISTANCE for battery ntc read temp compensation
    add COM_FG_METER_RESISTANCE for battery ntc read compensation for the
    question of battery ntc read inaccurate.
    
    Change-Id: Ia3ff759301605a4c05c7ea1385cba0a53e40d0be
    Signed-off-by: hq102 <hq102@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2351741
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wei Xu <xuwei9@lenovo.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/mtk_battery.h
+++ drivers/power/supply/mtk_battery.h
@@ -1092,6 +1092,7 @@ struct mtk_battery {
 	/* battery temperature table */
 	int no_bat_temp_compensate;
 	int enable_tmp_intr_suspend;
+	int fg_current_pn_label;
 	struct battery_temperature_table rbat;
 	struct fg_temp *tmp_table;
 
