60c165f63110d02f8129ee052a7393d3d31f943c
zhangshaohu <Shaohu.zhang@chino-e.com>
2022-08-05 19:25:40 +0800

    Revert "EKmoto_e22i-5538: dispaly: power off/on screen brightness is abnormal"
    This reverts commit 6224298e8d368bc5ac4c53636ddf1744fc70a35b.
    
    Change-Id: I18de134d8c365471abe1dc25cc9db4149c0d1370

--- drivers/misc/mediatek/video/mt6765/videox/disp_helper.c
+++ drivers/misc/mediatek/video/mt6765/videox/disp_helper.c
@@ -346,7 +346,7 @@ void disp_helper_option_init(void)
 
 	/* =================Begin: lowpower option setting================= */
 	disp_helper_set_option(DISP_OPT_SODI_SUPPORT, 1);
-	disp_helper_set_option(DISP_OPT_IDLE_MGR, 0);
+	disp_helper_set_option(DISP_OPT_IDLE_MGR, 1);
 
 	/* 1. vdo mode + screen idle(need idlemgr) */
 	disp_helper_set_option(DISP_OPT_IDLEMGR_SWTCH_DECOUPLE,	1);
@@ -371,7 +371,7 @@ void disp_helper_option_init(void)
 	/* use RGB565 format for decouple mode intermediate buffer */
 	disp_helper_set_option(DISP_OPT_DECOUPLE_MODE_USE_RGB565, 0);
 
-	disp_helper_set_option(DISP_OPT_BYPASS_PQ, 1);
+	disp_helper_set_option(DISP_OPT_BYPASS_PQ, 0);
 	disp_helper_set_option(DISP_OPT_MUTEX_EOF_EN_FOR_CMD_MODE, 1);
 	disp_helper_set_option(DISP_OPT_ESD_CHECK_RECOVERY, 1);
 	disp_helper_set_option(DISP_OPT_ESD_CHECK_SWITCH, 1);
