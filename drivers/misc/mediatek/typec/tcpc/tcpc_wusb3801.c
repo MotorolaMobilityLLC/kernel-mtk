ec442f93847e00a2b51f13ae21754d948b3514cb
houzenan <zenan.hou@chino-e.com>
2022-08-24 14:18:48 +0800

    moto_e22i-285: tuning typec cc orientation as wusb3801 FAE request
    Change-Id: I31e33e4e2db30eb271a9c3c9bc0783c52d449689

--- drivers/misc/mediatek/typec/tcpc/tcpc_wusb3801.c
+++ drivers/misc/mediatek/typec/tcpc/tcpc_wusb3801.c
@@ -483,55 +483,71 @@ static inline int wusb3801_i2c_read8(struct tcpc_device *tcpc, u8 reg)
 #ifdef __TEST_CC_PATCH__
 static int test_cc_patch(struct wusb3801_chip *chip)
 {
-	int rc;
+	int rc_reg_06 = 0;
 	int rc_reg_08 = 0, rc_reg_0f = 0, i = 0;
 
 	struct device *cdev = &chip->client->dev;
-	dev_err(cdev, "%s \n",__func__);
 
-	wusb3801_i2c_write8(chip->tcpc,
-			WUSB3801_REG_TEST_02, 0x82);
-	msleep(100);
-	wusb3801_i2c_write8(chip->tcpc,
-			WUSB3801_REG_TEST_09, 0xC0);
-	msleep(100);
-	rc = wusb3801_i2c_read8(chip->tcpc, WUSB3801_REG_TEST0);
-	msleep(10);
-	wusb3801_i2c_write8(chip->tcpc,
-			WUSB3801_REG_TEST_09, 0x00);
-	msleep(10);
-	wusb3801_i2c_write8(chip->tcpc,
-			WUSB3801_REG_TEST_02, 0x80);
-	msleep(10);
-	wusb3801_i2c_write8(chip->tcpc,
-			WUSB3801_REG_TEST_02, 0x00);
-	pr_err("dhx---add msleep 200\n");
-	dev_err(cdev, "%s rc = [0x%02x] \n", __func__, rc);
+	rc_reg_08 = wusb3801_i2c_read8(chip->tcpc, WUSB3801_REG_TEST_02);
+	dev_err(cdev, "WUSB3801_REG_08 = 0x%02x-------\n", rc_reg_08);
 
-//huanglei add for reg 0x08 write zero fail begin
+	wusb3801_i2c_write8(chip->tcpc, WUSB3801_REG_TEST_02, 0x82);
 	do{
-    		msleep(100);
-        	wusb3801_i2c_write8(chip->tcpc,
-        		WUSB3801_REG_TEST_02, 0x00);
-	    	msleep(100);
-	    	rc_reg_08 = wusb3801_i2c_read8(chip->tcpc, WUSB3801_REG_TEST_02);
-			i++;
-	}while(rc_reg_08 != 0 && i < 5);
-//end
-//huanglei add for reg 0x0F write zero fail begin
-    do{
-	    msleep(100);
-        wusb3801_i2c_write8(chip->tcpc,
-        WUSB3801_REG_TEST_09, 0x00);
-	    msleep(100);
-	    rc_reg_0f = wusb3801_i2c_read8(chip->tcpc, WUSB3801_REG_TEST_09);
-		if (rc_reg_0f < 0) {
-            dev_err(cdev, "%s: WUSB3801_REG_TEST_09 failed to read\n", __func__);
-        }
+		rc_reg_08 = wusb3801_i2c_read8(chip->tcpc, WUSB3801_REG_TEST_02);
+		dev_err(cdev, "WUSB3801_REG_08 = 0x%02x-------000\n", rc_reg_08);
+		if(rc_reg_08 != 0x82)
+			wusb3801_i2c_write8(chip->tcpc, WUSB3801_REG_TEST_02, 0x82);
 		i++;
-	}while(rc_reg_0f !=0 && i < 5);
-//end
-    return BITS_GET(rc, 0x40);
+	}while(rc_reg_08 != 0x82 && i < 5);
+
+	wusb3801_i2c_write8(chip->tcpc, WUSB3801_REG_TEST_09, 0xC0);
+	i = 0;
+	do{
+	    rc_reg_0f = wusb3801_i2c_read8(chip->tcpc, WUSB3801_REG_TEST_09);
+        dev_err(cdev, "WUSB3801_REG_0f = 0x%02x-------111\n", rc_reg_0f);
+		if(rc_reg_0f != 0xC0)
+			wusb3801_i2c_write8(chip->tcpc, WUSB3801_REG_TEST_09, 0xC0);
+		i++;
+	}while(rc_reg_0f != 0xC0 && i < 5);
+
+	if((rc_reg_08 == 0x82) && (rc_reg_0f == 0xC0)){
+		rc_reg_06 = wusb3801_i2c_read8(chip->tcpc, WUSB3801_REG_TEST0);
+		dev_err(cdev, "WUSB3801_REG_06 = 0x%02x\n", rc_reg_06);
+	}
+	else
+		dev_err(cdev, "WUSB3801_REG_06 error!!!\n");
+
+	wusb3801_i2c_write8(chip->tcpc, WUSB3801_REG_TEST_09, 0);
+	i = 0;
+	do{
+	    rc_reg_0f = wusb3801_i2c_read8(chip->tcpc, WUSB3801_REG_TEST_09);
+        dev_err(cdev, "WUSB3801_REG_0f = 0x%02x-------222\n", rc_reg_0f);
+		if(rc_reg_0f != 0x00)
+			wusb3801_i2c_write8(chip->tcpc, WUSB3801_REG_TEST_09, 0);
+		i++;
+	}while(rc_reg_0f != 0 && i < 5);
+
+	wusb3801_i2c_write8(chip->tcpc, WUSB3801_REG_TEST_02, 0x80);
+	i = 0;
+	do{
+		rc_reg_08 = wusb3801_i2c_read8(chip->tcpc, WUSB3801_REG_TEST_02);
+		dev_err(cdev, "WUSB3801_REG_08 = 0x%02x-------333\n", rc_reg_08);
+		if(rc_reg_08 != 0x80)
+			wusb3801_i2c_write8(chip->tcpc, WUSB3801_REG_TEST_02, 0x80);
+		i++;
+	}while(rc_reg_08 != 0x80 && i < 5);
+
+	wusb3801_i2c_write8(chip->tcpc, WUSB3801_REG_TEST_02, 0);
+	i = 0;
+	do{
+		rc_reg_08 = wusb3801_i2c_read8(chip->tcpc, WUSB3801_REG_TEST_02);
+		dev_err(cdev, "WUSB3801_REG_08 = 0x%02x-------444\n", rc_reg_08);
+		if(rc_reg_08 != 0)
+			wusb3801_i2c_write8(chip->tcpc, WUSB3801_REG_TEST_02, 0);
+		i++;
+	}while(rc_reg_08 != 0 && i < 5);
+
+    return BITS_GET(rc_reg_06, 0x40);
 }
 #endif /* __TEST_CC_PATCH__ */
 
@@ -635,6 +651,7 @@ static void wusb3801_irq_work_handler(struct kthread_work *work)
 			}
 		}
 		typec_cc_orientation = BITS_GET(rc, WUSB3801_CC_STS_MASK);
+		pr_err("wusb3801: typec_cc_orientation = %d\n", typec_cc_orientation);
 #endif	/* __TEST_CC_PATCH__ */
 
 		switch (type) {
