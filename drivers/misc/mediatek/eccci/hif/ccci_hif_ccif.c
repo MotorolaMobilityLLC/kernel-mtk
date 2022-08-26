d88f6005d78add92588eb466f3c13c3fc2c336b6
Zilin Wang <Zilin.Wang@mediatek.com>
2022-08-26 13:41:35 +0800

    [ALPS07557250] ccci:Kernel-4.19 add ccif wakeup irq0irq1wdt-irq
    Kernel-4.19 add ccif wakeup source irq0irq1wdt-irq
    
    MTK-Commit-Id: 42d6e1752d259cb7bb4c685437f1df4900306e69
    
    Signed-off-by: Zilin Wang <Zilin.Wang@mediatek.com>
    CR-Id: ALPS07557250
    Feature: ccmni
    Change-Id: Ide62450430eac11d43f7a68f94c7895ed6d04e23
    (cherry-pick from c8e005dc0125799325390dc82a7ab6899a6f6320)
    (cherry-pick from a2e8a3815fceef1bc753c59f5f29b952695ae18c)

--- drivers/misc/mediatek/eccci/hif/ccci_hif_ccif.c
+++ drivers/misc/mediatek/eccci/hif/ccci_hif_ccif.c
@@ -1879,9 +1879,14 @@ static int ccif_late_init(unsigned char hif_id)
 			ccif_ctrl->ap_ccif_irq1_id, ret);
 		return -1;
 	}
+	ret = irq_set_irq_wake(ccif_ctrl->ap_ccif_irq1_id, 1);
+	if (ret)
+		CCCI_ERROR_LOG(ccif_ctrl->md_id, TAG,
+			"irq_set_irq_wake ccif ap_ccif_irq1_id(%d) error %d\n",
+			ccif_ctrl->ap_ccif_irq1_id, ret);
+
 	/*need compare k5.10*/
 	md_ccif_ring_buf_init(CCIF_HIF_ID);
-
 	return 0;
 }
 
@@ -2170,6 +2175,12 @@ static int ccif_hif_hw_init(struct device *dev, struct md_ccif_ctrl *md_ctrl)
 			"request CCIF_AP_DATA IRQ0(%d) error %d\n",
 			md_ctrl->ap_ccif_irq0_id, ret);
 		return -1;
+	}
+	ret = irq_set_irq_wake(md_ctrl->ap_ccif_irq0_id, 1);
+	if (ret){
+		CCCI_ERROR_LOG(md_ctrl->md_id, TAG,
+				"irq_set_irq_wake ccif irq0(%d) error %d\n",
+				md_ctrl->ap_ccif_irq0_id, ret);
 	}
 	return 0;
 
