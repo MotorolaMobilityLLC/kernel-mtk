90502d45f9d5e73018eb1537a36e7d59170b05e6
qiaopf1 <qiaopf1@motorola.com>
2022-08-30 20:19:57 +0800

    devonf: update camera driver for device info
    devonf:
    --update camera driver for device info
    
    Change-Id: I811d2976a1706d10d8dc4781fe5a126de47768c2
    Signed-off-by: qiaopf1 <qiaopf1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2374776
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: <wangzl37@motorola.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_devonf_ov50a_mipi_raw/mot_devonf_ov50amipiraw_Sensor.h
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_devonf_ov50a_mipi_raw/mot_devonf_ov50amipiraw_Sensor.h
@@ -140,6 +140,7 @@ struct imgsensor_info_struct {
 	kal_uint32 min_gain_iso;
 	kal_uint32 gain_step;
 	kal_uint32 exp_step;
+	kal_uint32 exp_step_fullsize;
 	kal_uint32 gain_type;
 	kal_uint8 isp_driving_current; /* mclk driving current */
 	kal_uint8 sensor_interface_type; /* sensor_interface_type */
