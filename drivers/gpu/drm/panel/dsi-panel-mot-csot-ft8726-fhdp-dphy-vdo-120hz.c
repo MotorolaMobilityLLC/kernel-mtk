3409ff81736090d87fb725e53f6b132490218038
houdz1 <houdz1@lenovo.com>
2022-09-10 15:46:37 +0800

    drm/panel: devonn: update the hbm config for these three panels.
    1.For bronco, Set backligt through i2c bus.
    And Set HBM with DCS command.
    2, the max level of bl is 2047
    
    Change-Id: Ie94eff55665f848a22272672571da5ea00096594
    Signed-off-by: houdz1 <houdz1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2382159
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/dsi-panel-mot-csot-ft8726-fhdp-dphy-vdo-120hz.c
+++ drivers/gpu/drm/panel/dsi-panel-mot-csot-ft8726-fhdp-dphy-vdo-120hz.c
@@ -370,6 +370,8 @@ static struct mtk_panel_params ext_params = {
 	},
 	.lfr_enable = 1,
 	.lfr_minimum_fps = 60,
+	.max_bl_level = 2047,
+	.hbm_type = HBM_MODE_DCS_I2C,
 #if 0
 	.dyn = {
 		.switch_en = 1,
@@ -446,6 +448,8 @@ static struct mtk_panel_params ext_params_mode_1 = {
 	},
 	.lfr_enable = 1,
 	.lfr_minimum_fps = 60,
+	.max_bl_level = 2047,
+	.hbm_type = HBM_MODE_DCS_I2C,
 #if 0
 	.dyn = {
 		.switch_en = 1,
@@ -522,6 +526,8 @@ static struct mtk_panel_params ext_params_mode_2 = {
 	},
 	.lfr_enable = 1,
 	.lfr_minimum_fps = 60,
+	.max_bl_level = 2047,
+	.hbm_type = HBM_MODE_DCS_I2C,
 #if 0
 	.dyn = {
 		.switch_en = 1,
