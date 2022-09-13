40f7fbe310b98d4c81794d405f0413450915cc87
saurabh.jairath <saurabh.jairath@mediatek.com>
2022-09-13 13:10:01 +0800

    [ALPS07583128] disp: mt6785: update mmdvfs usage
    [Detail]
    Update usage for mmdvfs flow specific to 6382 build
    
    MTK-Commit-Id: 8710ef86dd4fdf035e6f6024022a7965934ad1f6
    
    Change-Id: I147d4e44d514e6001a957f946ee44037d5e8beb9
    Signed-off-by: saurabh.jairath <saurabh.jairath@mediatek.com>
    Feature: display_mobile
    CR-Id: ALPS07583128
    (cherry picked from commit dc4e25d8fa70b0576211ab56b37f0d517faa6814)

--- drivers/misc/mediatek/mmdvfs/mmdvfs_pmqos.c
+++ drivers/misc/mediatek/mmdvfs/mmdvfs_pmqos.c
@@ -2245,7 +2245,7 @@ int mmdvfs_qos_force_step(int step)
 	}
 	force_step = step;
 #if defined(CONFIG_MACH_MT6785) || defined(CONFIG_MACH_MT6768)
-#if (defined(CONFIG_MTK_MT6382_BDG) && defined(CONFIG_MTK_MT6382_VDO_MODE))
+#if defined(CONFIG_MTK_MT6382_BDG) && defined(CONFIG_MTK_MT6382_VDO_MODE)
 	force_step = 0;
 #endif
 #endif
@@ -2815,7 +2815,12 @@ MODULE_PARM_DESC(force_bwl,
 	"force bwl for each larb");
 
 late_initcall(mmdvfs_pmqos_late_init);
+#if defined(CONFIG_MACH_MT6785) && defined(CONFIG_MTK_MT6382_BDG) \
+	  && defined(CONFIG_MTK_MT6382_CMD_MODE)
+arch_initcall_sync(mmdvfs_pmqos_init);
+#else
 module_init(mmdvfs_pmqos_init);
+#endif
 module_exit(mmdvfs_pmqos_exit);
 
 MODULE_DESCRIPTION("MTK MMDVFS driver");
