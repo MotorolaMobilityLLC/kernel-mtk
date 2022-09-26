5cbcc487930bb936deadc655b2114be2c0995912
wingte73 <wingte73@motorola.com>
2022-09-26 10:56:14 +0800

    motorola/devonn:lcd reset  and suspend deepstandby  for txd csot ft8726 lcd timing
    Modify:
      reset control, suspend cmd2 enable for deepstandby
    
    Change-Id: Ia843462fefa3519fa5a9839dae95d9962bdb81d6
    Reviewed-on: https://gerrit.mot.com/2391181
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Dezhen Hou <houdz1@motorola.com>
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/dsi-panel-mot-tongxingda-ft8726-fhdp-dphy-vdo-120hz.c
+++ drivers/gpu/drm/panel/dsi-panel-mot-tongxingda-ft8726-fhdp-dphy-vdo-120hz.c
@@ -192,10 +192,21 @@ static int tongxingda_unprepare(struct drm_panel *panel)
 	pr_info("%s\n", __func__);
 
 	tongxingda_dcs_write_seq_static(ctx, 0x28);
+	msleep(20);
 	tongxingda_dcs_write_seq_static(ctx, 0x10);
 	msleep(120);
+	tongxingda_dcs_write_seq_static(ctx, 0x00, 0x00);//cmd2 enable
+	tongxingda_dcs_write_seq_static(ctx, 0xFF, 0x87, 0x20, 0x01);
+	tongxingda_dcs_write_seq_static(ctx, 0x00, 0x80);
+	tongxingda_dcs_write_seq_static(ctx, 0xFF, 0x87, 0x20);
+
 	tongxingda_dcs_write_seq_static(ctx, 0x00, 0x00);
 	tongxingda_dcs_write_seq_static(ctx, 0xF7, 0x5A, 0xA5, 0x95, 0x27);
+
+	tongxingda_dcs_write_seq_static(ctx, 0x00, 0x00);//cmd2 disable
+	tongxingda_dcs_write_seq_static(ctx, 0xFF, 0x00, 0x00, 0x00);
+	tongxingda_dcs_write_seq_static(ctx, 0x00, 0x80);
+	tongxingda_dcs_write_seq_static(ctx, 0xFF, 0x00, 0x00);
 
 	if(tp_gesture_flag == 0)
 	{
@@ -229,7 +240,9 @@ static int tongxingda_prepare(struct drm_panel *panel)
 	if (ctx->prepared)
 		return 0;
 
+	gpiod_set_value(ctx->reset_gpio, 0);
 	ret = ocp2138_BiasPower_enable(15,15,5);
+	msleep(5);
 
 	//lcm_power_enable();
 	tongxingda_panel_init(ctx);
