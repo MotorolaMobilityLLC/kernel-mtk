00dbffb755ee84d304fc7d24d5a14c98d4d3e2ad
weiweij <weiweij@lenovo.com>
2022-09-27 18:23:48 +0800

    thermal: reduce logs for thermal
    Lots of thermal logs affect performance.
    But these logs are important,
    so print a log every time when the temperature
    changes by 0.1C.
    
    Change-Id: I10088d767cfaf8a97700bd4d966f803d11d34343
    Signed-off-by: Guocheng Wang <wanggc6@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2394723
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wei Wei <weiweij@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/thermal/mediatek/board_temp.c
+++ drivers/thermal/mediatek/board_temp.c
@@ -231,6 +231,7 @@ static int board_ntc_get_temp(void *data, int *temp)
 	unsigned long long v_in;
 	unsigned int read_tia_reg_time = 0;
 	const unsigned int read_tia_reg_time_max = 3;
+	static int pre_temp = 200 * 10;//200C as init val
 
 	if (adc_data->is_print_tia_cg == true)
 		print_tia_reg(ntc_info->dev);
@@ -348,9 +349,12 @@ static int board_ntc_get_temp(void *data, int *temp)
 		*temp = board_ntc_r_to_temp(ntc_info, r_ntc);
 	}
 
-	//dev_dbg_ratelimited(ntc_info->dev, "val=0x%x, v_in/r_type/r_ntc/t=%d/%d/%d/%d\n",
-	dev_info(ntc_info->dev, "val=0x%x, v_in/r_type/r_ntc/t=%d/%d/%d/%d\n",
-		val, v_in, r_type, r_ntc, *temp);
+	if (pre_temp != *temp / 100) {
+		pre_temp = *temp / 100;
+		//dev_dbg_ratelimited(ntc_info->dev, "val=0x%x, v_in/r_type/r_ntc/t=%d/%d/%d/%d\n",
+		dev_info(ntc_info->dev, "val=0x%x, v_in/r_type/r_ntc/t=%d/%d/%d/%d\n",
+			val, v_in, r_type, r_ntc, *temp);
+	}
 
 	return 0;
 }
