e23b7276abc59e1f69f89390a5594849356baed0
xuwei9 <xuwei9@lenovo.com>
2022-10-01 14:26:53 +0800

    mmi PD 68w bringup
    Bring up PD 68W kernel
    changs.
    
    Change-Id: I87a5c8b33bc5867ac42db37e25d015286e6df2e5
    Signed-off-by: xuwei9 <xuwei9@mt.com>
    Reviewed-on: https://gerrit.mot.com/2396171
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Wang Wang <wangwang1@mt.com>
    Reviewed-on: https://gerrit.mot.com/2409653
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -573,6 +573,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(battery_type),
 	POWER_SUPPLY_ATTR(cycle_counts),
 	POWER_SUPPLY_ATTR(serial_number),
+	POWER_SUPPLY_ATTR(select_mux),
 };
 
 static struct attribute *
