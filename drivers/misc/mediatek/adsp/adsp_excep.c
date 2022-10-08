7a05577eab3146625be05b825cf6993f09d1b6a6
20220705155919 created <wei-yi.yang@mediatek.com>
2022-10-08 12:57:01 +0800

    [ALPS07645382] adsp: avoid double free in adsp_dump
    Using mutex to protect global buffer.
    In order to avoid double free and memory copy from NULL.
    
    MTK-Commit-Id: d7a36aaa5cfbc59a033bc1b662bd442ec76d166e
    
    CR-Id: ALPS07645382
    Feature: ADSP AP Driver
    Change-Id: I0437eb69ed7e5356786fdee527080df2dc29913b
    Signed-off-by: 20220705155919 created <wei-yi.yang@mediatek.com>
    (cherry-pick from a5f3948119b68f6acf6b2453c43dddf9f61cba8a)

--- drivers/misc/mediatek/adsp/adsp_excep.c
+++ drivers/misc/mediatek/adsp/adsp_excep.c
@@ -137,9 +137,11 @@ static int dump_buffer(struct adsp_exception_control *ctrl, int coredump_id)
 	n += dump_adsp_shared_memory(buf + n, total - n, ADSP_A_LOGGER_MEM_ID);
 	n += dump_adsp_shared_memory(buf + n, total - n, ADSP_B_LOGGER_MEM_ID);
 
+	mutex_lock(&ctrl->lock);
 	reinit_completion(&ctrl->done);
 	ctrl->buf_backup = buf;
 	ctrl->buf_size = total;
+	mutex_unlock(&ctrl->lock);
 
 	pr_debug("%s, vmalloc size %u, buffer %p, dump_size %u",
 		 __func__, total, buf, n);
@@ -280,13 +282,20 @@ void adsp_aed_worker(struct work_struct *ws)
 bool adsp_aed_dispatch(enum adsp_excep_id type, void *data)
 {
 	struct adsp_exception_control *ctrl = &excep_ctrl;
+	int ret = 0;
 
-	if (work_busy(&ctrl->aed_work))
+	mutex_lock(&ctrl->lock);
+	if (work_busy(&ctrl->aed_work)) {
+		mutex_unlock(&ctrl->lock);
 		return false;
+	}
 
 	ctrl->excep_id = type;
 	ctrl->priv_data = data;
-	return queue_work(ctrl->workq, &ctrl->aed_work);
+	ret = queue_work(ctrl->workq, &ctrl->aed_work);
+	mutex_unlock(&ctrl->lock);
+
+	return ret;
 }
 
 static void adsp_wdt_counter_reset(struct timer_list *t)
@@ -303,6 +312,8 @@ int init_adsp_exception_control(struct device *dev,
 				struct wait_queue_head *waitq)
 {
 	struct adsp_exception_control *ctrl = &excep_ctrl;
+
+	mutex_init(&ctrl->lock);
 
 	ctrl->waitq = waitq;
 	ctrl->workq = workq;
@@ -445,6 +456,7 @@ static ssize_t adsp_dump_show(struct file *filep, struct kobject *kobj,
 	ssize_t n = 0;
 	struct adsp_exception_control *ctrl = &excep_ctrl;
 
+	mutex_lock(&ctrl->lock);
 	if (ctrl->buf_backup) {
 		n = copy_from_buffer(buf, -1, ctrl->buf_backup,
 			ctrl->buf_size, offset, size);
@@ -458,6 +470,7 @@ static ssize_t adsp_dump_show(struct file *filep, struct kobject *kobj,
 			complete(&ctrl->done);
 		}
 	}
+	mutex_unlock(&ctrl->lock);
 
 	return n;
 }
