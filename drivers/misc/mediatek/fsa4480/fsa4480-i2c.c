4c6a81cbfb8408650c1d4e8940f50a8b752bcc5a
Yunxian He <heyx6@motorola.com>
2022-10-09 09:48:07 +0800

    drivers:add fsa4480 i2c driver
    London use was4780c (same as fsa4480) as typec analog headset
    switch chip.
    add fsa4480 i2c driver.
    use CONFIG_TYPEC_FSA4480 macro to enable.
    
    Change-Id: I0b065b239535f9240d687a3071f8b6cea89d5fa6
    Signed-off-by: Yunxian He <heyx6@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2399296
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Zhengming Yao <yaozm1@mt.com>
    (cherry picked from commit ab54944c66dccf23698c38ae11037e0e92943b59)
    Reviewed-on: https://gerrit.mot.com/2399786
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>

---
+++ drivers/misc/mediatek/fsa4480/fsa4480-i2c.c
@@ -0,0 +1,231 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/* Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/power_supply.h>
+#include <linux/regmap.h>
+#include <linux/i2c.h>
+#include <linux/mutex.h>
+#include <linux/usb/typec.h>
+#include "fsa4480-i2c.h"
+#include <linux/iio/consumer.h>
+
+#define FSA4480_I2C_NAME	"fsa4480-driver"
+
+#define FSA4480_SWITCH_SETTINGS 0x04
+#define FSA4480_SWITCH_CONTROL  0x05
+#define FSA4480_SWITCH_STATUS1  0x07
+#define FSA4480_SLOW_L          0x08
+#define FSA4480_SLOW_R          0x09
+#define FSA4480_SLOW_MIC        0x0A
+#define FSA4480_SLOW_SENSE      0x0B
+#define FSA4480_SLOW_GND        0x0C
+#define FSA4480_DELAY_L_R       0x0D
+#define FSA4480_DELAY_L_MIC     0x0E
+#define FSA4480_DELAY_L_SENSE   0x0F
+#define FSA4480_DELAY_L_AGND    0x10
+#define FSA4480_RESET           0x1E
+
+struct fsa4480_priv {
+	struct regmap *regmap;
+	struct device *dev;
+	struct power_supply *usb_psy;
+	struct notifier_block nb;
+	struct iio_channel *iio_ch;
+	atomic_t usbc_mode;
+	struct work_struct usbc_analog_work;
+	struct blocking_notifier_head fsa4480_notifier;
+	struct mutex notification_lock;
+	u32 use_powersupply;
+	int switch_control;
+};
+
+struct fsa4480_reg_val {
+	u16 reg;
+	u8 val;
+};
+
+static const struct regmap_config fsa4480_regmap_config = {
+	.reg_bits = 8,
+	.val_bits = 8,
+	.max_register = FSA4480_RESET,
+};
+
+static const struct fsa4480_reg_val fsa_reg_i2c_defaults[] = {
+	{FSA4480_SLOW_L, 0x00},
+	{FSA4480_SLOW_R, 0x00},
+	{FSA4480_SLOW_MIC, 0x00},
+	{FSA4480_SLOW_SENSE, 0x00},
+	{FSA4480_SLOW_GND, 0x00},
+	{FSA4480_DELAY_L_R, 0x00},
+	{FSA4480_DELAY_L_MIC, 0x00},
+	{FSA4480_DELAY_L_SENSE, 0x00},
+	{FSA4480_DELAY_L_AGND, 0x09},
+	{FSA4480_SWITCH_SETTINGS, 0x98},
+};
+
+static struct fsa4480_priv *fsa_priv;
+
+static void fsa4480_usbc_update_settings(struct fsa4480_priv *fsa_priv,
+		u32 switch_control, u32 switch_enable)
+{
+	u32 prev_control, prev_enable;
+
+	if (!fsa_priv->regmap) {
+		dev_err(fsa_priv->dev, "%s: regmap invalid\n", __func__);
+		return;
+	}
+
+	regmap_read(fsa_priv->regmap, FSA4480_SWITCH_CONTROL, &prev_control);
+	regmap_read(fsa_priv->regmap, FSA4480_SWITCH_SETTINGS, &prev_enable);
+
+	if (prev_control == switch_control && prev_enable == switch_enable) {
+		dev_dbg(fsa_priv->dev, "%s: settings unchanged\n", __func__);
+		return;
+	}
+
+	regmap_write(fsa_priv->regmap, FSA4480_SWITCH_SETTINGS, 0x80);
+	regmap_write(fsa_priv->regmap, FSA4480_SWITCH_CONTROL, switch_control);
+	/* FSA4480 chip hardware requirement */
+	usleep_range(50, 55);
+	regmap_write(fsa_priv->regmap, FSA4480_SWITCH_SETTINGS, switch_enable);
+}
+
+/*
+ * fsa4480_switch_event - configure FSA switch position based on event
+ *
+ * @node - phandle node to fsa4480 device
+ * @event - fsa_function enum
+ *
+ * Returns int on whether the switch happened or not
+ */
+int fsa4480_switch_event(enum fsa_function event)
+{
+	switch (event) {
+	case FSA_MIC_GND_SWAP:
+		regmap_read(fsa_priv->regmap, FSA4480_SWITCH_CONTROL,
+				&fsa_priv->switch_control);
+		if ((fsa_priv->switch_control & 0x07) == 0x07)
+			fsa_priv->switch_control = 0x0;
+		else
+			fsa_priv->switch_control = 0x7;
+		fsa4480_usbc_update_settings(fsa_priv, fsa_priv->switch_control,
+					     0x9F);
+		break;
+	case FSA_TYPEC_ACCESSORY_AUDIO:
+		/* activate switches */
+		fsa4480_usbc_update_settings(fsa_priv, 0x00, 0x9F);
+		break;
+	case FSA_TYPEC_ACCESSORY_NONE:
+		/* deactivate switches */
+		fsa4480_usbc_update_settings(fsa_priv, 0x18, 0x98);
+	default:
+		break;
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL(fsa4480_switch_event);
+
+static void fsa4480_update_reg_defaults(struct regmap *regmap)
+{
+	u8 i;
+	int ret;
+
+	for (i = 0; i < ARRAY_SIZE(fsa_reg_i2c_defaults); i++)
+		ret = regmap_write(regmap, fsa_reg_i2c_defaults[i].reg,
+				   fsa_reg_i2c_defaults[i].val);
+}
+
+static int fsa4480_probe(struct i2c_client *i2c,
+			 const struct i2c_device_id *id)
+{
+	int rc = 0;
+
+	fsa_priv = devm_kzalloc(&i2c->dev, sizeof(*fsa_priv),
+				GFP_KERNEL);
+	if (!fsa_priv)
+		return -ENOMEM;
+
+	memset(fsa_priv, 0, sizeof(struct fsa4480_priv));
+	fsa_priv->dev = &i2c->dev;
+
+	fsa_priv->regmap = devm_regmap_init_i2c(i2c, &fsa4480_regmap_config);
+	if (IS_ERR_OR_NULL(fsa_priv->regmap)) {
+		dev_err(fsa_priv->dev, "%s: Failed to initialize regmap: %d\n",
+			__func__, rc);
+		if (!fsa_priv->regmap) {
+			rc = -EINVAL;
+			goto err_data;
+		}
+		rc = PTR_ERR(fsa_priv->regmap);
+		goto err_data;
+	}
+
+	fsa4480_update_reg_defaults(fsa_priv->regmap);
+
+	mutex_init(&fsa_priv->notification_lock);
+	i2c_set_clientdata(i2c, fsa_priv);
+
+	return 0;
+
+err_data:
+	devm_kfree(&i2c->dev, fsa_priv);
+	return rc;
+}
+
+static int fsa4480_remove(struct i2c_client *i2c)
+{
+	struct fsa4480_priv *fsa_priv =
+			(struct fsa4480_priv *)i2c_get_clientdata(i2c);
+
+	if (!fsa_priv)
+		return -EINVAL;
+
+
+	fsa4480_usbc_update_settings(fsa_priv, 0x18, 0x98);
+	pm_relax(fsa_priv->dev);
+	mutex_destroy(&fsa_priv->notification_lock);
+	dev_set_drvdata(&i2c->dev, NULL);
+
+	return 0;
+}
+
+static const struct of_device_id fsa4480_i2c_dt_match[] = {
+	{
+		.compatible = "fsa,fsa4480-i2c",
+	},
+	{}
+};
+
+static struct i2c_driver fsa4480_i2c_driver = {
+	.driver = {
+		.name = FSA4480_I2C_NAME,
+		.of_match_table = fsa4480_i2c_dt_match,
+	},
+	.probe = fsa4480_probe,
+	.remove = fsa4480_remove,
+};
+
+static int __init fsa4480_init(void)
+{
+	int rc;
+
+	rc = i2c_add_driver(&fsa4480_i2c_driver);
+	if (rc)
+		pr_err("fsa4480: Failed to register I2C driver: %d\n", rc);
+
+	return rc;
+}
+module_init(fsa4480_init);
+
+static void __exit fsa4480_exit(void)
+{
+	i2c_del_driver(&fsa4480_i2c_driver);
+}
+module_exit(fsa4480_exit);
+
+MODULE_DESCRIPTION("FSA4480 I2C driver");
+MODULE_LICENSE("GPL v2");
