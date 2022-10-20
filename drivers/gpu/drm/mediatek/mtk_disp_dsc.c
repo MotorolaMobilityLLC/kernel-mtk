420c521a2788294ff32205f6b0acc6680ce56a48
Xiaoqing Li <ot_xiaoqing.li@mediatek.com>
2022-10-20 14:03:07 +0800

    [ALPS07644522] DISP: add dsc 10 bpc mode
    modify dsc pps table if bit_per_channel == 10
    
    MTK-Commit-Id: 44aa3f88aedc630756a047d12e6304e3a6ce6602
    
    CR-Id: ALPS07644522
    Feature: display_mobile
    Signed-off-by: Xiaoqing Li <ot_xiaoqing.li@mediatek.com>
    Change-Id: I211ecba740bbcf31c1e260aba611e6ad768d0c74

--- drivers/gpu/drm/mediatek/mtk_disp_dsc.c
+++ drivers/gpu/drm/mediatek/mtk_disp_dsc.c
@@ -867,7 +867,24 @@ static void mtk_dsc_config(struct mtk_ddp_comp *comp,
 				((dsc_params->rc_range_parameters[14].range_max_qp & 0x1f) << 5) |
 				(dsc_params->rc_range_parameters[14].range_min_qp & 0x1f);
 			mtk_ddp_write_relaxed(comp, reg_val, DISP_REG_DSC_PPS19, handle);
+		} else if (dsc_params->bit_per_channel == 10) {
+			//10bpc_to_8bpp_20_slice_h
+			mtk_ddp_write(comp, 0x20001007, DISP_REG_DSC_PPS6, handle);
+			mtk_ddp_write(comp, 0x330F0F06, DISP_REG_DSC_PPS7, handle);
+			mtk_ddp_write(comp, 0x382a1c0e, DISP_REG_DSC_PPS8, handle);
+			mtk_ddp_write(comp, 0x69625446, DISP_REG_DSC_PPS9, handle);
+			mtk_ddp_write(comp, 0x7b797770, DISP_REG_DSC_PPS10, handle);
+			mtk_ddp_write(comp, 0x00007e7d, DISP_REG_DSC_PPS11, handle);
+			mtk_ddp_write(comp, 0x01040900, DISP_REG_DSC_PPS12, handle);
+			mtk_ddp_write(comp, 0xF9450125, DISP_REG_DSC_PPS13, handle);
+			mtk_ddp_write(comp, 0xE967F167, DISP_REG_DSC_PPS14, handle);
+			mtk_ddp_write(comp, 0xE187E167, DISP_REG_DSC_PPS15, handle);
+			mtk_ddp_write(comp, 0xD9C7E1A7, DISP_REG_DSC_PPS16, handle);
+			mtk_ddp_write(comp, 0xD1E9D9C9, DISP_REG_DSC_PPS17, handle);
+			mtk_ddp_write(comp, 0xD20DD1E9, DISP_REG_DSC_PPS18, handle);
+			mtk_ddp_write(comp, 0x0000D230, DISP_REG_DSC_PPS19, handle);
 		} else {
+			//8bpc_to_8bpp_20_slice_h
 			mtk_ddp_write(comp, 0x20000c03, DISP_REG_DSC_PPS6, handle);
 			mtk_ddp_write(comp, 0x330b0b06, DISP_REG_DSC_PPS7, handle);
 			mtk_ddp_write(comp, 0x382a1c0e, DISP_REG_DSC_PPS8, handle);
