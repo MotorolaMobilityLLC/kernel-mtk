f3e355a882b24d4d8d7b28cd4d9d286cbacee2eb
huangsh4 <huangsh4@lenovo.com>
2022-10-24 14:52:07 +0800

    lyriq: HI1336 DW9800 AF bring up[1/3]
    CAMERA:
    Bringup dw9800 driver ic for wide camera.
    
    Change-Id: I3118ad98814def8aa2c9bb5403d5cadb1d4fefcc
    Signed-off-by: huangsh4 <huangsh4@mt.com>
    Reviewed-on: https://gerrit.mot.com/2411999
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Pengfei Qiao <qiaopf1@motorola.com>
    Reviewed-by: <wangzl37@motorola.com>
    Reviewed-by: Suna Li <lisn17@motorola.com>
    Reviewed-by: Darong Huang <huangdra@motorola.com>
    Reviewed-by: Xu Ji <jixu@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Shenhuai Huang <huangsh4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lens/mtk/main2/inc/lens_list.h
+++ drivers/misc/mediatek/lens/mtk/main2/inc/lens_list.h
@@ -145,5 +145,11 @@ extern long DW9718TAF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
 			    unsigned long a_u4Param);
 extern int DW9718TAF_Release(struct inode *a_pstInode, struct file *a_pstFile);
 extern int DW9718TAF_GetFileName(unsigned char *pFileName);
+extern int MOT_LYRIQ_DW9800AF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+                                 spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long MOT_LYRIQ_DW9800AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+                           unsigned long a_u4Param);
+extern int MOT_LYRIQ_DW9800AF_Release(struct inode *a_pstInode, struct file *a_pstFile);
+extern int MOT_LYRIQ_DW9800AF_GetFileName(unsigned char *pFileName);
 
 #endif
