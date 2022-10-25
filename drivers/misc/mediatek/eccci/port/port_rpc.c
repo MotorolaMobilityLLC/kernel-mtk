f6c3eb9769aec1b01ef41dcda69aeb0f28acc19a
xiewen3 <xiewen3@motorola.com>
2022-10-25 17:00:48 +0800

    drivers/ccci: Support the modem CID secure authentication
    Performing the secure authentication in Modem against the dangerous AT/Meta
    commands are critical. Previously we used the approach to securely protect the
    modem boot mode via the MTK special register(secure write only in ATF),
    but here on mt6855, the register cannot be provided though.
    Thus we would have to seek the alternative solution to maintain the signed
    Motorola data for the auth. The idea is to use the shared memory carved out from
    the LK and pass it over to modem for the ephemeral data management.
    
    Change-Id: I5ddfecd8d4fab5872ce2c4b48cf45547a83ddfbc
    Signed-off-by: xiewen3 <xiewen3@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2415739
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/eccci/port/port_rpc.c
+++ drivers/misc/mediatek/eccci/port/port_rpc.c
@@ -1175,7 +1175,42 @@ static void ccci_rpc_work_helper(struct port_t *port, struct rpc_pkt *pkt,
 			1, pkt[1].len, *((unsigned int *)pkt[1].buf));
 			break;
 		}
+#ifdef CONFIG_MOTO_CCCI_SEC_SUPPORT
+	case IPC_RPC_CIDDATA_OP:
+		{
+			struct ccci_security_data_t *security_data = NULL;
+			unsigned int security_data_len = 0;
+			unsigned int offset = 0;
+			unsigned int stepsize = 0;
+			offset = *(unsigned int *)(pkt[0].buf) & 0xffff;
+			stepsize = (*(unsigned int *)(pkt[0].buf)>>16) & 0xffff;
 
+			CCCI_NORMAL_LOG(md_id, RPC,
+				"enter IPC_RPC_CIDDATA_OP offset :%d, stepsize:%d\n", offset, stepsize);
+			security_data = ccci_rpc_get_security_data(&security_data_len);
+
+			if (!security_data || security_data_len <= 0) {
+
+				CCCI_ERROR_LOG(md_id, RPC, "%s:get security_data fail\n", __func__);
+				tmp_data[0] = -1;
+				pkt_num = 0;
+				pkt[pkt_num].len = sizeof(unsigned int);
+				pkt[pkt_num++].buf = (void *)(&tmp_data[0]);
+				pkt[pkt_num].len = sizeof(unsigned int);
+				pkt[pkt_num++].buf = (void *)(&tmp_data[1]);
+				break;
+			}
+
+			pkt_num = 0;
+			pkt[pkt_num].len = sizeof(unsigned int);
+			pkt[pkt_num++].buf = (void *)(&tmp_data[0]);
+
+			pkt[pkt_num].len = stepsize;
+			pkt[pkt_num++].buf = (void *)(&security_data->cid_data[offset]);
+
+			break;
+		}
+#endif
 	default:
 		CCCI_NORMAL_LOG(md_id, RPC,
 		"[Error]Unknown Operation ID (0x%08X)\n",
