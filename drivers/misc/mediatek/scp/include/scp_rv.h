ce268d6c0fcc181b574a40b0a1de0b3c49d3d129
liuwang2 <liuwang2@lenovo.com>
2022-11-02 11:17:13 +0800

    lyriq: boost scp cpu when sois working
    [detail]
    set sois frequency to 416Mhz
    
    Change-Id: Iad16428adb972f72b220ed31f2a29bb5b8bb341b
    Signed-off-by: liuwang2 <liuwang2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2418460
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yuchang Guo <guoyc1@lenovo.com>
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: XiaoFeng Wang <wangxf32@lenovo.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/scp/include/scp_rv.h
+++ drivers/misc/mediatek/scp/include/scp_rv.h
@@ -198,6 +198,13 @@ extern phys_addr_t scp_get_reserve_mem_size(enum scp_reserve_mem_id_t id);
 extern void scp_register_feature(enum feature_id id);
 extern void scp_deregister_feature(enum feature_id id);
 
+#ifdef CONFIG_SOIS_BOOST_CPU
+extern void scp_register_sensor(enum feature_id id,
+		int sensor_id);
+extern void scp_deregister_sensor(enum feature_id id,
+		int sensor_id);
+#endif
+
 /* APIs for reset scp */
 extern void scp_wdt_reset(int cpu_id);
 
