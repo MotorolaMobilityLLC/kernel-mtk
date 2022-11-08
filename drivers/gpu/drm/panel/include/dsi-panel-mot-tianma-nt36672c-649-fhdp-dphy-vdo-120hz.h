b2be73436611beee85d177e97ff3a5627476e597
djw <longc143@motorola.com>
2022-11-08 15:55:01 +0800

    drm/panel:devonf:update panel settings for RF desense
    devonf: update panel init code and panel porch settings
    for RF desense
    
    Change-Id: I8911f367d4503b27b3017f836cb7b351bf7aec49
    Signed-off-by: djw <longc143@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2423647
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jing Hu <hujing3@lenovo.com>
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/include/dsi-panel-mot-tianma-nt36672c-649-fhdp-dphy-vdo-120hz.h
+++ drivers/gpu/drm/panel/include/dsi-panel-mot-tianma-nt36672c-649-fhdp-dphy-vdo-120hz.h
@@ -21,7 +21,7 @@
 
 #define DATA_RATE                   1100
 #define HSA                         4//16
-#define HBP                         90//52
+#define HBP                         94//52
 #define VSA                         2//4
 #define VBP                         18
 
@@ -31,28 +31,28 @@
 /*Parameter setting for mode 0 Start*/
 #define MODE_60_FPS                  60
 #define MODE_60_VFP                  2528
-#define MODE_60_HFP                  77
+#define MODE_60_HFP                  80
 #define MODE_60_DATA_RATE            1100
 /*Parameter setting for mode 0 End*/
 
 /*Parameter setting for mode 30 Start*/
 #define MODE_30_FPS                  30
 #define MODE_30_VFP                  7476
-#define MODE_30_HFP                  77
+#define MODE_30_HFP                  80
 #define MODE_30_DATA_RATE            1100
 /*Parameter setting for mode 30 End*/
 
 /*Parameter setting for mode 1 Start*/
 #define MODE_90_FPS                  90
 #define MODE_90_VFP                  879
-#define MODE_90_HFP                  77
+#define MODE_90_HFP                  80
 #define MODE_90_DATA_RATE            1100
 /*Parameter setting for mode 1 End*/
 
 /*Parameter setting for mode 2 Start*/
 #define MODE_120_FPS                  120
 #define MODE_120_VFP                  54
-#define MODE_120_HFP                  77
+#define MODE_120_HFP                  80
 #define MODE_120_DATA_RATE            1100
 /*Parameter setting for mode 2 End*/
 
