b9dfc6b0981424885a65722b812c45bcdcbb10a8
lbh <longc134@motorola.com>
2022-11-16 11:48:21 +0800

    kernel/drm/mtk_disp:add ESD recovery notify events
    mtk disp: support TP recover notify for ESD case
    some touch IC need notify when ESD recover
    
    Change-Id: I4f51513ef59ca0a2a4a7373d94dc1a3daba3c712
    Signed-off-by: lbh <longc134@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2430582
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Jing Hu <hujing3@lenovo.com>
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_notify.h
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_notify.h
@@ -15,11 +15,22 @@
 #define MTK_DISP_EARLY_EVENT_BLANK	0x00
 #define MTK_DISP_EVENT_BLANK		0x01
 
+#ifdef CONFIG_DISP_ESD_NOTIFY_SUPPORT
+/*add event for display esd recovery notifier event*/
+#define MTK_ERALY_NOTIFY_TP_EVENT_BLANK           0x02
+#define MTK_NOTIFY_TP_EVENT_BLANK                 0x03
+#endif
 enum {
 	/* disp power on */
 	MTK_DISP_BLANK_UNBLANK,
 	/* disp power off */
 	MTK_DISP_BLANK_POWERDOWN,
+#ifdef CONFIG_DISP_ESD_NOTIFY_SUPPORT
+	/*disp esd power on*/
+	MTK_DISP_EARLY_BLANK_UNBLANK,
+	/*disp esd power off*/
+	MTK_DISP_EARLY_BLANK_POWERDOWN,
+#endif
 };
 
 int mtk_disp_notifier_register(const char *source, struct notifier_block *nb);
