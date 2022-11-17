5b91baf2366696f84999e5f5c32b8bc7feb9268d
Weiqiang Hao <weiqiang.hao@mediatek.com>
2022-11-17 13:36:31 +0800

    [ALPS07731983] ccci: set dpmaif irq wakeup source
    dpmaif_irq set wakeup source
    
    MTK-Commit-Id: 3d8375cd26629161fb0d0356703129e4e917c7ef
    
    Change-Id: Ib82ea311f7afd579869a5aab6f5ddd1830a220a9
    Signed-off-by: Weiqiang Hao <weiqiang.hao@mediatek.com>
    CR-Id: ALPS07731983
    Feature: CCCI_HIF_DATA
    (cherry picked from commit 30c3b0c057b34ee95a4a6001f51462c129473daf)
    (cherry picked from commit 40491c13432800820b9e6df25d30ca3005593788)

--- drivers/misc/mediatek/eccci/hif/ccci_hif_dpmaif.c
+++ drivers/misc/mediatek/eccci/hif/ccci_hif_dpmaif.c
@@ -3802,6 +3802,13 @@ int dpmaif_late_init(unsigned char hif_id)
 			dpmaif_ctrl->dpmaif_irq_id, ret);
 		return ret;
 	}
+
+	ret = irq_set_irq_wake(dpmaif_ctrl->dpmaif_irq_id, 1);
+	if (ret)
+		CCCI_ERROR_LOG(dpmaif_ctrl->md_id, TAG,
+			"irq_set_irq_wake dpmaif irq(%d) error %d\n",
+			dpmaif_ctrl->dpmaif_irq_id, ret);
+
 	atomic_set(&dpmaif_ctrl->dpmaif_irq_enabled, 1); /* init */
 	dpmaif_disable_irq(dpmaif_ctrl);
 
