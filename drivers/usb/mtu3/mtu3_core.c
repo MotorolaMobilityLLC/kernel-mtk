d26acd08afec194d6616af816fd5208fdb24ee95
yogesh.tundele <yogesh.tundele@mediatek.com>
2022-11-19 14:56:58 +0800

    [ALPS07728371] MTU3: Disable LGO
    [Detail]
    During U3 link if Host wait for transfer complete and Device nuke Eps,
    transfer remain unfinished and when enter LGO_ux,
    cause undefined symbol on USB bus,
    cause Device function to stop working
    
    MTK-Commit-Id: 6a2ec36f9f6787eb82c49f654b765cc30ffdf630
    
    Change-Id: Ic36500dc0ee3254f998f04fa67dccee71916b736
    CR-Id: ALPS07728371
    Signed-off-by: yogesh.tundele <yogesh.tundele@mediatek.com>
    Feature: [Android Default] MTP/PTP (Media File Transfer / Picture File Transfer)
    (cherry-pick from e9acfd9b7e31254699855d8b8853b8cefccd7444)

--- drivers/usb/mtu3/mtu3_core.c
+++ drivers/usb/mtu3/mtu3_core.c
@@ -245,12 +245,16 @@ static void mtu3_regs_init(struct mtu3 *mtu)
 		mtu3_clrbits(mbase, U3D_LINK_POWER_CONTROL,
 				SW_U1_REQUEST_ENABLE | SW_U2_REQUEST_ENABLE);
 		/* enable accept LGO_U1/U2 link command from host */
-		mtu3_setbits(mbase, U3D_LINK_POWER_CONTROL,
+		if (!of_find_compatible_node(NULL, NULL, "mediatek,MT6893")) {
+			dev_info(mtu->ssusb->dev, "enable accept_lgo\n");
+			mtu3_setbits(mbase, U3D_LINK_POWER_CONTROL,
 				SW_U1_ACCEPT_ENABLE | SW_U2_ACCEPT_ENABLE);
-		mtu3_setbits(mbase, U3D_MAC_U1_EN_CTRL,
+			mtu3_setbits(mbase, U3D_MAC_U1_EN_CTRL,
 				ACCEPT_BMU_RX_EMPTY_HCK);
-		mtu3_setbits(mbase, U3D_MAC_U2_EN_CTRL,
+			mtu3_setbits(mbase, U3D_MAC_U2_EN_CTRL,
 				ACCEPT_BMU_RX_EMPTY_HCK);
+		} else
+			dev_info(mtu->ssusb->dev, "disable accept_lgo\n");
 		/* device responses to u3_exit from host automatically */
 		mtu3_clrbits(mbase, U3D_LTSSM_CTRL, SOFT_U3_EXIT_EN);
 		/* automatically build U2 link when U3 detect fail */
