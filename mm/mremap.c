6eba51cdc7fb0c14e9eafacd2d7f0d7f0f6b51ea
Suren Baghdasaryan <surenb@google.com>
2022-11-18 17:06:03 -0800

    ANDROID: disable page table moves when speculative page faults are enabled
    move_page_tables() can move entire pmd or pud without locking individual
    ptes. This is problematic for speculative page faults which do not take
    mmap_lock because they rely on ptl lock when writing new pte value. To
    avoid possible race, disable move_page_tables() optimization when
    CONFIG_SPECULATIVE_PAGE_FAULT is enabled.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2023-20937
    
    Bug: 257443051
    Change-Id: Ib48dda08ecad1abc60d08fc089a6566a63393c13
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Lee Jones <joneslee@google.com>
    (cherry picked from commit e4ec065a37be637a6740e642c08053b8b4714727)
    Signed-off-by: Gajjala Chakradhar <gajjalac@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2496115
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key

--- mm/mremap.c
+++ mm/mremap.c
@@ -242,7 +242,11 @@ static inline void unlock_vma_ref_count(struct vm_area_struct *vma)
 }
 #endif	/* CONFIG_SPECULATIVE_PAGE_FAULT */
 
-#ifdef CONFIG_HAVE_MOVE_PMD
+/*
+ * Speculative page fault handlers will not detect page table changes done
+ * without ptl locking.
+ */
+#if defined(CONFIG_HAVE_MOVE_PMD) && !defined(CONFIG_SPECULATIVE_PAGE_FAULT)
 static bool move_normal_pmd(struct vm_area_struct *vma, unsigned long old_addr,
 		  unsigned long new_addr, pmd_t *old_pmd, pmd_t *new_pmd)
 {
@@ -318,7 +322,11 @@ static inline bool move_normal_pmd(struct vm_area_struct *vma,
 }
 #endif
 
-#ifdef CONFIG_HAVE_MOVE_PUD
+/*
+ * Speculative page fault handlers will not detect page table changes done
+ * without ptl locking.
+ */
+#if defined(CONFIG_HAVE_MOVE_PUD) && !defined(CONFIG_SPECULATIVE_PAGE_FAULT)
 static bool move_normal_pud(struct vm_area_struct *vma, unsigned long old_addr,
 		  unsigned long new_addr, pud_t *old_pud, pud_t *new_pud)
 {
