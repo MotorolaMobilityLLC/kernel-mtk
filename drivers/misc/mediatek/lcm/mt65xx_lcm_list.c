e97e7d563bab6c67e92ecb412a36109af8a6b602
hujing3 <hujing3@lenovo.com>
2022-11-21 16:19:02 +0800

    kernel/lcm:gnevan 2nd panel rename, remove temp 678 panel
    gnevan 2nd panel
    panel rename with correct panel vendor
    remove useless temp 6.78 panel
    
    Change-Id: If8b413ca1adc7243bc6128df20199c2f0d1c0269
    Signed-off-by: hujing3 <hujing3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2434721
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.c
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.c
@@ -1296,8 +1296,8 @@ struct LCM_DRIVER *lcm_driver_list[] = {
         &mipi_mot_vid_tm_ili7807s_hdp_653_lcm_drv,
 #endif
 
-#if defined(DSI_PANEL_MOT_MIPI_VID_COST_NT36672C_653_HDP)
-        &mipi_mot_vid_cost_nt36672c_hdp_653_lcm_drv,
+#if defined(DSI_PANEL_MOT_MIPI_VID_CSOT_NT36672C_653_HDP)
+        &mipi_mot_vid_csot_nt36672c_hdp_653_lcm_drv,
 #endif
 
 #if defined(DSI_PANEL_MOT_MIPI_VID_TM_ILI7807S_678_HDP)
