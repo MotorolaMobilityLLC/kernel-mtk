24bfe5bdefaa8ba7f349588d9a475b55cb81ef60
hujing3 <hujing3@lenovo.com>
2022-11-21 16:19:02 +0800

    kernel/lcm:gnevan 2nd panel rename, remove temp 678 panel
    gnevan 2nd panel
    panel rename with correct panel vendor
    remove useless temp 6.78 panel
    
    Change-Id: If8b413ca1adc7243bc6128df20199c2f0d1c0269
    Signed-off-by: hujing3 <hujing3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2434721
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
@@ -17,7 +17,7 @@ extern struct LCM_DRIVER mipi_mot_vid_djn_nt36525b_a_hdp_652_lcm_drv;
 extern struct LCM_DRIVER mipi_mot_vid_boe_nt36525c_652_hdp_lcm_drv;
 extern struct LCM_DRIVER mipi_mot_vid_tm_ili7807s_hdp_653_lcm_drv;
 extern struct LCM_DRIVER mipi_mot_vid_tm_ili7807s_hdp_678_lcm_drv;
-extern struct LCM_DRIVER mipi_mot_vid_cost_nt36672c_hdp_653_lcm_drv;
+extern struct LCM_DRIVER mipi_mot_vid_csot_nt36672c_hdp_653_lcm_drv;
 extern struct LCM_DRIVER mipi_mot_vid_tm_ili9883a_652_hdp_lcm_drv;
 extern struct LCM_DRIVER mipi_mot_vid_tm_icnl9916_652_hdp_lcm_drv;
 extern struct LCM_DRIVER mipi_mot_vid_txd_icnl9911c_652_hdp_lcm_drv;
