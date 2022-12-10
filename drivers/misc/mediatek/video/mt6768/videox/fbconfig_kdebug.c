099a1e5195b060fe94802a2e91f5c2bbd13f6fd3
ot_lalit.wadhwa <ot_lalit.wadhwa@mediatek.com>
2022-12-10 10:28:53 +0800

    [ALPS07671046] disp: mt6765/6768/6779:Update for security vulnerability
    [Detail] Potential security threat in the ioctl of fbconfig driver.
    
    [Solution] use lock to safeguard against race condition
    
    MTK-Commit-Id: e9986cb780fcb859989b6eea21bce1063130cee6
    
    Change-Id: If26d188e3423df7db847280905d463a5e1dc1b28
    CR-Id: ALPS07671046
    Feature: display_mobile
    Signed-off-by: ot_lalit.wadhwa <ot_lalit.wadhwa@mediatek.com>
    (cherry-pick from b2b64a2e357a57834c356d54eff6de4673f29878)

--- drivers/misc/mediatek/video/mt6768/videox/fbconfig_kdebug.c
+++ drivers/misc/mediatek/video/mt6768/videox/fbconfig_kdebug.c
@@ -117,6 +117,8 @@ static struct PM_TOOL_S pm_params = {
 	.pLcm_drv = NULL,
 };
 
+struct mutex fb_config_lock;
+
 static void *pm_get_handle(void)
 {
 	return (void *)&pm_params;
@@ -211,6 +213,7 @@ static int fbconfig_open(struct inode *inode, struct file *file)
 	struct PM_TOOL_S *pm_params = NULL;
 
 	file->private_data = inode->i_private;
+	mutex_init(&fb_config_lock);
 	pm_params = (struct PM_TOOL_S *) pm_get_handle();
 	if (pm_params == NULL) {
 		pr_debug("fbconfig_open=>pm_params is empty!!\n");
@@ -322,7 +325,9 @@ static long fbconfig_ioctl(struct file *file, unsigned int cmd,
 			record_tmp_list = NULL;
 			return -EFAULT;
 		}
+		mutex_lock(&fb_config_lock);
 		list_add(&record_tmp_list->list, &head_list.list);
+		mutex_unlock(&fb_config_lock);
 		return 0;
 	}
 	case DRIVER_IC_CONFIG_DONE:
@@ -330,7 +335,9 @@ static long fbconfig_ioctl(struct file *file, unsigned int cmd,
 		/* print_from_head_to_tail(); */
 		Panel_Master_dsi_config_entry("PM_DDIC_CONFIG", NULL);
 		/*free the memory ..... */
+		mutex_lock(&fb_config_lock);
 		free_list_memory();
+		mutex_unlock(&fb_config_lock);
 		return 0;
 	}
 	case MIPI_SET_CC:
