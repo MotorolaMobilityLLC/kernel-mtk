ff4abfeee358ea2b023e9dcb26a5dec91aa33be6
xieqiong2 <xieqiong2@lenovo.com>
2022-12-12 17:15:52 +0800

    mtk:[PATCH] [ALPS07741525] cmdq: modify check usage.
    From 6a2a6ab58e0dc77d4d8e04fb2a237c0353ff7b59 Mon Sep 17 00:00:00 2001
    From: mtk20335 <Rose-YH.Kuo@mediatek.com>
    Date: Tue, 22 Nov 2022 14:48:18 +0800
    Subject: [PATCH] [ALPS07741525] cmdq: modify check usage
    
    1. export cmdq_pkt_is_exec
    2. add check usage cb
    3. dump thread usage
    4. return ref count after mbox enable/disable
    
    Signed-off-by: mtk20335 <Rose-YH.Kuo@mediatek.com>
    CR-Id: ALPS07741525
    Feature: cmdq_gce
    (cherry picked from commit 560627a8daa33be08ca2f4963ce804cdcfb937a7)
    
    Change-Id: Ide0038f0094b2dc991715f09f26078f485e1e585
    Signed-off-by: xieqiong2 <xieqiong2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2459572
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Submit-Approved: Suting Xie <xiest1@motorola.com>
    Reviewed-by: Hao Li <lihao24@motorola.com>

--- include/linux/mailbox/mtk-cmdq-mailbox-ext.h
+++ include/linux/mailbox/mtk-cmdq-mailbox-ext.h
@@ -23,6 +23,8 @@ struct cmdq_util_controller_fp {
 typedef bool (*cmdq_mminfra_power)(void);
 typedef bool (*cmdq_mminfra_gce_cg)(u32);
 
+typedef void (*cmdq_usage_cb)(u32);
+
 void cmdq_controller_set_fp(struct cmdq_util_controller_fp *cust_cmdq_util);
 #endif
 
@@ -192,6 +194,8 @@ struct cmdq_thread {
 	u64			lock_time;
 	u64			irq_time;
 	u32			irq_task;
+	atomic_t		usage;
+	cmdq_usage_cb usage_cb;
 };
 
 extern int mtk_cmdq_log;
@@ -288,8 +292,8 @@ void cmdq_thread_dump_all(void *mbox_cmdq, const bool lock, const bool dump_pkt,
 void cmdq_thread_dump_all_seq(void *mbox_cmdq, struct seq_file *seq);
 void cmdq_mbox_thread_remove_task(struct mbox_chan *chan,
 	struct cmdq_pkt *pkt);
-void cmdq_mbox_enable(void *chan);
-void cmdq_mbox_disable(void *chan);
+s32 cmdq_mbox_enable(void *chan);
+s32 cmdq_mbox_disable(void *chan);
 s32 cmdq_mbox_get_usage(void *chan);
 void *cmdq_mbox_get_base(void *chan);
 phys_addr_t cmdq_mbox_get_base_pa(void *chan);
@@ -329,6 +333,7 @@ void cmdq_mmp_wait(struct mbox_chan *chan, void *pkt);
 s32 cmdq_sec_insert_backup_cookie(struct cmdq_pkt *pkt);
 void cmdq_mbox_dump_dbg(void *mbox_cmdq, void *chan, const bool lock);
 void cmdq_chan_dump_dbg(void *chan);
+void cmdq_get_usage_cb(struct mbox_chan *chan, cmdq_usage_cb usage_cb);
 void cmdq_get_mminfra_cb(cmdq_mminfra_power cb);
 void cmdq_get_mminfra_gce_cg_cb(cmdq_mminfra_gce_cg cb);
 void cmdq_dump_usage(void);
