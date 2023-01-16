73ad2cf5dbb42bdafc0b5acd3192e02fd4d19b3a
houdz1 <houdz1@lenovo.com>
2023-01-16 17:44:30 +0800

    drm/panel: aion: Optimize panel power down timing
    Optimize panel power down timing
    1. rest low->delay 1ms->power down dvdd
    2. do not power down the buck of dvdd.
    
    Change-Id: Ifb7d0bdffb6853a7722b9b982bfdfdc2ed1dad65
    Signed-off-by: houdz1 <houdz1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2488450
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/dsi-panel-mot-csot-vtdr6115-655-fhdp-dphy-vdo-144hz.c
+++ drivers/gpu/drm/panel/dsi-panel-mot-csot-vtdr6115-655-fhdp-dphy-vdo-144hz.c
@@ -232,6 +232,7 @@ static int gate_ic_Power_on(struct drm_panel *panel, int enabled)
 		}
 	} else {
 		for (i=3; i >= 0; i--) {
+			if (i == 2) continue;
 			pm_en_pin = NULL;
 			pm_en_pin = devm_gpiod_get_index(ctx->dev, "pm-enable", i, GPIOD_OUT_LOW);
 			if (IS_ERR(pm_en_pin)) {
@@ -980,6 +981,7 @@ static int panel_ext_powerdown(struct drm_panel *panel)
 	ctx->reset_gpio = devm_gpiod_get(ctx->dev, "reset", GPIOD_OUT_HIGH);
 	gpiod_set_value(ctx->reset_gpio, 0);
 	devm_gpiod_put(ctx->dev, ctx->reset_gpio);
+	usleep_range(1000, 1001);
 
 	gate_ic_Power_on(panel, 0);
 
