d834bce0464fffff0982fc7f41cbf06bedf6327d
tangguoxuan <am2203@motorola.com>
2023-02-01 16:01:26 +0800

    Lyriq: fix Camera ITS fail: sensor_fusion
    [details]
    Camera ITS Test Camera: 0/1,sensor_fusion
    
    Change-Id: I6984104d66f2bd8573aef434f5635b2d889a66f7
    Signed-off-by: tangguoxuan <am2203@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2499421
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <wangyc27@mt.com>
    Reviewed-by: <wuyh23@motorola.com>
    Reviewed-by: <mingjw1@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_lyriq_ov50a_mipi_raw/mot_lyriq_ov50amipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_lyriq_ov50a_mipi_raw/mot_lyriq_ov50amipiraw_Sensor.c
@@ -2201,7 +2201,7 @@ static kal_uint32 feature_control(MSDK_SENSOR_FEATURE_ENUM feature_id,
 		break;
 	case SENSOR_FEATURE_GET_OFFSET_TO_START_OF_EXPOSURE:
 		*(MUINT32 *)(uintptr_t)(*(feature_data + 1))
-			= 7248000;
+			= 1130000;
 		break;
 	case SENSOR_FEATURE_GET_PERIOD_BY_SCENARIO:
 		switch (*feature_data) {
