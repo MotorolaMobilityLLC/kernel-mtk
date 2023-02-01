52b31533a9675aa40b0c5dcf0e63cb5104a5dead
houdz1 <houdz1@lenovo.com>
2023-02-01 20:00:54 +0800

    Revert "(CR):kernel/devonn:Fix kenrnel panic on stress test"
    This reverts commit baeb82011d261877856b6aaf86e20f3a98cf873f.
    Mot-CRs-fixed: (CR) (CR)
    Change-Id: Ie482d6469a226ad41c69f672a504b8955f9eacf8
    Reviewed-on: https://gerrit.mot.com/2510044
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Hua Tan <tanhua1@motorola.com>
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/2515811
    Reviewed-by: Suting Xie <xiest1@motorola.com>
    Submit-Approved: Suting Xie <xiest1@motorola.com>

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_drv.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_drv.c
@@ -3549,6 +3549,7 @@ void mtk_drm_top_clk_disable_unprepare(struct drm_device *drm)
 		}
 		clk_disable_unprepare(priv->top_clk[i]);
 	}
+
 	DDPMSG("%s: mtcmos off\n", __func__);
 	pm_runtime_put_sync(priv->mmsys_dev);
 	if (priv->side_mmsys_dev)
@@ -4717,18 +4718,6 @@ int mtk_drm_fm_lcm_auto_test(struct drm_device *dev, void *data,
 	return 0;
 }
 
-static int suspend_meg=0;
-void suspend_status_set(int state)
-{
-	if (state == 1) {
-		suspend_meg = 1;
-	} else{
-		suspend_meg = 0;
-	}
-
-        //return 0;
-}
-EXPORT_SYMBOL(suspend_status_set);
 static int mtk_drm_ioctl_set_panel_feature(struct drm_device *dev, void *data,
 		struct drm_file *file_priv)
 {
@@ -4740,9 +4729,7 @@ static int mtk_drm_ioctl_set_panel_feature(struct drm_device *dev, void *data,
 	unsigned int bl_level = 0;
 
 	DDPMSG("%s: set param_idx %d to %d\n", __func__, param_info->param_idx, param_info->value);
-	if(suspend_meg == 1) {
-		return -1;
-	}
+
 	switch (param_info->param_idx) {
 		case PARAM_HBM:
 			if (panel_ext->hbm_type == HBM_MODE_DCS_ONLY) {
