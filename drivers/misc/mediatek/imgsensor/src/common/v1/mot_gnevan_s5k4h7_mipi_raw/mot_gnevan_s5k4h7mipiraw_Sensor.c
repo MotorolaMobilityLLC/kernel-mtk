40badc4e50019fbf0a0fbcf51b5152321a26e916
wangzixun <kankan33@motorola.com>
2023-02-08 20:23:49 +0800

    gnevan: Fixed a memory overflow problem with kasan test camera
    [detail]
    changed the drivers of the front camera and macro camera
    
    Change-Id: If32459cbe89fa2524ef427fcec382cdee23b55e3
    Signed-off-by: wangzixun <kankan33@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2505661
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: XiaoFeng Wang <wangxf32@lenovo.com>
    Reviewed-by: Suna Li <lisn17@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1/mot_gnevan_s5k4h7_mipi_raw/mot_gnevan_s5k4h7mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1/mot_gnevan_s5k4h7_mipi_raw/mot_gnevan_s5k4h7mipiraw_Sensor.c
@@ -1606,9 +1606,9 @@ static kal_uint32 feature_control(
 		*feature_para_len = 4;
 		break;
 	case SENSOR_FEATURE_SET_FRAMERATE:
-		LOG_INF("current fps :%d\n", (UINT32) *feature_data);
+		LOG_INF("current fps :%d\n",  *feature_data_32);
 		spin_lock(&imgsensor_drv_lock);
-		imgsensor.current_fps = *feature_data;
+		imgsensor.current_fps =(UINT16) *feature_data_32;
 		spin_unlock(&imgsensor_drv_lock);
 		break;
 	case SENSOR_FEATURE_SET_HDR:
