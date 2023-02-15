3f44ce17a4472ed712c85bcbf32cf19b34f3e7b2
yangcj <wingt101@motorola.com>
2023-02-15 10:25:04 +0800

    msdc: gnevan: fix sdcard exception
    Symptom:
    host->card is null when call __mmc_sd_suspend.
    
    Root Cause:
    the another process call mmc_sd_remove,then host->card = NULL.
    
    Solution:
    1.claim host when clearing NULL.
    2.add host->card check when it used
    
    Test result:
    The SD card functions normally
    
    Change-Id: I443bdee9a6f5eb3e5b308eff63988b701aaeb6a5
    Reviewed-on: https://gerrit.mot.com/2512042
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Ying Shen <shenying@motorola.com>
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -1097,7 +1097,9 @@ free_card:
 static void mmc_sd_remove(struct mmc_host *host)
 {
 	mmc_remove_card(host->card);
+	mmc_claim_host(host);
 	host->card = NULL;
+	mmc_release_host(host);
 }
 
 /*
@@ -1139,18 +1141,18 @@ static int _mmc_sd_suspend(struct mmc_host *host)
 	int err = 0;
 
 	mmc_claim_host(host);
+	if (host->card) {
+		if (mmc_card_suspended(host->card))
+			goto out;
 
-	if (mmc_card_suspended(host->card))
-		goto out;
+		if (!mmc_host_is_spi(host))
+			err = mmc_deselect_cards(host);
 
-	if (!mmc_host_is_spi(host))
-		err = mmc_deselect_cards(host);
-
-	if (!err) {
-		mmc_power_off(host);
-		mmc_card_set_suspended(host->card);
+		if (!err) {
+			mmc_power_off(host);
+			mmc_card_set_suspended(host->card);
+		}
 	}
-
 out:
 	mmc_release_host(host);
 	return err;
