f521ae44b93b1874a1aecd7820005edb7b36882b
mtk31598 <ryan.fu@mediatek.com>
2023-02-18 00:13:40 +0800

    [ALPS07589161] GPU: GPUSWERRATA-1420 + 1425 for CVE-2022-36449
    [Description]
    GPUSWERRATA-1420
    
    It is possible for physical addresses of
    userspace memory to be disclosed in 2 different ways.
    First, by allocating memory in the Mali driver
    (including user buffers that are imported into the
    GPU) and then mapping the driver file descriptor
    with an offset for dumping the MMU page
    tables. The MMU dumping feature is only used
    for testing and debug purposes.
    It was incorrectly
    included in production builds.
    
    Secondly, memory pages used by Malis MMU can
    be freed back to a pool and re-used without
    removing or invalidating page table entry data.
    This was a side-effect of an optimization to free
    empty page tables early. It can be easily achieved
    by allocating buffer of size N, freeing all of it
    which causes page tables to be freed, then allocating
    another buffer of size N + 5, causing the
    previously freed page tables to be reused in the
    second allocation. The physical addresses can be
    extracted simply by mapping the second allocation
    and scanning its contents.
    
    GPUSWERRATA-1425
    
    Incorrect ordering of freeing GPU page tables,
    provides a small window of time for which an
    attack can be performed.
    This is caused by the freeing of the page tables
    happening before the invalidation of the MMU
    entries and CPU/GPU cache flush.
    
    [Patch]
    
    [ARM Ticket]
    00487868
    
    MTK-Commit-Id: 1ee7804a67d18384bdfbb2898f89b47af7054a71
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2022-36449
    Bug: 259983537
    
    Signed-off-by: mtk31598 <ryan.fu@mediatek.com>
    CR-Id: ALPS07589161
    Feature: ARM GPU DDK driver
    Change-Id: I62e1ee63f0402b81d6dd01fbff587d61b963e866
    (cherry-pick from 19142ce6aa9edb0d8f25e434599b9e54407f535f)
    Signed-off-by: Gajjala Chakradhar <gajjalac@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2556672
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/mali_kbase_mem.h
+++ drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/mali_kbase_mem.h
@@ -1252,6 +1252,7 @@ void kbase_mmu_disable_as(struct kbase_device *kbdev, int as_nr);
 
 void kbase_mmu_interrupt(struct kbase_device *kbdev, u32 irq_stat);
 
+#if defined(CONFIG_MALI_VECTOR_DUMP)
 /**
  * kbase_mmu_dump() - Dump the MMU tables to a buffer.
  *
@@ -1271,6 +1272,7 @@ void kbase_mmu_interrupt(struct kbase_device *kbdev, u32 irq_stat);
  * (including if the @c nr_pages is too small)
  */
 void *kbase_mmu_dump(struct kbase_context *kctx, int nr_pages);
+#endif
 
 /**
  * kbase_sync_now - Perform cache maintenance on a memory region
