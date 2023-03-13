343acddbac81b747f17325818424b85671e741b4
houdz1 <houdz1@lenovo.com>
2023-03-13 10:31:08 +0800

    drm/panel: aion: add logs of gpios
    Add logs of gpios.
    That logs can indicate which gpios sent wrong
    
    Change-Id: Ic18bf229ce55116b530b4d722b94d6a82b45273c
    Signed-off-by: houdz1 <houdz1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2536726
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/dsi-panel-mot-csot-vtdr6115-655-fhdp-dphy-vdo-144hz.c
+++ drivers/gpu/drm/panel/dsi-panel-mot-csot-vtdr6115-655-fhdp-dphy-vdo-144hz.c
@@ -224,7 +224,7 @@ static int gate_ic_Power_on(struct drm_panel *panel, int enabled)
 			pm_en_pin = NULL;
 			pm_en_pin = devm_gpiod_get_index(ctx->dev, "pm-enable", i, GPIOD_OUT_HIGH);
 			if (IS_ERR(pm_en_pin)) {
-				pr_err("cannot get bias-gpios 0 %ld\n", PTR_ERR(pm_en_pin));
+				pr_err("cannot get bias-gpios %d %ld\n", i, PTR_ERR(pm_en_pin));
 				return PTR_ERR(pm_en_pin);
 			}
 			gpiod_set_value(pm_en_pin, gpio_status);
@@ -237,7 +237,7 @@ static int gate_ic_Power_on(struct drm_panel *panel, int enabled)
 			pm_en_pin = NULL;
 			pm_en_pin = devm_gpiod_get_index(ctx->dev, "pm-enable", i, GPIOD_OUT_LOW);
 			if (IS_ERR(pm_en_pin)) {
-				pr_err("cannot get bias-gpios 0 %ld\n", PTR_ERR(pm_en_pin));
+				pr_err("cannot get bias-gpios %d %ld\n", i, PTR_ERR(pm_en_pin));
 				return PTR_ERR(pm_en_pin);
 			}
 			gpiod_set_value(pm_en_pin, gpio_status);
