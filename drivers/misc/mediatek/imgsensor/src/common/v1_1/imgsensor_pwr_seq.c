56dc6cd618ec0c1390c083017cce3aaaa1facfb5
wuhao <am2204@motorola.com>
2023-04-14 18:07:05 +0800

    cancunf: modify af power supply method
    detail:
    modify af power supply method
    
    Change-Id: Iba311b1f1de73225188476e6355a55c9558167d4
    Signed-off-by: wuhao <am2204@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2576824
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Suna Li <lisn17@motorola.com>
    Reviewed-by: XiaoFeng Wang <wangxf32@mt.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/imgsensor_pwr_seq.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/imgsensor_pwr_seq.c
@@ -22,7 +22,7 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
 			{AVDD, Vol_High, 0},
 			{DVDD, Vol_High, 1},
 			{DVDD1, Vol_High, 1},
-			{AFVDD, Vol_2800, 1},
+			{AFVDD, Vol_High, 1},
 			{RST, Vol_High, 5},
 		},
 	},
@@ -49,8 +49,8 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
 			{DVDD, Vol_High, 1},
 			{AVDD, Vol_2800, 1},
 			{DOVDD, Vol_1800, 1},
-                        {AFVDD, Vol_High, 1},
-                        {RST, Vol_High, 4}
+			{AFVDD, Vol_2800, 1},
+			{RST, Vol_High, 4}
 		},
 	},
 #endif
