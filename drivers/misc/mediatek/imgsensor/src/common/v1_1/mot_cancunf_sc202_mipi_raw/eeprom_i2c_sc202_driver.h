ea8a9c95f5d667e30ef2f1dc03adc1ea7745e664
wuhao <am2204@motorola.com>
2023-04-14 18:08:26 +0800

    cancunf: bringup camera eeprom[1/3]
    detail:
    bringup camera eeprom
    
    Change-Id: I3886ad9476d018331f7f38e4f668a2bfdeeb8b73
    Signed-off-by: wuhao <am2204@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2576825
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Suna Li <lisn17@motorola.com>
    Reviewed-by: XiaoFeng Wang <wangxf32@lenovo.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_cancunf_sc202_mipi_raw/eeprom_i2c_sc202_driver.h
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_cancunf_sc202_mipi_raw/eeprom_i2c_sc202_driver.h
@@ -17,7 +17,7 @@
 extern int iReadRegI2C(u8 *a_pSendData, u16 a_sizeSendData,
 		u8 *a_pRecvData, u16 a_sizeRecvData, u16 i2cId);
 extern int iWriteRegI2C(u8 *a_pSendData, u16 a_sizeSendData, u16 i2cId);
-unsigned int sc202_read_region(struct i2c_client *client,
+unsigned int mot_cancunf_sc202_read_region(struct i2c_client *client,
 				unsigned int addr,
 				unsigned char *data,
 				unsigned int size);
