7c2b42b9917151ff0d7b7f39a428beaaad112f2d
Franz Wang <wangxf14@motorola.com>
2023-05-06 16:41:59 +0800

    drivers: input: keyboard: bury debug point for long pressing power key
    bury debug point for long pressing power key
    debug key word is "Power key long press over 5s"
    
    Change-Id: I3343ac987d1d92614269619804727e8ca616c427
    Signed-off-by: Franz Wang <wangxf14@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2599118
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/keyboard/mtk-pmic-keys.c
+++ drivers/input/keyboard/mtk-pmic-keys.c
@@ -13,6 +13,7 @@
 #include <linux/platform_device.h>
 #include <linux/pm_wakeup.h>
 #include <linux/regmap.h>
+#include <linux/timer.h>
 #include <linux/mfd/mt6323/registers.h>
 #include <linux/mfd/mt6359p/registers.h>
 #include <linux/mfd/mt6363/registers.h>
@@ -239,6 +240,14 @@ static void mtk_pmic_keys_lp_reset_setup(struct mtk_pmic_keys *keys,
 	}
 }
 
+/* MMI_STOPSHIP <BOOTLOADER>: bury debug point for long pressing power key */
+static void power_long_press_5s_handler(struct timer_list *timer)
+{
+	pr_err("Sharp: Power key long press over 5s");
+}
+
+DEFINE_TIMER(power_key_timer, power_long_press_5s_handler);
+
 static irqreturn_t mtk_pmic_keys_release_irq_handler_thread(
 				int irq, void *data)
 {
@@ -246,10 +255,15 @@ static irqreturn_t mtk_pmic_keys_release_irq_handler_thread(
 
 	input_report_key(info->keys->input_dev, info->keycode, 0);
 	input_sync(info->keys->input_dev);
+
+	del_timer(&power_key_timer);
+	pr_err("Sharp: del_timer2");
+
 	if (info->suspend_lock)
 		__pm_relax(info->suspend_lock);
 	dev_info(info->keys->dev, "release key =%d using PMIC\n",
 			info->keycode);
+
 	return IRQ_HANDLED;
 }
 
@@ -268,6 +282,17 @@ static irqreturn_t mtk_pmic_keys_irq_handler_thread(int irq, void *data)
 
 	input_report_key(info->keys->input_dev, info->keycode, pressed);
 	input_sync(info->keys->input_dev);
+
+	if(pressed)
+	{
+		mod_timer(&power_key_timer, jiffies + 5*HZ);
+		pr_err("Sharp: mod_timer");
+	}
+	else
+	{
+		del_timer(&power_key_timer);
+		pr_err("Sharp: del_timer");
+	}
 
 	if (pressed && info->suspend_lock)
 		__pm_stay_awake(info->suspend_lock);
