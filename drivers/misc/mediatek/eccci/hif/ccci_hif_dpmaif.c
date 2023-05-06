7a2ccd85b2f041e252ab06bdeefbf23a6f509b23
Weiqiang Hao <weiqiang.hao@mediatek.com>
2023-05-06 14:42:20 +0800

    [ALPS07981753] ccci: change dpmaif wait resume done flow
    Detail:
    The flow of net stack send skb to dpmaif is function call,
    cant wakeup system.
    
    When dpmaif tx send skb, it will first check whether dpmaif
    is in suspend state. If the last state of the dpmaif is
    suspend, net stack will encounter tx busy when send skb.
    
    Net stack receive busy will retry send skb cause an endless loop.
    
    Solution:
    Call pm_system_wakeup() to wake up system when dpmaif wait resume
    done fail.
    
    MTK-Commit-Id: beb764b8ede202b757d0c680c625086e45b712ee
    
    Signed-off-by: Weiqiang Hao <weiqiang.hao@mediatek.com>
    CR-Id: ALPS07981753
    Feature: CCCI_HIF_DATA
    Change-Id: I664acb86e23c3d1bbbe2f8df57b203a021be5cd7
    (cherry-pick from 673541bfc3a51ca689d690f703f7f6513e15f49e)

--- drivers/misc/mediatek/eccci/hif/ccci_hif_dpmaif.c
+++ drivers/misc/mediatek/eccci/hif/ccci_hif_dpmaif.c
@@ -31,6 +31,7 @@
 #include <linux/tcp.h>
 #include <linux/ipv6.h>
 #include <net/ipv6.h>
+#include <linux/suspend.h>
 
 #include "ccci_core.h"
 #include "modem_sys.h"
@@ -2482,6 +2483,7 @@ static int dpmaif_wait_resume_done(void)
 			CCCI_NORMAL_LOG(-1, TAG,
 				"[%s] warning: suspend_flag = 1; (cnt: %d)",
 				__func__, cnt);
+			pm_system_wakeup();
 			return -1;
 		}
 	}
