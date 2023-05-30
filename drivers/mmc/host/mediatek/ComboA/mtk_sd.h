c8cf82a77a429a73530fe383297aac7864d5f055
lin.gui <lin.gui@mediatek.com>
2023-05-30 20:58:16 +0800

    [ALPS08126324] eMMC: add remove bad sdcard flow
    add remove bad sdcard for comboa driver
    
    MTK-Commit-Id: 2cb0c046bc7f6f9d433c3fb56893b2d7fadd379a
    
    CR-Id: ALPS08126324
    Feature: eMMC
    Signed-off-by: lin.gui <lin.gui@mediatek.com>
    Change-Id: Iaac74dc9657b8076c180c4d116f5107d829d454a
    (cherry picked from commit 725643e2ee8efeeabbbf41e7ddccb312fb50794b)

--- drivers/mmc/host/mediatek/ComboA/mtk_sd.h
+++ drivers/mmc/host/mediatek/ComboA/mtk_sd.h
@@ -711,6 +711,7 @@ void msdc_restore_timing_setting(struct msdc_host *host);
 void msdc_save_timing_setting(struct msdc_host *host);
 void msdc_set_bad_card_and_remove(struct msdc_host *host);
 void msdc_remove_card(struct work_struct *work);
+void msdc_ops_set_bad_card_and_remove(struct mmc_host *mmc);
 
 /* Function provided by mmc/core/sd.c */
 /* FIX ME: maybe removed in kernel 4.4 */
