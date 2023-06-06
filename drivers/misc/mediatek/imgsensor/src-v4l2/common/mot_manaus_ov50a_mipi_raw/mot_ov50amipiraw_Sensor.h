4eb95e8f261bb4b60a609189e3585ba85abd4d7f
wangzl37 <wangzl37@motorola.com>
2023-06-06 09:42:10 +0800

    manaus: ov50a update setting[1/2]
    manaus:
    -- ov50a update Xtalk setp2/DINU setting
    -- ov50a add DPCoff sensor mode
    
    Change-Id: Ife3a4487d489b44671bed14eeafc3f3261495f83
    Signed-off-by: wangzl37 <wangzl37@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2634538
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Darong Huang <huangdra@motorola.com>
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: Shenhuai Huang <huangsh4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src-v4l2/common/mot_manaus_ov50a_mipi_raw/mot_ov50amipiraw_Sensor.h
+++ drivers/misc/mediatek/imgsensor/src-v4l2/common/mot_manaus_ov50a_mipi_raw/mot_ov50amipiraw_Sensor.h
@@ -41,6 +41,7 @@ enum IMGSENSOR_MODE {
 	IMGSENSOR_MODE_CUSTOM4,
 	IMGSENSOR_MODE_CUSTOM5,
 	IMGSENSOR_MODE_CUSTOM6,
+	IMGSENSOR_MODE_CUSTOM7,
 };
 
 struct imgsensor_mode_struct {
@@ -108,6 +109,7 @@ struct imgsensor_info_struct {
 	struct imgsensor_mode_struct custom4; //custom4 scenario information
 	struct imgsensor_mode_struct custom5; //custom5 scenario information
 	struct imgsensor_mode_struct custom6; //custom6 scenario information
+	struct imgsensor_mode_struct custom7; //custom7 scenario information
 	kal_uint8  ae_shut_delay_frame;	//shutter delay frame for AE cycle
 	//sensor gain delay frame for AE cycle
 	kal_uint8  ae_sensor_gain_delay_frame;
@@ -127,6 +129,7 @@ struct imgsensor_info_struct {
 	kal_uint8  custom4_delay_frame;     //enter custom4 delay frame num
 	kal_uint8  custom5_delay_frame;     //enter custom5 delay frame num
 	kal_uint8  custom6_delay_frame;     //enter custom6 delay frame num
+	kal_uint8  custom7_delay_frame;     //enter custom7 delay frame num
 	kal_uint8  frame_time_delay_frame;
 
 	kal_uint8  margin; //sensor framelength & shutter margin
