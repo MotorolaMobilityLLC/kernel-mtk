e543666018ff12536b6f234f20884a2670375520
yangzhenwei <yangzhenwei3@wingtech.com>
2023-06-16 15:36:33 +0800

    kernel/lcm:cancunf:1st/2nd panel porch update for 120hz
    cancunf: 1st/2nd panel
    porch values update for refresh rate 120hz
    
    Change-Id: I9544ec3936df387a0437fe1fa57d280e1e1e532f
    Reviewed-on: https://gerrit.mot.com/2646062
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jing Hu <hujing3@lenovo.com>
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/include/dsi-panel-mot-txd-ili7807s-650-fhdp-dphy-vdo-120hz.h
+++ drivers/gpu/drm/panel/include/dsi-panel-mot-txd-ili7807s-650-fhdp-dphy-vdo-120hz.h
@@ -21,35 +21,35 @@
 
 #define DATA_RATE                   824
 #define HSA                         4
-#define HBP                         12
+#define HBP                         10
 #define VSA                         2
 #define VBP                         40
 
 /*Parameter setting for mode 0 Start*/
 #define MODE_60_FPS                  60
 #define MODE_60_VFP                  2530
-#define MODE_60_HFP                  16
+#define MODE_60_HFP                  22
 #define MODE_60_DATA_RATE            824
 /*Parameter setting for mode 0 End*/
 
 /*Parameter setting for mode 1 Start*/
 #define MODE_90_FPS                  90
 #define MODE_90_VFP                  868
-#define MODE_90_HFP                  16
+#define MODE_90_HFP                  22
 #define MODE_90_DATA_RATE            824
 /*Parameter setting for mode 1 End*/
 
 /*Parameter setting for mode 2 Start*/
 #define MODE_120_FPS                  120
 #define MODE_120_VFP                  40
-#define MODE_120_HFP                  16
+#define MODE_120_HFP                  22
 #define MODE_120_DATA_RATE            824
 /*Parameter setting for mode 2 End*/
 
 /*Parameter setting for mode 3 Start*/
 #define MODE_30_FPS                  30
 #define MODE_30_VFP                  7480
-#define MODE_30_HFP                  16
+#define MODE_30_HFP                  22
 
 #define MODE_30_DATA_RATE            824
 /*Parameter setting for mode 3 End*/
