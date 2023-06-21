dddc105b8460cd479b25f3325271850fc358b8f7
Daniel Tao <daniel.tao@mediatek.com>
2023-06-21 15:38:42 +0800

    [ALPS08169268] GPU: GPUSWERRATA-1617 (1/2)
    [Description]
    Kbase accounts the GPU memory allocated for a context under the memory
    footprint of a process, so that kernels low memory killer or OoM killer
    can kill the suitable process to free up decent amount of system memory.
    For accounting, Kbase updates the MM_FILEPAGES counter inside the
    mm_struct corresponding to the process that created the Kbase context.
    To ensure mm_struct can always be safely accessed, a tracking page was
    used which Kbase mandated to be mapped right after the opening of
    /dev/mali0 file. When the mapping was closed Kbase updated the kernel
    counter to subtract all the GPU memory allocated so far for a context
    but the actual freeing of GPU memory was done later. This was usually
    not a problem as the mapping is closed by DDK Userspace just before the
    context termination. But the Userspace is allowed to close it at will.
    Malicious Userspace could have exploited this Kbase quirk by closing
    the mapping which would have mislead the OoM killer in killing the
    innocent processes before it gets around to the culprit process.
    
    This commit removes the use of tracking page and uses mmgrab() to take
    a reference on mm_struct for User created Kbase context.
    The reference doesnt prevent the freeing of address space on process
    exit, it merely keeps the mm_struct alive. The reference is dropped
    on context termination.
    For backward compatibility, the call from Base to create the mapping for
    tracking page isnt rejected by Kbase. The Base code has been updated to
    skip the mapping of tracking page only for newer Kbase so as to maintain
    forward compatibility with the older Kbase. BASE_UK_VERSION_MINOR has
    been incremented as the requirement to create a mapping for tracking
    page is relaxed by Kbase.
    
    "GPUCORE-15214" defect test has also been updated to fix the stdlib leak
    issue that was incidentally seen after the Kbase changes made.
    
    In addition to TI2 testing as detailed below, testing also included
    Running the defect test for GPUCORE-36765 from trunk (0e394f63cb8bcbb)
    and verifying both defect test cases pass with no WARN_ONs.
    
    [Patch]
    GPUCORE-36765: Stop the use of tracking page for GPU memory accounting
    
    [ARM Ticket]
    03422243
    
    MTK-Commit-Id: 4955bf723b34f23169354efbf63283c774e95e45
    
    Signed-off-by: Daniel Tao <daniel.tao@mediatek.com>
    CR-Id: ALPS08169268
    Feature: ARM GPU DDK driver
    Change-Id: I9cd70735ca48b8cf8c8e67d6295f58ebc08d04ed
    (cherry-pick from 6cbb6f9721177d0bd4f8220f4db2ef135fcee006)

--- drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/context/mali_kbase_context.c
+++ drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/context/mali_kbase_context.c
@@ -136,7 +136,6 @@ int kbase_context_common_init(struct kbase_context *kctx)
 	/* creating a context is considered a disjoint event */
 	kbase_disjoint_event(kctx->kbdev);
 
-	spin_lock_init(&kctx->mm_update_lock);
 	kctx->process_mm = NULL;
 	atomic_set(&kctx->nonmapped_pages, 0);
 	atomic_set(&kctx->permanent_mapped_pages, 0);
@@ -179,6 +178,16 @@ int kbase_context_common_init(struct kbase_context *kctx)
 			return err;
 	}
 
+	/* Check if this is a Userspace created context */
+	if (likely(kctx->filp)) {
+		/* This merely takes a reference on the mm_struct and not on the
+		 * address space and so won't block the freeing of address space
+		 * on process exit.
+		 */
+		mmgrab(current->mm);
+		kctx->process_mm = current->mm;
+	}
+
 	atomic_set(&kctx->used_pages, 0);
 
 	mutex_init(&kctx->reg_lock);
@@ -210,9 +219,11 @@ int kbase_context_common_init(struct kbase_context *kctx)
 	mutex_lock(&kctx->kbdev->kctx_list_lock);
 
 	err = kbase_insert_kctx_to_process(kctx);
-	if (err)
-		dev_err(kctx->kbdev->dev,
-		"(err:%d) failed to insert kctx to kbase_process\n", err);
+	if (err) {
+		dev_err(kctx->kbdev->dev, "(err:%d) failed to insert kctx to kbase_process", err);
+		if (likely(kctx->filp))
+			mmdrop(kctx->process_mm);
+	}
 
 	mutex_unlock(&kctx->kbdev->kctx_list_lock);
 	if (err) {
@@ -306,6 +317,9 @@ void kbase_context_common_term(struct kbase_context *kctx)
 	mutex_lock(&kctx->kbdev->kctx_list_lock);
 	kbase_remove_kctx_from_process(kctx);
 	mutex_unlock(&kctx->kbdev->kctx_list_lock);
+
+	if (likely(kctx->filp))
+		mmdrop(kctx->process_mm);
 
 	KBASE_KTRACE_ADD(kctx->kbdev, CORE_CTX_DESTROY, kctx, 0u);
 }
