4c97281730e548d507089ae11eadb969206b983a
Daniel Tao <daniel.tao@mediatek.com>
2023-06-21 15:38:42 +0800

    [ALPS08169268] GPU: GPUSWERRATA-1617 (1/2)
    [Description]
    Kbase accounts the GPU memory allocated for a context under the memory
    footprint of a process, so that kernels low memory killer or OoM killer
    can kill the suitable process to free up decent amount of system memory.
    For accounting, Kbase updates the MM_FILEPAGES counter inside the
    mm_struct corresponding to the process that created the Kbase context.
    To ensure mm_struct can always be safely accessed, a tracking page was
    used which Kbase mandated to be mapped right after the opening of
    /dev/mali0 file. When the mapping was closed Kbase updated the kernel
    counter to subtract all the GPU memory allocated so far for a context
    but the actual freeing of GPU memory was done later. This was usually
    not a problem as the mapping is closed by DDK Userspace just before the
    context termination. But the Userspace is allowed to close it at will.
    Malicious Userspace could have exploited this Kbase quirk by closing
    the mapping which would have mislead the OoM killer in killing the
    innocent processes before it gets around to the culprit process.
    
    This commit removes the use of tracking page and uses mmgrab() to take
    a reference on mm_struct for User created Kbase context.
    The reference doesnt prevent the freeing of address space on process
    exit, it merely keeps the mm_struct alive. The reference is dropped
    on context termination.
    For backward compatibility, the call from Base to create the mapping for
    tracking page isnt rejected by Kbase. The Base code has been updated to
    skip the mapping of tracking page only for newer Kbase so as to maintain
    forward compatibility with the older Kbase. BASE_UK_VERSION_MINOR has
    been incremented as the requirement to create a mapping for tracking
    page is relaxed by Kbase.
    
    "GPUCORE-15214" defect test has also been updated to fix the stdlib leak
    issue that was incidentally seen after the Kbase changes made.
    
    In addition to TI2 testing as detailed below, testing also included
    Running the defect test for GPUCORE-36765 from trunk (0e394f63cb8bcbb)
    and verifying both defect test cases pass with no WARN_ONs.
    
    [Patch]
    GPUCORE-36765: Stop the use of tracking page for GPU memory accounting
    
    [ARM Ticket]
    03422243
    
    MTK-Commit-Id: 4955bf723b34f23169354efbf63283c774e95e45
    
    Signed-off-by: Daniel Tao <daniel.tao@mediatek.com>
    CR-Id: ALPS08169268
    Feature: ARM GPU DDK driver
    Change-Id: I9cd70735ca48b8cf8c8e67d6295f58ebc08d04ed
    (cherry-pick from 6cbb6f9721177d0bd4f8220f4db2ef135fcee006)

--- drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/mali_kbase_defs.h
+++ drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/mali_kbase_defs.h
@@ -1565,11 +1565,13 @@ struct kbase_sub_alloc {
  *                        is scheduled in and an atom is pulled from the context's per
  *                        slot runnable tree in JM GPU or GPU command queue
  *                        group is programmed on CSG slot in CSF GPU.
- * @mm_update_lock:       lock used for handling of special tracking page.
  * @process_mm:           Pointer to the memory descriptor of the process which
  *                        created the context. Used for accounting the physical
  *                        pages used for GPU allocations, done for the context,
- *                        to the memory consumed by the process.
+ *                        to the memory consumed by the process. A reference is taken
+ *                        on this descriptor for the Userspace created contexts so that
+ *                        Kbase can safely access it to update the memory usage counters.
+ *                        The reference is dropped on context termination.
  * @gpu_va_end:           End address of the GPU va space (in 4KB page units)
  * @jit_va:               Indicates if a JIT_VA zone has been created.
  * @mem_profile_data:     Buffer containing the profiling information provided by
@@ -1791,8 +1793,7 @@ struct kbase_context {
 
 	atomic_t refcount;
 
-	spinlock_t         mm_update_lock;
-	struct mm_struct __rcu *process_mm;
+	struct mm_struct *process_mm;
 	u64 gpu_va_end;
 	bool jit_va;
 
