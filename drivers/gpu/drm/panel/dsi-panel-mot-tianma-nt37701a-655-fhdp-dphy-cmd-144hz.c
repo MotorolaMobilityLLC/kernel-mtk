2802addbe4c803b3f122cc5f87ccf07389b0a41b
Christian Bernard Gioia <chrisb@motorola.com>
2023-06-22 13:50:03 -0300

    kernel/lcm: tesla: revert change made on panels in (CR)
    Changes made on (CR) cause display instability when on 144hz option,
    revert it
    
    Change-Id: I712a9d134fef1e3e6d99461a898ff1cb74f527d7
    Reviewed-on: https://gerrit.mot.com/2652956
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Gilberto Gambugge Neto <gambugge@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/dsi-panel-mot-tianma-nt37701a-655-fhdp-dphy-cmd-144hz.c
+++ drivers/gpu/drm/panel/dsi-panel-mot-tianma-nt37701a-655-fhdp-dphy-cmd-144hz.c
@@ -432,7 +432,6 @@ static struct mtk_panel_params ext_params_48hz = {
 
 	.cust_esd_check = 0,
 	.esd_check_enable = 1,
-	.lp_perline_en = 1,
 	.lcm_esd_check_table[0] = {
 		.cmd = 0x0a,
 		.count = 1,
@@ -506,7 +505,6 @@ static struct mtk_panel_params ext_params_60hz = {
 
 	.cust_esd_check = 0,
 	.esd_check_enable = 1,
-	.lp_perline_en = 1,
 	.lcm_esd_check_table[0] = {
 		.cmd = 0x0a,
 		.count = 1,
@@ -579,7 +577,6 @@ static struct mtk_panel_params ext_params_90hz = {
 
 	.cust_esd_check = 0,
 	.esd_check_enable = 1,
-	.lp_perline_en = 1,
 	.lcm_esd_check_table[0] = {
 		.cmd = 0x0a,
 		.count = 1,
@@ -652,7 +649,6 @@ static struct mtk_panel_params ext_params_120hz = {
 
 	.cust_esd_check = 0,
 	.esd_check_enable = 1,
-	.lp_perline_en = 1,
 	.lcm_esd_check_table[0] = {
 		.cmd = 0x0a,
 		.count = 1,
@@ -726,7 +722,6 @@ static struct mtk_panel_params ext_params_144hz = {
 
 	.cust_esd_check = 0,
 	.esd_check_enable = 1,
-	.lp_perline_en = 1,
 	.lcm_esd_check_table[0] = {
 		.cmd = 0x0a,
 		.count = 1,
