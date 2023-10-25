0fa1838148fc1b978f2f5df5871530e18e53e967
Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
2023-10-25 11:04:21 +0800

    [ALPS08490250] mml: safe release frame config
    Do ref put before destroy config in put context
    to avoid config thread still working.
    
    MTK-Commit-Id: 8a05e63350818b5904dfd5ceafdc684214f2a8b8
    
    Signed-off-by: Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
    CR-Id: ALPS08490250
    Feature: mml
    Change-Id: Ibf90f9abaa0f449f0949e3ed82306d3f53d8c33e

--- drivers/gpu/drm/mediatek/mml/mtk-mml-drm-adaptor.c
+++ drivers/gpu/drm/mediatek/mml/mtk-mml-drm-adaptor.c
@@ -343,6 +343,20 @@ static struct mml_task *task_get_idle(struct mml_frame_config *cfg)
 	return task;
 }
 
+static void task_move_to_destroy(struct kref *kref)
+{
+	struct mml_task *task = container_of(kref, struct mml_task, ref);
+
+	if (task->config) {
+		struct mml_frame_config *cfg = task->config;
+
+		cfg->cfg_ops->put(cfg);
+		task->config = NULL;
+	}
+
+	mml_core_destroy_task(task);
+}
+
 static void frame_config_destroy(struct mml_frame_config *cfg)
 {
 	struct mml_task *task, *tmp;
@@ -356,8 +370,7 @@ static void frame_config_destroy(struct mml_frame_config *cfg)
 			 * print error but not destroy
 			 */
 			mml_err("[drm]busy task:%p", task);
-			list_del_init(&task->entry);
-			task->config = NULL;
+			kref_put(&task->ref, task_move_to_destroy);
 		}
 	}
 
@@ -368,10 +381,21 @@ static void frame_config_destroy(struct mml_frame_config *cfg)
 			 * print error but not destroy
 			 */
 			mml_err("[drm]busy task:%p", task);
-			list_del_init(&task->entry);
-			task->config = NULL;
+			kref_put(&task->ref, task_move_to_destroy);
 		}
 	}
+
+	list_for_each_entry_safe(task, tmp, &cfg->done_tasks, entry) {
+		list_del_init(&task->entry);
+		kref_put(&task->ref, task_move_to_destroy);
+	}
+
+	cfg->cfg_ops->put(cfg);
+}
+
+static void frame_config_free(struct kref *kref)
+{
+	struct mml_frame_config *cfg = container_of(kref, struct mml_frame_config, ref);
 
 	mml_core_deinit_config(cfg);
 	kfree(cfg);
@@ -387,9 +411,8 @@ static void frame_config_destroy_work(struct work_struct *work)
 	frame_config_destroy(cfg);
 }
 
-static void frame_config_queue_destroy(struct kref *kref)
+static void frame_config_queue_destroy(struct mml_frame_config *cfg)
 {
-	struct mml_frame_config *cfg = container_of(kref, struct mml_frame_config, ref);
 	struct mml_drm_ctx *ctx = cfg->ctx;
 
 	queue_work(ctx->wq_destroy, &cfg->work_destroy);
@@ -562,17 +585,6 @@ static void task_move_to_idle(struct mml_task *task)
 		atomic_read(&ctx->racing_cnt));
 }
 
-static void task_move_to_destroy(struct kref *kref)
-{
-	struct mml_task *task = container_of(kref,
-		struct mml_task, ref);
-
-	if (task->config)
-		kref_put(&task->config->ref, frame_config_queue_destroy);
-
-	mml_core_destroy_task(task);
-}
-
 static void task_submit_done(struct mml_task *task)
 {
 	struct mml_drm_ctx *ctx = task->ctx;
@@ -615,16 +627,6 @@ static void task_buf_put(struct mml_task *task)
 			dma_fence_put(task->buf.seg_map.fence);
 	}
 	mml_trace_ex_end();
-}
-
-static void task_put_idles(struct mml_frame_config *cfg)
-{
-	struct mml_task *task, *task_tmp;
-
-	list_for_each_entry_safe(task, task_tmp, &cfg->done_tasks, entry) {
-		list_del_init(&task->entry);
-		kref_put(&task->ref, task_move_to_destroy);
-	}
 }
 
 static void task_state_dec(struct mml_frame_config *cfg, struct mml_task *task,
@@ -708,8 +710,7 @@ static void task_frame_done(struct mml_task *task)
 		if (!list_empty(&cfg->tasks) || !list_empty(&cfg->await_tasks))
 			continue;
 		list_del_init(&cfg->entry);
-		task_put_idles(cfg);
-		kref_put(&cfg->ref, frame_config_queue_destroy);
+		frame_config_queue_destroy(cfg);
 		ctx->config_cnt--;
 		mml_msg("[drm]config %p send destroy remain %u",
 			cfg, ctx->config_cnt);
@@ -870,7 +871,7 @@ s32 mml_drm_submit(struct mml_drm_ctx *ctx, struct mml_submit *submit,
 			task->config = cfg;
 			task->state = MML_TASK_DUPLICATE;
 			/* add more count for new task create */
-			kref_get(&cfg->ref);
+			cfg->cfg_ops->get(cfg);
 		}
 	} else {
 		cfg = frame_config_create(ctx, submit);
@@ -885,6 +886,7 @@ s32 mml_drm_submit(struct mml_drm_ctx *ctx, struct mml_submit *submit,
 			list_del_init(&cfg->entry);
 			frame_config_destroy(cfg);
 			result = PTR_ERR(task);
+			task = NULL;
 			mml_err("%s create task fail", __func__);
 			goto err_unlock_exit;
 		}
@@ -901,7 +903,7 @@ s32 mml_drm_submit(struct mml_drm_ctx *ctx, struct mml_submit *submit,
 		}
 
 		/* add more count for new task create */
-		kref_get(&cfg->ref);
+		cfg->cfg_ops->get(cfg);
 	}
 
 	/* maintain racing ref count for easy query mode */
@@ -1213,7 +1215,7 @@ static void config_get(struct mml_frame_config *cfg)
 
 static void config_put(struct mml_frame_config *cfg)
 {
-	kref_put(&cfg->ref, frame_config_queue_destroy);
+	kref_put(&cfg->ref, frame_config_free);
 }
 
 static const struct mml_config_ops drm_config_ops = {
@@ -1330,7 +1332,7 @@ static void drm_ctx_release(struct mml_drm_ctx *ctx)
 	list_for_each_entry_safe_reverse(cfg, tmp, &local_list, entry) {
 		/* check and remove configs/tasks in this context */
 		list_del_init(&cfg->entry);
-		frame_config_destroy(cfg);
+		frame_config_queue_destroy(cfg);
 	}
 
 	destroy_workqueue(ctx->wq_destroy);
