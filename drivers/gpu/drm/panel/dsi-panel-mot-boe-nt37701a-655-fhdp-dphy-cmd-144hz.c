1f845fc736f6f28fd18e8aba5832e16d4e0a9dd2
Bruno Sampaio Leite <brunosmp@motorola.com>
2023-10-30 09:55:21 -0300

    Revert "(CR): kernel/lcm: vicky: make panel wait for data in every line"
    When LP perline mode is enabled for vicky, display will face
    issues where screen flickers and freezes. Fix it by reverting
    and settiing lp_perline_en = 0.
    
    This reverts commit bcf85140a7a584dcdc1634509090da546665f58e.
    
    Change-Id: Ibd7c70376caf517d53f91022ada0b02b498a1f9d
    signed-off-by: brunosmp@motorola.com
    Reviewed-on: https://gerrit.mot.com/2782019
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Bruno Oliveira <brunosoe@motorola.com>
    Reviewed-by: Rafael Ortolan <rafones@motorola.com>
    Reviewed-by: Hua Tan <tanhua1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/dsi-panel-mot-boe-nt37701a-655-fhdp-dphy-cmd-144hz.c
+++ drivers/gpu/drm/panel/dsi-panel-mot-boe-nt37701a-655-fhdp-dphy-cmd-144hz.c
@@ -370,7 +370,6 @@ static struct mtk_panel_params ext_params_48hz = {
 		.data_rate = 330,
 	},
 	.data_rate = 330,
-	.lp_perline_en = 1,
 
 	.cust_esd_check = 0,
 	.esd_check_enable = 1,
@@ -445,7 +444,6 @@ static struct mtk_panel_params ext_params_60hz = {
 		.data_rate = 380,
 	},
 	.data_rate = 380,
-	.lp_perline_en = 1,
 
 	.cust_esd_check = 0,
 	.esd_check_enable = 1,
@@ -519,7 +517,6 @@ static struct mtk_panel_params ext_params_90hz = {
 		.data_rate = 570,
 	},
 	.data_rate = 570,
-	.lp_perline_en = 1,
 
 	.cust_esd_check = 0,
 	.esd_check_enable = 1,
@@ -593,7 +590,6 @@ static struct mtk_panel_params ext_params_120hz = {
 		.data_rate = 760,
 	},
 	.data_rate = 760,
-	.lp_perline_en = 1,
 
 	.cust_esd_check = 0,
 	.esd_check_enable = 1,
@@ -665,10 +661,9 @@ static struct mtk_panel_params ext_params_120hz = {
 
 static struct mtk_panel_params ext_params_144hz = {
 	.dyn_fps = {
-		.data_rate = 950,
+		.data_rate = 920,
 	},
-	.data_rate = 950,
-	.lp_perline_en = 1,
+	.data_rate = 920,
 
 	.cust_esd_check = 0,
 	.esd_check_enable = 1,
