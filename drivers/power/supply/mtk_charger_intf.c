3282ad0c3e7d272c9f1f6cd4b29a059fb5feb17d
Haijian Ma <mahj8@motorola.com>
2023-11-17 11:49:48 +0800

    charge:add moto_wlc_alg driver
    -add moto_wlc_alg driver
    -solve issue that wireless charging boot,not in
    the charging status after boot up
    
    Change-Id: I935599ac7681246ef1730a883f4280a0763c4117
    Signed-off-by: Jiacheng Ou <oujc1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2207227
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Lu Chai <chailu1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/2796738
    Reviewed-by: Guocheng Wang <wanggc6@motorola.com>

--- drivers/power/supply/mtk_charger_intf.c
+++ drivers/power/supply/mtk_charger_intf.c
@@ -377,6 +377,8 @@ int get_charger_type(struct mtk_charger *info)
 		prop2.intval,
 		prop3.intval);
 
+	if((info->mmi.factory_mode == true) && info->wireless_online && (POWER_SUPPLY_TYPE_UNKNOWN != prop_wls.intval))
+		return prop_wls.intval;
 	if (POWER_SUPPLY_TYPE_UNKNOWN != prop2.intval)
 		return prop2.intval;
 	else if(POWER_SUPPLY_TYPE_UNKNOWN != prop_wls.intval)
