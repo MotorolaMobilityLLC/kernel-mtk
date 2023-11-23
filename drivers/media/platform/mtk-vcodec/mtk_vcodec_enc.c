623e568a1b77a685e3a80229e274ea03295dc897
Liangjun Kuang <ot_liangjun.kuang@mediatek.com>
2023-11-23 09:43:11 +0800

    ACK: Merge android12-5.10-2023-11 R2 into alps-mp-s0.mp1
    [Target]:
        226a9632f13d26e93fa416f6b003712b69118322
        BACKPORT: firmware_loader: Abort all upcoming firmware load request once reboot triggered
    Mot-CRs-fixed: (CR)
    
    [Source:] kernel/common android12-5.10-2023-11
    [From:] c475c0851364 UPSTREAM: regmap: Drop initial version of maximum transfer length fixes
    [  To:] 226a9632f13d BACKPORT: firmware_loader: Abort all upcoming firmware load request once reboot triggered
    
    Signed-off-by: Liangjun Kuang <ot_liangjun.kuang@mediatek.com>
    Feature: Kernel SI Operation
    CR-Id: ALPS08574261
    Change-Id: I9010dc0a923a3999fbe7ab102a2a7a558d3ef78d
    Reviewed-on: https://gerrit.mot.com/2838654
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Submit-Approved: Jiashi Zhou <zhoujs3@motorola.com>

--- drivers/media/platform/mtk-vcodec/mtk_vcodec_enc.c
+++ drivers/media/platform/mtk-vcodec/mtk_vcodec_enc.c
@@ -1962,6 +1962,8 @@ static int vb2ops_venc_queue_setup(struct vb2_queue *vq,
 	}
 
 	if (*nplanes) {
+		if (*nplanes != q_data->fmt->num_planes)
+			return -EINVAL;
 		for (i = 0; i < *nplanes; i++)
 			if (sizes[i] < q_data->sizeimage[i])
 				return -EINVAL;
