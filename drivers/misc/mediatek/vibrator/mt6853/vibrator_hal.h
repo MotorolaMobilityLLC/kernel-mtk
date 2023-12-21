5a5e61240e5a0b963466e3b826f1f2e74091b307
Bin Chu <ot_bin.chu@mediatek.com>
2023-12-21 21:23:31 +0800

    [ALPS08632891] vibrator:porting regulator API 6853
    change to use regulator API to enable vibrator
    
    MTK-Commit-Id: 7e02028fd8e3775bb8b3fdc87ef42d81e49a22a4
    
    Change-Id: I88c55e076302032f3b7d32a1f6e1cb109495f338
    Signed-off-by: Bin Chu <ot_bin.chu@mediatek.com>
    CR-Id: ALPS08632891
    Feature: pmic_driver

--- drivers/misc/mediatek/vibrator/mt6853/vibrator_hal.h
+++ drivers/misc/mediatek/vibrator/mt6853/vibrator_hal.h
@@ -3,7 +3,8 @@
  * Copyright (c) 2019 MediaTek Inc.
 */
 
-void vibr_Enable_HW(void);
-void vibr_Disable_HW(void);
+#include <linux/regulator/consumer.h>
+void vibr_Enable_HW(struct regulator *reg);
+void vibr_Disable_HW(struct regulator *reg);
 void vibr_power_set(void);
 struct vibrator_hw *mt_get_cust_vibrator_hw(void);
