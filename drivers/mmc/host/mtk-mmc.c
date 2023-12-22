19b94adcce49cf2214ec51c9049316dc959c98ee
lin.gui <lin.gui@mediatek.com>
2023-12-23 03:33:40 +0800

    [ALPS08633613] MSDC: mmc hw reset for cmd48 is failed
    Try to do eMMC hw reset when cmd48 is failed. In this case, the device
    status maybe is incorrect, use eMMC RST_n to reset the device.
    
    MTK-Commit-Id: 167df6e204501ad461dc3c21a153a34ad60bd72c
    
    Signed-off-by: lin.gui <lin.gui@mediatek.com>
    Change-Id: Ib02b3df698c2ffd14e924a392f20a82660fbbfad
    CR-Id: ALPS08633613
    Feature: eMMC

--- drivers/mmc/host/mtk-mmc.c
+++ drivers/mmc/host/mtk-mmc.c
@@ -1055,6 +1055,14 @@ static bool msdc_cmd_done(struct msdc_host *host, int events,
 				__func__, cmd->opcode, cmd->arg, rsp[0],
 				cmd->error);
 
+	if (cmd->opcode == MMC_CMDQ_TASK_MGMT) {
+		/* if resp is incorrect for cmd48, return a error to reset MMC device */
+		if	(cmd->resp[0] != 0x0900)
+			cmd->error = -EIO;
+		dev_info(host->dev, "%s: cmd=48, error=%d, resp=0x%08X\n",
+			__func__, cmd->error, cmd->resp[0]);
+	}
+
 	msdc_cmd_next(host, mrq, cmd);
 	return true;
 }
