070bd84b3d0d50faf582153af6af59f4fa3c433a
Bin Chu <ot_bin.chu@mediatek.com>
2023-12-26 17:11:29 +0800

    [ALPS08642845] vibrator:porting regulator API 6885
    change to use regulator API to enable vibrator
    
    MTK-Commit-Id: 9908255aa935d8408fbe872769d1013d6e89af7a
    
    Signed-off-by: Bin Chu <ot_bin.chu@mediatek.com>
    CR-Id: ALPS08642845
    Feature: pmic_driver
    Change-Id: If39f42ff7a6705474308a21e222093e1d2bb1b72
    (cherry-pick from 0aad578f8104139e92b6f02d06d4c5a3bfcad04a)

--- drivers/misc/mediatek/vibrator/mt6885/vibrator.c
+++ drivers/misc/mediatek/vibrator/mt6885/vibrator.c
@@ -15,7 +15,8 @@
 #include <linux/delay.h>
 #include <mt-plat/upmu_common.h>
 #include "vibrator.h"
-
+#include "vibrator_hal.h"
+#include <linux/regulator/consumer.h>
 struct vibrator_hw *pvib_cust;
 
 static int debug_enable_vib_hal = 1;
@@ -31,20 +32,22 @@ static int debug_enable_vib_hal = 1;
 
 #define OC_INTR_INIT_DELAY      (3)
 
-void vibr_Enable_HW(void)
+void vibr_Enable_HW(struct regulator *reg)
 {
 #ifdef CONFIG_MTK_PMIC_NEW_ARCH
-	pmic_set_register_value(PMIC_RG_LDO_VIBR_EN, 1);
+	if (regulator_enable(reg))
+		pr_notice("set vibr_reg enable failed!\n");
 	mdelay(OC_INTR_INIT_DELAY);
 	pmic_enable_interrupt(INT_VIBR_OC, 1, "vibr");
 #endif
 }
 
-void vibr_Disable_HW(void)
+void vibr_Disable_HW(struct regulator *reg)
 {
 #ifdef CONFIG_MTK_PMIC_NEW_ARCH
 	pmic_enable_interrupt(INT_VIBR_OC, 0, "vibr");
-	pmic_set_register_value(PMIC_RG_LDO_VIBR_EN, 0);
+	if (regulator_disable(reg))
+		pr_notice("set vibr_reg enable failed!\n");
 #endif
 }
 
