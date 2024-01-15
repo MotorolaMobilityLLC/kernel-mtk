5bee0ba981bc4f2eddba5bb6636fd3eca7e95e7b
Gilberto Gambugge <gambugge@motorola.com>
2024-01-15 20:51:34 -0300

    maui: drm/panel: add ldo display feature to hlt
    Maui/6765:
    - Add ldo display feature to hlt display
    
    Change-Id: Ia5139191611561fabdccf8bebf22b70856762bda
    Reviewed-on: https://gerrit.mot.com/2854241
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Reviewed-by: Dezhen Hou <houdz1@motorola.com>
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/dsi-panel-mot-hlt-ft8006s-652-hdp-dphy-video-90hz.c
+++ drivers/gpu/drm/panel/dsi-panel-mot-hlt-ft8006s-652-hdp-dphy-video-90hz.c
@@ -43,6 +43,7 @@ struct hlt {
 	struct backlight_device *backlight;
 	struct gpio_desc *pm_enable_gpio;
 	struct gpio_desc *reset_gpio;
+	struct gpio_desc *ldo_en_gpio;
 
 	bool prepared;
 	bool enabled;
@@ -218,19 +219,31 @@ static int hlt_unprepare(struct drm_panel *panel)
 
 	if(tp_gesture_flag == 0)
 	{
-	ctx->reset_gpio = devm_gpiod_get(ctx->dev, "reset", GPIOD_OUT_HIGH);
-	if (IS_ERR(ctx->reset_gpio)) {
-		dev_err(ctx->dev, "%s: cannot get reset_gpio %ld\n",
+		ctx->reset_gpio = devm_gpiod_get(ctx->dev, "reset", GPIOD_OUT_HIGH);
+		if (IS_ERR(ctx->reset_gpio)) {
+			dev_err(ctx->dev, "%s: cannot get reset_gpio %ld\n",
 			__func__, PTR_ERR(ctx->reset_gpio));
-		return -1;
-	}
-	gpiod_set_value(ctx->reset_gpio, 1);
-	msleep(5);
-	devm_gpiod_put(ctx->dev, ctx->reset_gpio);
+			return -1;
+		}
 
-	ret = sm5109_bias_power_disable(3);
-	}
+		ctx->ldo_en_gpio = devm_gpiod_get(ctx->dev, "ldo_en", GPIOD_OUT_HIGH);
+		if (IS_ERR(ctx->ldo_en_gpio)) {
+			dev_err(ctx->dev, "%s: cannot get ldo_en_gpio %ld\n",
+				__func__, PTR_ERR(ctx->ldo_en_gpio));
+			return -1;
+		}
 
+		gpiod_set_value(ctx->reset_gpio, 1);
+		msleep(5);
+		devm_gpiod_put(ctx->dev, ctx->reset_gpio);
+
+		ret = sm5109_bias_power_disable(3);
+
+		msleep(1);
+		gpiod_set_value(ctx->ldo_en_gpio, 0);
+		msleep(5);
+		devm_gpiod_put(ctx->dev, ctx->ldo_en_gpio);
+	}
 
 	ctx->error = 0;
 	ctx->prepared = false;
@@ -245,6 +258,16 @@ static int hlt_prepare(struct drm_panel *panel)
 	pr_info("%s\n", __func__);
 	if (ctx->prepared)
 		return 0;
+
+	ctx->ldo_en_gpio = devm_gpiod_get(ctx->dev, "ldo_en", GPIOD_OUT_HIGH);
+	if (IS_ERR(ctx->ldo_en_gpio)) {
+		dev_err(ctx->dev, "%s: cannot get ldo_en_gpio %ld\n",
+			__func__, PTR_ERR(ctx->ldo_en_gpio));
+	}
+
+	gpiod_set_value(ctx->ldo_en_gpio, 1);
+	devm_gpiod_put(ctx->dev, ctx->ldo_en_gpio);
+	msleep(1);
 
 	gpiod_set_value(ctx->reset_gpio, 0);
 	usleep_range(5000,5001);
@@ -718,6 +741,15 @@ static int hlt_probe(struct mipi_dsi_device *dsi)
 		return PTR_ERR(ctx->reset_gpio);
 	}
 	devm_gpiod_put(dev, ctx->reset_gpio);
+
+
+	ctx->ldo_en_gpio = devm_gpiod_get(dev, "ldo_en", GPIOD_OUT_HIGH);
+	if (IS_ERR(ctx->ldo_en_gpio)) {
+		dev_err(dev, "cannot get ldo_en-gpios %ld\n",
+			PTR_ERR(ctx->ldo_en_gpio));
+		return PTR_ERR(ctx->ldo_en_gpio);
+	}
+	devm_gpiod_put(dev, ctx->ldo_en_gpio);
 
 	ctx->prepared = true;
 	ctx->enabled = true;
