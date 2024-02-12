408189df87e092def1d23b7a78707f2fd66210a2
Gustavo Couto Fonseca <gcouto@motorola.com>
2024-02-12 17:25:06 -0300

    kyoto: misc: Fix auto focus problem
    Details: After Kernel update from 4.14 to 4.19 lens driver changes leads
    to auto focus issue.
    - Create separated directory for host lens drivers for kyoto, reverting
    critical changes in lens driver.
    
    mot-cam-category: misc
    
    Change-Id: I6e0e58a016ba7fbfdb6a5dcb429e1de70de33ad4
    Reviewed-on: https://gerrit.mot.com/2881778
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Monica Mabuchi <monicam@motorola.com>
    Reviewed-by: <maxsouza@motorola.com>
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lens/mtk/main3/inc/lens_list.h
+++ drivers/misc/mediatek/lens/mtk/main3/inc/lens_list.h
@@ -53,4 +53,8 @@ extern int MOT_DW9714VAF_PowerDown(struct i2c_client *pstAF_I2Cclient,
 				int *pAF_Opened);
 extern int MOT_DW9714VAF_GetFileName(unsigned char *pFileName);
 
+#if defined(CONFIG_MOT_KYOTO)
+extern void mot_af_poweron(int state);
+#endif
+
 #endif
