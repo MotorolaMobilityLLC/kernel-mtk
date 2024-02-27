635c183c02d6ebb4e65be8c2ae6f9536b1873119
haiyong.tang <haiyong.tang@mediatek.com>
2024-02-27 18:59:00 +0800

    [ALPS08718037] Disp_color: fix color issue
    Boot logo color will change from lk to kernel
    
    MTK-Commit-Id: 16ecbb51232d7b1f34119f97bf4713349ff39fe1
    
    Signed-off-by: haiyong.tang <haiyong.tang@mediatek.com>
    CR-Id: ALPS08718037
    Feature: display_pq
    Change-Id: I50ca481b519a2ee3bfda1f2f33d59f4964b1ef68

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_color.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_color.c
@@ -3544,6 +3544,7 @@ static void ddp_color_restore(struct mtk_ddp_comp *comp)
 static void mtk_color_prepare(struct mtk_ddp_comp *comp)
 {
 	struct mtk_disp_color *color = comp_to_color(comp);
+	bool is_color_restore = (g_color_backup.COLOR_CFG_MAIN != 0);
 
 	mtk_ddp_comp_clk_prepare(comp);
 	atomic_set(&g_color_is_clock_on[index_of_color(comp->id)], 1);
@@ -3554,7 +3555,8 @@ static void mtk_color_prepare(struct mtk_ddp_comp *comp)
 			DISP_COLOR_SHADOW_CTRL, COLOR_BYPASS_SHADOW);
 
 	// restore DISP_COLOR_CFG_MAIN register
-	ddp_color_restore(comp);
+	if (is_color_restore)
+		ddp_color_restore(comp);
 }
 
 static void mtk_color_unprepare(struct mtk_ddp_comp *comp)
