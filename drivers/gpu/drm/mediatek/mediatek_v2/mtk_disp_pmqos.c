59a3e872af9bef9cf844458a9138a5a8c6c13deb
Hongjiang He <Hongjiang.He@mediatek.com>
2024-02-29 21:31:56 +0800

    [ALPS08701374] disp: fix legacy chip bw set issue
    [Description]
    for legacy chip, no need to set disp all hw bw to max
    
    [Test]
    Build pass
    
    MTK-Commit-Id: c603b0ab94cb8f6d4df343af859e2cad9365ce6a
    
    Signed-off-by: Hongjiang He <Hongjiang.He@mediatek.com>
    CR-Id: ALPS08701374
    Feature: display_mobile
    Change-Id: I603f4045c7e8a7d43f781ae8867ea8d0a77f1869
    (cherry picked from commit 4326e4e69427866b0e9ab8c31039010d4ef6ceae)

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_pmqos.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_pmqos.c
@@ -412,22 +412,27 @@ int mtk_disp_set_hrt_bw(struct mtk_drm_crtc *mtk_crtc, unsigned int bw)
 	int i, j, ret = 0;
 	tmp = bw;
 
-	for (i = 0; i < DDP_PATH_NR; i++) {
-		if (mtk_crtc->ddp_mode < DDP_MODE_NR) {
-			if (!(mtk_crtc->ddp_ctx[mtk_crtc->ddp_mode].req_hrt[i]))
+	if (priv->data->mmsys_id == MMSYS_MT6768 ||
+		priv->data->mmsys_id == MMSYS_MT6761 ||
+		priv->data->mmsys_id == MMSYS_MT6765) {
+		DDPMSG("%s: no need to set module hrt bw for legacy!\n", __func__);
+	} else {
+		for (i = 0; i < DDP_PATH_NR; i++) {
+			if (mtk_crtc->ddp_mode < DDP_MODE_NR) {
+				if (!(mtk_crtc->ddp_ctx[mtk_crtc->ddp_mode].req_hrt[i]))
+					continue;
+			}
+			for_each_comp_in_crtc_target_path(comp, mtk_crtc, j, i) {
+				ret |= mtk_ddp_comp_io_cmd(comp, NULL, PMQOS_SET_HRT_BW,
+							&tmp);
+			}
+			if (!mtk_crtc->is_dual_pipe)
 				continue;
+			for_each_comp_in_dual_pipe(comp, mtk_crtc, j, i)
+				ret |= mtk_ddp_comp_io_cmd(comp, NULL, PMQOS_SET_HRT_BW,
+						&tmp);
 		}
-		for_each_comp_in_crtc_target_path(comp, mtk_crtc, j, i) {
-			ret |= mtk_ddp_comp_io_cmd(comp, NULL, PMQOS_SET_HRT_BW,
-						   &tmp);
-		}
-		if (!mtk_crtc->is_dual_pipe)
-			continue;
-		for_each_comp_in_dual_pipe(comp, mtk_crtc, j, i)
-			ret |= mtk_ddp_comp_io_cmd(comp, NULL, PMQOS_SET_HRT_BW,
-					&tmp);
 	}
-
 	if (ret == RDMA_REQ_HRT)
 		tmp = mtk_drm_primary_frame_bw(crtc);
 
