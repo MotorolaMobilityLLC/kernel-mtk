af6a9a0d829ec9ce8ab42141f398cc399e5175bf
lin.gui <lin.gui@mediatek.com>
2024-04-14 03:39:03 +0800

    [ALPS08851540]  eMMC: support reset eMMC device
    [Description]
    Add support reset eMMC device.
    
    [Test]
    pass
    
    MTK-Commit-Id: cd44905d60287b8914657e0873ebe815f34336d4
    
    CR-Id: ALPS08851540
    Feature: eMMC
    Signed-off-by: lin.gui <lin.gui@mediatek.com>
    (cherry picked from commit 3d65c88b906169d7e2f0c7456ef160d87e45323e)
    Change-Id: I0bbadd83a0d96a9d8522852fe9473bc100838216
    (cherry picked from commit 9255d22ae4119bed1014351593d8843500fd3add)
    (cherry picked from commit 2a0d0f108af3fc2228bafb8e02350ad863365e6f)

--- drivers/mmc/host/mtk-mmc.c
+++ drivers/mmc/host/mtk-mmc.c
@@ -38,6 +38,7 @@
 #include "mtk-mmc.h"
 #include "mtk-mmc-dbg.h"
 #include "rpmb-mtk.h"
+#include "../core/mmc_ops.h"
 #include <linux/arm-smccc.h>
 #include <linux/soc/mediatek/mtk_sip_svc.h>
 #include <mt-plat/dvfsrc-exp.h>
@@ -2392,8 +2393,9 @@ static void msdc_hw_reset(struct mmc_host *mmc)
 {
 	struct msdc_host *host = mmc_priv(mmc);
 
+	dev_info(mmc_dev(mmc), "hw reset device\n");
 	sdr_set_bits(host->base + EMMC_IOCON, 1);
-	udelay(10); /* 10us is enough */
+	mdelay(10); /* 10ms is enough */
 	sdr_clr_bits(host->base + EMMC_IOCON, 1);
 }
 
@@ -2462,6 +2464,26 @@ static void mmc_mtk_crypto_enable(struct mmc_host *mmc)
 	}
 }
 
+static void msdc_enable_rst_n_func(struct mmc_host *mmc, struct mmc_card *card)
+{
+	int ret = 0;
+
+	if(!mmc || !card)
+		return;
+
+	if ((mmc->caps & MMC_CAP_HW_RESET) && !card->ext_csd.rst_n_function) {
+		ret = mmc_switch(card, EXT_CSD_CMD_SET_NORMAL,
+			EXT_CSD_RST_N_FUNCTION, 1, 1000);
+		if (!ret)
+			card->ext_csd.rst_n_function = 1;
+		else {
+			mmc->caps &= ~MMC_CAP_HW_RESET;
+			dev_info(mmc_dev(mmc), "%s: set ext_csd.rst_n_function = 1 with fail: %d\n",
+				__func__, ret);
+		}
+	}
+}
+
 static void msdc_cqe_enable(struct mmc_host *mmc)
 {
 	struct msdc_host *host = mmc_priv(mmc);
@@ -2477,6 +2499,7 @@ static void msdc_cqe_enable(struct mmc_host *mmc)
 	msdc_set_busy_timeout(host, 20 * 1000000000ULL, 0);
 	/* default read data timeout 1s */
 	msdc_set_timeout(host, 1000000000ULL, 0);
+	msdc_enable_rst_n_func(mmc, mmc->card);
 }
 
 static void msdc_cqe_disable(struct mmc_host *mmc, bool recovery)
