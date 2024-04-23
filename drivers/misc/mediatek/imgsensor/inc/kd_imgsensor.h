4a2ae6859c534f34c1ea77d7a79baf953f0d5450
Suna Li <lisn17@motorola.com>
2024-04-22 20:59:48 -0500

    Revert "(CR): taipei: camera sensor bringup[6/6]"
    This reverts commit 792ee865f61ed86d1519b0b56eff85348a16db8c.
    
    Change-Id: Iaf857b4b436e7478677d1ffdd886e60002190be3
    Signed-off-by: Li Suna <lisn17@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2945969
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Suna Li <lisn17@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/inc/kd_imgsensor.h
+++ drivers/misc/mediatek/imgsensor/inc/kd_imgsensor.h
@@ -410,17 +410,6 @@
 #define SENSOR_DRVNAME_MOT_DEVONF_S5K4H7_MIPI_RAW          "mot_devonf_s5k4h7_mipi_raw"
 //end
 
-//Begin: Add sensor id for TAIPEI
-#define MOT_TAIPEI_S5KJNS_SENSOR_ID             0x38ee
-#define SENSOR_DRVNAME_MOT_TAIPEI_S5KJNS_MIPI_RAW          "mot_taipei_s5kjns_mipi_raw"
-#define MOT_TAIPEI_HI1634Q_SENSOR_ID            0x1634
-#define SENSOR_DRVNAME_MOT_TAIPEI_HI1634Q_MIPI_RAW         "mot_taipei_hi1634q_mipi_raw"
-#define MOT_TAIPEI_S5K3P9SP04_SENSOR_ID         0x3109
-#define SENSOR_DRVNAME_MOT_TAIPEI_S5K3P9SP04_MIPI_RAW      "mot_taipei_s5k3p9sp04_mipi_raw"
-#define MOT_TAIPEI_S5K4H7_SENSOR_ID             0x487b
-#define SENSOR_DRVNAME_MOT_TAIPEI_S5K4H7_MIPI_RAW          "mot_taipei_s5k4h7_mipi_raw"
-//end
-
 //Begin: Add sensor id for CANCUNN
 #define MOT_CANCUNN_OV50D_SENSOR_ID              0x565044
 #define SENSOR_DRVNAME_MOT_CANCUNN_OV50D_MIPI_RAW           "mot_cancunn_ov50d_mipi_raw"
