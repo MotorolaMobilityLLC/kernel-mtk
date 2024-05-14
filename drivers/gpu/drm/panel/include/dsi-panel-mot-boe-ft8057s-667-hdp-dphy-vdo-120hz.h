36637c6030d190a0d4916709a12165b09d959be0
hujing3 <hujing3@lenovo.com>
2024-05-14 11:00:52 +0800

    drm/panel: kansas add 2nd boe panel
    kansas: 2nd boe panel
    Add 2nd boe panel files
    
    Change-Id: Icc780bfce6ccd27aa8f4a4d1d74a29f7bb37481e
    Signed-off-by: hujing3 <hujing3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2966613
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Qiong Xie <xieq2@motorola.com>

---
+++ drivers/gpu/drm/panel/include/dsi-panel-mot-boe-ft8057s-667-hdp-dphy-vdo-120hz.h
@@ -0,0 +1,94 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2023 MediaTek Inc.
+ */
+
+#ifndef PANEL_BOE_FT8057S_667_HDP_DSI_VDO_120HZ
+#define PANEL_BOE_FT8057S_667_HDP_DSI_VDO_120HZ
+
+#define PANEL_ESD_RECOVERY_NOFLASH	0x01
+#define PANEL_ESD_RECOVERY_VDD          0x02
+#define REGFLAG_DELAY           0xFFFC
+#define REGFLAG_UDELAY          0xFFFB
+#define REGFLAG_END_OF_TABLE    0xFFFD
+#define REGFLAG_RESET_LOW       0xFFFE
+#define REGFLAG_RESET_HIGH      0xFFFF
+
+#define FRAME_WIDTH                 720
+#define FRAME_HEIGHT                1604
+
+#define PHYSICAL_WIDTH              67716
+#define PHYSICAL_HEIGHT            150480
+
+#define DATA_RATE                   1254
+#define HSA                         4
+#define HBP                         24
+#define VSA                         2
+#define VBP                         28
+
+/*Parameter setting for mode 0 Start*/
+#define MODE_60_FPS                  60
+#define MODE_60_VFP                  2358
+#define MODE_60_HFP                  32
+
+#define MODE_0_DATA_RATE            1045
+/*Parameter setting for mode 0 End*/
+
+/*Parameter setting for mode 2 Start*/
+#define MODE_90_FPS                  90
+#define MODE_90_VFP                  1025
+#define MODE_90_HFP                  32
+
+#define MODE_90_DATA_RATE            1045
+/*Parameter setting for mode 2 End*/
+
+/*Parameter setting for mode 3 Start*/
+#define MODE_120_FPS                  120
+#define MODE_120_VFP                  362
+#define MODE_120_HFP                  32
+
+#define MODE_120_DATA_RATE            1045
+/*Parameter setting for mode 3 End*/
+
+
+
+#define LFR_EN                      1
+/* DSC RELATED */
+
+
+#define DSC_ENABLE                  1
+#define DSC_VER                     17
+#define DSC_SLICE_MODE              1
+#define DSC_RGB_SWAP                0
+#define DSC_DSC_CFG                 34
+#define DSC_RCT_ON                  1
+#define DSC_BIT_PER_CHANNEL         8
+#define DSC_DSC_LINE_BUF_DEPTH      9
+#define DSC_BP_ENABLE               1
+#define DSC_BIT_PER_PIXEL           128
+#define DSC_SLICE_HEIGHT            8
+#define DSC_SLICE_WIDTH             540
+#define DSC_CHUNK_SIZE              540
+#define DSC_XMIT_DELAY              170
+#define DSC_DEC_DELAY               526
+#define DSC_SCALE_VALUE             32
+#define DSC_INCREMENT_INTERVAL      43
+#define DSC_DECREMENT_INTERVAL      7
+#define DSC_LINE_BPG_OFFSET         12
+#define DSC_NFL_BPG_OFFSET          3511
+#define DSC_SLICE_BPG_OFFSET        3255
+#define DSC_INITIAL_OFFSET          6144
+#define DSC_FINAL_OFFSET            7072
+#define DSC_FLATNESS_MINQP          3
+#define DSC_FLATNESS_MAXQP          12
+#define DSC_RC_MODEL_SIZE           8192
+#define DSC_RC_EDGE_FACTOR          6
+#define DSC_RC_QUANT_INCR_LIMIT0    11
+#define DSC_RC_QUANT_INCR_LIMIT1    11
+#define DSC_RC_TGT_OFFSET_HI        3
+#define DSC_RC_TGT_OFFSET_LO        3
+
+int panel_esd_register_client(const char *source, struct notifier_block *nb);
+int panel_esd_unregister_client(struct notifier_block *nb);
+int panel_esd_notifier_call_chain(unsigned long val, void *v);
+#endif //end of PANEL_BOE_FT8057S_667_HDP_DSI_VDO_120HZ
