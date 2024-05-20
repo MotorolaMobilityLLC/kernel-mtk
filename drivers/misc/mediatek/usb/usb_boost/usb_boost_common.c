b56dc2d94ac887e5688fe00c1931959f535fbea3
shenying2 <shenying2@lenovo.com>
2024-05-20 11:31:53 +0800

    usb: enable usb_boost when MTP or accessory type
    AS change: https://gerrit.mot.com/#/c/2049070/
    MtpInterface,mclass=255,mSubclass=255,mProtocol=0, which same as Accessory.
    
    propagration from: https://gerrit.mot.com/#/c/2823042/
    
    Change-Id: Ib83194415e427aa46987417b0ec62cb0eaa0d05b
    Signed-off-by: shenying2 <shenying2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2972514
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Yifan Xu <xuyf4@motorola.com>

--- drivers/misc/mediatek/usb/usb_boost/usb_boost_common.c
+++ drivers/misc/mediatek/usb/usb_boost/usb_boost_common.c
@@ -90,6 +90,7 @@ enum {
 	USB_TYPE_MTP,
 	/* USB_TYPE_PTP, */
 	USB_TYPE_RNDIS,
+	USB_TYPE_ACCESSORY,
 };
 
 #if IS_ENABLED(CONFIG_USB_MTU3)
@@ -709,6 +710,9 @@ static int mtu3_get_ep_type(struct usb_descriptor_header **f_desc)
 		} else if (int_class == 0xe0 && int_subclass == 0x1
 			&& int_protocol == 0x3) {
 			return USB_TYPE_RNDIS;
+		} else if (int_class == 0xff && int_subclass == 0xff
+			&& int_protocol == 0x00) {
+			return USB_TYPE_ACCESSORY;
 		}
 	}
 
@@ -790,8 +794,10 @@ static void boost_gadget_queue(void *unused, struct mtu3_request *mreq)
 
 	switch (type) {
 	case USB_TYPE_MTP:
+	case USB_TYPE_ACCESSORY:
 		//if (req->length >= 8192)
 		usb_boost();
+		pr_info("USB_TYPE_MTP/USB_TYPE_ACCESSORY boost!");
 		break;
 	case USB_TYPE_RNDIS:
 		if (mep->type == USB_ENDPOINT_XFER_BULK)
