8255e583ab7deb8ecf11186b13124651250e26ea
hujing3 <hujing3@lenovo.com>
2024-05-23 15:27:07 +0800

    drm/mediatek_v2: mtk display add esd recovery flag for tp
    drm/mediatek_v2:
    add esd recovery flag for tp notify
    porting (CR): 2691209
    
    Change-Id: I83c0490d59385acfc4288a314a2df49e07289a62
    Signed-off-by: hujing3 <hujing3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3045298
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_dsi.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_dsi.c
@@ -422,6 +422,21 @@ int touch_set_state(int state, int panel_idx)
 }
 EXPORT_SYMBOL(touch_set_state);
 
+#ifdef PANEL_TP_ESD_RECOVERY
+static int tp_esd_recovery_flag = 0;
+int touch_set_esd_recovery_state(int state, int panel_idx)
+{
+        if (state == 1) {
+                tp_esd_recovery_flag = 1;
+        } else{
+                tp_esd_recovery_flag = 0;
+        }
+
+        return 0;
+}
+EXPORT_SYMBOL(touch_set_esd_recovery_state);
+#endif
+
 struct mtk_panel_ext *mtk_dsi_get_panel_ext(struct mtk_ddp_comp *comp);
 static void mtk_dsi_set_targetline(struct mtk_ddp_comp *comp,
 				struct cmdq_pkt *handle, unsigned int hacive);
@@ -3595,6 +3610,9 @@ static void mtk_output_dsi_enable(struct mtk_dsi *dsi,
 	struct mtk_drm_private *priv = (crtc && crtc->dev)
 		? crtc->dev->dev_private : NULL;
 	unsigned int crtc_idx;
+#ifdef PANEL_TP_ESD_RECOVERY
+	struct mtk_panel_ext *panel_ext = mtk_crtc->panel_ext;
+#endif
 
 	DDPINFO("%s +\n", __func__);
 
@@ -3611,6 +3629,11 @@ static void mtk_output_dsi_enable(struct mtk_dsi *dsi,
 			return;
 		}
 	}
+
+#ifdef PANEL_TP_ESD_RECOVERY
+	if (panel_ext && panel_ext->funcs && panel_ext->funcs->set_esd_recovery_flag)
+                panel_ext->funcs->set_esd_recovery_flag(tp_esd_recovery_flag);
+#endif
 
 	if (dsi->output_en) {
 		if (mtk_dsi_doze_status_change(dsi)) {
