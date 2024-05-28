08b9b84059b3f09df7c81c39a9bf1e4fb4bb6b0f
huandong <tinno80@motorola.com>
2024-05-28 15:22:36 +0800

    [Kansas]Sc202acs eeprom bring up[2/3]
    Details:
    a. add eeprom data apply.
    b. add eeprom dtsi.
    
    Change-Id: I64fbf93f8b3fda1f88efaba5100bfaa75b363b4a
    Signed-off-by: xubing <tinno82@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2984463
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: <guanxin6@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_kansas_sc202acs_mipi_raw/mot_kansas_sc202acs_mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_kansas_sc202acs_mipi_raw/mot_kansas_sc202acs_mipiraw_Sensor.c
@@ -36,7 +36,7 @@
 #define LOG_1 LOG_INF("MOT_KANSAS_SC202ACS,MIPI 1LANE\n")
 #define LOG_2 LOG_INF("preview 1600*1200@30fps,864Mbps/lane; video 1600*1200@30fps,864Mbps/lane; capture 2M@30fps,864Mbps/lane\n")
 /****************************   Modify end    *******************************************/
-
+extern void sc202acs_read_otp_data(void);
 //#define LOG_INF(format, args...)    xlog_printk(ANDROID_LOG_INFO   , PFX, "[%s] " format, __FUNCTION__, ##args)
 
 #define LOG_INF(format, args...)    \
@@ -821,6 +821,7 @@ static kal_uint32 get_imgsensor_id(UINT32 *sensor_id)
 			pr_err("cwd return_sensor_id:0x%x\n",*sensor_id);
 			if (*sensor_id == imgsensor_info.sensor_id) {
 				LOG_INF("i2c write id: 0x%x, sensor id: 0x%x\n", imgsensor.i2c_write_id,*sensor_id);
+				sc202acs_read_otp_data();
 				return ERROR_NONE;
 			}
 			LOG_INF("Read sensor id fail, id: 0x%x\n", imgsensor.i2c_write_id,*sensor_id);
@@ -959,7 +960,7 @@ static kal_uint32 preview(MSDK_SENSOR_EXPOSURE_WINDOW_STRUCT *image_window,
 	imgsensor.autoflicker_en = KAL_FALSE;
 	spin_unlock(&imgsensor_drv_lock);
 	preview_setting();
-	set_mirror_flip(sensor_config_data->SensorImageMirror);
+	set_mirror_flip(imgsensor.mirror);
 	return ERROR_NONE;
 }    /*    preview   */
 
@@ -1001,7 +1002,7 @@ static kal_uint32 capture(MSDK_SENSOR_EXPOSURE_WINDOW_STRUCT *image_window,
 	}
 	spin_unlock(&imgsensor_drv_lock);
 	capture_setting(imgsensor.current_fps);
-	set_mirror_flip(sensor_config_data->SensorImageMirror);
+	set_mirror_flip(imgsensor.mirror);
 	return ERROR_NONE;
 }    /* capture() */
 static kal_uint32 normal_video(MSDK_SENSOR_EXPOSURE_WINDOW_STRUCT *image_window,
@@ -1018,7 +1019,7 @@ static kal_uint32 normal_video(MSDK_SENSOR_EXPOSURE_WINDOW_STRUCT *image_window,
 	imgsensor.autoflicker_en = KAL_FALSE;
 	spin_unlock(&imgsensor_drv_lock);
 	normal_video_setting(imgsensor.current_fps);
-	set_mirror_flip(sensor_config_data->SensorImageMirror);
+	set_mirror_flip(imgsensor.mirror);
 	return ERROR_NONE;
 }    /*    normal_video   */
 
@@ -1038,7 +1039,7 @@ static kal_uint32 hs_video(MSDK_SENSOR_EXPOSURE_WINDOW_STRUCT *image_window,
 	imgsensor.autoflicker_en = KAL_FALSE;
 	spin_unlock(&imgsensor_drv_lock);
 	hs_video_setting();
-	set_mirror_flip(sensor_config_data->SensorImageMirror);
+	set_mirror_flip(imgsensor.mirror);
 	return ERROR_NONE;
 }    /*    hs_video   */
 
@@ -1057,7 +1058,7 @@ static kal_uint32 slim_video(MSDK_SENSOR_EXPOSURE_WINDOW_STRUCT *image_window,
 	imgsensor.autoflicker_en = KAL_FALSE;
 	spin_unlock(&imgsensor_drv_lock);
 	slim_video_setting();
-	set_mirror_flip(sensor_config_data->SensorImageMirror);
+	set_mirror_flip(imgsensor.mirror);
 	return ERROR_NONE;
 }    /*    slim_video     */
 #if 1
