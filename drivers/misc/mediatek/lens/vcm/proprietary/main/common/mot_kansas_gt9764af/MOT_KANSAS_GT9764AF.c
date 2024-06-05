35be567b65e827ad92a2b834d1eca3cc36eb5e10
huandong <tinno80@motorola.com>
2024-06-05 12:48:43 +0800

    [Kansas]update gt9764 SAC setting
    Details:
    a. update gt9764 SAC setting
    
    Change-Id: I348fac37fb13365e9ac692bbb00f2b724d3b3f34
    Signed-off-by: huandong <tinno80@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2991296
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <wangzl37@motorola.com>
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lens/vcm/proprietary/main/common/mot_kansas_gt9764af/MOT_KANSAS_GT9764AF.c
+++ drivers/misc/mediatek/lens/vcm/proprietary/main/common/mot_kansas_gt9764af/MOT_KANSAS_GT9764AF.c
@@ -142,9 +142,9 @@ static int initAF(void)
 
 		s4AF_ReadReg(0x00, &Temp);  //ic info
 		LOG_INF("Check HW version: 0x00 is %x\n", Temp);
-		ret = s4AF_WriteReg(0, 0x02, 0x00); //CONTROL
-
-
+		ret = s4AF_WriteReg(0, 0x02, 0x02); //CONTROL
+		s4AF_WriteReg(0, 0x06, 0x80);
+		s4AF_WriteReg(0, 0x07, 0x62);
 
 		spin_lock(g_pAF_SpinLock);
 		*g_pAF_Opened = 2;
