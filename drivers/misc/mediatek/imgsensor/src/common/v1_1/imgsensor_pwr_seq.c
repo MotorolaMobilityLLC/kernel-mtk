4dc87d9d6d0c032b4ccabcc0341820dd12a88007
Lc_Sl <longc169@motorola.com>
2024-06-05 20:53:48 +0800

    vegasNA: [OV50D][CMS1.0][1.0.5]Readout - 04:03 Default mode@30fps Driver
    details:
    -Add sw configuration and driver
    
    Change-Id: I2c8c8b89d140c7edafe729c6a4637ea24de6db28
    Reviewed-on: https://gerrit.mot.com/2991962
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: Simon C <chenzm8@motorola.com>
    Reviewed-by: <huangsw2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/imgsensor_pwr_seq.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/imgsensor_pwr_seq.c
@@ -11,6 +11,21 @@
 
 /* Legacy design */
 struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
+#if defined(CONFIG_MOT_VEGAS_CAMERA_PROJECT)
+#if defined(MOT_VEGAS_OV50D_MIPI_RAW)
+	{
+		SENSOR_DRVNAME_MOT_VEGAS_OV50D_MIPI_RAW,
+		{
+			{RST, Vol_Low, 1},
+			{SensorMCLK, Vol_High, 1},
+			{DOVDD, Vol_1800, 1},
+			{AVDD, Vol_2800, 1},
+			{DVDD, Vol_1200, 1},
+			{RST, Vol_High, 5},
+		},
+	},
+#endif
+#endif
 #if defined(CONFIG_MOT_KANSAS_CAMERA_PROJECT)
 #if defined(MOT_KANSAS_S5KJNS_MIPI_RAW)
 	{
