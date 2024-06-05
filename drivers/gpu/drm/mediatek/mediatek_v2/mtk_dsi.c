784eb1421faf32bad48ad0a05a4c0795aa1f3f54
mtk24049 <mark.xu@mediatek.com>
2024-06-06 04:09:38 +0800

    [ALPS09023139] disp: fix mipi hopping params
    [Description]
    
    1.when mipi hopping on, get data_rate from dyn;
    2.when mipi hopping on, get porch setting from dyn;
    3.video mode do not support mipi clock change only;
    
    [Test]
    1.bootup success
    2.switch fps is ok
    3.mipi hopping is ok
    
    MTK-Commit-Id: f63d514259ad85e7342e35f18ef5bce35ec7c8df
    
    Signed-off-by: mtk24049 <mark.xu@mediatek.com>
    CR-Id: ALPS09023139
    Feature: display_mobile
    Change-Id: I33645991dc43b8d03713e16999c4614aa461e5ae
    (cherry-pick from 17e957c75bc2b09b64f28e455fb2116b9d854d1d)

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_dsi.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_dsi.c
@@ -966,6 +966,21 @@ unsigned int mtk_dsi_default_rate(struct mtk_dsi *dsi)
 		(dsi->d_rate != 0)) {
 		data_rate = dsi->d_rate;
 		DDPMSG("%s, data rate=%d\n", __func__, data_rate);
+
+	} else if (dsi->mipi_hopping_sta
+		&& dsi->ext && dsi->ext->params
+		&& dsi->ext->params->dyn.switch_en
+		&& dsi->ext->params->dyn.data_rate) {
+		data_rate = dsi->ext->params->dyn.data_rate;
+		DDPMSG("%s, mipi_hopping_sta, data rate=%d\n", __func__, data_rate);
+
+	} else if (dsi->mipi_hopping_sta
+		&& dsi->ext && dsi->ext->params
+		&& dsi->ext->params->dyn.switch_en
+		&& dsi->ext->params->dyn.pll_clk) {
+		data_rate = dsi->ext->params->dyn.pll_clk * 2;
+		DDPMSG("%s, mipi_hopping_sta, data rate=%d\n", __func__, data_rate);
+
 	} else if (priv && mtk_drm_helper_get_opt(priv->helper_opt,
 		MTK_DRM_OPT_DYN_MIPI_CHANGE)
 		&& dsi->ext && dsi->ext->params
@@ -8825,9 +8840,12 @@ static void mtk_dsi_vdo_timing_change(struct mtk_dsi *dsi,
 	}
 	mtk_crtc_pkt_create(&handle, &(mtk_crtc->base), client);
 
+	/* video mode do not support to only change mipi clock */
 	if (fps_chg_index & MODE_DSI_CLK) {
 		DDPINFO("%s, change MIPI Clock\n", __func__);
-	} else if (fps_chg_index & MODE_DSI_HFP) {
+	}
+
+	if (fps_chg_index & MODE_DSI_HFP) {
 		DDPINFO("%s, change HFP\n", __func__);
 		/*wait and clear EOF
 		 * avoid other display related task break fps change task
@@ -8845,13 +8863,20 @@ static void mtk_dsi_vdo_timing_change(struct mtk_dsi *dsi,
 			if (dsi->data_rate != 0)
 				mtk_dsi_phy_timconfig(dsi, NULL);
 		}
-		if (dsi->mipi_hopping_sta) {
+
+		/* if change hfp, get hfp */
+		if (dsi->mipi_hopping_sta
+			&& dsi->ext && dsi->ext->params
+			&& dsi->ext->params->dyn.switch_en
+			&& dsi->ext->params->dyn.hfp) {
 			DDPINFO("%s,mipi_clk_change_sta\n", __func__);
 			hfp = dsi->ext->params->dyn.hfp;
 		} else
 			hfp = adjusted_mode.hsync_start -
 				adjusted_mode.hdisplay;
 		dsi->vm.hfront_porch = hfp;
+
+		/* if change vfp, get vfp, refer to hfp modification */
 
 		mtk_dsi_calc_vdo_timing(dsi);
 		mtk_dsi_porch_setting(comp, handle, DSI_HFP, dsi->hfp_byte);
@@ -8889,8 +8914,9 @@ static void mtk_dsi_vdo_timing_change(struct mtk_dsi *dsi,
 
 		if (dsi && dsi->ext && dsi->ext->params
 			&& (dsi->mipi_hopping_sta
-			|| (is_bdg_supported() && dsi->bdg_mipi_hopping_sta)
-			) && dsi->ext->params->dyn.vfp) {
+				|| (is_bdg_supported() && dsi->bdg_mipi_hopping_sta))
+			&& dsi->ext->params->dyn.switch_en
+			&& dsi->ext->params->dyn.vfp) {
 			DDPINFO("%s,mipi_clk_change_sta\n", __func__);
 			vfp = dsi->ext->params->dyn.vfp;
 		} else
