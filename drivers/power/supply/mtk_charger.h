991af7697c479e8da24699bedc78989f7fa4716e
xuyf4 <xuyf4@motorola.com>
2024-06-11 10:34:48 +0800

    charge: add charger power uevent
    1.For showing turbo charging UI, add charger power
    uevent to upper
    2.add tcmd chg_watt file node
    3.update PD apdo
    
    port from:
    https://gerrit.mot.com/#/c/2811817/
    https://gerrit.mot.com/#/c/2569527/
    https://gerrit.mot.com/#/c/2814280/
    
    Change-Id: I3ff6ece8a8f4125636ff404072ba49a0e0af6f8b
    Reviewed-on: https://gerrit.mot.com/2996451
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yuecai Liu <liuyc20@motorola.com>
    Reviewed-by: liujian <liujian24@lenovo.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/mtk_charger.h
+++ drivers/power/supply/mtk_charger.h
@@ -362,6 +362,14 @@ struct mmi_params {
 	int			wls_boost_en;
 	int			charge_rate;
 	unsigned int	active_fast_alg;
+	int			typec_rp_max_current;
+
+	int			pd_pmax_mw;
+	struct adapter_auth_data	apdo_cap;
+	int			pd_cap_max_watt;
+	int			vbus_h;
+	int			vbus_l;
+	int			charger_watt;
 };
 /*moto mmi Functionality end*/
 
