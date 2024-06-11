ee9c3d1161ee8ae5e45a96353cfbedb34b6dc0b9
Lc_Myy <longc170@motorola.com>
2024-06-11 21:23:22 +0800

    VegasNA: [S5K3P9][CMS1.0][1.0.5]Readout - 04:03 Default mode@30fps Driver.
    details:
    - Add sw configuration and driver
    
    Change-Id: I24b0201b345a83cdefd82447bed98b2ae14d6e23
    Signed-off-by: Lc_Myy <longc170@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2997048
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-by: Simon C <chenzm8@motorola.com>

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/imgsensor_pwr_seq.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/imgsensor_pwr_seq.c
@@ -25,6 +25,19 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
 		},
 	},
 #endif
+#if defined(MOT_VEGAS_S5K3P9_MIPI_RAW)
+	{
+		SENSOR_DRVNAME_MOT_VEGAS_S5K3P9_MIPI_RAW,
+		{
+			{RST, Vol_Low, 1},
+			{SensorMCLK, Vol_High, 1},
+			{DOVDD, Vol_1800, 1},
+			{DVDD, Vol_1050, 1},
+			{AVDD, Vol_2800, 1},
+			{RST, Vol_High, 12},
+		},
+	},
+#endif
 #endif
 #if defined(CONFIG_MOT_KANSAS_CAMERA_PROJECT)
 #if defined(MOT_KANSAS_S5KJNS_MIPI_RAW)
