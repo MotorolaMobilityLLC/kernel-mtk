7dc50d81557cc06ad24af7fda1e20a4cfae9f0c8
mtk25099 <wanwan.liu@mediatek.com>
2024-06-19 14:07:41 +0800

    [ALPS09060915] GenieZone:change gz_log_wq init order
    [Description]
    Init gz_log_wq before registering call_notifier to avoid NULL list.
    
    [Test]
    Build pass/Boot pass/UT pass.
    
    MTK-Commit-Id: 1030933e1c59ac9ee2fbdc27fd1a2bafe4d72d02
    
    Signed-off-by: mtk25099 <wanwan.liu@mediatek.com>
    CR-Id: ALPS09060915
    Feature: GenieZone
    Change-Id: I53bb16cb7779041b2ca015773a305f9d8c2a4205
    (cherry-pick from 771d34465650c0d5a1004ee869049b879af85711)

--- drivers/misc/mediatek/geniezone/gz-trusty/gz-log.c
+++ drivers/misc/mediatek/geniezone/gz-trusty/gz-log.c
@@ -874,6 +874,7 @@ static int trusty_gz_log_probe(struct platform_device *pdev)
 	}
 #endif
 
+	init_waitqueue_head(&gls->gz_log_wq);
 	gls->call_notifier.notifier_call = trusty_log_call_notify;
 	ret = trusty_call_notifier_register(gls->trusty_dev,
 					       &gls->call_notifier);
@@ -890,7 +891,6 @@ static int trusty_gz_log_probe(struct platform_device *pdev)
 		dev_info(&pdev->dev, "failed to register panic notifier\n");
 		goto error_panic_notifier;
 	}
-	init_waitqueue_head(&gls->gz_log_wq);
 	atomic_set(&gls->gz_log_event_count, 0);
 	atomic_set(&gls->readable, 1);
 	platform_set_drvdata(pdev, gls);
