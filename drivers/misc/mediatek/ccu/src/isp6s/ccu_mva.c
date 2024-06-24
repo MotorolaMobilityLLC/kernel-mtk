18476f7354b1404dd9254b28f429bfda14191795
carl-nh.lin <carl-nh.lin@mediatek.com>
2024-06-25 04:48:10 +0800

    [ALPS09037038] CCU: fix OOB of dealloc_mem
    [Description]
    Fix OOB of dealloc_mem.
    
    [Test]
    Build pass and UT pass.
    
    MTK-Commit-Id: a7875a7bc495d58a180d84ecfa59e9e1003123a2
    
    Signed-off-by: carl-nh.lin <carl-nh.lin@mediatek.com>
    CR-Id: ALPS09037038
    Feature: ccu
    Change-Id: Ib89cf4de598a4f8fb8a0c96ad99e13c99ad9ab3a
    (cherry-pick from 6f48cd55497e718dad31332ba568cf67c4f2e738)

--- drivers/misc/mediatek/ccu/src/isp6s/ccu_mva.c
+++ drivers/misc/mediatek/ccu/src/isp6s/ccu_mva.c
@@ -43,7 +43,15 @@ int ccu_allocate_mem(struct ccu_device_s *dev, struct CcuMemHandle *memHandle,
 
 int ccu_deallocate_mem(struct ccu_device_s *dev, struct CcuMemHandle *memHandle)
 {
-	struct CcuMemHandle *handle = &ccu_buffer_handle[memHandle->meminfo.cached];
+	struct CcuMemHandle *handle;
+
+	if (dev == NULL)
+		return -1;
+
+	if (memHandle == NULL)
+		return -2;
+
+	handle = &ccu_buffer_handle[(memHandle->meminfo.cached) ? 1 : 0];
 
 	if (handle->meminfo.va != NULL) {
 		dma_free_attrs(dev->dev, handle->meminfo.size,
