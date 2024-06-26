7e75cdba3a43acd836dcc57255d233c63ff49f1b
LiZidong <tnlizd@motorola.com>
2024-06-26 14:55:35 +0800

    kansan: add cp enable otg interface [1/2]
    why
      acdrv1 mos can not open causing out of 4.5v voltage in otg mode
      add otg interface
    
    Change-Id: I2ff0fcbaf8abaefd889b6bf42f7cf97b05dbb64b
    Reviewed-on: https://gerrit.mot.com/3011153
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: liujian <liujian24@lenovo.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/charger_class.c
+++ drivers/power/supply/charger_class.c
@@ -698,6 +698,21 @@ int charger_dev_set_boost_current_limit(struct charger_device *chg_dev, u32 uA)
 }
 EXPORT_SYMBOL(charger_dev_set_boost_current_limit);
 
+int charger_dev_is_enable_otg(struct charger_device *chg_dev, bool en)
+{
+	if (chg_dev != NULL && chg_dev->ops != NULL && chg_dev->ops->is_enable_otg)
+		return chg_dev->ops->is_enable_otg(chg_dev, en);
+	return -EOPNOTSUPP;
+}
+EXPORT_SYMBOL(charger_dev_is_enable_otg);
+int charger_dev_is_enable_acdrv1(struct charger_device *chg_dev, bool en)
+{
+	if (chg_dev != NULL && chg_dev->ops != NULL && chg_dev->ops->is_enable_acdrv1)
+		return chg_dev->ops->is_enable_acdrv1(chg_dev, en);
+	return -EOPNOTSUPP;
+}
+EXPORT_SYMBOL(charger_dev_is_enable_acdrv1);
+
 int charger_dev_get_zcv(struct charger_device *chg_dev, u32 *uV)
 {
 	if (chg_dev != NULL && chg_dev->ops != NULL && chg_dev->ops->get_zcv)
