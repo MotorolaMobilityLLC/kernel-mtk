c32f42875c44690acb9a123c8a94bb0875979446
huandong <tinno80@motorola.com>
2024-09-19 11:44:32 +0800

    kansas: Fix Cam0 Cam1 test_sensor_fusion.py fail
    root cause: OFFSET_TO_START_OF_EXPOSURE is too big.
    solution: Modify to appropriate parameters.
    test result: Test pass.
    
    Change-Id: Ieb7a76b4707578fa9f6110d6f3905bf44eb88e5d
    Signed-off-by: huandong <tinno80@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3090945
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: <wangzl37@motorola.com>
    Reviewed-by: Guan Xin <guanxin6@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_kansas_s5k3p9_mipi_raw/mot_kansas_s5k3p9mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_kansas_s5k3p9_mipi_raw/mot_kansas_s5k3p9mipiraw_Sensor.c
@@ -5020,7 +5020,7 @@ static kal_uint32 feature_control(MSDK_SENSOR_FEATURE_ENUM feature_id,
 		break;
 	case SENSOR_FEATURE_GET_OFFSET_TO_START_OF_EXPOSURE:
 		*(MUINT32 *)(uintptr_t)(*(feature_data + 1))
-			= 2500000;
+			= 1055000;
 		break;
 	case SENSOR_FEATURE_GET_PERIOD_BY_SCENARIO:
 		switch (*feature_data) {
