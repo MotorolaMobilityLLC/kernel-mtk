b298df105a910e9b94db9914a8e1e1b483edef23
Akshatha R <a24159@motorola.com>
2024-10-03 15:23:49 +0530

    ccci_ciq not listed for kansas and vegas
    Update the CIQ port related info to kernel-5.15
    Porting https://gerrit.mot.com/#/c/2368113/
    Add TX/RX channels for IQI CCCI - 185, 186
    Add channel mapping for same with name "ccci_ciq"
    Ensure packets are not dropped if modem send messages before AP opens
    CCCI node.
    
    Change-Id: I3a5fcfca389ba7fbea70a2ed425f0e24d539c239
    Reviewed-on: https://gerrit.mot.com/3102426
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Thiago Kunsch Rocha <tkunsch@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/eccci/inc/ccci_core.h
+++ drivers/misc/mediatek/eccci/inc/ccci_core.h
@@ -481,6 +481,8 @@ enum CCCI_CH {
 	CCCI_TCHE_TX			= 182,
 	CCCI_DISP_RX			= 183,
 	CCCI_DISP_TX			= 184,
+        CCCI_CIQ_RX                     = 185,
+        CCCI_CIQ_TX                     = 186,
 	CCCI_WIFI_RX			= 187,
 	CCCI_WIFI_TX			= 188,
 	CCCI_VTS_RX			= 189,
