7685c13b0efa4a59c63e3d8f5cb25d2314305520
xubing <tinno82@motorola.com>
2024-11-21 17:56:13 +0800

    bogota: IMX882 && GC08A8 camera driver info update
    rootcause:n/a
    solution: update IMX882 && GC08A8 mirror flip info
    
    Change-Id: I49a05c1f970cc565d178eab7de3dc49afe47aa98
    Signed-off-by: xubing <tinno82@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3150693
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: <huangfx5@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_bogota_imx882_mipi_raw/mot_bogota_imx882mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_bogota_imx882_mipi_raw/mot_bogota_imx882mipiraw_Sensor.c
@@ -149,7 +149,7 @@ static struct imgsensor_info_struct imgsensor_info = {
 	/* 0,MIPI_OPHY_NCSI2;  1,MIPI_OPHY_CSI2 */
 	.mipi_sensor_type = MIPI_OPHY_NCSI2, /* 0,MIPI_OPHY_NCSI2; 1,MIPI_OPHY_CSI2 */
 	.mipi_settle_delay_mode = 0,
-	.sensor_output_dataformat = SENSOR_OUTPUT_FORMAT_RAW_4CELL_R,
+	.sensor_output_dataformat = SENSOR_OUTPUT_FORMAT_RAW_4CELL_B,
 	.mclk = 24, /* mclk value, suggest 24 or 26 for 24Mhz or 26Mhz */
 	/*.mipi_lane_num = SENSOR_MIPI_4_LANE,*/
 	.mipi_lane_num = SENSOR_MIPI_4_LANE,
@@ -160,7 +160,7 @@ static struct imgsensor_info_struct imgsensor_info = {
 	.i2c_speed = 1000, /* i2c read/write speed */
 };
 static struct imgsensor_struct imgsensor = {
-	.mirror = IMAGE_NORMAL,	/* mirrorflip information */
+	.mirror = IMAGE_HV_MIRROR,	/* mirrorflip information */
 	/* IMGSENSOR_MODE enum value,record current sensor mode,such as:
 	 * INIT, Preview, Capture, Video,High Speed Video, Slim Video
 	 */
