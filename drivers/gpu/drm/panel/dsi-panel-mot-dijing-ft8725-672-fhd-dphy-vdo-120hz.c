0703a6e851cf369fa1121bb85d08b5a30922ce66
hujing3 <hujing3@lenovo.com>
2024-11-26 14:41:09 +0800

    drm/panel: bogota 2nd dj focal touch enable tp_gesture_flag check
    bogota: 2nd dijing focal panel
    gesture support with tp_gesture_flag check
    
    Change-Id: If8d9e4b1c8b8aa4a9796df8713d1f082b3909ac9
    Signed-off-by: hujing3 <hujing3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3154105
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/dsi-panel-mot-dijing-ft8725-672-fhd-dphy-vdo-120hz.c
+++ drivers/gpu/drm/panel/dsi-panel-mot-dijing-ft8725-672-fhd-dphy-vdo-120hz.c
@@ -191,7 +191,7 @@ static int lcm_disable(struct drm_panel *panel)
 	return 0;
 }
 
-#if 0
+#if 1
 static int panel_set_gesture_flag(int state)
 {
 	if(state == 1)
@@ -720,7 +720,7 @@ static struct mtk_panel_funcs ext_funcs = {
 	.ext_param_set = mtk_panel_ext_param_set,
 	.get_lcm_version = panel_get_lcm_version,
 //	.ata_check = panel_ata_check,
-//	.set_gesture_flag = panel_set_gesture_flag,
+	.set_gesture_flag = panel_set_gesture_flag,
 	.panel_feature_set = panel_feature_set,
 };
 #endif
