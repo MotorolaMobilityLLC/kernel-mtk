f566eea440d47e54acfe83ea956c0b4a7cad28f4
wangkexin <tinno64@motorola.com>
2024-12-12 10:09:21 +0800

    drm/panel: bogota modify 1st panel initcode to v04
    bogota: modify 1st panel initcode to v04
    Fix the splash screen at the top of the 1st panel
    
    Change-Id: I8ce7b491df459b292cefacf377a03fe924dd226f
    Signed-off-by: wangkexin <tinno64@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3169115
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jing Hu <hujing3@lenovo.com>
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/include/dsi-panel-mot-boe-icnl9922c-672-fhd-dphy-vdo-120hz.h
+++ drivers/gpu/drm/panel/include/dsi-panel-mot-boe-icnl9922c-672-fhd-dphy-vdo-120hz.h
@@ -20,11 +20,11 @@
 #define PHYSICAL_HEIGHT            154560
 
 #define HSA                         4
-#define HBP                         8
-#define HFP                         16
+#define HBP                         48
+#define HFP                         48
 #define VSA                         4
 #define VBP                         32
-#define DATA_RATE                   810
+#define DATA_RATE                   948
 
 //Parameter setting for mode 120
 #define MODE_120_FPS                  120
