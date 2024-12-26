71105cf0f4d4d745e66b80979862234d8ca581a8
longcheer <longc134@motorola.com>
2024-12-26 16:11:38 +0800

    drm/panel: Vegas 3rd TM picture residue issue with new cmds
    Vegas: 3rd TM panel
    picture residue issue fix with new init cmds
    
    Change-Id: Iee6dcf49437cdeb41761ef71869df7680ab6c85d
    Signed-off-by: longcheer <longc134@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3183493
    Reviewed-by: Jing Hu <hujing3@lenovo.com>
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/dsi-panel-mot-vdo-tm-ili77600a-1080-2388-dphy-120hz.c
+++ drivers/gpu/drm/panel/dsi-panel-mot-vdo-tm-ili77600a-1080-2388-dphy-120hz.c
@@ -199,6 +199,12 @@ static void tianma_panel_init(struct tianma *ctx)
 	pr_info("tm ili77600a init start!\n");
 	tianma_dcs_write_seq_static(ctx,0xFF, 0x5A,0xA5,0x06);
 	tianma_dcs_write_seq_static(ctx,0x3E, 0xE2);
+	tianma_dcs_write_seq_static(ctx,0xFF, 0x5A,0xA5,0x02);
+	tianma_dcs_write_seq_static(ctx,0x80, 0x23);
+	tianma_dcs_write_seq_static(ctx,0x76, 0x13);
+	tianma_dcs_write_seq_static(ctx,0x79, 0x00);
+	tianma_dcs_write_seq_static(ctx,0xAF, 0x01);
+	tianma_dcs_write_seq_static(ctx,0x5B, 0x00);
 	tianma_dcs_write_seq_static(ctx,0xFF, 0x5A,0xA5,0x03);
 	tianma_dcs_write_seq_static(ctx,0x83, 0xC8);
 	tianma_dcs_write_seq_static(ctx,0x84, 0x04);
