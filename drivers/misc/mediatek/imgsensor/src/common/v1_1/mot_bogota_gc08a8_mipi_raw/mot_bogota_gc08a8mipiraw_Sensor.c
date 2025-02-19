0e4871bcb62394cfd0658dc1ef3b457941087a8b
wangminchao <tinno81@motorola.com>
2025-02-19 17:59:30 +0800

    bogota: GC08A8 video setting update
    root cause: update video 3264*1836 setting
    solution: update video 3264*1836 setting
    test result: test pass.
    
    Change-Id: Id5ef7e62caa6558c35312d32ee2da3cd1ceb5b21
    Reviewed-on: https://gerrit.mot.com/3251621
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: <shihl5@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_bogota_gc08a8_mipi_raw/mot_bogota_gc08a8mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_bogota_gc08a8_mipi_raw/mot_bogota_gc08a8mipiraw_Sensor.c
@@ -96,7 +96,7 @@ static struct imgsensor_info_struct imgsensor_info = {
 		.startx = 0,
 		.starty = 0,
 		.grabwindow_width = 3264,
-		.grabwindow_height = 2448,
+		.grabwindow_height = 1836,
 		.mipi_data_lp2hs_settle_dc = 85,
 		.mipi_pixel_rate = 268800000,
 		.max_framerate = 300,
@@ -207,7 +207,7 @@ static struct imgsensor_struct imgsensor = {
 static struct SENSOR_WINSIZE_INFO_STRUCT imgsensor_winsize_info[5] = {
 	{ 3264, 2448, 0, 0, 3264, 2448, 3264, 2448, 0, 0, 3264, 2448, 0, 0, 3264, 2448}, /* Preview */
 	{ 3264, 2448, 0, 0, 3264, 2448, 3264, 2448, 0, 0, 3264, 2448, 0, 0, 3264, 2448}, /* capture */
-	{ 3264, 2448, 0, 0, 3264, 2448, 3264, 2448, 0, 0, 3264, 2448, 0, 0, 3264, 2448}, /* video */
+	{ 3264, 2448, 0, 306, 3264, 1836, 3264, 1836, 0, 0, 3264, 1836, 0, 0, 3264, 1836}, /* video */
 	{ 3264, 2448, 0, 0, 3264, 2448, 3264, 2448, 0, 0, 3264, 2448, 0, 0, 3264, 2448}, /* hs_video */
 	{ 3264, 2448, 0, 0, 3264, 2448, 3264, 2448, 0, 0, 3264, 2448, 0, 0, 3264, 2448}, /* slim_video */
 };
@@ -569,8 +569,8 @@ static void capture_setting(kal_uint16 currefps)
 static void normal_video_setting(kal_uint16 currefps)
 {
 	pr_debug(PFX,"[%s] normal_video_Start, currefps:%d\n", __func__, currefps);
-	table_write_cmos_sensor(mot_bogota_gc08a8_3264x2448_addr_data,
-		sizeof(mot_bogota_gc08a8_3264x2448_addr_data)/sizeof(kal_uint16));
+	table_write_cmos_sensor(mot_bogota_gc08a8_3264x1836_addr_data,
+		sizeof(mot_bogota_gc08a8_3264x1836_addr_data)/sizeof(kal_uint16));
 	pr_debug(PFX,"[%s] normal_video_End\n", __func__);
 }
 
