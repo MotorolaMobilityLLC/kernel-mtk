4169e7ca2ae184fab63f0e7b218b13179fdb9935
wangkexin <tinno64@motorola.com>
2025-03-13 14:50:14 +0800

    drm/panel: kansas display driver improve panel ext reset
    kansas: Display driver improve panel ext reset
    panel driver add null pointer judgment
    
    Change-Id: I520ea293267c34a3a26cbcaa069040ec3b8ea5ac
    Signed-off-by: wangkexin <tinno64@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3303782
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jing Hu <hujing3@lenovo.com>
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/dsi-panel-mot-tm-nt36528-667-hdp-dphy-vdo-120hz.c
+++ drivers/gpu/drm/panel/dsi-panel-mot-tm-nt36528-667-hdp-dphy-vdo-120hz.c
@@ -572,9 +572,13 @@ static int panel_ext_reset(struct drm_panel *panel, int on)
 
 	ctx->reset_gpio =
 		devm_gpiod_get(ctx->dev, "reset", GPIOD_OUT_HIGH);
-	gpiod_set_value(ctx->reset_gpio, on);
-	devm_gpiod_put(ctx->dev, ctx->reset_gpio);
-
+	if (!IS_ERR(ctx->reset_gpio)) {
+		gpiod_set_value(ctx->reset_gpio, on);
+		devm_gpiod_put(ctx->dev, ctx->reset_gpio);
+		pr_info("%s- reset %d done exit\n", __func__, on);
+		return 0;
+	}
+	pr_info("%s- exit\n", __func__);
 	return 0;
 }
 
