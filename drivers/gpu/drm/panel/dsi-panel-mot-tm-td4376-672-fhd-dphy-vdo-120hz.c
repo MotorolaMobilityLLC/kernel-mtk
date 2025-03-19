9dffdb71ad7af63840a0d99f09c3a7884a505948
wangkexin <tinno64@motorola.com>
2025-03-19 17:56:53 +0800

    drm/panel: bogota add shutdown process of 3th panel
    bogota: add shutdown process of 3th tm panel
    Fix shutdown bias reset while power down issue
    
    Change-Id: I606a8c4d218fb13c6e49d4c7a31dcf1959d8fa64
    Signed-off-by: wangkexin <tinno64@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3313797
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jing Hu <hujing3@lenovo.com>
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/dsi-panel-mot-tm-td4376-672-fhd-dphy-vdo-120hz.c
+++ drivers/gpu/drm/panel/dsi-panel-mot-tm-td4376-672-fhd-dphy-vdo-120hz.c
@@ -1169,6 +1169,17 @@ static int lcm_remove(struct mipi_dsi_device *dsi)
 	return 0;
 }
 
+static void lcm_shutdown(struct mipi_dsi_device *dsi)
+{
+
+        pr_info("%s\n", __func__);
+
+#ifdef BIAS_OCP2138
+                pr_info("%s: ocp2138_BiasPower_disable\n", __func__);
+                ocp2138_BiasPower_disable(5);
+#endif
+}
+
 static const struct of_device_id tm_of_match[] = {
 	{
 #if defined(CONFIG_DRM_PANEL_NUM_NO_LIMIT)
@@ -1185,6 +1196,7 @@ MODULE_DEVICE_TABLE(of, tm_of_match);
 static struct mipi_dsi_driver tm_driver = {
 	.probe = lcm_probe,
 	.remove = lcm_remove,
+	.shutdown = lcm_shutdown,
 	.driver = {
 		.name = "tm_td4376_vid_672_1080",
 		.owner = THIS_MODULE,
