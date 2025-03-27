d96044afe67bdafd1b0fd94225303d2fa64def7c
zhangchao <tinno63@motorola.com>
2025-03-27 15:20:08 +0800

    nevada:kernel:hac aw88166 bring up
    1.aw88166 bring up
    2.kernel config
    
    Change-Id: Ieb036a4114bb8a048700c620dc6d8175e42cfff2
    Reviewed-on: https://gerrit.mot.com/3326317
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yunfei Guo <guoyf2@motorola.com>
    Reviewed-by: Shengchun Li <lisc2@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/mediatek/mt6835/mt6835-mt6377.c
+++ sound/soc/mediatek/mt6835/mt6835-mt6377.c
@@ -567,6 +567,15 @@ SND_SOC_DAILINK_DEFS(i2s0,
 	                   COMP_CODEC("tfa98xx.6-0035", "tfa98xx-aif-6-35")),
 	DAILINK_COMP_ARRAY(COMP_EMPTY()));
 //kansas audio bring up end
+//nevada audio bring up begin
+#elif defined(CONFIG_SND_SOC_NEVADA_AUDIO)
+SND_SOC_DAILINK_DEFS(i2s0,
+	DAILINK_COMP_ARRAY(COMP_CPU("I2S0")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("tfa98xx.6-0034", "tfa98xx-aif-6-34"),
+	                   COMP_CODEC("tfa98xx.6-0035", "tfa98xx-aif-6-35"),
+	                   COMP_CODEC("aw883xx_smartpa.6-0037", "aw883xx-aif-6-37")),
+	DAILINK_COMP_ARRAY(COMP_EMPTY()));
+//nevada audio bring up end
 #else
 #ifdef CONFIG_SND_SOC_VEGAS_AUDIO
 //veags audio bringup begin
@@ -605,6 +614,15 @@ SND_SOC_DAILINK_DEFS(i2s3,
 	                   COMP_CODEC("tfa98xx.6-0035", "tfa98xx-aif-6-35")),
 	DAILINK_COMP_ARRAY(COMP_EMPTY()));
 //kansas audio bring up end
+//nevada audio bring up begin
+#elif defined(CONFIG_SND_SOC_NEVADA_AUDIO)
+SND_SOC_DAILINK_DEFS(i2s3,
+	DAILINK_COMP_ARRAY(COMP_CPU("I2S3")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("tfa98xx.6-0034", "tfa98xx-aif-6-34"),
+	                   COMP_CODEC("tfa98xx.6-0035", "tfa98xx-aif-6-35"),
+	                   COMP_CODEC("aw883xx_smartpa.6-0037", "aw883xx-aif-6-37")),
+	DAILINK_COMP_ARRAY(COMP_EMPTY()));
+//nevada audio bring up end
 #else
 #ifdef CONFIG_SND_SOC_VEGAS_AUDIO
 SND_SOC_DAILINK_DEFS(i2s3,
