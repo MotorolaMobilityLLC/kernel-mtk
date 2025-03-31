38dd181e6e2fab3da778f0c2a890a1cf1689d563
xubing <tinno82@motorola.com>
2025-03-31 11:10:05 +0800

    Nevada: nevada s5kkds front bring up (4/6)
    rootcause: s5kkds front bringup
    solution: NA
    
    Change-Id: I85f6faf8b0008bc93a6d27b5adcec668fd9738bd
    Signed-off-by: xubing <tinno82@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3331711
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: Guan Xin <guanxin6@motorola.com>
    Reviewed-by: Simon C <chenzm8@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/inc/kd_imgsensor.h
+++ drivers/misc/mediatek/imgsensor/inc/kd_imgsensor.h
@@ -391,6 +391,8 @@
 #define SENSOR_DRVNAME_MOT_NEVADA_S5KKD1_MIPI_RAW        "mot_nevada_s5kkd1_mipi_raw"
 #define MOT_NEVADA_OV02F_SENSOR_ID                       0x5602
 #define SENSOR_DRVNAME_MOT_NEVADA_OV02F_MIPI_RAW         "mot_nevada_ov02f_mipi_raw"
+#define MOT_NEVADA_S5KKDS_FM_SENSOR_ID                    0x484f
+#define SENSOR_DRVNAME_MOT_NEVADA_S5KKDS_FM_MIPI_RAW      "mot_nevada_s5kkds_fm_mipi_raw"
 
 #define MOT_KANSAS_S5KJNS_SENSOR_ID                      0x38EE
 #define SENSOR_DRVNAME_MOT_KANSAS_S5KJNS_MIPI_RAW        "mot_kansas_s5kjns_mipi_raw"
