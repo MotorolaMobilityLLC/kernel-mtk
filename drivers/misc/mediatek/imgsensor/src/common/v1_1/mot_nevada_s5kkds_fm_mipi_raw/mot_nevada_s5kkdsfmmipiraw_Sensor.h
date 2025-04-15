75b26b5a47bdc126a7d6270a97a2d528dcfdacde
xubing <tinno82@motorola.com>
2025-04-15 15:43:03 +0800

    utah: s5kkds_fm eeprom mnf porting
    rootcause: s5kkds_fm eeprom mnf porting
    solution: NA
    
    Change-Id: Ia9c79e2900c0a29c8d9c32d8b3d3ccd5f743fa2c
    Signed-off-by: xubing <tinno82@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3349585
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: Guan Xin <guanxin6@motorola.com>
    Reviewed-by: Simon C <chenzm8@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_nevada_s5kkds_fm_mipi_raw/mot_nevada_s5kkdsfmmipiraw_Sensor.h
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_nevada_s5kkds_fm_mipi_raw/mot_nevada_s5kkdsfmmipiraw_Sensor.h
@@ -149,6 +149,27 @@ struct imgsensor_info_struct {
 	kal_uint8 i2c_addr_table[5];
 };
 
+typedef enum {
+	NO_ERRORS,
+	CRC_FAILURE,
+	LIMIT_FAILURE
+} calibration_status_t;
+
+struct NEVADA_S5KKDS_FM_eeprom_t{
+	uint8_t eeprom_table_version[1];
+	uint8_t cal_hw_ver[1];
+	uint8_t cal_sw_ver[1];
+	uint8_t mpn[8];
+	uint8_t actuator_id[1];
+	uint8_t lens_id[1];
+	uint8_t manufacturer_id[2];
+	uint8_t factory_id[2];
+	uint8_t manufacture_line[1];
+	uint8_t manufacture_date[3];
+	uint8_t serial_number[16];
+	uint8_t manufacture_crc16[2];
+};
+
 #define AWB_R_MIN 200
 #define AWB_R_MAX 880
 #define AWB_GR_MIN 760
