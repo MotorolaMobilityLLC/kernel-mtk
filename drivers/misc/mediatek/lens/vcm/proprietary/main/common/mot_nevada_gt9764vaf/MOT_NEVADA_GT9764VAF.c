07fadae95605b8677a5e5c63e05d1881c424b403
huandong <tinno80@motorola.com>
2025-04-23 19:23:41 +0800

    Nevada: fix s5kkds cant focus
    rootcause: lack of gt9764vaf init setting
    solution: add init setting
    
    Change-Id: Idf4a00b39b6df5fbc4df9e3906d661cfac055c7b
    Signed-off-by: huandong <tinno80@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3362610
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: Pengfei Qiao <qiaopf1@motorola.com>
    Reviewed-by: <wangzl37@motorola.com>
    Reviewed-by: Guan Xin <guanxin6@motorola.com>
    Reviewed-by: Simon C <chenzm8@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lens/vcm/proprietary/main/common/mot_nevada_gt9764vaf/MOT_NEVADA_GT9764VAF.c
+++ drivers/misc/mediatek/lens/vcm/proprietary/main/common/mot_nevada_gt9764vaf/MOT_NEVADA_GT9764VAF.c
@@ -144,8 +144,9 @@ static int initAF(void)
 		s4AF_ReadReg(0x00, &Temp);  //ic info
 		LOG_INF("Check HW version: 0x00 is %x\n", Temp);
 		ret = s4AF_WriteReg(0, 0x02, 0x02); //CONTROL
-		s4AF_WriteReg(0, 0x06, 0x80);
-		s4AF_WriteReg(0, 0x07, 0x62);
+		s4AF_WriteReg(0, 0x06, 0x40);
+		s4AF_WriteReg(0, 0x07, 0x06);
+		s4AF_WriteReg(0, 0x0B, 0x12);
 
 		spin_lock(g_pAF_SpinLock);
 		*g_pAF_Opened = 2;
