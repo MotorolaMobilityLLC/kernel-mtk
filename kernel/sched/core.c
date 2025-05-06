5d010f2dd40216215cb14b18f57dae0e7c9c0cbb
Liangjun Kuang <ot_liangjun.kuang@mediatek.com>
2025-05-06 13:43:58 +0800

    ACK: Merge android14-6.1-2025-04_r4 into u0.mp1 branch
    PROPAGATED_from (CR)
    
    [Description]
    ACK merge android14-6.1-2025-04_r4
    
    [Test]
    Build pass && Boot pass && Stability test
    
    Signed-off-by: Liangjun Kuang <ot_liangjun.kuang@mediatek.com>
    CR-Id: ALPS10001053
    Feature: Kernel SI Operation
    Change-Id: If7167e34daef3300e5e536b827638d147d6999a5
    Reviewed-on: https://gerrit.mot.com/3385022
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Submit-Approved: Chen Zidi <chenzd7@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3547320
    Reviewed-by: Bruno Oliveira <brunosoe@motorola.com>
    Reviewed-by: Levy Gabriel <levy@motorola.com>
    Reviewed-by: Rafael Ortolan <rafones@motorola.com>
    Reviewed-by: Leandro Silverio <silverio@motorola.com>
    Submit-Approved: Leandro Silverio <silverio@motorola.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -734,13 +734,15 @@ static void update_rq_clock_task(struct rq *rq, s64 delta)
 #endif
 #ifdef CONFIG_PARAVIRT_TIME_ACCOUNTING
 	if (static_key_false((&paravirt_steal_rq_enabled))) {
-		steal = paravirt_steal_clock(cpu_of(rq));
+		u64 prev_steal;
+
+		steal = prev_steal = paravirt_steal_clock(cpu_of(rq));
 		steal -= rq->prev_steal_time_rq;
 
 		if (unlikely(steal > delta))
 			steal = delta;
 
-		rq->prev_steal_time_rq += steal;
+		rq->prev_steal_time_rq = prev_steal;
 		delta -= steal;
 	}
 #endif
@@ -8227,9 +8229,14 @@ do_sched_setscheduler(pid_t pid, int policy, struct sched_param __user *param)
 	rcu_read_lock();
 	retval = -ESRCH;
 	p = find_process_by_pid(pid);
-	if (p != NULL)
-		retval = sched_setscheduler(p, policy, &lparam);
+	if (likely(p))
+		get_task_struct(p);
 	rcu_read_unlock();
+
+	if (likely(p)) {
+		retval = sched_setscheduler(p, policy, &lparam);
+		put_task_struct(p);
+	}
 
 	return retval;
 }
