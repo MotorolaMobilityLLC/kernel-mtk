9d0fa81acbbc5e3553a4bc59d6c02c89c752e53e
Xion Wang <xion.wang@mediatek.com>
2025-05-11 01:09:46 +0800

    [ALPS10003823] Fix UAF in monitor_hang driver
    [Description]
    Removed code that sends signals to various tasks (system_server_task,
    monkey_task) during the backtrace dump process to address a
    use-after-free (UAF) issue caused by a race condition. This
    change mitigates the security vulnerability in the MonitorHang driver.
    
    Issue: Security Vulnerability in monitor_hang - UAF due to race
    condition in MonitorHang Driver (H1#3114190)
    
    [Test]
    build pass, test pass
    
    MTK-Commit-Id: df1fb9ac1ab0d4bbf9700015c0d6330c61967d46
    
    Signed-off-by: Xion Wang <xion.wang@mediatek.com>
    CR-Id: ALPS10003823
    Feature: monitorhang
    Change-Id: I45d08a115674c87fab0a637302d2bfaf9e5fe16a
    (cherry-pick from 726bac9730d9156bf5ac3a14e6a1c262b9920e95)

--- drivers/misc/mediatek/monitor_hang/hang_detect.c
+++ drivers/misc/mediatek/monitor_hang/hang_detect.c
@@ -1498,8 +1498,7 @@ static void show_task_info(void)
 
 static void show_task_backtrace(void)
 {
-	struct task_struct *p, *system_server_task = NULL;
-	struct task_struct *monkey_task = NULL;
+	struct task_struct *p = NULL;
 	struct task_info task_info_arr[MAX_NR_SPECIAL_PROCESS];
 	int i = 0, task_array_idx = 0;
 	int size_special_process = ARRAY_SIZE(special_process);
@@ -1526,12 +1525,6 @@ static void show_task_backtrace(void)
 	rcu_read_lock();
 	for_each_process(p) {
 		get_task_struct(p);
-		if (Hang_first_done == false) {
-			if (!strcmp(p->comm, "system_server"))
-				system_server_task = p;
-			if (strstr(p->comm, "monkey"))
-				monkey_task = p;
-		}
 		/* specify process, need dump maps file and native backtrace */
 		if (!first_dump_blocked && (task_array_idx < MAX_NR_SPECIAL_PROCESS)) {
 			for (i = 0; i < size_special_process; i++) {
@@ -1577,12 +1570,6 @@ static void show_task_backtrace(void)
 		}
 	}
 	log_hang_info("dump backtrace end: %llu\n", local_clock());
-	if (Hang_first_done == false) {
-		if (system_server_task)
-			send_sig(SIGQUIT, system_server_task, 1);
-		if (monkey_task)
-			send_sig(SIGQUIT, monkey_task, 1);
-	}
 }
 
 #if IS_ENABLED(CONFIG_ANDROID_DEBUG_SYMBOLS)
