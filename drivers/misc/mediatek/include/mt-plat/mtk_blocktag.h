36c8475db93c0fb4eadff8b5a6abcd0c9f4a1b5b
yanxu <yanxu.wei@mediatek.com>
2025-05-17 00:21:51 +0800

    [ALPS10040632] Revert blocktag call perf mode func
    [Description]
    set perf mode func is in hctx_lock scope, and will call might sleep func
    thus causing kernel panic or exception
    
    [Test]
    Build, bootup, sdcard perf test pass
    
    MTK-Commit-Id: 143ddcd8085d54002551aa794ddd39e7f60feaa2
    
    CR-Id: ALPS10040632
    Feature: blocktag
    Change-Id: Iad4f4fd86555b51b8d85b24e3c11b4f3a6219ca7
    Signed-off-by: yanxu <yanxu.wei@mediatek.com>
    (cherry-pick from b6c44ac6f1dcc2650fd6f532743fdf45a0efbd81)

--- drivers/misc/mediatek/include/mt-plat/mtk_blocktag.h
+++ drivers/misc/mediatek/include/mt-plat/mtk_blocktag.h
@@ -326,8 +326,6 @@ void *mtk_btag_seq_debug_start(struct seq_file *seq, loff_t *pos);
 void *mtk_btag_seq_debug_next(struct seq_file *seq, void *v, loff_t *pos);
 void mtk_btag_seq_debug_stop(struct seq_file *seq, void *v);
 
-void set_mmc_perf_mode(struct mmc_host *mmc, bool boost);
-
 #else
 
 #define mtk_btag_mictx_enable(...)
@@ -345,7 +343,6 @@ void set_mmc_perf_mode(struct mmc_host *mmc, bool boost);
 #define mmc_mtk_biolog_init(...)
 #define mmc_mtk_biolog_exit(...)
 #define mmc_mtk_biolog_check(...)
-#define set_mmc_perf_mode(...)
 #endif
 
 #endif
