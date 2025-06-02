76552572f072b6c3e0e98c9bd09369022d2c3244
Xi Ruoyao <xry111@xry111.site>
2025-06-02 12:33:21 +0800

    BACKPORT: FROMGIT: arm64: Add override for MPAM
    As the message of the commit 09e6b306f3ba ("arm64: cpufeature: discover
    CPU support for MPAM") already states, if a buggy firmware fails to
    either enable MPAM or emulate the trap as if it were disabled, the
    kernel will just fail to boot.  While upgrading the firmware should be
    the best solution, we have some hardware of which the vendor have made
    no response 2 months after we requested a firmware update.  Allow
    overriding it so our devices don't become some e-waste.
    
    Cc: James Morse <james.morse@arm.com>
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Will Deacon <will@kernel.org>
    Cc: Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>
    Cc: Mingcong Bai <jeffbai@aosc.io>
    Cc: Shaopeng Tan <tan.shaopeng@fujitsu.com>
    Cc: Ben Horgan <ben.horgan@arm.com>
    Signed-off-by: Xi Ruoyao <xry111@xry111.site>
    Reviewed-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20250602043723.216338-1-xry111@xry111.site
    Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit 10f885d63a0efd50b0d22bf27eb3cf727838e99e
     arm64 for-next/core)
    Bug: 228613614
    [willdeacon@: Preserve SYS_MPAM2_EL2 initialisation in el2_setup.h]
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Change-Id: I73fc3fba7466173b3159c5729d998bd1741d4ce9

--- arch/arm64/include/asm/el2_setup.h
+++ arch/arm64/include/asm/el2_setup.h
@@ -220,21 +220,6 @@
 	msr	spsr_el2, x0
 .endm
 
-.macro __init_el2_mpam
-	/* Memory Partitioning And Monitoring: disable EL2 traps */
-	mrs	x1, id_aa64pfr0_el1
-	ubfx	x0, x1, #ID_AA64PFR0_EL1_MPAM_SHIFT, #4
-	cbz	x0, .Lskip_mpam_\@		// skip if no MPAM
-	mov_q	x0, MPAM2_HOST_FLAGS
-	msr_s	SYS_MPAM2_EL2, x0		// use the default partition
-						// and disable lower traps
-						// don't trap access to MPAMSM_EL1
-	mrs_s	x0, SYS_MPAMIDR_EL1
-	tbz	x0, #MPAMIDR_EL1_HAS_HCR_SHIFT, .Lskip_mpam_\@	// skip if no MPAMHCR reg
-	msr_s	SYS_MPAMHCR_EL2, xzr		// clear TRAP_MPAMIDR_EL1 -> EL2
-.Lskip_mpam_\@:
-.endm
-
 /**
  * Initialize EL2 registers to sane values. This should be called early on all
  * cores that were booted in EL2. Note that everything gets initialised as
@@ -252,7 +237,6 @@
 	__init_el2_stage2
 	__init_el2_gicv3
 	__init_el2_hstr
-	__init_el2_mpam
 	__init_el2_nvhe_idregs
 	__init_el2_cptr
 	__init_el2_fgt
@@ -297,6 +281,18 @@
 #endif
 
 .macro finalise_el2_state
+	check_override id_aa64pfr0, ID_AA64PFR0_EL1_MPAM_SHIFT, .Linit_mpam_\@, .Lskip_mpam_\@, x1, x2
+
+.Linit_mpam_\@:
+	mov_q	x0, MPAM2_HOST_FLAGS
+	msr_s	SYS_MPAM2_EL2, x0		// use the default partition
+						// and disable lower traps
+						// don't trap access to MPAMSM_EL1
+	mrs_s	x0, SYS_MPAMIDR_EL1
+	tbz	x0, #MPAMIDR_EL1_HAS_HCR_SHIFT, .Lskip_mpam_\@  // skip if no MPAMHCR reg
+	msr_s   SYS_MPAMHCR_EL2, xzr		// clear TRAP_MPAMIDR_EL1 -> EL2
+
+.Lskip_mpam_\@:
 	check_override id_aa64pfr0, ID_AA64PFR0_EL1_SVE_SHIFT, .Linit_sve_\@, .Lskip_sve_\@, x1, x2
 
 .Linit_sve_\@:	/* SVE register access */
