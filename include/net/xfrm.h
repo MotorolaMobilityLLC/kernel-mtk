4e650a3d5970584a51163fbcb186aae9667fbcd5
Greg Kroah-Hartman <gregkh@google.com>
2025-06-06 11:52:40 +0000

    Revert "espintcp: remove encap socket caching to avoid reference leak"
    This reverts commit 9cbca30102028f9ad3d2098f935c4368f581fd07 which is
    commit 028363685bd0b7a19b4a820f82dd905b1dc83999 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: Ifec9e4a0d3e7404091dd8bcc60e2cf06815d9c3b
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- include/net/xfrm.h
+++ include/net/xfrm.h
@@ -233,6 +233,7 @@ struct xfrm_state {
 
 	/* Data for encapsulator */
 	struct xfrm_encap_tmpl	*encap;
+	struct sock __rcu	*encap_sk;
 
 	/* NAT keepalive */
 	u32			nat_keepalive_interval; /* seconds */
