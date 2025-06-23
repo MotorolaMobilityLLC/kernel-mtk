6760e983e59ed20706a4ccb1135be876a3bc0e3e
Chunhui Li <chunhui.li@mediatek.com>
2025-06-23 11:33:13 +0800

    [ALPS10122938] eint: fix dual-edge abnormal case
    [Description]
    In dual-edge case, if signal pulse last too short, it will
    run soft set flow, but it maybe enter pending=0/soft=1 if get
    ack unexpectly in suspend/resume scenario, eint cant handle
    interrupt afterwards.
    Fix by move soft clear after soft set to avoid pending=0/soft=1.
    
    [Test]
    Local test mannually:  trigger eint->wait signal invert->ack->
    soft set->soft clear-> eint trigger secondly as expect.
    
    MTK-Commit-Id: ff4f049c8c55f6646a3e63ba3e2f3db63c42ea5d
    
    Signed-off-by: Chunhui Li <chunhui.li@mediatek.com>
    CR-Id: ALPS10122938
    Feature: eint
    Change-Id: I57e983370bb90b8e516cf3c728cb3910ca61a819
    (cherry picked from commit 7c10e57cc8e30f3b290b7a657ac752da4bc71bff)
    (cherry picked from commit 7d959a002ddbe74a6401107b719018d86c5b6c52)

--- drivers/pinctrl/mediatek/mtk-eint.c
+++ drivers/pinctrl/mediatek/mtk-eint.c
@@ -543,17 +543,10 @@ static void mtk_eint_irq_handler(struct irq_desc *desc)
 				}
 
 				dual_edge = eint->pins[eint_num].dual_edge;
-				if (dual_edge) {
-					/*
-					 * Clear soft-irq in case we raised it last
-					 * time.
-					 */
-					mtk_eint_soft_clr(eint, eint_num);
-
+				if (dual_edge)
 					start_level =
 					eint->gpio_xlate->get_gpio_state(eint->pctl,
 									 eint_num);
-				}
 
 				generic_handle_irq(virq);
 
@@ -566,6 +559,9 @@ static void mtk_eint_irq_handler(struct irq_desc *desc)
 					 */
 					if (start_level != curr_level)
 						mtk_eint_soft_set(eint, eint_num);
+
+					/* Clear soft-irq here to avoid pending=0/soft=1 */
+					mtk_eint_soft_clr(eint, eint_num);
 				}
 
 				if (eint->pins[eint_num].debounce)
