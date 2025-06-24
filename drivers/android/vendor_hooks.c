643740489bb7df2a67d6a1768f636af0fa1b884e
liujinbao1 <liujinbao1@xiaomi.corp-partner.google.com>
2025-06-24 21:46:25 +0800

    ANDROID: GKI: add vendor_hook android_vh_health_report
    android_vh_health_report is specifically designed for OGKI
    to monitor F2FS health status and report key metrics.
    
    Bug: 427430423
    Bug: 428905660
    
    Change-Id: I6ddfd3a4c2cddc0e053b65440d7794bf8e2754ba
    Signed-off-by: liujinbao1 <liujinbao1@xiaomi.corp-partner.google.com>
    (cherry picked from commit 8c8d462c9dd5dcf1c7303392179b99fa638316d0)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -33,6 +33,7 @@
 #include <trace/hooks/pm_domain.h>
 #include <trace/hooks/cpuidle_psci.h>
 #include <trace/hooks/vmscan.h>
+#include <trace/hooks/health_report.h>
 #include <trace/hooks/avc.h>
 #include <trace/hooks/creds.h>
 #include <trace/hooks/selinux.h>
@@ -200,6 +201,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_tune_mmap_readaround);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_hw_protection_shutdown);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shrink_slab_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_shrink_slab_ex);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_health_report);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_drain_all_pages_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_security_audit_log_setid);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_selinux_avc_insert);
