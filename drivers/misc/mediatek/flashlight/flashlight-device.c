3a27b3a40ef49b5ab9e6ead30c648b4780fbb967
Lc_Lg <longc168@motorola.com>
2025-06-27 14:18:19 +0800

    orlando: drv: flashlights ocp8135_pwm porting[1/2]
    details:
    -Add sw configuration and flash driver
    
    Change-Id: Ibbacce249a51690b9281bfbedde3b83f40d7c5ec
    Signed-off-by: Lc_Lg <longc168@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3446006
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: Guan Xin <guanxin6@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/flashlight/flashlight-device.c
+++ drivers/misc/mediatek/flashlight/flashlight-device.c
@@ -22,6 +22,11 @@ const struct flashlight_device_id flashlight_id[] = {
 	/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
 	{0, 0, 0, "flashlights-ocp8135-pwm", 0, 0},
 };
+#elif defined(CONFIG_MTK_FLASHLIGHT_ORLANDO)
+const struct flashlight_device_id flashlight_id[] = {
+	/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
+	{0, 0, 0, "flashlights_orlando_ocp8135_pwm", 0, 0},
+};
 #elif defined(mt6739)
 const struct flashlight_device_id flashlight_id[] = {
 	/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
