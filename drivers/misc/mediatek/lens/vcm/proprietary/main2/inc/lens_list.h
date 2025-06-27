e982ee9c2c535ea2c2e518fb0a33f635e34675a1
Lc_Lg <longc168@motorola.com>
2025-06-27 12:25:04 +0800

    orlando: drv: ov08d sensor af gt9764v bring up[1/2]
    details:
    -Add sw configuration and af driver
    
    Change-Id: I825c7d60b58b36246cef2b772f3336e18b5ae2d3
    Signed-off-by: Lc_Lg <longc168@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3445892
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guan Xin <guanxin6@motorola.com>
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lens/vcm/proprietary/main2/inc/lens_list.h
+++ drivers/misc/mediatek/lens/vcm/proprietary/main2/inc/lens_list.h
@@ -175,4 +175,20 @@ extern int MOT_VEGAS_GT9764VAF_PowerDown(struct i2c_client *pstAF_I2Cclient,
 extern int MOT_VEGAS_GT9764VAF_GetFileName(unsigned char *pFileName);
 //End
 
+//Begin: Add lens driver interface for Orlando
+#define MOT_ORLANDO_GT9764V_SetI2CClient MOT_ORLANDO_GT9764VAF_SetI2Cclient
+#define MOT_ORLANDO_GT9764V_Ioctl MOT_ORLANDO_GT9764VAF_Ioctl
+#define MOT_ORLANDO_GT9764V_Release MOT_ORLANDO_GT9764VAF_Release
+#define MOT_ORLANDO_GT9764V_PowerDown MOT_ORLANDO_GT9764VAF_PowerDown
+#define MOT_ORLANDO_GT9764V_GetFileName MOT_ORLANDO_GT9764VAF_GetFileName
+extern int MOT_ORLANDO_GT9764VAF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+				spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long MOT_ORLANDO_GT9764VAF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+				unsigned long a_u4Param);
+extern int MOT_ORLANDO_GT9764VAF_Release(struct inode *a_pstInode, struct file *a_pstFile);
+extern int MOT_ORLANDO_GT9764VAF_PowerDown(struct i2c_client *pstAF_I2Cclient,
+				int *pAF_Opened);
+extern int MOT_ORLANDO_GT9764VAF_GetFileName(unsigned char *pFileName);
+//End
+
 #endif
