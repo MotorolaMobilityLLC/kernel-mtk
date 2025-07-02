146b201af73df0d8fd718f5fb04d3c2b98fd9a25
Lc_Lg <longc168@motorola.com>
2025-07-02 15:30:46 +0800

    orlando: drv: [S5KKDS][CMS1.5]EEPROM driver debug[1/3]
    details:
    1.EEPROM driver debug.
    
    Change-Id: If2f605a54425575538025c233067642e3bcb3755
    Signed-off-by: Lc_Lg <longc168@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3450666
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: Guan Xin <guanxin6@motorola.com>
    Reviewed-by: <shihl5@motorola.com>
    Reviewed-by: Pengfei Qiao <qiaopf1@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_orlando_s5kkds_mipi_raw/mot_orlando_s5kkdsmipiraw_Sensor.h
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_orlando_s5kkds_mipi_raw/mot_orlando_s5kkdsmipiraw_Sensor.h
@@ -155,6 +155,21 @@ typedef enum {
 	LIMIT_FAILURE
 } calibration_status_t;
 
+struct ORLANDO_S5KKDS_eeprom_t{
+	uint8_t eeprom_table_version[1];
+	uint8_t cal_hw_ver[1];
+	uint8_t cal_sw_ver[1];
+	uint8_t mpn[8];
+	uint8_t actuator_id[1];
+	uint8_t lens_id[1];
+	uint8_t manufacturer_id[2];
+	uint8_t factory_id[2];
+	uint8_t manufacture_line[1];
+	uint8_t manufacture_date[3];
+	uint8_t serial_number[16];
+	uint8_t manufacture_crc16[2];
+};
+
 #define AWB_R_MIN 200
 #define AWB_R_MAX 880
 #define AWB_GR_MIN 760
