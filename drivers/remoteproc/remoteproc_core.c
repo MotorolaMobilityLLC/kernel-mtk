36fc49735bcb6834b03c17f2bdabd08b29dd5c41
Liangjun Kuang <ot_liangjun.kuang@mediatek.com>
2025-07-09 19:25:27 +0800

    Merge branch android14-6.1-2025-05 into alps-mp-u0.mp1
    Mot-CRs-fixed: (CR)
    Signed-off-by: Liangjun Kuang <ot_liangjun.kuang@mediatek.com>
    Change-Id: Ie4d6e8eaa920c26b658a86ac290271d97c29290d
    Reviewed-on: https://gerrit.mot.com/3459902
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiaojun Ji <jixj@motorola.com>
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Submit-Approved: Chen Zidi <chenzd7@motorola.com>

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -2067,6 +2067,7 @@ int rproc_shutdownx(struct rproc *rproc, unsigned int uid)
 	kfree(rproc->cached_table);
 	rproc->cached_table = NULL;
 	rproc->table_ptr = NULL;
+	rproc->table_sz = 0;
 out:
 	mutex_unlock(&rproc->lock);
 	return ret;
