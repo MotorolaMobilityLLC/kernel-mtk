e8fc0723140655a68fb6b683d4377b75640c1b31
Pengfei Li <pengfei.kernel@vivo.corp-partner.google.com>
2025-07-17 10:04:42 +0800

    ANDROID: vendor_hook: Added hook to tune reclaimed huge page
    android_vh_mm_customize_longterm_pinnable - This hook allows vendors to
    customize some special folios that are not migrated. They should not be
    migrated to avoid application lags.
    
    android_vh_mm_do_madvise_bypass - This hook allows vendors to customize
    some madvise behaviors so that some madvise requests can be completed
    quickly in certain scenarios.
    
    android_vh_mm_migrate_one_page - This hook allows vendors to track
    certain folios that are being migrated.
    
    android_vh_mm_remove_migration_pte_bypass - This hook is used to
    properly handle the migration of the aforementioned special folio.
    
    android_vh_mm_split_huge_page_bypass - This hook is used in certain
    scenarios where large folios should not be split to avoid the overhead
    caused by splitting.
    
    android_vh_mm_try_split_folio_bypass - This hook is used to skip
    splitting large folios during migration in certain scenarios.
    
    Bug: 431672372
    Bug: 435545311
    Change-Id: I3c3580651e330dc8cfa6fa6f7a4f85fba1ba9807
    Signed-off-by: Pengfei Li <pengfei.kernel@vivo.corp-partner.google.com>
    (cherry picked from commit 792718bb234ac19db3efdbc3215485e874c41584)

--- mm/madvise.c
+++ mm/madvise.c
@@ -1685,6 +1685,7 @@ int do_madvise(struct mm_struct *mm, unsigned long start, size_t len_in, int beh
 	int write;
 	size_t len;
 	struct blk_plug plug;
+	bool bypass = false;
 
 	if (!madvise_behavior_valid(behavior))
 		return -EINVAL;
@@ -1708,6 +1709,11 @@ int do_madvise(struct mm_struct *mm, unsigned long start, size_t len_in, int beh
 	if (behavior == MADV_HWPOISON || behavior == MADV_SOFT_OFFLINE)
 		return madvise_inject_error(behavior, start, start + len_in);
 #endif
+
+	trace_android_vh_mm_do_madvise_bypass(mm, start, len, behavior,
+					      &error, &bypass);
+	if (bypass)
+		return error;
 
 	write = madvise_need_mmap_write(behavior);
 	if (write) {
