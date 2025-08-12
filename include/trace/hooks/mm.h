a6ebefa18f30c7e2567d3f4750aed7f74efece81
yan chang <changyan1@xiaomi.com>
2025-08-12 14:34:39 +0800

    ANDROID: vendor_hook: customize gfp and decide whether to bypass in the large folio allocation path
    This hook is used as a supplement to the previous hook to dynamic modify the gfp and bypass of order pmd:
    https://android-review.googlesource.com/c/kernel/common/+/3227165
    
    Bug: 438090847
    Change-Id: Ia33fdc62ab466cf5cfcc53d02b515e28a1ac1431
    Signed-off-by: yan chang <changyan1@xiaomi.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -603,6 +603,9 @@ DECLARE_HOOK(android_vh_mm_try_split_folio_bypass,
 DECLARE_HOOK(android_vh_customize_thp_gfp_orders,
 	TP_PROTO(gfp_t *gfp_mask, unsigned long *orders, int *order),
 	TP_ARGS(gfp_mask, orders, order));
+DECLARE_HOOK(android_vh_customize_pmd_gfp_bypass,
+	TP_PROTO(gfp_t *gfp_mask, bool *bypass),
+	TP_ARGS(gfp_mask, bypass));
 DECLARE_HOOK(android_vh_thp_vma_allowable_orders,
 	TP_PROTO(struct vm_area_struct *vma, unsigned long *orders),
 	TP_ARGS(vma, orders));
