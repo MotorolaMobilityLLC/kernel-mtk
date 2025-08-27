43395a354c68c65effc0198ee18b00dc1cdb7a68
longcheer <longc134@motorola.com>
2025-08-27 14:37:44 +0800

    kernel: Orlando fix max VFP to 0x7ffe for 30hz
    Fix max VFP value to 0x7ffe for 30hz
    follow MTK patch ALPS10281489.
    
    Change-Id: I4bec17b5f71aa7a8d44e3e8239405f6ccb226ab8
    Signed-off-by: longcheer <longc134@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3527826
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Jing Hu <hujing3@lenovo.com>
    Reviewed-by: Hua Tan <tanhua1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/mediatek/mediatek_v2/platform/mtk_drm_6835.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/platform/mtk_drm_6835.c
@@ -215,7 +215,7 @@ const struct mtk_dsi_driver_data mt6835_dsi_driver_data = {
 	.dsi_buffer = false,
 	.buffer_unit = 18,
 	.sram_unit = 18,
-	.max_vfp = 0x1194,
+	.max_vfp = 0x7ffe,
 	.mmclk_by_datarate = mtk_dsi_set_mmclk_by_datarate,
 };
 
