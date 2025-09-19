13eaf015976414ada1c603bb0f458dc22cfdf421
WeiQiang He <lxhwq1@motorola.com>
2025-09-19 15:35:01 +0800

    naples: drv: gc32e1 eeprom bringup (2/4)
    details:
           1.add ge32e1 eeprom config
    
    Change-Id: Iecfe01bed4d436bfb89e4dbc5857093a2f74c90a
    Reviewed-on: https://gerrit.mot.com/3563912
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: <wangzl37@motorola.com>
    Reviewed-by: Mingfeng Yang <yangmf3@motorola.com>
    Reviewed-by: Guan Xin <guanxin6@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    SLTApproved: Zhichao Chen <chenzc2@motorola.com>
    Tested-by: Zhichao Chen <chenzc2@motorola.com>
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/cam_cal/src/custom/cam_cal_list.c
+++ drivers/misc/mediatek/cam_cal/src/custom/cam_cal_list.c
@@ -52,6 +52,7 @@ struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
 	{MOT_NAPLES_S5KHM9_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_16K},
 	{MOT_NAPLES_IMX882_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_16K},
 	{MOT_NAPLES_S5KKDS_SENSOR_ID, 0xA2, Common_read_region, MAX_EEPROM_SIZE_16K},
+	{MOT_NAPLES_GC32E1_SENSOR_ID, 0xA2, Common_read_region, MAX_EEPROM_SIZE_16K},
 	{MOT_NAPLES_OV08F_SENSOR_ID, 0x6C, ov08f_read_region}, // otp
 	{MOT_NAPLES_GC08A3_SENSOR_ID, 0x62, gc08a3_read_region}, // otp normal
 #endif
