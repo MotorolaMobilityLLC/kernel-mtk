eb8953aebf77695e5556fa6df06cdbaa8839dda2
Sebastian Ene <sebastianene@google.com>
2025-10-17 07:57:10 +0000

    FROMGIT: KVM: arm64: Check the untrusted offset in FF-A memory share
    Verify the offset to prevent OOB access in the hypervisor
    FF-A buffer in case an untrusted large enough value
    [U32_MAX - sizeof(struct ffa_composite_mem_region) + 1, U32_MAX]
    is set from the host kernel.
    
    Bug: 439862698
    Change-Id: I59de97caaca12dce1d01d4fd44a4e4bacf2d2d1c
    (cherry picked from commit 103e17aac09cdd358133f9e00998b75d6c1f1518
    https://git.kernel.org/pub/scm/linux/kernel/git/kvmarm/kvmarm.git fixes)
    Link: https://lore.kernel.org/all/20251017075710.2605118-1-sebastianene@google.com/
    Acked-by: Will Deacon <will@kernel.org>
    Signed-off-by: Sebastian Ene <sebastianene@google.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Lee Jones <joneslee@google.com>

--- arch/arm64/kvm/hyp/nvhe/ffa.c
+++ arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -898,7 +898,7 @@ static int __do_ffa_mem_xfer(const u64 func_id,
 	struct ffa_composite_mem_region *reg, *temp_reg;
 	struct ffa_mem_region *buf;
 	struct kvm_ffa_buffers *ffa_buf;
-	u32 offset, nr_ranges;
+	u32 offset, nr_ranges, checked_offset;
 	int ret = 0;
 	struct ffa_mem_transfer *transfer = NULL;
 	u64 ffa_handle;
@@ -954,7 +954,12 @@ static int __do_ffa_mem_xfer(const u64 func_id,
 		goto out_unlock;
 	}
 
-	if (fraglen < offset + sizeof(struct ffa_composite_mem_region)) {
+	if (check_add_overflow(offset, sizeof(struct ffa_composite_mem_region), &checked_offset)) {
+		ret = FFA_RET_INVALID_PARAMETERS;
+		goto out_unlock;
+	}
+
+	if (fraglen < checked_offset) {
 		ffa_to_smccc_error(res, FFA_RET_INVALID_PARAMETERS);
 		goto out_unlock;
 	}
