7e1d686132b4a0e0a71f54a7d10620d9b485f77e
Lc_Myy <longc170@motorola.com>
2025-10-29 19:07:09 +0800

    drm/panel: Orlando 2nd txd ft8725 panel disable lfr
    Orlando 2nd txd ft8725 panel
    disable lfr to improve TP performance per ft8725 IC req
    
    Change-Id: I7612f2f765013df6a3315d026984b72baf8f7bfb
    Reviewed-on: https://gerrit.mot.com/3614336
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jing Hu <hujing3@lenovo.com>
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Reviewed-by: Obi-Wan Kenobi
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/dsi-panel-mot-vdo-txd-ft8725-1080-2388-dphy-fhd-120hz.c
+++ drivers/gpu/drm/panel/dsi-panel-mot-vdo-txd-ft8725-1080-2388-dphy-fhd-120hz.c
@@ -465,8 +465,6 @@ static struct mtk_panel_params ext_params_mode_30 = {
 		.rc_tgt_offset_hi      =  DSC_RC_TGT_OFFSET_HI,
 		.rc_tgt_offset_lo      =  DSC_RC_TGT_OFFSET_LO,
 	},
-	.lfr_enable = LFR_EN,
-	.lfr_minimum_fps = MODE_30_FPS,
 
 };
 
@@ -536,8 +534,6 @@ static struct mtk_panel_params ext_params_mode_60 = {
 		.rc_tgt_offset_hi      =  DSC_RC_TGT_OFFSET_HI,
 		.rc_tgt_offset_lo      =  DSC_RC_TGT_OFFSET_LO,
 	},
-	.lfr_enable = LFR_EN,
-	.lfr_minimum_fps = MODE_30_FPS,
 
 };
 
@@ -606,8 +602,6 @@ static struct mtk_panel_params ext_params_mode_90 = {
 		.rc_tgt_offset_hi      =  DSC_RC_TGT_OFFSET_HI,
 		.rc_tgt_offset_lo      =  DSC_RC_TGT_OFFSET_LO,
 	},
-	.lfr_enable = LFR_EN,
-	.lfr_minimum_fps = MODE_30_FPS,
 };
 
 static struct mtk_panel_params ext_params_mode_120 = {
@@ -675,8 +669,6 @@ static struct mtk_panel_params ext_params_mode_120 = {
 		.rc_tgt_offset_hi      =  DSC_RC_TGT_OFFSET_HI,
 		.rc_tgt_offset_lo      =  DSC_RC_TGT_OFFSET_LO,
 	},
-	.lfr_enable = LFR_EN,
-	.lfr_minimum_fps = MODE_30_FPS,
 
 };
 
