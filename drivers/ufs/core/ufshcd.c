ee7532a21fcfd8b5defd2e024fa63de3d1211ac8
lin.gui <lin.gui@mediatek.com>
2025-11-04 17:17:18 +0800

    [ALPS10650229] ufs: drain all request of ufs queue in shutdown sequence
    [Description]
    Added a wait for doorbell clearance in the shutdown sequence to ensure
    requests are executed
    
    [Test]
    boot up
    
    Signed-off-by: Qilin Tan <qilin.tan@mediatek.com>
    Signed-off-by: lin.gui <lin.gui@mediatek.com>
    CR-Id: ALPS10650229
    Feature: ufs
    Change-Id: I578bda63570016f489c297b9bd59d6e65f372f01
    (cherry picked from commit 14edca1a5e396c5cfe759a7663d84a4599f4d74c)
    (cherry picked from commit 2c71f47a9dbf7835914902b26b3940a44c32af82)

--- drivers/ufs/core/ufshcd.c
+++ drivers/ufs/core/ufshcd.c
@@ -10268,6 +10268,9 @@ static void ufshcd_wl_shutdown(struct device *dev)
 		scsi_device_set_state(sdev, SDEV_OFFLINE);
 		mutex_unlock(&sdev->state_mutex);
 	}
+#if IS_ENABLED(CONFIG_MTK_UFS_DEBUG_BUILD)
+	ufshcd_wait_for_doorbell_clr(hba, 3 * USEC_PER_SEC);
+#endif
 	__ufshcd_wl_suspend(hba, UFS_SHUTDOWN_PM);
 
 	/*
