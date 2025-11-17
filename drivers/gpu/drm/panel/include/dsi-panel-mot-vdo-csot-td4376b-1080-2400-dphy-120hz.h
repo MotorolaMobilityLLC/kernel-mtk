a7dba66c1ae05d5890ea0bfecd563a60c0605e28
hq <gaoyuehq@motorola.com>
2025-11-17 17:36:54 +0800

    target/k6835v2_64: kernel csot td4376 as sydney bring up
    Reason:
    kernel bring up as sydney
    
    Solution:
    bring up
    
    Test:
    local test passed
    
    Change-Id: If755cc509124008a92ca799c8b16de8041fe9ea4
    Signed-off-by: gaoyue <gaoyuehq@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3640791
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yongchao Ma <mayc10@motorola.com>
    Reviewed-by: Hua Tan <tanhua1@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/gpu/drm/panel/include/dsi-panel-mot-vdo-csot-td4376b-1080-2400-dphy-120hz.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2025 Motorola Mobility LLC
+ * All Rights Reserved.
+ * Motorola Mobility Confidential Restricted.
+ */
+
+#ifndef PANEL_TD4376_667_HDPLUS_DSI_VDO_120HZ_CSOT
+#define PANEL_TD4376_667_HDPLUS_DSI_VDO_120HZ_CSOT
+
+#define PANEL_ESD_RECOVERY_NOFLASH	0x01
+#define PANEL_ESD_RECOVERY_VDD          0x02
+#define REGFLAG_DELAY           0xFFFC
+#define REGFLAG_UDELAY          0xFFFB
+#define REGFLAG_END_OF_TABLE    0xFFFD
+#define REGFLAG_RESET_LOW       0xFFFE
+#define REGFLAG_RESET_HIGH      0xFFFF
+
+#define FRAME_WIDTH                 1080
+#define FRAME_HEIGHT                2400
+
+#define PHYSICAL_WIDTH              72756
+#define PHYSICAL_HEIGHT            160142
+
+#define DATA_RATE                   904
+#define HSA                         4
+#define HBP                         32
+#define HFP                         32
+#define VSA                         4
+#define VBP                         28
+
+/*Parameter setting for mode 0 Start*/
+
+#define MODE_60_FPS                  60
+#define MODE_60_VFP                  2588
+#define MODE_60_HFP                  32
+
+
+#define MODE_0_DATA_RATE            904
+/*Parameter setting for mode 0 End*/
+
+/*Parameter setting for mode 2 Start*/
+#define MODE_90_FPS                  90
+#define MODE_90_VFP                  896
+#define MODE_90_HFP                  32
+
+#define MODE_90_DATA_RATE            904
+/*Parameter setting for mode 2 End*/
+
+/*Parameter setting for mode 3 Start*/
+#define MODE_120_FPS                  120
+#define MODE_120_VFP                  64
+#define MODE_120_HFP                  32
+
+#define MODE_120_DATA_RATE            904
+/*Parameter setting for mode 3 End*/
+
+#define LFR_EN                      1
+/* DSC RELATED */
+
+#define DSC_ENABLE                  1
+#define DSC_VER                     17
+#define DSC_SLICE_MODE              1
+#define DSC_RGB_SWAP                0
+#define DSC_DSC_CFG                 34
+#define DSC_RCT_ON                  1
+#define DSC_BIT_PER_CHANNEL         8
+#define DSC_DSC_LINE_BUF_DEPTH      9
+#define DSC_BP_ENABLE               1
+#define DSC_BIT_PER_PIXEL           128
+#define DSC_SLICE_HEIGHT            8
+#define DSC_SLICE_WIDTH             540
+#define DSC_CHUNK_SIZE              540
+#define DSC_XMIT_DELAY              512
+#define DSC_DEC_DELAY               526
+#define DSC_SCALE_VALUE             32
+#define DSC_INCREMENT_INTERVAL      187
+#define DSC_DECREMENT_INTERVAL      7
+#define DSC_LINE_BPG_OFFSET         12
+#define DSC_NFL_BPG_OFFSET          3511
+#define DSC_SLICE_BPG_OFFSET        3255
+#define DSC_INITIAL_OFFSET          6144
+#define DSC_FINAL_OFFSET            4336
+#define DSC_FLATNESS_MINQP          3
+#define DSC_FLATNESS_MAXQP          12
+#define DSC_RC_MODEL_SIZE           8192
+#define DSC_RC_EDGE_FACTOR          6
+#define DSC_RC_QUANT_INCR_LIMIT0    11
+#define DSC_RC_QUANT_INCR_LIMIT1    11
+#define DSC_RC_TGT_OFFSET_HI        3
+#define DSC_RC_TGT_OFFSET_LO        3
+
+int panel_esd_register_client(const char *source, struct notifier_block *nb);
+int panel_esd_unregister_client(struct notifier_block *nb);
+int panel_esd_notifier_call_chain(unsigned long val, void *v);
+int panel_gesture_register_client(const char *source, struct notifier_block *nb);
+int panel_gesture_unregister_client(struct notifier_block *nb);
+int panel_gesture_notifier_call_chain(unsigned long val, void *v);
+#endif //end of PANEL_TD4376_667_HDPLUS_DSI_VDO_120HZ_CSOT
