97b4a5a8be22b729830f9483129b40a2fd6a8b5f
shuqi diao <diaosq01@motorola.com>
2025-11-25 22:03:18 +0800

    sydney: drv: sydney ldo et5904 BringUp
    sydney:
    --et5904 BringUp
    
    Change-Id: Ib8288d3a2be3f551bfd159140c24b89cc730e264
    Reviewed-on: https://gerrit.mot.com/3654697
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    SLTApproved: Zhichao Chen <chenzc2@motorola.com>
    Tested-by: Zhichao Chen <chenzc2@motorola.com>
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- drivers/regulator/sgm38121-regulator.c
+++ drivers/regulator/sgm38121-regulator.c
@@ -20,9 +20,10 @@
 
 /* Registers */
 #define SGM38121_REG_NUM (SGM38121_SEQ_STATUS-SGM38121_CHIP_REV+1)
+#define ET5904_REG_NUM (ET5904_ILIMT_COAR-SGM38121_CHIP_REV+1)
 
 #define SGM38121_CHIP_REV 0x00
-//#define SGM38121_CURRENT_LIMITSEL 0x01
+#define SGM38121_CURRENT_LIMITSEL 0x01
 #define SGM38121_DISCHARGE_RESISTORS 0x02
 #define SGM38121_LDO1_VOUT 0x03
 #define SGM38121_LDO2_VOUT 0x04
@@ -32,6 +33,19 @@
 #define SGM38121_LDO3_LDO4_SEQ 0x0b
 #define SGM38121_LDO_EN 		0x0e
 #define SGM38121_SEQ_STATUS 	0x0f
+#define ET5904_ILIMT_COAR		0x10	//Only ET5904
+
+#define ET5904_DVDD_MIN_UV			600000
+#define ET5904_DVDD_MAX_UV			1800000
+#define ET5904_DVDD_MIN_REG_VAL		0x00
+#define ET5904_DVDD_MAX_REG_VAL		0xFF
+#define ET5904_DVDD_STEP_UV			6000
+
+#define ET5904_AVDD_MIN_UV			1200000
+#define ET5904_AVDD_MAX_UV			4387500
+#define ET5904_AVDD_MIN_REG_VAL		0x00
+#define ET5904_AVDD_MAX_REG_VAL		0xFF
+#define ET5904_AVDD_STEP_UV			12500
 
 #define DVDD_MIN_MV				504
 #define DVDD_MAX_MV				1504
@@ -58,6 +72,8 @@
 #define  SGM38121_N_VOLTAGES                     256
 
 #define  SGM38121_ID                             0x80
+#define	 ET5904_ID								 0x00
+#define	 ET5924A_ID								 0x0A
 
 static int ldo_chipid = -1;
 
@@ -140,6 +156,22 @@ static int sgm38121_get_current_limit(struct regulator_dev *rdev)
 	return 0;
 }
 
+static int et5904_get_current_limit(struct regulator_dev *rdev)
+{
+	struct sgm38121 *chip = rdev_get_drvdata(rdev);
+	uint8_t reg_dump[ET5904_REG_NUM];
+	uint8_t reg_idx;
+	unsigned int val = 0;
+	for (reg_idx = 0; reg_idx < ET5904_REG_NUM; reg_idx++) {
+		regmap_read(chip->regmap, reg_idx, &val);
+		reg_dump[reg_idx] = val;
+		dev_err(chip->dev, "Reg[0x%02x] = 0x%x", reg_idx, reg_dump[reg_idx]);
+	}
+	dev_err(chip->dev, "************ end dump et5904 register ************\n");
+
+	return 0;
+}
+
 static int sgm38121_get_status(struct regulator_dev * rdev)
 {
 	struct sgm38121 *chip = rdev_get_drvdata(rdev);
@@ -171,6 +203,39 @@ static int sgm38121_get_status(struct regulator_dev * rdev)
 		return REGULATOR_STATUS_OFF;
 	}
 }
+
+static int et5904_get_status(struct regulator_dev * rdev)
+{
+	struct sgm38121 *chip = rdev_get_drvdata(rdev);
+	int ret, id = rdev_get_id(rdev);
+	unsigned int status = 0;
+
+	ret = regulator_is_enabled_regmap(rdev);
+	if (ret < 0) {
+		dev_err(chip->dev, "Failed to read enable register(%d)\n",
+			ret);
+		return ret;
+	}
+
+	if (!ret)
+		return REGULATOR_STATUS_OFF;
+
+	et5904_get_current_limit(rdev);
+
+	ret = regmap_read(chip->regmap, sgm38121_status_reg.sreg, &status);
+	if (ret < 0) {
+		dev_err(chip->dev, "Failed to read status register(%d)\n",
+			ret);
+		return ret;
+	}
+
+	if (status & (0x01ul << id)) {
+		return REGULATOR_STATUS_ON;
+	} else {
+		return REGULATOR_STATUS_OFF;
+	}
+}
+
 static int sgm38121_output_voltage_set(struct sgm38121 *sgm38121, int channel, int mV)
 {
 	uint8_t reg = 0;
@@ -178,7 +243,7 @@ static int sgm38121_output_voltage_set(struct sgm38121 *sgm38121, int channel, i
 	int ret = 0;
 	switch(channel)
 	{
-	case SGM38121_REGULATOR_LDO1: //Vout =  (504 + 8 × d) × 1mV
+	case SGM38121_REGULATOR_LDO1: //Vout =  (504 + 8 × d) × 1mV //et5904 vout = (6000 + 6 * d) * 1mv
 		reg = SGM38121_LDO1_VOUT;
 		if (mV < 528) {
 			mV = 528;
@@ -234,6 +299,70 @@ static int sgm38121_output_voltage_set(struct sgm38121 *sgm38121, int channel, i
 	return ret;
 }
 
+static int et5904_output_voltage_set(struct sgm38121 *sgm38121, int channel, int uV)
+{
+	uint8_t reg = 0;
+	uint8_t reg_val = 0;
+	int ret = 0;
+
+	switch(channel)
+	{
+	case SGM38121_REGULATOR_LDO1: //et5904 vout = (6000 + 6 * d) * 1mv
+		reg = SGM38121_LDO1_VOUT;
+		if (uV < ET5904_DVDD_MIN_UV) {
+			uV = ET5904_DVDD_MIN_UV;
+			reg_val = ET5904_DVDD_MIN_REG_VAL;
+		} else if (uV > ET5904_DVDD_MAX_UV) {
+			uV = ET5904_DVDD_MAX_UV;
+			reg_val = ET5904_DVDD_MAX_REG_VAL;
+		} else {
+			reg_val = (uV - ET5904_DVDD_MIN_UV) / ET5904_DVDD_STEP_UV;
+		}
+		break;
+	case SGM38121_REGULATOR_LDO2:
+		reg = SGM38121_LDO2_VOUT;
+		if (uV < ET5904_DVDD_MIN_UV) {
+			uV = ET5904_DVDD_MIN_UV;
+			reg_val = ET5904_DVDD_MIN_REG_VAL;
+		} else if (uV > ET5904_DVDD_MAX_UV) {
+			uV = ET5904_DVDD_MAX_UV;
+			reg_val = ET5904_DVDD_MAX_REG_VAL;
+		} else {
+			reg_val = (uV - ET5904_DVDD_MIN_UV) / ET5904_DVDD_STEP_UV;
+		}
+		break;
+	case SGM38121_REGULATOR_LDO3:	//VOUT = (1384 + 8 × d) × 1mV
+		reg = SGM38121_LDO3_VOUT;
+		if (uV < ET5904_AVDD_MIN_UV) {
+			uV = ET5904_AVDD_MIN_UV;
+			reg_val = ET5904_AVDD_MIN_REG_VAL;
+		} else if (uV > ET5904_AVDD_MAX_UV) {
+			uV = ET5904_AVDD_MAX_UV;
+			reg_val = ET5904_AVDD_MAX_REG_VAL;
+		} else {
+			reg_val = (uV - ET5904_AVDD_MIN_UV) / ET5904_AVDD_STEP_UV;
+		}
+		break;
+	case SGM38121_REGULATOR_LDO4:
+		reg = SGM38121_LDO4_VOUT;
+		if (uV < ET5904_AVDD_MIN_UV) {
+			uV = ET5904_AVDD_MIN_UV;
+			reg_val = ET5904_AVDD_MIN_REG_VAL;
+		} else if (uV > ET5904_AVDD_MAX_UV) {
+			uV = ET5904_AVDD_MAX_UV;
+			reg_val = ET5904_AVDD_MAX_REG_VAL;
+		} else {
+			reg_val = (uV - ET5904_AVDD_MIN_UV) / ET5904_AVDD_STEP_UV;
+		}
+		break;
+	default:
+		break;
+	}
+	ret = regmap_write(sgm38121->regmap, reg, reg_val);
+	dev_info(sgm38121->dev,"set ldo:%d, voltage = %d",channel+ 1, uV);
+	return ret;
+}
+
 static int sgm38121_output_voltage_get(struct sgm38121 *sgm38121, int channel, int *value)
 {
 	uint8_t reg = 0;
@@ -275,6 +404,49 @@ static int sgm38121_output_voltage_get(struct sgm38121 *sgm38121, int channel, i
 	dev_info(sgm38121->dev,"get ldo:%d, voltage = %d",channel+ 1, *value);
 	return ret;
 }
+
+static int et5904_output_voltage_get(struct sgm38121 *sgm38121, int channel, int *value)
+{
+	uint8_t reg = 0;
+	int reg_val = 0;
+	int ret = 0;
+	switch(channel)
+	{
+	case SGM38121_REGULATOR_LDO1:
+		reg = SGM38121_LDO1_VOUT;
+		ret = regmap_read(sgm38121->regmap, reg, &reg_val);
+		if (reg_val < ET5904_DVDD_MIN_REG_VAL) reg_val =  ET5904_DVDD_MIN_REG_VAL;
+		else if (reg_val > ET5904_DVDD_MAX_REG_VAL) reg_val =  ET5904_DVDD_MAX_REG_VAL;
+		*value = reg_val * ET5904_DVDD_STEP_UV + ET5904_DVDD_MIN_UV;
+		break;
+	case SGM38121_REGULATOR_LDO2:
+		reg = SGM38121_LDO2_VOUT;
+		ret = regmap_read(sgm38121->regmap, reg, &reg_val);
+		if (reg_val < ET5904_DVDD_MIN_REG_VAL) reg_val =  ET5904_DVDD_MIN_REG_VAL;
+		else if (reg_val > ET5904_DVDD_MAX_REG_VAL) reg_val =  ET5904_DVDD_MAX_REG_VAL;
+		*value = reg_val * ET5904_DVDD_STEP_UV + ET5904_DVDD_MIN_UV;
+		break;
+	case SGM38121_REGULATOR_LDO3:
+		reg = SGM38121_LDO3_VOUT;
+		ret = regmap_read(sgm38121->regmap, reg, &reg_val);
+		if (reg_val < ET5904_AVDD_MIN_REG_VAL) reg_val =  ET5904_AVDD_MIN_REG_VAL;
+		else if (reg_val > ET5904_AVDD_MAX_REG_VAL) reg_val =  ET5904_AVDD_MAX_REG_VAL;
+		*value = reg_val * ET5904_AVDD_STEP_UV + ET5904_AVDD_MIN_UV;
+		break;
+	case SGM38121_REGULATOR_LDO4:
+		reg = SGM38121_LDO4_VOUT;
+		ret = regmap_read(sgm38121->regmap, reg, &reg_val);
+		if (reg_val < ET5904_AVDD_MIN_REG_VAL) reg_val =  ET5904_AVDD_MIN_REG_VAL;
+		else if (reg_val > ET5904_AVDD_MAX_REG_VAL) reg_val =  ET5904_AVDD_MAX_REG_VAL;
+		*value = reg_val * ET5904_AVDD_STEP_UV + ET5904_AVDD_MIN_UV;
+		break;
+	default:
+		break;
+	}
+	dev_info(sgm38121->dev,"get ldo:%d, voltage = %d",channel+ 1, *value);
+	return ret;
+}
+
 static int sgm38121_regulator_set_voltage(struct regulator_dev *rdev, int min_uV, int max_uV, unsigned int *selector)
 {
 	struct sgm38121 *chip = rdev_get_drvdata(rdev);
@@ -295,6 +467,30 @@ static int sgm38121_regulator_set_voltage(struct regulator_dev *rdev, int min_uV
 	}
 	ret = sgm38121_output_voltage_set(chip, index, max_uV/1000);
 	dev_info(chip->dev,"sgm38121_regulaton_set_voltage: Mv:%d,index:%d\n", max_uV/1000,index);
+
+	return ret;
+}
+
+static int et5904_regulator_set_voltage(struct regulator_dev *rdev, int min_uV, int max_uV, unsigned int *selector)
+{
+	struct sgm38121 *chip = rdev_get_drvdata(rdev);
+	int ret = 0;
+	int index = 0;
+	dev_info(chip->dev,"et5904_regulaton_set_voltage: name = %s\n", rdev->desc->name);
+
+	if (0 == strcmp(rdev->desc->name,"ldo1")) {
+		index = 0;
+	} else if (0 == strcmp(rdev->desc->name,"ldo2")) {
+		index = 1;
+	}else if (0 == strcmp(rdev->desc->name,"ldo3")) {
+		index = 2;
+	}else if (0 == strcmp(rdev->desc->name,"ldo4")) {
+		index = 3;
+	} else {
+		dev_err(chip->dev,"et5904_regulaton_set_voltage: name error.\n");
+	}
+	ret = et5904_output_voltage_set(chip, index, max_uV);
+	dev_info(chip->dev,"et5904_regulaton_set_voltage: uV:%d,index:%d\n", max_uV ,index);
 
 	return ret;
 }
@@ -322,6 +518,31 @@ static int sgm38121_regulator_get_voltage(struct regulator_dev *rdev)
 					chip->ldo_output[index], rdev->desc->name);
 	return chip->ldo_output[index] * 1000;
 }
+
+static int et5904_regulator_get_voltage(struct regulator_dev *rdev)
+{
+	struct sgm38121 *chip = rdev_get_drvdata(rdev);
+	int ret = 0;
+	int index = 0;
+	if (0 == strcmp(rdev->desc->name,"ldo1")) {
+		index = 0;
+	} else if (0 == strcmp(rdev->desc->name,"ldo2")) {
+		index = 1;
+	}else if (0 == strcmp(rdev->desc->name,"ldo3")) {
+		index = 2;
+	}else if (0 == strcmp(rdev->desc->name,"ldo4")) {
+		index = 3;
+	} else {
+		dev_err(chip->dev,"et5904_regulaton_get_voltage: name error.\n");
+	}
+	ret = et5904_output_voltage_get(chip,index, &chip->ldo_output[index]);
+	if (ret < 0)
+		return -1;
+	dev_info(chip->dev,"et5904_regulaton_get_voltage:voltage = %d, name = %s\n",
+					chip->ldo_output[index], rdev->desc->name);
+	return chip->ldo_output[index];
+}
+
 static const struct regulator_ops sgm38121_regl_ops = {
 	.enable = regulator_enable_regmap,
 	.disable = regulator_disable_regmap,
@@ -334,6 +555,20 @@ static const struct regulator_ops sgm38121_regl_ops = {
 	.get_current_limit = sgm38121_get_current_limit,
 	.set_voltage = sgm38121_regulator_set_voltage,
 	.get_voltage = sgm38121_regulator_get_voltage,
+};
+
+static const struct regulator_ops et5904_regl_ops = {
+	.enable = regulator_enable_regmap,
+	.disable = regulator_disable_regmap,
+	.is_enabled = regulator_is_enabled_regmap,
+	.list_voltage = regulator_list_voltage_linear,
+	.map_voltage = regulator_map_voltage_linear,
+	.get_voltage_sel = regulator_get_voltage_sel_regmap,
+	.set_voltage_sel = regulator_set_voltage_sel_regmap,
+	.get_status = et5904_get_status,
+	.get_current_limit = et5904_get_current_limit,
+	.set_voltage = et5904_regulator_set_voltage,
+	.get_voltage = et5904_regulator_get_voltage,
 };
 
 static int sgm38121_of_parse_cb(struct device_node *np,
@@ -377,6 +612,35 @@ static struct regulator_desc sgm38121_regls_desc[SGM38121_MAX_REGULATORS] = {
 	SGM38121_REGL_DESC(LDO4, ldo4, "vin2", 1200000, 12500),
 };
 
+
+#define ET5904_REGL_DESC(_id, _name, _s_name, _min, _step)       \
+	[SGM38121_REGULATOR_##_id] = {                             \
+		.name = #_name,                                    \
+		.supply_name = _s_name,                            \
+		.id = SGM38121_REGULATOR_##_id,                    \
+		.of_match = of_match_ptr(#_name),                  \
+		.of_parse_cb = sgm38121_of_parse_cb,               \
+		.ops = &et5904_regl_ops,                         \
+		.regulators_node = of_match_ptr("regulators"),     \
+		.n_voltages = SGM38121_N_VOLTAGES,                  \
+		.min_uV = _min,                                    \
+		.uV_step = _step,                                  \
+		.linear_min_sel = 0,                               \
+		.vsel_mask = SGM38121_VSEL_MASK,                   \
+		.vsel_reg = SGM38121_##_id##_VSEL,                 \
+		.enable_reg = SGM38121_LDO_EN,       \
+		.enable_mask = BIT(SGM38121_REGULATOR_##_id),      \
+		.type = REGULATOR_VOLTAGE,                         \
+		.owner = THIS_MODULE,                              \
+	}
+
+static struct regulator_desc et5904_regls_desc[SGM38121_MAX_REGULATORS] = {
+	ET5904_REGL_DESC(LDO1, ldo1, "vin1", 600000, 6000),
+	ET5904_REGL_DESC(LDO2, ldo2, "vin1", 600000, 6000),
+	ET5904_REGL_DESC(LDO3, ldo3, "vin2", 1200000, 12500),
+	ET5904_REGL_DESC(LDO4, ldo4, "vin2", 1200000, 12500),
+};
+
 static int sgm38121_regulator_init(struct sgm38121 *chip)
 {
 	struct regulator_config config = { };
@@ -404,7 +668,10 @@ static int sgm38121_regulator_init(struct sgm38121 *chip)
 		return ret;
 	}
 	for (id = 0; id < SGM38121_MAX_REGULATORS; id++) {
-		chip->rdesc[id] = &sgm38121_regls_desc[id];
+		if (ldo_chipid == SGM38121_ID)
+			chip->rdesc[id] = &sgm38121_regls_desc[id];
+		else if ((ldo_chipid == ET5904_ID) || (ldo_chipid == ET5924A_ID))
+			chip->rdesc[id] = &et5904_regls_desc[id];
 		rdesc = chip->rdesc[id];
 		config.regmap = chip->regmap;
 		config.dev = chip->dev;
@@ -519,6 +786,10 @@ static int sgm38121_i2c_probe(struct i2c_client *client,
 	} else {
 		if (ldo_chipid == SGM38121_ID) {
 			dev_info(chip->dev, "SGM38121 CHIP ID matched!\n");
+		} else if (ldo_chipid == ET5904_ID) {
+			dev_info(chip->dev, "ET5904 CHIP ID matched!\n");
+		} else if (ldo_chipid == ET5924A_ID) {
+			dev_info(chip->dev, "ET5924A CHIP ID matched!\n");
 		} else {
 			dev_err(dev, "Failed to read other CHIP ID:0x%x, ret:%d\n", ldo_chipid,ret);
 			ret = -ENODEV;
@@ -537,12 +808,17 @@ static int sgm38121_i2c_probe(struct i2c_client *client,
 		dev_err(chip->dev, "Failed to init regulator(%d)\n", ret);
 		return ret;
 	}
-	regmap_write(chip->regmap, SGM38121_LDO1_VOUT, 0x4B);
-	regmap_write(chip->regmap, SGM38121_LDO2_VOUT, 0x57);
-	regmap_write(chip->regmap, SGM38121_LDO3_VOUT, 0xB1);
-	regmap_write(chip->regmap, SGM38121_LDO4_VOUT, 0xB1);
 
-	sgm38121_get_current_limit(chip->rdev[0]);
+	if (ldo_chipid == SGM38121_ID) {
+		regmap_write(chip->regmap, SGM38121_LDO1_VOUT, 0x4B);
+		regmap_write(chip->regmap, SGM38121_LDO2_VOUT, 0x57);
+		regmap_write(chip->regmap, SGM38121_LDO3_VOUT, 0xB1);
+		regmap_write(chip->regmap, SGM38121_LDO4_VOUT, 0xB1);
+		sgm38121_get_current_limit(chip->rdev[0]);
+	} else if ((ldo_chipid == ET5904_ID) || (ldo_chipid == ET5924A_ID)){
+		et5904_get_current_limit(chip->rdev[0]);
+	}
+
 	dev_info(chip->dev, "sgm38121_i2c_probe Exit...\n");
 	return ret;
 }
