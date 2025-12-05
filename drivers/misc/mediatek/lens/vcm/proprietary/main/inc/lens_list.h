4872ffffa509c5ee7bbc14c555e4ca2cd5425854
Chen yulong <chenyl01@motorola.com>
2025-12-05 16:57:19 +0800

    sydney: drv: s5khm9 af gt9764 bring up 1/2
    sydnyl:
     --Add s5khm9 af gt9754 bring up code
     --Add Pinctrl configuration for cam0 AF module GPIO35 and enable camera_af_hw_node
    
    Change-Id: I4fc6d8f037f6ff6b8099b9235040eee42ae4a29c
    Reviewed-on: https://gerrit.mot.com/3669045
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Simon C <chenzm8@motorola.com>
    Tested-by: Jira Key
    Reviewed-by: Guan Xin <guanxin6@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lens/vcm/proprietary/main/inc/lens_list.h
+++ drivers/misc/mediatek/lens/vcm/proprietary/main/inc/lens_list.h
@@ -523,6 +523,22 @@ extern int MOT_SYDNEY_GT9778WAF_PowerDown(struct i2c_client *pstAF_I2Cclient,
 extern int MOT_SYDNEY_GT9778WAF_GetFileName(unsigned char *pFileName);
 //End
 
+//Begin: Add lens driver interface for SYDNEY
+#define MOT_SYDNEY_GT9764_SetI2CClient MOT_SYDNEY_GT9764AF_SetI2Cclient
+#define MOT_SYDNEY_GT9764_Ioctl MOT_SYDNEY_GT9764AF_Ioctl
+#define MOT_SYDNEY_GT9764_Release MOT_SYDNEY_GT9764AF_Release
+#define MOT_SYDNEY_GT9764_PowerDown MOT_SYDNEY_GT9764AF_PowerDown
+#define MOT_SYDNEY_GT9764_GetFileName MOT_SYDNEY_GT9764AF_GetFileName
+extern int MOT_SYDNEY_GT9764AF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+				spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long MOT_SYDNEY_GT9764AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+				unsigned long a_u4Param);
+extern int MOT_SYDNEY_GT9764AF_Release(struct inode *a_pstInode, struct file *a_pstFile);
+extern int MOT_SYDNEY_GT9764AF_PowerDown(struct i2c_client *pstAF_I2Cclient,
+				int *pAF_Opened);
+extern int MOT_SYDNEY_GT9764AF_GetFileName(unsigned char *pFileName);
+//End
+
 //Begin: Add lens driver interface for Naples
 #define MOT_NAPLES_GT9764_SetI2CClient MOT_NAPLES_GT9764AF_SetI2Cclient
 #define MOT_NAPLES_GT9764_Ioctl MOT_NAPLES_GT9764AF_Ioctl
