1af1d8f5118724f29b3d0e67b39a8fd03f0477cf
daiyali <yali.dai@chino-e.com>
2025-12-16 15:21:33 +0800

    EKmoto_g06-612: bsp: add BUILD_SMT BUILD_D85 VER_TAG -- kernel

---
+++ include/linux/kasan-tags.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_KASAN_TAGS_H
+#define _LINUX_KASAN_TAGS_H
+
+#define KASAN_TAG_KERNEL	0xFF /* native kernel pointers tag */
+#define KASAN_TAG_INVALID	0xFE /* inaccessible memory tag */
+#define KASAN_TAG_MAX		0xFD /* maximum value for random tags */
+
+#ifdef CONFIG_KASAN_HW_TAGS
+#ifdef CONFIG_MTK_MTE_DEBUG
+#define KASAN_TAG_MIN		0xF1 /* minimum value for random tags */
+#else
+#define KASAN_TAG_MIN		0xF0 /* minimum value for random tags */
+#endif
+#else
+#define KASAN_TAG_MIN		0x00 /* minimum value for random tags */
+#endif
+
+#endif /* LINUX_KASAN_TAGS_H */
