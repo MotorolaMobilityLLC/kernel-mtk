1ed6a64dd315dd7a65be35038ce649367e5a364d
xiaorn ni <nixr0616@motorola.com>
2025-12-17 11:03:09 +0800

    sydney: drv: sc821cs awb calc and mnf data(1/2)
    sydney:
    --sc821cs awb calc and mnf data
    
    Change-Id: I2d88caf6a54e9c50814cd2bf98d1c72c95c9cf0f
    Reviewed-on: https://gerrit.mot.com/3685101
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: <guojx7@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_sydney_sc821cs_mipi_raw/mot_sydney_sc821cs_mipi_raw_otp.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/mot_sydney_sc821cs_mipi_raw/mot_sydney_sc821cs_mipi_raw_otp.c
@@ -52,12 +52,12 @@ static void SYDNEY_SC821CS_eeprom_get_mnf_data(void *data,
 	uint8_t* module_info = data;
     	// lens_id
 	struct SYDNEY_SC821CS_eeprom_t eeprom = {
-        	.lens_id = module_info[10],
+        	.lens_id = module_info[12],
     	};
 
 	LOG_INF("eeprom.lens_id:0x%x", eeprom.lens_id);
 	if (eeprom.lens_id == 0x01){
-		ret = snprintf(mnf->lens_id, MAX_CALIBRATION_STRING, "HX-M0846A");
+		ret = snprintf(mnf->lens_id, MAX_CALIBRATION_STRING, "S0892A");
 	} else {
 		ret = snprintf(mnf->lens_id, MAX_CALIBRATION_STRING, "Unknown");
 		LOG_INF("unknown lens_id");
