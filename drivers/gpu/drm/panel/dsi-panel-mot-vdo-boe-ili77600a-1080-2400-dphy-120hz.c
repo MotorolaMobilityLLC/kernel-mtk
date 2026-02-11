ca1c913d437c3e11b7221d355efa918f7e83708b
maolinli <mallhq@motorola.com>
2026-02-11 15:13:23 +0800

    [Sydney & Sydnyl] IL(CR) IC LCD reset mipi issues cmd time modification[1/2]
    Reason:
    The time for LCD reset mipi to issue cmd is less than 10ms
    
    Solution:
    Modify LCD reset mipi to issue cmd with a time of 10ms
    
    Test:
    local test passed
    
    Change-Id: If436662593065859fe6b39117b90d602d061a2d0
    Signed-off-by: hq <gaoyuehq@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3765581
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yongchao Ma <mayc10@motorola.com>
    Reviewed-by: Hua Tan <tanhua1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/panel/dsi-panel-mot-vdo-boe-ili77600a-1080-2400-dphy-120hz.c
+++ drivers/gpu/drm/panel/dsi-panel-mot-vdo-boe-ili77600a-1080-2400-dphy-120hz.c
@@ -250,7 +250,7 @@ static void boe_ili77600a_panel_init(struct boe_ili77600a *ctx)
 		gpiod_set_value(ctx->reset_gpio, 0);
 		usleep_range(10 * 1000, 12 * 1000);
 		gpiod_set_value(ctx->reset_gpio, 1);
-		usleep_range(5000, 7000);
+		usleep_range(10000, 12000);
 		devm_gpiod_put(ctx->dev, ctx->reset_gpio);
 		pr_info("disp: %s reset_gpio\n", __func__);
 	}
